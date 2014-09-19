9f1db3f2bcf67f49c9b145e1d1e8f090220372df
Misko Hevery <misko@hevery.com>
2014-09-19 21:55:37 +0000

    chore: correct postinstall.sh script

--- postinstall.sh
+++ postinstall.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
+git submodule init && git submodule update
+
 rm node_modules/traceur
 ln -s ../tools/traceur node_modules/traceur
 
 rm node_modules/js2dart
-ln -s ../tools/j2dart node_modules/js2dart
+ln -s ../tools/js2dart node_modules/js2dart
 
-cd tools/traceur
-npm install
+(cd tools/traceur; npm install)
 
-cd ../js2dart
-npm install
+(cd tools/js2dart; npm install)
