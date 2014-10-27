c11ca944d4e2b168bbead1486f73ccf466f594f8
vsavkin <vic.savkin@gmail.com>
2014-10-27 16:09:10 -0400

    perf(ElementInjector): add a benchmark measuring the instantiation of element injectors without using reflection

--- modules/benchmarks/src/element_injector/benchmark.dart
+++ modules/benchmarks/src/element_injector/benchmark.dart
@@ -1,11 +1,23 @@
 library element_injector_benchmark;
 
 import './instantiate_benchmark.dart' as ib;
+import './instantiate_benchmark_codegen.dart' as ibc;
+import './instantiate_directive_benchmark.dart' as idb;
 import 'dart:js' as js;
 
 main () {
   js.context['benchmarkSteps'].add(new js.JsObject.jsify({
       "name": "ElementInjector.instantiate + instantiateDirectives",
       "fn": new js.JsFunction.withThis((_) => ib.run())
+  }));
+
+  js.context['benchmarkSteps'].add(new js.JsObject.jsify({
+      "name": "ElementInjector.instantiateDirectives",
+      "fn": new js.JsFunction.withThis((_) => idb.run())
+  }));
+
+  js.context['benchmarkSteps'].add(new js.JsObject.jsify({
+      "name": "ElementInjector.instantiate + instantiateDirectives (codegen)",
+      "fn": new js.JsFunction.withThis((_) => ibc.run())
   }));
 }
--- modules/benchmarks/src/element_injector/instantiate_benchmark_codegen.js
+++ modules/benchmarks/src/element_injector/instantiate_benchmark_codegen.js
@@ -1,4 +1,4 @@
-import {Injector} from 'di/di';
+import {Binding, Dependency, Key, Injector} from 'di/di';
 import {ProtoElementInjector} from 'core/compiler/element_injector';
 
 var ITERATIONS = 20000;
@@ -6,8 +6,16 @@ var count = 0;
 
 export function run () {
   var appInjector = new Injector([]);
+  
+  var bindings = [
+    new Binding(Key.get(A), () => new A(), [], false),
+    new Binding(Key.get(B), () => new B(), [], false),
+    new Binding(Key.get(C), (a,b) => new C(a,b), [
+      new Dependency(Key.get(A), false, false, []),
+      new Dependency(Key.get(B), false, false, [])
+    ], false)];
 
-  var bindings = [A, B, C];
+
   var proto = new ProtoElementInjector(null, bindings, []);
   for (var i = 0; i < ITERATIONS; ++i) {
     var ei = proto.instantiate({view:null});
