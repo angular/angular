aa74be134aee57ab5f135cac6a32540538dfa050
Tobias Bosch <tbosch1009@gmail.com>
2014-11-17 11:01:21 -0800

    refactor(benchmark): simplify writing benchmarks
    Adds `benchmarks/benchpress` module and adjusts the compiler
    benchmarks to use it. Also adds the Angular 1.3 benchmark
    to the compiler benchmarks.
    
    Closes #202

--- modules/benchmarks/src/compiler/benchmark.dart
+++ modules/benchmarks/src/compiler/benchmark.dart
@@ -2,35 +2,8 @@ library compiler_benchmark;
 
 import './selector_benchmark.dart' as sbm;
 import './compiler_benchmark.dart' as cbm;
-import 'dart:js' as js;
 
 main () {
-  sbm.setup();
-  cbm.setup();
-
-  js.context['benchmarkSteps'].add(new js.JsObject.jsify({
-      "name": "CssSelector.parse * 1000",
-      "fn": new js.JsFunction.withThis((_) => sbm.runParse())
-  }));
-  js.context['benchmarkSteps'].add(new js.JsObject.jsify({
-      "name": "SelectorMatcher.addSelectable * 1000",
-      "fn": new js.JsFunction.withThis((_) => sbm.runAdd())
-  }));
-  js.context['benchmarkSteps'].add(new js.JsObject.jsify({
-      "name": "SelectorMatcher.match * 1000",
-      "fn": new js.JsFunction.withThis((_) => sbm.runMatch())
-  }));
-
-  js.context['benchmarkSteps'].add(new js.JsObject.jsify({
-      "name": "Compiler.compile empty template",
-      "fn": new js.JsFunction.withThis((_) => cbm.compileEmptyTemplate())
-  }));
-  js.context['benchmarkSteps'].add(new js.JsObject.jsify({
-      "name": "Compiler.compile 25 element no bindings",
-      "fn": new js.JsFunction.withThis((_) => cbm.compile25ElementsNoBindings())
-  }));
-  js.context['benchmarkSteps'].add(new js.JsObject.jsify({
-      "name": "Compiler.compile 25 element with bindings",
-      "fn": new js.JsFunction.withThis((_) => cbm.compile25ElementsWithBindings())
-  }));
+  sbm.main();
+  cbm.main();
 }
