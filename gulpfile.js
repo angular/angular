a14ed45a5635826907f03028a5b1ed81163659d0
Peter Bacon Darwin <pete@bacondarwin.com>
2014-11-25 14:25:59 +0000

    chore(package.json): add dgeni related libraries

--- gulpfile.js
+++ gulpfile.js
@@ -399,3 +399,18 @@ gulp.task('build', function(done) {
 gulp.task('analyze', function(done) {
   runSequence('analyze/analyzer.dart');
 });
+
+
+// --------------
+// doc generation
+var Dgeni = require('dgeni');
+gulp.task('dgeni', function() {
+  try {
+    var dgeni = new Dgeni([require('./docs')]);
+    return dgeni.generate();
+  } catch(x) {
+    console.log(x.stack);
+    throw x;
+  }
+});
+
