0cba16508ba862b9674af05dd9ad7610daf7778b
Peter Bacon Darwin <pete@bacondarwin.com>
2014-11-27 10:49:53 +0000

    chore(docs): refactor parsing into a service and add tests)

--- docs/index.js
+++ docs/index.js
@@ -3,29 +3,41 @@ var jsdocPackage = require('dgeni-packages/jsdoc');
 var nunjucksPackage = require('dgeni-packages/nunjucks');
 var path = require('canonical-path');
 
+// Define the dgeni package for generating the docs
 module.exports = new Package('angular', [jsdocPackage, nunjucksPackage])
 
+// Register the services and file readers
+.factory(require('./services/atParser'))
 .factory(require('./readers/atScript'))
 
+
+// Register the processors
 .processor(require('./processors/generateDocsFromComments'))
 
+
+// Configure the log service
 .config(function(log) {
   log.level = 'debug';
 })
 
+
+// Configure file reading
 .config(function(readFilesProcessor, atScriptFileReader) {
   readFilesProcessor.fileReaders = [atScriptFileReader];
   readFilesProcessor.basePath = path.resolve(__dirname, '..');
   readFilesProcessor.sourceFiles = [
-    { module: 'di', include: 'modules/di/src/annotations.js', basePath: 'modules/di/src' }
+    { module: 'di', include: 'modules/di/src/annotations.js', basePath: 'modules' }
   ];
 })
 
+
+// Configure file writing
 .config(function(writeFilesProcessor) {
   writeFilesProcessor.outputFolder  = 'build/docs';
 })
 
 
+// Configure rendering
 .config(function(templateFinder, templateEngine) {
 
   // Nunjucks and Angular conflict in their template bindings so change Nunjucks
@@ -47,7 +59,14 @@ module.exports = new Package('angular', [jsdocPackage, nunjucksPackage])
 })
 
 
-.config(function(computePathsProcessor) {
+// Configure ids and paths
+.config(function(computeIdsProcessor, computePathsProcessor) {
+
+  computeIdsProcessor.idTemplates.push({
+    docTypes: ['module'],
+    idTemplate: '${moduleName}'
+  });
+
   computePathsProcessor.pathTemplates.push({
     docTypes: ['atScriptDoc'],
     outputPathTemplate: '${docType}-${startingLine}.html'
--- docs/readers/atScript.js
+++ docs/readers/atScript.js
@@ -1,14 +1,4 @@
-var _ = require('lodash');
-var traceur = require('traceur/src/node/traceur.js');
-
-traceur.options.setFromObject({
-  annotations: true, // parse annotations
-  types: true, // parse types
-  script: false, // parse as a module
-  memberVariables: true, // parse class fields
-  modules: 'instantiate',
-  commentCallback: true
-});
+var path = require('canonical-path');
 
 /**
  * @dgService atScriptFileReader
@@ -16,65 +6,22 @@ traceur.options.setFromObject({
  * This file reader will create a simple doc for each
  * file including a code AST of the AtScript in the file.
  */
-module.exports = function atScriptFileReader(log) {
-  return {
+module.exports = function atScriptFileReader(log, atParser) {
+  var reader = {
     name: 'atScriptFileReader',
     defaultPattern: /\.js$/,
     getDocs: function(fileInfo) {
 
-      try {
+      var moduleDoc = atParser.parseModule(fileInfo);
+      moduleDoc.docType = 'module';
 
-        parseFile(fileInfo);
-
-      } catch(ex) {
-        log.error(ex.stack);
-        throw ex;
-      }
-
-      return [{
-        docType: 'atScriptFile'
-      }];
+      // Readers return a collection of docs read from the file
+      // but in this read there is only one document (module) to return
+      return [moduleDoc];
     }
   };
 
-  function parseFile(fileInfo) {
+  return reader;
 
-    var sourceFile = new traceur.syntax.SourceFile(fileInfo.relativePath, fileInfo.content);
-    var parser = new traceur.syntax.Parser(sourceFile);
-
-    parser.handleComment = function(range) {
-      fileInfo.comments.push({ range: range });
-    };
-
-    fileInfo.comments = [];
-    fileInfo.codeTree = parser.parseModule();
-    attachComments(fileInfo.codeTree, fileInfo.comments);
-  }
-
-  function attachComments(tree, comments) {
-
-    var ParseTreeVisitor = System.get("traceur@0.0.74/src/syntax/ParseTreeVisitor").ParseTreeVisitor;
-
-    var visitor = new ParseTreeVisitor();
-    var index = 0;
-    var currentComment = comments[index];
-
-    if (currentComment) log.silly('comment: ' + currentComment.range.start.line + ' - ' + currentComment.range.end.line);
-
-    visitor.visitAny = function(tree) {
-      if (tree && tree.location && currentComment) {
-        if (currentComment.range.end.offset < tree.location.start.offset) {
-          log.silly('tree: ' + tree.constructor.name + ' - ' + tree.location.start.line);
-          tree.commentBefore = currentComment;
-          currentComment.treeAfter = tree;
-          index++;
-          currentComment = comments[index];
-          if (currentComment) log.silly('comment: ' + currentComment.range.start.line + ' - ' + currentComment.range.end.line);
-        }
-      }
-      return ParseTreeVisitor.prototype.visitAny.call(this, tree);
-    };
-    visitor.visitAny(tree);
-  }
 
 };
