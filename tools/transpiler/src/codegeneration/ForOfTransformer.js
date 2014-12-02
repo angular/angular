40b6939ce9867b5a23944d7c2787d54ac9a120cf
Tommy Odom <tommy.odom@gmail.com>
2014-12-02 07:07:05 -0500

    feat(transpiler): Removed comments that don't apply

--- tools/transpiler/src/codegeneration/ForOfTransformer.js
+++ tools/transpiler/src/codegeneration/ForOfTransformer.js
@@ -3,9 +3,6 @@ import {ForInStatement} from 'traceur/src/syntax/trees/ParseTrees';
 
 /**
  * Transforms for-of into for-in.
- * If the initializer is an array pattern then a temp variable is assigned
- * to the result of the iterator and the destructuring pattern set up within
- * the loop.
  */
 export class ForOfTransformer extends ParseTreeTransformer {
   /**
