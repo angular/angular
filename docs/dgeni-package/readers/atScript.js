6d0e090fb35eca53766f083c77b3750cf97c896c
Peter Bacon Darwin <pete@bacondarwin.com>
2014-12-04 14:02:03 +0000

    chore(docs): remove unnecessary clever aliasing of module ids

--- docs/dgeni-package/readers/atScript.js
+++ docs/dgeni-package/readers/atScript.js
@@ -1,20 +1,5 @@
 var path = require('canonical-path');
 
-// This creates aliases by pulling off each path segment in turn:
-// "a/b/c" will have aliases ["a/b/c", "b/c", "c"]
-// @rado - IS THIS WHAT WE WANT OR ARE MODULE NAMES NOT RELATIVE LIKE THIS?
-function getModuleAliases(doc) {
-  var aliases = [];
-
-  if ( !doc.id ) return [];
-
-  var parts = doc.id.split('/');
-  while(parts.length) {
-    aliases.push(parts.join('/'));
-    parts.shift();
-  }
-  return aliases;
-}
 
 /**
  * @dgService atScriptFileReader
@@ -31,7 +16,7 @@ module.exports = function atScriptFileReader(log, atParser) {
       var moduleDoc = atParser.parseModule(fileInfo);
       moduleDoc.docType = 'module';
       moduleDoc.id = moduleDoc.moduleTree.moduleName;
-      moduleDoc.aliases = getModuleAliases(moduleDoc);
+      moduleDoc.aliases = [moduleDoc.id];
 
       // Readers return a collection of docs read from the file
       // but in this read there is only one document (module) to return
