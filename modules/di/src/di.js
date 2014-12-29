e5dbc69f42a803519ecb11ea8ef986599589bb7c
vsavkin <vic.savkin@gmail.com>
2014-12-29 15:00:33 -0800

    cleanup(di): change di.js to declare all exported symbols

--- modules/di/src/di.js
+++ modules/di/src/di.js
@@ -1,7 +1,7 @@
-export * from './annotations';
-export * from './injector';
-export * from './binding';
-export * from './key';
-export * from './module';
-export * from './exceptions';
-export * from './opaque_token';
+export {Inject, InjectPromise, InjectLazy, DependencyAnnotation} from './annotations';
+export {Injector} from './injector';
+export {Binding, Dependency, bind} from './binding';
+export {Key, KeyRegistry} from './key';
+export {KeyMetadataError, NoProviderError, ProviderError, AsyncBindingError, CyclicDependencyError,
+  InstantiationError, InvalidBindingError, NoAnnotationError} from './exceptions';
+export {OpaqueToken} from './opaque_token';
