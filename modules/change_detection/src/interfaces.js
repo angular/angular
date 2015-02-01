9a526be2c77c9ac6a3698ef8c94f0e1666000a7c
vsavkin <vic.savkin@gmail.com>
2015-02-01 12:09:35 -0800

    feat(change_detection): add mode to ChangeDetector

--- modules/change_detection/src/interfaces.js
+++ modules/change_detection/src/interfaces.js
@@ -19,12 +19,38 @@ export class ChangeRecord {
   }
 }
 
+
+/**
+ * CHECK_ONCE means that after calling detectChanges the mode of the change detector
+ * will become CHECKED.
+ */
+export const CHECK_ONCE="CHECK_ONCE";
+
+/**
+ * CHECKED means that the change detector should be skipped until its mode changes to
+ * CHECK_ONCE or CHECK_ALWAYS.
+ */
+export const CHECKED="CHECKED";
+
+/**
+ * CHECK_ALWAYS means that after calling detectChanges the mode of the change detector
+ * will remain CHECK_ALWAYS.
+ */
+export const CHECK_ALWAYS="ALWAYS_CHECK";
+
+/**
+ * DETACHED means that the change detector sub tree is not a part of the main tree and
+ * should be skipped.
+ */
+export const DETACHED="DETACHED";
+
 export class ChangeDispatcher {
   onRecordChange(groupMemento, records:List<ChangeRecord>) {}
 }
 
 export class ChangeDetector {
   parent:ChangeDetector;
+  mode:string;
 
   addChild(cd:ChangeDetector) {}
   removeChild(cd:ChangeDetector) {}
