93c18f5396fabd284038fc45c25f5b3120793830
Alex Eagle <alexeagle@google.com>
2015-02-04 16:40:04 -0800

    fix(build): Escape dollar signs in dart-transpiled string literals
    Escape dollar signs in string literals - dart should not interpolate them.
    Closes #509

--- tools/transpiler/unittest/transpilertests.js
+++ tools/transpiler/unittest/transpilertests.js
@@ -13,15 +13,38 @@ var OPTIONS = {
 describe('transpile to dart', function(){
 
   // https://github.com/angular/angular/issues/509
-  it('should not interpolate inside old quotes', function(){
-    var result = compiler.compile(OPTIONS, "test.js",
-      "var a = 1;" +
-      "var s1 = '${a}';" +
-      "var s2 = `${a}`;");
-    expect(result.js).toBe("library test;\n" +
-      "var a = 1;\n" +
-        // FIXME: this should escape the interpolation with backslash to fix the issue
-      "var s1 = '${a}';\n" +
-      "var s2 = '''${a}''';\n");
-  })
+  describe('string interpolation', function() {
+    it('should not interpolate inside old quotes', function(){
+      var result = compiler.compile(OPTIONS, "test.js",
+        "var a:number = 1;" +
+        "var s1:string = \"${a}\";" +
+        "var s2:string = '\\${a}';" +
+        "var s3:string = '$a';");
+      expect(result.js).toBe("library test;\n" +
+      "num a = 1;\n" +
+      "String s1 = \"\\${a}\";\n" +
+      "String s2 = '\\${a}';\n" +
+      "String s3 = '\\$a';\n");
+    });
+
+    it('should not interpolate without curly braces', function() {
+      var result = compiler.compile(OPTIONS, "test.js",
+        "var a:number = 1;" +
+        "var s1:string = `$a`;" +
+        "var s2:string = `\\$a`;");
+      expect(result.js).toBe("library test;\n" +
+      "num a = 1;\n" +
+      "String s1 = '''\\$a''';\n" +
+      "String s2 = '''\\$a''';\n");
+    });
+
+    it('should interpolate inside template quotes', function() {
+      var result = compiler.compile(OPTIONS, "test.js",
+        "var a:number = 1;" +
+        "var s1:string = `${a}`;");
+      expect(result.js).toBe("library test;\n" +
+      "num a = 1;\n" +
+      "String s1 = '''${a}''';\n");
+    });
+  });
 });
