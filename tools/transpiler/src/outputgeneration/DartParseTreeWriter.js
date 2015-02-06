fe672a1d92fcc222269ffc13952f70b22eac7e91
Alex Eagle <alexeagle@google.com>
2015-02-05 16:24:01 -0800

    add more string interpolation cases

--- tools/transpiler/src/outputgeneration/DartParseTreeWriter.js
+++ tools/transpiler/src/outputgeneration/DartParseTreeWriter.js
@@ -99,9 +99,15 @@ export class DartParseTreeWriter extends JavaScriptParseTreeWriter {
   }
 
   visitTemplateLiteralPortion(tree) {
-    this.writeRaw_(tree.value.toString().replace(/('|")/g, "\\$&"));
+    this.writeRaw_(tree.value.toString()
+      .replace(/('|")/g, "\\$&")
+      .replace(/([^\\])\$/g, "$1\\\$")
+      .replace(/^\$/, '\\\$'));
   }
 
+  visitLiteralExpression(tree) {
+    this.write_(('' + tree.literalToken).replace(/([^\\])\$/g, "$1\\\$"));
+  }
 
   // FUNCTIONS
   // - remove the "function" keyword
@@ -442,12 +448,6 @@ export class DartParseTreeWriter extends JavaScriptParseTreeWriter {
       this.write_('const');
     }
     super.visitArrayLiteralExpression(tree);
-  }
-
-  visitLiteralExpression(tree) {
-    // JavaScript regex doesn't support lookbehind so eat a char before
-    // dollar and then print it in the replacement
-    this.write_(('' + tree.literalToken).replace(/([^\\])\$/g, "$1\\\$"));
   }
 
   visitNewExpression(tree) {
--- tools/transpiler/unittest/transpilertests.js
+++ tools/transpiler/unittest/transpilertests.js
@@ -1,4 +1,3 @@
-var assert = require("assert");
 var compiler = require('../index');
 
 // fixme: copied from top of gulpfile
@@ -18,12 +17,32 @@ describe('transpile to dart', function(){
     var result = compiler.compile(OPTIONS, "test.js",
       "var a:number = 1;" +
       "var s1:string = '${a}';" +
-      "var s2:string = `${a}`;" +
-      "var s3:string = '\\${a}';");
+      "var s2:string = '\\${a}';" +
+      "var s3:string = '$a';");
     expect(result.js).toBe("library test;\n" +
       "num a = 1;\n" +
       "String s1 = '\\${a}';\n" +
-      "String s2 = '''${a}''';\n" +
-      "String s3 = '\\${a}';\n");
-  })
+      "String s2 = '\\${a}';\n" +
+      "String s3 = '\\$a';\n");
+  });
+
+  it('should not interpolate without curly braces', function() {
+    var result = compiler.compile(OPTIONS, "test.js",
+      "var a:number = 1;" +
+      "var s1:string = `$a`;" +
+      "var s2:string = `\\$a`;");
+    expect(result.js).toBe("library test;\n" +
+    "num a = 1;\n" +
+    "String s1 = '''\\$a''';\n" +
+    "String s2 = '''\\$a''';\n");
+  });
+
+  it('should interpolate inside template quotes', function() {
+    var result = compiler.compile(OPTIONS, "test.js",
+      "var a:number = 1;" +
+      "var s1:string = `${a}`;");
+    expect(result.js).toBe("library test;\n" +
+    "num a = 1;\n" +
+    "String s1 = '''${a}''';\n");
+  });
 });
