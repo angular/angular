cc813c7abe2e1eb73011f45654eadb8bfec0ad94
gdi2290 <github@gdi2290.com>
2015-02-08 22:36:19 -0800

    feat(directives): style decorator
    mvp style decorator that extends inline styles. There are a lot more
    features I can add (prefixer). I also need tests, but I want feedback
    first etc

--- modules/angular2/directives.js
+++ modules/angular2/directives.js
@@ -1,4 +1,5 @@
 export * from './src/directives/foreach';
 export * from './src/directives/if';
 export * from './src/directives/non_bindable';
+export * from './src/directives/style';
 export * from './src/directives/switch';
