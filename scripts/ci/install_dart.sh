c27e6a7af6394d414b919c1876bf05a38eb98a3d
Kevin Moore <kevmoo@google.com>
2015-02-12 15:00:43 -0800

    build against both stable and dev channels of Dart

--- scripts/ci/install_dart.sh
+++ scripts/ci/install_dart.sh
@@ -7,15 +7,7 @@ AVAILABLE_DART_VERSION=$(curl "https://storage.googleapis.com/dart-archive/chann
 
 echo Fetch Dart channel: $CHANNEL
 
-SVN_REVISION=latest
-# TODO(chirayu): Remove this once issue 20896 is fixed.
-# Dart 1.7.0-dev.1.0 and 1.7.0-dev.2.0 are both broken so use version
-# 1.7.0-dev.0.1 instead.
-if [[ "$AVAILABLE_DART_VERSION" == "1.7.0-dev.2.0" ]]; then
-  SVN_REVISION=39661  # Use version 1.7.0-dev.0.1
-fi
-
-URL_PREFIX=https://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/$SVN_REVISION
+URL_PREFIX=https://storage.googleapis.com/dart-archive/channels/$CHANNEL/release/latest
 DART_SDK_URL="$URL_PREFIX/sdk/dartsdk-$ARCH-release.zip"
 DARTIUM_URL="$URL_PREFIX/dartium/dartium-$ARCH-release.zip"
 
