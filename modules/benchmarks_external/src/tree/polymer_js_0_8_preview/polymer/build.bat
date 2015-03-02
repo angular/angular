9c5639c0c31876b725a43e8ad0fe78a2654739d6
Jacob MacDonald <jakemac@google.com>
2015-03-02 14:59:37 -0800

    add polymer_js as a git dependency

--- modules/benchmarks_external/src/tree/polymer_js_0_8_preview/polymer/build.bat
+++
@@ -1,2 +0,0 @@
-START /B vulcanize polymer-simplex.html --inline --strip -output dist/polymer-simplex.html
-START /B vulcanize polymer.html --inline  --strip -output dist/polymer.html
--- modules/benchmarks_external/src/tree/polymer_js_0_8_preview/polymer/wct.conf.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  suites: ['test/runner.html'], 
-};
