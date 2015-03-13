e9f70293ac33e5d0c5b03f0744ba7341deac5230
Rado Kirov <radokirov@google.com>
2015-03-13 11:39:15 -0700

    feat(query): adds initial implementation of the query api.
    Queries allow a directive to inject a live list of directives of a given
    type from its LightDom. The injected list is Iterable (in JS and Dart).
    It will be Observable when Observables are support in JS, for now it
    maintains a simple list of onChange callbacks API.
    
    To support queries, element injectors now maintain a list of
    child injectors in the correct DOM order (dynamically updated by
    viewports).
    
    For performance reasons we allow only 3 active queries in an injector
    subtree. The feature adds no overhead to the application when not
    used. Queries walk the injector tree only during dynamic view
    addition/removal as triggered by viewport directives.
    
    Syncs changes between viewContainer on the render and logic sides.
    
    Closes #792

---
+++ modules/angular2/src/core/compiler/query_list.dart
@@ -0,0 +1,47 @@
+library angular2.src.core.compiler.query_list;
+
+import 'package:angular2/src/core/annotations/annotations.dart';
+import 'dart:collection';
+
+/**
+ * Injectable Objects that contains a live list of child directives in the light Dom of a directive.
+ * The directives are kept in depth-first pre-order traversal of the DOM.
+ *
+ * In the future this class will implement an Observable interface.
+ * For now it uses a plain list of observable callbacks.
+ */
+class QueryList extends Object with IterableMixin<Directive> {
+  List<Directive> _results;
+  List _callbacks;
+  bool _dirty;
+
+  QueryList(): _results = [], _callbacks = [], _dirty = false;
+
+  Iterator<Directive> get iterator => _results.iterator;
+
+  reset(newList) {
+    _results = newList;
+    _dirty = true;
+  }
+
+  add(obj) {
+    _results.add(obj);
+    _dirty = true;
+  }
+
+  // TODO(rado): hook up with change detection after #995.
+  fireCallbacks() {
+    if (_dirty) {
+      _callbacks.forEach((c) => c());
+      _dirty = false;
+    }
+  }
+
+  onChange(callback) {
+    this._callbacks.add(callback);
+  }
+
+  removeCallback(callback) {
+    this._callbacks.remove(callback);
+  }
+}
