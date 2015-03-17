8b2fe48b3277d659476035206820433c7371effd
Peter Bacon Darwin <pete@bacondarwin.com>
2015-03-17 16:41:54 +0000

    chore(doc-gen): track modules in a dgeni service

--- docs/dgeni-package/readers/atScript.js
+++ docs/dgeni-package/readers/atScript.js
@@ -7,7 +7,7 @@ var path = require('canonical-path');
  * This file reader will create a simple doc for each
  * file including a code AST of the AtScript in the file.
  */
-module.exports = function atScriptFileReader(log, atParser) {
+module.exports = function atScriptFileReader(log, atParser, modules) {
   var reader = {
     name: 'atScriptFileReader',
     defaultPattern: /\.js$/,
@@ -17,6 +17,8 @@ module.exports = function atScriptFileReader(log, atParser) {
       moduleDoc.docType = 'module';
       moduleDoc.id = moduleDoc.moduleTree.moduleName;
       moduleDoc.aliases = [moduleDoc.id];
+
+      modules[moduleDoc.id] = moduleDoc;
 
       // Readers return a collection of docs read from the file
       // but in this read there is only one document (module) to return
