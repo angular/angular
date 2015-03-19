6e15d7211c388588d0d63555018e5867b9034fcc
Patrice Chalin <pchalin@gmail.com>
2015-03-19 11:36:27 -0700

    chore(install+test): single cmd to full install/test & test JS w/o Dart
    * `npm install` now does a full install; auxiliary installation steps
    have been integrated into the `postinstall` script.
    * Updated developer docs `DEVELOPER.md` accordingly; also added
    instructions to dev docs for performing full tests (via `npm test`) --
    same as those run on Travis.
    * Reorg in tests so that JS tests can run without a Dart env.
    
    Partly fixes #945 **under the assumption that when running JS tests
    locally, `ChromeCanary` is the desired browser to use**. Note that CI
    tests (Travis) still uses `DartiumWithWebPlatform` across the board
    (Maybe because ChromeCanary isn't being installed?)
    
    Fixes #1012.

--- scripts/ci/test_e2e_dart.sh
+++ scripts/ci/test_e2e_dart.sh
@@ -4,7 +4,6 @@ set -e
 echo =============================================================================
 # go to project dir
 SCRIPT_DIR=$(dirname $0)
-source $SCRIPT_DIR/env_dart.sh
 cd $SCRIPT_DIR/../..
 
 ./node_modules/.bin/webdriver-manager update
