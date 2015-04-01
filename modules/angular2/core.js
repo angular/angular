fcec08f831529f0e135d4ad5dd08f6c51ffe9461
Andrew <andrewtjoslin@gmail.com>
2015-04-01 15:56:16 -0600

    fix(angular2): export PrivateComponent{Loader,Location} in angular2/core

--- modules/angular2/core.js
+++ modules/angular2/core.js
@@ -7,6 +7,8 @@ export * from './src/core/annotations/di';
 export * from './src/core/compiler/compiler';
 
 export * from './src/core/compiler/template_loader';
+export * from './src/core/compiler/private_component_loader';
+export * from './src/core/compiler/private_component_location';
 export * from './src/core/compiler/view';
 export * from './src/core/compiler/view_container';
 
