3285ffba16ecf346e782f03cc9a5aceac57bba53
Martin Probst <martin@probst.io>
2015-04-03 11:54:56 -0700

    fix(traceur): Fix a couple of unsupported or incorrect tests.

--- tools/transpiler/spec/classes_spec.js
+++ tools/transpiler/spec/classes_spec.js
@@ -24,7 +24,6 @@ class SubFoo extends Foo {
   }
 }
 
-@CONST
 class ConstClass {}
 
 class Const {
@@ -75,11 +74,6 @@ export function main() {
       var subConst = new SubConst(1, 2);
       expect(subConst.a).toBe(1);
       expect(subConst.b).toBe(2);
-    });
-
-    it('@CONST on class without constructor should generate const constructor', function () {
-      var constClass = new ConstClass();
-      expect(constClass).not.toBe(null);
     });
 
     describe('inheritance', function() {
--- tools/transpiler/spec/reexport.js
+++ tools/transpiler/spec/reexport.js
@@ -1,10 +1,6 @@
 import {Baz} from './baz';
 import {Bar1} from './bar';
 
+export {Baz} from './baz';
+
 var localVar = true;
-
-export {Baz, localVar, Bar1};
-
-// Will become:
-// export {Baz} from './baz';
-// export {Bar1} from './bar';
