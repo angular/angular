c9f3f8d5329e80f5e6257febd4b2bf39bab15214
keertip <keertip@google.com>
2015-04-10 08:39:31 -0700

    feat: angular 2 dart analysis
    Initial commit for analysis support to Angular 2 Dart

---
+++ modules/angular2/src/analysis/analyzer_plugin/plugin.dart
@@ -0,0 +1,23 @@
+library angular2.src.analysis.analyzer_plugin;
+
+import 'package:analyzer/plugin/plugin.dart';
+
+/// Contribute a plugin to the dart analyzer for analysis of
+/// Angular2 dart code.
+class AngularAnalyzerPlugin extends Plugin {
+
+  /// the unique indetifier for this plugin
+  static const String UNIQUE_IDENTIFIER = 'angular2.analysis';
+
+  @override
+  String get uniqueIdentifier => UNIQUE_IDENTIFIER;
+
+  @override
+  void registerExtensionPoints(RegisterExtensionPoint registerExtensionPoint) {}
+
+  @override
+  void registerExtensions(RegisterExtension registerExtension) {
+    // TODO(keerti): register extension for analysis
+
+  }
+}
