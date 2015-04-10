fc0ad720bcb1b786cef098d42993990bc4f84617
Tobias Bosch <tbosch1009@gmail.com>
2015-04-10 16:57:26 -0700

    fix(shadow_dom): redistribute light dom when a dynamic component is attached.
    Fixes #1077

--- modules/angular2/src/render/dom/view/view.js
+++ modules/angular2/src/render/dom/view/view.js
@@ -69,6 +69,9 @@ export class RenderView {
     this.componentChildViews[elementIndex] = childView;
     if (this._hydrated) {
       childView.hydrate(lightDom);
+      if (isPresent(lightDom)) {
+        lightDom.redistribute();
+      }
     }
   }
 
