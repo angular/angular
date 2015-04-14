13b5ae528d80d4bbf124dfd0110cee998c057cdb
Alex Eagle <alexeagle@google.com>
2015-04-14 13:04:12 -0700

    chore(build): change typescript plugin to extend from broccoli-caching-writer

--- tools/broccoli/typescript/index.ts
+++ tools/broccoli/typescript/index.ts
@@ -1,18 +1,24 @@
-/// <reference path="../broccoli-writer.d.ts" />
+/// <reference path="../broccoli.d.ts" />
 /// <reference path="../../typings/node/node.d.ts" />
 /// <reference path="../../../node_modules/typescript/bin/typescript.d.ts" />
 
 import fs = require('fs');
 import path = require('path');
 import ts = require('typescript');
+import CachingWriter = require('broccoli-caching-writer');
 var walkSync = require('walk-sync');
-import Writer = require('broccoli-writer');
 var xtend = require('xtend');
 
-class TSCompiler extends Writer {
-  constructor(private inputTree, private options: ts.CompilerOptions = {}) { super(); }
+class TSCompiler extends CachingWriter {
+  constructor(private inputTrees, private options: ts.CompilerOptions = {}) {
+    super(inputTrees, {});
+    CachingWriter.apply(this, arguments);
+  }
 
-  write(readTree, destDir) {
+  updateCache(srcDirs: string | string[], destDir) {
+    if (!Array.isArray(srcDirs)) {
+      srcDirs = [<string>srcDirs];
+    }
     var options: ts.CompilerOptions = xtend({outDir: destDir}, this.options);
     if (this.options.outDir) {
       options.outDir = path.resolve(destDir, options.outDir);
@@ -21,8 +27,8 @@ class TSCompiler extends Writer {
       options.out = path.resolve(destDir, options.out);
     }
     options.target = (<any>ts).ScriptTarget[options.target];
-    return readTree(this.inputTree)
-        .then(srcDir => {
+    (<string[]>srcDirs)
+        .forEach(srcDir => {
           var files = walkSync(srcDir)
                           .filter(filepath => path.extname(filepath).toLowerCase() === '.ts')
                           .map(filepath => path.resolve(srcDir, filepath));
@@ -50,6 +56,7 @@ class TSCompiler extends Writer {
             }
           }
         });
+    return true;
   }
 }
 module.exports = TSCompiler;
