6896305e34082c246769829e4258631c1d2363d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-04-23 20:40:38 +0200

    fix: export ShadowDom strategies
    Fixes #1510
    Closes #1511

--- modules/angular2/core.js
+++ modules/angular2/core.js
@@ -15,7 +15,11 @@ export * from './src/core/compiler/query_list';
 export * from './src/core/compiler/compiler';
 
 // TODO(tbosch): remove this once render migration is complete
-export * from 'angular2/src/render/dom/compiler/template_loader';
+export * from './src/render/dom/compiler/template_loader';
+export * from './src/render/dom/shadow_dom/shadow_dom_strategy';
+export * from './src/render/dom/shadow_dom/native_shadow_dom_strategy';
+export * from './src/render/dom/shadow_dom/emulated_scoped_shadow_dom_strategy';
+export * from './src/render/dom/shadow_dom/emulated_unscoped_shadow_dom_strategy';
 export * from './src/core/compiler/dynamic_component_loader';
 export {ElementRef, ComponentRef} from './src/core/compiler/element_injector';
 export * from './src/core/compiler/view';
