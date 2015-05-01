4b62a722f0590a232c129d5d4c2e2b3702bc21ab
PatrickJS <github@gdi2290.com>
2015-04-30 18:09:51 -0700

    feat(change_detection.js): export null pipes
    rather then having developers creating their own null pipes
    
    Closes #1624

--- modules/angular2/change_detection.ts
+++ modules/angular2/change_detection.ts
@@ -47,6 +47,7 @@ export {ChangeDetectorRef} from './src/change_detection/change_detector_ref';
 export {PipeRegistry} from './src/change_detection/pipes/pipe_registry';
 export {uninitialized} from './src/change_detection/change_detection_util';
 export {WrappedValue, Pipe} from './src/change_detection/pipes/pipe';
+export {NullPipe, NullPipeFactory} from './src/change_detection/pipes/null_pipe';
 export {
   defaultPipes,
   DynamicChangeDetection,
