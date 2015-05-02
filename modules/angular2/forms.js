480d4d1f4b37c455c7bba8316677bf136113e607
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-05-02 11:59:44 +0200

    fix(forms): export collection of form directives as const in Dart
    Fixes #1283

--- modules/angular2/forms.js
+++ modules/angular2/forms.js
@@ -12,6 +12,7 @@
 
 export * from './src/forms/model';
 export * from './src/forms/directives';
+export * from './src/forms/directives_all';
 export * from './src/forms/validators';
 export * from './src/forms/validator_directives';
 export * from './src/forms/form_builder';
--- modules/angular2/src/forms/directives.js
+++ modules/angular2/src/forms/directives.js
@@ -262,16 +262,3 @@ export class ControlGroupDirective {
     }
   }
 }
-
-/**
- *
- * A list of all the form directives used as part of a `@View` annotation.
- *
- *  This is a shorthand for importing them each individually.
- *
- * @exportedAs angular2/forms
- */
-// todo(misko): rename to lover case as it is not a Type but a var.
-export var FormDirectives = [
-  ControlGroupDirective, ControlDirective, CheckboxControlValueAccessor, DefaultValueAccessor
-];
