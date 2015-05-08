d717529e9a8648a602c98dd5df09f0165866d51d
vsavkin <vic.savkin@gmail.com>
2015-05-08 08:32:02 -0700

    fix(change_detection): updated dynamic change detector not to mutate when throwing
    Closes #1762

--- modules/angular2/test/change_detection/change_detection_spec.js
+++ modules/angular2/test/change_detection/change_detection_spec.js
@@ -420,6 +420,23 @@ export function main() {
                 cd.checkNoChanges();
               }).toThrowError(new RegExp("Expression 'a in location' has changed after it was checked"));
             });
+
+            it("should not break the next run", () => {
+              var pcd = createProtoChangeDetector([
+                BindingRecord.createForElement(ast("a"), 0, "a")
+              ]);
+
+              var dispatcher = new TestDispatcher();
+              var cd = pcd.instantiate(dispatcher);
+              cd.hydrate(new TestData('value'), null, null);
+
+              expect(() => cd.checkNoChanges()).toThrowError(new RegExp(
+                "Expression 'a in location' has changed after it was checked."));
+
+              cd.detectChanges();
+
+              expect(dispatcher.loggedValues).toEqual(['value']);
+            });
           });
 
           //TODO vsavkin: implement it
