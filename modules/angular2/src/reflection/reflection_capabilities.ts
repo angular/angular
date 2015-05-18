2488ab24a290abe5d0a9acdb292c08c14544b989
Josef Meier <jo.meier@gmx.de>
2015-05-18 08:58:17 +0200

    Update reflection_capabilities.ts

--- modules/angular2/src/reflection/reflection_capabilities.ts
+++ modules/angular2/src/reflection/reflection_capabilities.ts
@@ -50,17 +50,20 @@ export class ReflectionCapabilities {
   _zipTypesAndAnnotaions(paramTypes, paramAnnotations): List<List<any>> {
     var result;
 
-    if(typeof paramTypes === 'undefined')
-      result = ListWrapper.createFixedSize(paramAnnotations.length);  
-    else
+    if(typeof paramTypes === 'undefined'){
+      result = ListWrapper.createFixedSize(paramAnnotations.length);
+    }
+    else{
       result = ListWrapper.createFixedSize(paramTypes.length);
-
+    }
+    
     for (var i = 0; i < result.length; i++) {
       // TS outputs Object for parameters without types, while Traceur omits
       // the annotations. For now we preserve the Traceur behavior to aid
       // migration, but this can be revisited.
-      if (typeof paramTypes === 'undefined')
+      if (typeof paramTypes === 'undefined'){
         result[i] = [];
+      }
       else if (paramTypes[i] != Object) {
         result[i] = [paramTypes[i]];
       } else {
