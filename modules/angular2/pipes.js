155b1e2b35da9b5b9ee09e2d0f633f0fb5b40d28
vsavkin <vic.savkin@gmail.com>
2015-05-18 17:47:44 -0700

    feat(pipe): reexported pipes to genereate docs

--- modules/angular2/pipes.js
+++ modules/angular2/pipes.js
@@ -4,3 +4,11 @@
  * @description
  * This module provides advanced support for extending change detection.
  */
+
+export {PromisePipe} from './src/change_detection/pipes/promise_pipe';
+export {UpperCasePipe} from './src/change_detection/pipes/uppercase_pipe';
+export {LowerCasePipe} from './src/change_detection/pipes/lowercase_pipe';
+export {ObservablePipe} from './src/change_detection/pipes/observable_pipe';
+export {JsonPipe} from './src/change_detection/pipes/json_pipe';
+export {IterableChanges} from './src/change_detection/pipes/iterable_changes';
+export {KeyValueChanges} from './src/change_detection/pipes/keyvalue_changes';
