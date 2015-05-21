f35dbb99b5e41fd3c84f238da8671ed4741b0cd0
Marc Laval <mlaval@amadeus.com>
2015-05-21 11:50:04 +0200

    fix(browser_adapter): element.getBoundingClientRect fails when element not in DOM (IE11)

--- modules/angular2/src/dom/browser_adapter.ts
+++ modules/angular2/src/dom/browser_adapter.ts
@@ -270,7 +270,11 @@ defaultDoc() {
   return document;
 }
 getBoundingClientRect(el) {
-  return el.getBoundingClientRect();
+  try {
+    return el.getBoundingClientRect();
+  } catch (e) {
+    return {top: 0, bottom: 0, left: 0, right: 0, width: 0, height: 0};
+  }
 }
 getTitle(): string {
   return document.title;
