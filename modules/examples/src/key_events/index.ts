6c1cb089b581d51d47a09d5ba7be971d1b5b938b
Victor Berchet <victor@suumit.com>
2015-05-22 14:39:00 +0200

    refactor(examples): ts’ify
    relates to #2008

--- modules/examples/src/key_events/index.ts
+++ modules/examples/src/key_events/index.ts
@@ -1,18 +1,10 @@
-import {bootstrap} from 'angular2/angular2';
+import {bootstrap, Component, View} from 'angular2/angular2';
 import {KeyEventsPlugin} from 'angular2/src/render/dom/events/key_events';
 
-// TODO(radokirov): Once the application is transpiled by TS instead of Traceur,
-// add those imports back into 'angular2/angular2';
-import {Component} from 'angular2/src/core/annotations_impl/annotations';
-import {View} from 'angular2/src/core/annotations_impl/view';
-
-// 2 imports for the Dart version:
 import {reflector} from 'angular2/src/reflection/reflection';
 import {ReflectionCapabilities} from 'angular2/src/reflection/reflection_capabilities';
 
-@Component({
-  selector: 'key-events-app'
-})
+@Component({selector: 'key-events-app'})
 @View({
   template: `Click in the following area and press a key to display its name:<br>
   <div (keydown)="onKeyDown($event)" class="sample-area" tabindex="0">{{lastKey}}</div><br>
@@ -25,31 +17,23 @@ import {ReflectionCapabilities} from 'angular2/src/reflection/reflection_capabil
   >{{shiftEnter ? 'You pressed shift.enter!' : ''}}</div>`
 })
 class KeyEventsApp {
-  lastKey: string;
-  shiftEnter: boolean;
+  lastKey: string = '(none)';
+  shiftEnter: boolean = false;
 
-  constructor() {
-    this.lastKey = '(none)';
-    this.shiftEnter = false;
-  }
-
-  onKeyDown(event) {
+  onKeyDown(event): void {
     this.lastKey = KeyEventsPlugin.getEventFullKey(event);
     event.preventDefault();
   }
 
-  onShiftEnter(event) {
+  onShiftEnter(event): void {
     this.shiftEnter = true;
     event.preventDefault();
   }
 
-  resetShiftEnter() {
-    this.shiftEnter = false;
-  }
-
+  resetShiftEnter(): void { this.shiftEnter = false; }
 }
 
 export function main() {
-  reflector.reflectionCapabilities = new ReflectionCapabilities(); // for the Dart version
+  reflector.reflectionCapabilities = new ReflectionCapabilities();  // for the Dart version
   bootstrap(KeyEventsApp);
 }
---
+++ modules/examples/src/todo/index.ts
@@ -0,0 +1,49 @@
+import {bootstrap, NgFor, Component, View} from 'angular2/angular2';
+import {Store, Todo, TodoFactory} from './services/TodoStore';
+import {reflector} from 'angular2/src/reflection/reflection';
+import {ReflectionCapabilities} from 'angular2/src/reflection/reflection_capabilities';
+
+@Component({selector: 'todo-app', appInjector: [Store, TodoFactory]})
+@View({templateUrl: 'todo.html', directives: [NgFor]})
+class TodoApp {
+  todoEdit: Todo = null;
+
+  constructor(public todoStore: Store, public factory: TodoFactory) {}
+
+  enterTodo($event, inputElement): void {
+    this.addTodo(inputElement.value);
+    inputElement.value = '';
+  }
+
+  editTodo(todo: Todo): void { this.todoEdit = todo; }
+
+  doneEditing($event, todo: Todo): void {
+    var which = $event.which;
+    var target = $event.target;
+    if (which === 13) {
+      todo.title = target.value;
+      this.todoEdit = null;
+    } else if (which === 27) {
+      this.todoEdit = null;
+      target.value = todo.title;
+    }
+  }
+
+  addTodo(newTitle: string): void { this.todoStore.add(this.factory.create(newTitle, false)); }
+
+  completeMe(todo: Todo): void { todo.completed = !todo.completed; }
+
+  deleteMe(todo: Todo): void { this.todoStore.remove(todo); }
+
+  toggleAll($event): void {
+    var isComplete = $event.target.checked;
+    this.todoStore.list.forEach((todo: Todo) => { todo.completed = isComplete; });
+  }
+
+  clearCompleted(): void { this.todoStore.removeBy((todo) => todo.completed); }
+}
+
+export function main() {
+  reflector.reflectionCapabilities = new ReflectionCapabilities();  // for the Dart version
+  bootstrap(TodoApp);
+}
