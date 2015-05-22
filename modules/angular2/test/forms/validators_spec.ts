3525c9c074b9a03cd67b830a3662c26c8669ef4a
vsavkin <vic.savkin@gmail.com>
2015-05-22 12:32:49 -0700

    chore(forms): moved tests/forms to typescript

--- modules/angular2/test/forms/validators_spec.ts
+++ modules/angular2/test/forms/validators_spec.ts
@@ -1,9 +1,19 @@
-import {ddescribe, describe, it, iit, xit, expect, beforeEach, afterEach, el} from 'angular2/test_lib';
+import {
+  ddescribe,
+  describe,
+  it,
+  iit,
+  xit,
+  expect,
+  beforeEach,
+  afterEach,
+  el
+} from 'angular2/test_lib';
 import {ControlGroup, Control, Validators} from 'angular2/forms';
 
 export function main() {
-  function validator(key:string, error:any){
-    return function(c:Control) {
+  function validator(key: string, error: any) {
+    return function(c: Control) {
       var r = {};
       r[key] = error;
       return r;
@@ -12,28 +22,25 @@ export function main() {
 
   describe("Validators", () => {
     describe("required", () => {
-      it("should error on an empty string", () => {
-        expect(Validators.required(new Control(""))).toEqual({"required" : true});
-      });
+      it("should error on an empty string",
+         () => { expect(Validators.required(new Control(""))).toEqual({"required": true}); });
 
-      it("should error on null", () => {
-        expect(Validators.required(new Control(null))).toEqual({"required" : true});
-      });
+      it("should error on null",
+         () => { expect(Validators.required(new Control(null))).toEqual({"required": true}); });
 
-      it("should not error on a non-empty string", () => {
-        expect(Validators.required(new Control("not empty"))).toEqual(null);
-      });
+      it("should not error on a non-empty string",
+         () => { expect(Validators.required(new Control("not empty"))).toEqual(null); });
     });
 
     describe("compose", () => {
       it("should collect errors from all the validators", () => {
         var c = Validators.compose([validator("a", true), validator("b", true)]);
-        expect(c(new Control(""))).toEqual({"a" : true, "b" : true});
+        expect(c(new Control(""))).toEqual({"a": true, "b": true});
       });
 
       it("should run validators left to right", () => {
         var c = Validators.compose([validator("a", 1), validator("a", 2)]);
-        expect(c(new Control(""))).toEqual({"a" : 2});
+        expect(c(new Control(""))).toEqual({"a": 2});
       });
 
       it("should return null when no errors", () => {
@@ -46,28 +53,21 @@ export function main() {
       it("should collect errors from the child controls", () => {
         var one = new Control("one", validator("a", true));
         var two = new Control("one", validator("b", true));
-        var g = new ControlGroup({"one" : one, "two" : two});
+        var g = new ControlGroup({"one": one, "two": two});
 
-        expect(Validators.group(g)).toEqual({
-          "a" : [one],
-          "b" : [two]
-        });
+        expect(Validators.group(g)).toEqual({"a": [one], "b": [two]});
       });
 
       it("should not include controls that have no errors", () => {
         var one = new Control("one", validator("a", true));
         var two = new Control("two");
-        var g = new ControlGroup({"one" : one, "two" : two});
+        var g = new ControlGroup({"one": one, "two": two});
 
-        expect(Validators.group(g)).toEqual({
-          "a": [one]
-        });
+        expect(Validators.group(g)).toEqual({"a": [one]});
       });
 
       it("should return null when no errors", () => {
-        var g = new ControlGroup({
-          "one" : new Control("one")
-        });
+        var g = new ControlGroup({"one": new Control("one")});
 
         expect(Validators.group(g)).toEqual(null);
       });
