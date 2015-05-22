662da0d72802165e906c37269a5499c190e8c711
gdi2290 <github@gdi2290.com>
2015-05-22 16:06:38 -0700

    feat(render): re-export render and export `DirectiveResolver`
    Closes #2026

--- modules/angular2/core.ts
+++ modules/angular2/core.ts
@@ -13,13 +13,7 @@ export * from './src/core/annotations/di';
 export * from './src/core/compiler/compiler';
 export * from './src/core/compiler/interfaces';
 export * from './src/core/compiler/query_list';
-
-// TODO(tbosch): remove this once render migration is complete
-export * from './src/render/dom/compiler/template_loader';
-export * from './src/render/dom/shadow_dom/shadow_dom_strategy';
-export * from './src/render/dom/shadow_dom/native_shadow_dom_strategy';
-export * from './src/render/dom/shadow_dom/emulated_scoped_shadow_dom_strategy';
-export * from './src/render/dom/shadow_dom/emulated_unscoped_shadow_dom_strategy';
+export * from './src/core/compiler/directive_resolver';
 export * from './src/core/compiler/dynamic_component_loader';
 export {ViewRef, ProtoViewRef} from './src/core/compiler/view_ref';
 export {ViewContainerRef} from './src/core/compiler/view_container_ref';
---
+++ modules/angular2/render.ts
@@ -0,0 +1,13 @@
+/**
+ * @module
+ * @public
+ * @description
+ * This module provides advanced support for extending dom strategy.
+ */
+
+export * from './src/render/dom/compiler/template_loader';
+export * from './src/render/dom/shadow_dom/shadow_dom_strategy';
+export * from './src/render/dom/shadow_dom/native_shadow_dom_strategy';
+export * from './src/render/dom/shadow_dom/emulated_scoped_shadow_dom_strategy';
+export * from './src/render/dom/shadow_dom/emulated_unscoped_shadow_dom_strategy';
+export * from './src/render/api';
