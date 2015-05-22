7aeb1d337b6015c1f0cc259de83caafd06173c47
Naomi Black <naomitraveller@gmail.com>
2015-05-22 13:16:36 -0700

    docs(di): fix missing export for dgeni docgen

--- modules/angular2/di.ts
+++ modules/angular2/di.ts
@@ -9,7 +9,7 @@ export * from './src/di/annotations';
 export * from './src/di/decorators';
 export * from './src/di/forward_ref';
 export {resolveBindings, Injector} from './src/di/injector';
-export {Binding, ResolvedBinding, Dependency, bind} from './src/di/binding';
+export {Binding, BindingBuilder, ResolvedBinding, Dependency, bind} from './src/di/binding';
 export {Key, KeyRegistry, TypeLiteral} from './src/di/key';
 export {
   NoBindingError,
