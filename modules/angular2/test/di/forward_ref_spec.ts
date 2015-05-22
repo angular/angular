df59e969cf0312891d9c28d6221d9876a3f10f3d
vsavkin <vic.savkin@gmail.com>
2015-05-22 15:39:28 -0700

    chore(di): moved tests to typescript

--- modules/angular2/test/di/forward_ref_spec.ts
+++ modules/angular2/test/di/forward_ref_spec.ts
@@ -13,7 +13,7 @@ import {forwardRef, resolveForwardRef} from 'angular2/di';
 import {Type} from 'angular2/src/facade/lang';
 
 export function main() {
-  describe("forwardRef", function () {
+  describe("forwardRef", function() {
     it('should wrap and unwrap the reference', () => {
       var ref = forwardRef(() => String);
       expect(ref instanceof Type).toBe(true);
