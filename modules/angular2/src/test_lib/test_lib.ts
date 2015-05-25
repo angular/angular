95a01f95b7591402c8a4fbaca6298d436db3cb3d
Aleksandar Bogatinov <bogatinov@outlook.com>
2015-05-25 22:29:50 +0200

    style(test_lib.ts): Revert to commit #324f6ce3

--- modules/angular2/src/test_lib/test_lib.ts
+++ modules/angular2/src/test_lib/test_lib.ts
@@ -19,13 +19,13 @@ var _global: jasmine.GlobalPolluter = <any>(typeof window === 'undefined' ? glob
 export var afterEach = _global.afterEach;
 
 export interface NgMatchers extends jasmine.Matchers {
-  toBe(expected: any): boolean;
-  toEqual(expected: any): boolean;
-  toBePromise(): boolean;
-  toBeAnInstanceOf(expected: any): boolean;
-  toHaveText(expected: any): boolean;
-  toImplement(expected: any): boolean;
-  not: NgMatchers;
+    toBe(expected: any): boolean;
+    toEqual(expected: any): boolean;
+    toBePromise(expected: any): boolean;
+    toBeAnInstanceOf(expected: any): boolean;
+    toHaveText(expected: any): boolean;
+    toImplement(expected: any): boolean;
+    not: NgMatchers;
 }
 
 export var expect: (actual: any) => NgMatchers = <any>_global.expect;
@@ -33,11 +33,11 @@ export var expect: (actual: any) => NgMatchers = <any>_global.expect;
 export var IS_DARTIUM = false;
 
 export class AsyncTestCompleter {
-  _done: Function;
+    _done: Function;
 
-  constructor(done: Function) { this._done = done; }
+    constructor(done: Function) { this._done = done; }
 
-  done() { this._done(); }
+    done() { this._done(); }
 }
 
 var jsmBeforeEach = _global.beforeEach;
@@ -54,57 +54,57 @@ var inIt = false;
 var testBindings;
 
 class BeforeEachRunner {
-  _fns: List<FunctionWithParamTokens>;
-  _parent: BeforeEachRunner;
-  constructor(parent: BeforeEachRunner) {
-    this._fns = [];
-    this._parent = parent;
-  }
+    _fns: List<FunctionWithParamTokens>;
+    _parent: BeforeEachRunner;
+    constructor(parent: BeforeEachRunner) {
+        this._fns = [];
+        this._parent = parent;
+    }
 
-  beforeEach(fn: FunctionWithParamTokens) { this._fns.push(fn); }
+    beforeEach(fn: FunctionWithParamTokens) { this._fns.push(fn); }
 
-  run(injector) {
-    if (this._parent) this._parent.run(injector);
-    this._fns.forEach((fn) => fn.execute(injector));
-  }
+    run(injector) {
+        if (this._parent) this._parent.run(injector);
+        this._fns.forEach((fn) => fn.execute(injector));
+    }
 }
 
 // Reset the test bindings before each test
 jsmBeforeEach(() => { testBindings = []; });
 
-function _describe(jsmFn, ... args) {
-  var parentRunner = runnerStack.length === 0 ? null : runnerStack[runnerStack.length - 1];
-  var runner = new BeforeEachRunner(parentRunner);
-  runnerStack.push(runner);
-  var suite = jsmFn(... args);
-  runnerStack.pop();
-  return suite;
+function _describe(jsmFn, ...args) {
+    var parentRunner = runnerStack.length === 0 ? null : runnerStack[runnerStack.length - 1];
+    var runner = new BeforeEachRunner(parentRunner);
+    runnerStack.push(runner);
+    var suite = jsmFn(...args);
+    runnerStack.pop();
+    return suite;
 }
 
-export function describe(... args) {
-  return _describe(jsmDescribe, ... args);
+export function describe(...args) {
+    return _describe(jsmDescribe, ...args);
 }
 
-export function ddescribe(... args) {
-  return _describe(jsmDDescribe, ... args);
+export function ddescribe(...args) {
+    return _describe(jsmDDescribe, ...args);
 }
 
-export function xdescribe(... args) {
-  return _describe(jsmXDescribe, ... args);
+export function xdescribe(...args) {
+    return _describe(jsmXDescribe, ...args);
 }
 
 export function beforeEach(fn) {
-  if (runnerStack.length > 0) {
-    // Inside a describe block, beforeEach() uses a BeforeEachRunner
-    var runner = runnerStack[runnerStack.length - 1];
-    if (!(fn instanceof FunctionWithParamTokens)) {
-      fn = inject([], fn);
+    if (runnerStack.length > 0) {
+        // Inside a describe block, beforeEach() uses a BeforeEachRunner
+        var runner = runnerStack[runnerStack.length - 1];
+        if (!(fn instanceof FunctionWithParamTokens)) {
+            fn = inject([], fn);
+        }
+        runner.beforeEach(fn);
+    } else {
+        // Top level beforeEach() are delegated to jasmine
+        jsmBeforeEach(fn);
     }
-    runner.beforeEach(fn);
-  } else {
-    // Top level beforeEach() are delegated to jasmine
-    jsmBeforeEach(fn);
-  }
 }
 
 /**
@@ -120,53 +120,53 @@ export function beforeEach(fn) {
  *   ]);
  */
 export function beforeEachBindings(fn) {
-  jsmBeforeEach(() => {
-    var bindings = fn();
-    if (!bindings) return;
-    testBindings = [... testBindings, ... bindings];
-  });
+    jsmBeforeEach(() => {
+        var bindings = fn();
+        if (!bindings) return;
+        testBindings = [...testBindings, ...bindings];
+    });
 }
 
 function _it(jsmFn, name, fn) {
-  var runner = runnerStack[runnerStack.length - 1];
+    var runner = runnerStack[runnerStack.length - 1];
 
-  jsmFn(name, function(done) {
-    var async = false;
+    jsmFn(name, function (done) {
+        var async = false;
 
-    var completerBinding =
-        bind(AsyncTestCompleter)
-            .toFactory(() => {
-              // Mark the test as async when an AsyncTestCompleter is injected in an it()
-              if (!inIt) throw new Error('AsyncTestCompleter can only be injected in an "it()"');
-              async = true;
-              return new AsyncTestCompleter(done);
+        var completerBinding =
+            bind(AsyncTestCompleter)
+                .toFactory(() => {
+                // Mark the test as async when an AsyncTestCompleter is injected in an it()
+                if (!inIt) throw new Error('AsyncTestCompleter can only be injected in an "it()"');
+                async = true;
+                return new AsyncTestCompleter(done);
             });
 
-    var injector = createTestInjector([... testBindings, completerBinding]);
-    runner.run(injector);
+        var injector = createTestInjector([...testBindings, completerBinding]);
+        runner.run(injector);
 
-    if (!(fn instanceof FunctionWithParamTokens)) {
-      fn = inject([], fn);
-    }
+        if (!(fn instanceof FunctionWithParamTokens)) {
+            fn = inject([], fn);
+        }
 
-    inIt = true;
-    fn.execute(injector);
-    inIt = false;
+        inIt = true;
+        fn.execute(injector);
+        inIt = false;
 
-    if (!async) done();
-  });
+        if (!async) done();
+    });
 }
 
 export function it(name, fn) {
-  return _it(jsmIt, name, fn);
+    return _it(jsmIt, name, fn);
 }
 
 export function xit(name, fn) {
-  return _it(jsmXIt, name, fn);
+    return _it(jsmXIt, name, fn);
 }
 
 export function iit(name, fn) {
-  return _it(jsmIIt, name, fn);
+    return _it(jsmIIt, name, fn);
 }
 
 // Some Map polyfills don't polyfill Map.toString correctly, which
@@ -174,190 +174,191 @@ export function iit(name, fn) {
 // The only way to do this in Jasmine is to monkey patch a method
 // to the object :-(
 Map.prototype['jasmineToString'] =
-    function() {
-      var m = this;
-      if (!m) {
+function () {
+    var m = this;
+    if (!m) {
         return '' + m;
-      }
-      var res = [];
-      m.forEach((v, k) => { res.push(`${k}:${v}`); });
-      return `{ ${res.join(',') } }`;
     }
+    var res = [];
+    m.forEach((v, k) => { res.push(`${k}:${v}`); });
+    return `{ ${res.join(',') } }`;
+}
 
-    _global.beforeEach(function() {
-      jasmine.addMatchers({
+_global.beforeEach(function () {
+    jasmine.addMatchers({
         // Custom handler for Map as Jasmine does not support it yet
-        toEqual: function(util, customEqualityTesters) {
-          return {
-            compare: function(actual, expected) {
-              return {pass: util.equals(actual, expected, [compareMap])};
-            }
-          };
-
-          function compareMap(actual, expected) {
-            if (actual instanceof Map) {
-              var pass = actual.size === expected.size;
-              if (pass) {
-                actual.forEach((v, k) => { pass = pass && util.equals(v, expected.get(k)); });
-              }
-              return pass;
-            } else {
-              return undefined;
-            }
-          }
-        },
-
-        toBePromise: function() {
-          return {
-            compare: function(actual, expectedClass) {
-              var pass = typeof actual === 'object' && typeof actual.then === 'function';
-              return {
-                pass: pass,
-                get message() { return 'Expected ' + actual + ' to be a promise'; }
-              };
-            }
-          };
-        },
-
-        toBeAnInstanceOf: function() {
-          return {
-            compare: function(actual, expectedClass) {
-              var pass = typeof actual === 'object' && actual instanceof expectedClass;
-              return {
-                pass: pass,
-                get message() {
-                  return 'Expected ' + actual + ' to be an instance of ' + expectedClass;
+        toEqual: function (util, customEqualityTesters) {
+            return {
+                compare: function (actual, expected) {
+                    return { pass: util.equals(actual, expected, [compareMap]) };
                 }
-              };
+            };
+
+            function compareMap(actual, expected) {
+                if (actual instanceof Map) {
+                    var pass = actual.size === expected.size;
+                    if (pass) {
+                        actual.forEach((v, k) => { pass = pass && util.equals(v, expected.get(k)); });
+                    }
+                    return pass;
+                } else {
+                    return undefined;
+                }
             }
-          };
         },
 
-        toHaveText: function() {
-          return {
-            compare: function(actual, expectedText) {
-              var actualText = elementText(actual);
-              return {
-                pass: actualText == expectedText,
-                get message() {
-                  return 'Expected ' + actualText + ' to be equal to ' + expectedText;
+        toBePromise: function () {
+            return {
+                compare: function (actual, expectedClass) {
+                    var pass = typeof actual === 'object' && typeof actual.then === 'function';
+                    return {
+                        pass: pass,
+                        get message() { return 'Expected ' + actual + ' to be a promise'; }
+                    };
                 }
-              };
-            }
-          };
+            };
         },
 
-        toImplement: function() {
-          return {
-            compare: function(actualObject, expectedInterface) {
-              var objProps = Object.keys(actualObject.constructor.prototype);
-              var intProps = Object.keys(expectedInterface.prototype);
-
-              var missedMethods = [];
-              intProps.forEach((k) => {
-                if (!actualObject.constructor.prototype[k]) missedMethods.push(k);
-              });
-
-              return {
-                pass: missedMethods.length == 0,
-                get message() {
-                  return 'Expected ' + actualObject + ' to have the following methods: ' +
-                         missedMethods.join(", ");
+        toBeAnInstanceOf: function () {
+            return {
+                compare: function (actual, expectedClass) {
+                    var pass = typeof actual === 'object' && actual instanceof expectedClass;
+                    return {
+                        pass: pass,
+                        get message() {
+                            return 'Expected ' + actual + ' to be an instance of ' + expectedClass;
+                        }
+                    };
                 }
-              };
-            }
-          };
+            };
+        },
+
+        toHaveText: function () {
+            return {
+                compare: function (actual, expectedText) {
+                    var actualText = elementText(actual);
+                    return {
+                        pass: actualText == expectedText,
+                        get message() {
+                            return 'Expected ' + actualText + ' to be equal to ' + expectedText;
+                        }
+                    };
+                }
+            };
+        },
+
+        toImplement: function () {
+            return {
+                compare: function (actualObject, expectedInterface) {
+                    var objProps = Object.keys(actualObject.constructor.prototype);
+                    var intProps = Object.keys(expectedInterface.prototype);
+
+                    var missedMethods = [];
+                    intProps.forEach((k) => {
+                        if (!actualObject.constructor.prototype[k]) missedMethods.push(k);
+                    });
+
+                    return {
+                        pass: missedMethods.length == 0,
+                        get message() {
+                            return 'Expected ' + actualObject + ' to have the following methods: ' +
+                                missedMethods.join(", ");
+                        }
+                    };
+                }
+            };
         }
-      });
     });
+});
 
 export interface GuinessCompatibleSpy extends jasmine.Spy {
-  /** By chaining the spy with and.returnValue, all calls to the function will return a specific
-   * value. */
-  andReturn(val: any): void;
-  /** By chaining the spy with and.callFake, all calls to the spy will delegate to the supplied
-   * function. */
-  andCallFake(fn: Function): GuinessCompatibleSpy;
+    /** By chaining the spy with and.returnValue, all calls to the function will return a specific
+     * value. */
+    andReturn(val: any): void;
+    /** By chaining the spy with and.callFake, all calls to the spy will delegate to the supplied
+     * function. */
+    andCallFake(fn: Function): GuinessCompatibleSpy;
 }
 
 export class SpyObject {
-  constructor(type = null) {
-    if (type) {
-      for (var prop in type.prototype) {
-        var m = null;
-        try {
-          m = type.prototype[prop];
-        } catch (e) {
-          // As we are creating spys for abstract classes,
-          // these classes might have getters that throw when they are accessed.
-          // As we are only auto creating spys for methods, this
-          // should not matter.
+    constructor(type = null) {
+        if (type) {
+            for (var prop in type.prototype) {
+                var m = null;
+                try {
+                    m = type.prototype[prop];
+                } catch (e) {
+                    // As we are creating spys for abstract classes,
+                    // these classes might have getters that throw when they are accessed.
+                    // As we are only auto creating spys for methods, this
+                    // should not matter.
+                }
+                if (typeof m === 'function') {
+                    this.spy(prop);
+                }
+            }
         }
-        if (typeof m === 'function') {
-          this.spy(prop);
+    }
+    spy(name) {
+        if (!this[name]) {
+            this[name] = this._createGuinnessCompatibleSpy(name);
         }
-      }
-    }
-  }
-  spy(name) {
-    if (!this[name]) {
-      this[name] = this._createGuinnessCompatibleSpy(name);
-    }
-    return this[name];
-  }
-
-  static stub(object = null, config = null, overrides = null) {
-    if (!(object instanceof SpyObject)) {
-      overrides = config;
-      config = object;
-      object = new SpyObject();
+        return this[name];
     }
 
-    var m = StringMapWrapper.merge(config, overrides);
-    StringMapWrapper.forEach(m, (value, key) => { object.spy(key).andReturn(value); });
-    return object;
-  }
+    static stub(object = null, config = null, overrides = null) {
+        if (!(object instanceof SpyObject)) {
+            overrides = config;
+            config = object;
+            object = new SpyObject();
+        }
 
-  rttsAssert(value) { return true; }
+        var m = StringMapWrapper.merge(config, overrides);
+        StringMapWrapper.forEach(m, (value, key) => { object.spy(key).andReturn(value); });
+        return object;
+    }
 
-  _createGuinnessCompatibleSpy(name): GuinessCompatibleSpy {
-    var newSpy: GuinessCompatibleSpy = <any>jasmine.createSpy(name);
-    newSpy.andCallFake = <any>newSpy.and.callFake;
-    newSpy.andReturn = <any>newSpy.and.returnValue;
-    // return null by default to satisfy our rtts asserts
-    newSpy.and.returnValue(null);
-    return newSpy;
-  }
+    rttsAssert(value) { return true; }
+
+    _createGuinnessCompatibleSpy(name): GuinessCompatibleSpy {
+        var newSpy: GuinessCompatibleSpy = <any>jasmine.createSpy(name);
+        newSpy.andCallFake = <any>newSpy.and.callFake;
+        newSpy.andReturn = <any>newSpy.and.returnValue;
+        // return null by default to satisfy our rtts asserts
+        newSpy.and.returnValue(null);
+        return newSpy;
+    }
 }
 
 function elementText(n) {
-  var hasNodes = (n) => { var children = DOM.childNodes(n);
-  return children && children.length > 0;
-}
+    var hasNodes = (n) => {
+        var children = DOM.childNodes(n);
+        return children && children.length > 0;
+    }
 
-if (n instanceof Array) {
-  return n.map((nn) => elementText(nn)).join("");
-}
+    if (n instanceof Array) {
+        return n.map((nn) => elementText(nn)).join("");
+    }
 
-if (DOM.isCommentNode(n)) {
-  return '';
-}
+    if (DOM.isCommentNode(n)) {
+        return '';
+    }
 
-if (DOM.isElementNode(n) && DOM.tagName(n) == 'CONTENT') {
-  return elementText(Array.prototype.slice.apply(DOM.getDistributedNodes(n)));
-}
+    if (DOM.isElementNode(n) && DOM.tagName(n) == 'CONTENT') {
+        return elementText(Array.prototype.slice.apply(DOM.getDistributedNodes(n)));
+    }
 
-if (DOM.hasShadowRoot(n)) {
-  return elementText(DOM.childNodesAsList(DOM.getShadowRoot(n)));
-}
+    if (DOM.hasShadowRoot(n)) {
+        return elementText(DOM.childNodesAsList(DOM.getShadowRoot(n)));
+    }
 
-if (hasNodes(n)) {
-  return elementText(DOM.childNodesAsList(n));
-}
+    if (hasNodes(n)) {
+        return elementText(DOM.childNodesAsList(n));
+    }
 
-return DOM.getText(n);
+    return DOM.getText(n);
 }
 
 export function isInInnerZone(): boolean {
-  return (<NgZoneZone>global.zone)._innerZone === true;
+    return (<NgZoneZone>global.zone)._innerZone === true;
 }
