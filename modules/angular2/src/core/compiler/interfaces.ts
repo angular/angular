2b6a6530506bd80b88bf9c65b080a83eb3d3a9d2
vsavkin <vic.savkin@gmail.com>
2015-05-27 08:07:37 -0700

    feat(core): added missing interfaces for onDestroy and onAllChangesDone lifecycle events

--- modules/angular2/src/core/compiler/interfaces.ts
+++ modules/angular2/src/core/compiler/interfaces.ts
@@ -5,3 +5,13 @@ import {StringMap} from 'angular2/src/facade/collection';
  * properties are updated.
  */
 export interface OnChange { onChange(changes: StringMap<string, any>): void; }
+
+/**
+ * Defines lifecycle method [onDestroy] called when a directive is being destroyed.
+ */
+export interface OnDestroy { onDestroy(): void; }
+
+/**
+ * Defines lifecycle method [onAllChangesDone ] called when the bindings of all its children have been changed.
+ */
+export interface OnAllChangesDone { onAllChangesDone (): void; }
