1db6870a811caa0d46a6e9bab7c770863bf0277c
gdi2290 <github@gdi2290.com>
2015-05-26 21:53:59 -0700

    docs(ng_for.ts): For => NgFor

--- modules/angular2/src/directives/ng_for.ts
+++ modules/angular2/src/directives/ng_for.ts
@@ -4,14 +4,14 @@ import {isPresent, isBlank} from 'angular2/src/facade/lang';
 import {ListWrapper} from 'angular2/src/facade/collection';
 
 /**
- * The `For` directive instantiates a template once per item from an iterable. The context for each
- * instantiated template inherits from the outer context with the given loop variable set to the
- * current item from the iterable.
+ * The `NgFor` directive instantiates a template once per item from an iterable. The context for
+ * each instantiated template inherits from the outer context with the given loop variable set
+ * to the current item from the iterable.
  *
  * It is possible to alias the `index` to a local variable that will be set to the current loop
  * iteration in the template context.
  *
- * When the contents of the iterator changes, `For` makes the corresponding changes to the DOM:
+ * When the contents of the iterator changes, `NgFor` makes the corresponding changes to the DOM:
  *
  * * When an item is added, a new instance of the template is added to the DOM.
  * * When an item is removed, its template instance is removed from the DOM.
