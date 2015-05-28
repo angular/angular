f58701e68080201240c8ce3bfd1d9bb17914f509
elimach <michael.loibl@gmail.com>
2015-05-28 17:18:49 +0200

    fix(binding): unbalanced curly brackets in documentation

--- modules/angular2/src/di/binding.ts
+++ modules/angular2/src/di/binding.ts
@@ -392,8 +392,8 @@ export class BindingBuilder {
    *
    * ```javascript
    * var injector = Injector.resolveAndCreate([
-   *   bind(Number).toFactory(() => { return 1+2; }}),
-   *   bind(String).toFactory((v) => { return "Value: " + v; }, [Number] })
+   *   bind(Number).toFactory(() => { return 1+2; }),
+   *   bind(String).toFactory((v) => { return "Value: " + v; }, [Number])
    * ]);
    *
    * expect(injector.get(Number)).toEqual(3);
