c571b2693ec87f44c8ad77f16ab3faf1ab014baa
gdi2290 <github@gdi2290.com>
2015-05-29 14:27:46 -0700

    feat(OpaqueToken): now a const constructor
    BREAKING CHANGE:
    
    now a `const` constructor

--- modules/angular2/src/di/opaque_token.ts
+++ modules/angular2/src/di/opaque_token.ts
@@ -1,12 +1,16 @@
+import {CONST} from 'angular2/src/facade/lang';
+
 /**
  *
  *
  * @exportedAs angular2/di
  */
+
+@CONST()
 export class OpaqueToken {
   _desc: string;
 
-  constructor(desc: string) { this._desc = `Token(${desc})`; }
+  constructor(desc: string) { this._desc = 'Token(' + desc + ')'; }
 
   toString(): string { return this._desc; }
 }
