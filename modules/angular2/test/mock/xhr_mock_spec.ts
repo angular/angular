2c5580f1ecc549ca864c18b0d1137df34fc56d70
Ian Riley <ian.riley.915@gmail.com>
2015-06-02 17:41:44 -0700

    refactor(test/mock): Ts'ify angular2/test/mock
    Translates the last test directory -- angular2/test/mock -- to TypeScript.

--- modules/angular2/test/mock/xhr_mock_spec.ts
+++ modules/angular2/test/mock/xhr_mock_spec.ts
@@ -18,63 +18,63 @@ export function main() {
   describe('MockXHR', () => {
     var xhr;
 
-    beforeEach(() => {
-      xhr = new MockXHR();
-    });
+    beforeEach(() => { xhr = new MockXHR(); });
 
-    function expectResponse(request: Promise, url: string, response: string, done = null) {
-      function onResponse(text: string) {
+    function expectResponse(request: Promise<string>, url: string, response: string, done = null) {
+      function onResponse(text: string): string {
         if (response === null) {
           throw `Unexpected response ${url} -> ${text}`;
         } else {
           expect(text).toEqual(response);
           if (isPresent(done)) done();
         }
+        return text;
       }
 
-      function onError(error: string) {
+      function onError(error: string): string {
         if (response !== null) {
           throw `Unexpected error ${url}`;
         } else {
           expect(error).toEqual(`Failed to load ${url}`);
           if (isPresent(done)) done();
         }
+        return error;
       }
 
       PromiseWrapper.then(request, onResponse, onError);
     }
 
     it('should return a response from the definitions', inject([AsyncTestCompleter], (async) => {
-      var url = '/foo';
-      var response = 'bar';
-      xhr.when(url, response);
-      expectResponse(xhr.get(url), url, response, () => async.done());
-      xhr.flush();
-    }));
+         var url = '/foo';
+         var response = 'bar';
+         xhr.when(url, response);
+         expectResponse(xhr.get(url), url, response, () => async.done());
+         xhr.flush();
+       }));
 
     it('should return an error from the definitions', inject([AsyncTestCompleter], (async) => {
-      var url = '/foo';
-      var response = null;
-      xhr.when(url, response);
-      expectResponse(xhr.get(url), url, response, () => async.done());
-      xhr.flush();
-    }));
+         var url = '/foo';
+         var response = null;
+         xhr.when(url, response);
+         expectResponse(xhr.get(url), url, response, () => async.done());
+         xhr.flush();
+       }));
 
     it('should return a response from the expectations', inject([AsyncTestCompleter], (async) => {
-      var url = '/foo';
-      var response = 'bar';
-      xhr.expect(url, response);
-      expectResponse(xhr.get(url), url, response, () => async.done());
-      xhr.flush();
-    }));
+         var url = '/foo';
+         var response = 'bar';
+         xhr.expect(url, response);
+         expectResponse(xhr.get(url), url, response, () => async.done());
+         xhr.flush();
+       }));
 
     it('should return an error from the expectations', inject([AsyncTestCompleter], (async) => {
-      var url = '/foo';
-      var response = null;
-      xhr.expect(url, response);
-      expectResponse(xhr.get(url), url, response, () => async.done());
-      xhr.flush();
-    }));
+         var url = '/foo';
+         var response = null;
+         xhr.expect(url, response);
+         expectResponse(xhr.get(url), url, response, () => async.done());
+         xhr.flush();
+       }));
 
     it('should not reuse expectations', () => {
       var url = '/foo';
@@ -82,40 +82,31 @@ export function main() {
       xhr.expect(url, response);
       xhr.get(url);
       xhr.get(url);
-      expect(() => {
-        xhr.flush();
-      }).toThrowError('Unexpected request /foo');
+      expect(() => { xhr.flush(); }).toThrowError('Unexpected request /foo');
     });
 
     it('should return expectations before definitions', inject([AsyncTestCompleter], (async) => {
-      var url = '/foo';
-      xhr.when(url, 'when');
-      xhr.expect(url, 'expect');
-      expectResponse(xhr.get(url), url, 'expect');
-      expectResponse(xhr.get(url), url, 'when', () => async.done());
-      xhr.flush();
-    }));
+         var url = '/foo';
+         xhr.when(url, 'when');
+         xhr.expect(url, 'expect');
+         expectResponse(xhr.get(url), url, 'expect');
+         expectResponse(xhr.get(url), url, 'when', () => async.done());
+         xhr.flush();
+       }));
 
     it('should throw when there is no definitions or expectations', () => {
       xhr.get('/foo');
-      expect(() => {
-        xhr.flush();
-      }).toThrowError('Unexpected request /foo');
+      expect(() => { xhr.flush(); }).toThrowError('Unexpected request /foo');
     });
 
-    it('should throw when flush is called without any pending requests', () => {
-      expect(() => {
-        xhr.flush();
-      }).toThrowError('No pending requests to flush');
-    });
+    it('should throw when flush is called without any pending requests',
+       () => { expect(() => { xhr.flush(); }).toThrowError('No pending requests to flush'); });
 
     it('should throw on unstatisfied expectations', () => {
       xhr.expect('/foo', 'bar');
       xhr.when('/bar', 'foo');
       xhr.get('/bar');
-      expect(() => {
-        xhr.flush();
-      }).toThrowError('Unsatisfied requests: /foo');
+      expect(() => { xhr.flush(); }).toThrowError('Unsatisfied requests: /foo');
     });
   });
 }
