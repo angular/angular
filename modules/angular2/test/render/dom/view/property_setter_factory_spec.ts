87b3b718e329ccf9cc720d2e0f1b5a10d611daaf
Tobias Bosch <tbosch1009@gmail.com>
2015-06-05 11:10:07 -0700

    refactor(render): don’t use a global cache for property setters
    Related to #2359

--- modules/angular2/test/render/dom/view/property_setter_factory_spec.ts
+++ modules/angular2/test/render/dom/view/property_setter_factory_spec.ts
@@ -9,25 +9,28 @@ import {
   beforeEach,
   el
 } from 'angular2/test_lib';
-import {setterFactory} from 'angular2/src/render/dom/view/property_setter_factory';
+import {PropertySetterFactory} from 'angular2/src/render/dom/view/property_setter_factory';
 import {DOM} from 'angular2/src/dom/dom_adapter';
 
 export function main() {
-  var div;
-  beforeEach(() => { div = el('<div></div>'); });
+  var div, setterFactory;
+  beforeEach(() => {
+    div = el('<div></div>');
+    setterFactory = new PropertySetterFactory();
+  });
   describe('property setter factory', () => {
 
     it('should return a setter for a property', () => {
-      var setterFn = setterFactory('title');
+      var setterFn = setterFactory.createSetter('title');
       setterFn(div, 'Hello');
       expect(div.title).toEqual('Hello');
 
-      var otherSetterFn = setterFactory('title');
+      var otherSetterFn = setterFactory.createSetter('title');
       expect(setterFn).toBe(otherSetterFn);
     });
 
     it('should return a setter for an attribute', () => {
-      var setterFn = setterFactory('attr.role');
+      var setterFn = setterFactory.createSetter('attr.role');
       setterFn(div, 'button');
       expect(DOM.getAttribute(div, 'role')).toEqual('button');
       setterFn(div, null);
@@ -35,49 +38,49 @@ export function main() {
       expect(() => { setterFn(div, 4); })
           .toThrowError("Invalid role attribute, only string values are allowed, got '4'");
 
-      var otherSetterFn = setterFactory('attr.role');
+      var otherSetterFn = setterFactory.createSetter('attr.role');
       expect(setterFn).toBe(otherSetterFn);
     });
 
     it('should return a setter for a class', () => {
-      var setterFn = setterFactory('class.active');
+      var setterFn = setterFactory.createSetter('class.active');
       setterFn(div, true);
       expect(DOM.hasClass(div, 'active')).toEqual(true);
       setterFn(div, false);
       expect(DOM.hasClass(div, 'active')).toEqual(false);
 
-      var otherSetterFn = setterFactory('class.active');
+      var otherSetterFn = setterFactory.createSetter('class.active');
       expect(setterFn).toBe(otherSetterFn);
     });
 
     it('should return a setter for a style', () => {
-      var setterFn = setterFactory('style.width');
+      var setterFn = setterFactory.createSetter('style.width');
       setterFn(div, '40px');
       expect(DOM.getStyle(div, 'width')).toEqual('40px');
       setterFn(div, null);
       expect(DOM.getStyle(div, 'width')).toEqual('');
 
-      var otherSetterFn = setterFactory('style.width');
+      var otherSetterFn = setterFactory.createSetter('style.width');
       expect(setterFn).toBe(otherSetterFn);
     });
 
     it('should return a setter for a style with a unit', () => {
-      var setterFn = setterFactory('style.height.px');
+      var setterFn = setterFactory.createSetter('style.height.px');
       setterFn(div, 40);
       expect(DOM.getStyle(div, 'height')).toEqual('40px');
       setterFn(div, null);
       expect(DOM.getStyle(div, 'height')).toEqual('');
 
-      var otherSetterFn = setterFactory('style.height.px');
+      var otherSetterFn = setterFactory.createSetter('style.height.px');
       expect(setterFn).toBe(otherSetterFn);
     });
 
     it('should return a setter for innerHtml', () => {
-      var setterFn = setterFactory('innerHtml');
+      var setterFn = setterFactory.createSetter('innerHtml');
       setterFn(div, '<span></span>');
       expect(DOM.getInnerHTML(div)).toEqual('<span></span>');
 
-      var otherSetterFn = setterFactory('innerHtml');
+      var otherSetterFn = setterFactory.createSetter('innerHtml');
       expect(setterFn).toBe(otherSetterFn);
     });
 
