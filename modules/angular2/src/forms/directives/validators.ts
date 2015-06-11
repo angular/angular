35197acc1acc3b7a4da8e504a06ed8af9106490c
vsavkin <avix1000@gmail.com>
2015-06-11 11:30:28 -0700

    fix(forms): fixed the selector of NgRequiredValidator

--- modules/angular2/src/forms/directives/validators.ts
+++ modules/angular2/src/forms/directives/validators.ts
@@ -2,7 +2,7 @@ import {Directive} from '../../../angular2';
 import {Validators} from '../validators';
 import {NgControl} from '../directives';
 
-@Directive({selector: '[required]'})
+@Directive({selector: '[required][ng-control],[required][ng-form-control],[required][ng-model]'})
 export class NgRequiredValidator {
   constructor(c: NgControl) {
     c.validator = Validators.compose([c.validator, Validators.required]);
