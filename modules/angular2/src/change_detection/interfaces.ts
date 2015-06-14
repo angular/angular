5a542d8ba80b7c4a6baf98137488055b97d3fef8
Peter Bacon Darwin <pete@bacondarwin.com>
2015-06-15 00:16:10 +0100

    docs(change_detection): fix link to `PreGeneratedChangeDetection`

--- modules/angular2/src/change_detection/interfaces.ts
+++ modules/angular2/src/change_detection/interfaces.ts
@@ -24,7 +24,7 @@ export class ProtoChangeDetector {
  * `JitChangeDetection` strategy at compile time.
  *
  *
- * See: {@link DynamicChangeDetection}, {@link JitChangeDetection}, {@link PregenChangeDetection}
+ * See: {@link DynamicChangeDetection}, {@link JitChangeDetection}, {@link PreGeneratedChangeDetection}
  *
  * # Example
  * ```javascript
