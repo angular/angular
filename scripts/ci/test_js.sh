a0bb0ddc90e8018edfacf3ce9f50d52c1c20b8a2
Igor Minar <igor@angularjs.org>
2015-06-23 10:36:54 -0700

    build(gulp/travis): move circular check and style check to before pre/post-test tasks
    In order to speedup the startup time of test.unit.js task, we are moving the circular dependency check into
    a pre-test check that executes only on travis. Similarly we are moving the style check to a post-test check
    that executes on travis.
    
    This way if a circular dependency issue occurs, we find it before running tests on CI and if the code
    is not formatted we fail the build only if all the tests pass.
    
    Related to #2536
    Related to #2094

--- scripts/ci/test_js.sh
+++ scripts/ci/test_js.sh
@@ -12,5 +12,7 @@ if  ${SCRIPT_DIR}/env_dart.sh 2>&1 > /dev/null ; then
     source $SCRIPT_DIR/env_dart.sh
 fi
 
+./node_modules/.bin/gulp pre-test-checks
 ./node_modules/.bin/gulp test.js --browsers=${KARMA_BROWSERS:-ChromeCanary}
 ${SCRIPT_DIR}/test_e2e_js.sh
+./node_modules/.bin/gulp post-test-checks
