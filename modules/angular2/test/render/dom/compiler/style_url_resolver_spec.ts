305d802cedb724453f4f0eebaa3414334481b63b
Victor Berchet <victor@suumit.com>
2015-06-24 10:27:53 +0200

    test(refactor): cleanup test to use the new UrlMapper

--- modules/angular2/test/render/dom/compiler/style_url_resolver_spec.ts
+++ modules/angular2/test/render/dom/compiler/style_url_resolver_spec.ts
@@ -5,8 +5,13 @@ import {UrlResolver} from 'angular2/src/services/url_resolver';
 
 export function main() {
   describe('StyleUrlResolver', () => {
+    let styleUrlResolver;
+
+    beforeEach(() => {
+      styleUrlResolver = new StyleUrlResolver(new UrlResolver());
+    });
+
     it('should resolve "url()" urls', () => {
-      var styleUrlResolver = new StyleUrlResolver(new FakeUrlResolver());
       var css = `
       .foo {
         background-image: url("double.jpg");
@@ -15,64 +20,57 @@ export function main() {
       }`;
       var expectedCss = `
       .foo {
-        background-image: url('base/double.jpg');
-        background-image: url('base/simple.jpg');
-        background-image: url('base/noquote.jpg');
+        background-image: url('http://ng.io/double.jpg');
+        background-image: url('http://ng.io/simple.jpg');
+        background-image: url('http://ng.io/noquote.jpg');
       }`;
 
-      var resolvedCss = styleUrlResolver.resolveUrls(css, 'base');
+      var resolvedCss = styleUrlResolver.resolveUrls(css, 'http://ng.io');
       expect(resolvedCss).toEqual(expectedCss);
     });
 
     it('should resolve "@import" urls', () => {
-      var styleUrlResolver = new StyleUrlResolver(new FakeUrlResolver());
       var css = `
       @import '1.css';
       @import "2.css";
       `;
       var expectedCss = `
-      @import 'base/1.css';
-      @import 'base/2.css';
+      @import 'http://ng.io/1.css';
+      @import 'http://ng.io/2.css';
       `;
 
-      var resolvedCss = styleUrlResolver.resolveUrls(css, 'base');
+      var resolvedCss = styleUrlResolver.resolveUrls(css, 'http://ng.io');
       expect(resolvedCss).toEqual(expectedCss);
     });
 
     it('should resolve "@import url()" urls', () => {
-      var styleUrlResolver = new StyleUrlResolver(new FakeUrlResolver());
       var css = `
       @import url('3.css');
       @import url("4.css");
       @import url(5.css);
       `;
       var expectedCss = `
-      @import url('base/3.css');
-      @import url('base/4.css');
-      @import url('base/5.css');
+      @import url('http://ng.io/3.css');
+      @import url('http://ng.io/4.css');
+      @import url('http://ng.io/5.css');
       `;
 
-      var resolvedCss = styleUrlResolver.resolveUrls(css, 'base');
+      var resolvedCss = styleUrlResolver.resolveUrls(css, 'http://ng.io');
       expect(resolvedCss).toEqual(expectedCss);
     });
 
     it('should support media query in "@import"', () => {
-      var styleUrlResolver = new StyleUrlResolver(new FakeUrlResolver());
       var css = `
       @import 'print.css' print;
       @import url(print.css) print;
       `;
       var expectedCss = `
-      @import 'base/print.css' print;
-      @import url('base/print.css') print;
+      @import 'http://ng.io/print.css' print;
+      @import url('http://ng.io/print.css') print;
       `;
 
-      var resolvedCss = styleUrlResolver.resolveUrls(css, 'base');
+      var resolvedCss = styleUrlResolver.resolveUrls(css, 'http://ng.io');
       expect(resolvedCss).toEqual(expectedCss);
     });
   });
-}
-
-class FakeUrlResolver extends UrlResolver {
-  resolve(baseUrl: string, url: string): string { return baseUrl + '/' + url; }
 }
