7e5b11caccdcefe1a96c2e34c97d81d4f56f2d5e
Pascal Precht <pascal.precht@googlemail.com>
2015-06-24 10:19:09 +0200

    fix(api): export missing shadow dom strategies
    Can be merged once decided in #2688
    
    Closes #2688

--- modules/angular2/angular2.ts
+++ modules/angular2/angular2.ts
@@ -75,3 +75,5 @@ export * from './http';
 export {Observable, EventEmitter} from 'angular2/src/facade/async';
 export * from 'angular2/src/render/api';
 export {DomRenderer, DOCUMENT_TOKEN} from 'angular2/src/render/dom/dom_renderer';
+export {EmulatedScopedShadowDomStrategy} from 'angular2/src/render/dom/shadow_dom/emulated_scoped_shadow_dom_strategy';
+export {NativeShadowDomStrategy} from 'angular2/src/render/dom/shadow_dom/native_shadow_dom_strategy';
