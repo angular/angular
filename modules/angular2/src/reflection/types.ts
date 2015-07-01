1c9573562e81d85f6566410404b2a17a5832ec8e
Alex Eagle <alexeagle@google.com>
2015-06-30 18:10:14 -0700

    chore(typing): restore some defn's now that dgeni is fixed
    fixes #2446

--- modules/angular2/src/reflection/types.ts
+++ modules/angular2/src/reflection/types.ts
@@ -1,13 +1,6 @@
 import {Type} from 'angular2/src/facade/lang';
 import {List} from 'angular2/src/facade/collection';
 
-export {Function as GetterFn};
-export {Function as SetterFn};
-export {Function as MethodFn};
-
-// TODO replace once dgeni is fixed
-/**
 export type SetterFn = (obj: any, value: any) => void;
 export type GetterFn = (obj: any) => any;
 export type MethodFn = (obj: any, args: List<any>) => any;
-**/
