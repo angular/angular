e5405e4ba2465b36f72b7233ca36039b428bf40b
gdi2290 <github@gdi2290.com>
2015-07-02 14:20:50 -0700

    fix(forms): Remove cyclic dependency
    correctly resolve Renderer by removing cyclic dependencies
    
    Closes #2856

--- modules/angular2/src/forms/directives/ng_control_group.ts
+++ modules/angular2/src/forms/directives/ng_control_group.ts
@@ -1,4 +1,4 @@
-import {Directive, onDestroy, onInit} from 'angular2/angular2';
+import {Directive, onDestroy, onInit} from 'angular2/annotations';
 import {Inject, Ancestor, forwardRef, Binding} from 'angular2/di';
 import {List, ListWrapper} from 'angular2/src/facade/collection';
 import {CONST_EXPR} from 'angular2/src/facade/lang';
