f7dfd2325da2a08cac25e9130362a977182d4b30
Rado Kirov <radokirov@google.com>
2015-07-14 16:06:33 -0700

    chore(query): refactor QueryList and BaseQueryList.
    Closes #3035, #3016

--- modules/angular2/core.ts
+++ modules/angular2/core.ts
@@ -15,8 +15,8 @@ export {DirectiveResolver} from 'angular2/src/core/compiler/directive_resolver';
 export {Compiler} from 'angular2/src/core/compiler/compiler';
 
 export {AppViewManager} from 'angular2/src/core/compiler/view_manager';
+export {IQueryList} from 'angular2/src/core/compiler/interface_query';
 export {QueryList} from 'angular2/src/core/compiler/query_list';
-export {BaseQueryList} from 'angular2/src/core/compiler/base_query_list';
 export {ElementRef} from 'angular2/src/core/compiler/element_ref';
 export {RenderElementRef} from 'angular2/src/render/api';
 export {ViewRef, ProtoViewRef} from 'angular2/src/core/compiler/view_ref';
