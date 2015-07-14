427a4dac7add426c0399f08cc85d0386b9786ca2
Sigmund Cherem <sigmund@google.com>
2015-07-14 16:04:49 -0700

    fix(examples): add a couple entrypoints, adjust pubspec, fix change detector bug in Dart

--- modules/examples/src/sourcemap/index.ts
+++ modules/examples/src/sourcemap/index.ts
@@ -1,5 +1,7 @@
 import {BaseException} from 'angular2/src/facade/lang';
 import {bootstrap, Component, View} from 'angular2/angular2';
+import {reflector} from 'angular2/src/reflection/reflection';
+import {ReflectionCapabilities} from 'angular2/src/reflection/reflection_capabilities';
 
 @Component({
   selector: 'error-app',
@@ -13,5 +15,6 @@ export class ErrorComponent {
 }
 
 export function main() {
+  reflector.reflectionCapabilities = new ReflectionCapabilities();  // for the Dart version
   bootstrap(ErrorComponent);
 }
