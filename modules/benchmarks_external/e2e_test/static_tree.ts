854b5b7da85ae25d22979b47e63a54eade45f8a7
Tobias Bosch <tbosch1009@gmail.com>
2015-07-21 15:28:10 -0700

    feat(benchmark): add static_tree benchmark
    Static binary component tree of depth 10, i.e. 1024 components.
    
    Current numbers for `pureScriptTime` are:
    
    JavaScript:
    
    Baseline: 27.10+-9%
    Ng2: 26.84+-8%
    Ng1: 55.30+-14%
    
    Dart:
    
    Baseline: 30.13+-4%
    Ng2: 45.94+-3%
    Ng1: 128.88+-10%
    
    I.e. in JS we are same speed as baseline right now!
    
    Some background: We had a recent change in the compiler that merges components into their parents already during compilation (#2529). This made Ng2 2x faster in this benchmark (before the Ng2 JS time was 49.59+-14%ms).
    
    Closes #3196

---
+++ modules/benchmarks_external/e2e_test/static_tree.ts
@@ -0,0 +1,15 @@
+import {runClickBenchmark, verifyNoBrowserErrors} from 'angular2/src/test_lib/perf_util';
+
+describe('ng1.x tree benchmark', function() {
+
+  var URL = 'benchmarks_external/src/static_tree/tree_benchmark.html';
+
+  afterEach(verifyNoBrowserErrors);
+
+  it('should log the stats', function(done) {
+    runClickBenchmark(
+        {url: URL, buttons: ['#destroyDom', '#createDom'], id: 'ng1.static.tree', params: []})
+        .then(done, done.fail);
+  });
+
+});
