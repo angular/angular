c5f8c9586ff188ed7d13ae8e95245b7a972bb4a7
Misko Hevery <misko@hevery.com>
2015-07-27 17:00:01 -0700

    chore: added presubmit-queue to travis

---
+++ scripts/ci/after-script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e -o pipefail
+
+echo '*******************'
+echo '** AFTER_SUCCESS **'
+echo '*******************'
+
+
+echo '---------------------'
+echo '-- WAIT FOR OTHERS --'
+echo '---------------------'
+
+curl -Lo travis_after_all.py https://raw.github.com/jbdeboer/travis_after_all/master/travis_after_all.py
+python travis_after_all.py
+. .to_export_back
+
+echo BUILD_LEADER=$BUILD_LEADER
+echo BUILD_AGGREGATE_STATUS=$BUILD_AGGREGATE_STATUS
+
+if [ "$BUILD_LEADER" = "YES" ]; then
+  if [ "$BUILD_AGGREGATE_STATUS" = "others_succeeded" ]; then
+    ./scripts/ci/presubmit-queue-success.sh
+  else
+    echo "ERROR: Some Failed, not submitting"
+  fi
+else
+  echo "ERROR: Other builds have not finished, not submitting"
+fi
