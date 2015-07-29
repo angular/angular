70bc485755e4bc65c27c2fa8998618cb780ac931
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-07-29 16:02:14 +0200

    fix(browser_adapter): fix clearNodes() in IE
    Fixes #3295
    
    Closes #3355

--- modules/angular2/src/dom/browser_adapter.ts
+++ modules/angular2/src/dom/browser_adapter.ts
@@ -130,14 +130,14 @@ export class BrowserDomAdapter extends GenericBrowserDomAdapter {
   }
   clearNodes(el) {
     while (el.firstChild) {
-      el.firstChild.remove();
+      el.removeChild(el.firstChild);
     }
   }
   appendChild(el, node) { el.appendChild(node); }
   removeChild(el, node) { el.removeChild(node); }
   replaceChild(el: Node, newChild, oldChild) { el.replaceChild(newChild, oldChild); }
   remove(node): Node {
-    node.remove();
+    node.parentNode.removeChild(node);
     return node;
   }
   insertBefore(el, node) { el.parentNode.insertBefore(node, el); }
