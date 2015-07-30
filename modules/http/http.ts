5725f717774dbd6b53de3d62fe4032d43b6fb29e
Pascal Precht <pascal.precht@googlemail.com>
2015-07-30 16:29:16 +0200

    fix(http): allow using JSONP_INJECTABLES and HTTP_INJECTABLES in same injector
    Fixes #3365
    
    Closes #3390

--- modules/http/http.ts
+++ modules/http/http.ts
@@ -62,19 +62,26 @@ export {URLSearchParams} from './src/url_search_params';
  *
  */
 export const HTTP_BINDINGS: List<any> = [
-  bind(ConnectionBackend)
-      .toClass(XHRBackend),
+  // TODO(pascal): use factory type annotations once supported in DI
+  // issue: https://github.com/angular/angular/issues/3183
+  bind(Http)
+      .toFactory((xhrBackend, requestOptions) => { return new Http(xhrBackend, requestOptions);},
+                 [XHRBackend, RequestOptions]),
   BrowserXhr,
   bind(RequestOptions).toClass(BaseRequestOptions),
   bind(ResponseOptions).toClass(BaseResponseOptions),
-  Http
+  XHRBackend
 ];
 
 export const JSONP_BINDINGS: List<any> = [
-  bind(ConnectionBackend)
-      .toClass(JSONPBackend),
+  // TODO(pascal): use factory type annotations once supported in DI
+  // issue: https://github.com/angular/angular/issues/3183
+  bind(Jsonp)
+      .toFactory(
+          (jsonpBackend, requestOptions) => { return new Jsonp(jsonpBackend, requestOptions);},
+          [JSONPBackend, RequestOptions]),
   BrowserJsonp,
   bind(RequestOptions).toClass(BaseRequestOptions),
   bind(ResponseOptions).toClass(BaseResponseOptions),
-  Jsonp
+  JSONPBackend
 ];
