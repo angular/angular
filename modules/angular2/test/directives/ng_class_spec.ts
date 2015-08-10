ed25a29cc8f50ae9e21ee49941c7c574b4b41b9e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-08-10 12:25:46 +0200

    fix(NgClass): take initial classes into account during cleanup
    Closes #3557

--- modules/angular2/test/directives/ng_class_spec.ts
+++ modules/angular2/test/directives/ng_class_spec.ts
@@ -130,6 +130,25 @@ export function main() {
                  async.done();
                });
          }));
+
+      it('should remove active classes when expression evaluates to null',
+         inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
+           var template = '<div [ng-class]="objExpr"></div>';
+
+           tcb.overrideTemplate(TestComponent, template)
+               .createAsync(TestComponent)
+               .then((rootTC) => {
+                 detectChangesAndCheck(rootTC, 'ng-binding foo');
+
+                 rootTC.componentInstance.objExpr = null;
+                 detectChangesAndCheck(rootTC, 'ng-binding');
+
+                 rootTC.componentInstance.objExpr = {'foo': false, 'bar': true};
+                 detectChangesAndCheck(rootTC, 'ng-binding bar');
+
+                 async.done();
+               });
+         }));
     });
 
     describe('expressions evaluating to lists', () => {
@@ -184,6 +203,22 @@ export function main() {
                  async.done();
                });
          }));
+
+      it('should take initial classes into account when a reference changes',
+         inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
+           var template = '<div class="foo" [ng-class]="arrExpr"></div>';
+
+           tcb.overrideTemplate(TestComponent, template)
+               .createAsync(TestComponent)
+               .then((rootTC) => {
+                 detectChangesAndCheck(rootTC, 'foo ng-binding');
+
+                 rootTC.componentInstance.arrExpr = ['bar'];
+                 detectChangesAndCheck(rootTC, 'ng-binding foo bar');
+
+                 async.done();
+               });
+         }));
     });
 
     describe('expressions evaluating to string', () => {
@@ -220,7 +255,6 @@ export function main() {
                });
          }));
 
-
       it('should remove active classes when switching from string to null',
          inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
            var template = `<div [ng-class]="strExpr"></div>`;
@@ -236,65 +270,133 @@ export function main() {
                  async.done();
                });
          }));
+
+      it('should take initial classes into account when switching from string to null',
+         inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
+           var template = `<div class="foo" [ng-class]="strExpr"></div>`;
+
+           tcb.overrideTemplate(TestComponent, template)
+               .createAsync(TestComponent)
+               .then((rootTC) => {
+                 detectChangesAndCheck(rootTC, 'foo ng-binding');
+
+                 rootTC.componentInstance.strExpr = null;
+                 detectChangesAndCheck(rootTC, 'ng-binding foo');
+
+                 async.done();
+               });
+         }));
+
     });
 
-    it('should remove active classes when expression evaluates to null',
-       inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
-         var template = '<div [ng-class]="objExpr"></div>';
+    describe('cooperation with other class-changing constructs', () => {
 
-         tcb.overrideTemplate(TestComponent, template)
-             .createAsync(TestComponent)
-             .then((rootTC) => {
-               detectChangesAndCheck(rootTC, 'ng-binding foo');
+      it('should co-operate with the class attribute',
+         inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
+           var template = '<div [ng-class]="objExpr" class="init foo"></div>';
 
-               rootTC.componentInstance.objExpr = null;
-               detectChangesAndCheck(rootTC, 'ng-binding');
+           tcb.overrideTemplate(TestComponent, template)
+               .createAsync(TestComponent)
+               .then((rootTC) => {
+                 StringMapWrapper.set(rootTC.componentInstance.objExpr, 'bar', true);
+                 detectChangesAndCheck(rootTC, 'init foo ng-binding bar');
 
-               rootTC.componentInstance.objExpr = {'foo': false, 'bar': true};
-               detectChangesAndCheck(rootTC, 'ng-binding bar');
+                 StringMapWrapper.set(rootTC.componentInstance.objExpr, 'foo', false);
+                 detectChangesAndCheck(rootTC, 'init ng-binding bar');
 
-               async.done();
-             });
-       }));
+                 rootTC.componentInstance.objExpr = null;
+                 detectChangesAndCheck(rootTC, 'init ng-binding foo');
 
-    it('should co-operate with the class attribute',
-       inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
-         var template = '<div [ng-class]="objExpr" class="init foo"></div>';
+                 async.done();
+               });
+         }));
 
-         tcb.overrideTemplate(TestComponent, template)
-             .createAsync(TestComponent)
-             .then((rootTC) => {
-               StringMapWrapper.set(rootTC.componentInstance.objExpr, 'bar', true);
-               detectChangesAndCheck(rootTC, 'init foo ng-binding bar');
+      it('should co-operate with the interpolated class attribute',
+         inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
+           var template = `<div [ng-class]="objExpr" class="{{'init foo'}}"></div>`;
 
-               StringMapWrapper.set(rootTC.componentInstance.objExpr, 'foo', false);
-               detectChangesAndCheck(rootTC, 'init ng-binding bar');
+           tcb.overrideTemplate(TestComponent, template)
+               .createAsync(TestComponent)
+               .then((rootTC) => {
+                 StringMapWrapper.set(rootTC.componentInstance.objExpr, 'bar', true);
+                 detectChangesAndCheck(rootTC, `{{'init foo'}} ng-binding init foo bar`);
 
-               async.done();
-             });
-       }));
+                 StringMapWrapper.set(rootTC.componentInstance.objExpr, 'foo', false);
+                 detectChangesAndCheck(rootTC, `{{'init foo'}} ng-binding init bar`);
 
-    it('should co-operate with the class attribute and class.name binding',
-       inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
-         var template = '<div class="init foo" [ng-class]="objExpr" [class.baz]="condition"></div>';
+                 rootTC.componentInstance.objExpr = null;
+                 detectChangesAndCheck(rootTC, `{{'init foo'}} ng-binding init foo`);
 
-         tcb.overrideTemplate(TestComponent, template)
-             .createAsync(TestComponent)
-             .then((rootTC) => {
-               detectChangesAndCheck(rootTC, 'init foo ng-binding baz');
+                 async.done();
+               });
+         }));
 
-               StringMapWrapper.set(rootTC.componentInstance.objExpr, 'bar', true);
-               detectChangesAndCheck(rootTC, 'init foo ng-binding baz bar');
+      it('should co-operate with the class attribute and binding to it',
+         inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
+           var template = `<div [ng-class]="objExpr" class="init" [class]="'foo'"></div>`;
 
-               StringMapWrapper.set(rootTC.componentInstance.objExpr, 'foo', false);
-               detectChangesAndCheck(rootTC, 'init ng-binding baz bar');
+           tcb.overrideTemplate(TestComponent, template)
+               .createAsync(TestComponent)
+               .then((rootTC) => {
+                 StringMapWrapper.set(rootTC.componentInstance.objExpr, 'bar', true);
+                 detectChangesAndCheck(rootTC, `init ng-binding foo bar`);
 
-               rootTC.componentInstance.condition = false;
-               detectChangesAndCheck(rootTC, 'init ng-binding bar');
+                 StringMapWrapper.set(rootTC.componentInstance.objExpr, 'foo', false);
+                 detectChangesAndCheck(rootTC, `init ng-binding bar`);
 
-               async.done();
-             });
-       }));
+                 rootTC.componentInstance.objExpr = null;
+                 detectChangesAndCheck(rootTC, `init ng-binding foo`);
+
+                 async.done();
+               });
+         }));
+
+      it('should co-operate with the class attribute and class.name binding',
+         inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
+           var template =
+               '<div class="init foo" [ng-class]="objExpr" [class.baz]="condition"></div>';
+
+           tcb.overrideTemplate(TestComponent, template)
+               .createAsync(TestComponent)
+               .then((rootTC) => {
+                 detectChangesAndCheck(rootTC, 'init foo ng-binding baz');
+
+                 StringMapWrapper.set(rootTC.componentInstance.objExpr, 'bar', true);
+                 detectChangesAndCheck(rootTC, 'init foo ng-binding baz bar');
+
+                 StringMapWrapper.set(rootTC.componentInstance.objExpr, 'foo', false);
+                 detectChangesAndCheck(rootTC, 'init ng-binding baz bar');
+
+                 rootTC.componentInstance.condition = false;
+                 detectChangesAndCheck(rootTC, 'init ng-binding bar');
+
+                 async.done();
+               });
+         }));
+
+      it('should co-operate with initial class and class attribute binding when binding changes',
+         inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
+           var template = '<div class="init" [ng-class]="objExpr" [class]="strExpr"></div>';
+
+           tcb.overrideTemplate(TestComponent, template)
+               .createAsync(TestComponent)
+               .then((rootTC) => {
+                 detectChangesAndCheck(rootTC, 'init ng-binding foo');
+
+                 StringMapWrapper.set(rootTC.componentInstance.objExpr, 'bar', true);
+                 detectChangesAndCheck(rootTC, 'init ng-binding foo bar');
+
+                 rootTC.componentInstance.strExpr = 'baz';
+                 detectChangesAndCheck(rootTC, 'init ng-binding bar baz foo');
+
+                 rootTC.componentInstance.objExpr = null;
+                 detectChangesAndCheck(rootTC, 'init ng-binding baz');
+
+                 async.done();
+               });
+         }));
+
+    });
   })
 }
 
