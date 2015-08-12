3e06a50a2d04675524470b54e986c0dba2370541
Misko Hevery <misko@hevery.com>
2015-08-12 13:03:28 -0700

    chore: disable dart for HTTP package
    BREAKING CHANGE
    
    Stop supporting http module in Dart. This is because Dart has a
    well developed http package which should be used by Dart
    customers instead.

--- modules/http/src/http_utils.dart
+++
@@ -1,8 +0,0 @@
-library angular2_http.src.http_utils;
-
-import 'dart:js' show JsObject;
-import 'dart:collection' show LinkedHashMap, LinkedHashSet;
-
-bool isJsObject(o) {
-  return o is JsObject || o is LinkedHashMap || o is LinkedHashSet;
-}
