b8be4bfaafbca2f8f30a500753d8bc5458065aa6
Misko Hevery <misko@hevery.com>
2015-08-18 15:37:27 -0700

    fix(router): re-export of Type
    Closes #3632
    
    Closes #3704

--- modules/angular2/router.ts
+++ modules/angular2/router.ts
@@ -21,7 +21,7 @@ export {OnActivate, OnDeactivate, OnReuse, CanDeactivate, CanReuse} from './src/
 export {CanActivate} from './src/router/lifecycle_annotations';
 export {Instruction, ComponentInstruction} from './src/router/instruction';
 export {Url} from './src/router/url_parser';
-export {OpaqueToken, Type} from 'angular2/angular2';
+export {OpaqueToken} from 'angular2/angular2';
 export {ROUTE_DATA} from './src/router/route_data';
 
 import {LocationStrategy} from './src/router/location_strategy';
