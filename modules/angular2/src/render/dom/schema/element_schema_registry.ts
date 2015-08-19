10dfac926ee056e155e8cd91188584ae308e5e84
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-08-19 22:10:46 +0200

    feat(ElementSchemaRegistry): introduce schema for event names

--- modules/angular2/src/render/dom/schema/element_schema_registry.ts
+++ modules/angular2/src/render/dom/schema/element_schema_registry.ts
@@ -1,4 +1,5 @@
 export class ElementSchemaRegistry {
   hasProperty(elm: any, propName: string): boolean { return true; }
   getMappedPropName(propName: string): string { return propName; }
+  hasEvent(elm: any, eventName: string): boolean { return true; }
 }
