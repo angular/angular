0ff613205f147542418a934be7a9388d72b37660
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-08-19 22:10:46 +0200

    feat(ElementSchemaRegistry): introduce schema for event names

--- modules/angular2/test/render/dom/schema/dom_element_schema_registry_spec.ts
+++ modules/angular2/test/render/dom/schema/dom_element_schema_registry_spec.ts
@@ -25,24 +25,47 @@ export function main() {
 
   describe('DOMElementSchema', () => {
 
-    it('should detect properties on regular elements', () => {
-      var divEl = DOM.createElement('div');
-      expect(registry.hasProperty(divEl, 'id')).toBeTruthy();
-      expect(registry.hasProperty(divEl, 'title')).toBeTruthy();
-      expect(registry.hasProperty(divEl, 'unknown')).toBeFalsy();
+    describe('properties', () => {
+
+      it('should detect properties on regular elements', () => {
+        var divEl = DOM.createElement('div');
+        expect(registry.hasProperty(divEl, 'id')).toBeTruthy();
+        expect(registry.hasProperty(divEl, 'title')).toBeTruthy();
+        expect(registry.hasProperty(divEl, 'unknown')).toBeFalsy();
+      });
+
+      it('should return true for custom-like elements', () => {
+        var customLikeEl = DOM.createElement('custom-like');
+        expect(registry.hasProperty(customLikeEl, 'unknown')).toBeTruthy();
+      });
+
+      it('should re-map property names that are specified in DOM facade',
+         () => { expect(registry.getMappedPropName('readonly')).toEqual('readOnly'); });
+
+      it('should not re-map property names that are not specified in DOM facade', () => {
+        expect(registry.getMappedPropName('title')).toEqual('title');
+        expect(registry.getMappedPropName('exotic-unknown')).toEqual('exotic-unknown');
+      });
+
     });
 
-    it('should return true for custom-like elements', () => {
-      var customLikeEl = DOM.createElement('custom-like');
-      expect(registry.hasProperty(customLikeEl, 'unknown')).toBeTruthy();
+    describe('event names', () => {
+
+      it('should allow letters only for "standard" (not custom-like) elements', () => {
+        var divEl = DOM.createElement('div');
+        expect(registry.hasEvent(divEl, 'click')).toBeTruthy();
+        expect(registry.hasEvent(divEl, 'fancy-with-numbers-1234')).toBeFalsy();
+      });
+
+      it('should allow alphanumeric and some special chars for custom-like elements', () => {
+        var customLikeEl = DOM.createElement('custom-like');
+        expect(registry.hasEvent(customLikeEl, 'click')).toBeTruthy();
+        expect(registry.hasEvent(customLikeEl, 'fancy-with-numbers-1234')).toBeTruthy();
+        expect(registry.hasEvent(customLikeEl, 'fancy-with-special-chars-_$')).toBeTruthy();
+        expect(registry.hasEvent(customLikeEl, 'disallowed-chars-[]')).toBeFalsy();
+        expect(registry.hasEvent(customLikeEl, 'disallowed-chars-()')).toBeFalsy();
+      });
     });
 
-    it('should not re-map property names that are not specified in DOM facade',
-       () => { expect(registry.getMappedPropName('readonly')).toEqual('readOnly'); });
-
-    it('should not re-map property names that are not specified in DOM facade', () => {
-      expect(registry.getMappedPropName('title')).toEqual('title');
-      expect(registry.getMappedPropName('exotic-unknown')).toEqual('exotic-unknown');
-    });
   });
 }
