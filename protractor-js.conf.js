09e0b0ac6ffa39844303b61eba2dae4cbb11debe
Brian Ford <btford@umich.edu>
2015-08-20 11:17:36 -0700

    chore(benchmarks): disable broken benchmarks
    Remove once https://github.com/angular/angular/issues/3757 is resolved

--- protractor-js.conf.js
+++ protractor-js.conf.js
@@ -5,6 +5,10 @@ config.baseUrl = 'http://localhost:8001/';
 // TODO: remove exclusion when JS verison of scrolling benchmark is available
 config.exclude.push('dist/js/cjs/benchmarks_external/e2e_test/naive_infinite_scroll_spec.js');
 config.exclude.push('dist/js/cjs/benchmarks_external/e2e_test/naive_infinite_scroll_perf.js');
+// TODO: remove once https://github.com/angular/angular/issues/3757 is resolved
+config.exclude.push('dist/js/cjs/benchmarks_external/e2e_test/compiler_perf.js');
+config.exclude.push('dist/js/cjs/benchmarks_external/e2e_test/naive_infinite_scroll.js');
+config.exclude.push('dist/js/cjs/benchmarks_external/e2e_test/tree.js');
 
 data.createBenchpressRunner({ lang: 'js' });
 
