d2a3b76a71d4f1513a4660a6b27f6832b5149502
Alex Eagle <alexeagle@google.com>
2015-08-21 12:14:20 -0700

    chore(build): change comment to indicate the temporary fix is permanent.
    Fixes #3770
    
    Closes #3775

--- tools/broccoli/broccoli-ts2dart.ts
+++ tools/broccoli/broccoli-ts2dart.ts
@@ -20,7 +20,10 @@ class TSToDartTranspiler implements DiffingBroccoliPlugin {
   }
 
   rebuild(treeDiff: DiffResult) {
-    // replace with toEmit = [] once https://github.com/angular/angular/issues/3770 is fixed
+    // Matches rootFilePaths in node_tree.ts
+    // These files are not compatible with Typescript's ES6 library
+    // so they must be explicitly included when targetting ES5, as ts2dart does.
+    // see https://github.com/angular/angular/issues/3770
     let toEmit = [path.resolve(this.inputPath, 'angular2/traceur-runtime.d.ts')];
     let getDartFilePath = (path: string) => path.replace(/((\.js)|(\.ts))$/i, '.dart');
     treeDiff.addedPaths.concat(treeDiff.changedPaths)
