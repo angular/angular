e72305e685d8e4c9e66a77696b1e18470916b76e
Marc Laval <mlaval@amadeus.com>
2015-08-25 19:51:57 +0200

    fix(build): do not run build/pubbuild.dart twice
    Closes #3831

--- scripts/ci/test_e2e_dart.sh
+++ scripts/ci/test_e2e_dart.sh
@@ -16,7 +16,7 @@ function killServer () {
 # So we do this only for post-commit testing.
 # Pull requests test with Dartium and pub serve
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  ./node_modules/.bin/gulp build/pubbuild.dart
+  # WARNING: the build/pubbuild.dart task is assumed to have been run before, in test_server_dart.sh 
   ./node_modules/.bin/gulp serve.js.dart2js&
   serverPid=$!
 else
