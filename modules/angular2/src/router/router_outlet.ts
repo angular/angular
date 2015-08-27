65e34c017314e6093769fb3da36b6ccb616fdc49
Mohamed Hegazy <mhegazy@microsoft.com>
2015-08-27 09:16:21 -0700

    Cast to Promise<any>

--- modules/angular2/src/router/router_outlet.ts
+++ modules/angular2/src/router/router_outlet.ts
@@ -152,10 +152,11 @@ export class RouterOutlet {
   deactivate(nextInstruction: Instruction): Promise<any> {
     var outletInstruction = this._getInstruction(nextInstruction);
     var componentInstruction = isPresent(outletInstruction) ? outletInstruction.component : null;
-    return (isPresent(this.childRouter) ?
+
+    return (<Promise<any>>(isPresent(this.childRouter) ?
                 this.childRouter.deactivate(isPresent(outletInstruction) ? outletInstruction.child :
                                                                            null) :
-                PromiseWrapper.resolve(true))
+                PromiseWrapper.resolve(true)))
         .then((_) => {
           if (isPresent(this._componentRef) && isPresent(this._currentInstruction) &&
               hasLifecycleHook(hookMod.onDeactivate, this._currentInstruction.componentType)) {
