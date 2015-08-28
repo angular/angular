b0711c64dcb54bcf5740fda9969573c8829a3665
Jeff Cross <middlefloor@gmail.com>
2015-08-27 17:26:25 -0700

    chore: add imports for core sans renderer
    This is so that a core bundle can be completely self-sufficient when
    using the standard DOM renderer, but for cases where another renderer
    should be used, all of the core imports except for the renderer can
    be easily imported from one module/bundle.

---
+++ modules/angular2/core_no_render.dart
@@ -0,0 +1,41 @@
+library angular2.core_no_render;
+
+// DI
+export 'package:angular2/src/core/di/decorators.dart';
+export 'package:angular2/src/core/di/metadata.dart' show InjectMetadata, OptionalMetadata, InjectableMetadata, SelfMetadata, HostMetadata, SkipSelfMetadata, DependencyMetadata;
+export 'package:angular2/src/core/di/forward_ref.dart' show forwardRef, resolveForwardRef, ForwardRefFn;
+export 'package:angular2/src/core/di/injector.dart' show Injector, ProtoInjector, BindingWithVisibility, DependencyProvider, Visibility, UNDEFINED;
+export 'package:angular2/src/core/di/binding.dart' show Binding, BindingBuilder, ResolvedBinding, Dependency, bind;
+export 'package:angular2/src/core/di/key.dart' show Key, KeyRegistry, TypeLiteral;
+export 'package:angular2/src/core/di/exceptions.dart' show NoBindingError, AbstractBindingError, CyclicDependencyError, InstantiationError, InvalidBindingError, NoAnnotationError, OutOfBoundsError;
+export 'package:angular2/src/core/di/opaque_token.dart' show OpaqueToken;
+
+export 'package:angular2/src/core/application_tokens.dart' show APP_COMPONENT;
+export 'package:angular2/src/core/application_ref.dart' show ApplicationRef;
+
+// Compiler Related Dependencies.
+export 'package:angular2/src/core/services/app_root_url.dart' show AppRootUrl;
+export 'package:angular2/src/core/services/url_resolver.dart' show UrlResolver;
+export 'package:angular2/src/core/compiler/component_url_mapper.dart'
+    show ComponentUrlMapper;
+export 'package:angular2/src/core/compiler/directive_resolver.dart'
+    show DirectiveResolver;
+export 'package:angular2/src/core/compiler/compiler.dart' show Compiler;
+
+export 'package:angular2/src/core/compiler/view_manager.dart' show AppViewManager;
+export 'package:angular2/src/core/compiler/query_list.dart' show QueryList;
+export 'package:angular2/src/core/compiler/dynamic_component_loader.dart'
+    show DynamicComponentLoader;
+export 'package:angular2/src/core/life_cycle/life_cycle.dart' show LifeCycle;
+
+export 'package:angular2/src/core/compiler/element_ref.dart' show ElementRef;
+export 'package:angular2/src/core/compiler/template_ref.dart' show TemplateRef;
+export 'package:angular2/src/core/compiler/view_ref.dart'
+    show ViewRef, HostViewRef, ProtoViewRef;
+export 'package:angular2/src/core/compiler/view_container_ref.dart'
+    show ViewContainerRef;
+export 'package:angular2/src/core/compiler/dynamic_component_loader.dart'
+    show ComponentRef;
+
+export 'package:angular2/src/core/zone/ng_zone.dart' show NgZone;
+export 'package:angular2/src/core/facade/async.dart' show Stream, EventEmitter;
---
+++ modules/angular2/core_no_render.ts
@@ -0,0 +1,66 @@
+/**
+ * @module
+ * @description
+ * Define angular core API here.
+ */
+// DI
+export {
+  InjectMetadata,
+  OptionalMetadata,
+  InjectableMetadata,
+  SelfMetadata,
+  HostMetadata,
+  SkipSelfMetadata,
+  DependencyMetadata
+} from './src/core/di/metadata';
+
+// we have to reexport * because Dart and TS export two different sets of types
+export * from './src/core/di/decorators';
+
+export {forwardRef, resolveForwardRef, ForwardRefFn} from './src/core/di/forward_ref';
+export {
+  Injector,
+  ProtoInjector,
+  BindingWithVisibility,
+  DependencyProvider,
+  Visibility,
+  UNDEFINED
+} from './src/core/di/injector';
+export {Binding, BindingBuilder, ResolvedBinding, Dependency, bind} from './src/core/di/binding';
+export {Key, KeyRegistry, TypeLiteral} from './src/core/di/key';
+export {
+  NoBindingError,
+  AbstractBindingError,
+  CyclicDependencyError,
+  InstantiationError,
+  InvalidBindingError,
+  NoAnnotationError,
+  OutOfBoundsError
+} from './src/core/di/exceptions';
+export {OpaqueToken} from './src/core/di/opaque_token';
+
+export {APP_COMPONENT} from 'angular2/src/core/application_tokens';
+export {Type} from 'angular2/src/core/facade/lang';
+export {ApplicationRef} from 'angular2/src/core/application_ref';
+
+
+// Compiler Related Dependencies.
+export {AppRootUrl} from 'angular2/src/core/services/app_root_url';
+export {UrlResolver} from 'angular2/src/core/services/url_resolver';
+export {ComponentUrlMapper} from 'angular2/src/core/compiler/component_url_mapper';
+export {DirectiveResolver} from 'angular2/src/core/compiler/directive_resolver';
+export {Compiler} from 'angular2/src/core/compiler/compiler';
+
+export {AppViewManager} from 'angular2/src/core/compiler/view_manager';
+export {QueryList} from 'angular2/src/core/compiler/query_list';
+export {DynamicComponentLoader} from 'angular2/src/core/compiler/dynamic_component_loader';
+export {LifeCycle} from 'angular2/src/core/life_cycle/life_cycle';
+
+export {ElementRef} from 'angular2/src/core/compiler/element_ref';
+export {TemplateRef} from 'angular2/src/core/compiler/template_ref';
+export {ViewRef, HostViewRef, ProtoViewRef} from 'angular2/src/core/compiler/view_ref';
+export {ViewContainerRef} from 'angular2/src/core/compiler/view_container_ref';
+export {ComponentRef} from 'angular2/src/core/compiler/dynamic_component_loader';
+
+export {NgZone} from 'angular2/src/core/zone/ng_zone';
+export {Observable, EventEmitter} from 'angular2/src/core/facade/async';
