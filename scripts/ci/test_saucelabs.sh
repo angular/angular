c28250d62d42ed7c85d82f58dccfe2b52b8a58cf
Marc Laval <mlaval@amadeus.com>
2015-08-31 17:39:40 +0200

    feat(build): run e2e tests in SauceLabs

--- scripts/ci/test_saucelabs.sh
+++ scripts/ci/test_saucelabs.sh
@@ -8,5 +8,23 @@ cd $SCRIPT_DIR/../..
 
 ./scripts/sauce/sauce_connect_setup.sh
 ./scripts/sauce/sauce_connect_block.sh
-./node_modules/.bin/gulp build.js.dev
-./node_modules/.bin/gulp test.unit.js.sauce/ci
+./node_modules/.bin/gulp build.js
+#./node_modules/.bin/gulp test.unit.js.sauce/ci
+
+./node_modules/.bin/webdriver-manager update
+
+function killServer () {
+  kill $serverPid
+}
+
+./node_modules/.bin/gulp serve.js.prod&
+serverPid=$!
+
+./node_modules/.bin/gulp build.css.material&
+
+trap killServer EXIT
+
+# wait for server to come up!
+sleep 10
+
+./node_modules/.bin/protractor protractor-js-sauce.conf.js
