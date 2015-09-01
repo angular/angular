98a99b690d1689f68a09893cc23dd0304a115c54
Jeff Cross <middlefloor@gmail.com>
2015-09-01 09:34:27 -0700

    fix(web_worker): fix outdated imports

--- modules/angular2/web_worker/worker.ts
+++ modules/angular2/web_worker/worker.ts
@@ -1,6 +1,5 @@
 export * from '../metadata';
 export * from '../change_detection';
 export * from '../core';
-export * from '../render';
 export * from '../profile';
 export * from '../src/web_workers/worker/application';
