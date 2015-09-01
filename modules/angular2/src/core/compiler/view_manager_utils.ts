38e25e715a09b3ee5df07da4aa3d17bb59a08849
Jeff Cross <middlefloor@gmail.com>
2015-09-01 01:30:41 -0700

    chore: fix clang formatting issues

--- modules/angular2/src/core/compiler/view_manager_utils.ts
+++ modules/angular2/src/core/compiler/view_manager_utils.ts
@@ -1,10 +1,5 @@
 import {Injector, Binding, Injectable, ResolvedBinding} from 'angular2/core';
-import {
-  ListWrapper,
-  MapWrapper,
-  Map,
-  StringMapWrapper
-} from 'angular2/src/core/facade/collection';
+import {ListWrapper, MapWrapper, Map, StringMapWrapper} from 'angular2/src/core/facade/collection';
 import * as eli from './element_injector';
 import {isPresent, isBlank, BaseException} from 'angular2/src/core/facade/lang';
 import * as viewModule from './view';
