193792c27fd87ac236690d2c11d362171fb9b3b0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2015-09-03 12:55:08 -0700

    refactor(router): Switch from using APP_COMPONENT binding to a manual ROUTER_PRIMARY_COMPONENT binding.
    With the coming bootstrapping changes, a single application (and thus Router) can have multiple root components. One of these needs to be identified as the "primary" component from which the Router will load its configuration. This is now done by providing a ROUTER_PRIMARY_COMPONENT binding to the primary component type.

--- modules/examples/src/routing/index.ts
+++ modules/examples/src/routing/index.ts
@@ -1,12 +1,13 @@
 import {InboxApp} from './inbox-app';
 import {bind} from 'angular2/angular2';
 import {bootstrap} from 'angular2/bootstrap';
-import {ROUTER_BINDINGS, HashLocationStrategy, LocationStrategy} from 'angular2/router';
+import {routerBindings, HashLocationStrategy, LocationStrategy} from 'angular2/router';
 
 import {reflector} from 'angular2/src/core/reflection/reflection';
 import {ReflectionCapabilities} from 'angular2/src/core/reflection/reflection_capabilities';
 
 export function main() {
   reflector.reflectionCapabilities = new ReflectionCapabilities();
-  bootstrap(InboxApp, [ROUTER_BINDINGS, bind(LocationStrategy).toClass(HashLocationStrategy)]);
+  bootstrap(InboxApp,
+            [routerBindings(InboxApp), bind(LocationStrategy).toClass(HashLocationStrategy)]);
 }
