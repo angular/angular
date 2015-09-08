f628261574638b524d1773bc0793b8d17980c5ff
Marc Laval <mlaval@amadeus.com>
2015-09-08 12:38:19 +0200

    fix(shims): function.name to return empty string when no name

--- modules/angular2/src/test_lib/shims_for_IE.js
+++ modules/angular2/src/test_lib/shims_for_IE.js
@@ -3,7 +3,8 @@
 if (!Object.hasOwnProperty('name')) {
   Object.defineProperty(Function.prototype, 'name', {
     get: function() {
-      var name = this.toString().match(/^\s*function\s*(\S*)\s*\(/)[1];
+      var matches = this.toString().match(/^\s*function\s*(\S*)\s*\(/);
+      var name = matches && matches.length > 1 ? matches[1] : "";
       // For better performance only parse once, and then cache the
       // result through a new accessor for repeated access.
       Object.defineProperty(this, 'name', {value: name});
