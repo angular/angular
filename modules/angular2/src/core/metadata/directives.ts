75771fe4e02760b98c992c1b31ed818aefeb38ce
Kevin Huang <huang.kev@gmail.com>
2015-09-11 22:02:13 -0700

    docs(DirectiveMetadata): fix typo

--- modules/angular2/src/core/metadata/directives.ts
+++ modules/angular2/src/core/metadata/directives.ts
@@ -542,7 +542,7 @@ export class DirectiveMetadata extends InjectableMetadata {
    *
    * - `event1`: the DOM event that the directive listens to.
    * - `statement`: the statement to execute when the event occurs.
-   * If the evalutation of the statement returns `false`, then `preventDefault`is applied on the DOM
+   * If the evaluation of the statement returns `false`, then `preventDefault`is applied on the DOM
    * event.
    *
    * To listen to global events, a target must be added to the event name.
