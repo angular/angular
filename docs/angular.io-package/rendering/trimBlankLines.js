17437ab5d8d9a5552b52f3459c28596aa6c7aa69
Peter Bacon Darwin <pete@bacondarwin.com>
2015-09-16 23:01:47 +0100

    chore(doc-gen): fix blank line trimming for Jade docs

--- docs/angular.io-package/rendering/trimBlankLines.js
+++ docs/angular.io-package/rendering/trimBlankLines.js
@@ -1,9 +1,9 @@
-module.exports = function(encodeCodeBlock) {
+module.exports = function() {
   return {
     name: 'trimBlankLines',
     process: function(str) {
       var lines = str.split(/\r?\n/);
-      while(lines[0] === '') {
+      while(lines.length && (lines[0].trim() === '')) {
         lines.shift();
       }
       return lines.join('\n');
