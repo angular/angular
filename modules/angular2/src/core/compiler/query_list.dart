2eeba8f6a61efb1f207353bc58dc6f98fc2cd707
Rado Kirov <radokirov@google.com>
2015-09-17 22:33:51 -0700

    docs(query): edit and extend query and view query docs.

--- modules/angular2/src/core/compiler/query_list.dart
+++ modules/angular2/src/core/compiler/query_list.dart
@@ -3,73 +3,7 @@ library angular2.src.core.compiler.query_list;
 import 'dart:collection';
 
 /**
- * An iterable and observable live list of components in the DOM.
- *
- * A QueryList contains a live list of child directives in the DOM of a directive.
- * The directives are kept in depth-first pre-order traversal of the DOM.
- *
- * The `QueryList` is iterable, therefore it can be used in both javascript code with `for..of` loop
- * as well as in template with `*ng-for="of"` directive.
- *
- * QueryList is updated as part of the change-detection cycle of a directive. Since change detection
- * happens after construction of a directive, QueryList will always be empty when observed in the
- * constructor.
- *
- *
- * NOTE: In the future this class will implement an `Observable` interface. For now it uses a plain
- * list of observable callbacks.
- *
- * # Example:
- *
- * Assume that `<tabs>` component would like to get a list its children which are `<pane>`
- * components as shown in this example:
- *
- * ```html
- * <tabs>
- *   <pane title="Overview">...</pane>
- *   <pane *ng-for="#o of objects" [title]="o.title">{{o.text}}</pane>
- * </tabs>
- * ```
- *
- * In the above example the list of `<tabs>` elements needs to get a list of `<pane>` elements so
- * that it could render tabs with the correct titles and in the correct order.
- *
- * A possible solution would be for a `<pane>` to inject `<tabs>` component and then register itself
- * with `<tabs>` component's on `hydrate` and deregister on `dehydrate` event. While a reasonable
- * approach, this would only work partialy since `*ng-for` could rearrange the list of `<pane>`
- * components which would not be reported to `<tabs>` component and thus the list of `<pane>`
- * components would be out of sync with respect to the list of `<pane>` elements.
- *
- * A preferred solution is to inject a `QueryList` which is a live list of directives in the
- * component`s light DOM.
- *
- * ```javascript
- * @Component(
- *   selector: 'tabs'
- * )
- * @View(
- *  template: `
- *    <ul>
- *      <li *ng-for="#pane of panes">{{pane.title}}</li>
- *    </ul>
- *    <content></content>
- *  `
- * )
- * class Tabs {
- *   QueryList<Pane> panes;
- *
- *   constructor(@Query(Pane) QueryList<Pane> this.panes) { }
- * }
- *
- * @Component(
- *   selector: 'pane',
- *   properties: ['title']
- * )
- * @View(...)
- * class Pane {
- *   String title;
- * }
- * ```
+ * See query_list.ts
  */
 class QueryList<T> extends Object
     with IterableMixin<T> {
@@ -79,6 +13,7 @@ class QueryList<T> extends Object
 
   Iterator<T> get iterator => _results.iterator;
 
+  /** @private */
   void reset(List<T> newList) {
     _results = newList;
     _dirty = true;
@@ -88,7 +23,6 @@ class QueryList<T> extends Object
     _results.add(obj);
     _dirty = true;
   }
-
 
   void onChange(callback) {
     _callbacks.add(callback);
@@ -114,7 +48,7 @@ class QueryList<T> extends Object
     return this._results.map(fn).toList();
   }
 
-  // Internal to the framework.
+  /** @private */
   void fireCallbacks() {
     if (_dirty) {
       _callbacks.forEach((c) => c());
