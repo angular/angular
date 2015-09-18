76b4334b8b72287c93683a0b6c8b42329ab11b49
Igor Minar <igor@angularjs.org>
2015-09-18 15:46:26 -0700

    docs(compiler): compiler related api docs

--- modules/angular2/src/core/compiler/template_ref.ts
+++ modules/angular2/src/core/compiler/template_ref.ts
@@ -3,17 +3,26 @@ import {ElementRef} from './element_ref';
 import * as viewModule from './view';
 
 /**
- * Reference to a template within a component.
+ * Represents an Embedded Template that can be used for creating Embedded Views.
  *
- * Represents an opaque reference to the underlying template that can
- * be instantiated using the {@link ViewContainerRef}.
+ * Use {@link ViewContainerRef#createEmbeddedView} method to instantiate an Embedded View and attach
+ * it to a View Container.
+ *
+ * <!-- TODO: how to get hold of it? -->
  */
 export class TemplateRef {
+
   /**
-   * The location of the template
+   * The location in the View where the Embedded View logically belong to.
+   *
+   * This `ElementRef` provides the data-binding and injection context for Embedded Views created
+   * from this `TemplateRef`.
    */
   elementRef: ElementRef;
 
+  /**
+   * @private
+   */
   constructor(elementRef: ElementRef) { this.elementRef = elementRef; }
 
   private _getProtoView(): viewModule.AppProtoView {
@@ -23,10 +32,14 @@ export class TemplateRef {
         .nestedProtoView;
   }
 
+  /**
+   * Reference to the ProtoView created by compiling the original Embedded Template, from which the
+   * Embedded View is instatiated.
+   */
   get protoViewRef(): ProtoViewRef { return this._getProtoView().ref; }
 
   /**
-   * Whether this template has a local variable with the given name
+   * Returns true if the Template declares a Local Variable with the given name.
    */
   hasLocal(name: string): boolean { return this._getProtoView().variableBindings.has(name); }
 }
