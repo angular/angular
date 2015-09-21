5c0a9eff62b417816d0a7917e311508f3aa3e80b
Brian Ford <btford@umich.edu>
2015-09-21 14:43:24 -0700

    docs(router): improve docs for RouteDefinition classes

--- modules/angular2/src/router/route_definition.ts
+++ modules/angular2/src/router/route_definition.ts
@@ -1,5 +1,16 @@
 import {CONST, Type} from 'angular2/src/core/facade/lang';
 
+/**
+ * `RouteDefinition` defines a route within a {@link RouteConfig} decorator.
+ *
+ * Supported keys:
+ * - `path` (required)
+ * - `component`, `loader`,  `redirectTo` (requires exactly one of these)
+ * - `as` (optional)
+ * - `data` (optional)
+ *
+ * See also {@link Route}, {@link AsyncRoute}, {@link AuxRoute}, and {@link Redirect}.
+ */
 export interface RouteDefinition {
   path: string;
   component?: Type | ComponentDefinition;
