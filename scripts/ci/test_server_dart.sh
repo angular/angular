502494701cd4725ad6970b7d796c1dae42e3aad9
Jeff Cross <middlefloor@gmail.com>
2015-09-22 11:32:14 -0700

    fix(build): temporarily test dart with dart2js instead of pub serve
    Should be changed back when #4316 is resolved.

--- scripts/ci/test_server_dart.sh
+++ scripts/ci/test_server_dart.sh
@@ -14,14 +14,15 @@ ps -ef | grep webdriver-manager
 # Serving pre-compiled dart JS takes an extra 15m.
 # So we do this only for post-commit testing.
 # Pull requests test with Dartium and pub serve
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+# TODO(jeffbcross): restore conditional dart2js/pubserve #4316
+#if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   ./node_modules/.bin/gulp build/pubbuild.dart
   ./node_modules/.bin/gulp serve.js.dart2js&
   serverPid=$!
-else
-  ./node_modules/.bin/gulp serve.dart&
-  serverPid=$!
-fi
+#else
+#  ./node_modules/.bin/gulp serve.dart&
+#  serverPid=$!
+#fi
 
 function killAllServers () {
   kill $serverPid
