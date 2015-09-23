4d4d20edb9e08175bc3898eadc0261a692f1dc8b
Igor Minar <igor@angularjs.org>
2015-09-23 09:37:04 -0700

    build(travis): block travis while we are shutting down the tunnel
    This is to prevent Travis from prematurely shut down the VM while we are still waiting for the sauce connect client
    to tear down the tunnel.
    
    Closes #4335

--- scripts/sauce/sauce_connect_teardown.sh
+++ scripts/sauce/sauce_connect_teardown.sh
@@ -4,4 +4,13 @@ set -e -o pipefail
 
 
 echo "Shutting down Sauce Connect tunnel"
+
 killall sc
+
+while [[ -n `ps -ef | grep "bin/sc" | grep -v "grep"` ]]; do
+  printf "."
+  sleep .5
+done
+
+echo ""
+echo "Sauce Connect tunnel has been shut down"
