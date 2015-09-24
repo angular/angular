bdb99f3ea4ff74ece599caa6b8d29b1600e65d7e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-09-24 15:36:24 +0200

    test(view_splitter): correct type in a util method

--- modules/angular2/test/core/render/dom/compiler/view_splitter_spec.ts
+++ modules/angular2/test/core/render/dom/compiler/view_splitter_spec.ts
@@ -29,7 +29,7 @@ export function main() {
       return new CompilePipeline([new ViewSplitter(new Parser(new Lexer()))]);
     }
 
-    function proceess(el): CompileElement[] {
+    function process(el): CompileElement[] {
       return createPipeline().processElements(el, ViewType.COMPONENT, createViewDefinition());
     }
 
@@ -37,7 +37,7 @@ export function main() {
 
       it('should move the content into a new <template> element and mark that as viewRoot', () => {
         var rootElement = DOM.createTemplate('<template if="true">a</template>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
 
         expect(stringifyElement(results[1].element))
             .toEqual('<template class="ng-binding" if="true"></template>');
@@ -48,32 +48,32 @@ export function main() {
 
       it('should mark the new <template> element as viewRoot', () => {
         var rootElement = DOM.createTemplate('<template if="true">a</template>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].isViewRoot).toBe(true);
       });
 
       it('should not wrap the root element', () => {
         var rootElement = DOM.createTemplate('');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results.length).toBe(1);
         expect(stringifyElement(rootElement)).toEqual('<template></template>');
       });
 
       it('should copy over the elementDescription', () => {
         var rootElement = DOM.createTemplate('<template if="true">a</template>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].elementDescription).toBe(results[1].elementDescription);
       });
 
       it('should clean out the inheritedElementBinder', () => {
         var rootElement = DOM.createTemplate('<template if="true">a</template>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].inheritedElementBinder).toBe(null);
       });
 
       it('should create a nestedProtoView', () => {
         var rootElement = DOM.createTemplate('<template if="true">a</template>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].inheritedProtoView).not.toBe(null);
         expect(results[2].inheritedProtoView)
             .toBe(results[1].inheritedElementBinder.nestedProtoView);
@@ -89,7 +89,7 @@ export function main() {
       it('should replace the element with an empty <template> element', () => {
         var rootElement = DOM.createTemplate('<span template=""></span>');
         var originalChild = DOM.firstChild(DOM.content(rootElement));
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[0].element).toBe(rootElement);
         expect(stringifyElement(results[0].element))
             .toEqual('<template><template class="ng-binding"></template></template>');
@@ -101,7 +101,7 @@ export function main() {
       it('should work with top-level template node', () => {
         var rootElement = DOM.createTemplate('<div template>x</div>');
         var originalChild = DOM.content(rootElement).childNodes[0];
-        var results = proceess(rootElement);
+        var results = process(rootElement);
 
         expect(results[0].element).toBe(rootElement);
         expect(results[0].isViewRoot).toBe(true);
@@ -113,13 +113,13 @@ export function main() {
 
       it('should mark the element as viewRoot', () => {
         var rootElement = DOM.createTemplate('<div template></div>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].isViewRoot).toBe(true);
       });
 
       it('should add property bindings from the template attribute', () => {
         var rootElement = DOM.createTemplate('<div template="some-prop:expr"></div>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[1].inheritedElementBinder.propertyBindings.get('someProp').source)
             .toEqual('expr');
         expect(results[1].attrs().get('some-prop')).toEqual('expr');
@@ -127,14 +127,14 @@ export function main() {
 
       it('should add variable mappings from the template attribute to the nestedProtoView', () => {
         var rootElement = DOM.createTemplate('<div template="var var-name=mapName"></div>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].inheritedProtoView.variableBindings)
             .toEqual(MapWrapper.createFromStringMap({'mapName': 'varName'}));
       });
 
       it('should add entries without value as attributes to the element', () => {
         var rootElement = DOM.createTemplate('<div template="varname"></div>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[1].attrs().get('varname')).toEqual('');
         expect(results[1].inheritedElementBinder.propertyBindings).toEqual(new Map());
         expect(results[1].inheritedElementBinder.variableBindings).toEqual(new Map());
@@ -142,26 +142,26 @@ export function main() {
 
       it('should iterate properly after a template dom modification', () => {
         var rootElement = DOM.createTemplate('<div template></div><after></after>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         // 1 root + 2 initial + 2 generated template elements
         expect(results.length).toEqual(5);
       });
 
       it('should copy over the elementDescription', () => {
         var rootElement = DOM.createTemplate('<span template=""></span>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].elementDescription).toBe(results[1].elementDescription);
       });
 
       it('should clean out the inheritedElementBinder', () => {
         var rootElement = DOM.createTemplate('<span template=""></span>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].inheritedElementBinder).toBe(null);
       });
 
       it('should create a nestedProtoView', () => {
         var rootElement = DOM.createTemplate('<span template=""></span>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].inheritedProtoView).not.toBe(null);
         expect(results[2].inheritedProtoView)
             .toBe(results[1].inheritedElementBinder.nestedProtoView);
@@ -176,7 +176,7 @@ export function main() {
       it('should replace the element with an empty <template> element', () => {
         var rootElement = DOM.createTemplate('<span *ng-if></span>');
         var originalChild = DOM.firstChild(DOM.content(rootElement));
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[0].element).toBe(rootElement);
         expect(stringifyElement(results[0].element))
             .toEqual('<template><template class="ng-binding" ng-if=""></template></template>');
@@ -187,14 +187,14 @@ export function main() {
 
       it('should mark the element as viewRoot', () => {
         var rootElement = DOM.createTemplate('<div *foo="bar"></div>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].isViewRoot).toBe(true);
       });
 
       it('should work with top-level template node', () => {
         var rootElement = DOM.createTemplate('<div *foo>x</div>');
         var originalChild = DOM.content(rootElement).childNodes[0];
-        var results = proceess(rootElement);
+        var results = process(rootElement);
 
         expect(results[0].element).toBe(rootElement);
         expect(results[0].isViewRoot).toBe(true);
@@ -206,7 +206,7 @@ export function main() {
 
       it('should add property bindings from the template attribute', () => {
         var rootElement = DOM.createTemplate('<div *prop="expr"></div>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[1].inheritedElementBinder.propertyBindings.get('prop').source)
             .toEqual('expr');
         expect(results[1].attrs().get('prop')).toEqual('expr');
@@ -214,14 +214,14 @@ export function main() {
 
       it('should add variable mappings from the template attribute to the nestedProtoView', () => {
         var rootElement = DOM.createTemplate('<div *foreach="var varName=mapName"></div>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].inheritedProtoView.variableBindings)
             .toEqual(MapWrapper.createFromStringMap({'mapName': 'varName'}));
       });
 
       it('should add entries without value as attribute to the element', () => {
         var rootElement = DOM.createTemplate('<div *varname></div>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[1].attrs().get('varname')).toEqual('');
         expect(results[1].inheritedElementBinder.propertyBindings).toEqual(new Map());
         expect(results[1].inheritedElementBinder.variableBindings).toEqual(new Map());
@@ -229,26 +229,26 @@ export function main() {
 
       it('should iterate properly after a template dom modification', () => {
         var rootElement = DOM.createTemplate('<div *foo></div><after></after>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         // 1 root + 2 initial + 2 generated template elements
         expect(results.length).toEqual(5);
       });
 
       it('should copy over the elementDescription', () => {
         var rootElement = DOM.createTemplate('<span *foo></span>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].elementDescription).toBe(results[1].elementDescription);
       });
 
       it('should clean out the inheritedElementBinder', () => {
         var rootElement = DOM.createTemplate('<span *foo></span>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].inheritedElementBinder).toBe(null);
       });
 
       it('should create a nestedProtoView', () => {
         var rootElement = DOM.createTemplate('<span *foo></span>');
-        var results = proceess(rootElement);
+        var results = process(rootElement);
         expect(results[2].inheritedProtoView).not.toBe(null);
         expect(results[2].inheritedProtoView)
             .toBe(results[1].inheritedElementBinder.nestedProtoView);
@@ -263,7 +263,7 @@ export function main() {
       it('should detect duplicate *directive_name attributes', () => {
         var rootElement = DOM.createTemplate(
             `<span *ng-if="true" *ng-for="#person of peopleService.people"></span>`);
-        expect(() => { proceess(rootElement); })
+        expect(() => { process(rootElement); })
             .toThrowErrorWith(
                 'Only one template directive per element is allowed: *ng-if and *ng-for cannot be used simultaneously in someComponent');
       });
@@ -271,7 +271,7 @@ export function main() {
       it('should detect template and *directive_name attribute', () => {
         var rootElement = DOM.createTemplate(
             `<span *ng-if="true" template="ng-for #person of peopleService.people"></span>`);
-        expect(() => { proceess(rootElement); })
+        expect(() => { process(rootElement); })
             .toThrowErrorWith(
                 'Only one template directive per element is allowed: template and *ng-if cannot be used simultaneously in someComponent');
       });
