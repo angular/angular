589ce31dfc4fc2840b675ed0266d4e2c60b26f07
vsavkin <avix1000@gmail.com>
2015-09-25 13:32:58 -0700

    feat(transformers): record setters for query fields
    Closes #4344

--- modules_dart/transform/lib/src/transform/bind_generator/generator.dart
+++ modules_dart/transform/lib/src/transform/bind_generator/generator.dart
@@ -2,12 +2,69 @@ library angular2.transform.bind_generator.generator;
 
 import 'dart:async';
 
+import 'package:analyzer/analyzer.dart';
 import 'package:angular2/src/transform/common/asset_reader.dart';
 import 'package:angular2/src/transform/common/ng_deps.dart';
 import 'package:angular2/src/transform/common/property_utils.dart' as prop;
 import 'package:barback/barback.dart';
 
 import 'visitor.dart';
+
+class _ExtractQueryFieldsFromAnnotation extends Object
+    with RecursiveAstVisitor<Object> {
+  final ConstantEvaluator _evaluator = new ConstantEvaluator();
+  final List<String> queryFields = [];
+
+  @override
+  Object visitNamedExpression(NamedExpression node) {
+    if ('${node.name.label}' == "queries") {
+      if (node.expression is! MapLiteral) {
+        throw new FormatException(
+            'Expected a map value for "queries", but got  ${node.expression}',
+            node.toSource());
+      }
+      MapLiteral queries = node.expression;
+      queryFields.addAll(queries.entries.map((e) => e.key.accept(_evaluator)));
+    }
+    return super.visitNamedExpression(node);
+  }
+
+  Map asMap() {
+    return new Map.fromIterable(queryFields, value: (_) => 'Object');
+  }
+}
+
+
+class _ExtractQueryFieldsFromPropMetadata extends Object
+    with RecursiveAstVisitor<Object> {
+  final ConstantEvaluator _evaluator = new ConstantEvaluator();
+  final List<String> queryFields = [];
+
+  @override
+  Object visitMapLiteralEntry(MapLiteralEntry node) {
+    if (_hasQueryAnnotation(node.value)) {
+      queryFields.add(node.key.accept(_evaluator));
+    }
+    return super.visitMapLiteralEntry(node);
+  }
+
+  bool _hasQueryAnnotation(list) {
+    var res = false;
+    list.elements.forEach((item) {
+      var n = item.constructorName.toString();
+      if(n == "ContentChild" || n == "ViewChild" || n == "ContentChildren" || n == "ViewChildren") {
+        res = true;
+      }
+    });
+
+    return res;
+  }
+
+  asMap() {
+    return new Map.fromIterable(queryFields, value: (_) => 'Object');
+  }
+}
+
 
 Future<String> createNgSettersAndGetters(
     AssetReader reader, AssetId entryPoint) async {
@@ -16,6 +73,18 @@ Future<String> createNgSettersAndGetters(
   String code = ngDeps.code;
   var setters = _generateSetters(_createPropertiesMap(ngDeps));
   var getters = _generateGetters(_createEventPropertiesList(ngDeps));
+
+  ngDeps.registeredTypes.forEach((t) {
+    final fromAnnotation = new _ExtractQueryFieldsFromAnnotation();
+    t.annotations.accept(fromAnnotation);
+
+    final fromPropMetadata = new _ExtractQueryFieldsFromPropMetadata();
+    if (t.propMetadata != null) {
+      t.propMetadata.accept(fromPropMetadata);
+    }
+    setters.addAll(_generateSetters(fromAnnotation.asMap()));
+    setters.addAll(_generateSetters(fromPropMetadata.asMap()));
+  });
 
   if (setters.isEmpty && getters.isEmpty) return code;
   var out = new StringBuffer();
