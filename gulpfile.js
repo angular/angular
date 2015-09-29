f48eed2999cbfbc5040ef79b0440bf53cb2e3f15
Mark Ethan Trostler <mark@zzo.com>
2015-09-29 16:04:25 -0700

    detach child from parent

--- gulpfile.js
+++ gulpfile.js
@@ -720,15 +720,13 @@ gulp.task('test.unit.router/ci', function (done) {
 gulp.task('test.unit.js/ci', function (done) {
   var browserConf = getBrowsersFromCLI();
   var output_file = path.join(process.env.HOME, 'output-' + process.env.MODE + '-' + process.env.DART_CHANNEL + '.mpg');
-  var rec = spawn('/usr/bin/avconv', ['-f', 'x11grab', '-r', 25, '-s', '1024x768', '-i', ':99.0', '-threads', 4, output_file], {}
-  /*
+  var rec = spawn('/usr/bin/avconv', ['-f', 'x11grab', '-r', 25, '-s', '1024x768', '-i', ':99.0', '-threads', 4, output_file],
   {
       detached: true,
       stdio: [ 'ignore', 'ignore', 'ignore' ]
   }
-  */
   );
-  //rec.unref();
+  rec.unref();
 
   new karma.Server({
         configFile: __dirname + '/karma-js.conf.js',
