9b87fdc8127f0393c2efe06fbbbf696b093793c9
Tim Blasi <tjblasi@google.com>
2015-09-30 10:55:52 -0700

    Clean up import code generation

--- modules_dart/transform/lib/src/transform/common/url_resolver.dart
+++ modules_dart/transform/lib/src/transform/common/url_resolver.dart
@@ -10,21 +10,48 @@ class TransformerUrlResolver implements UrlResolver {
     Uri uri = Uri.parse(url);
 
     if (!uri.isAbsolute) {
-      Uri baseUri = Uri.parse(baseUrl);
-      uri = baseUri.resolveUri(uri);
+      uri = Uri.parse(baseUrl).resolveUri(uri);
     }
 
-    var retVal;
-    if (uri.scheme == 'package') {
-      var package = uri.pathSegments.first;
-      var pathInPackage = (['lib']..addAll(uri.pathSegments.skip(1))).join('/');
-      retVal = 'asset:${package}/${pathInPackage}';
-    } else if (uri.scheme == 'asset') {
-      retVal = uri.toString();
-    } else {
-      throw new FormatException('Unsupported URI encountered: $uri', url);
+    return toAssetScheme(uri).toString();
+  }
+
+  /// Converts `absoluteUri` to use the 'asset' scheme used in the Angular 2
+  /// template compiler.
+  ///
+  /// The `scheme` of `absoluteUri` is expected to be either 'package' or
+  /// 'asset'.
+  Uri toAssetScheme(Uri absoluteUri) {
+    if (absoluteUri == null) return null;
+
+    if (!absoluteUri.isAbsolute) {
+      throw new ArgumentError.value(
+          absoluteUri, 'absoluteUri', 'Value passed must be an absolute uri');
+    }
+    if (absoluteUri.scheme == 'asset') {
+      if (absoluteUri.pathSegments.length < 3) {
+        throw new FormatException(
+            'An asset: URI must have at least 3 path '
+            'segments, for example '
+            'asset:<package-name>/<first-level-dir>/<path-to-dart-file>.',
+            absoluteUri);
+      }
+      return absoluteUri;
+    }
+    if (absoluteUri.scheme != 'package') {
+      throw new ArgumentError.value(
+          absoluteUri, 'absoluteUri', 'Unsupported URI scheme encountered');
     }
 
-    return retVal;
+    if (absoluteUri.pathSegments.length < 2) {
+      throw new FormatException(
+          'A package: URI must have at least 2 path '
+          'segments, for example '
+          'package:<package-name>/<path-to-dart-file>',
+          absoluteUri);
+    }
+
+    var pathSegments = absoluteUri.pathSegments.toList()..insert(1, 'lib');
+    return new Uri(scheme: 'asset', pathSegments: pathSegments);
   }
 }
