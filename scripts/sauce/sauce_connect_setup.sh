81f79c3d2bf6ae570a6778105a9b75b8f0f76437
Julie Ralph <ju.ralph@gmail.com>
2015-10-01 15:05:56 -0700

    chore(ci): update sauce connect to 4.3.11
    From 4.3.8.

--- scripts/sauce/sauce_connect_setup.sh
+++ scripts/sauce/sauce_connect_setup.sh
@@ -12,7 +12,7 @@ set -e -o pipefail
 # before_script:
 #   - curl https://gist.github.com/santiycr/5139565/raw/sauce_connect_setup.sh | bash
 
-CONNECT_URL="https://saucelabs.com/downloads/sc-4.3.8-linux.tar.gz"
+CONNECT_URL="https://saucelabs.com/downloads/sc-4.3.11-linux.tar.gz"
 CONNECT_DIR="/tmp/sauce-connect-$RANDOM"
 CONNECT_DOWNLOAD="sc-latest-linux.tar.gz"
 
