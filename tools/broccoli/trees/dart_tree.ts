adaea210e0d4df1103ad18da943479f8855d3f41
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-10-03 18:48:44 +0200

    chore: remove devDependencies from package.json published to npm

--- tools/broccoli/trees/dart_tree.ts
+++ tools/broccoli/trees/dart_tree.ts
@@ -128,14 +128,7 @@ function getTemplatedPubspecsTree() {
     license: BASE_PACKAGE_JSON.license,
     contributors: BASE_PACKAGE_JSON.contributors,
     dependencies: BASE_PACKAGE_JSON.dependencies,
-    devDependencies: {
-      "yargs": BASE_PACKAGE_JSON.devDependencies['yargs'],
-      "gulp-sourcemaps": BASE_PACKAGE_JSON.devDependencies['gulp-sourcemaps'],
-      "gulp-traceur": BASE_PACKAGE_JSON.devDependencies['gulp-traceur'],
-      "gulp": BASE_PACKAGE_JSON.devDependencies['gulp'],
-      "gulp-rename": BASE_PACKAGE_JSON.devDependencies['gulp-rename'],
-      "through2": BASE_PACKAGE_JSON.devDependencies['through2']
-    }
+    devDependencies: {}
   };
   // Generate pubspec.yaml from templates.
   var pubspecs = modulesFunnel(['**/pubspec.yaml']);
