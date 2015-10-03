fccb5c207eac395724eb7c7e7f8efa46861d2899
Tim Blasi <tjblasi@google.com>
2015-10-02 17:01:27 -0700

    test(dart/transform): e2e test inliner_for_test
    Add an e2e test for the `inliner_for_test` transformer.

---
+++ modules_dart/transform/test/transform/inliner_for_test/multiple_style_urls_files/expected/hello.dart
@@ -0,0 +1,13 @@
+library examples.src.hello_world.multiple_style_urls_files;
+
+import 'package:angular2/angular2.dart'
+    show Component, Directive, View, NgElement;
+
+@Component(selector: 'hello-app')
+@View(
+    template: r'''{{greeting}}''',
+    styles: const [
+      r'''.greeting { .color: blue; }''',
+      r'''.hello { .color: red; }''',
+    ])
+class HelloCmp {}
