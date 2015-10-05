075011f2fe5b05d0debb5c04e1b555d84d05be9b
Brandon Roberts <robertsbt@gmail.com>
2015-10-04 20:11:31 -0500

    fix(docs): Updated docs for default router location strategy
    Closes #4517

--- modules/angular2/src/router/hash_location_strategy.ts
+++ modules/angular2/src/router/hash_location_strategy.ts
@@ -9,9 +9,6 @@ import {EventListener, History, Location} from 'angular2/src/core/facade/browser
  * [hash fragment](https://en.wikipedia.org/wiki/Uniform_Resource_Locator#Syntax)
  * of the browser's URL.
  *
- * `HashLocationStrategy` is the default binding for {@link LocationStrategy}
- * provided in {@link routerBindings} and {@link ROUTER_BINDINGS}.
- *
  * For instance, if you call `location.go('/foo')`, the browser's URL will become
  * `example.com#/foo`.
  *
@@ -38,7 +35,7 @@ import {EventListener, History, Location} from 'angular2/src/core/facade/browser
  * }
  *
  * bootstrap(AppCmp, [
- *   routerBindings(AppCmp) // includes binding to HashLocationStrategy
+ *   routerBindings(AppCmp)
  * ]);
  * ```
  */
