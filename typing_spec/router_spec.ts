601d0e28defb8adccb003ca94fa51a8f59ee53ca
Alex Eagle <alexeagle@google.com>
2015-10-10 09:45:04 -0700

    chore(typings): add router_spec to test.typings

--- typing_spec/router_spec.ts
+++ typing_spec/router_spec.ts
@@ -1,6 +1,3 @@
-///<reference path="../dist/docs/typings/angular2/angular2.d.ts"/>
-///<reference path="../dist/docs/typings/angular2/router.d.ts"/>
-
 import {Component, bootstrap, View} from 'angular2/angular2';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
