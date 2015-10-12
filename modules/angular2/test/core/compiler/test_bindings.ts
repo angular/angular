6abed8d996168f695e638c0c2d0fde1ad7ae273c
Igor Minar <igor@angularjs.org>
2015-10-12 10:23:51 -0700

    fix(test_lib): don't mock out XHR via MockXHR by default in tests
    This is useful for the compiler tests, but otherwise it's not useful.
    
    Additionally if an application has external templates (as is common) then
    we should actually fetch these templates in tests.
    
    Fixes #4539
    
    Closes #4682

--- modules/angular2/test/core/compiler/test_bindings.ts
+++ modules/angular2/test/core/compiler/test_bindings.ts
@@ -1,6 +1,10 @@
 import {provide, Provider} from 'angular2/src/core/di';
 import {MockSchemaRegistry} from './schema_registry_mock';
 import {ElementSchemaRegistry} from 'angular2/src/core/compiler/schema/element_schema_registry';
+import {MockXHR} from 'angular2/src/core/compiler/xhr_mock';
+import {XHR} from 'angular2/src/core/compiler/xhr';
 
-export var TEST_PROVIDERS =
-    [provide(ElementSchemaRegistry, {asValue: new MockSchemaRegistry({}, {})})];
+export var TEST_PROVIDERS = [
+  provide(ElementSchemaRegistry, {asValue: new MockSchemaRegistry({}, {})}),
+  provide(XHR, {asClass: MockXHR})
+];
