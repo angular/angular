1869d7991290e8014b34d1f3254566449d4467fc
Kevin Moore <kevmoo@google.com>
2015-10-13 10:47:05 -0700

    test dart bleeding edge

--- scripts/ci/install_dart.sh
+++ scripts/ci/install_dart.sh
@@ -6,12 +6,12 @@ DART_CHANNEL=$1
 VERSION=$2
 ARCH=$3
 
-AVAILABLE_DART_VERSION=$(curl "https://storage.googleapis.com/dart-archive/channels/${DART_CHANNEL}/release/${VERSION}/VERSION" | python -c \
+AVAILABLE_DART_VERSION=$(curl "https://storage.googleapis.com/dart-archive/channels/${DART_CHANNEL}/raw/${VERSION}/VERSION" | python -c \
     'import sys, json; print(json.loads(sys.stdin.read())["version"])')
 
 echo Fetch Dart channel: ${DART_CHANNEL}
 
-URL_PREFIX=https://storage.googleapis.com/dart-archive/channels/${DART_CHANNEL}/release/${VERSION}
+URL_PREFIX=https://storage.googleapis.com/dart-archive/channels/${DART_CHANNEL}/raw/${VERSION}
 DART_SDK_URL="$URL_PREFIX/sdk/dartsdk-$ARCH-release.zip"
 DARTIUM_URL="$URL_PREFIX/dartium/dartium-$ARCH-release.zip"
 
