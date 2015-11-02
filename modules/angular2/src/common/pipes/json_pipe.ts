94cf671c152e914e267a231bdd5e060cfb065edb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2015-11-02 15:46:59 -0800

    docs(pipes): Add examples for all Angular pipes.
    Closes #5103

--- modules/angular2/src/common/pipes/json_pipe.ts
+++ modules/angular2/src/common/pipes/json_pipe.ts
@@ -4,25 +4,10 @@ import {PipeTransform, WrappedValue} from 'angular2/src/core/change_detection';
 import {Pipe} from 'angular2/src/core/metadata';
 
 /**
- * Implements json transforms to any object.
+ * Transforms any input value using `JSON.stringify`. Useful for debugging.
  *
  * ### Example
- *
- * In this example we transform the user object to json.
- *
- *  ```
- * @Component({
- *   selector: "user-cmp",
- *   template: "User: {{ user | json }}"
- * })
- * class Username {
- *  user:Object
- *  constructor() {
- *    this.user = { name: "PatrickJS" };
- *  }
- * }
- *
- * ```
+ * {@example core/pipes/ts/json_pipe/json_pipe_example.ts region='JsonPipe'}
  */
 @CONST()
 @Pipe({name: 'json', pure: false})
