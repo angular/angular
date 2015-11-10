e6bf33efbfbb8545ef4b1adb94d825a2fecdb416
Tobias Bosch <tbosch1009@gmail.com>
2015-11-10 11:50:33 -0800

    patch(renderer): make `glyph` not an svg element
    This is required because of a conflict with an internal project.
    This will go away soon as we are working on a proper SVG 
    support..

--- modules/angular2/src/core/render/dom/dom_renderer.ts
+++ modules/angular2/src/core/render/dom/dom_renderer.ts
@@ -86,7 +86,10 @@ const SVG_ELEMENT_NAMES = CONST_EXPR({
   'font-face-uri': true,
   'foreignObject': true,
   'g': true,
-  'glyph': true,
+  // TODO(tbosch): this needs to be disabled
+  // because of an internal project.
+  // We will fix SVG soon, so this will go away...
+  // 'glyph': true,
   'glyphRef': true,
   'hkern': true,
   'image': true,
