36a78f51ffd8014966e5755508859aa3a42c6f34
Igor Minar <igor@angularjs.org>
2015-11-11 04:11:56 -0800

    fix(core): reexport PLATFORM_DIRECTIVES and PLATFORM_PIPES in dart
    Looks like we missed this in the original PR.

--- modules/angular2/core.dart
+++ modules/angular2/core.dart
@@ -18,3 +18,4 @@ export 'package:angular2/src/common/directives.dart';
 export 'package:angular2/src/common/forms.dart';
 export 'package:angular2/src/core/debug.dart';
 export 'package:angular2/src/core/change_detection.dart';
+export 'package:angular2/src/core/platform_directives_and_pipes.dart';
