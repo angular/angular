a31e2f5f4799e9ce20ae508f9f91dfeba05805f2
Tim Blasi <tjblasi@google.com>
2015-11-13 17:13:47 -0800

    fix(dart/transform): Consider of line numbers in inliner_for_test
    Ensure that line numbers aren't changed by inliner_for_test.
    
    Fixes #5281
    
    Closes #5285

--- modules_dart/transform/test/transform/inliner_for_test/absolute_url_expression_files/expected/hello.dart
+++ modules_dart/transform/test/transform/inliner_for_test/absolute_url_expression_files/expected/hello.dart
@@ -4,9 +4,9 @@ import 'package:angular2/angular2.dart'
     show Component, Directive, View, NgElement;
 
 @Component(selector: 'hello-app')
-@View(
-    template: r'''{{greeting}}''',
-    styles: const [r'''.greeting { .color: blue; }''',])
+@View(template: _template0, styles: const [_style1,])
 class HelloCmp {}
 
 @Injectable() hello() {}
+const _template0 = r'''{{greeting}}''';
+const _style1 = r'''.greeting { .color: blue; }''';
