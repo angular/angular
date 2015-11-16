ad6fb067e9dc1c52314f9002947045c370ecd851
vsavkin <avix1000@gmail.com>
2015-11-16 13:58:24 -0800

    fix(dart): fix the static_browser platform not to include compiler
    Closes #5321

--- modules/angular2/src/compiler/xhr.ts
+++ modules/angular2/src/compiler/xhr.ts
@@ -1,6 +1,6 @@
 import {Promise} from 'angular2/src/facade/async';
 
-// TODO: vsavkin rename it into TemplateLoader and do not reexport it via DomAdapter
+// TODO: vsavkin rename it into TemplateLoader
 export class XHR {
   get(url: string): Promise<string> { return null; }
 }
