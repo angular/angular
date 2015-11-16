c5045ac8fe0b28986a5dfa3729c189b3009997ca
Yegor Jbanov <yjbanov@google.com>
2015-11-16 14:22:54 -0800

    chore(build): print commands in presubmit-queue-setup.sh

--- scripts/ci/presubmit-queue-setup.sh
+++ scripts/ci/presubmit-queue-setup.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-set -e -o pipefail
+set -e -x -o pipefail
 
 if [ "$TRAVIS_REPO_SLUG" = "angular/angular" ]; then
   if [[ $TRAVIS_BRANCH == "presubmit-"* || $MODE == "build_only" ]]; then
-
-    echo '*********************'
-    echo '** PRESUBMIT SETUP **'
-    echo '*********************'
-
     git config credential.helper "store --file=.git/credentials"
     # travis encrypt GITHUB_TOKEN_ANGULAR=??? --repo=angular/angular
     echo "https://${GITHUB_TOKEN_ANGULAR}:@github.com" > .git/credentials
@@ -15,6 +10,10 @@ if [ "$TRAVIS_REPO_SLUG" = "angular/angular" ]; then
     git config user.email "`git --no-pager show -s --format='%cE' HEAD`"
 
     if [[ $TRAVIS_BRANCH == "presubmit-"* ]]; then
+      echo '*********************'
+      echo '** PRESUBMIT SETUP **'
+      echo '*********************'
+
       git remote add upstream https://github.com/angular/angular.git
       git fetch upstream master
       git rebase upstream/master
