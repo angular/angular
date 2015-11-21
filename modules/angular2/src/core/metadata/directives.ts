f5fbe126a3db72edc6eb0b5e43e7ef7644c2269e
Tim Ruffles <timruffles@gmail.com>
2015-11-20 16:02:29 -0800

    docs(core): @View annotations are no longer mandatory
    @View is [now optional](angular@bd31b01).

--- modules/angular2/src/core/metadata/directives.ts
+++ modules/angular2/src/core/metadata/directives.ts
@@ -783,7 +783,7 @@ export class DirectiveMetadata extends InjectableMetadata {
 /**
  * Declare reusable UI building blocks for an application.
  *
- * Each Angular component requires a single `@Component` and at least one `@View` annotation. The
+ * Each Angular component requires a single `@Component` annotation. The
  * `@Component`
  * annotation specifies when a component is instantiated, and which properties and hostListeners it
  * binds to.
