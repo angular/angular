a8c3b9d67c39a0b12f61a602bb876a36d219efed
Igor Minar <igor@angularjs.org>
2015-11-26 23:45:40 -0800

    build(gulp): lazy-require modules in order to improve startup time
    Closes #5498

--- tools/build/dartapidocs.js
+++ tools/build/dartapidocs.js
@@ -5,41 +5,35 @@ var util = require('./util');
 
 module.exports = function(gulp, plugins, config) {
   config.output = config.output || 'doc/api';
-  return function() {
-    return util.forEachSubDirSequential(config.dest, function(dir) {
-      var defer = Q.defer();
-      var done = defer.makeNodeResolver();
+  return util.forEachSubDirSequential(config.dest, function(dir) {
+    var defer = Q.defer();
+    var done = defer.makeNodeResolver();
 
-      var supportedModules = [
-        'dist/dart/angular2',
-        // TODO: blocked by https://github.com/angular/angular/issues/3518
-        // 'dist/dart/angular2_material',
-        'dist/dart/benchpress'
-      ];
+    var supportedModules = [
+      'dist/dart/angular2',
+      // TODO: blocked by https://github.com/angular/angular/issues/3518
+      // 'dist/dart/angular2_material',
+      'dist/dart/benchpress'
+    ];
 
-      if (supportedModules.indexOf(dir) === -1) {
-        done();
-      } else {
-        console.log('INFO: running dartdoc for ', dir);
+    if (supportedModules.indexOf(dir) === -1) {
+      done();
+    } else {
+      console.log('INFO: running dartdoc for ', dir);
 
-        var stream = spawn(config.command, ['--input=.', '--output=' + config.output], {
-          stdio: [process.stdin, process.stdout, process.stderr],
-          cwd: dir
-        });
+      var stream = spawn(config.command, ['--input=.', '--output=' + config.output],
+                         {stdio: [process.stdin, process.stdout, process.stderr], cwd: dir});
 
-        stream.on('exit', function(code) {
-          if (code !== 0) {
-            done('ERROR: dartdoc exited with non-zero status ' + code);
-          } else {
-            done();
-          }
-        });
+      stream.on('exit', function(code) {
+        if (code !== 0) {
+          done('ERROR: dartdoc exited with non-zero status ' + code);
+        } else {
+          done();
+        }
+      });
 
-        stream.on('error', function(e) {
-          done('ERROR: dartdoc reported error: ' + e);
-        });
-      }
-      return defer.promise;
-    });
-  };
+      stream.on('error', function(e) { done('ERROR: dartdoc reported error: ' + e); });
+    }
+    return defer.promise;
+  });
 };
