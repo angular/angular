174cf27142052f6cdcb7e64c505a4a987f555cb9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-11-30 16:41:31 +0000

    e216c98 chore(bundles): don't distribute sfx bundles for http

--- es6/prod/src/http/http_sfx.js
+++
@@ -1,2 +0,0 @@
-import * as ngHttp from './http';
-window.ngHttp = ngHttp;
--- src/http/http_sfx.js
+++
@@ -1,3 +0,0 @@
-'use strict';var ngHttp = require('./http');
-window.ngHttp = ngHttp;
-//# sourceMappingURL=http_sfx.js.map
