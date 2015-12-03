1b8c7afadeea4b4d82d5653c7f3336387b9c53b9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-12-03 21:53:25 +0000

    d6f84f2 refactor(broccoli-check-imports): allow imports from self by default

--- ts/src/core/reflection/reflection.dart
+++ ts/src/core/reflection/reflection.dart
@@ -7,53 +7,45 @@ import 'platform_reflection_capabilities.dart';
 import 'package:angular2/src/facade/lang.dart';
 
 class NoReflectionCapabilities implements PlatformReflectionCapabilities {
-  @override
   bool isReflectionEnabled() {
     return false;
   }
 
-  @override
   Function factory(Type type) {
     throw "Cannot find reflection information on ${stringify(type)}";
   }
 
-  @override
   List interfaces(Type type) {
     throw "Cannot find reflection information on ${stringify(type)}";
   }
 
-  @override
-  List parameters(dynamic type) {
+  List parameters(Type type) {
     throw "Cannot find reflection information on ${stringify(type)}";
   }
 
-  @override
-  List annotations(dynamic type) {
+  List annotations(Type type) {
     throw "Cannot find reflection information on ${stringify(type)}";
   }
 
-  @override
-  Map propMetadata(dynamic type) {
+  Map propMetadata(Type type) {
     throw "Cannot find reflection information on ${stringify(type)}";
   }
 
-  @override
   GetterFn getter(String name) {
     throw "Cannot find getter ${name}";
   }
 
-  @override
   SetterFn setter(String name) {
     throw "Cannot find setter ${name}";
   }
 
-  @override
   MethodFn method(String name) {
     throw "Cannot find method ${name}";
   }
 
-  @override
   String importUri(Type type) => './';
+
+  String moduleId(Type type) => './';
 }
 
 final Reflector reflector = new Reflector(new NoReflectionCapabilities());
