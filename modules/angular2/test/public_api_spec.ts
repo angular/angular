0a3a17ff106129edfb3ce377c69f5dd99af7e0c7
Tim Blasi <tjblasi@google.com>
2015-12-03 09:47:57 -0800

    fix(dart/reflection): Fix `NoReflectionCapabilities` interface
    Make `NoReflectionCapabilities` conform to the `PlatformReflectionCapbilities`
    api, which prevents some confusing error messages.
    
    Closes #5559
    
    Closes #5578

--- modules/angular2/test/public_api_spec.ts
+++ modules/angular2/test/public_api_spec.ts
@@ -1449,7 +1449,6 @@ var NG_ALL = [
   "NoReflectionCapabilities.interfaces():dart",
   "NoReflectionCapabilities.isReflectionEnabled():dart",
   "NoReflectionCapabilities.method():dart",
-  "NoReflectionCapabilities.moduleId():dart",
   "NoReflectionCapabilities.parameters():dart",
   "NoReflectionCapabilities.propMetadata():dart",
   "NoReflectionCapabilities.setter():dart",
