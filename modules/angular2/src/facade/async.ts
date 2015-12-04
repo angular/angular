32c8d422ac9137eb278d05d4d7dce4a2db764ebe
Rob Wormald <robwormald@gmail.com>
2015-12-04 15:33:41 -0800

    fix(async): improve Rx support in ObservableWrapper

--- modules/angular2/src/facade/async.ts
+++ modules/angular2/src/facade/async.ts
@@ -32,7 +32,7 @@ export class ObservableWrapper {
     return emitter.subscribe({next: onNext, error: onError, complete: onComplete});
   }
 
-  static isObservable(obs: any): boolean { return obs instanceof RxObservable; }
+  static isObservable(obs: any): boolean { return !!obs.subscribe; }
 
   /**
    * Returns whether `obs` has any subscribers listening to events.
