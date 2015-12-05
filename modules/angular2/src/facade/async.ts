angular/angular

commit 5514dc19d9779fb6b0d7bbae8268dc21dd1dde39

refactor(facade): use rxjs package
move to new RxJS distribution.

BREAKING CHANGE:

RxJS imports now are via `rxjs` instead of `@reactivex/rxjs`
Individual operators can be imported `import 'rxjs/operators/map'`

modules/angular2/src/facade/async.ts
@@ -4,11 +4,16 @@ import {global, isPresent, noop} from 'angular2/src/facade/lang';
 import {PromiseWrapper, Promise, PromiseCompleter} from 'angular2/src/facade/promise';
 export {PromiseWrapper, Promise, PromiseCompleter} from 'angular2/src/facade/promise';
 
-import {Subject} from '@reactivex/rxjs/dist/cjs/Subject';
-import {Observable as RxObservable} from '@reactivex/rxjs/dist/cjs/Observable';
-import {Subscription} from '@reactivex/rxjs/dist/cjs/Subscription';
-import {Operator} from '@reactivex/rxjs/dist/cjs/Operator';
-export {Subject} from '@reactivex/rxjs/dist/cjs/Subject';
+import {Subject} from 'rxjs/Subject';
+import {Observable as RxObservable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
+import {Operator} from 'rxjs/Operator';
+
+import 'rxjs/observable/fromPromise';
+import 'rxjs/operators/toPromise';
+
+export {Subject} from 'rxjs/Subject';
+
 
 
 export namespace NodeJS {

modules/angular2/src/http/backends/mock_backend.ts
@@ -5,7 +5,8 @@ import {ReadyStates} from '../enums';
 import {Connection, ConnectionBackend} from '../interfaces';
 import {isPresent} from 'angular2/src/facade/lang';
 import {BaseException, WrappedException} from 'angular2/src/facade/exceptions';
-import {Subject, ReplaySubject} from '@reactivex/rxjs/dist/cjs/Rx';
+import {Subject} from 'rxjs/Subject';
+import {ReplaySubject} from 'rxjs/subjects/ReplaySubject';
 
 /**
  *

gulpfile.js
@@ -146,7 +146,7 @@ var BENCHPRESS_BUNDLE_CONFIG = {
   packageJson: './dist/js/cjs/benchpress/package.json',
   includes: ['angular2'],
   excludes: ['reflect-metadata', 'selenium-webdriver', 'zone.js'],
-  ignore: ['@reactivex/rxjs'],
+  ignore: [],
   dest: CONFIG.dest.benchpress_bundle
 };
 
@@ -815,8 +815,7 @@ gulp.task('static-checks', ['!build.tools'], function(done) {
 // Make sure the two typings tests are isolated, by running this one in a tempdir
 var tmpdir = path.join(os.tmpdir(), 'test.typings', new Date().getTime().toString());
 gulp.task('!pre.test.typings.layoutNodeModule', ['build.js.cjs'], function() {
-  return gulp.src(['dist/js/cjs/angular2/**/*', 'node_modules/@reactivex/rxjs/dist/cjs/**'],
-                  {base: 'dist/js/cjs'})
+  return gulp.src(['dist/js/cjs/angular2/**/*', 'node_modules/rxjs/**'], {base: 'dist/js/cjs'})
       .pipe(gulp.dest(path.join(tmpdir, 'node_modules')));
 });
 gulp.task('!pre.test.typings.copyTypingsSpec', function() {
@@ -1152,9 +1151,11 @@ var JS_DEV_DEPS = [
 
 // Splice in RX license if rx is in the bundle.
 function insertRXLicense(source) {
-  var n = source.indexOf('System.register("@reactivex/rxjs/dist/cjs/Subject"');
+  var n = source.indexOf('System.register("rxjs/Subject"');
   if (n >= 0) {
-    var rxLicense = licenseWrap('node_modules/@reactivex/rxjs/LICENSE.txt');
+    // TODO: point this to Rx once Rx includes license in dist
+    // https://github.com/angular/angular/issues/5558
+    var rxLicense = licenseWrap('LICENSE');
     return source.slice(0, n) + rxLicense + source.slice(n);
   } else {
     return source;

karma-js.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
       // Including systemjs because it defines `__eval`, which produces correct stack traces.
       'modules/angular2/src/testing/shims_for_IE.js',
       'node_modules/systemjs/dist/system.src.js',
-      {pattern: 'node_modules/@reactivex/rxjs/**', included: false, watched: false, served: true},
+      {pattern: 'node_modules/rxjs/**', included: false, watched: false, served: true},
       'node_modules/reflect-metadata/Reflect.js',
       'tools/build/file2modulename.js',
       'test-main.js',

modules/angular2/test/http/http_spec.ts
@@ -29,7 +29,8 @@ import {
   Http,
   Jsonp
 } from 'angular2/http';
-import {Observable, Subject} from '@reactivex/rxjs/dist/cjs/Rx';
+import {Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
 
 class SpyObserver extends SpyObject {
   onNext: Function;

modules/angular2/test/public_api_spec.ts
@@ -689,47 +689,12 @@ var NG_ALL = [
   'EventEmitter.emit():js',
   'Observable:js',
   'Observable#create():js',
-  'Observable.buffer():js',
-  'Observable.bufferCount():js',
-  'Observable.bufferTime():js',
-  'Observable.bufferToggle():js',
-  'Observable.bufferWhen():js',
-  'Observable.catch():js',
-  'Observable.combineAll():js',
-  'Observable.combineLatest():js',
-  'Observable.concat():js',
-  'Observable.concatAll():js',
-  'Observable.concatMap():js',
-  'Observable.concatMapTo():js',
-  'Observable.count():js',
-  'Observable.debounce():js',
-  'Observable.debounceTime():js',
-  'Observable.defaultIfEmpty():js',
-  'Observable.delay():js',
-  'Observable.dematerialize():js',
-  'Observable.distinctUntilChanged():js',
-  'Observable.do():js',
-  'Observable.every():js',
-  'Observable.expand():js',
-  'Observable.filter():js',
-  'Observable.finally():js',
-  'Observable.first():js',
-  'Observable.flatMap():js',
-  'Observable.flatMapTo():js',
   'Observable.forEach():js',
-  'Observable.groupBy():js',
-  'Observable.ignoreElements():js',
-  'Observable.last():js',
+  'Observable#fromPromise():js',
   'Observable.lift():js',
-  'Observable.map():js',
-  'Observable.mapTo():js',
-  'Observable.materialize():js',
-  'Observable.merge():js',
-  'Observable.mergeAll():js',
-  'Observable.mergeMap():js',
-  'Observable.mergeMapTo():js',
-  'Observable.multicast():js',
-  'Observable.observeOn():js',
+  'Observable.subscribe():js',
+  'Observable.take():js',
+  'Observable.toPromise():js',
 
   'OutputMetadata',
   'OutputMetadata.bindingPropertyName',

test-main.js
@@ -14,7 +14,7 @@ System.config({
     'benchpress/*': 'dist/js/dev/es5/benchpress/*.js',
     'angular2/*': 'dist/js/dev/es5/angular2/*.js',
     'angular2_material/*': 'dist/js/dev/es5/angular2_material/*.js',
-    '@reactivex/rxjs/*': 'node_modules/@reactivex/rxjs/*.js'
+    'rxjs/*': 'node_modules/rxjs/*.js'
   }
 });
 

tools/broccoli/broccoli-check-imports.ts
@@ -17,7 +17,7 @@ class CheckImports implements DiffingBroccoliPlugin {
       "angular2/src/core",
       "angular2/src/facade",
     ],
-    "angular2/src/facade": ["angular2/src/facade", "@reactivex/rxjs"],
+    "angular2/src/facade": ["angular2/src/facade", "rxjs"],
     "angular2/src/common": ["angular2/core", "angular2/src/facade", "angular2/src/common"]
     //"angular2/src/render": [
     //  "angular2/animate",

commit e1fc7827a9631c50b6db0f7b4c70bf3f24579e53

fix(http,facade):rxjs paths broken after rxjs alpha-12 changed folder names
Commit #5514dc19d9779fb6b0d7bbae8268dc21dd1dde39 introduced direct dependency on
rxjs, and changed most of the paths. However, Rxjs introduced some changes since alpha.10

Alpha.11 was apparently removed and alpha.12 (current) changes some folder names
for dependencies from plural to singular.

I noticed the following changes:

/operators/map       => /operator/map
/operators/take      => /operator/take
/operators/toPromise => /operator/toPromise

modules/angular2/src/facade/async.ts
@@ -10,7 +10,7 @@ import {Subscription} from 'rxjs/Subscription';
 import {Operator} from 'rxjs/Operator';
 
 import 'rxjs/observable/fromPromise';
-import 'rxjs/operators/toPromise';
+import 'rxjs/operator/toPromise';
 
 export {Subject} from 'rxjs/Subject';
 

modules/angular2/src/http/backends/mock_backend.ts
@@ -6,8 +6,8 @@ import {Connection, ConnectionBackend} from '../interfaces';
 import {isPresent} from 'angular2/src/facade/lang';
 import {BaseException, WrappedException} from 'angular2/src/facade/exceptions';
 import {Subject} from 'rxjs/Subject';
-import {ReplaySubject} from 'rxjs/subjects/ReplaySubject';
-import 'rxjs/operators/take';
+import {ReplaySubject} from 'rxjs/subject/ReplaySubject';
+import 'rxjs/operator/take';
 
 /**
  *

modules/playground/src/http/http_comp.ts
@@ -1,6 +1,6 @@
 import {Component, View, NgFor} from 'angular2/angular2';
 import {Http, Response} from 'angular2/http';
-import 'rxjs/operators/map';
+import 'rxjs/operator/map';
 
 @Component({selector: 'http-app'})
 @View({