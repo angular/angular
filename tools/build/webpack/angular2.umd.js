fb4f1e8dc9f3f8135b37f8b4dce7b7b7f74df96b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-12-08 14:56:22 +0100

    fix(bundles): clean-up and re-organize UMD bundles
    Fixes #5593
    Part of #5665
    
    BREAKING CHANGE:
    
    Number and content of UMD bundles have changed:
    - we only publish one bundle that contains: core, common, platform/browser, http, router, instrumentation and upgrade
    - exported names have changed and now:
      - core is exported as `ng.core`
      - common is exported as `ng.common`
      - platform/browser is exported as `ng.platform.browser`
      - http is exported as `ng.http`
      - router is exported as `ng.router`
      - instrumentation is exported as `ng.instrumentation`
      - upgrade is exported as `ng.upgrade`
    
    Closes #5697

---
+++ tools/build/webpack/angular2.umd.js
@@ -0,0 +1,11 @@
+exports.core = require('angular2/core');
+exports.common = require('angular2/common');
+exports.platform = {
+  browser: require('angular2/platform/browser'),
+  common_dom: require('angular2/platform/common_dom')
+};
+exports.http = require('angular2/http');
+exports.router = require('angular2/router');
+exports.router_link_dsl = require('angular2/router/router_link_dsl.js');
+exports.instrumentation = require('angular2/instrumentation');
+exports.upgrade = require('angular2/upgrade');
