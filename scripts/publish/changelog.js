8d706e93bad7ca03701dca507c4993be892568d5
Jeff Cross <middlefloor@gmail.com>
2015-12-09 15:41:13 -0800

    chore(changelog): add logic to show breaking changes regardless of commit type
    Previously, commits that were not a "fix", "feat", "perf" or 
    "revert" commit type would be ommitted from the Changelog, even if they had
    breaking changes. 
    
    Now any change that contains a valid BREAKING CHANGE note will be included in 
    the changelog. 
    
    Closes #5672

--- scripts/publish/changelog.js
+++ scripts/publish/changelog.js
@@ -13,14 +13,63 @@
 
 var fs = require('fs');
 var cl = require('conventional-changelog');
+var angularPreset = require('conventional-changelog/presets/angular');
+var _ = require('lodash');
 
 var changelogStream = fs.createWriteStream('CHANGELOG.md');
+
+var typesToHeadings = {
+  refactor: 'Refactor',
+  build: 'Build',
+  cleanup: 'Cleanup',
+  chore: 'Chore'
+};
 
 var config = {
   preset: 'angular',
   releaseCount: 1,
 };
 
-cl(config).on('error', function(err) {
-            console.error('Failed to generate changelog: ' + err);
-          }).pipe(changelogStream);
+angularPreset(function(err, opts) {
+  cl(config, null, null, null, {
+    transform: transformFn.bind(opts)
+  }).on('error', function(err) {
+              console.error('Failed to generate changelog: ' + err);
+            }).pipe(changelogStream);
+});
+
+function transformFn(commit) {
+  var transformed = this.writerOpts.transform(commit);
+
+  /**
+   * If a commit has breaking changes, but didn't pass the angular preset,
+   * include it in the Changelog.
+  **/
+  if (!transformed && commit.notes.filter(function(note) {
+      return note.title.indexOf('BREAKING CHANGE') > -1}).length) {
+    transformed = commit;
+    transformed.type = typesToHeadings[commit.type];
+
+    /**
+     * Begin copying of code from angular preset
+    **/
+    if (typeof commit.hash === 'string') {
+      commit.hash = commit.hash.substring(0, 7);
+    }
+
+    if (typeof commit.subject === 'string') {
+      commit.subject = commit.subject.substring(0, 80);
+    }
+
+    _.map(commit.notes, function(note) {
+      if (note.title === 'BREAKING CHANGE') {
+        note.title = 'BREAKING CHANGES';
+      }
+      return note;
+    });
+    /**
+     * End copying of code from angular preset
+    **/
+  }
+  return transformed;
+};
