2ca5e38a78cf423d35fcb12e9829bf2ebd9e5548
Alejandro Caravaca Puchades <alex94puchades@gmail.com>
2015-12-10 13:23:47 +0100

    fix(upgrade): allow directives with empty template

--- modules/angular2/test/upgrade/upgrade_spec.ts
+++ modules/angular2/test/upgrade/upgrade_spec.ts
@@ -304,6 +304,27 @@ export function main() {
                });
          }));
 
+      it('should support empty template', inject([AsyncTestCompleter], (async) => {
+           var adapter = new UpgradeAdapter();
+           var ng1Module = angular.module('ng1', []);
+
+           var ng1 = function() { return {template: ''}; };
+           ng1Module.directive('ng1', ng1);
+           var Ng2 = Component({
+                       selector: 'ng2',
+                       template: '<ng1></ng1>',
+                       directives: [adapter.upgradeNg1Component('ng1')]
+                     }).Class({constructor: function() {}});
+           ng1Module.directive('ng2', adapter.downgradeNg2Component(Ng2));
+           var element = html(`<div><ng2></ng2></div>`);
+           adapter.bootstrap(element, ['ng1'])
+               .ready((ref) => {
+                 expect(multiTrim(document.body.textContent)).toEqual('');
+                 ref.dispose();
+                 async.done();
+               });
+         }));
+
       it('should support templateUrl fetched from $templateCache',
          inject([AsyncTestCompleter], (async) => {
            var adapter = new UpgradeAdapter();
