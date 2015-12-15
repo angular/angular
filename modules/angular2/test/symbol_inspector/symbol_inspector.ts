406d32e7b43b005a87026cc695b0914f14946e51
Igor Minar <igor@angularjs.org>
2015-12-15 09:06:41 -0800

    style(public_api): fix clang-format

--- modules/angular2/test/symbol_inspector/symbol_inspector.ts
+++ modules/angular2/test/symbol_inspector/symbol_inspector.ts
@@ -40,64 +40,61 @@ const LIB_MAP = {
   ngTesting
 };
 
-const IGNORE =
-    {
-      captureStackTrace: true,
-      stackTraceLimit: true,
-      toString: true,
-      originalException: true,
-      originalStack: true,
-      wrapperMessage: true,
-      wrapperStack: true, '@@observable': true
-    };
+const IGNORE = {
+  captureStackTrace: true,
+  stackTraceLimit: true,
+  toString: true,
+  originalException: true,
+  originalStack: true,
+  wrapperMessage: true,
+  wrapperStack: true, '@@observable': true
+};
 
-function collectClassSymbols(symbols: string[], prefix: String, type: Function):
-    void {
-      // static
-      for (var name in type) {
-        if (IGNORE[name] || name.charAt(0) == '_') continue;
-        var suf = type[name] instanceof Function ? '()' : '';
-        var symbol = `${prefix}#${name}${suf}`;
-        symbols.push(symbol);
-      }
+function collectClassSymbols(symbols: string[], prefix: String, type: Function): void {
+  // static
+  for (var name in type) {
+    if (IGNORE[name] || name.charAt(0) == '_') continue;
+    var suf = type[name] instanceof Function ? '()' : '';
+    var symbol = `${prefix}#${name}${suf}`;
+    symbols.push(symbol);
+  }
 
-      // instance
-      for (var name in type.prototype) {
-        if (IGNORE[name] || name.charAt(0) == '_') continue;
-        if (name == 'constructor') continue;
-        var suf = '';
-        try {
-          if (type.prototype[name] instanceof Function) suf = '()';
-        } catch (e) {
-        }
-        var symbol = `${prefix}.${name}${suf}`;
-        symbols.push(symbol);
-      }
+  // instance
+  for (var name in type.prototype) {
+    if (IGNORE[name] || name.charAt(0) == '_') continue;
+    if (name == 'constructor') continue;
+    var suf = '';
+    try {
+      if (type.prototype[name] instanceof Function) suf = '()';
+    } catch (e) {
     }
+    var symbol = `${prefix}.${name}${suf}`;
+    symbols.push(symbol);
+  }
+}
 
-function collectTopLevelSymbols(prefix: string, lib: any):
-    string[] {
-      var symbols: string[] = [];
-      for (var name in lib) {
-        var symbol = `${name}`;
-        var ref = lib[name];
-        if (ref instanceof Function) {
-          if (symbol.charAt(0) == symbol.charAt(0).toLowerCase()) {
-            // assume it is top level function
-            symbols.push(symbol + '()');
-          } else {
-            symbols.push(symbol);
-            collectClassSymbols(symbols, symbol, ref);
-          }
-        } else {
-          symbols.push(symbol);
-        }
+function collectTopLevelSymbols(prefix: string, lib: any): string[] {
+  var symbols: string[] = [];
+  for (var name in lib) {
+    var symbol = `${name}`;
+    var ref = lib[name];
+    if (ref instanceof Function) {
+      if (symbol.charAt(0) == symbol.charAt(0).toLowerCase()) {
+        // assume it is top level function
+        symbols.push(symbol + '()');
+      } else {
+        symbols.push(symbol);
+        collectClassSymbols(symbols, symbol, ref);
       }
-      return symbols;
+    } else {
+      symbols.push(symbol);
     }
+  }
+  return symbols;
+}
 
 export function getSymbolsFromLibrary(name: string): string[] {
-  if(!LIB_MAP.hasOwnProperty(name)) {
+  if (!LIB_MAP.hasOwnProperty(name)) {
     throw new Error(`Unknown barrel ${name}.`);
   }
   var symbols = collectTopLevelSymbols(name, LIB_MAP[name]);
