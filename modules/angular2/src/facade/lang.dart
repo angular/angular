27c4456adc8520bda6e722fe35182bcdc0f51561
vsavkin <avix1000@gmail.com>
2015-12-15 14:29:35 -0800

    feat(core): improve stringify for dart to handle closures

--- modules/angular2/src/facade/lang.dart
+++ modules/angular2/src/facade/lang.dart
@@ -30,7 +30,15 @@ bool isPromise(obj) => obj is Future;
 bool isNumber(obj) => obj is num;
 bool isDate(obj) => obj is DateTime;
 
-String stringify(obj) => obj.toString();
+String stringify(obj) {
+  final exp = new RegExp(r"from Function '(\w+)'");
+  final str = obj.toString();
+  if (exp.firstMatch(str) != null) {
+    return exp.firstMatch(str).group(1);
+  } else {
+    return str;
+  }
+}
 
 int serializeEnum(val) {
   return val.index;
