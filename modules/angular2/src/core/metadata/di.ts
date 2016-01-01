dda2e81aec90d64b9dbd19f9643a5cd811c00390
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2016-01-01 12:44:20 +0900

    docs(core): update <content> to <ng-content>

--- modules/angular2/src/core/metadata/di.ts
+++ modules/angular2/src/core/metadata/di.ts
@@ -67,7 +67,7 @@ export class AttributeMetadata extends DependencyMetadata {
  *    <ul>
  *      <li *ngFor="#pane of panes">{{pane.title}}</li>
  *    </ul>
- *    <content></content>
+ *    <ng-content></ng-content>
  *  `
  * })
  * class Tabs {
