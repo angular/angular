644c001655041afc79a7163e6a43ff99bfef681b
Jeremy Elbourn <jelbourn@google.com>
2016-01-08 15:42:23 -0800

    chore(): initial setup for travis ci

---
+++ scripts/build-and-test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -ex
+
+echo "=======  Starting build-and-test.sh  ========================================"
+
+# Go to project dir
+SCRIPT_DIR=$(dirname $0)
+cd ${SCRIPT_DIR}/../..
+
+ng build
+karma start ./karma.conf.js
