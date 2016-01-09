b9c96443e42e3d03c26ca7fa799c3f47f6325491
Jeremy Elbourn <jelbourn@google.com>
2016-01-08 16:30:38 -0800

    chore(): change karma to run on Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
