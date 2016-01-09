db9f55a3a44b6f6596094784b51acf2caae25db3
Jeremy Elbourn <jelbourn@google.com>
2016-01-08 16:07:23 -0800

    chore(): fix file permissions on ci script

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
     singleRun: false
   });
 };
