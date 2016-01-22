9c5f07f0a3311694c7e1a4806a6d840c1448a613
Timothy Blasi <kegluneq@users.noreply.github.com>
2016-01-22 00:08:38 +0000

    47154d5 feat(dart/transform): Promote missing Directive warning to error

--- bundles/testing.dev.js
+++ bundles/testing.dev.js
@@ -1787,11 +1787,12 @@ System.register("angular2/src/testing/test_injector", ["angular2/core", "angular
   return module.exports;
 });
 
-System.register("angular2/src/testing/testing", ["angular2/src/facade/lang", "angular2/src/testing/test_injector", "angular2/src/testing/test_injector", "angular2/src/testing/matchers"], true, function(require, exports, module) {
+System.register("angular2/src/testing/testing", ["angular2/src/facade/lang", "angular2/src/facade/collection", "angular2/src/testing/test_injector", "angular2/src/testing/test_injector", "angular2/src/testing/matchers"], true, function(require, exports, module) {
   var global = System.global,
       __define = global.define;
   global.define = undefined;
   var lang_1 = require("angular2/src/facade/lang");
+  var collection_1 = require("angular2/src/facade/collection");
   var test_injector_1 = require("angular2/src/testing/test_injector");
   var test_injector_2 = require("angular2/src/testing/test_injector");
   exports.inject = test_injector_2.inject;
@@ -1828,22 +1829,75 @@ System.register("angular2/src/testing/testing", ["angular2/src/facade/lang", "an
   function _isPromiseLike(input) {
     return input && !!(input.then);
   }
+  function runInTestZone(fnToExecute, finishCallback, failCallback) {
+    var pendingMicrotasks = 0;
+    var pendingTimeouts = [];
+    var ngTestZone = lang_1.global.zone.fork({
+      onError: function(e) {
+        failCallback(e);
+      },
+      '$run': function(parentRun) {
+        return function() {
+          try {
+            return parentRun.apply(this, arguments);
+          } finally {
+            if (pendingMicrotasks == 0 && pendingTimeouts.length == 0) {
+              finishCallback();
+            }
+          }
+        };
+      },
+      '$scheduleMicrotask': function(parentScheduleMicrotask) {
+        return function(fn) {
+          pendingMicrotasks++;
+          var microtask = function() {
+            try {
+              fn();
+            } finally {
+              pendingMicrotasks--;
+            }
+          };
+          parentScheduleMicrotask.call(this, microtask);
+        };
+      },
+      '$setTimeout': function(parentSetTimeout) {
+        return function(fn, delay) {
+          var args = [];
+          for (var _i = 2; _i < arguments.length; _i++) {
+            args[_i - 2] = arguments[_i];
+          }
+          var id;
+          var cb = function() {
+            fn();
+            collection_1.ListWrapper.remove(pendingTimeouts, id);
+          };
+          id = parentSetTimeout(cb, delay, args);
+          pendingTimeouts.push(id);
+          return id;
+        };
+      },
+      '$clearTimeout': function(parentClearTimeout) {
+        return function(id) {
+          parentClearTimeout(id);
+          collection_1.ListWrapper.remove(pendingTimeouts, id);
+        };
+      }
+    });
+    return ngTestZone.run(fnToExecute);
+  }
   function _it(jsmFn, name, testFn, testTimeOut) {
     var timeOut = testTimeOut;
     if (testFn instanceof test_injector_1.FunctionWithParamTokens) {
       jsmFn(name, function(done) {
-        var returnedTestValue;
-        try {
-          returnedTestValue = testInjector.execute(testFn);
-        } catch (err) {
-          done.fail(err);
-          return ;
-        }
+        var finishCallback = function() {
+          setTimeout(done, 0);
+        };
+        var returnedTestValue = runInTestZone(function() {
+          return testInjector.execute(testFn);
+        }, finishCallback, done.fail);
         if (testFn.isAsync) {
           if (_isPromiseLike(returnedTestValue)) {
-            returnedTestValue.then(function() {
-              done();
-            }, function(err) {
+            returnedTestValue.then(null, function(err) {
               done.fail(err);
             });
           } else {
@@ -1853,7 +1907,6 @@ System.register("angular2/src/testing/testing", ["angular2/src/facade/lang", "an
           if (!(returnedTestValue === undefined)) {
             done.fail('Error: inject returned a value. Did you mean to use injectAsync? Returned ' + 'value was: ' + returnedTestValue);
           }
-          done();
         }
       }, timeOut);
     } else {
@@ -1863,18 +1916,15 @@ System.register("angular2/src/testing/testing", ["angular2/src/facade/lang", "an
   function beforeEach(fn) {
     if (fn instanceof test_injector_1.FunctionWithParamTokens) {
       jsmBeforeEach(function(done) {
-        var returnedTestValue;
-        try {
-          returnedTestValue = testInjector.execute(fn);
-        } catch (err) {
-          done.fail(err);
-          return ;
-        }
+        var finishCallback = function() {
+          setTimeout(done, 0);
+        };
+        var returnedTestValue = runInTestZone(function() {
+          return testInjector.execute(fn);
+        }, finishCallback, done.fail);
         if (fn.isAsync) {
           if (_isPromiseLike(returnedTestValue)) {
-            returnedTestValue.then(function() {
-              done();
-            }, function(err) {
+            returnedTestValue.then(null, function(err) {
               done.fail(err);
             });
           } else {
@@ -1884,7 +1934,6 @@ System.register("angular2/src/testing/testing", ["angular2/src/facade/lang", "an
           if (!(returnedTestValue === undefined)) {
             done.fail('Error: inject returned a value. Did you mean to use injectAsync? Returned ' + 'value was: ' + returnedTestValue);
           }
-          done();
         }
       });
     } else {
