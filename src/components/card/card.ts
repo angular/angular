d823bf29bbdb6ec70fa0ea16ea9d6ff60156c22d
Kara Erickson <karakara@google.com>
2016-01-28 11:39:16 -0800

    feat(card): Add card component

---
+++ src/components/card/card.ts
@@ -0,0 +1,71 @@
+import {Component, View, ViewEncapsulation} from 'angular2/core';
+import {CONST_EXPR, Type} from 'angular2/src/facade/lang';
+
+/*
+
+<md-card> is a basic content container component that adds the styles of a material design card.
+
+While you can use this component alone, it also provides a number of preset styles for common card sections, including:
+ - md-card-title
+ - md-card-subtitle
+ - md-card-content
+ - md-card-actions
+ - md-card-footer
+
+ You can see some examples of cards here:
+ http://embed.plnkr.co/s5O4YcyvbLhIApSrIhtj/
+
+ TODO(kara): update link to demo site when it exists
+
+*/
+
+@Component({
+  selector: 'md-card',
+  templateUrl: './components/card/card.html',
+  styleUrls: ['./components/card/card.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MdCard {}
+
+
+/*  The following components don't have any behavior. They simply use content projection to wrap user content 
+ for flex layout purposes in <md-card> (and thus allow a cleaner, boilerplate-free API). 
+
+ 
+<md-card-header> is a component intended to be used within the <md-card> component.
+It adds styles for a preset header section (i.e. a title, subtitle, and avatar layout).
+
+You can see an example of a card with a header here:
+http://embed.plnkr.co/tvJl19z3gZTQd6WmwkIa/
+
+TODO(kara): update link to demo site when it exists
+*/
+
+@Component({
+  selector: 'md-card-header',
+  templateUrl: '/components/card/card-header.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class MdCardHeader {}
+
+/*
+
+<md-card-title-group> is a component intended to be used within the <md-card> component.
+It adds styles for a preset layout that groups an image with a title section.
+
+You can see an example of a card with a title-group section here:
+http://embed.plnkr.co/EDfgCF9eKcXjini1WODm/
+
+TODO(kara): update link to demo site when it exists
+*/
+
+@Component({
+  selector: 'md-card-title-group',
+  templateUrl: './components/card/card-title-group.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class MdCardTitleGroup {}
+
+export const MD_CARD_DIRECTIVES: Type[] =
+    CONST_EXPR([MdCard, MdCardHeader, MdCardTitleGroup]);
+
--- src/demo-app/demo-app.ts
+++ src/demo-app/demo-app.ts
@@ -1,12 +1,13 @@
 import {Component} from 'angular2/core';
 import {MdButton} from '../components/button/button';
-
+import {MD_CARD_DIRECTIVES} from '../components/card/card';
 
 @Component({
   selector: 'demo-app',
   providers: [],
   templateUrl: 'demo-app/demo-app.html',
-  directives: [MdButton],
+  styleUrls: ['demo-app/demo-app.css'],
+  directives: [MdButton, MD_CARD_DIRECTIVES],
   pipes: []
 })
 export class DemoApp { }
