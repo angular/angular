078f7208eb7ded731be6fca809fe6730e9a61c60
Andrei Alecu <andreialecu@users.noreply.github.com>
2016-02-03 18:07:43 +0200

    fix(upgrade): leak when angular1 destroys element, fix test

--- modules/angular2/test/upgrade/upgrade_spec.ts
+++ modules/angular2/test/upgrade/upgrade_spec.ts
@@ -222,35 +222,33 @@ export function main() {
 
          }));
 
-      it('should properly run cleanup when ng1 directive is destroyed', inject([AsyncTestCompleter], (async) => {
+      it('should properly run cleanup when ng1 directive is destroyed',
+         inject([AsyncTestCompleter], (async) => {
            var adapter: UpgradeAdapter = new UpgradeAdapter();
            var ng1Module = angular.module('ng1', []);
-           var onDestroyed: EventEmitter<boolean> = new EventEmitter<boolean>();
+           var onDestroyed: EventEmitter<string> = new EventEmitter<string>();
 
-           var Ng2 =
-               Component({
-                 selector: 'ng2',
-                 template: "test"
-               })
-                   .Class({
-                     constructor: function() {
+           ng1Module.directive('ng1', () => {
+             return {
+               template: '<div ng-if="!destroyIt"><ng2></ng2></div>',
+               controller: function($rootScope, $timeout) {
+                 $timeout(function() { $rootScope.destroyIt = true; });
+               }
+             };
+           });
 
-                     },
-                     ngOnDestroy: function(changes) {
-                       onDestroyed.emit(true);
-                     }
-                   });
+           var Ng2 = Component({selector: 'ng2', template: 'test'})
+                         .Class({
+                           constructor: function() {},
+                           ngOnDestroy: function() { onDestroyed.emit('destroyed'); }
+                         });
            ng1Module.directive('ng2', adapter.downgradeNg2Component(Ng2));
-           var element = html(`<div>
-              <ng2></ng2>
-              </div>`);
+           var element = html('<ng1></ng1>');
            adapter.bootstrap(element, ['ng1'])
-               .ready((ref) => {
-                 onDestroyed.subscribe(() => {
-                   ref.dispose();
-                   async.done();
-                 })
-               });
+               .ready((ref) => {onDestroyed.subscribe(() => {
+                        ref.dispose();
+                        async.done();
+                      })});
          }));
     });
 
