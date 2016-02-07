24d5b665e1e3a66238b0b8cbf36a66eae5aeda3f
Mike Ryan <mike.ryan52@gmail.com>
2016-02-07 12:51:20 -0600

    docs(HostBindingMetadata): Removed brackets from host bindings in HostBindingsMetadata example
    Closes #6941

--- modules/angular2/src/core/metadata/directives.ts
+++ modules/angular2/src/core/metadata/directives.ts
@@ -1071,8 +1071,8 @@ export class OutputMetadata {
  * @Directive({selector: '[ngModel]'})
  * class NgModelStatus {
  *   constructor(public control:NgModel) {}
- *   @HostBinding('[class.valid]') get valid { return this.control.valid; }
- *   @HostBinding('[class.invalid]') get invalid { return this.control.invalid; }
+ *   @HostBinding('class.valid') get valid { return this.control.valid; }
+ *   @HostBinding('class.invalid') get invalid { return this.control.invalid; }
  * }
  *
  * @Component({
