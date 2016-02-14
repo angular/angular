8fb31f299599f3e0f437dcef451412f00f7a61d5
Peter Bacon Darwin <pete@bacondarwin.com>
2016-02-14 17:59:11 +0000

    refactor(router): move route handlers into their own folder

--- modules/angular2/src/router/rule_set.ts
+++ modules/angular2/src/router/rule_set.ts
@@ -11,8 +11,8 @@ import {
   PathMatch
 } from './rules';
 import {Route, AsyncRoute, AuxRoute, Redirect, RouteDefinition} from './route_config_impl';
-import {AsyncRouteHandler} from './async_route_handler';
-import {SyncRouteHandler} from './sync_route_handler';
+import {AsyncRouteHandler} from './route_handlers/async_route_handler';
+import {SyncRouteHandler} from './route_handlers/sync_route_handler';
 import {Url} from './url_parser';
 import {ComponentInstruction} from './instruction';
 
--- modules/angular2/src/router/rules.ts
+++ modules/angular2/src/router/rules.ts
@@ -3,7 +3,7 @@ import {BaseException} from 'angular2/src/facade/exceptions';
 import {PromiseWrapper} from 'angular2/src/facade/promise';
 import {Map} from 'angular2/src/facade/collection';
 
-import {RouteHandler} from './route_handler';
+import {RouteHandler} from './route_handlers/route_handler';
 import {Url} from './url_parser';
 import {ComponentInstruction} from './instruction';
 import {PathRecognizer, GeneratedUrlSegment} from './path_recognizer';
