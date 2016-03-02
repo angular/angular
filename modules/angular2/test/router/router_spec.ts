520add7b0b0e657ee8444dd949cfe9d6d3cb0201
Peter Bacon Darwin <pete@bacondarwin.com>
2016-03-02 15:04:08 +0000

    fix(router): handle URL that does not match a route
    Closes #7349
    Closes #7203

--- modules/angular2/test/router/router_spec.ts
+++ modules/angular2/test/router/router_spec.ts
@@ -145,6 +145,22 @@ export function main() {
              });
        }));
 
+
+    it('should trigger the onError callback of a router change subscription if the URL does not match a route',
+       inject([AsyncTestCompleter], (async) => {
+         var outlet = makeDummyOutlet();
+
+         router.registerPrimaryOutlet(outlet)
+             .then((_) => router.config([new Route({path: '/a', component: DummyComponent})]))
+             .then((_) => {
+               router.subscribe((_) => {}, (url) => {
+                 expect(url).toEqual('b');
+                 async.done();
+               });
+               (<SpyLocation>location).simulateHashChange('b');
+             });
+       }));
+
     it('should navigate after being configured', inject([AsyncTestCompleter], (async) => {
          var outlet = makeDummyOutlet();
 
