756f5d884f9fa8be60c29cdae13b1d01c19881d3
Tim Blasi <tjblasi@google.com>
2016-03-07 09:17:06 -0800

    refactor(dart/transform): AnnotationMatcher tests
    These were previously not being run.
    
    Bring them up to modern usage, move them to package:test, and include
    them in transform.server.spec.dart.
    
    Closes #7463

--- modules_dart/transform/test/transform/transform.server.spec.dart
+++ modules_dart/transform/test/transform/transform.server.spec.dart
@@ -3,12 +3,14 @@ library angular2.test.transform.transform.server.spec;
 
 import 'package:test/test.dart';
 
+import 'common/annotation_matcher_test.dart' as annotationMatcher;
 import 'common/async_string_writer_tests.dart' as asyncStringWriter;
 import 'common/code/ng_deps_code_tests.dart' as ngDepsCode;
 import 'common/ng_meta_test.dart' as ngMetaTest;
 import 'common/url_resolver_tests.dart' as urlResolver;
 
 main() {
+  group('AnnotationMatcher', annotationMatcher.allTests);
   group('AsyncStringWriter', asyncStringWriter.allTests);
   group('NgDepsCode', ngDepsCode.allTests);
   group('NgMeta', ngMetaTest.allTests);
