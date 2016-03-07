2c7c3e3c69f2d80023cd8ef3ff2dec828d367c18
PatrickJS <github@gdi2290.com>
2016-03-06 22:39:36 -0800

    feat(TAG_DEFINITIONS): include <meta> and <base>
    needed to parse index.html as a component template
    Closes #7455

--- modules/angular2/src/compiler/html_tags.ts
+++ modules/angular2/src/compiler/html_tags.ts
@@ -327,6 +327,8 @@ export class HtmlTagDefinition {
 // see http://www.w3.org/TR/html51/syntax.html#optional-tags
 // This implementation does not fully conform to the HTML5 spec.
 var TAG_DEFINITIONS: {[key: string]: HtmlTagDefinition} = {
+  'base': new HtmlTagDefinition({isVoid: true}),
+  'meta': new HtmlTagDefinition({isVoid: true}),
   'area': new HtmlTagDefinition({isVoid: true}),
   'embed': new HtmlTagDefinition({isVoid: true}),
   'link': new HtmlTagDefinition({isVoid: true}),
