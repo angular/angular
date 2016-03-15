a7fe983be2b03f75a7e310a996e719bc7d8a34cf
vsavkin <avix1000@gmail.com>
2016-03-15 15:01:08 -0700

    feat(i18n): create i18n barrel

---
+++ modules/angular2/i18n.ts
@@ -0,0 +1,8 @@
+/**
+ * @module
+ * @description
+ * Entry point to i18n
+ */
+export * from './src/i18n/message';
+export * from './src/i18n/xmb_serializer';
+export * from './src/i18n/message_extractor';
