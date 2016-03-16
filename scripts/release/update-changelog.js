70ab791addbc1a0b540edb6bb0a555f464992750
DevVersion <paulgschwendtner@me.com>
2016-03-16 17:51:18 +0100

    chore: add conventional changelog script.
    * The script prepends the new conventional changelog from the latest tag to HEAD.
    * When using the `--force` flag, the complete changelog will be generated and overwrites, when present the previous changelog.
    
    Fixes #171.
    
    Closes #179

---
+++ scripts/release/update-changelog.js
@@ -0,0 +1,45 @@
+'use strict';
+/**
+ * Creates a conventional changelog from the current git repository / metadata.
+ */
+
+var fs = require('fs');
+var addStream = require('add-stream');
+var cl = require('conventional-changelog');
+var inStream = fs.createReadStream('CHANGELOG.md');
+
+/**
+ * When the command line argument `--force` is provided, then the full changelog will created and overwritten.
+ * By default, it will only create the changelog from the latest tag to head and prepends it to the changelog.
+ */
+var isForce = process.argv.indexOf('--force') !== -1;
+
+inStream.on('error', function(err) {
+  console.error('An error occurred, while reading the previous changelog file.\n' +
+    'If there is no previous changelog, then you should create an empty file or use the `--force` flag.\n' + err);
+
+  process.exit(1);
+});
+
+var config = {
+  preset: 'angular',
+  releaseCount: isForce ? 0 : 1
+};
+
+var stream = cl(config)
+  .on('error', function(err) {
+    console.error('An error occurred while generating the changelog: ' + err);
+  })
+  .pipe(!isForce && addStream(inStream) || getOutputStream());
+
+// When we are pre-pending the new changelog, then we need to wait for the input stream to be ending,
+// otherwise we can't write into the same destination.
+if (!isForce) {
+  inStream.on('end', function() {
+    stream.pipe(getOutputStream());
+  });
+}
+
+function getOutputStream() {
+  return fs.createWriteStream('CHANGELOG.md');
+}
