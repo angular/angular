17c8ec8a5d7646c5828985f0a4e5160f655413cf
vsavkin <avix1000@gmail.com>
2016-03-23 13:43:28 -0700

    feat(html_parser): change HtmlElementAst to store both the start and the end positions

--- modules/angular2/src/compiler/legacy_template.ts
+++ modules/angular2/src/compiler/legacy_template.ts
@@ -50,7 +50,8 @@ export class LegacyHtmlAstTransformer implements HtmlAstVisitor {
     this.visitingTemplateEl = ast.name.toLowerCase() == 'template';
     let attrs = ast.attrs.map(attr => attr.visit(this, null));
     let children = ast.children.map(child => child.visit(this, null));
-    return new HtmlElementAst(ast.name, attrs, children, ast.sourceSpan);
+    return new HtmlElementAst(ast.name, attrs, children, ast.sourceSpan, ast.startSourceSpan,
+                              ast.endSourceSpan);
   }
 
   visitAttr(originalAst: HtmlAttrAst, context: any): HtmlAttrAst {
