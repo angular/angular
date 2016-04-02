2182e2ba7563a4d4dfd347148fecbe7ba2e0a078
kurthong <kurt@kurt.pe.kr>
2016-04-02 22:32:28 +0900

    formatted with clang-format

--- modules/angular2/test/router/route_registry_spec.ts
+++ modules/angular2/test/router/route_registry_spec.ts
@@ -298,19 +298,20 @@ export function main() {
        }));
 
     it('should match query params on the root component even when the next URL segment is null',
-      inject([AsyncTestCompleter], (async) => {
-        registry.config(RootHostCmp, new Route({ path: '/first/...', component: SingleSlashChildCmp }));
+       inject([AsyncTestCompleter], (async) => {
+         registry.config(RootHostCmp,
+                         new Route({path: '/first/...', component: SingleSlashChildCmp}));
 
-        registry.recognize('/first?comments=all', [])
-          .then((instruction) => {
-            expect(instruction.component.componentType).toBe(SingleSlashChildCmp);
-            expect(instruction.component.params).toEqual({ 'comments': 'all' });
+         registry.recognize('/first?comments=all', [])
+             .then((instruction) => {
+               expect(instruction.component.componentType).toBe(SingleSlashChildCmp);
+               expect(instruction.component.params).toEqual({'comments': 'all'});
 
-            expect(instruction.child.component.componentType).toBe(DummyCmpB);
-            expect(instruction.child.component.params).toEqual({});
-            async.done();
-          });
-      }));
+               expect(instruction.child.component.componentType).toBe(DummyCmpB);
+               expect(instruction.child.component.params).toEqual({});
+               async.done();
+             });
+       }));
 
     it('should generate URLs with matrix and query params', () => {
       registry.config(
