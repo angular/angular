8da9419738dc934443073789d4807cb093d5a164
Vikram Subramanian <viks@google.com>
2016-04-06 15:58:23 -0700

    feat(compiler): Add an implementation for XHR that uses a template cache to load template files.
    Useful for avoiding doing an actual XHR during testing.
    Part of the solution for #4051 (Other part is a Karma plugin that will create the template cache).

--- modules/angular2/platform/testing/browser.ts
+++ modules/angular2/platform/testing/browser.ts
@@ -2,11 +2,14 @@ import {
   TEST_BROWSER_STATIC_PLATFORM_PROVIDERS,
   ADDITIONAL_TEST_BROWSER_PROVIDERS
 } from 'angular2/platform/testing/browser_static';
-
 import {BROWSER_APP_PROVIDERS} from 'angular2/platform/browser';
-
-
 import {CONST_EXPR} from 'angular2/src/facade/lang';
+
+/**
+ * Providers for using template cache to avoid actual XHR.
+ * Re-exported here so that tests import from a single place.
+ */
+export {CACHED_TEMPLATE_PROVIDER} from 'angular2/platform/browser';
 
 /**
  * Default patform providers for testing.
