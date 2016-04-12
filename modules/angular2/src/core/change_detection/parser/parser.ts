2abb414cfbdedd4bf58f1adcee9916107fe1c943
Kara Erickson <karakara@google.com>
2016-04-12 10:19:35 -0700

    feat(i18n): add support for custom placeholder names
    Closes #7799
    Closes #8010

--- modules/angular2/src/core/change_detection/parser/parser.ts
+++ modules/angular2/src/core/change_detection/parser/parser.ts
@@ -50,6 +50,7 @@ import {
 var _implicitReceiver = new ImplicitReceiver();
 // TODO(tbosch): Cannot make this const/final right now because of the transpiler...
 var INTERPOLATION_REGEXP = /\{\{([\s\S]*?)\}\}/g;
+var COMMENT_REGEX = /\/\//g;
 
 class ParseException extends BaseException {
   constructor(message: string, input: string, errLocation: string, ctxLocation?: any) {
@@ -73,7 +74,7 @@ export class Parser {
 
   parseAction(input: string, location: any): ASTWithSource {
     this._checkNoInterpolation(input, location);
-    var tokens = this._lexer.tokenize(input);
+    var tokens = this._lexer.tokenize(this._stripComments(input));
     var ast = new _ParseAST(input, location, tokens, this._reflector, true).parseChain();
     return new ASTWithSource(ast, input, location);
   }
@@ -102,7 +103,7 @@ export class Parser {
     }
 
     this._checkNoInterpolation(input, location);
-    var tokens = this._lexer.tokenize(input);
+    var tokens = this._lexer.tokenize(this._stripComments(input));
     return new _ParseAST(input, location, tokens, this._reflector, false).parseChain();
   }
 
@@ -128,7 +129,7 @@ export class Parser {
     let expressions = [];
 
     for (let i = 0; i < split.expressions.length; ++i) {
-      var tokens = this._lexer.tokenize(split.expressions[i]);
+      var tokens = this._lexer.tokenize(this._stripComments(split.expressions[i]));
       var ast = new _ParseAST(input, location, tokens, this._reflector, false).parseChain();
       expressions.push(ast);
     }
@@ -162,6 +163,10 @@ export class Parser {
 
   wrapLiteralPrimitive(input: string, location: any): ASTWithSource {
     return new ASTWithSource(new LiteralPrimitive(input), input, location);
+  }
+
+  private _stripComments(input: string): string {
+    return StringWrapper.split(input, COMMENT_REGEX)[0].trim();
   }
 
   private _checkNoInterpolation(input: string, location: any): void {
