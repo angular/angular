33dec8d743a74da5f2eb4c53ea8153f1069a955f
Andrei Tserakhau <andrei_tserakhau@epam.com>
2016-04-13 16:47:24 +0300

    fix: add g spot to end of this regexp to prevent dart lint

--- modules/angular2/src/common/pipes/date_pipe.ts
+++ modules/angular2/src/common/pipes/date_pipe.ts
@@ -102,7 +102,7 @@ export class DatePipe implements PipeTransform {
   };
   /** @internal */
   static _ISO8601_STR: RegExp =
-      /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/;
+      /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/g;
 
   transform(value: any, args: any[]): string {
     if (isBlank(value)) return null;
