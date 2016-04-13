d99823e2fd0ef78647de4294d6bb6ab3a40c7145
Kara Erickson <karakara@google.com>
2016-04-13 16:03:30 -0700

    docs(core): fix some grammar
    Closes #8055

--- modules/angular2/src/core/metadata/di.ts
+++ modules/angular2/src/core/metadata/di.ts
@@ -287,14 +287,15 @@ export class ViewQueryMetadata extends QueryMetadata {
 /**
  * Declares a list of child element references.
  *
- * Angular automatically updates the list when the DOM was updated.
+ * Angular automatically updates the list when the DOM is updated.
  *
  * `ViewChildren` takes an argument to select elements.
  *
  * - If the argument is a type, directives or components with the type will be bound.
  *
- * - If the argument is a string, the string behaviors as comma-separated selectors. For each
- * selector, an element matched template variables (e.g. `#child`) will be bound.
+ * - If the argument is a string, the string is interpreted as a list of comma-separated selectors.
+ * For each selector, an element containing the matching template variable (e.g. `#child`) will be
+ * bound.
  *
  * View children are set before the `ngAfterViewInit` callback is called.
  *
@@ -373,11 +374,11 @@ export class ViewChildrenMetadata extends ViewQueryMetadata {
  *
  * - If the argument is a type, a directive or a component with the type will be bound.
  *
- * - If the argument is a string, the string behaviors as a selectors. An element matched template
- * variables (e.g. `#child`) will be bound.
+ If the argument is a string, the string is interpreted as a selector. An element containing the
+ matching template variable (e.g. `#child`) will be bound.
  *
- * In either case, `@ViewChild()` assigns the first (looking from above) element if the result is
- * multiple.
+ * In either case, `@ViewChild()` assigns the first (looking from above) element if there are
+ multiple matches.
  *
  * View child is set before the `ngAfterViewInit` callback is called.
  *
