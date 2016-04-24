80d61db4b030dd16bf38bd343debe09e49f1131f
Igor Minar <igor@angularjs.org>
2016-04-24 13:54:40 -0700

    WIP: bump versions

--- publish-igorminar.sh
+++ publish-igorminar.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-cd `dirname $0 `
+set -ex
+
+cd `dirname $0`
+./build.sh
+
+echo "====== RENAMING @angular to @igorminar ======"
+find ./dist/packages-dist/ -type f -print0 | xargs -0 sed -i '' 's/\@angular/\@igorminar/g'
 
 for PACKAGE in \
   core \
