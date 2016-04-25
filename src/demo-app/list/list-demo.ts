7b3698cb39384f3e96f8783641fa43b40314bb9b
Kara Erickson <karakara@google.com>
2016-04-25 10:23:04 -0700

    feat(list): add icon support

--- src/demo-app/list/list-demo.ts
+++ src/demo-app/list/list-demo.ts
@@ -1,12 +1,13 @@
 import {Component} from 'angular2/core';
 import {MdButton} from '../../components/button/button';
 import {MD_LIST_DIRECTIVES} from '../../components/list/list';
+import {MdIcon} from '../../components/icon/icon';
 
 @Component({
   selector: 'list-demo',
   templateUrl: 'demo-app/list/list-demo.html',
   styleUrls: ['demo-app/list/list-demo.css'],
-  directives: [MD_LIST_DIRECTIVES, MdButton]
+  directives: [MD_LIST_DIRECTIVES, MdButton, MdIcon]
 })
 export class ListDemo {
   items: string[] = [
