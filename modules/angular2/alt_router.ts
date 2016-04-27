4affc9a7bcaba5d672fb547c29ed0322789e789a
vsavkin <avix1000@gmail.com>
2016-04-27 15:37:20 -0700

    feat(router): add RouterLink

--- modules/angular2/alt_router.ts
+++ modules/angular2/alt_router.ts
@@ -8,10 +8,14 @@ export {Router, RouterOutletMap} from './src/alt_router/router';
 export {RouteSegment} from './src/alt_router/segments';
 export {Routes} from './src/alt_router/metadata/decorators';
 export {Route} from './src/alt_router/metadata/metadata';
-export {RouterUrlParser, DefaultRouterUrlParser} from './src/alt_router/router_url_parser';
+export {
+  RouterUrlSerializer,
+  DefaultRouterUrlSerializer
+} from './src/alt_router/router_url_serializer';
 export {OnActivate} from './src/alt_router/interfaces';
 
 import {RouterOutlet} from './src/alt_router/directives/router_outlet';
+import {RouterLink} from './src/alt_router/directives/router_link';
 import {CONST_EXPR} from './src/facade/lang';
 
-export const ROUTER_DIRECTIVES: any[] = CONST_EXPR([RouterOutlet]);
+export const ROUTER_DIRECTIVES: any[] = CONST_EXPR([RouterOutlet, RouterLink]);
--- modules/angular2/test/alt_router/integration_spec.ts
+++ modules/angular2/test/alt_router/integration_spec.ts
@@ -12,9 +12,12 @@ import {
   inject,
   beforeEachProviders,
   it,
-  xit
+  xit,
+  fakeAsync,
+  tick
 } from 'angular2/testing_internal';
 import {provide, Component, ComponentResolver} from 'angular2/core';
+
 
 import {
   Router,
@@ -23,103 +26,127 @@ import {
   Route,
   ROUTER_DIRECTIVES,
   Routes,
-  RouterUrlParser,
-  DefaultRouterUrlParser,
+  RouterUrlSerializer,
+  DefaultRouterUrlSerializer,
   OnActivate
 } from 'angular2/alt_router';
+import {DOM} from 'angular2/src/platform/dom/dom_adapter';
 
 export function main() {
   describe('navigation', () => {
     beforeEachProviders(() => [
-      provide(RouterUrlParser, {useClass: DefaultRouterUrlParser}),
+      provide(RouterUrlSerializer, {useClass: DefaultRouterUrlSerializer}),
       RouterOutletMap,
       provide(Router,
               {
                 useFactory: (resolver, urlParser, outletMap) =>
                                 new Router(RootCmp, resolver, urlParser, outletMap),
-                deps: [ComponentResolver, RouterUrlParser, RouterOutletMap]
+                deps: [ComponentResolver, RouterUrlSerializer, RouterOutletMap]
               })
     ]);
 
     it('should support nested routes',
-       inject([AsyncTestCompleter, Router, TestComponentBuilder], (async, router, tcb) => {
-         let fixture;
-         compileRoot(tcb)
-             .then((rtc) => {fixture = rtc})
-             .then((_) => router.navigateByUrl('/team/22/user/victor'))
-             .then((_) => {
-               fixture.detectChanges();
-               expect(fixture.debugElement.nativeElement)
-                   .toHaveText('team 22 { hello victor, aux:  }');
-               async.done();
-             });
-       }));
+       fakeAsync(inject([Router, TestComponentBuilder], (router, tcb) => {
+         let fixture = tcb.createFakeAsync(RootCmp);
+
+         router.navigateByUrl('/team/22/user/victor');
+         advance(fixture);
+
+         expect(fixture.debugElement.nativeElement).toHaveText('team 22 { hello victor, aux:  }');
+       })));
 
     it('should support aux routes',
-       inject([AsyncTestCompleter, Router, TestComponentBuilder], (async, router, tcb) => {
-         let fixture;
-         compileRoot(tcb)
-             .then((rtc) => {fixture = rtc})
-             .then((_) => router.navigateByUrl('/team/22/user/victor(/simple)'))
-             .then((_) => {
-               fixture.detectChanges();
-               expect(fixture.debugElement.nativeElement)
-                   .toHaveText('team 22 { hello victor, aux: simple }');
-               async.done();
-             });
-       }));
+       fakeAsync(inject([Router, TestComponentBuilder], (router, tcb) => {
+         let fixture = tcb.createFakeAsync(RootCmp);
 
-    it('should unload outlets',
-       inject([AsyncTestCompleter, Router, TestComponentBuilder], (async, router, tcb) => {
-         let fixture;
-         compileRoot(tcb)
-             .then((rtc) => {fixture = rtc})
-             .then((_) => router.navigateByUrl('/team/22/user/victor(/simple)'))
-             .then((_) => router.navigateByUrl('/team/22/user/victor'))
-             .then((_) => {
-               fixture.detectChanges();
-               expect(fixture.debugElement.nativeElement)
-                   .toHaveText('team 22 { hello victor, aux:  }');
-               async.done();
-             });
-       }));
+         router.navigateByUrl('/team/22/user/victor(/simple)');
+         advance(fixture);
+
+         expect(fixture.debugElement.nativeElement)
+             .toHaveText('team 22 { hello victor, aux: simple }');
+       })));
+
+    it('should unload outlets', fakeAsync(inject([Router, TestComponentBuilder], (router, tcb) => {
+         let fixture = tcb.createFakeAsync(RootCmp);
+
+         router.navigateByUrl('/team/22/user/victor(/simple)');
+         advance(fixture);
+
+         router.navigateByUrl('/team/22/user/victor');
+         advance(fixture);
+
+         expect(fixture.debugElement.nativeElement).toHaveText('team 22 { hello victor, aux:  }');
+       })));
 
     it('should unload nested outlets',
-       inject([AsyncTestCompleter, Router, TestComponentBuilder], (async, router, tcb) => {
-         let fixture;
-         compileRoot(tcb)
-             .then((rtc) => {fixture = rtc})
-             .then((_) => router.navigateByUrl('/team/22/user/victor(/simple)'))
-             .then((_) => router.navigateByUrl('/'))
-             .then((_) => {
-               fixture.detectChanges();
-               expect(fixture.debugElement.nativeElement).toHaveText('');
-               async.done();
-             });
-       }));
+       fakeAsync(inject([Router, TestComponentBuilder], (router, tcb) => {
+         let fixture = tcb.createFakeAsync(RootCmp);
+
+         router.navigateByUrl('/team/22/user/victor(/simple)');
+         advance(fixture);
+
+         router.navigateByUrl('/');
+         advance(fixture);
+
+         expect(fixture.debugElement.nativeElement).toHaveText('');
+       })));
 
     it('should update nested routes when url changes',
-       inject([AsyncTestCompleter, Router, TestComponentBuilder], (async, router, tcb) => {
-         let fixture;
-         let team1;
-         let team2;
-         compileRoot(tcb)
-             .then((rtc) => {fixture = rtc})
-             .then((_) => router.navigateByUrl('/team/22/user/victor'))
-             .then((_) => { team1 = fixture.debugElement.children[1].componentInstance; })
-             .then((_) => router.navigateByUrl('/team/22/user/fedor'))
-             .then((_) => { team2 = fixture.debugElement.children[1].componentInstance; })
-             .then((_) => {
-               fixture.detectChanges();
-               expect(team1).toBe(team2);
-               expect(fixture.debugElement.nativeElement)
-                   .toHaveText('team 22 { hello fedor, aux:  }');
-               async.done();
-             });
-       }));
+       fakeAsync(inject([Router, TestComponentBuilder], (router, tcb) => {
+         let fixture = tcb.createFakeAsync(RootCmp);
 
-    // unload unused nodes
+         router.navigateByUrl('/team/22/user/victor');
+         advance(fixture);
+         let team1 = fixture.debugElement.children[1].componentInstance;
+
+         router.navigateByUrl('/team/22/user/fedor');
+         advance(fixture);
+         let team2 = fixture.debugElement.children[1].componentInstance;
+
+         expect(team1).toBe(team2);
+         expect(fixture.debugElement.nativeElement).toHaveText('team 22 { hello fedor, aux:  }');
+       })));
+
+    it("should support router links",
+       fakeAsync(inject([Router, TestComponentBuilder], (router, tcb) => {
+         let fixture = tcb.createFakeAsync(RootCmp);
+         advance(fixture);
+
+         router.navigateByUrl('/team/22/link');
+         advance(fixture);
+         expect(fixture.debugElement.nativeElement).toHaveText('team 22 { link, aux:  }');
+
+         let native = DOM.querySelector(fixture.debugElement.nativeElement, "a");
+         expect(DOM.getAttribute(native, "href")).toEqual("/team/33/simple");
+         DOM.dispatchEvent(native, DOM.createMouseEvent('click'));
+         advance(fixture);
+
+         expect(fixture.debugElement.nativeElement).toHaveText('team 33 { simple, aux:  }');
+       })));
+
+    it("should update router links when router changes",
+       fakeAsync(inject([Router, TestComponentBuilder], (router, tcb) => {
+         let fixture = tcb.createFakeAsync(RootCmp);
+         advance(fixture);
+
+         router.navigateByUrl('/team/22/link(simple)');
+         advance(fixture);
+         expect(fixture.debugElement.nativeElement).toHaveText('team 22 { link, aux: simple }');
+
+         let native = DOM.querySelector(fixture.debugElement.nativeElement, "a");
+         expect(DOM.getAttribute(native, "href")).toEqual("/team/33/simple(aux:simple)");
+
+         router.navigateByUrl('/team/22/link(simple2)');
+         advance(fixture);
+
+         expect(DOM.getAttribute(native, "href")).toEqual("/team/33/simple(aux:simple2)");
+       })));
   });
+}
+
+function advance(fixture: ComponentFixture): void {
+  tick();
+  fixture.detectChanges();
 }
 
 function compileRoot(tcb: TestComponentBuilder): Promise<ComponentFixture> {
@@ -136,6 +163,18 @@ class UserCmp implements OnActivate {
 class SimpleCmp {
 }
 
+@Component({selector: 'simple2-cmp', template: `simple2`})
+class Simple2Cmp {
+}
+
+@Component({
+  selector: 'link-cmp',
+  template: `<a [routerLink]="['team', '33', 'simple']">link</a>`,
+  directives: ROUTER_DIRECTIVES
+})
+class LinkCmp {
+}
+
 @Component({
   selector: 'team-cmp',
   template: `team {{id}} { <router-outlet></router-outlet>, aux: <router-outlet name="aux"></router-outlet> }`,
@@ -143,7 +182,9 @@ class SimpleCmp {
 })
 @Routes([
   new Route({path: 'user/:name', component: UserCmp}),
-  new Route({path: 'simple', component: SimpleCmp})
+  new Route({path: 'simple', component: SimpleCmp}),
+  new Route({path: 'simple2', component: Simple2Cmp}),
+  new Route({path: 'link', component: LinkCmp})
 ])
 class TeamCmp implements OnActivate {
   id: string;
