849f094d9b059171838c34e16074c3b10dfaa2ec
Tobias Bosch <tbosch1009@gmail.com>
2016-04-29 12:09:29 -0700

    refactor(core): use `Function.bind` for referring to event listeners instead of a closure.

--- modules/angular2/src/compiler/view_compiler/event_binder.ts
+++ modules/angular2/src/compiler/view_compiler/event_binder.ts
@@ -82,13 +82,9 @@ export class CompileEventListener {
 
   listenToRenderer() {
     var listenExpr;
-    var eventListener = o.THIS_EXPR.callMethod('eventHandler', [
-      o.fn([this._eventParam],
-           [
-             new o.ReturnStatement(
-                 o.THIS_EXPR.callMethod(this._methodName, [EventHandlerVars.event]))
-           ])
-    ]);
+    var eventListener = o.THIS_EXPR.callMethod(
+        'eventHandler',
+        [o.THIS_EXPR.prop(this._methodName).callMethod(o.BuiltinMethod.bind, [o.THIS_EXPR])]);
     if (isPresent(this.eventTarget)) {
       listenExpr = ViewProperties.renderer.callMethod(
           'listenGlobal', [o.literal(this.eventTarget), o.literal(this.eventName), eventListener]);
@@ -105,10 +101,9 @@ export class CompileEventListener {
   listenToDirective(directiveInstance: o.Expression, observablePropName: string) {
     var subscription = o.variable(`subscription_${this.compileElement.view.subscriptions.length}`);
     this.compileElement.view.subscriptions.push(subscription);
-    var eventListener = o.THIS_EXPR.callMethod('eventHandler', [
-      o.fn([this._eventParam],
-           [o.THIS_EXPR.callMethod(this._methodName, [EventHandlerVars.event]).toStmt()])
-    ]);
+    var eventListener = o.THIS_EXPR.callMethod(
+        'eventHandler',
+        [o.THIS_EXPR.prop(this._methodName).callMethod(o.BuiltinMethod.bind, [o.THIS_EXPR])]);
     this.compileElement.view.createMethod.addStmt(
         subscription.set(directiveInstance.prop(observablePropName)
                              .callMethod(o.BuiltinMethod.SubscribeObservable, [eventListener]))
