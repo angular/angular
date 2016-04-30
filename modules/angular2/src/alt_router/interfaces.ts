deba80467141812a30408ab967ca80694f96d547
vsavkin <avix1000@gmail.com>
2016-04-29 18:05:06 -0700

    feat(router): add CanDeactivate

--- modules/angular2/src/alt_router/interfaces.ts
+++ modules/angular2/src/alt_router/interfaces.ts
@@ -4,3 +4,8 @@ export interface OnActivate {
   routerOnActivate(curr: RouteSegment, prev?: RouteSegment, currTree?: Tree<RouteSegment>,
                    prevTree?: Tree<RouteSegment>): void;
 }
+
+export interface CanDeactivate {
+  routerCanDeactivate(currTree?: Tree<RouteSegment>,
+                      futureTree?: Tree<RouteSegment>): Promise<boolean>;
+}
