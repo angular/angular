angular/angular

commit 152a117d5c27e56d1b32d69df2f69d34b94c0760

fix(compiler): properly implement pure pipes and change pipe syntax
Pure pipes as well as arrays and maps are
implemented via proxy functions. This is
faster than the previous implementation
and also generates less code.

BREAKING CHANGE:
- pipes now take a variable number of arguments, and not an array that contains all arguments.

modules/angular2/src/compiler/identifiers.ts
@@ -5,7 +5,18 @@ import {
   ViewUtils,
   flattenNestedViewRenderNodes,
   interpolate,
-  checkBinding
+  checkBinding,
+  castByValue,
+  pureProxy1,
+  pureProxy2,
+  pureProxy3,
+  pureProxy4,
+  pureProxy5,
+  pureProxy6,
+  pureProxy7,
+  pureProxy8,
+  pureProxy9,
+  pureProxy10
 } from 'angular2/src/core/linker/view_utils';
 import {
   uninitialized,
@@ -59,6 +70,7 @@ var impFlattenNestedViewRenderNodes = flattenNestedViewRenderNodes;
 var impDevModeEqual = devModeEqual;
 var impInterpolate = interpolate;
 var impCheckBinding = checkBinding;
+var impCastByValue = castByValue;
 
 export class Identifiers {
   static ViewUtils = new CompileIdentifierMetadata({
@@ -162,6 +174,31 @@ export class Identifiers {
       {name: 'devModeEqual', moduleUrl: CD_MODULE_URL, runtime: impDevModeEqual});
   static interpolate = new CompileIdentifierMetadata(
       {name: 'interpolate', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: impInterpolate});
+  static castByValue = new CompileIdentifierMetadata(
+      {name: 'castByValue', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: impCastByValue});
+  static pureProxies = [
+    null,
+    new CompileIdentifierMetadata(
+        {name: 'pureProxy1', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: pureProxy1}),
+    new CompileIdentifierMetadata(
+        {name: 'pureProxy2', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: pureProxy2}),
+    new CompileIdentifierMetadata(
+        {name: 'pureProxy3', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: pureProxy3}),
+    new CompileIdentifierMetadata(
+        {name: 'pureProxy4', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: pureProxy4}),
+    new CompileIdentifierMetadata(
+        {name: 'pureProxy5', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: pureProxy5}),
+    new CompileIdentifierMetadata(
+        {name: 'pureProxy6', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: pureProxy6}),
+    new CompileIdentifierMetadata(
+        {name: 'pureProxy7', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: pureProxy7}),
+    new CompileIdentifierMetadata(
+        {name: 'pureProxy8', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: pureProxy8}),
+    new CompileIdentifierMetadata(
+        {name: 'pureProxy9', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: pureProxy9}),
+    new CompileIdentifierMetadata(
+        {name: 'pureProxy10', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: pureProxy10}),
+  ];
 }
 
 export function identifierToken(identifier: CompileIdentifierMetadata): CompileTokenMetadata {

modules/angular2/src/compiler/view_compiler/compile_view.ts
@@ -1,14 +1,13 @@
 import {isPresent, isBlank} from 'angular2/src/facade/lang';
-import {ListWrapper, StringMapWrapper} from 'angular2/src/facade/collection';
-import {BaseException} from 'angular2/src/facade/exceptions';
+import {ListWrapper, StringMapWrapper, MapWrapper} from 'angular2/src/facade/collection';
 
 import * as o from '../output/output_ast';
-import {Identifiers, identifierToken} from '../identifiers';
 import {EventHandlerVars} from './constants';
 import {CompileQuery, createQueryList, addQueryToTokenMap} from './compile_query';
 import {NameResolver} from './expression_converter';
 import {CompileElement, CompileNode} from './compile_element';
 import {CompileMethod} from './compile_method';
+import {CompilePipe} from './compile_pipe';
 import {ViewType} from 'angular2/src/core/linker/view_type';
 import {
   CompileDirectiveMetadata,
@@ -20,16 +19,11 @@ import {
   getViewFactoryName,
   injectFromViewParentInjector,
   createDiTokenExpression,
-  getPropertyInView
+  getPropertyInView,
+  createPureProxy
 } from './util';
 import {CompilerConfig} from '../config';
 import {CompileBinding} from './compile_binding';
-
-import {bindPipeDestroyLifecycleCallbacks} from './lifecycle_binder';
-
-export class CompilePipe {
-  constructor() {}
-}
 
 export class CompileView implements NameResolver {
   public viewType: ViewType;
@@ -60,7 +54,8 @@ export class CompileView implements NameResolver {
   public subscriptions: o.Expression[] = [];
 
   public componentView: CompileView;
-  public pipes = new Map<string, o.Expression>();
+  public purePipes = new Map<string, CompilePipe>();
+  public pipes: CompilePipe[] = [];
   public variables = new Map<string, o.Expression>();
   public className: string;
   public classType: o.Type;
@@ -68,6 +63,7 @@ export class CompileView implements NameResolver {
 
   public literalArrayCount = 0;
   public literalMapCount = 0;
+  public pipeCount = 0;
 
   constructor(public component: CompileDirectiveMetadata, public genConfig: CompilerConfig,
               public pipeMetas: CompilePipeMetadata[], public styles: o.Expression,
@@ -124,39 +120,17 @@ export class CompileView implements NameResolver {
     }
   }
 
-  createPipe(name: string): o.Expression {
-    var pipeMeta: CompilePipeMetadata = null;
-    for (var i = this.pipeMetas.length - 1; i >= 0; i--) {
-      var localPipeMeta = this.pipeMetas[i];
-      if (localPipeMeta.name == name) {
-        pipeMeta = localPipeMeta;
-        break;
+  callPipe(name: string, input: o.Expression, args: o.Expression[]): o.Expression {
+    var compView = this.componentView;
+    var pipe = compView.purePipes.get(name);
+    if (isBlank(pipe)) {
+      pipe = new CompilePipe(compView, name);
+      if (pipe.pure) {
+        compView.purePipes.set(name, pipe);
       }
+      compView.pipes.push(pipe);
     }
-    if (isBlank(pipeMeta)) {
-      throw new BaseException(
-          `Illegal state: Could not find pipe ${name} although the parser should have detected this error!`);
-    }
-    var pipeFieldName = pipeMeta.pure ? `_pipe_${name}` : `_pipe_${name}_${this.pipes.size}`;
-    var pipeExpr = this.pipes.get(pipeFieldName);
-    if (isBlank(pipeExpr)) {
-      var deps = pipeMeta.type.diDeps.map((diDep) => {
-        if (diDep.token.equalsTo(identifierToken(Identifiers.ChangeDetectorRef))) {
-          return o.THIS_EXPR.prop('ref');
-        }
-        return injectFromViewParentInjector(diDep.token, false);
-      });
-      this.fields.push(
-          new o.ClassField(pipeFieldName, o.importType(pipeMeta.type), [o.StmtModifier.Private]));
-      this.createMethod.resetDebugInfo(null, null);
-      this.createMethod.addStmt(o.THIS_EXPR.prop(pipeFieldName)
-                                    .set(o.importExpr(pipeMeta.type).instantiate(deps))
-                                    .toStmt());
-      pipeExpr = o.THIS_EXPR.prop(pipeFieldName);
-      this.pipes.set(pipeFieldName, pipeExpr);
-      bindPipeDestroyLifecycleCallbacks(pipeMeta, pipeExpr, this);
-    }
-    return pipeExpr;
+    return pipe.call(this, [input].concat(args));
   }
 
   getVariable(name: string): o.Expression {
@@ -165,29 +139,49 @@ export class CompileView implements NameResolver {
     }
     var currView: CompileView = this;
     var result = currView.variables.get(name);
-    var viewPath = [];
     while (isBlank(result) && isPresent(currView.declarationElement.view)) {
       currView = currView.declarationElement.view;
       result = currView.variables.get(name);
-      viewPath.push(currView);
     }
     if (isPresent(result)) {
-      return getPropertyInView(result, viewPath);
+      return getPropertyInView(result, this, currView);
     } else {
       return null;
     }
   }
 
   createLiteralArray(values: o.Expression[]): o.Expression {
-    return o.THIS_EXPR.callMethod('literalArray',
-                                  [o.literal(this.literalArrayCount++), o.literalArr(values)]);
+    var proxyExpr = o.THIS_EXPR.prop(`_arr_${this.literalArrayCount++}`);
+    var proxyParams: o.FnParam[] = [];
+    var proxyReturnEntries: o.Expression[] = [];
+    for (var i = 0; i < values.length; i++) {
+      var paramName = `p${i}`;
+      proxyParams.push(new o.FnParam(paramName));
+      proxyReturnEntries.push(o.variable(paramName));
+    }
+    createPureProxy(o.fn(proxyParams, [new o.ReturnStatement(o.literalArr(proxyReturnEntries))]),
+                    values.length, proxyExpr, this);
+    return proxyExpr.callFn(values);
   }
-  createLiteralMap(values: Array<Array<string | o.Expression>>): o.Expression {
-    return o.THIS_EXPR.callMethod('literalMap',
-                                  [o.literal(this.literalMapCount++), o.literalMap(values)]);
+
+  createLiteralMap(entries: Array<Array<string | o.Expression>>): o.Expression {
+    var proxyExpr = o.THIS_EXPR.prop(`_map_${this.literalMapCount++}`);
+    var proxyParams: o.FnParam[] = [];
+    var proxyReturnEntries: Array<Array<string | o.Expression>> = [];
+    var values: o.Expression[] = [];
+    for (var i = 0; i < entries.length; i++) {
+      var paramName = `p${i}`;
+      proxyParams.push(new o.FnParam(paramName));
+      proxyReturnEntries.push([entries[i][0], o.variable(paramName)]);
+      values.push(<o.Expression>entries[i][1]);
+    }
+    createPureProxy(o.fn(proxyParams, [new o.ReturnStatement(o.literalMap(proxyReturnEntries))]),
+                    entries.length, proxyExpr, this);
+    return proxyExpr.callFn(values);
   }
 
   afterNodes() {
+    this.pipes.forEach((pipe) => pipe.create());
     this.viewQueries.values().forEach(
         (queries) => queries.forEach((query) => query.afterChildren(this.updateViewQueriesMethod)));
   }

modules/angular2/src/core/linker/view_utils.ts
@@ -10,7 +10,7 @@ import {ListWrapper, StringMapWrapper} from 'angular2/src/facade/collection';
 import {BaseException} from 'angular2/src/facade/exceptions';
 import {AppElement} from './element';
 import {ExpressionChangedAfterItHasBeenCheckedException} from './exceptions';
-import {devModeEqual} from 'angular2/src/core/change_detection/change_detection';
+import {devModeEqual, uninitialized} from 'angular2/src/core/change_detection/change_detection';
 import {Inject, Injectable} from 'angular2/src/core/di';
 import {RootRenderer, RenderComponentType, Renderer} from 'angular2/src/core/render/api';
 import {APP_ID} from 'angular2/src/core/application_tokens';
@@ -157,4 +157,210 @@ export function mapLooseIdentical<V>(m1: {[key: string]: V}, m2: {[key: string]:
     }
   }
   return true;
+}
+
+export function castByValue<T>(input: any, value: T): T {
+  return <T>input;
+}
+
+export function pureProxy1<P0, R>(fn: (p0: P0) => R): (p0: P0) => R {
+  var result: R;
+  var v0;
+  v0 = uninitialized;
+  return (p0) => {
+    if (!looseIdentical(v0, p0)) {
+      v0 = p0;
+      result = fn(p0);
+    }
+    return result;
+  };
+}
+
+export function pureProxy2<P0, P1, R>(fn: (p0: P0, p1: P1) => R): (p0: P0, p1: P1) => R {
+  var result: R;
+  var v0, v1;
+  v0 = v1 = uninitialized;
+  return (p0, p1) => {
+    if (!looseIdentical(v0, p0) || !looseIdentical(v1, p1)) {
+      v0 = p0;
+      v1 = p1;
+      result = fn(p0, p1);
+    }
+    return result;
+  };
+}
+
+export function pureProxy3<P0, P1, P2, R>(fn: (p0: P0, p1: P1, p2: P2) => R): (p0: P0, p1: P1,
+                                                                               p2: P2) => R {
+  var result: R;
+  var v0, v1, v2;
+  v0 = v1 = v2 = uninitialized;
+  return (p0, p1, p2) => {
+    if (!looseIdentical(v0, p0) || !looseIdentical(v1, p1) || !looseIdentical(v2, p2)) {
+      v0 = p0;
+      v1 = p1;
+      v2 = p2;
+      result = fn(p0, p1, p2);
+    }
+    return result;
+  };
+}
+
+export function pureProxy4<P0, P1, P2, P3, R>(fn: (p0: P0, p1: P1, p2: P2, p3: P3) => R): (
+    p0: P0, p1: P1, p2: P2, p3: P3) => R {
+  var result: R;
+  var v0, v1, v2, v3;
+  v0 = v1 = v2 = v3 = uninitialized;
+  return (p0, p1, p2, p3) => {
+    if (!looseIdentical(v0, p0) || !looseIdentical(v1, p1) || !looseIdentical(v2, p2) ||
+        !looseIdentical(v3, p3)) {
+      v0 = p0;
+      v1 = p1;
+      v2 = p2;
+      v3 = p3;
+      result = fn(p0, p1, p2, p3);
+    }
+    return result;
+  };
+}
+
+export function pureProxy5<P0, P1, P2, P3, P4, R>(
+    fn: (p0: P0, p1: P1, p2: P2, p3: P3, p4: P4) => R): (p0: P0, p1: P1, p2: P2, p3: P3, p4: P4) =>
+    R {
+  var result: R;
+  var v0, v1, v2, v3, v4;
+  v0 = v1 = v2 = v3 = v4 = uninitialized;
+  return (p0, p1, p2, p3, p4) => {
+    if (!looseIdentical(v0, p0) || !looseIdentical(v1, p1) || !looseIdentical(v2, p2) ||
+        !looseIdentical(v3, p3) || !looseIdentical(v4, p4)) {
+      v0 = p0;
+      v1 = p1;
+      v2 = p2;
+      v3 = p3;
+      v4 = p4;
+      result = fn(p0, p1, p2, p3, p4);
+    }
+    return result;
+  };
+}
+
+
+export function pureProxy6<P0, P1, P2, P3, P4, P5, R>(
+    fn: (p0: P0, p1: P1, p2: P2, p3: P3, p4: P4, p5: P5) => R): (p0: P0, p1: P1, p2: P2, p3: P3,
+                                                                 p4: P4, p5: P5) => R {
+  var result: R;
+  var v0, v1, v2, v3, v4, v5;
+  v0 = v1 = v2 = v3 = v4 = v5 = uninitialized;
+  return (p0, p1, p2, p3, p4, p5) => {
+    if (!looseIdentical(v0, p0) || !looseIdentical(v1, p1) || !looseIdentical(v2, p2) ||
+        !looseIdentical(v3, p3) || !looseIdentical(v4, p4) || !looseIdentical(v5, p5)) {
+      v0 = p0;
+      v1 = p1;
+      v2 = p2;
+      v3 = p3;
+      v4 = p4;
+      v5 = p5;
+      result = fn(p0, p1, p2, p3, p4, p5);
+    }
+    return result;
+  };
+}
+
+export function pureProxy7<P0, P1, P2, P3, P4, P5, P6, R>(
+    fn: (p0: P0, p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6) =>
+        R): (p0: P0, p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6) => R {
+  var result: R;
+  var v0, v1, v2, v3, v4, v5, v6;
+  v0 = v1 = v2 = v3 = v4 = v5 = v6 = uninitialized;
+  return (p0, p1, p2, p3, p4, p5, p6) => {
+    if (!looseIdentical(v0, p0) || !looseIdentical(v1, p1) || !looseIdentical(v2, p2) ||
+        !looseIdentical(v3, p3) || !looseIdentical(v4, p4) || !looseIdentical(v5, p5) ||
+        !looseIdentical(v6, p6)) {
+      v0 = p0;
+      v1 = p1;
+      v2 = p2;
+      v3 = p3;
+      v4 = p4;
+      v5 = p5;
+      v6 = p6;
+      result = fn(p0, p1, p2, p3, p4, p5, p6);
+    }
+    return result;
+  };
+}
+
+export function pureProxy8<P0, P1, P2, P3, P4, P5, P6, P7, R>(
+    fn: (p0: P0, p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6, p7: P7) =>
+        R): (p0: P0, p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6, p7: P7) => R {
+  var result: R;
+  var v0, v1, v2, v3, v4, v5, v6, v7;
+  v0 = v1 = v2 = v3 = v4 = v5 = v6 = v7 = uninitialized;
+  return (p0, p1, p2, p3, p4, p5, p6, p7) => {
+    if (!looseIdentical(v0, p0) || !looseIdentical(v1, p1) || !looseIdentical(v2, p2) ||
+        !looseIdentical(v3, p3) || !looseIdentical(v4, p4) || !looseIdentical(v5, p5) ||
+        !looseIdentical(v6, p6) || !looseIdentical(v7, p7)) {
+      v0 = p0;
+      v1 = p1;
+      v2 = p2;
+      v3 = p3;
+      v4 = p4;
+      v5 = p5;
+      v6 = p6;
+      v7 = p7;
+      result = fn(p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+    return result;
+  };
+}
+
+export function pureProxy9<P0, P1, P2, P3, P4, P5, P6, P7, P8, R>(
+    fn: (p0: P0, p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6, p7: P7, p8: P8) =>
+        R): (p0: P0, p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6, p7: P7, p8: P8) => R {
+  var result: R;
+  var v0, v1, v2, v3, v4, v5, v6, v7, v8;
+  v0 = v1 = v2 = v3 = v4 = v5 = v6 = v7 = v8 = uninitialized;
+  return (p0, p1, p2, p3, p4, p5, p6, p7, p8) => {
+    if (!looseIdentical(v0, p0) || !looseIdentical(v1, p1) || !looseIdentical(v2, p2) ||
+        !looseIdentical(v3, p3) || !looseIdentical(v4, p4) || !looseIdentical(v5, p5) ||
+        !looseIdentical(v6, p6) || !looseIdentical(v7, p7) || !looseIdentical(v8, p8)) {
+      v0 = p0;
+      v1 = p1;
+      v2 = p2;
+      v3 = p3;
+      v4 = p4;
+      v5 = p5;
+      v6 = p6;
+      v7 = p7;
+      v8 = p8;
+      result = fn(p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+    return result;
+  };
+}
+
+export function pureProxy10<P0, P1, P2, P3, P4, P5, P6, P7, P8, P9, R>(
+    fn: (p0: P0, p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6, p7: P7, p8: P8, p9: P9) =>
+        R): (p0: P0, p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6, p7: P7, p8: P8, p9: P9) => R {
+  var result: R;
+  var v0, v1, v2, v3, v4, v5, v6, v7, v8, v9;
+  v0 = v1 = v2 = v3 = v4 = v5 = v6 = v7 = v8 = v9 = uninitialized;
+  return (p0, p1, p2, p3, p4, p5, p6, p7, p8, p9) => {
+    if (!looseIdentical(v0, p0) || !looseIdentical(v1, p1) || !looseIdentical(v2, p2) ||
+        !looseIdentical(v3, p3) || !looseIdentical(v4, p4) || !looseIdentical(v5, p5) ||
+        !looseIdentical(v6, p6) || !looseIdentical(v7, p7) || !looseIdentical(v8, p8) ||
+        !looseIdentical(v9, p9)) {
+      v0 = p0;
+      v1 = p1;
+      v2 = p2;
+      v3 = p3;
+      v4 = p4;
+      v5 = p5;
+      v6 = p6;
+      v7 = p7;
+      v8 = p8;
+      v9 = p9;
+      result = fn(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+    return result;
+  };
 }

modules/angular2/test/core/linker/change_detection_integration_spec.ts
@@ -460,6 +460,13 @@ export function main() {
            }));
 
         it('should associate pipes right-to-left', fakeAsync(() => {
+             var ctx = _bindSimpleValue('name | multiArgPipe:"a":"b" | multiArgPipe:0:1', Person);
+             ctx.componentInstance.name = 'value';
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['value a b default 0 1 default']);
+           }));
+
+        it('should support calling pure pipes with different number of arguments', fakeAsync(() => {
              var ctx = _bindSimpleValue('name | multiArgPipe:"a":"b" | multiArgPipe:0:1:2', Person);
              ctx.componentInstance.name = 'value';
              ctx.detectChanges(false);
@@ -490,6 +497,56 @@ export function main() {
              ctx.detectChanges(false);
 
              expect(renderLog.log).toEqual(['someProp=Megatron']);
+           }));
+
+        it('should call pure pipes only if the arguments change', fakeAsync(() => {
+             var ctx = _bindSimpleValue('name | countingPipe', Person);
+             // change from undefined -> null
+             ctx.componentInstance.name = null;
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['null state:0']);
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['null state:0']);
+
+             // change from null -> some value
+             ctx.componentInstance.name = 'bob';
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['null state:0', 'bob state:1']);
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['null state:0', 'bob state:1']);
+
+             // change from some value -> some other value
+             ctx.componentInstance.name = 'bart';
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues)
+                 .toEqual(['null state:0', 'bob state:1', 'bart state:2']);
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues)
+                 .toEqual(['null state:0', 'bob state:1', 'bart state:2']);
+
+           }));
+
+        it('should call pure pipes that are used multiple times only when the arguments change',
+           fakeAsync(() => {
+             var ctx = createCompFixture(`<div [someProp]="name | countingPipe"></div><div [someProp]="age | countingPipe"></div>`, Person);
+             ctx.componentInstance.name = 'a';
+             ctx.componentInstance.age = 10;
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['a state:0', '10 state:1']);
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['a state:0', '10 state:1']);
+             ctx.componentInstance.age = 11;
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['a state:0', '10 state:1', '11 state:2']);
+           }));
+
+        it('should call impure pipes on each change detection run', fakeAsync(() => {
+             var ctx = _bindSimpleValue('name | countingImpurePipe', Person);
+             ctx.componentInstance.name = 'bob';
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['bob state:0']);
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['bob state:0', 'bob state:1']);
            }));
       });
 
@@ -1014,6 +1071,7 @@ const ALL_DIRECTIVES = CONST_EXPR([
 
 const ALL_PIPES = CONST_EXPR([
   forwardRef(() => CountingPipe),
+  forwardRef(() => CountingImpurePipe),
   forwardRef(() => MultiArgPipe),
   forwardRef(() => PipeWithOnDestroy),
   forwardRef(() => IdentityPipe),
@@ -1086,7 +1144,13 @@ class DirectiveLog {
 @Pipe({name: 'countingPipe'})
 class CountingPipe implements PipeTransform {
   state: number = 0;
-  transform(value, args = null) { return `${value} state:${this.state ++}`; }
+  transform(value) { return `${value} state:${this.state ++}`; }
+}
+
+@Pipe({name: 'countingImpurePipe', pure: false})
+class CountingImpurePipe implements PipeTransform {
+  state: number = 0;
+  transform(value) { return `${value} state:${this.state ++}`; }
 }
 
 @Pipe({name: 'pipeWithOnDestroy'})
@@ -1095,27 +1159,22 @@ class PipeWithOnDestroy implements PipeTransform, OnDestroy {
 
   ngOnDestroy() { this.directiveLog.add('pipeWithOnDestroy', 'ngOnDestroy'); }
 
-  transform(value, args = null) { return null; }
+  transform(value) { return null; }
 }
 
 @Pipe({name: 'identityPipe'})
 class IdentityPipe implements PipeTransform {
-  transform(value, args = null) { return value; }
+  transform(value) { return value; }
 }
 
 @Pipe({name: 'wrappedPipe'})
 class WrappedPipe implements PipeTransform {
-  transform(value, args = null) { return WrappedValue.wrap(value); }
+  transform(value) { return WrappedValue.wrap(value); }
 }
 
 @Pipe({name: 'multiArgPipe'})
 class MultiArgPipe implements PipeTransform {
-  transform(value, args = null) {
-    var arg1 = args[0];
-    var arg2 = args[1];
-    var arg3 = args.length > 2 ? args[2] : 'default';
-    return `${value} ${arg1} ${arg2} ${arg3}`;
-  }
+  transform(value, arg1, arg2, arg3 = 'default') { return `${value} ${arg1} ${arg2} ${arg3}`; }
 }
 
 @Component({selector: 'test-cmp', template: '', directives: ALL_DIRECTIVES, pipes: ALL_PIPES})

modules/angular2/src/common/pipes/async_pipe.ts
@@ -1,13 +1,6 @@
 import {isBlank, isPresent, isPromise, CONST} from 'angular2/src/facade/lang';
 import {ObservableWrapper, Observable, EventEmitter} from 'angular2/src/facade/async';
-import {
-  Pipe,
-  Injectable,
-  ChangeDetectorRef,
-  OnDestroy,
-  PipeTransform,
-  WrappedValue
-} from 'angular2/core';
+import {Pipe, Injectable, ChangeDetectorRef, OnDestroy, WrappedValue} from 'angular2/core';
 
 import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
 
@@ -55,7 +48,7 @@ var __unused: Promise<any>;  // avoid unused import when Promise union types are
  */
 @Pipe({name: 'async', pure: false})
 @Injectable()
-export class AsyncPipe implements PipeTransform, OnDestroy {
+export class AsyncPipe implements OnDestroy {
   /** @internal */
   _latestValue: Object = null;
   /** @internal */
@@ -76,7 +69,7 @@ export class AsyncPipe implements PipeTransform, OnDestroy {
     }
   }
 
-  transform(obj: Observable<any>| Promise<any>| EventEmitter<any>, args?: any[]): any {
+  transform(obj: Observable<any>| Promise<any>| EventEmitter<any>): any {
     if (isBlank(this._obj)) {
       if (isPresent(obj)) {
         this._subscribe(obj);

modules/angular2/src/common/pipes/date_pipe.ts
@@ -101,14 +101,13 @@ export class DatePipe implements PipeTransform {
   };
 
 
-  transform(value: any, args: any[]): string {
+  transform(value: any, pattern: string = 'mediumDate'): string {
     if (isBlank(value)) return null;
 
     if (!this.supports(value)) {
       throw new InvalidPipeArgumentException(DatePipe, value);
     }
 
-    var pattern: string = isPresent(args) && args.length > 0 ? args[0] : 'mediumDate';
     if (isNumber(value)) {
       value = DateWrapper.fromMillis(value);
     }

modules/angular2/src/common/pipes/i18n_plural_pipe.ts
@@ -45,10 +45,9 @@ var interpolationExp: RegExp = RegExpWrapper.create('#');
 @Pipe({name: 'i18nPlural', pure: true})
 @Injectable()
 export class I18nPluralPipe implements PipeTransform {
-  transform(value: number, args: any[] = null): string {
+  transform(value: number, pluralMap: {[count: string]: string}): string {
     var key: string;
     var valueStr: string;
-    var pluralMap: {[count: string]: string} = <{[count: string]: string}>(args[0]);
 
     if (!isStringMap(pluralMap)) {
       throw new InvalidPipeArgumentException(I18nPluralPipe, pluralMap);

modules/angular2/src/common/pipes/i18n_select_pipe.ts
@@ -36,8 +36,7 @@ import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
 @Pipe({name: 'i18nSelect', pure: true})
 @Injectable()
 export class I18nSelectPipe implements PipeTransform {
-  transform(value: string, args: any[] = null): string {
-    var mapping: {[key: string]: string} = <{[count: string]: string}>(args[0]);
+  transform(value: string, mapping: {[key: string]: string}): string {
     if (!isStringMap(mapping)) {
       throw new InvalidPipeArgumentException(I18nSelectPipe, mapping);
     }

commit d843aa44d94230de52ef7f2f5361932b991e6ef3

fix(compiler): support empty array and map literals.
This was broken after 152a117d5c27e56d1b32d69df2f69d34b94c0760

Fixes #8336

modules/angular2/src/compiler/identifiers.ts
@@ -7,6 +7,8 @@ import {
   interpolate,
   checkBinding,
   castByValue,
+  EMPTY_ARRAY,
+  EMPTY_MAP,
   pureProxy1,
   pureProxy2,
   pureProxy3,
@@ -72,6 +74,8 @@ var impDevModeEqual = devModeEqual;
 var impInterpolate = interpolate;
 var impCheckBinding = checkBinding;
 var impCastByValue = castByValue;
+var impEMPTY_ARRAY = EMPTY_ARRAY;
+var impEMPTY_MAP = EMPTY_MAP;
 
 export class Identifiers {
   static ViewUtils = new CompileIdentifierMetadata({
@@ -179,6 +183,11 @@ export class Identifiers {
       {name: 'interpolate', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: impInterpolate});
   static castByValue = new CompileIdentifierMetadata(
       {name: 'castByValue', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: impCastByValue});
+  static EMPTY_ARRAY = new CompileIdentifierMetadata(
+      {name: 'EMPTY_ARRAY', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: impEMPTY_ARRAY});
+  static EMPTY_MAP = new CompileIdentifierMetadata(
+      {name: 'EMPTY_MAP', moduleUrl: VIEW_UTILS_MODULE_URL, runtime: impEMPTY_MAP});
+
   static pureProxies = [
     null,
     new CompileIdentifierMetadata(

modules/angular2/src/compiler/view_compiler/compile_view.ts
@@ -24,6 +24,7 @@ import {
 } from './util';
 import {CompilerConfig} from '../config';
 import {CompileBinding} from './compile_binding';
+import {Identifiers} from '../identifiers';
 
 export class CompileView implements NameResolver {
   public viewType: ViewType;
@@ -155,6 +156,9 @@ export class CompileView implements NameResolver {
   }
 
   createLiteralArray(values: o.Expression[]): o.Expression {
+    if (values.length === 0) {
+      return o.importExpr(Identifiers.EMPTY_ARRAY);
+    }
     var proxyExpr = o.THIS_EXPR.prop(`_arr_${this.literalArrayCount++}`);
     var proxyParams: o.FnParam[] = [];
     var proxyReturnEntries: o.Expression[] = [];
@@ -169,6 +173,9 @@ export class CompileView implements NameResolver {
   }
 
   createLiteralMap(entries: Array<Array<string | o.Expression>>): o.Expression {
+    if (entries.length === 0) {
+      return o.importExpr(Identifiers.EMPTY_MAP);
+    }
     var proxyExpr = o.THIS_EXPR.prop(`_map_${this.literalMapCount++}`);
     var proxyParams: o.FnParam[] = [];
     var proxyReturnEntries: Array<Array<string | o.Expression>> = [];

modules/angular2/src/core/linker/view_utils.ts
@@ -156,6 +156,9 @@ export function castByValue<T>(input: any, value: T): T {
   return <T>input;
 }
 
+export const EMPTY_ARRAY = CONST_EXPR([]);
+export const EMPTY_MAP = CONST_EXPR({});
+
 export function pureProxy1<P0, R>(fn: (p0: P0) => R): (p0: P0) => R {
   var result: R;
   var v0;

modules/angular2/test/core/linker/change_detection_integration_spec.ts
@@ -371,6 +371,12 @@ export function main() {
            expect(renderLog.loggedValues).toEqual([[1, 2]]);
          }));
 
+      it('should support empty literal array', fakeAsync(() => {
+           var ctx = _bindSimpleValue('[]');
+           ctx.detectChanges(false);
+           expect(renderLog.loggedValues).toEqual([[]]);
+         }));
+
       it('should support literal array made of expressions', fakeAsync(() => {
            var ctx = _bindSimpleValue('[1, a]', TestData);
            ctx.componentInstance.a = 2;
@@ -393,6 +399,12 @@ export function main() {
            var ctx = _bindSimpleValue('{z: 1}');
            ctx.detectChanges(false);
            expect(renderLog.loggedValues[0]['z']).toEqual(1);
+         }));
+
+      it('should support empty literal map', fakeAsync(() => {
+           var ctx = _bindSimpleValue('{}');
+           ctx.detectChanges(false);
+           expect(renderLog.loggedValues).toEqual([{}]);
          }));
 
       it('should support literal maps made of expressions', fakeAsync(() => {