d930ad1816ca0b0ee8b3068a58c7558d21c8c00a
Miško Hevery <misko@hevery.com>
2016-05-02 17:11:21 +0000

    chore: router move-only

--- modules/@angular/router/src/directives/router_link.ts
+++ modules/@angular/router/src/directives/router_link.ts
@@ -1,83 +1,62 @@
-import {Directive} from '@angular/core';
-import {Location} from '@angular/common';
-import {isString} from '../../src/facade/lang';
-import {Router} from '../router';
-import {Instruction} from '../instruction';
+import {
+  ResolvedReflectiveProvider,
+  Directive,
+  DynamicComponentLoader,
+  ViewContainerRef,
+  Attribute,
+  ComponentRef,
+  ComponentFactory,
+  ReflectiveInjector,
+  OnInit,
+  HostListener,
+  HostBinding,
+  Input,
+  OnDestroy,
+  Optional
+} from '@angular/core';
+import {RouterOutletMap, Router} from '../router';
+import {RouteSegment, UrlSegment, Tree} from '../segments';
+import {isString, isPresent} from '@angular/facade/src/lang';
+import {ObservableWrapper} from '@angular/facade/src/async';
 
-/**
- * The RouterLink directive lets you link to specific parts of your app.
- *
- * Consider the following route configuration:
+@Directive({selector: '[routerLink]'})
+export class RouterLink implements OnDestroy {
+  @Input() target: string;
+  private _changes: any[] = [];
+  private _subscription: any;
 
- * ```
- * @RouteConfig([
- *   { path: '/user', component: UserCmp, as: 'User' }
- * ]);
- * class MyComp {}
- * ```
- *
- * When linking to this `User` route, you can write:
- *
- * ```
- * <a [routerLink]="['./User']">link to user component</a>
- * ```
- *
- * RouterLink expects the value to be an array of route names, followed by the params
- * for that level of routing. For instance `['/Team', {teamId: 1}, 'User', {userId: 2}]`
- * means that we want to generate a link for the `Team` route with params `{teamId: 1}`,
- * and with a child route `User` with params `{userId: 2}`.
- *
- * The first route name should be prepended with `/`, `./`, or `../`.
- * If the route begins with `/`, the router will look up the route from the root of the app.
- * If the route begins with `./`, the router will instead look in the current component's
- * children for the route. And if the route begins with `../`, the router will look at the
- * current component's parent.
- */
-@Directive({
-  selector: '[routerLink]',
-  inputs: ['routeParams: routerLink', 'target: target'],
-  host: {
-    '(click)': 'onClick()',
-    '[attr.href]': 'visibleHref',
-    '[class.router-link-active]': 'isRouteActive'
-  }
-})
-export class RouterLink {
-  private _routeParams: any[];
+  @HostBinding() private href: string;
+  @HostBinding('class.router-link-active') private isActive: boolean = false;
 
-  // the url displayed on the anchor element.
-  visibleHref: string;
-  target: string;
-
-  // the instruction passed to the router to navigate
-  private _navigationInstruction: Instruction;
-
-  constructor(private _router: Router, private _location: Location) {
-    // we need to update the link whenever a route changes to account for aux routes
-    this._router.subscribe((_) => this._updateLink());
+  constructor(@Optional() private _routeSegment: RouteSegment, private _router: Router) {
+    this._subscription =
+        ObservableWrapper.subscribe(_router.changes, (_) => { this._updateTargetUrlAndHref(); });
   }
 
-  // because auxiliary links take existing primary and auxiliary routes into account,
-  // we need to update the link whenever params or other routes change.
-  private _updateLink(): void {
-    this._navigationInstruction = this._router.generate(this._routeParams);
-    var navigationHref = this._navigationInstruction.toLinkUrl();
-    this.visibleHref = this._location.prepareExternalUrl(navigationHref);
+  ngOnDestroy() { ObservableWrapper.dispose(this._subscription); }
+
+  @Input()
+  set routerLink(data: any[]) {
+    this._changes = data;
+    this._updateTargetUrlAndHref();
   }
 
-  get isRouteActive(): boolean { return this._router.isRouteActive(this._navigationInstruction); }
-
-  set routeParams(changes: any[]) {
-    this._routeParams = changes;
-    this._updateLink();
-  }
-
+  @HostListener("click")
   onClick(): boolean {
-    // If no target, or if target is _self, prevent default browser behavior
     if (!isString(this.target) || this.target == '_self') {
-      this._router.navigateByInstruction(this._navigationInstruction);
+      this._router.navigate(this._changes, this._routeSegment);
       return false;
     }
     return true;
+  }
+
+  private _updateTargetUrlAndHref(): void {
+    let tree = this._router.createUrlTree(this._changes, this._routeSegment);
+    if (isPresent(tree)) {
+      this.href = this._router.serializeUrl(tree);
+      this.isActive = this._router.urlTree.contains(tree);
+    } else {
+      this.isActive = false;
+    }
   }
 }
--- modules/angular2/src/alt_router/router_providers.ts
+++
@@ -1,8 +0,0 @@
-import {ROUTER_PROVIDERS_COMMON} from './router_providers_common';
-import {BrowserPlatformLocation} from '@angular/platform-browser';
-import {PlatformLocation} from '@angular/common';
-
-export const ROUTER_PROVIDERS: any[] = /*@ts2dart_const*/[
-  ROUTER_PROVIDERS_COMMON,
-  /*@ts2dart_Provider*/ {provide: PlatformLocation, useClass: BrowserPlatformLocation},
-];
