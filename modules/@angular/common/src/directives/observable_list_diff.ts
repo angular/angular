52595f52f92df74ff62018cba4d5518fab2a5c97
Igor Minar <igor@angularjs.org>
2016-05-04 01:07:18 -0600

    chore: make workaround_empty_observable_list_diff @internal

--- modules/@angular/common/src/directives/observable_list_diff.ts
+++ modules/@angular/common/src/directives/observable_list_diff.ts
@@ -6,5 +6,6 @@ import {} from '../../src/facade/lang';
 /**
  * This module exists in Dart, but not in Typescript. This exported symbol
  * is only here to help Typescript think this is a module.
+ * @internal
  */
 export var workaround_empty_observable_list_diff: any;
