82a20cf2e7b1f9c6e8f480f0d6c9ab64b9fc979a
Tobias Bosch <tbosch1009@gmail.com>
2016-05-04 11:02:01 -0700

    fix(compiler): emit correct types for literal arrays and maps.

--- modules/angular2/src/compiler/view_compiler/compile_view.ts
+++ modules/angular2/src/compiler/view_compiler/compile_view.ts
@@ -154,7 +154,8 @@ export class CompileView implements NameResolver {
       proxyParams.push(new o.FnParam(paramName));
       proxyReturnEntries.push(o.variable(paramName));
     }
-    createPureProxy(o.fn(proxyParams, [new o.ReturnStatement(o.literalArr(proxyReturnEntries))]),
+    createPureProxy(o.fn(proxyParams, [new o.ReturnStatement(o.literalArr(proxyReturnEntries))],
+                         new o.ArrayType(o.DYNAMIC_TYPE)),
                     values.length, proxyExpr, this);
     return proxyExpr.callFn(values);
   }
@@ -173,7 +174,8 @@ export class CompileView implements NameResolver {
       proxyReturnEntries.push([entries[i][0], o.variable(paramName)]);
       values.push(<o.Expression>entries[i][1]);
     }
-    createPureProxy(o.fn(proxyParams, [new o.ReturnStatement(o.literalMap(proxyReturnEntries))]),
+    createPureProxy(o.fn(proxyParams, [new o.ReturnStatement(o.literalMap(proxyReturnEntries))],
+                         new o.MapType(o.DYNAMIC_TYPE)),
                     entries.length, proxyExpr, this);
     return proxyExpr.callFn(values);
   }
