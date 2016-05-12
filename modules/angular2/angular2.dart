2bbcc822a99a29d6375ae5026bd6f282e67008d1
Tobias Bosch <tbosch1009@gmail.com>
2016-05-12 09:09:12 -0700

    fix(compiler): don’t pull in the whole compiler when using `angular2/angular2.dart`.

--- modules/angular2/angular2.dart
+++ modules/angular2/angular2.dart
@@ -14,5 +14,12 @@ export 'package:angular2/src/core/application_tokens.dart'
 export 'package:angular2/src/platform/dom/dom_tokens.dart';
 export 'package:angular2/src/platform/dom/dom_adapter.dart';
 export 'package:angular2/src/platform/dom/events/event_manager.dart';
-export 'package:angular2/src/compiler/compiler.dart' show UrlResolver, DirectiveResolver, ViewResolver;
-
+// Attention: Don't use angular2/src/compiler/compiler.dart
+// for re exports as this will pull in the whole compiler,
+// even if only some parts are shown.
+// Background: Our transformer generates `initReflector` calls
+// for all referenced modules, which will keep all
+// types reachable from that module!
+export 'package:angular2/src/compiler/url_resolver.dart';
+export 'package:angular2/src/compiler/directive_resolver.dart';
+export 'package:angular2/src/compiler/view_resolver.dart';
