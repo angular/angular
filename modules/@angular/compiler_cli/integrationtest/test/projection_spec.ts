ff36b0384a33d415bf420543163d54b2fa5a45d5
Tobias Bosch <tbosch1009@gmail.com>
2016-05-16 12:06:21 -0700

    fix(compiler_cli): normalize used directives
    - e.g. needed for content projection.
    
    Closes #8677

---
+++ modules/@angular/compiler_cli/integrationtest/test/projection_spec.ts
@@ -0,0 +1,17 @@
+import {MainCompNgFactory} from '../src/projection.ngfactory';
+import {CompWithProjection} from '../src/projection';
+import {ReflectiveInjector, DebugElement, getDebugNode} from '@angular/core';
+import {browserPlatform, BROWSER_APP_STATIC_PROVIDERS, By} from '@angular/platform-browser';
+
+describe("content projection", () => {
+  it("should support basic content projection", () => {
+    const appInjector = ReflectiveInjector.resolveAndCreate(BROWSER_APP_STATIC_PROVIDERS,
+                                                            browserPlatform().injector);
+    var mainComp = MainCompNgFactory.create(appInjector);
+
+    var debugElement = <DebugElement>getDebugNode(mainComp.location.nativeElement);
+    var compWithProjection = debugElement.query(By.directive(CompWithProjection));
+    expect(compWithProjection.children.length).toBe(1);
+    expect(compWithProjection.children[0].attributes['greeting']).toEqual('Hello world!');
+  });
+});
