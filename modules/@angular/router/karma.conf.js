a8efdf0be6101f18916d2e3e4653fa27428a92b6
vsavkin <avix1000@gmail.com>
2016-05-24 13:23:05 -0700

    test: set up karma to support fakeAsync

--- modules/@angular/router/karma.conf.js
+++ modules/@angular/router/karma.conf.js
@@ -18,8 +18,10 @@ module.exports = function(config) {
 
       // Zone.js dependencies
       'node_modules/zone.js/dist/zone.js',
+      'node_modules/zone.js/dist/long-stack-trace-zone.js',
       'node_modules/zone.js/dist/jasmine-patch.js',
       'node_modules/zone.js/dist/async-test.js',
+      'node_modules/zone.js/dist/fake-async-test.js',
 
       // RxJs.
       { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
