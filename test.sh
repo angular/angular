acc3a2de83566854ea94d2932969f751db762e39
Igor Minar <igor@angularjs.org>
2016-05-24 14:42:07 -0700

    build: add comment for the tools watch option

--- test.sh
+++ test.sh
@@ -7,3 +7,4 @@ export NODE_PATH=$NODE_PATH:$(pwd)/dist/all:$(pwd)/dist/tools
 $(npm bin)/tsc -p tools
 node dist/tools/tsc-watch/ node watch
 # node dist/tools/tsc-watch/ browser watch
+# node dist/tools/tsc-watch/ tools watch
