10938ed6c1c1cba454614e3514fe16d32ff8107b
Paul Gschwendtner <paulgschwendtner@me.com>
2016-05-24 18:33:45 +0200

    chore: update typings dependency. (#506)
    * Typings recently released the 1.0 version, which contained some breaking changes.
       The new typings versions >= 1.0 are no longer supporting the `ambient` name. (It's now `global`)
    
    Official details / information can be found here
    - https://github.com/typings/typings#updating-from-0x-to-10

--- src/demo-app/typings.d.ts
+++ src/demo-app/typings.d.ts
@@ -1,3 +1,3 @@
-/// <reference path="../../typings/browser.d.ts" />
+/// <reference path="../../typings/index.d.ts" />
 
 declare var module: { id: string };
--- src/e2e-app/typings.d.ts
+++ src/e2e-app/typings.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/browser.d.ts" />
+/// <reference path="../../typings/index.d.ts" />
 
 declare var module: { id: string };
 
