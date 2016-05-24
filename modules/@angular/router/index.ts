4b2740f2708c8770217aac5960e138502d8afa3f
vsavkin <avix1000@gmail.com>
2016-05-24 14:44:47 -0700

    refactor: move index.ts into src

--- modules/@angular/router/index.ts
+++
@@ -1,9 +0,0 @@
-export { Router } from './src/router';
-export { UrlSerializer, DefaultUrlSerializer } from './src/url_serializer';
-export { RouterState, ActivatedRoute } from './src/router_state';
-export { RouterOutletMap } from './src/router_outlet_map';
-
-import { RouterOutlet } from './src/directives/router_outlet';
-import { RouterLink } from './src/directives/router_link';
-
-export const ROUTER_DIRECTIVES = [RouterOutlet, RouterLink];
