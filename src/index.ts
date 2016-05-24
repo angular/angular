611c7d576a4a831e5e0c525dc568d611e228acc7
vsavkin <avix1000@gmail.com>
2016-05-24 14:44:47 -0700

    refactor: move index.ts into src

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { Router } from './router';
+export { UrlSerializer, DefaultUrlSerializer } from './url_serializer';
+export { RouterState, ActivatedRoute } from './router_state';
+export { RouterOutletMap } from './router_outlet_map';
+
+import { RouterOutlet } from './directives/router_outlet';
+import { RouterLink } from './directives/router_link';
+
+export const ROUTER_DIRECTIVES = [RouterOutlet, RouterLink];
