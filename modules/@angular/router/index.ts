5b371736b2f71cc1c3591850686e83c339c7b822
vsavkin <avix1000@gmail.com>
2016-05-24 14:33:34 -0700

    feat: add RouterLink

--- modules/@angular/router/index.ts
+++ modules/@angular/router/index.ts
@@ -4,4 +4,6 @@ export { RouterState, ActivatedRoute } from './src/router_state';
 export { RouterOutletMap } from './src/router_outlet_map';
 
 import { RouterOutlet } from './src/directives/router_outlet';
-export const ROUTER_DIRECTIVES = [RouterOutlet];
+import { RouterLink } from './src/directives/router_link';
+
+export const ROUTER_DIRECTIVES = [RouterOutlet, RouterLink];
