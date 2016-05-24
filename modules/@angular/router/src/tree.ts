c83d5304d065d7abddee20666913dfc83427de76
vsavkin <avix1000@gmail.com>
2016-05-24 13:22:49 -0700

    cleanup: fix type errors when compiling with strictNullChecks enabled

--- modules/@angular/router/src/tree.ts
+++ modules/@angular/router/src/tree.ts
@@ -12,53 +12,53 @@ export class Tree<T> {
   }
 
   children(t: T): T[] {
-    const n = _findNode(t, this._root);
+    const n = findNode(t, this._root);
     return n ? n.children.map(t => t.value) : [];
   }
 
   firstChild(t: T): T | null {
-    const n = _findNode(t, this._root);
+    const n = findNode(t, this._root);
     return n && n.children.length > 0 ? n.children[0].value : null;
   }
 
-  pathFromRoot(t: T): T[] { return _findPath(t, this._root, []).map(s => s.value); }
+  pathFromRoot(t: T): T[] { return findPath(t, this._root, []).map(s => s.value); }
 
-  contains(tree: Tree<T>): boolean { return _contains(this._root, tree._root); }
+  contains(tree: Tree<T>): boolean { return contains(this._root, tree._root); }
 }
 
 export function rootNode<T>(tree: Tree<T>): TreeNode<T> {
   return tree._root;
 }
 
-function _findNode<T>(expected: T, c: TreeNode<T>): TreeNode<T> | null {
+function findNode<T>(expected: T, c: TreeNode<T>): TreeNode<T> | null {
   if (expected === c.value) return c;
   for (let cc of c.children) {
-    const r = _findNode(expected, cc);
+    const r = findNode(expected, cc);
     if (r) return r;
   }
   return null;
 }
 
-function _findPath<T>(expected: T, c: TreeNode<T>, collected: TreeNode<T>[]): TreeNode<T>[] {
+function findPath<T>(expected: T, c: TreeNode<T>, collected: TreeNode<T>[]): TreeNode<T>[] {
   collected.push(c);
   if (expected === c.value) return collected;
 
   for (let cc of c.children) {
     const cloned = collected.slice(0);
-    const r = _findPath(expected, cc, cloned);
+    const r = findPath(expected, cc, cloned);
     if (r) return r;
   }
 
   return [];
 }
 
-function _contains<T>(tree: TreeNode<T>, subtree: TreeNode<T>): boolean {
+function contains<T>(tree: TreeNode<T>, subtree: TreeNode<T>): boolean {
   if (tree.value !== subtree.value) return false;
 
   for (let subtreeNode of subtree.children) {
     const s = tree.children.filter(child => child.value === subtreeNode.value);
     if (s.length === 0) return false;
-    if (!_contains(s[0], subtreeNode)) return false;
+    if (!contains(s[0], subtreeNode)) return false;
   }
 
   return true;
