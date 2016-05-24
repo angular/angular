83e99b8281d728d00432dc3163e132380d0b90d9
Victor Berchet <victor@suumit.com>
2016-05-24 16:12:34 -0700

    fix(platform-browser): fix rollup config

--- modules/@angular/platform-browser/rollup.config.js
+++ modules/@angular/platform-browser/rollup.config.js
@@ -7,6 +7,7 @@ export default {
   globals: {
     '@angular/core': 'ng.core',
     '@angular/common': 'ng.common',
+    '@angular/compiler': 'ng.compiler',
     'rxjs/Subject': 'Rx',
     'rxjs/observable/PromiseObservable': 'Rx', // this is wrong, but this stuff has changed in rxjs b.6 so we need to fix it when we update.
     'rxjs/operator/toPromise': 'Rx.Observable.prototype',
