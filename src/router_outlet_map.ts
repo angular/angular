7167420794884d0e052871cfee467d46879eadc8
vsavkin <avix1000@gmail.com>
2016-05-24 13:21:55 -0700

    feat: implement RouterOutletMap

---
+++ src/router_outlet_map.ts
@@ -0,0 +1,10 @@
+import { RouterOutlet } from './directives/router_outlet';
+
+/**
+ * @internal
+ */
+export class RouterOutletMap {
+  /** @internal */
+  _outlets: {[name: string]: RouterOutlet} = {};
+  registerOutlet(name: string, outlet: RouterOutlet): void { this._outlets[name] = outlet; }
+}
