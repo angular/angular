b9347eb01ca91211d8a1be266e2b60a89d75fe51
Igor Minar <igor@angularjs.org>
2016-05-25 15:37:40 -0700

    build: remove dependency on tsd and use @types/* instead

--- tools/types.d.ts
+++ tools/types.d.ts
@@ -1,5 +1,4 @@
 // This file contains all ambient imports needed to compile the tools source code
 
-/// <reference path="../typings/jasmine/jasmine.d.ts" />
-/// <reference path="../typings/node/node.d.ts" />
-/// <reference path="../typings/fs-extra/fs-extra.d.ts" />
+/// <reference path="../node_modules/@types/jasmine/index.d.ts" />
+/// <reference path="../node_modules/@types/node/index.d.ts" />
