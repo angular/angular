632e90cb737e8cdb367a617ae7b454f7361eaa0c
Sendil Kumar N <sendilkumarn@live.com>
2016-05-27 00:43:14 +0800

    protractor-accessibility-plugin for audit on CI (#521)

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -10,6 +10,12 @@ require('ts-node').register({
 const E2E_BASE_URL = process.env['E2E_BASE_URL'] || 'http://localhost:4200';
 const config = {
   useAllAngular2AppRoots: true,
+  plugins: [{
+    chromeA11YDevTools: {
+      treatWarningsAsFailures: true
+    },
+    package: 'protractor-accessibility-plugin'
+  }],
   specs: [ path.join(__dirname, '../e2e/**/*.e2e.ts') ],
   baseUrl: E2E_BASE_URL
 };
