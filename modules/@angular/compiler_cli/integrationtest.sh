9cdfd36458e183a6e2915490fc7741ef6299c37b
Tobias Bosch <tbosch1009@gmail.com>
2016-05-26 13:16:53 -0700

    chore: enable the compiler_cli tests again.

--- modules/@angular/compiler_cli/integrationtest.sh
+++ modules/@angular/compiler_cli/integrationtest.sh
@@ -5,7 +5,7 @@ set -e -o pipefail
 cd $(dirname $0)
 cd $(pwd)/../../..
 export NODE_PATH=$NODE_PATH:$(pwd)/dist/all:$(pwd)/dist/tools
-readonly TESTDIR="modules/@angular/compiler_cli/integrationtest"
-rm $TESTDIR/src/*.{ngfactory,css.shim}.ts
-node dist/all/@angular/compiler_cli/src/main -p $TESTDIR
+readonly TESTDIR="@angular/compiler_cli/integrationtest"
+rm -f modules/$TESTDIR/src/*.{ngfactory,css.shim}.ts
+node dist/all/@angular/compiler_cli/src/main -p modules/$TESTDIR
 node dist/tools/cjs-jasmine -- $TESTDIR/**/*_spec.js
