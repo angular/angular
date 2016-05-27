8aef86f4a0279402f6426f1c339bf2301359dc20
vsavkin <avix1000@gmail.com>
2016-05-27 13:49:42 -0700

    feat(router): export all public api tokens via index

--- modules/@angular/router/src/index.ts
+++ modules/@angular/router/src/index.ts
@@ -1,7 +1,10 @@
 export { Router } from './router';
 export { UrlSerializer, DefaultUrlSerializer } from './url_serializer';
 export { RouterState, ActivatedRoute } from './router_state';
+export { UrlTree, UrlSegment} from './url_tree';
 export { RouterOutletMap } from './router_outlet_map';
+export { RouterConfig, Route } from './config';
+export { Params, PRIMARY_OUTLET } from './shared';
 
 import { RouterOutlet } from './directives/router_outlet';
 import { RouterLink } from './directives/router_link';
