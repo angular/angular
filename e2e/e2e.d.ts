a68597ef1cd90c6261d5abf086f719b58fe81239
Hans <hans@hansl.ca>
2016-06-06 16:31:10 -0700

    fix: remove duplicated typing. TS stdlib provides enough for e2e tests (#637)

--- e2e/e2e.d.ts
+++ e2e/e2e.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="typings/node/index.d.ts" />
 /// <reference path="typings/es6-shim/es6-shim.d.ts" />
 /// <reference path="typings/jasmine/jasmine.d.ts" />
 /// <reference path="typings/selenium-webdriver/index.d.ts" />
