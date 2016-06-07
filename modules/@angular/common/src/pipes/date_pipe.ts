668072e8d558f1c86dd8552d095ec36e4429840e
Idir Ouhab Meskine <idirouhab@users.noreply.github.com>
2016-06-07 23:06:58 +0200

    Update date doc reference in date_pipe.ts
    docs(Date pipe format): set a newest documentation about date format
    
    Change the link of the Dart date format because the oldest version is not longert available:
    *Before: https://www.dartdocs.org/documentation/intl/0.13.0/intl/DateFormat-class.html
    *After: https://api.dartlang.org/apidocs/channels/stable/dartdoc-viewer/intl/intl.DateFormat.

--- modules/@angular/common/src/pipes/date_pipe.ts
+++ modules/@angular/common/src/pipes/date_pipe.ts
@@ -53,7 +53,7 @@ var defaultLocale: string = 'en-US';
  * punctuations, ...) and details of the formatting will be dependent on the locale.
  * On the other hand in Dart version, you can also include quoted text as well as some extra
  * date/time components such as quarter. For more information see:
- * https://api.dartlang.org/apidocs/channels/stable/dartdoc-viewer/intl/intl.DateFormat.
+ * https://www.dartdocs.org/documentation/intl/0.13.0/intl/DateFormat-class.html
  *
  * `format` can also be one of the following predefined formats:
  *
