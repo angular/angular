82fe65ca3cadcd124a842e948150bcfd3f375016
Tobias Bosch <tbosch1009@gmail.com>
2016-06-07 09:24:57 -0700

    Revert "fix(compiler): add ability to parse : in * directives"
    This reverts commit 53628e19ac902a73b47cfe617ca3ed3bd3d7cc9e.
    as it breaks pipe arguments in `*ngFor`, ...
    
    See #9062

--- modules/@angular/compiler/test/template_parser_spec.ts
+++ modules/@angular/compiler/test/template_parser_spec.ts
@@ -508,7 +508,7 @@ export function main() {
         it('should support optional directive properties', () => {
           var dirA = CompileDirectiveMetadata.create({
             selector: 'div',
-            type: new CompileTypeMetadata({moduleUrl: someModuleUrl, name: 'Dir'}),
+            type: new CompileTypeMetadata({moduleUrl: someModuleUrl, name: 'DirA'}),
             inputs: ['a']
           });
           expect(humanizeTplAst(parse('<div></div>', [dirA])))
@@ -1064,18 +1064,6 @@ Reference "#a" is defined several times ("<div #a></div><div [ERROR ->]#a></div>
                 [EmbeddedTemplateAst],
                 [DirectiveAst, ngIf],
                 [BoundDirectivePropertyAst, 'ngIf', 'null'],
-                [ElementAst, 'div']
-              ]);
-        });
-
-        it('should work with *... and :', () => {
-          var dirA = CompileDirectiveMetadata.create(
-              {selector: '[a]', type: new CompileTypeMetadata({name: 'DirA'}), inputs: ['a', 'b']});
-          expect(humanizeTplAst(parse('<div *a="b: foo">', [dirA])))
-              .toEqual([
-                [EmbeddedTemplateAst],
-                [DirectiveAst, dirA],
-                [BoundDirectivePropertyAst, 'a', 'b'],
                 [ElementAst, 'div']
               ]);
         });
