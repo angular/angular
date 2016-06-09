777eb2f1599ae713eea19c8a74388eb15950a15a
vsavkin <avix1000@gmail.com>
2016-06-09 14:31:49 -0700

    feat(router): emit an event when routes are recognized

--- modules/@angular/router/src/index.ts
+++ modules/@angular/router/src/index.ts
@@ -3,7 +3,7 @@ import {RouterOutlet} from './directives/router_outlet';
 
 export {Route, RouterConfig} from './config';
 export {CanActivate, CanDeactivate} from './interfaces';
-export {Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from './router';
+export {Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, RoutesRecognized, Router} from './router';
 export {RouterOutletMap} from './router_outlet_map';
 export {provideRouter} from './router_providers';
 export {ActivatedRoute, ActivatedRouteSnapshot, RouterState, RouterStateSnapshot} from './router_state';
