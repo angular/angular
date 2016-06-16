3d606137fad3f8b15b225ec2e1947f32c1ec821e
vsavkin <avix1000@gmail.com>
2016-06-16 14:36:51 -0700

    feat(router): add route config validation

--- src/config.ts
+++ src/config.ts
@@ -12,3 +12,24 @@ export interface Route {
   redirectTo?: string;
   children?: Route[];
 }
+
+export function validateConfig(config: RouterConfig): void {
+  config.forEach(validateNode);
+}
+
+function validateNode(route: Route): void {
+  if (!!route.redirectTo && !!route.children) {
+    throw new Error(
+        `Invalid configuration of route '${route.path}': redirectTo and children cannot be used together`);
+  }
+  if (!!route.redirectTo && !!route.component) {
+    throw new Error(
+        `Invalid configuration of route '${route.path}': redirectTo and component cannot be used together`);
+  }
+  if (route.path === undefined) {
+    throw new Error(`Invalid route configuration: routes must have path specified`);
+  }
+  if (route.path.startsWith('/')) {
+    throw new Error(`Invalid route configuration of route '/a': path cannot start with a slash`);
+  }
+}
