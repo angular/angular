249a6bdd98ad285aa94cf302eaf7503c11f206a3
Jason Choi <jasonchoi@google.com>
2016-06-22 11:28:40 -0700

    test: upgrade ts-api-guardian to v0.1.2

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,8 @@ const gulp = require('gulp');
 const path = require('path');
 const os = require('os');
 
-const srcsToFmt = ['tools/**/*.ts', 'modules/@angular/**/*.ts'];
+const srcsToFmt =
+    ['tools/**/*.ts', 'modules/@angular/**/*.ts', '!tools/public_api_guard/**/*.d.ts'];
 
 gulp.task('format:enforce', () => {
   const format = require('gulp-clang-format');
@@ -24,6 +25,59 @@ gulp.task('format', () => {
   const clangFormat = require('clang-format');
   return gulp.src(srcsToFmt, { base: '.' }).pipe(
     format.format('file', clangFormat)).pipe(gulp.dest('.'));
+});
+
+const entrypoints = [
+  'dist/packages-dist/core/index.d.ts',
+  'dist/packages-dist/core/testing.d.ts',
+  'dist/packages-dist/common/index.d.ts',
+  'dist/packages-dist/common/testing.d.ts',
+  'dist/packages-dist/compiler/index.d.ts',
+  'dist/packages-dist/compiler/testing.d.ts',
+  'dist/packages-dist/upgrade/index.d.ts',
+  'dist/packages-dist/platform-browser/index.d.ts',
+  'dist/packages-dist/platform-browser/testing.d.ts',
+  'dist/packages-dist/platform-browser/testing_e2e.d.ts',
+  'dist/packages-dist/platform-browser-dynamic/index.d.ts',
+  'dist/packages-dist/platform-browser-dynamic/testing.d.ts',
+  'dist/packages-dist/platform-server/index.d.ts',
+  'dist/packages-dist/platform-server/testing.d.ts',
+  'dist/packages-dist/http/index.d.ts',
+  'dist/packages-dist/http/testing.d.ts'
+];
+const publicApiDir = 'tools/public_api_guard';
+const publicApiArgs = [
+  '--rootDir', 'dist/packages-dist',
+  '--stripExportPattern', '^__',
+  '--allowModuleIdentifiers', 'jasmine',
+  '--allowModuleIdentifiers', 'protractor',
+  '--allowModuleIdentifiers', 'angular'
+].concat(entrypoints);
+
+// Note that these two commands work on built d.ts files instead of the source
+gulp.task('public-api:enforce', (done) => {
+  const child_process = require('child_process');
+  child_process
+      .spawn(
+          `${__dirname}/node_modules/.bin/ts-api-guardian`,
+          ['--verifyDir', publicApiDir].concat(publicApiArgs), {stdio: 'inherit'})
+      .on('close', (errorCode) => {
+        if (errorCode !== 0) {
+          done(new Error(
+              'Public API differs from golden file. Please run `gulp public-api:update`.'));
+        } else {
+          done();
+        }
+      });
+});
+
+gulp.task('public-api:update', (done) => {
+  const child_process = require('child_process');
+  child_process
+      .spawn(
+          `${__dirname}/node_modules/.bin/ts-api-guardian`,
+          ['--outDir', publicApiDir].concat(publicApiArgs), {stdio: 'inherit'})
+      .on('close', (errorCode) => done(errorCode));
 });
 
 gulp.task('lint', ['format:enforce', 'tools:build'], () => {
