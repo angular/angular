894747c34cf9bbef2b891d9cdfdd0b699a6a95fb
Julie Ralph <ju.ralph@gmail.com>
2016-06-23 16:14:31 -0700

    fix(platform-browser/testing-e2e): clean up unused exports from e2e testing helpers (#9387)

--- tools/public_api_guard/platform-browser/testing_e2e.d.ts
+++ tools/public_api_guard/platform-browser/testing_e2e.d.ts
@@ -1,7 +1,1 @@
-export declare var $: cssSelectorHelper;
-
-export declare var browser: protractor.IBrowser;
-
-export declare function clickAll(buttonSelectors: any): void;
-
 export declare function verifyNoBrowserErrors(): void;
