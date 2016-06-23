0ad1215a92e16aa36f3969e6116f68f4aa854cba
Jeff Cross <middlefloor@gmail.com>
2016-06-23 16:23:15 -0700

    build(changelog): remove old changelog script and add gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -113,6 +113,23 @@ gulp.task('serve', () => {
 });
 
 
+gulp.task('changelog', () => {
+  const conventionalChangelog = require('gulp-conventional-changelog');
+
+  return gulp.src('CHANGELOG.md')
+    .pipe(conventionalChangelog({
+      preset: 'angular',
+      releaseCount: 1
+    }, {
+      // Conventional Changelog Context
+      // We have to manually set version number so it doesn't get prefixed with `v`
+      // See https://github.com/conventional-changelog/conventional-changelog-core/issues/10
+      currentTag: require('./package.json').version
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+
 function tsc(projectPath, done) {
   let child_process = require('child_process');
 
