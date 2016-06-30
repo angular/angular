2d73583253b79774b6e1c0b45cbfcdb06efc7dc4
Tobias Bosch <tbosch1009@gmail.com>
2016-06-30 13:34:15 -0700

    chore(compiler): fix cyclic dependency

---
+++ modules/@angular/compiler/src/output/value_util.ts
@@ -0,0 +1,33 @@
+import {CompileIdentifierMetadata} from '../compile_metadata';
+import {StringMapWrapper} from '../facade/collection';
+import {BaseException} from '../facade/exceptions';
+import {ValueTransformer, visitValue} from '../util';
+
+import * as o from './output_ast';
+
+export function convertValueToOutputAst(value: any, type: o.Type = null): o.Expression {
+  return visitValue(value, new _ValueOutputAstTransformer(), type);
+}
+
+class _ValueOutputAstTransformer implements ValueTransformer {
+  visitArray(arr: any[], type: o.Type): o.Expression {
+    return o.literalArr(arr.map(value => visitValue(value, this, null)), type);
+  }
+  visitStringMap(map: {[key: string]: any}, type: o.MapType): o.Expression {
+    var entries: Array<string|o.Expression>[] = [];
+    StringMapWrapper.forEach(map, (value: any, key: string) => {
+      entries.push([key, visitValue(value, this, null)]);
+    });
+    return o.literalMap(entries, type);
+  }
+  visitPrimitive(value: any, type: o.Type): o.Expression { return o.literal(value, type); }
+  visitOther(value: any, type: o.Type): o.Expression {
+    if (value instanceof CompileIdentifierMetadata) {
+      return o.importExpr(value);
+    } else if (value instanceof o.Expression) {
+      return value;
+    } else {
+      throw new BaseException(`Illegal state: Don't now how to compile value ${value}`);
+    }
+  }
+}
