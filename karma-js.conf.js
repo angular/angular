de89bb783d100f88105d5f16ff381c6c5bb8ad00
Victor Berchet <victor@suumit.com>
2016-07-13 11:51:07 -0700

    test: execute router tests on .`/test.sh browser`

--- karma-js.conf.js
+++ karma-js.conf.js
@@ -38,6 +38,7 @@ module.exports = function(config) {
     exclude: [
       'dist/all/@angular/**/e2e_test/**',
       'dist/all/@angular/examples/**',
+      'dist/all/@angular/router/**',
       'dist/all/@angular/compiler-cli/**',
       'dist/all/angular1_router.js',
       'dist/all/@angular/platform-browser/testing/e2e_util.js'
