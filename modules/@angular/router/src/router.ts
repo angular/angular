c586656d433bcc446fb733b99da6951dab3d4e25
vsavkin <avix1000@gmail.com>
2016-08-05 08:24:40 -0700

    fix(router): fix type definition

--- modules/@angular/router/src/router.ts
+++ modules/@angular/router/src/router.ts
@@ -115,7 +115,8 @@ export class RoutesRecognized {
 /**
  * @stable
  */
-export type Event = NavigationStart | NavigationEnd | NavigationCancel | NavigationError;
+export type Event =
+    NavigationStart | NavigationEnd | NavigationCancel | NavigationError | RoutesRecognized;
 
 /**
  * The `Router` is responsible for mapping URLs to components.
