cc6749c15877f98ffb5be18806c2126b29b71e8b
Victor Savkin <vic.savkin@gmail.com>
2016-08-11 22:59:53 -0700

    fix(router): lazy loading keeps refetching modules (#10707)

--- modules/@angular/router/src/apply_redirects.ts
+++ modules/@angular/router/src/apply_redirects.ts
@@ -244,10 +244,14 @@ class ApplyRedirects {
     } else if (route.loadChildren) {
       return runGuards(injector, route).mergeMap(shouldLoad => {
         if (shouldLoad) {
-          return this.configLoader.load(injector, route.loadChildren).map(r => {
-            (<any>route)._loadedConfig = r;
-            return r;
-          });
+          if ((<any>route)._loadedConfig) {
+            return of ((<any>route)._loadedConfig);
+          } else {
+            return this.configLoader.load(injector, route.loadChildren).map(r => {
+              (<any>route)._loadedConfig = r;
+              return r;
+            });
+          }
         } else {
           return canLoadFails(route);
         }
