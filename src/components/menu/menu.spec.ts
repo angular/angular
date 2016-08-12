ff18edd3364044eb5d130fd3bf22e8bd5b235bf8
Jeremy Elbourn <jelbourn@gmail.com>
2016-08-12 11:25:37 -0700

    chore(menu): remove TestComponentBuilder (#1007)

--- src/components/menu/menu.spec.ts
+++ src/components/menu/menu.spec.ts
@@ -1,39 +1,24 @@
-import {
-  inject,
-  TestComponentBuilder,
-  TestBed,
-  async,
-} from '@angular/core/testing';
+import {TestBed, async} from '@angular/core/testing';
 import {Component} from '@angular/core';
 import {MdMenuModule} from './menu';
 
+
 describe('MdMenu', () => {
-  let builder: TestComponentBuilder;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [MdMenuModule],
-      declarations: [TestList],
+      declarations: [TestMenu],
     });
 
     TestBed.compileComponents();
   }));
 
-  beforeEach(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
-    builder = tcb;
-  }));
-
-  it('should add and remove focus class on focus/blur', async(() => {
-    var template = ``;
-    builder.overrideTemplate(TestList, template).createAsync(TestList).then(fixture => {
-       expect(true).toBe(true);
-    });
-  }));
-
+  it('should add and remove focus class on focus/blur', () => {
+    let fixture = TestBed.createComponent(TestMenu);
+    expect(fixture).toBeTruthy();
+  });
 });
 
-@Component({
-  selector: 'test-menu',
-  template: ``
-})
-class TestList {}
+@Component({template: ``})
+class TestMenu {}
