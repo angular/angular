708662d06357a3da4ff99380931c207a3e34055a
Jeremy Elbourn <jelbourn@gmail.com>
2016-08-12 12:57:29 -0700

    chore(toolbar): remove TestComponentBuilder (#1015)
    * chore(toolbar): remove TestComponentBuilder
    
    * minor cleanup

--- src/components/toolbar/toolbar.spec.ts
+++ src/components/toolbar/toolbar.spec.ts
@@ -1,15 +1,10 @@
 import {Component} from '@angular/core';
-import {
-  inject,
-  TestComponentBuilder,
-  TestBed,
-  async,
-} from '@angular/core/testing';
+import {TestBed, async} from '@angular/core/testing';
 import {By} from '@angular/platform-browser';
 import {MdToolbarModule} from './toolbar';
 
+
 describe('MdToolbar', () => {
-  let builder: TestComponentBuilder;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,45 +15,32 @@ describe('MdToolbar', () => {
     TestBed.compileComponents();
   }));
 
-  beforeEach(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
-    builder = tcb;
-  }));
+  it('should apply class based on color attribute', () => {
+    let fixture = TestBed.createComponent(TestApp);
+    let testComponent = fixture.debugElement.componentInstance;
+    let toolbarElement = fixture.debugElement.query(By.css('md-toolbar')).nativeElement;
 
-  it('should apply class based on color attribute', (done: () => void) => {
-    return builder.createAsync(TestApp).then((fixture) => {
-      let testComponent = fixture.debugElement.componentInstance;
-      let toolbarDebugElement = fixture.debugElement.query(By.css('md-toolbar'));
+    testComponent.toolbarColor = 'primary';
+    fixture.detectChanges();
 
-      testComponent.toolbarColor = 'primary';
-      fixture.detectChanges();
+    expect(toolbarElement.classList.contains('md-primary')).toBe(true);
 
-      expect(toolbarDebugElement.nativeElement.classList.contains('md-primary')).toBe(true);
+    testComponent.toolbarColor = 'accent';
+    fixture.detectChanges();
 
-      testComponent.toolbarColor = 'accent';
-      fixture.detectChanges();
+    expect(toolbarElement.classList.contains('md-primary')).toBe(false);
+    expect(toolbarElement.classList.contains('md-accent')).toBe(true);
 
-      expect(toolbarDebugElement.nativeElement.classList.contains('md-primary')).toBe(false);
-      expect(toolbarDebugElement.nativeElement.classList.contains('md-accent')).toBe(true);
+    testComponent.toolbarColor = 'warn';
+    fixture.detectChanges();
 
-      testComponent.toolbarColor = 'warn';
-      fixture.detectChanges();
-
-      expect(toolbarDebugElement.nativeElement.classList.contains('md-accent')).toBe(false);
-      expect(toolbarDebugElement.nativeElement.classList.contains('md-warn')).toBe(true);
-
-      done();
-    });
+    expect(toolbarElement.classList.contains('md-accent')).toBe(false);
+    expect(toolbarElement.classList.contains('md-warn')).toBe(true);
   });
-
 });
 
-@Component({
-  selector: 'test-app',
-  template: `
-    <md-toolbar [color]="toolbarColor">
-      <span>Test Toolbar</span>
-    </md-toolbar>`
-})
+
+@Component({template: `<md-toolbar [color]="toolbarColor">Test Toolbar</md-toolbar>`})
 class TestApp {
   toolbarColor: string;
 }
