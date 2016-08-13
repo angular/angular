63e76399ae7ea0c5c34da9072d3f8f12f73fdd2f
laco0416 <laco0416+github@gmail.com>
2016-08-13 23:07:11 +0900

    docs(NgTemplateOutlet): improve documentation

--- modules/@angular/common/src/directives/ng_template_outlet.ts
+++ modules/@angular/common/src/directives/ng_template_outlet.ts
@@ -19,9 +19,21 @@ import {Directive, EmbeddedViewRef, Input, OnChanges, TemplateRef, ViewContainer
  * ### Syntax
  *
  * ```
- * <template [ngTemplateOutlet]="templateRefExpression"
- *           [ngOutletContext]="objectExpression">
+ * <ng-container [ngTemplateOutlet]="templateRefExpression"
+ *               [ngOutletContext]="objectExpression">
+ * </ng-container>
+ * ```
+ *
+ * ### Example
+ *
+ * ```
+ * <template #t let-foo let-bar="explicit">
+ *   <p>{{foo}}{{bar}}</p>
  * </template>
+ *
+ * <ng-container [ngTemplateOutlet]="t"
+ *               [ngOutletContext]="{'$implicit': 'Foo', explicit: 'Bar'}">
+ * </ng-container>
  * ```
  *
  * @experimental
