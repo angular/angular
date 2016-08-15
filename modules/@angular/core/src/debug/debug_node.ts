a97b62efd185b29933c0334faf2111d9257546ba
Ward Bell <wardbell@hotmail.com>
2016-08-15 13:45:45 -0700

    fix(core): optional eventObj for DebugNode.triggerEventHandler
    Can write `btn.triggerEventHandler('click')` instead of  `btn.triggerEventHandler('click', null)`

--- modules/@angular/core/src/debug/debug_node.ts
+++ modules/@angular/core/src/debug/debug_node.ts
@@ -131,7 +131,7 @@ export class DebugElement extends DebugNode {
     return children;
   }
 
-  triggerEventHandler(eventName: string, eventObj: any) {
+  triggerEventHandler(eventName: string, eventObj?: any) {
     this.listeners.forEach((listener) => {
       if (listener.name == eventName) {
         listener.callback(eventObj);
