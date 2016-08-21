42ca53c0c2a5e77993d5a79b1629e46a0cec7b96
AlmogShaul <shaul.almog@gmail.com>
2016-08-21 18:16:46 +0300

    copyrights and removed duplicate imports

--- modules/@angular/common/src/directives/ng_focus.ts
+++ modules/@angular/common/src/directives/ng_focus.ts
@@ -1,6 +1,12 @@
-import {Directive, ElementRef, Input} from '@angular/core';
-import {Inject} from '@angular/core';
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 
+import {Inject,Directive, ElementRef, Input} from '@angular/core';
 
 
 /**
