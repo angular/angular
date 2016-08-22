7862dc7062437cbe10dbbca269434f66db8801a1
AlmogShaul <shaul.almog@gmail.com>
2016-08-22 13:45:56 +0300

    add function main

--- modules/@angular/common/test/directives/ng_focus_spec.ts
+++ modules/@angular/common/test/directives/ng_focus_spec.ts
@@ -6,41 +6,40 @@ import {describe, expect, iit, inject, it} from '@angular/core/testing/testing_i
 
 
 export function main() {
-
   describe('Focus Directive', () => {
 
     it('Should set focus when the directive get truthy boolean',
-      async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
-        var template = '<div><input class="test-input" [ngFocus]="inFocus" ></div>';
-        tcb.overrideTemplate(TestComponent, template).createAsync(TestComponent).then((fixture) => {
-          let el = fixture.nativeElement.querySelector('.test-input');
-          let spy = spyOn(el, 'focus').and.callThrough();
-          fixture.componentInstance.inFocus = true;
-          fixture.detectChanges();
-          fixture.whenStable().then(() => {
-            expect(spy).toHaveBeenCalled();
-          });
+       async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+         var template = '<div><input class="test-input" [ngFocus]="inFocus" ></div>';
+         tcb.overrideTemplate(TestComponent, template)
+             .createAsync(TestComponent)
+             .then((fixture) => {
+               let el = fixture.nativeElement.querySelector('.test-input');
+               let spy = spyOn(el, 'focus').and.callThrough();
+               fixture.componentInstance.inFocus = true;
+               fixture.detectChanges();
+               fixture.whenStable().then(() => { expect(spy).toHaveBeenCalled(); });
 
 
-        });
-      })));
+             });
+       })));
 
     it('Should get out of focus when the directive get falsey boolean',
-      async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
-        var template = '<div><input class="test-input" [ngFocus]="inFocus" ></div>';
-        tcb.overrideTemplate(TestComponent, template).createAsync(TestComponent).then((fixture) => {
-          let el = fixture.nativeElement.querySelector('.test-input');
-          let spy = spyOn(el, 'blur').and.callThrough();
-          fixture.componentInstance.inFocus = true;
-          fixture.detectChanges();
-          fixture.componentInstance.inFocus = false;
-          fixture.detectChanges();
-          fixture.whenStable().then(() => {
-            expect(spy).toHaveBeenCalled();
-          });
+       async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+         var template = '<div><input class="test-input" [ngFocus]="inFocus" ></div>';
+         tcb.overrideTemplate(TestComponent, template)
+             .createAsync(TestComponent)
+             .then((fixture) => {
+               let el = fixture.nativeElement.querySelector('.test-input');
+               let spy = spyOn(el, 'blur').and.callThrough();
+               fixture.componentInstance.inFocus = true;
+               fixture.detectChanges();
+               fixture.componentInstance.inFocus = false;
+               fixture.detectChanges();
+               fixture.whenStable().then(() => { expect(spy).toHaveBeenCalled(); });
 
-        });
-      })));
+             });
+       })));
 
 
   });
