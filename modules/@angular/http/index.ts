4ec80c157d9bbaa288a47447d0e18698a62494e8
Igor Minar <igor@angularjs.org>
2016-08-23 10:54:00 -0700

    fix(http): restructure exports so that we don't leak private factory functions
    Ref #10615

--- modules/@angular/http/index.ts
+++ modules/@angular/http/index.ts
@@ -6,4 +6,16 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-export * from './http';
+export {HttpModule, JsonpModule} from './http';
+export {BrowserXhr} from './src/backends/browser_xhr';
+export {JSONPBackend, JSONPConnection} from './src/backends/jsonp_backend';
+export {CookieXSRFStrategy, XHRBackend, XHRConnection} from './src/backends/xhr_backend';
+export {BaseRequestOptions, RequestOptions} from './src/base_request_options';
+export {BaseResponseOptions, ResponseOptions} from './src/base_response_options';
+export {ReadyState, RequestMethod, ResponseContentType, ResponseType} from './src/enums';
+export {Headers} from './src/headers';
+export {Http, Jsonp} from './src/http';
+export {Connection, ConnectionBackend, RequestOptionsArgs, ResponseOptionsArgs, XSRFStrategy} from './src/interfaces';
+export {Request} from './src/static_request';
+export {Response} from './src/static_response';
+export {QueryEncoder, URLSearchParams} from './src/url_search_params';
