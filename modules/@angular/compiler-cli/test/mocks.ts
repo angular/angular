09043124bde6a23da9526d2d4a8d6fc4768fb220
Rob Wormald <robwormald@gmail.com>
2016-08-24 11:50:49 -0700

    chore(deps): update to TS 2.0.0 beta

--- modules/@angular/compiler-cli/test/mocks.ts
+++ modules/@angular/compiler-cli/test/mocks.ts
@@ -6,9 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {ReflectorHostContext} from '@angular/compiler-cli/src/reflector_host';
 import * as ts from 'typescript';
-
-import {ReflectorHostContext} from '../src/reflector_host';
 
 export type Entry = string | Directory;
 
@@ -60,6 +59,15 @@ export class MockContext implements ReflectorHostContext {
       current = next;
     }
     return current;
+  }
+
+  getDirectories(path: string): string[] {
+    const dir = this.getEntry(path);
+    if (typeof dir !== 'object') {
+      return [];
+    } else {
+      return Object.keys(dir).filter(key => typeof dir[key] === 'object');
+    }
   }
 }
 
@@ -117,4 +125,6 @@ export class MockCompilerHost implements ts.CompilerHost {
   useCaseSensitiveFileNames(): boolean { return false; }
 
   getNewLine(): string { return '\n'; }
+
+  getDirectories(path: string): string[] { return this.context.getDirectories(path); }
 }
