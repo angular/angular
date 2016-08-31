d941863e8dd0de2c1d734ead29baa403b7ad45e3
Victor Berchet <victor@suumit.com>
2016-08-30 21:02:02 -0700

    fix(Router): fix type

--- modules/@angular/router/src/router.ts
+++ modules/@angular/router/src/router.ts
@@ -546,7 +546,7 @@ export class Router {
                   resolvePromise(false);
                 }
               },
-              e => {
+            (e: any) => {
                 if (e instanceof NavigationCancelingError) {
                   this.navigated = true;
                   this.routerEvents.next(
