887040d93e61c7192e176d97b6b23636de3c4412
mlaval <mlaval@amadeus.com>
2016-08-31 12:16:01 +0200

    build: update the symlinks scripts for Windows to new packaging

--- scripts/windows/create-symlinks.sh
+++ scripts/windows/create-symlinks.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 while read RAW_PACKAGE
 do
   PACKAGE=${RAW_PACKAGE: : -1}
-  DESTDIR=./../../modules/\@angular/${PACKAGE}/src
+  DESTDIR=./../../modules/\@angular/${PACKAGE}
   mv ${DESTDIR}/facade ${DESTDIR}/facade.old
-  cmd <<< "mklink \"..\\..\\modules\\\@angular\\"${PACKAGE}"\\src\\facade\" \"..\\..\\facade\\src\\\""
+  cmd <<< "mklink \"..\\..\\modules\\\@angular\\"${PACKAGE}"\\facade\" \"..\\..\\facade\\src\\\""
 done < packages.txt
