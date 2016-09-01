b3d19b4deefd57d06991aad2e293c17c5ee0ee23
Tobias Bosch <tbosch1009@gmail.com>
2016-09-01 10:29:35 -0700

    feat(benchmarks): add static tree benchmark

---
+++ modules/benchmarks/src/tree/ng2_static/index.ts
@@ -0,0 +1,40 @@
+import {ApplicationRef, NgModule, enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+import {bindAction, profile} from '../../util';
+import {TreeNode, buildTree, emptyTree} from '../util';
+
+import {AppModule, RootTreeComponent} from './tree';
+
+export function main() {
+  var tree: RootTreeComponent;
+  var appRef: ApplicationRef;
+
+  function destroyDom() {
+    tree.data = emptyTree;
+    appRef.tick();
+  }
+
+  function createDom() {
+    tree.data = buildTree();
+    appRef.tick();
+  }
+
+  function noop() {}
+
+  function init() {
+    enableProdMode();
+    platformBrowserDynamic().bootstrapModule(AppModule).then((ref) => {
+      const injector = ref.injector;
+      appRef = injector.get(ApplicationRef);
+
+      tree = appRef.components[0].instance;
+      bindAction('#destroyDom', destroyDom);
+      bindAction('#createDom', createDom);
+      bindAction('#updateDomProfile', profile(createDom, noop, 'update'));
+      bindAction('#createDomProfile', profile(createDom, destroyDom, 'create'));
+    });
+  }
+
+  init();
+}
