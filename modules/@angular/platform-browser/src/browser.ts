5534c13ac796ea839783b762f03595aa2db14715
PatrickJS <github@gdi2290.com>
2016-09-01 01:01:48 -0700

    refactor(errorHandler): rename to _errorHandler

--- modules/@angular/platform-browser/src/browser.ts
+++ modules/@angular/platform-browser/src/browser.ts
@@ -53,7 +53,7 @@ export function initDomAdapter() {
   BrowserGetTestability.init();
 }
 
-export function errorHandler(): ErrorHandler {
+export function _errorHandler(): ErrorHandler {
   return new ErrorHandler();
 }
 
@@ -75,7 +75,7 @@ export function _resolveDefaultAnimationDriver(): AnimationDriver {
  */
 @NgModule({
   providers: [
-    BROWSER_SANITIZATION_PROVIDERS, {provide: ErrorHandler, useFactory: errorHandler, deps: []},
+    BROWSER_SANITIZATION_PROVIDERS, {provide: ErrorHandler, useFactory: _errorHandler, deps: []},
     {provide: DOCUMENT, useFactory: _document, deps: []},
     {provide: EVENT_MANAGER_PLUGINS, useClass: DomEventsPlugin, multi: true},
     {provide: EVENT_MANAGER_PLUGINS, useClass: KeyEventsPlugin, multi: true},
