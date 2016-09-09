baded51470fbb883432d315a808fba1113b25431
Victor Berchet <victor@suumit.com>
2016-09-08 18:30:12 -0700

    refactor(common): cleanup, strip deprecated doc

--- modules/@angular/common/src/pipes.ts
+++ modules/@angular/common/src/pipes.ts
@@ -11,13 +11,3 @@
  * @description
  * This module provides a set of common Pipes.
  */
-
-export {AsyncPipe} from './pipes/async_pipe';
-export {DatePipe} from './pipes/date_pipe';
-export {I18nPluralPipe} from './pipes/i18n_plural_pipe';
-export {I18nSelectPipe} from './pipes/i18n_select_pipe';
-export {JsonPipe} from './pipes/json_pipe';
-export {LowerCasePipe} from './pipes/lowercase_pipe';
-export {CurrencyPipe, DecimalPipe, PercentPipe} from './pipes/number_pipe';
-export {SlicePipe} from './pipes/slice_pipe';
-export {UpperCasePipe} from './pipes/uppercase_pipe';
