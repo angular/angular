42f60ca303543cb3a869a6b42bbcd5890b09f131
vsavkin <avix1000@gmail.com>
2016-09-14 15:12:25 -0700

    docs(core): update dts file

--- modules/@angular/core/src/metadata/directives.ts
+++ modules/@angular/core/src/metadata/directives.ts
@@ -56,14 +56,15 @@ export interface DirectiveDecorator {
    *
    * **Metadata Properties:**
    *
-   * **exportAs** - name under which the component instance is exported in a template
-   * **host** - map of class property to host element bindings for events, properties and attributes
-   * **inputs** - list of class property names to data-bind as component inputs
-   * **outputs** - list of class property names that expose output events that others can subscribe
-   * to
-   * **providers** - list of providers available to this component and its children
-   * **queries** -  configure queries that can be injected into the component
-   * **selector** - css selector that identifies this component in a template
+   * * **exportAs** - name under which the component instance is exported in a template
+   * * **host** - map of class property to host element bindings for events, properties and
+   * attributes
+   * * **inputs** - list of class property names to data-bind as component inputs
+   * * **outputs** - list of class property names that expose output events that others can
+   * subscribe to
+   * * **providers** - list of providers available to this component and its children
+   * * **queries** -  configure queries that can be injected into the component
+   * * **selector** - css selector that identifies this component in a template
    *
    * @stable
    * @Annotation
@@ -422,16 +423,7 @@ export interface ComponentDecorator {
    *
    * @howToUse
    *
-   * ```
-   * import {Component} from '@angular/core';
-   *
-   * @Component({
-   *   selector: 'my-component',
-   *   templateUrl: 'myComponent.html'
-   * })
-   * export class MyComponent {
-   * }
-   * ```
+   * {@example core/ts/metadata/metadata.ts region='component'}
    *
    * @description
    * Component decorator allows you to mark a class as an Angular component and provide additional
@@ -452,26 +444,27 @@ export interface ComponentDecorator {
    *
    * **Metadata Properties:**
    *
-   * **animations** - list of animations of this component
-   * **changeDetection** - change detection strategy used by this component
-   * **encapsulation** - style encapsulation strategy used by this component
-   * **entryComponents** - list of components that are dynamically inserted into the view of this
+   * * **animations** - list of animations of this component
+   * * **changeDetection** - change detection strategy used by this component
+   * * **encapsulation** - style encapsulation strategy used by this component
+   * * **entryComponents** - list of components that are dynamically inserted into the view of this
    * component
-   * **exportAs** - name under which the component instance is exported in a template
-   * **host** - map of class property to host element bindings for events, properties and attributes
-   * **inputs** - list of class property names to data-bind as component inputs
-   * **interpolation** - custom interpolation markers used in this component's template
-   * **moduleId** - ES/CommonJS module id of the file in which this component is defined
-   * **outputs** - list of class property names that expose output events that others can subscribe
-   * to
-   * **providers** - list of providers available to this component and its children
-   * **queries** -  configure queries that can be injected into the component
-   * **selector** - css selector that identifies this component in a template
-   * **styleUrls** - list of urls to stylesheets to be applied to this component's view
-   * **styles** - inline-defined styles to be applied to this component's view
-   * **template** - inline-defined template for the view
-   * **templateUrl** - url to an external file containing a template for the view
-   * **viewProviders** - list of providers available to this component and its view children
+   * * **exportAs** - name under which the component instance is exported in a template
+   * * **host** - map of class property to host element bindings for events, properties and
+   * attributes
+   * * **inputs** - list of class property names to data-bind as component inputs
+   * * **interpolation** - custom interpolation markers used in this component's template
+   * * **moduleId** - ES/CommonJS module id of the file in which this component is defined
+   * * **outputs** - list of class property names that expose output events that others can
+   * subscribe to
+   * * **providers** - list of providers available to this component and its children
+   * * **queries** -  configure queries that can be injected into the component
+   * * **selector** - css selector that identifies this component in a template
+   * * **styleUrls** - list of urls to stylesheets to be applied to this component's view
+   * * **styles** - inline-defined styles to be applied to this component's view
+   * * **template** - inline-defined template for the view
+   * * **templateUrl** - url to an external file containing a template for the view
+   * * **viewProviders** - list of providers available to this component and its view children
    *
    * ### Example
    *
