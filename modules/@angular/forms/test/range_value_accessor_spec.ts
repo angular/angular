86cce94fc6d0657857bcbfe7a439e2775976765f
AlmogShaul <shaul.almog@gmail.com>
2016-09-15 14:51:11 +0300

    bug(forms) added range value accessor to convert range default string value to number value Added test

---
+++ modules/@angular/forms/test/range_value_accessor_spec.ts
@@ -0,0 +1,48 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+import {ComponentFixture, TestBed, async} from '@angular/core/testing';
+
+export function main() {
+  describe('range value accessor', () => {
+    let fixture: ComponentFixture<any>;
+
+    function getComponent(): TestComponent {
+      return fixture.componentInstance;
+    }
+
+    afterEach(() => {
+      fixture = null;
+    });
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({declarations: [TestComponent], imports: [CommonModule]});
+    });
+
+    it('should add styles specified in an object literal', async(() => {
+      const template = `<input type="range" [(ngModel)]="val">`;
+      fixture = createTestComponent(template);
+      expect(typeof(fixture.componentInstance.val)).toBe("number")
+    }));
+
+  });
+
+}
+
+@Component({selector: 'test-cmp', template: ''})
+class TestComponent {
+  val: any;
+}
+
+function createTestComponent(template: string): ComponentFixture<TestComponent> {
+  return TestBed.overrideComponent(TestComponent, {set: {template: template}})
+    .createComponent(TestComponent);
+}
+
