bf81b06a28675527f9c297fbb11a4c187a2c4fa9
Kara <kara@users.noreply.github.com>
2016-09-19 16:25:33 -0700

    docs(forms): add select control examples (#11728)

--- modules/@angular/forms/src/directives/ng_model.ts
+++ modules/@angular/forms/src/directives/ng_model.ts
@@ -76,6 +76,11 @@ const resolvedPromise = Promise.resolve(null);
  *
  * {@example forms/ts/simpleForm/simple_form_example.ts region='Component'}
  *
+ * To see `ngModel` examples with different form control types, see:
+ *
+ * * Radio buttons: {@link RadioControlValueAccessor}
+ * * Selects: {@link SelectControlValueAccessor}
+ *
  * **npm package**: `@angular/forms`
  *
  * **NgModule**: `FormsModule`
