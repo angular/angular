b85e3b19ae182703c5366923a1a9a49900c48149
Victor Berchet <victor@suumit.com>
2016-09-19 15:13:05 -0700

    fix(OfflineCompiler): support older TS versions

--- modules/@angular/compiler/src/offline_compiler.ts
+++ modules/@angular/compiler/src/offline_compiler.ts
@@ -192,10 +192,12 @@ export class OfflineCompiler {
 function _resolveViewStatements(compileResult: ViewCompileResult): o.Statement[] {
   compileResult.dependencies.forEach((dep) => {
     if (dep instanceof ViewFactoryDependency) {
-      dep.placeholder.moduleUrl = _ngfactoryModuleUrl(dep.comp.moduleUrl);
+      const vfd = <ViewFactoryDependency>dep;
+      vfd.placeholder.moduleUrl = _ngfactoryModuleUrl(vfd.comp.moduleUrl);
     } else if (dep instanceof ComponentFactoryDependency) {
-      dep.placeholder.name = _componentFactoryName(dep.comp);
-      dep.placeholder.moduleUrl = _ngfactoryModuleUrl(dep.comp.moduleUrl);
+      const cfd = <ComponentFactoryDependency>dep;
+      cfd.placeholder.name = _componentFactoryName(cfd.comp);
+      cfd.placeholder.moduleUrl = _ngfactoryModuleUrl(cfd.comp.moduleUrl);
     }
   });
   return compileResult.statements;
