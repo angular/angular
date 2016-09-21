4d5991a930f73ec6cfb3730e81497dd116cdf1d3
Victor Berchet <victor@suumit.com>
2016-09-21 11:14:28 -0700

    refactor(TemplateParser): clearer error message for on* binding
    fixes #11756

--- modules/@angular/compiler/src/template_parser/template_parser.ts
+++ modules/@angular/compiler/src/template_parser/template_parser.ts
@@ -854,7 +854,7 @@ class TemplateParseVisitor implements html.Visitor {
         boundPropertyName = this._schemaRegistry.getMappedPropName(partValue);
         securityContext = this._schemaRegistry.securityContext(elementName, boundPropertyName);
         bindingType = PropertyBindingType.Property;
-        this._assertNoEventBinding(boundPropertyName, sourceSpan);
+        this._assertNoEventBinding(boundPropertyName, sourceSpan, false);
         if (!this._schemaRegistry.hasProperty(elementName, boundPropertyName, this._schemas)) {
           let errorMsg =
               `Can't bind to '${boundPropertyName}' since it isn't a known property of '${elementName}'.`;
@@ -869,7 +869,7 @@ class TemplateParseVisitor implements html.Visitor {
     } else {
       if (parts[0] == ATTRIBUTE_PREFIX) {
         boundPropertyName = parts[1];
-        this._assertNoEventBinding(boundPropertyName, sourceSpan);
+        this._assertNoEventBinding(boundPropertyName, sourceSpan, true);
         // NB: For security purposes, use the mapped property name, not the attribute name.
         const mapPropName = this._schemaRegistry.getMappedPropName(boundPropertyName);
         securityContext = this._schemaRegistry.securityContext(elementName, mapPropName);
@@ -902,12 +902,23 @@ class TemplateParseVisitor implements html.Visitor {
         boundPropertyName, bindingType, securityContext, ast, unit, sourceSpan);
   }
 
-  private _assertNoEventBinding(propName: string, sourceSpan: ParseSourceSpan): void {
+  /**
+   * @param propName the name of the property / attribute
+   * @param sourceSpan
+   * @param isAttr true when binding to an attribute
+   * @private
+   */
+  private _assertNoEventBinding(propName: string, sourceSpan: ParseSourceSpan, isAttr: boolean):
+      void {
     if (propName.toLowerCase().startsWith('on')) {
-      this._reportError(
-          `Binding to event attribute '${propName}' is disallowed ` +
-              `for security reasons, please use (${propName.slice(2)})=...`,
-          sourceSpan, ParseErrorLevel.FATAL);
+      let msg = `Binding to event attribute '${propName}' is disallowed for security reasons, ` +
+          `please use (${propName.slice(2)})=...`;
+      if (!isAttr) {
+        msg +=
+            `\nIf '${propName}' is a directive input, make sure the directive is imported by the` +
+            ` current module.`;
+      }
+      this._reportError(msg, sourceSpan, ParseErrorLevel.FATAL);
     }
   }
 
