5b70688cec3421a2c4232bbfbf1d65f9b9f0cf79
Craig Nishina <craig.nishina@gmail.com>
2016-09-22 01:37:03 -0700

    update missing typing after running ./test.sh node

--- modules/@angular/benchpress/test/firefox_extension/sample_benchmark.ts
+++ modules/@angular/benchpress/test/firefox_extension/sample_benchmark.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {browser} from 'protractor';
+import {$, browser} from 'protractor';
 
 var benchpress = require('../../index.js');
 var runner = new benchpress.Runner([
