324cc6ff29ae3a24a7d37c29e545387ca4232a56
danielcrisp <daniel@danielcrisp.com>
2016-10-03 18:09:51 +0100

    fix(enableDebugTools): create AngularTools by merging into context.ng
    When using `enableDebugTools` the tools are merged into `context.ng` to prevent `ng.probe` and `ng.coreTokens` from being clobbered.
    
    Fixes #12002

--- modules/@angular/platform-browser/src/browser/tools/tools.ts
+++ modules/@angular/platform-browser/src/browser/tools/tools.ts
@@ -27,7 +27,7 @@ var context = <any>global;
  * @experimental All debugging apis are currently experimental.
  */
 export function enableDebugTools<T>(ref: ComponentRef<T>): ComponentRef<T> {
-  context.ng = new AngularTools(ref);
+  (<any>Object).assign(context.ng, new AngularTools(ref));
   return ref;
 }
 
@@ -37,5 +37,5 @@ export function enableDebugTools<T>(ref: ComponentRef<T>): ComponentRef<T> {
  * @experimental All debugging apis are currently experimental.
  */
 export function disableDebugTools(): void {
-  delete context.ng;
+  delete context.ng.profiler;
 }
