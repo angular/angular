4678e5d37b983bd620726583d33353be313faa71
Victor Berchet <victor@suumit.com>
2016-10-04 17:02:38 -0700

    refactor(core): simplify Reflector code, add types

--- modules/@angular/core/src/reflection/reflection.ts
+++ modules/@angular/core/src/reflection/reflection.ts
@@ -16,4 +16,4 @@ export {ReflectionInfo, Reflector} from './reflector';
  * The {@link Reflector} used internally in Angular to access metadata
  * about symbols.
  */
-export var reflector = new Reflector(new ReflectionCapabilities());
+export const reflector = new Reflector(new ReflectionCapabilities());
