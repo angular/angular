606e51881a20242abc1adf1922057a67c79843fc
Victor Berchet <victor@suumit.com>
2016-10-12 16:36:18 -0700

    perf(benchmarks): update ng2-switch to match ng2
    - use the ~same template layout (text nodes),
    - use trackBy
    
    both benchmark now show about the same perfs.

--- modules/benchmarks/src/largetable/ng2/table.ts
+++ modules/benchmarks/src/largetable/ng2/table.ts
@@ -5,8 +5,13 @@ import {TableCell, emptyTable} from '../util';
 
 @Component({
   selector: 'largetable',
-  template:
-      `<table><tbody><tr *ngFor="let row of data; trackBy: trackByIndex"><td *ngFor="let cell of row; trackBy: trackByIndex" [style.backgroundColor]="cell.row % 2 ? '' : 'grey'">{{cell.value}}</td></tr></tbody></table>`
+  template: `<table><tbody>
+    <tr *ngFor="let row of data; trackBy: trackByIndex">
+      <td *ngFor="let cell of row; trackBy: trackByIndex" [style.backgroundColor]="cell.row % 2 ? '' : 'grey'">
+      {{cell.value}}
+      </td>
+    </tr>
+  </tbody></table>`,
 })
 export class TableComponent {
   @Input()
