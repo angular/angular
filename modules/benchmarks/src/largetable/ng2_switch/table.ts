5735cddddbad214f192ea5dc3ad225d6694eb2ec
Victor Berchet <victor@suumit.com>
2016-10-12 16:36:18 -0700

    perf(benchmarks): update ng2-switch to match ng2
    - use the ~same template layout (text nodes),
    - use trackBy
    
    both benchmark now show about the same perfs.

--- modules/benchmarks/src/largetable/ng2_switch/table.ts
+++ modules/benchmarks/src/largetable/ng2_switch/table.ts
@@ -6,18 +6,18 @@ import {TableCell, emptyTable} from '../util';
 @Component({
   selector: 'largetable',
   template: `<table><tbody>
-    <tr *ngFor="let row of data">
-    <template ngFor [ngForOf]="row" let-cell>
-      <ng-container [ngSwitch]="cell.row % 2">
-        <td *ngSwitchCase="0" style="background-color: grey">{{cell.value}}</td>
-        <td *ngSwitchDefault>{{cell.value}}</td>
-      </ng-container>
-    </template>
-    </tr></tbody></table>`
+    <tr *ngFor="let row of data; trackBy: trackByIndex">
+    <template ngFor [ngForOf]="row" [ngForTrackBy]="trackByIndex" let-cell><ng-container [ngSwitch]="cell.row % 2">
+        <td *ngSwitchCase="0" style="background-color: grey">{{cell.value}}</td><td *ngSwitchDefault>{{cell.value}}</td>    
+    </ng-container></template>
+    </tr>
+  </tbody></table>`
 })
 export class TableComponent {
   @Input()
   data: TableCell[][] = emptyTable;
+
+  trackByIndex(index: number, item: any) { return index; }
 }
 
 @NgModule({imports: [BrowserModule], bootstrap: [TableComponent], declarations: [TableComponent]})
