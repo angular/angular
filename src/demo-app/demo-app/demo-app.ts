29546317effebb7e27b18989b353774ec72d5dd7
Andrew Seguin <andrewjs@google.com>
2016-10-19 15:23:54 -0700

    chore: close sidenav on nav item click (#1535)

--- src/demo-app/demo-app/demo-app.ts
+++ src/demo-app/demo-app/demo-app.ts
@@ -18,4 +18,33 @@ export class Home {}
   styleUrls: ['demo-app.css'],
   encapsulation: ViewEncapsulation.None,
 })
-export class DemoApp { }
+export class DemoApp {
+  navItems = [
+    {name: 'Button', route: 'button'},
+    {name: 'Button Toggle', route: 'button-toggle'},
+    {name: 'Card', route: 'card'},
+    {name: 'Checkbox', route: 'checkbox'},
+    {name: 'Dialog', route: 'dialog'},
+    {name: 'Gestures', route: 'gestures'},
+    {name: 'Grid List', route: 'grid-list'},
+    {name: 'Icon', route: 'icon'},
+    {name: 'Input', route: 'input'},
+    {name: 'List', route: 'list'},
+    {name: 'Menu', route: 'menu'},
+    {name: 'Live Announcer', route: 'live-announcer'},
+    {name: 'Overlay', route: 'overlay'},
+    {name: 'Portal', route: 'portal'},
+    {name: 'Progress Bar', route: 'progress-bar'},
+    {name: 'Progress Circle', route: 'progress-circle'},
+    {name: 'Radio', route: 'radio'},
+    {name: 'Ripple', route: 'ripple'},
+    {name: 'Select', route: 'select'},
+    {name: 'Sidenav', route: 'sidenav'},
+    {name: 'Slider', route: 'slider'},
+    {name: 'Slide Toggle', route: 'slide-toggle'},
+    {name: 'Snack Bar', route: 'snack-bar'},
+    {name: 'Tabs', route: 'tabs'},
+    {name: 'Toolbar', route: 'toolbar'},
+    {name: 'Tooltip', route: 'tooltip'}
+  ];
+}
