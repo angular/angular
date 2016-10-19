e395de49355e28b46953bccdb135dff5e74fadf2
vsavkin <avix1000@gmail.com>
2016-10-19 14:32:03 -0700

    cleanup(router): add a test verifying than NavigationEnd is not emitted after NavigationCancel

--- modules/@angular/router/test/integration.spec.ts
+++ modules/@angular/router/test/integration.spec.ts
@@ -887,6 +887,9 @@ describe('Integration', () => {
         it('works', fakeAsync(inject([Router, Location], (router: Router, location: Location) => {
              const fixture = createRoot(router, RootCmp);
 
+             const recordedEvents: any[] = [];
+             router.events.forEach(e => recordedEvents.push(e));
+
              router.resetConfig(
                  [{path: 'team/:id', component: TeamCmp, canActivate: ['alwaysFalse']}]);
 
@@ -894,6 +897,10 @@ describe('Integration', () => {
              advance(fixture);
 
              expect(location.path()).toEqual('/');
+             expectEvents(recordedEvents, [
+               [NavigationStart, '/team/22'], [RoutesRecognized, '/team/22'],
+               [NavigationCancel, '/team/22']
+             ]);
            })));
       });
 
