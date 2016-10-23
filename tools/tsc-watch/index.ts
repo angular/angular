bf843b68b7a561407b62b79ca4ce826f4718e86c
Joao Dias <alfa.project@gmail.com>
2016-10-23 22:37:15 +0200

    style: add missing copyright headers

--- tools/tsc-watch/index.ts
+++ tools/tsc-watch/index.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
 import {spawn} from 'child_process';
 import {existsSync, mkdirSync, writeFileSync} from 'fs';
 
