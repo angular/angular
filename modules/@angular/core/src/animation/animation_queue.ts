383f23b57801bfe14f5a2f5cddf84d2071fdaa0d
Matias Niemelä <matias@yearofmoo.com>
2016-11-04 15:15:27 -0700

    fix(animations): always trigger animations after the change detection check (#12713)
    This patch ensures that animations are run outside of change detection
    thus allowing for start and done callbacks to modify application data
    without causing a cycle loop.
    
    Closes #12713

--- modules/@angular/core/src/animation/animation_queue.ts
+++ modules/@angular/core/src/animation/animation_queue.ts
@@ -17,6 +17,15 @@ export function queueAnimation(player: AnimationPlayer) {
 
 /** @internal */
 export function triggerQueuedAnimations() {
+  // this code is wrapped into a single promise such that the
+  // onStart and onDone player callbacks are triggered outside
+  // of the digest cycle of animations
+  if (_queuedAnimations.length) {
+    Promise.resolve(null).then(_triggerAnimations);
+  }
+}
+
+function _triggerAnimations() {
   for (var i = 0; i < _queuedAnimations.length; i++) {
     var player = _queuedAnimations[i];
     player.play();
