9bb4809f896d458c1baca611fa0c1be42b691230
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-11-08 21:27:28 +0100

    chore(saucelabs): interrupt if saucelabs tunnel does not start (#1747)

--- scripts/browserstack/waitfor_tunnel.sh
+++ scripts/browserstack/waitfor_tunnel.sh
@@ -2,18 +2,18 @@
 
 
 # Wait for Connect to be ready before exiting
-# Time out if we wait for more than 2 minutes, so that we can print logs.
+# Time out if we wait for more than 2 minutes, so the process won't run forever.
 let "counter=0"
 
 while [ ! -f $BROWSER_PROVIDER_READY_FILE ]; do
   let "counter++"
+
   if [ $counter -gt 240 ]; then
-    echo "Timed out after 2 minutes waiting for browser provider ready file"
-    # We must manually print logs here because travis will not run
-    # after_script commands if the failure occurs before the script
-    # phase.
-    ./scripts/ci/print-logs.sh
+    echo
+    echo "Timed out after 2 minutes waiting for tunnel ready file"
     exit 5
   fi
+
+  printf "."
   sleep .5
 done
--- scripts/sauce/sauce_connect_block.sh
+++ scripts/sauce/sauce_connect_block.sh
@@ -2,9 +2,24 @@
 
 # Wait for Connect to be ready before exiting
 echo "Connecting to Sauce Labs"
+
+
+# Wait for Saucelabs Connect to be ready before exiting
+# Time out if we wait for more than 2 minutes, so the process won't run forever.
+let "counter=0"
+
 while [ ! -f $BROWSER_PROVIDER_READY_FILE ]; do
+  let "counter++"
+
+  if [ $counter -gt 240 ]; then
+    echo
+    echo "Timed out after 2 minutes waiting for tunnel ready file"
+    exit 5
+  fi
+
   printf "."
   sleep .5
 done
+
 echo
 echo "Connected"
