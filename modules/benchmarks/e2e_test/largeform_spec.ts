4e6596e0ed0f18e027f696639e143488e0d79d14
Michael Giambalvo <heathkit@google.com>
2016-11-07 16:14:57 -0800

    Fix typings in benchmarks.

--- modules/benchmarks/e2e_test/largeform_spec.ts
+++ modules/benchmarks/e2e_test/largeform_spec.ts
@@ -6,6 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {$, element, By} from 'protractor';
 import {openBrowser, verifyNoBrowserErrors} from 'e2e_util/e2e_util';
 
 describe('largeform benchmark spec', () => {
