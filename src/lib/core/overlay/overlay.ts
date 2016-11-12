26747363486a042d8cb405d97033a2d818edeaa1
Jeremy Elbourn <jelbourn@gmail.com>
2016-11-11 17:06:36 -0800

    chore: fix lint error (#1835)

--- src/lib/core/overlay/overlay.ts
+++ src/lib/core/overlay/overlay.ts
@@ -1,8 +1,13 @@
-import {ComponentFactoryResolver, Injectable, ApplicationRef, Injector, NgZone} from '@angular/core';
+import {
+  ComponentFactoryResolver,
+  Injectable,
+  ApplicationRef,
+  Injector,
+  NgZone,
+} from '@angular/core';
 import {OverlayState} from './overlay-state';
 import {DomPortalHost} from '../portal/dom-portal-host';
 import {OverlayRef} from './overlay-ref';
-
 import {OverlayPositionBuilder} from './position/overlay-position-builder';
 import {ViewportRuler} from './position/viewport-ruler';
 import {OverlayContainer} from './overlay-container';
