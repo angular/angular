2452cd14e0b9ae1375c1cd82e02f070341c770c3
Tobias Bosch <tbosch1009@gmail.com>
2016-11-23 14:47:05 -0800

    refactor(compiler): drop old codegen tests that run inside of `test.sh`
    These tests were hard to maintain and only yielded little value,
    now that we have the full integration with TypeScript.

--- modules/@angular/compiler/test/output/js_emitter_spec.ts
+++ modules/@angular/compiler/test/output/js_emitter_spec.ts
@@ -9,18 +9,21 @@
 import {CompileIdentifierMetadata} from '@angular/compiler/src/compile_metadata';
 import {JavaScriptEmitter} from '@angular/compiler/src/output/js_emitter';
 import * as o from '@angular/compiler/src/output/output_ast';
-import {beforeEach, describe, expect, it} from '@angular/core/testing/testing_internal';
-
-import {SimpleJsImportGenerator} from './output_emitter_util';
+import {ImportResolver} from '@angular/compiler/src/output/path_util';
 
 const someModuleUrl = 'somePackage/somePath';
 const anotherModuleUrl = 'somePackage/someOtherPath';
 
 const sameModuleIdentifier =
     new CompileIdentifierMetadata({name: 'someLocalId', moduleUrl: someModuleUrl});
-
 const externalModuleIdentifier =
     new CompileIdentifierMetadata({name: 'someExternalId', moduleUrl: anotherModuleUrl});
+
+class SimpleJsImportGenerator implements ImportResolver {
+  fileNameToModuleName(importedUrlStr: string, moduleUrlStr: string): string {
+    return importedUrlStr;
+  }
+}
 
 export function main() {
   // Note supported features of our OutputAstin JavaScript / ES5:
--- tools/tsc-watch/index.ts
+++ tools/tsc-watch/index.ts
@@ -15,44 +15,6 @@ import {TSC, TscWatch, reportError} from './tsc_watch';
 export * from './tsc_watch';
 import 'reflect-metadata';
 
-const OFFLINE_COMPILE =
-    ['output/output_emitter_codegen_untyped', 'output/output_emitter_codegen_typed'];
-
-function processOutputEmitterCodeGen(): Promise<number> {
-  return new Promise((resolve, reject) => {
-           const outDir = 'dist/all/@angular/compiler/test/';
-           const promises: Promise<any>[] = [];
-           console.log('Processing codegen...');
-           OFFLINE_COMPILE.forEach((file: string) => {
-             const codegen = require('../../all/@angular/compiler/test/' + file + '.js');
-             if (codegen.emit) {
-               console.log(`  ${file} has changed, regenerating...`);
-               promises.push(Promise.resolve(codegen.emit()).then((code) => {
-                 writeFileSync(outDir + file + '.ts', code);
-               }));
-             }
-           });
-           if (promises.length) {
-             Promise.all(promises)
-                 .then(() => {
-                   const args =
-                       ['--project', 'tools/cjs-jasmine/tsconfig-output_emitter_codegen.json'];
-                   console.log('    compiling changes: tsc ' + args.join(' '));
-                   const tsc = spawn(TSC, args, {stdio: 'pipe'});
-                   tsc.stdout.on('data', (data: any) => process.stdout.write(data));
-                   tsc.stderr.on('data', (data: any) => process.stderr.write(data));
-                   tsc.on(
-                       'close',
-                       (code: any) => code ? reject('Tsc exited with: ' + code) : resolve(code));
-                 })
-                 .catch(reportError);
-           } else {
-             resolve(0);
-           }
-         })
-      .catch(reportError);
-}
-
 function md(dir: string, folders: string[]) {
   if (folders.length) {
     const next = folders.shift();
@@ -77,13 +39,10 @@ if (platform == 'node') {
   tscWatch = new TscWatch(Object.assign(
       {
         tsconfig: 'modules/tsconfig.json',
-        onChangeCmds: [
-          processOutputEmitterCodeGen,
-          [
-            'node', 'dist/tools/cjs-jasmine', '--', '@angular/**/*_spec.js',
-            '@angular/compiler-cli/test/**/*_spec.js', '@angular/benchpress/test/**/*_spec.js'
-          ]
-        ]
+        onChangeCmds: [[
+          'node', 'dist/tools/cjs-jasmine', '--', '@angular/**/*_spec.js',
+          '@angular/compiler-cli/test/**/*_spec.js', '@angular/benchpress/test/**/*_spec.js'
+        ]]
       },
       BaseConfig));
 } else if (platform == 'browser') {
