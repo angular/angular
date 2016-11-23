e39e16ae6afcab8ce909fe50a2ecd8950bef62d6
Bowen Ni <bowen1897@gmail.com>
2016-11-23 13:49:57 -0800

    Keep `console.log` that are not called during compilation.

--- modules/@angular/examples/_common/e2e_util.ts
+++ modules/@angular/examples/_common/e2e_util.ts
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+/* tslint:disable:no-console  */
 import * as webdriver from 'selenium-webdriver';
 declare var browser: any;
 declare var expect: any;
@@ -15,6 +16,7 @@ export function verifyNoBrowserErrors() {
     const errors: any[] = [];
     browserLog.filter(logEntry => {
       const msg = logEntry.message;
+      console.log('>> ' + msg);
       if (logEntry.level.value >= webdriver.logging.Level.INFO.value) {
         errors.push(msg);
       }
