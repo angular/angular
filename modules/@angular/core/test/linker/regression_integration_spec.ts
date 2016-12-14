efa2d80df886f7e686c6558574542d2af38e9d84
Tobias Bosch <tbosch1009@gmail.com>
2016-12-13 16:27:40 -0800

    fix(compiler): make sure provider values with `name` property don’t break.
    Fixes #13394
    Closes #13445

--- modules/@angular/core/test/linker/regression_integration_spec.ts
+++ modules/@angular/core/test/linker/regression_integration_spec.ts
@@ -8,7 +8,6 @@
 
 import {Component, Injector, OpaqueToken, Pipe, PipeTransform, Provider} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
-import {beforeEach, describe, it} from '@angular/core/testing/testing_internal';
 import {expect} from '@angular/platform-browser/testing/matchers';
 
 export function main() {
@@ -138,6 +137,15 @@ function declareTests({useJit}: {useJit: boolean}) {
 
         expect(injector.get(token1)).toEqual(tokenValue1);
         expect(injector.get(token2)).toEqual(tokenValue2);
+      });
+
+      it('should support providers that have a `name` property with a number value', () => {
+        class TestClass {
+          constructor(public name: number) {}
+        }
+        const data = [new TestClass(1), new TestClass(2)];
+        const injector = createInjector([{provide: 'someToken', useValue: data}]);
+        expect(injector.get('someToken')).toEqual(data);
       });
     });
 
