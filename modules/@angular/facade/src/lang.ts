8ce759872a547dca4474f181f9f984b8dfec3a98
Tobias Bosch <tbosch1009@gmail.com>
2016-12-13 16:27:40 -0800

    fix(compiler): make sure provider values with `name` property don’t break.
    Fixes #13394

--- modules/@angular/facade/src/lang.ts
+++ modules/@angular/facade/src/lang.ts
@@ -95,11 +95,11 @@ export function stringify(token: any): string {
   }
 
   if (token.overriddenName) {
-    return token.overriddenName;
+    return `${token.overriddenName}`;
   }
 
   if (token.name) {
-    return token.name;
+    return `${token.name}`;
   }
 
   const res = token.toString();
