a0a05041ac15598c3baccc3f84294199b589a195
Victor Berchet <victor@suumit.com>
2016-12-13 17:44:52 -0800

    refactor: format & lint

--- modules/@angular/compiler-cli/integrationtest/ngtools_src/app.module.ts
+++ modules/@angular/compiler-cli/integrationtest/ngtools_src/app.module.ts
@@ -1,23 +1,26 @@
-import { NgModule, Component } from '@angular/core';
-import { BrowserModule  } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 
-@Component({
-  selector: 'home-view',
-  template: 'home!'
-})
-export class HomeView {}
+import {Component, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+
+import {AppComponent} from './app.component';
+
+@Component({selector: 'home-view', template: 'home!'})
+export class HomeView {
+}
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeView
-  ],
+  declarations: [AppComponent, HomeView],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot([
+    BrowserModule, RouterModule.forRoot([
       {path: 'lazy', loadChildren: './lazy.module#LazyModule'},
       {path: 'feature2', loadChildren: 'feature2/feature2.module#Feature2Module'},
       {path: '', component: HomeView}
@@ -25,4 +28,5 @@ export class HomeView {}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
