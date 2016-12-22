82af1bd8b910f93ff4e054178700ed2c6fbb4eaf
Chuck Jazdzewski <chuckj@google.com>
2016-12-22 15:28:20 -0800

    fix(language-service): support TypeScript 2.1
    @angular/language-service now supports using TypeScript 2.1 as the
    the TypeScript host. TypeScript 2.1 is now also partially supported
    in `ngc` but is not recommended as Tsickle does not yet support 2.1.

--- scripts/ci-lite/offline_compiler_test.sh
+++ scripts/ci-lite/offline_compiler_test.sh
@@ -6,9 +6,10 @@ LINKABLE_PKGS=(
   $(pwd)/dist/packages-dist/{common,forms,core,compiler,compiler-cli,platform-{browser,server},platform-browser-dynamic,router}
   $(pwd)/dist/tools/@angular/tsc-wrapped
 )
+TYPESCRIPT_2_0=typescript@2.0.2
+TYPESCRIPT_2_1=typescript@2.1.4
 PKGS=(
   reflect-metadata@0.1.8
-  typescript@2.0.2
   zone.js@0.6.25
   rxjs@5.0.1
   @types/{node@6.0.38,jasmine@2.2.33}
@@ -30,7 +31,7 @@ cp -v package.json $TMP
 (
   cd $TMP
   set -ex -o pipefail
-  npm install ${PKGS[*]}
+  npm install ${PKGS[*]} $TYPESCRIPT_2_0
   # TODO(alexeagle): allow this to be npm link instead
   npm install ${LINKABLE_PKGS[*]}
 
@@ -61,4 +62,22 @@ cp -v package.json $TMP
   # Compile again with a differently named tsconfig file
   mv tsconfig-build.json othername.json
   ./node_modules/.bin/ngc -p othername.json
+)
+
+# Repeat selected parts of the above with TypeScript 2.1
+readonly TMP_2_1=$TMPDIR/e2e_test.$(date +%s)
+mkdir -p $TMP_2_1
+cp -R -v modules/@angular/compiler-cli/integrationtest/* $TMP_2_1
+cp -R -v modules/benchmarks $TMP_2_1
+cp -v package.json $TMP_2_1
+(
+  cd $TMP_2_1
+  set -ex -o pipefail
+
+  npm install ${PKGS[*]} $TYPESCRIPT_2_1
+  npm install ${LINKABLE_PKGS[*]}
+
+  ./node_modules/.bin/tsc --version
+  node ./node_modules/@angular/tsc-wrapped/src/main -p third_party_src/tsconfig-build.json
+  ./node_modules/.bin/ngc -p tsconfig-build.json --i18nFile=src/messages.fi.xlf --locale=fi --i18nFormat=xlf
 )
