41fc75f7212c942259c94562a8ec5f5da02ef193
Jacob Windsor <jacob.windsor@ymail.com>
2016-12-29 17:53:58 +0100

    (fix) typo: Remove =

--- modules/@angular/http/src/interfaces.ts
+++ modules/@angular/http/src/interfaces.ts
@@ -68,7 +68,7 @@ export interface RequestArgs extends RequestOptionsArgs { url: string; }
  *
  * @experimental
  */
-export interface ResponseOptionsArgs = {
+export interface ResponseOptionsArgs {
   body?: string | Object | FormData | ArrayBuffer | Blob; status?: number; statusText?: string;
   headers?: Headers;
   type?: ResponseType;
