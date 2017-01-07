f1ee29e39f830eb2ec7aac5792102a2b0ea14946
SystemDisc <tim@zornco.com>
2017-01-07 09:58:20 -0600

    feat(forms): update validity on touched
    closes #7113

--- modules/@angular/forms/src/directives/shared.ts
+++ modules/@angular/forms/src/directives/shared.ts
@@ -47,7 +47,10 @@ export function setUpControl(control: FormControl, dir: NgControl): void {
   });
 
   // touched
-  dir.valueAccessor.registerOnTouched(() => control.markAsTouched());
+  dir.valueAccessor.registerOnTouched(() => {
+    control.updateValueAndValidity();
+    control.markAsTouched();
+  });
 
   control.registerOnChange((newValue: any, emitModelEvent: boolean) => {
     // control -> view
