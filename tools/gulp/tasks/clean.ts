0c75643c7d82f69d19407ebeaf43335d236aba8e
limerick <guods2015@gmail.com>
2017-01-13 05:26:14 +0800

    build:change gulp clean dist dir (#2582)

--- tools/gulp/tasks/clean.ts
+++ tools/gulp/tasks/clean.ts
@@ -1,5 +1,6 @@
 import {task} from 'gulp';
+import {DIST_ROOT} from '../constants';
 import {cleanTask} from '../task_helpers';
 
 
-task('clean', cleanTask('dist'));
+task('clean', cleanTask(DIST_ROOT));
