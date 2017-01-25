3355ec3c2dade3311e1dbae3f0b95ba73eedc6c2
Matias Niemelä <matias@yearofmoo.com>
2017-01-25 11:46:15 -0800

    docs(core): add docs for `AnimationStyles` and `AnimationKeyframe` (#14107)

--- modules/@angular/core/src/animation/animation_keyframe.ts
+++ modules/@angular/core/src/animation/animation_keyframe.ts
@@ -9,7 +9,14 @@
 import {AnimationStyles} from './animation_styles';
 
 /**
- * @experimental
+ * `AnimationKeyframe` consists of a series of styles (contained within {@link AnimationStyles
+ * `AnimationStyles`})
+ * and an offset value indicating when those styles are applied within the `duration/delay/easing`
+ * timings.
+ * `AnimationKeyframe` is mostly an internal class which is designed to be used alongside {@link
+ * Renderer#animate-anchor `Renderer.animate`}.
+ *
+ * @experimental Animation support is experimental
  */
 export class AnimationKeyframe {
   constructor(public offset: number, public styles: AnimationStyles) {}
--- modules/@angular/core/src/animation/animation_styles.ts
+++ modules/@angular/core/src/animation/animation_styles.ts
@@ -6,8 +6,19 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+// having an import prevents dgeni from truncating out
+// the class description in the docs. DO NOT REMOVE.
+import {isPresent} from '../facade/lang';
+
 /**
- * @experimental
+ * `AnimationStyles` consists of a collection of key/value maps containing CSS-based style data
+ * that can either be used as initial styling data or apart of a series of keyframes within an
+ * animation.
+ * This class is mostly internal, and it is designed to be used alongside
+ * {@link AnimationKeyframe `AnimationKeyframe`} and {@link Renderer#animate-anchor
+ * `Renderer.animate`}.
+ *
+ * @experimental Animation support is experimental
  */
 export class AnimationStyles {
   constructor(public styles: {[key: string]: string | number}[]) {}
