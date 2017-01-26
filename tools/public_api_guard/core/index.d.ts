d5f1419afe7845c00a1ec95140048660a6f7e999
Misko Hevery <misko@hevery.com>
2017-01-25 22:36:38 -0800

    refactor(size): Use abstract keyword where possible to decrease file size. (#14112)

--- tools/public_api_guard/core/index.d.ts
+++ tools/public_api_guard/core/index.d.ts
@@ -167,10 +167,10 @@ export declare class ApplicationModule {
 export declare abstract class ApplicationRef {
     componentTypes: Type<any>[];
     components: ComponentRef<any>[];
-    viewCount: any;
-    attachView(view: ViewRef): void;
+    viewCount: number;
+    abstract attachView(view: ViewRef): void;
     abstract bootstrap<C>(componentFactory: ComponentFactory<C> | Type<C>): ComponentRef<C>;
-    detachView(view: ViewRef): void;
+    abstract detachView(view: ViewRef): void;
     abstract tick(): void;
 }
 
@@ -512,7 +512,7 @@ export interface InjectDecorator {
 
 /** @stable */
 export declare abstract class Injector {
-    get(token: any, notFoundValue?: any): any;
+    abstract get(token: any, notFoundValue?: any): any;
     static NULL: Injector;
     static THROW_IF_NOT_FOUND: Object;
 }
@@ -703,8 +703,8 @@ export declare const platformCore: (extraProviders?: Provider[]) => PlatformRef;
 export declare abstract class PlatformRef {
     destroyed: boolean;
     injector: Injector;
-    /** @stable */ bootstrapModule<M>(moduleType: Type<M>, compilerOptions?: CompilerOptions | CompilerOptions[]): Promise<NgModuleRef<M>>;
-    /** @experimental */ bootstrapModuleFactory<M>(moduleFactory: NgModuleFactory<M>): Promise<NgModuleRef<M>>;
+    /** @stable */ abstract bootstrapModule<M>(moduleType: Type<M>, compilerOptions?: CompilerOptions | CompilerOptions[]): Promise<NgModuleRef<M>>;
+    /** @experimental */ abstract bootstrapModuleFactory<M>(moduleFactory: NgModuleFactory<M>): Promise<NgModuleRef<M>>;
     abstract destroy(): void;
     abstract onDestroy(callback: () => void): void;
 }
@@ -739,11 +739,11 @@ export declare class QueryList<T> {
 /** @stable */
 export declare abstract class ReflectiveInjector implements Injector {
     parent: Injector;
-    createChildFromResolved(providers: ResolvedReflectiveProvider[]): ReflectiveInjector;
+    abstract createChildFromResolved(providers: ResolvedReflectiveProvider[]): ReflectiveInjector;
     abstract get(token: any, notFoundValue?: any): any;
-    instantiateResolved(provider: ResolvedReflectiveProvider): any;
-    resolveAndCreateChild(providers: Provider[]): ReflectiveInjector;
-    resolveAndInstantiate(provider: Provider): any;
+    abstract instantiateResolved(provider: ResolvedReflectiveProvider): any;
+    abstract resolveAndCreateChild(providers: Provider[]): ReflectiveInjector;
+    abstract resolveAndInstantiate(provider: Provider): any;
     /** @experimental */ static fromResolvedProviders(providers: ResolvedReflectiveProvider[], parent?: Injector): ReflectiveInjector;
     static resolve(providers: Provider[]): ResolvedReflectiveProvider[];
     static resolveAndCreate(providers: Provider[], parent?: Injector): ReflectiveInjector;
