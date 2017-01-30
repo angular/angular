1a26aa960c8f2f63f8e8d9456ecc468c5f1456fa
Chuck Jazdzewski <chuckj@google.com>
2017-01-30 15:32:08 -0800

    feat(compiler): generate type parameters for generic type references (#14104)

--- modules/@angular/compiler/test/output/ts_emitter_spec.ts
+++ modules/@angular/compiler/test/output/ts_emitter_spec.ts
@@ -28,6 +28,7 @@ class SimpleJsImportGenerator implements ImportResolver {
     return importedUrlStr;
   }
   getImportAs(symbol: StaticSymbol): StaticSymbol { return null; }
+  getTypeArity(symbol: StaticSymbol): number /*|null*/ { return null; }
 }
 
 export function main() {
@@ -429,11 +430,19 @@ export function main() {
     });
 
     it('should support expression types', () => {
+      expect(
+          emitStmt(o.variable('a').set(o.NULL_EXPR).toDeclStmt(o.expressionType(o.variable('b')))))
+          .toEqual('var a:b = (null as any);');
+    });
+
+    it('should support expressions with type parameters', () => {
       expect(emitStmt(o.variable('a')
                           .set(o.NULL_EXPR)
-                          .toDeclStmt(o.expressionType(
-                              o.variable('b'), [o.expressionType(o.variable('c'))]))))
-          .toEqual('var a:b<c> = (null as any);');
+                          .toDeclStmt(o.importType(externalModuleIdentifier, [o.STRING_TYPE]))))
+          .toEqual([
+            `import * as import0 from 'somePackage/someOtherPath';`,
+            `var a:import0.someExternalId<string> = (null as any);`
+          ].join('\n'));
     });
 
     it('should support combined types', () => {
