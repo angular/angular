09591cf123dc06a948bc09012b818ad000f60efd
Raven Avalon <raven.avalon@rangle.io>
2017-01-30 16:31:16 -0500

    docs: Rename and update overview and readme files (#2614)

--- scripts/release/copy-docs.sh
+++ scripts/release/copy-docs.sh
@@ -7,7 +7,7 @@
 # Use OVERVIEW.html when possible. If there's no OVERVIEW file exists, use README.html
 
 usage='Usage: copy-docs.sh $destinationFolder'
-if [ $# -ne 1 ]; then 
+if [ $# -ne 1 ]; then
   echo "Missing destination folder. $usage"
   exit
 fi
@@ -23,7 +23,7 @@ fi
 for file in $originFolder*
 do
   name=${file#$originFolder}
-  overviewFile=$originFolder$name/OVERVIEW.html
+  overviewFile=$originFolder$name/$name.html
   readmeFile=$originFolder$name/README.html
   destFile=$destFolder/$name.html
   if [ -f $overviewFile ]; then
--- src/lib/chips/README.md
+++ src/lib/chips/README.md
@@ -1,7 +1,1 @@
-# md-chips
-
-`md-chips` provides a horizontal display of (optionally) selectable, addable, and removable,
-items and an input to create additional ones (again; optional). You can read more about chips
-in the [Material Design spec](https://material.google.com/components/chips.html).
-
-This is a placeholder README for the eventual chips component.
+Please see the official documentation at https://material.angular.io/components/component/chips
