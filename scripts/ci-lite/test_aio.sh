676081fe66c3521a1adacad5ee41a5f289f6b97c
George Kalpakas <kalpakas.g@gmail.com>
2017-02-01 07:59:37 +0200

    ci(aio): fix and enable e2e tests on Travis (#14174)

--- scripts/ci-lite/test_aio.sh
+++ scripts/ci-lite/test_aio.sh
@@ -45,18 +45,15 @@ cd -
 echo 'travis_fold:end:test.aio.unit'
 
 
-# TODO: Chrome crashes with the following error:
-#   E/launcher - unknown error: Chrome failed to start: crashed
-#   (Driver info: chromedriver=2.26.436382 (70eb799287ce4c2208441fc057053a5b07ceabac),platform=Linux 4.8.12-040812-generic x86_64)
-# echo 'travis_fold:start:test.aio.e2e'
-#
-# # Run e2e tests
-# cd "`dirname $0`/../../angular.io"
-# yarn start &
-# yarn run e2e
-# cd -
-#
-# echo 'travis_fold:end:test.aio.e2e'
+echo 'travis_fold:start:test.aio.e2e'
+
+# Run e2e tests
+cd "`dirname $0`/../../angular.io"
+yarn start &
+yarn run e2e
+cd -
+
+echo 'travis_fold:end:test.aio.e2e'
 
 
 echo 'travis_fold:end:test.aio'
