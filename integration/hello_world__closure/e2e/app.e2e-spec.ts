7cf3e684e8378628827e7537253f61cdfdf2f202
Alex Eagle <alexeagle@google.com>
2017-02-02 07:57:06 -0800

    fix travis xvfb order

--- integration/hello_world__closure/e2e/app.e2e-spec.ts
+++ integration/hello_world__closure/e2e/app.e2e-spec.ts
@@ -3,6 +3,6 @@ import { browser, element, by } from 'protractor';
 describe('Hello world E2E Tests', function () {
   it('should display: Hello world!', function () {
     browser.get('');
-    expect(element(by.css('div')).getText()).toEqual('Hello woraold!');
+    expect(element(by.css('div')).getText()).toEqual('Hello world!');
   });
 });
