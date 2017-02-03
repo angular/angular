96ca52c195e2c3689920e9cb795dda1b3d1c7d4a
Ward Bell <wardbell@hotmail.com>
2017-02-02 23:02:23 -0800

    feat(aio): delete unused kruft

--- angular.io/src/app/app.component.spec.ts
+++ angular.io/src/app/app.component.spec.ts
@@ -1,38 +1,31 @@
 // /* tslint:disable:no-unused-variable */
 
-// import { TestBed, async } from '@angular/core/testing';
-// import { AppComponent } from './app.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+
+import { NavEngine } from './nav-engine/nav-engine.service';
 
 describe('AppComponent', () => {
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [
-  //       AppComponent
-  //     ],
-  //   });
-  //   TestBed.compileComponents();
-  // });
-  it('should work', () => {
-    expect(true).toBe(true);
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ AppModule ],
+      providers: [
+        { provide: NavEngine, useValue: { currentDoc: undefined } }
+      ]
+    });
+    TestBed.compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeDefined();
   });
 });
-
-//   it('should create the app', async(() => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.debugElement.componentInstance;
-//     expect(app).toBeTruthy();
-//   }));
-
-//   it(`should have as title 'app works!'`, async(() => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.debugElement.componentInstance;
-//     expect(app.title).toEqual('app works!');
-//   }));
-
-//   it('should render title in a h1 tag', async(() => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.debugElement.nativeElement;
-//     expect(compiled.querySelector('h1').textContent).toContain('app works!');
-//   }));
-// });
