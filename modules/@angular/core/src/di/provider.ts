414ec25c5784a80bcafc881f16057b89f6ba809d
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2017-02-04 14:41:18 +0100

    docs(core): fix typo (#14299)
    Replace `than` with `then`.
    
    PR Close #14299

--- modules/@angular/core/src/di/provider.ts
+++ modules/@angular/core/src/di/provider.ts
@@ -62,7 +62,7 @@ export interface ValueProvider {
   useValue: any;
 
   /**
-   * If true, than injector returns an array of instances. This is useful to allow multiple
+   * If true, then injector returns an array of instances. This is useful to allow multiple
    * providers spread across many files to provide configuration information to a common token.
    *
    * ### Example
@@ -106,7 +106,7 @@ export interface ClassProvider {
   useClass: Type<any>;
 
   /**
-   * If true, than injector returns an array of instances. This is useful to allow multiple
+   * If true, then injector returns an array of instances. This is useful to allow multiple
    * providers spread across many files to provide configuration information to a common token.
    *
    * ### Example
@@ -144,7 +144,7 @@ export interface ExistingProvider {
   useExisting: any;
 
   /**
-   * If true, than injector returns an array of instances. This is useful to allow multiple
+   * If true, then injector returns an array of instances. This is useful to allow multiple
    * providers spread across many files to provide configuration information to a common token.
    *
    * ### Example
@@ -189,13 +189,13 @@ export interface FactoryProvider {
   useFactory: Function;
 
   /**
-   * A list of `token`s which need to be resolved by the injector. The list of values is than
+   * A list of `token`s which need to be resolved by the injector. The list of values is then
    * used as arguments to the `useFactory` function.
    */
   deps?: any[];
 
   /**
-   * If true, than injector returns an array of instances. This is useful to allow multiple
+   * If true, then injector returns an array of instances. This is useful to allow multiple
    * providers spread across many files to provide configuration information to a common token.
    *
    * ### Example
