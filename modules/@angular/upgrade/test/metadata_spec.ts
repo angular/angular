5bb47db887635fc245deb9550f81c5fa92601c51
George Kalpakas <kalpakas.g@gmail.com>
2017-02-07 20:02:11 +0200

    fix(upgrade): allow non-element selectors for downgraded components (#14291)
    This affects the dynamic version of `upgrade` and makes it more consistent with
    the static version, while removing an artificial limitation.
    
    (This commit backports the fix from 9aafdc7 to 2.4.x.)

--- modules/@angular/upgrade/test/metadata_spec.ts
+++ modules/@angular/upgrade/test/metadata_spec.ts
@@ -13,17 +13,12 @@ import {getComponentInfo, parseFields} from '@angular/upgrade/src/metadata';
 export function main() {
   describe('upgrade metadata', () => {
     it('should extract component selector', () => {
-      expect(getComponentInfo(ElementNameComponent).selector).toEqual('elementNameDashed');
+      expect(getComponentInfo(ElementNameComponent).selector).toBe('element-name-dashed');
     });
 
 
     describe('errors', () => {
       it('should throw on missing selector', () => {
-        expect(() => getComponentInfo(AttributeNameComponent))
-            .toThrowError('Only selectors matching element names are supported, got: [attr-name]');
-      });
-
-      it('should throw on non element names', () => {
         expect(() => getComponentInfo(NoAnnotationComponent))
             .toThrowError('No Directive annotation found on NoAnnotationComponent');
       });
