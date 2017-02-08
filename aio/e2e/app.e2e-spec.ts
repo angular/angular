f722df7a49f5f7c6d0380fba715a47ecf55f38b2
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-02-08 16:03:50 +0200

    test(aio): remove unnecessary hack from e2e tests
    May address #14347.

--- aio/e2e/app.e2e-spec.ts
+++ aio/e2e/app.e2e-spec.ts
@@ -3,34 +3,20 @@ import { SitePage } from './app.po';
 describe('site App', function() {
   let page: SitePage;
 
-  beforeAll(done => {
-    // Hack:  CI has been failing on first test so
-    // buying time by giving the browser a wake-up call.
-    // Todo:  Find and fix the root cause for flakes.
-    new SitePage().navigateTo().then(done);
-  });
-
   beforeEach(() => {
     page = new SitePage();
+    page.navigateTo();
   });
 
   it('should show features text after clicking "Features"', () => {
-    page.navigateTo()
-      .then(() => {
-        return page.featureLink.click();
-      })
-      .then(() => {
-        expect(page.getDocViewerText()).toContain('Progressive web apps');
-      });
+    page.featureLink.click().then(() => {
+      expect(page.getDocViewerText()).toContain('Progressive web apps');
+    });
   });
 
   it('should convert code-example in pipe.html', () => {
-    page.navigateTo()
-      .then(() => {
-        return page.datePipeLink.click();
-      })
-      .then(() => {
-        expect(page.codeExample.count()).toBeGreaterThan(0, 'should have code-example content');
-      });
+    page.datePipeLink.click().then(() => {
+      expect(page.codeExample.count()).toBeGreaterThan(0, 'should have code-example content');
+    });
   });
 });
