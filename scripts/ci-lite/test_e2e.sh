b0914335439ec730b5ab767beaae87603a7d380b
George Kalpakas <kalpakas.g@gmail.com>
2017-02-09 19:08:16 +0200

    build(integration): fix tests (#14371)
    Previously, the `integration/` tests were failing, because `concurrently "foo"`
    does not inherit the `PATH` env var ([more info][1]).
    
    This commit fixes it, by setting the `PATH` env var explicitly:
    `concurrently "PATH=$PATH foo"`.
    
    This commit also includes some minor refactoring of the `integration/` tests scripts:
    
    - Move build-related operations to `ci-lite/build.sh` (for consistency).
    - Use `yarn run ...` instead of `npm run ...` inside package.json scripts.
    - Use global `yarn` (since we are already using it for `aio/`).
    - Fix some `travis_fold` statements.
    
    [1]: https://github.com/kimmobrunfeldt/concurrently/issues/61#issuecomment-252081610

--- scripts/ci-lite/test_e2e.sh
+++ scripts/ci-lite/test_e2e.sh
@@ -7,7 +7,7 @@ if [[ ${TRAVIS} && ${CI_MODE} != "e2e" ]]; then
 fi
 
 
-echo 'travis_fold:start:test.js'
+echo 'travis_fold:start:test.e2e'
 
 # Setup environment
 cd `dirname $0`
@@ -15,26 +15,32 @@ source ./env.sh
 cd ../..
 
 
-echo 'travis_fold:start:test.buildPackages'
-
+echo 'travis_fold:start:test.e2e.buildPackages'
 ./build.sh
+echo 'travis_fold:end:test.e2e.buildPackages'
 
-echo 'travis_fold:end:test.buildPackages'
 
 if [[ ${TRAVIS} ]]; then
+  echo 'travis_fold:start:test.e2e.localChrome'
   sh -e /etc/init.d/xvfb start
+  echo 'travis_fold:end:test.e2e.localChrome'
 fi
 
-./integration/build_rxjs_es6.sh
+
+echo 'travis_fold:start:test.e2e.integration'
 ./integration/run_tests.sh
 #TODO(alexeagle): move offline_compiler_test to integration/
 ./scripts/ci-lite/offline_compiler_test.sh
+echo 'travis_fold:end:test.e2e.integration'
 
+
+echo 'travis_fold:start:test.e2e.apiAndCircularDeps'
 $(npm bin)/gulp public-api:enforce
-
 $(npm bin)/gulp check-cycle
+echo 'travis_fold:end:test.e2e.apiAndCircularDeps'
 
-echo 'travis_fold:start:test.e2e.localChrome'
+
+echo 'travis_fold:start:test.e2e.protractor'
 cd dist/
 $(npm bin)/gulp serve &
 $(npm bin)/gulp serve-examples &
@@ -42,9 +48,10 @@ cd ..
 NODE_PATH=$NODE_PATH:./dist/all $(npm bin)/protractor ./protractor-e2e.conf.js --bundles=true
 NODE_PATH=$NODE_PATH:./dist/all $(npm bin)/protractor ./protractor-examples-e2e.conf.js --bundles=true
 NODE_PATH=$NODE_PATH:./dist/all $(npm bin)/protractor ./protractor-perf.conf.js --bundles=true --dryrun
-echo 'travis_fold:end:test.e2e.localChrome'
+echo 'travis_fold:end:test.e2e.protractor'
 
-echo 'travis_fold:end:test.js'
+echo 'travis_fold:end:test.e2e'
+
 
 if [[ ${TRAVIS} ]]; then
   ./scripts/publish/publish-build-artifacts.sh
