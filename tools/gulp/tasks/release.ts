5e038c3ef227bd2cc775e32185a58b2c1aa81bd5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-02-10 22:41:35 +0100

    build: fix invalid glob to remove specs (#3019)
    * Fixes an invalid glob that is used to cleanup the dist folder before cutting a release.
       Currently all spec.d.ts files will be shipped as well.

--- tools/gulp/tasks/release.ts
+++ tools/gulp/tasks/release.ts
@@ -12,7 +12,7 @@ const argv = minimist(process.argv.slice(3));
 
 /** Removes redundant spec files from the release. TypeScript creates definition files for specs. */
 // TODO(devversion): tsconfig files should share code and don't generate spec files for releases.
-task(':build:release:clean-spec', cleanTask('dist/**/*(-|.)spec.*'));
+task(':build:release:clean-spec', cleanTask('dist/**/*+(-|.)spec.*'));
 
 
 task('build:release', function(done: () => void) {
