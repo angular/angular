fcdde4deca8a3afa81ad7c98c41ddc42fb3b6a03
Chuck Jazdzewski <chuckj@google.com>
2017-02-15 10:18:20 -0800

    feat(tsc-wrapped): `ngc` has an option to produce import bundles (#14509)

--- tools/@angular/tsc-wrapped/index.ts
+++ tools/@angular/tsc-wrapped/index.ts
@@ -10,6 +10,8 @@ export {MetadataWriterHost} from './src/compiler_host';
 export {CodegenExtension, UserError, main} from './src/main';
 
 export {default as AngularCompilerOptions} from './src/options';
+export * from './src/bundler';
 export * from './src/cli_options';
 export * from './src/collector';
+export * from './src/index_writer';
 export * from './src/schema';
