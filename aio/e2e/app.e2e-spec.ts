c4811aba3be4cfed134e2b3bea86e046a013d209
Ward Bell <wardbell@hotmail.com>
2017-02-20 22:28:40 -0800

    test(aio): redefine pipe test as a pending test to confirm code example

--- aio/e2e/app.e2e-spec.ts
+++ aio/e2e/app.e2e-spec.ts
@@ -14,9 +14,5 @@ describe('site App', function() {
     });
   });
 
-  it('should convert code-example in pipe.html', () => {
-    page.datePipeLink.click().then(() => {
-      expect(page.codeExample.count()).toBeGreaterThan(0, 'should have code-example content');
-    });
-  });
+  it('should convert a doc with a code-example');
 });
--- aio/e2e/app.po.ts
+++ aio/e2e/app.po.ts
@@ -3,7 +3,6 @@ import { browser, element, by } from 'protractor';
 export class SitePage {
 
   links = element.all(by.css('md-toolbar a'));
-  datePipeLink = element(by.css('md-toolbar a[aioNavLink="api/common/date-pipe"]'));
   docViewer = element(by.css('aio-doc-viewer'));
   codeExample = element.all(by.css('aio-doc-viewer pre > code'));
   featureLink = element(by.css('md-toolbar a[aioNavLink="features"]'));
