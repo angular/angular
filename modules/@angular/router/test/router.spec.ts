846fea9d03c940b9ecfe943dbb6a8dc484700762
Victor Berchet <victor@suumit.com>
2017-02-20 19:48:43 -0800

    Revert "feat(router): add an option to rerun guards and resolvers when query changes"
    This reverts commit c2e0f71a78368482be4b86a60c773cc9385ad190.

--- modules/@angular/router/test/router.spec.ts
+++ modules/@angular/router/test/router.spec.ts
@@ -115,6 +115,6 @@ function checkResolveData(
 
 function createActivatedRouteSnapshot(cmp: string, extra: any = {}): ActivatedRouteSnapshot {
   return new ActivatedRouteSnapshot(
-      <any>[], {}, <any>null, <any>null, <any>null, <any>null, <any>cmp, <any>{}, <any>null, -1,
+      <any>[], {}, <any>null, <any>null, <any>null, <any>null, <any>cmp, <any>null, <any>null, -1,
       extra.resolve);
 }
