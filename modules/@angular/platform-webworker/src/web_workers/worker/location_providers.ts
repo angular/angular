3c3c06ed859e4a9f4273a6bff8c3d71bdc483c30
Victor Berchet <victor@suumit.com>
2017-02-20 18:37:38 -0800

    fix(router): do not finish bootstrap until all the routes are resolved (#14608)
    Fixes #12162
    closes #14155

--- modules/@angular/platform-webworker/src/web_workers/worker/location_providers.ts
+++ modules/@angular/platform-webworker/src/web_workers/worker/location_providers.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {PlatformLocation} from '@angular/common';
+import {LOCATION_INITIALIZED, PlatformLocation} from '@angular/common';
 import {APP_INITIALIZER, InjectionToken, NgZone} from '@angular/core';
 
 import {WebWorkerPlatformLocation} from './platform_location';
@@ -25,9 +25,18 @@ export const WORKER_APP_LOCATION_PROVIDERS = [
     multi: true,
     deps: [PlatformLocation, NgZone],
   },
+  {
+    provide: LOCATION_INITIALIZED,
+    useFactory: locationInitialized,
+    deps: [PlatformLocation],
+  },
 ];
 
-function appInitFnFactory(platformLocation: WebWorkerPlatformLocation, zone: NgZone): () =>
+export function locationInitialized(platformLocation: WebWorkerPlatformLocation) {
+  return platformLocation.initialized;
+}
+
+export function appInitFnFactory(platformLocation: WebWorkerPlatformLocation, zone: NgZone): () =>
     Promise<boolean> {
   return () => zone.runGuarded(() => platformLocation.init());
 }
