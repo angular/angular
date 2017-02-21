cde0eeef23ec72b6a57b9309fb729d9fd57fddf0
Peter Bacon Darwin <pete@bacondarwin.com>
2017-02-21 13:04:30 +0000

    fix(aio): remove unused `getExampleFilename` service

--- aio/transforms/examples-package/services/getExampleFilename.js
+++
@@ -1,9 +0,0 @@
-module.exports = function getExampleFilename() {
-
-  function getExampleFilenameImpl(relativePath) {
-    return getExampleFilenameImpl.examplesFolder + relativePath;
-  }
-
-  getExampleFilenameImpl.examplesFolder = '@angular/examples/';
-  return getExampleFilenameImpl;
-};
