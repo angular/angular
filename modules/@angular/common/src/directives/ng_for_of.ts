a23634dfd0d0cb16c4e097c890b6d71bfc66d458
Chuck Jazdzewski <chuckj@google.com>
2017-02-23 17:23:56 -0800

    fix(common): do not reference deprecated classes in providers (#14523) (#14523)
    References to `NgFor` are now an alias for `NgForOf` instead of a
    derived class.
    
    Fixes #14521

--- modules/@angular/common/src/directives/ng_for_of.ts
+++ modules/@angular/common/src/directives/ng_for_of.ts
@@ -85,12 +85,8 @@ export class NgForOfRow<T> {
  *
  * @stable
  */
-@Directive({
-  selector: '[ngFor][ngForOf]',
-  providers: [{provide: forwardRef(() => NgFor), useExisting: forwardRef(() => NgForOf)}]
-})
-export class NgForOf<T> implements DoCheck,
-    OnChanges {
+@Directive({selector: '[ngFor][ngForOf]'})
+export class NgForOf<T> implements DoCheck, OnChanges {
   @Input() ngForOf: NgIterable<T>;
   @Input()
   set ngForTrackBy(fn: TrackByFunction<T>) {
@@ -191,66 +187,11 @@ class RecordViewTuple<T> {
 }
 
 /**
- * The `NgFor` directive instantiates a template once per item from an iterable. The context
- * for each instantiated template inherits from the outer context with the given loop variable
- * set to the current item from the iterable.
- *
- * ### Local Variables
- *
- * `NgFor` provides several exported values that can be aliased to local variables:
- *
- * * `index` will be set to the current loop iteration for each template context.
- * * `first` will be set to a boolean value indicating whether the item is the first one in the
- *   iteration.
- * * `last` will be set to a boolean value indicating whether the item is the last one in the
- *   iteration.
- * * `even` will be set to a boolean value indicating whether this item has an even index.
- * * `odd` will be set to a boolean value indicating whether this item has an odd index.
- *
- * ### Change Propagation
- *
- * When the contents of the iterator changes, `NgFor` makes the corresponding changes to the DOM:
- *
- * * When an item is added, a new instance of the template is added to the DOM.
- * * When an item is removed, its template instance is removed from the DOM.
- * * When items are reordered, their respective templates are reordered in the DOM.
- * * Otherwise, the DOM element for that item will remain the same.
- *
- * Angular uses object identity to track insertions and deletions within the iterator and reproduce
- * those changes in the DOM. This has important implications for animations and any stateful
- * controls (such as `<input>` elements which accept user input) that are present. Inserted rows can
- * be animated in, deleted rows can be animated out, and unchanged rows retain any unsaved state
- * such as user input.
- *
- * It is possible for the identities of elements in the iterator to change while the data does not.
- * This can happen, for example, if the iterator produced from an RPC to the server, and that
- * RPC is re-run. Even if the data hasn't changed, the second response will produce objects with
- * different identities, and Angular will tear down the entire DOM and rebuild it (as if all old
- * elements were deleted and all new elements inserted). This is an expensive operation and should
- * be avoided if possible.
- *
- * To customize the default tracking algorithm, `NgFor` supports `trackBy` option.
- * `trackBy` takes a function which has two arguments: `index` and `item`.
- * If `trackBy` is given, Angular tracks changes by the return value of the function.
- *
- * ### Syntax
- *
- * - `<li *ngFor="let item of items; let i = index; trackBy: trackByFn">...</li>`
- * - `<li template="ngFor let item of items; let i = index; trackBy: trackByFn">...</li>`
- *
- * With `<template>` element:
- *
- * ```
- * <template ngFor let-item [ngForOf]="items" let-i="index" [ngForTrackBy]="trackByFn">
- *   <li>...</li>
- * </template>
- * ```
- *
- * ### Example
- *
- * See a [live demo](http://plnkr.co/edit/KVuXxDp0qinGDyo307QW?p=preview) for a more detailed
- * example.
- *
- * @deprecated v4.0.0 - Use `NgForOf<T>` instead.
+ * @deprecated from v4.0.0 - Use NgForOf<any> instead.
  */
-export class NgFor extends NgForOf<any> {}
+export type NgFor = NgForOf<any>;
+
+/**
+ * @deprecated from v4.0.0 - Use NgForOf instead.
+ */
+export const NgFor = NgForOf;
