ced1a52595eaf3c6f95387fa4d2a1386ca0d2613
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-02-28 21:10:46 +0200

    feat(aio): verify uploaded builds based on JWT from Travis

--- aio/aio-builds-setup/dockerbuild/scripts-js/lib/upload-server/index.ts
+++ aio/aio-builds-setup/dockerbuild/scripts-js/lib/upload-server/index.ts
@@ -2,15 +2,16 @@
 process.setuid('www-data');
 
 // Imports
-import {GithubPullRequests} from '../common/github-pull-requests';
 import {getEnvVar} from '../common/utils';
-import {CreatedBuildEvent} from './build-events';
 import {uploadServerFactory} from './upload-server-factory';
 
 // Constants
 const AIO_BUILDS_DIR = getEnvVar('AIO_BUILDS_DIR');
-const AIO_GITHUB_TOKEN = getEnvVar('AIO_GITHUB_TOKEN', true);
-const AIO_REPO_SLUG = getEnvVar('AIO_REPO_SLUG', true);
+const AIO_GITHUB_ORGANIZATION = getEnvVar('AIO_GITHUB_ORGANIZATION');
+const AIO_GITHUB_TEAM_SLUGS = getEnvVar('AIO_GITHUB_TEAM_SLUGS');
+const AIO_GITHUB_TOKEN = getEnvVar('AIO_GITHUB_TOKEN');
+const AIO_PREVIEW_DEPLOYMENT_TOKEN = getEnvVar('AIO_PREVIEW_DEPLOYMENT_TOKEN');
+const AIO_REPO_SLUG = getEnvVar('AIO_REPO_SLUG');
 const AIO_UPLOAD_HOSTNAME = getEnvVar('AIO_UPLOAD_HOSTNAME');
 const AIO_UPLOAD_PORT = +getEnvVar('AIO_UPLOAD_PORT');
 
@@ -20,23 +21,13 @@ _main();
 // Functions
 function _main() {
   uploadServerFactory.
-    create(AIO_BUILDS_DIR).
-    on(CreatedBuildEvent.type, createOnBuildCreatedHanlder()).
+    create({
+      buildsDir: AIO_BUILDS_DIR,
+      githubOrganization: AIO_GITHUB_ORGANIZATION,
+      githubTeamSlugs: AIO_GITHUB_TEAM_SLUGS.split(','),
+      githubToken: AIO_GITHUB_TOKEN,
+      repoSlug: AIO_REPO_SLUG,
+      secret: AIO_PREVIEW_DEPLOYMENT_TOKEN,
+    }).
     listen(AIO_UPLOAD_PORT, AIO_UPLOAD_HOSTNAME);
-}
-
-function createOnBuildCreatedHanlder() {
-  if (!AIO_REPO_SLUG) {
-    console.warn('No repo specified. Preview links will not be posted on PRs.');
-    return () => null;
-  }
-
-  const githubPullRequests = new GithubPullRequests(AIO_REPO_SLUG, AIO_GITHUB_TOKEN);
-
-  return ({pr, sha}: CreatedBuildEvent) => {
-    const body = `The angular.io preview for ${sha.slice(0, 7)} is available [here][1].\n\n` +
-                 `[1]: https://pr${pr}-${sha}.ngbuilds.io/`;
-
-    githubPullRequests.addComment(pr, body);
-  };
 }
