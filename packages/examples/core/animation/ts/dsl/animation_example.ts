8925ff7db81e42c043ab9e0ffcb3e62ee2708a6b
Matias Niemelä <matias@yearofmoo.com>
2017-02-28 17:49:37 -0800

    refactor(platform-browser): move platform-browser/animations to animations/browser

--- packages/examples/core/animation/ts/dsl/animation_example.ts
+++ packages/examples/core/animation/ts/dsl/animation_example.ts
@@ -7,7 +7,8 @@
  */
 
 // #docregion Component
-import {Component, NgModule, animate, state, style, transition, trigger} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {Component, NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @Component({
