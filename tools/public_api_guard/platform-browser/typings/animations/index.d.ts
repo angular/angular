0aad27026743b5ce3ff12a5555e69c88291f88eb
Matias Niemelä <matias@yearofmoo.com>
2017-02-28 17:49:37 -0800

    refactor(platform-browser): move platform-browser/animations to animations/browser (#15130)
    Closes: #15130

--- tools/public_api_guard/platform-browser/typings/animations/index.d.ts
+++ tools/public_api_guard/platform-browser/typings/animations/index.d.ts
@@ -1,12 +1,4 @@
 /** @experimental */
-export declare abstract class AnimationDriver {
-    abstract animate(element: any, keyframes: {
-        [key: string]: string | number;
-    }[], duration: number, delay: number, easing: string, previousPlayers?: any[]): any;
-    static NOOP: AnimationDriver;
-}
-
-/** @experimental */
 export declare class BrowserAnimationsModule {
 }
 
