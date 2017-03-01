0633d3fed3b9d11b62fa6181413450fd042643ef
Peter Bacon Darwin <pete@bacondarwin.com>
2017-03-01 22:43:14 +0000

    test(aio): fix up links in app e2e test

--- aio/e2e/app.po.ts
+++ aio/e2e/app.po.ts
@@ -5,7 +5,7 @@ export class SitePage {
   links = element.all(by.css('md-toolbar a'));
   docViewer = element(by.css('aio-doc-viewer'));
   codeExample = element.all(by.css('aio-doc-viewer pre > code'));
-  featureLink = element(by.css('md-toolbar a[aioNavLink="features"]'));
+  featureLink = element(by.css('md-toolbar a[href="features"]'));
 
   navigateTo() {
     return browser.get('/');
