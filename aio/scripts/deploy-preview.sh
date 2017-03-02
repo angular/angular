875a7c6631a8a1b34b8c32b126a98ea2bd2bd063
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-03-03 01:58:31 +0200

    ci(aio): fail the build if preview deployment fails

--- aio/scripts/deploy-preview.sh
+++ aio/scripts/deploy-preview.sh
@@ -6,10 +6,26 @@ set -eux -o pipefail
 INPUT_DIR=dist/
 OUTPUT_FILE=/tmp/snapshot.tar.gz
 AIO_BUILDS_HOST=https://ngbuilds.io
+UPLOAD_URL=$AIO_BUILDS_HOST/create-build/$TRAVIS_PULL_REQUEST/$TRAVIS_PULL_REQUEST_SHA
 
 cd "`dirname $0`/.."
+
 yarn run build -- --prod
 tar --create --gzip --directory "$INPUT_DIR" --file "$OUTPUT_FILE" .
-curl -iLX POST --header "Authorization: Token $NGBUILDS_IO_KEY" --data-binary "@$OUTPUT_FILE" \
-  "$AIO_BUILDS_HOST/create-build/$TRAVIS_PULL_REQUEST/$TRAVIS_COMMIT"
+
+exec 3>&1
+httpCode=$(
+  curl --include --location --request POST --silent --write-out "\nHTTP_CODE: %{http_code}\n" \
+       --header "Authorization: Token $NGBUILDS_IO_KEY" --data-binary "@$OUTPUT_FILE" "$UPLOAD_URL" \
+  | sed 's/\r\n/\n/' \
+  | tee /dev/fd/3 \
+  | tail -1 \
+  | sed 's/HTTP_CODE: //'
+)
+
+# Exit with an error if the request failed
+if [ $httpCode -lt 200 ] || [ $httpCode -ge 400 ]; then
+  exit 1
+fi
+
 cd -
