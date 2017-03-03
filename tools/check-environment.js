00fdcf4e58f95ce252e79151088c26a3bcecd937
Peter Bacon Darwin <pete@bacondarwin.com>
2017-03-03 11:17:19 +0000

    build: allow check-environment.js to be run directly

--- tools/check-environment.js
+++ tools/check-environment.js
@@ -53,6 +53,17 @@ try {
   printWarning(issues);
 }
 
+if (require.main === module) {
+  // we are running this script directly so just run checkEnvironment against the main angular
+  // package.json
+  var engines = require(__dirname + '/../package.json').engines;
+  checkEnvironment({
+    requiredNodeVersion: engines.node,
+    requiredNpmVersion: engines.npm,
+    requiredYarnVersion: engines.yarn
+  });
+}
+
 function checkEnvironment(reqs) {
   exec('npm --version', function(npmErr, npmStdout) {
     exec('yarn --version', function(yarnErr, yarnStdout) {
