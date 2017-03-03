adf22bea7fd18004cb36341ebeda9da91f4052e1
Ward Bell <wardbell@hotmail.com>
2017-03-03 02:50:19 -0800

    feat(aio): add API list page

--- aio/src/app/embedded/index.ts
+++ aio/src/app/embedded/index.ts
@@ -1,9 +1,10 @@
+import { ApiListComponent } from './api/api-list.component';
 import { CodeExampleComponent } from './code-example.component';
 import { DocTitleComponent } from './doc-title.component';
 
 /** Components that can be embedded in docs such as CodeExampleComponent, LiveExampleComponent,... */
 export const embeddedComponents: any[] = [
-  CodeExampleComponent, DocTitleComponent
+  ApiListComponent, CodeExampleComponent, DocTitleComponent
 ];
 
 /** Injectable class w/ property returning components that can be embedded in docs */
