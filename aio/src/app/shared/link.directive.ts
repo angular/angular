4908c6e6b0eac08d371e0164fa42ab3769539992
Peter Bacon Darwin <pete@bacondarwin.com>
2017-03-05 19:46:59 +0000

    fix(aio): correctly intercept links with LinkDirective

--- aio/src/app/shared/link.directive.ts
+++ aio/src/app/shared/link.directive.ts
@@ -1,10 +1,10 @@
-import { Directive, HostListener, HostBinding, Input } from '@angular/core';
+import { Directive, HostListener, HostBinding, Input, OnChanges } from '@angular/core';
 import { LocationService } from 'app/shared/location.service';
 @Directive({
   /* tslint:disable-next-line:directive-selector */
   selector: 'a[href]'
 })
-export class LinkDirective {
+export class LinkDirective implements OnChanges {
 
   // We need both these decorators to ensure that we can access
   // the href programmatically, and that it appears as a real
@@ -13,11 +13,27 @@ export class LinkDirective {
   @HostBinding()
   href: string;
 
+  @HostBinding()
+  target: string;
+
   @HostListener('click', ['$event'])
   onClick($event) {
-    this.location.go(this.href);
-    return false;
+    if (this.isAbsolute(this.href)) {
+      return true;
+    } else {
+      this.location.go(this.href);
+      return false;
+    }
+  }
+
+  private isAbsolute(url) {
+    return /^[a-z]+:\/\/|\/\//i.test(url);
   }
 
   constructor(private location: LocationService) { }
+
+  ngOnChanges() {
+    this.target = this.isAbsolute(this.href) ? '_blank' : '_self';
+  }
+
 }
