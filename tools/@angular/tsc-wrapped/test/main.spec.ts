495b3f54b8e75d2e6d7fbdf5c5febb887c73f9c3
Lucas Sloan <lucassloan@google.com>
2017-03-15 16:39:47 -0700

    build(tsc-wrapped): update tsickle to version 0.21.6
    Update tsickle to version 0.21.6 which fixes a bug where input source maps which specified filenames differently than the names supplied to tsc didn't get composed with tsc's source maps.  Also adds a test that the bug was fixed.

--- tools/@angular/tsc-wrapped/test/main.spec.ts
+++ tools/@angular/tsc-wrapped/test/main.spec.ts
@@ -284,8 +284,45 @@ describe('tsc-wrapped', () => {
     // Provide a file called test.ts that has an inline source map
     // which says that it was transpiled from a file other_test.ts
     // with exactly the same content.
+    const inputSourceMap =
+        `{"version":3,"sources":["other_test.ts"],"names":[],"mappings":"AAAA,MAAM,EAAE,EAAE,CAAC","file":"../test.ts","sourceRoot":""}`;
+    const encodedSourceMap = new Buffer(inputSourceMap, 'utf8').toString('base64');
     write('test.ts', `const x = 3;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm90aGVyX3Rlc3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsTUFBTSxFQUFFLEVBQUUsQ0FBQyIsImZpbGUiOiIuLi90ZXN0LnRzIiwic291cmNlUm9vdCI6IiJ9`);
+//# sourceMappingURL=data:application/json;base64,${encodedSourceMap}`);
+
+    main(basePath, {basePath})
+        .then(() => {
+          const out = readOut('js.map');
+          expect(out).toContain('"sources":["other_test.ts"]');
+          done();
+        })
+        .catch(e => done.fail(e));
+  });
+
+  it(`should accept input source maps that don't match the file name`, (done) => {
+    write('tsconfig.json', `{
+      "compilerOptions": {
+        "experimentalDecorators": true,
+        "types": [],
+        "outDir": "built",
+        "declaration": true,
+        "moduleResolution": "node",
+        "target": "es2015",
+        "sourceMap": true
+      },
+      "angularCompilerOptions": {
+        "annotateForClosureCompiler": true
+      },
+      "files": ["test.ts"]
+    }`);
+    // Provide a file called test.ts that has an inline source map
+    // which says that it was transpiled from a file other_test.ts
+    // with exactly the same content.
+    const inputSourceMap =
+        `{"version":3,"sources":["other_test.ts"],"names":[],"mappings":"AAAA,MAAM,EAAE,EAAE,CAAC","file":"test.ts","sourceRoot":""}`;
+    const encodedSourceMap = new Buffer(inputSourceMap, 'utf8').toString('base64');
+    write('test.ts', `const x = 3;
+//# sourceMappingURL=data:application/json;base64,${encodedSourceMap}`);
 
     main(basePath, {basePath})
         .then(() => {
