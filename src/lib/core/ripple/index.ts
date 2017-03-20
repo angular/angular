c524438b70510af0a27fe4e53c1970b8a42f0705
mertdeg2 <mertdeg@google.com>
2017-03-20 15:56:13 -0700

    chore: export ripple timing constants (#3650)
    Exports RIPPLE_FADE_IN_DURATION  and RIPPLE_FADE_OUT_DURATION so that they can be used by users with `fakeAsync` in unit tests

--- src/lib/core/ripple/index.ts
+++ src/lib/core/ripple/index.ts
@@ -6,7 +6,7 @@ import {SCROLL_DISPATCHER_PROVIDER} from '../overlay/scroll/scroll-dispatcher';
 
 export {MdRipple, RippleGlobalOptions, MD_RIPPLE_GLOBAL_OPTIONS} from './ripple';
 export {RippleRef, RippleState} from './ripple-ref';
-export {RippleConfig} from './ripple-renderer';
+export {RippleConfig, RIPPLE_FADE_IN_DURATION, RIPPLE_FADE_OUT_DURATION} from './ripple-renderer';
 
 @NgModule({
   imports: [CompatibilityModule],
