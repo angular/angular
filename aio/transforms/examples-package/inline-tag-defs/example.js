8b4edcc7adc73957f08b546b84b7b3acefd180ec
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-22 20:24:40 +0000

    build(aio): output `{@example}` tags as `<code-example>` elements (#15382)

--- aio/transforms/examples-package/inline-tag-defs/example.js
+++ aio/transforms/examples-package/inline-tag-defs/example.js
@@ -28,40 +28,48 @@ module.exports = function exampleInlineTagDef(
       var unnamedArgs = tagArgs._;
       var relativePath = unnamedArgs[0];
       var regionName = tagArgs.region || (unnamedArgs.length > 1 ? unnamedArgs[1] : '');
-      var title = tagArgs.title || (unnamedArgs.length > 2 ? unnamedArgs[2] : null);
+      if (regionName === '\'\'') regionName = '';
+      var title = tagArgs.title || (unnamedArgs.length > 2 ? unnamedArgs.slice(2).join(' ') : null);
+      var linenums = tagArgs.linenums;
       var stylePattern = tagArgs.stylePattern;  // TODO: not yet implemented here
 
-      // Find the example in the folders
-      var exampleFile;
-      // Try an "annotated" version first
-      EXAMPLES_FOLDERS.some(
-          EXAMPLES_FOLDER => { return exampleFile = exampleMap[EXAMPLES_FOLDER][relativePath + '.annotated']; });
+      const sourceCode = getExampleRegion();
 
-      // If no annotated version is available then try the actual file
-      if (!exampleFile) {
-        EXAMPLES_FOLDERS.some(
-            EXAMPLES_FOLDER => { return exampleFile = exampleMap[EXAMPLES_FOLDER][relativePath]; });
+      const attributes = [];
+      if (title) attributes.push(` title="${title}"`);
+      if (linenums !== undefined) attributes.push(` linenums="${linenums}"`);
+
+      return '<code-example' + attributes.join('') + '>\n' + sourceCode + '\n</code-example>';
+
+
+      function getExampleRegion() {
+        // Find the example in the folders
+        var exampleFile;
+        // Try an "annotated" version first
+        EXAMPLES_FOLDERS.some(EXAMPLES_FOLDER => { return exampleFile = exampleMap[EXAMPLES_FOLDER][relativePath + '.annotated']; });
+
+        // If no annotated version is available then try the actual file
+        if (!exampleFile) {
+          EXAMPLES_FOLDERS.some(EXAMPLES_FOLDER => { return exampleFile = exampleMap[EXAMPLES_FOLDER][relativePath]; });
+        }
+
+        // If still no file then we error
+        if (!exampleFile) {
+          log.error(createDocMessage('Missing example file... relativePath: "' + relativePath + '".', doc));
+          log.error('Example files can be found in: ' + EXAMPLES_FOLDERS.join(', '));
+          return '';
+        }
+
+        var sourceCodeDoc = exampleFile.regions[regionName];
+        if (!sourceCodeDoc) {
+          log.error(createDocMessage('Missing example region... relativePath: "' + relativePath + '", region: "' + regionName + '".', doc));
+          log.error('Regions available are:', Object.keys[exampleFile.regions]);
+          return '';
+        }
+
+        return sourceCodeDoc.renderedContent;
       }
-
-      // If still no file then we error
-      if (!exampleFile) {
-        log.error(
-            createDocMessage('Missing example file... relativePath: "' + relativePath + '".', doc));
-        log.error('Example files can be found in: ' + EXAMPLES_FOLDERS.join(', '));
-        return '';
-      }
-
-      var sourceCode = exampleFile.regions[regionName];
-      if (!sourceCode) {
-        log.error(createDocMessage(
-            'Missing example region... relativePath: "' + relativePath + '", region: "' +
-                regionName + '".',
-            doc));
-        log.error('Regions available are:', Object.keys[exampleFile.regions]);
-        return '';
-      }
-
-      return sourceCode.renderedContent;
     }
   };
 };
+
