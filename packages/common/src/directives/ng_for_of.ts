0047aa2a2ead88453173593fedd0f2e5f036e830
Josep Sayol <jsayol@users.noreply.github.com>
2017-03-23 01:18:22 +0100

    docs(common): update ngFor docs to new `as` syntax (#15166)

--- packages/common/src/directives/ng_for_of.ts
+++ packages/common/src/directives/ng_for_of.ts
@@ -45,7 +45,7 @@ export class NgForOfContext<T> {
  * - `odd: boolean`: True when the item has an odd index in the iterable.
  *
  * ```
- * <li *ngFor="let user of userObservable | async as users; indexes as i; first as isFirst">
+ * <li *ngFor="let user of userObservable | async as users; index as i; first as isFirst">
  *    {{i}}/{{users.length}}. {{user}} <span *ngIf="isFirst">default</span>
  * </li>
  * ```
@@ -78,8 +78,8 @@ export class NgForOfContext<T> {
  *
  * ### Syntax
  *
- * - `<li *ngFor="let item of items; let i = index; trackBy: trackByFn">...</li>`
- * - `<li template="ngFor let item of items; let i = index; trackBy: trackByFn">...</li>`
+ * - `<li *ngFor="let item of items; index as i; trackBy: trackByFn">...</li>`
+ * - `<li template="ngFor let item of items; index as i; trackBy: trackByFn">...</li>`
  *
  * With `<ng-template>` element:
  *
