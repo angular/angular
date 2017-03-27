73c9b74805856cecf951f02edb833c16de24ea6d
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-03-27 19:13:50 +0300

    build(aio): update project config for @angular/cli v1.0.0
    This is a follow-up to 487a0e1. The changes are based on [this wiki entry][1].
    
    [1]: https://github.com/angular/angular-cli/wiki/stories-1.0-update.

--- aio/karma.conf.js
+++ aio/karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -28,7 +29,6 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     angularCli: {
-      config: './.angular-cli.json',
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
