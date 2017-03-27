15662efec4f54c15d89c98393d489a058922d3bc
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-03-27 19:13:50 +0300

    build(aio): update project config for @angular/cli v1.0.0
    This is a follow-up to 487a0e1. The changes are based on [this wiki entry][1].
    
    [1]: https://github.com/angular/angular-cli/wiki/stories-1.0-update.

--- aio/protractor.conf.js
+++ aio/protractor.conf.js
@@ -1,7 +1,6 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-/*global jasmine */
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
@@ -26,7 +25,7 @@ exports.config = {
   },
   beforeLaunch: function() {
     require('ts-node').register({
-      project: 'e2e'
+      project: 'e2e/tsconfig.e2e.json'
     });
   },
   onPrepare() {
