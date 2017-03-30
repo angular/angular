54de70683f37d49628252b3140f21c54b77d7d5c
Stefanie Fluin <sjtrimble@gmail.com>
2017-03-30 16:59:03 -0700

    feat(aio): add footer background

--- aio/content/examples/cb-i18n/src/app/app.component.ts
+++ aio/content/examples/cb-i18n/src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   wolves = 0;
   gender = 'f';
   fly = true;
-  logo = 'https://angular.io/resources/images/logos/angular2/angular.png';
+  logo = 'https://angular.io/resources/images/logos/angular/angular.png';
   inc(i: number) {
     this.wolves = Math.min(5, Math.max(0, this.wolves + i));
   }
