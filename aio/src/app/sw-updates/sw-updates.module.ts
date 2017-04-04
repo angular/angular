2d45dc9924156797135c4e5fd56e93f0d88eb3b0
Ward Bell <wardbell@hotmail.com>
2017-04-04 14:24:24 -0600

    fix(aio): v2 - when browser lacks service worker, disable sw feature

--- aio/src/app/sw-updates/sw-updates.module.ts
+++ aio/src/app/sw-updates/sw-updates.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { MdSnackBarModule } from '@angular/material';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { noopNgServiceWorkerProviders } from './noop-ng-service-worker';
 import { SwUpdateNotificationsService } from './sw-update-notifications.service';
 import { SwUpdatesService } from './sw-updates.service';
-
 
 @NgModule({
   imports: [
@@ -12,6 +12,7 @@ import { SwUpdatesService } from './sw-updates.service';
     ServiceWorkerModule
   ],
   providers: [
+    noopNgServiceWorkerProviders,
     SwUpdateNotificationsService,
     SwUpdatesService
   ]
