59c62893aca789918971b468fac622b8e53de8ec
Mac <Harlow44@hotmail.com>
2017-04-04 17:49:44 -0700

    docs(dialog): document viewContainerRef config option (#3825)

--- src/lib/dialog/dialog-config.ts
+++ src/lib/dialog/dialog-config.ts
@@ -15,6 +15,13 @@ export interface DialogPosition {
  * Configuration for opening a modal dialog with the MdDialog service.
  */
 export class MdDialogConfig {
+
+  /**
+   * Where the attached component should live in Angular's *logical* component tree.
+   * This affects what is available for injection and the change detection order for the
+   * component instantiated inside of the dialog. This does not affect where the dialog
+   * content will be rendered.
+   */
   viewContainerRef?: ViewContainerRef;
 
   /** The ARIA role of the dialog element. */
