5ef06f18c834bbfeb01ffd5bc9d277ddb23cbd07
dherges <david@spektrakel.de>
2017-04-05 08:05:23 +0200

    docs(core): EventEmitter docs for isAsync defaults
    - solves #15758

--- packages/core/src/event_emitter.ts
+++ packages/core/src/event_emitter.ts
@@ -64,8 +64,11 @@ export class EventEmitter<T> extends Subject<T> {
   __isAsync: boolean;
 
   /**
-   * Creates an instance of [EventEmitter], which depending on [isAsync],
+   * Creates an instance of {@link EventEmitter}, which depending on `isAsync`,
    * delivers events synchronously or asynchronously.
+   *
+   * @param isAsync By default, events are delivered synchronously (default value: `false`).
+   * Set to `true` for asynchronous event delivery.
    */
   constructor(isAsync: boolean = false) {
     super();
