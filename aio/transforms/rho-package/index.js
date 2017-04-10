8bb0fbabc102880f0b6abad44d630110300f1435
Peter Bacon Darwin <pete@bacondarwin.com>
2017-04-10 17:21:03 +0100

    build(aio): remove unused Rho package

--- aio/transforms/rho-package/index.js
+++
@@ -1,9 +0,0 @@
-var Package = require('dgeni').Package;
-
-/**
- * @dgPackage rho
- * @description Overrides the renderMarkdown service with an implementation based on Rho
- */
-module.exports = new Package('rho', ['nunjucks'])
-
-                     .factory(require('./services/renderMarkdown'));
