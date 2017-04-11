03093fe7f3899fd92c6ce642866c441d0c533901
Ward Bell <wardbell@hotmail.com>
2017-04-11 16:08:53 -0700

    test(aio): add sidenav tests and refactor related tests

--- aio/src/testing/location.service.ts
+++ aio/src/testing/location.service.ts
@@ -5,9 +5,11 @@ export class MockLocationService {
   currentUrl = this.urlSubject.asObservable();
   search = jasmine.createSpy('search').and.returnValue({});
   setSearch = jasmine.createSpy('setSearch');
-  go = jasmine.createSpy('Location.go');
+  go = jasmine.createSpy('Location.go').and
+              .callFake((url: string) => this.urlSubject.next(url));
   handleAnchorClick = jasmine.createSpy('Location.handleAnchorClick')
       .and.returnValue(false); // prevent click from causing a browser navigation
+
   constructor(private initialUrl) {}
 }
 
