d3e7c177bbfea6588c82c2310278c01a08a98ce9
Isaac Brown <irbrown89@gmail.com>
2017-04-14 09:03:57 -0700

    fix(packaging): increased buffer size (#15840)

--- tools/gulp-tasks/build.js
+++ tools/gulp-tasks/build.js
@@ -2,5 +2,6 @@
 module.exports = (gulp) => (done) => {
   const path = require('path');
   const childProcess = require('child_process');
-  childProcess.exec(path.join(__dirname, '../../build.sh'), done);
+  // increase maxbuffer to address out of memory exception when running certain tasks
+  childProcess.exec(path.join(__dirname, '../../build.sh'), {maxBuffer: 300 * 1024}, done);
 };
