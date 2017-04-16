1fff3795eb8c5895ea16aeee8848916e42c2b18b
Peter Bacon Darwin <pete@bacondarwin.com>
2017-04-16 22:13:09 +0100

    build(aio): extract the title from the content if necessary
    Documents can specify their title via the `title` or `name` jsdoc tags.
    
    This change adds that, if neither are provided, the first `<h1>` element
    is removed from the `renderedContent` and used for the title.
    
    If there is still no title then it is set to the empty string and a warning
    is logged.

--- aio/tools/transforms/angular.io-package/processors/convertToJson.js
+++ aio/tools/transforms/angular.io-package/processors/convertToJson.js
@@ -1,4 +1,4 @@
-module.exports = function convertToJsonProcessor() {
+module.exports = function convertToJsonProcessor(log, createDocMessage) {
 
   return {
     $runAfter: ['checkUnbalancedBackTicks'],
@@ -8,13 +8,27 @@ module.exports = function convertToJsonProcessor() {
       const docTypes = this.docTypes;
       docs.forEach((doc) => {
         if (docTypes.indexOf(doc.docType) !== -1) {
-          const output = {
-            title: doc.title || doc.name,
-            contents: doc.renderedContent
-          };
-          doc.renderedContent = JSON.stringify(output, null, 2);
+          let title = doc.title || doc.name;
+          let contents = doc.renderedContent || '';
+
+          // If there is no title then try to extract it from the first h1 in the renderedContent
+          if (title === undefined) {
+            const match = /<h1[^>]*>(.+?)<\/h1>/.exec(contents);
+            if (match) {
+              title = match[1];
+            }
+          }
+
+          // If there is still no title then log a warning
+          if (title === undefined) {
+            title = '';
+            log.warn(createDocMessage('Title property expected', doc));
+          }
+
+          doc.renderedContent = JSON.stringify({ title, contents }, null, 2);
         }
       });
     }
   };
+
 };
