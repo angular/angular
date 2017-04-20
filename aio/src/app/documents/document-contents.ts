554eca23568650163204c0b9422061da53c53e79
Peter Bacon Darwin <pete@bacondarwin.com>
2017-04-20 14:16:36 +0100

    refactor(aio): rename DocumentContents `url` to `id` and tidy up
    Dgeni is now providing the `id` for all the documents to be viewed. So we
    no longer need to add this to the DocumentContents object.
    
    There are some notable changes in the refactoring:
    
    `DocumentService`:
    
    * The id of the document to render is now obtained from `LocationService.path()`.
    * The `getFileNotFoundDoc` and `getErrorDoc` methods have been extracted from
    the `fetchDocument` method.
    
    `AppComponent`:
    
    * the `pageId` is now computed in a separate `setPageId` method.
    
    `AppComponen` spec file:
    
    * The `TestHttp` has had the hard coded documents removed and replaced with
    a function that will generate docs as needed.

--- aio/src/app/documents/document-contents.ts
+++ aio/src/app/documents/document-contents.ts
@@ -1,5 +1,8 @@
 export interface DocumentContents {
-  url: string;
+  /** The unique identifier for this document */
+  id: string;
+  /** The string to display in the browser tab when this document is being viewed */
   title: string;
+  /** The HTML to display in the doc viewer */
   contents: string;
 }
