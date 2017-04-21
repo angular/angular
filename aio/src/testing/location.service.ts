9c1318d7315027ebd67d112bf827a08a64ca7e26
George Kalpakas <kalpakas.g@gmail.com>
2017-04-21 23:35:40 +0300

    fix(aio): strip leading slashes from path (and improve DRY-ness) (#16238)
    Previously, the path returned by `LocationService.path()` preserved leading
    slashes, which resulted in requests with consequtive slashes in the URL. Such
    requests would fail (with a 404) on staging.
    
    This commit fixes it, by removing leading slashes from the path. It also
    refactors `LocationService` a bit, converting path to an observable,
    `currentPath` (similar to `currentUrl`), and applies certain clean-ups (e.g.
    stripping slashes, query, hash) in one place, which simplifies consumption.
    
    Closes #16230

--- aio/src/testing/location.service.ts
+++ aio/src/testing/location.service.ts
@@ -3,12 +3,11 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 export class MockLocationService {
   urlSubject = new BehaviorSubject<string>(this.initialUrl);
   currentUrl = this.urlSubject.asObservable();
+  currentPath = this.currentUrl.map(url => url.match(/[^?#]*/)[0]);
   search = jasmine.createSpy('search').and.returnValue({});
   setSearch = jasmine.createSpy('setSearch');
   go = jasmine.createSpy('Location.go').and
               .callFake((url: string) => this.urlSubject.next(url));
-  path = jasmine.createSpy('Location.path').and
-              .callFake(() => this.urlSubject.getValue().split('?')[0]);
   handleAnchorClick = jasmine.createSpy('Location.handleAnchorClick')
       .and.returnValue(false); // prevent click from causing a browser navigation
 
