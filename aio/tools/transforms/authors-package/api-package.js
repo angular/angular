c889fb1ef57a0b366d5f1313a87db0ab37345066
Peter Bacon Darwin <pete@bacondarwin.com>
2017-04-26 19:02:28 +0100

    build(aio): include new packages in docs-watch

--- aio/tools/transforms/authors-package/api-package.js
+++ aio/tools/transforms/authors-package/api-package.js
@@ -10,17 +10,18 @@ const apiPackage = require('../angular-api-package');
 const { API_SOURCE_PATH } = require('../config');
 
 const packageMap = {
+  animations: ['animations/index.ts', 'animations/browser/index.ts', 'animations/browser/testing/index.ts'],
   common: ['common/index.ts', 'common/testing/index.ts'],
   core: ['core/index.ts', 'core/testing/index.ts'],
   forms: ['forms/index.ts'],
   http: ['http/index.ts', 'http/testing/index.ts'],
-  'platform-browser': ['platform-browser/index.ts', 'platform-browser/testing/index.ts'],
+  'platform-browser': ['platform-browser/index.ts', 'platform-browser/animations/index.ts', 'platform-browser/testing/index.ts'],
   'platform-browser-dynamic': ['platform-browser-dynamic/index.ts', 'platform-browser-dynamic/testing/index.ts'],
   'platform-server': ['platform-server/index.ts', 'platform-server/testing/index.ts'],
   'platform-webworker': ['platform-webworker/index.ts'],
   'platform-webworker-dynamic': 'platform-webworker-dynamic/index.ts',
-  router: ['router/index.ts', 'router/testing/index.ts'],
-  upgrade: ['upgrade/index.ts', 'upgrade/static.ts']
+  router: ['router/index.ts', 'router/testing/index.ts', 'router/upgrade/index.ts'],
+  upgrade: ['upgrade/index.ts', 'upgrade/static/index.ts']
 };
 
 
