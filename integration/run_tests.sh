cc03dc08c860ecb7410a938c273a01ffc4b81fc8
Alex Eagle <alexeagle@google.com>
2017-05-03 10:45:45 -0700

    test: cleanup rxjs custom build
    The latest rxjs release works with closure compiler out of the box.
    We no longer need to compile our own.
    
    Also put closure options into a file rather than using a shell script.

--- integration/run_tests.sh
+++ integration/run_tests.sh
@@ -4,12 +4,6 @@ set -e -o pipefail
 
 cd `dirname $0`
 
-if [ ! -d "rxjs/dist/es6" ]; then
-  echo "You must run build the ES2015 version of RxJS for some tests:"
-  echo "./integration/build_rxjs_es6.sh"
-  exit 1
-fi
-
 # Workaround https://github.com/yarnpkg/yarn/issues/2165
 # Yarn will cache file://dist URIs and not update Angular code
 readonly cache=.yarn_local_cache
@@ -20,7 +14,7 @@ rm_cache
 mkdir $cache
 trap rm_cache EXIT
 
-for testDir in $(ls | grep -v rxjs | grep -v node_modules) ; do
+for testDir in $(ls | grep -v node_modules) ; do
   [[ -d "$testDir" ]] || continue
   echo "#################################"
   echo "Running integration test $testDir"
