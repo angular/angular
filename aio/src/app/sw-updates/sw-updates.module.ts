089f39e6777b21a53068426dba8c2af7dcbe01db
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-05-04 00:01:22 +0300

    refactor(aio): remove work-around for browsers without ServiceWorker support
    This essentially reverts #15731, which is no longer necessary after
    angular/mobile-toolkit@eeb4b22 (which is included in v1.0.0-beta.10).

--- aio/src/app/sw-updates/sw-updates.module.ts
+++ aio/src/app/sw-updates/sw-updates.module.ts
@@ -3,7 +3,6 @@ import { MdSnackBarModule } from '@angular/material';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { globalProvider } from './global.value';
-import { noopNgServiceWorkerProviders } from './noop-ng-service-worker';
 import { SwUpdateNotificationsService } from './sw-update-notifications.service';
 import { SwUpdatesService } from './sw-updates.service';
 
@@ -15,7 +14,6 @@ import { SwUpdatesService } from './sw-updates.service';
   ],
   providers: [
     globalProvider,
-    noopNgServiceWorkerProviders,
     SwUpdateNotificationsService,
     SwUpdatesService
   ]
