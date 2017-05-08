2ce940220a9c66236689178c80a2ac0847d8adc7
Peter Bacon Darwin <pete@bacondarwin.com>
2017-05-08 15:38:52 +0100

    build(aio): local assets are not dangling links
    Closes #16615
    Closes #16616

--- aio/tools/transforms/config.js
+++ aio/tools/transforms/config.js
@@ -6,7 +6,8 @@ const AIO_PATH = resolve(PROJECT_ROOT, 'aio');
 const TEMPLATES_PATH = resolve(AIO_PATH, 'tools/transforms/templates');
 const API_TEMPLATES_PATH = resolve(TEMPLATES_PATH, 'api');
 const CONTENTS_PATH = resolve(AIO_PATH, 'content');
-const OUTPUT_PATH = resolve(AIO_PATH, 'src/generated');
+const SRC_PATH = resolve(AIO_PATH, 'src');
+const OUTPUT_PATH = resolve(SRC_PATH, 'generated');
 const DOCS_OUTPUT_PATH = resolve(OUTPUT_PATH, 'docs');
 const API_SOURCE_PATH = resolve(PROJECT_ROOT, 'packages');
 
@@ -17,5 +18,5 @@ function requireFolder(dirname, folderPath) {
     .map(p => require(resolve(absolutePath, p)));
 }
 
-module.exports = { PROJECT_ROOT, AIO_PATH, TEMPLATES_PATH, API_TEMPLATES_PATH, CONTENTS_PATH, OUTPUT_PATH, DOCS_OUTPUT_PATH, API_SOURCE_PATH, requireFolder };
+module.exports = { PROJECT_ROOT, AIO_PATH, TEMPLATES_PATH, API_TEMPLATES_PATH, CONTENTS_PATH, SRC_PATH, OUTPUT_PATH, DOCS_OUTPUT_PATH, API_SOURCE_PATH, requireFolder };
 
