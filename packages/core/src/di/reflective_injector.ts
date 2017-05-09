d0e72a8f8f776c5f92506a6fa60d9cf5b12b61bc
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-09 23:51:37 +0100

    docs(*) fix dangling links in API docs (#16632)
    * docs(animations): fix links to `Component` animations
    
    * docs(core): fix links to `ReflectiveInjector` methods
    
    The `resolve` and other methods were moved from the
    `Injector` to the `ReflectiveInjector`.
    
    * docs(core): fix links to `Renderer`
    
    The local links were assuming that that methods were on the
    current document (e.g. `RootRenderer`), but they are actually
    on the `Renderer` class.
    
    * docs(router): fix links to methods
    
    * docs(forms): fix links to methods
    
    * docs(core): fix links to methods
    
    * docs(router): fix API page links and an internal link

--- packages/core/src/di/reflective_injector.ts
+++ packages/core/src/di/reflective_injector.ts
@@ -113,7 +113,7 @@ export abstract class ReflectiveInjector implements Injector {
    *
    * This function is slower than the corresponding `fromResolvedProviders`
    * because it needs to resolve the passed-in providers first.
-   * See {@link Injector#resolve} and {@link Injector#fromResolvedProviders}.
+   * See {@link ReflectiveInjector#resolve} and {@link ReflectiveInjector#fromResolvedProviders}.
    */
   static resolveAndCreate(providers: Provider[], parent?: Injector): ReflectiveInjector {
     const ResolvedReflectiveProviders = ReflectiveInjector.resolve(providers);
@@ -190,7 +190,7 @@ export abstract class ReflectiveInjector implements Injector {
    *
    * This function is slower than the corresponding `createChildFromResolved`
    * because it needs to resolve the passed-in providers first.
-   * See {@link Injector#resolve} and {@link Injector#createChildFromResolved}.
+   * See {@link ReflectiveInjector#resolve} and {@link ReflectiveInjector#createChildFromResolved}.
    */
   abstract resolveAndCreateChild(providers: Provider[]): ReflectiveInjector;
 
