6f3ee2ee4526f6a33d04c6a29260c565d7c4c556
Peter Bacon Darwin <pete@bacondarwin.com>
2017-05-09 17:11:22 +0100

    docs(core): remove link to non-existent class in `InjectableDecorator`
    Closes #16640

--- packages/core/src/di/metadata.ts
+++ packages/core/src/di/metadata.ts
@@ -127,7 +127,7 @@ export interface InjectableDecorator {
    *
    * {@example core/di/ts/metadata_spec.ts region='Injectable'}
    *
-   * {@link Injector} will throw {@link NoAnnotationError} when trying to instantiate a class that
+   * {@link Injector} will throw an error when trying to instantiate a class that
    * does not have `@Injectable` marker, as shown in the example below.
    *
    * {@example core/di/ts/metadata_spec.ts region='InjectableThrows'}
