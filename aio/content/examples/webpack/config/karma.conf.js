d64cb354a49f1272272262518e47f1a7e44ba576
Jesus Rodriguez <Foxandxss@gmail.com>
2017-05-10 12:34:15 +0200

    docs(aio): add a progress reporter for the webpack guide

--- aio/content/examples/webpack/config/karma.conf.js
+++ aio/content/examples/webpack/config/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       noInfo: true
     },
 
-    reporters: ['kjhtml'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
