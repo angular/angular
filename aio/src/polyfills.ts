7916d6c438f4bcb87b7dbe25002308976db28774
Peter Bacon Darwin <pete@bacondarwin.com>
2017-05-15 21:22:06 +0100

    fix(aio): support IE via conditionally loaded polyfills
    Closes #16519

--- aio/src/polyfills.ts
+++ aio/src/polyfills.ts
@@ -18,27 +18,14 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-// import 'core-js/es6/symbol';
-// import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-// import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
-// import 'core-js/es6/set';
-
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following to support `@angular/animation`. */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
+/**
+ * INTERNET EXPLORER
+ *
+ * All the Internet Explorer polyfills are defined separately, in the `ie-polyfills.js` file.
+ * They are also built separately from the main app, via the `yarn build-ie-polyfills` task.
+ * The output of this build is conditionally loaded (only if we are running IE) in the browser,
+ * and executed before the rest of the application files are executed.
+ */
 
 /** HACK: force import of environment.ts/environment.prod.ts to load env specific polyfills */
 import './environments/environment';
