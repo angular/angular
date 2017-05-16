2850e9a024d58ab8a1f7b5036a0b0eb42a8aa55c
Victor Berchet <victor@suumit.com>
2017-05-16 15:48:57 -0700

    build: do not create the bundles when updating the public API
    - 1.7x faster on my machine (2.7 vs 4.6 min)
    - should solve the memory issue

--- tools/gulp-tasks/build.js
+++ tools/gulp-tasks/build.js
@@ -1,7 +1,14 @@
 
-module.exports = (gulp) => (done) => {
+module.exports = {
+  // build everything and generate bundles
+  'all': (gulp) => (done) => execBuild(done),
+  // same as all without the bundling part - faster / lower memory usage
+  'no-bundle': (gulp) => (done) => execBuild(done, '--bundle=false'),
+};
+
+function execBuild(done, args = '') {
   const path = require('path');
   const childProcess = require('child_process');
   // increase maxbuffer to address out of memory exception when running certain tasks
-  childProcess.exec(path.join(__dirname, '../../build.sh'), {maxBuffer: 300 * 1024}, done);
-};
+  childProcess.exec(path.join(__dirname, `../../build.sh ${args}`), {maxBuffer: 300 * 1024}, done);
+}
