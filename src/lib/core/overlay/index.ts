caee5a56cc5a8d0a0ff6da3d13d3e31a668c4c46
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-05-17 19:47:06 +0200

    feat(core): expose ScrollDispatchModule (#4501)
    With #4251 the `ScrollDispatchModule` has been created and it is used by a lot of different components.
    
    The class is already exported inside of the file but the core package doesn't export it to the public.

--- src/lib/core/overlay/index.ts
+++ src/lib/core/overlay/index.ts
@@ -4,19 +4,12 @@ export {FullscreenOverlayContainer} from './fullscreen-overlay-container';
 export {OverlayRef} from './overlay-ref';
 export {OverlayState} from './overlay-state';
 export {ConnectedOverlayDirective, OverlayOrigin, OverlayModule} from './overlay-directives';
-export {ScrollDispatcher} from './scroll/scroll-dispatcher';
 export {ViewportRuler} from './position/viewport-ruler';
 
 export * from './position/connected-position';
+export * from './scroll/index';
 
 // Export pre-defined position strategies and interface to build custom ones.
 export {PositionStrategy} from './position/position-strategy';
 export {GlobalPositionStrategy} from './position/global-position-strategy';
 export {ConnectedPositionStrategy} from './position/connected-position-strategy';
-
-// Export pre-defined scroll strategies and interface to build custom ones.
-export {ScrollStrategy} from './scroll/scroll-strategy';
-export {RepositionScrollStrategy} from './scroll/reposition-scroll-strategy';
-export {CloseScrollStrategy} from './scroll/close-scroll-strategy';
-export {NoopScrollStrategy} from './scroll/noop-scroll-strategy';
-export {BlockScrollStrategy} from './scroll/block-scroll-strategy';
--- src/lib/core/overlay/scroll/index.ts
+++ src/lib/core/overlay/scroll/index.ts
@@ -6,6 +6,13 @@ import {PlatformModule} from '../../platform/index';
 export {Scrollable} from './scrollable';
 export {ScrollDispatcher} from './scroll-dispatcher';
 
+// Export pre-defined scroll strategies and interface to build custom ones.
+export {ScrollStrategy} from './scroll-strategy';
+export {RepositionScrollStrategy} from './reposition-scroll-strategy';
+export {CloseScrollStrategy} from './close-scroll-strategy';
+export {NoopScrollStrategy} from './noop-scroll-strategy';
+export {BlockScrollStrategy} from './block-scroll-strategy';
+
 @NgModule({
   imports: [PlatformModule],
   exports: [Scrollable],
