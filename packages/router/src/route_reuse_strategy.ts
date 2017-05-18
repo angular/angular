86b7bd9c8e90b1b8f863abce162e081d49cde885
Matias Niemelä <matias@yearofmoo.com>
2017-05-18 11:28:50 -0700

    revert: refactor(router): cleanup, simplification
    This reverts commit 44d48d9d7aa10bbf03c412ce7e59821c748b5d2d.

--- packages/router/src/route_reuse_strategy.ts
+++ packages/router/src/route_reuse_strategy.ts
@@ -48,16 +48,3 @@ export abstract class RouteReuseStrategy {
   /** Determines if a route should be reused */
   abstract shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean;
 }
-
-/**
- * Does not detach any subtrees. Reuses routes as long as their route config is the same.
- */
-export class DefaultRouteReuseStrategy implements RouteReuseStrategy {
-  shouldDetach(route: ActivatedRouteSnapshot): boolean { return false; }
-  store(route: ActivatedRouteSnapshot, detachedTree: DetachedRouteHandle): void {}
-  shouldAttach(route: ActivatedRouteSnapshot): boolean { return false; }
-  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle|null { return null; }
-  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-    return future.routeConfig === curr.routeConfig;
-  }
-}
