1d544df40086de6212fbb4f9478c12822f3d458b
Jason Aden <jasonaden@google.com>
2017-05-19 12:34:57 -0700

    build: import tslib rather than output TS helpers

--- packages/platform-webworker-dynamic/rollup.config.js
+++ packages/platform-webworker-dynamic/rollup.config.js
@@ -6,6 +6,17 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import resolve from 'rollup-plugin-node-resolve';
+
+const globals = {
+  '@angular/core': 'ng.core',
+  '@angular/common': 'ng.common',
+  '@angular/compiler': 'ng.compiler',
+  '@angular/platform-browser': 'ng.platformBrowser',
+  '@angular/platform-browser-dynamic': 'ng.platformBrowserDynamic',
+  '@angular/platform-webworker': 'ng.platformWebworker',
+};
+
 export default {
   entry:
       '../../dist/packages-dist/platform-webworker-dynamic/@angular/platform-webworker-dynamic.es5.js',
@@ -14,12 +25,7 @@ export default {
   format: 'umd',
   exports: 'named',
   moduleName: 'ng.platformWebworkerDynamic',
-  globals: {
-    '@angular/core': 'ng.core',
-    '@angular/common': 'ng.common',
-    '@angular/compiler': 'ng.compiler',
-    '@angular/platform-browser': 'ng.platformBrowser',
-    '@angular/platform-browser-dynamic': 'ng.platformBrowserDynamic',
-    '@angular/platform-webworker': 'ng.platformWebworker',
-  }
+  plugins: [resolve()],
+  external: Object.keys(globals),
+  globals: globals
 };
