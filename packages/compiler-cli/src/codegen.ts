8f109684337cd3a13494cbb1bbe523791bbd1048
Tobias Bosch <tbosch1009@gmail.com>
2017-05-19 10:13:58 -0700

    fix(compiler): compile `.ngfactory.ts` files even if nobody references them.
    This is especially important for library authors, as they will
    not reference the .ngfactory.ts files.
    
    Fixes #16741

--- packages/compiler-cli/src/codegen.ts
+++ packages/compiler-cli/src/codegen.ts
@@ -36,17 +36,18 @@ export class CodeGenerator {
       public host: ts.CompilerHost, private compiler: compiler.AotCompiler,
       private ngCompilerHost: CompilerHost) {}
 
-  codegen(): Promise<any> {
+  codegen(): Promise<string[]> {
     return this.compiler
         .compileAll(this.program.getSourceFiles().map(
             sf => this.ngCompilerHost.getCanonicalFileName(sf.fileName)))
         .then(generatedModules => {
-          generatedModules.forEach(generatedModule => {
+          return generatedModules.map(generatedModule => {
             const sourceFile = this.program.getSourceFile(generatedModule.srcFileUrl);
             const emitPath = this.ngCompilerHost.calculateEmitPath(generatedModule.genFileUrl);
             const source = GENERATED_META_FILES.test(emitPath) ? generatedModule.source :
                                                                  generatedModule.source;
             this.host.writeFile(emitPath, source, false, () => {}, [sourceFile]);
+            return emitPath;
           });
         });
   }
