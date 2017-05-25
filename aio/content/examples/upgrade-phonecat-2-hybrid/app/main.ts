b37a0484d411b783d2bd33194d3a9fc4ae9e69a2
Jesús Rodríguez <Foxandxss@gmail.com>
2017-05-26 01:44:03 +0200

    docs(aio): restore missing files (#16995)

---
+++ aio/content/examples/upgrade-phonecat-2-hybrid/app/main.ts
@@ -0,0 +1,11 @@
+// #docregion bootstrap
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { UpgradeModule } from '@angular/upgrade/static';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
+  const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
+  upgrade.bootstrap(document.documentElement, ['phonecatApp']);
+});
+// #enddocregion bootstrap
