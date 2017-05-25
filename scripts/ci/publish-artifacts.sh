58c8404d73384f87b7b0f45ffcffffacdbaa9878
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-05-25 22:37:00 +0200

    build: use travis build stages (#4766)

---
+++ scripts/ci/publish-artifacts.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script that runs after the testing stage of Travis passed.
+# Build artifacts and docs content will be published to different repositories.
+
+# Go to the project root directory
+cd $(dirname $0)/../..
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "Build artifacts and docs content will only be deployed in Travis push builds."
+  exit 0;
+fi
+
+echo "Starting to publish the build artifacts and docs content..."
+echo ""
+
+# Build Material, CDK and the docs before publishing artifacts
+$(npm bin)/gulp material:build-release:clean
+$(npm bin)/gulp material-examples:build-release
+$(npm bin)/gulp docs
+
+# Run publishing of artifacts in parallel.
+# This is possible because the output has been built before.
+./scripts/release/publish-build-artifacts.sh --no-build &
+./scripts/release/publish-docs-content.sh --no-build &
+
+# Deploy the screenshot functions for each push build.
+./scripts/release/deploy-screenshot-functions.sh &
+
+wait
