e022c376159e01b51e13b294c8f4a346ad52a7a8
Jesus Rodriguez <Foxandxss@gmail.com>
2017-05-26 15:14:50 +0200

    docs(aio): more George feedback

--- packages/examples/platform-browser/browser/e2e_test/title_spec.ts
+++ packages/examples/platform-browser/browser/e2e_test/title_spec.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {ElementFinder, ExpectedConditions, browser, by, element} from 'protractor';
+import {ElementFinder, browser, by, element} from 'protractor';
 
 import {verifyNoBrowserErrors} from '../../../_common/e2e_util';
 
