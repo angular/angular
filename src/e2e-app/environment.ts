0103d2a13aac1a6c5a0b1c4d56e0c9f162a8d76f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-05-30 18:24:38 +0200

    chore: remove unused environment file (#4845)
    * Removes the unused envrionment file from the old build process with the Angular CLI

--- src/e2e-app/environment.ts
+++
@@ -1,7 +0,0 @@
-// The file for the current environment will overwrite this one during build
-// Different environments can be found in config/environment.{dev|prod}.ts
-// The build system defaults to the dev environment
-
-export const environment = {
-  production: false
-};
