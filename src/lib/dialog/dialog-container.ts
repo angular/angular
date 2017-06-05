c8b1e200eea78fe28ce911bd58fd61a634829c52
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-06-06 00:44:16 +0200

    fix(dialog): blurry content and slow animation in IE (#4809)
    Fixes the dialog content being blurry and the animation being really janky in IE11.

--- src/lib/dialog/dialog-container.ts
+++ src/lib/dialog/dialog-container.ts
@@ -45,8 +45,12 @@ export function throwMdDialogContentAlreadyAttachedError() {
   encapsulation: ViewEncapsulation.None,
   animations: [
     trigger('slideDialog', [
+      // Note: The `enter` animation doesn't transition to something like `translate3d(0, 0, 0)
+      // scale(1)`, because for some reason specifying the transform explicitly, causes IE both
+      // to blur the dialog content and decimate the animation performance. Leaving it blank
+      // solves both issues.
+      state('enter', style({ opacity: 1 })),
       state('void', style({ transform: 'translate3d(0, 25%, 0) scale(0.9)', opacity: 0 })),
-      state('enter', style({ transform: 'translate3d(0, 0, 0) scale(1)', opacity: 1 })),
       state('exit', style({ transform: 'translate3d(0, 25%, 0)', opacity: 0 })),
       transition('* => *', animate('400ms cubic-bezier(0.25, 0.8, 0.25, 1)')),
     ])
