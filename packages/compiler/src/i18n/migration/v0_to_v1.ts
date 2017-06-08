d297238f79509046a4558ffaf1537595e34d2a2c
Olivier Combe <olivier.combe@gmail.com>
2017-06-08 15:58:27 -0700

    feat(compiler): compute i18n conflicts

--- packages/compiler/src/i18n/migration/v0_to_v1.ts
+++ packages/compiler/src/i18n/migration/v0_to_v1.ts
@@ -13,9 +13,15 @@ import {Message} from '../i18n_ast';
 import {MessageSpan} from '../i18n_ast';
 import {MessageBundle} from '../message_bundle';
 import {createSerializer} from '../serializers/factory';
+import {getXliffMsgTextById} from '../serializers/xliff';
+import {getXliff2MsgTextById} from '../serializers/xliff2';
+import {getXtbMsgTextById} from '../serializers/xtb';
 
 export type V1ToV0Map = {
   [v1: string]: {ids: string[], sources?: MessageSpan[]}
+};
+export type V1ToV0Conflicts = {
+  [v1: string]: {id: string, msg: string}[]
 };
 
 /**
@@ -49,4 +55,65 @@ export function generateV1ToV0Map(messageBundle: MessageBundle, formatName: stri
   });
 
   return v1ToV0;
+}
+
+/**
+ * A conflict occurs when not all of the V0 messages replaced by a single V1 message have the exact
+ * same translation.
+ *
+ * Such conflicts need to be resolved before we can migrate the translations.
+ *
+ * The output a this function has an entry only for v1 message that conflict:
+ * - `id` is the v0 message id
+ * - `msg` is the content of the v0 message
+ */
+export function computeConflicts(map: V1ToV0Map, content: string, format: string): V1ToV0Conflicts {
+  const candidates: {[v1: string]: string[]} = {};
+
+  // Candidates are v1 messages that replace multiple v0 messages
+  Object.keys(map).forEach(v1 => {
+    if (map[v1].ids.length > 1) {
+      candidates[v1] = map[v1].ids;
+    }
+  });
+
+  const conflicts: V1ToV0Conflicts = {};
+
+  let msgById: {[v0: string]: string};
+
+  switch (format) {
+    case 'xtb':
+      msgById = getXtbMsgTextById(content, '');
+      break;
+    case 'xlf':
+    case 'xliff':
+      msgById = getXliffMsgTextById(content, '');
+      break;
+    case 'xlf2':
+    case 'xliff2':
+      msgById = getXliff2MsgTextById(content, '');
+      break;
+    default:
+      throw Error(`Unsupported format "${format}"`);
+  }
+
+  // There is a conflict when not all of the v0 translations are the same for a given v1 message
+  Object.keys(candidates).forEach((v1) => {
+    const seenMsgs: string[] = [];
+    const uniques: {id: string, msg: string}[] = [];
+    candidates[v1].forEach((v0) => {
+      if (msgById.hasOwnProperty(v0)) {
+        const msg = msgById[v0];
+        if (seenMsgs.indexOf(msg) === -1) {
+          seenMsgs.push(msg);
+          uniques.push({id: v0, msg});
+        }
+      }
+    });
+    if (uniques.length > 1) {
+      conflicts[v1] = uniques;
+    }
+  });
+
+  return conflicts;
 }
--- packages/compiler/test/i18n/migration/v0_to_v1_spec.ts
+++ packages/compiler/test/i18n/migration/v0_to_v1_spec.ts
@@ -7,7 +7,7 @@
  */
 
 import {MessageBundle} from '../../../src/i18n/message_bundle';
-import {generateV1ToV0Map} from '../../../src/i18n/migration/v0_to_v1';
+import {computeConflicts, generateV1ToV0Map} from '../../../src/i18n/migration/v0_to_v1';
 import {HtmlParser} from '../../../src/ml_parser/html_parser';
 import {DEFAULT_INTERPOLATION_CONFIG} from '../../../src/ml_parser/interpolation_config';
 
@@ -49,7 +49,70 @@ export function main(): void {
           },
         });
       });
+    });
 
+    describe(' Compute conflicts', () => {
+      const v1ToV0Map = {
+        'single.v1': {ids: ['single.v0']},
+        'mul_ok.v1': {ids: ['mul_ok.1.v0', 'mul_ok.2.v0']},
+        'mul_ko.v1': {ids: ['mul_ko.1.v0', 'mul_ko.2.v0', 'mul_ko.3.v0']},
+        'no_0.v1': {ids: ['no_0.v0']},
+      };
+
+      const EXPECTED_CONFLICTS = {
+        'mul_ko.v1': [
+          {id: 'mul_ko.1.v0', msg: 'conflict?'},
+          {id: 'mul_ko.2.v0', msg: 'yes'},
+        ],
+      };
+
+      it('should work XTB', () => {
+        const TRANS = `
+          <translationbundle>
+            <translation id="single.v0">single</translation>
+            <translation id="mul_ok.1.v0">no conflict</translation>
+            <translation id="mul_ok.2.v0">no conflict</translation>
+            <translation id="mul_ko.1.v0">conflict?</translation>
+            <translation id="mul_ko.2.v0">yes</translation>
+            <translation id="mul_ko.3.v0">yes</translation>
+          </translationbundle>`;
+
+        expect(computeConflicts(v1ToV0Map, TRANS, 'xtb')).toEqual(EXPECTED_CONFLICTS);
+      });
+
+      it('should work XLIFF', () => {
+        const TRANS = `<?xml version="1.0" encoding="UTF-8" ?>
+          <xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
+            <file source-language="en">
+              <body>
+                <trans-unit id="single.v0"><target>single</target></trans-unit>
+                <trans-unit id="mul_ok.1.v0"><target>no conflict</target></trans-unit>
+                <trans-unit id="mul_ok.2.v0"><target>no conflict</target></trans-unit>
+                <trans-unit id="mul_ko.1.v0"><target>conflict?</target></trans-unit>
+                <trans-unit id="mul_ko.2.v0"><target>yes</target></trans-unit>
+                <trans-unit id="mul_ko.3.v0"><target>yes</target></trans-unit>
+              </body>
+            </file>
+          </xliff>`;
+
+        expect(computeConflicts(v1ToV0Map, TRANS, 'xliff')).toEqual(EXPECTED_CONFLICTS);
+      });
+
+      it('should work XLIFF2', () => {
+        const TRANS = `<?xml version="1.0" encoding="UTF-8" ?>;
+          <xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0">
+            <file original="ng.template" id="ngi18n">
+              <unit id="single.v0"><target>single</target></unit>
+              <unit id="mul_ok.1.v0"><target>no conflict</target></unit>
+              <unit id="mul_ok.2.v0"><target>no conflict</target></unit>
+              <unit id="mul_ko.1.v0"><target>conflict?</target></unit>
+              <unit id="mul_ko.2.v0"><target>yes</target></unit>
+              <unit id="mul_ko.3.v0"><target>yes</target></unit>
+            </file>
+          </xliff>`;
+
+        expect(computeConflicts(v1ToV0Map, TRANS, 'xliff2')).toEqual(EXPECTED_CONFLICTS);
+      });
     });
   });
 }
