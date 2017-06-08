658e1bbdf4cd727bb8979771d562dbecc646f9e5
Matej Minarik <mato.minarik@azet.sk>
2017-06-08 13:24:45 +0200

    fix(compiler): Better error messages for missing let in ngFor directive

--- packages/compiler/test/template_parser/template_parser_spec.ts
+++ packages/compiler/test/template_parser/template_parser_spec.ts
@@ -2091,6 +2091,10 @@ The pipe 'test' could not be found ("{{[ERROR ->]a | test}}"): TestComp@0:2`);
         [AttrAst, 'template', 'ngFor'],
       ]);
     });
+
+    it('should throw invalid ngFor syntax error for *ngFor', () => {
+      expect(() => parse('<p *ngFor="object of objects">{{object}}</p>', [])).toThrowError(/missing the let keyword/);
+    });
   });
 }
 
