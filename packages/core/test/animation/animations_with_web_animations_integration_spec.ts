0bd31209fe9fbfcecf5b788120bda91dd8b0b023
Matias Niemelä <matias@yearofmoo.com>
2017-06-09 14:39:36 -0700

    fix(animations): compute removal node height correctly

--- packages/core/test/animation/animations_with_web_animations_integration_spec.ts
+++ packages/core/test/animation/animations_with_web_animations_integration_spec.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import {animate, style, transition, trigger} from '@angular/animations';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 import {AnimationDriver, ɵAnimationEngine} from '@angular/animations/browser';
 import {ɵWebAnimationsDriver, ɵWebAnimationsPlayer, ɵsupportsWebAnimations} from '@angular/animations/browser'
 import {Component, ViewChild} from '@angular/core';
@@ -24,6 +24,97 @@ export function main() {
         providers: [{provide: AnimationDriver, useClass: ɵWebAnimationsDriver}],
         imports: [BrowserAnimationsModule]
       });
+    });
+
+    it('should compute (*) animation styles for a container that is being removed', () => {
+      @Component({
+        selector: 'ani-cmp',
+        template: `
+          <div @auto *ngIf="exp">
+            <div style="line-height:20px;">1</div>
+            <div style="line-height:20px;">2</div>
+            <div style="line-height:20px;">3</div>
+            <div style="line-height:20px;">4</div>
+            <div style="line-height:20px;">5</div>
+          </div>
+        `,
+        animations: [trigger(
+            'auto',
+            [
+              state('void', style({height: '0px'})), state('*', style({height: '*'})),
+              transition('* => *', animate(1000))
+            ])]
+      })
+      class Cmp {
+        public exp: boolean = false;
+      }
+
+      TestBed.configureTestingModule({declarations: [Cmp]});
+
+      const engine = TestBed.get(ɵAnimationEngine);
+      const fixture = TestBed.createComponent(Cmp);
+      const cmp = fixture.componentInstance;
+
+      cmp.exp = true;
+      fixture.detectChanges();
+      engine.flush();
+
+      expect(engine.players.length).toEqual(1);
+      let webPlayer = engine.players[0].getRealPlayer() as ɵWebAnimationsPlayer;
+
+      expect(webPlayer.keyframes).toEqual([
+        {height: '0px', offset: 0}, {height: '100px', offset: 1}
+      ]);
+
+      cmp.exp = false;
+      fixture.detectChanges();
+      engine.flush();
+
+      expect(engine.players.length).toEqual(1);
+      webPlayer = engine.players[0].getRealPlayer() as ɵWebAnimationsPlayer;
+
+      expect(webPlayer.keyframes).toEqual([
+        {height: '100px', offset: 0}, {height: '0px', offset: 1}
+      ]);
+    });
+
+    it('should compute (!) animation styles for a container that is being inserted', () => {
+      @Component({
+        selector: 'ani-cmp',
+        template: `
+          <div @auto *ngIf="exp">
+            <div style="line-height:20px;">1</div>
+            <div style="line-height:20px;">2</div>
+            <div style="line-height:20px;">3</div>
+            <div style="line-height:20px;">4</div>
+            <div style="line-height:20px;">5</div>
+          </div>
+        `,
+        animations: [trigger(
+            'auto',
+            [transition(
+                ':enter', [style({height: '!'}), animate(1000, style({height: '120px'}))])])]
+      })
+      class Cmp {
+        public exp: boolean = false;
+      }
+
+      TestBed.configureTestingModule({declarations: [Cmp]});
+
+      const engine = TestBed.get(ɵAnimationEngine);
+      const fixture = TestBed.createComponent(Cmp);
+      const cmp = fixture.componentInstance;
+
+      cmp.exp = true;
+      fixture.detectChanges();
+      engine.flush();
+
+      expect(engine.players.length).toEqual(1);
+      let webPlayer = engine.players[0].getRealPlayer() as ɵWebAnimationsPlayer;
+
+      expect(webPlayer.keyframes).toEqual([
+        {height: '100px', offset: 0}, {height: '120px', offset: 1}
+      ]);
     });
 
     it('should compute pre (!) and post (*) animation styles with different dom states', () => {
