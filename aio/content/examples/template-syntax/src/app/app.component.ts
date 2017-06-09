6a37722ec35436d196ca0b95a5176567a77d2818
Trotyl Yu <trotyl@qq.com>
2017-06-09 16:05:16 +0800

    fix(aio): fix trackBy demo in template-syntax article

--- aio/content/examples/template-syntax/src/app/app.component.ts
+++ aio/content/examples/template-syntax/src/app/app.component.ts
@@ -127,6 +127,7 @@ export class AppComponent implements AfterViewInit, OnInit {
   resetHeroes() {
     this.heroes = Hero.heroes.map(hero => hero.clone());
     this.currentHero = this.heroes[0];
+    this.hero = this.currentHero;
     this.heroesWithTrackByCountReset = 0;
   }
 
@@ -172,8 +173,8 @@ function trackChanges(views: QueryList<ElementRef>, changed: () => void) {
   let oldRefs = views.toArray();
   views.changes.subscribe((changes: QueryList<ElementRef>) => {
       const changedRefs = changes.toArray();
-      // Is every changed ElemRef the same as old and in the same position
-      const isSame = oldRefs.every((v, i) => v === changedRefs[i]);
+      // Check if every changed Element is the same as old and in the same position
+      const isSame = oldRefs.every((v, i) => v.nativeElement === changedRefs[i].nativeElement);
       if (!isSame) {
         oldRefs = changedRefs;
         // wait a tick because called after views are constructed
