8054adf1155bff18d5109da83fbcc8f28766f049
Chuck Jazdzewski <chuckj@google.com>
2017-06-09 14:50:57 -0700

    feat(compiler-cli): new compiler api and command-line using TypeScript transformers

---
+++ packages/compiler-cli/src/transformers/entry_points.ts
@@ -0,0 +1,24 @@
+import * as ts from 'typescript';
+
+import {CompilerHost, CompilerOptions, Program} from './api';
+import {createModuleFilenameResolver} from './module_filename_resolver';
+export {createProgram} from './program';
+export {createModuleFilenameResolver};
+
+export function createHost({tsHost, options}: {tsHost: ts.CompilerHost, options: CompilerOptions}):
+    CompilerHost {
+  const resolver = createModuleFilenameResolver(tsHost, options);
+
+  const host = Object.create(tsHost);
+
+  host.moduleNameToFileName = resolver.moduleNameToFileName.bind(resolver);
+  host.fileNameToModuleName = resolver.fileNameToModuleName.bind(resolver);
+  host.getNgCanonicalFileName = resolver.getNgCanonicalFileName.bind(resolver);
+  host.assumeFileExists = resolver.assumeFileExists.bind(resolver);
+
+  // Make sure we do not `host.realpath()` from TS as we do not want to resolve symlinks.
+  // https://github.com/Microsoft/TypeScript/issues/9552
+  host.realpath = (fileName: string) => fileName;
+
+  return host;
+}
--- packages/compiler/src/parse_util.ts
+++ packages/compiler/src/parse_util.ts
@@ -118,12 +118,14 @@ export class ParseError {
       public span: ParseSourceSpan, public msg: string,
       public level: ParseErrorLevel = ParseErrorLevel.ERROR) {}
 
-  toString(): string {
+  contextualMessage(): string {
     const ctx = this.span.start.getContext(100, 3);
-    const contextStr =
-        ctx ? ` ("${ctx.before}[${ParseErrorLevel[this.level]} ->]${ctx.after}")` : '';
+    return ctx ? ` ("${ctx.before}[${ParseErrorLevel[this.level]} ->]${ctx.after}")` : '';
+  }
+
+  toString(): string {
     const details = this.span.details ? `, ${this.span.details}` : '';
-    return `${this.msg}${contextStr}: ${this.span.start}${details}`;
+    return `${this.msg}${this.contextualMessage()}: ${this.span.start}${details}`;
   }
 }
 
