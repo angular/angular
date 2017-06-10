bb474f65660774e5e4692512ce008653a6d13dab
Igor Minar <igor@angularjs.org>
2017-06-09 18:25:03 -0700

    fix(animations): remove duplicate license header

--- packages/animations/browser/src/dsl/animation_ast.ts
+++ packages/animations/browser/src/dsl/animation_ast.ts
@@ -4,12 +4,6 @@
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
  */
 import {AnimateTimings, AnimationOptions, ɵStyleData} from '@angular/animations';
 
