1488d833bf850b6c4f86503a5a895102c7567889
Tobias Bosch <tbosch1009@gmail.com>
2017-06-12 14:10:08 -0700

    refactor(compiler): don’t always compile `.ngfactory.ts` files by default
    This puts the behavior introduced in https://github.com/angular/angular/commit/573b8611bc4ab85a7436ee110526678a0ecf6c76 behind the new flag
    `alwaysCompileGeneratedCode` to not break users that might have relied
    on this behavior.

--- tools/@angular/tsc-wrapped/src/options.ts
+++ tools/@angular/tsc-wrapped/src/options.ts
@@ -83,8 +83,13 @@ interface Options extends ts.CompilerOptions {
   enableLegacyTemplate?: boolean;
 
   // Whether to generate .ngsummary.ts files that allow to use AOTed artifacts
-  // in JIT mode.
+  // in JIT mode. This is off by default.
   enableSummariesForJit?: boolean;
+
+  // Whether to compile generated .ngfacgtory.ts files, even when they are no
+  // matched by the `files` / `includes` in the `tsconfig.json`.
+  // This is off by default.
+  alwaysCompileGeneratedCode?: boolean;
 }
 
 export default Options;
