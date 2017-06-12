9bb6b00eef114546a5e2c9126ee95f89c0784ce0
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-12 23:51:39 +0100

    build(aio): add staging environment
    You can now specify what environment you are building
    by add it to the `yarn build` command. For example:
    
    ```
    yarn build -- --env=stage
    ```
    
    Moreover the `deploy-to-firebase.sh` script will automatically apply the
    appropriate environment.

--- aio/scripts/deploy-to-firebase.sh
+++ aio/scripts/deploy-to-firebase.sh
@@ -8,11 +8,13 @@ readonly deployEnv=$1
 
 case $deployEnv in
   staging)
+    readonly buildEnv=stage
     readonly projectId=aio-staging
     readonly deployedUrl=https://$projectId.firebaseapp.com/
     readonly firebaseToken=$FIREBASE_TOKEN
     ;;
   production)
+    readonly buildEnv=prod
     readonly projectId=angular-io
     readonly deployedUrl=https://angular.io/
     readonly firebaseToken=$FIREBASE_TOKEN
@@ -27,7 +29,7 @@ esac
   cd "`dirname $0`/.."
 
   # Build the app
-  yarn build
+  yarn build -- --env=$buildEnv
 
   # Deploy to Firebase
   firebase use "$projectId" --token "$firebaseToken"
---
+++ aio/src/environments/environment.stage.ts
@@ -0,0 +1,5 @@
+// This is for the staging site, which is hosted at https://aio-staging.firebaseapp.org
+export const environment = {
+  gaId: 'UA-8594346-26',
+  production: true
+};
