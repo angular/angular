c9304ed6145c9fec1bf44d602aec92e0746e2624
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-13 11:01:04 +0100

    fix(aio): make search results better
    * update to latest version of lunr search
    * add trailing wildcard to search terms to increase matches
    * fix unwanted error when escape was pressed
    
    Closes #17417

--- aio/src/app/search/search-box/search-box.component.spec.ts
+++ aio/src/app/search/search-box/search-box.component.spec.ts
@@ -7,10 +7,10 @@ import { LocationService } from 'app/shared/location.service';
 import { MockLocationService } from 'testing/location.service';
 
 @Component({
-  template: '<aio-search-box (search)="doSearch($event)"></aio-search-box>'
+  template: '<aio-search-box (onSearch)="searchHandler($event)"></aio-search-box>'
 })
 class HostComponent {
-  doSearch = jasmine.createSpy('doSearch');
+  searchHandler = jasmine.createSpy('searchHandler');
 }
 
 describe('SearchBoxComponent', () => {
@@ -39,40 +39,61 @@ describe('SearchBoxComponent', () => {
       location.search.and.returnValue({ search: 'initial search' });
       component.ngOnInit();
       expect(location.search).toHaveBeenCalled();
-      expect(host.doSearch).toHaveBeenCalledWith('initial search');
+      expect(host.searchHandler).toHaveBeenCalledWith('initial search');
       expect(component.searchBox.nativeElement.value).toEqual('initial search');
     }));
   });
 
   describe('on input', () => {
-    it('should trigger the search event', () => {
+    it('should trigger the onSearch event', () => {
       const input = fixture.debugElement.query(By.css('input'));
-      input.triggerEventHandler('input', { target: { value: 'some query' } });
-      expect(host.doSearch).toHaveBeenCalledWith('some query');
+      input.nativeElement.value = 'some query (input)';
+      input.triggerEventHandler('input', { });
+      expect(host.searchHandler).toHaveBeenCalledWith('some query (input)');
     });
   });
 
   describe('on keyup', () => {
-    it('should trigger the search event', () => {
+    it('should trigger the onSearch event', () => {
       const input = fixture.debugElement.query(By.css('input'));
-      input.triggerEventHandler('keyup', { target: { value: 'some query' } });
-      expect(host.doSearch).toHaveBeenCalledWith('some query');
+      input.nativeElement.value = 'some query (keyup)';
+      input.triggerEventHandler('keyup', { });
+      expect(host.searchHandler).toHaveBeenCalledWith('some query (keyup)');
     });
   });
 
   describe('on focus', () => {
-    it('should trigger the search event', () => {
+    it('should trigger the onSearch event', () => {
       const input = fixture.debugElement.query(By.css('input'));
-      input.triggerEventHandler('focus', { target: { value: 'some query' } });
-      expect(host.doSearch).toHaveBeenCalledWith('some query');
+      input.nativeElement.value = 'some query (focus)';
+      input.triggerEventHandler('focus', { });
+      expect(host.searchHandler).toHaveBeenCalledWith('some query (focus)');
     });
   });
 
   describe('on click', () => {
     it('should trigger the search event', () => {
       const input = fixture.debugElement.query(By.css('input'));
-      input.triggerEventHandler('click', { target: { value: 'some query'}});
-      expect(host.doSearch).toHaveBeenCalledWith('some query');
+      input.nativeElement.value = 'some query (click)';
+      input.triggerEventHandler('click', { });
+      expect(host.searchHandler).toHaveBeenCalledWith('some query (click)');
+    });
+  });
+
+  describe('event filtering', () => {
+    it('should only send events if the search value has changed', () => {
+      const input = fixture.debugElement.query(By.css('input'));
+
+      input.nativeElement.value = 'some query';
+      input.triggerEventHandler('input', { });
+      expect(host.searchHandler).toHaveBeenCalledTimes(1);
+
+      input.triggerEventHandler('input', { });
+      expect(host.searchHandler).toHaveBeenCalledTimes(1);
+
+      input.nativeElement.value = 'some other query';
+      input.triggerEventHandler('input', { });
+      expect(host.searchHandler).toHaveBeenCalledTimes(2);
     });
   });
 
