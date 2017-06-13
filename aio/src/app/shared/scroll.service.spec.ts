dd3ee1d334f685a1f93fc36992bb3d2ea50192d8
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-06-13 13:14:58 +0300

    fix(aio): fix scrolling to elements near the bottom of the page
    Previously, we always assumed that elements would be scrolled to the top of the
    page, when calling `element.scrollIntoView()`. This is not true for elements
    that cannot be scrolled to the top, e.g. when the viewport height is larger than
    the height of the content after the element (common for small sections near the
    end of the page).
    In such cases, we would unnecessarily scroll up to account for the static
    toolbar, which was unnecessary (since the element was not behind the toolbar
    anyway) and caused ScrollSpy to fail to identify the scrolled-to section as
    active.
    
    This commit fixes it by ensuring that we do not scroll more than necessary in
    order to align the top of the element with the bottom of the toolbar.
    
    Fixes #17452

--- aio/src/app/shared/scroll.service.spec.ts
+++ aio/src/app/shared/scroll.service.spec.ts
@@ -22,6 +22,8 @@ describe('ScrollService', () => {
   }
 
   class MockElement {
+    getBoundingClientRect = jasmine.createSpy('Element getBoundingClientRect')
+                                   .and.returnValue({top: 0});
     scrollIntoView = jasmine.createSpy('Element scrollIntoView');
   }
 
@@ -142,6 +144,22 @@ describe('ScrollService', () => {
       scrollService.scrollToElement(element);
       expect(element.scrollIntoView).toHaveBeenCalled();
       expect(window.scrollBy).toHaveBeenCalledWith(0, -scrollService.topOffset);
+    });
+
+    it('should not scroll more than necessary (e.g. for elements close to the bottom)', () => {
+      const element: Element = new MockElement() as any;
+      const getBoundingClientRect = element.getBoundingClientRect as jasmine.Spy;
+      const topOffset = scrollService.topOffset;
+
+      getBoundingClientRect.and.returnValue({top: topOffset + 100});
+      scrollService.scrollToElement(element);
+      expect(element.scrollIntoView).toHaveBeenCalledTimes(1);
+      expect(window.scrollBy).toHaveBeenCalledWith(0, 100);
+
+      getBoundingClientRect.and.returnValue({top: topOffset - 10});
+      scrollService.scrollToElement(element);
+      expect(element.scrollIntoView).toHaveBeenCalledTimes(2);
+      expect(window.scrollBy).toHaveBeenCalledWith(0, -10);
     });
 
     it('should scroll all the way to the top if close enough', () => {
