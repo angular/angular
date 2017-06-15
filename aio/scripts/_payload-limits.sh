c7bbf0573aa5cb8976767b89b77233309aebf866
Yuan Gao <tinagao@google.com>
2017-06-15 10:29:24 -0700

    ci(aio): updated limits

--- aio/scripts/_payload-limits.sh
+++ aio/scripts/_payload-limits.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 set -u -e -o pipefail
 
 declare -A limitUncompressed
-limitUncompressed=(["0"]=16000 ["inline"]=1600 ["main"]=360000 ["polyfills"]=40000 ["vendor"]=520000)
+limitUncompressed=(["inline"]=1600 ["main"]=600000 ["polyfills"]=40000)
 declare -A limitGzip7
-limitGzip7=(["0"]=7000 ["inline"]=1000 ["main"]=60000 ["polyfills"]=13000 ["vendor"]=120000)
+limitGzip7=(["inline"]=1000 ["main"]=140000 ["polyfills"]=12000)
 declare -A limitGzip9
-limitGzip9=(["0"]=7000 ["inline"]=1000 ["main"]=60000 ["polyfills"]=13000 ["vendor"]=120000)
+limitGzip9=(["inline"]=1000 ["main"]=140000 ["polyfills"]=12000)
