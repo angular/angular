75a14c010964e7c1e23959170264430c058b9f2d
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-06-17 21:24:16 +0300

    refactor(aio): simplify preview server build events

--- aio/aio-builds-setup/dockerbuild/scripts-js/lib/upload-server/build-events.ts
+++ aio/aio-builds-setup/dockerbuild/scripts-js/lib/upload-server/build-events.ts
@@ -1,15 +1,8 @@
 // Classes
-export class BuildEvent {
-  // Constructor
-  constructor(public type: string, public pr: number, public sha: string) {}
-}
-
-export class CreatedBuildEvent extends BuildEvent {
+export class CreatedBuildEvent {
   // Properties - Public, Static
   public static type = 'build.created';
 
   // Constructor
-  constructor(pr: number, sha: string) {
-    super(CreatedBuildEvent.type, pr, sha);
-  }
+  constructor(public pr: number, public sha: string) {}
 }
