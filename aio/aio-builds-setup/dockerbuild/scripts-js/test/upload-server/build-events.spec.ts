0fe685102fd92d32c16169e639ac51f0def508b0
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-06-17 21:24:16 +0300

    refactor(aio): simplify preview server build events

--- aio/aio-builds-setup/dockerbuild/scripts-js/test/upload-server/build-events.spec.ts
+++ aio/aio-builds-setup/dockerbuild/scripts-js/test/upload-server/build-events.spec.ts
@@ -1,30 +1,7 @@
 // Imports
-import {BuildEvent, CreatedBuildEvent} from '../../lib/upload-server/build-events';
+import {CreatedBuildEvent} from '../../lib/upload-server/build-events';
 
 // Tests
-describe('BuildEvent', () => {
-  let evt: BuildEvent;
-
-  beforeEach(() => evt = new BuildEvent('foo', 42, 'bar'));
-
-
-  it('should have a \'type\' property', () => {
-    expect(evt.type).toBe('foo');
-  });
-
-
-  it('should have a \'pr\' property', () => {
-    expect(evt.pr).toBe(42);
-  });
-
-
-  it('should have a \'sha\' property', () => {
-    expect(evt.sha).toBe('bar');
-  });
-
-});
-
-
 describe('CreatedBuildEvent', () => {
   let evt: CreatedBuildEvent;
 
@@ -33,19 +10,6 @@ describe('CreatedBuildEvent', () => {
 
   it('should have a static \'type\' property', () => {
     expect(CreatedBuildEvent.type).toBe('build.created');
-  });
-
-
-  it('should extend BuildEvent', () => {
-    expect(evt).toEqual(jasmine.any(CreatedBuildEvent));
-    expect(evt).toEqual(jasmine.any(BuildEvent));
-
-    expect(Object.getPrototypeOf(evt)).toBe(CreatedBuildEvent.prototype);
-  });
-
-
-  it('should automatically set the \'type\'', () => {
-    expect(evt.type).toBe(CreatedBuildEvent.type);
   });
 
 
