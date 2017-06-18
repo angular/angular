8abc3995ea943cf195fad7329809fb2948671ccd
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-06-19 01:15:07 +0300

    feat(aio): enable previews for any PR
    This commit introduces the ability to show previews for PRs by any author. It works as follows:
    
    - The build artifacts of all PRs are uploaded to the preview server.
    - Automatically verified PRs (i.e. from trusted authors or having a specific label) are deployed and
      publicly accessible as usual.
    - PRs that could not be automatically verified are stored for later use (after re-verification).
    - A PR can be marked as "trusted" and make its preview publicly accessible by adding the GitHub
      label specified in the `AIO_TRUSTED_PR_LABEL` env var of the preview server.
    
    At the moment, there is no automatic mechanism for notifying the preview server about changes to the
    PR's verification status. The PR's "visibility" will be checked and updated every time a new build
    is uploaded.

--- aio/aio-builds-setup/dockerbuild/scripts-js/lib/upload-server/index-test.ts
+++ aio/aio-builds-setup/dockerbuild/scripts-js/lib/upload-server/index-test.ts
@@ -1,10 +1,10 @@
 // Imports
 import {GithubPullRequests} from '../common/github-pull-requests';
-import {BuildVerifier} from './build-verifier';
+import {BUILD_VERIFICATION_STATUS, BuildVerifier} from './build-verifier';
 
 // Run
 // TODO(gkalpak): Add e2e tests to cover these interactions as well.
 GithubPullRequests.prototype.addComment = () => Promise.resolve();
-BuildVerifier.prototype.verify = () => Promise.resolve();
+BuildVerifier.prototype.verify = () => Promise.resolve(BUILD_VERIFICATION_STATUS.verifiedAndTrusted);
 // tslint:disable-next-line: no-var-requires
 require('./index');
