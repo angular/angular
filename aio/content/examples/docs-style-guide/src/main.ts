4b3d5247f79ebd8de26d2867351a92099ab96caf
Ward Bell <wardbell@hotmail.com>
2017-06-19 23:32:30 -0700

    docs(aio): ward's changes

---
+++ aio/content/examples/docs-style-guide/src/main.ts
@@ -0,0 +1,4 @@
+// #docregion
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
