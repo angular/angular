b754e600e30476a22e29019d89df71dd9b7fcbd5
Victor Berchet <victor@suumit.com>
2017-06-21 16:53:37 -0700

    feat(compiler): add support `::ng-deep`
    - /deep/ is deprecated and being removed from Chrome
    - >>> is semantically invalid in a stylesheet
    - sass will no longer support either in any version of sass
    
    -> use ::ng-deep in emulated shadow DOM mode
    
    Because the deep combinator is deprecated in the CSS spec,
    `/deep/`, `>>>` and `::ng-deep` are also deprecated in emulated shadow DOM mode
    and will be removed in the future.
    
    see https://www.chromestatus.com/features/6750456638341120

--- packages/compiler/src/shadow_css.ts
+++ packages/compiler/src/shadow_css.ts
@@ -513,7 +513,11 @@ const _shadowDOMSelectorsRe = [
   /\/shadow-deep\//g,
   /\/shadow\//g,
 ];
-const _shadowDeepSelectors = /(?:>>>)|(?:\/deep\/)/g;
+
+// The deep combinator is deprecated in the CSS spec
+// Support for `>>>`, `deep`, `::ng-deep` is then also deprecated and will be removed in the future.
+// see https://github.com/angular/angular/pull/17677
+const _shadowDeepSelectors = /(?:>>>)|(?:\/deep\/)|(?:::ng-deep)/g;
 const _selectorReSuffix = '([>\\s~+\[.,{:][\\s\\S]*)?$';
 const _polyfillHostRe = /-shadowcsshost/gim;
 const _colonHostRe = /:host/gim;
