12a20992658a039a0e974813c465d046bf131bc8
Igor Minar <igor@angularjs.org>
2017-06-24 00:08:18 -0700

    fix(router): export missing UrlMatcher and UrlMatchResult types
    Fixes #15140

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -7,7 +7,7 @@
  */
 
 
-export {Data, LoadChildren, LoadChildrenCallback, ResolveData, Route, Routes, RunGuardsAndResolvers} from './config';
+export {Data, LoadChildren, LoadChildrenCallback, ResolveData, Route, Routes, RunGuardsAndResolvers, UrlMatchResult, UrlMatcher} from './config';
 export {RouterLink, RouterLinkWithHref} from './directives/router_link';
 export {RouterLinkActive} from './directives/router_link_active';
 export {RouterOutlet} from './directives/router_outlet';
