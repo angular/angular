cf828046c0f17555220cdd800dcf9d9e90f08196
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-30 13:54:08 -0700

    refactor: move DOCUMENT from platform/browser to common

--- packages/common/src/dom_tokens.ts
+++ packages/common/src/dom_tokens.ts
@@ -8,14 +8,12 @@
 
 import {InjectionToken} from '@angular/core';
 
-import {DOCUMENT as commonDOCUMENT} from '@angular/common';
-
 /**
  * A DI Token representing the main rendering context. In a browser this is the DOM Document.
  *
  * Note: Document might not be available in the Application Context when Application and Rendering
  * Contexts are not the same (e.g. when running the application into a Web Worker).
  *
- * @deprecated, import from `@angular/common` instead.
+ * @stable
  */
-export const DOCUMENT = commonDOCUMENT;
+export const DOCUMENT = new InjectionToken<Document>('DocumentToken');
