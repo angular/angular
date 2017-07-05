8ae4d9d0855826b17a254817412e7201204ccf1f
Jeremy Elbourn <jelbourn@gmail.com>
2017-07-05 10:35:22 -0700

    build: add flatModuleIndexRedirect to generated metadata redirect (#5512)

--- tools/package-tools/metadata-reexport.ts
+++ tools/package-tools/metadata-reexport.ts
@@ -3,7 +3,11 @@ import {join} from 'path';
 
 /** Creates a metadata file that re-exports the metadata bundle inside of the typings. */
 export function createMetadataReexportFile(packageDir: string, packageName: string) {
-  const metadataReExport =
-    `{"__symbolic":"module","version":3,"metadata":{},"exports":[{"from":"./typings/index"}]}`;
+  const metadataReExport = `{
+    "__symbolic":"module",
+    "version":3,"metadata":{},
+    "exports":[{"from":"./typings/index"}],
+    "flatModuleIndexRedirect": true
+  }`;
   writeFileSync(join(packageDir, `${packageName}.metadata.json`), metadataReExport, 'utf-8');
 }
