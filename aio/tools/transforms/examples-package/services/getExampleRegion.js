2738829f5719ecec92ae1647470607b029085f28
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-06 12:17:57 +0100

    build(aio): abort doc-gen if an example is missing
    Closes #16936

--- aio/tools/transforms/examples-package/services/getExampleRegion.js
+++ aio/tools/transforms/examples-package/services/getExampleRegion.js
@@ -1,4 +1,4 @@
-module.exports = function getExampleRegion(exampleMap, createDocMessage, log, collectExamples) {
+module.exports = function getExampleRegion(exampleMap, createDocMessage, collectExamples) {
   return function getExampleRegionImpl(doc, relativePath, regionName) {
     const EXAMPLES_FOLDERS = collectExamples.exampleFolders;
 
@@ -14,16 +14,16 @@ module.exports = function getExampleRegion(exampleMap, createDocMessage, log, co
 
     // If still no file then we error
     if (!exampleFile) {
-      log.error(createDocMessage('Missing example file... relativePath: "' + relativePath + '".', doc));
-      log.error('Example files can be found in: ' + EXAMPLES_FOLDERS.join(', '));
-      return '';
+      const message = createDocMessage('Missing example file... relativePath: "' + relativePath + '".', doc) + '\n' +
+                      'Example files can be found in: ' + EXAMPLES_FOLDERS.join(', ');
+      throw new Error(message);
     }
 
     var sourceCodeDoc = exampleFile.regions[regionName || ''];
     if (!sourceCodeDoc) {
-      log.error(createDocMessage('Missing example region... relativePath: "' + relativePath + '", region: "' + regionName + '".', doc));
-      log.error('Regions available are:', Object.keys[exampleFile.regions]);
-      return '';
+      const message = createDocMessage('Missing example region... relativePath: "' + relativePath + '", region: "' + regionName + '".', doc) + '\n' +
+                      'Regions available are:' + Object.keys[exampleFile.regions];
+      throw new Error(message);
     }
 
     return sourceCodeDoc.renderedContent;
