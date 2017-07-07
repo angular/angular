f4aec4ad128f77a21235df0009c98c429b46d00f
George Kalpakas <kalpakas.g@gmail.com>
2017-07-07 21:17:19 +0300

    fix(aio): activate ServiceWorker updates asap (#17699)
    Previouly, whenever a new ServiceWorker update was detected the user was
    prompted to update (with a notification). This turned out to be more distracting
    than helpful. Also, one would get notifications on all open browser tabs/windows
    and had to manually reload each one in order for the whole content (including
    the app) to be updated.
    
    This commit changes the update strategy as follows:
    - Whenever a new update is detected, it is immediately activated (and all
      tabs/windows will be notified).
    - Once an update is activated (regardless of whether the activation was
      initiated by the current tab/window or not), a flag will be set to do a
      "full page navigation" the next time the user navigates to a document.
    
    Benefits:
    - All tabs/windows are updated asap.
    - The updates are applied authomatically, without the user's needing to do
      anything.
    - The updates are applied in a way that:
      a. Ensures that the app and content versions are always compatible.
      b. Does not distract the user from their usual workflow.
    
    NOTE:
    The "full page navigation" may cause a flash (while the page is loading from
    scratch), but this is expected to be minimal, since at that point almost all
    necessary resources are cached by and served from the ServiceWorker.
    
    Fixes #17539

--- aio/src/app/sw-updates/sw-updates.service.spec.ts
+++ aio/src/app/sw-updates/sw-updates.service.spec.ts
@@ -4,6 +4,7 @@ import { NgServiceWorker } from '@angular/service-worker';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/take';
 
+import { Logger } from 'app/shared/logger.service';
 import { SwUpdatesService } from './sw-updates.service';
 
 describe('SwUpdatesService', () => {
@@ -14,11 +15,13 @@ describe('SwUpdatesService', () => {
 
   // Helpers
   // NOTE:
-  //  Because `SwUpdatesService` uses the `debounceTime` operator, it needs to be instantiated
-  //  inside the `fakeAsync` zone (when `fakeAsync` is used for the test). Thus, we can't run
-  //  `setup()` in a `beforeEach()` block. We use the `run()` helper to call it inside each test' zone.
+  //   Because `SwUpdatesService` uses the `debounceTime` operator, it needs to be instantiated
+  //   inside the `fakeAsync` zone (when `fakeAsync` is used for the test). Thus, we can't run
+  //   `setup()` in a `beforeEach()` block. We use the `run()` helper to call `setup()` inside each
+  //   test's zone.
   const setup = () => {
     injector = ReflectiveInjector.resolveAndCreate([
+      { provide: Logger, useClass: MockLogger },
       { provide: NgServiceWorker, useClass: MockNgServiceWorker },
       SwUpdatesService
     ]);
@@ -39,7 +42,7 @@ describe('SwUpdatesService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should immediatelly check for updates when instantiated', run(() => {
+  it('should immediately check for updates when instantiated', run(() => {
     expect(sw.checkForUpdate).toHaveBeenCalled();
   }));
 
@@ -51,9 +54,10 @@ describe('SwUpdatesService', () => {
 
     tick(checkInterval);
     expect(sw.checkForUpdate).toHaveBeenCalled();
+    expect(sw.activateUpdate).not.toHaveBeenCalled();
   })));
 
-  it('should not schedule a new check if there is an update available', fakeAsync(run(() => {
+  it('should activate new updates immediately', fakeAsync(run(() => {
     sw.checkForUpdate.calls.reset();
 
     sw.$$checkForUpdateSubj.next(true);
@@ -61,153 +65,92 @@ describe('SwUpdatesService', () => {
 
     tick(checkInterval);
     expect(sw.checkForUpdate).not.toHaveBeenCalled();
+    expect(sw.activateUpdate).toHaveBeenCalled();
   })));
 
-  describe('#activateUpdate()', () => {
-    it('should return a promise', run(() => {
-      expect(service.activateUpdate()).toEqual(jasmine.any(Promise));
-    }));
+  it('should not pass a specific version to `NgServiceWorker.activateUpdate()`', fakeAsync(run(() => {
+    sw.$$checkForUpdateSubj.next(true);
+    tick(checkInterval);
 
-    it('should call `NgServiceWorker.activateUpdate()`', run(() => {
-      expect(sw.activateUpdate).not.toHaveBeenCalled();
+    expect(sw.activateUpdate).toHaveBeenCalledWith(null);
+  })));
 
-      service.activateUpdate();
+  it('should schedule a new check after activating the update', fakeAsync(run(() => {
+    sw.checkForUpdate.calls.reset();
+    sw.$$checkForUpdateSubj.next(true);
+
+    tick(checkInterval);
+    expect(sw.checkForUpdate).not.toHaveBeenCalled();
+
+    sw.$$activateUpdateSubj.next();
+    expect(sw.checkForUpdate).not.toHaveBeenCalled();
+
+    tick(checkInterval);
+    expect(sw.checkForUpdate).toHaveBeenCalled();
+  })));
+
+  it('should emit on `updateActivated` when an update has been activated', run(() => {
+    const activatedVersions: string[] = [];
+    service.updateActivated.subscribe(v => activatedVersions.push(v));
+
+    sw.$$updatesSubj.next({type: 'pending', version: 'foo'});
+    sw.$$updatesSubj.next({type: 'activation', version: 'bar'});
+    sw.$$updatesSubj.next({type: 'pending', version: 'baz'});
+    sw.$$updatesSubj.next({type: 'activation', version: 'qux'});
+
+    expect(activatedVersions).toEqual(['bar', 'qux']);
+  }));
+
+  describe('when destroyed', () => {
+    it('should not schedule a new check for update (after current check)', fakeAsync(run(() => {
+      sw.checkForUpdate.calls.reset();
+
+      service.ngOnDestroy();
+      sw.$$checkForUpdateSubj.next(false);
+      tick(checkInterval);
+
+      expect(sw.checkForUpdate).not.toHaveBeenCalled();
+    })));
+
+    it('should not schedule a new check for update (after activating an update)', fakeAsync(run(() => {
+      sw.checkForUpdate.calls.reset();
+
+      sw.$$checkForUpdateSubj.next(true);
       expect(sw.activateUpdate).toHaveBeenCalled();
+
+      service.ngOnDestroy();
+      sw.$$activateUpdateSubj.next();
+      tick(checkInterval);
+
+      expect(sw.checkForUpdate).not.toHaveBeenCalled();
+    })));
+
+    it('should stop emitting on `updateActivated`', run(() => {
+      const activatedVersions: string[] = [];
+      service.updateActivated.subscribe(v => activatedVersions.push(v));
+
+      sw.$$updatesSubj.next({type: 'pending', version: 'foo'});
+      sw.$$updatesSubj.next({type: 'activation', version: 'bar'});
+      service.ngOnDestroy();
+      sw.$$updatesSubj.next({type: 'pending', version: 'baz'});
+      sw.$$updatesSubj.next({type: 'activation', version: 'qux'});
+
+      expect(activatedVersions).toEqual(['bar']);
     }));
-
-    it('should not pass a specific version to `NgServiceWorker.activateUpdate()`', run(() => {
-      (service.activateUpdate as Function)('foo');
-      expect(sw.activateUpdate).toHaveBeenCalledWith(null);
-    }));
-
-    it('should resolve the promise with the activation outcome', fakeAsync(run(() => {
-      let outcome;
-
-      service.activateUpdate().then(v => outcome = v);
-      sw.$$activateUpdateSubj.next(true);
-      tick();
-      expect(outcome).toBe(true);
-
-      service.activateUpdate().then(v => outcome = v);
-      sw.$$activateUpdateSubj.next(false);
-      tick();
-      expect(outcome).toBe(false);
-    })));
-
-    it('should schedule a new check (if the activation succeeded)', fakeAsync(run(() => {
-      sw.checkForUpdate.calls.reset();
-
-      service.activateUpdate();
-
-      tick(checkInterval);
-      expect(sw.checkForUpdate).not.toHaveBeenCalled();
-
-      sw.$$activateUpdateSubj.next(true);
-      expect(sw.checkForUpdate).not.toHaveBeenCalled();
-
-      tick(checkInterval);
-      expect(sw.checkForUpdate).toHaveBeenCalled();
-    })));
-
-    it('should schedule a new check (if the activation failed)', fakeAsync(run(() => {
-      sw.checkForUpdate.calls.reset();
-
-      service.activateUpdate();
-
-      tick(checkInterval);
-      expect(sw.checkForUpdate).not.toHaveBeenCalled();
-
-      sw.$$activateUpdateSubj.next(false);
-      expect(sw.checkForUpdate).not.toHaveBeenCalled();
-
-      tick(checkInterval);
-      expect(sw.checkForUpdate).toHaveBeenCalled();
-    })));
-  });
-
-  describe('#isUpdateAvailable', () => {
-    let emittedValues: boolean[];
-
-    // Helpers
-    const withSubscription = specFn => () => {
-      emittedValues = [];
-      service.isUpdateAvailable.subscribe(v => emittedValues.push(v));
-      specFn();
-    };
-
-
-    it('should emit `false/true` when there is/isn\'t an update available',
-      fakeAsync(run(withSubscription(() => {
-        expect(emittedValues).toEqual([]);
-
-        sw.$$checkForUpdateSubj.next(false);
-        expect(emittedValues).toEqual([false]);
-
-        tick(checkInterval);
-        sw.$$checkForUpdateSubj.next(true);
-        expect(emittedValues).toEqual([false, true]);
-      })))
-    );
-
-    it('should emit only when the value has changed',
-      fakeAsync(run(withSubscription(() => {
-        expect(emittedValues).toEqual([]);
-
-        sw.$$checkForUpdateSubj.next(false);
-        expect(emittedValues).toEqual([false]);
-
-        tick(checkInterval);
-        sw.$$checkForUpdateSubj.next(false);
-        expect(emittedValues).toEqual([false]);
-
-        tick(checkInterval);
-        sw.$$checkForUpdateSubj.next(false);
-        expect(emittedValues).toEqual([false]);
-      })))
-    );
-
-    it('should emit `false` after a successful activation',
-      fakeAsync(run(withSubscription(() => {
-        sw.$$checkForUpdateSubj.next(true);
-        expect(emittedValues).toEqual([true]);
-
-        service.activateUpdate();
-        sw.$$activateUpdateSubj.next(true);
-
-        expect(emittedValues).toEqual([true, false]);
-      })))
-    );
-
-    it('should emit `false` after a failed activation',
-      fakeAsync(run(withSubscription(() => {
-        sw.$$checkForUpdateSubj.next(true);
-        expect(emittedValues).toEqual([true]);
-
-        service.activateUpdate();
-        sw.$$activateUpdateSubj.next(false);
-
-        expect(emittedValues).toEqual([true, false]);
-      })))
-    );
-
-    it('should not emit a new value after activation if already `false`',
-      fakeAsync(run(withSubscription(() => {
-        sw.$$checkForUpdateSubj.next(false);
-        expect(emittedValues).toEqual([false]);
-
-        service.activateUpdate();
-        sw.$$activateUpdateSubj.next(true);
-
-        expect(emittedValues).toEqual([false]);
-      })))
-    );
   });
 });
 
 // Mocks
+class MockLogger {
+  log = jasmine.createSpy('MockLogger.log');
+}
+
 class MockNgServiceWorker {
   $$activateUpdateSubj = new Subject<boolean>();
   $$checkForUpdateSubj = new Subject<boolean>();
+  $$updatesSubj = new Subject<{type: string, version: string}>();
+
+  updates = this.$$updatesSubj.asObservable();
 
   activateUpdate = jasmine.createSpy('MockNgServiceWorker.activateUpdate')
                           .and.callFake(() => this.$$activateUpdateSubj.take(1));
