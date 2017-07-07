ff8526d5c1d2c212d412f4376b2809b69700165e
Filipe Silva <filipematossilva@gmail.com>
2017-07-08 00:34:47 +0100

    feat(aio): use new ngo (#17977)

--- aio/tools/cli-patches/patch.js
+++ aio/tools/cli-patches/patch.js
@@ -4,8 +4,8 @@ const sh = require('shelljs');
 PATCH_LOCK = 'node_modules/@angular/cli/models/webpack-configs/.patched';
 
 if (!fs.existsSync(PATCH_LOCK)) {
-  sh.exec('patch -p0 -i tools/cli-patches/ngo-loader.patch');
-  sh.exec('patch -p0 -i node_modules/purify/angular-cli.patch');
+  sh.exec('patch -p0 -i tools/cli-patches/ngo.patch');
+  sh.exec('patch -p0 -i tools/cli-patches/purify.patch');
   sh.exec('patch -p0 -i tools/cli-patches/scope-hoisting.patch');
   sh.exec('patch -p0 -i tools/cli-patches/uglify-config.patch');
   sh.touch(PATCH_LOCK);
