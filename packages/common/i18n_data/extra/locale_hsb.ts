a73389bc717be20e93a173d9bcfab3ccd02e8252
Olivier Combe <olivier.combe@gmail.com>
2017-07-20 11:00:44 +0200

    build(common): add generated i18n locale data files (#18284)
    PR Close #18284

---
+++ packages/common/i18n_data/extra/locale_hsb.ts
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+// THIS CODE IS GENERATED - DO NOT MODIFY
+// See angular/tools/gulp-tasks/cldr/extract.js
+
+export default [];
