cbe4b6c9a3b94017eeea3447c42a5c066ff6bf88
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-22 06:59:50 +0100

    ci: short-circuit npm install for aio builds that use yarn only

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -28,17 +28,18 @@ mkdir -p ${LOGS_DIR}
 # Install node
 #nvm install ${NODE_VERSION}
 
+if [[ ${CI_MODE} != "aio" && ${CI_MODE} != 'docs_test' ]]; then
+  # Install version of npm that we are locked against
+  travisFoldStart "install-npm"
+    npm install -g npm@${NPM_VERSION}
+  travisFoldEnd "install-npm"
 
-# Install version of npm that we are locked against
-travisFoldStart "install-npm"
-  npm install -g npm@${NPM_VERSION}
-travisFoldEnd "install-npm"
 
-
-# Install all npm dependencies according to shrinkwrap.json
-travisFoldStart "npm-install"
-  node tools/npm/check-node-modules --purge || npm install
-travisFoldEnd "npm-install"
+  # Install all npm dependencies according to shrinkwrap.json
+  travisFoldStart "npm-install"
+    node tools/npm/check-node-modules --purge || npm install
+  travisFoldEnd "npm-install"
+fi
 
 
 if [[ ${TRAVIS} && (${CI_MODE} == "e2e" || ${CI_MODE} == "e2e_2" || ${CI_MODE} == "aio" || ${CI_MODE} == "aio_e2e" || ${CI_MODE} == "docs_test") ]]; then
