e79dc291cdf890ad113701fd8f3b1da715e729e5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-07-27 18:48:22 +0200

    build: missing semicolon for typings re-exports (#6057)
    * Adds a missing semicolon to the typing re-export statements.

--- tools/package-tools/typings-reexport.ts
+++ tools/package-tools/typings-reexport.ts
@@ -4,8 +4,7 @@ import {join} from 'path';
 
 /** Create a typing file that links to the bundled definitions of NGC. */
 export function createTypingsReexportFile(outDir: string, from: string, fileName: string) {
-  writeFileSync(
-      join(outDir, `${fileName}.d.ts`),
-      `${buildConfig.licenseBanner}\nexport * from '${from}'`,
-      'utf-8');
+  writeFileSync(join(outDir, `${fileName}.d.ts`),
+    `${buildConfig.licenseBanner}\nexport * from '${from}';\n`,
+    'utf-8');
 }
