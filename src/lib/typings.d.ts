c9d67c08defcbf23cc71cbb535d87f2f57c543f7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-07-28 17:00:44 +0200

    chore: remove unused jasmine matcher types (#6089)
    * Removes unused jasmine matcher type definitions since the `jasmine-matchers.ts` file has been removed.

--- src/lib/typings.d.ts
+++ src/lib/typings.d.ts
@@ -1,9 +1,1 @@
 declare var module: {id: string};
-
-/** Extends the interface for jasmine matchers to allow for custom matchers. */
-declare namespace jasmine {
-  interface Matchers {
-    toBeRole(expectedRole: string): boolean;
-    toMatchTableContent(expectedContent: any[]): boolean;
-  }
-}
