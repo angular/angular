4afcc0a7271a46ea02c45ce8207f2fa53f9822af
Andrew Seguin <andrewseguin@users.noreply.github.com>
2017-08-01 12:49:47 -0700

    fix(table): Fix change det issue with http example (#6169)
    * fix(table): Fix change det issue with http example
    
    * Update table-http-example.ts

--- src/material-examples/table-http/table-http-example.ts
+++ src/material-examples/table-http/table-http-example.ts
@@ -68,8 +68,8 @@ export class ExampleHttpDao {
 export class ExampleDataSource extends DataSource<GithubIssue> {
   // The number of issues returned by github matching the query.
   resultsLength = 0;
-  isLoadingResults: boolean;
-  isRateLimitReached: boolean;
+  isLoadingResults = false;
+  isRateLimitReached = false;
 
   constructor(private exampleDatabase: ExampleHttpDao,
               private paginator: MdPaginator,
