51eca3b1d09409cfb4364523a04db66dea7fbf0a
Austin <amcdaniel2@gmail.com>
2017-08-01 14:49:27 -0500

    demos(*): fix inputs spilling over when screen width small (#6155)
    * demos(*): fix inputs spilling over when screen width small
    
    * chore(*): fix line break

--- src/material-examples/input-overview/input-overview-example.ts
+++ src/material-examples/input-overview/input-overview-example.ts
@@ -5,6 +5,7 @@ import {Component} from '@angular/core';
  */
 @Component({
   selector: 'input-overview-example',
+  styleUrls: ['input-overview-example.css'],
   templateUrl: 'input-overview-example.html',
 })
 export class InputOverviewExample {}
