0dc57f0344f6429de003e8ade869d4b1926ab1c9
Kara Erickson <karakara@google.com>
2017-08-01 16:09:57 -0700

    docs(forms): update and re-organize validation guide

--- aio/content/examples/form-validation/src/app/shared/forbidden-name.directive.ts
+++ aio/content/examples/form-validation/src/app/shared/forbidden-name.directive.ts
@@ -6,9 +6,8 @@ import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, Validators } fr
 /** A hero's name can't match the given regular expression */
 export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} => {
-    const name = control.value;
-    const no = nameRe.test(name);
-    return no ? {'forbiddenName': {name}} : null;
+    const forbidden = nameRe.test(control.value);
+    return forbidden ? {'forbiddenName': {value: control.value}} : null;
   };
 }
 // #enddocregion custom-validator
