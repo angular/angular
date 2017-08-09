65e26d713ccb3c665c0e07954a0d6c96ce56731a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-09 11:28:53 -0700

    fix(common): fix improper packaging for @angular/common/http (#18613)
    PR Close #18613

--- packages/common/http/rollup.config.js
+++ packages/common/http/rollup.config.js
@@ -6,16 +6,25 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+const globals = {
+  '@angular/core': 'ng.core',
+  '@angular/platform-browser': 'ng.platformBrowser',
+  'rxjs/Observable': 'Rx',
+  'rxjs/Subject': 'Rx',
+
+  'rxjs/observable/of': 'Rx.Observable.prototype',
+
+  'rxjs/operator/concatMap': 'Rx.Observable.prototype',
+  'rxjs/operator/filter': 'Rx.Observable.prototype',
+  'rxjs/operator/map': 'Rx.Observable.prototype',
+};
+
 export default {
   entry: '../../../dist/packages-dist/common/@angular/common/http.es5.js',
   dest: '../../../dist/packages-dist/common/bundles/common-http.umd.js',
   format: 'umd',
   exports: 'named',
-  moduleName: 'ng.commmon.http',
-  globals: {
-    '@angular/core': 'ng.core',
-    '@angular/platform-browser': 'ng.platformBrowser',
-    'rxjs/Observable': 'Rx',
-    'rxjs/Subject': 'Rx'
-  }
+  moduleName: 'ng.common.http',
+  external: Object.keys(globals),
+  globals: globals
 };
