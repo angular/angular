954742bae3454d02fa0ca118c3a8bb77ad162261
Kara Erickson <karakara@google.com>
2017-08-09 16:39:49 -0700

    perf(aio): update to new version of build-optimizer

--- aio/scripts/_payload-limits.sh
+++ aio/scripts/_payload-limits.sh
@@ -3,7 +3,7 @@
 set -u -e -o pipefail
 
 declare -A limitUncompressed
-limitUncompressed=(["inline"]=1600 ["main"]=600000 ["polyfills"]=35000)
+limitUncompressed=(["inline"]=1600 ["main"]=550000 ["polyfills"]=35000)
 declare -A limitGzip7
 limitGzip7=(["inline"]=1000 ["main"]=140000 ["polyfills"]=12500)
 declare -A limitGzip9
