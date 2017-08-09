c15604e1f1eb5ef43f583aaee57cc67018c7c50c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2017-08-09 14:29:17 +0200

    feat(common): mark NgTemplateOutlet API as stable

--- packages/common/src/directives/ng_template_outlet.ts
+++ packages/common/src/directives/ng_template_outlet.ts
@@ -30,7 +30,7 @@ import {Directive, EmbeddedViewRef, Input, OnChanges, SimpleChange, SimpleChange
  *
  * {@example common/ngTemplateOutlet/ts/module.ts region='NgTemplateOutlet'}
  *
- * @experimental
+ * @stable
  */
 @Directive({selector: '[ngTemplateOutlet]'})
 export class NgTemplateOutlet implements OnChanges {
@@ -72,7 +72,7 @@ export class NgTemplateOutlet implements OnChanges {
    * - templateRef has changed
    * - context has changes
    *
-   * To mark context object as changed when the corresponding object
+   * We mark context object as changed when the corresponding object
    * shape changes (new properties are added or existing properties are removed).
    * In other words we consider context with the same properties as "the same" even
    * if object reference changes (see https://github.com/angular/angular/issues/13407).
