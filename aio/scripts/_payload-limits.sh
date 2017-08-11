1990c3c722a02e304be754f6949d5b9600a181d1
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-08-11 18:36:03 +0300

    build(aio): upgrade `zone.js` to 0.8.16
    ```
    $ ls -l dist/*.js
    
     14942            dist/0.b19e913fbdd6507d346b.chunk.js
      1535            dist/inline.a1b446562b36eebb766d.bundle.js
    523703            dist/main.19fec4390ff7837ee6ef.bundle.js
     37402  (+ 3088)  dist/polyfills.9f7e0e53bce2a6c8326e.bundle.js
     54001            dist/worker-basic.min.js
    
    631583  (+ 3088)  total
    ```

--- aio/scripts/_payload-limits.sh
+++ aio/scripts/_payload-limits.sh
@@ -3,7 +3,7 @@
 set -u -e -o pipefail
 
 declare -A limitUncompressed
-limitUncompressed=(["inline"]=1600 ["main"]=525000 ["polyfills"]=35000)
+limitUncompressed=(["inline"]=1600 ["main"]=525000 ["polyfills"]=38000)
 declare -A limitGzip7
 limitGzip7=(["inline"]=1000 ["main"]=127000 ["polyfills"]=12500)
 declare -A limitGzip9
