fbbe5b78c5bc2fdc5d52f1d1687dcf058f67741d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2017-08-18 12:13:17 +0200

    fix(compiler-cli): propagate preserveWhitespaces option to codegen

--- packages/compiler-cli/src/codegen.ts
+++ packages/compiler-cli/src/codegen.ts
@@ -103,6 +103,7 @@ export class CodeGenerator {
       locale: cliOptions.locale || undefined, missingTranslation,
       enableLegacyTemplate: options.enableLegacyTemplate !== false,
       enableSummariesForJit: options.enableSummariesForJit !== false,
+      preserveWhitespaces: options.preserveWhitespaces,
     });
     return new CodeGenerator(options, program, tsCompilerHost, aotCompiler, ngCompilerHost);
   }
