2229736c5e25588221bebf4be791ed172531e188
Olivier Combe <olivier.combe@gmail.com>
2017-08-18 14:54:55 +0200

    refactor(router): remove deprecated `initialNavigation` options (#18781)
    BREAKING CHANGE: the values `true`, `false`, `legacy_enabled` and `legacy_disabled` for the router parameter `initialNavigation` have been removed as they were deprecated. Use `enabled` or `disabled` instead.
    
    PR Close #18781

--- packages/router/src/router_module.ts
+++ packages/router/src/router_module.ts
@@ -218,24 +218,14 @@ export function provideRoutes(routes: Routes): any {
  * The bootstrap is blocked until the initial navigation is complete.
  * * 'disabled' - the initial navigation is not performed. The location listener is set up before
  * the root component gets created.
- * * 'legacy_enabled'- the initial navigation starts after the root component has been created.
- * The bootstrap is not blocked until the initial navigation is complete. @deprecated
- * * 'legacy_disabled'- the initial navigation is not performed. The location listener is set up
- * after @deprecated
- * the root component gets created.
- * * `true` - same as 'legacy_enabled'. @deprecated since v4
- * * `false` - same as 'legacy_disabled'. @deprecated since v4
  *
  * The 'enabled' option should be used for applications unless there is a reason to have
  * more control over when the router starts its initial navigation due to some complex
  * initialization logic. In this case, 'disabled' should be used.
  *
- * The 'legacy_enabled' and 'legacy_disabled' should not be used for new applications.
- *
  * @experimental
  */
-export type InitialNavigation =
-    true | false | 'enabled' | 'disabled' | 'legacy_enabled' | 'legacy_disabled';
+export type InitialNavigation = 'enabled' | 'disabled';
 
 /**
  * @whatItDoes Represents options to configure the router.
@@ -254,7 +244,7 @@ export interface ExtraOptions {
   useHash?: boolean;
 
   /**
-   * Disables the initial navigation.
+   * Enables/Disables the initial navigation (enabled by default).
    */
   initialNavigation?: InitialNavigation;
 
@@ -332,14 +322,12 @@ export class RouterInitializer {
       const router = this.injector.get(Router);
       const opts = this.injector.get(ROUTER_CONFIGURATION);
 
-      if (this.isLegacyDisabled(opts) || this.isLegacyEnabled(opts)) {
-        resolve(true);
-
-      } else if (opts.initialNavigation === 'disabled') {
+      if (opts.initialNavigation === 'disabled') {
         router.setUpLocationChangeListener();
         resolve(true);
 
-      } else if (opts.initialNavigation === 'enabled') {
+      } else if (
+          opts.initialNavigation === 'enabled' || typeof opts.initialNavigation === 'undefined') {
         router.hooks.afterPreactivation = () => {
           // only the initial navigation should be delayed
           if (!this.initNavigation) {
@@ -372,25 +360,10 @@ export class RouterInitializer {
       return;
     }
 
-    if (this.isLegacyEnabled(opts)) {
-      router.initialNavigation();
-    } else if (this.isLegacyDisabled(opts)) {
-      router.setUpLocationChangeListener();
-    }
-
     preloader.setUpPreloading();
     router.resetRootComponentType(ref.componentTypes[0]);
     this.resultOfPreactivationDone.next(null !);
     this.resultOfPreactivationDone.complete();
-  }
-
-  private isLegacyEnabled(opts: ExtraOptions): boolean {
-    return opts.initialNavigation === 'legacy_enabled' || opts.initialNavigation === true ||
-        opts.initialNavigation === undefined;
-  }
-
-  private isLegacyDisabled(opts: ExtraOptions): boolean {
-    return opts.initialNavigation === 'legacy_disabled' || opts.initialNavigation === false;
   }
 }
 
