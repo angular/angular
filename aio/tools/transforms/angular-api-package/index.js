dc7de0896c73d0aec2ab1677517af1ce755d6237
Peter Bacon Darwin <pete@bacondarwin.com>
2017-08-21 19:32:22 +0100

    build(aio): ensure dgeni can load all angular Typescript modules (#18927)
    PR Close #18927

--- aio/tools/transforms/angular-api-package/index.js
+++ aio/tools/transforms/angular-api-package/index.js
@@ -27,7 +27,11 @@ module.exports = new Package('angular-api', [basePackage, typeScriptPackage])
   .processor(require('./processors/simplifyMemberAnchors'))
 
   // Where do we get the source files?
-  .config(function(readTypeScriptModules, readFilesProcessor, collectExamples) {
+  .config(function(readTypeScriptModules, readFilesProcessor, collectExamples, tsParser) {
+
+    // Tell TypeScript how to load modules that start with with `@angular`
+    tsParser.options.paths = { '@angular/*': [API_SOURCE_PATH + '/*'] };
+    tsParser.options.baseUrl = '.';
 
     // API files are typescript
     readTypeScriptModules.basePath = API_SOURCE_PATH;
