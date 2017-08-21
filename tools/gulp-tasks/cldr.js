7ae909fecb091054e836ea06f9701eb39026d82e
Olivier Combe <olivier.combe@gmail.com>
2017-08-21 19:11:07 +0200

    feat(common): generate i18n data file `locale_all` file for closure
    Fixes #18763

--- tools/gulp-tasks/cldr.js
+++ tools/gulp-tasks/cldr.js
@@ -25,5 +25,15 @@ module.exports = {
       fs.mkdirSync(cldrDataFolder);
     }
     cldrDownloader(path.join(__dirname, 'cldr/cldr-urls.json'), cldrDataFolder, done);
-  }
+  },
+
+  'locale-all': gulp => done => {
+    const {RELATIVE_I18N_DATA_FOLDER} = require('./cldr/extract');
+    console.log(RELATIVE_I18N_DATA_FOLDER, fs.existsSync(RELATIVE_I18N_DATA_FOLDER));
+    if (!fs.existsSync(RELATIVE_I18N_DATA_FOLDER)) {
+      throw new Error(`You must run "gulp cldr:extract" before you can create the all.ts file`);
+    }
+    const localeAll = require('./cldr/locale-all');
+    return localeAll(gulp, done);
+  },
 };
