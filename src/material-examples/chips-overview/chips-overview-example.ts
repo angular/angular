ec4ea060a519282b362db0dd066aab54c4994cc1
Jeffrey Bosch <Jefiozie@users.noreply.github.com>
2017-08-24 19:21:33 +0200

    docs(chips): Added stylesUrls to reference css. (#6620)

--- src/material-examples/chips-overview/chips-overview-example.ts
+++ src/material-examples/chips-overview/chips-overview-example.ts
@@ -6,5 +6,6 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'chips-overview-example',
   templateUrl: 'chips-overview-example.html',
+  styleUrls: ['chips-overview-example.css'],
 })
 export class ChipsOverviewExample {}
