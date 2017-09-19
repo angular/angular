6b7453c9a27485d378f710a6e28126f2c436ce36
Igor Minar <igor@angularjs.org>
2017-09-19 09:30:23 -0700

    build: automatically version tsc-wrapped just like other packages
    This removes manual steps from the release list.
    
    I had to modify a few integration tests not to install compiler-cli dirrectly
    because it depends on tsc-wrapped which yarn was eager to fetch from npm rather
    than from the local build.

--- integration/run_tests.sh
+++ integration/run_tests.sh
@@ -24,6 +24,15 @@ for testDir in $(ls | grep -v node_modules) ; do
     # Workaround for https://github.com/yarnpkg/yarn/issues/2256
     rm -f yarn.lock
     yarn install --cache-folder ../$cache
+
+    # this is a workaround for resolving tsc-wrapped as the local version not available on npm
+    # tsc-wrapped is installed as a transitive dependency of compiler-cli
+    if [[ $testDir == "hello_world__closure" || $$testDir == "language_service_plugin" ]]; then
+      # compiler-cli needs to be installed while offline so that it doesn't try to look up tsc-wrapped version on npm
+      # it should instead use the version that is already installed in the project via package.json
+      yarn add --offline ../../dist/packages-dist/compiler-cli --cache-folder ../$cache
+    fi
+
     yarn test || exit 1
   )
 done
