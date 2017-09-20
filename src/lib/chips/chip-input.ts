3293a6d085b06bb0d90f9046f77a81a9411eaf4a
Andrew Seguin <andrewseguin@users.noreply.github.com>
2017-09-20 15:49:57 -0700

    compat(chip): add mat-compatible chip input event (#7184)

--- src/lib/chips/chip-input.ts
+++ src/lib/chips/chip-input.ts
@@ -68,6 +68,8 @@ export class MdChipInput {
   @Output('mdChipInputTokenEnd')
   chipEnd = new EventEmitter<MdChipInputEvent>();
 
+  @Output('matChipInputTokenEnd') _matChipInputTokenEnd = this.chipEnd;
+
   @Input('matChipInputFor')
   set matChipList(value: MdChipList) { this.chipList = value; }
 
