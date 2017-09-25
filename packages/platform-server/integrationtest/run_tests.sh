2d5ef15e085c70c349a27df25e82e82604a49c0a
Olivier Combe <olivier.combe@gmail.com>
2017-09-25 10:10:16 +0200

    build: switch from npm to yarn

--- packages/platform-server/integrationtest/run_tests.sh
+++ packages/platform-server/integrationtest/run_tests.sh
@@ -4,10 +4,28 @@ set -eu -o pipefail
 
 cd `dirname $0`
 
+# Workaround https://github.com/yarnpkg/yarn/issues/2165
+# Yarn will cache file://dist URIs and not update Angular code
+readonly cache=.yarn_local_cache
+function rm_cache {
+  rm -rf $cache
+}
+rm_cache
+mkdir $cache
+trap rm_cache EXIT
+
+# Workaround for https://github.com/yarnpkg/yarn/issues/2256
+rm -f yarn.lock
+yarn install --cache-folder ./$cache
+
 echo "#################################"
 echo "Running platform-server end to end tests"
 echo "#################################"
 
-npm install
+# this is a workaround for resolving tsc-wrapped as the local version not available on npm
+# tsc-wrapped is installed as a transitive dependency of compiler-cli
+# compiler-cli needs to be installed while offline so that it doesn't try to look up tsc-wrapped version on npm
+# it should instead use the version that is already installed in the project via package.json
+yarn add --offline ../../../dist/packages-dist/compiler-cli --cache-folder ./$cache
 
-npm run test
+yarn test || exit 1
