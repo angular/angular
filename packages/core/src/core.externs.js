9b4f94bd3cca3a8cd14e7c559ad8548387dda7e5
Tobias Bosch <tbosch1009@gmail.com>
2017-09-26 13:55:36 -0700

    fix: create proper externs so that closure does not clobber e.g. `ng` for internal variables

---
+++ packages/core/src/core.externs.js
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ *
+ * @externs
+ */
+
+/**
+ * @suppress {duplicate}
+ */
+var ng;
+/**
+ * @suppress {duplicate}
+ */
+var wtf;
