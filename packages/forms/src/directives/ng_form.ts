c2506a78a1495e3109e17a7efc8f8ad4b65dedf8
Kapunahele Wong <kapunahelewong@gmail.com>
2017-10-02 16:51:58 -0400

    docs(aio): add copy about ngNoForm to NgForm API doc (#19561)
    PR Close #19561

--- packages/forms/src/directives/ng_form.ts
+++ packages/forms/src/directives/ng_form.ts
@@ -48,6 +48,13 @@ const resolvedPromise = Promise.resolve(null);
  * triggered a form submission. The `ngSubmit` event will be emitted with the original form
  * submission event.
  *
+ * In template driven forms, all `<form>` tags are automatically tagged as `NgForm`.
+ * If you want to import the `FormsModule` but skip its usage in some forms,
+ * for example, to use native HTML5 validation, you can add `ngNoForm` and the `<form>`
+ * tags won't create an `NgForm` directive. In reactive forms, using `ngNoForm` is
+ * unnecessary because the `<form>` tags are inert. In that case, you would
+ * refrain from using the `formGroup` directive.
+ *
  * {@example forms/ts/simpleForm/simple_form_example.ts region='Component'}
  *
  * * **npm package**: `@angular/forms`
