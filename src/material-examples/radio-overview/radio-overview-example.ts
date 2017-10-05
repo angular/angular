ff52d95ec28580590a2ea0d343092cf581ba2596
Alyssa Nicoll <mail@alyssanicoll.com>
2017-10-04 20:04:29 -0500

    docs(radio-button): add a bit of padding to radio button example (#7433)

--- src/material-examples/radio-overview/radio-overview-example.ts
+++ src/material-examples/radio-overview/radio-overview-example.ts
@@ -6,5 +6,6 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'radio-overview-example',
   templateUrl: 'radio-overview-example.html',
+  styleUrls: ['radio-overview-example.css'],
 })
 export class RadioOverviewExample {}
