0834a31d03ae6707633e01d43200703840b70c5a
Jeremy Elbourn <jelbourn@gmail.com>
2017-10-06 14:31:05 -0400

    chore: update copyright to Google LLC (#7589)

--- build-config.js
+++ build-config.js
@@ -15,7 +15,7 @@ const angularVersion = package.dependencies['@angular/core'];
 /** License that will be placed inside of all created bundles. */
 const buildLicense = `/**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
