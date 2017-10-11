1a4b8c7e6dad7a5eea242d5f64a1ea582a80db2f
Jeremy Elbourn <jelbourn@google.com>
2017-10-11 16:46:18 -0400

    docs: change Google Inc. to Google LLC
    For more information, see https://www.bloomberg.com/news/articles/2017-09-01/alphabet-wraps-up-reorganization-with-a-new-company-called-xxvi

--- aio/src/assets/js/prettify.js
+++ aio/src/assets/js/prettify.js
@@ -1,6 +1,6 @@
 !function(){/*
 
- Copyright (C) 2006 Google Inc.
+ Copyright (C) 2006 Google LLC
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
