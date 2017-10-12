97969a85cda41f0a525d5c4d7eabf91265d2b636
Peter Bacon Darwin <pete@bacondarwin.com>
2017-10-12 12:33:28 +0100

    ci(aio): raise payload limit to accommodate the new search feature (#19682)
    PR Close #19682

--- aio/scripts/_payload-limits.sh
+++ aio/scripts/_payload-limits.sh
@@ -4,7 +4,7 @@ set -u -e -o pipefail
 
 declare -A payloadLimits
 payloadLimits["aio", "uncompressed", "inline"]=1600
-payloadLimits["aio", "uncompressed", "main"]=525000
+payloadLimits["aio", "uncompressed", "main"]=525500
 payloadLimits["aio", "uncompressed", "polyfills"]=38000
 payloadLimits["aio", "gzip7", "inline"]=1000
 payloadLimits["aio", "gzip7", "main"]=127000
