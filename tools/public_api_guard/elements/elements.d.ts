d580dd5b7e4eaf1024c879ed706da385fb997174
George Kalpakas <kalpakas.g@gmail.com>
2017-10-12 12:36:34 +0300

    feat(elements): implement `NgElement`

--- tools/public_api_guard/elements/elements.d.ts
+++ tools/public_api_guard/elements/elements.d.ts
@@ -1,2 +1,19 @@
 /** @experimental */
+export interface NgElement<T> extends HTMLElement {
+    componentRef: ComponentRef<T> | null;
+    ngElement: NgElement<T> | null;
+    attributeChangedCallback(attrName: string, oldValue: string | null, newValue: string, namespace?: string): void;
+    connectedCallback(): void;
+    detach(): void;
+    detectChanges(): void;
+    disconnectedCallback(): void;
+    getHost(): HTMLElement;
+    markDirty(): void;
+}
+
+/** @experimental */
+export declare type NgElementWithProps<T, P> = NgElement<T> & {
+    [property in keyof
+
+/** @experimental */
 export declare const VERSION: Version;
