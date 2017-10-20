40a615540f941f9463795fef69b562659a88cd5e
Igor Minar <igor@angular.io>
2017-10-19 21:39:53 -0700

    build(aio): upgrade to @angular/cli@1.5.0-rc.2 (#19702)
    -rw-r--r--  1 iminar  eng   84219 Oct 19 21:37 dist/0.0f327734d18211139822.chunk.js
    -rw-r--r--  1 iminar  eng   14942 Oct 19 21:37 dist/4.c719ac5645940382cdce.chunk.js
    -rw-r--r--  1 iminar  eng    1560 Oct 19 21:37 dist/inline.887757679ff553e20b54.bundle.js
    -rw-r--r--  1 iminar  eng  492354 Oct 19 21:37 dist/main.5e5dc9ed980c9f5dc2bd.bundle.js
    -rw-r--r--  1 iminar  eng   37402 Oct 19 21:37 dist/polyfills.f8409a9eb69060ac1aa6.bundle.js
    
    PR Close #19702

--- aio/tools/cli-patches/patch.js
+++ aio/tools/cli-patches/patch.js
@@ -3,8 +3,7 @@ const sh = require('shelljs');
 
 const PATCH_LOCK = 'node_modules/@angular/cli/models/webpack-configs/.patched';
 
-if (!fs.existsSync(PATCH_LOCK)) {
-  sh.exec('patch -p0 -i tools/cli-patches/rxjs-esm-aliases.patch');
-  sh.touch(PATCH_LOCK);
-}
+// if (!fs.existsSync(PATCH_LOCK)) {
+//   sh.touch(PATCH_LOCK);
+// }
 
