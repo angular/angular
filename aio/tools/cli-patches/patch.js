db7f2a9ca7068b04649b52b6f9c6f22802d33f0c
Igor Minar <igor@angular.io>
2017-10-20 22:18:48 -0700

    build(aio): disable 'global' support in webpack (#19702)
    This will be fixed in CLI once https://github.com/angular/angular-cli/pull/8130 lands.
    
    -rw-r--r--  1 iminar  eng   14942 Oct 20 22:23 dist/0.b19e913fbdd6507d346b.chunk.js
    -rw-r--r--  1 iminar  eng    1535 Oct 20 22:23 dist/inline.5d66b81ec9e01af9d28d.bundle.js
    -rw-r--r--  1 iminar  eng  528395 Oct 20 22:23 dist/main.e36bb99245ca52ae546f.bundle.js
    -rw-r--r--  1 iminar  eng   37205 Oct 20 22:23 dist/polyfills.0dfca732c5a075c110d0.bundle.js
    
    PR Close #19702

--- aio/tools/cli-patches/patch.js
+++ aio/tools/cli-patches/patch.js
@@ -3,7 +3,8 @@ const sh = require('shelljs');
 
 const PATCH_LOCK = 'node_modules/@angular/cli/models/webpack-configs/.patched';
 
-// if (!fs.existsSync(PATCH_LOCK)) {
-//   sh.touch(PATCH_LOCK);
-// }
+if (!fs.existsSync(PATCH_LOCK)) {
+  sh.exec('patch -p0 -i tools/cli-patches/webpack-no-global.patch');
+  sh.touch(PATCH_LOCK);
+}
 
