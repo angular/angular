c46ba07151a619f2eda138dc4618d7a9eacf67d2
mmalerba <mmalerba@google.com>
2017-10-23 13:20:29 -0700

    lint: ensure all files are owned by someone (#7937)
    * lint: ensure all files are owned by someone
    
    * address comments

--- tools/gulp/tasks/lint.ts
+++ tools/gulp/tasks/lint.ts
@@ -1,8 +1,11 @@
-import {task} from 'gulp';
-import {execNodeTask} from '../util/task_helpers';
-import {join} from 'path';
-import {buildConfig} from 'material2-build-tools';
 import {red} from 'chalk';
+import {readdirSync, readFileSync, statSync} from 'fs';
+import {task} from 'gulp';
+import {colors, log} from 'gulp-util';
+import {buildConfig} from 'material2-build-tools';
+import {IMinimatch, Minimatch} from 'minimatch';
+import {join} from 'path';
+import {execNodeTask} from '../util/task_helpers';
 
 // These types lack of type definitions
 const madge = require('madge');
@@ -19,7 +22,13 @@ const materialOutPath = join(buildConfig.outputDir, 'packages', 'material');
 /** Path to the output of the CDK package. */
 const cdkOutPath = join(buildConfig.outputDir, 'packages', 'cdk');
 
-task('lint', ['tslint', 'stylelint', 'madge']);
+/** Path for the Github owners file. */
+const ownersFilePath = '.github/CODEOWNERS';
+
+/** Path for the .gitignore file. */
+const gitIgnorePath = '.gitignore';
+
+task('lint', ['tslint', 'stylelint', 'madge', 'ownerslint']);
 
 /** Task to lint Angular Material's scss stylesheets. */
 task('stylelint', execNodeTask(
@@ -45,6 +54,67 @@ task('madge', ['material:clean-build'], () => {
     }
   });
 });
+
+task('ownerslint', () => {
+  let errors = 0;
+
+  let ownedPaths = readFileSync(ownersFilePath, 'utf8').split('\n')
+      // Trim lines.
+      .map(line => line.trim())
+      // Remove empty lines and comments.
+      .filter(line => line && !line.startsWith('#'))
+      // Split off just the path glob.
+      .map(line => line.split(/\s+/)[0])
+      // Turn paths into Minimatch objects.
+      .map(path => new Minimatch(path, {dot: true, matchBase: true}));
+
+  let ignoredPaths = readFileSync(gitIgnorePath, 'utf8').split('\n')
+      // Trim lines.
+      .map(line => line.trim())
+      // Remove empty lines and comments.
+      .filter(line => line && !line.startsWith('#'))
+      // Turn paths into Minimatch objects.
+      .map(path => new Minimatch(path, {dot: true, matchBase: true}));
+
+  for (let paths = getChildPaths('.'); paths.length;) {
+    paths = Array.prototype.concat(...paths
+        // Remove ignored paths
+        .filter(path => !ignoredPaths.reduce(
+            (isIgnored, ignoredPath) => isIgnored || ignoredPath.match('/' + path), false))
+        // Remove paths that match an owned path.
+        .filter(path => !ownedPaths.reduce(
+            (isOwned, ownedPath) => isOwned || isOwnedBy(path, ownedPath), false))
+        // Report an error for any files that didn't match any owned paths.
+        .filter(path => {
+          if (statSync(path).isFile()) {
+            log(colors.red(`No code owner found for "${path}".`));
+            errors++;
+            return false;
+          }
+          return true;
+        })
+        // Get the next level of children for any directories.
+        .map(path => getChildPaths(path)));
+  }
+
+  if (errors) {
+    throw Error(`Found ${errors} files with no owner.`);
+  }
+});
+
+/** Check if the given path is owned by the given owned path matcher. */
+function isOwnedBy(path: string, ownedPath: IMinimatch) {
+  // If the owned path ends with `**` its safe to eliminate whole directories.
+  if (ownedPath.pattern.endsWith('**') || statSync(path).isFile()) {
+    return ownedPath.match('/' + path);
+  }
+  return false;
+}
+
+/** Get the immediate child paths of the given path. */
+function getChildPaths(path: string) {
+  return readdirSync(path).map(child => join(path, child));
+}
 
 /** Returns a string that formats the graph of circular modules. */
 function formatMadgeCircularModules(circularModules: string[][]): string {
