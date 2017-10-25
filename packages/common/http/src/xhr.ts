1b7547f9efac555be131c468acb9d9fd1bd906e3
linskeyd <linskeyd@gmail.com>
2017-10-24 21:58:59 -0700

    fix(common): strip XSSI prefix for all JSON responses
    This fixes a regression where the common XSSI prefix
    used to be stripped for all json responses, including
    error responses.

--- packages/common/http/src/xhr.ts
+++ packages/common/http/src/xhr.ts
@@ -180,24 +180,22 @@ export class HttpXhrBackend implements HttpBackend {
 
         // Check whether the body needs to be parsed as JSON (in many cases the browser
         // will have done that already).
-        if (ok && req.responseType === 'json' && typeof body === 'string') {
-          // Attempt the parse. If it fails, a parse error should be delivered to the user.
+        if (req.responseType === 'json' && typeof body === 'string') {
+          // Strip the common XSSI prefix if it is present.
           body = body.replace(XSSI_PREFIX, '');
+
           try {
             body = JSON.parse(body);
           } catch (error) {
-            // Even though the response status was 2xx, this is still an error.
-            ok = false;
-            // The parse error contains the text of the body that failed to parse.
-            body = { error, text: body } as HttpJsonParseError;
-          }
-        } else if (!ok && req.responseType === 'json' && typeof body === 'string') {
-          try {
-            // Attempt to parse the body as JSON.
-            body = JSON.parse(body);
-          } catch (error) {
-            // Cannot be certain that the body was meant to be parsed as JSON.
-            // Leave the body as a string.
+            if (ok) {
+              // Even though the response status was 2xx, this is still an error.
+              ok = false;
+              // The parse error contains the text of the body that failed to parse.
+              body = { error, text: body } as HttpJsonParseError;
+            } else {
+              // Cannot be certain that the body was meant to be parsed as JSON.
+              // Leave the body as a string.
+            }
           }
         }
 
