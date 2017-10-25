fcf65863d94a852b4b69c55095f83e66ee0fdcc7
Olivier Combe <olivier.combe@gmail.com>
2017-10-25 23:07:55 +0200

    docs(aio): updated i18n guide and example

--- aio/content/examples/i18n/src/main.ts
+++ aio/content/examples/i18n/src/main.ts
@@ -1,10 +1,12 @@
 // #docregion
-import { platformBrowserDynamic }  from '@angular/platform-browser-dynamic';
-import { getTranslationProviders } from './app/i18n-providers';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-getTranslationProviders().then(providers => {
-  const options = { providers };
-  platformBrowserDynamic().bootstrapModule(AppModule, options);
-});
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
