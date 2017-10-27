a324d1f3a86428b083e1a3dac41db779fb1f90e4
Ward Bell <wardbell@hotmail.com>
2017-10-27 15:48:50 -0700

    docs: testing guide for CLI
    - updates tests
    - heavy prose revisions
    - uses HttpClient (with angular-in-memory-web-api)
    - test HeroService using `HttpClientTestingModule`
    - scrub away most By.CSS
    - fake async observable with `asyncData()`
    - extensive Twain work
    - different take on retryWhen
    - remove app barrels (& systemjs.extras) which troubled plunker/systemjs
    - add dummy export const to hero.ts (plunkr/systemjs fails w/o it)
    - shrink and re-organize TOC
    - add marble testing package and tests
    - demonstrate the "no beforeEach()" test coding style
    - add section on Http service testing
    - prepare for stackblitz
    - confirm works in plunker except excluded marble test
    - add tests for avoidFile class feature of CodeExampleComponent

--- aio/content/examples/testing/src/app/about.component.ts
+++
@@ -1,9 +0,0 @@
-// #docregion
-import { Component } from '@angular/core';
-@Component({
-  template: `
-  <h2 highlight="skyblue">About</h2>
-  <twain-quote></twain-quote>
-  <p>All about this sample</p>`
-})
-export class AboutComponent { }
