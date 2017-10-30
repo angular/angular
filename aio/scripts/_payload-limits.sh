cd55643f85e7d6b31c38c03fe2d93ef94475e781
Igor Minar <igor@angular.io>
2017-10-30 11:34:54 +0100

    build(aio): upgrade to @angular/cli@1.5.0-rc.6 (#19702)
    -rw-r--r--  1 iminar  eng   14880 Oct 30 11:29 dist/0.b19e913fbdd6507d346b.chunk.js
    -rw-r--r--  1 iminar  eng    1533 Oct 30 11:29 dist/inline.25600c3b48de18b97581.bundle.js
    -rw-r--r--  1 iminar  eng  486476 Oct 30 11:29 dist/main.d1292a34401056535884.bundle.js
    -rw-r--r--  1 iminar  eng   37070 Oct 30 11:29 dist/polyfills.0dfca732c5a075c110d0.bundle.js
    
    PR Close #19702

--- aio/scripts/_payload-limits.sh
+++ aio/scripts/_payload-limits.sh
@@ -4,11 +4,11 @@ set -u -e -o pipefail
 
 declare -A payloadLimits
 payloadLimits["aio", "uncompressed", "inline"]=1600
-payloadLimits["aio", "uncompressed", "main"]=522500
+payloadLimits["aio", "uncompressed", "main"]=487000
 payloadLimits["aio", "uncompressed", "polyfills"]=38000
 payloadLimits["aio", "gzip7", "inline"]=1000
-payloadLimits["aio", "gzip7", "main"]=127000
-payloadLimits["aio", "gzip7", "polyfills"]=12500
+payloadLimits["aio", "gzip7", "main"]=120000
+payloadLimits["aio", "gzip7", "polyfills"]=11900
 payloadLimits["aio", "gzip9", "inline"]=1000
-payloadLimits["aio", "gzip9", "main"]=127000
-payloadLimits["aio", "gzip9", "polyfills"]=12500
+payloadLimits["aio", "gzip9", "main"]=120000
+payloadLimits["aio", "gzip9", "polyfills"]=11900
