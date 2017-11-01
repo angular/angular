296f01cfde24a83f18cea8ce67bc4bc93d556f94
brachi-wernick <brachi.w@sapiens.com>
2017-11-01 12:53:24 +0200

    fix(core): fix test
    error `ExpressionChangedAfterItHasBeenCheckedError` has now the binding field name, fix testing accordingly

--- packages/core/test/view/component_view_spec.ts
+++ packages/core/test/view/component_view_spec.ts
@@ -132,7 +132,7 @@ export function main() {
         value = 'v2';
         expect(() => Services.checkNoChangesView(view))
             .toThrowError(
-                `ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked. Previous value: 'v1'. Current value: 'v2'.`);
+                `ExpressionChangedAfterItHasBeenCheckedError: Expression 'a' has changed after it was checked. Previous value: 'v1'. Current value: 'v2'.`);
       });
 
       it('should support detaching and attaching component views for dirty checking', () => {
