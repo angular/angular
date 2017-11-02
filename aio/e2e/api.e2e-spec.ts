196ce6d475959bf8cdaf0ac0ce448ab6557c865a
George Kalpakas <kalpakas.g@gmail.com>
2017-11-02 21:31:31 +0200

    build(aio): use correct types for e2e tests

--- aio/e2e/api.e2e-spec.ts
+++ aio/e2e/api.e2e-spec.ts
@@ -3,23 +3,22 @@ import { ApiPage } from './api.po';
 describe('Api pages', function() {
   it('should show direct subclasses of a class', () => {
     const page = new ApiPage('api/forms/AbstractControlDirective');
-    // We must use `as any` (here and below) because of broken typings for jasmine
-    expect(page.getDescendants('class', true)).toEqual(['ControlContainer', 'NgControl'] as any);
+    expect(page.getDescendants('class', true)).toEqual(['ControlContainer', 'NgControl']);
   });
 
   it('should show direct and indirect subclasses of a class', () => {
     const page = new ApiPage('api/forms/AbstractControlDirective');
-    expect(page.getDescendants('class')).toEqual(['ControlContainer', 'AbstractFormGroupDirective', 'NgControl'] as any);
+    expect(page.getDescendants('class')).toEqual(['ControlContainer', 'AbstractFormGroupDirective', 'NgControl']);
   });
 
   it('should show child interfaces that extend an interface', () => {
     const page = new ApiPage('api/forms/Validator');
-    expect(page.getDescendants('interface')).toEqual(['AsyncValidator'] as any);
+    expect(page.getDescendants('interface')).toEqual(['AsyncValidator']);
   });
 
   it('should show classes that implement an interface', () => {
     const page = new ApiPage('api/animations/AnimationPlayer');
-    expect(page.getDescendants('class')).toEqual(['NoopAnimationPlayer', 'MockAnimationPlayer'] as any);
+    expect(page.getDescendants('class')).toEqual(['NoopAnimationPlayer', 'MockAnimationPlayer']);
   });
 
   it('should show type params of type-aliases', () => {
