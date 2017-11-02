6f05dab2fc65ccb8a953d2c3b894122e4a7ec137
Olivier Combe <olivier.combe@gmail.com>
2017-11-02 22:22:09 +0100

    docs(aio): updated i18n guide and example (#19975)

--- aio/content/examples/i18n/src/app/app.locale_data_extra.ts
+++
@@ -1,7 +0,0 @@
-// #docregion import-locale-extra
-import { registerLocaleData } from '@angular/common';
-import localeEnGB from '@angular/common/locales/en-GB';
-import localeEnGBExtra from '@angular/common/locales/extra/en-GB';
-
-registerLocaleData(localeEnGB, localeEnGBExtra);
-// #enddocregion import-locale-extra
