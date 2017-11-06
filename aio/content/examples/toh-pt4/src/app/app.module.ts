3ab0963309a55231331f85441e6e9efdc2eca7c1
Jesús Rodríguez <Foxandxss@gmail.com>
2017-11-06 19:02:18 +0100

    docs(aio): update ToH for CLI (#19811)

--- aio/content/examples/toh-pt4/src/app/app.module.ts
+++ aio/content/examples/toh-pt4/src/app/app.module.ts
@@ -1,19 +1,28 @@
-import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
+  // #docregion providers
+  providers: [ HeroService, MessageService ],
+  // #enddocregion providers
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- aio/content/examples/toh-pt5/src/app/dashboard.component.1.ts
+++
@@ -1,8 +0,0 @@
-// #docregion
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
-})
-export class DashboardComponent { }
