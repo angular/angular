424a323316be70871897bbcb5eb0b055cb7d2224
Chuck Jazdzewski <chuckj@google.com>
2017-11-09 14:06:02 -0800

    fix(compiler): show explanatory text in template errors (#20313)
    Fixes: #20076
    
    PR Close #20313

--- packages/compiler/src/parse_util.ts
+++ packages/compiler/src/parse_util.ts
@@ -120,12 +120,13 @@ export class ParseError {
 
   contextualMessage(): string {
     const ctx = this.span.start.getContext(100, 3);
-    return ctx ? ` ("${ctx.before}[${ParseErrorLevel[this.level]} ->]${ctx.after}")` : '';
+    return ctx ? `${this.msg} ("${ctx.before}[${ParseErrorLevel[this.level]} ->]${ctx.after}")` :
+                 this.msg;
   }
 
   toString(): string {
     const details = this.span.details ? `, ${this.span.details}` : '';
-    return `${this.msg}${this.contextualMessage()}: ${this.span.start}${details}`;
+    return `${this.contextualMessage()}: ${this.span.start}${details}`;
   }
 }
 
