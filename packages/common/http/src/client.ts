7b7757dd3d38a95ed20a6dd2d6db44f269eec087
George Kalpakas <kalpakas.g@gmail.com>
2017-11-10 13:52:21 +0200

    docs(common): add `HttpParamsOptions` to the public API (#20332)
    Fixes #20276
    
    PR Close #20332

--- packages/common/http/src/client.ts
+++ packages/common/http/src/client.ts
@@ -12,7 +12,7 @@ import {concatMap, filter, map} from 'rxjs/operators';
 
 import {HttpHandler} from './backend';
 import {HttpHeaders} from './headers';
-import {HttpParams, HttpParamsOptions} from './params';
+import {HttpParams} from './params';
 import {HttpRequest} from './request';
 import {HttpEvent, HttpResponse} from './response';
 
@@ -361,7 +361,7 @@ export class HttpClient {
         if (options.params instanceof HttpParams) {
           params = options.params;
         } else {
-          params = new HttpParams({ fromObject: options.params } as HttpParamsOptions);
+          params = new HttpParams({fromObject: options.params});
         }
       }
 
