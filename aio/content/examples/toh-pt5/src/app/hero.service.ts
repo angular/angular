52b50c4b6cc539288b74eae0edd8df488d8b2134
Trotyl <trotyl@qq.com>
2017-11-11 18:14:55 +0800

    docs(aio): fix rxjs import (#20350) closes #20349
    PR Close #20350

--- aio/content/examples/toh-pt5/src/app/hero.service.ts
+++ aio/content/examples/toh-pt5/src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
