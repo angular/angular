a5b0872e07ac6653408a5b84d76dbddd2439a77d
mmalerba <mmalerba@google.com>
2017-11-20 11:22:51 -0800

    chore(prerender): output prerendered index.html for debugging (#8483)

--- src/universal-app/prerender.ts
+++ src/universal-app/prerender.ts
@@ -1,11 +1,10 @@
-import 'reflect-metadata';
-import 'zone.js';
-
 import {enableProdMode} from '@angular/core';
 import {renderModuleFactory} from '@angular/platform-server';
-import {join} from 'path';
-import {readFileSync} from 'fs-extra';
+import {readFileSync, writeFileSync} from 'fs-extra';
 import {log} from 'gulp-util';
+import {join} from 'path';
+import 'reflect-metadata';
+import 'zone.js';
 import {KitchenSinkServerModuleNgFactory} from './kitchen-sink/kitchen-sink.ngfactory';
 
 enableProdMode();
@@ -15,7 +14,10 @@ const result = renderModuleFactory(KitchenSinkServerModuleNgFactory, {
 });
 
 result
-  .then(() => log('Prerender done.'))
+  .then(content => {
+    writeFileSync(join(__dirname, 'index-prerendered.html'), content, 'utf-8');
+    log('Prerender done.');
+  })
   // If rendering the module factory fails, exit the process with an error code because otherwise
   // the CI task will not recognize the failure and will show as "success". The error message
   // will be printed automatically by the `renderModuleFactory` method.
