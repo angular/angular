bb4613646d1d24b69b1a19e37e9c172d30d1ad9b
Mario Nebl <hello@mario-nebl.de>
2017-11-23 22:46:16 +0100

    ci: derive scopes from available packages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,32 +1,25 @@
+const glob = require('glob');
+
+const scopes = ['aio', 'changelog', 'packaging']
+  .concat(getPackages());
+
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
     'header-max-length': [2, 'always', 120],
     'scope-enum': [
       2, 'always',
-      [
-        'aio',
-        'animations',
-        'bazel',
-        'benchpress',
-        'common',
-        'compiler',
-        'compiler-cli',
-        'core',
-        'forms',
-        'http',
-        'language-service',
-        'platform-browser',
-        'platform-browser-dynamic',
-        'platform-server',
-        'platform-webworker',
-        'platform-webworker-dynamic',
-        'router',
-        'service-worker',
-        'upgrade',
-        'packaging',
-        'changelog'
-      ]
+      scopes
     ]
   }
 };
+
+function getPackages() {
+  return glob
+    .sync('packages/*/package.json')
+    .map(manifest => require(`./${manifest}`).name)
+    .map(name => {
+      const f = name.split('/');
+      return f[f.length - 1];
+    });
+}
