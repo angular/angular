c55bb0f425a6640991aabe1397485dde046794bf
Mario Nebl <hello@mario-nebl.de>
2017-11-23 22:54:46 +0100

    style: reformat config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,17 @@
 const glob = require('glob');
 
-const scopes = ['aio', 'changelog', 'packaging']
-  .concat(getPackages());
+const scopes = ['aio', 'changelog', 'packaging'].concat(getPackages());
 
 module.exports = {
   extends: ['@commitlint/config-angular'],
-  rules: {
-    'header-max-length': [2, 'always', 120],
-    'scope-enum': [
-      2, 'always',
-      scopes
-    ]
-  }
+  rules: {'header-max-length': [2, 'always', 120], 'scope-enum': [2, 'always', scopes]}
 };
 
 function getPackages() {
-  return glob
-    .sync('packages/*/package.json')
-    .map(manifest => require(`./${manifest}`).name)
-    .map(name => {
-      const f = name.split('/');
-      return f[f.length - 1];
-    });
+  return glob.sync('packages/*/package.json')
+      .map(manifest => require(`./${manifest}`).name)
+      .map(name => {
+        const f = name.split('/');
+        return f[f.length - 1];
+      });
 }
