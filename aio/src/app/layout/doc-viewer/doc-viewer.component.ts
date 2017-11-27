c0f08be3fba4ef83b3474d39b49fae208641be7f
George Kalpakas <kalpakas.g@gmail.com>
2017-11-27 23:06:09 +0200

    fix(aio): fix embedded ToC and improve ToC, destroying components and scroll timing (#18428)
    - Fix embedded ToC:
      Previously, the element was added too late and was never instantiated.
    
    - Improve ToC update timing:
      Previously, the ToC was updated after the entering animation was over, which
      resulted in the ToC being outdated for the duration of the animation.
    
    - Improve destroying components timing:
      Previously, the old embedded components were destroyed as soon as a
      new document was requested. Even if the transition ended up never
      happening (e.g. due to error while preparing the new document), the
      embedded components would have been destroyed and the displayed
      document would not work as expected.
      Now the old embedded components are destroyed only after the new
      document has been fully prepared.
    
    - Improve scroll-to-top timing:
      Previously, the page was scrolled to top after the entering animation was
      over, which resulted in "jumpi-ness". Now the scrolling happens after the
      leaving document has been removed and before the entering document has been
      inserted.
    
    PR Close #18428

--- aio/src/app/layout/doc-viewer/doc-viewer.component.ts
+++ aio/src/app/layout/doc-viewer/doc-viewer.component.ts
@@ -48,8 +48,21 @@ export class DocViewerComponent implements DoCheck, OnDestroy {
     }
   }
 
-  @Output()
-  docRendered = new EventEmitter<void>();
+  // The new document is ready to be inserted into the viewer.
+  // (Embedded components have been loaded and instantiated, if necessary.)
+  @Output() docReady = new EventEmitter<void>();
+
+  // The previous document has been removed from the viewer.
+  // (The leaving animation (if any) has been completed and the node has been removed from the DOM.)
+  @Output() docRemoved = new EventEmitter<void>();
+
+  // The new document has been inserted into the viewer.
+  // (The node has been inserted into the DOM, but the entering animation may still be in progress.)
+  @Output() docInserted = new EventEmitter<void>();
+
+  // The new document has been fully rendered into the viewer.
+  // (The entering animation has been completed.)
+  @Output() docRendered = new EventEmitter<void>();
 
   constructor(
     elementRef: ElementRef,
@@ -70,9 +83,7 @@ export class DocViewerComponent implements DoCheck, OnDestroy {
 
     this.onDestroy$.subscribe(() => this.destroyEmbeddedComponents());
     this.docContents$
-        .do(() => this.destroyEmbeddedComponents())
         .switchMap(newDoc => this.render(newDoc))
-        .do(() => this.docRendered.emit())
         .takeUntil(this.onDestroy$)
         .subscribe();
   }
@@ -86,27 +97,6 @@ export class DocViewerComponent implements DoCheck, OnDestroy {
   }
 
   /**
-   * Set up the window title and ToC.
-   */
-  protected addTitleAndToc(docId: string): void {
-    this.tocService.reset();
-    const titleEl = this.currViewContainer.querySelector('h1');
-    let title = '';
-
-    // Only create TOC for docs with an <h1> title
-    // If you don't want a TOC, add "no-toc" class to <h1>
-    if (titleEl) {
-      title = (typeof titleEl.innerText === 'string') ? titleEl.innerText : titleEl.textContent;
-      if (!/(no-toc|notoc)/i.test(titleEl.className)) {
-        this.tocService.genToc(this.currViewContainer, docId);
-        titleEl.insertAdjacentHTML('afterend', '<aio-toc class="embedded"></aio-toc>');
-      }
-    }
-
-    this.titleService.setTitle(title ? `Angular - ${title}` : 'Angular');
-  }
-
-  /**
    * Destroy the embedded components to avoid memory leaks.
    */
   protected destroyEmbeddedComponents(): void {
@@ -115,19 +105,52 @@ export class DocViewerComponent implements DoCheck, OnDestroy {
   }
 
   /**
+   * Prepare for setting the window title and ToC.
+   * Return a function to actually set them.
+   */
+  protected prepareTitleAndToc(targetElem: HTMLElement, docId: string): () => void {
+    const titleEl = targetElem.querySelector('h1');
+    const hasToc = !!titleEl && !/no-?toc/i.test(titleEl.className);
+
+    if (hasToc) {
+      titleEl.insertAdjacentHTML('afterend', '<aio-toc class="embedded"></aio-toc>');
+    }
+
+    return () => {
+      this.tocService.reset();
+      let title = '';
+
+      // Only create ToC for docs with an `<h1>` heading.
+      // If you don't want a ToC, add "no-toc" class to `<h1>`.
+      if (titleEl) {
+        title = (typeof titleEl.innerText === 'string') ? titleEl.innerText : titleEl.textContent;
+
+        if (hasToc) {
+          this.tocService.genToc(targetElem, docId);
+        }
+      }
+
+      this.titleService.setTitle(title ? `Angular - ${title}` : 'Angular');
+    };
+  }
+
+  /**
    * Add doc content to host element and build it out with embedded components.
    */
   protected render(doc: DocumentContents): Observable<void> {
+    let addTitleAndToc: () => void;
+
     return this.void$
-        .do(() => {
-          // Security: `doc.contents` is always authored by the documentation team
-          //           and is considered to be safe.
-          this.nextViewContainer.innerHTML = doc.contents || '';
-        })
+        // Security: `doc.contents` is always authored by the documentation team
+        //           and is considered to be safe.
+        .do(() => this.nextViewContainer.innerHTML = doc.contents || '')
+        .do(() => addTitleAndToc = this.prepareTitleAndToc(this.nextViewContainer, doc.id))
         .switchMap(() => this.embedComponentsService.embedInto(this.nextViewContainer))
+        .do(() => this.docReady.emit())
+        .do(() => this.destroyEmbeddedComponents())
         .do(componentRefs => this.embeddedComponentRefs = componentRefs)
-        .switchMap(() => this.swapViews())
-        .do(() => this.addTitleAndToc(doc.id))
+        .switchMap(() => this.swapViews(addTitleAndToc))
+        .do(() => this.docRendered.emit())
         .catch(err => {
           this.nextViewContainer.innerHTML = '';
           this.logger.error(`[DocViewer]: Error preparing document '${doc.id}'.`, err);
@@ -139,8 +162,12 @@ export class DocViewerComponent implements DoCheck, OnDestroy {
    * Swap the views, removing `currViewContainer` and inserting `nextViewContainer`.
    * (At this point all content should be ready, including having loaded and instantiated embedded
    *  components.)
+   *
+   * Optionally, run a callback as soon as `nextViewContainer` has been inserted, but before the
+   * entering animation has been completed. This is useful for work that needs to be done as soon as
+   * the element has been attached to the DOM.
    */
-  protected swapViews(): Observable<void> {
+  protected swapViews(onInsertedCb = () => undefined): Observable<void> {
     const raf$ = new Observable<void>(subscriber => {
       const rafId = requestAnimationFrame(() => {
         subscriber.next();
@@ -174,12 +201,15 @@ export class DocViewerComponent implements DoCheck, OnDestroy {
       done$ = done$
           // Remove the current view from the viewer.
           .switchMap(() => animateLeave(this.currViewContainer))
-          .do(() => this.currViewContainer.parentElement.removeChild(this.currViewContainer));
+          .do(() => this.currViewContainer.parentElement.removeChild(this.currViewContainer))
+          .do(() => this.docRemoved.emit());
     }
 
     return done$
         // Insert the next view into the viewer.
         .do(() => this.hostElement.appendChild(this.nextViewContainer))
+        .do(() => onInsertedCb())
+        .do(() => this.docInserted.emit())
         .switchMap(() => animateEnter(this.nextViewContainer))
         // Update the view references and clean up unused nodes.
         .do(() => {
