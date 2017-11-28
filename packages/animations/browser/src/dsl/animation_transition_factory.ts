fb24085d9d6b71c46ddc50b8ce205185edd1f254
Miško Hevery <misko@hevery.com>
2017-11-28 15:08:31 -0600

    Revert "fix(animations): ensure multi-level leave animations work (#19455)"
    This reverts commit 1366762d12e0faa857deb793255099bfe0e0f717.

--- packages/animations/browser/src/dsl/animation_transition_factory.ts
+++ packages/animations/browser/src/dsl/animation_transition_factory.ts
@@ -37,8 +37,7 @@ export class AnimationTransitionFactory {
 
   build(
       driver: AnimationDriver, element: any, currentState: any, nextState: any,
-      enterClassName: string, leaveClassName: string, currentOptions?: AnimationOptions,
-      nextOptions?: AnimationOptions,
+      enterClassName: string, currentOptions?: AnimationOptions, nextOptions?: AnimationOptions,
       subInstructions?: ElementInstructionMap): AnimationTransitionInstruction {
     const errors: any[] = [];
 
@@ -56,8 +55,8 @@ export class AnimationTransitionFactory {
     const animationOptions = {params: {...transitionAnimationParams, ...nextAnimationParams}};
 
     const timelines = buildAnimationTimelines(
-        driver, element, this.ast.animation, enterClassName, leaveClassName, currentStateStyles,
-        nextStateStyles, animationOptions, subInstructions, errors);
+        driver, element, this.ast.animation, enterClassName, currentStateStyles, nextStateStyles,
+        animationOptions, subInstructions, errors);
 
     if (errors.length) {
       return createTransitionInstruction(
