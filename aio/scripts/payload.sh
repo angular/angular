angular/angular

commit 00358a6f76f470d96b780a0a5ac662c90a87be55

ci: Update 1% payload size test (#20524)
PR Close #20524

aio/scripts/payload.sh
@@ -7,7 +7,6 @@ readonly parentDir=$(dirname $thisDir)
 
 # Track payload size functions
 source ../scripts/ci/payload-size.sh
-source ${thisDir}/_payload-limits.sh
 
-trackPayloadSize "aio" "dist/*.bundle.js" true true
+trackPayloadSize "aio" "dist/*.bundle.js" true true "${thisDir}/_payload-limits.json"
 

scripts/ci/payload-size.js
@@ -1,21 +1,30 @@
 
 const fs = require('fs');
-const latest = JSON.parse(fs.readFileSync('/tmp/latest.log', 'utf8'));
+// Get branch and project name from command line arguments
+const [, , limitFile, project, branch] = process.argv;
+
+const limit = JSON.parse(fs.readFileSync(limitFile, 'utf8'));
 const current = JSON.parse(fs.readFileSync('/tmp/current.log', 'utf8'));
 
+
+const limitData = limit[project][branch] || limit[project]["master"];
+  
+if (!limitData) {
+  console.error(`No limit data found.`);
+  // If no payload limit file found, we don't need to check the size
+  exit(0);
+}
+
 let failed = false;
-for (let commit in latest) {
-  if (typeof latest[commit] === 'object') {
-    for (let compressionType in latest[commit]) {
-      if (typeof latest[commit][compressionType] === 'object') {
-        for (let file in latest[commit][compressionType]) {
-          const name = `${compressionType}/${file}`;
-          const number = latest[commit][compressionType][file];
-          if (current[name] - number > number / 100) {
-            console.log(`Commit ${commit} ${compressionType} ${file} increase from ${number} to ${current[name]}`);
-            failed = true;
-          }
-        }
+for (let compressionType in limitData) {
+  if (typeof limitData[compressionType] === 'object') {
+    for (let file in limitData[compressionType]) {
+      const name = `${compressionType}/${file}`;
+      const number = limitData[compressionType][file];
+      if (Math.abs(current[name] - number) > number / 100) {
+        console.log(`Commit ${commit} ${compressionType} ${file} changed from ${number} to ${current[name]}.
+            If this is a desired change, please update the payload size limits in file ${limitFile}`);
+        failed = true;
       }
     }
   }

scripts/ci/payload-size.sh
@@ -22,12 +22,9 @@ calculateSize() {
 # Write to global variable $failed
 # Read from global variables $size, $size7, $size9, $label, $limitUncompressed
 checkSize() {
-  for fileType in "uncompressed" "gzip7" "gzip9"; do
-    if [[ ${size[$fileType]} -gt ${payloadLimits[$name, $fileType, $label]} ]]; then
-      failed=true
-      echo "$fileType $label size is ${size[$fileType]} which is greater than ${payloadLimits[$name, $fileType, $label]}"
-    fi
-  done
+  name="$1"
+  limitFile="$2"
+  node ${PROJECT_ROOT}/scripts/ci/payload-size.js $limitFile $name $TRAVIS_BRANCH
 }
 
 # Write timestamp to global variable $payloadData
@@ -92,14 +89,12 @@ uploadData() {
     set +x
     $PROJECT_ROOT/node_modules/.bin/firebase database:update --data "$payloadData" --project $PROJECT_NAME --confirm --token "$ANGULAR_PAYLOAD_FIREBASE_TOKEN" $dbPath
   fi
-
-  curl "https://angular-payload-size.firebaseio.com/payload/${name}/${safeBranchName}.json?orderBy=\"timestamp\"&limitToLast=1" > /tmp/latest.log
-  node ${PROJECT_ROOT}/scripts/ci/payload-size.js
 }
 
 # Track payload size, $1 is the name in database, $2 is the file path
 # $3 is whether we check the payload size and fail the test if the size exceeds
 # limit, $4 is whether record the type of changes: true | false
+# $5 is the payload size limit file
 trackPayloadSize() {
   name="$1"
   path="$2"
@@ -111,9 +106,6 @@ trackPayloadSize() {
   for filename in $path; do
     declare -A size
     calculateSize
-    if [[ $checkSize = true ]]; then
-      checkSize
-    fi
   done
   addTimestamp
   if [[ $trackChange = true ]]; then
@@ -121,6 +113,9 @@ trackPayloadSize() {
   fi
   addMessage
   uploadData $name
+  if [[ $checkSize = true ]]; then
+    checkSize $name "$5"
+  fi
   if [[ $failed = true ]]; then
     echo exit 1
     exit 1

aio/scripts/_payload-limits.sh
@@ -1,14 +1,0 @@
-#!/bin/bash
-
-set -u -e -o pipefail
-
-declare -A payloadLimits
-payloadLimits["aio", "uncompressed", "inline"]=1600
-payloadLimits["aio", "uncompressed", "main"]=487000
-payloadLimits["aio", "uncompressed", "polyfills"]=38000
-payloadLimits["aio", "gzip7", "inline"]=1000
-payloadLimits["aio", "gzip7", "main"]=120000
-payloadLimits["aio", "gzip7", "polyfills"]=11900
-payloadLimits["aio", "gzip9", "inline"]=1000
-payloadLimits["aio", "gzip9", "main"]=120000
-payloadLimits["aio", "gzip9", "polyfills"]=11900

integration/_payload-limits.sh
@@ -1,19 +1,0 @@
-#!/bin/bash
-
-set -u -e -o pipefail
-
-declare -A payloadLimits
-payloadLimits["hello_world__closure", "uncompressed", "bundle"]=106000
-payloadLimits["hello_world__closure", "gzip7", "bundle"]=35000
-payloadLimits["hello_world__closure", "gzip9", "bundle"]=35000
-
-payloadLimits["cli-hello-world", "uncompressed", "inline"]=1500
-payloadLimits["cli-hello-world", "uncompressed", "main"]=160000
-payloadLimits["cli-hello-world", "uncompressed", "polyfills"]=66000
-payloadLimits["cli-hello-world", "gzip7", "inline"]=900
-payloadLimits["cli-hello-world", "gzip7", "main"]=45000
-payloadLimits["cli-hello-world", "gzip7", "polyfills"]=22000
-payloadLimits["cli-hello-world", "gzip9", "inline"]=900
-payloadLimits["cli-hello-world", "gzip9", "main"]=45000
-payloadLimits["cli-hello-world", "gzip9", "polyfills"]=22000
-

integration/run_tests.sh
@@ -4,9 +4,10 @@ set -e -o pipefail
 
 cd `dirname $0`
 
+readonly thisDir=$(cd $(dirname $0); pwd)
+
 # Track payload size functions
 source ../scripts/ci/payload-size.sh
-source ./_payload-limits.sh
 
 # Workaround https://github.com/yarnpkg/yarn/issues/2165
 # Yarn will cache file://dist URIs and not update Angular code
@@ -48,7 +49,7 @@ for testDir in $(ls | grep -v node_modules) ; do
       if [[ $testDir == cli-hello-world ]]; then
         yarn build
       fi
-      trackPayloadSize "$testDir" "dist/*.js" true false
+      trackPayloadSize "$testDir" "dist/*.js" true false "${thisDir}/_payload-limits.json"
     fi
   )
 done

commit b361ce1929205bbdf13fe88ec270f97eb04437f3

ci: fix the payload-size checking scripts
The scripts were accidentally broken in #20524. More specifically, when a limit
was exceeded the script would break while trying to log an error message due to
a missing `commit` variable.
This commit fixes it and also does some minor clean-up (improve docs, use more
descriptive variable names, remove dead code, etc).

aio/scripts/payload.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -eu -o pipefail
 

scripts/ci/payload-size.js
@@ -1,30 +1,32 @@
+'use strict';
 
+// Imports
 const fs = require('fs');
-// Get branch and project name from command line arguments
-const [, , limitFile, project, branch] = process.argv;
 
-const limit = JSON.parse(fs.readFileSync(limitFile, 'utf8'));
-const current = JSON.parse(fs.readFileSync('/tmp/current.log', 'utf8'));
+// Get branch and project name from command line arguments.
+const [, , limitFile, project, branch, commit] = process.argv;
 
+// Load sizes.
+const currentSizes = JSON.parse(fs.readFileSync('/tmp/current.log', 'utf8'));
+const allLimitSizes = JSON.parse(fs.readFileSync(limitFile, 'utf8'));
+const limitSizes = allLimitSizes[project][branch] || allLimitSizes[project]['master'];
 
-const limitData = limit[project][branch] || limit[project]["master"];
-  
-if (!limitData) {
-  console.error(`No limit data found.`);
-  // If no payload limit file found, we don't need to check the size
-  exit(0);
-}
-
+// Check current sizes against limits.
 let failed = false;
-for (let compressionType in limitData) {
-  if (typeof limitData[compressionType] === 'object') {
-    for (let file in limitData[compressionType]) {
-      const name = `${compressionType}/${file}`;
-      const number = limitData[compressionType][file];
-      if (Math.abs(current[name] - number) > number / 100) {
-        console.log(`Commit ${commit} ${compressionType} ${file} changed from ${number} to ${current[name]}.
-            If this is a desired change, please update the payload size limits in file ${limitFile}`);
+for (const compressionType in limitSizes) {
+  if (typeof limitSizes[compressionType] === 'object') {
+    const limitPerFile = limitSizes[compressionType];
+
+    for (const filename in limitPerFile) {
+      const expectedSize = limitPerFile[filename];
+      const actualSize = currentSizes[`${compressionType}/${filename}`];
+
+      if (Math.abs(actualSize - expectedSize) > expectedSize / 100) {
         failed = true;
+        console.log(
+            `Commit ${commit} ${compressionType} ${filename} exceeded expected size by >1% ` +
+            `(expected: ${expectedSize}, actual: ${actualSize}).\n` +
+            `If this is a desired change, please update the size limits in file '${limitFile}'.`);
       }
     }
   }
@@ -33,5 +35,5 @@ for (let compressionType in limitData) {
 if (failed) {
   process.exit(1);
 } else {
-  console.log(`Payload size 1% check okay`);
+  console.log('Payload size <1% change check passed.');
 }

scripts/ci/payload-size.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail
+
 readonly PROJECT_NAME="angular-payload-size"
 
 # Calculate the size of target file uncompressed size, gzip7 size, gzip9 size
@@ -18,23 +20,24 @@ calculateSize() {
   payloadData="$payloadData\"gzip9/$label\": ${size["gzip9"]}, "
 }
 
-# Check whether the file size is under limit
-# Write to global variable $failed
-# Read from global variables $size, $size7, $size9, $label, $limitUncompressed
+# Check whether the file size is under limit.
+# Exit with an error if limit is exceeded.
+#   $1: string - The name in database.
+#   $2: string - The payload size limit file.
 checkSize() {
   name="$1"
   limitFile="$2"
-  node ${PROJECT_ROOT}/scripts/ci/payload-size.js $limitFile $name $TRAVIS_BRANCH
+  node ${PROJECT_ROOT}/scripts/ci/payload-size.js $limitFile $name $TRAVIS_BRANCH $TRAVIS_COMMIT
 }
 
-# Write timestamp to global variable $payloadData
+# Write timestamp to global variable `$payloadData`.
 addTimestamp() {
   # Add Timestamp
   timestamp=$(date +%s)
   payloadData="$payloadData\"timestamp\": $timestamp, "
 }
 
-# Write travis commit message to global variable $payloadData
+# Write travis commit message to global variable `$payloadData`.
 addMessage() {
   # Grab the set of SHAs for the message. This can fail when you force push or do initial build
   # because $TRAVIS_COMMIT_RANGE will contain the previous SHA which will not be in the
@@ -44,9 +47,9 @@ addMessage() {
   payloadData="$payloadData\"message\": \"$message\""
 }
 
-# Add change source: application, dependencies, or 'application+dependencies'
-# Read from global variables $parentDir
-# Update the change source to global variable $payloadData
+# Add change source: `application`, `dependencies`, or `application+dependencies`
+# Read from global variable `$parentDir`.
+# Update the change source in global variable `$payloadData`.
 addChange() {
   yarnChanged=false
   allChangedFiles=$(git diff --name-only $TRAVIS_COMMIT_RANGE $parentDir | wc -l)
@@ -70,8 +73,8 @@ addChange() {
   payloadData="$payloadData\"change\": \"$change\", "
 }
 
-# Upload data to firebase database if it's commit, print out data for pull
-# requests
+# Upload data to firebase database if it's commit, print out data for pull requests.
+#   $1: string - The name in database.
 uploadData() {
   name="$1"
   payloadData="{${payloadData}}"
@@ -91,18 +94,21 @@ uploadData() {
   fi
 }
 
-# Track payload size, $1 is the name in database, $2 is the file path
-# $3 is whether we check the payload size and fail the test if the size exceeds
-# limit, $4 is whether record the type of changes: true | false
-# $5 is the payload size limit file
+# Track payload size.
+#   $1: string       - The name in database.
+#   $2: string       - The file path.
+#   $3: true | false - Whether to check the payload size and fail the test if it exceeds limit.
+#   $4: true | false - Whether to record the type of changes.
+#   $5: [string]     - The payload size limit file. Only necessary if `$3` is `true`.
 trackPayloadSize() {
   name="$1"
   path="$2"
   checkSize="$3"
-  trackChange=$4
+  trackChange="$4"
+  limitFile="${5:-}"
 
   payloadData=""
-  failed=false
+
   for filename in $path; do
     declare -A size
     calculateSize
@@ -114,10 +120,6 @@ trackPayloadSize() {
   addMessage
   uploadData $name
   if [[ $checkSize = true ]]; then
-    checkSize $name "$5"
-  fi
-  if [[ $failed = true ]]; then
-    echo exit 1
-    exit 1
+    checkSize $name $limitFile
   fi
 }