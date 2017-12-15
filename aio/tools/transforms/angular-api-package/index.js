f4fb4883fad4c62cd24c4030291551094a556617
Jason Aden <jasonaden@google.com>
2017-12-15 09:58:42 -0800

    ci: add router/testing to public API guard

--- aio/tools/transforms/angular-api-package/index.js
+++ aio/tools/transforms/angular-api-package/index.js
@@ -38,6 +38,8 @@ module.exports = new Package('angular-api', [basePackage, typeScriptPackage])
     readTypeScriptModules.basePath = API_SOURCE_PATH;
     readTypeScriptModules.ignoreExportsMatching = [/^[_ɵ]|^VERSION$/];
     readTypeScriptModules.hidePrivateMembers = true;
+
+    // NOTE: This list shold be in sync with tools/gulp-tasks/public-api.js
     readTypeScriptModules.sourceFiles = [
       'animations/index.ts',
       'animations/browser/index.ts',
