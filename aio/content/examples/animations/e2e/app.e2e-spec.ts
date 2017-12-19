8f225e9c421a0ef417203bb712491c8320fcc242
George Kalpakas <kalpakas.g@gmail.com>
2017-12-19 15:48:18 +0200

    ci(aio): fix aio examples e2e flakes
    This still doesn't fix the flakes :(

--- aio/content/examples/animations/e2e/app.e2e-spec.ts
+++ aio/content/examples/animations/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 'use strict'; // necessary for es6 output in node
 
-import { browser, element, by, ElementFinder } from 'protractor';
+import { ElementFinder, ExpectedConditions, browser, by, element } from 'protractor';
 import { logging, promise } from 'selenium-webdriver';
 
 /**
@@ -20,9 +20,7 @@ describe('Animation Tests', () => {
   const ACTIVE_COLOR = 'rgba(207, 216, 220, 1)';
   const NO_TRANSFORM_MATRIX_REGEX = /matrix\(1,\s*0,\s*0,\s*1,\s*0,\s*0\)/;
 
-  beforeEach(() => {
-    browser.get('');
-  });
+  beforeEach(() => browser.get(''));
 
   describe('basic states', () => {
 
@@ -33,22 +31,21 @@ describe('Animation Tests', () => {
     });
 
     it('animates between active and inactive', () => {
+      const li = host.element(by.css('li'));
+      const toHaveBgColor = (expectedBgColor: string) =>
+        toHaveCssValue(li, 'backgroundColor', expectedBgColor);
+
       addInactiveHero();
-
-      let li = host.element(by.css('li'));
-
+      browser.wait(toHaveBgColor(INACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.0);
-      expect(li.getCssValue('backgroundColor')).toBe(INACTIVE_COLOR);
 
       li.click();
-      browser.driver.sleep(300);
+      browser.wait(toHaveBgColor(ACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.1);
-      expect(li.getCssValue('backgroundColor')).toBe(ACTIVE_COLOR);
 
       li.click();
-      browser.driver.sleep(300);
+      browser.wait(toHaveBgColor(INACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.0);
-      expect(li.getCssValue('backgroundColor')).toBe(INACTIVE_COLOR);
     });
 
   });
@@ -62,14 +59,19 @@ describe('Animation Tests', () => {
     });
 
     it('are not kept after animation', () => {
-      addInactiveHero();
+      const li = host.element(by.css('li'));
+      const toHaveBgColor = (expectedBgColor: string) =>
+        toHaveCssValue(li, 'backgroundColor', expectedBgColor);
 
-      let li = host.element(by.css('li'));
+      addInactiveHero();
+      browser.wait(toHaveBgColor(INACTIVE_COLOR), 1000);
 
       li.click();
-      browser.driver.sleep(300);
+      // We can't detect the animation, since the styles are not permanent.
+      // Just wait and assume it happended.
+      browser.sleep(500);
+      browser.wait(toHaveBgColor(INACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.0);
-      expect(li.getCssValue('backgroundColor')).toBe(INACTIVE_COLOR);
     });
 
   });
@@ -83,22 +85,21 @@ describe('Animation Tests', () => {
     });
 
     it('animates between active and inactive', () => {
+      const li = host.element(by.css('li'));
+      const toHaveBgColor = (expectedBgColor: string) =>
+        toHaveCssValue(li, 'backgroundColor', expectedBgColor);
+
       addInactiveHero();
-
-      let li = host.element(by.css('li'));
-
+      browser.wait(toHaveBgColor(INACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.0);
-      expect(li.getCssValue('backgroundColor')).toBe(INACTIVE_COLOR);
 
       li.click();
-      browser.driver.sleep(300);
+      browser.wait(toHaveBgColor(ACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.1);
-      expect(li.getCssValue('backgroundColor')).toBe(ACTIVE_COLOR);
 
       li.click();
-      browser.driver.sleep(300);
+      browser.wait(toHaveBgColor(INACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.0);
-      expect(li.getCssValue('backgroundColor')).toBe(INACTIVE_COLOR);
     });
 
   });
@@ -112,22 +113,21 @@ describe('Animation Tests', () => {
     });
 
     it('animates between active and inactive', () => {
+      const li = host.element(by.css('li'));
+      const toHaveBgColor = (expectedBgColor: string) =>
+        toHaveCssValue(li, 'backgroundColor', expectedBgColor);
+
       addInactiveHero();
-
-      let li = host.element(by.css('li'));
-
+      browser.wait(toHaveBgColor(INACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.0);
-      expect(li.getCssValue('backgroundColor')).toBe(INACTIVE_COLOR);
 
       li.click();
-      browser.driver.sleep(300);
+      browser.wait(toHaveBgColor(ACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.1);
-      expect(li.getCssValue('backgroundColor')).toBe(ACTIVE_COLOR);
 
       li.click();
-      browser.driver.sleep(300);
+      browser.wait(toHaveBgColor(INACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.0);
-      expect(li.getCssValue('backgroundColor')).toBe(INACTIVE_COLOR);
     });
 
   });
@@ -141,13 +141,15 @@ describe('Animation Tests', () => {
     });
 
     it('adds and removes element', () => {
-      addInactiveHero();
+      const li = host.element(by.css('li'));
+      const toMatchTransform = (expectedTransformRe: RegExp) =>
+        toMatchCssValue(li, 'transform', expectedTransformRe);
 
-      let li = host.element(by.css('li'));
-      expect(li.getCssValue('transform')).toMatch(NO_TRANSFORM_MATRIX_REGEX);
+      addInactiveHero();
+      browser.wait(toMatchTransform(NO_TRANSFORM_MATRIX_REGEX));
 
       removeHero();
-      expect(li.isPresent()).toBe(false);
+      browser.wait(ExpectedConditions.stalenessOf(li), 1500);
     });
 
   });
@@ -161,22 +163,23 @@ describe('Animation Tests', () => {
     });
 
     it('adds and removes and animates between active and inactive', () => {
+      const li = host.element(by.css('li'));
+      const toHaveScaleX = (expectedScaleX: number) =>
+        () => getScaleX(li).then(actualScaleX => actualScaleX === expectedScaleX);
+      const toMatchTransform = (expectedTransformRe: RegExp) =>
+        toMatchCssValue(li, 'transform', expectedTransformRe);
+
       addInactiveHero();
-
-      let li = host.element(by.css('li'));
-
-      expect(li.getCssValue('transform')).toMatch(NO_TRANSFORM_MATRIX_REGEX);
+      browser.wait(toMatchTransform(NO_TRANSFORM_MATRIX_REGEX), 1000);
 
       li.click();
-      browser.driver.sleep(300);
-      expect(getScaleX(li)).toBe(1.1);
+      browser.wait(toHaveScaleX(1.1), 1000);
 
       li.click();
-      browser.driver.sleep(300);
-      expect(li.getCssValue('transform')).toMatch(NO_TRANSFORM_MATRIX_REGEX);
+      browser.wait(toMatchTransform(NO_TRANSFORM_MATRIX_REGEX), 1000);
 
       removeHero();
-      expect(li.isPresent()).toBe(false);
+      browser.wait(ExpectedConditions.stalenessOf(li), 1500);
     });
 
   });
@@ -190,13 +193,16 @@ describe('Animation Tests', () => {
     });
 
     it('adds and removes element', () => {
-      addInactiveHero();
+      const li = host.element(by.css('li'));
+      const toMatchTransform = (expectedTransformRe: RegExp) =>
+        toMatchCssValue(li, 'transform', expectedTransformRe);
 
-      let li = host.element(by.css('li'));
+      addInactiveHero();
+      browser.wait(toMatchTransform(NO_TRANSFORM_MATRIX_REGEX));
       expect(li.getCssValue('height')).toBe('50px');
 
       removeHero();
-      expect(li.isPresent()).toBe(false);
+      browser.wait(ExpectedConditions.stalenessOf(li), 1500);
     });
 
   });
@@ -210,14 +216,16 @@ describe('Animation Tests', () => {
     });
 
     it('adds and removes element', () => {
-      addInactiveHero();
+      const li = host.element(by.css('li'));
+      const toMatchTransform = (expectedTransformRe: RegExp) =>
+        toMatchCssValue(li, 'transform', expectedTransformRe);
 
-      let li = host.element(by.css('li'));
-      expect(li.getCssValue('transform')).toMatch(NO_TRANSFORM_MATRIX_REGEX);
-      expect(li.getCssValue('opacity')).toMatch('1');
+      addInactiveHero();
+      browser.wait(toMatchTransform(NO_TRANSFORM_MATRIX_REGEX));
+      expect(li.getCssValue('opacity')).toBe('1');
 
       removeHero();
-      expect(li.isPresent()).toBe(false);
+      browser.wait(ExpectedConditions.stalenessOf(li), 1500);
     });
 
   });
@@ -231,14 +239,16 @@ describe('Animation Tests', () => {
     });
 
     it('adds and removes element', () => {
-      addInactiveHero();
+      const li = host.element(by.css('li'));
+      const toMatchTransform = (expectedTransformRe: RegExp) =>
+        toMatchCssValue(li, 'transform', expectedTransformRe);
 
-      let li = host.element(by.css('li'));
-      expect(li.getCssValue('transform')).toMatch(NO_TRANSFORM_MATRIX_REGEX);
-      expect(li.getCssValue('opacity')).toMatch('1');
+      addInactiveHero();
+      browser.wait(toMatchTransform(NO_TRANSFORM_MATRIX_REGEX));
+      expect(li.getCssValue('opacity')).toBe('1');
 
       removeHero();
-      expect(li.isPresent()).toBe(false);
+      browser.wait(ExpectedConditions.stalenessOf(li), 1500);
     });
 
   });
@@ -252,14 +262,16 @@ describe('Animation Tests', () => {
     });
 
     it('adds and removes element', () => {
+      const li = host.element(by.css('li'));
+      const toMatchTransform = (expectedTransformRe: RegExp) =>
+        toMatchCssValue(li, 'transform', expectedTransformRe);
+
       addInactiveHero();
+      browser.wait(toMatchTransform(NO_TRANSFORM_MATRIX_REGEX));
+      expect(li.getCssValue('opacity')).toBe('1');
 
-      let li = host.element(by.css('li'));
-      expect(li.getCssValue('transform')).toMatch(NO_TRANSFORM_MATRIX_REGEX);
-      expect(li.getCssValue('opacity')).toMatch('1');
-
-      removeHero(700);
-      expect(li.isPresent()).toBe(false);
+      removeHero();
+      browser.wait(ExpectedConditions.stalenessOf(li), 1500);
     });
 
   });
@@ -273,79 +285,71 @@ describe('Animation Tests', () => {
     });
 
     it('animates between active and inactive', () => {
+      const li = host.element(by.css('li'));
+      const toHaveBgColor = (expectedBgColor: string) =>
+        toHaveCssValue(li, 'backgroundColor', expectedBgColor);
+
       addActiveHero();
 
-      let li = host.element(by.css('li'));
-
+      browser.wait(toHaveBgColor(ACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.1);
-      expect(li.getCssValue('backgroundColor')).toBe(ACTIVE_COLOR);
 
       li.click();
-      browser.driver.sleep(300);
+      browser.wait(toHaveBgColor(INACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.0);
-      expect(li.getCssValue('backgroundColor')).toBe(INACTIVE_COLOR);
 
       li.click();
-      browser.driver.sleep(300);
+      browser.wait(toHaveBgColor(ACTIVE_COLOR), 1000);
       expect(getScaleX(li)).toBe(1.1);
-      expect(li.getCssValue('backgroundColor')).toBe(ACTIVE_COLOR);
     });
   });
 
   describe('callbacks', () => {
     it('fires a callback on start and done', () => {
-      addActiveHero();
-      browser.manage().logs().get(logging.Type.BROWSER)
-        .then((logs: logging.Entry[]) => {
-          const animationMessages = logs.filter((log) => {
-            return log.message.indexOf('Animation') !== -1 ? true : false;
-          });
+      const toHaveAnimationLogs = () => browser.manage().logs().get(logging.Type.BROWSER)
+          .then(logs => logs.filter(log => log.message.indexOf('Animation') !== -1))
+          .then(animationLogs => animationLogs.length);
 
-          expect(animationMessages.length).toBeGreaterThan(0);
-        });
+      addActiveHero();
+      browser.wait(toHaveAnimationLogs, 1000);
     });
   });
 
-  function addActiveHero(sleep?: number) {
-    sleep = sleep || 500;
-    element(by.buttonText('Add active hero')).click();
-    browser.driver.sleep(sleep);
+  function addActiveHero() {
+    return element(by.buttonText('Add active hero')).click();
   }
 
-  function addInactiveHero(sleep?: number) {
-    sleep = sleep || 500;
-    element(by.buttonText('Add inactive hero')).click();
-    browser.driver.sleep(sleep);
+  function addInactiveHero() {
+    return element(by.buttonText('Add inactive hero')).click();
   }
 
-  function removeHero(sleep?: number) {
-    sleep = sleep || 500;
-    element(by.buttonText('Remove hero')).click();
-    browser.driver.sleep(sleep);
+  function removeHero() {
+    return element(by.buttonText('Remove hero')).click();
   }
 
   function getScaleX(el: ElementFinder) {
-    return Promise.all([
-      getBoundingClientWidth(el),
-      getOffsetWidth(el)
-    ]).then(function(promiseResolutions) {
-      let clientWidth = promiseResolutions[0];
-      let offsetWidth = promiseResolutions[1];
-      return clientWidth / offsetWidth;
-    });
+    return Promise.all([getWidth(el), getOffsetWidth(el)])
+        .then(([clientWidth, offsetWidth]) => clientWidth / offsetWidth);
   }
 
-  function getBoundingClientWidth(el: ElementFinder) {
+  function getWidth(el: ElementFinder) {
     return browser.executeScript(
-      'return arguments[0].getBoundingClientRect().width',
-      el.getWebElement()
-    ) as PromiseLike<number>;
+        'return arguments[0].getBoundingClientRect().width',
+        el) as PromiseLike<number>;
   }
 
   function getOffsetWidth(el: ElementFinder) {
     return browser.executeScript(
-      'return arguments[0].offsetWidth',
-      el.getWebElement()
-    ) as PromiseLike<number>;
+        'return arguments[0].offsetWidth',
+        el) as PromiseLike<number>;
   }
+
+  function toHaveCssValue(el: ElementFinder, propName: string, expectedValue: any) {
+    return () => el.getCssValue(propName).then(actualValue => actualValue === expectedValue);
+  }
+
+  function toMatchCssValue(el: ElementFinder, propName: string, expectedValueRe: RegExp) {
+    return () => el.getCssValue(propName).then(actualValue => expectedValueRe.test(actualValue));
+  }
+
 });
