444784bcc9f5e509470dab5b8bf4befe4edd6813
Jeremy Elbourn <jelbourn@gmail.com>
2017-12-19 17:12:39 -0800

    build(material): add bazel rules for all components (#8457)

--- WORKSPACE
+++ WORKSPACE
@@ -16,7 +16,7 @@ node_repositories(package_json = ["//:package.json"])
 git_repository(
   name = "io_bazel_rules_sass",
   remote = "https://github.com/bazelbuild/rules_sass.git",
-  tag = "0.0.2",
+  tag = "0.0.3",
 )
 
 load("@io_bazel_rules_sass//sass:sass.bzl", "sass_repositories")
@@ -38,6 +38,7 @@ local_repository(
   path = "node_modules/@angular/bazel",
 )
 
+# Add rxjs
 local_repository(
   name = "rxjs",
   path = "node_modules/rxjs/src",
--- src/lib/radio/radio-module.ts
+++ src/lib/radio/radio-module.ts
@@ -6,21 +6,17 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {A11yModule} from '@angular/cdk/a11y';
 import {UNIQUE_SELECTION_DISPATCHER_PROVIDER} from '@angular/cdk/collections';
-import {VIEWPORT_RULER_PROVIDER} from '@angular/cdk/overlay';
-import {
-  MatRippleModule,
-  MatCommonModule,
-} from '@angular/material/core';
-import {MatRadioGroup, MatRadioButton} from './radio';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {MatCommonModule, MatRippleModule} from '@angular/material/core';
+import {MatRadioButton, MatRadioGroup} from './radio';
 
 @NgModule({
   imports: [CommonModule, MatRippleModule, MatCommonModule, A11yModule],
   exports: [MatRadioGroup, MatRadioButton, MatCommonModule],
-  providers: [UNIQUE_SELECTION_DISPATCHER_PROVIDER, VIEWPORT_RULER_PROVIDER],
+  providers: [UNIQUE_SELECTION_DISPATCHER_PROVIDER],
   declarations: [MatRadioGroup, MatRadioButton],
 })
 export class MatRadioModule {}
