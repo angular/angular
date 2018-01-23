48bf0edcd53da888e2895e55799a12bc5faabcb6
Emilio <emilio-martinez@users.noreply.github.com>
2018-01-23 08:02:11 -0800

    chore(overlay): export OverlayPositionBuilder (#9264)

--- src/cdk/overlay/public-api.ts
+++ src/cdk/overlay/public-api.ts
@@ -18,6 +18,7 @@ export {OverlayRef, OverlaySizeConfig} from './overlay-ref';
 export {ViewportRuler} from '@angular/cdk/scrolling';
 export {ComponentType} from '@angular/cdk/portal';
 export {OverlayKeyboardDispatcher} from './keyboard/overlay-keyboard-dispatcher';
+export {OverlayPositionBuilder} from './position/overlay-position-builder';
 
 // Export pre-defined position strategies and interface to build custom ones.
 export {PositionStrategy} from './position/position-strategy';
