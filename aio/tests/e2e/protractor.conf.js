22e866d857dc6864a13787bafb0b763c426ad5a0
Pete Bacon Darwin <pete@bacondarwin.com>
2018-01-24 14:21:38 +0000

    build(aio): move test config and e2e tests into subfolders (#21763)
    This is in preparation of putting firebase and service worker
    deployment tests into the project.
    
    PR Close #21763

--- aio/tests/e2e/protractor.conf.js
+++ aio/tests/e2e/protractor.conf.js
@@ -6,7 +6,7 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './e2e/**/*.e2e-spec.ts'
+    './*.e2e-spec.ts'
   ],
   capabilities: {
     browserName: 'chrome',
@@ -26,7 +26,7 @@ exports.config = {
   },
   beforeLaunch: function() {
     require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
+      project: 'tests/e2e/tsconfig.e2e.json'
     });
   },
   onPrepare() {
