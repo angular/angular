5a11373433d06d19f41a0b1c98f2c01331815379
Marc Laval <mlaval@amadeus.com>
2018-01-24 11:56:14 +0100

    perf(ivy): use buildOptimizer in hello_world__render3__rollup integration test (#21744)
    PR Close #21744

--- integration/hello_world__render3__rollup/rollup.config.js
+++ integration/hello_world__render3__rollup/rollup.config.js
@@ -1,32 +1,21 @@
+import buildOptimizer from 'rollup-plugin-angular-optimizer'
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import typescript2 from 'rollup-plugin-typescript2';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
-  input: `src/index.ts`,
+  input: `built/index.js`,
   output: {
     name: 'hw',
     file: `dist/bundle.js`,
     format: 'iife',
-    sourcemap: false
+    sourcemap: true
   },
   plugins: [
-    typescript2({
-      typescript: require('typescript'),
-      "experimentalDecorators": true,
-      "emitDecoratorMetadata": true
-    }),
-    replace({
-      delimiters: ['', ''],
-      values: {
-        '/** @class */': '/** @__PURE__ */'
-      }
-    }),
+    buildOptimizer(),
     nodeResolve({jsnext: true, module: true}),
     commonjs({
-      include: 'node_modules/rxjs/**',
+      include: 'node_modules/rxjs/**'
     }),
     uglify({
       mangle: true,
