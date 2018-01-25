bf29936af930f1848b5a85e32327edcba1694fe2
Pete Bacon Darwin <pete@bacondarwin.com>
2018-01-25 10:13:30 +0000

    build(aio): test Firebase hosting redirection configuration (#21763)
    PR Close #21763

---
+++ aio/tools/firebase-test-utils/FirebaseRedirect.ts
@@ -0,0 +1,16 @@
+import * as XRegExp from 'xregexp';
+import { FirebaseGlob } from './FirebaseGlob';
+
+export class FirebaseRedirect {
+  glob = new FirebaseGlob(this.source);
+  constructor(public source: string, public destination: string) {}
+
+  replace(url: string) {
+    const match = this.glob.match(url);
+    if (match) {
+      const paramReplacers = Object.keys(this.glob.namedParams).map(name => [ XRegExp(`:${name}`, 'g'), match[name] ]);
+      const restReplacers = Object.keys(this.glob.restParams).map(name => [ XRegExp(`:${name}\\*`, 'g'), match[name] ]);
+      return XRegExp.replaceEach(this.destination, [...paramReplacers, ...restReplacers]);
+    }
+  }
+}
