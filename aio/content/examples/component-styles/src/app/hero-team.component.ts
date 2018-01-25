b13403ea4a9654ae2db93684e9247d8a90d3d360
Pete Bacon Darwin <pete@bacondarwin.com>
2018-01-25 12:12:44 +0000

    docs(aio): fix paths to imported CSS stylesheets
    The AOT compiler needs relative paths so that it can find
    the imported stylesheets.

--- aio/content/examples/component-styles/src/app/hero-team.component.ts
+++ aio/content/examples/component-styles/src/app/hero-team.component.ts
@@ -5,7 +5,8 @@ import { Hero } from './hero';
 @Component({
   selector: 'app-hero-team',
   template: `
-    <link rel="stylesheet" href="assets/hero-team.component.css">
+    <!-- We must use a relative URL so that the AOT compiler can find the stylesheet -->
+    <link rel="stylesheet" href="../assets/hero-team.component.css">
     <h3>Team</h3>
     <ul>
       <li *ngFor="let member of hero.team">
