1038950dc69bb2c1a00b3de0cfa15c19d76b26bc
CDDelta <ngjunyen@gmail.com>
2018-01-27 02:34:50 +1100

    feat(layout): add window breakpoints from spec (#9318)

--- src/cdk/layout/breakpoints.ts
+++ src/cdk/layout/breakpoints.ts
@@ -8,6 +8,12 @@
 // PascalCase is being used as Breakpoints is used like an enum.
 // tslint:disable-next-line:variable-name
 export const Breakpoints = {
+  XSmall: '(max-width: 599px)',
+  Small: '(min-width: 600px) and (max-width: 959px)',
+  Medium: '(min-width: 960px) and (max-width: 1279px)',
+  Large: '(min-width: 1280px) and (max-width: 1919px)',
+  XLarge: '(min-width: 1920px)',
+
   Handset: '(max-width: 599px) and (orientation: portrait), ' +
            '(max-width: 959px) and (orientation: landscape)',
   Tablet: '(min-width: 600px) and (max-width: 839px) and (orientation: portrait), ' +
