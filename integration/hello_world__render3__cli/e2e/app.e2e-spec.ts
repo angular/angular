1f5314f44a0c3e12159fe644542ec14b891d0d8f
Chuck Jazdzewski <chuckj@google.com>
2018-01-25 16:43:29 -0800

    ci(ivy): enable size tracking of a minimal cli render3 application

---
+++ integration/hello_world__render3__cli/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('hw2 App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
