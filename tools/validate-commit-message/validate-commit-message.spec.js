885537861cf1aecc499466368e4ae8085d7028c2
Misko Hevery <misko@hevery.com>
2018-01-25 21:08:38 -0800

    fixup! build: autosquashes SHAs as part of merge-pr script

--- tools/validate-commit-message/validate-commit-message.spec.js
+++ tools/validate-commit-message/validate-commit-message.spec.js
@@ -9,6 +9,7 @@
 describe('validate-commit-message.js', function() {
   var validateMessage = require('./validate-commit-message');
   var SCOPES = validateMessage.config.scopes.join(', ');
+  var TYPES = validateMessage.config.types.join(', ');
   var errors = [];
   var logs = [];
 
@@ -43,6 +44,7 @@ describe('validate-commit-message.js', function() {
       expect(validateMessage('release(packaging): something')).toBe(VALID);
       expect(validateMessage('fixup! release(packaging): something')).toBe(VALID);
       expect(validateMessage('squash! release(packaging): something')).toBe(VALID);
+      expect(validateMessage('Revert: "release(packaging): something"')).toBe(VALID);
       expect(errors).toEqual([]);
     });
 
@@ -53,8 +55,6 @@ describe('validate-commit-message.js', function() {
       expect(validateMessage('style(webworker): something')).toBe(INVALID);
       expect(validateMessage('refactor(security): something')).toBe(INVALID);
       expect(validateMessage('refactor(docs): something')).toBe(INVALID);
-      expect(validateMessage('fixup! refactor(docs): something')).toBe(INVALID);
-      expect(validateMessage('squash! refactor(docs): something')).toBe(INVALID);
       ['INVALID COMMIT MSG: "fix(Compiler): something"\n' +
            ' => ERROR: "Compiler" is not an allowed scope.\n' +
            ' => SCOPES: ' + SCOPES,
@@ -66,12 +66,6 @@ describe('validate-commit-message.js', function() {
            ' => SCOPES: ' + SCOPES,
        'INVALID COMMIT MSG: "refactor(security): something"\n' +
            ' => ERROR: "security" is not an allowed scope.\n' +
-           ' => SCOPES: ' + SCOPES,
-       'INVALID COMMIT MSG: "refactor(docs): something"\n' +
-           ' => ERROR: "docs" is not an allowed scope.\n' +
-           ' => SCOPES: ' + SCOPES,
-       'INVALID COMMIT MSG: "refactor(docs): something"\n' +
-           ' => ERROR: "docs" is not an allowed scope.\n' +
            ' => SCOPES: ' + SCOPES,
        'INVALID COMMIT MSG: "refactor(docs): something"\n' +
            ' => ERROR: "docs" is not an allowed scope.\n' +
@@ -99,21 +93,24 @@ describe('validate-commit-message.js', function() {
 
       expect(validateMessage(msg)).toBe(INVALID);
       expect(errors).toEqual([
-        'INVALID COMMIT MSG: "not correct format"\n => ERROR: The commit message does not match the format of "<type>(<scope>): <subject> OR revert: type(<scope>): <subject>"'
+        'INVALID COMMIT MSG: "not correct format"\n => ERROR: The commit message does not match the format of \'<type>(<scope>): <subject>\' OR \'Revert: "type(<scope>): <subject>"\''
       ]);
     });
 
 
-    it('should support "revert: type(scope):" syntax and reject "revert(scope):" syntax', function() {
-      let correctMsg = 'revert: fix(compiler): reduce generated code payload size by 65%';
-      expect(validateMessage(correctMsg)).toBe(VALID);
+    it('should support "revert: type(scope):" syntax and reject "revert(scope):" syntax',
+       function() {
+         let correctMsg = 'revert: fix(compiler): reduce generated code payload size by 65%';
+         expect(validateMessage(correctMsg)).toBe(VALID);
 
-      let incorretMsg = 'revert(compiler): reduce generated code payload size by 65%';
-      expect(validateMessage(incorretMsg)).toBe(INVALID);
-      expect(errors).toEqual([
-        'INVALID COMMIT MSG: "revert(compiler): reduce generated code payload size by 65%"\n => ERROR: The commit message does not match the format of "<type>(<scope>): <subject> OR revert: type(<scope>): <subject>"'
-      ]);
-    });
+         let incorretMsg = 'revert(compiler): reduce generated code payload size by 65%';
+         expect(validateMessage(incorretMsg)).toBe(INVALID);
+         expect(errors).toEqual([
+           'INVALID COMMIT MSG: "revert(compiler): reduce generated code payload size by 65%"\n' +
+           ' => ERROR: revert is not an allowed type.\n' +
+           ' => TYPES: ' + TYPES
+         ]);
+       });
 
 
     it('should validate type', function() {
@@ -121,7 +118,7 @@ describe('validate-commit-message.js', function() {
       expect(errors).toEqual(
           ['INVALID COMMIT MSG: "weird($filter): something"\n' +
            ' => ERROR: weird is not an allowed type.\n' +
-           ' => TYPES: build, ci, docs, feat, fix, perf, refactor, release, style, test']);
+           ' => TYPES: ' + TYPES]);
     });
 
 
