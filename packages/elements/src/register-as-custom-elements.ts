8ce2ea37436af299bc5b6a80c42d60f388fbbd9a
Rob Wormald <robwormald@gmail.com>
2018-01-29 15:42:38 -0800

    docs(elements): add basic API docs example

--- packages/elements/src/register-as-custom-elements.ts
+++ packages/elements/src/register-as-custom-elements.ts
@@ -12,15 +12,52 @@ import {NgElements} from './ng-elements';
 import {isFunction} from './utils';
 
 /**
- * TODO(gkalpak): Add docs.
+ * @whatItDoes Registers an array of Angular Components as Custom Elements.
+ *
+ * @description registers the `customElementComponents` using an existing `PlatformRef` and compiled `NgModuleFactory`
+ *
+ * @param {Type<any>[]} customElementComponents
+ * @param {PlatformRef} platformRef
+ * @param {NgModuleFactory<T>} moduleFactory
+ * @returns {Promise<NgModuleRef<T>>}
  * @experimental
  */
 export function registerAsCustomElements<T>(
     customElementComponents: Type<any>[], platformRef: PlatformRef,
     moduleFactory: NgModuleFactory<T>): Promise<NgModuleRef<T>>;
+
+/**
+ *
+ * @description registers the `customElementComponents` using a async bootstrap function, allowing JIT usage (not recommended)
+ * @param {Type<any>[]} customElementComponents
+ * @param {() => Promise<NgModuleRef<T>>} bootstrapFn
+ * @returns {Promise<NgModuleRef<T>>}
+ */
 export function registerAsCustomElements<T>(
     customElementComponents: Type<any>[],
     bootstrapFn: () => Promise<NgModuleRef<T>>): Promise<NgModuleRef<T>>;
+/**
+ *
+ * @description The `customElement` components passed into this function are wrapped in a subclass of `NgElement` and registered with the browser's [`CustomElementRegistry`](https://developer.mozilla.org/en-US/docs/Web/API/CustomElementRegistry)
+ *
+ * ## Example
+ *
+ * ```typescript
+ * //index.ts
+ * import { registerAsCustomElements } from '@angular/elements';
+ * import { platformBrowser } from '@angular/platform-browser';
+ * import { HelloWorld } from './hello-world.ngfactory';
+ * import { HelloWorldModuleNgFactory } from './hello-world.ngfactory';
+ *
+ * registerAsCustomElements([HelloWorld], platformBrowser(), HelloWorldModuleNgFactory);
+ *   .catch(err => console.log(err));
+ * ```
+ *
+ * @param {Type<any>[]} customElementComponents
+ * @param {(PlatformRef | (() => Promise<NgModuleRef<T>>))} platformRefOrBootstrapFn
+ * @param {NgModuleFactory<T>} [moduleFactory]
+ * @returns {Promise<NgModuleRef<T>>}
+ */
 export function registerAsCustomElements<T>(
     customElementComponents: Type<any>[],
     platformRefOrBootstrapFn: PlatformRef | (() => Promise<NgModuleRef<T>>),
