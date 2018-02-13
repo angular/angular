f755db78dcdf461dc42e709b3ab728ceba353d1d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-13 12:51:21 -0800

    fix(core): require factory to be provided for shakeable InjectionToken (#22207)
    InjectionToken can be created with an ngInjectableDef, and previously
    this allowed the full expressiveness of @Injectable. However, this
    requires a runtime reflection system in order to generate factories
    from expressed provider declarations.
    
    Instead, this change requires scoped InjectionTokens to provide the
    factory directly (likely using inject() for the arguments), bypassing
    the need for a reflection system.
    
    Fixes #22205
    
    PR Close #22207

--- tools/public_api_guard/core/core.d.ts
+++ tools/public_api_guard/core/core.d.ts
@@ -492,7 +492,8 @@ export declare class InjectionToken<T> {
     readonly ngInjectableDef: Injectable | undefined;
     constructor(_desc: string, options?: {
         scope: Type<any>;
-    } & InjectionTokenProvider);
+        factory: () => T;
+    });
     toString(): string;
 }
 
