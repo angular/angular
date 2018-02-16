d02f9958c8ed0e157b43803d99b60548d9828746
Andrew Seguin <andrewjs@google.com>
2018-02-16 11:46:23 -0800

    docs(aio): make lint happy

--- tools/public_api_guard/elements/elements.d.ts
+++ tools/public_api_guard/elements/elements.d.ts
@@ -1,27 +1,16 @@
 /** @experimental */
-<<<<<<< HEAD
-export declare function registerAsCustomElements<T>(
-    customElementComponents: Type<any>[],
-    platformRef: PlatformRef,
-    moduleFactory: NgModuleFactory<T>): Promise<NgModuleRef<T>>;
-=======
-<<<<<<< Updated upstream
-export declare function createNgElementConstructor<T, P>(componentFactory: ComponentFactory<T>, injector: Injector): NgElementConstructor<T, P>;
-
-/** @experimental */
-export interface NgElementConstructor<T, P> {
-    injector: Injector;
-    readonly observedAttributes: string[];
-    new (): NgElementWithProps<T, P>;
-=======
 export declare function createNgElementConstructor<T, P>(componentFactory: ComponentFactory<T>, config: NgElementConfig): NgElementConstructor<T, P>;
 
 /** @experimental */
 export interface NgElementConfig {
-    injector: Injector;
->>>>>>> Stashed changes
+  injector: Injector;
 }
->>>>>>> dab48556dc... feat(elements): change param to config
+
+/** @experimental */
+export interface NgElementConstructor<T, P> {
+  readonly observedAttributes: string[];
+  new (): NgElementWithProps<T, P>;
+}
 
 /** @experimental */
 export declare const VERSION: Version;
