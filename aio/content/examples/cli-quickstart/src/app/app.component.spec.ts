5b7b20863728722f8e2090fe0bfa6bbba24e612d
Ward Bell <wardbell@hotmail.com>
2018-02-20 17:10:46 -0800

    docs: fix cli-quickstart doc and specs (#22338)
    * tests were broken
    * incorrect instructions.
    * didn't match current CLI template for new project
    
    PR Close #22338

--- aio/content/examples/cli-quickstart/src/app/app.component.spec.ts
+++ aio/content/examples/cli-quickstart/src/app/app.component.spec.ts
@@ -1,7 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
-
 import { AppComponent } from './app.component';
-
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,13 +18,13 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toMatch(/app/i);
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+    expect(compiled.querySelector('h1').textContent).toMatch(/app/i);
   }));
 });
