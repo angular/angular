544d2b53298def7640835ba0b475fdee9fc0c1fd
Otto Nascarella <otto@udacity.com>
2018-02-21 09:08:06 -0300

    fix(common):  inconsistency on TitleCasePipe
    TitleCasePipe was not behaving as expected with on strings containing accents like é ó ú ã, etc.
    
    refactor(common): TitleCasePipe not using localeLowerCase/localeUpperCase

--- packages/common/test/pipes/case_conversion_pipes_spec.ts
+++ packages/common/test/pipes/case_conversion_pipes_spec.ts
@@ -19,6 +19,7 @@ import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
     it('should lowercase when there is a new value', () => {
       expect(pipe.transform('FOO')).toEqual('foo');
       expect(pipe.transform('BAr')).toEqual('bar');
+      expect(pipe.transform('avanÇaDO')).toEqual('avançado');
     });
 
     it('should not support other objects',
@@ -30,15 +31,24 @@ import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
 
     beforeEach(() => { pipe = new TitleCasePipe(); });
 
-    it('should return titlecase', () => { expect(pipe.transform('foo')).toEqual('Foo'); });
+    it('should return titlecase', () => {
+      expect(pipe.transform('foo')).toEqual('Foo');
+      expect(pipe.transform('intermediário')).toEqual('Intermediário');
+      expect(pipe.transform('føderation')).toEqual('Føderation');
+      expect(pipe.transform('bênção')).toEqual('Bênção');
+    });
 
-    it('should return titlecase for subsequent words',
-       () => { expect(pipe.transform('one TWO Three fouR')).toEqual('One Two Three Four'); });
+    it('should return titlecase for subsequent words', () => {
+      expect(pipe.transform('one TWO Three fouR')).toEqual('One Two Three Four');
+      expect(pipe.transform('isto é um teSTE avançado')).toEqual('Isto É Um Teste Avançado');
+    });
 
     it('should support empty strings', () => { expect(pipe.transform('')).toEqual(''); });
 
-    it('should persist whitespace',
-       () => { expect(pipe.transform('one   two')).toEqual('One   Two'); });
+    it('should persist whitespace', () => {
+      expect(pipe.transform('one   two')).toEqual('One   Two');
+      expect(pipe.transform('coração   átomo')).toEqual('Coração   Átomo');
+    });
 
     it('should titlecase when there is a new value', () => {
       expect(pipe.transform('bar')).toEqual('Bar');
@@ -59,9 +69,17 @@ import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
     it('should uppercase when there is a new value', () => {
       expect(pipe.transform('foo')).toEqual('FOO');
       expect(pipe.transform('bar')).toEqual('BAR');
+      expect(pipe.transform('doçura')).toEqual('DOÇURA');
     });
 
     it('should not support other objects',
        () => { expect(() => pipe.transform(<any>{})).toThrowError(); });
+
+    it('should not replace \n \t by space',
+       () => { expect(pipe.transform('isto\té\tum\nteste')).toEqual('Isto\tÉ\tUm\nTeste'); });
+
+    it('should capitalize after comma, dot and whitespaces',
+       () => { expect(pipe.transform('um,dois.três, quatro')).toEqual('Um,Dois.Três, Quatro'); });
+
   });
 }
