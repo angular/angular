4473da7de7f0bb1e5c5dc556789ba71a4054d6e0
George Kalpakas <kalpakas.g@gmail.com>
2018-02-28 01:24:07 +0200

    ci(aio): add monitoring for angular.io (#23093)
    This commit configures a periodic job to be run on CircleCI, performing several
    checks against the actual apps deployed to production (https://angular.io) and
    staging (https://next.angular.io).
    
    Fixes #21942
    
    PR Close #23093

--- aio/tests/deployment-config/shared/helpers.ts
+++ aio/tests/deployment-config/shared/helpers.ts
@@ -1,6 +1,8 @@
 import { resolve } from 'canonical-path';
 import { load as loadJson } from 'cjson';
 import { readFileSync } from 'fs';
+import { get as httpGet } from 'http';
+import { get as httpsGet } from 'https';
 
 import { FirebaseRedirector, FirebaseRedirectConfig } from '../../../tools/firebase-test-utils/FirebaseRedirector';
 
@@ -17,18 +19,33 @@ export function loadRedirects(): FirebaseRedirectConfig[] {
   return contents.hosting.redirects;
 }
 
-export function loadSitemapUrls() {
-  const pathToSiteMap = `${AIO_DIR}/src/generated/sitemap.xml`;
-  const xml = readFileSync(pathToSiteMap, 'utf8');
-  const urls: string[] = [];
-  xml.replace(/<loc>([^<]+)<\/loc>/g, (_, loc) => urls.push(loc.replace('%%DEPLOYMENT_HOST%%', '')));
-  return urls;
-}
-
 export function loadLegacyUrls() {
   const pathToLegacyUrls = `${__dirname}/URLS_TO_REDIRECT.txt`;
   const urls = readFileSync(pathToLegacyUrls, 'utf8').split('\n').map(line => line.split('\t'));
   return urls;
+}
+
+export function loadLocalSitemapUrls() {
+  const pathToSiteMap = `${AIO_DIR}/src/generated/sitemap.xml`;
+  const xml = readFileSync(pathToSiteMap, 'utf8');
+  return extractSitemapUrls(xml);
+}
+
+export async function loadRemoteSitemapUrls(host: string) {
+  const urlToSiteMap = `${host}/generated/sitemap.xml`;
+  const get = /^https:/.test(host) ? httpsGet : httpGet;
+
+  const xml = await new Promise<string>((resolve, reject) => {
+    let responseText = '';
+    get(urlToSiteMap, res => res
+        .on('data', chunk => responseText += chunk)
+        .on('end', () => resolve(responseText))
+        .on('error', reject));
+  });
+
+  // Currently, all sitemaps use `angular.io` as host in URLs (which is fine since we only use the
+  // sitemap `angular.io`). See also `aio/src/extra-files/*/robots.txt`.
+  return extractSitemapUrls(xml, 'https://angular.io/');
 }
 
 export function loadSWRoutes() {
@@ -49,4 +66,11 @@ export function loadSWRoutes() {
         throw new Error(`unknown route config: ${route} - ${routeConfig.match}`);
     }
   });
+}
+
+// Private functions
+function extractSitemapUrls(xml: string, host = '%%DEPLOYMENT_HOST%%') {
+  const urls: string[] = [];
+  xml.replace(/<loc>([^<]+)<\/loc>/g, (_, loc) => urls.push(loc.replace(host, '')) as any);
+  return urls;
 }
