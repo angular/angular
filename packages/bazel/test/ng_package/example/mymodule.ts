aabe16c08c9886ff2ee0bed212034bf29336ecce
Alex Eagle <alexeagle@google.com>
2018-02-28 09:12:39 -0800

    fix(bazel): ng_package includes transitive .d.ts and flatModuleMetadata (#22499)
    Fixes #22419
    
    PR Close #22499

--- packages/bazel/test/ng_package/example/mymodule.ts
+++ packages/bazel/test/ng_package/example/mymodule.ts
@@ -1,4 +1,13 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
 import {NgModule} from '@angular/core';
+import {a} from './secondary/secondarymodule';
 
 @NgModule({})
 export class MyModule {
