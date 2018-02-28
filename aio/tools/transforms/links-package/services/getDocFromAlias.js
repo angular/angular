c4e5c5704cd11048b40e0da929e2286de0ac0ee9
Pete Bacon Darwin <pete@bacondarwin.com>
2018-02-28 14:52:29 +0000

    build(aio): move link disambiguation from `getLinkInfo` to `getDocFromAlias` (#22494)
    The disambiguation needs to be done earlier so that the auto-link-code
    post-processor can benefit from it.
    
    PR Close #22494

--- aio/tools/transforms/links-package/services/getDocFromAlias.js
+++ aio/tools/transforms/links-package/services/getDocFromAlias.js
@@ -1,31 +1,23 @@
-var _ = require('lodash');
-
 /**
  * @dgService getDocFromAlias
  * @description Get an array of docs that match this alias, relative to the originating doc.
+ *
+ * @property {Array<(alias: string, originatingDoc: Doc, ambiguousDocs: Doc[]) => Doc[]>} disambiguators
+ *           a collection of functions that attempt to resolve ambiguous links. Each disambiguator returns
+ *           a new collection of docs with unwanted ambiguous docs removed (see links-package/service/disambiguators
+ *           for examples).
  */
 module.exports = function getDocFromAlias(aliasMap) {
 
-  return function getDocFromAlias(alias, originatingDoc) {
-    var docs = aliasMap.getDocs(alias);
+  getDocFromAlias.disambiguators = [];
+  return getDocFromAlias;
 
-    // If there is more than one item with this name then try to filter them by the originatingDoc's
-    // area
-    if (docs.length > 1 && originatingDoc && originatingDoc.area) {
-      docs = _.filter(docs, function(doc) { return doc.area === originatingDoc.area; });
-    }
-
-    // If filtering by area left us with none then let's start again
-    if (docs.length === 0) {
-      docs = aliasMap.getDocs(alias);
-    }
-
-    // If there is more than one item with this name then try to filter them by the originatingDoc's
-    // module
-    if (docs.length > 1 && originatingDoc && originatingDoc.module) {
-      docs = _.filter(docs, function(doc) { return doc.module === originatingDoc.module; });
-    }
-
-    return docs;
-  };
+  function getDocFromAlias(alias, originatingDoc) {
+    return getDocFromAlias.disambiguators.reduce(
+      // Run the disambiguators while there is more than 1 doc found
+      (docs, disambiguater) => docs.length > 1 ? disambiguater(alias, originatingDoc, docs) : docs,
+      // Start with the docs that match the alias
+      aliasMap.getDocs(alias)
+    );
+  }
 };
