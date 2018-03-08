112431db693a3f274675ff7b4b3f01154f40d417
Miško Hevery <misko@hevery.com>
2018-03-08 13:57:56 -0800

    test(ivy): add canonical compiler spec for class/style (#22719)
    Adds a stub for `elementStyle` and `elementClass` instruction
    with a canonical spec for the compiler. The spec shows the the
    compiler should be using `elementStyle` and `elementClass` instruction
    in place of `[class]` and `[style]` bindings respectively.
    PR Close #22719

--- modules/benchmarks/src/tree/render3/tree.ts
+++ modules/benchmarks/src/tree/render3/tree.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {ɵC as C, ɵE as E, ɵT as T, ɵV as V, ɵb as b, ɵcR as cR, ɵcr as cr, ɵdefineComponent as defineComponent, ɵdetectChanges as _detectChanges, ɵe as e, ɵi1 as i1, ɵp as p, ɵs as s, ɵt as t, ɵv as v} from '@angular/core';
+import {ɵC as C, ɵE as E, ɵT as T, ɵV as V, ɵb as b, ɵcR as cR, ɵcr as cr, ɵdefineComponent as defineComponent, ɵdetectChanges as _detectChanges, ɵe as e, ɵi1 as i1, ɵp as p, ɵsn as sn, ɵt as t, ɵv as v} from '@angular/core';
 import {ComponentDef} from '@angular/core/src/render3/interfaces/definition';
 
 import {TreeNode, buildTree, emptyTree} from '../util';
@@ -46,7 +46,7 @@ export class TreeComponent {
         C(2);
         C(3);
       }
-      s(0, 'background-color', b(ctx.data.depth % 2 ? '' : 'grey'));
+      sn(0, 'background-color', b(ctx.data.depth % 2 ? '' : 'grey'));
       t(1, i1(' ', ctx.data.value, ' '));
       cR(2);
       {
@@ -114,7 +114,7 @@ export function TreeTpl(ctx: TreeNode, cm: boolean) {
     }
     e();
   }
-  s(1, 'background-color', b(ctx.depth % 2 ? '' : 'grey'));
+  sn(1, 'background-color', b(ctx.depth % 2 ? '' : 'grey'));
   t(2, i1(' ', ctx.value, ' '));
   cR(3);
   {
