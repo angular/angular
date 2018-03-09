e2010949799338f2c43775a47e53b8f3683497fd
Igor Minar <igor@angular.io>
2018-03-08 20:39:41 -0800

    build: introduce a temporary patch to make node_modules/rxjs compile with typescript 2.7
    This patch can be removed once we update to rxjs v6. See #22573.

--- tools/postinstall-patches.js
+++ tools/postinstall-patches.js
@@ -9,5 +9,11 @@ set('-v');
 // jump to project root
 cd(path.join(__dirname, '../'));
 
+// https://github.com/ReactiveX/rxjs/pull/3302
+// make node_modules/rxjs compilable with Typescript 2.7
+// remove when we update to rxjs v6
+console.log('\n# patch: reactivex/rxjs#3302 make node_modules/rxjs compilable with Typescript 2.7')
+sed('-i', "('response' in xhr)", "('response' in (xhr as any))", "node_modules/rxjs/src/observable/dom/AjaxObservable.ts")
+
 
 console.log('===== finished running the postinstall.js script =====');
