5c7229824acd5e37eece77998e9171a146b0a41f
Pete Bacon Darwin <pete@bacondarwin.com>
2018-03-09 08:34:58 +0000

    build(aio): render doc-gen issues in overview dump
    Related to #22138

--- aio/tools/transforms/angular-api-package/tag-defs/stable.js
+++ aio/tools/transforms/angular-api-package/tag-defs/stable.js
@@ -1,6 +1,7 @@
 module.exports = function(log, createDocMessage) {
   return {
     name: 'stable',
+    deprecated: true,
     transforms(doc, tag, value) {
       log.warn(createDocMessage('Deprecated `@stable` tag found', doc));
       log.warn('PLEASE REMOVE - its value is now computed.');
