f133da94d0a0e79e2155129c0e9b2ff1835cfbb9
mmalerba <mmalerba@google.com>
2018-03-13 09:00:28 -0700

    refactor(form-field): remove deprecated @Inputs (#10294)
    small piece of #10164, split out for presubmit reasons
    
    BREAKING CHANGES:
    - dividerColor which was deprecated in 5.0.0 has been removed. Use color instead.
    - floatPlaceholder which was deprecated in 5.0.0 has been removed. Use floatLabel instead.

--- src/lib/form-field/form-field.ts
+++ src/lib/form-field/form-field.ts
@@ -32,7 +32,6 @@ import {
   LabelOptions,
   MAT_LABEL_GLOBAL_OPTIONS,
   mixinColor,
-  ThemePalette
 } from '@angular/material/core';
 import {fromEvent} from 'rxjs/observable/fromEvent';
 import {startWith} from 'rxjs/operators/startWith';
@@ -138,14 +137,6 @@ export class MatFormField extends _MatFormFieldMixinBase
   }
   _appearance: MatFormFieldAppearance;
 
-  /**
-   * @deprecated Use `color` instead.
-   * @deletion-target 6.0.0
-   */
-  @Input()
-  get dividerColor(): ThemePalette { return this.color; }
-  set dividerColor(value: ThemePalette) { this.color = value; }
-
   /** Whether the required marker should be hidden. */
   @Input()
   get hideRequiredMarker(): boolean { return this._hideRequiredMarker; }
@@ -179,15 +170,6 @@ export class MatFormField extends _MatFormFieldMixinBase
 
   // Unique id for the hint label.
   _hintLabelId: string = `mat-hint-${nextUniqueId++}`;
-
-  /**
-   * Whether the placeholder should always float, never float or float as the user types.
-   * @deprecated Use floatLabel instead.
-   * @deletion-target 6.0.0
-   */
-  @Input()
-  get floatPlaceholder(): FloatLabelType { return this.floatLabel; }
-  set floatPlaceholder(value: FloatLabelType) { this.floatLabel = value; }
 
   /**
    * Whether the label should always float, never float or float as the user types.
