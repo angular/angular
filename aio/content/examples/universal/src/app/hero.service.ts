98b18cd49f4d4e477facd3a267ad953b3b6ef9ce
Daniel Sogl <daniel@sogls.de>
2018-03-13 16:06:03 +0100

    docs: unified console.log single string style (#22737)
    PR Close #22737

--- aio/content/examples/universal/src/app/hero.service.ts
+++ aio/content/examples/universal/src/app/hero.service.ts
@@ -30,7 +30,7 @@ export class HeroService {
   getHeroes (): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
-        tap(heroes => this.log(`fetched heroes`)),
+        tap(heroes => this.log('fetched heroes')),
         catchError(this.handleError('getHeroes', []))
       );
   }
