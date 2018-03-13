b31ebc0237a8d98f863301e55b62d5d21495ecb5
Aaron Gussman <agussman@thehumangeo.com>
2018-03-13 01:53:27 -0400

    docs: update docs to use HttpClientModule instead of HttpModule (#22727)
    Updated most examples to use HttpClientModule instead of deprecated HttpModule
    
    fix #19280
    
    PR Close #22727

--- aio/content/examples/bootstrapping/src/app/app.module.ts
+++ aio/content/examples/bootstrapping/src/app/app.module.ts
@@ -5,7 +5,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 // #docregion directive-import
@@ -24,7 +24,7 @@ import { ItemDirective } from './item.directive';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- aio/content/examples/lazy-loading-ngmodules/src/app/app.module.ts
+++ aio/content/examples/lazy-loading-ngmodules/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -13,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
