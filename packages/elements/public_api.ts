0af168869e1598801100ea6959161cc35b60faa4
Andrew Seguin <andrewjs@google.com>
2018-03-14 13:24:27 -0700

    feat(elements): rename API to createCustomElement (#22413)
    PR Close #22413

--- packages/elements/public_api.ts
+++ packages/elements/public_api.ts
@@ -12,7 +12,7 @@
  * Entry point for all public APIs of the `elements` package.
  */
 export {NgElementStrategy, NgElementStrategyEvent, NgElementStrategyFactory} from './src/element-strategy';
-export {NgElement, NgElementConfig, NgElementConstructor, WithProperties, createNgElementConstructor} from './src/ng-element-constructor';
+export {NgElement, NgElementConfig, NgElementConstructor, WithProperties, createCustomElement} from './src/create-custom-element';
 export {VERSION} from './src/version';
 
 // This file only reexports content of the `src` folder. Keep it that way.
