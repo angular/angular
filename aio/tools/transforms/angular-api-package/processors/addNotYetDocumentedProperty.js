5b2b04f7e447332814a7246dec051c9007b82c82
Pete Bacon Darwin <pete@bacondarwin.com>
2018-03-14 19:15:11 +0000

    build(aio): fix `addNotYetDocumentedProperty` processor
    It was running too late and so was being confused by the
    description being split into `shortDescription` and `description`
    properties.
    
    Closes #22748

--- aio/tools/transforms/angular-api-package/processors/addNotYetDocumentedProperty.js
+++ aio/tools/transforms/angular-api-package/processors/addNotYetDocumentedProperty.js
@@ -1,37 +1,19 @@
-module.exports = function addNotYetDocumentedProperty(EXPORT_DOC_TYPES, log, createDocMessage) {
+module.exports = function addNotYetDocumentedProperty(log, createDocMessage) {
   return {
-    $runAfter: ['tags-parsed'],
-    $runBefore: ['rendering-docs'],
-    $process: function(docs) {
-      docs.forEach(function(doc) {
-
-        if (EXPORT_DOC_TYPES.indexOf(doc.docType) === -1) return;
-
-        // NotYetDocumented means that no top level comments and no member level comments
-        doc.notYetDocumented = notYetDocumented(doc);
-
-        if (doc.constructorDoc) {
-          doc.constructorDoc.notYetDocumented = notYetDocumented(doc.constructorDoc);
-          doc.notYetDocumented = doc.notYetDocumented && doc.constructorDoc.notYetDocumented;
-        }
-
-        if (doc.members) {
-          doc.members.forEach(function(member) {
-            member.notYetDocumented = notYetDocumented(member);
-            doc.notYetDocumented = doc.notYetDocumented && member.notYetDocumented;
-          });
-        }
-
-        if (doc.notYetDocumented) {
+    docTypes: [],
+    $runAfter: ['tags-extracted'],
+    $runBefore: ['processing-docs', 'splitDescription'],
+    $process(docs) {
+      docs.forEach(doc => {
+        if (
+          this.docTypes.indexOf(doc.docType) !== -1 &&
+          !doc.noDescription &&
+          (!doc.description || doc.description.trim().length === 0)
+        ) {
+          doc.notYetDocumented = true;
           log.debug(createDocMessage('Not yet documented', doc));
         }
       });
-
-      return docs;
     }
   };
 };
-
-function notYetDocumented(doc) {
-  return !doc.noDescription && doc.description.trim().length == 0;
-}
