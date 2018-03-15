b4b1745677b0deb54343cee1e4fa16c1af1047e3
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-03-15 21:40:12 +0530

    Changed directive name from `MdAccordion` to `MatAccordion` (#10434)

--- src/lib/expansion/expansion-panel.ts
+++ src/lib/expansion/expansion-panel.ts
@@ -45,7 +45,7 @@ let uniqueId = 0;
  * `<mat-expansion-panel>`
  *
  * This component can be used as a single element to show expandable content, or as one of
- * multiple children of an element with the MdAccordion directive attached.
+ * multiple children of an element with the MatAccordion directive attached.
  */
 @Component({
   moduleId: module.id,
