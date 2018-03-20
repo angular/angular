14403bf0a00c198dd1616113c7415491e66a94c7
Igor Minar <igor@angular.io>
2018-03-20 14:38:03 -0700

    fixup! build: add rxjs patch to make bazel build work with rxjs@6.0.0-beta.0

--- tools/postinstall-patches.js
+++ tools/postinstall-patches.js
@@ -28,7 +28,8 @@ sed('-i', '(\'response\' in xhr)', '(\'response\' in (xhr as any))',
 */
 
 // fixes rxjs bazel build: https://github.com/ReactiveX/rxjs/pull/3454
-sed('-i', /^  srcs = glob.*/, 'srcs = glob(["*.ts", "**/*.ts"], exclude = ["internal/Rx.ts", "internal-compatibility/**",  "internal/patching/**", "umd.ts"]),',
-  'node_modules/rxjs/src/BUILD.bazel');
+sed('-i', /^  srcs = glob.*/,
+    'srcs = glob(["*.ts", "**/*.ts"], exclude = ["internal/Rx.ts", "internal-compatibility/**",  "internal/patching/**", "umd.ts"]),',
+    'node_modules/rxjs/src/BUILD.bazel');
 
 log('===== finished running the postinstall-patches.js script =====');
