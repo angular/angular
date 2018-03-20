a59a2c3d8ac79028d8750b94c7736676b3a3f429
cexbrayat <cedric@ninja-squad.com>
2018-03-20 15:45:09 +0100

    fix(aio): fix error in import after RxJS 6 migration (#22886)
    PR Close #22886

--- aio/content/examples/pipes/src/app/app.module.ts
+++ aio/content/examples/pipes/src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular//common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {
