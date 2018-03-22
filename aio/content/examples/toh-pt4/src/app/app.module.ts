7d04c13dc08fabe6ef65566a9c4636323bc2087b
Judy Bogart <jbogart@gmail.com>
2018-03-22 10:37:50 -0700

    docs: recommend best practice for providers (#22934)
    PR Close #22934

--- aio/content/examples/toh-pt4/src/app/app.module.ts
+++ aio/content/examples/toh-pt4/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -19,13 +18,9 @@ import { MessagesComponent } from './messages/messages.component';
     FormsModule
   ],
   // #docregion providers
-  // #docregion providers-heroservice
   providers: [
-    // #enddocregion providers-heroservice
     // no need to place any providers due to the `providedIn` flag...
-    // #docregion providers-heroservice
   ],
-  // #enddocregion providers-heroservice
   // #enddocregion providers
   bootstrap: [ AppComponent ]
 })
