fd1f2436ed8b2424a8a5be992d8190dc5dc317fc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-23 08:36:48 -0700

    docs: update examples for tree-shakeable providers

--- aio/content/examples/dependency-injection/src/app/heroes/hero.service.3.ts
+++ aio/content/examples/dependency-injection/src/app/heroes/hero.service.3.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { HEROES }     from './mock-heroes';
 
 @Injectable({
+  // we declare that this service should be created
+  // by the root application injector.
+
   providedIn: 'root',
 })
 export class HeroService {
--- aio/content/examples/providers/src/app/user.service.0.ts
+++ aio/content/examples/providers/src/app/user.service.0.ts
@@ -3,6 +3,5 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class HeroService {
-  constructor() { }
+export class UserService {
 }
