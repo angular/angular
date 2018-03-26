70fdbf5d0a8a4d3b82b0b483d6b99c2ff53e3e41
Olivier Combe <olivier.combe@gmail.com>
2018-03-26 19:05:39 +0200

    test(ivy): make closure global for hello world i18n demo

--- packages/core/test/bundling/hello_world_i18n/index.ts
+++ packages/core/test/bundling/hello_world_i18n/index.ts
@@ -8,15 +8,6 @@
 
 import {Component, NgModule, ɵrenderComponent as renderComponent} from '@angular/core';
 
-// simulate translations for now
-const translations: {[key: string]: string} = {
-  'Hello World!': 'Bonjour Monde!',
-  'Hello Title!': 'Bonjour Titre!',
-};
-
-// simulate Google Closure getMsg for now
-(window as any).goog = {getMsg: (key: string) => { return translations[key] || key; }};
-
 @Component({
   selector: 'hello-world',
   template: `<div i18n i18n-title title="Hello Title!">Hello World!</div>`
@@ -26,7 +17,7 @@ export class HelloWorld {
 // TODO(misko): Forgetting to export HelloWorld and not having NgModule fails silently.
 
 @NgModule({declarations: [HelloWorld]})
-export class INeedToExistEvenThoughtIAmNotNeeded {
+export class INeedToExistEvenThoughIAmNotNeeded {
 }
 // TODO(misko): Package should not be required to make this work.
 
