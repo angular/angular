0b8240ce9241fa22d721434c46ccd99d34a7ef10
tinayuangao <tinagao@google.com>
2018-03-27 11:46:34 -0700

    fix(tree): fix the MatTreeNodeOutlet not exported issue (#10581)

--- src/lib/tree/public-api.ts
+++ src/lib/tree/public-api.ts
@@ -12,5 +12,6 @@ export * from './padding';
 export * from './tree';
 export * from './tree-module';
 export * from './toggle';
+export * from './outlet';
 export * from './data-source/flat-data-source';
 export * from './data-source/nested-data-source';
