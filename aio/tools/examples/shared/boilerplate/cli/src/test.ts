10c91ecdd5ad623bf0d9f456a904b36cf122ccbd
JiaLi.Passion <JiaLi.Passion@gmail.com>
2018-04-01 00:44:38 +0900

    build: upgrade zone.js (#23108)
    PR Close #23108

--- aio/tools/examples/shared/boilerplate/cli/src/test.ts
+++ aio/tools/examples/shared/boilerplate/cli/src/test.ts
@@ -1,11 +1,6 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
