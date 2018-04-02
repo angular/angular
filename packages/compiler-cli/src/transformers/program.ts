f99cb5c995884fc0e6783172380c292ffbf0b510
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 13:05:08 -0700

    fix(compiler-cli): flat module index metadata should be transformed (#23129)
    Currently, the flat module index metadata is produced directly from
    the source metadata. The compiler, however, applies transformations
    on the Typescript sources during transpilation, and also equivalent
    transformations on the metadata itself. This transformed metadata
    doesn't end up in the flat module index.
    
    This changes the compiler to generate the flat module index metadata
    from its transformed version instead of directly from source.
    
    PR Close #23129

--- packages/compiler-cli/src/transformers/program.ts
+++ packages/compiler-cli/src/transformers/program.ts
@@ -149,7 +149,7 @@ class AngularCompilerProgram implements Program {
 
     if (options.flatModuleOutFile) {
       const {host: bundleHost, indexName, errors} =
-          createBundleIndexHost(options, this.rootNames, host);
+          createBundleIndexHost(options, this.rootNames, host, () => this.metadataCache);
       if (errors) {
         this._optionsDiagnostics.push(...errors.map(e => ({
                                                       category: e.category,
@@ -338,7 +338,6 @@ class AngularCompilerProgram implements Program {
       writeFile: writeTsFile, emitOnlyDtsFiles,
       customTransformers: tsCustomTransformers
     });
-
     return emitResult;
   }
 
@@ -518,6 +517,7 @@ class AngularCompilerProgram implements Program {
         `- ${genJsonFiles.length + metadataJsonCount} generated json files`,
       ].join('\n'))]);
     }
+
     return emitResult;
   }
 
