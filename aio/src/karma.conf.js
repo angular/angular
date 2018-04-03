eb852e5a213113821d40c7ec1200ccd35daa1d3b
Filipe Silva <filipematossilva@gmail.com>
2018-04-03 17:33:20 +0100

    build(aio): followup fixes to @angular/cli@6.0.0-rc.0 update

--- aio/src/karma.conf.js
+++ aio/src/karma.conf.js
@@ -16,7 +16,8 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'),
+      reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
     angularCli: {
