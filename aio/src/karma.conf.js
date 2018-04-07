fc5af69fb2f97c2c753f417432df87f28aefafd6
Igor Minar <igor@angular.io>
2018-04-06 21:09:12 -0700

    build(aio): update to @angular/cli@6.0.0-rc.2 + project layout update (#23234)
    project layout was updated using:
    yarn ng update @angular/cli --migrate-only --from=1.7.3
    
    PR Close #23234

--- aio/src/karma.conf.js
+++ aio/src/karma.conf.js
@@ -4,19 +4,19 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
     angularCli: {
