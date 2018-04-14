ac47a3cd93893061c7603a60d7e844c31c24dc92
Igor Minar <igor@angular.io>
2018-04-14 10:31:24 -0700

    test(aio): move reflect-metadata polyfills to test.ts (#23234)
    This resolves https://github.com/angular/angular-cli/issues/10333 and nicely cleans up the code.
    
    PR Close #23234

--- aio/src/environments/environment.ts
+++ aio/src/environments/environment.ts
@@ -4,12 +4,10 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 
-// Reflect.metadata polyfill is only needed in the JIT/dev mode.
-//
-// In order to load these polyfills early enough (before app code), polyfill.ts imports this file to
-// to change the order in the final bundle.
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+// Reflect.metadata polyfill is only needed in the JIT, which use use only for tests
+// to make the unit tests work we load these polyfills in tests.ts instead
+// import 'core-js/es6/reflect';
+// import 'core-js/es7/reflect';
 
 
 export const environment = {
