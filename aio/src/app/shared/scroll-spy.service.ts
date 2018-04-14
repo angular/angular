8ee11aeaa63fb5b75cb956ed1201d027736cf3c0
Igor Minar <igor@angular.io>
2018-04-14 00:44:09 -0700

    build(aio): update tslint and codelyzer (#23234)
    PR Close #23234

--- aio/src/app/shared/scroll-spy.service.ts
+++ aio/src/app/shared/scroll-spy.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
-import { DOCUMENT } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
 import { fromEvent, Observable, ReplaySubject, Subject } from 'rxjs';
 import { auditTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
