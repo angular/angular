7d1990e4d190270885227d52af249d0c356bd9d6
Igor Minar <igor@angular.io>
2018-04-14 02:36:42 -0700

    style(aio): lint fixes for examples (#23234)
    PR Close #23234

--- aio/content/examples/dynamic-component-loader/src/app/hero-profile.component.ts
+++ aio/content/examples/dynamic-component-loader/src/app/hero-profile.component.ts
@@ -8,7 +8,7 @@ import { AdComponent }       from './ad.component';
     <div class="hero-profile">
       <h3>Featured Hero Profile</h3>
       <h4>{{data.name}}</h4>
-      
+
       <p>{{data.bio}}</p>
 
       <strong>Hire this hero today!</strong>
