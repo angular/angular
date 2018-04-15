7de69ba29b407186a80a6d05ad8bad465c162d90
George Kalpakas <kalpakas.g@gmail.com>
2018-04-16 00:02:28 +0300

    ci(aio): fix `aio-monitoring` tests (#23390)
    Previously, we were running the e2e tests from master against
    `https://angular.io` (deployed from the stable branch). Often the e2e
    tests from master do not apply to the stable branch, since the app has
    deviated slightly.
    
    This commit fixes this by stop running the full e2e tests against the
    deployed versions, but a smaller set of "smoke tests", which check basic
    functionality that is less likely to change between versions.
    
    PR Close #23390

--- aio/tests/deployment/e2e/redirection.e2e-spec.ts
+++ aio/tests/deployment/e2e/redirection.e2e-spec.ts
@@ -1,31 +1,18 @@
 import { browser } from 'protractor';
+import { SitePage } from './site.po';
 
 describe(browser.baseUrl, () => {
-  const sitemapUrls = browser.params.sitemapUrls;
-  const legacyUrls = browser.params.legacyUrls;
-  const goTo = async url => {
-    // Go to the specified URL and then unregister the ServiceWorker
-    // to ensure subsequent requests are passed through to the server.
-    await browser.get(url);
-    await browser.executeAsyncScript(cb => navigator.serviceWorker
-        .getRegistrations()
-        .then(regs => Promise.all(regs.map(reg => reg.unregister())))
-        .then(cb));
-  };
+  const page = new SitePage();
 
-  beforeAll(async done => {
-    // Make an initial request to unregister the ServiceWorker.
-    await goTo(browser.baseUrl);
-    done();
-  });
+  beforeAll(done => page.init().then(done));
 
   beforeEach(() => browser.waitForAngularEnabled(false));
   afterEach(() => browser.waitForAngularEnabled(true));
 
   describe('(with sitemap URLs)', () => {
-    sitemapUrls.forEach((url, i) => {
-      it(`should not redirect '${url}' (${i + 1}/${sitemapUrls.length})`, async () => {
-        await goTo(url);
+    page.sitemapUrls.forEach((url, i) => {
+      it(`should not redirect '${url}' (${i + 1}/${page.sitemapUrls.length})`, async () => {
+        await page.goTo(url);
 
         const expectedUrl = browser.baseUrl + url;
         const actualUrl = (await browser.getCurrentUrl()).replace(/\?.*$/, '');
@@ -36,9 +23,9 @@ describe(browser.baseUrl, () => {
   });
 
   describe('(with legacy URLs)', () => {
-    legacyUrls.forEach(([fromUrl, toUrl], i) => {
-      it(`should redirect '${fromUrl}' to '${toUrl}' (${i + 1}/${legacyUrls.length})`, async () => {
-        await goTo(fromUrl);
+    page.legacyUrls.forEach(([fromUrl, toUrl], i) => {
+      it(`should redirect '${fromUrl}' to '${toUrl}' (${i + 1}/${page.legacyUrls.length})`, async () => {
+        await page.goTo(fromUrl);
 
         const expectedUrl = (/^http/.test(toUrl) ? '' : browser.baseUrl.replace(/\/$/, '')) + toUrl;
         const actualUrl = (await browser.getCurrentUrl()).replace(/\?.*$/, '');
---
+++ aio/tests/deployment/e2e/smoke-tests.e2e-spec.ts
@@ -0,0 +1,103 @@
+import { browser } from 'protractor';
+import { SitePage } from './site.po';
+
+describe(browser.baseUrl, () => {
+  const page = new SitePage();
+
+  beforeAll(done => page.init().then(done));
+
+  beforeEach(() => browser.waitForAngularEnabled(false));
+  afterEach(() => browser.waitForAngularEnabled(true));
+
+  describe('(smoke tests)', () => {
+    it('should show the home page', () => {
+      page.goTo('');
+      const text = page.getDocViewerText();
+
+      expect(text).toContain('one framework');
+      expect(text).toContain('mobile & desktop');
+    });
+
+    describe('(marketing pages)', () => {
+      const textPerUrl = {
+        features: 'features & benefits',
+        docs: 'what is angular?',
+        events: 'events',
+        resources: 'explore angular resources',
+      };
+
+      Object.keys(textPerUrl).forEach(url => {
+        it(`should show the page at '${url}'`, () => {
+          page.goTo(url);
+          expect(page.getDocViewerText()).toContain(textPerUrl[url]);
+        });
+      });
+    });
+
+    describe('(docs pages)', () => {
+      const textPerUrl = {
+        api: 'api list',
+        'guide/architecture': 'architecture',
+        'guide/http': 'httpclient',
+        'guide/quickstart': 'quickstart',
+        'guide/security': 'security',
+        tutorial: 'tutorial',
+      };
+
+      Object.keys(textPerUrl).forEach(url => {
+        it(`should show the page at '${url}'`, () => {
+          page.goTo(url);
+          expect(page.getDocViewerText()).toContain(textPerUrl[url]);
+        });
+      });
+    });
+
+    describe('(api docs pages)', () => {
+      const textPerUrl = {
+        /* Class */ 'api/core/Injector': 'class injector',
+        /* Const */ 'api/forms/NG_VALIDATORS': 'const ng_validators',
+        /* Decorator */ 'api/core/Component': '@component',
+        /* Directive */ 'api/common/NgIf': 'class ngif',
+        /* Enum */ 'api/core/ChangeDetectionStrategy': 'enum changedetectionstrategy',
+        /* Function */ 'api/animations/animate': 'animate(',
+        /* Interface */ 'api/core/OnDestroy': 'interface ondestroy',
+        /* Pipe */ 'api/common/JsonPipe': '| json',
+        /* Type-Alias */ 'api/common/http/HttpEvent': 'type httpevent',
+      };
+
+      Object.keys(textPerUrl).forEach(url => {
+        it(`should show the page at '${url}'`, () => {
+          page.goTo(url);
+          expect(page.getDocViewerText()).toContain(textPerUrl[url]);
+        });
+      });
+    });
+
+    describe('(search results)', () => {
+      beforeEach(() => page.goTo(''));
+
+      it('should find pages when searching by a partial word in the title', () => {
+        page.enterSearch('ngCont');
+        expect(page.getSearchResults()).toContain('NgControl');
+      });
+
+      it('should find API docs when searching for an instance member name', () => {
+        page.enterSearch('writeValue');
+        expect(page.getSearchResults()).toContain('ControlValueAccessor');
+      });
+
+      it('should find API docs when searching for a static member name', () => {
+        page.enterSearch('compose');
+        expect(page.getSearchResults()).toContain('Validators');
+      });
+    });
+
+    it('should show relevant results on 404', () => {
+      page.goTo('http/router');
+      const results = page.getSearchResults();
+
+      expect(results).toContain('HttpClient');
+      expect(results).toContain('Router');
+    });
+  });
+});
