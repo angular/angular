0035d41030df59ed6f13eaf48d12ed8738c99ef1
Brandon Roberts <robertsbt@gmail.com>
2018-04-15 22:07:42 -0500

    docs(aio): Fix failing upgrade-module tests (#23234)
    PR Close #23234

--- aio/content/examples/upgrade-module/src/systemjs.config.1.js
+++ aio/content/examples/upgrade-module/src/systemjs.config.1.js
@@ -27,7 +27,8 @@
 
       // other libraries
       'rxjs': 'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      'tslib': 'npm:tslib/tslib.js',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -40,7 +41,7 @@
       'rxjs/operators': {main: 'index.js', defaultExtension: 'js' },
       'rxjs/testing': {main: 'index.js', defaultExtension: 'js' },
       'rxjs/websocket': {main: 'index.js', defaultExtension: 'js' },
-      'rxjs': { main: 'index.js', defaultExtension: 'js' },
+      'rxjs': { main: 'index.js', defaultExtension: 'js' }
     }
   });
 })(this);
