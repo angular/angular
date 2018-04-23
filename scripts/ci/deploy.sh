27f60b1743ae091c18c1f9836c0556f582cb020e
Alex Eagle <alexeagle@google.com>
2018-04-23 11:46:02 -0700

    ci: publish build snapshots from Bazel/CircleCI (#23512)
    This uses a new script and CircleCI job called "build-packages-dist"
    which shims the new Bazel build to produce outputs matching the legacy
    build. We'll use this to get AIO testing onto CircleCI as well.
    
    We move the integration tests to a new circleCI job that depends on this
    one, as well as the build publishing job.
    
    Note that every PR will have a trivial green publishing status, because
    we always create this job even for PRs. We'd rather not - see
    https://discuss.circleci.com/t/workflows-pull-request-filter/14396/4
    
    PR Close #23512

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -15,12 +15,7 @@ if [[ ${TRAVIS_TEST_RESULT=0} == 1 ]]; then
 fi
 
 
-# Don't deploy if not running against angular/angular
-# TODO(i): because we don't let deploy to run outside of angular/angular folks can't use their
-#   private travis build to deploy anywhere. This is likely ok, but this means that @alexeagle's
-#   fancy setup to publish ES2015 packages to github -build repos no longer works. This is ok
-#   since with flat modules we'll have this feature built-in. We should still go and remove
-#   stuff that Alex put in for this from publish-build-artifacts.sh
+# Don't deploy Angular.io if we are running in a fork
 if [[ ${TRAVIS_REPO_SLUG} != "angular/angular" ]]; then
   echo "Skipping deploy because this is not angular/angular."
   exit 0
@@ -28,19 +23,6 @@ fi
 
 
 case ${CI_MODE} in
-
-  e2e)
-    # Don't deploy if this is a PR build
-    if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]; then
-      echo "Skipping deploy because this is a PR build."
-      exit 0
-    fi
-
-    travisFoldStart "deploy.packages"
-      ${thisDir}/publish-build-artifacts.sh
-    travisFoldEnd "deploy.packages"
-    ;;
-
   aio)
     travisFoldStart "deploy.aio"
     (
