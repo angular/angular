8062f7de9eb671b527b5eceba5a8a63be8054a44
Olivier Combe <olivier.combe@gmail.com>
2018-04-24 20:30:23 +0200

    test: add i18n to cli-hello-world integration test (#23527)
    PR Close #23527

--- integration/cli-hello-world/e2e/app.po.ts
+++ integration/cli-hello-world/e2e/app.po.ts
@@ -8,4 +8,8 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getPipeContent() {
+    return element(by.css('app-root p')).getText();
+  }
 }
