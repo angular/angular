eed5c67d9b2c58fa15568e041790c582cc85f95d
Olivier Combe <olivier.combe@gmail.com>
2018-04-24 20:30:23 +0200

    test: add i18n to cli-hello-world integration test (#23527)
    PR Close #23527

--- integration/cli-hello-world/src/app/app.module.ts
+++ integration/cli-hello-world/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
 
+// adding this code to detect issues like https://github.com/angular/angular-cli/issues/10322
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
@@ -12,7 +15,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
