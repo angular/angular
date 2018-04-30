66076e1500a49968c2951a0aaab4d48c48d38fc6
Brandon Roberts <robertsbt@gmail.com>
2018-04-30 07:23:24 -0500

    build(aio): align stackblitz files with Angular CLI V6 (#23521)
    Also cleans up legacy references to `.angular-cli.json`
    
    PR Close #23521

--- aio/tools/examples/shared/boilerplate/cli/src/environments/environment.ts
+++ aio/tools/examples/shared/boilerplate/cli/src/environments/environment.ts
@@ -1,7 +1,6 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false
