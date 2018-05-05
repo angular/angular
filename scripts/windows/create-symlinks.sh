318a9980e2dc646bfa0a2dc2f99a99ebcb26a95c
Dean M Zamzow <zamzowd@rushenterprises.com>
2018-05-05 09:22:53 -0500

    build: remove animation from ./scripts/windows/create-symlink.sh
    The animation folder has been removed from core, and no longer needs updated symlinks.

--- scripts/windows/create-symlinks.sh
+++ scripts/windows/create-symlinks.sh
@@ -2,9 +2,6 @@
 
 cd `dirname $0`
 
-CORE_SRC_ANIMATION_DIR=./../../packages/core/src/animation
 UPGRADE_STATIC_DIR=./../../packages/upgrade/static
-mv ${CORE_SRC_ANIMATION_DIR}/dsl.ts ${CORE_SRC_ANIMATION_DIR}/dsl.ts.old
 mv ${UPGRADE_STATIC_DIR}/src ${UPGRADE_STATIC_DIR}/src.old
-cmd <<< "mklink \"..\\..\\packages\\core\\src\\animation\\dsl.ts\" \"..\\..\\..\\animations\\src\\animation_metadata.ts\""
 cmd <<< "mklink /d \"..\\..\\packages\\upgrade\\static\\src\" \"..\\src\""
