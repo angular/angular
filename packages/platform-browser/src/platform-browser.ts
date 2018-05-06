dc80245b4d6c60bf02db31e8cd430f7630fc96d2
ShawnOzark <38896077+ShawnOzark@users.noreply.github.com>
2018-05-06 18:36:55 -0400

    Add EventManagerPlugin to platform-browser barrel
    Angular provides the ability to include an EVENT_MANAGER_PLUGIN provider to a module. 
    We cannot extend the EventManagerPlugin class to do this since it is not included in the package's barrel.

--- packages/platform-browser/src/platform-browser.ts
+++ packages/platform-browser/src/platform-browser.ts
@@ -13,7 +13,7 @@ export {disableDebugTools, enableDebugTools} from './browser/tools/tools';
 export {BrowserTransferStateModule, StateKey, TransferState, makeStateKey} from './browser/transfer_state';
 export {By} from './dom/debug/by';
 export {DOCUMENT} from './dom/dom_tokens';
-export {EVENT_MANAGER_PLUGINS, EventManager} from './dom/events/event_manager';
+export {EVENT_MANAGER_PLUGINS, EventManager, EventManagerPlugin} from './dom/events/event_manager';
 export {HAMMER_GESTURE_CONFIG, HammerGestureConfig} from './dom/events/hammer_gestures';
 export {DomSanitizer, SafeHtml, SafeResourceUrl, SafeScript, SafeStyle, SafeUrl, SafeValue} from './security/dom_sanitization_service';
 export * from './private_export';
