2b965db00a869d646603004221e7ec3696fedcbd
Pete Bacon Darwin <pete@bacondarwin.com>
2018-05-10 13:56:07 +0100

    ci(docs-infra): change AIO preview server stuff to pull builds from CircleCI
    Previously, Travis pushed the build artitfacts to the preview server.
    This required us to use JWT to secure the POST request from Travis, to
    ensure we couldn't receive malicious builds.
    
    JWT has been deprecated and we are moving our builds to CircleCI.
    
    This commit rewrites the TypeScript part of the preview server that
    handles converting build artifact into hosted previews of the docs.

--- aio/aio-builds-setup/dockerbuild/scripts-js/lib/upload-server/index.ts
+++ aio/aio-builds-setup/dockerbuild/scripts-js/lib/upload-server/index.ts
@@ -1,34 +1,41 @@
 // Imports
-import {getEnvVar} from '../common/utils';
-import {uploadServerFactory} from './upload-server-factory';
-
-// Constants
-const AIO_BUILDS_DIR = getEnvVar('AIO_BUILDS_DIR');
-const AIO_DOMAIN_NAME = getEnvVar('AIO_DOMAIN_NAME');
-const AIO_GITHUB_ORGANIZATION = getEnvVar('AIO_GITHUB_ORGANIZATION');
-const AIO_GITHUB_TEAM_SLUGS = getEnvVar('AIO_GITHUB_TEAM_SLUGS');
-const AIO_GITHUB_TOKEN = getEnvVar('AIO_GITHUB_TOKEN');
-const AIO_PREVIEW_DEPLOYMENT_TOKEN = getEnvVar('AIO_PREVIEW_DEPLOYMENT_TOKEN');
-const AIO_REPO_SLUG = getEnvVar('AIO_REPO_SLUG');
-const AIO_TRUSTED_PR_LABEL = getEnvVar('AIO_TRUSTED_PR_LABEL');
-const AIO_UPLOAD_HOSTNAME = getEnvVar('AIO_UPLOAD_HOSTNAME');
-const AIO_UPLOAD_PORT = +getEnvVar('AIO_UPLOAD_PORT');
+import {AIO_DOWNLOADS_DIR} from '../common/constants';
+import {
+  AIO_ARTIFACT_PATH,
+  AIO_BUILDS_DIR,
+  AIO_CIRCLE_CI_TOKEN,
+  AIO_DOMAIN_NAME,
+  AIO_GITHUB_ORGANIZATION,
+  AIO_GITHUB_REPO,
+  AIO_GITHUB_TEAM_SLUGS,
+  AIO_GITHUB_TOKEN,
+  AIO_SIGNIFICANT_FILES_PATTERN,
+  AIO_TRUSTED_PR_LABEL,
+  AIO_UPLOAD_HOSTNAME,
+  AIO_UPLOAD_MAX_SIZE,
+  AIO_UPLOAD_PORT,
+} from '../common/env-variables';
+import {UploadServerFactory} from './upload-server-factory';
 
 // Run
 _main();
 
 // Functions
 function _main() {
-  uploadServerFactory.
-    create({
+  UploadServerFactory
+    .create({
+      buildArtifactPath: AIO_ARTIFACT_PATH,
       buildsDir: AIO_BUILDS_DIR,
+      circleCiToken: AIO_CIRCLE_CI_TOKEN,
       domainName: AIO_DOMAIN_NAME,
-      githubOrganization: AIO_GITHUB_ORGANIZATION,
+      downloadSizeLimit: AIO_UPLOAD_MAX_SIZE,
+      downloadsDir: AIO_DOWNLOADS_DIR,
+      githubOrg: AIO_GITHUB_ORGANIZATION,
+      githubRepo: AIO_GITHUB_REPO,
       githubTeamSlugs: AIO_GITHUB_TEAM_SLUGS.split(','),
       githubToken: AIO_GITHUB_TOKEN,
-      repoSlug: AIO_REPO_SLUG,
-      secret: AIO_PREVIEW_DEPLOYMENT_TOKEN,
+      significantFilesPattern: AIO_SIGNIFICANT_FILES_PATTERN,
       trustedPrLabel: AIO_TRUSTED_PR_LABEL,
-    }).
-    listen(AIO_UPLOAD_PORT, AIO_UPLOAD_HOSTNAME);
+    })
+    .listen(AIO_UPLOAD_PORT, AIO_UPLOAD_HOSTNAME);
 }
