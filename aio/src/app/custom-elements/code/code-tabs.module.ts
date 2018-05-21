ddd61248021c3d9344ccae2614c992b24d450d29
Pete Bacon Darwin <pete@bacondarwin.com>
2018-05-21 12:55:22 +0100

    feat(aio): display code-tabs in a Material Design "card" (#24027)
    This helps to connect the "content" of the tab to its label.
    
    Closes #23985
    
    PR Close #24027

--- aio/src/app/custom-elements/code/code-tabs.module.ts
+++ aio/src/app/custom-elements/code/code-tabs.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeTabsComponent } from './code-tabs.component';
-import { MatTabsModule } from '@angular/material';
+import { MatCardModule, MatTabsModule } from '@angular/material';
 import { CodeModule } from './code.module';
 import { WithCustomElementComponent } from '../element-registry';
 
 @NgModule({
-  imports: [ CommonModule, MatTabsModule, CodeModule ],
+  imports: [ CommonModule, MatCardModule, MatTabsModule, CodeModule ],
   declarations: [ CodeTabsComponent ],
   exports: [ CodeTabsComponent ],
   entryComponents: [ CodeTabsComponent ]
