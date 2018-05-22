f860752902f96e28ffe6cdcfe9770b2d3cf71d3d
George Kalpakas <kalpakas.g@gmail.com>
2018-05-22 17:31:05 +0300

    refactor(aio): rename method (loadContainingCustomElements --> loadContainedCustomElements) (#23944)
    PR Close #23944

--- aio/src/app/layout/doc-viewer/doc-viewer.component.ts
+++ aio/src/app/layout/doc-viewer/doc-viewer.component.ts
@@ -136,7 +136,7 @@ export class DocViewerComponent implements OnDestroy {
         //           and is considered to be safe.
         tap(() => this.nextViewContainer.innerHTML = doc.contents || ''),
         tap(() => addTitleAndToc = this.prepareTitleAndToc(this.nextViewContainer, doc.id)),
-        switchMap(() => this.elementsLoader.loadContainingCustomElements(this.nextViewContainer)),
+        switchMap(() => this.elementsLoader.loadContainedCustomElements(this.nextViewContainer)),
         tap(() => this.docReady.emit()),
         switchMap(() => this.swapViews(addTitleAndToc)),
         tap(() => this.docRendered.emit()),
