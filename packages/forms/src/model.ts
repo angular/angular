575f803e02eef0dabb0127920f66c16ecfe0145a
Johnson Lin <konglong_1984@msn.com>
2018-05-24 16:02:47 +1000

    docs(forms): fix API doc
    closes #24090

--- packages/forms/src/model.ts
+++ packages/forms/src/model.ts
@@ -363,7 +363,7 @@ export abstract class AbstractControl {
    *
    * An event will be emitted by `statusChanges` by default.
    *
-   * Passing `false` for `emitEvent` will cause `statusChanges` to not event an event.
+   * Passing `false` for `emitEvent` will cause `statusChanges` to not emit an event.
    */
   markAsPending(opts: {onlySelf?: boolean, emitEvent?: boolean} = {}): void {
     (this as{status: string}).status = PENDING;
