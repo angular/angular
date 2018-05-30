5fd44fb634b3d6ef2b553c323d30a68a754207cb
Vani <psreep@gmail.com>
2018-05-29 22:17:59 -0700

    docs(animations): updated animation docs (#24206)
    PR Close #24206

---
+++ aio/content/examples/animations/src/app/open-close.component.4.ts
@@ -0,0 +1,47 @@
+// #docplaster
+// #docregion
+import { Component } from '@angular/core';
+import { trigger, transition, state, animate, style } from '@angular/animations';
+
+// #docregion toggle-animation
+@Component({
+// #enddocregion toggle-animation
+  selector: 'app-open-close-toggle',
+  templateUrl: 'open-close.component.4.html',
+  styleUrls: ['open-close.component.css'],
+  // #docregion toggle-animation
+  animations: [
+    trigger('childAnimation', [
+      // ...
+// #enddocregion toggle-animation
+      state('open', style({
+        width: '250px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        width: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('* => *', [
+        animate('1s')
+      ]),
+// #docregion toggle-animation
+    ]),
+  ],
+})
+export class OpenCloseChildComponent {
+  isDisabled = false;
+  isOpen = false;
+// #enddocregion toggle-animation
+  toggleAnimations() {
+    this.isDisabled = !this.isDisabled;
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+// #docregion toggle-animation
+}
+// #enddocregion toggle-animation
