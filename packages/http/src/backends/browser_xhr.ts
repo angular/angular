ee17001638340600d40a2e03a4ab1ea8ed4dde77
Alan Agius <alan.agius4@gmail.com>
2018-06-01 22:09:01 +0200

    fix(common): deprecation notices are not parsed by tslint
    Closes: #24237

--- packages/http/src/backends/browser_xhr.ts
+++ packages/http/src/backends/browser_xhr.ts
@@ -13,7 +13,7 @@ import {Injectable} from '@angular/core';
  *
  * Take care not to evaluate this in non-browser contexts.
  *
- * @deprecated use @angular/common/http instead
+ * @deprecated see https://angular.io/guide/http
  */
 @Injectable()
 export class BrowserXhr {
