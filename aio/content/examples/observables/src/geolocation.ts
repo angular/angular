4e1e477c0d0dc2b4049a76fac9f81a47be7ed536
Janghyun Han <han41858@gmail.com>
2018-06-03 01:19:30 +0900

    observables.md translated

--- aio/content/examples/observables/src/geolocation.ts
+++ aio/content/examples/observables/src/geolocation.ts
@@ -2,31 +2,31 @@ import { Observable } from 'rxjs';
 
 // #docregion
 
-// Create an Observable that will start listening to geolocation updates
-// when a consumer subscribes.
+// 구독자가 구독을 실행하면 새로운 Observable 인스턴스를 생성하고
+// 클라이언트의 접속 위치를 추적하기 시작합니다.
 const locations = new Observable((observer) => {
-  // Get the next and error callbacks. These will be passed in when
-  // the consumer subscribes.
+  // 스트림으로 전달되는 데이터와 에러를 처리할 콜백 함수를 가져옵니다.
+  // 이 핸들러들은 구독자가 구독할 때 전달됩니다.
   const {next, error} = observer;
   let watchId;
 
-  // Simple geolocation API check provides values to publish
+  // 접속 위치를 처리하는 API는 간단하게 사용해 봅니다.
   if ('geolocation' in navigator) {
     watchId = navigator.geolocation.watchPosition(next, error);
   } else {
     error('Geolocation not available');
   }
 
-  // When the consumer unsubscribes, clean up data ready for next subscription.
+  // 구독자가 구독을 해지하면 사용하던 데이터를 모두 지웁니다.
   return {unsubscribe() { navigator.geolocation.clearWatch(watchId); }};
 });
 
-// Call subscribe() to start listening for updates.
+// 옵저버블을 시작하려면 subscribe() 함수를 실행합니다.
 const locationsSubscription = locations.subscribe({
   next(position) { console.log('Current Position: ', position); },
   error(msg) { console.log('Error Getting Location: ', msg); }
 });
 
-// Stop listening for location after 10 seconds
+// 옵저버블은 10초 후에 구독을 해지합니다.
 setTimeout(() => { locationsSubscription.unsubscribe(); }, 10000);
 // #enddocregion
