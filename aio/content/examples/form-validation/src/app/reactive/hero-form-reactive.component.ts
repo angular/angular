7916de83b9381f23620d48f23a3d9d9225a3af5a
Tomasz Kula <tomaszkulawebdev@gmail.com>
2018-06-04 11:40:56 +0200

    docs(aio): address pr review issues (#23743)
    PR Close #23743

--- aio/content/examples/form-validation/src/app/reactive/hero-form-reactive.component.ts
+++ aio/content/examples/form-validation/src/app/reactive/hero-form-reactive.component.ts
@@ -5,23 +5,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { forbiddenNameValidator } from '../shared/forbidden-name.directive';
 import { identityRevealedValidator } from '../shared/identity-revealed.directive';
 
-// #docregion cross-validation-component
 @Component({
   selector: 'app-hero-form-reactive',
   templateUrl: './hero-form-reactive.component.html',
   styleUrls: ['./hero-form-reactive.component.css'],
 })
 export class HeroFormReactiveComponent implements OnInit {
-// #enddocregion cross-validation-component
 
   powers = ['Really Smart', 'Super Flexible', 'Weather Changer'];
 
   hero = { name: 'Dr.', alterEgo: 'Dr. What', power: this.powers[0] };
 
-  // #docregion cross-validation-component
   heroForm: FormGroup;
 
-  // #docregion cross-validation-register
   ngOnInit(): void {
     this.heroForm = new FormGroup({
       'name': new FormControl(this.hero.name, [
@@ -33,12 +29,8 @@ export class HeroFormReactiveComponent implements OnInit {
       'power': new FormControl(this.hero.power, Validators.required)
     },  { validators: identityRevealedValidator }); // <-- add custom validator at the FormGroup level
   }
-  // #enddocregion cross-validation-register
-  // #enddocregion cross-validation-component
 
   get name() { return this.heroForm.get('name'); }
 
   get power() { return this.heroForm.get('power'); }
-// #docregion cross-validation-component
 }
-// #enddocregion cross-validation-component
