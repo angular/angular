ca4305a121181a454e48b1ff86707dbc55e7cff3
George Kalpakas <kalpakas.g@gmail.com>
2018-06-04 22:38:08 +0300

    fix(language-service): do not overwrite native `Reflect`
    Fixes #21420

--- packages/language-service/rollup.config.js
+++ packages/language-service/rollup.config.js
@@ -9,12 +9,12 @@
 const commonjs = require('rollup-plugin-commonjs');
 const sourcemaps = require('rollup-plugin-sourcemaps');
 const path = require('path');
+const fs = require('fs');
 
 var m = /^\@angular\/((\w|\-)+)(\/(\w|\d|\/|\-)+)?$/;
 var location = normalize('../../dist/packages-dist') + '/';
 var rxjsLocation = normalize('../../node_modules/rxjs');
 var tslibLocation = normalize('../../node_modules/tslib');
-var esm = 'esm/';
 
 var locations = {'compiler-cli': normalize('../../dist/packages') + '/'};
 
@@ -43,21 +43,7 @@ function resolve(id, from) {
   }
 }
 
-var banner = `
-var $reflect = {defineMetadata: function() {}, getOwnMetadata: function(){}};
-((typeof global !== 'undefined' && global)||{})['Reflect'] = $reflect;
-var $deferred, $resolved, $provided;
-function $getModule(name) { return $provided[name] || require(name); }
-function define(modules, cb) { $deferred = { modules: modules, cb: cb }; }
-module.exports = function(provided) {
-  if ($resolved) return $resolved;
-  var result = {};
-  $provided = Object.assign({'reflect-metadata': $reflect}, provided || {}, { exports: result });
-  $deferred.cb.apply(this, $deferred.modules.map($getModule));
-  $resolved = result;
-  return result;
-}
-`;
+var banner = fs.readFileSync('bundles/banner.js.txt', 'utf8');
 
 module.exports = {
   entry: '../../dist/packages-dist/language-service/fesm5/language-service.js',
