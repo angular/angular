d700a409da22f00ddebc3d6fbe0638c3c9b8fbeb
Greg Magolan <gmagolan@gmail.com>
2018-06-03 23:06:17 -0700

    build(bazel): enable manual ts_web_test_suite tests that require static_files (#24279)
    PR Close #24279

--- packages/platform-browser-dynamic/test/testing_public_browser_spec.ts
+++ packages/platform-browser-dynamic/test/testing_public_browser_spec.ts
@@ -26,7 +26,7 @@ class FancyService {
 
 @Component({
   selector: 'external-template-comp',
-  templateUrl: '/base/packages/platform-browser/test/static_assets/test.html'
+  templateUrl: '/base/angular/packages/platform-browser/test/static_assets/test.html'
 })
 class ExternalTemplateComp {
 }
@@ -48,7 +48,7 @@ class BadTemplateUrl {
 
       it('should run async tests with ResourceLoaders', async(() => {
            const resourceLoader = new ResourceLoaderImpl();
-           resourceLoader.get('/base/packages/platform-browser/test/static_assets/test.html')
+           resourceLoader.get('/base/angular/packages/platform-browser/test/static_assets/test.html')
                .then(() => { actuallyDone = true; });
          }),
          10000);  // Long timeout here because this test makes an actual ResourceLoader.
