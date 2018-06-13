ff3183f0c2e04e7c281300d86f93fb3e0b4dda7e
Miško Hevery <misko@hevery.com>
2018-06-13 10:21:51 -0700

    fixup! fix(ivy): remove debugger statement

--- packages/core/src/di/injector.ts
+++ packages/core/src/di/injector.ts
@@ -34,7 +34,7 @@ export class NullInjector implements Injector {
     if (notFoundValue === _THROW_IF_NOT_FOUND) {
       // Intentionally left behind: With dev tools open the debugger will stop here. There is no
       // reason why correctly written application should cause this exception.
-      // TODO(misko): on-comment the next line once `ngDevMode` works with closure.
+      // TODO(misko): uncomment the next line once `ngDevMode` works with closure.
       // if(ngDevMode) debugger;
       throw new Error(`NullInjectorError: No provider for ${stringify(token)}!`);
     }
