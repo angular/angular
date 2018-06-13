c99ab5508798235c4f4a005127559d226c975b7f
ribbon-otter <39717159+ribbon-otter@users.noreply.github.com>
2018-06-13 15:50:08 -0700

    build: switch to using a cd model instead of paths in mklink
    Add automatically creating the directories if they do not exist and
    change the links making code to place the links in the correct locations
    in the file tree, instead of into the windows scipt folder in one case.
    Adding the links like this allows the code to successfully build which
    it didn't under cygwin/windows before.

--- scripts/windows/create-symlinks.sh
+++ scripts/windows/create-symlinks.sh
@@ -4,7 +4,18 @@ cd `dirname $0`
 
 CORE_SRC_ANIMATION_DIR=./../../packages/core/src/animation
 UPGRADE_STATIC_DIR=./../../packages/upgrade/static
+
 mv ${CORE_SRC_ANIMATION_DIR}/dsl.ts ${CORE_SRC_ANIMATION_DIR}/dsl.ts.old
 mv ${UPGRADE_STATIC_DIR}/src ${UPGRADE_STATIC_DIR}/src.old
-cmd <<< "mklink \"..\\..\\packages\\core\\src\\animation\\dsl.ts\" \"..\\..\\..\\animations\\src\\animation_metadata.ts\""
-cmd <<< "mklink /d \"..\\..\\packages\\upgrade\\static\\src\" \"..\\src\""
+
+#make sure that the target locations exist
+mkdir -p $CORE_SRC_ANIMATION_DIR
+mkdir -p $UPGRADE_STATIC_DIR
+
+pushd .
+cd $CORE_SRC_ANIMATION_DIR
+cmd <<< "mklink dsl.ts \"..\\..\\..\\animations\\src\\animation_metadata.ts\""
+
+popd
+cd $UPGRADE_STATIC_DIR
+cmd <<< "mklink /d \"src\" \"..\\src\""
