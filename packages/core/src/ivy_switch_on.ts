0ede987ceda8fdddc3d5c0aff7198927f7791861
Miško Hevery <misko@hevery.com>
2018-06-22 19:05:31 -0700

    feat(ivy): Support resource resolution in JIT mode. (#24637)
    Used to resolve resource URLs on `@Component` when used with JIT compilation.
    
    ```
    @Component({
      selector: 'my-comp',
      templateUrl: 'my-comp.html', // This requires asynchronous resolution
    })
    class MyComponnent{
    }
    
    // Calling `renderComponent` will fail because `MyComponent`'s `@Compenent.templateUrl`
    // needs to be resolved because `renderComponent` is synchronous process.
    // renderComponent(MyComponent);
    
    // Calling `resolveComponentResources` will resolve `@Compenent.templateUrl` into
    // `@Compenent.template`, which would allow `renderComponent` to proceed in synchronous manner.
    // Use browser's `fetch` function as the default resource resolution strategy.
    resolveComponentResources(fetch).then(() => {
      // After resolution all URLs have been converted into strings.
      renderComponent(MyComponent);
    });
    
    ```
    
    PR Close #24637

--- packages/core/src/ivy_switch_on.ts
+++ packages/core/src/ivy_switch_on.ts
@@ -6,12 +6,12 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {compileComponentDecorator, compileDirective} from './render3/jit/directive';
+import {compileComponent, compileDirective} from './render3/jit/directive';
 import {compileInjectable} from './render3/jit/injectable';
 import {compileNgModule} from './render3/jit/module';
 
 export const ivyEnabled = true;
-export const R3_COMPILE_COMPONENT = compileComponentDecorator;
+export const R3_COMPILE_COMPONENT = compileComponent;
 export const R3_COMPILE_DIRECTIVE = compileDirective;
 export const R3_COMPILE_INJECTABLE = compileInjectable;
 export const R3_COMPILE_NGMODULE = compileNgModule;
