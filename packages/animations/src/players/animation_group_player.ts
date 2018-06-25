9d6e8698999a89669cfd197fba0097bc60039ab3
Judy Bogart <jbogart@gmail.com>
2018-06-25 16:16:59 -0700

    docs: add api doc for programmatic animation classes (#24668)
    PR Close #24668

--- packages/animations/src/players/animation_group_player.ts
+++ packages/animations/src/players/animation_group_player.ts
@@ -9,6 +9,14 @@
 import {scheduleMicroTask} from '../util';
 import {AnimationPlayer} from './animation_player';
 
+/**
+ * A programmatic controller for a group of reusable animations.
+ * Used internally to control animations.
+ *
+ * @see `AnimationPlayer`
+ * @see `{@link animations/group group()}`
+ *
+ */
 export class AnimationGroupPlayer implements AnimationPlayer {
   private _onDoneFns: Function[] = [];
   private _onStartFns: Function[] = [];
