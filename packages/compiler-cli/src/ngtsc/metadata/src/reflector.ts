5e98421d3315d52f316a3daf42f142bbcb0ba275
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 14:08:59 +0100

    style(ivy): remove underscore from TypeScriptReflectionHost._getDeclarationOfSymbol (#24897)
    The linter complains that non-private members must be marked
    with `@internal` if they start with an underscore.
    
    PR Close #24897

--- packages/compiler-cli/src/ngtsc/metadata/src/reflector.ts
+++ packages/compiler-cli/src/ngtsc/metadata/src/reflector.ts
@@ -119,7 +119,7 @@ export class TypeScriptReflectionHost implements ReflectionHost {
     }
     this.checker.getExportsOfModule(symbol).forEach(exportSymbol => {
       // Map each exported Symbol to a Declaration and add it to the map.
-      const decl = this._getDeclarationOfSymbol(exportSymbol);
+      const decl = this.getDeclarationOfSymbol(exportSymbol);
       if (decl !== null) {
         map.set(exportSymbol.name, decl);
       }
@@ -138,7 +138,7 @@ export class TypeScriptReflectionHost implements ReflectionHost {
     if (symbol === undefined) {
       return null;
     }
-    return this._getDeclarationOfSymbol(symbol);
+    return this.getDeclarationOfSymbol(symbol);
   }
 
   /**
@@ -146,7 +146,7 @@ export class TypeScriptReflectionHost implements ReflectionHost {
    *
    * @internal
    */
-  protected _getDeclarationOfSymbol(symbol: ts.Symbol): Declaration|null {
+  protected getDeclarationOfSymbol(symbol: ts.Symbol): Declaration|null {
     let viaModule: string|null = null;
     // Look through the Symbol's immediate declarations, and see if any of them are import-type
     // statements.
