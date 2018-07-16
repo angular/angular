70059dc3867bf39d32016d8380d4667d00a9e04a
Jan De Wilde <jan.de.wilde87@gmail.com>
2018-07-16 11:52:17 +0200

    fixup! docs: add e2e tests for `elements` example

--- aio/content/examples/elements/e2e/src/app.e2e-spec.ts
+++ aio/content/examples/elements/e2e/src/app.e2e-spec.ts
@@ -18,7 +18,32 @@ describe('Elements', () => {
     expect(popupComponent.getText()).toContain('Popup: Message');
   });
 
-  it('should display popup element on button click', () => {
+  it('should close popup component on close button click', () => {
+    const popupComponent = element(by.css('popup-component'));
+    const popupComponentCloseButton = popupComponent.element(by.css('button'));
+
+    expect(popupComponent.isPresent()).toBe(true);
+
+    popupComponentCloseButton.click();
+    expect(popupComponent.isPresent()).toBe(false);
+  });
+
+  it('should be able to change the message', () => {
+    const popupMessageInput = element(by.css('app-root input'));
+    popupMessageInput.clear().then(() => popupMessageInput.sendKeys('Angular rocks!'));
+
+    const popupComponent = element(by.css('popup-component'));
+    const popupComponentButton = element.all(by.css('button')).get(0);
+
+    expect(popupComponent.isPresent()).toBe(false);
+
+    popupComponentButton.click();
+    expect(popupComponent.isPresent()).toBe(true);
+    expect(popupComponent.getText()).toContain('Popup: Angular rocks!');
+  });
+
+  /* Provide solution (separate build or polyfill) since Custom Elements can only be used with ES2015 classes according to the spec. */
+  xit('should display popup element on button click', () => {
     const popupElement = element(by.css('popup-element'));
     const popupElementButton = element.all(by.css('button')).get(1);
 
@@ -27,6 +52,30 @@ describe('Elements', () => {
     popupElementButton.click();
     expect(popupElement.isPresent()).toBe(true);
     expect(popupElement.getText()).toContain('Popup: Message');
+  });
+
+  xit('should close popup element on close button click', () => {
+    const popupElement = element(by.css('popup-element'));
+    const popupElementCloseButton = popupElement.element(by.css('button'));
+
+    expect(popupElement.isPresent()).toBe(true);
+
+    popupElementCloseButton.click();
+    expect(popupElement.isPresent()).toBe(false);
+  });
+
+  xit('should be able to change the message', () => {
+    const popupMessageInput = element(by.css('app-root input'));
+    popupMessageInput.clear().then(() => popupMessageInput.sendKeys('Angular rocks!'));
+
+    const popupElement = element(by.css('popup-element'));
+    const popupComponentButton = element.all(by.css('button')).get(0);
+
+    expect(popupElement.isPresent()).toBe(false);
+
+    popupComponentButton.click();
+    expect(popupElement.isPresent()).toBe(true);
+    expect(popupElement.getText()).toContain('Popup: Angular rocks!');
   });
 
 });
