890f212cb3cfcaa1ee3e63c62e4e869d7b27ea2d
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 10:24:32 +0100

    feat(ivy): implement initial ngcc package transformer (#24897)
    PR Close #24897

--- packages/compiler-cli/src/ngcc/src/main.ts
+++ packages/compiler-cli/src/ngcc/src/main.ts
@@ -6,9 +6,16 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import {resolve} from 'path';
+import {PackageTransformer} from './transform/package_transformer';
 
 export function mainNgcc(args: string[]): number {
   const packagePath = resolve(args[0]);
+
+  // TODO: find all the package tyoes to transform
+  // TODO: error/warning logging/handling etc
+
+  const transformer = new PackageTransformer();
+  transformer.transform(packagePath, 'fesm2015');
 
   return 0;
 }
