629e4798fe91bccb99db0637c9bebdef2bcad9dd
Martin Probst <martin@probst.io>
2018-07-17 15:49:06 +0200

    fix(animations): @internal must use JSDoc tags.
    This change fixes up several comments that accidentally used the JSDoc
    tag @internal in regular block comments (`/*` instead of `/**`).
    
    This prevents a problem with Closure Compiler that balks at `@` tags
    occuring in regular block comments, because it assumes they were
    intended to be tags for the compiler.
    
    When occuring in `/**` JSDoc, tsickle escapes the tags, so they do not
    cause problems.

--- packages/animations/browser/src/render/css_keyframes/css_keyframes_player.ts
+++ packages/animations/browser/src/render/css_keyframes/css_keyframes_player.ts
@@ -124,7 +124,7 @@ export class CssKeyframesPlayer implements AnimationPlayer {
         DEFAULT_FILL_MODE, () => this.finish());
   }
 
-  /* @internal */
+  /** @internal */
   triggerCallback(phaseName: string): void {
     const methods = phaseName == 'start' ? this._onStartFns : this._onDoneFns;
     methods.forEach(fn => fn());
