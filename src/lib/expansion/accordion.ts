3b2c5a6832c49e9e43cea05382558cca08da60a4
Ivan Tham <pickfire@riseup.net>
2018-07-20 22:35:47 +0800

    docs(cdk): fix spelling (#12289)

--- src/lib/expansion/accordion.ts
+++ src/lib/expansion/accordion.ts
@@ -34,7 +34,7 @@ export class MatAccordion extends CdkAccordion {
    * The display mode used for all expansion panels in the accordion. Currently two display
    * modes exist:
    *  default - a gutter-like spacing is placed around any expanded panel, placing the expanded
-   *     panel at a different elevation from the reset of the accordion.
+   *     panel at a different elevation from the rest of the accordion.
    *  flat - no spacing is placed around expanded panels, showing all panels at the same
    *     elevation.
    */
