b3a1c5a8a6f8892a7249c1cc2227197396403de1
Alex Eagle <alexeagle@google.com>
2018-07-23 18:33:07 -0700

    build: update to newer circleCI bazel remote cache proxy (#25054)
    it fixes the error we currently get on CI
    
    PR Close #25054

--- .circleci/setup_cache.sh
+++ .circleci/setup_cache.sh
@@ -5,7 +5,7 @@
 
 set -u -e
 
-readonly DOWNLOAD_URL="https://5-116431813-gh.circle-artifacts.com/0/pkg/bazel-remote-proxy-$(uname -s)_$(uname -m)"
+readonly DOWNLOAD_URL="https://6-116431813-gh.circle-artifacts.com/0/pkg/bazel-remote-proxy-$(uname -s)_$(uname -m)"
 
 curl --fail -o ~/bazel-remote-proxy "$DOWNLOAD_URL"
 chmod +x ~/bazel-remote-proxy
