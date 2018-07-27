1891264252b5a263e4e0d0243adb4147eadde997
George Kalpakas <kalpakas.g@gmail.com>
2018-07-27 22:36:54 +0300

    refactor(ivy): make `ReflectionHost` a parameter of `Renderer` (#25406)
    PR Close #25406

--- packages/compiler-cli/src/ngcc/src/rendering/esm5_renderer.ts
+++ packages/compiler-cli/src/ngcc/src/rendering/esm5_renderer.ts
@@ -5,12 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import * as ts from 'typescript';
-import MagicString from 'magic-string';
-import {NgccReflectionHost} from '../host/ngcc_host';
-import {AnalyzedClass, AnalyzedFile} from '../analyzer';
 import {Esm2015Renderer} from './esm2015_renderer';
 
-export class Esm5Renderer extends Esm2015Renderer {
-  constructor(host: NgccReflectionHost) { super(host); }
-}
+export class Esm5Renderer extends Esm2015Renderer {}
