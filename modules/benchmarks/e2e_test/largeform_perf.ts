angular/angular

commit 1d051c58416939b7eabde2966033b0e68a4fb073

build(bazel): use bazel managed node_modules for downstream angular from source build support (#24663)
PR Close #24663

modules/benchmarks/e2e_test/largeform_perf.ts
@@ -29,7 +29,7 @@ describe('largeform benchmark perf', () => {
 
   [CreateAndDestroyWorker].forEach((worker) => {
     describe(worker.id, () => {
-      it('should run for ng2', (done) => {
+      it('should run for ng2', (done: any) => {
         runLargeFormBenchmark({
           id: `largeform.ng2.${worker.id}`,
           url: 'all/benchmarks/src/largeform/ng2/index.html',

modules/benchmarks/e2e_test/largetable_perf.ts
@@ -40,7 +40,7 @@ describe('largetable benchmark perf', () => {
 
   [CreateOnlyWorker, CreateAndDestroyWorker, UpdateWorker].forEach((worker) => {
     describe(worker.id, () => {
-      it('should run for ng2', (done) => {
+      it('should run for ng2', (done: any) => {
         runTableBenchmark({
           id: `largeTable.ng2.${worker.id}`,
           url: 'all/benchmarks/src/largetable/ng2/index.html',
@@ -48,7 +48,7 @@ describe('largetable benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should run for ng2 with ngSwitch', (done) => {
+      it('should run for ng2 with ngSwitch', (done: any) => {
         runTableBenchmark({
           id: `largeTable.ng2_switch.${worker.id}`,
           url: 'all/benchmarks/src/largetable/ng2_switch/index.html',
@@ -56,7 +56,7 @@ describe('largetable benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should run for render3', (done) => {
+      it('should run for render3', (done: any) => {
         runTableBenchmark({
           id: `largeTable.render3.${worker.id}`,
           url: 'all/benchmarks/src/largetable/render3/index.html',
@@ -65,7 +65,7 @@ describe('largetable benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should run for iv', (done) => {
+      it('should run for iv', (done: any) => {
         runTableBenchmark({
           id: `largeTable.iv.${worker.id}`,
           url: 'all/benchmarks/src/largetable/iv/index.html',
@@ -74,7 +74,7 @@ describe('largetable benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should run for the baseline', (done) => {
+      it('should run for the baseline', (done: any) => {
         runTableBenchmark({
           id: `largeTable.baseline.${worker.id}`,
           url: 'all/benchmarks/src/largetable/baseline/index.html',
@@ -83,7 +83,7 @@ describe('largetable benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should run for incremental-dom', (done) => {
+      it('should run for incremental-dom', (done: any) => {
         runTableBenchmark({
           id: `largeTable.incremental_dom.${worker.id}`,
           url: 'all/benchmarks/src/largetable/incremental_dom/index.html',

modules/benchmarks/e2e_test/tree_perf.ts
@@ -24,7 +24,7 @@ describe('tree benchmark perf', () => {
   Benchmarks.forEach(benchmark => {
     describe(benchmark.id, () => {
       // This is actually a destroyOnly benchmark
-      it('should work for createOnly', (done) => {
+      it('should work for createOnly', (done: any) => {
         runTreeBenchmark({
           id: 'createOnly',
           benchmark,
@@ -33,7 +33,7 @@ describe('tree benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should work for createOnlyForReal', (done) => {
+      it('should work for createOnlyForReal', (done: any) => {
         runTreeBenchmark({
           id: 'createOnlyForReal',
           benchmark,
@@ -42,7 +42,7 @@ describe('tree benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should work for createDestroy', (done) => {
+      it('should work for createDestroy', (done: any) => {
         runTreeBenchmark({
           id: 'createDestroy',
           benchmark,
@@ -53,13 +53,13 @@ describe('tree benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should work for update', (done) => {
+      it('should work for update', (done: any) => {
         runTreeBenchmark({id: 'update', benchmark, work: () => $(CreateBtn).click()})
             .then(done, done.fail);
       });
 
       if (benchmark.buttons.indexOf(DetectChangesBtn) !== -1) {
-        it('should work for detectChanges', (done) => {
+        it('should work for detectChanges', (done: any) => {
           runTreeBenchmark({
             id: 'detectChanges',
             benchmark,

BUILD.bazel
@@ -15,220 +15,19 @@ alias(
     actual = "@nodejs//:yarn",
 )
 
-node_modules_filegroup(
+alias(
     name = "node_modules",
-    packages = [
-        "adm-zip",
-        "ajv",
-        "angular",
-        "angular-1.5",
-        "angular-mocks",
-        "angular-mocks-1.5",
-        "anymatch",
-        "arr-diff",
-        "arr-flatten",
-        "arr-union",
-        "array-unique",
-        "asn1",
-        "assert-plus",
-        "assign-symbols",
-        "async-each",
-        "asynckit",
-        "atob",
-        "aws-sign2",
-        "aws4",
-        "balanced-match",
-        "base",
-        "base64-js",
-        "binary-extensions",
-        "blocking-proxy",
-        "brace-expansion",
-        "braces",
-        "bytebuffer",
-        "cache-base",
-        "caseless",
-        "chokidar",
-        "class-utils",
-        "co",
-        "collection-visit",
-        "combined-stream",
-        "component-emitter",
-        "concat-map",
-        "copy-descriptor",
-        "core-util-is",
-        "debug",
-        "decode-uri-component",
-        "define-property",
-        "delayed-stream",
-        "domino",
-        "expand-brackets",
-        "expand-range",
-        "extend",
-        "extend-shallow",
-        "extglob",
-        "extsprintf",
-        "fast-deep-equal",
-        "fast-json-stable-stringify",
-        "filename-regex",
-        "fill-range",
-        "for-in",
-        "for-own",
-        "forever-agent",
-        "form-data",
-        "fragment-cache",
-        "fs.realpath",
-        "get-value",
-        "glob",
-        "glob-base",
-        "glob-parent",
-        "graceful-fs",
-        "hammerjs",
-        "har-schema",
-        "har-validator",
-        "has-value",
-        "has-values",
-        "http-signature",
-        "https-proxy-agent",
-        "inflight",
-        "inherits",
-        "is-accessor-descriptor",
-        "is-binary-path",
-        "is-buffer",
-        "is-data-descriptor",
-        "is-descriptor",
-        "is-dotfile",
-        "is-equal-shallow",
-        "is-extendable",
-        "is-extglob",
-        "is-glob",
-        "is-number",
-        "is-plain-object",
-        "is-posix-bracket",
-        "is-primitive",
-        "is-typedarray",
-        "is-windows",
-        "isarray",
-        "isobject",
-        "isstream",
-        "jasmine",
-        "jasmine-core",
-        "jasminewd2",
-        "json-schema",
-        "json-schema-traverse",
-        "json-stable-stringify",
-        "json-stringify-safe",
-        "jsprim",
-        "kind-of",
-        "long",
-        "lru-cache",
-        "map-cache",
-        "map-visit",
-        "math-random",
-        "micromatch",
-        "mime-db",
-        "mime-types",
-        "minimatch",
-        "minimist",
-        "mixin-deep",
-        "nanomatch",
-        "normalize-path",
-        "oauth-sign",
-        "object.omit",
-        "object.pick",
-        "object-copy",
-        "object-visit",
-        "once",
-        "optimist",
-        "options",
-        "os-tmpdir",
-        "parse-glob",
-        "pascalcase",
-        "path-dirname",
-        "path-is-absolute",
-        "performance-now",
-        "posix-character-classes",
-        "preserve",
-        "process-nextick-args",
-        "protobufjs",
-        "protractor",
-        "qs",
-        "randomatic",
-        "readable-stream",
-        "readdirp",
-        "reflect-metadata",
-        "regex-cache",
-        "regex-not",
-        "remove-trailing-separator",
-        "repeat-element",
-        "repeat-string",
-        "request",
-        "ret",
-        "rimraf",
-        "safe-buffer",
-        "safe-regex",
-        "sax",
-        "semver",
-        "set-immediate-shim",
-        "set-value",
-        "shelljs",
-        "sigmund",
-        "snapdragon",
-        "snapdragon-node",
-        "snapdragon-util",
-        "source-map",
-        "source-map-resolve",
-        "source-map-support",
-        "source-map-url",
-        "split-string",
-        "sshpk",
-        "static-extend",
-        "stringstream",
-        "tmp",
-        "to-object-path",
-        "to-regex",
-        "to-regex-range",
-        "tough-cookie",
-        "tsickle",
-        "tslib",
-        "tsutils",
-        "tunnel-agent",
-        "typescript",
-        "union-value",
-        "unset-value",
-        "upath",
-        "uri-js",
-        "urix",
-        "use",
-        "util-deprecate",
-        "uuid",
-        "verror",
-        "webdriver-js-extender",
-        "webdriver-manager",
-        "wordwrap",
-        "wrappy",
-        "xhr2",
-        "xml2js",
-        "xmlbuilder",
-        "zone.js",
-        "@angular-devkit/core",
-        "@angular-devkit/schematics",
-        "@types",
-        "@webcomponents/custom-elements",
-    ],
-    patterns = [
-        "node_modules/protractor/**",
-        "node_modules/@schematics/angular/**",
-    ],
+    actual = "@angular_deps//:node_modules",
 )
 
 filegroup(
     name = "web_test_bootstrap_scripts",
     # do not sort
     srcs = [
-        "//:node_modules/reflect-metadata/Reflect.js",
-        "//:node_modules/zone.js/dist/zone.js",
-        "//:node_modules/zone.js/dist/zone-testing.js",
-        "//:node_modules/zone.js/dist/task-tracking.js",
+        "@angular_deps//:node_modules/reflect-metadata/Reflect.js",
+        "@angular_deps//:node_modules/zone.js/dist/zone.js",
+        "@angular_deps//:node_modules/zone.js/dist/zone-testing.js",
+        "@angular_deps//:node_modules/zone.js/dist/task-tracking.js",
         "//:test-events.js",
     ],
 )
@@ -236,9 +35,9 @@ filegroup(
 filegroup(
     name = "angularjs_scripts",
     srcs = [
-        "//:node_modules/angular-1.5/angular.js",
-        "//:node_modules/angular-mocks-1.5/angular-mocks.js",
-        "//:node_modules/angular-mocks/angular-mocks.js",
-        "//:node_modules/angular/angular.js",
+        "@angular_deps//:node_modules/angular-1.5/angular.js",
+        "@angular_deps//:node_modules/angular-mocks-1.5/angular-mocks.js",
+        "@angular_deps//:node_modules/angular-mocks/angular-mocks.js",
+        "@angular_deps//:node_modules/angular/angular.js",
     ],
 )

WORKSPACE
@@ -6,9 +6,9 @@ workspace(name = "angular")
 
 http_archive(
     name = "build_bazel_rules_nodejs",
-    url = "https://github.com/bazelbuild/rules_nodejs/archive/0.10.1.zip",
-    strip_prefix = "rules_nodejs-0.10.1",
-    sha256 = "634206524d90dc03c52392fa3f19a16637d2bcf154910436fe1d669a0d9d7b9c",
+    url = "https://github.com/bazelbuild/rules_nodejs/archive/20ff5892612f8359aec8aaf26dd3902a24976ada.zip",
+    strip_prefix = "rules_nodejs-20ff5892612f8359aec8aaf26dd3902a24976ada",
+    sha256 = "07da9d4c3e688a02745d0f50709a87744706d4f5d1959b799b0ac38e97acd622",
 )
 
 http_archive(
@@ -72,6 +72,22 @@ http_archive(
 )
 
 #
+# Point Bazel to WORKSPACEs that live in subdirectories
+#
+
+local_repository(
+    name = "rxjs",
+    path = "node_modules/rxjs/src",
+)
+
+# Point to the integration test workspace just so that Bazel doesn't descend into it
+# when expanding the //... pattern
+local_repository(
+    name = "bazel_integration_test",
+    path = "integration/bazel",
+)
+
+#
 # Load and install our dependencies downloaded above.
 #
 
@@ -100,25 +116,9 @@ load("@build_bazel_rules_typescript//:defs.bzl", "ts_setup_workspace")
 
 ts_setup_workspace()
 
-load("//packages/bazel/src:ng_setup_workspace.bzl", "ng_setup_workspace")
+load("@angular//:index.bzl", "ng_setup_workspace")
 
 ng_setup_workspace()
-
-#
-# Point Bazel to WORKSPACEs that live in subdirectories
-#
-
-local_repository(
-    name = "rxjs",
-    path = "node_modules/rxjs/src",
-)
-
-# Point to the integration test workspace just so that Bazel doesn't descend into it
-# when expanding the //... pattern
-local_repository(
-    name = "bazel_integration_test",
-    path = "integration/bazel",
-)
 
 #
 # Ask Bazel to manage these toolchain dependencies for us.

index.bzl
@@ -1,0 +1,19 @@
+# Copyright Google Inc. All Rights Reserved.
+#
+# Use of this source code is governed by an MIT-style license that can be
+# found in the LICENSE file at https://angular.io/license
+""" Public API surface is re-exported here.
+"""
+
+load("//packages/bazel:index.bzl",
+    _ng_module = "ng_module",
+    _ng_package = "ng_package",
+    _protractor_web_test = "protractor_web_test",
+    _protractor_web_test_suite = "protractor_web_test_suite")
+load("//tools:ng_setup_workspace.bzl", _ng_setup_workspace = "ng_setup_workspace")
+
+ng_module = _ng_module
+ng_package = _ng_package
+protractor_web_test = _protractor_web_test
+protractor_web_test_suite = _protractor_web_test_suite
+ng_setup_workspace = _ng_setup_workspace

integration/bazel/WORKSPACE
@@ -39,6 +39,20 @@ http_archive(
 )
 
 #
+# Point Bazel to WORKSPACEs that live in subdirectories
+#
+
+local_repository(
+    name = "angular",
+    path = "node_modules/@angular/bazel",
+)
+
+local_repository(
+    name = "rxjs",
+    path = "node_modules/rxjs/src",
+)
+
+#
 # Load and install our dependencies downloaded above.
 #
 
@@ -67,24 +81,6 @@ ts_setup_workspace()
 load("@io_bazel_rules_sass//sass:sass_repositories.bzl", "sass_repositories")
 
 sass_repositories()
-
-#
-# Point Bazel to WORKSPACEs that live in subdirectories
-#
-
-local_repository(
-    name = "angular",
-    path = "node_modules/@angular/bazel",
-)
-
-local_repository(
-    name = "rxjs",
-    path = "node_modules/rxjs/src",
-)
-
-#
-# Load and install our dependencies from local repositories
-#
 
 load("@angular//:index.bzl", "ng_setup_workspace")
 

karma-js.conf.js
@@ -110,6 +110,7 @@ module.exports = function(config) {
     // don't need this entire config file.
     proxies: {
       '/base/angular/': '/base/',
+      '/base/angular_deps/': '/base/',
     },
 
     reporters: ['dots'],

commit 5a68162547a59e4b50ed06a0f78671de8885b69d

test: fix typings for `DoneFn`
This also fixes CI tests, which were accidentally broken in #24663.

modules/benchmarks/e2e_test/largeform_perf.ts
@@ -29,7 +29,7 @@ describe('largeform benchmark perf', () => {
 
   [CreateAndDestroyWorker].forEach((worker) => {
     describe(worker.id, () => {
-      it('should run for ng2', (done: any) => {
+      it('should run for ng2', done => {
         runLargeFormBenchmark({
           id: `largeform.ng2.${worker.id}`,
           url: 'all/benchmarks/src/largeform/ng2/index.html',

modules/benchmarks/e2e_test/largetable_perf.ts
@@ -40,7 +40,7 @@ describe('largetable benchmark perf', () => {
 
   [CreateOnlyWorker, CreateAndDestroyWorker, UpdateWorker].forEach((worker) => {
     describe(worker.id, () => {
-      it('should run for ng2', (done: any) => {
+      it('should run for ng2', done => {
         runTableBenchmark({
           id: `largeTable.ng2.${worker.id}`,
           url: 'all/benchmarks/src/largetable/ng2/index.html',
@@ -48,7 +48,7 @@ describe('largetable benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should run for ng2 with ngSwitch', (done: any) => {
+      it('should run for ng2 with ngSwitch', done => {
         runTableBenchmark({
           id: `largeTable.ng2_switch.${worker.id}`,
           url: 'all/benchmarks/src/largetable/ng2_switch/index.html',
@@ -56,7 +56,7 @@ describe('largetable benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should run for render3', (done: any) => {
+      it('should run for render3', done => {
         runTableBenchmark({
           id: `largeTable.render3.${worker.id}`,
           url: 'all/benchmarks/src/largetable/render3/index.html',
@@ -65,7 +65,7 @@ describe('largetable benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should run for iv', (done: any) => {
+      it('should run for iv', done => {
         runTableBenchmark({
           id: `largeTable.iv.${worker.id}`,
           url: 'all/benchmarks/src/largetable/iv/index.html',
@@ -74,7 +74,7 @@ describe('largetable benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should run for the baseline', (done: any) => {
+      it('should run for the baseline', done => {
         runTableBenchmark({
           id: `largeTable.baseline.${worker.id}`,
           url: 'all/benchmarks/src/largetable/baseline/index.html',
@@ -83,7 +83,7 @@ describe('largetable benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should run for incremental-dom', (done: any) => {
+      it('should run for incremental-dom', done => {
         runTableBenchmark({
           id: `largeTable.incremental_dom.${worker.id}`,
           url: 'all/benchmarks/src/largetable/incremental_dom/index.html',

modules/benchmarks/e2e_test/tree_perf.ts
@@ -24,7 +24,7 @@ describe('tree benchmark perf', () => {
   Benchmarks.forEach(benchmark => {
     describe(benchmark.id, () => {
       // This is actually a destroyOnly benchmark
-      it('should work for createOnly', (done: any) => {
+      it('should work for createOnly', done => {
         runTreeBenchmark({
           id: 'createOnly',
           benchmark,
@@ -33,7 +33,7 @@ describe('tree benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should work for createOnlyForReal', (done: any) => {
+      it('should work for createOnlyForReal', done => {
         runTreeBenchmark({
           id: 'createOnlyForReal',
           benchmark,
@@ -42,7 +42,7 @@ describe('tree benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should work for createDestroy', (done: any) => {
+      it('should work for createDestroy', done => {
         runTreeBenchmark({
           id: 'createDestroy',
           benchmark,
@@ -53,13 +53,13 @@ describe('tree benchmark perf', () => {
         }).then(done, done.fail);
       });
 
-      it('should work for update', (done: any) => {
+      it('should work for update', done => {
         runTreeBenchmark({id: 'update', benchmark, work: () => $(CreateBtn).click()})
             .then(done, done.fail);
       });
 
       if (benchmark.buttons.indexOf(DetectChangesBtn) !== -1) {
-        it('should work for detectChanges', (done: any) => {
+        it('should work for detectChanges', done => {
           runTreeBenchmark({
             id: 'detectChanges',
             benchmark,

tools/types-ext/jasminewd2.d.ts
@@ -6,6 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+/// <reference types="jasminewd2" />
+
 /**
  * Extended typings for `jasminewd2`.
  *

modules/benchmarks/src/largetable/largetable_perf.spec.ts
@@ -41,7 +41,7 @@ describe('largetable benchmark perf', () => {
 
   [CreateOnlyWorker, CreateAndDestroyWorker, UpdateWorker].forEach((worker) => {
     describe(worker.id, () => {
-      it('should run for render3', (done) => {
+      it('should run for render3', done => {
         runTableBenchmark({
           id: `largeTable.render3.${worker.id}`,
           url: 'index.html',

packages/core/test/bundling/hello_world_r2/treeshaking_spec.ts
@@ -6,7 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {withBody} from '@angular/core/testing';
 import * as fs from 'fs';
 import * as path from 'path';
 

packages/core/testing/src/render3.ts
@@ -35,18 +35,14 @@
 * @param blockFn function to wrap. The function can return promise or be `async`.
 * @experimental
 */
-export function withBody<T>(html: string, blockFn: T): T {
-  return function(done: {(): void, fail(): void}) {
+export function withBody<T extends Function>(html: string, blockFn: T): T {
+  return function(done: DoneFn) {
     ensureDocument();
-    let returnValue: any = undefined;
     if (typeof blockFn === 'function') {
       document.body.innerHTML = html;
-      // TODO(i): I'm not sure why a cast is required here but otherwise I get
-      //   TS2349: Cannot invoke an expression whose type lacks a call signature. Type 'never' has
-      //   no compatible call signatures.
-      let blockReturn = (blockFn as any)();
+      const blockReturn = blockFn();
       if (blockReturn instanceof Promise) {
-        blockReturn = blockReturn.then(done, done.fail);
+        blockReturn.then(done, done.fail);
       } else {
         done();
       }

packages/platform-browser-dynamic/test/testing_public_browser_spec.ts
@@ -104,9 +104,9 @@ class BadTemplateUrl {
           reject = rej;
         });
         originalJasmineIt = jasmine.getEnv().it;
-        jasmine.getEnv().it = (description: string, fn: any /** TODO #9100 */): any => {
-          const done = () => { resolve(null); };
-          (<any>done).fail = (err: any /** TODO #9100 */) => { reject(err); };
+        jasmine.getEnv().it = (description: string, fn: (done: DoneFn) => void): any => {
+          const done = (() => resolve(null)) as DoneFn;
+          done.fail = reject;
           fn(done);
           return null;
         };
@@ -115,7 +115,7 @@ class BadTemplateUrl {
 
       const restoreJasmineIt = () => { jasmine.getEnv().it = originalJasmineIt; };
 
-      it('should fail when an ResourceLoader fails', (done: any /** TODO #9100 */) => {
+      it('should fail when an ResourceLoader fails', done => {
         const itPromise = patchJasmineIt();
 
         it('should fail with an error from a promise', async(() => {