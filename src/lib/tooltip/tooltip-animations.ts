4e358c37398428148233924973fb3395b457ecf9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-07-29 06:49:44 +0200

    feat(tooltip): align with 2018 material design spec (#12310)
    Adjusts the tooltip to align it with the new Material design spec.

--- src/lib/tooltip/tooltip-animations.ts
+++ src/lib/tooltip/tooltip-animations.ts
@@ -7,11 +7,12 @@
  */
 import {
   animate,
+  AnimationTriggerMetadata,
+  keyframes,
   state,
   style,
   transition,
   trigger,
-  AnimationTriggerMetadata,
 } from '@angular/animations';
 
 /** Animations used by MatTooltip. */
@@ -20,9 +21,13 @@ export const matTooltipAnimations: {
 } = {
   /** Animation that transitions a tooltip in and out. */
   tooltipState: trigger('state', [
-    state('initial, void, hidden', style({transform: 'scale(0)'})),
+    state('initial, void, hidden', style({opacity: 0, transform: 'scale(0)'})),
     state('visible', style({transform: 'scale(1)'})),
-    transition('* => visible', animate('150ms cubic-bezier(0.0, 0.0, 0.2, 1)')),
-    transition('* => hidden', animate('150ms cubic-bezier(0.4, 0.0, 1, 1)')),
+    transition('* => visible', animate('200ms cubic-bezier(0, 0, 0.2, 1)', keyframes([
+      style({opacity: 0, transform: 'scale(0)', offset: 0}),
+      style({opacity: 0.5, transform: 'scale(0.99)', offset: 0.5}),
+      style({opacity: 1, transform: 'scale(1)', offset: 1})
+    ]))),
+    transition('* => hidden', animate('100ms cubic-bezier(0, 0, 0.2, 1)', style({opacity: 0}))),
   ])
 };
