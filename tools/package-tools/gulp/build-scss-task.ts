e07400354abe031bd7767a3f10d3c9615f8c9e78
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-07-29 06:49:04 +0200

    build: switch to dart-sass (#12370)
    * Switches to dart-sass in order to be consistent with the Bazel output. That way we can identify potential issues with the SASS output (e.g. demo-app) before we at some point make the full-switch.
    * Also speeds-up the project installs because libsass bindings are no longer needed (trade-off with compilation time though; but pretty minimal due to our amount of Sass files)

--- tools/package-tools/gulp/build-scss-task.ts
+++ tools/package-tools/gulp/build-scss-task.ts
@@ -2,7 +2,7 @@ import {src, dest} from 'gulp';
 import {join} from 'path';
 
 // These imports lack of type definitions.
-const gulpSass = require('gulp-sass');
+const gulpDartSass = require('gulp-dart-sass');
 const gulpIf = require('gulp-if');
 const gulpCleanCss = require('gulp-clean-css');
 
@@ -10,7 +10,7 @@ const gulpCleanCss = require('gulp-clean-css');
 export function buildScssTask(outputDir: string, sourceDir: string, minifyOutput = false) {
   return () => {
     return src(join(sourceDir, '**/*.scss'))
-      .pipe(gulpSass().on('error', gulpSass.logError))
+      .pipe(gulpDartSass.sync().on('error', gulpDartSass.logError))
       .pipe(gulpIf(minifyOutput, gulpCleanCss()))
       .pipe(dest(outputDir));
   };
