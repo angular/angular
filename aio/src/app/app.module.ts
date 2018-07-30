3eaceb22bd02c24dbad83eae18ec31de3e0b8b65
Stefanie Fluin <sjtrimble@gmail.com>
2018-07-30 16:08:05 -0700

    docs(docs-infra): add GitHub stars notification bar and custom styles

--- aio/src/app/app.module.ts
+++ aio/src/app/app.module.ts
@@ -83,6 +83,18 @@ export const svgIconProviders = [
       '</svg>'
     },
     multi: true
+  },
+  {
+    provide: SVG_ICONS,
+    useValue: {
+      name: 'star_rate',
+      svgSource:
+      '<svg width="32" height="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" >' +
+        '<path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>' +
+        '<path fill="none" d="M0 0h18v18H0z"/>' +
+      '</svg>'
+    },
+    multi: true
   }
 ];
 
