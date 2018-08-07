3357e70d5f9acabf65fd78fdc924abffd1766acc
Joey Perrott <josephperrott.github@gmail.com>
2018-08-07 13:27:33 -0700

    chore: allow for scss imports from node_modules (#12475)

--- tools/package-tools/gulp/build-scss-pipeline.ts
+++ tools/package-tools/gulp/build-scss-pipeline.ts
@@ -1,14 +1,19 @@
 import {src} from 'gulp';
 import {join} from 'path';
+import {buildConfig} from '../build-config';
 
 // These imports lack of type definitions.
 const gulpSass = require('gulp-sass');
 const gulpIf = require('gulp-if');
 const gulpCleanCss = require('gulp-clean-css');
 
+const sassIncludePaths = [
+  join(buildConfig.projectDir, 'node_modules/')
+];
+
 /** Create a gulp task that builds SCSS files. */
 export function buildScssPipeline(sourceDir: string, minifyOutput = false) {
   return src(join(sourceDir, '**/*.scss'))
-    .pipe(gulpSass().on('error', gulpSass.logError))
+    .pipe(gulpSass({includePaths: sassIncludePaths}).on('error', gulpSass.logError))
     .pipe(gulpIf(minifyOutput, gulpCleanCss()));
 }
