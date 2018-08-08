432ee2ef1d94d5588676478118ff50206da5250d
Pete Bacon Darwin <pete@bacondarwin.com>
2018-08-08 22:10:16 +0100

    feat(ivy): compile all package formats in ngcc

--- integration/ngcc/test.sh
+++ integration/ngcc/test.sh
@@ -4,7 +4,7 @@ set -x
 
 PATH=$PATH:$(npm bin)
 
-ivy-ngcc node_modules/@angular/common esm2015
+ivy-ngcc node_modules/@angular/common
 cp -r node_modules_ngtsc/* node_modules/
 ngc -p tsconfig-app.json
 
