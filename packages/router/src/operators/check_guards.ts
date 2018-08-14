29d3f3f6dd748ce38afe0c6a26a2ed0b94a79e96
Jason Aden <jasonaden@google.com>
2018-08-14 10:12:55 -0700

    refactor(router): create pipeable checkGuards function (#25740)
    PR Close #25740

---
+++ packages/router/src/operators/check_guards.ts
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Injector, Type} from '@angular/core';
+import {Observable, OperatorFunction} from 'rxjs';
+import {mergeMap} from 'rxjs/operators';
+
+import {Route} from '../config';
+import {PreActivation} from '../pre_activation';
+import {recognize as recognizeFn} from '../recognize';
+import {ChildrenOutletContexts} from '../router_outlet_context';
+import {RouterStateSnapshot} from '../router_state';
+import {UrlTree} from '../url_tree';
+
+export function checkGuards(
+    rootContexts: ChildrenOutletContexts, currentSnapshot: RouterStateSnapshot,
+    moduleInjector: Injector, preActivation: PreActivation): OperatorFunction<UrlTree, boolean> {
+  return function(source: Observable<UrlTree>) {
+    return source.pipe(
+        mergeMap((appliedUrl): Observable<boolean> => { return preActivation.checkGuards(); }));
+  };
+}
