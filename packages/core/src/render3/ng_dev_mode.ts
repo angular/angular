c54045d6efed5071a58d277527fcd2f6b4840089
Ben Lesh <ben@benlesh.com>
2018-08-13 17:12:34 -0700

    fix(ivy): reordering how root is acquired

--- packages/core/src/render3/ng_dev_mode.ts
+++ packages/core/src/render3/ng_dev_mode.ts
@@ -38,8 +38,8 @@ declare global {
 declare let global: any;
 
 const __global: {ngDevMode: NgDevModePerfCounters | boolean} =
-    typeof window != 'undefined' && window || typeof self != 'undefined' && self ||
-    typeof global != 'undefined' && global;
+    typeof window != 'undefined' && window || typeof global != 'undefined' && global ||
+    typeof self != 'undefined' && self;
 
 export function ngDevModeResetPerfCounters(): NgDevModePerfCounters {
   // Make sure to refer to ngDevMode as ['ngDevMode'] for clousre.
