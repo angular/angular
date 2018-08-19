db7c04b1886f491eb54a540ecadc6a56c070153f
Igor Barsi <ibarsi92@gmail.com>
2018-08-19 12:15:13 -0400

    test(common): Add tests from @sanbornsen's PR.

--- packages/common/test/directives/ng_let_spec.ts
+++ packages/common/test/directives/ng_let_spec.ts
@@ -7,13 +7,13 @@
  */
 
 import {CommonModule} from '@angular/common';
-import {Component, EventEmitter, WrappedValue} from '@angular/core';
+import {Component, EventEmitter} from '@angular/core';
 import {ComponentFixture, TestBed, async} from '@angular/core/testing';
 import {By} from '@angular/platform-browser/src/dom/debug/by';
 import {expect} from '@angular/platform-browser/testing/src/matchers';
 
 {
-  describe('ngLet  directive', () => {
+  describe('ngLet directive', () => {
     let fixture: ComponentFixture<any>;
 
     function getComponent(): TestComponent { return fixture.componentInstance; }
