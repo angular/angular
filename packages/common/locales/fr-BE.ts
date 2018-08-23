86a275823301b048bc68c94cc8ed896d3272ca64
jester <jestersimpps@gmail.com>
2018-08-23 11:03:46 +0200

    Update fr-BE.ts
    TSLint: Identifier 'i' is never reassigned; use 'const' instead of 'let'. (prefer-const)

--- packages/common/locales/fr-BE.ts
+++ packages/common/locales/fr-BE.ts
@@ -12,7 +12,7 @@
 const u = undefined;
 
 function plural(n: number): number {
-  let i = Math.floor(Math.abs(n));
+  const i = Math.floor(Math.abs(n));
   if (i === 0 || i === 1) return 1;
   return 5;
 }
