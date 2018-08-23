38f624d7e3c4d39b618bef37a4f0bbb9500dc3f3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-08-23 14:34:55 -0700

    feat(ivy): output diagnostics for many errors in ngtsc (#25647)
    This commit takes the first steps towards ngtsc producing real
    TypeScript diagnostics instead of simply throwing errors when
    encountering incorrect code.
    
    A new class is introduced, FatalDiagnosticError, which can be thrown by
    handlers whenever a condition in the code is encountered which by
    necessity prevents the class from being compiled. This error type is
    convertable to a ts.Diagnostic which represents the type and source of
    the error.
    
    Error codes are introduced for Angular errors, and are prefixed with -99
    (so error code 1001 becomes -991001) to distinguish them from other TS
    errors.
    
    A function is provided which will read TS diagnostic output and convert
    the TS errors to NG errors if they match this negative error code
    format.
    
    PR Close #25647

---
+++ packages/compiler-cli/src/ngtsc/diagnostics/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+export {ErrorCode} from './src/code';
+export {FatalDiagnosticError, isFatalDiagnosticError} from './src/error';
+export {replaceTsWithNgInErrors} from './src/util';
