73d54a428b71a78abb73fecc850e3f00988d2e12
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-08-28 22:37:06 +0200

    chore: fix selection lint warning (#12872)
    Fixes a lint warning that got in through a long-running PR.

--- src/cdk/collections/selection.ts
+++ src/cdk/collections/selection.ts
@@ -39,7 +39,7 @@ export class SelectionModel<T> {
   /**
    * Event emitted when the value has changed.
    * @deprecated Use `changed` instead.
-   * @deletion-target 8.0.0 To be changed to `changed`
+   * @breaking-change 8.0.0 To be changed to `changed`
    */
   onChange: Subject<SelectionChange<T>> | null = this.changed;
 
