9837e10794b5005daa181be8fde0af3ee94aebbf
Pete Bacon Darwin <pete@bacondarwin.com>
2018-08-31 15:57:53 +0100

    build(docs-infra): improve directive API doc templates
    Closes #22790
    Closes #25530

--- aio/tools/transforms/angular-base-package/rendering/hasValues.js
+++ aio/tools/transforms/angular-base-package/rendering/hasValues.js
@@ -3,7 +3,14 @@ module.exports = function hasValues() {
     name: 'hasValues',
     process: function(list, property) {
       if (!list || !Array.isArray(list)) return false;
-      return list.some(item => item[property]);
+      return list.some(item => readProperty(item, property.split('.'), 0));
     }
   };
 };
+
+// Read deeply into an object via a collection of property segments
+// E.g. if `obj = { a: { b: { c: 10 }}}` then `readProperty(obj, ['a', 'b', 'c'], 0)` will return `10`;
+function readProperty(obj, propertySegments, index) {
+  const value = obj[propertySegments[index]];
+  return index < (propertySegments.length - 1) ? readProperty(value, propertySegments, index + 1) : value;
+}
