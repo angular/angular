2a5e62d10e33138b640080cab83abd2d087cf8e6
Brandon Roberts <robertsbt@gmail.com>
2018-09-07 07:39:58 -0500

    docs: update shared example dependencies to Angular 7 and CLI 7

--- aio/tools/examples/shared/boilerplate/cli/src/environments/environment.ts
+++ aio/tools/examples/shared/boilerplate/cli/src/environments/environment.ts
@@ -7,9 +7,10 @@ export const environment = {
 };
 
 /*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
