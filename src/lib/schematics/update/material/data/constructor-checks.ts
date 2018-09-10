05563d95f8de9e15ab0972467e242c10be0dc908
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-11 01:03:24 +0200

    chore(schematics): add missing constructor checks data (#13052)
    Adds the constructor signature check data for https://github.com/angular/material2/pull/11706 which is already published in `7.0.0-beta.0`.

--- src/lib/schematics/update/material/data/constructor-checks.ts
+++ src/lib/schematics/update/material/data/constructor-checks.ts
@@ -26,4 +26,13 @@ export const constructorChecks = [
 
   // https://github.com/angular/material2/pull/9775
   'MatCalendar',
+
+  // TODO(devversion): The constructor check rule doesn't distinguish data based on the target
+  // TODO(devversion): version. Though it would be more readable to structure the data.
+
+  // https://github.com/angular/material2/pull/11706
+  'MatDrawerContent',
+
+  // https://github.com/angular/material2/pull/11706
+  'MatSidenavContent',
 ];
