7d586e4f6e35631da6cb2c6e4f20fa5a5a712b11
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-14 15:37:50 +0200

    fix(input): do not focus input element twice (#12851)
    * Currently if a user clicks on a input, the element receives focus and the `click` event bubbles up. Once it reaches the `<mat-form-field>`, the click event will cause the `onContainerClick` method to be invoked. This method then manually focused the input element (again). Resulting in unexpected behavior for time inputs in Firefox.
    
    Fixes #12849

--- src/lib/input/input.ts
+++ src/lib/input/input.ts
@@ -392,5 +392,12 @@ export class MatInput extends _MatInputMixinBase implements MatFormFieldControl<
    * Implemented as part of MatFormFieldControl.
    * @docs-private
    */
-  onContainerClick() { this.focus(); }
+  onContainerClick() {
+    // Do not re-focus the input element if the element is already focused. Otherwise it can happen
+    // that someone clicks on a time input and the cursor resets to the "hours" field while the
+    // "minutes" field was actually clicked. See: https://github.com/angular/material2/issues/12849
+    if (!this.focused) {
+      this.focus();
+    }
+  }
 }
