a9a81f91cf48e6a24bb3901c4d1fce2f425efa39
Brandon Roberts <robertsbt@gmail.com>
2018-09-17 14:36:00 -0500

    docs(forms): update form apis based on review feedback (#25724)
    PR Close #25724

--- packages/forms/src/directives/validators.ts
+++ packages/forms/src/directives/validators.ts
@@ -36,11 +36,11 @@ export type ValidationErrors = {
  *
  * ```typescript
  * @Directive({
- *   selector: '[custom-validator]',
+ *   selector: '[customValidator]',
  *   providers: [{provide: NG_VALIDATORS, useExisting: CustomValidatorDirective, multi: true}]
  * })
  * class CustomValidatorDirective implements Validator {
- *   validate(c: AbstractControl): {[key: string]: any} {
+ *   validate(c: AbstractControl): ValidationErrors|null {
  *     return {'custom': true};
  *   }
  * }
@@ -49,7 +49,7 @@ export type ValidationErrors = {
 export interface Validator {
   /**
    * @description
-   * Method that performs synchronous verification against the provided control.
+   * Method that performs synchronous validation against the provided control.
    *
    * @param c The control to validate against.
    *
@@ -82,12 +82,12 @@ export interface Validator {
  * import { of as observableOf } from 'rxjs';
  *
  * @Directive({
- *   selector: '[custom-async-validator]',
+ *   selector: '[customAsyncValidator]',
  *   providers: [{provide: NG_ASYNC_VALIDATORS, useExisting: CustomAsyncValidatorDirective, multi:
  * true}]
  * })
  * class CustomAsyncValidatorDirective implements AsyncValidator {
- *   validate(c: AbstractControl): Observable<{[key: string]: any}> {
+ *   validate(c: AbstractControl): Observable<ValidationErrors|null> {
  *     return observableOf({'custom': true});
  *   }
  * }
@@ -98,7 +98,7 @@ export interface Validator {
 export interface AsyncValidator extends Validator {
   /**
    * @description
-   * Method that performs async verification against the provided control.
+   * Method that performs async validation against the provided control.
    *
    * @param c The control to validate against.
    *
