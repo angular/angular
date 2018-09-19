a43bd4f5e573c7547895c52757a56c8dd2acf07a
Brandon Roberts <robertsbt@gmail.com>
2018-09-19 14:15:49 -0500

    docs: more overview feedback changes (#25663)
    PR Close #25663

--- aio/content/examples/forms-overview/src/app/template/favorite-color/favorite-color.component.ts
+++ aio/content/examples/forms-overview/src/app/template/favorite-color/favorite-color.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-template-favorite-color',
@@ -7,12 +7,6 @@ import { Component, OnInit } from '@angular/core';
   `,
   styles: []
 })
-export class FavoriteColorComponent implements OnInit {
+export class FavoriteColorComponent {
   favoriteColor = '';
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
 }
