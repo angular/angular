966699915d49821ed013c4ab789722ce9278a803
musicq <liang.kui@hotmail.com>
2018-09-20 15:16:17 +0800

    style(elements): remove unused imports

--- packages/elements/src/element-strategy.ts
+++ packages/elements/src/element-strategy.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import {ComponentFactory, Injector} from '@angular/core';
+import {Injector} from '@angular/core';
 import {Observable} from 'rxjs';
 
 /**
