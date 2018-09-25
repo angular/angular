16774a49021c27a08827843d41ec79ab4549c250
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-25 14:58:40 -0700

    feat(ivy): introduce the type check block compiler (#26203)
    This commit introduces the main functionality of the type-check compiler:
    generation of type check blocks. Type check blocks are blocks of TypeScript
    code which can be inlined into source files, and when processed by the
    TypeChecker will give information about any typing errors in template
    expressions.
    
    PR Close #26203

--- packages/compiler-cli/src/ngtsc/typecheck/src/api.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/src/api.ts
@@ -6,9 +6,38 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {BoundTarget, DirectiveMeta} from '@angular/compiler';
+import * as ts from 'typescript';
+
+import {Reference} from '../../metadata';
+
 /**
- * Metadata to generate a type constructor for a particular directive.
-*/
+ * Extension of `DirectiveMeta` that includes additional information required to type-check the
+ * usage of a particular directive.
+ */
+export interface TypeCheckableDirectiveMeta extends DirectiveMeta {
+  ref: Reference<ts.ClassDeclaration>;
+  queries: string[];
+  ngTemplateGuards: string[];
+  hasNgTemplateContextGuard: boolean;
+}
+
+/**
+ * Metadata required in addition to a component class in order to generate a type check block (TCB)
+ * for that component.
+ */
+export interface TypeCheckBlockMetadata {
+  /**
+   * Semantic information about the template of the component.
+   */
+  boundTarget: BoundTarget<TypeCheckableDirectiveMeta>;
+
+  /**
+   * The name of the requested type check block function.
+   */
+  fnName: string;
+}
+
 export interface TypeCtorMetadata {
   /**
    * The name of the requested type constructor function.
