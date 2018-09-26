0078bc3de1bc400b436911c86d92c97be3f8f660
Pete Bacon Darwin <pete@bacondarwin.com>
2018-09-26 17:28:30 +0100

    refactor(ivy): rename and move ngcc `Parsed...` to `Decorated...`

--- packages/compiler-cli/src/ngcc/src/parsing/esm2015_parser.ts
+++ packages/compiler-cli/src/ngcc/src/parsing/esm2015_parser.ts
@@ -9,20 +9,20 @@
 import * as ts from 'typescript';
 
 import {NgccReflectionHost} from '../host/ngcc_host';
+import {DecoratedClass} from '../host/decorated_class';
+import {DecoratedFile} from '../host/decorated_file';
 import {getOriginalSymbol, isDefined} from '../utils';
 
 import {FileParser} from './file_parser';
-import {ParsedClass} from './parsed_class';
-import {ParsedFile} from './parsed_file';
 
 export class Esm2015FileParser implements FileParser {
   checker = this.program.getTypeChecker();
 
   constructor(protected program: ts.Program, protected host: NgccReflectionHost) {}
 
-  parseFile(file: ts.SourceFile): ParsedFile[] {
+  parseFile(file: ts.SourceFile): DecoratedFile[] {
     const moduleSymbol = this.checker.getSymbolAtLocation(file);
-    const map = new Map<ts.SourceFile, ParsedFile>();
+    const map = new Map<ts.SourceFile, DecoratedFile>();
     if (moduleSymbol) {
       const exportedSymbols =
           this.checker.getExportsOfModule(moduleSymbol).map(getOriginalSymbol(this.checker));
@@ -38,7 +38,7 @@ export class Esm2015FileParser implements FileParser {
                       undefined;
                   const decorators = this.host.getDecoratorsOfDeclaration(declaration);
                   return decorators && isDefined(name) ?
-                      new ParsedClass(name, declaration, decorators) :
+                      new DecoratedClass(name, declaration, decorators) :
                       undefined;
                 }
                 return undefined;
@@ -48,7 +48,7 @@ export class Esm2015FileParser implements FileParser {
       decoratedClasses.forEach(clazz => {
         const file = clazz.declaration.getSourceFile();
         if (!map.has(file)) {
-          map.set(file, new ParsedFile(file));
+          map.set(file, new DecoratedFile(file));
         }
         map.get(file) !.decoratedClasses.push(clazz);
       });
