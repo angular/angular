978d1abe676b024f4383c315527cdabb901c9a10
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-26 09:43:52 -0700

    feat(ivy): introduce typecheck package and a type constructor generator
    This commit introduces //packages/compiler-cli/src/ngtsc/typecheck as a
    container for template type-checking code, and implements an initial API:
    type constructor generation.
    
    Type constructors are static methods on component/directive types with
    no runtime implementation. The methods are used during compilation to
    enable inference of a component or directive's generic type parameters
    from the types of expressions bound to any of their @Inputs. A type
    constructor looks like:
    
    class Directive<T> {
      someInput: T;
      static ngTypeCtor<T>(init: Partial<Pick<Directive<T>, 'someInput'>>): Directive<T>;
    }
    
    It can be used to infer a type for T based on the input:
    
    const _dir = Directive.ngTypeCtor({someInput: 'string'}); // Directive<T>

---
+++ packages/compiler-cli/src/ngtsc/typecheck/src/api.ts
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * Metadata to generate a type constructor for a particular directive.
+*/
+export interface TypeCtorMetadata {
+  /**
+   * The name of the requested type constructor function.
+   */
+  fnName: string;
+
+  /**
+   * Whether to generate a body for the function or not.
+   */
+  body: boolean;
+
+  /**
+   * Input, output, and query field names in the type which should be included as constructor input.
+   */
+  fields: {inputs: string[]; outputs: string[]; queries: string[];};
+}
