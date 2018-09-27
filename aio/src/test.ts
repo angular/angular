de2544d5ce8d648e74e2601638b2661aa8cb121f
George Kalpakas <kalpakas.g@gmail.com>
2018-09-27 18:24:07 +0300

    build(docs-infra): upgrade @angular/* to 7.0.0-beta.7 (#26145)
    PR Close #26145

--- aio/src/test.ts
+++ aio/src/test.ts
@@ -1,5 +1,9 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+// Reflect.metadata polyfill is only needed in the JIT mode which we use only for unit tests
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
@@ -8,10 +12,6 @@ import {
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: any;
-
-// Reflect.metadata polyfill is only needed in the JIT mode which we use only for unit tests
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
