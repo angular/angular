621d82a44c9d46f69f3296b302984f5949b8dee8
Brandon Roberts <robertsbt@gmail.com>
2018-10-01 11:13:27 -0500

    docs: add testing styles in index.html, remove additional configuration

--- aio/tools/examples/example-boilerplate.js
+++ aio/tools/examples/example-boilerplate.js
@@ -61,11 +61,6 @@ BOILERPLATE_PATHS.i18n = [
   'package.json'
 ];
 
-BOILERPLATE_PATHS.testing = [
-  ...cliRelativePath,
-  'angular.json'
-];
-
 BOILERPLATE_PATHS.universal = [
   ...cliRelativePath,
   'angular.json',
