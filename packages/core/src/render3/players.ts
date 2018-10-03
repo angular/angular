fa8e633be5dae243bf05e7216c20a19157bcb016
Matias Niemelä <matias@yearofmoo.com>
2018-10-03 14:09:59 -0700

    feat(ivy): enhance [style] and [class] bindings to be animation aware (#26096)
    PR Close #26096

--- packages/core/src/render3/players.ts
+++ packages/core/src/render3/players.ts
@@ -5,43 +5,60 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import {getContext} from './context_discovery';
-import {scheduleTick} from './instructions';
-import {ComponentInstance, DirectiveInstance, PlayState, Player} from './interfaces/player';
-import {RootContextFlags} from './interfaces/view';
-import {CorePlayerHandler} from './styling/core_player_handler';
-import {getOrCreatePlayerContext} from './styling/util';
-import {getRootContext} from './util';
+import './ng_dev_mode';
 
+import {getContext} from './context_discovery';
+import {getRootContext} from './discovery_utils';
+import {scheduleTick} from './instructions';
+import {ComponentInstance, DirectiveInstance, Player} from './interfaces/player';
+import {HEADER_OFFSET, RootContextFlags} from './interfaces/view';
+import {addPlayerInternal, getOrCreatePlayerContext, getPlayerContext, getPlayersInternal, getStylingContext, throwInvalidRefError} from './styling/util';
+
+/**
+ * Adds a player to an element, directive or component instance that will later be
+ * animated once change detection has passed.
+ *
+ * When a player is added to a reference it will stay active until `player.destroy()`
+ * is called. Once called then the player will be removed from the active players
+ * present on the associated ref instance.
+ *
+ * To get a list of all the active players on an element see [getPlayers].
+ *
+ * @param ref The element, directive or component that the player will be placed on.
+ * @param player The player that will be triggered to play once change detection has run.
+ */
 export function addPlayer(
     ref: ComponentInstance | DirectiveInstance | HTMLElement, player: Player): void {
-  const elementContext = getContext(ref) !;
-  const animationContext = getOrCreatePlayerContext(elementContext.native, elementContext) !;
-  animationContext.push(player);
-
-  player.addEventListener(PlayState.Destroyed, () => {
-    const index = animationContext.indexOf(player);
-    if (index >= 0) {
-      animationContext.splice(index, 1);
-    }
-    player.destroy();
-  });
-
-  const rootContext = getRootContext(elementContext.lViewData);
-  const playerHandler =
-      rootContext.playerHandler || (rootContext.playerHandler = new CorePlayerHandler());
-  playerHandler.queuePlayer(player, ref);
-
-  const nothingScheduled = rootContext.flags === RootContextFlags.Empty;
-
-  // change detection may or may not happen therefore
-  // the core code needs to be kicked off to flush the animations
-  rootContext.flags |= RootContextFlags.FlushPlayers;
-  if (nothingScheduled) {
-    scheduleTick(rootContext);
+  const context = getContext(ref);
+  if (!context) {
+    ngDevMode && throwInvalidRefError();
+    return;
   }
+
+  const element = context.native as HTMLElement;
+  const lViewData = context.lViewData;
+  const playerContext = getOrCreatePlayerContext(element, context) !;
+  const rootContext = getRootContext(lViewData);
+  addPlayerInternal(playerContext, rootContext, element, player, 0, ref);
+  scheduleTick(rootContext, RootContextFlags.FlushPlayers);
 }
 
+/**
+ * Returns a list of all the active players present on the provided ref instance (which can
+ * be an instance of a directive, component or element).
+ *
+ * This function will only return players that have been added to the ref instance using
+ * `addPlayer` or any players that are active through any template styling bindings
+ * (`[style]`, `[style.prop]`, `[class]` and `[class.name]`).
+ */
 export function getPlayers(ref: ComponentInstance | DirectiveInstance | HTMLElement): Player[] {
-  return getOrCreatePlayerContext(ref);
+  const context = getContext(ref);
+  if (!context) {
+    ngDevMode && throwInvalidRefError();
+    return [];
+  }
+
+  const stylingContext = getStylingContext(context.nodeIndex - HEADER_OFFSET, context.lViewData);
+  const playerContext = stylingContext ? getPlayerContext(stylingContext) : null;
+  return playerContext ? getPlayersInternal(playerContext) : [];
 }
