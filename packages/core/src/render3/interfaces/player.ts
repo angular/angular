ce4463e7a2a7d66ddec7805f77333f142ba18671
Matias Niemelä <matias@yearofmoo.com>
2018-10-03 14:09:59 -0700

    feat(ivy): enhance [style] and [class] bindings to be animation aware (#26096)
    PR Close #26096

--- packages/core/src/render3/interfaces/player.ts
+++ packages/core/src/render3/interfaces/player.ts
@@ -19,6 +19,43 @@ export interface Player {
   addEventListener(state: PlayState|string, cb: (data?: any) => any): void;
 }
 
+export const enum BindingType {
+  Unset = 0,
+  Class = 2,
+  Style = 3,
+}
+
+export interface BindingStore { setValue(prop: string, value: any): void; }
+
+/**
+ * Defines the shape which produces the Player.
+ *
+ * Used to produce a player that will be placed on an element that contains
+ * styling bindings that make use of the player. This function is designed
+ * to be used with `PlayerFactory`.
+ */
+export interface PlayerFactoryBuildFn {
+  (element: HTMLElement, type: BindingType, values: {[key: string]: any},
+   currentPlayer: Player|null): Player|null;
+}
+
+/**
+ * Used as a reference to build a player from a styling template binding
+ * (`[style]` and `[class]`).
+ *
+ * The `fn` function will be called once any styling-related changes are
+ * evaluated on an element and is expected to return a player that will
+ * be then run on the element.
+ *
+ * `[style]`, `[style.prop]`, `[class]` and `[class.name]` template bindings
+ * all accept a `PlayerFactory` as input and this player factories.
+ */
+export interface PlayerFactory { '__brand__': 'Brand for PlayerFactory that nothing will match'; }
+
+export interface PlayerBuilder extends BindingStore {
+  buildPlayer(currentPlayer: Player|null): Player|undefined|null;
+}
+
 /**
  * The state of a given player
  *
@@ -29,11 +66,15 @@ export interface Player {
 export const enum PlayState {Pending = 0, Running = 1, Paused = 2, Finished = 100, Destroyed = 200}
 
 /**
- * The context that stores all active animation players present on an element.
+ * The context that stores all the active players and queued player factories present on an element.
  */
-export declare type PlayerContext = Player[];
-export declare type ComponentInstance = {};
-export declare type DirectiveInstance = {};
+export interface PlayerContext extends Array<null|number|Player|PlayerBuilder> {
+  [PlayerIndex.NonBuilderPlayersStart]: number;
+  [PlayerIndex.ClassMapPlayerBuilderPosition]: PlayerBuilder|null;
+  [PlayerIndex.ClassMapPlayerPosition]: Player|null;
+  [PlayerIndex.StyleMapPlayerBuilderPosition]: PlayerBuilder|null;
+  [PlayerIndex.StyleMapPlayerPosition]: Player|null;
+}
 
 /**
  * Designed to be used as an injection service to capture all animation players.
@@ -54,3 +95,29 @@ export interface PlayerHandler {
    */
   queuePlayer(player: Player, context: ComponentInstance|DirectiveInstance|HTMLElement): void;
 }
+
+export const enum PlayerIndex {
+  // The position where the index that reveals where players start in the PlayerContext
+  NonBuilderPlayersStart = 0,
+  // The position where the player builder lives (which handles {key:value} map expression) for
+  // classes
+  ClassMapPlayerBuilderPosition = 1,
+  // The position where the last player assigned to the class player builder is stored
+  ClassMapPlayerPosition = 2,
+  // The position where the player builder lives (which handles {key:value} map expression) for
+  // styles
+  StyleMapPlayerBuilderPosition = 3,
+  // The position where the last player assigned to the style player builder is stored
+  StyleMapPlayerPosition = 4,
+  // The position where any player builders start in the PlayerContext
+  PlayerBuildersStartPosition = 1,
+  // The position where non map-based player builders start in the PlayerContext
+  SinglePlayerBuildersStartPosition = 5,
+  // For each player builder there is a player in the player context (therefore size = 2)
+  PlayerAndPlayerBuildersTupleSize = 2,
+  // The player exists next to the player builder in the list
+  PlayerOffsetPosition = 1,
+}
+
+export declare type ComponentInstance = {};
+export declare type DirectiveInstance = {};
--- packages/core/test/bundling/animation_world/index.ts
+++ packages/core/test/bundling/animation_world/index.ts
@@ -9,16 +9,19 @@
 import '@angular/core/test/bundling/util/src/reflect_metadata';
 
 import {CommonModule} from '@angular/common';
-import {Component, ElementRef, NgModule, ɵPlayState as PlayState, ɵPlayer as Player, ɵPlayerHandler as PlayerHandler, ɵaddPlayer as addPlayer, ɵrenderComponent as renderComponent} from '@angular/core';
+import {Component, ElementRef, NgModule, ɵPlayState as PlayState, ɵPlayer as Player, ɵPlayerHandler as PlayerHandler, ɵaddPlayer as addPlayer, ɵbindPlayerFactory as bindPlayerFactory, ɵmarkDirty as markDirty, ɵrenderComponent as renderComponent} from '@angular/core';
 
 @Component({
   selector: 'animation-world',
   template: `
     <nav>
-      <button (click)="doAnimate()">Populate List</button>
+      <button (click)="animateWithCustomPlayer()">Animate List (custom player)</button>
+      <button (click)="animateWithStyles()">Populate List (style bindings)</button>
     </nav>
     <div class="list">
-      <div *ngFor="let item of items" class="record" [class]="makeClass(item)">
+      <div
+        *ngFor="let item of items" class="record" [class]="makeClass(item)" style="border-radius: 10px"
+        [style]="styles">
         {{ item }}
       </div>
     </div>
@@ -27,12 +30,18 @@ import {Component, ElementRef, NgModule, ɵPlayState as PlayState, ɵPlayer as P
 class AnimationWorldComponent {
   items: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   private _hostElement: HTMLElement;
+  public styles: {[key: string]: any}|null = null;
 
   constructor(element: ElementRef) { this._hostElement = element.nativeElement; }
 
   makeClass(index: number) { return `record-${index}`; }
 
-  doAnimate() {
+  animateWithStyles() {
+    this.styles = animateStyleFactory([{opacity: 0}, {opacity: 1}], 300, 'ease-out');
+    markDirty(this);
+  }
+
+  animateWithCustomPlayer() {
     const elements = this._hostElement.querySelectorAll('div.record') as any as HTMLElement[];
     for (let i = 0; i < elements.length; i++) {
       const element = elements[i];
@@ -55,13 +64,13 @@ function buildAnimationPlayer(element: HTMLElement, animationName: string, time:
 class SimpleKeyframePlayer implements Player {
   state = PlayState.Pending;
   parent: Player|null = null;
-  private _animationStyle: string;
+  private _animationStyle: string = '';
   private _listeners: {[stateName: string]: (() => any)[]} = {};
   constructor(private _element: HTMLElement, private _animationName: string, time: string) {
     this._animationStyle = `${time} ${_animationName}`;
   }
   private _start() {
-    this._element.style.animation = this._animationStyle;
+    (this._element as any).style.animation = this._animationStyle;
     const animationFn = (event: AnimationEvent) => {
       if (event.animationName == this._animationName) {
         this._element.removeEventListener('animationend', animationFn);
@@ -134,3 +143,66 @@ class AnimationDebugger implements PlayerHandler {
 
 const playerHandler = new AnimationDebugger();
 renderComponent(AnimationWorldComponent, {playerHandler});
+
+function animateStyleFactory(keyframes: any[], duration: number, easing: string) {
+  const limit = keyframes.length - 1;
+  const finalKeyframe = keyframes[limit];
+  return bindPlayerFactory((element: HTMLElement, type: number, values: {[key: string]: any}) => {
+    const kf = keyframes.slice(0, limit);
+    kf.push(values);
+    return new WebAnimationsPlayer(element, keyframes, duration, easing);
+  }, finalKeyframe);
+}
+
+class WebAnimationsPlayer implements Player {
+  state = PlayState.Pending;
+  parent: Player|null = null;
+  private _listeners: {[stateName: string]: (() => any)[]} = {};
+  constructor(
+      private _element: HTMLElement, private _keyframes: {[key: string]: any}[],
+      private _duration: number, private _easing: string) {}
+  private _start() {
+    const player = this._element.animate(
+        this._keyframes as any[], {duration: this._duration, easing: this._easing, fill: 'both'});
+    player.addEventListener('finish', e => { this.finish(); });
+  }
+  addEventListener(state: PlayState|string, cb: () => any): void {
+    const key = state.toString();
+    const arr = this._listeners[key] = (this._listeners[key] || []);
+    arr.push(cb);
+  }
+  play(): void {
+    if (this.state <= PlayState.Pending) {
+      this._start();
+    }
+    if (this.state != PlayState.Running) {
+      this.state = PlayState.Running;
+      this._emit(this.state);
+    }
+  }
+  pause(): void {
+    if (this.state != PlayState.Paused) {
+      this.state = PlayState.Paused;
+      this._emit(this.state);
+    }
+  }
+  finish(): void {
+    if (this.state < PlayState.Finished) {
+      this._element.style.animation = '';
+      this.state = PlayState.Finished;
+      this._emit(this.state);
+    }
+  }
+  destroy(): void {
+    if (this.state < PlayState.Destroyed) {
+      this.finish();
+      this.state = PlayState.Destroyed;
+      this._emit(this.state);
+    }
+  }
+  capture(): any {}
+  private _emit(state: PlayState) {
+    const arr = this._listeners[state.toString()] || [];
+    arr.forEach(cb => cb());
+  }
+}
