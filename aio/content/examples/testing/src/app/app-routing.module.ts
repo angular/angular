4c173f6271cf3bca7ad88bc554c6ccd0265962bc
Brandon Roberts <robertsbt@gmail.com>
2018-10-06 15:02:14 -0500

    docs: fix broken lazy loading examples with Angular CLI RC upgrade

--- aio/content/examples/testing/src/app/app-routing.module.ts
+++ aio/content/examples/testing/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { AboutComponent } from './about/about.component';
     RouterModule.forRoot([
       { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
       { path: 'about', component: AboutComponent },
-      { path: 'heroes', loadChildren: 'app/hero/hero.module#HeroModule'}
+      { path: 'heroes', loadChildren: './hero/hero.module#HeroModule'}
     ])
   ],
   exports: [ RouterModule ] // re-export the module declarations
