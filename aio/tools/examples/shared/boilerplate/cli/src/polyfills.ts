72ff9c880c01ec684306c3591779ea50bcff33fc
Brandon Roberts <robertsbt@gmail.com>
2018-10-08 14:34:44 -0500

    docs: sync boilerplate configuration with Angular CLI RC2 (#25892)
    Revers Jasmine version back to ~2.99.1
    
    PR Close #25892

--- aio/tools/examples/shared/boilerplate/cli/src/polyfills.ts
+++ aio/tools/examples/shared/boilerplate/cli/src/polyfills.ts
@@ -34,17 +34,18 @@
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
 
+/**
+ * If the application will be indexed by Google Search, the following is required.
+ * Googlebot uses a renderer based on Chrome 41.
+ * https://developers.google.com/search/docs/guides/rendering
+ **/
+// import 'core-js/es6/array';
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following for the Reflect API. */
 // import 'core-js/es6/reflect';
-
-
-/** Evergreen browsers require these. **/
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect';
-
 
 /**
  * Web Animations `@angular/platform-browser/animations`
