60946767369239156f1a1c0ecd4801798e5a35cd
Brandon Roberts <robertsbt@gmail.com>
2018-10-09 16:06:47 -0400

    docs: add core-js polyfills for StackBlitz support

--- aio/tools/examples/shared/boilerplate/cli/src/polyfills.ts
+++ aio/tools/examples/shared/boilerplate/cli/src/polyfills.ts
@@ -45,7 +45,8 @@
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es6/reflect';
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
