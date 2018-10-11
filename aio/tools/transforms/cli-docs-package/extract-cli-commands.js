e2615d92dd6ccc3e1d8ba505f890180b88e62147
George Kalpakas <kalpakas.g@gmail.com>
2018-10-11 11:52:27 +0300

    build(docs-infra): make the git ref for cli command docs configurable (#26391)
    PR Close #26391

--- aio/tools/transforms/cli-docs-package/extract-cli-commands.js
+++ aio/tools/transforms/cli-docs-package/extract-cli-commands.js
@@ -1,7 +1,16 @@
-const shelljs = require('shelljs');
-const {resolve}  = require('canonical-path');
+const {resolve} = require('canonical-path');
+const sh = require('shelljs');
 const {CONTENTS_PATH} = require('../config');
 
-shelljs.cd(resolve(CONTENTS_PATH, 'cli-src'));
-shelljs.exec('git clean -Xfd');
-shelljs.exec('yarn install');
+const cliGitRef = process.argv[2] || 'master';  // Can be a branch, commit or tag.
+const pkgContent = JSON.stringify({
+  dependencies: {
+    '@angular/cli': `https://github.com/angular/cli-builds#${cliGitRef}`,
+  },
+}, null, 2);
+
+sh.set('-e');
+sh.cd(resolve(CONTENTS_PATH, 'cli-src'));
+sh.exec('git clean -Xfd');
+sh.echo(pkgContent).to('package.json');
+sh.exec('yarn install --no-lockfile --non-interactive');
