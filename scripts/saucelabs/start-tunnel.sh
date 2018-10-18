e4bf74a5273fd4a943000cbc9bcd5428cf0572ac
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-18 19:08:41 +0200

    build: update saucelabs connect version (#13656)

--- scripts/saucelabs/start-tunnel.sh
+++ scripts/saucelabs/start-tunnel.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail
 
-TUNNEL_FILE="sc-4.5.0-linux.tar.gz"
+TUNNEL_FILE="sc-4.5.1-linux.tar.gz"
 TUNNEL_URL="https://saucelabs.com/downloads/${TUNNEL_FILE}"
 TUNNEL_DIR="/tmp/saucelabs-connect"
 
