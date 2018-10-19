5ab808e644d1a172957c2729c34a0fab872f1f95
Pete Bacon Darwin <pete@bacondarwin.com>
2018-10-19 14:37:01 +0100

    docs: update platform-browser with `@publicApi` tags (#26595)
    PR Close #26595

--- packages/platform-browser/src/browser.ts
+++ packages/platform-browser/src/browser.ts
@@ -41,6 +41,9 @@ export const BROWSER_SANITIZATION_PROVIDERS: StaticProvider[] = [
   {provide: DomSanitizer, useClass: DomSanitizerImpl, deps: [DOCUMENT]},
 ];
 
+/**
+ * @publicApi
+ */
 export const platformBrowser: (extraProviders?: StaticProvider[]) => PlatformRef =
     createPlatformFactory(platformCore, 'browser', INTERNAL_BROWSER_PLATFORM_PROVIDERS);
 
@@ -95,7 +98,7 @@ export const BROWSER_MODULE_PROVIDERS: StaticProvider[] = [
  * Re-exports `CommonModule` and `ApplicationModule`, making their
  * exports and providers available to all apps.
  *
- *
+ * @publicApi
  */
 @NgModule({providers: BROWSER_MODULE_PROVIDERS, exports: [CommonModule, ApplicationModule]})
 export class BrowserModule {
@@ -113,8 +116,6 @@ export class BrowserModule {
    * @param params An object containing an identifier for the app to transition.
    * The ID must match between the client and server versions of the app.
    * @returns The reconfigured `BrowserModule` to import into the app's root `AppModule`.
-   *
-   * @publicApi
    */
   static withServerTransition(params: {appId: string}): ModuleWithProviders<BrowserModule> {
     return {
