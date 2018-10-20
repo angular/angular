f8fcea333f993ca7d74730571103c1fb621ac6d2
Igor Minar <igor@angular.io>
2018-10-20 00:30:21 -0700

    build: remove obsolete files (#26471)
    PR Close #26471

--- tools/build/systemjs/payload_tests_import.js
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-
-System.import('index').catch(console.error.bind(console));
