53b0c845a6a0c7830c9d8bf154b722e1828be721
Andrew Kushnir <akushnir@google.com>
2018-10-25 11:35:11 -0700

    fixup! fix(ivy): move HostListeners generation to factory function

--- packages/compiler/src/injectable_compiler_2.ts
+++ packages/compiler/src/injectable_compiler_2.ts
@@ -42,7 +42,7 @@ export function compileInjectable(meta: R3InjectableMetadata): InjectableDef {
     type: meta.type,
     deps: meta.ctorDeps,
     injectFn: Identifiers.inject,
-    extraStatementFn: null
+    extraStatementFn: null,
   };
 
   if (meta.useClass !== undefined) {
