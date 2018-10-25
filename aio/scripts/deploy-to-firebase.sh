b77228b89c7a3a32b8c965e68bfab23cf57fca30
George Kalpakas <kalpakas.g@gmail.com>
2018-10-25 13:13:24 +0300

    ci(docs-infra): reduce verbosity of `yarn build` on CI (#26746)
    PR Close #26746

--- aio/scripts/deploy-to-firebase.sh
+++ aio/scripts/deploy-to-firebase.sh
@@ -93,7 +93,7 @@ fi
   cd "`dirname $0`/.."
 
   # Build the app
-  yarn build-for $deployEnv
+  yarn build-for $deployEnv --progress=false
 
   # Include any mode-specific files
   cp -rf src/extra-files/$deployEnv/. dist/
