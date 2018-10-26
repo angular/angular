3da82338d1a6f81fd9501366f855f90cf91952ef
Greg Magolan <gmagolan@gmail.com>
2018-10-25 17:02:50 -0700

    build(bazel): turn on --nolegacy-external-runfiles (#26770)
    PR Close #26770

--- packages/bazel/test/ngc-wrapped/tsconfig_template.ts
+++ packages/bazel/test/ngc-wrapped/tsconfig_template.ts
@@ -70,7 +70,7 @@ export function createTsConfig(options: TsConfigOptions) {
       'tsickleExternsPath': '',
       // we don't copy the node_modules into our tmp dir, so we should look in
       // the original workspace directory for it
-      'nodeModulesPrefix': '../angular/external/ngdeps/node_modules',
+      'nodeModulesPrefix': '../ngdeps/node_modules',
     },
     'files': options.files,
     'angularCompilerOptions': {
