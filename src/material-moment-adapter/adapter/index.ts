a479366c76169cd84aae53ffd90c0b5ff2fe33a5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-29 15:51:02 +0100

    build: test remaining packages with bazel (#13844)
    * Sets up Bazel build and testing for the `cdk-experimental`, `material-experimental` and `material-moment-adapter` package.

--- src/material-moment-adapter/adapter/index.ts
+++ src/material-moment-adapter/adapter/index.ts
@@ -7,20 +7,12 @@
  */
 
 import {NgModule} from '@angular/core';
-import {
-  DateAdapter,
-  MAT_DATE_LOCALE,
-  MAT_DATE_FORMATS
-} from '@angular/material';
-import {
-  MomentDateAdapter,
-  MAT_MOMENT_DATE_ADAPTER_OPTIONS
-} from './moment-date-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material';
+import {MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter} from './moment-date-adapter';
 import {MAT_MOMENT_DATE_FORMATS} from './moment-date-formats';
 
 export * from './moment-date-adapter';
 export * from './moment-date-formats';
-
 
 @NgModule({
   providers: [
