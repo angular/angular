01304080d1b00276d2a9da324220fbdbfe5862f8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-29 21:25:00 +0100

    build: fix ts-api-guardian does not work on windows w/ bazel (#26761)
    * Fixes that the `ts-api-guardian` package does not work on Windows with Bazel. This is because `ts-api-guardian` does not resolve the runfiles through theNodeJS `require` function that properly handles runfiles within Bazel.
    
    PR Close #26761

--- tools/ts-api-guardian/test/bootstrap.ts
+++ tools/ts-api-guardian/test/bootstrap.ts
@@ -7,6 +7,12 @@
  */
 
 import * as path from 'path';
-// Before running tests, change directories to our directory under the runfiles
-// From runfiles we want to go to the angular/tools/ts-api-guardian subfolder.
-process.chdir(path.join(process.env['TEST_SRCDIR'], 'angular', 'tools', 'ts-api-guardian'));
+
+// Resolve the path to the package.json of the ts-api-guardian. We need to resolve an actual
+// path of a runfile because we want to determine the path to the directory that includes all
+// test fixture runfiles. On Windows this is usually the original non-sandboxed disk location,
+// otherwise this just refers to the runfile directory with all the proper symlinked files.
+// TODO: remove the whole bootstrap file once the tests are Bazel and Windows compatible.
+const runfilesDirectory = path.dirname(require.resolve('../package.json'));
+
+process.chdir(runfilesDirectory);
