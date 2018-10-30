06395111f29e44e6310b21cefc61a9aba9b8d7c4
Jason Aden <jasonaden@google.com>
2018-10-30 14:13:55 -0700

    feat(router): add pathParamsChange mode for runGuardsAndResolvers (#26861)
    This option means guards and resolvers will ignore changes to optional
    parameters such as query and matrix params. When the path or any path
    params change, guards and resolvers will be run
    
    Related to discussion in #18253
    FW-560 #resolve
    
    PR Close #26861

--- packages/router/src/utils/preactivation.ts
+++ packages/router/src/utils/preactivation.ts
@@ -11,6 +11,7 @@ import {Injector} from '@angular/core';
 import {LoadedRouterConfig, RunGuardsAndResolvers} from '../config';
 import {ChildrenOutletContexts, OutletContext} from '../router_outlet_context';
 import {ActivatedRouteSnapshot, RouterStateSnapshot, equalParamsAndUrlSegments} from '../router_state';
+import {equalPath} from '../url_tree';
 import {forEach, shallowEqual} from '../utils/collection';
 import {TreeNode, nodeChildrenAsMap} from '../utils/tree';
 
@@ -147,6 +148,9 @@ function shouldRunGuardsAndResolvers(
     curr: ActivatedRouteSnapshot, future: ActivatedRouteSnapshot,
     mode: RunGuardsAndResolvers | undefined): boolean {
   switch (mode) {
+    case 'pathParamsChange':
+      return !equalPath(curr.url, future.url);
+
     case 'always':
       return true;
 
