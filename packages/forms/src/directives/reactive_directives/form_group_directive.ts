52bab6defce56c5c8507f41721016e5bc4616577
natcohen <natcohen@users.noreply.github.com>
2018-11-09 10:10:58 +0100

    Fix dynamic change of reactive form
    Fix the following issue: https://github.com/angular/angular/issues/26998

--- packages/forms/src/directives/reactive_directives/form_group_directive.ts
+++ packages/forms/src/directives/reactive_directives/form_group_directive.ts
@@ -249,11 +249,15 @@ export class FormGroupDirective extends ControlContainer implements Form,
   /** @internal */
   _updateDomValue() {
     this.directives.forEach(dir => {
-      const newCtrl: any = this.form.get(dir.path);
+      const newCtrl = this.form.get(dir.path);
       if (dir.control !== newCtrl) {
         cleanUpControl(dir.control, dir);
-        if (newCtrl) setUpControl(newCtrl, dir);
-        (dir as{control: FormControl}).control = newCtrl;
+        if (newCtrl && newCtrl instanceof FormControl) {
+          setUpControl(newCtrl, dir);
+          dir.control = newCtrl;
+        } else {
+          dir.control = null;
+        }
       }
     });
 
