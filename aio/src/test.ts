4f965ad2a11c5bd670e8ebb8a5852ce02d6fe1a1
Igor Minar <igor@angular.io>
2018-11-16 05:33:08 -0800

    test(docs-infra): remove reflect-metadata polyfill from aio tests (#27130)
    v7 CLI includes the polyfill automatically whenever JIT compiler is used, so we don't need
    to do it explicitly.
    
    PR Close #27130

--- aio/src/test.ts
+++ aio/src/test.ts
@@ -1,9 +1,5 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-// Reflect.metadata polyfill is only needed in the JIT mode which we use only for unit tests
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
-
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
