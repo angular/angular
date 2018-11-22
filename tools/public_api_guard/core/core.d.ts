afd52570d806dd09c9041ee5f73456c1794dde3a
Teamop <zhangwenlp@vip.qq.com>
2018-11-22 18:07:29 +0800

    refactor(core): improve type for EventEmitter (#27228)
    PR Close #27228

--- tools/public_api_guard/core/core.d.ts
+++ tools/public_api_guard/core/core.d.ts
@@ -313,7 +313,7 @@ export declare class EventEmitter<T> extends Subject<T> {
     __isAsync: boolean;
     constructor(isAsync?: boolean);
     emit(value?: T): void;
-    subscribe(generatorOrNext?: any, error?: any, complete?: any): any;
+    subscribe(generatorOrNext?: any, error?: any, complete?: any): Subscription;
 }
 
 export interface ExistingProvider extends ExistingSansProvider {
