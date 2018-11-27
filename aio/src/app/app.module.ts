27b196f585e961d9775ca992e35c8843b8e95c8d
George Kalpakas <kalpakas.g@gmail.com>
2018-11-27 14:35:16 +0200

    fix(docs-infra): remove hard-coded color from inlined SVG icons (#27250)
    This allows the icons to inherit the color of their HTMLElement parent.
    
    PR Close #27250

--- aio/src/app/app.module.ts
+++ aio/src/app/app.module.ts
@@ -51,7 +51,7 @@ export const svgIconProviders = [
     useValue: {
       name: 'close',
       svgSource:
-        '<svg fill="#ffffff" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">' +
+        '<svg focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">' +
           '<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />' +
           '<path d="M0 0h24v24H0z" fill="none" />' +
         '</svg>',
@@ -75,7 +75,7 @@ export const svgIconProviders = [
     useValue: {
       name: 'insert_comment',
       svgSource:
-        '<svg fill="#ffffff" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">' +
+        '<svg focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">' +
           '<path d="M20 2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z" />' +
           '<path d="M0 0h24v24H0z" fill="none" />' +
         '</svg>',
