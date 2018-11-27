b448bc8934bcc988c439675cf0042ca5cf14b73a
Ricardo M <lordrip@gmail.com>
2018-11-27 16:32:05 +0100

    docs(ripple): fix typo in comments (#14286)

--- src/lib/core/ripple/ripple-renderer.ts
+++ src/lib/core/ripple/ripple-renderer.ts
@@ -201,7 +201,7 @@ export class RippleRenderer {
       this._containerRect = null;
     }
 
-    // For ripples that are not active anymore, don't re-un the fade-out animation.
+    // For ripples that are not active anymore, don't re-run the fade-out animation.
     if (!wasActive) {
       return;
     }
