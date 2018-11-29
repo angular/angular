94ad518b096a1005163b417c239dcd401067137a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-30 00:47:51 +0100

    build: fix cronjob tests not running against snapshots (#14327)
    * Due to the fact that the `CIRCLE_WORKFLOW_ID` does not actually return the name of the workflow, but a unique id that is generated dynamically, the cronjob does not run against the Angular github builds.

--- scripts/circleci/run-local-browser-tests.sh
+++ scripts/circleci/run-local-browser-tests.sh
@@ -7,12 +7,6 @@ set -e
 # Go to project directory.
 cd $(dirname ${0})/../..
 
-# In case the "snapshot_tests" workflow is currently running this script, we
-# want to run the local browser tests against the Angular snapshot builds.
-if [[ "${CIRCLE_WORKFLOW_ID}" == "snapshot_tests" ]]; then
-  ./scripts/install-angular-snapshots.sh
-fi
-
 # Setup the test platform environment variable that will be read
 # by the Karma configuration script.
 export TEST_PLATFORM="local"
