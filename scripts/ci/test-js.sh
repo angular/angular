8a63f6e245d0095ca1a23b874e1ccf1f3a512575
Igor Minar <igor@angular.io>
2018-12-01 03:47:06 -0800

    ci: don't run node unit tests on travis (#27386)
    we run all these tests on CircleCI with bazel.
    
    PR Close #27386

--- scripts/ci/test-js.sh
+++ scripts/ci/test-js.sh
@@ -6,19 +6,6 @@ set -u -e -o pipefail
 readonly thisDir=$(cd $(dirname $0); pwd)
 source ${thisDir}/_travis-fold.sh
 
-# Run unit tests in node
-travisFoldStart "test.unit.node"
-  node --harmony ./dist/tools/tsc-watch/ node runCmdsOnly
-travisFoldEnd "test.unit.node"
-
-
-# rebuild to revert files in @angular/compiler/test
-# TODO(tbosch): remove this and teach karma to serve the right files
-travisFoldStart "test.unit.rebuildHack"
-  node dist/tools/@angular/compiler-cli/src/main -p packages/tsconfig-metadata.json
-travisFoldStart "test.unit.rebuildHack"
-
-
 travisFoldStart "test.unit.localChrome"
   $(npm bin)/karma start ./karma-js.conf.js --single-run --browsers=${KARMA_JS_BROWSERS}
 travisFoldEnd "test.unit.localChrome"
