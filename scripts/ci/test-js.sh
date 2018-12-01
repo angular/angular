6d0f81328c767964b68e8d67246004853cc7ef81
Igor Minar <igor@angular.io>
2018-12-01 02:18:44 -0800

    ci: remove validate-commit-message tests from travis (#27386)
    we alredy run them via bazel.
    
    I also removed a bunch of obsolete files that bazel doesn't need.
    
    PR Close #27386

--- scripts/ci/test-js.sh
+++ scripts/ci/test-js.sh
@@ -6,19 +6,6 @@ set -u -e -o pipefail
 readonly thisDir=$(cd $(dirname $0); pwd)
 source ${thisDir}/_travis-fold.sh
 
-
-# Run unit tests for our tools/ directory
-travisFoldStart "test.unit.tools"
-  # TODO(i): could this be rolled into the tools tests above? why is it separate?
-  travisFoldStart "test.unit.validate-commit-message"
-    (
-      cd tools/validate-commit-message
-      $(npm bin)/jasmine
-    )
-  travisFoldEnd "test.unit.validate-commit-message"
-travisFoldEnd "test.unit.tools"
-
-
 # Run unit tests in node
 travisFoldStart "test.unit.node"
   node --harmony ./dist/tools/tsc-watch/ node runCmdsOnly
