c07afd9db1184e5d625301fe154a4ef2d9974e52
Olivier Combe <olivier.combe@gmail.com>
2018-12-04 15:13:10 +0100

    test(ivy): enable ivy tests for platform-browser (#27460) PR Close #27460

--- packages/platform-browser/test/browser/bootstrap_spec.ts
+++ packages/platform-browser/test/browser/bootstrap_spec.ts
@@ -18,6 +18,7 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {getDOM} from '@angular/platform-browser/src/dom/dom_adapter';
 import {DOCUMENT} from '@angular/platform-browser/src/dom/dom_tokens';
 import {expect} from '@angular/platform-browser/testing/src/matchers';
+import {fixmeIvy} from '@angular/private/testing';
 
 @Component({selector: 'non-existent', template: ''})
 class NonExistentComp {
@@ -159,17 +160,18 @@ function bootstrap(
 
     afterEach(destroyPlatform);
 
-    it('should throw if bootstrapped Directive is not a Component',
-       inject([AsyncTestCompleter], (done: AsyncTestCompleter) => {
-         const logger = new MockConsole();
-         const errorHandler = new ErrorHandler();
-         (errorHandler as any)._console = logger as any;
-         expect(
-             () => bootstrap(
-                 HelloRootDirectiveIsNotCmp, [{provide: ErrorHandler, useValue: errorHandler}]))
-             .toThrowError(`HelloRootDirectiveIsNotCmp cannot be used as an entry component.`);
-         done.done();
-       }));
+    fixmeIvy('unknown') &&
+        it('should throw if bootstrapped Directive is not a Component',
+           inject([AsyncTestCompleter], (done: AsyncTestCompleter) => {
+             const logger = new MockConsole();
+             const errorHandler = new ErrorHandler();
+             (errorHandler as any)._console = logger as any;
+             expect(
+                 () => bootstrap(
+                     HelloRootDirectiveIsNotCmp, [{provide: ErrorHandler, useValue: errorHandler}]))
+                 .toThrowError(`HelloRootDirectiveIsNotCmp cannot be used as an entry component.`);
+             done.done();
+           }));
 
     it('should throw if no element is found',
        inject([AsyncTestCompleter], (async: AsyncTestCompleter) => {
@@ -186,40 +188,42 @@ function bootstrap(
          });
        }));
 
-    it('should throw if no provider', inject([AsyncTestCompleter], (async: AsyncTestCompleter) => {
-         const logger = new MockConsole();
-         const errorHandler = new ErrorHandler();
-         (errorHandler as any)._console = logger as any;
+    fixmeIvy('unknown') &&
+        it('should throw if no provider',
+           inject([AsyncTestCompleter], (async: AsyncTestCompleter) => {
+             const logger = new MockConsole();
+             const errorHandler = new ErrorHandler();
+             (errorHandler as any)._console = logger as any;
 
-         class IDontExist {}
+             class IDontExist {}
 
-         @Component({selector: 'cmp', template: 'Cmp'})
-         class CustomCmp {
-           constructor(iDontExist: IDontExist) {}
-         }
+             @Component({selector: 'cmp', template: 'Cmp'})
+             class CustomCmp {
+               constructor(iDontExist: IDontExist) {}
+             }
 
-         @Component({
-           selector: 'hello-app',
-           template: '<cmp></cmp>',
-         })
-         class RootCmp {
-         }
+             @Component({
+               selector: 'hello-app',
+               template: '<cmp></cmp>',
+             })
+             class RootCmp {
+             }
 
-         @NgModule({declarations: [CustomCmp], exports: [CustomCmp]})
-         class CustomModule {
-         }
+             @NgModule({declarations: [CustomCmp], exports: [CustomCmp]})
+             class CustomModule {
+             }
 
-         bootstrap(RootCmp, [{provide: ErrorHandler, useValue: errorHandler}], [], [
-           CustomModule
-         ]).then(null, (e: Error) => {
-           expect(e.message).toContain(
-               'StaticInjectorError(TestModule)[CustomCmp -> IDontExist]: \n' +
-               '  StaticInjectorError(Platform: core)[CustomCmp -> IDontExist]: \n' +
-               '    NullInjectorError: No provider for IDontExist!');
-           async.done();
-           return null;
-         });
-       }));
+             bootstrap(RootCmp, [{provide: ErrorHandler, useValue: errorHandler}], [], [
+               CustomModule
+             ]).then(null, (e: Error) => {
+               expect(e.message).toContain(
+                   'StaticInjectorError(TestModule)[CustomCmp -> IDontExist]: \n' +
+                   '  StaticInjectorError(Platform: core)[CustomCmp -> IDontExist]: \n' +
+                   '    NullInjectorError: No provider for IDontExist!');
+               async.done();
+               return null;
+             });
+           }));
 
     if (getDOM().supportsDOMEvents()) {
       it('should forward the error to promise when bootstrap fails',
