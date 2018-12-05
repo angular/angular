768859753b86077de03f88255b8b5ec40531d1ec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-05 13:08:55 -0800

    fix(ivy): generate correct moduleNames for factories and summaries (#27483)
    In ngtsc, files loaded into the ts.Program have a "module name", set via
    ts.SourceFile.moduleName, which ends up being written into an AMD module
    name triple-slash directive in the generated .js file.
    
    For generated shim files (ngfactories, ngsummaries) that are constructed
    synthetically, there was previously no moduleName set, which caused some
    issues with downstream tests.
    
    This commit adds logic to compute and set moduleNames for both generated
    ngfactory and ngsummary shims.
    
    PR Close #27483

--- packages/compiler-cli/src/ngtsc/shims/src/util.ts
+++ packages/compiler-cli/src/ngtsc/shims/src/util.ts
@@ -6,9 +6,24 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import * as path from 'path';
+import * as ts from 'typescript';
+
 const TS_FILE = /\.tsx?$/;
 const D_TS_FILE = /\.d\.ts$/;
 
 export function isNonDeclarationTsFile(file: string): boolean {
   return TS_FILE.exec(file) !== null && D_TS_FILE.exec(file) === null;
+}
+
+export function generatedModuleName(
+    originalModuleName: string, originalFileName: string, genSuffix: string): string {
+  let moduleName: string;
+  if (originalFileName.endsWith('/index.ts')) {
+    moduleName = originalModuleName + '/index' + genSuffix;
+  } else {
+    moduleName = originalModuleName + genSuffix;
+  }
+
+  return moduleName;
 }
