368dbfc6457427982109271625b48bc33132f656
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-05 16:05:29 -0800

    feat(ivy): generate flat module index files
    Previously, ngtsc did not respect the angularCompilerOptions settings
    for generating flat module indices. This commit adds a
    FlatIndexGenerator which is used to implement those options.
    
    FW-738 #resolve

--- packages/compiler-cli/src/ngtsc/shims/index.ts
+++ packages/compiler-cli/src/ngtsc/shims/index.ts
@@ -9,5 +9,6 @@
 /// <reference types="node" />
 
 export {FactoryGenerator, FactoryInfo, generatedFactoryTransform} from './src/factory_generator';
-export {GeneratedShimsHostWrapper} from './src/host';
+export {FlatIndexGenerator} from './src/flat_index_generator';
+export {GeneratedShimsHostWrapper, ShimGenerator} from './src/host';
 export {SummaryGenerator} from './src/summary_generator';
