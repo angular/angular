52fd4a2d35a7c3a0a4d04d9e10f79ab90e8bc2fb
Igor Minar <igor@angular.io>
2018-12-11 22:49:14 -0800

    build: disable postinstall-patch.js log output (#27619)
    currently we have no patches so the logging only adds noise to our dev environment.
    
    PR Close #27619

--- tools/postinstall-patches.js
+++ tools/postinstall-patches.js
@@ -24,21 +24,24 @@ const {set, cd, sed, rm} = require('shelljs');
 const path = require('path');
 const log = console.log;
 
-log('===== about to run the postinstall-patches.js script     =====');
-// fail on first error
-set('-e');
-// print commands as being executed
-set('-v');
-// jump to project root
-cd(path.join(__dirname, '../'));
+// COMMENTED OUT BECAUSE WE CURRENTLY REQUIRE NO PATCHES
+// UNCOMMENT TO REENABLE PATCHING AND LOG OUTPUT
+//
+// log('===== about to run the postinstall-patches.js script     =====');
+// // fail on first error
+// set('-e');
+// // print commands as being executed
+// set('-v');
+// // jump to project root
+// cd(path.join(__dirname, '../'));
 
-/* EXAMPLE PATCH:
-// https://github.com/ReactiveX/rxjs/pull/3302
-// make node_modules/rxjs compilable with Typescript 2.7
-// remove when we update to rxjs v6
-log('\n# patch: reactivex/rxjs#3302 make node_modules/rxjs compilable with Typescript 2.7');
-sed('-i', '(\'response\' in xhr)', '(\'response\' in (xhr as any))',
-    'node_modules/rxjs/src/observable/dom/AjaxObservable.ts');
-*/
+// /* EXAMPLE PATCH:
+// // https://github.com/ReactiveX/rxjs/pull/3302
+// // make node_modules/rxjs compilable with Typescript 2.7
+// // remove when we update to rxjs v6
+// log('\n# patch: reactivex/rxjs#3302 make node_modules/rxjs compilable with Typescript 2.7');
+// sed('-i', '(\'response\' in xhr)', '(\'response\' in (xhr as any))',
+//     'node_modules/rxjs/src/observable/dom/AjaxObservable.ts');
+// */
 
-log('===== finished running the postinstall-patches.js script =====');
+// log('===== finished running the postinstall-patches.js script =====');
