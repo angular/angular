927e56a35d061b2a2af0901858281fc1153bc83e
Igor Minar <igor@angular.io>
2018-12-12 21:21:52 -0800

    test(ivy): remove stale fixmes from  //packages/platform-server/test (#27639)
    these issues have already been resolved but we forgot to reenable the tests.
    
    PR Close #27639

--- packages/platform-server/test/integration_spec.ts
+++ packages/platform-server/test/integration_spec.ts
@@ -589,18 +589,16 @@ class HiddenModule {
                 });
               }));
 
-      fixmeIvy(
-          `FW-643: Components with animations throw with "Failed to execute 'setAttribute' on 'Element'`)
-          .it('works with animation', async(() => {
-                renderModule(AnimationServerModule, {document: doc}).then(output => {
-                  expect(output).toContain('Works!');
-                  expect(output).toContain('ng-trigger-myAnimation');
-                  expect(output).toContain('opacity:1;');
-                  expect(output).toContain('transform:translate3d(0 , 0 , 0);');
-                  expect(output).toContain('font-weight:bold;');
-                  called = true;
-                });
-              }));
+      it('works with animation', async(() => {
+           renderModule(AnimationServerModule, {document: doc}).then(output => {
+             expect(output).toContain('Works!');
+             expect(output).toContain('ng-trigger-myAnimation');
+             expect(output).toContain('opacity:1;');
+             expect(output).toContain('transform:translate3d(0 , 0 , 0);');
+             expect(output).toContain('font-weight:bold;');
+             called = true;
+           });
+         }));
 
       it('should handle ViewEncapsulation.Native', async(() => {
            renderModule(NativeExampleModule, {document: doc}).then(output => {
