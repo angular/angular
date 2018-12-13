a3da8d2b5bd3c3a92c39ff34d98f1acaf95a56b0
vivian-hu <41269630+vivian-hu@users.noreply.github.com>
2018-12-12 16:17:03 -0800

    Revert "fix(menu): allow text wrapping in menu items" (#14493)
    * Revert "build: update nodejs bazel rules (#14466)"
    
    This reverts commit a3fef46a8eade83ca763c6a83842581a201d8cf7.
    
    * Revert "docs(form-field): native select reset values not working in examples (#14236)"
    
    This reverts commit 97f52c092426cf6f097f08426b064d4d0b61a957.
    
    * Revert "fix(menu): allow text wrapping in menu items (#11430)"
    
    This reverts commit 3661abe06109d1f675ae4089b09169f9170f65dd.

--- src/dev-app/menu/menu-demo.ts
+++ src/dev-app/menu/menu-demo.ts
@@ -19,13 +19,13 @@ export class MenuDemo {
   selected = '';
   items = [
     {text: 'Refresh'},
-    {text: 'Settings with a longer name so we can test text wrapping'},
+    {text: 'Settings'},
     {text: 'Help', disabled: true},
     {text: 'Sign Out'}
   ];
 
   iconItems = [
-    {text: 'Redial with a longer name so we can test text wrapping', icon: 'dialpad'},
+    {text: 'Redial', icon: 'dialpad'},
     {text: 'Check voicemail', icon: 'voicemail', disabled: true},
     {text: 'Disable alerts', icon: 'notifications_off'}
   ];
