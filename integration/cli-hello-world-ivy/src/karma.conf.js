fb962be80be1ac195862ad27064b2962ca5d62ac
cexbrayat <cedric@ninja-squad.com>
2018-12-14 15:46:01 +0200

    test: update cli-hello-world-ivy to cli@7.2.0-rc.0
    Updates the app itself to reflect the result of using the  `experimentalIvy` flag on the CLI.
    The result is similar to:
    
        npx @angular/cli@next new cli-hello-world-ivy --experimental-ivy --defaults
    
    But replaces the current (cli `7.2.0-rc.0`) `renderComponent` bootstrap with the usual `platformBrowserDynamic` one.
    It also keeps what the app did (display a pipe, tests it).

--- integration/cli-hello-world-ivy/src/karma.conf.js
+++ integration/cli-hello-world-ivy/src/karma.conf.js
@@ -12,15 +12,13 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
--- integration/cli-hello-world-ivy/src/main.ts
+++ integration/cli-hello-world-ivy/src/main.ts
@@ -9,4 +9,4 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+  .catch(err => console.error(err));
