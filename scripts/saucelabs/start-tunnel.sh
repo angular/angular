47fba31c5817d94a23a36a9fee2739271af37643
Greg Magolan <gmagolan@gmail.com>
2018-12-17 22:09:39 -0800

    build(bazel): run a number of web tests with karma_web_test in saucelabs in CircleCI (#27721)
    PR Close #27721

--- scripts/saucelabs/start-tunnel.sh
+++ scripts/saucelabs/start-tunnel.sh
@@ -7,16 +7,17 @@ readonly currentDir=$(cd $(dirname $0); pwd)
 # Command arguments that will be passed to sauce-connect.
 sauceArgs=""
 
-if [[ ! -z "${SAUCE_READY_FILE}" ]]; then
+if [[ ! -z "${SAUCE_READY_FILE:-}" ]]; then
+  mkdir -p $(dirname ${SAUCE_READY_FILE})
   sauceArgs="${sauceArgs} --readyfile ${SAUCE_READY_FILE}"
 fi
 
-if [[ ! -z "${SAUCE_PID_FILE}" ]]; then
+if [[ ! -z "${SAUCE_PID_FILE:-}" ]]; then
   mkdir -p $(dirname ${SAUCE_PID_FILE})
   sauceArgs="${sauceArgs} --pidfile ${SAUCE_PID_FILE}"
 fi
 
-if [[ ! -z "${SAUCE_TUNNEL_IDENTIFIER}" ]]; then
+if [[ ! -z "${SAUCE_TUNNEL_IDENTIFIER:-}" ]]; then
   sauceArgs="${sauceArgs} --tunnel-identifier ${SAUCE_TUNNEL_IDENTIFIER}"
 fi
 
