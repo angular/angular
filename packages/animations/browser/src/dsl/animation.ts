de9864d4a0559f15b241db9f96c9e6a4e9b7470c
Igor Minar <igor@angular.io>
2018-12-19 09:46:25 -0800

    just testing code owners - do not merge

--- packages/animations/browser/src/dsl/animation.ts
+++ packages/animations/browser/src/dsl/animation.ts
@@ -4,6 +4,7 @@
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
+ * lalalalala
  */
 import {AnimationMetadata, AnimationMetadataType, AnimationOptions, ɵStyleData} from '@angular/animations';
 
