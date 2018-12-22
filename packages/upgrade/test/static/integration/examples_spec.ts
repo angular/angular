fecacb4f85b83fcbc4920966a886c9af17b3f32c
Pete Bacon Darwin <pete@bacondarwin.com>
2018-12-22 16:02:34 +0000

    fix(ivy): pass ngContentSelectors through to `defineComponent()` calls (#27867)
    Libraries that create components dynamically using component factories,
    such as `@angular/upgrade` need to pass blocks of projected content
    through to the `ComponentFactory.create()` method. These blocks
    are extracted from the content by matching CSS selectors defined in
    `<ng-content select="..">` tags found in the component's template.
    
    The Angular compiler collects these CSS selectors when compiling a component's
    template, and exposes them via the `ComponentFactory.ngContentSelectors`
    property.
    
    This change ensures that this property is filled correctly when the
    component factory is created by compiling a component with the Ivy engine.
    
    PR Close #27867

--- packages/upgrade/test/static/integration/examples_spec.ts
+++ packages/upgrade/test/static/integration/examples_spec.ts
@@ -24,70 +24,69 @@ withEachNg1Version(() => {
 
     it('should have AngularJS loaded', () => expect(angular.version.major).toBe(1));
 
-    fixmeIvy('FW-714: ng1 projected content is not being rendered')
-        .it('should verify UpgradeAdapter example', async(() => {
+    it('should verify UpgradeAdapter example', async(() => {
 
-              // This is wrapping (upgrading) an AngularJS component to be used in an Angular
-              // component
-              @Directive({selector: 'ng1'})
-              class Ng1Component extends UpgradeComponent {
-                // TODO(issue/24571): remove '!'.
-                @Input() title !: string;
+         // This is wrapping (upgrading) an AngularJS component to be used in an Angular
+         // component
+         @Directive({selector: 'ng1'})
+         class Ng1Component extends UpgradeComponent {
+           // TODO(issue/24571): remove '!'.
+           @Input() title !: string;
 
-                constructor(elementRef: ElementRef, injector: Injector) {
-                  super('ng1', elementRef, injector);
-                }
-              }
+           constructor(elementRef: ElementRef, injector: Injector) {
+             super('ng1', elementRef, injector);
+           }
+         }
 
-              // This is an Angular component that will be downgraded
-              @Component({
-                selector: 'ng2',
-                template: 'ng2[<ng1 [title]="nameProp">transclude</ng1>](<ng-content></ng-content>)'
-              })
-              class Ng2Component {
-                // TODO(issue/24571): remove '!'.
-                @Input('name') nameProp !: string;
-              }
+         // This is an Angular component that will be downgraded
+         @Component({
+           selector: 'ng2',
+           template: 'ng2[<ng1 [title]="nameProp">transclude</ng1>](<ng-content></ng-content>)'
+         })
+         class Ng2Component {
+           // TODO(issue/24571): remove '!'.
+           @Input('name') nameProp !: string;
+         }
 
-              // This module represents the Angular pieces of the application
-              @NgModule({
-                declarations: [Ng1Component, Ng2Component],
-                entryComponents: [Ng2Component],
-                imports: [BrowserModule, UpgradeModule]
-              })
-              class Ng2Module {
-                ngDoBootstrap() { /* this is a placeholder to stop the bootstrapper from
-                                     complaining */
-                }
-              }
+         // This module represents the Angular pieces of the application
+         @NgModule({
+           declarations: [Ng1Component, Ng2Component],
+           entryComponents: [Ng2Component],
+           imports: [BrowserModule, UpgradeModule]
+         })
+         class Ng2Module {
+           ngDoBootstrap() { /* this is a placeholder to stop the bootstrapper from
+                                complaining */
+           }
+         }
 
-              // This module represents the AngularJS pieces of the application
-              const ng1Module =
-                  angular
-                      .module('myExample', [])
-                      // This is an AngularJS component that will be upgraded
-                      .directive(
-                          'ng1',
-                          () => {
-                            return {
-                              scope: {title: '='},
-                              transclude: true,
-                              template: 'ng1[Hello {{title}}!](<span ng-transclude></span>)'
-                            };
-                          })
-                      // This is wrapping (downgrading) an Angular component to be used in
-                      // AngularJS
-                      .directive('ng2', downgradeComponent({component: Ng2Component}));
+         // This module represents the AngularJS pieces of the application
+         const ng1Module =
+             angular
+                 .module('myExample', [])
+                 // This is an AngularJS component that will be upgraded
+                 .directive(
+                     'ng1',
+                     () => {
+                       return {
+                         scope: {title: '='},
+                         transclude: true,
+                         template: 'ng1[Hello {{title}}!](<span ng-transclude></span>)'
+                       };
+                     })
+                 // This is wrapping (downgrading) an Angular component to be used in
+                 // AngularJS
+                 .directive('ng2', downgradeComponent({component: Ng2Component}));
 
-              // This is the (AngularJS) application bootstrap element
-              // Notice that it is actually a downgraded Angular component
-              const element = html('<ng2 name="World">project</ng2>');
+         // This is the (AngularJS) application bootstrap element
+         // Notice that it is actually a downgraded Angular component
+         const element = html('<ng2 name="World">project</ng2>');
 
-              // Let's use a helper function to make this simpler
-              bootstrap(platformBrowserDynamic(), Ng2Module, element, ng1Module).then(upgrade => {
-                expect(multiTrim(element.textContent))
-                    .toBe('ng2[ng1[Hello World!](transclude)](project)');
-              });
-            }));
+         // Let's use a helper function to make this simpler
+         bootstrap(platformBrowserDynamic(), Ng2Module, element, ng1Module).then(upgrade => {
+           expect(multiTrim(element.textContent))
+               .toBe('ng2[ng1[Hello World!](transclude)](project)');
+         });
+       }));
   });
 });
