65ecb0886e59c0e39520d43917b79fb6f4d0e75e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-01-04 20:55:08 +0200

    fix(typography): deprecation warning in latest sass version (#14673)
    * Updates to the latest `node-sass` version.
    * Fixes the build not using the `node-sass` version from the package.json.
    * Fixes a deprecation warning from the latest SASS version when the consumer has configured a non-string `font-family` (e.g. `Helvetica Neue`).
    
    Fixes #14636.

--- tools/package-tools/gulp/build-scss-pipeline.ts
+++ tools/package-tools/gulp/build-scss-pipeline.ts
@@ -4,12 +4,16 @@ import {buildConfig} from '../build-config';
 
 // These imports lack of type definitions.
 const gulpSass = require('gulp-sass');
+const nodeSass = require('node-sass');
 const gulpIf = require('gulp-if');
 const gulpCleanCss = require('gulp-clean-css');
 
 const sassIncludePaths = [
   join(buildConfig.projectDir, 'node_modules/')
 ];
+
+// Set the compiler to our version of `node-sass`, rather than the one that `gulp-sass` depends on.
+gulpSass.compiler = nodeSass;
 
 /** Create a gulp task that builds SCSS files. */
 export function buildScssPipeline(sourceDir: string, minifyOutput = false) {
