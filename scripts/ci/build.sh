0a06005fcae8b880d64848c1dcefa31a4913746e
Igor Minar <igor@angular.io>
2019-01-05 00:17:24 -0800

    ci: remove build steps that are no longer needed (#27937)
    the metadata build seems to be needed only by the offline compiler tests which is currently disabled
    
    PR Close #27937

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -7,29 +7,7 @@ readonly thisDir=$(cd $(dirname $0); pwd)
 source ${thisDir}/_travis-fold.sh
 
 
-# If the previous commands in the `script` section of .travis.yaml failed, then abort.
-# The variable is not set in early stages of the build, so we default to 0 there.
-# https://docs.travis-ci.com/user/environment-variables/
-if [[ ${TRAVIS_TEST_RESULT=0} == 1 ]]; then
-  exit 1;
-fi
-
-# No build needed for bazel or aio docs tests
-if [[ ${CI_MODE:-} == "bazel" ]]; then
-  exit 0;
-fi
-
-travisFoldStart "tsc tools"
-  # TODO: I think these three can be deleted... but I'm not sure
-  #       let's delete them one at a time and test on CI
-  $(npm bin)/tsc -p tools
-  $(npm bin)/tsc -p packages/compiler/tsconfig-tools.json
-  $(npm bin)/tsc -p packages/compiler-cli/tsconfig-tools.json
-travisFoldEnd "tsc tools"
-
-
 travisFoldStart "tsc all"
-  node dist/tools/@angular/compiler-cli/src/main -p packages/tsconfig-metadata.json
   $(npm bin)/tsc -p packages
   $(npm bin)/tsc -p packages/examples
   $(npm bin)/tsc -p modules
