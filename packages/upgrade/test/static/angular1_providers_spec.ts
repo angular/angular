cfac2373b0f9eb8eb0101b2da57a0c0713f98405
George Kalpakas <kalpakas.g@gmail.com>
2019-01-10 17:18:05 +0200

    test(upgrade): log more info to help debug CI flakes (#28045)
    Related Jira issue: FW-939
    
    PR Close #28045

--- packages/upgrade/test/static/angular1_providers_spec.ts
+++ packages/upgrade/test/static/angular1_providers_spec.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {Ng1Token} from '@angular/upgrade/static/src/common/angular1';
+import {IInjectorService, Ng1Token} from '@angular/upgrade/static/src/common/angular1';
 import {compileFactory, injectorFactory, parseFactory, rootScopeFactory, setTempInjectorRef} from '@angular/upgrade/static/src/static/angular1_providers';
 
 {
@@ -22,19 +22,30 @@ import {compileFactory, injectorFactory, parseFactory, rootScopeFactory, setTemp
 
     describe('injectorFactory', () => {
       it('should return the injector value that was previously set', () => {
-        const mockInjector = {get: () => {}, has: () => false};
+        const mockInjector = {get: () => undefined, has: () => false};
         setTempInjectorRef(mockInjector);
         const injector = injectorFactory();
         expect(injector).toBe(mockInjector);
       });
 
       it('should throw if the injector value has not been set yet', () => {
-        const mockInjector = {get: () => {}, has: () => false};
-        expect(injectorFactory).toThrowError();
+        let injector: IInjectorService|null = null;
+
+        try {
+          injector = injectorFactory();
+        } catch {
+          // Throwing an error is the expected behavior.
+          return;
+        }
+
+        // Normally, we should never get here (but sometimes we do on CI).
+        // Log some info to help debug the issue.
+        console.error(`Unexpected injector (${typeof injector}):`, injector);
+        fail(`Expected no injector, but got: ${jasmine.pp(injector)}`);
       });
 
       it('should unset the injector after the first call (to prevent memory leaks)', () => {
-        const mockInjector = {get: () => {}, has: () => false};
+        const mockInjector = {get: () => undefined, has: () => false};
         setTempInjectorRef(mockInjector);
         injectorFactory();
         expect(injectorFactory).toThrowError();  // ...because it has been unset
