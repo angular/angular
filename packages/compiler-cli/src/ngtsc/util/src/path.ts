c15c7da1889392547da2cd7f697a432638faf5e4
Alan Agius <alan.agius4@gmail.com>
2019-01-10 21:07:22 +0100

    fixup! fix(ivy): normalize summary and factory shim files paths

--- packages/compiler-cli/src/ngtsc/util/src/path.ts
+++ packages/compiler-cli/src/ngtsc/util/src/path.ts
@@ -28,5 +28,6 @@ export function relativePathBetween(from: string, to: string): string|null {
 }
 
 export function normalizeSeparators(path: string): string {
+  // TODO: normalize path only for OS that need it.
   return path.replace(/\\/g, '/');
 }
