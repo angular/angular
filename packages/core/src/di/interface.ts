2e44e3cfe3d84148518d8f04b0f099d3b3d9527b
Miško Hevery <misko@hevery.com>
2019-01-11 15:06:30 -0800

    fixup! refactor(ivy): create Injector interface; remove dependency on Ivy

---
+++ packages/core/src/di/interface.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * This file should not be necessary because node resolution should just default to `./di/index`!
+ *
+ * However it does not seem to work and it breaks web tests.
+ */
+
+export * from './interface/index';
