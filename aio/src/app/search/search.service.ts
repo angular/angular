0c3ac24cf52b6fdd491144164646b68a02ac77e5
Igor Minar <igor@angular.io>
2019-01-10 22:07:49 -0800

    fixup! build: bump year

--- aio/src/app/search/search.service.ts
+++ aio/src/app/search/search.service.ts
@@ -1,5 +1,5 @@
 /*
-Copyright 2016-2019 Google LLC. All Rights Reserved.
+Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
 */
