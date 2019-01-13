1108da3313bf4b0725f3c6821ed30a1a654d1222
Yinyin L <yinyinl@gmail.com>
2019-01-13 23:44:13 +0800

    fix(router): explicitly use pathname of host page to make external URL
    Makes HashLocationStrategy to generate external URL with pathname of
    host page (index.html)'s location. This PR should resolve the deploying
    host page and rest static files separately issue mentioned in
    angular#13482 and angular#20033.
    
    Modified HashLocationStrategy will build external URL by concating:
    host-page's-pathname, '#', APP_BASE_HREF (if given) and UI-path.
    
    The HashLocationStrategy used to push only hash part (eg: #/ui/path) to
    history API. This will make browser change the host page part of URL in
    address bar to path pointed by base-href instead of path of host page.
    
    With this patch, the HashLocationStrategy will push both path and hash
    part to history API. In this way the browser will be able to keep the host
    page part of URL in address bar to the path of host page.

--- packages/common/src/location/hash_location_strategy.ts
+++ packages/common/src/location/hash_location_strategy.ts
@@ -35,14 +35,15 @@ import {LocationChangeListener, PlatformLocation} from './platform_location';
  */
 @Injectable()
 export class HashLocationStrategy extends LocationStrategy {
-  private _baseHref: string = '';
+  private _baseHref: string;
+  private _hashFragmentPrefix: string;
   constructor(
       private _platformLocation: PlatformLocation,
       @Optional() @Inject(APP_BASE_HREF) _baseHref?: string) {
     super();
-    if (_baseHref != null) {
-      this._baseHref = _baseHref;
-    }
+    const fragmentPrefix = Location.stripTrailingSlash(_baseHref || '');
+    this._baseHref = _platformLocation.pathname;
+    this._hashFragmentPrefix = '#' + fragmentPrefix;
   }
 
   onPopState(fn: LocationChangeListener): void {
@@ -50,20 +51,28 @@ export class HashLocationStrategy extends LocationStrategy {
     this._platformLocation.onHashChange(fn);
   }
 
-  getBaseHref(): string { return this._baseHref; }
+  getBaseHref(): string { return this._baseHref + this._hashFragmentPrefix; }
 
   path(includeHash: boolean = false): string {
     // the hash value is always prefixed with a `#`
     // and if it is empty then it will stay empty
     let path = this._platformLocation.hash;
-    if (path == null) path = '#';
-
+    if (path == null) {
+      path = this._hashFragmentPrefix;
+    }
+    if (path.startsWith(this._hashFragmentPrefix)) {
+      return path.substring(this._hashFragmentPrefix.length);
+    }
     return path.length > 0 ? path.substring(1) : path;
   }
 
   prepareExternalUrl(internal: string): string {
-    const url = Location.joinWithSlash(this._baseHref, internal);
-    return url.length > 0 ? ('#' + url) : url;
+    if (internal.length === 0) {
+      return this._baseHref + this._hashFragmentPrefix + '/';
+    }
+    const mark =
+        internal.startsWith('/') ? this._hashFragmentPrefix : (this._hashFragmentPrefix + '/');
+    return this._baseHref + mark + internal;
   }
 
   pushState(state: any, title: string, path: string, queryParams: string) {
