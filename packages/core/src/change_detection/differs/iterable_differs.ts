c5d240457d3acea1a10e0e5be554e8dc9871baaa
Judy Bogart <jbogart@gmail.com>
2019-01-16 14:40:03 -0800

    docs: add doc for TrackByFunction (#27377)
    PR Close #27377

--- packages/core/src/change_detection/differs/iterable_differs.ts
+++ packages/core/src/change_detection/differs/iterable_differs.ts
@@ -119,10 +119,8 @@ export interface CollectionChangeRecord<V> extends IterableChangeRecord<V> {}
 /**
  * An optional function passed into the `NgForOf` directive that defines how to track
  * changes for items in an iterable.
+ * The function takes the iteration index and item ID.
  * When supplied, Angular tracks changes by the return value of the function.
- * @param index The iteration index.
- * @param item The item ID.
- * @return The value to use for tracking changes.
  *
  * @publicApi
  */
