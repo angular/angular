403aff2415cdb0ab09aa3000c70ea02119180d3e
Keen Yee Liau <kyliau@google.com>
2019-01-17 11:35:06 -0800

    fix(bazel): Bazel-workspace schematics should run in ScopedTree
    Users should be able to add Bazel workspace to an existing project.
    The current approach assumes that the schematics is working on the same
    tree as that of ng-new, which includes the top-level directory. Instead,
    the schematic should work on the tree rooted at `appRoot` to enable
    Bazel files to be added to existing project.
    
    This change uses the newly implemented ScopedTree
    https://github.com/angular/angular-cli/commit/a0ac4b0e3dd60c75e1edafd6bb8cced47d10a8d3
    to achieve this.
    
    NOTE: The version of `@angular-devkit/schematics` that is installed is
    used to run the `@angular/bazel` schematic. Even if a different version
    is used in the schematic itself, it has no effect.
    Therefore, the *latest* Angular CLI should be used to generate the
    files. As of this commit, the latest version is @angular/cli@7.3.0-rc.0

--- packages/bazel/src/schematics/bazel-workspace/index_spec.ts
+++ packages/bazel/src/schematics/bazel-workspace/index_spec.ts
@@ -21,24 +21,24 @@ describe('Bazel-workspace Schematic', () => {
     const options = {...defaultOptions};
     const host = schematicRunner.runSchematic('bazel-workspace', options);
     const files = host.files;
-    expect(files).toContain('/demo/.bazelignore');
-    expect(files).toContain('/demo/.bazelrc');
-    expect(files).toContain('/demo/BUILD.bazel');
-    expect(files).toContain('/demo/src/BUILD.bazel');
-    expect(files).toContain('/demo/WORKSPACE');
-    expect(files).toContain('/demo/yarn.lock');
+    expect(files).toContain('/.bazelignore');
+    expect(files).toContain('/.bazelrc');
+    expect(files).toContain('/BUILD.bazel');
+    expect(files).toContain('/src/BUILD.bazel');
+    expect(files).toContain('/WORKSPACE');
+    expect(files).toContain('/yarn.lock');
   });
 
   it('should find existing Angular version', () => {
     let host = new UnitTestTree(new HostTree);
-    host.create('/demo/node_modules/@angular/core/package.json', JSON.stringify({
+    host.create('/node_modules/@angular/core/package.json', JSON.stringify({
       name: '@angular/core',
       version: '6.6.6',
     }));
     const options = {...defaultOptions};
     host = schematicRunner.runSchematic('bazel-workspace', options, host);
-    expect(host.files).toContain('/demo/WORKSPACE');
-    const workspace = host.readContent('/demo/WORKSPACE');
+    expect(host.files).toContain('/WORKSPACE');
+    const workspace = host.readContent('/WORKSPACE');
     expect(workspace).toMatch('ANGULAR_VERSION = "6.6.6"');
   });
 
@@ -46,19 +46,19 @@ describe('Bazel-workspace Schematic', () => {
     const options = {...defaultOptions, name: 'demo-app'};
     const host = schematicRunner.runSchematic('bazel-workspace', options);
     const {files} = host;
-    expect(files).toContain('/demo-app/src/BUILD.bazel');
-    const content = host.readContent('/demo-app/src/BUILD.bazel');
+    expect(files).toContain('/src/BUILD.bazel');
+    const content = host.readContent('/src/BUILD.bazel');
     expect(content).toContain('entry_module = "demo_app/src/main.dev"');
   });
 
   it('should add router if project contains routing module', () => {
     let host = new UnitTestTree(new HostTree);
-    host.create('/demo/src/app/app-routing.module.ts', '');
-    expect(host.files).toContain('/demo/src/app/app-routing.module.ts');
+    host.create('/src/app/app-routing.module.ts', '');
+    expect(host.files).toContain('/src/app/app-routing.module.ts');
     const options = {...defaultOptions};
     host = schematicRunner.runSchematic('bazel-workspace', options, host);
-    expect(host.files).toContain('/demo/src/BUILD.bazel');
-    const content = host.readContent('/demo/src/BUILD.bazel');
+    expect(host.files).toContain('/src/BUILD.bazel');
+    const content = host.readContent('/src/BUILD.bazel');
     expect(content).toContain('@angular//packages/router');
   });
 
@@ -66,16 +66,16 @@ describe('Bazel-workspace Schematic', () => {
     it('should contain project name', () => {
       const options = {...defaultOptions};
       const host = schematicRunner.runSchematic('bazel-workspace', options);
-      expect(host.files).toContain('/demo/WORKSPACE');
-      const content = host.readContent('/demo/WORKSPACE');
+      expect(host.files).toContain('/WORKSPACE');
+      const content = host.readContent('/WORKSPACE');
       expect(content).toContain('workspace(name = "demo")');
     });
 
     it('should convert dashes in name to underscore', () => {
       const options = {...defaultOptions, name: 'demo-project'};
       const host = schematicRunner.runSchematic('bazel-workspace', options);
-      expect(host.files).toContain('/demo-project/WORKSPACE');
-      const content = host.readContent('/demo-project/WORKSPACE');
+      expect(host.files).toContain('/WORKSPACE');
+      const content = host.readContent('/WORKSPACE');
       expect(content).toContain('workspace(name = "demo_project"');
     });
   });
@@ -83,35 +83,35 @@ describe('Bazel-workspace Schematic', () => {
   describe('SASS', () => {
     let host = new UnitTestTree(new HostTree);
     beforeAll(() => {
-      host.create('/demo/src/app/app.component.scss', '');
-      expect(host.files).toContain('/demo/src/app/app.component.scss');
+      host.create('/src/app/app.component.scss', '');
+      expect(host.files).toContain('/src/app/app.component.scss');
       const options = {...defaultOptions};
       host = schematicRunner.runSchematic('bazel-workspace', options, host);
-      expect(host.files).toContain('/demo/WORKSPACE');
-      expect(host.files).toContain('/demo/src/BUILD.bazel');
+      expect(host.files).toContain('/WORKSPACE');
+      expect(host.files).toContain('/src/BUILD.bazel');
     });
 
     it('should download rules_sass in WORKSPACE', () => {
-      const content = host.readContent('/demo/WORKSPACE');
+      const content = host.readContent('/WORKSPACE');
       expect(content).toContain('RULES_SASS_VERSION');
       expect(content).toContain('io_bazel_rules_sass');
     });
 
     it('should load sass_repositories in WORKSPACE', () => {
-      const content = host.readContent('/demo/WORKSPACE');
+      const content = host.readContent('/WORKSPACE');
       expect(content).toContain(
           'load("@io_bazel_rules_sass//sass:sass_repositories.bzl", "sass_repositories")');
       expect(content).toContain('sass_repositories()');
     });
 
     it('should add sass_binary rules in src/BUILD', () => {
-      const content = host.readContent('/demo/src/BUILD.bazel');
+      const content = host.readContent('/src/BUILD.bazel');
       expect(content).toContain('load("@io_bazel_rules_sass//:defs.bzl", "sass_binary")');
       expect(content).toMatch(/sass_binary\((.*\n)+\)/);
     });
 
     it('should add SASS targets to assets of ng_module in src/BUILD', () => {
-      const content = host.readContent('/demo/src/BUILD.bazel');
+      const content = host.readContent('/src/BUILD.bazel');
       expect(content).toContain(`
     assets = glob([
       "**/*.css",
