5c033c42c8a74c1b374435dac6a0c14993316d1f
Sander Elias <sanderelias+git@gmail.com>
2019-01-20 09:25:15 +0100

    fix(ivy): add missging getDirectives export
    fix(ivy): expose missing getDirectives from dicovery_utils

--- packages/core/src/core_render3_private_export.ts
+++ packages/core/src/core_render3_private_export.ts
@@ -74,6 +74,7 @@ export {
   pureFunction8 as ɵpureFunction8,
   pureFunctionV as ɵpureFunctionV,
   getCurrentView as ɵgetCurrentView,
+  getDirectives as ɵgetDirectives,
   getHostElement as ɵgetHostElement,
   restoreView as ɵrestoreView,
   containerRefreshStart as ɵcontainerRefreshStart,
