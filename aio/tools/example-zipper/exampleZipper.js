408bf819bb03fce5d3974be5537a393c8e4e1d25
Judy Bogart <jbogart@gmail.com>
2019-01-24 08:45:34 -0800

    docs: add schematics guide

--- aio/tools/example-zipper/exampleZipper.js
+++ aio/tools/example-zipper/exampleZipper.js
@@ -102,7 +102,9 @@ class ExampleZipper {
       'src/typings.d.ts',
       'src/environments/**/*',
       'src/tsconfig.*',
-      'src/tslint.*'
+      'src/tslint.*',
+      // Only ignore root package.json
+      '!package.json'
     ];
     var alwaysExcludes = [
       '!**/bs-config.e2e.json',
@@ -110,7 +112,6 @@ class ExampleZipper {
       '!**/*zipper.*',
       '!**/systemjs.config.js',
       '!**/npm-debug.log',
-      '!**/package.json',
       '!**/example-config.json',
       '!**/wallaby.js',
       // AoT related files
