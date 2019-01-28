626546a4113ac3d584a6e8443f755177b08ef105
Filipe Silva <filipematossilva@gmail.com>
2019-01-28 14:54:34 +0000

    build: rename cli-hello-world-ivy test to cli-hello-world-ivy-compat
    There are two ways to bootstrap an Ivy app: with `platformBrowserDynamic().bootstrapModule` and `renderComponent`.
    
    To distinguish between these two approaches we call the `platformBrowserDynamic().bootstrapModule` way `ivy-compat` and the `renderComponent` way just `ivy`.

--- integration/cli-hello-world-ivy-compat/src/app/app.component.ts
+++ integration/cli-hello-world-ivy-compat/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cli-hello-world-ivy';
+  title = 'cli-hello-world-ivy-compat';
 }
