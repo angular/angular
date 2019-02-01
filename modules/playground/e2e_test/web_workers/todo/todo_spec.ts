37d0b95987bef6c79a23f6e99fd6c66512570091
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-02-01 14:55:11 +0100

    build: disable failing ivy playground example e2e tests (#28490)
    With ed1ba88ffd9d0fc266808413fa517e7a31943bc8 we switched the
    examples to run with Bazel. This means that we can now also run the
    e2e tests for these examples against Ivy. All playground e2e tests,
    **except** the `web_worker` examples, successfully run with Ivy.
    
    The failing webworker e2e tests have been temporarily disabled with
    `fixmeIvy` and need to be investigated in a follow-up.
    
    PR Close #28490

--- modules/playground/e2e_test/web_workers/todo/todo_spec.ts
+++ modules/playground/e2e_test/web_workers/todo/todo_spec.ts
@@ -6,27 +6,30 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {verifyNoBrowserErrors} from 'e2e_util/e2e_util';
+import {fixmeIvy} from '@angular/private/testing';
 import {browser, by, element, protractor} from 'protractor';
 
-describe('WebWorkers Todo', function() {
-  afterEach(() => {
-    verifyNoBrowserErrors();
-    browser.ignoreSynchronization = false;
-  });
+import {verifyNoBrowserErrors} from '../../../../e2e_util/e2e_util';
 
-  const URL = 'all/playground/src/web_workers/todo/index.html';
+fixmeIvy('NullInjectorError: No provider for InjectionToken ROOT_CONTEXT_TOKEN!')
+    .describe('WebWorkers Todo', function() {
+      afterEach(() => {
+        verifyNoBrowserErrors();
+        browser.ignoreSynchronization = false;
+      });
 
-  it('should bootstrap', () => {
-    // This test can't wait for Angular as Testability is not available when using WebWorker
-    browser.ignoreSynchronization = true;
-    browser.get(URL);
+      const URL = '/';
 
-    waitForBootstrap();
-    expect(element(by.css('#todoapp header')).getText()).toEqual('todos');
-  });
+      it('should bootstrap', () => {
+        // This test can't wait for Angular as Testability is not available when using WebWorker
+        browser.ignoreSynchronization = true;
+        browser.get(URL);
 
-});
+        waitForBootstrap();
+        expect(element(by.css('#todoapp header')).getText()).toEqual('todos');
+      });
+
+    });
 
 function waitForBootstrap(): void {
   browser.wait(protractor.until.elementLocated(by.css('todo-app #todoapp')), 15000);
