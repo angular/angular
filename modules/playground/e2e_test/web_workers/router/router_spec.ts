cd143f67af444f63b169e24af24bcdb315065f73
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-02-01 14:55:11 +0100

    build: disable failing ivy playground example e2e tests (#28697)
    With ed1ba88ffd9d0fc266808413fa517e7a31943bc8 we switched the
    examples to run with Bazel. This means that we can now also run the
    e2e tests for these examples against Ivy. All playground e2e tests,
    **except** the `web_worker` examples, successfully run with Ivy.
    
    The failing webworker e2e tests have been temporarily disabled with
    `fixmeIvy` and need to be investigated in a follow-up.
    
    PR Close #28697

--- modules/playground/e2e_test/web_workers/router/router_spec.ts
+++ modules/playground/e2e_test/web_workers/router/router_spec.ts
@@ -6,79 +6,82 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {verifyNoBrowserErrors} from 'e2e_util/e2e_util';
+import {fixmeIvy} from '@angular/private/testing';
 import {browser, by, element, protractor} from 'protractor';
 
-describe('WebWorker Router', () => {
-  beforeEach(() => {
-    // This test can't wait for Angular as Testability is not available when using WebWorker
-    browser.ignoreSynchronization = true;
-    browser.get('/');
-  });
+import {verifyNoBrowserErrors} from '../../../../e2e_util/e2e_util';
 
-  afterEach(() => {
-    verifyNoBrowserErrors();
-    browser.ignoreSynchronization = false;
-  });
+fixmeIvy('NullInjectorError: No provider for InjectionToken ROOT_CONTEXT_TOKEN!')
+    .describe('WebWorker Router', () => {
+      beforeEach(() => {
+        // This test can't wait for Angular as Testability is not available when using WebWorker
+        browser.ignoreSynchronization = true;
+        browser.get('/');
+      });
 
-  const contentSelector = 'app main h1';
-  const navSelector = 'app nav ul';
-  const baseUrl = 'all/playground/src/web_workers/router/index.html';
+      afterEach(() => {
+        verifyNoBrowserErrors();
+        browser.ignoreSynchronization = false;
+      });
 
-  it('should route on click', () => {
-    browser.get(baseUrl);
+      const contentSelector = 'app main h1';
+      const navSelector = 'app nav ul';
+      const baseUrl = '/';
 
-    waitForElement(contentSelector);
-    let content = element(by.css(contentSelector));
-    expect(content.getText()).toEqual('Start');
+      it('should route on click', () => {
+        browser.get(baseUrl);
 
-    const aboutBtn = element(by.css(navSelector + ' .about'));
-    aboutBtn.click();
-    waitForUrl(/\/about/);
-    waitForElement(contentSelector);
-    waitForElementText(contentSelector, 'About');
-    content = element(by.css(contentSelector));
-    expect(content.getText()).toEqual('About');
-    expect(browser.getCurrentUrl()).toMatch(/\/about/);
+        waitForElement(contentSelector);
+        let content = element(by.css(contentSelector));
+        expect(content.getText()).toEqual('Start');
 
-    const contactBtn = element(by.css(navSelector + ' .contact'));
-    contactBtn.click();
-    waitForUrl(/\/contact/);
-    waitForElement(contentSelector);
-    waitForElementText(contentSelector, 'Contact');
-    content = element(by.css(contentSelector));
-    expect(content.getText()).toEqual('Contact');
-    expect(browser.getCurrentUrl()).toMatch(/\/contact/);
-  });
+        const aboutBtn = element(by.css(navSelector + ' .about'));
+        aboutBtn.click();
+        waitForUrl(/\/about/);
+        waitForElement(contentSelector);
+        waitForElementText(contentSelector, 'About');
+        content = element(by.css(contentSelector));
+        expect(content.getText()).toEqual('About');
+        expect(browser.getCurrentUrl()).toMatch(/\/about/);
 
-  it('should load the correct route from the URL', () => {
-    browser.get(baseUrl + '#/about');
+        const contactBtn = element(by.css(navSelector + ' .contact'));
+        contactBtn.click();
+        waitForUrl(/\/contact/);
+        waitForElement(contentSelector);
+        waitForElementText(contentSelector, 'Contact');
+        content = element(by.css(contentSelector));
+        expect(content.getText()).toEqual('Contact');
+        expect(browser.getCurrentUrl()).toMatch(/\/contact/);
+      });
 
-    waitForElement(contentSelector);
-    waitForElementText(contentSelector, 'About');
-    const content = element(by.css(contentSelector));
-    expect(content.getText()).toEqual('About');
-  });
+      it('should load the correct route from the URL', () => {
+        browser.get(baseUrl + '#/about');
 
-  function waitForElement(selector: string): void {
-    browser.wait(protractor.until.elementLocated(by.css(selector)), 15000);
-  }
+        waitForElement(contentSelector);
+        waitForElementText(contentSelector, 'About');
+        const content = element(by.css(contentSelector));
+        expect(content.getText()).toEqual('About');
+      });
 
-  function waitForElementText(contentSelector: string, expected: string): void {
-    browser.wait(() => {
-      const deferred = protractor.promise.defer();
-      const elem = element(by.css(contentSelector));
-      elem.getText().then((text: string) => { return deferred.fulfill(text === expected); });
-      return deferred.promise;
-    }, 5000);
-  }
+      function waitForElement(selector: string): void {
+        browser.wait(protractor.until.elementLocated(by.css(selector)), 15000);
+      }
 
-  function waitForUrl(regex: RegExp): void {
-    browser.wait(() => {
-      const deferred = protractor.promise.defer();
-      browser.getCurrentUrl().then(
-          (url: string) => { return deferred.fulfill(url.match(regex) !== null); });
-      return deferred.promise;
-    }, 5000);
-  }
-});
+      function waitForElementText(contentSelector: string, expected: string): void {
+        browser.wait(() => {
+          const deferred = protractor.promise.defer();
+          const elem = element(by.css(contentSelector));
+          elem.getText().then((text: string) => { return deferred.fulfill(text === expected); });
+          return deferred.promise;
+        }, 5000);
+      }
+
+      function waitForUrl(regex: RegExp): void {
+        browser.wait(() => {
+          const deferred = protractor.promise.defer();
+          browser.getCurrentUrl().then(
+              (url: string) => { return deferred.fulfill(url.match(regex) !== null); });
+          return deferred.promise;
+        }, 5000);
+      }
+    });
