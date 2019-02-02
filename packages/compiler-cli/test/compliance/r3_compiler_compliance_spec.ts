90df445d3adbed13816eb5f3a04863961e839c82
Andrew Kushnir <akushnir@google.com>
2019-02-02 11:20:33 -0800

    refactor(ivy): combine contentQueries and contentQueriesRefresh functions (#28503)
    Prior to this update we had separate contentQueries and contentQueriesRefresh functions to handle creation and update phases. This approach was inconsistent with View Queries, Host Bindings and Template functions that we generate for Component/Directive defs. Now the mentioned 2 functions are combines into one (contentQueries), creation and update logic is separated with RenderFlags (similar to what we have in other generated functions).
    
    PR Close #28503

--- packages/compiler-cli/test/compliance/r3_compiler_compliance_spec.ts
+++ packages/compiler-cli/test/compliance/r3_compiler_compliance_spec.ts
@@ -1553,15 +1553,16 @@ describe('compiler compliance', () => {
             factory: function ContentQueryComponent_Factory(t) {
               return new (t || ContentQueryComponent)();
             },
-            contentQueries: function ContentQueryComponent_ContentQueries(dirIndex) {
+            contentQueries: function ContentQueryComponent_ContentQueries(rf, ctx, dirIndex) {
+              if (rf & 1) {
               $r3$.ɵcontentQuery(dirIndex, SomeDirective, true);
               $r3$.ɵcontentQuery(dirIndex, SomeDirective, false);
-            },
-            contentQueriesRefresh: function ContentQueryComponent_ContentQueriesRefresh(dirIndex) {
-              const instance = $r3$.ɵload(dirIndex);
+              }
+              if (rf & 2) {
               var $tmp$;
-              ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && ($instance$.someDir = $tmp$.first));
-              ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && ($instance$.someDirList = $tmp$));
+                ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (ctx.someDir = $tmp$.first));
+                ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (ctx.someDirList = $tmp$));
+              }
             },
             ngContentSelectors: _c0,
             consts: 2,
@@ -1612,15 +1613,16 @@ describe('compiler compliance', () => {
           …
           ContentQueryComponent.ngComponentDef = $r3$.ɵdefineComponent({
             …
-            contentQueries: function ContentQueryComponent_ContentQueries(dirIndex) {
+            contentQueries: function ContentQueryComponent_ContentQueries(rf, ctx, dirIndex) {
+              if (rf & 1) {
               $r3$.ɵcontentQuery(dirIndex, $e0_attrs$, true);
               $r3$.ɵcontentQuery(dirIndex, $e1_attrs$, false);
-            },
-            contentQueriesRefresh: function ContentQueryComponent_ContentQueriesRefresh(dirIndex) {
-              const instance = $r3$.ɵload(dirIndex);
+              }
+              if (rf & 2) {
               var $tmp$;
-              ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (instance.myRef = $tmp$.first));
-              ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (instance.myRefs = $tmp$));
+                ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (ctx.myRef = $tmp$.first));
+                ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (ctx.myRefs = $tmp$));
+              }
             },
             …
           });`;
@@ -1665,19 +1667,20 @@ describe('compiler compliance', () => {
           …
           ContentQueryComponent.ngComponentDef = $r3$.ɵdefineComponent({
             …
-            contentQueries: function ContentQueryComponent_ContentQueries(dirIndex) {
-              $r3$.ɵcontentQuery(dirIndex, $e0_attrs$ , true, TemplateRef);
+            contentQueries: function ContentQueryComponent_ContentQueries(rf, ctx, dirIndex) {
+              if (rf & 1) {
+                $r3$.ɵcontentQuery(dirIndex, $e0_attrs$, true, TemplateRef);
               $r3$.ɵcontentQuery(dirIndex, SomeDirective, true, ElementRef);
               $r3$.ɵcontentQuery(dirIndex, $e1_attrs$, false, ElementRef);
               $r3$.ɵcontentQuery(dirIndex, SomeDirective, false, TemplateRef);
-            },
-            contentQueriesRefresh: function ContentQueryComponent_ContentQueriesRefresh(dirIndex) {
-              const instance = $r3$.ɵload(dirIndex);
+              }
+              if (rf & 2) {
               var $tmp$;
-              ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (instance.myRef = $tmp$.first));
-              ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (instance.someDir = $tmp$.first));
-              ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (instance.myRefs = $tmp$));
-              ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (instance.someDirs = $tmp$));
+                ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (ctx.myRef = $tmp$.first));
+                ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (ctx.someDir = $tmp$.first));
+                ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (ctx.myRefs = $tmp$));
+                ($r3$.ɵqueryRefresh(($tmp$ = $r3$.ɵloadContentQuery())) && (ctx.someDirs = $tmp$));
+              }
             },
             …
           });`;
