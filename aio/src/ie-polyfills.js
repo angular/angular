930e22b45af58cd1a7921df2030d96b351b8fc66
WilliamKoza <william.koza@gmail.com>
2019-02-04 15:17:42 +0100

    fix(docs-infra): remove polyfill web-animations since we use Angular > 6 and we don't use `AnimationBuilder`

--- aio/src/ie-polyfills.js
+++ aio/src/ie-polyfills.js
@@ -16,6 +16,3 @@ import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js';
-
-/** IE10 and IE11 requires the following to support `@angular/animation`. */
-import 'web-animations-js';
