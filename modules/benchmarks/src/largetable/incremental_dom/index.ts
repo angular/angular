b07346c8e37156ebd04a21e7a335cb802c88a06a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-02-11 17:55:37 +0100

    build: serve largetable benchmark tests with bazel (#28697)
    PR Close #28697

--- modules/benchmarks/src/largetable/incremental_dom/index.ts
+++ modules/benchmarks/src/largetable/incremental_dom/index.ts
@@ -10,24 +10,26 @@ import {bindAction, profile} from '../../util';
 import {buildTable, emptyTable} from '../util';
 import {TableComponent} from './table';
 
-export function main() {
-  let table: TableComponent;
+let table: TableComponent;
 
-  function destroyDom() { table.data = emptyTable; }
-
-  function createDom() { table.data = buildTable(); }
-
-  function noop() {}
-
-  function init() {
-    table = new TableComponent(document.querySelector('largetable'));
-
-    bindAction('#destroyDom', destroyDom);
-    bindAction('#createDom', createDom);
-
-    bindAction('#updateDomProfile', profile(createDom, noop, 'update'));
-    bindAction('#createDomProfile', profile(createDom, destroyDom, 'create'));
-  }
-
-  init();
+function destroyDom() {
+  table.data = emptyTable;
 }
+
+function createDom() {
+  table.data = buildTable();
+}
+
+function noop() {}
+
+function init() {
+  table = new TableComponent(document.querySelector('largetable'));
+
+  bindAction('#destroyDom', destroyDom);
+  bindAction('#createDom', createDom);
+
+  bindAction('#updateDomProfile', profile(createDom, noop, 'update'));
+  bindAction('#createDomProfile', profile(createDom, destroyDom, 'create'));
+}
+
+init();
