df9b3c55fa4d2d8d2aa412b9d6578ac27fa9ff4f
Kapunahele Wong <kapunahelewong@gmail.com>
2019-02-13 12:50:30 -0500

    docs: clarify hierarchical injectors (#28700)
    PR Close #28700

---
+++ aio/content/examples/providers-viewproviders/src/app/animal.service.ts
@@ -0,0 +1,10 @@
+// #docregion animal-service
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimalService {
+  emoji = '🐳';
+}
+// #enddocregion animal-service
