01337cfe117a1a4fbacab9b68378b6313cc444e9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-02-14 23:16:07 +0100

    test: disable failing ivy example e2e tests
    *Note for patch branch:* We had to disable more examples in Ivy because
    the patch branch does not contain all Ivy/ngtsc fixes.

--- packages/examples/common/ngComponentOutlet/ts/e2e_test/ngComponentOutlet_spec.ts
+++ packages/examples/common/ngComponentOutlet/ts/e2e_test/ngComponentOutlet_spec.ts
@@ -6,6 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {fixmeIvy} from '@angular/private/testing';
 import {$, ExpectedConditions, browser, by, element} from 'protractor';
 
 import {verifyNoBrowserErrors} from '../../../../test-utils';
@@ -16,29 +17,34 @@ function waitForElement(selector: string) {
   browser.wait(EC.presenceOf($(selector)), 20000);
 }
 
-describe('ngComponentOutlet', () => {
-  const URL = 'common/ngComponentOutlet/ts/';
-  afterEach(verifyNoBrowserErrors);
+fixmeIvy(
+    'unknown. Run "yarn bazel run packages/examples/common:devserver --define=compile=aot" ' +
+    'to debug')
+    .describe('ngComponentOutlet', () => {
+      const URL = '/ngComponentOutlet';
+      afterEach(verifyNoBrowserErrors);
 
-  describe('ng-component-outlet-example', () => {
-    it('should render simple', () => {
-      browser.get(URL);
-      waitForElement('ng-component-outlet-simple-example');
-      expect(element.all(by.css('hello-world')).getText()).toEqual(['Hello World!']);
-    });
+      describe('ng-component-outlet-example', () => {
+        it('should render simple', () => {
+          browser.get(URL);
+          waitForElement('ng-component-outlet-simple-example');
+          expect(element.all(by.css('hello-world')).getText()).toEqual(['Hello World!']);
+        });
 
-    it('should render complete', () => {
-      browser.get(URL);
-      waitForElement('ng-component-outlet-complete-example');
-      expect(element.all(by.css('complete-component')).getText()).toEqual(['Complete: AhojSvet!']);
-    });
+        it('should render complete', () => {
+          browser.get(URL);
+          waitForElement('ng-component-outlet-complete-example');
+          expect(element.all(by.css('complete-component')).getText()).toEqual([
+            'Complete: AhojSvet!'
+          ]);
+        });
 
-    it('should render other module', () => {
-      browser.get(URL);
-      waitForElement('ng-component-outlet-other-module-example');
-      expect(element.all(by.css('other-module-component')).getText()).toEqual([
-        'Other Module Component!'
-      ]);
+        it('should render other module', () => {
+          browser.get(URL);
+          waitForElement('ng-component-outlet-other-module-example');
+          expect(element.all(by.css('other-module-component')).getText()).toEqual([
+            'Other Module Component!'
+          ]);
+        });
+      });
     });
-  });
-});
--- packages/examples/forms/ts/simpleNgModel/e2e_test/simple_ng_model_spec.ts
+++ packages/examples/forms/ts/simpleNgModel/e2e_test/simple_ng_model_spec.ts
@@ -6,40 +6,45 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {fixmeIvy} from '@angular/private/testing';
 import {ElementArrayFinder, ElementFinder, browser, by, element} from 'protractor';
-import {verifyNoBrowserErrors} from '../../../../_common/e2e_util';
 
-describe('simpleNgModel example', () => {
-  afterEach(verifyNoBrowserErrors);
-  let input: ElementFinder;
-  let paragraphs: ElementArrayFinder;
-  let button: ElementFinder;
+import {verifyNoBrowserErrors} from '../../../../test-utils';
 
-  beforeEach(() => {
-    browser.get('/forms/ts/simpleNgModel/index.html');
-    input = element(by.css('input'));
-    paragraphs = element.all(by.css('p'));
-    button = element(by.css('button'));
-  });
+fixmeIvy(
+    'unkown; verifyNoBrowserErrors fails due to "ExpressionChangedAfterItHasBeenCheckedError"' +
+    'Debug by running "yarn bazel run //packages/examples/forms:devserver --define=compile=aot')
+    .describe('simpleNgModel example', () => {
+      afterEach(verifyNoBrowserErrors);
+      let input: ElementFinder;
+      let paragraphs: ElementArrayFinder;
+      let button: ElementFinder;
 
-  it('should update the domain model as you type', () => {
-    input.click();
-    input.sendKeys('Carson');
+      beforeEach(() => {
+        browser.get('/simpleNgModel');
+        input = element(by.css('input'));
+        paragraphs = element.all(by.css('p'));
+        button = element(by.css('button'));
+      });
 
-    expect(paragraphs.get(0).getText()).toEqual('Value: Carson');
-  });
+      it('should update the domain model as you type', () => {
+        input.click();
+        input.sendKeys('Carson');
 
-  it('should report the validity correctly', () => {
-    expect(paragraphs.get(1).getText()).toEqual('Valid: false');
-    input.click();
-    input.sendKeys('a');
+        expect(paragraphs.get(0).getText()).toEqual('Value: Carson');
+      });
 
-    expect(paragraphs.get(1).getText()).toEqual('Valid: true');
-  });
+      it('should report the validity correctly', () => {
+        expect(paragraphs.get(1).getText()).toEqual('Valid: false');
+        input.click();
+        input.sendKeys('a');
 
-  it('should set the value by changing the domain model', () => {
-    button.click();
-    expect(input.getAttribute('value')).toEqual('Nancy');
-  });
+        expect(paragraphs.get(1).getText()).toEqual('Valid: true');
+      });
 
-});
+      it('should set the value by changing the domain model', () => {
+        button.click();
+        expect(input.getAttribute('value')).toEqual('Nancy');
+      });
+
+    });
