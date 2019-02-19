efcb6f5e54d5d810edf11e5d247eb0d61f06ec8e
Brandon <robertsbt@gmail.com>
2019-02-19 16:00:03 +0000

    docs: backport fix to event-binding example for production build
    Examples are run in production mode and the methods weren't optional.

--- aio/content/examples/event-binding/src/app/app.component.ts
+++ aio/content/examples/event-binding/src/app/app.component.ts
@@ -11,17 +11,17 @@ export class AppComponent {
   currentItem = { name: 'teapot'} ;
   clickMessage = '';
 
-  onSave(event: KeyboardEvent) {
+  onSave(event?: KeyboardEvent) {
     const evtMsg = event ? ' Event target is ' + (<HTMLElement>event.target).textContent : '';
     alert('Saved.' + evtMsg);
     if (event) { event.stopPropagation(); }
   }
 
-  deleteItem(item: Item) {
+  deleteItem(item?: Item) {
     alert(`Delete the ${item}.`);
   }
 
-  onClickMe(event: KeyboardEvent) {
+  onClickMe(event?: KeyboardEvent) {
     const evtMsg = event ? ' Event target class is ' + (<HTMLElement>event.target).className  : '';
     alert('Click me.' + evtMsg);
   }
