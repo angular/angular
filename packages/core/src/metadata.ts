0e14dbcbe47fd316ab3895023d0b6a07f8e1d79a
Brandon <robertsbt@gmail.com>
2019-02-19 19:52:21 +0000

    docs(core): export the `...Decorator` interfaces to ensure they are documented (#28836)
    If an interface is not exported publicly from its package, then the doc-gen
    does not see it, and so cannot include it in the generated documentation.
    
    This was the case for a number of `...Decorator` interfaces, such as
    `PipeDecorator` and `InputDecorator.
    
    This commit adds these interfaces to the public export to fix this problem.
    
    PR Close #28836

--- packages/core/src/metadata.ts
+++ packages/core/src/metadata.ts
@@ -17,7 +17,7 @@ import {DoBootstrap, ModuleWithProviders, NgModule, SchemaMetadata} from './meta
 import {ViewEncapsulation} from './metadata/view';
 
 export {ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, ContentChild, ContentChildDecorator, ContentChildren, ContentChildrenDecorator, Query, ViewChild, ViewChildDecorator, ViewChildren, ViewChildrenDecorator} from './metadata/di';
-export {Component, ComponentDecorator, Directive, DirectiveDecorator, HostBinding, HostListener, Input, Output, Pipe} from './metadata/directives';
+export {Component, ComponentDecorator, Directive, DirectiveDecorator, HostBinding, HostBindingDecorator, HostListener, HostListenerDecorator, Input, InputDecorator, Output, OutputDecorator, Pipe, PipeDecorator} from './metadata/directives';
 export {AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, DoCheck, OnChanges, OnDestroy, OnInit} from './metadata/lifecycle_hooks';
 export {CUSTOM_ELEMENTS_SCHEMA, DoBootstrap, ModuleWithProviders, NO_ERRORS_SCHEMA, NgModule, SchemaMetadata} from './metadata/ng_module';
 export {ViewEncapsulation} from './metadata/view';
