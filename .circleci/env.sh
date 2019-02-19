9d0db2fe4b7bfebf6226fdb9365d5a591931d3ac
Alex Eagle <alexeagle@angular.io>
2019-02-19 13:15:42 -0800

    ci: Vendor yarn into our repo (#28839)
    This avoids a dynamic dependency on fetching a package from the internet in our CI.
    We have observed that this is not 100% reliable.
    
    PR Close #28839

--- .circleci/env.sh
+++ .circleci/env.sh
@@ -32,7 +32,6 @@ setPublicVar CI_COMMIT_RANGE "`[[ ${CIRCLE_PR_NUMBER:-false} != false ]] && echo
 setPublicVar CI_PULL_REQUEST "${CIRCLE_PR_NUMBER:-false}";
 setPublicVar CI_REPO_NAME "$CIRCLE_PROJECT_REPONAME";
 setPublicVar CI_REPO_OWNER "$CIRCLE_PROJECT_USERNAME";
-setPublicVar CI_YARN_VERSION "1.13.0";
 
 
 ####################################################################################################
@@ -61,6 +60,13 @@ setPublicVar SAUCE_TUNNEL_IDENTIFIER "angular-${CIRCLE_BUILD_NUM}-${CIRCLE_NODE_
 # Amount of seconds we wait for sauceconnect to establish a tunnel instance. In order to not
 # acquire CircleCI instances for too long if sauceconnect failed, we need a connect timeout.
 setPublicVar SAUCE_READY_FILE_TIMEOUT 120
+
+
+####################################################################################################
+# Define our own yarn command for CircleCI.
+####################################################################################################
+echo "function yarn () { /usr/bin/env node $projectDir/third_party/github.com/yarnpkg/yarn/releases/yarn-1.13.0.js \"\$@\"; }" >> $BASH_ENV;
+echo "export -f yarn;" >> $BASH_ENV;
 
 
 # Source `$BASH_ENV` to make the variables available immediately.
