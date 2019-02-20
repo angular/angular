172fedd588c496a02df78718b62d2685cf4fd0fb
Keen Yee Liau <kyliau@google.com>
2019-02-19 17:58:21 -0800

    refactor(bazel): Schematics should fetch angular from distro (#28851)
    PR Close #28851

--- packages/bazel/src/schematics/bazel-workspace/index.ts
+++ packages/bazel/src/schematics/bazel-workspace/index.ts
@@ -9,31 +9,11 @@
  */
 
 import {strings} from '@angular-devkit/core';
-import {Rule, SchematicContext, SchematicsException, Tree, apply, applyTemplates, mergeWith, move, url} from '@angular-devkit/schematics';
+import {Rule, SchematicContext, Tree, apply, applyTemplates, mergeWith, url} from '@angular-devkit/schematics';
 import {getWorkspace} from '@schematics/angular/utility/config';
-import {latestVersions} from '@schematics/angular/utility/latest-versions';
 import {validateProjectName} from '@schematics/angular/utility/validation';
 
 import {Schema as BazelWorkspaceOptions} from './schema';
-
-
-/**
- * Look for package.json file for package with `packageName` in node_modules and
- * extract its version.
- */
-function findVersion(packageName: string, host: Tree): string|null {
-  const candidate = `node_modules/${packageName}/package.json`;
-  if (host.exists(candidate)) {
-    try {
-      const packageJson = JSON.parse(host.read(candidate).toString());
-      if (packageJson.name === packageName && packageJson.version) {
-        return packageJson.version;
-      }
-    } catch {
-    }
-  }
-  return null;
-}
 
 /**
  * Clean the version string and return version in the form "1.2.3". Return
@@ -72,20 +52,6 @@ export default function(options: BazelWorkspaceOptions): Rule {
     }
     validateProjectName(name);
 
-    // If the project already has some deps installed, Bazel should use existing
-    // versions.
-    const existingVersions = {
-      Angular: findVersion('@angular/core', host),
-      RxJs: findVersion('rxjs', host),
-    };
-
-    Object.keys(existingVersions).forEach((name: 'Angular' | 'RxJs') => {
-      const version = existingVersions[name] as string;
-      if (version) {
-        context.logger.info(`Bazel will reuse existing version for ${name}: ${version}`);
-      }
-    });
-
     if (!host.exists('yarn.lock')) {
       host.create('yarn.lock', '');
     }
@@ -93,8 +59,6 @@ export default function(options: BazelWorkspaceOptions): Rule {
     const workspaceVersions = {
       'RULES_NODEJS_VERSION': '0.18.6',
       'RULES_NODEJS_SHA256': '1416d03823fed624b49a0abbd9979f7c63bbedfd37890ddecedd2fe25cccebc6',
-      'ANGULAR_VERSION': existingVersions.Angular || clean(latestVersions.Angular),
-      // TODO(kyliau): Consider moving this to latest-versions.ts
       'RULES_SASS_VERSION': '1.17.0',
     };
 
