cdc720bbcddbcc148c8308e76a096ba3c6f0ae96
Vani <psreep@gmail.com>
2019-02-20 20:44:44 -0800

    docs: fix lint error (#28507)
    PR Close #28507

--- packages/common/http/src/params.ts
+++ packages/common/http/src/params.ts
@@ -14,11 +14,11 @@
  * @publicApi
  **/
 export interface HttpParameterCodec {
-  encodeKey(key: string):string;
-  encodeValue(value: string):string;
+  encodeKey(key: string): string;
+  encodeValue(value: string): string;
 
-  decodeKey(key: string):string;
-  decodeValue(value: string):string;
+  decodeKey(key: string): string;
+  decodeValue(value: string): string;
 }
 
 /**
