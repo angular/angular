13081f713f183ec4a6c96d5d997dac5b6f85422c
Andrew Kushnir <akushnir@google.com>
2019-02-26 11:10:13 -0800

    fixup! fix(ivy): avoid duplicate i18n consts to be present in generated output

--- packages/compiler-cli/test/compliance/r3_view_compiler_i18n_spec.ts
+++ packages/compiler-cli/test/compliance/r3_view_compiler_i18n_spec.ts
@@ -24,6 +24,7 @@ const angularFiles = setup({
 
 const htmlParser = new HtmlParser();
 
+// TODO: update translation extraction RegExp to support i18nLocalize calls once #28689 lands.
 const EXTRACT_GENERATED_TRANSLATIONS_REGEXP =
     /const\s*(.*?)\s*=\s*goog\.getMsg\("(.*?)",?\s*(.*?)\)/g;
 
