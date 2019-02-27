dc643a1b7a488e65d6e2389be9b555f6968a4fc7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-02-27 19:57:28 +0100

    ci(docs-infra): disable failing ivy jit systemjs examples
    As a side effect of 09b34bae8655d4251516655c317b150c46cd3653,
    we fixed that the docs systemjs examples currently do not run
    with Ivy in JIT mode. This now uncovered new failures with the JIT
    resource loading. e.g.
    
    ```
    zone.js:665 Unhandled Promise rejection: Component 'PhoneListComponent' is not resolved:
     - templateUrl: ./phone-list.template.html
    Did you run and wait for 'resolveComponentResources()'? ; Zone: <root> ; Task: Promise.then ; Value: Error: Component 'PhoneListComponent' is not resolved:
     - templateUrl: ./phone-list.template.html
    Did you run and wait for 'resolveComponentResources()'?
        at Function.get (directive.ts:54)
        at getComponentDef (definition.ts:648)
        at verifyDeclarationsHaveDefinitions (module.ts:185)
        at Array.forEach (<anonymous>)
        at verifySemanticsOfNgModuleDef (module.ts:159)
        at Function.get (module.ts:132)
        at getInjectorDef (defs.ts:181)
        at R3Injector.processInjectorType (r3_injector.ts:230)
        at eval (r3_injector.ts:114)
        at eval (r3_injector.ts:451) Error: Component 'PhoneListComponent' is not resolved:
    ```
    
    We temporarily disable these two failing SystemJS examples by adding them to the
    `fixmeIvyExamples` list.

--- aio/tools/examples/run-example-e2e.js
+++ aio/tools/examples/run-example-e2e.js
@@ -24,7 +24,11 @@ const fixmeIvyExamples = [
   // fixmeIvy('FW-1069: ngtsc does not support inline <style> and <link>')
   'component-styles',
   // fixmeIvy('unknown') app fails at runtime due to missing external service (goog is undefined)
-  'i18n'
+  'i18n',
+  // fixmeIvy('unknown') JIT app fails with external resources not loaded.
+  'upgrade-phonecat-2-hybrid',
+  // fixmeIvy('unknown') JIT app fails with external resources not loaded.
+  'upgrade-phonecat-3-final',
 ];
 
 if (argv.ivy) {
