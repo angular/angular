654ba38bc88f39b57c81e13917fdc38c33762158
Andrew Kushnir <akushnir@google.com>
2019-03-04 12:40:06 -0800

    fixup! fix(ivy): wrap "inputs" and "outputs" keys if they contain unsafe characters

--- packages/compiler/src/render3/view/util.ts
+++ packages/compiler/src/render3/view/util.ts
@@ -17,6 +17,9 @@ import {isI18nAttribute} from './i18n/util';
  * Checks whether an object key contains potentially unsafe chars, thus the key should be wrapped in
  * quotes. Note: we do not wrap all keys into quotes, as it may have impact on minification and may
  * bot work in some cases when object keys are mangled by minifier.
+ *
+ * TODO(FW-1136): this is a temporary solution, we need to come up with a better way of working with
+ * inputs that contain potentially unsafe chars.
  */
 const UNSAFE_OBJECT_KEY_NAME_REGEXP = /-/g;
 
