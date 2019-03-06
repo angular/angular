f99febcdf927355dcafef22e82bf7b0f552e4ba1
George Kalpakas <kalpakas.g@gmail.com>
2019-03-06 12:17:17 +0200

    ci(docs-infra): fix deployment to Firebase
    This is a backport of f1a860fbf to 6.1.x.
    Related to #29029.

--- aio/scripts/deploy-to-firebase.sh
+++ aio/scripts/deploy-to-firebase.sh
@@ -100,8 +100,8 @@ fi
   yarn payload-size
 
   # Deploy to Firebase
-  firebase use "$projectId" --token "$firebaseToken"
-  firebase deploy --message "Commit: $TRAVIS_COMMIT" --non-interactive --token "$firebaseToken"
+  yarn firebase use "$projectId" --token "$firebaseToken"
+  yarn firebase deploy --message "Commit: $TRAVIS_COMMIT" --non-interactive --token "$firebaseToken"
 
   # Run PWA-score tests
   yarn test-pwa-score "$deployedUrl" "$AIO_MIN_PWA_SCORE"
