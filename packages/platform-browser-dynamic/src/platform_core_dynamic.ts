9e835b659e6997c69c00ca446510552101fe4521
WilliamKoza <william.koza@gmail.com>
2019-03-06 21:39:52 +0100

    refactor(platform-browser): clean up platform-browser-dynamic's imports

--- packages/platform-browser-dynamic/src/platform_core_dynamic.ts
+++ packages/platform-browser-dynamic/src/platform_core_dynamic.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {COMPILER_OPTIONS, CompilerFactory, PlatformRef, StaticProvider, createPlatformFactory, platformCore} from '@angular/core';
+import {COMPILER_OPTIONS, CompilerFactory, createPlatformFactory, platformCore} from '@angular/core';
 import {JitCompilerFactory} from './compiler_factory';
 
 /**
