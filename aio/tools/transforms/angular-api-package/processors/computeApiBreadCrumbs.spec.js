f43a50481573580ce231e4bde512ab34335fa580
Adam Plumer <caerus.karu@gmail.com>
2019-03-07 10:16:19 -0600

    fixup! docs: update Universal guide

--- aio/tools/transforms/angular-api-package/processors/computeApiBreadCrumbs.spec.js
+++ aio/tools/transforms/angular-api-package/processors/computeApiBreadCrumbs.spec.js
@@ -17,10 +17,17 @@ describe('angular-api-package: computeApiBreadCrumbs processor', () => {
     const API_DOC_TYPES_TO_RENDER = ['class', 'interface', 'package'];
     const processor = processorFactory(API_DOC_TYPES_TO_RENDER);
 
+    const corePackage = { docType: 'package', name: '@angular/core', id: 'core', path: 'core', isPrimaryPackage: true };
+    const coreTestingPackage = { docType: 'package', name: '@angular/core/testing', id: 'core/testing', path: 'core/testing', packageInfo: { primary: corePackage } };
+    const testRequestClass = { docType: 'class', name: 'InjectSetupWrapper', path: 'core/testing/test_bed', moduleDoc: coreTestingPackage };
+
     const docs = [
       { docType: 'class', name: 'ClassA', path: 'module-1/class-a', moduleDoc: { id: 'moduleOne', path: 'module-1' } },
       { docType: 'interface', name: 'InterfaceB', path: 'module-2/interface-b', moduleDoc: { id: 'moduleTwo', path: 'module-2' } },
       { docType: 'guide', name: 'Guide One', path: 'guide/guide-1' },
+      corePackage,
+      coreTestingPackage,
+      testRequestClass
     ];
     processor.$process(docs);
 
@@ -35,5 +42,20 @@ describe('angular-api-package: computeApiBreadCrumbs processor', () => {
       { text: 'InterfaceB', path: 'module-2/interface-b' },
     ]);
     expect(docs[2].breadCrumbs).toBeUndefined();
+    expect(docs[3].breadCrumbs).toEqual([
+      { text: 'API', path: '/api' },
+      { text: '@angular/core', path: 'core' },
+    ]);
+    expect(docs[4].breadCrumbs).toEqual([
+      { text: 'API', path: '/api' },
+      { text: '@angular/core', path: 'core' },
+      { text: '@angular/core/testing', path: 'core/testing' },
+    ]);
+    expect(docs[5].breadCrumbs).toEqual([
+      { text: 'API', path: '/api' },
+      { text: '@angular/core', path: 'core' },
+      { text: '@angular/core/testing', path: 'core/testing' },
+      { text: 'InjectSetupWrapper', path: 'core/testing/test_bed' },
+    ]);
   });
 });
