bd38173d9acf562ae50114ea1acbd90e59beadbd
Keen Yee Liau <kyliau@google.com>
2019-03-07 11:13:10 -0800

    feat(bazel): Eject Bazel (#29167)
    Add command line flag to expose Bazel files on disk.
    
    `ng build --leaveBazelFilesOnDisk`
    
    PR Close #29167

--- packages/bazel/src/builders/schema.d.ts
+++ packages/bazel/src/builders/schema.d.ts
@@ -9,14 +9,27 @@
  * Options for Bazel Builder
  */
 export interface Schema {
+  /**
+   * Common commands supported by Bazel.
+   */
   bazelCommand: BazelCommand;
+  /**
+   * If true, leave Bazel files on disk after running command.
+   */
+  leaveBazelFilesOnDisk?: boolean;
   /**
    * Target to be executed under Bazel.
    */
   targetLabel: string;
+  /**
+   * If true, watch the filesystem using ibazel.
+   */
   watch?: boolean;
 }
 
+/**
+ * Common commands supported by Bazel.
+ */
 export enum BazelCommand {
   Build = 'build',
   Run = 'run',
