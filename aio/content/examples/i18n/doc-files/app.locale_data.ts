9c558d97c50d712e65a0d3e62cd73ff0d8861c03
Janghyun Han <han41858@gmail.com>
2019-03-11 00:47:14 +0900

    docs: i18n.md translated

--- aio/content/examples/i18n/doc-files/app.locale_data.ts
+++ aio/content/examples/i18n/doc-files/app.locale_data.ts
@@ -2,6 +2,6 @@
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 
-// the second parameter 'fr' is optional
+// 두번째 인자 `fr`은 생략해도 됩니다.
 registerLocaleData(localeFr, 'fr');
 // #enddocregion import-locale
