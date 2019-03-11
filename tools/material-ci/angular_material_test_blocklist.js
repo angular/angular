3bbedc99ac97f7712f1e3ecbc846bfa68698e1ac
Marc Laval <mlaval@amadeus.com>
2019-03-11 17:12:50 +0100

    test(ivy): diagnose root causes of failures in CdkTree and MatTree (#29224)
    PR Close #29224

--- tools/material-ci/angular_material_test_blocklist.js
+++ tools/material-ci/angular_material_test_blocklist.js
@@ -87,27 +87,27 @@ window.testBlocklist = {
   },
   "CdkTree flat tree should initialize should be able to use units different from px for the indentation": {
     "error": "Error: Failed: Expected node level to be 15rem but was 28px",
-    "notes": "Unknown"
+    "notes": "Breaking change: Change detection follows insertion tree only, not declaration tree (CdkTree is OnPush)"
   },
   "CdkTree flat tree should initialize should default to px if no unit is set for string value indentation": {
     "error": "Error: Failed: Expected node level to be 17px but was 28px",
-    "notes": "Unknown"
+    "notes": "Breaking change: Change detection follows insertion tree only, not declaration tree (CdkTree is OnPush)"
   },
   "CdkTree flat tree with toggle should expand/collapse the node": {
     "error": "Error: Failed: Expected node level to be 40px but was ",
-    "notes": "Unknown"
+    "notes": "Breaking change: Static directive inputs evaluated in creation mode (CdkTreeNode.mostRecentTreeNode.data is set after inputs)"
   },
   "CdkTree flat tree with toggle should expand/collapse the node recursively": {
     "error": "Error: Failed: Expected node level to be 40px but was ",
-    "notes": "Unknown"
+    "notes": "Breaking change: Static directive inputs evaluated in creation mode (CdkTreeNode.mostRecentTreeNode.data is set after inputs)"
   },
   "CdkTree nested tree with toggle should expand/collapse the node multiple times": {
     "error": "Error: Expected 3 to be 1, 'Expect node expanded'.",
-    "notes": "Unknown"
+    "notes": "1) Breaking change: Change detection follows insertion tree only, not declaration tree (CdkTree is OnPush) - 2) Breaking change: Content queries and descendants flag (fix test by removing <div> in NestedCdkTreeAppWithToggle)"
   },
   "CdkTree nested tree with toggle should expand/collapse the node recursively": {
     "error": "Error: Failed: Expected node descendant num to be 2 but was 0",
-    "notes": "Unknown"
+    "notes": "Breaking change: Content queries and descendants flag (fix test by removing <div> in NestedCdkTreeAppWithToggle)"
   },
   "MatChipList StandardChipList basic behaviors should toggle the chips disabled state based on whether it is disabled": {
     "error": "Error: Expected true to be false.",
@@ -227,19 +227,19 @@ window.testBlocklist = {
   },
   "MatTree flat tree with toggle should expand/collapse the node": {
     "error": "Error: Expected 0 to be 1, 'Expect node expanded one level'.",
-    "notes": "Unknown"
+    "notes": "Breaking change: Static directive inputs evaluated in creation mode (CdkTreeNode.mostRecentTreeNode.data is set after inputs)"
   },
   "MatTree flat tree with toggle should expand/collapse the node recursively": {
     "error": "Error: Expected 0 to be 3, 'Expect nodes expanded'.",
-    "notes": "Unknown"
+    "notes": "Breaking change: Static directive inputs evaluated in creation mode (CdkTreeNode.mostRecentTreeNode.data is set after inputs)"
   },
   "MatTree nested tree with toggle should expand/collapse the node": {
     "error": "Error: Expected 0 to be 1, 'Expect node expanded'.",
-    "notes": "Unknown"
+    "notes": "1) Breaking change: Content queries and descendants flag (fix test by removing <div> in NestedMatTreeAppWithToggle) - 2) Breaking change: Ivy inherits metadata from superclasses (remove (click) in MatTreeNodeToggle)"
   },
   "MatTree nested tree with toggle should expand/collapse the node recursively": {
     "error": "Error: Expected 0 to be 3, 'Expect node expanded'.",
-    "notes": "Unknown"
+    "notes": "1) Breaking change: Content queries and descendants flag (fix test by removing <div> in NestedMatTreeAppWithToggle) - 2) Breaking change: Ivy inherits metadata from superclasses (remove (click) in MatTreeNodeToggle)"
   },
   "MatInput without forms validates the type": {
     "error": "Error: Input type \"file\" isn't supported by matInput.",
