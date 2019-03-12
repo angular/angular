1877e6c3f888b0843c45e1340e1e4d589ee4034c
Matias Niemelä <matias@yearofmoo.com>
2019-03-12 14:14:08 -0700

    fix(ivy): ensure template styles/classes are applied before directives are instantiated (#29269)
    Angular Ivy interprets inline static style/class attribute values as instructions that
    are processed whilst an element gets created. Because these inline style values are
    referenced by style/class bindings, their inline style values are applied at a later
    stage. Despite them being eventually applied, their values should be applied earlier
    before any directives are instantiated so that directive code can rely on any inline
    style/class changes.
    
    This patch ensures that all static style/class attribute values are applied (rendered)
    on the element before directives are instantiated.
    
    Jira Issue: FW-1133
    
    PR Close #29269

--- tools/material-ci/angular_material_test_blocklist.js
+++ tools/material-ci/angular_material_test_blocklist.js
@@ -104,14 +104,6 @@ window.testBlocklist = {
   "MatSnackBar with TemplateRef should be able to open a snack bar using a TemplateRef": {
     "error": "Error: Expected ' Fries Pizza  ' to contain 'Pasta'.",
     "notes": "Breaking change: Change detection follows insertion tree only, not declaration tree (MatSnackBarContainer is OnPush)"
-  },
-  "MatTooltip special cases should clear the `user-select` when a tooltip is set on a text field": {
-    "error": "Error: Expected 'none' to be falsy.",
-    "notes": "FW-1133: Inline styles are not applied before constructor is run"
-  },
-  "MatTooltip special cases should clear the `-webkit-user-drag` on draggable elements": {
-    "error": "Error: Expected 'none' to be falsy.",
-    "notes": "FW-1133: Inline styles are not applied before constructor is run"
   }
 };
 // clang-format on
