a9020a028f143f052e9eaf083e30eca9e30ed789
Rob Gregorius <stormystormy@users.noreply.github.com>
2019-03-12 23:35:15 -0600

    docs(core): fix typo in NgModule imports example description (#29272)
    PR Close #29272

--- packages/core/src/metadata/ng_module.ts
+++ packages/core/src/metadata/ng_module.ts
@@ -200,7 +200,7 @@ export interface NgModule {
    *
    * ### Example
    *
-   * The following example allows MainModule to use anthing exported by
+   * The following example allows MainModule to use anything exported by
    * `CommonModule`:
    *
    * ```javascript
