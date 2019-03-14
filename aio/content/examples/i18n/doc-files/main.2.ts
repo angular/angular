f373e0167e4155cc08c9e0f884e77194c5199ecf
Janghyun Han <han41858@gmail.com>
2019-03-15 00:55:14 +0900

    docs: i18n.md translated

--- aio/content/examples/i18n/doc-files/main.2.ts
+++ aio/content/examples/i18n/doc-files/main.2.ts
@@ -9,9 +9,9 @@ if (environment.production) {
   enableProdMode();
 }
 
-// use the require method provided by webpack
+// webpack에 사용할 require 메소드를 선언합니다.
 declare const require;
-// we use the webpack raw-loader to return the content as a string
+// webpack raw-loader를 사용해서 파일의 내용을 문자열로 읽어옵니다.
 const translations = require(`raw-loader!./locale/messages.fr.xlf`);
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
