c8dac0731a001c7e1cf67e50b036975ba40d402b
Alex Di Mango <alex.dimango@gmail.com>
2019-03-18 16:22:30 +0100

    Update hero-birthday2.component.ts

--- aio/content/examples/pipes/src/app/hero-birthday2.component.ts
+++ aio/content/examples/pipes/src/app/hero-birthday2.component.ts
@@ -12,7 +12,7 @@ import { Component } from '@angular/core';
 })
 // #docregion class
 export class HeroBirthday2Component {
-  birthday = new Date(1988, 3, 15); // April 15, 1988
+  birthday = new Date(1988, 4, 15); // April 15, 1988
   toggle = true; // start with true == shortDate
 
   get format()   { return this.toggle ? 'shortDate' : 'fullDate'; }
