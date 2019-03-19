2892279c633c942f93bae64899f43c15aa5c0ba6
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 14:10:31 -0700

    docs(ivy): add description for instructions directory structure (#29406)
    PR Close #29406

--- packages/core/src/render3/instructions/all.ts
+++ packages/core/src/render3/instructions/all.ts
@@ -5,5 +5,25 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+
+/*
+ * This file re-exports all symbols contained in this directory.
+ *
+ * Why is this file not `index.ts`?
+ *
+ * There seems to be an inconsistent path resolution of an `index.ts` file
+ * when only the parent directory is referenced. This could be due to the
+ * node module resolution configuration differing from rollup and/or typescript.
+ *
+ * With commit
+ * https://github.com/angular/angular/commit/d5e3f2c64bd13ce83e7c70788b7fc514ca4a9918
+ * the `instructions.ts` file was moved to `instructions/instructions.ts` and an
+ * `index.ts` file was used to re-export everything. Having had file names that were
+ * importing from `instructions' directly (not the from the sub file or the `index.ts`
+ * file) caused strange CI issues. `index.ts` had to be renamed to `all.ts` for this
+ * to work.
+ *
+ * Jira Issue = FW-1184
+ */
 export * from './instructions';
 export * from './styling_instructions';
