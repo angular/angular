d02ce15df376e4ea7740313487d2efbdd7005d0a
George Kalpakas <kalpakas.g@gmail.com>
2019-03-20 12:10:58 +0200

    fix(ivy): handle class declarations consistently in ES5 code (#29209)
    PR Close #29209

--- packages/compiler-cli/ngcc/src/host/decorated_class.ts
+++ packages/compiler-cli/ngcc/src/host/decorated_class.ts
@@ -17,7 +17,10 @@ export class DecoratedClass {
    * Initialize a `DecoratedClass` that was found in a `DecoratedFile`.
    * @param name The name of the class that has been found. This is mostly used
    * for informational purposes.
-   * @param declaration The TypeScript AST node where this class is declared
+   * @param declaration The TypeScript AST node where this class is declared. In ES5 code, where a
+   * class can be represented by both a variable declaration and a function declaration (inside an
+   * IIFE), `declaration` will always refer to the outer variable declaration, which represents the
+   * class to the rest of the program.
    * @param decorators The collection of decorators that have been found on this class.
    */
   constructor(
