8954205d0b5bf3e4aae3ba27e9af577020063cc5
Pete Bacon Darwin <pete@bacondarwin.com>
2019-03-20 13:47:57 +0000

    build: allow build-packages-dist.sh to be run from anywhere

--- scripts/build-packages-dist.sh
+++ scripts/build-packages-dist.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source ./scripts/package-builder.sh
+source $(dirname $0)/package-builder.sh
 
 # Build the legacy (view engine) npm packages into dist/packages-dist
 buildTargetPackages "dist/packages-dist" "legacy" "Production"
