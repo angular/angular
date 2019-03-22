d508f7359bfd87d5394d9b7d613c5be3e7e92252
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-03-22 11:16:55 -0700

    feat(ivy): add backwards compatibility config to template type-checking (#29698)
    View Engine's implementation of naive template type-checking is less
    advanced than the current Ivy implementation. As a result, Ivy catches lots
    of typing bugs which VE does not. As a result, it's necessary to tone down
    the Ivy template type-checker in the default case.
    
    This commit introduces a mechanism for doing that, by passing a config to
    the template type-checking engine. Through this configuration, particular
    checks can be loosened or disabled entirely.
    
    Testing strategy: TCB tests included.
    
    PR Close #29698

--- packages/compiler-cli/src/ngtsc/typecheck/src/expression.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/src/expression.ts
@@ -9,6 +9,7 @@
 import {AST, ASTWithSource, Binary, Conditional, Interpolation, KeyedRead, LiteralArray, LiteralMap, LiteralPrimitive, MethodCall, NonNullAssert, PropertyRead} from '@angular/compiler';
 import * as ts from 'typescript';
 
+import {TypeCheckingConfig} from './api';
 const BINARY_OPS = new Map<string, ts.SyntaxKind>([
   ['+', ts.SyntaxKind.PlusToken],
   ['-', ts.SyntaxKind.MinusToken],
@@ -34,7 +35,8 @@ const BINARY_OPS = new Map<string, ts.SyntaxKind>([
  * AST.
  */
 export function astToTypescript(
-    ast: AST, maybeResolve: (ast: AST) => ts.Expression | null): ts.Expression {
+    ast: AST, maybeResolve: (ast: AST) => ts.Expression | null,
+    config: TypeCheckingConfig): ts.Expression {
   const resolved = maybeResolve(ast);
   if (resolved !== null) {
     return resolved;
@@ -42,17 +44,17 @@ export function astToTypescript(
   // Branch based on the type of expression being processed.
   if (ast instanceof ASTWithSource) {
     // Fall through to the underlying AST.
-    return astToTypescript(ast.ast, maybeResolve);
+    return astToTypescript(ast.ast, maybeResolve, config);
   } else if (ast instanceof PropertyRead) {
     // This is a normal property read - convert the receiver to an expression and emit the correct
     // TypeScript expression to read the property.
-    const receiver = astToTypescript(ast.receiver, maybeResolve);
+    const receiver = astToTypescript(ast.receiver, maybeResolve, config);
     return ts.createPropertyAccess(receiver, ast.name);
   } else if (ast instanceof Interpolation) {
-    return astArrayToExpression(ast.expressions, maybeResolve);
+    return astArrayToExpression(ast.expressions, maybeResolve, config);
   } else if (ast instanceof Binary) {
-    const lhs = astToTypescript(ast.left, maybeResolve);
-    const rhs = astToTypescript(ast.right, maybeResolve);
+    const lhs = astToTypescript(ast.left, maybeResolve, config);
+    const rhs = astToTypescript(ast.right, maybeResolve, config);
     const op = BINARY_OPS.get(ast.operation);
     if (op === undefined) {
       throw new Error(`Unsupported Binary.operation: ${ast.operation}`);
@@ -67,30 +69,30 @@ export function astToTypescript(
       return ts.createLiteral(ast.value);
     }
   } else if (ast instanceof MethodCall) {
-    const receiver = astToTypescript(ast.receiver, maybeResolve);
+    const receiver = astToTypescript(ast.receiver, maybeResolve, config);
     const method = ts.createPropertyAccess(receiver, ast.name);
-    const args = ast.args.map(expr => astToTypescript(expr, maybeResolve));
+    const args = ast.args.map(expr => astToTypescript(expr, maybeResolve, config));
     return ts.createCall(method, undefined, args);
   } else if (ast instanceof Conditional) {
-    const condExpr = astToTypescript(ast.condition, maybeResolve);
-    const trueExpr = astToTypescript(ast.trueExp, maybeResolve);
-    const falseExpr = astToTypescript(ast.falseExp, maybeResolve);
+    const condExpr = astToTypescript(ast.condition, maybeResolve, config);
+    const trueExpr = astToTypescript(ast.trueExp, maybeResolve, config);
+    const falseExpr = astToTypescript(ast.falseExp, maybeResolve, config);
     return ts.createParen(ts.createConditional(condExpr, trueExpr, falseExpr));
   } else if (ast instanceof LiteralArray) {
-    const elements = ast.expressions.map(expr => astToTypescript(expr, maybeResolve));
+    const elements = ast.expressions.map(expr => astToTypescript(expr, maybeResolve, config));
     return ts.createArrayLiteral(elements);
   } else if (ast instanceof LiteralMap) {
     const properties = ast.keys.map(({key}, idx) => {
-      const value = astToTypescript(ast.values[idx], maybeResolve);
+      const value = astToTypescript(ast.values[idx], maybeResolve, config);
       return ts.createPropertyAssignment(ts.createStringLiteral(key), value);
     });
     return ts.createObjectLiteral(properties, true);
   } else if (ast instanceof KeyedRead) {
-    const receiver = astToTypescript(ast.obj, maybeResolve);
-    const key = astToTypescript(ast.key, maybeResolve);
+    const receiver = astToTypescript(ast.obj, maybeResolve, config);
+    const key = astToTypescript(ast.key, maybeResolve, config);
     return ts.createElementAccess(receiver, key);
   } else if (ast instanceof NonNullAssert) {
-    const expr = astToTypescript(ast.expression, maybeResolve);
+    const expr = astToTypescript(ast.expression, maybeResolve, config);
     return ts.createNonNullExpression(expr);
   } else {
     throw new Error(`Unknown node type: ${Object.getPrototypeOf(ast).constructor}`);
@@ -102,13 +104,14 @@ export function astToTypescript(
  * and separating them with commas.
  */
 function astArrayToExpression(
-    astArray: AST[], maybeResolve: (ast: AST) => ts.Expression | null): ts.Expression {
+    astArray: AST[], maybeResolve: (ast: AST) => ts.Expression | null,
+    config: TypeCheckingConfig): ts.Expression {
   // Reduce the `asts` array into a `ts.Expression`. Multiple expressions are combined into a
   // `ts.BinaryExpression` with a comma separator. First make a copy of the input array, as
   // it will be modified during the reduction.
   const asts = astArray.slice();
   return asts.reduce(
-      (lhs, ast) =>
-          ts.createBinary(lhs, ts.SyntaxKind.CommaToken, astToTypescript(ast, maybeResolve)),
-      astToTypescript(asts.pop() !, maybeResolve));
+      (lhs, ast) => ts.createBinary(
+          lhs, ts.SyntaxKind.CommaToken, astToTypescript(ast, maybeResolve, config)),
+      astToTypescript(asts.pop() !, maybeResolve, config));
 }
