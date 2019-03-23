0fdeba1865e0e8786aeaf844449fea4f29c127e4
sarkiroka <sarkiroka@users.noreply.github.com>
2019-03-23 16:06:36 +0100

    refactor: remove extra line break in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+
 'use strict';
 
 // THIS CHECK SHOULD BE THE FIRST THING IN THIS FILE
