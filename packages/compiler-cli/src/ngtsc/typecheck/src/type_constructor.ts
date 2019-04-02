6b7860dd2fe367179b90bfd3d459519a20e8825f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-02 11:25:33 -0700

    perf(ivy): template type-check the entire program in 1 file if possible
    The template type-checking engine previously would assemble a type-checking
    program by inserting Type Check Blocks (TCBs) into existing user files. This
    approach proved expensive, as TypeScript has to re-parse and re-type-check
    those files when processing the type-checking program.
    
    Instead, a far more performant approach is to augment the program with a
    single type-checking file, into which all TCBs are generated. Additionally,
    type constructors are also inlined into this file.
    
    This is not always possible - both TCBs and type constructors can sometimes
    require inlining into user code, particularly if bound generic type
    parameters are present, so the approach taken is actually a hybrid. These
    operations are inlined if necessary, but are otherwise generated in a single
    file.
    
    It is critically important that the original program also include an empty
    version of the type-checking file, otherwise the shape of the two programs
    will be different and TypeScript will throw away all the old program
    information. This leads to a painfully slow type checking pass, on the same
    order as the original program creation. A shim to generate this file in the
    original program is therefore added.
    
    Testing strategy: this commit is largely a refactor with no externally
    observable behavioral differences, and thus no tests are needed.

--- packages/compiler-cli/src/ngtsc/typecheck/src/type_constructor.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/src/type_constructor.ts
@@ -10,41 +10,113 @@ import * as ts from 'typescript';
 
 import {ClassDeclaration} from '../../reflection';
 import {TypeCtorMetadata} from './api';
+import {checkIfGenericTypesAreUnbound} from './ts_util';
+
+export function generateTypeCtorDeclarationFn(
+    node: ClassDeclaration<ts.ClassDeclaration>, meta: TypeCtorMetadata,
+    nodeTypeRef: ts.Identifier | ts.QualifiedName): ts.Statement {
+  if (requiresInlineTypeCtor(node)) {
+    throw new Error(`${node.name.text} requires an inline type constructor`);
+  }
+
+  const rawTypeArgs =
+      node.typeParameters !== undefined ? generateGenericArgs(node.typeParameters) : undefined;
+  const rawType: ts.TypeNode = ts.createTypeReferenceNode(nodeTypeRef, rawTypeArgs);
+
+  const initParam = constructTypeCtorParameter(node, meta, rawType);
+
+  if (meta.body) {
+    const fnType = ts.createFunctionTypeNode(
+        /* typeParameters */ node.typeParameters,
+        /* parameters */[initParam],
+        /* type */ rawType, );
+
+    const decl = ts.createVariableDeclaration(
+        /* name */ meta.fnName,
+        /* type */ fnType,
+        /* body */ ts.createNonNullExpression(ts.createNull()));
+    const declList = ts.createVariableDeclarationList([decl], ts.NodeFlags.Const);
+    return ts.createVariableStatement(
+        /* modifiers */ undefined,
+        /* declarationList */ declList);
+  } else {
+    return ts.createFunctionDeclaration(
+        /* decorators */ undefined,
+        /* modifiers */[ts.createModifier(ts.SyntaxKind.DeclareKeyword)],
+        /* asteriskToken */ undefined,
+        /* name */ meta.fnName,
+        /* typeParameters */ node.typeParameters,
+        /* parameters */[initParam],
+        /* type */ rawType,
+        /* body */ undefined);
+  }
+}
 
 /**
- * Generate a type constructor for the given class and metadata.
+ * Generate an inline type constructor for the given class and metadata.
  *
- * A type constructor is a specially shaped TypeScript static method, intended to be placed within
- * a directive class itself, that permits type inference of any generic type parameters of the class
- * from the types of expressions bound to inputs or outputs, and the types of elements that match
- * queries performed by the directive. It also catches any errors in the types of these expressions.
- * This method is never called at runtime, but is used in type-check blocks to construct directive
- * types.
+ * An inline type constructor is a specially shaped TypeScript static method, intended to be placed
+ * within a directive class itself, that permits type inference of any generic type parameters of
+ * the class from the types of expressions bound to inputs or outputs, and the types of elements
+ * that match queries performed by the directive. It also catches any errors in the types of these
+ * expressions. This method is never called at runtime, but is used in type-check blocks to
+ * construct directive types.
  *
- * A type constructor for NgFor looks like:
+ * An inline type constructor for NgFor looks like:
  *
  * static ngTypeCtor<T>(init: Partial<Pick<NgForOf<T>, 'ngForOf'|'ngForTrackBy'|'ngForTemplate'>>):
  *   NgForOf<T>;
  *
- * A typical usage would be:
+ * A typical constructor would be:
  *
  * NgForOf.ngTypeCtor(init: {ngForOf: ['foo', 'bar']}); // Infers a type of NgForOf<string>.
+ *
+ * Inline type constructors are used when the type being created has bounded generic types which
+ * make writing a declared type constructor (via `generateTypeCtorDeclarationFn`) difficult or
+ * impossible.
  *
  * @param node the `ClassDeclaration<ts.ClassDeclaration>` for which a type constructor will be
  * generated.
  * @param meta additional metadata required to generate the type constructor.
  * @returns a `ts.MethodDeclaration` for the type constructor.
  */
-export function generateTypeCtor(
+export function generateInlineTypeCtor(
     node: ClassDeclaration<ts.ClassDeclaration>, meta: TypeCtorMetadata): ts.MethodDeclaration {
   // Build rawType, a `ts.TypeNode` of the class with its generic parameters passed through from
   // the definition without any type bounds. For example, if the class is
   // `FooDirective<T extends Bar>`, its rawType would be `FooDirective<T>`.
-  const rawTypeArgs = node.typeParameters !== undefined ?
-      node.typeParameters.map(param => ts.createTypeReferenceNode(param.name, undefined)) :
-      undefined;
+  const rawTypeArgs =
+      node.typeParameters !== undefined ? generateGenericArgs(node.typeParameters) : undefined;
   const rawType: ts.TypeNode = ts.createTypeReferenceNode(node.name, rawTypeArgs);
 
+  const initParam = constructTypeCtorParameter(node, meta, rawType);
+
+  // If this constructor is being generated into a .ts file, then it needs a fake body. The body
+  // is set to a return of `null!`. If the type constructor is being generated into a .d.ts file,
+  // it needs no body.
+  let body: ts.Block|undefined = undefined;
+  if (meta.body) {
+    body = ts.createBlock([
+      ts.createReturn(ts.createNonNullExpression(ts.createNull())),
+    ]);
+  }
+
+  // Create the type constructor method declaration.
+  return ts.createMethod(
+      /* decorators */ undefined,
+      /* modifiers */[ts.createModifier(ts.SyntaxKind.StaticKeyword)],
+      /* asteriskToken */ undefined,
+      /* name */ meta.fnName,
+      /* questionToken */ undefined,
+      /* typeParameters */ node.typeParameters,
+      /* parameters */[initParam],
+      /* type */ rawType,
+      /* body */ body, );
+}
+
+function constructTypeCtorParameter(
+    node: ClassDeclaration<ts.ClassDeclaration>, meta: TypeCtorMetadata,
+    rawType: ts.TypeNode): ts.ParameterDeclaration {
   // initType is the type of 'init', the single argument to the type constructor method.
   // If the Directive has any inputs, outputs, or queries, its initType will be:
   //
@@ -77,35 +149,22 @@ export function generateTypeCtor(
     initType = ts.createTypeReferenceNode('Partial', [pickType]);
   }
 
-  // If this constructor is being generated into a .ts file, then it needs a fake body. The body
-  // is set to a return of `null!`. If the type constructor is being generated into a .d.ts file,
-  // it needs no body.
-  let body: ts.Block|undefined = undefined;
-  if (meta.body) {
-    body = ts.createBlock([
-      ts.createReturn(ts.createNonNullExpression(ts.createNull())),
-    ]);
-  }
-
   // Create the 'init' parameter itself.
-  const initParam = ts.createParameter(
+  return ts.createParameter(
       /* decorators */ undefined,
       /* modifiers */ undefined,
       /* dotDotDotToken */ undefined,
       /* name */ 'init',
       /* questionToken */ undefined,
       /* type */ initType,
-      /* initializer */ undefined, );
+      /* initializer */ undefined);
+}
 
-  // Create the type constructor method declaration.
-  return ts.createMethod(
-      /* decorators */ undefined,
-      /* modifiers */[ts.createModifier(ts.SyntaxKind.StaticKeyword)],
-      /* asteriskToken */ undefined,
-      /* name */ meta.fnName,
-      /* questionToken */ undefined,
-      /* typeParameters */ node.typeParameters,
-      /* parameters */[initParam],
-      /* type */ rawType,
-      /* body */ body, );
+function generateGenericArgs(params: ReadonlyArray<ts.TypeParameterDeclaration>): ts.TypeNode[] {
+  return params.map(param => ts.createTypeReferenceNode(param.name, undefined));
+}
+
+export function requiresInlineTypeCtor(node: ClassDeclaration<ts.ClassDeclaration>): boolean {
+  // The class requires an inline type constructor if it has constrained (bound) generics.
+  return !checkIfGenericTypesAreUnbound(node);
 }
