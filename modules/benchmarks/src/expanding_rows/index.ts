7c9728b5164d60c6666aca5278e7f8d7de56e4cf
Miško Hevery <misko@hevery.com>
2019-04-04 15:03:40 -0700

    test(ivy): add expanding_rows performance benchmark which runs in ViewEngine and Ivy (#30449)
    PR Close #30449

---
+++ modules/benchmarks/src/expanding_rows/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {platformBrowser} from '@angular/platform-browser';
+
+import {ExpandingRowBenchmarkModule} from './benchmark';
+import {ExpandingRowBenchmarkModuleNgFactory} from './benchmark.ngfactory';
+
+setMode(ExpandingRowBenchmarkModule.hasOwnProperty('ngModuleDef') ? 'Ivy' : 'ViewEngine');
+platformBrowser().bootstrapModuleFactory(ExpandingRowBenchmarkModuleNgFactory);
+
+function setMode(name: string): void {
+  document.querySelector('#rendererMode') !.textContent = `Render Mode: ${name}`;
+}
