1940a672d4a590825530c684de2150852c621e19
Keen Yee Liau <kyliau@umich.edu>
2019-04-09 15:04:51 -0700

    refactor(bazel): Remove NodeJsSyncHost (#29796)
    `NodeJsSyncHost` is no longer provided by BuilderContext in
    architect v2, and its usage caused subtle path resolution issues
    in Windows.
    
    This PR cleans up `@angular/bazel` builder to use all native path
    and fs methods.
    
    PR Close #29796

--- packages/bazel/src/builders/index.ts
+++ packages/bazel/src/builders/index.ts
@@ -9,33 +9,29 @@
  */
 
 import {BuilderContext, BuilderOutput, createBuilder,} from '@angular-devkit/architect/src/index2';
-import {JsonObject, normalize} from '@angular-devkit/core';
+import {JsonObject} from '@angular-devkit/core';
 import {checkInstallation, copyBazelFiles, deleteBazelFiles, getTemplateDir, runBazel} from './bazel';
 import {Schema} from './schema';
-import {NodeJsSyncHost} from '@angular-devkit/core/node';
 
 async function _bazelBuilder(options: JsonObject & Schema, context: BuilderContext, ):
     Promise<BuilderOutput> {
-      const root = normalize(context.workspaceRoot);
-      const {logger} = context;
+      const {logger, workspaceRoot} = context;
       const {bazelCommand, leaveBazelFilesOnDisk, targetLabel, watch} = options;
       const executable = watch ? 'ibazel' : 'bazel';
-      const binary = checkInstallation(executable, root);
-
-      const host = new NodeJsSyncHost();
-      const templateDir = await getTemplateDir(host, root);
-      const bazelFiles = await copyBazelFiles(host, root, templateDir);
+      const binary = checkInstallation(executable, workspaceRoot);
+      const templateDir = getTemplateDir(workspaceRoot);
+      const bazelFiles = copyBazelFiles(workspaceRoot, templateDir);
 
       try {
         const flags: string[] = [];
-        await runBazel(root, binary, bazelCommand, targetLabel, flags);
+        await runBazel(workspaceRoot, binary, bazelCommand, targetLabel, flags);
         return {success: true};
       } catch (err) {
         logger.error(err.message);
         return {success: false};
       } finally {
         if (!leaveBazelFilesOnDisk) {
-          await deleteBazelFiles(host, bazelFiles);  // this will never throw
+          deleteBazelFiles(bazelFiles);  // this will never throw
         }
       }
     }
