159be34c60a6f07c2534b80cafdbf88baf90e5a0
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-04-10 16:39:42 +0300

    docs: correct types in transform method to match specification.

--- aio/content/examples/pipes/src/app/exponential-strength.pipe.ts
+++ aio/content/examples/pipes/src/app/exponential-strength.pipe.ts
@@ -11,8 +11,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 */
 @Pipe({name: 'exponentialStrength'})
 export class ExponentialStrengthPipe implements PipeTransform {
-  transform(value: number, exponent: string): number {
-    let exp = parseFloat(exponent);
-    return Math.pow(value, isNaN(exp) ? 1 : exp);
+  transform(value: number, exponent?: number): number {
+    return Math.pow(value, isNaN(exponent) ? 1 : exponent);
   }
 }
