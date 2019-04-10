de29f9e0aa2607ca2508aebdcac6d4ef1c0deeb3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-04-10 18:49:28 +0200

    refactor(compiler): export static-query detection logic (#29815)
    The `@angular/compiler` package currently contains the logic for determining whether
    given queries are used statically or dynamically. This logic would be necessary in order
    to build a schematic that leverages the Angular compiler API's in order to simulate the
    query timing based on what ViewEngine computed at compilation-time/runtime.
    
    Exporting the logic that is necessary to detect the timing should not affect the public
    API as the `@angular/compiler` package is denoted as private in `PUBLIC_API.md`
    
    PR Close #29815

--- packages/compiler/src/compiler.ts
+++ packages/compiler/src/compiler.ts
@@ -85,7 +85,7 @@ export * from './schema/dom_element_schema_registry';
 export * from './selector';
 export * from './style_compiler';
 export * from './template_parser/template_parser';
-export {ViewCompiler} from './view_compiler/view_compiler';
+export {ViewCompiler, findStaticQueryIds, staticViewQueryIds} from './view_compiler/view_compiler';
 export {getParseErrors, isSyntaxError, syntaxError, Version} from './util';
 export {SourceMap} from './output/source_map';
 export * from './injectable_compiler_2';
