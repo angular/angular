a9f00fd1f9ada49564754896197afad5bbd33c70
Alex Eagle <alexeagle@angular.io>
2019-04-10 16:06:22 -0700

    docs(aio): add missing mentors for collaborators (#29142)
    Also improve the presentation of the org chart
    
    PR Close #29142

--- aio/scripts/contributors/generate_org_chart.sh
+++ aio/scripts/contributors/generate_org_chart.sh
@@ -2,6 +2,7 @@
 (    
     cd $(dirname $0)
     echo "digraph {"
+    echo "rankdir=RL; splines=ortho; node [shape=box];"
     jq -f org_chart.jq --raw-output < ../../content/marketing/contributors.json
     echo "}"
 ) | dot -Tpng > org.png
