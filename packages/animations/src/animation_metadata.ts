22294dfad195c2de9ca4b81753dc2fc8a3995dde
Kevin Newman <knewmanium@gmail.com>
2019-04-11 21:19:09 -0400

    docs(animations): fixed some closing brackets on query animation page (#29854) (#29855)
    PR Close #29855

--- packages/animations/src/animation_metadata.ts
+++ packages/animations/src/animation_metadata.ts
@@ -1200,8 +1200,8 @@ export function useAnimation(
  *        query('.content', style({ opacity: 0 })),
  *
  *        // animate the inner elements in, one by one
- *        query('h1', animate(1000, style({ opacity: 1 })),
- *        query('.content', animate(1000, style({ opacity: 1 })),
+ *        query('h1', animate(1000, style({ opacity: 1 }))),
+ *        query('.content', animate(1000, style({ opacity: 1 }))),
  *      ])
  *    ])
  *  ]
