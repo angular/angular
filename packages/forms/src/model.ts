d2c83ea81b60366d1ef23e0b1c8c4abff0354a0e
Ly-lns <lynetteli1219@gmail.com>
2019-04-15 14:44:28 +0800

    docs(forms): add comment of the value (#29898)
    PR Close #29898

--- packages/forms/src/model.ts
+++ packages/forms/src/model.ts
@@ -159,7 +159,9 @@ export abstract class AbstractControl {
    * The current value of the control.
    *
    * * For a `FormControl`, the current value.
-   * * For a `FormGroup`, the values of enabled controls as an object
+   * * For an enabled `FormGroup`, the values of enabled controls as an object
+   * with a key-value pair for each member of the group.
+   * * For a disabled `FormGroup`, the values of all controls as an object
    * with a key-value pair for each member of the group.
    * * For a `FormArray`, the values of enabled controls as an array.
    *
