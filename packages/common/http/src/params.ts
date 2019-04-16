a6c951f807a245a9e2908fbc34dcf19b2ed8829c
José I. Escudero <joseignacioescudero97@hotmail.com>
2019-04-16 12:45:00 -0500

    docs: fixed typo on HttpParamsOptions (#29930) PR Close #29930

--- packages/common/http/src/params.ts
+++ packages/common/http/src/params.ts
@@ -83,7 +83,7 @@ export interface HttpParamsOptions {
    */
   fromString?: string;
 
-  /** Object map of the HTTP params. Mutally exclusive with `fromString`. */
+  /** Object map of the HTTP params. Mutually exclusive with `fromString`. */
   fromObject?: {[param: string]: string | string[]};
 
   /** Encoding codec used to parse and serialize the params. */
