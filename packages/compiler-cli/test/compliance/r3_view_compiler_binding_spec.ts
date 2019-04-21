538b6b187521b2f1405fbf1321f93d00063d17e8
Ben Lesh <ben@benlesh.com>
2019-04-20 18:05:46 -0700

    feat(ivy): generate ɵɵpropertyInterpolateX instructions (#30008)
    - Compiler now generates `ɵɵpropertyInterpolateX` instructions.
    
    PR Close #30008

--- packages/compiler-cli/test/compliance/r3_view_compiler_binding_spec.ts
+++ packages/compiler-cli/test/compliance/r3_view_compiler_binding_spec.ts
@@ -117,7 +117,7 @@ describe('compiler compliance: bindings', () => {
         }
         if (rf & 2) {
           $i0$.ɵɵselect(0);
-          $i0$.ɵɵelementProperty(0, "title", $i0$.ɵɵinterpolation1("Hello ", $ctx$.name, ""));
+          $i0$.ɵɵpropertyInterpolate1("title", "Hello ", $ctx$.name, "");
         }
       }`;
       const result = compile(files, angularFiles);
@@ -450,6 +450,50 @@ describe('compiler compliance: bindings', () => {
           @NgModule({declarations: [MyComponent]})
           export class MyModule {}`
       }
+    });
+
+    it('should generate the proper update instructions for interpolated properties', () => {
+      const files: MockDirectory = getAppFiles(`
+        <div title="a{{one}}b{{two}}c{{three}}d{{four}}e{{five}}f{{six}}g{{seven}}h{{eight}}i{{nine}}j"></div>
+        <div title="a{{one}}b{{two}}c{{three}}d{{four}}e{{five}}f{{six}}g{{seven}}h{{eight}}i"></div>
+        <div title="a{{one}}b{{two}}c{{three}}d{{four}}e{{five}}f{{six}}g{{seven}}h"></div>
+        <div title="a{{one}}b{{two}}c{{three}}d{{four}}e{{five}}f{{six}}g"></div>
+        <div title="a{{one}}b{{two}}c{{three}}d{{four}}e{{five}}f"></div>
+        <div title="a{{one}}b{{two}}c{{three}}d{{four}}e"></div>
+        <div title="a{{one}}b{{two}}c{{three}}d"></div>
+        <div title="a{{one}}b{{two}}c"></div>
+        <div title="a{{one}}b"></div>
+        <div title="{{one}}"></div>
+      `);
+
+      const template = `
+      …
+        if (rf & 2) {
+          i0.ɵɵselect(0);
+          i0.ɵɵpropertyInterpolateV("title", "a", ctx.one, "b", ctx.two, "c", ctx.three, "d", ctx.four, "e", ctx.five, "f", ctx.six, "g", ctx.seven, "h", ctx.eight, "i", ctx.nine, "j");
+          i0.ɵɵselect(1);
+          i0.ɵɵpropertyInterpolate8("title", "a", ctx.one, "b", ctx.two, "c", ctx.three, "d", ctx.four, "e", ctx.five, "f", ctx.six, "g", ctx.seven, "h", ctx.eight, "i");
+          i0.ɵɵselect(2);
+          i0.ɵɵpropertyInterpolate7("title", "a", ctx.one, "b", ctx.two, "c", ctx.three, "d", ctx.four, "e", ctx.five, "f", ctx.six, "g", ctx.seven, "h");
+          i0.ɵɵselect(3);
+          i0.ɵɵpropertyInterpolate6("title", "a", ctx.one, "b", ctx.two, "c", ctx.three, "d", ctx.four, "e", ctx.five, "f", ctx.six, "g");
+          i0.ɵɵselect(4);
+          i0.ɵɵpropertyInterpolate5("title", "a", ctx.one, "b", ctx.two, "c", ctx.three, "d", ctx.four, "e", ctx.five, "f");
+          i0.ɵɵselect(5);
+          i0.ɵɵpropertyInterpolate4("title", "a", ctx.one, "b", ctx.two, "c", ctx.three, "d", ctx.four, "e");
+          i0.ɵɵselect(6);
+          i0.ɵɵpropertyInterpolate3("title", "a", ctx.one, "b", ctx.two, "c", ctx.three, "d");
+          i0.ɵɵselect(7);
+          i0.ɵɵpropertyInterpolate2("title", "a", ctx.one, "b", ctx.two, "c");
+          i0.ɵɵselect(8);
+          i0.ɵɵpropertyInterpolate1("title", "a", ctx.one, "b");
+          i0.ɵɵselect(9);
+          i0.ɵɵpropertyInterpolate("title", ctx.one);
+      }
+      …
+      `;
+      const result = compile(files, angularFiles);
+      expectEmit(result.source, template, 'Incorrect handling of interpolated properties');
     });
 
     it('should keep local ref for host element', () => {
