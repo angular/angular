80d3ec442f8d6c2fdd4f2396f1b1c8496a668ad3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-23 14:30:09 -0700

    feat(router): deprecate loadChildren:string (#30073)
    The proposed ES dynamic import() is now supported by the Angular CLI and the
    larger toolchain. This renders the `loadChildren: string` API largely
    redundant, as import() is far more natural, is less error-prone, and is
    standards compliant. This commit deprecates the `string` form of
    `loadChildren` in favor of dynamic import().
    
    DEPRECATION:
    
    When defining lazy-loaded route, Angular previously offered two options for
    configuring the module to be loaded, both via the `loadChildren` parameter
    of the route. Most Angular developers are familiar withthe `string` form of
    this API. For example, the following route definition configures Angular to
    load a `LazyModule` NgModule from `lazy-route/lazy.module.ts`:
    
    ```
    [{
      path: 'lazy',
      loadChildren: 'lazy-route/lazy.module#LazyModule',
    }]
    ```
    
    This "magic string" configuration was previously necessary as there was
    no dynamic module loading standard on the web. This has changed with the
    pending standardization of dynamic `import()` expressions, which are now
    supported in the Angular CLI and in web tooling in general. `import()`
    offers a more natural and robust solution to dynamic module loading. The
    above example can be rewritten to use dynamic `import()`:
    
    ```
    [{
      path: 'lazy',
      loadChildren: () => import('./lazy-route/lazy.module').then(mod => mod.LazyModule),
    }]
    ```
    
    This form of lazy loading offers significant advantages in terms of:
    
    * type checking via TypeScript
    * simplicity of generated code
    * future potential to run natively in supporting browsers
      (see: [caniuse: dynamic import()](https://caniuse.com/#feat=es6-module-dynamic-import))
    
    As a result, Angular is deprecating the `loadChildren: string` syntax in
    favor of ES dynamic `import()`. An automatic migration will run during
    `ng upgrade` to convert your existing Angular code to the new syntax.
    
    PR Close #30073

--- tools/public_api_guard/router/router.d.ts
+++ tools/public_api_guard/router/router.d.ts
@@ -101,6 +101,9 @@ export declare class DefaultUrlSerializer implements UrlSerializer {
     serialize(tree: UrlTree): string;
 }
 
+/** @deprecated */
+export declare type DeprecatedLoadChildren = string;
+
 export declare type DetachedRouteHandle = {};
 
 export declare type Event = RouterEvent | RouteConfigLoadStart | RouteConfigLoadEnd | ChildActivationStart | ChildActivationEnd | ActivationStart | ActivationEnd | Scroll;
@@ -145,7 +148,7 @@ export declare class GuardsCheckStart extends RouterEvent {
     toString(): string;
 }
 
-export declare type LoadChildren = string | LoadChildrenCallback;
+export declare type LoadChildren = LoadChildrenCallback | DeprecatedLoadChildren;
 
 export declare type LoadChildrenCallback = () => Type<any> | NgModuleFactory<any> | Observable<Type<any>> | Promise<NgModuleFactory<any> | Type<any> | any>;
 
