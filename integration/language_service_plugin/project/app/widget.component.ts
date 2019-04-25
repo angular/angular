05eabb19d624791c17cf447e8fea3a2ebee295ff
Keen Yee Liau <kyliau@google.com>
2019-04-25 10:50:19 -0700

    test(language-service): Add external template (#30128)
    This is in preparation for more rigorous testing of external templates, since it'll work differently under the new tsserver plugin model.
    
    PR Close #30128

---
+++ integration/language_service_plugin/project/app/widget.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-widget',
+  templateUrl: './widget.component.html',
+})
+export class WidgetComponent  { name = 'Angular'; }
