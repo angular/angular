153835d5e92ee24184bc2357dc77f9a444d5bbec
Ben Lesh <ben@benlesh.com>
2019-04-25 10:41:45 -0700

    fix: ensure strict mode when evaluating in JIT (#30122)
    PR Close #30122

--- packages/compiler/test/output/output_jit_spec.ts
+++ packages/compiler/test/output/output_jit_spec.ts
@@ -8,7 +8,8 @@
 
 import {EmitterVisitorContext} from '@angular/compiler/src/output/abstract_emitter';
 import * as o from '@angular/compiler/src/output/output_ast';
-import {JitEmitterVisitor} from '@angular/compiler/src/output/output_jit';
+import {JitEmitterVisitor, JitEvaluator} from '@angular/compiler/src/output/output_jit';
+import {R3JitReflector} from '@angular/compiler/src/render3/r3_jit';
 import {JitReflector} from '@angular/platform-browser-dynamic/src/compiler_reflector';
 
 const anotherModuleUrl = 'somePackage/someOtherPath';
@@ -31,6 +32,32 @@ const anotherModuleUrl = 'somePackage/someOtherPath';
         const args = converter.getArgs();
         expect(Object.keys(args).length).toBe(20);
       });
+    });
+
+    it('should use strict mode', () => {
+      const evaluator = new JitEvaluator();
+      expect(() => {
+        evaluator.evaluateStatements(
+            'http://angular.io/something.ts',
+            [
+              // Set an undeclared variable
+              // foo = "bar";
+              o.variable('foo').equals(o.literal('bar')).toStmt(),
+            ],
+            new R3JitReflector({}), false);
+      }).toThrowError();
+    });
+
+    it('should not add more than one strict mode statement if there is already one present', () => {
+      const converter = new JitEmitterVisitor(new JitReflector());
+      const ctx = EmitterVisitorContext.createRoot();
+      converter.visitAllStatements(
+          [
+            o.literal('use strict').toStmt(),
+          ],
+          ctx);
+      const matches = ctx.toSource().match(/'use strict';/g) !;
+      expect(matches.length).toBe(1);
     });
   });
 }
