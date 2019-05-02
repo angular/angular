820db13467c432a768e7f737bc126686cbd77281
Filipe Silva <filipematossilva@gmail.com>
2019-05-02 09:34:01 +0100

    fix(compiler-cli): log ngcc skipping messages as debug instead of info
    Related to https://github.com/angular/angular-cli/issues/14194, https://github.com/angular/angular-cli/pull/14320

--- packages/compiler-cli/ngcc/src/main.ts
+++ packages/compiler-cli/ngcc/src/main.ts
@@ -90,7 +90,7 @@ export function mainNgcc(
   if (absoluteTargetEntryPointPath &&
       hasProcessedTargetEntryPoint(
           fs, absoluteTargetEntryPointPath, propertiesToConsider, compileAllFormats)) {
-    logger.info('The target entry-point has already been processed');
+    logger.debug('The target entry-point has already been processed');
     return;
   }
 
@@ -123,7 +123,7 @@ export function mainNgcc(
 
       if (hasBeenProcessed(entryPointPackageJson, property)) {
         compiledFormats.add(formatPath);
-        logger.info(`Skipping ${entryPoint.name} : ${property} (already compiled).`);
+        logger.debug(`Skipping ${entryPoint.name} : ${property} (already compiled).`);
         continue;
       }
 
@@ -146,7 +146,7 @@ export function mainNgcc(
               `Skipping ${entryPoint.name} : ${format} (no valid entry point file for this format).`);
         }
       } else if (!compileAllFormats) {
-        logger.info(`Skipping ${entryPoint.name} : ${property} (already compiled).`);
+        logger.debug(`Skipping ${entryPoint.name} : ${property} (already compiled).`);
       }
 
       // Either this format was just compiled or its underlying format was compiled because of a
