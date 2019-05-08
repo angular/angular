3592bda5ffcb6d91b8f9d1d65fe9ecf3edd6f085
Pete Bacon Darwin <pete@bacondarwin.com>
2019-05-08 16:30:56 +0100

    refactor(ivy): clean up ngtsc code
    No behavioural changes.

--- packages/compiler-cli/src/ngtsc/annotations/test/component_spec.ts
+++ packages/compiler-cli/src/ngtsc/annotations/test/component_spec.ts
@@ -5,9 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-
-import * as ts from 'typescript';
-
 import {CycleAnalyzer, ImportGraph} from '../../cycles';
 import {ErrorCode, FatalDiagnosticError} from '../../diagnostics';
 import {ModuleResolver, NOOP_DEFAULT_IMPORT_RECORDER, ReferenceEmitter} from '../../imports';
