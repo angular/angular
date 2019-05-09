264872868ab81e4e447f0559400fcb3c047a08ad
Brandon Roberts <robertsbt@gmail.com>
2019-05-09 15:56:24 -0500

    docs: update docs example dependencies to version 8 (#30385)
    PR Close #30385

--- aio/tools/examples/shared/boilerplate/cli/e2e/protractor.conf.js
+++ aio/tools/examples/shared/boilerplate/cli/e2e/protractor.conf.js
@@ -1,15 +1,19 @@
+// @ts-check
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
+/**
+ * @type { import("protractor").Config }
+ */
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome',
+    'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -21,7 +25,7 @@ exports.config = {
   },
   onPrepare() {
     require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.e2e.json')
+      project: require('path').join(__dirname, './tsconfig.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
