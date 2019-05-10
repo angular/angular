b62a9d0bc6698c1bcb5025985713260621796fd4
George Kalpakas <kalpakas.g@gmail.com>
2019-05-10 19:48:33 +0300

    build(docs-infra): ensure hidden cli commands are excluded from `sitemap.xml` (#30395)
    Previously, the processor that excludes certain cli commands
    (`filterHiddenCommand`) was being run after the `createSitemap`
    processor, resulting in those commands to be present in `sitemap.xml`,
    while the actual pages where missing. This also resulted in 404s, when
    search engine crawlers tried to index the missing URLs.
    
    This commit fixes it by ensuring that the `filterHiddenCommand`
    processor is run before the `createSitemap` processor.
    
    PR Close #30395

--- aio/tools/transforms/cli-docs-package/processors/filterHiddenCommands.js
+++ aio/tools/transforms/cli-docs-package/processors/filterHiddenCommands.js
@@ -1,7 +1,7 @@
 module.exports = function filterHiddenCommands() {
   return {
     $runAfter: ['files-read'],
-    $runBefore: ['processCliContainerDoc'],
+    $runBefore: ['processCliContainerDoc', 'createSitemap'],
     $process(docs) {
       return docs.filter(doc => doc.docType !== 'cli-command' || doc.hidden !== true);
     }
