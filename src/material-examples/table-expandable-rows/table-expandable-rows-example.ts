c1859500ea77f0da349695a6128b9a2a5cbeecdc
Dario Braun <dario.braun@adesso.de>
2019-05-10 22:45:48 +0200

    docs(table): improve TableExpandableRowsExample performance (#15962)
    Fix TableExpandableRowsExample for iOS and Edge by removing unnecessary styling rule

--- src/material-examples/table-expandable-rows/table-expandable-rows-example.ts
+++ src/material-examples/table-expandable-rows/table-expandable-rows-example.ts
@@ -10,7 +10,7 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   templateUrl: 'table-expandable-rows-example.html',
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('collapsed', style({height: '0px', minHeight: '0'})),
       state('expanded', style({height: '*'})),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
