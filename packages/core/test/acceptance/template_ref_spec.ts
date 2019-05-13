1407b82307e34a2e1017ce8849383077793b87f9
Ben Lesh <ben@benlesh.com>
2019-05-13 16:24:49 -0700

    test(ivy): port TemplateRef tests over to acceptance tests
    - Moves template ref tests from render3 unit tests to acceptance tests.
    - Marks tests testing ivy specific changes as `onlyInIvy`.
    - Deletes old render3 unit tests that are no longer necessary

--- packages/core/test/acceptance/template_ref_spec.ts
+++ packages/core/test/acceptance/template_ref_spec.ts
@@ -7,8 +7,10 @@
  */
 
 import {Component, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
+import {EmbeddedViewRef} from '@angular/core/src/core';
 import {TestBed} from '@angular/core/testing';
 import {expect} from '@angular/platform-browser/testing/src/matchers';
+import {onlyInIvy} from '@angular/private/testing';
 
 describe('TemplateRef', () => {
   describe('rootNodes', () => {
@@ -52,6 +54,122 @@ describe('TemplateRef', () => {
 
       expect(rootNodeTextContent).toEqual(['Header', 'Item one', 'Item two']);
     });
-  });
 
+    it('should return root render nodes for an embedded view instance', () => {
+      @Component({
+        template: `
+          <ng-template #templateRef>
+            <div></div>
+            some text
+            <span></span>
+          </ng-template>
+        `
+      })
+      class App {
+        @ViewChild('templateRef')
+        templateRef !: TemplateRef<any>;
+      }
+
+      TestBed.configureTestingModule({
+        declarations: [App],
+      });
+      const fixture = TestBed.createComponent(App);
+      fixture.detectChanges();
+
+      const embeddedView = fixture.componentInstance.templateRef.createEmbeddedView({});
+      expect(embeddedView.rootNodes.length).toBe(3);
+    });
+
+    /**
+     * This is different as compared to the view engine implementation which returns a comment node
+     * in this case:
+     * https://stackblitz.com/edit/angular-uiqry6?file=src/app/app.component.ts
+     *
+     * Returning a comment node for a template ref with no nodes is wrong and should be fixed in
+     * ivy.
+     */
+    onlyInIvy('Fixed: Ivy no longer adds a comment node in this case.')
+        .it('should return an empty array for embedded view with no nodes', () => {
+          @Component({
+            template: `
+          <ng-template #templateRef></ng-template>
+        `
+          })
+          class App {
+            @ViewChild('templateRef')
+            templateRef !: TemplateRef<any>;
+          }
+
+          TestBed.configureTestingModule({
+            declarations: [App],
+          });
+          const fixture = TestBed.createComponent(App);
+          fixture.detectChanges();
+
+          const embeddedView = fixture.componentInstance.templateRef.createEmbeddedView({});
+          expect(embeddedView.rootNodes.length).toBe(0);
+        });
+
+    /**
+     * This is somehow surprising but the current view engine doesn't descend into containers when
+     * getting root nodes of an embedded view:
+     * https://stackblitz.com/edit/angular-z8zev7?file=src/app/app.component.ts
+     */
+    onlyInIvy('Ivy will get root nodes from containers in embedded views')
+        .it('should not descend into containers when retrieving root nodes', () => {
+          @Component({
+            template: `
+          <ng-template #templateRef>
+            <ng-template [ngIf]="true">text</ng-template>
+          </ng-template>
+        `
+          })
+          class App {
+            @ViewChild('templateRef')
+            templateRef !: TemplateRef<any>;
+          }
+
+          TestBed.configureTestingModule({
+            declarations: [App],
+          });
+          const fixture = TestBed.createComponent(App);
+          fixture.detectChanges();
+
+          const embeddedView = fixture.componentInstance.templateRef.createEmbeddedView({});
+          expect(embeddedView.rootNodes.length).toBe(1);
+          expect(embeddedView.rootNodes[0].nodeType).toBe(8);
+        });
+
+    /**
+     * Contrary to containers (<ng-template>) we _do_ descend into element containers
+     * (<ng-container):
+     * https://stackblitz.com/edit/angular-yovmmp?file=src/app/app.component.ts
+     */
+    it('should descend into element containers when retrieving root nodes', () => {
+      @Component({
+        template: `
+          <ng-template #templateRef>
+            <ng-container>text</ng-container>
+          </ng-template>
+        `
+      })
+      class App {
+        @ViewChild('templateRef')
+        templateRef !: TemplateRef<any>;
+      }
+
+      TestBed.configureTestingModule({
+        declarations: [App],
+      });
+      const fixture = TestBed.createComponent(App);
+      fixture.detectChanges();
+
+      const embeddedView = fixture.componentInstance.templateRef.createEmbeddedView({});
+
+      expect(embeddedView.rootNodes.length).toBe(2);
+      expect(embeddedView.rootNodes[0].nodeType)
+          .toBe(8);  // a comment node (only!) corresponding to <ng-container>
+      expect(embeddedView.rootNodes[1].nodeType).toBe(3);  // a text node
+    });
+  });
 });
