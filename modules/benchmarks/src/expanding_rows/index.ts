f39401a02626a444ff6208b4777bc22a64640129
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2019-05-14 14:50:24 +0200

    fixup! test(ivy): enableProdMode (#30449)
    PR Close #30449

--- modules/benchmarks/src/expanding_rows/index.ts
+++ modules/benchmarks/src/expanding_rows/index.ts
@@ -5,13 +5,14 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-
+import {enableProdMode} from '@angular/core';
 import {platformBrowser} from '@angular/platform-browser';
 
 import {ExpandingRowBenchmarkModule} from './benchmark';
 import {ExpandingRowBenchmarkModuleNgFactory} from './benchmark.ngfactory';
 
 setMode(ExpandingRowBenchmarkModule.hasOwnProperty('ngModuleDef') ? 'Ivy' : 'ViewEngine');
+enableProdMode();
 platformBrowser().bootstrapModuleFactory(ExpandingRowBenchmarkModuleNgFactory);
 
 function setMode(name: string): void {
