83c7d59526e67479149ce94dc89b0ff7039c16bf
Misko Hevery <misko@hevery.com>
2019-05-17 22:17:23 -0700

    fixup! refactor(ivy): rewrite flatten function to be more memory efficient

--- packages/core/src/util/array_utils.ts
+++ packages/core/src/util/array_utils.ts
@@ -19,55 +19,23 @@ export function addAllToArray(items: any[], arr: any[]) {
 }
 
 /**
- * A stack used by flatten to prevent recursive implementation.
+ * Flattens an array.
  */
-const flattenQueue: (any[] | number | null)[] = [];
-
-/**
- * Flattens an array in non-recursive way. Input arrays are not modified.
- *
- * This implementation is memory efficient and only allocates objects when needed.
- */
-export function flatten(list: any[]): any[] {
-  // This is an optimization so that we don't allocate memory until we have to.
-  // We assume that list is already flat.
-  let flat: any[] = list;
-  let queueIndex = 0;
-  let index = 0;
-  let length = list.length;
-  while (true) {
-    while (index < length) {
-      let item = list[index++];
-      if (Array.isArray(item)) {
-        // we need to inline it.
-        if (flat === list) {
-          // Our assumption that the list was already flat was wrong and
-          // we need to clone flat since we need to write to it.
-          flat = list.slice(0, index - 1);
-        }
-        // push current position on the stack.
-        flattenQueue[queueIndex++] = index;
-        flattenQueue[queueIndex++] = list;
-        list = item;
-        index = 0;
-        length = list.length;
-      } else if (flat !== list) {
-        flat.push(item);
+export function flatten(list: any[], dst?: any[]): any[] {
+  if (dst === undefined) dst = list;
+  for (let i = 0; i < list.length; i++) {
+    let item = list[i];
+    if (Array.isArray(item)) {
+      // we need to inline it.
+      if (dst === list) {
+        // Our assumption that the list was already flat was wrong and
+        // we need to clone flat since we need to write to it.
+        dst = list.slice(0, i);
       }
-    }
-    if (queueIndex > 0) {
-      // There are items in the flattenQueue which need to be processed.
-      // Pop them.
-      list = flattenQueue[--queueIndex] as any[];
-      flattenQueue[queueIndex] = null;
-      length = list.length;
-      index = flattenQueue[--queueIndex] as number;
-      // The array is not cleared because doing so would take extra time.
-      // Any memory leaks would be just temporary until next flattening.
-    } else {
-      break;
+      flatten(item, dst);
+    } else if (dst !== list) {
+      dst.push(item);
     }
   }
-
-  return flat;
+  return dst;
 }
