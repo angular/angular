251da64a0ff51ab35e3e17428aeafed186066fb7
Misko Hevery <misko@hevery.com>
2019-05-20 22:50:30 -0700

    refactor(core): cleanup code with side-effects which was preventing tree-shaking (#30580)
    PR Close #30580

--- integration/side-effects/snapshots/core/esm5.js
+++ integration/side-effects/snapshots/core/esm5.js
@@ -3,15 +3,3 @@ import "tslib";
 import "rxjs";
 
 import "rxjs/operators";
-
-function getGlobal() {
-    var __globalThis = "undefined" !== typeof globalThis && globalThis;
-    var __window = "undefined" !== typeof window && window;
-    var __self = "undefined" !== typeof self && "undefined" !== typeof WorkerGlobalScope && self instanceof WorkerGlobalScope && self;
-    var __global = "undefined" !== typeof global && global;
-    return __globalThis || __global || __window || __self;
-}
-
-var _global = getGlobal();
-
-if ("undefined" === typeof ngI18nClosureMode) _global["ngI18nClosureMode"] = "undefined" !== typeof goog && "function" === typeof goog.getMsg;
