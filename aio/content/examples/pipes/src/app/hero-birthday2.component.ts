cbc9a51aaf44310b88accadf1d5df5d3f9bef9a2
Janghyun Han <han41858@gmail.com>
2019-06-03 02:53:41 +0900

    docs: translation error fixed #32

--- aio/content/examples/pipes/src/app/hero-birthday2.component.ts
+++ aio/content/examples/pipes/src/app/hero-birthday2.component.ts
@@ -13,7 +13,7 @@ import { Component } from '@angular/core';
 // #docregion class
 export class HeroBirthday2Component {
   birthday = new Date(1988, 3, 15); // April 15, 1988
-  toggle = true; // 기본 형식을 shortDate로 지정하기 위해 ture 값을 할당합니다.
+  toggle = true; // 기본 형식을 shortDate로 지정하기 위해 true 값을 할당합니다.
 
   get format()   { return this.toggle ? 'shortDate' : 'fullDate'; }
   toggleFormat() { this.toggle = !this.toggle; }
