31c2167db646d0e948f21da82847a24fa1f411b8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-06-10 19:08:54 +0200

    feat(ivy): introduce missing-injectable migration for google3
    Introduces a new migration schematic for adding the "@Injectable()"
    decorator to provider classes which are currently not migrated. Previously
    in ViewEngine, classes which are declared as providers sometimes don't
    require the "@Injectable()" decorator
    (e.g. https://stackblitz.com/edit/angular-hpo7gw)
    
    With Ivy, provider classes need to be explicitly decorated with
    the "@Injectable()" decorator if they are declared as providers
    of a given module. This commit introduces a migration schematic
    which automatically adds the explicit decorator to places where
    the decorator is currently missing.
    
    The migration logic is designed in a CLI devkit and TSlint agnostic
    way so that we can also have this migration run as part of a public
    CLI migration w/ `ng update`. This will be handled as part of a follow-up to reiterate on console output etc.
    
    Resolves FW-1371

---
+++ packages/core/schematics/migrations/missing-injectable/google3/tslint_update_recorder.ts
@@ -0,0 +1,42 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Replacement, RuleFailure} from 'tslint';
+import * as ts from 'typescript';
+
+import {UpdateRecorder} from '../update_recorder';
+
+export class TslintUpdateRecorder implements UpdateRecorder {
+  failures: RuleFailure[] = [];
+
+  constructor(private ruleName: string, private sourceFile: ts.SourceFile) {}
+
+  addClassDecorator(node: ts.ClassDeclaration, decoratorText: string) {
+    this.failures.push(new RuleFailure(
+        this.sourceFile, node.getStart(), 0, `Class needs to be decorated with: ${decoratorText}`,
+        this.ruleName, Replacement.appendText(node.getStart(), `${decoratorText}\n`)));
+  }
+
+  addNewImport(start: number, importText: string) {
+    this.failures.push(new RuleFailure(
+        this.sourceFile, start, 0, `Source file needs to have import: "${importText}"`,
+        this.ruleName, Replacement.appendText(start, importText)));
+  }
+
+  updateExistingImport(namedBindings: ts.NamedImports, newNamedBindings: string): void {
+    const fix = [
+      Replacement.deleteText(namedBindings.getStart(), namedBindings.getWidth()),
+      Replacement.appendText(namedBindings.getStart(), newNamedBindings),
+    ];
+    this.failures.push(new RuleFailure(
+        this.sourceFile, namedBindings.getStart(), namedBindings.getEnd(),
+        `Import needs to be updated to import symbols: "${newNamedBindings}"`, this.ruleName, fix));
+  }
+
+  commitUpdate() {}
+}
---
+++ packages/core/schematics/migrations/missing-injectable/transform.ts
@@ -0,0 +1,116 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Reference} from '@angular/compiler-cli/src/ngtsc/imports';
+import {DynamicValue, PartialEvaluator, ResolvedValue} from '@angular/compiler-cli/src/ngtsc/partial_evaluator';
+import {TypeScriptReflectionHost} from '@angular/compiler-cli/src/ngtsc/reflection';
+import * as ts from 'typescript';
+
+import {getAngularDecorators} from '../../utils/ng_decorators';
+
+import {ImportManager} from './import_manager';
+import {ResolvedNgModule} from './module_collector';
+import {UpdateRecorder} from './update_recorder';
+
+/** Name of decorators which imply that a given class does not need to be migrated. */
+const NO_MIGRATE_DECORATORS = ['Injectable', 'Directive', 'Component', 'Pipe'];
+
+export interface AnalysisFailure {
+  node: ts.Node;
+  message: string;
+}
+
+export class MissingInjectableTransform {
+  private printer = ts.createPrinter();
+  private importManager = new ImportManager(this.getUpdateRecorder, this.printer);
+  private partialEvaluator: PartialEvaluator;
+
+  /** Set of provider class declarations which were already checked or migrated. */
+  private visitedProviderClasses = new Set<ts.ClassDeclaration>();
+
+  constructor(
+      private typeChecker: ts.TypeChecker,
+      private getUpdateRecorder: (sf: ts.SourceFile) => UpdateRecorder) {
+    this.partialEvaluator =
+        new PartialEvaluator(new TypeScriptReflectionHost(typeChecker), typeChecker);
+  }
+
+  recordChanges() { this.importManager.recordChanges(); }
+
+  /** Migrates a given NgModule by walking through the referenced providers. */
+  migrateModule(module: ResolvedNgModule): AnalysisFailure[] {
+    if (module.providersExpr === null) {
+      return [];
+    }
+
+    const evaluatedExpr = this.partialEvaluator.evaluate(module.providersExpr);
+
+    if (!(evaluatedExpr instanceof Array)) {
+      return [{
+        node: module.providersExpr,
+        message: 'Providers of module are not statically analyzable.'
+      }];
+    }
+
+    return this._visitProviderResolvedValue(evaluatedExpr);
+  }
+
+  /**
+   * Migrates a given provider class if it is not decorated with
+   * any Angular decorator.
+   */
+  migrateProviderClass(node: ts.ClassDeclaration) {
+    if (this.visitedProviderClasses.has(node)) {
+      return;
+    }
+    this.visitedProviderClasses.add(node);
+
+    if (node.decorators &&
+        getAngularDecorators(this.typeChecker, node.decorators)
+            .some(d => NO_MIGRATE_DECORATORS.indexOf(d.name) !== -1)) {
+      return;
+    }
+
+    const sourceFile = node.getSourceFile();
+
+    const importExpr =
+        this.importManager.addImportToSourceFile(sourceFile, 'Injectable', '@angular/core');
+    const newDecoratorExpr = ts.createDecorator(ts.createCall(importExpr, undefined, undefined));
+    this.getUpdateRecorder(sourceFile)
+        .addClassDecorator(
+            node, this.printer.printNode(ts.EmitHint.Unspecified, newDecoratorExpr, sourceFile));
+  }
+
+  /**
+   * Visits the given resolved value of a provider. Providers can be nested in
+   * arrays and we need to recursively walk through the providers to be able to
+   * migrate all referenced provider classes. e.g. "providers: [[A, [B]]]".
+   */
+  private _visitProviderResolvedValue(value: ResolvedValue): AnalysisFailure[] {
+    if (value instanceof Reference && ts.isClassDeclaration(value.node)) {
+      this.migrateProviderClass(value.node);
+    } else if (value instanceof Map) {
+      if (!value.has('provide') || value.has('useValue') || value.has('useFactory')) {
+        return [];
+      }
+      if (value.has('useExisting')) {
+        return this._visitProviderResolvedValue(value.get('useExisting') !);
+      } else if (value.has('useClass')) {
+        return this._visitProviderResolvedValue(value.get('useClass') !);
+      } else {
+        return this._visitProviderResolvedValue(value.get('provide') !);
+      }
+    } else if (value instanceof Array) {
+      return value.reduce(
+          (res, v) => res.concat(this._visitProviderResolvedValue(v)), [] as AnalysisFailure[]);
+    } else if (value instanceof DynamicValue) {
+      return [{node: value.node, message: `Provider is not statically analyzable.`}];
+    }
+    return [];
+  }
+}
