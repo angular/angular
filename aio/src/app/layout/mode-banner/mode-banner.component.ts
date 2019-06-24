a55c6df07bf9d9531035ee014318da131d8c3e67
Stefanie Fluin <sjtrimble@gmail.com>
2019-06-24 13:37:25 -0700

    feat(docs-infra): add prominent archive mode banner styling (#31245)
    Fixes #25968
    
    PR Close #31245

--- aio/src/app/layout/mode-banner/mode-banner.component.ts
+++ aio/src/app/layout/mode-banner/mode-banner.component.ts
@@ -4,9 +4,9 @@ import { VersionInfo } from 'app/navigation/navigation.service';
 @Component({
   selector: 'aio-mode-banner',
   template: `
-  <div *ngIf="mode == 'archive'" class="mode-banner">
-    This is the <strong>archived documentation for Angular v{{version?.major}}.</strong>
-    Please visit <a href="https://angular.io/">angular.io</a> to see documentation for the current version of Angular.
+  <div *ngIf="mode == 'archive'" class="mode-banner alert archive-warning">
+    <p>This is the <strong>archived documentation for Angular v{{version?.major}}.</strong>
+    Please visit <a href="https://angular.io/">angular.io</a> to see documentation for the current version of Angular.</p>
   </div>
   `
 })
