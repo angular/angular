d2b64c8f93bb8e77076a6abb971f0d9da4557221
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-25 13:11:50 -0700

    Revert "build(bazel): update //packages/bazel/test/ngc-wrapped:ngc_test test (#31019)" (#31267)
    This reverts commit 708d0a2db20705e4cd88a8247bea92a8b70d575c.
    
    Reason: this causes failures in g3 with i18n extraction. See #31267.
    
    PR Close #31267

--- packages/bazel/test/ngc-wrapped/index_test.ts
+++ packages/bazel/test/ngc-wrapped/index_test.ts
@@ -13,7 +13,7 @@ import {setup} from './test_support';
 describe('ngc_wrapped', () => {
 
   it('should work', () => {
-    const {read, write, runOneBuild, writeConfig, shouldExist, basePath, typesRoots} = setup();
+    const {read, write, runOneBuild, writeConfig, shouldExist, basePath} = setup();
 
     write('some_project/index.ts', `
       import {Component} from '@angular/core';
@@ -21,8 +21,11 @@ describe('ngc_wrapped', () => {
       console.log('works: ', Component);
     `);
 
+    const tsconfig = writeConfig({
+      srcTargetPath: 'some_project',
+    });
     const typesFile = path.resolve(
-        basePath, typesRoots, 'thing',
+        tsconfig.compilerOptions.rootDir, tsconfig.compilerOptions.typeRoots[0], 'thing',
         'index.d.ts');
 
     write(typesFile, `
@@ -30,11 +33,6 @@ describe('ngc_wrapped', () => {
         declare const a = 1;
       }
     `);
-
-    writeConfig({
-      srcTargetPath: 'some_project',
-      depPaths: [path.dirname(typesFile)],
-    });
 
     // expect no error
     expect(runOneBuild()).toBe(true);
