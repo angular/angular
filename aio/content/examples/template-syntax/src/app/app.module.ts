34a9441fc6bbebe43001ce9499b2e16417c2b297
Michael Prentice <splaktar@gmail.com>
2019-06-30 02:34:15 -0400

    docs(core): in template syntax guide, make SVG example more clear (#31356)
    add e2e test for SVG template example
    fix template syntax example app
    - linting errors
    - runtime exceptions
    - template type errors
    - deprecated type casting
    - deprecated currency pipe example
    
    Relates to #30559
    
    PR Close #31356

--- aio/content/examples/template-syntax/src/app/app.module.ts
+++ aio/content/examples/template-syntax/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BigHeroDetailComponent, HeroDetailComponent } from './hero-detail.component';
 import { ClickDirective, ClickDirective2 } from './click.directive';
-import { HeroFormComponent }      from './hero-form.component';
-import { heroSwitchComponents }   from './hero-switch.components';
-import { SizerComponent }         from './sizer.component';
-import { SvgComponent }           from './svg.component';
+import { HeroFormComponent } from './hero-form.component';
+import { heroSwitchComponents } from './hero-switch.components';
+import { SizerComponent } from './sizer.component';
+import { SvgComponent } from './svg.component';
 
 @NgModule({
   imports: [
