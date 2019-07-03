8fb348df1e02f1058a64049c9850dbba582c130c
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2019-07-03 15:10:40 -0700

    feat(ivy): index template elements for selectors, attributes, directives (#31240)
    Add support for indexing elements in the indexing module.
    Opening and self-closing HTML tags have their selector indexed, as well
    as the attributes on the element and the directives applied to an
    element.
    
    PR Close #31240

--- packages/compiler-cli/src/ngtsc/indexer/test/template_spec.ts
+++ packages/compiler-cli/src/ngtsc/indexer/test/template_spec.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {AbsoluteSourceSpan, IdentifierKind} from '..';
+import {AbsoluteSourceSpan, AttributeIdentifier, ElementIdentifier, IdentifierKind} from '..';
 import {runInEachFileSystem} from '../../file_system/testing';
 import {getTemplateIdentifiers} from '../src/template';
 import * as util from './util';
@@ -20,8 +20,8 @@ function bind(template: string) {
 
 runInEachFileSystem(() => {
   describe('getTemplateIdentifiers', () => {
-    it('should generate nothing in HTML-only template', () => {
-      const refs = getTemplateIdentifiers(bind('<div></div>'));
+    it('should generate nothing in empty template', () => {
+      const refs = getTemplateIdentifiers(bind(''));
 
       expect(refs.size).toBe(0);
     });
@@ -33,14 +33,14 @@ runInEachFileSystem(() => {
     });
 
     it('should handle arbitrary whitespace', () => {
-      const template = '<div>\n\n   {{foo}}</div>';
+      const template = '\n\n   {{foo}}';
       const refs = getTemplateIdentifiers(bind(template));
 
       const [ref] = Array.from(refs);
       expect(ref).toEqual({
         name: 'foo',
         kind: IdentifierKind.Property,
-        span: new AbsoluteSourceSpan(12, 15),
+        span: new AbsoluteSourceSpan(7, 10),
       });
     });
 
@@ -75,11 +75,11 @@ runInEachFileSystem(() => {
         const refs = getTemplateIdentifiers(bind(template));
 
         const refArr = Array.from(refs);
-        expect(refArr).toEqual(jasmine.arrayContaining([{
+        expect(refArr).toContain({
           name: 'foo',
           kind: IdentifierKind.Property,
           span: new AbsoluteSourceSpan(13, 16),
-        }]));
+        });
       });
 
       it('should ignore identifiers that are not implicitly received by the template', () => {
@@ -111,11 +111,11 @@ runInEachFileSystem(() => {
         const refs = getTemplateIdentifiers(bind(template));
 
         const refArr = Array.from(refs);
-        expect(refArr).toEqual(jasmine.arrayContaining([{
+        expect(refArr).toContain({
           name: 'foo',
           kind: IdentifierKind.Method,
           span: new AbsoluteSourceSpan(13, 16),
-        }]));
+        });
       });
 
       it('should ignore identifiers that are not implicitly received by the template', () => {
@@ -125,6 +125,128 @@ runInEachFileSystem(() => {
 
         const [ref] = Array.from(refs);
         expect(ref.name).toBe('foo');
+      });
+    });
+
+    describe('generates identifiers for elements', () => {
+      it('should record elements as ElementIdentifiers', () => {
+        const template = '<test-selector>';
+        const refs = getTemplateIdentifiers(bind(template));
+        expect(refs.size).toBe(1);
+
+        const [ref] = Array.from(refs);
+        expect(ref.kind).toBe(IdentifierKind.Element);
+      });
+
+      it('should record element names as their selector', () => {
+        const template = '<test-selector>';
+        const refs = getTemplateIdentifiers(bind(template));
+        expect(refs.size).toBe(1);
+
+        const [ref] = Array.from(refs);
+        expect(ref as ElementIdentifier).toEqual({
+          name: 'test-selector',
+          kind: IdentifierKind.Element,
+          span: new AbsoluteSourceSpan(1, 14),
+          attributes: new Set(),
+          usedDirectives: new Set(),
+        });
+      });
+
+      it('should discover selectors in self-closing elements', () => {
+        const template = '<img />';
+        const refs = getTemplateIdentifiers(bind(template));
+        expect(refs.size).toBe(1);
+
+        const [ref] = Array.from(refs);
+        expect(ref as ElementIdentifier).toEqual({
+          name: 'img',
+          kind: IdentifierKind.Element,
+          span: new AbsoluteSourceSpan(1, 4),
+          attributes: new Set(),
+          usedDirectives: new Set(),
+        });
+      });
+
+      it('should discover selectors in elements with adjacent open and close tags', () => {
+        const template = '<test-selector></test-selector>';
+        const refs = getTemplateIdentifiers(bind(template));
+        expect(refs.size).toBe(1);
+
+        const [ref] = Array.from(refs);
+        expect(ref as ElementIdentifier).toEqual({
+          name: 'test-selector',
+          kind: IdentifierKind.Element,
+          span: new AbsoluteSourceSpan(1, 14),
+          attributes: new Set(),
+          usedDirectives: new Set(),
+        });
+      });
+
+      it('should discover selectors in elements with non-adjacent open and close tags', () => {
+        const template = '<test-selector> text </test-selector>';
+        const refs = getTemplateIdentifiers(bind(template));
+        expect(refs.size).toBe(1);
+
+        const [ref] = Array.from(refs);
+        expect(ref as ElementIdentifier).toEqual({
+          name: 'test-selector',
+          kind: IdentifierKind.Element,
+          span: new AbsoluteSourceSpan(1, 14),
+          attributes: new Set(),
+          usedDirectives: new Set(),
+        });
+      });
+
+      it('should discover nested selectors', () => {
+        const template = '<div><span></span></div>';
+        const refs = getTemplateIdentifiers(bind(template));
+
+        const refArr = Array.from(refs);
+        expect(refArr).toContain({
+          name: 'span',
+          kind: IdentifierKind.Element,
+          span: new AbsoluteSourceSpan(6, 10),
+          attributes: new Set(),
+          usedDirectives: new Set(),
+        });
+      });
+
+      it('should generate information about attributes', () => {
+        const template = '<div attrA attrB="val"></div>';
+        const refs = getTemplateIdentifiers(bind(template));
+
+        const [ref] = Array.from(refs);
+        const attrs = (ref as ElementIdentifier).attributes;
+        expect(attrs).toEqual(new Set<AttributeIdentifier>([
+          {
+            name: 'attrA',
+            kind: IdentifierKind.Attribute,
+            span: new AbsoluteSourceSpan(5, 10),
+          },
+          {
+            name: 'attrB',
+            kind: IdentifierKind.Attribute,
+            span: new AbsoluteSourceSpan(11, 22),
+          }
+        ]));
+      });
+
+      it('should generate information about used directives', () => {
+        const declA = util.getComponentDeclaration('class A {}', 'A');
+        const declB = util.getComponentDeclaration('class B {}', 'B');
+        const declC = util.getComponentDeclaration('class C {}', 'C');
+        const template = '<a-selector b-selector></a-selector>';
+        const boundTemplate = util.getBoundTemplate(template, {}, [
+          {selector: 'a-selector', declaration: declA},
+          {selector: '[b-selector]', declaration: declB},
+          {selector: ':not(never-selector)', declaration: declC},
+        ]);
+
+        const refs = getTemplateIdentifiers(boundTemplate);
+        const [ref] = Array.from(refs);
+        const usedDirectives = (ref as ElementIdentifier).usedDirectives;
+        expect(usedDirectives).toEqual(new Set([declA, declB, declC]));
       });
     });
   });
