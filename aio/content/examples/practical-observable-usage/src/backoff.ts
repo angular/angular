242ec8ff4258868d187fd143c3463417622938ac
Ivan Tham <pickfire@riseup.net>
2019-07-14 22:35:38 +0800

    docs(observable): backoff that propagates error
    - propagate error from backoff to allow error handling
    - add random jitter to avoid thundering herd problem

--- aio/content/examples/practical-observable-usage/src/backoff.ts
+++ aio/content/examples/practical-observable-usage/src/backoff.ts
@@ -1,23 +1,30 @@
 
-import { pipe, range, timer, zip } from 'rxjs';
+import { iif, of, pipe, throwError } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { retryWhen, map, mergeMap } from 'rxjs/operators';
+import { concatMap, delay, retryWhen } from 'rxjs/operators';
 
 function backoff(maxTries, ms) {
- return pipe(
-   retryWhen(attempts => zip(range(1, maxTries), attempts)
-     .pipe(
-       map(([i]) => i * i),
-       mergeMap(i =>  timer(i * ms))
-     )
-   )
- );
+  return pipe(
+    retryWhen(errors => errors.pipe(
+      // retry in order
+      concatMap((err, n) => iif(
+        () => n < maxTries,
+        // add delay to error with random jitter
+        of(err).pipe(delay((2 + Math.random()) ** n * ms)),
+        throwError(err)
+      ))
+    ))
+  );
 }
 
 ajax('/api/endpoint')
   .pipe(backoff(3, 250))
-  .subscribe(data => handleData(data));
+  .subscribe(data => handleData(data), err => handleError(err));
 
 function handleData(data) {
+  // ...
+}
+
+function handleError(data) {
   // ...
 }
