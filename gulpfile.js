9c11612f22d4a26d920bed19b1a0c4934242938c
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-07-25 23:50:39 -0700

    build(zone.js): add changelog gulptask for zone.js

--- gulpfile.js
+++ gulpfile.js
@@ -53,6 +53,7 @@ gulp.task('tools:build', loadTask('tools-build'));
 gulp.task('check-cycle', loadTask('check-cycle'));
 gulp.task('serve', loadTask('serve', 'default'));
 gulp.task('changelog', loadTask('changelog'));
+gulp.task('changelog:zonejs', loadTask('changelog-zonejs'));
 gulp.task('check-env', () => {/* this is a noop because the env test ran already above */});
 gulp.task('cldr:extract', loadTask('cldr', 'extract'));
 gulp.task('cldr:download', loadTask('cldr', 'download'));
