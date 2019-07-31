85ece5646af9382e14cf96a1c1cd1f0743641f19
George Kalpakas <kalpakas.g@gmail.com>
2019-08-01 00:10:52 +0300

    build(docs-infra): correct list of files included in ZIPs to match current layout (#31937)
    PR Close #31937

--- aio/tools/example-zipper/exampleZipper.js
+++ aio/tools/example-zipper/exampleZipper.js
@@ -84,25 +84,23 @@ class ExampleZipper {
     const outputFileName = path.join(outputDirName, relativeDirName, exampleZipName + '.zip');
     let defaultIncludes = ['**/*.ts', '**/*.js', '**/*.es6', '**/*.css', '**/*.html', '**/*.md', '**/*.json', '**/*.png', '**/*.svg'];
     let alwaysIncludes = [
-      'bs-config.json',
-      'e2e/protractor.conf.js',
-      'angular.json',
       '.editorconfig',
       '.gitignore',
-      'tslint.json',
+      'angular.json',
+      'browserslist',
+      'bs-config.json',
+      'karma.conf.js',
       'karma-test-shim.js',
-      'tsconfig.json',
-      'src/testing/**/*',
-      'src/.babelrc',
-      'src/browserslist',
+      'tsconfig.*',
+      'tslint.*',
+      'e2e/protractor.conf.js',
+      'e2e/tsconfig.json',
       'src/favicon.ico',
-      'src/karma.conf.js',
       'src/polyfills.ts',
       'src/test.ts',
       'src/typings.d.ts',
       'src/environments/**/*',
-      'src/tsconfig.*',
-      'src/tslint.*',
+      'src/testing/**/*',
       // Only ignore root package.json
       '!package.json'
     ];
