cd1002c3391415958b74a36c83935ebaae8c014e
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2019-07-31 14:47:25 -0700

    fix(ivy): correctly associate output bound events with directives
    Previously, bound events were incorrectly bound to directives with
    inputs matching the bound event attribute. This fixes that so bound
    events can only be bound to directives with matching outputs.
    
    Adds tests for all kinds of directive matching on bound attributes.

--- packages/compiler/src/render3/view/t2_binder.ts
+++ packages/compiler/src/render3/view/t2_binder.ts
@@ -269,20 +269,29 @@ class DirectiveBinder<DirectiveT extends DirectiveMeta> implements Visitor {
     });
 
     // Associate attributes/bindings on the node with directives or with the node itself.
-    const processAttribute = (attribute: BoundAttribute | BoundEvent | TextAttribute) => {
-      let dir = directives.find(dir => dir.inputs.hasOwnProperty(attribute.name));
-      if (dir !== undefined) {
-        this.bindings.set(attribute, dir);
-      } else {
-        this.bindings.set(attribute, node);
-      }
-    };
-    node.attributes.forEach(processAttribute);
-    node.inputs.forEach(processAttribute);
-    node.outputs.forEach(processAttribute);
+    type BoundNode = BoundAttribute | BoundEvent | TextAttribute;
+    const processAttribute =
+        (attribute: BoundNode, filter: (attribute: BoundNode) => DirectiveT | undefined) => {
+          const dir = filter(attribute);
+          if (dir !== undefined) {
+            this.bindings.set(attribute, dir);
+          } else {
+            this.bindings.set(attribute, node);
+          }
+        };
+    const filterInputs = (attribute: BoundNode) =>
+        directives.find(dir => dir.inputs.hasOwnProperty(attribute.name));
+    const filterOutputs = (attribute: BoundNode) =>
+        directives.find(dir => dir.outputs.hasOwnProperty(attribute.name));
+    // Node attributes, template attributes, and node inputs can be bound to an input on a
+    // directive.
+    node.attributes.forEach(attr => processAttribute(attr, filterInputs));
+    node.inputs.forEach(input => processAttribute(input, filterInputs));
     if (node instanceof Template) {
-      node.templateAttrs.forEach(processAttribute);
+      node.templateAttrs.forEach(attr => processAttribute(attr, filterInputs));
     }
+    // Node outputs can be bound to an output on a directive.
+    node.outputs.forEach(output => processAttribute(output, filterOutputs));
 
     // Recurse into the node's children.
     node.children.forEach(child => child.visit(this));
