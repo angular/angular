e918ffea494a5b44067e4655dd4e7b531bfcea9f
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-08-01 13:41:37 -0600

    build(zone.js): update gulp task to gen changelog automatically

--- tools/gulp-tasks/changelog-zonejs.js
+++ tools/gulp-tasks/changelog-zonejs.js
@@ -10,13 +10,20 @@ module.exports = (gulp) => () => {
   const tag = process.env.TAG;
   const ptag = process.env.PREVIOUS_ZONE_TAG;
   const conventionalChangelog = require('gulp-conventional-changelog');
+  // the tag of zone.js will start with `zone.js-`, such as `zone.js-0.10.0`
+  // we will remove the first 8 (zone.js-) chars to get the real version.
+  const version = tag.replace(/^zone\.js-/, '');
   return gulp.src('packages/zone.js/CHANGELOG.md')
-      .pipe(conventionalChangelog({preset: 'angular'}, {}, {
-        // Ignore commits that have a different scope than `zone.js`.
-        extendedRegexp: true,
-        grep: '^[^(]+\\(zone\\.js\\)',
-        from: ptag,
-        to: tag,
-      }))
+      .pipe(conventionalChangelog(
+          {
+            preset: 'angular',
+          },
+          {linkCompare: true, previousTag: ptag, currentTag: tag, version: version}, {
+            // Ignore commits that have a different scope than `zone.js`.
+            extendedRegexp: true,
+            grep: '^[^(]+\\(zone\\.js\\)',
+            from: ptag,
+            to: tag,
+          }))
       .pipe(gulp.dest('./packages/zone.js/'));
 };
