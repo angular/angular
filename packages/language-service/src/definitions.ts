fad797986c7a5c972fdef09d994f90aebe9e3eb2
Keen Yee Liau <kyliau@google.com>
2019-08-01 11:15:53 -0700

    fixup! feat(language-service): Introduce 'angularOnly' flag

--- packages/language-service/src/definitions.ts
+++ packages/language-service/src/definitions.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import * as tss from 'typescript/lib/tsserverlibrary'
+import * as tss from 'typescript/lib/tsserverlibrary';
 
 import {TemplateInfo} from './common';
 import {locateSymbol} from './locate_symbol';
