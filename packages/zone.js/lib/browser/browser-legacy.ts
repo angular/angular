86f9cd43eba2a17e2b52bdff062651d738f99557
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-08-06 09:54:54 +0900

    fix(zone.js): browser-legacy should not reference Zone

--- packages/zone.js/lib/browser/browser-legacy.ts
+++ packages/zone.js/lib/browser/browser-legacy.ts
@@ -10,15 +10,20 @@
  * @suppress {missingRequire}
  */
 
-import {propertyPatch} from './define-property';
+import {_redefineProperty, propertyPatch} from './define-property';
 import {eventTargetLegacyPatch} from './event-target-legacy';
 import {propertyDescriptorLegacyPatch} from './property-descriptor-legacy';
 import {registerElementPatch} from './register-element';
 
 (function(_global: any) {
-  _global[Zone.__symbol__('legacyPatch')] = function() {
+  const symbolPrefix = _global['__Zone_symbol_prefix'] || '__zone_symbol__';
+  function __symbol__(name: string) { return symbolPrefix + name; }
+  _global[__symbol__('legacyPatch')] = function() {
     const Zone = _global['Zone'];
-    Zone.__load_patch('defineProperty', () => { propertyPatch(); });
+    Zone.__load_patch('defineProperty', (global: any, Zone: ZoneType, api: _ZonePrivate) => {
+      api._redefineProperty = _redefineProperty;
+      propertyPatch();
+    });
     Zone.__load_patch('registerElement', (global: any, Zone: ZoneType, api: _ZonePrivate) => {
       registerElementPatch(global, api);
     });
