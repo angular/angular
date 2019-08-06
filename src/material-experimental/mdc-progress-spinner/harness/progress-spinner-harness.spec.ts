725a2b3c5d45f97336548c83575f67b53ddfe291
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-08-06 12:17:43 -0700

    feat(material-experimental): add progress spinner test harness… (#16622)
    * feat(material-experimental): add progress spinner test harness skeleton
    
    * use number coercion
    
    * fix build
    
    * address feedback

---
+++ src/material-experimental/mdc-progress-spinner/harness/progress-spinner-harness.spec.ts
@@ -0,0 +1,59 @@
+import {HarnessLoader} from '@angular/cdk-experimental/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk-experimental/testing/testbed';
+import {Component} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MatProgressSpinnerModule, ProgressSpinnerMode} from '@angular/material/progress-spinner';
+
+import {MatProgressSpinnerHarness} from './progress-spinner-harness';
+
+let fixture: ComponentFixture<ProgressSpinnerHarnessTest>;
+let loader: HarnessLoader;
+let progressSpinnerHarness: typeof MatProgressSpinnerHarness;
+
+describe('MatProgressSpinnerHarness', () => {
+  beforeEach(async () => {
+    await TestBed
+        .configureTestingModule({
+          imports: [MatProgressSpinnerModule],
+          declarations: [ProgressSpinnerHarnessTest],
+        })
+        .compileComponents();
+
+    fixture = TestBed.createComponent(ProgressSpinnerHarnessTest);
+    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    progressSpinnerHarness = MatProgressSpinnerHarness;
+  });
+
+  runTests();
+});
+
+function runTests() {
+  it('should load all progress spinner harnesses', async () => {
+    const progressSpinners = await loader.getAllHarnesses(progressSpinnerHarness);
+    expect(progressSpinners.length).toBe(2);
+  });
+
+  it('should get the value', async () => {
+    fixture.componentInstance.value = 50;
+    const [determinate, indeterminate] = await loader.getAllHarnesses(progressSpinnerHarness);
+    expect(await determinate.getValue()).toBe(50);
+    expect(await indeterminate.getValue()).toBe(null);
+  });
+
+  it('should get the mode', async () => {
+    const [determinate, indeterminate] = await loader.getAllHarnesses(progressSpinnerHarness);
+    expect<ProgressSpinnerMode>(await determinate.getMode()).toBe('determinate');
+    expect<ProgressSpinnerMode>(await indeterminate.getMode()).toBe('indeterminate');
+  });
+}
+
+@Component({
+  template: `
+    <mat-progress-spinner mode="determinate" [value]="value"></mat-progress-spinner>
+    <mat-progress-spinner mode="indeterminate"></mat-progress-spinner>
+  `
+})
+class ProgressSpinnerHarnessTest {
+  value: number;
+}
---
+++ src/material-experimental/mdc-progress-spinner/harness/progress-spinner-harness.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {ComponentHarness} from '@angular/cdk-experimental/testing';
+import {ProgressSpinnerMode} from '@angular/material/progress-spinner';
+import {coerceNumberProperty} from '@angular/cdk/coercion';
+
+/**
+ * Harness for interacting with a standard mat-progress-spinner in tests.
+ * @dynamic
+ */
+export class MatProgressSpinnerHarness extends ComponentHarness {
+  static hostSelector = 'mat-progress-spinner';
+
+  /** Gets a promise for the progress spinner's value. */
+  async getValue(): Promise<number|null> {
+    const host = await this.host();
+    const ariaValue = await host.getAttribute('aria-valuenow');
+    return ariaValue ? coerceNumberProperty(ariaValue) : null;
+  }
+
+  /** Gets a promise for the progress spinner's mode. */
+  async getMode(): Promise<ProgressSpinnerMode> {
+    const modeAttr = (await this.host()).getAttribute('mode');
+    return await modeAttr as ProgressSpinnerMode;
+  }
+}
