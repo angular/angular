38b90b6abda6fb19cc2a652fabd41e7b596064fc
Kristiyan Kostadinov <crisbeto@abv.bg>
2019-08-15 12:40:53 +0300

    feat(core): add undecorated classes with decorated fields schematic (#32130)
    Adds a schematic that adds a `Directive` decorator to undecorated classes that have fields that use Angular decorators.
    
    PR Close #32130

---
+++ packages/core/schematics/migrations/undecorated-classes-with-decorated-fields/index.ts
@@ -0,0 +1,93 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Rule, SchematicContext, SchematicsException, Tree} from '@angular-devkit/schematics';
+import {dirname, relative} from 'path';
+import * as ts from 'typescript';
+
+import {getProjectTsConfigPaths} from '../../utils/project_tsconfig_paths';
+import {parseTsconfigFile} from '../../utils/typescript/parse_tsconfig';
+import {FALLBACK_DECORATOR, addImport, getNamedImports, getUndecoratedClassesWithDecoratedFields, hasNamedImport} from './utils';
+
+
+/**
+ * Migration that adds an Angular decorator to classes that have Angular field decorators.
+ * https://hackmd.io/vuQfavzfRG6KUCtU7oK_EA
+ */
+export default function(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const {buildPaths, testPaths} = getProjectTsConfigPaths(tree);
+    const basePath = process.cwd();
+    const allPaths = [...buildPaths, ...testPaths];
+    const logger = context.logger;
+
+    logger.info('------ Undecorated classes with decorated fields migration ------');
+    logger.info(
+        'As of Angular 9, it is no longer supported to have Angular field ' +
+        'decorators on a class that does not have an Angular decorator.');
+
+    if (!allPaths.length) {
+      throw new SchematicsException(
+          'Could not find any tsconfig file. Cannot add an Angular decorator to undecorated classes.');
+    }
+
+    for (const tsconfigPath of allPaths) {
+      runUndecoratedClassesMigration(tree, tsconfigPath, basePath);
+    }
+  };
+}
+
+function runUndecoratedClassesMigration(tree: Tree, tsconfigPath: string, basePath: string) {
+  const parsed = parseTsconfigFile(tsconfigPath, dirname(tsconfigPath));
+  const host = ts.createCompilerHost(parsed.options, true);
+
+  // We need to overwrite the host "readFile" method, as we want the TypeScript
+  // program to be based on the file contents in the virtual file tree. Otherwise
+  // if we run the migration for multiple tsconfig files which have intersecting
+  // source files, it can end up updating them multiple times.
+  host.readFile = fileName => {
+    const buffer = tree.read(relative(basePath, fileName));
+    // Strip BOM as otherwise TSC methods (Ex: getWidth) will return an offset which
+    // which breaks the CLI UpdateRecorder.
+    // See: https://github.com/angular/angular/pull/30719
+    return buffer ? buffer.toString().replace(/^\uFEFF/, '') : undefined;
+  };
+
+  const program = ts.createProgram(parsed.fileNames, parsed.options, host);
+  const typeChecker = program.getTypeChecker();
+  const printer = ts.createPrinter();
+  const sourceFiles = program.getSourceFiles().filter(
+      file => !file.isDeclarationFile && !program.isSourceFileFromExternalLibrary(file));
+
+  sourceFiles.forEach(sourceFile => {
+    const update = tree.beginUpdate(relative(basePath, sourceFile.fileName));
+    const classes = getUndecoratedClassesWithDecoratedFields(sourceFile, typeChecker);
+
+    classes.forEach((current, index) => {
+      // If it's the first class that we're processing in this file, add `Directive` to the imports.
+      if (index === 0 && !hasNamedImport(current.importDeclaration, FALLBACK_DECORATOR)) {
+        const namedImports = getNamedImports(current.importDeclaration);
+
+        if (namedImports) {
+          update.remove(namedImports.getStart(), namedImports.getWidth());
+          update.insertRight(
+              namedImports.getStart(),
+              printer.printNode(
+                  ts.EmitHint.Unspecified, addImport(namedImports, FALLBACK_DECORATOR),
+                  sourceFile));
+        }
+      }
+
+      // We don't need to go through the AST to insert the decorator, because the change
+      // is pretty basic. Also this has a better chance of preserving the user's formatting.
+      update.insertLeft(current.classDeclaration.getStart(), `@${FALLBACK_DECORATOR}()\n`);
+    });
+
+    tree.commitUpdate(update);
+  });
+}
---
+++ packages/core/schematics/test/undecorated_classes_with_decorated_fields_migration_spec.ts
@@ -0,0 +1,218 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {getSystemPath, normalize, virtualFs} from '@angular-devkit/core';
+import {TempScopedNodeJsSyncHost} from '@angular-devkit/core/node/testing';
+import {HostTree} from '@angular-devkit/schematics';
+import {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
+import * as shx from 'shelljs';
+
+describe('Undecorated classes with decorated fields migration', () => {
+  let runner: SchematicTestRunner;
+  let host: TempScopedNodeJsSyncHost;
+  let tree: UnitTestTree;
+  let tmpDirPath: string;
+  let previousWorkingDir: string;
+
+  beforeEach(() => {
+    runner = new SchematicTestRunner('test', require.resolve('../migrations.json'));
+    host = new TempScopedNodeJsSyncHost();
+    tree = new UnitTestTree(new HostTree(host));
+
+    writeFile('/tsconfig.json', JSON.stringify({compilerOptions: {lib: ['es2015']}}));
+    writeFile('/angular.json', JSON.stringify({
+      projects: {t: {architect: {build: {options: {tsConfig: './tsconfig.json'}}}}}
+    }));
+
+    previousWorkingDir = shx.pwd();
+    tmpDirPath = getSystemPath(host.root);
+
+    // Switch into the temporary directory path. This allows us to run
+    // the schematic against our custom unit test tree.
+    shx.cd(tmpDirPath);
+  });
+
+  afterEach(() => {
+    shx.cd(previousWorkingDir);
+    shx.rm('-r', tmpDirPath);
+  });
+
+  it(`should add an import for Directive if there isn't one already`, async() => {
+    writeFile('/index.ts', `
+      import { Input } from '@angular/core';
+
+      export class Base {
+        @Input() isActive: boolean;
+      }
+    `);
+
+    await runMigration();
+    expect(tree.readContent('/index.ts'))
+        .toContain(`import { Input, Directive } from '@angular/core';`);
+  });
+
+  it('should not change the imports if there is an import for Directive already', async() => {
+    writeFile('/index.ts', `
+      import { Directive, Input } from '@angular/core';
+
+      export class Base {
+        @Input() isActive: boolean;
+      }
+
+      @Directive()
+      export class Child extends Base {
+      }
+    `);
+
+    await runMigration();
+    expect(tree.readContent('/index.ts'))
+        .toContain(`import { Directive, Input } from '@angular/core';`);
+  });
+
+  it('should add @Directive to undecorated classes that have @Input', async() => {
+    writeFile('/index.ts', `
+      import { Input } from '@angular/core';
+
+      export class Base {
+        @Input() isActive: boolean;
+      }
+    `);
+
+    await runMigration();
+    expect(tree.readContent('/index.ts')).toContain(`@Directive()\nexport class Base {`);
+  });
+
+  it('should not change decorated classes', async() => {
+    writeFile('/index.ts', `
+      import { Input, Component, Output, EventEmitter } from '@angular/core';
+
+      @Component({})
+      export class Base {
+        @Input() isActive: boolean;
+      }
+
+      export class Child extends Base {
+        @Output() clicked = new EventEmitter<void>();
+      }
+    `);
+
+    await runMigration();
+    const content = tree.readContent('/index.ts');
+    expect(content).toContain(
+        `import { Input, Component, Output, EventEmitter, Directive } from '@angular/core';`);
+    expect(content).toContain(`@Component({})\n      export class Base {`);
+    expect(content).toContain(`@Directive()\nexport class Child extends Base {`);
+  });
+
+  it('should add @Directive to undecorated classes that have @Output', async() => {
+    writeFile('/index.ts', `
+      import { Output, EventEmitter } from '@angular/core';
+
+      export class Base {
+        @Output() clicked = new EventEmitter<void>();
+      }
+    `);
+
+    await runMigration();
+    expect(tree.readContent('/index.ts')).toContain(`@Directive()\nexport class Base {`);
+  });
+
+  it('should add @Directive to undecorated classes that have a host binding', async() => {
+    writeFile('/index.ts', `
+      import { HostBinding } from '@angular/core';
+
+      export class Base {
+        @HostBinding('attr.id')
+        get id() {
+          return 'id-' + Date.now();
+        }
+      }
+    `);
+
+    await runMigration();
+    expect(tree.readContent('/index.ts')).toContain(`@Directive()\nexport class Base {`);
+  });
+
+  it('should add @Directive to undecorated classes that have a host listener', async() => {
+    writeFile('/index.ts', `
+      import { HostListener } from '@angular/core';
+
+      export class Base {
+        @HostListener('keydown')
+        handleKeydown() {
+          console.log('Key has been pressed');
+        }
+      }
+    `);
+
+    await runMigration();
+    expect(tree.readContent('/index.ts')).toContain(`@Directive()\nexport class Base {`);
+  });
+
+  it('should add @Directive to undecorated classes that have a ViewChild query', async() => {
+    writeFile('/index.ts', `
+      import { ViewChild, ElementRef } from '@angular/core';
+
+      export class Base {
+        @ViewChild('button', { static: false }) button: ElementRef<HTMLElement>;
+      }
+    `);
+
+    await runMigration();
+    expect(tree.readContent('/index.ts')).toContain(`@Directive()\nexport class Base {`);
+  });
+
+  it('should add @Directive to undecorated classes that have a ViewChildren query', async() => {
+    writeFile('/index.ts', `
+      import { ViewChildren, ElementRef } from '@angular/core';
+
+      export class Base {
+        @ViewChildren('button') button: ElementRef<HTMLElement>;
+      }
+    `);
+
+    await runMigration();
+    expect(tree.readContent('/index.ts')).toContain(`@Directive()\nexport class Base {`);
+  });
+
+  it('should add @Directive to undecorated classes that have a ContentChild query', async() => {
+    writeFile('/index.ts', `
+      import { ContentChild, ElementRef } from '@angular/core';
+
+      export class Base {
+        @ContentChild('button', { static: false }) button: ElementRef<HTMLElement>;
+      }
+    `);
+
+    await runMigration();
+    expect(tree.readContent('/index.ts')).toContain(`@Directive()\nexport class Base {`);
+  });
+
+  it('should add @Directive to undecorated classes that have a ContentChildren query', async() => {
+    writeFile('/index.ts', `
+      import { ContentChildren, ElementRef } from '@angular/core';
+
+      export class Base {
+        @ContentChildren('button') button: ElementRef<HTMLElement>;
+      }
+    `);
+
+    await runMigration();
+    expect(tree.readContent('/index.ts')).toContain(`@Directive()\nexport class Base {`);
+  });
+
+  function writeFile(filePath: string, contents: string) {
+    host.sync.write(normalize(filePath), virtualFs.stringToFileBuffer(contents));
+  }
+
+  function runMigration() {
+    return runner
+        .runSchematicAsync('migration-v9-undecorated-classes-with-decorated-fields', {}, tree)
+        .toPromise();
+  }
+});
