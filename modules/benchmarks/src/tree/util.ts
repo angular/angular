2d76677da0407f66ba47ca90f5e19cb2fd72d0ef
Misko Hevery <misko@hevery.com>
2019-08-15 20:59:08 -0700

    fixup! perf: don't create holey arrays

--- modules/benchmarks/src/tree/util.ts
+++ modules/benchmarks/src/tree/util.ts
@@ -69,10 +69,12 @@ export function flattenTree(node: TreeNode, target: TreeNode[] = []): TreeNode[]
   return target;
 }
 
-export function newArray(size: number): any[] {
+export function newArray(size: number): any[];
+export function newArray<T>(size: number, value: T): T[];
+export function newArray<T>(size: number, value?: T): any[] {
   const list: any[] = [];
   for (let i = 0; i < size; i++) {
-    list.push();
+    list.push(value);
   }
   return list;
 }
