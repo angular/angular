angular/angular

commit 9537b2ff8459c445f05545520e3076ab1b5c2bbd

refactor(ivy): ngcc - fix return type on `makeEntryPointBundle()` (#32052)
In commit 7b55ba58b (part of PR #29092), the implementation of
`makeEntryPointBundle()` was changed such that it now always return
`EntryPointBundle` (and not `null`).
However, the return type was not updated and as result we continued to
unnecessarily handle `null` as a potential return value in some places.

This commit fixes the return type to reflect the implementation and
removes the redundant code that was dealing with `null`.

PR Close #32052

packages/compiler-cli/ngcc/src/main.ts
@@ -134,24 +134,19 @@ export function mainNgcc(
             fileSystem, entryPoint, formatPath, isCore, property, format, processDts, pathMappings,
             true);
 
-        if (bundle) {
-          logger.info(`Compiling ${entryPoint.name} : ${property} as ${format}`);
-          const transformedFiles = transformer.transform(bundle);
-          fileWriter.writeBundle(entryPoint, bundle, transformedFiles);
-          compiledFormats.add(formatPath);
+        logger.info(`Compiling ${entryPoint.name} : ${property} as ${format}`);
+        const transformedFiles = transformer.transform(bundle);
+        fileWriter.writeBundle(entryPoint, bundle, transformedFiles);
+        compiledFormats.add(formatPath);
 
-          const propsToMarkAsProcessed = pathToPropsMap.get(formatPath) !;
-          if (processDts) {
-            propsToMarkAsProcessed.push('typings');
-            processDts = false;
-          }
-
-          markAsProcessed(
-              fileSystem, entryPointPackageJson, entryPointPackageJsonPath, propsToMarkAsProcessed);
-        } else {
-          logger.warn(
-              `Skipping ${entryPoint.name} : ${format} (no valid entry point file for this format).`);
+        const propsToMarkAsProcessed = pathToPropsMap.get(formatPath) !;
+        if (processDts) {
+          propsToMarkAsProcessed.push('typings');
+          processDts = false;
         }
+
+        markAsProcessed(
+            fileSystem, entryPointPackageJson, entryPointPackageJsonPath, propsToMarkAsProcessed);
       }
     }
 

packages/compiler-cli/ngcc/src/packages/entry_point_bundle.ts
@@ -44,7 +44,7 @@ export interface EntryPointBundle {
 export function makeEntryPointBundle(
     fs: FileSystem, entryPoint: EntryPoint, formatPath: string, isCore: boolean,
     formatProperty: EntryPointJsonProperty, format: EntryPointFormat, transformDts: boolean,
-    pathMappings?: PathMappings, mirrorDtsFromSrc: boolean = false): EntryPointBundle|null {
+    pathMappings?: PathMappings, mirrorDtsFromSrc: boolean = false): EntryPointBundle {
   // Create the TS program and necessary helpers.
   const options: ts.CompilerOptions = {
     allowJs: true,

packages/compiler-cli/ngcc/test/packages/entry_point_bundle_spec.ts
@@ -145,7 +145,7 @@ runInEachFileSystem(() => {
            compiledByAngular: true,
          };
          const esm5bundle =
-             makeEntryPointBundle(fs, entryPoint, './index.js', false, 'esm5', 'esm5', true) !;
+             makeEntryPointBundle(fs, entryPoint, './index.js', false, 'esm5', 'esm5', true);
 
          expect(esm5bundle.src.program.getSourceFiles().map(sf => sf.fileName))
              .toEqual(jasmine.arrayWithExactContents([
@@ -193,7 +193,7 @@ runInEachFileSystem(() => {
          };
          const esm5bundle = makeEntryPointBundle(
              fs, entryPoint, './index.js', false, 'esm5', 'esm5', /* transformDts */ true,
-             /* pathMappings */ undefined, /* mirrorDtsFromSrc */ true) !;
+             /* pathMappings */ undefined, /* mirrorDtsFromSrc */ true);
          expect(esm5bundle.src.program.getSourceFiles().map(sf => sf.fileName))
              .toContain(absoluteFrom('/node_modules/test/internal.js'));
          expect(esm5bundle.dts !.program.getSourceFiles().map(sf => sf.fileName))
@@ -214,7 +214,7 @@ runInEachFileSystem(() => {
          };
          const esm5bundle = makeEntryPointBundle(
              fs, entryPoint, './index.js', false, 'esm5', 'esm5', /* transformDts */ true,
-             /* pathMappings */ undefined, /* mirrorDtsFromSrc */ false) !;
+             /* pathMappings */ undefined, /* mirrorDtsFromSrc */ false);
          expect(esm5bundle.src.program.getSourceFiles().map(sf => sf.fileName))
              .toContain(absoluteFrom('/node_modules/test/internal.js'));
          expect(esm5bundle.dts !.program.getSourceFiles().map(sf => sf.fileName))

packages/compiler-cli/ngcc/test/writing/new_entry_point_file_writer_spec.ts
@@ -357,6 +357,6 @@ runInEachFileSystem(() => {
       format: EntryPointFormat): EntryPointBundle {
     return makeEntryPointBundle(
         fs, entryPoint, entryPoint.packageJson[formatProperty] !, false, formatProperty, format,
-        true) !;
+        true);
   }
 });

commit 0bfccadd7ebf0742a47ce5ecf71e43ebe98d5140

fix(ngcc): ignore format properties that exist but are undefined
Previously, `ngcc` assumed that if a format property was defined in
`package.json` it would point to a valid format-path (i.e. a file that
is an entry-point for a specific format). This is generally the case,
except if a format property is set to a non-string value (such as
`package.json`) - either directly in the `package.json` (which is unusual)
or in ngcc.config.js (which is a valid usecase, when one wants a
format property to be ignored by `ngcc`).

For example, the following config file would cause `ngcc` to throw:

```
module.exports = {
  packages: {
    'test-package': {
      entryPoints: {
        '.': {
          override: {
            fesm2015: undefined,
          },
        },
      },
    },
  },
};
```

This commit fixes it by ensuring that only format properties whose value
is a string are considered by `ngcc`.

For reference, this regression was introduced in #32052.

Fixes #32188

packages/compiler-cli/ngcc/src/main.ts
@@ -375,10 +375,10 @@ function getPropertiesToProcessAndMarkAsProcessed(
 
   const propertiesToProcess: EntryPointJsonProperty[] = [];
   for (const prop of propertiesToConsider) {
-    // Ignore properties that are not in `package.json`.
-    if (!packageJson.hasOwnProperty(prop)) continue;
+    const formatPath = packageJson[prop];
 
-    const formatPath = packageJson[prop] !;
+    // Ignore properties that are not defined in package.json.
+    if (typeof formatPath !== 'string') continue;
 
     // Ignore properties that map to the same format-path as a preceding property.
     if (formatPathsToConsider.has(formatPath)) continue;
@@ -390,10 +390,10 @@ function getPropertiesToProcessAndMarkAsProcessed(
 
   const formatPathToProperties: {[formatPath: string]: EntryPointJsonProperty[]} = {};
   for (const prop of SUPPORTED_FORMAT_PROPERTIES) {
-    // Ignore properties that are not in `package.json`.
-    if (!packageJson.hasOwnProperty(prop)) continue;
+    const formatPath = packageJson[prop];
 
-    const formatPath = packageJson[prop] !;
+    // Ignore properties that are not defined in package.json.
+    if (typeof formatPath !== 'string') continue;
 
     // Ignore properties that do not map to a format-path that will be considered.
     if (!formatPathsToConsider.has(formatPath)) continue;