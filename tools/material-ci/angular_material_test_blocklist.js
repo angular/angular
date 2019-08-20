55e8f03d3831a88d9d1a35cd5834713a749b8d86
Kara Erickson <karakara@google.com>
2019-08-20 16:15:20 -0700

    Revert "ci: remove material-unit-tests failure blocklist (#32138)" (#32226)
    This reverts commit 0660903784a32261f2ba5e057088166441779b75 so we
    can add some tests to the blocklist.
    
    PR Close #32226

---
+++ tools/material-ci/angular_material_test_blocklist.js
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * Blocklist of unit tests from angular/material2 with ivy that are skipped when running on
+ * angular/angular. As bugs are resolved, items should be removed from this blocklist.
+ *
+ * The `notes` section should be used to keep track of specific issues associated with the failures.
+ */
+
+// clang-format off
+// tslint:disable
+
+window.testBlocklist = {};
+// clang-format on
