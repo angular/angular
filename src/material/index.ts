0c1d369dcfca506f4017f36e0a7a0331855dcae3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-09-11 12:47:49 +0200

    refactor(material): remove re-exports from primary entry-point
    Removes the re-exports to all secondary entry-points from the Angular
    Material primary entry-point.
    
    BREAKING CHANGE: Components can no longer be imported through "@angular/material". Use the individual secondary entry-points.

--- src/material/index.ts
+++ src/material/index.ts
@@ -6,4 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-export * from './public-api';
+// primary entry-point which is empty as of version 9. All components should
+// be imported through their individual entry-points. This file is needed to
+// satisfy the "ng_package" bazel rule which also requires a primary entry-point.
