68633b32edb267504a809636a9d1e24d643574ea
JoostK <joost.koehoorn@gmail.com>
2019-09-12 21:08:21 +0200

    fix(ngcc): resolve imports in `.d.ts` files for UMD/CommonJS bundles (#32619)
    In ngcc's reflection host for UMD and CommonJS bundles, custom logic is
    present to resolve import details of an identifier. However, this custom
    logic is unable to resolve an import for an identifier inside of
    declaration files, as such files use the regular ESM import syntax.
    
    As a consequence of this limitation, ngtsc is unable to resolve
    `ModuleWithProviders` imports that are declared in an external library.
    In that situation, ngtsc determines the type of the actual `NgModule`
    that is imported, by looking in the library's declaration files for the
    generic type argument on `ModuleWithProviders`. In this process, ngtsc
    resolves the import for the `ModuleWithProviders` identifier to verify
    that it is indeed the `ModuleWithProviders` type from `@angular/core`.
    So, when the UMD reflection host was in use this resolution would fail,
    therefore no `NgModule` type could be detected.
    
    This commit fixes the bug by using the regular import resolution logic
    in addition to the custom resolution logic that is required for UMD
    and CommonJS bundles.
    
    Fixes #31791
    
    PR Close #32619

--- packages/compiler-cli/ngcc/src/host/umd_host.ts
+++ packages/compiler-cli/ngcc/src/host/umd_host.ts
@@ -25,6 +25,11 @@ export class UmdReflectionHost extends Esm5ReflectionHost {
   }
 
   getImportOfIdentifier(id: ts.Identifier): Import|null {
+    const superImport = super.getImportOfIdentifier(id);
+    if (superImport !== null) {
+      return superImport;
+    }
+
     const importParameter = this.findUmdImportParameter(id);
     const from = importParameter && this.getUmdImportPath(importParameter);
     return from !== null ? {from, name: id.text} : null;
