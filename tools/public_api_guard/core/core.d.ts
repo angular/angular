fece9196a90bb92ebd47c9f1b01c3e6cab679574
crisbeto <crisbeto@abv.bg>
2019-09-15 17:24:00 +0200

    feat(core): make static query flag optional (#32686)
    Switches back to having the `static` flag be optional on `ViewChild` and `ContentChild` queries, in preparation for changing its default value.
    
    PR Close #32686

--- tools/public_api_guard/core/core.d.ts
+++ tools/public_api_guard/core/core.d.ts
@@ -177,13 +177,13 @@ export interface ConstructorSansProvider {
 export declare type ContentChild = Query;
 
 export interface ContentChildDecorator {
-    (selector: Type<any> | Function | string, opts: {
+    (selector: Type<any> | Function | string, opts?: {
         read?: any;
-        static: boolean;
+        static?: boolean;
     }): any;
-    new (selector: Type<any> | Function | string, opts: {
+    new (selector: Type<any> | Function | string, opts?: {
         read?: any;
-        static: boolean;
+        static?: boolean;
     }): ContentChild;
 }
 
@@ -1151,7 +1151,7 @@ export interface Query {
     isViewQuery: boolean;
     read: any;
     selector: any;
-    static: boolean;
+    static?: boolean;
 }
 
 export declare abstract class Query {
@@ -1445,13 +1445,13 @@ export declare const VERSION: Version;
 export declare type ViewChild = Query;
 
 export interface ViewChildDecorator {
-    (selector: Type<any> | Function | string, opts: {
+    (selector: Type<any> | Function | string, opts?: {
         read?: any;
-        static: boolean;
+        static?: boolean;
     }): any;
-    new (selector: Type<any> | Function | string, opts: {
+    new (selector: Type<any> | Function | string, opts?: {
         read?: any;
-        static: boolean;
+        static?: boolean;
     }): ViewChild;
 }
 
