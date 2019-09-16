57493487c6168817518fca7c92cb515d5a8d8fce
Misko Hevery <misko@hevery.com>
2019-09-15 21:41:46 -0700

    fix(ivy): correct debug array names (#32691)
    PR Close #32691

--- packages/core/src/util/named_array_type.ts
+++ packages/core/src/util/named_array_type.ts
@@ -30,7 +30,7 @@ export function createNamedArrayType(name: string): typeof Array {
     try {
       // We need to do it this way so that TypeScript does not down-level the below code.
       const FunctionConstructor: any = createNamedArrayType.constructor;
-      return (new FunctionConstructor('Array', `return class ABC extends Array{}`))(Array);
+      return (new FunctionConstructor('Array', `return class ${name} extends Array{}`))(Array);
     } catch (e) {
       // If it does not work just give up and fall back to regular Array.
       return Array;
