bade3e5229eed74cd01d660d160f945f321c1e09
1988-03-15 <55544967+1988-03-15@users.noreply.github.com>
2019-09-19 15:43:52 +0200

    Change March to April for consistency
    I resolved an issue wherein the given example would create a Date object with the value March 15, 1988, although the comment besides it (and other appearances of the date related to it) mention a date of April 15, 1988.

--- aio/content/examples/pipes/src/app/app.component.ts
+++ aio/content/examples/pipes/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  birthday = new Date(1988, 3, 15); // April 15, 1988
+  birthday = new Date(1988, 4, 15); // April 15, 1988
 }
