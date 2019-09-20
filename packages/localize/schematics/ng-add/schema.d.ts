e41cbfb585d1e790e5ee8a8b9dc90be0986a0a00
Filipe Silva <filipematossilva@gmail.com>
2019-09-20 16:05:45 +0100

    feat(ivy): support ng-add in localize package (#32791)
    PR Close #32791

---
+++ packages/localize/schematics/ng-add/schema.d.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+export interface Schema {
+  /**
+   * The name of the project.
+   */
+  name?: string;
+}
