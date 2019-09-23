2112fac64e2f75c3691f2a78d0f16993dc3a94c6
Nathan Tate <nathantate@google.com>
2019-09-23 15:48:50 -0700

    feat(material-experimental/radio): use MDC DOM structure and styles (#17179)

--- src/material-experimental/mdc-radio/radio.ts
+++ src/material-experimental/mdc-radio/radio.ts
@@ -6,7 +6,10 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ViewEncapsulation, Input} from '@angular/core';
+
+// Increasing integer for generating unique ids for radio components.
+let nextUniqueId = 0;
 
 @Component({
   moduleId: module.id,
@@ -15,11 +18,21 @@ import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/co
   styleUrls: ['radio.css'],
   host: {
     'class': 'mat-mdc-radio',
+    '[attr.id]': 'id',
   },
   exportAs: 'matRadio',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MatRadio {
+
+  private _uniqueId: string = `mat-radio-${++nextUniqueId}`;
+
+  /** The unique ID for the radio button. */
+  @Input() id: string = this._uniqueId;
+
+  /** ID of the native input element inside `<mat-radio-button>` */
+  get inputId(): string { return `${this.id || this._uniqueId}-input`; }
+
   // TODO: set up MDC foundation class.
 }
