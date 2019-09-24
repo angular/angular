2e530c07168ad020eee6f5efb29af0758f057eea
crisbeto <crisbeto@abv.bg>
2019-09-24 20:28:14 +0200

    build: temporarily disable dynamic queries migration (#32837)
    Disables the dynamic queries migration until we can land the relevant framework changes (#32686 and #32720).
    
    PR Close #32837

--- packages/core/schematics/test/dynamic_queries_migration_spec.ts
+++ packages/core/schematics/test/dynamic_queries_migration_spec.ts
@@ -20,7 +20,7 @@ describe('dynamic queries migration', () => {
   let previousWorkingDir: string;
 
   beforeEach(() => {
-    runner = new SchematicTestRunner('test', require.resolve('../migrations.json'));
+    runner = new SchematicTestRunner('test', require.resolve('./test-migrations.json'));
     host = new TempScopedNodeJsSyncHost();
     tree = new UnitTestTree(new HostTree(host));
 
