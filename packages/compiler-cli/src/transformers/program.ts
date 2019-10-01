55a03f412a2092a9a4ff97e061a52aadebe4dfdd
Igor Minar <igor@angular.io>
2019-10-01 16:44:50 -0700

    feat: typescript 3.6 support
    BREAKING CHANGE: typescript 3.4 and 3.5 are no longer supported, please update to typescript 3.6

--- packages/compiler-cli/src/transformers/program.ts
+++ packages/compiler-cli/src/transformers/program.ts
@@ -71,14 +71,14 @@ const defaultEmitCallback: TsEmitCallback =
  * Minimum supported TypeScript version
  * ∀ supported typescript version v, v >= MIN_TS_VERSION
  */
-const MIN_TS_VERSION = '3.4.0';
+const MIN_TS_VERSION = '3.6.3';
 
 /**
  * Supremum of supported TypeScript versions
  * ∀ supported typescript version v, v < MAX_TS_VERSION
  * MAX_TS_VERSION is not considered as a supported TypeScript version
  */
-const MAX_TS_VERSION = '3.6.0';
+const MAX_TS_VERSION = '3.7.0';
 
 class AngularCompilerProgram implements Program {
   private rootNames: string[];
