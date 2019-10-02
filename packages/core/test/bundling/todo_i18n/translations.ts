53311de9136231c4fe66aa9c816b7b8ce06c5187
Pete Bacon Darwin <pete@bacondarwin.com>
2019-10-02 18:17:56 +0100

    fix(ivy): i18n - do not render message ids unnecessarily (#32867)
    In an attempt to be compatible with previous translation files
    the Angular compiler was generating instructions that always
    included the message id. This was because it was not possible
    to accurately re-generate the id from the calls to `$localize()` alone.
    
    In line with https://hackmd.io/EQF4_-atSXK4XWg8eAha2g this
    commit changes the compiler so that it only renders ids if they are
    "custom" ones provided by the template author.
    
    NOTE:
    
    When translating messages generated by the Angular compiler
    from i18n tags in templates, the `$localize.translate()` function
    will compute message ids, if no custom id is provided, using a
    common digest function that only relies upon the information
    available in the `$localize()` calls.
    
    This computed message id will not be the same as the message
    ids stored in legacy translation files. Such files will need to be
    migrated to use the new common digest function.
    
    This only affects developers who have been trialling `$localize`, have
    been calling `loadTranslations()`, and are not exclusively using custom
    ids in their templates.
    
    PR Close #32867

--- packages/core/test/bundling/todo_i18n/translations.ts
+++ packages/core/test/bundling/todo_i18n/translations.ts
@@ -8,30 +8,25 @@
 // Make the `$localize()` global function available to the compiled templates, and the direct calls
 // below. This would normally be done inside the application `polyfills.ts` file.
 import '@angular/localize/init';
+
+import {computeMsgId} from '@angular/compiler';
 import {loadTranslations} from '@angular/localize';
 
 export const translations = {
-  // What needs to be done?
-  '5102526651904871634': `Qu'y a-t-il à faire ?`,
-  // {$START_HEADING_LEVEL1}todos{$CLOSE_HEADING_LEVEL1}{$TAG_INPUT}
-  '8643091609122689720':
+  [computeMsgId('What needs to be done?', '')]: `Qu'y a-t-il à faire ?`,
+  [computeMsgId('{$START_HEADING_LEVEL1}todos{$CLOSE_HEADING_LEVEL1}{$TAG_INPUT}', '')]:
       '{$START_HEADING_LEVEL1}liste de tâches{$CLOSE_HEADING_LEVEL1}{$TAG_INPUT}',
-  // {VAR_PLURAL, plural, =1 {item left} other {items left}}
-  '271375439086996113': '{VAR_PLURAL, plural, =1 {tâche restante} other {tâches restantes}}',
-  // {$START_TAG_STRONG}{$INTERPOLATION}{$CLOSE_TAG_STRONG}{$ICU}
-  '4169337202119891309': '{$START_TAG_STRONG}{$INTERPOLATION}{$CLOSE_TAG_STRONG} {$ICU}',
-  // Clear Completed
-  '3329962774478249377': ' Effacer terminés ',
-  // Demonstrate Components
-  '5738403869589701812': ' Démontrer les components',
-  // Demonstrate Structural Directives
-  '4405796757024158842': 'Démontrer les directives structurelles',
-  // Demonstrate {$value}
-  '2762077329405284613': 'Démontrer {$value}',
-  // Demonstrate zoneless change detection
-  '3484387157632222646': 'Démontrer la détection des changements sans zonejs',
-  // Demonstrate internationalization
-  '442837859415373816': `Démontrer l'internationalisation`
+  [computeMsgId('{VAR_PLURAL, plural, =1 {item left} other {items left}}', '')]:
+      '{VAR_PLURAL, plural, =1 {tâche restante} other {tâches restantes}}',
+  [computeMsgId('{$START_TAG_STRONG}{$INTERPOLATION}{$CLOSE_TAG_STRONG}{$ICU}', '')]:
+      '{$START_TAG_STRONG}{$INTERPOLATION}{$CLOSE_TAG_STRONG} {$ICU}',
+  [computeMsgId('Clear Completed', '')]: ' Effacer terminés ',
+  [computeMsgId('Demonstrate Components', '')]: ' Démontrer les components',
+  [computeMsgId('Demonstrate Structural Directives', '')]: 'Démontrer les directives structurelles',
+  [computeMsgId('Demonstrate {$value}', '')]: 'Démontrer {$value}',
+  [computeMsgId('Demonstrate zoneless change detection', '')]:
+      'Démontrer la détection des changements sans zonejs',
+  [computeMsgId('Demonstrate internationalization', '')]: `Démontrer l'internationalisation`
 };
 
 loadTranslations(translations);
