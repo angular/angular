641e95db825a7756d1af88ce196c6c747f6e989a
Stephen Fluin <stephen.fluin@gmail.com>
2019-10-02 11:00:00 -0700

    docs: fix stackblitz example polyfills

--- aio/tools/examples/shared/boilerplate/cli/src/polyfills.ts
+++ aio/tools/examples/shared/boilerplate/cli/src/polyfills.ts
@@ -21,16 +21,6 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
-/** IE10 and IE11 requires the following for the Reflect API. */
-/**
- * DO NOT REMOVE
- * By default, Reflect polyfills are auto-included by the CLI and
- * are required for JIT compilation.  StackBlitz examples are
- * compiled using JIT.
- */
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
-
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
@@ -65,8 +55,7 @@ import 'core-js/es7/reflect';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-import 'zone.js/dist/zone-patch-canvas';
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
