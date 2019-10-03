3673f3d3084b91e6f619b835a3431532c8e1909d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-03 20:32:18 +0200

    fix(a11y): focus monitor not identifying touch focus inside shadow root (#17167)
    `FocusMonitor` uses a `touchstart` listener on the `document` to identify when an element was focused via touch. The problem is that if an element is inside the shadow DOM, the `event.target` will be set to the shadow root. These changes use `composedPath` to get the event target which accounts for the shadow DOM.

--- src/cdk/a11y/focus-monitor/focus-monitor.ts
+++ src/cdk/a11y/focus-monitor/focus-monitor.ts
@@ -117,7 +117,11 @@ export class FocusMonitor implements OnDestroy {
     if (this._touchTimeoutId != null) {
       clearTimeout(this._touchTimeoutId);
     }
-    this._lastTouchTarget = event.target;
+
+    // Since this listener is bound on the `document` level, any events coming from the shadow DOM
+    // will have their `target` set to the shadow root. If available, use `composedPath` to
+    // figure out the event target.
+    this._lastTouchTarget = event.composedPath ? event.composedPath()[0] : event.target;
     this._touchTimeoutId = setTimeout(() => this._lastTouchTarget = null, TOUCH_BUFFER_MS);
   }
 
