5557dec120e3e81f5078089cb7821187fae501e0
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-05 23:41:37 +0200

    refactor(core): missing-injectable migration should respect providers of directives and components (#33011)
    Currenly the `missing-injectable` migration only migrates providers referenced from
    `@NgModule` definitions. The schematic currently does not cover the migration for
    providers referenced in `@Directive` or `@Component` definitions.
    
    We need to handle the following keys for directives/components:
    
    - `@Directive` -> `providers`
    - `@Component` -> `providers` and `viewProviders`.
    
    This commit ensures that the migration handles providers for these
    definitions.
    
    PR Close #33011

--- packages/core/schematics/migrations/missing-injectable/definition_collector.ts
+++ packages/core/schematics/migrations/missing-injectable/definition_collector.ts
@@ -18,12 +18,21 @@ export interface ResolvedNgModule {
   providersExpr: ts.Expression|null;
 }
 
+export interface ResolvedDirective {
+  name: string;
+  node: ts.ClassDeclaration;
+  decorator: NgDecorator;
+  providersExpr: ts.Expression|null;
+  viewProvidersExpr: ts.Expression|null;
+}
+
 /**
  * Visitor that walks through specified TypeScript nodes and collects all
- * found NgModule definitions.
+ * found NgModule, Directive or Component definitions.
  */
-export class NgModuleCollector {
+export class NgDefinitionCollector {
   resolvedModules: ResolvedNgModule[] = [];
+  resolvedDirectives: ResolvedDirective[] = [];
 
   constructor(public typeChecker: ts.TypeChecker) {}
 
@@ -41,11 +50,38 @@ export class NgModuleCollector {
     }
 
     const ngDecorators = getAngularDecorators(this.typeChecker, node.decorators);
+    const directiveDecorator =
+        ngDecorators.find(({name}) => name === 'Component' || name == 'Directive');
     const ngModuleDecorator = ngDecorators.find(({name}) => name === 'NgModule');
 
     if (ngModuleDecorator) {
       this._visitNgModuleClass(node, ngModuleDecorator);
+    } else if (directiveDecorator) {
+      this._visitDirectiveClass(node, directiveDecorator);
     }
+  }
+
+  private _visitDirectiveClass(node: ts.ClassDeclaration, decorator: NgDecorator) {
+    const decoratorCall = decorator.node.expression;
+    const metadata = decoratorCall.arguments[0];
+
+    if (!metadata || !ts.isObjectLiteralExpression(metadata)) {
+      return;
+    }
+
+    const providersNode = metadata.properties.filter(ts.isPropertyAssignment)
+                              .find(p => getPropertyNameText(p.name) === 'providers');
+
+    const viewProvidersNode = metadata.properties.filter(ts.isPropertyAssignment)
+                                  .find(p => getPropertyNameText(p.name) === 'viewProviders');
+
+    this.resolvedDirectives.push({
+      name: node.name ? node.name.text : 'default',
+      node,
+      decorator,
+      providersExpr: providersNode !== undefined ? providersNode.initializer : null,
+      viewProvidersExpr: viewProvidersNode !== undefined ? viewProvidersNode.initializer : null,
+    });
   }
 
   private _visitNgModuleClass(node: ts.ClassDeclaration, decorator: NgDecorator) {
