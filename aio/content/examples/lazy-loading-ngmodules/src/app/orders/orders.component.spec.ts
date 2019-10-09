f12bf360753c0ea5e57b1095d79b919b433c6df4
Amadou Sall <ahasall.dev@gmail.com>
2019-10-09 23:48:05 +0200

    docs: update Stackblitz example for lazy-loading (#33070)
    PR Close #33070

--- aio/content/examples/lazy-loading-ngmodules/src/app/orders/orders.component.spec.ts
+++ aio/content/examples/lazy-loading-ngmodules/src/app/orders/orders.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CustomerListComponent } from './customer-list.component';
+import { OrdersComponent } from './orders.component';
 
-describe('CustomerListComponent', () => {
-  let component: CustomerListComponent;
-  let fixture: ComponentFixture<CustomerListComponent>;
+describe('OrderListComponent', () => {
+  let component: OrdersComponent;
+  let fixture: ComponentFixture<OrdersComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CustomerListComponent ]
+      declarations: [ OrdersComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CustomerListComponent);
+    fixture = TestBed.createComponent(OrdersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
