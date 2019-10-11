56e69d2eaddd1a2261df918db448f6e7d70c1ea3
Pete Bacon Darwin <pete@bacondarwin.com>
2019-10-11 13:42:43 +0100

    feat(ivy): i18n - support source locale inlining
    Add a new flag to `localize-translate` that allows the
    source locale to be specified. When this locale is
    provided an extra copy of the files is made for this
    locale where the is no translation but all the calls to
    `$localize` are stripped out.
    
    Resolves FW-1623

--- packages/localize/src/tools/test/translate/source_files/source_file_translation_handler_spec.ts
+++ packages/localize/src/tools/test/translate/source_files/source_file_translation_handler_spec.ts
@@ -8,6 +8,7 @@
 import {Diagnostics} from '../../../src/diagnostics';
 import {FileUtils} from '../../../src/file_utils';
 import {SourceFileTranslationHandler} from '../../../src/translate/source_files/source_file_translation_handler';
+import {TranslationBundle} from '../../../src/translate/translator';
 
 describe('SourceFileTranslationHandler', () => {
   describe('canTranslate()', () => {
@@ -39,6 +40,19 @@ describe('SourceFileTranslationHandler', () => {
              .toHaveBeenCalledWith('/translations/fr/relative/path', contents);
        });
 
+    it('should copy files to the source locale if they contain no reference to `$localize` and `sourceLocale` is provided',
+       () => {
+         const diagnostics = new Diagnostics();
+         const handler = new SourceFileTranslationHandler();
+         const translations: TranslationBundle[] = [];
+         const contents = Buffer.from('contents');
+         handler.translate(
+             diagnostics, '/root/path', 'relative/path', contents, mockOutputPathFn, translations,
+             'en-US');
+         expect(FileUtils.writeFile)
+             .toHaveBeenCalledWith('/translations/en-US/relative/path', contents);
+       });
+
     it('should transform each $localize template tag', () => {
       const diagnostics = new Diagnostics();
       const handler = new SourceFileTranslationHandler();
@@ -56,6 +70,23 @@ describe('SourceFileTranslationHandler', () => {
       expect(FileUtils.writeFile).toHaveBeenCalledWith('/translations/en/relative/path.js', output);
       expect(FileUtils.writeFile).toHaveBeenCalledWith('/translations/fr/relative/path.js', output);
     });
+
+    it('should transform each $localize template tag and write it to the source locale if provided',
+       () => {
+         const diagnostics = new Diagnostics();
+         const handler = new SourceFileTranslationHandler();
+         const translations: TranslationBundle[] = [];
+         const contents = Buffer.from(
+             '$localize`a${1}b${2}c`;\n' +
+             '$localize(__makeTemplateObject(["a", "b", "c"], ["a", "b", "c"]), 1, 2);');
+         const output = '"a"+1+"b"+2+"c";"a"+1+"b"+2+"c";';
+         handler.translate(
+             diagnostics, '/root/path', 'relative/path.js', contents, mockOutputPathFn,
+             translations, 'en-US');
+
+         expect(FileUtils.writeFile)
+             .toHaveBeenCalledWith('/translations/en-US/relative/path.js', output);
+       });
 
     it('should error if the file is not valid JS', () => {
       const diagnostics = new Diagnostics();
