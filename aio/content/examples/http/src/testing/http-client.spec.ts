2f0d8d55529db932cfc4908f5c9288a6f202a301
Jason Ruhlin <jasonruhlin@Jasons-MBP.fios-router.home>
2019-10-10 20:53:08 -0400

    docs(http): update httpClient request docs
    Add POST request example

--- aio/content/examples/http/src/testing/http-client.spec.ts
+++ aio/content/examples/http/src/testing/http-client.spec.ts
@@ -67,6 +67,33 @@ describe('HttpClient testing', () => {
     httpTestingController.verify();
   });
   // #enddocregion get-test
+  // #docregion post-test
+  it('can test HttpClient.post', () => {
+    const testData: Data = {name: 'Test Data'};
+
+    // Make an HTTP POST request
+    httpClient.post<Data>(testUrl)
+      .subscribe(data =>
+        // When observable resolves, result should match test data
+        expect(data).toEqual(testData)
+      );
+
+    // The following `expectOne()` will match the request's URL.
+    // If no requests or multiple requests matched that URL
+    // `expectOne()` would throw.
+    const req = httpTestingController.expectOne('/data');
+
+    // Assert that the request is a POST.
+    expect(req.request.method).toEqual('POST');
+
+    // Respond with mock data, causing Observable to resolve.
+    // Subscribe callback asserts that correct data was returned.
+    req.flush(testData);
+
+    // Finally, assert that there are no outstanding requests.
+    httpTestingController.verify();
+  });
+  // #enddocregion post-test
   it('can test HttpClient.get with matching header', () => {
     const testData: Data = {name: 'Test Data'};
 
