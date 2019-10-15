6b0cef50200b7f4c272b9b2ac0f199377de83a7f
Filipe Silva <filipematossilva@gmail.com>
2019-10-15 14:42:31 +0100

    test: update integration/cli-hello-world project structure (#33175)
    PR Close #33175

--- integration/cli-hello-world/karma.conf.js
+++ integration/cli-hello-world/karma.conf.js
@@ -12,28 +12,21 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, './coverage/latest-app'),
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['CustomChrome'],
+    browsers: ['Chrome'],
     singleRun: false,
-    customLaunchers: {
-      CustomChrome: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    }
+    restartOnFileChange: true
   });
 };
