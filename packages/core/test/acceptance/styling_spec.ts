893a6d4e2bfb4b32a242c9205ce5b7a0b1b8eb83
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 14:10:33 -0700

    fixup! fix(ivy): ensure errors are thrown during checkNoChanges for style/class bindings

--- packages/core/test/acceptance/styling_spec.ts
+++ packages/core/test/acceptance/styling_spec.ts
@@ -2244,97 +2244,53 @@ describe('styling', () => {
 
   it('should throw an error if a prop-based style/class binding value is changed during checkNoChanges',
      () => {
-       let updateStyleBinding = false;
-       let updateStyleCount = 0;
-       let updateClassBinding = false;
-       let updateClassCount = 0;
-
        @Component({
          template: `
         <div [style.color]="color" [class.foo]="fooClass"></div>
       `
        })
        class Cmp {
-         private _color = 'red';
-         private _fooClass = true;
+         color = 'red';
+         fooClass = true;
 
-         get color() {
-           if (updateStyleBinding && updateStyleCount++) {
-             this._color = 'blue';
-           }
-           return this._color;
-         }
-
-         get fooClass() {
-           if (updateClassBinding && updateClassCount++) {
-             this._fooClass = false;
-           }
-           return this._fooClass;
+         ngAfterViewInit() {
+           this.color = 'blue';
+           this.fooClass = false;
          }
        }
 
        TestBed.configureTestingModule({declarations: [Cmp]});
        const fixture = TestBed.createComponent(Cmp);
-       fixture.detectChanges();
 
-       updateStyleBinding = true;
-       updateStyleCount = 0;
-
-       expect(() => { fixture.detectChanges(); }).toThrow();
-
-       updateStyleBinding = false;
-       updateClassBinding = true;
-       updateClassCount = 0;
-
-       expect(() => { fixture.detectChanges(); }).toThrow();
+       expect(() => {
+         fixture.detectChanges();
+       }).toThrowError(/ExpressionChangedAfterItHasBeenCheckedError/);
      });
 
   onlyInIvy('only ivy allows for map-based style AND class bindings')
       .it('should throw an error if a map-based style/class binding value is changed during checkNoChanges',
           () => {
-            let updateStyleBinding = false;
-            let updateStyleCount = 0;
-            let updateClassBinding = false;
-            let updateClassCount = 0;
-
             @Component({
               template: `
-        <div [style]="style" [class]="klass"></div>
-      `
+                <div [style]="style" [class]="klass"></div>
+              `
             })
             class Cmp {
-              private _style = {width: '100px'};
-              private _klass = {foo: true, bar: false};
+              style: any = {width: '100px'};
+              klass: any = {foo: true, bar: false};
 
-              get style() {
-                if (updateStyleBinding && updateStyleCount++) {
-                  this._style = {width: '200px'};
-                }
-                return this._style;
-              }
-
-              get klass() {
-                if (updateClassBinding && updateClassCount++) {
-                  this._klass = {foo: false, bar: true};
-                }
-                return this._klass;
+              ngAfterViewInit() {
+                this.style = {height: '200px'};
+                this.klass = {foo: false};
               }
             }
 
             TestBed.configureTestingModule({declarations: [Cmp]});
             const fixture = TestBed.createComponent(Cmp);
-            fixture.detectChanges();
 
-            updateStyleBinding = true;
-            updateStyleCount = 0;
-
-            expect(() => { fixture.detectChanges(); }).toThrow();
-
-            updateStyleBinding = false;
-            updateClassBinding = true;
-            updateClassCount = 0;
-
-            expect(() => { fixture.detectChanges(); }).toThrow();
+            expect(() => {
+              fixture.detectChanges();
+            }).toThrowError(/ExpressionChangedAfterItHasBeenCheckedError/);
           });
 });
 
