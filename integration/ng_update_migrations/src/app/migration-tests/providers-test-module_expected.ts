275fe18b045f2aec37fcdeeb461bfe177a8edcb4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-17 12:22:44 +0200

    test: improve missing-injectable migration test case in ng-update integration test (#33223)
    Improves the `missing-injectable` migration test case in the
    `ng_update_migrations` integration test by adding scenarios
    for the recent changes that have been made to the migration.
    
    e.g. 5557dec120e3e81f5078089cb7821187fae501e0
    
    PR Close #33223

--- integration/ng_update_migrations/src/app/migration-tests/providers-test-module_expected.ts
+++ integration/ng_update_migrations/src/app/migration-tests/providers-test-module_expected.ts
@@ -1,4 +1,35 @@
-import { Directive, NgModule, NgZone, Injectable } from '@angular/core';
+import { Component, Directive, NgModule, NgZone, Injectable } from '@angular/core';
+
+@Injectable()
+export class ComponentTypeProvider {}
+export class ComponentDontNeedCase {}
+@Injectable()
+export class ComponentProvider {}
+@Injectable()
+export class ComponentProvider2 {}
+
+@Component({
+  template: '',
+  viewProviders: [ComponentTypeProvider, [
+    {provide: ComponentDontNeedCase, useExisting: ComponentProvider}]
+  ],
+  providers: [ComponentProvider2]
+})
+export class ProvidersTestComponent {}
+
+@Injectable()
+export class DirectiveTypeProvider {}
+export class DirectiveDontNeedCase {}
+@Injectable()
+export class DirectiveProvider {}
+
+@Directive({
+  selector: 'test-dir',
+  providers: [DirectiveTypeProvider, [
+    {provide: DirectiveDontNeedCase, useExisting: DirectiveProvider}]
+  ],
+})
+export class ProvidersTestDirective {}
 
 @Injectable()
 export class TypeCase {}
@@ -30,6 +61,8 @@ export class DirectiveCase {}
     {provide: DontNeedCase, useExisting: TypeCase},
     {provide: DontNeedCase, useClass: ProviderClass},
     DirectiveCase,
-  ]
+  ],
+  declarations: [ProvidersTestDirective, ProvidersTestComponent],
 })
 export class ProvidersTestModule {}
+
