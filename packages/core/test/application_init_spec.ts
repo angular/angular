710892b70b49f32c8a3a839695e7e2e3e267a401
vthinkxie <yadong.xyd@alibaba-inc.com>
2019-10-17 17:48:35 +0800

    feat(core): support APP_INITIALIZER work with observable
    This commit adds support for Observables that now can be used as a part of APP_INITIALIZER. Previously, only
    Primises were supported.
    
    Closes #15088.

--- packages/core/test/application_init_spec.ts
+++ packages/core/test/application_init_spec.ts
@@ -5,8 +5,8 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import {Injector} from '@angular/core';
 import {APP_INITIALIZER, ApplicationInitStatus} from '@angular/core/src/application_init';
+import {Observable, Subscriber} from 'rxjs';
 
 import {inject, TestBed, waitForAsync} from '../testing';
 
@@ -28,49 +28,151 @@ import {inject, TestBed, waitForAsync} from '../testing';
          })));
     });
 
-    describe('with async initializers', () => {
+    describe('with async promise initializers', () => {
       let resolve: (result: any) => void;
+      let reject: (reason?: any) => void;
       let promise: Promise<any>;
-      let completerResolver = false;
+      let initFnInvoked = false;
       beforeEach(() => {
-        let initializerFactory = (injector: Injector) => {
-          return () => {
-            const initStatus = injector.get(ApplicationInitStatus);
-            initStatus.donePromise.then(() => {
-              expect(completerResolver).toBe(true);
-            });
-          };
-        };
-        promise = new Promise((res) => {
+        promise = new Promise((res, rej) => {
           resolve = res;
+          reject = rej;
         });
         TestBed.configureTestingModule({
           providers: [
             {provide: APP_INITIALIZER, multi: true, useValue: () => promise},
-            {
-              provide: APP_INITIALIZER,
-              multi: true,
-              useFactory: initializerFactory,
-              deps: [Injector]
-            },
           ]
         });
       });
 
-      it('should update the status once all async initializers are done',
+      it('should update the status once all async promise initializers are done',
          waitForAsync(inject([ApplicationInitStatus], (status: ApplicationInitStatus) => {
+           // Accessing internal `runInitializers` function of the `ApplicationInitStatus` class
+           // instance for testing purposes to invoke initializer functions.
            (status as any).runInitializers();
 
            setTimeout(() => {
-             completerResolver = true;
+             initFnInvoked = true;
              resolve(null);
            });
 
            expect(status.done).toBe(false);
            status.donePromise.then(() => {
              expect(status.done).toBe(true);
-             expect(completerResolver).toBe(true);
+             expect(initFnInvoked).toBe(true);
            });
+         })));
+
+      it('should handle a case when promise is rejected',
+         waitForAsync(inject([ApplicationInitStatus], (status: ApplicationInitStatus) => {
+           // Accessing internal `runInitializers` function of the `ApplicationInitStatus` class
+           // instance for testing purposes to invoke initializer functions.
+           (status as any).runInitializers();
+
+           setTimeout(() => {
+             initFnInvoked = true;
+             reject();
+           });
+
+           expect(status.done).toBe(false);
+           status.donePromise
+               .then(
+                   () => fail('`donePromise.then` should not be invoked when promise is rejected'))
+               .catch(() => {
+                 expect(status.done).toBe(false);
+                 expect(initFnInvoked).toBe(true);
+               });
+         })));
+    });
+
+    describe('with app initializers represented using observables', () => {
+      let subscriber: Subscriber<any>;
+      let observable: Observable<any>;
+      let initFnInvoked = false;
+      beforeEach(() => {
+        observable = new Observable((res) => {
+          subscriber = res;
+        });
+        TestBed.configureTestingModule({
+          providers: [
+            {provide: APP_INITIALIZER, multi: true, useValue: () => observable},
+          ]
+        });
+      });
+
+      it('should update the status once all async observable initializers are completed',
+         waitForAsync(inject([ApplicationInitStatus], (status: ApplicationInitStatus) => {
+           // Accessing internal `runInitializers` function of the `ApplicationInitStatus` class
+           // instance for testing purposes to invoke initializer functions.
+           (status as any).runInitializers();
+
+           setTimeout(() => {
+             initFnInvoked = true;
+             subscriber.complete();
+           });
+
+           expect(status.done).toBe(false);
+           status.donePromise.then(() => {
+             expect(status.done).toBe(true);
+             expect(initFnInvoked).toBe(true);
+           });
+         })));
+
+      it('should update the status once all async observable initializers nexted and completed',
+         waitForAsync(inject([ApplicationInitStatus], (status: ApplicationInitStatus) => {
+           // Accessing internal `runInitializers` function of the `ApplicationInitStatus` class
+           // instance for testing purposes to invoke initializer functions.
+           (status as any).runInitializers();
+
+           subscriber.next('one');
+           subscriber.next('two');
+
+           setTimeout(() => {
+             initFnInvoked = true;
+             subscriber.complete();
+           });
+
+           expect(status.done).toBe(false);
+           status.donePromise.then(() => {
+             expect(status.done).toBe(true);
+             expect(initFnInvoked).toBe(true);
+           });
+         })));
+
+      it('should update the status if all async observable initializers are completed before runInitializers',
+         waitForAsync(inject([ApplicationInitStatus], (status: ApplicationInitStatus) => {
+           subscriber.complete();
+           // Accessing internal `runInitializers` function of the `ApplicationInitStatus` class
+           // instance for testing purposes to invoke initializer functions.
+           (status as any).runInitializers();
+
+           expect(status.done).toBe(false);
+
+           status.donePromise.then(() => {
+             expect(status.done).toBe(true);
+           });
+         })));
+
+      it('should handle a case when observable emits an error',
+         waitForAsync(inject([ApplicationInitStatus], (status: ApplicationInitStatus) => {
+           // Accessing internal `runInitializers` function of the `ApplicationInitStatus` class
+           // instance for testing purposes to invoke initializer functions.
+           (status as any).runInitializers();
+
+           setTimeout(() => {
+             initFnInvoked = true;
+             subscriber.error();
+           });
+
+           expect(status.done).toBe(false);
+           status.donePromise
+               .then(
+                   () => fail(
+                       '`donePromise.then` should not be invoked when observable emits an error'))
+               .catch(() => {
+                 expect(status.done).toBe(false);
+                 expect(initFnInvoked).toBe(true);
+               });
          })));
     });
   });
