35e963bd2163ef651e53c77ac25b41cd625a576e
Filipe Silva <filipematossilva@gmail.com>
2019-10-21 18:06:03 +0100

    test: fix typo in cli e2e descriptions (#33175)
    PR Close #33175

--- integration/cli-hello-world-lazy/e2e/src/app.e2e-spec.ts
+++ integration/cli-hello-world-lazy/e2e/src/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { AppPage } from './app.po';
 import { browser, logging, element, by } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('cli-hello-world-lazy App', () => {
   let page: AppPage;
 
   beforeEach(() => {
