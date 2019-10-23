df509086827019a27cc235b7e45cbc5e295944bc
George Kalpakas <kalpakas.g@gmail.com>
2019-10-23 14:51:33 +0300

    fix(docs-infra): use `.tooltip` in `aio-top-menu` items (#33351)
    The top-menu items have both a `title` and a `tooltip` property. The
    `title` is used as text content, so there is little point in using it as
    "tooltip" (via the HTMLElement's `title` property) too.
    
    This commit switches to using the `tooltip` property to populate the
    `title`. Note that in many cases, the `tooltip` property is derived from
    `title` anyway (so there is no practical change in behavior in these
    cases).
    
    PR Close #33351

--- aio/src/app/layout/top-menu/top-menu.component.ts
+++ aio/src/app/layout/top-menu/top-menu.component.ts
@@ -6,7 +6,7 @@ import { NavigationNode } from 'app/navigation/navigation.service';
   template: `
     <ul role="navigation">
       <li *ngFor="let node of nodes">
-        <a class="nav-link" [href]="node.url" [title]="node.title">
+        <a class="nav-link" [href]="node.url" [title]="node.tooltip">
           <span class="nav-link-inner">{{ node.title }}</span>
         </a>
       </li>
