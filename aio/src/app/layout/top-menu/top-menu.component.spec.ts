2aa940f55c036212ec3ec17f18dffa98c219b71c
George Kalpakas <kalpakas.g@gmail.com>
2019-10-23 15:01:38 +0300

    fix(docs-infra): highlight the currently active node in top-bar (#33351)
    Related to #33239 and #33255.
    
    PR Close #33351

--- aio/src/app/layout/top-menu/top-menu.component.spec.ts
+++ aio/src/app/layout/top-menu/top-menu.component.spec.ts
@@ -10,6 +10,8 @@ describe('TopMenuComponent', () => {
     const list: HTMLUListElement = fixture.debugElement.nativeElement.querySelector('ul');
     return Array.from(list.querySelectorAll('li'));
   };
+  const getSelected = (items: HTMLLIElement[]) =>
+    items.filter(item => item.classList.contains('selected'));
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -37,5 +39,48 @@ describe('TopMenuComponent', () => {
     expect(links.map(link => link.pathname)).toEqual(['/api', '/features']);
     expect(links.map(link => link.textContent)).toEqual(['API', 'Features']);
     expect(links.map(link => link.title)).toEqual(['API docs', 'Angular features overview']);
+  });
+
+  it('should mark the currently selected node with `.selected`', () => {
+    const items = getListItems();
+    expect(getSelected(items)).toEqual([]);
+
+    component.currentNode = {url: 'api', view: 'foo', nodes: []};
+    fixture.detectChanges();
+    expect(getSelected(items)).toEqual([items[0]]);
+
+    component.currentNode = {url: 'features', view: 'foo', nodes: []};
+    fixture.detectChanges();
+    expect(getSelected(items)).toEqual([items[1]]);
+
+    component.currentNode = {url: 'something/else', view: 'foo', nodes: []};
+    fixture.detectChanges();
+    expect(getSelected(items)).toEqual([]);
+  });
+
+  it('should not mark any node with `.selected` if the current URL is undefined', () => {
+    component.nodes = [
+      {url: '', title: 'API', tooltip: 'API docs'},
+      {url: undefined, title: 'Features', tooltip: 'Angular features overview'},
+    ];
+    fixture.detectChanges();
+    const items = getListItems();
+
+    component.currentNode = undefined;
+    fixture.detectChanges();
+    expect(getSelected(items)).toEqual([]);
+  });
+
+  it('should correctly mark a node with `.selected` even if its URL is empty', () => {
+    component.nodes = [
+      {url: '', title: 'API', tooltip: 'API docs'},
+      {url: undefined, title: 'Features', tooltip: 'Angular features overview'},
+    ];
+    fixture.detectChanges();
+    const items = getListItems();
+
+    component.currentNode = {url: '', view: 'Empty url', nodes: []};
+    fixture.detectChanges();
+    expect(getSelected(items)).toEqual([items[0]]);
   });
 });
