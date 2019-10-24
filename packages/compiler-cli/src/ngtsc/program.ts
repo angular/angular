f50df7f6ae5819c30aca5d73bc8bd1c403114b24
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-23 17:47:47 -0700

    fix(ivy): split checkTypeOfReferences into DOM and non-DOM flags.
    View Engine correctly infers the type of local refs to directives or to
    <ng-template>s, just not to DOM nodes. This commit splits the
    checkTypeOfReferences flag into two separate halves, allowing the compiler
    to align with this behavior.

--- packages/compiler-cli/src/ngtsc/program.ts
+++ packages/compiler-cli/src/ngtsc/program.ts
@@ -436,7 +436,8 @@ export class NgtscProgram implements api.Program {
         // - error TS2531: Object is possibly 'null'.
         // - error TS2339: Property 'value' does not exist on type 'EventTarget'.
         checkTypeOfDomEvents: false,
-        checkTypeOfReferences: true,
+        checkTypeOfDomReferences: true,
+        checkTypeOfNonDomReferences: true,
         checkTypeOfPipes: true,
         strictSafeNavigationTypes: true,
       };
@@ -452,7 +453,8 @@ export class NgtscProgram implements api.Program {
         checkTypeOfOutputEvents: false,
         checkTypeOfAnimationEvents: false,
         checkTypeOfDomEvents: false,
-        checkTypeOfReferences: false,
+        checkTypeOfDomReferences: false,
+        checkTypeOfNonDomReferences: false,
         checkTypeOfPipes: false,
         strictSafeNavigationTypes: false,
       };
