1b3cccfd918b999fab9a80f06bf612ba91ba14fe
Greg Magolan <gmagolan@gmail.com>
2019-10-27 16:28:03 -0700

    build: update zone.js to use the new rollup_bundle (#33329)
    PR Close #33329

---
+++ packages/zone.js/rollup-es5.config.js
@@ -0,0 +1,19 @@
+const node = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+
+const banner = `/**
+* @license Angular v0.0.0-PLACEHOLDER
+* (c) 2010-2019 Google LLC. https://angular.io/
+* License: MIT
+*/`;
+
+module.exports = {
+  plugins: [
+    node({
+      mainFields: ['es2015', 'module', 'jsnext:main', 'main'],
+    }),
+    commonjs(),
+  ],
+  external: ['electron'],
+  output: {globals: {electron: 'electron'}, banner},
+}
