2e6ff0f9d72f4546dbe681b80cf8b77bb9317d39
Pete Bacon Darwin <pete@bacondarwin.com>
2019-10-31 19:33:00 +0000

    build: generate global locale files (#33523)
    This commit contains the global locale files generated by the change to
    the CLDR `extract.js` tool, from the previous commit.
    
    PR Close #33523

---
+++ packages/common/locales/global/kea.js
@@ -0,0 +1,67 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+// THIS CODE IS GENERATED - DO NOT MODIFY
+// See angular/tools/gulp-tasks/cldr/extract.js
+
+(function(global) {
+  global.ng = global.ng || {};
+  global.ng.common = global.ng.common || {};
+  global.ng.common.locales = global.ng.common.locales || {};
+  const u = undefined;
+  function plural(n) { return 5; }
+  root.ng.common.locales['kea'] = [
+    'kea',
+    [['a', 'p'], ['am', 'pm'], u],
+    [['am', 'pm'], u, u],
+    [
+      ['D', 'S', 'T', 'K', 'K', 'S', 'S'], ['dum', 'sig', 'ter', 'kua', 'kin', 'ses', 'sab'],
+      [
+        'dumingu', 'sigunda-fera', 'tersa-fera', 'kuarta-fera', 'kinta-fera', 'sesta-fera', 'sabadu'
+      ],
+      ['du', 'si', 'te', 'ku', 'ki', 'se', 'sa']
+    ],
+    [
+      ['D', 'S', 'T', 'K', 'K', 'S', 'S'], ['dum', 'sig', 'ter', 'kua', 'kin', 'ses', 'sab'],
+      [
+        'dumingu', 'sigunda-fera', 'tersa-fera', 'kuarta-fera', 'kinta-fera', 'sesta-fera',
+        'sábadu'
+      ],
+      ['du', 'si', 'te', 'ku', 'ki', 'se', 'sa']
+    ],
+    [
+      ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
+      ['Jan', 'Feb', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Otu', 'Nuv', 'Diz'],
+      [
+        'Janeru', 'Febreru', 'Marsu', 'Abril', 'Maiu', 'Junhu', 'Julhu', 'Agostu', 'Setenbru',
+        'Otubru', 'Nuvenbru', 'Dizenbru'
+      ]
+    ],
+    u,
+    [['AK', 'DK'], u, ['Antis di Kristu', 'Dispos di Kristu']],
+    1,
+    [6, 0],
+    ['d/M/y', 'd MMM y', 'd \'di\' MMMM \'di\' y', 'EEEE, d \'di\' MMMM \'di\' y'],
+    ['HH:mm', 'HH:mm:ss', 'HH:mm:ss z', 'HH:mm:ss zzzz'],
+    ['{1} {0}', u, u, u],
+    [',', ' ', ';', '%', '+', '-', 'E', '×', '‰', '∞', 'NaN', ':'],
+    ['#,##0.###', '#,##0%', '#,##0.00 ¤', '#E0'],
+    '​',
+    'Skudu Kabuverdianu',
+    {
+      'AUD': ['AU$', '$'],
+      'CVE': ['​'],
+      'JPY': ['JP¥', '¥'],
+      'THB': ['฿'],
+      'USD': ['US$', '$']
+    },
+    plural,
+    []
+  ];
+})(typeof globalThis !== 'undefined' && globalThis || typeof global !== 'undefined' && global ||
+   typeof window !== 'undefined' && window);
---
+++ packages/common/locales/global/sq.js
@@ -0,0 +1,80 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+// THIS CODE IS GENERATED - DO NOT MODIFY
+// See angular/tools/gulp-tasks/cldr/extract.js
+
+(function(global) {
+  global.ng = global.ng || {};
+  global.ng.common = global.ng.common || {};
+  global.ng.common.locales = global.ng.common.locales || {};
+  const u = undefined;
+  function plural(n) {
+    if (n === 1) return 1;
+    return 5;
+  }
+  root.ng.common.locales['sq'] =
+      [
+        'sq',
+        [['e paradites', 'e pasdites'], u, u],
+        [['paradite', 'pasdite'], u, u],
+        [
+          ['D', 'H', 'M', 'M', 'E', 'P', 'Sh'], ['Die', 'Hën', 'Mar', 'Mër', 'Enj', 'Pre', 'Sht'],
+          ['e diel', 'e hënë', 'e martë', 'e mërkurë', 'e enjte', 'e premte', 'e shtunë'],
+          ['Die', 'Hën', 'Mar', 'Mër', 'Enj', 'Pre', 'Sht']
+        ],
+        [
+          ['D', 'H', 'M', 'M', 'E', 'P', 'Sh'], ['Die', 'Hën', 'Mar', 'Mër', 'Enj', 'Pre', 'Sht'],
+          ['E diel', 'E hënë', 'E martë', 'E mërkurë', 'E enjte', 'E premte', 'E shtunë'],
+          ['Die', 'Hën', 'Mar', 'Mër', 'Enj', 'Pre', 'Sht']
+        ],
+        [
+          ['j', 'sh', 'm', 'p', 'm', 'q', 'k', 'g', 'sh', 't', 'n', 'dh'],
+          ['jan', 'shk', 'mar', 'pri', 'maj', 'qer', 'korr', 'gush', 'sht', 'tet', 'nën', 'dhj'],
+          [
+            'janar', 'shkurt', 'mars', 'prill', 'maj', 'qershor', 'korrik', 'gusht', 'shtator',
+            'tetor', 'nëntor', 'dhjetor'
+          ]
+        ],
+        [
+          ['J', 'Sh', 'M', 'P', 'M', 'Q', 'K', 'G', 'Sh', 'T', 'N', 'Dh'],
+          ['Jan', 'Shk', 'Mar', 'Pri', 'Maj', 'Qer', 'Korr', 'Gush', 'Sht', 'Tet', 'Nën', 'Dhj'],
+          [
+            'Janar', 'Shkurt', 'Mars', 'Prill', 'Maj', 'Qershor', 'Korrik', 'Gusht', 'Shtator',
+            'Tetor', 'Nëntor', 'Dhjetor'
+          ]
+        ],
+        [['p.K.', 'mb.K.'], u, ['para Krishtit', 'mbas Krishtit']],
+        1,
+        [6, 0],
+        ['d.M.yy', 'd MMM y', 'd MMMM y', 'EEEE, d MMMM y'],
+        ['h:mm a', 'h:mm:ss a', 'h:mm:ss a, z', 'h:mm:ss a, zzzz'],
+        ['{1}, {0}', u, '{1} \'në\' {0}', u],
+        [',', ' ', ';', '%', '+', '-', 'E', '×', '‰', '∞', 'NaN', ':'],
+        ['#,##0.###', '#,##0%', '#,##0.00 ¤', '#E0'],
+        'Lekë',
+        'Leku shqiptar',
+        {'ALL': ['Lekë'], 'JPY': ['JP¥', '¥'], 'THB': ['฿'], 'USD': ['US$', '$']},
+        plural,
+        [
+          [
+            [
+              'e mesnatës', 'e mesditës', 'e mëngjesit', 'e paradites', 'e pasdites',
+              'e mbrëmjes', 'e natës'
+            ],
+            u, u
+          ],
+          [['mesnatë', 'mesditë', 'mëngjes', 'paradite', 'pasdite', 'mbrëmje', 'natë'], u, u],
+          [
+            '00:00', '12:00', ['04:00', '09:00'], ['09:00', '12:00'], ['12:00', '18:00'],
+            ['18:00', '24:00'], ['00:00', '04:00']
+          ]
+        ]
+      ];
+})(typeof globalThis !== 'undefined' && globalThis || typeof global !== 'undefined' && global ||
+   typeof window !== 'undefined' && window);
