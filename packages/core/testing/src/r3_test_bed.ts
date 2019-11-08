f8e9c1e6f1962fb61d221362cb3e0d308fcfbb51
Andrew Scott <atscott01@gmail.com>
2019-11-08 10:16:40 -0800

    revert: fix(ivy): Only restore registered modules if user compiles modules with TestBed (#32944) (#33663)
    This commit reverts 63256b5.
    
    PR Close #33663

--- packages/core/testing/src/r3_test_bed.ts
+++ packages/core/testing/src/r3_test_bed.ts
@@ -35,7 +35,7 @@ import {MetadataOverride} from './metadata_override';
 import {TestBed} from './test_bed';
 import {ComponentFixtureAutoDetect, ComponentFixtureNoNgZone, TestBedStatic, TestComponentRenderer, TestModuleMetadata} from './test_bed_common';
 import {R3TestBedCompiler} from './r3_test_bed_compiler';
-import {clearRegisteredModuleState} from '../../src/linker/ng_module_factory_registration';
+import {clearModuleRegistry} from '../../src/linker/ng_module_factory_registration';
 
 let _nextRootElementId = 0;
 
@@ -229,6 +229,7 @@ export class TestBedRender3 implements TestBed {
   }
 
   resetTestingModule(): void {
+    clearModuleRegistry();
     this.checkGlobalCompilationFinished();
     resetCompiledComponents();
     if (this._compiler !== null) {
