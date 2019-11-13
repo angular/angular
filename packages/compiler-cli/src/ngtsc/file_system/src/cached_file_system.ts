f288df0f3048eadca0b65d8ed6f31cd50c7ad44c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-13 14:45:46 -0800

    test(ivy): support chdir() on the compiler's filesystem abstraction
    This commit adds the ability to change directories using the compiler's
    internal filesystem abstraction. This is a prerequisite for writing tests
    which are sensitive to the current working directory.
    
    In addition to supporting the `chdir()` operation, this commit also fixes
    `getDefaultLibLocation()` for mock filesystems to not assume `node_modules`
    is in the current directory, but to resolve it similarly to how Node does
    by progressively looking higher in the directory tree.

--- packages/compiler-cli/src/ngtsc/file_system/src/cached_file_system.ts
+++ packages/compiler-cli/src/ngtsc/file_system/src/cached_file_system.ts
@@ -99,6 +99,7 @@ export class CachedFileSystem implements FileSystem {
   // The following methods simply call through to the delegate.
   readdir(path: AbsoluteFsPath): PathSegment[] { return this.delegate.readdir(path); }
   pwd(): AbsoluteFsPath { return this.delegate.pwd(); }
+  chdir(path: AbsoluteFsPath): void { this.delegate.chdir(path); }
   extname(path: AbsoluteFsPath|PathSegment): string { return this.delegate.extname(path); }
   isCaseSensitive(): boolean { return this.delegate.isCaseSensitive(); }
   isRoot(path: AbsoluteFsPath): boolean { return this.delegate.isRoot(path); }
