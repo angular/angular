2f74561784c0f895a5e1c2e869ae20465ca9a52f
Pete Bacon Darwin <pete@bacondarwin.com>
2019-11-14 16:15:36 +0000

    docs(ivy): improve the missing `$localize` error message
    If the application is not running directly in the browser, e.g.
    universal or app-shell, then the `$localize` import must be
    adding to a different file than for normal browser applications.
    
    This commit adds more information about this to avoid any
    confusion.
    
    // FW-1557

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -48,6 +48,7 @@ if (ngDevMode) {
         'It looks like your application or one of its dependencies is using i18n.\n' +
         'Angular 9 introduced a global `$localize()` function that needs to be loaded.\n' +
         'Please run `ng add @angular/localize` from the Angular CLI.\n' +
-        '(For non-CLI projects, add `import \'@angular/localize/init\';` to your polyfills.ts file)');
+        '(For non-CLI projects, add `import \'@angular/localize/init\';` to your `polyfills.ts` file.\n' +
+        'For server-side rendering applications add the import to your `main.server.ts` file.)');
   };
 }
