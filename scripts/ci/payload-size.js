025c584be7d5d41176c0b0feea9f7a2346fef1db
Kara Erickson <karakara@google.com>
2019-11-21 11:42:11 -0800

    ci: tighten size threshold to 1% or 500 bytes
    The size diff threshold of 1% has proven to be too lenient for us
    to catch size regressions in AIO. Since the AIO main bundle is
    between 400-500 KB, a size regression must be between 4-5 KB before
    it will cause the tests to fail. As a result, we may merge many
    changes with smaller regressions of a few KB before the size test
    eventually lets us know that the number has increased. The hope is
    that lowering the threshold will help us catch the smaller
    regressions during code review and prevent the size tests failing at
    a random later time when someone catches the size "hot potato".

--- scripts/ci/payload-size.js
+++ scripts/ci/payload-size.js
@@ -28,16 +28,20 @@ for (const compressionType in limitSizes) {
         console.log(
             `Commit ${commit} ${compressionType} ${filename} meassurement is missing. ` +
             'Maybe the file was renamed or removed.');
-      } else if (Math.abs(actualSize - expectedSize) > expectedSize / 100) {
-        failed = true;
-        // We must also catch when the size is significantly lower than the payload limit, so
-        // we are forced to update the expected payload number when the payload size reduces.
-        // Otherwise, we won't be able to catch future regressions that happen to be below
-        // the artificially inflated limit.
-        const operator = actualSize > expectedSize ? 'exceeded' : 'fell below';
-        console.log(
-            `Commit ${commit} ${compressionType} ${filename} ${operator} expected size by >1% ` +
+      } else {
+        const absoluteSizeDiff = Math.abs(actualSize - expectedSize);
+        // If size diff is larger than 1% or 500 bytes...
+        if (absoluteSizeDiff > 500 || absoluteSizeDiff > expectedSize / 100) {
+          failed = true;
+          // We must also catch when the size is significantly lower than the payload limit, so
+          // we are forced to update the expected payload number when the payload size reduces.
+          // Otherwise, we won't be able to catch future regressions that happen to be below
+          // the artificially inflated limit.
+          const operator = actualSize > expectedSize ? 'exceeded' : 'fell below';
+          console.log(
+            `Commit ${commit} ${compressionType} ${filename} ${operator} expected size by 500 bytes or >1% ` +
             `(expected: ${expectedSize}, actual: ${actualSize}).`);
+        }
       }
     }
   }
@@ -47,5 +51,5 @@ if (failed) {
   console.log(`If this is a desired change, please update the size limits in file '${limitFile}'.`);
   process.exit(1);
 } else {
-  console.log('Payload size <1% change check passed.');
+  console.log('Payload size check passed. The diff is less than 1% or 500 bytes.');
 }
