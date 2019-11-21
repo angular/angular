fb51499feaa8bf08af320eeba3b0ec42565b4e1d
crisbeto <crisbeto@abv.bg>
2019-11-21 21:07:54 +0100

    fix(ivy): add flag to skip non-exported classes (#33921)
    In ViewEngine we were only generating code for exported classes, however with Ivy we do it no matter whether the class has been exported or not. These changes add an extra flag that allows consumers to opt into the ViewEngine behavior. The flag works by treating non-exported classes as if they're set to `jit: true`.
    
    Fixes #33724.
    
    PR Close #33921

--- packages/compiler-cli/src/ngtsc/program.ts
+++ packages/compiler-cli/src/ngtsc/program.ts
@@ -642,7 +642,8 @@ export class NgtscProgram implements api.Program {
 
     return new IvyCompilation(
         handlers, this.reflector, this.importRewriter, this.incrementalDriver, this.perfRecorder,
-        this.sourceToFactorySymbols, scopeRegistry);
+        this.sourceToFactorySymbols, scopeRegistry,
+        this.options.compileNonExportedClasses !== false);
   }
 
   private getI18nLegacyMessageFormat(): string {
