3f6a1fda916c0e490a5a38708222116dac3aa52f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-22 01:19:51 +0100

    build: enable bazel node modules symlinking [resubmit w/ fixes] (#17776)
    Apparently we accidentally skipped a few post-install patches because
    we added the edit marker file for the first patch being applied to a file. This
    causes subsequent patches targeting the same file to be skipped accidentally

--- tools/bazel/postinstall-patches.js
+++ tools/bazel/postinstall-patches.js
@@ -8,17 +8,23 @@ const shelljs = require('shelljs');
 const path = require('path');
 const fs = require('fs');
 
+/**
+ * Version of the post install patch. Needs to be incremented when patches
+ * have been added or removed.
+ */
+const PATCH_VERSION = 1;
+
 /** Path to the project directory. */
 const projectDir = path.join(__dirname, '../..');
 
+/**
+ * Object that maps a given file path to a list of patches that need to be
+ * applied.
+ */
+const PATCHES_PER_FILE = {};
+
 shelljs.set('-e');
 shelljs.cd(projectDir);
-
-// Do not apply postinstall patches when running "postinstall" outside. The
-// "generate_build_file.js" file indicates that we run in Bazel managed node modules.
-if (!shelljs.test('-e', 'generate_build_file.js')) {
-  return;
-}
 
 // Workaround for https://github.com/angular/angular/issues/18810.
 shelljs.exec('ngc -p angular-tsconfig.json');
@@ -69,7 +75,7 @@ searchAndReplace(
   const hasFlatModuleBundle = fs.existsSync(filePath.replace('.d.ts', '.metadata.json'));
   if ((filePath.includes('node_modules/') || !hasFlatModuleBundle) && $1`,
     'node_modules/@angular/compiler-cli/src/transformers/compiler_host.js');
-shelljs.cat(path.join(__dirname, './flat_module_factory_resolution.patch')).exec('patch -p0');
+applyPatch(path.join(__dirname, './flat_module_factory_resolution.patch'));
 // The three replacements below ensure that metadata files can be read by NGC and
 // that metadata files are collected as Bazel action inputs.
 searchAndReplace(
@@ -90,7 +96,7 @@ searchAndReplace(
     'node_modules/@angular/bazel/src/ng_module.bzl');
 
 // Workaround for: https://github.com/bazelbuild/rules_nodejs/issues/1208.
-shelljs.cat(path.join(__dirname, './manifest_externs_hermeticity.patch')).exec('patch -p0');
+applyPatch(path.join(__dirname, './manifest_externs_hermeticity.patch'));
 
 // Workaround for using Ngcc with "--create-ivy-entry-points". This is a special
 // issue for our repository since we want to run Ivy by default in the module resolution,
@@ -136,18 +142,67 @@ shelljs.rm('-rf', [
   'node_modules/rxjs/Subscription.*',
 ]);
 
+// Apply all collected patches on a per-file basis. This is necessary because
+// multiple edits might apply to the same file, and we only want to mark a given
+// file as patched once all edits have been made.
+Object.keys(PATCHES_PER_FILE).forEach(filePath => {
+  if (hasFileBeenPatched(filePath)) {
+    console.info('File ' + filePath + ' is already patched. Skipping..');
+    return;
+  }
+
+  let content = fs.readFileSync(filePath, 'utf8');
+  const patchFunctions = PATCHES_PER_FILE[filePath];
+
+  console.info(`Patching file ${filePath} with ${patchFunctions.length} edits..`);
+  patchFunctions.forEach(patchFn => content = patchFn(content));
+
+  fs.writeFileSync(filePath, content, 'utf8');
+  writePatchMarker(filePath);
+});
+
 /**
- * Reads the specified file and replaces matches of the search expression
- * with the given replacement. Throws if no changes were made.
+ * Applies the given patch if not done already. Throws if the patch does
+ * not apply cleanly.
+ */
+function applyPatch(patchFile) {
+  const patchMarkerFileName = `${path.basename(patchFile)}.patch_marker`;
+  const patchMarkerPath = path.join(projectDir, 'node_modules/', patchMarkerFileName);
+
+  if (hasFileBeenPatched(patchMarkerPath)) {
+    return;
+  }
+
+  writePatchMarker(patchMarkerPath);
+  shelljs.cat(patchFile).exec('patch -p0');
+}
+
+/**
+ * Schedules an edit where the specified file is read and its content replaced based on
+ * the given search expression and corresponding replacement. Throws if no changes were made
+ * and the patch has not been applied.
  */
 function searchAndReplace(search, replacement, relativeFilePath) {
   const filePath = path.join(projectDir, relativeFilePath);
-  const originalContent = fs.readFileSync(filePath, 'utf8');
-  const newFileContent = originalContent.replace(search, replacement);
+  const fileEdits = PATCHES_PER_FILE[filePath] || (PATCHES_PER_FILE[filePath] = []);
 
-  if (originalContent === newFileContent) {
-    throw Error(`Could not perform replacement in: ${filePath}.`);
-  }
+  fileEdits.push(originalContent => {
+    const newFileContent = originalContent.replace(search, replacement);
+    if (originalContent === newFileContent) {
+      throw Error(`Could not perform replacement in: ${filePath}.`);
+    }
+    return newFileContent;
+  });
+}
 
-  fs.writeFileSync(filePath, newFileContent, 'utf8');
+/** Marks the specified file as patched. */
+function writePatchMarker(filePath) {
+  new shelljs.ShellString(PATCH_VERSION).to(`${filePath}.patch_marker`);
+}
+
+/** Checks if the given file has been patched. */
+function hasFileBeenPatched(filePath) {
+  const markerFilePath = `${filePath}.patch_marker`;
+  return shelljs.test('-e', markerFilePath) &&
+      shelljs.cat(markerFilePath).toString().trim() === `${PATCH_VERSION}`;
 }
