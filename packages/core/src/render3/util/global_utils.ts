3b00ebd825e2f74288228124096ab55e41ada88e
Joey Perrott <josephperrott.github@gmail.com>
2019-11-22 12:07:29 -0800

    build: migrate references and scripts that set to build with ivy via compile=aot to use config=ivy
    Since config=ivy now sets the define=compile flag and the define=angular_ivy_enabled
    flag to cause usage of Ivy, we can update all of the documentation and scripts that
    reference compile=aot to use config=ivy.

--- packages/core/src/render3/util/global_utils.ts
+++ packages/core/src/render3/util/global_utils.ts
@@ -17,7 +17,7 @@ import {getComponent, getContext, getDebugNode, getDirectives, getHostElement, g
  *
  * To see this in action run the following command:
  *
- *   bazel run --define=compile=aot
+ *   bazel run --config=ivy
  *   //packages/core/test/bundling/todo:devserver
  *
  *  Then load `localhost:5432` and start using the console tools.
