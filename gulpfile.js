796992ed8876477806e2891b5dc2f4eb82c69bfe
Igor Minar <igor@angular.io>
2019-11-22 16:45:46 -0800

    build: remove obsolete build related tools and helper scripts (#34058)
    none of these files are needed any more as they were replaced by Bazel.
    
    PR Close #34058

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,6 @@ gulp.task('validate-commit-messages', loadTask('validate-commit-message'));
 gulp.task('source-map-test', loadTask('source-map-test'));
 gulp.task('tools:build', loadTask('tools-build'));
 gulp.task('check-cycle', loadTask('check-cycle'));
-gulp.task('serve', loadTask('serve', 'default'));
 gulp.task('changelog', loadTask('changelog'));
 gulp.task('changelog:zonejs', loadTask('changelog-zonejs'));
 gulp.task('check-env', () => {/* this is a noop because the env test ran already above */});
