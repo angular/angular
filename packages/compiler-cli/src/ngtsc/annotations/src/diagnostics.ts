f563c7c656ba9fac36e29836dd920f4ee9f4c69a
crisbeto <crisbeto@abv.bg>
2019-11-26 19:33:26 +0100

    feat(ivy): error in ivy when inheriting a ctor from an undecorated base (#34460)
    Angular View Engine uses global knowledge to compile the following code:
    
    ```typescript
    export class Base {
      constructor(private vcr: ViewContainerRef) {}
    }
    
    @Directive({...})
    export class Dir extends Base {
      // constructor inherited from base
    }
    ```
    
    Here, `Dir` extends `Base` and inherits its constructor. To create a `Dir`
    the arguments to this inherited constructor must be obtained via dependency
    injection. View Engine is able to generate a correct factory for `Dir` to do
    this because via metadata it knows the arguments of `Base`'s constructor,
    even if `Base` is declared in a different library.
    
    In Ivy, DI is entirely a runtime concept. Currently `Dir` is compiled with
    an ngDirectiveDef field that delegates its factory to `getInheritedFactory`.
    This looks for some kind of factory function on `Base`, which comes up
    empty. This case looks identical to an inheritance chain with no
    constructors, which works today in Ivy.
    
    Both of these cases will now become an error in this commit. If a decorated
    class inherits from an undecorated base class, a diagnostic is produced
    informing the user of the need to either explicitly declare a constructor or
    to decorate the base class.
    
    PR Close #34460

--- packages/compiler-cli/src/ngtsc/annotations/src/diagnostics.ts
+++ packages/compiler-cli/src/ngtsc/annotations/src/diagnostics.ts
@@ -10,8 +10,12 @@ import * as ts from 'typescript';
 
 import {ErrorCode, makeDiagnostic} from '../../diagnostics';
 import {Reference} from '../../imports';
-import {InjectableClassRegistry} from '../../metadata';
-import {ClassDeclaration} from '../../reflection';
+import {InjectableClassRegistry, MetadataReader} from '../../metadata';
+import {PartialEvaluator} from '../../partial_evaluator';
+import {ClassDeclaration, ReflectionHost} from '../../reflection';
+import {LocalModuleScopeRegistry} from '../../scope';
+
+import {makeDuplicateDeclarationError, readBaseClass} from './util';
 
 /**
  * Gets the diagnostics for a set of provider classes.
@@ -40,4 +44,91 @@ Either add the @Injectable() decorator to '${provider.node.name.text}', or confi
   }
 
   return diagnostics;
+}
+
+export function getDirectiveDiagnostics(
+    node: ClassDeclaration, reader: MetadataReader, evaluator: PartialEvaluator,
+    reflector: ReflectionHost, scopeRegistry: LocalModuleScopeRegistry,
+    kind: string): ts.Diagnostic[]|null {
+  let diagnostics: ts.Diagnostic[]|null = [];
+
+  const addDiagnostics = (more: ts.Diagnostic | ts.Diagnostic[] | null) => {
+    if (more === null) {
+      return;
+    } else if (diagnostics === null) {
+      diagnostics = Array.isArray(more) ? more : [more];
+    } else if (Array.isArray(more)) {
+      diagnostics.push(...more);
+    } else {
+      diagnostics.push(more);
+    }
+  };
+
+  const duplicateDeclarations = scopeRegistry.getDuplicateDeclarations(node);
+
+  if (duplicateDeclarations !== null) {
+    addDiagnostics(makeDuplicateDeclarationError(node, duplicateDeclarations, kind));
+  }
+
+  addDiagnostics(checkInheritanceOfDirective(node, reader, reflector, evaluator));
+  return diagnostics;
+}
+
+export function checkInheritanceOfDirective(
+    node: ClassDeclaration, reader: MetadataReader, reflector: ReflectionHost,
+    evaluator: PartialEvaluator): ts.Diagnostic|null {
+  if (!reflector.isClass(node) || reflector.getConstructorParameters(node) !== null) {
+    // We should skip nodes that aren't classes. If a constructor exists, then no base class
+    // definition is required on the runtime side - it's legal to inherit from any class.
+    return null;
+  }
+
+  // The extends clause is an expression which can be as dynamic as the user wants. Try to
+  // evaluate it, but fall back on ignoring the clause if it can't be understood. This is a View
+  // Engine compatibility hack: View Engine ignores 'extends' expressions that it cannot understand.
+  let baseClass = readBaseClass(node, reflector, evaluator);
+
+  while (baseClass !== null) {
+    if (baseClass === 'dynamic') {
+      return null;
+    }
+
+    // We can skip the base class if it has metadata.
+    const baseClassMeta = reader.getDirectiveMetadata(baseClass);
+    if (baseClassMeta !== null) {
+      return null;
+    }
+
+    // If the base class has a blank constructor we can skip it since it can't be using DI.
+    const baseClassConstructorParams = reflector.getConstructorParameters(baseClass.node);
+    const newParentClass = readBaseClass(baseClass.node, reflector, evaluator);
+
+    if (baseClassConstructorParams !== null && baseClassConstructorParams.length > 0) {
+      // This class has a non-trivial constructor, that's an error!
+      return getInheritedUndecoratedCtorDiagnostic(node, baseClass, reader);
+    } else if (baseClassConstructorParams !== null || newParentClass === null) {
+      // This class has a trivial constructor, or no constructor + is the
+      // top of the inheritance chain, so it's okay.
+      return null;
+    }
+
+    // Go up the chain and continue
+    baseClass = newParentClass;
+  }
+
+  return null;
+}
+
+function getInheritedUndecoratedCtorDiagnostic(
+    node: ClassDeclaration, baseClass: Reference, reader: MetadataReader) {
+  const subclassMeta = reader.getDirectiveMetadata(new Reference(node)) !;
+  const dirOrComp = subclassMeta.isComponent ? 'Component' : 'Directive';
+  const baseClassName = baseClass.debugName;
+
+  return makeDiagnostic(
+      ErrorCode.DIRECTIVE_INHERITS_UNDECORATED_CTOR, node.name,
+      `The ${dirOrComp.toLowerCase()} ${node.name.text} inherits its constructor from ${baseClassName}, ` +
+          `but the latter does not have an Angular decorator of its own. Dependency injection will not be able to ` +
+          `resolve the parameters of ${baseClassName}'s constructor. Either add a @Directive decorator ` +
+          `to ${baseClassName}, or add an explicit constructor to ${node.name.text}.`);
 }
