7762c280e17f4b4c9ccc9d8a689e70ad71ffd600
Sonu Kapoor <sonukapoor@gmail.com>
2019-11-26 14:11:45 -0500

    test(docs-infra): ensure RxJS-related docs examples can compile without errors (#34063)
    Previously, some RxJS-related examples (which are not proper Angular apps) were not
    tested on CI as part of the `example-e2e` npm script. This meant that the examples
    could get out-of-date or contain compile errors without as noticing.
    
    This commit ensures that the `example-e2e` script picks up these examples and checks
    that they compile successfully.
    
    Partly addresses #28017.
    
    PR Close #34063

--- aio/content/examples/practical-observable-usage/src/typeahead.ts
+++ aio/content/examples/practical-observable-usage/src/typeahead.ts
@@ -1,12 +1,12 @@
-
 import { fromEvent } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap } from 'rxjs/operators';
+
 
 const searchBox = document.getElementById('search-box');
 
 const typeahead = fromEvent(searchBox, 'input').pipe(
-  map((e: KeyboardEvent) => e.target.value),
+  map((e: KeyboardEvent) => (e.target as HTMLInputElement).value),
   filter(text => text.length > 2),
   debounceTime(10),
   distinctUntilChanged(),
