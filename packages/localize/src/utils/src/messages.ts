80d326bd49ddcba42140427c6aa8548af5f45ca2
Pete Bacon Darwin <pete@bacondarwin.com>
2019-12-03 08:36:38 +0000

    refactor(ivy): i18n - remove hack around TS tagged literals (#34135)
    When first written there was no way to specify the raw text when
    programmatically creating a template tagged literal AST node.
    
    This is now fixed in TS and so the hack is no longer needed.
    
    PR Close #34135

--- packages/localize/src/utils/src/messages.ts
+++ packages/localize/src/utils/src/messages.ts
@@ -182,18 +182,6 @@ export function parseMetadata(cooked: string, raw: string): MessageMetadata {
  * Since blocks are optional, it is possible that the content of a message block actually starts
  * with a block marker. In this case the marker must be escaped `\:`.
  *
- * ---
- *
- * If the template literal was synthesized and downleveled by TypeScript to ES5 then its
- * raw array will only contain empty strings. This is because the current TypeScript compiler uses
- * the original source code to find the raw text and in the case of synthesized AST nodes, there is
- * no source code to draw upon.
- *
- * The workaround in this function is to assume that the template literal did not contain an escaped
- * placeholder name, and fall back on checking the cooked array instead.
- * This is a limitation if compiling to ES5 in TypeScript but is not a problem if the TypeScript
- * output is ES2015 and the code is downlevelled by a separate tool as happens in the Angular CLI.
- *
  * @param cooked The cooked version of the message part to parse.
  * @param raw The raw version of the message part to parse.
  * @returns An object containing the `text` of the message part and the text of the `block`, if it
@@ -201,7 +189,6 @@ export function parseMetadata(cooked: string, raw: string): MessageMetadata {
  * @throws an error if the `block` is unterminated
  */
 export function splitBlock(cooked: string, raw: string): {text: string, block?: string} {
-  raw = raw || cooked;
   if (raw.charAt(0) !== BLOCK_MARKER) {
     return {text: cooked};
   } else {
