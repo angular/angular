85736775288410531e6ecfcea3356d6f257920b4
Pete Bacon Darwin <pete@bacondarwin.com>
2019-12-19 22:43:12 +0000

    refactor(ngcc): pass dependency info to `collectDependencies()` (#34494)
    Rather than return a new object of dependency info from calls to
    `collectDependencies()` we now pass in an object that will be updated
    with the dependency info. This is in preparation of a change where
    we will collect dependency information from more than one
    `DependencyHost`.
    
    Also to better fit with this approach the name is changed from
    `findDependencies()` to `collectDependencies()`.
    
    PR Close #34494

--- packages/compiler-cli/ngcc/src/dependencies/commonjs_dependency_host.ts
+++ packages/compiler-cli/ngcc/src/dependencies/commonjs_dependency_host.ts
@@ -27,7 +27,7 @@ export class CommonJsDependencyHost extends DependencyHostBase {
    * @param alreadySeen A set that is used to track internal dependencies to prevent getting stuck
    * in a circular dependency loop.
    */
-  protected recursivelyFindDependencies(
+  protected recursivelyCollectDependencies(
       file: AbsoluteFsPath, dependencies: Set<AbsoluteFsPath>, missing: Set<string>,
       deepImports: Set<AbsoluteFsPath>, alreadySeen: Set<AbsoluteFsPath>): void {
     const fromContents = this.fs.readFile(file);
@@ -53,7 +53,7 @@ export class CommonJsDependencyHost extends DependencyHostBase {
               const internalDependency = resolvedModule.modulePath;
               if (!alreadySeen.has(internalDependency)) {
                 alreadySeen.add(internalDependency);
-                this.recursivelyFindDependencies(
+                this.recursivelyCollectDependencies(
                     internalDependency, dependencies, missing, deepImports, alreadySeen);
               }
             } else {
