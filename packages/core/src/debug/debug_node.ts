de90b5dd3c98fe22619b17cc7473cb3710b9d0b9
Andrew Scott <atscott01@gmail.com>
2019-12-20 10:20:59 -0800

    fix(ivy): ensure eventListeners added outside angular context are not called... (#34514)
    by DebugElement.triggerEventHandler. ZoneJS tracks the eventListeners on
    a node but we need to be able to differentiate between those added by
    Angular and those that were added outside the Angular context. This fix
    aligns with the behavior that was present in View Engine (not calling
    those listeners). If we decide later that we want to call those
    listeners, we still need a way to differentiate between those that
    we have wrapped in dom_renderer and those that were not (because they
    were added outside the Angular context).
    
    PR Close #34514

--- packages/core/src/debug/debug_node.ts
+++ packages/core/src/debug/debug_node.ts
@@ -410,7 +410,7 @@ class DebugElement__POST_R3__ extends DebugNode__POST_R3__ implements DebugEleme
     this.listeners.forEach(listener => {
       if (listener.name === eventName) {
         const callback = listener.callback;
-        callback(eventObj);
+        callback.call(node, eventObj);
         invokedListeners.push(callback);
       }
     });
@@ -419,11 +419,20 @@ class DebugElement__POST_R3__ extends DebugNode__POST_R3__ implements DebugEleme
     // that Zone.js only adds to `EventTarget` in browser environments.
     if (typeof node.eventListeners === 'function') {
       // Note that in Ivy we wrap event listeners with a call to `event.preventDefault` in some
-      // cases. We use `Function` as a special token that gives us access to the actual event
+      // cases. We use '__ngUnwrap__' as a special token that gives us access to the actual event
       // listener.
       node.eventListeners(eventName).forEach((listener: Function) => {
-        const unwrappedListener = listener(Function);
-        return invokedListeners.indexOf(unwrappedListener) === -1 && unwrappedListener(eventObj);
+        // In order to ensure that we can detect the special __ngUnwrap__ token described above, we
+        // use `toString` on the listener and see if it contains the token. We use this approach to
+        // ensure that it still worked with compiled code since it cannot remove or rename string
+        // literals. We also considered using a special function name (i.e. if(listener.name ===
+        // special)) but that was more cumbersome and we were also concerned the compiled code could
+        // strip the name, turning the condition in to ("" === "") and always returning true.
+        if (listener.toString().indexOf('__ngUnwrap__') !== -1) {
+          const unwrappedListener = listener('__ngUnwrap__');
+          return invokedListeners.indexOf(unwrappedListener) === -1 &&
+              unwrappedListener.call(node, eventObj);
+        }
       });
     }
   }
