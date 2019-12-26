2514cf0addf8f3f1c53c69d1f0667537d6037fb7
ayazhafiz <ayaz.hafiz.1@gmail.com>
2019-12-26 09:30:07 -0600

    fixup! feat(language-service): provide completion for $event variable

--- packages/language-service/test/completions_spec.ts
+++ packages/language-service/test/completions_spec.ts
@@ -749,7 +749,6 @@ describe('completions', () => {
     it('should suggest $event in event bindings', () => {
       mockHost.override(TEST_TEMPLATE, `<div (click)="myClick(~{cursor});"></div>`);
       const marker = mockHost.getLocationMarkerFor(TEST_TEMPLATE, 'cursor');
-      debugger;
       const completions = ngLS.getCompletionsAt(TEST_TEMPLATE, marker.start);
       expectContain(completions, CompletionKind.VARIABLE, ['$event']);
     });
