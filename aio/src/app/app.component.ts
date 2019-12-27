c59e448b196828ea3e4d4b4d76df90b059fd787b
Sonu Kapoor <sonukapoor@gmail.com>
2019-12-27 12:01:35 -0500

    docs: add back to top link for narrow view
    In narrow screens where the viewport is less than 800px, we
    hide the ToC, so there is no way for the user to scroll to top.
    This PR adds a link to the bottom right corner to overcome this.
    
    Closes #16223

--- aio/src/app/app.component.ts
+++ aio/src/app/app.component.ts
@@ -400,6 +400,10 @@ export class AppComponent implements OnInit {
     this.showSearchResults = !!query;
   }
 
+  scrollToTop() {
+    this.scrollService.scrollToTop();
+  }
+
   @HostListener('document:keyup', ['$event.key', '$event.which'])
   onKeyUp(key: string, keyCode: number) {
     // forward slash "/"
