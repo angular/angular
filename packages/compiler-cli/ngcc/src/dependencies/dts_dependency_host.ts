32d4d9711181cf69f867fc8606f522931bf722e5
Pete Bacon Darwin <pete@bacondarwin.com>
2020-01-09 14:23:50 +0000

    fix(ngcc): do not add DTS deep imports to missing packages list (#34695)
    When searching the typings program for a package for imports a
    distinction is drawn between missing entry-points and deep imports.
    
    Previously in the `DtsDependencyHost` these deep imports may be
    marked as missing if there was no typings file at the deep import path.
    Instead there may be a javascript file instead. In practice this means
    the import is "deep" and not "missing".
    
    Now the `DtsDependencyHost` will also consider `.js` files when checking
    for deep-imports, and it will also look inside `@types/...` for a suitable
    deep-imported typings file.
    
    Fixes #34720
    
    PR Close #34695

--- packages/compiler-cli/ngcc/src/dependencies/dts_dependency_host.ts
+++ packages/compiler-cli/ngcc/src/dependencies/dts_dependency_host.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import {FileSystem} from '../../../src/ngtsc/file_system';
+import {AbsoluteFsPath, FileSystem} from '../../../src/ngtsc/file_system';
 import {PathMappings} from '../utils';
 import {EsmDependencyHost} from './esm_dependency_host';
 import {ModuleResolver} from './module_resolver';
@@ -15,6 +15,18 @@ import {ModuleResolver} from './module_resolver';
  */
 export class DtsDependencyHost extends EsmDependencyHost {
   constructor(fs: FileSystem, pathMappings?: PathMappings) {
-    super(fs, new ModuleResolver(fs, pathMappings, ['', '.d.ts', '/index.d.ts']));
+    super(
+        fs, new ModuleResolver(fs, pathMappings, ['', '.d.ts', '/index.d.ts', '.js', '/index.js']));
+  }
+
+  /**
+   * Attempts to process the `importPath` directly and also inside `@types/...`.
+   */
+  protected processImport(
+      importPath: string, file: AbsoluteFsPath, dependencies: Set<AbsoluteFsPath>,
+      missing: Set<string>, deepImports: Set<string>, alreadySeen: Set<AbsoluteFsPath>): boolean {
+    return super.processImport(importPath, file, dependencies, missing, deepImports, alreadySeen) ||
+        super.processImport(
+            `@types/${importPath}`, file, dependencies, missing, deepImports, alreadySeen);
   }
 }
