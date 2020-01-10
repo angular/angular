e238de33eb71cc82c7ec22328b64f17a1c3de305
atscott <atscott@google.com>
2020-01-10 13:39:57 -0800

    Revert "build: update to rules nodejs 1.0.1 (#34589)" (#34730)
    This reverts commit 8042433cb09098c658fdec39c49edaeac2ac2c1d.
    
    PR Close #34730

--- packages/zone.js/test/node_error_disable_policy_entry_point.ts
+++ packages/zone.js/test/node_error_disable_policy_entry_point.ts
@@ -6,10 +6,13 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-// bootstrap the bazel require patch since this bootstrap script is loaded with
-// `--node_options=--require=$(rlocation $(location script.js))`
-if (process.env['BAZEL_NODE_RUNFILES_HELPER']) {
-  require(process.env['BAZEL_NODE_RUNFILES_HELPER'] as string).patchRequire();
+if (process.env['TEST_SRCDIR']) {
+  // bootstrap the bazel require resolve patch since this
+  // script is a bootstrap script loaded with --node_options=--require=...
+  const path = require('path');
+  require(path.posix.join(
+      process.env['TEST_SRCDIR'], process.env['TEST_WORKSPACE'],
+      (process.env['TEST_BINARY'] as string).replace(/\.(sh|bat)$/, '_loader.js'), ));
 }
 
 process.env['errorpolicy'] = (global as any)['__Zone_Error_BlacklistedStackFrames_policy'] =
