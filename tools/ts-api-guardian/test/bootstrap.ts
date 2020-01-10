0331ee7d677aac2d11d96b60a208a2db58fd91e4
atscott <atscott@google.com>
2020-01-10 13:39:34 -0800

    Revert "build: cleanup //tools/ts-api-guardian:tests target (#34589)"
    This reverts commit 7a63e4f7d5b3a59a1ae45340cdd72ffb8b22730c.

--- tools/ts-api-guardian/test/bootstrap.ts
+++ tools/ts-api-guardian/test/bootstrap.ts
@@ -6,11 +6,13 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-const runfiles = require(process.env['BAZEL_NODE_RUNFILES_HELPER']);
+import * as path from 'path';
 
-// Change directories to the path of the ts-api-guardian source tree. We need to resolve an actual
-// path of a tree because we want to determine the path to the directory that includes all
+// Resolve the path to the package.json of the ts-api-guardian. We need to resolve an actual
+// path of a runfile because we want to determine the path to the directory that includes all
 // test fixture runfiles. On Windows this is usually the original non-sandboxed disk location,
 // otherwise this just refers to the runfile directory with all the proper symlinked files.
 // TODO: remove the whole bootstrap file once the tests are Bazel and Windows compatible.
-process.chdir(runfiles.resolve('angular/tools/ts-api-guardian'));
+const runfilesDirectory = path.dirname(require.resolve('../package.json'));
+
+process.chdir(runfilesDirectory);
