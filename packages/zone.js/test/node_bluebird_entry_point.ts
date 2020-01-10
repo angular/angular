4955c256d25f059ba3db4268233b86cc892ec7af
atscott <atscott@google.com>
2020-01-10 13:40:02 -0800

    Revert "refactor: handle breaking changes in rules_nodejs 1.0.0 (#34589)" (#34730)
    This reverts commit 9bb349e1c8089ee29b82c7234a11ad4256f1d06f.
    
    PR Close #34730

--- packages/zone.js/test/node_bluebird_entry_point.ts
+++ packages/zone.js/test/node_bluebird_entry_point.ts
@@ -6,15 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-if (process.env['TEST_SRCDIR']) {
-  // bootstrap the bazel require resolve patch since this
-  // script is a bootstrap script loaded with --node_options=--require=...
-  const path = require('path');
-  require(path.posix.join(
-      process.env['TEST_SRCDIR'], process.env['TEST_WORKSPACE'],
-      (process.env['TEST_BINARY'] as string).replace(/\.(sh|bat)$/, '_loader.js'), ));
-}
-
 // Must be loaded before zone loads, so that zone can detect WTF.
 import './test_fake_polyfill';
 
