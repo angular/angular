c9c13ecdd014f886485452cd96412f04a9e9b355
Joey Perrott <josephperrott.github@gmail.com>
2020-01-28 07:03:01 -0800

    fixup! build: create tdd performance test script

--- scripts/local-dev/test-tdd.sh
+++ scripts/local-dev/test-tdd.sh
@@ -1,11 +1,11 @@
-# Short cuts to set output as bold and normal
+# Shortcuts to set output as bold and normal
 bold=$(tput bold)
 normal=$(tput sgr0)
 
 # Package being tested for turnaround time.
 test_package="//packages/core/test/acceptance:acceptance"
 # Bazel test command
-test_command="yarn -s bazel test ${test_package} --define=compile=aot > /dev/null 2>&1"
+test_command="yarn -s bazel test ${test_package} --config=ivy > /dev/null 2>&1"
 echo "Testing ${test_package}"
 
 # Set up a clean bazel environment
@@ -21,7 +21,7 @@ echo "  Initial build time: ${bold}$first_run seconds${normal}"
 
 # Make a change to invalidate cache
 echo "Adding empty new line to attributes_spec.ts file"
-echo >> ../../packages/core/test/acceptance/attributes_spec.ts
+echo "console.log('hello');" >> ../../packages/core/test/acceptance/attributes_spec.ts
 
 # Perform test again to determine turn around time.
 echo "Testing acceptance target"
