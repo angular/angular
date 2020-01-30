f7b023364ae954b5ab1430a837e26ca24b537e58
ajitsinghkaler <ajitsinghkaler0@gmail.com>
2020-01-30 21:33:43 +0530

    feat(docs-infra): change navigation in resources page
    https://angular.io/resources needs to be sturctured to be able to navigate to all resources with improved user experience. A lone scroll bar in this page will not help the reader a great deal in exploring the resources
    
    Fixes #33526

--- aio/src/app/custom-elements/resource/resource-list.component.ts
+++ aio/src/app/custom-elements/resource/resource-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Category } from './resource.model';
 import { ResourceService } from './resource.service';
+import { LocationService } from 'app/shared/location.service';
 
 /* tslint:disable:template-accessibility-elements-content */
 @Component({
@@ -11,16 +12,27 @@ import { ResourceService } from './resource.service';
 export class ResourceListComponent implements OnInit {
 
   categories: Category[];
-  location: string;
+  selectedCategory: Category;
 
   constructor(
-    private resourceService: ResourceService) {
-    this.location = location.pathname.replace(/^\/+/, '');
+    private resourceService: ResourceService,
+    private locationService: LocationService) {
   }
 
   ngOnInit() {
+    const category =  this.locationService.search()['category'] || '';
     // Not using async pipe because cats appear twice in template
     // No need to unsubscribe because categories observable completes.
-    this.resourceService.categories.subscribe(cats => this.categories = cats);
+    this.resourceService.categories.subscribe(cats => {
+      this.categories = cats;
+      this.selectCategory(category);
+    });
+  }
+
+  selectCategory(id: string) {
+    id = id.toLowerCase();
+    this.selectedCategory =
+      this.categories.find(category => category.id.toLowerCase() === id) || this.categories[0];
+    this.locationService.setSearch('', {category: this.selectedCategory.id});
   }
 }
