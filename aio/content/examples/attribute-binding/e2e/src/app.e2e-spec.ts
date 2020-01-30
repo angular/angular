2fd475ad49cdbf1f2e1636918c1a43646ad586c2
Kara Erickson <karakara@google.com>
2020-01-30 14:09:10 -0800

    docs(ivy): add docs for styling priority order (#35066)
    PR Close #35066

--- aio/content/examples/attribute-binding/e2e/src/app.e2e-spec.ts
+++ aio/content/examples/attribute-binding/e2e/src/app.e2e-spec.ts
@@ -25,23 +25,12 @@ describe('Attribute binding example', function () {
   });
 
   it('should display a blue div with a red border', function () {
-    expect(element.all(by.css('div')).get(4).getCssValue('border')).toEqual('2px solid rgb(212, 30, 46)');
+    expect(element.all(by.css('div')).get(1).getCssValue('border')).toEqual('2px solid rgb(212, 30, 46)');
   });
 
-  it('should display a div with replaced classes', function () {
-    expect(element.all(by.css('div')).get(5).getAttribute('class')).toEqual('new-class');
-  });
-
-  it('should display four buttons', function() {
-    let redButton = element.all(by.css('button')).get(1);
-    let saveButton = element.all(by.css('button')).get(2);
-    let bigButton = element.all(by.css('button')).get(3);
-    let smallButton = element.all(by.css('button')).get(4);
-
-    expect(redButton.getCssValue('color')).toEqual('rgba(255, 0, 0, 1)');
-    expect(saveButton.getCssValue('background-color')).toEqual('rgba(0, 255, 255, 1)');
-    expect(bigButton.getText()).toBe('Big');
-    expect(smallButton.getText()).toBe('Small');
+  it('should display a div with many classes', function () {
+    expect(element.all(by.css('div')).get(1).getAttribute('class')).toContain('special');
+    expect(element.all(by.css('div')).get(1).getAttribute('class')).toContain('clearance');
   });
 
 });
--- aio/content/examples/attribute-binding/src/app/app.module.ts
+++ aio/content/examples/attribute-binding/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CompWithHostBindingComponent } from './comp-with-host-binding.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompWithHostBindingComponent
   ],
   imports: [
     BrowserModule
