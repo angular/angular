73f655a4ced4fc3a7820092346dcfd975ba860f4
AleksanderBodurri <alex.bodurri@mail.utoronto.ca>
2020-01-31 17:17:14 -0500

    refactor(devtools): remove getWindow functions

--- src/iframe-message-bus.ts
+++ src/iframe-message-bus.ts
@@ -70,8 +70,4 @@ export class IFrameMessageBus extends MessageBus<Events> {
     this._listeners.forEach(l => window.removeEventListener('message', l));
     this._listeners = [];
   }
-
-  getWindow(): Window {
-    return this._docWindow;
-  }
 }
