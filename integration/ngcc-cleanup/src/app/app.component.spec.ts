2b00fe816b9413c198cd693fabb233ea88a0b5ec
Igor Minar <igor@angular.io>
2020-02-01 09:15:34 -0800

    test: add integration test for ngcc cleanup based on #35095
    it's a bit hacky, but it should repro the issue well while keeping the shape of the integration test similar
    to the other integration tests.

---
+++ integration/ngcc-cleanup/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'cli-hello-world'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('cli-hello-world');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('cli-hello-world app is running!');
+  });
+});
