d937bfac5bd6df71c896903fb873e1ebcdf83652
Miško Hevery <misko@hevery.com>
2020-02-03 13:22:36 -0800

    fixup! refactor(ivy): Explicitly pass in `TView`

--- packages/core/test/render3/perf/setup.ts
+++ packages/core/test/render3/perf/setup.ts
@@ -9,7 +9,7 @@ import {addToViewTree, createLContainer, createLView, createTNode, createTView, 
 import {ComponentTemplate, DirectiveDefList} from '../../../src/render3/interfaces/definition';
 import {TAttributes, TNodeType, TViewNode} from '../../../src/render3/interfaces/node';
 import {RendererFactory3, domRendererFactory3} from '../../../src/render3/interfaces/renderer';
-import {LView, LViewFlags, TView, TViewType} from '../../../src/render3/interfaces/view';
+import {LView, LViewFlags, TVIEW, TView, TViewType} from '../../../src/render3/interfaces/view';
 import {insertView} from '../../../src/render3/node_manipulation';
 
 import {MicroBenchmarkRendererFactory} from './noop_renderer';
@@ -22,7 +22,7 @@ const renderer = rendererFactory.createRenderer(null, null);
 export function createAndRenderLView(parentLView: LView, tView: TView, hostTNode: TViewNode) {
   const embeddedLView = createLView(
       parentLView, tView, {}, LViewFlags.CheckAlways, null, hostTNode, rendererFactory, renderer);
-  renderView(embeddedLView, tView, null);
+  renderView(tView, embeddedLView, null);
 }
 
 export function setupRootViewWithEmbeddedViews(
@@ -68,17 +68,18 @@ export function setupTestHarness(
     const embeddedLView = createLView(
         hostLView, embeddedTView, embeddedViewContext, LViewFlags.CheckAlways, null, viewTNode,
         rendererFactory, renderer);
-    renderView(embeddedLView, embeddedTView, embeddedViewContext);
+    renderView(embeddedTView, embeddedLView, embeddedViewContext);
     return embeddedLView;
   }
 
   function detectChanges(): void {
-    refreshView(hostLView, hostTView, hostTView.template, embeddedViewContext);
+    refreshView(hostTView, hostLView, hostTView.template, embeddedViewContext);
   }
 
   // create embedded views and add them to the container
   for (let i = 0; i < noOfViews; i++) {
-    insertView(createEmbeddedLView(), lContainer, i);
+    const lView = createEmbeddedLView();
+    insertView(lView[TVIEW], lView, lContainer, i);
   }
 
   return {
