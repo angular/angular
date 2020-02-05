d303ffb4fa901615f7321dcd0e925eef2ea6fa69
Sumit Arora <er.sumitarora@gmail.com>
2020-02-05 10:39:46 -0500

    style(devtools): Applying consistent styling (rangle/angular-devtools#36)

--- projects/ng-devtools/src/lib/devtools-tabs/devtools-tabs.module.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/devtools-tabs.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DevToolsTabsComponent } from './devtools-tabs.component';
 
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+
 import { DirectiveExplorerModule } from './directive-explorer/directive-explorer.module';
 import { ProfilerModule } from './directive-explorer/profiler/profiler.module';
 
 @NgModule({
   declarations: [DevToolsTabsComponent],
-  imports: [MatTabsModule, DirectiveExplorerModule, ProfilerModule, CommonModule],
+  imports: [MatTabsModule, MatIconModule, DirectiveExplorerModule, ProfilerModule, CommonModule],
   exports: [DevToolsTabsComponent]
 })
-export class DevToolsTabModule {}
+export class DevToolsTabModule { }
