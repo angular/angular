cec2c533cf6003c57f618061f139c575c9e1b67c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-02-06 14:17:03 +0100

    ci: ensure saucelabs browsers can load karma test page (#35171)
    In the past we had connecitivity issues on Saucelabs. Browsers on
    mobile devices were not able to properly resolve the `localhost`
    hostname through the tunnel. This is because the device resolves
    `localhost` or `127.0.0.1` to the actual Saucelabs device, while it
    should resolve to the tunnel host machine (in our case the CircleCI VM).
    
    In the past, we simply disabled the failing devices and re-enabled the
    devices later. At this point, the Saucelabs team claimed that the
    connecitivy/proxy issues were fixed.
    
    Saucelabs seems to have a process for VMs which ensures that requests to
    `localhost` / `127.0.0.1` are properly resolved through the tunnel. This
    process is not very reliable and can cause tests to fail. Related issues have been
    observed/mentioned in the Saucelabs support docs. e.g.
    
    https://support.saucelabs.com/hc/en-us/articles/115002212447-Unable-to-Reach-Application-on-localhost-for-Tests-Run-on-Safari-8-and-9-and-Edge
    https://support.saucelabs.com/hc/en-us/articles/225106887-Safari-and-Internet-Explorer-Won-t-Load-Website-When-Using-Sauce-Connect-on-Localhost
    
    In order to ensure that requests are always resolved through the tunnel,
    we add our own domain alias in the CircleCI's hosts file, and enforce that
    it is always resolved through the tunnel (using the `--tunnel-domains` SC flag).
    Saucelabs devices by default will never resolve this domain/hostname to the
    actual local Saucelabs device.
    
    PR Close #35171

--- tools/saucelabs/karma-saucelabs.js
+++ tools/saucelabs/karma-saucelabs.js
@@ -21,22 +21,18 @@ try {
   // KARMA_WEB_TEST_MODE is set which informs /karma-js.conf.js that it should
   // run the test with the karma saucelabs launcher
   process.env['KARMA_WEB_TEST_MODE'] = 'SL_REQUIRED';
+  // Saucelabs parameters read from a temporary file that is created by the `sauce-service`. This
+  // will be `null` if the test runs locally without the `sauce-service` being started.
+  const saucelabsParams = readLocalSauceConnectParams();
   // Setup required SAUCE_* env if they are not already set
   if (!process.env['SAUCE_USERNAME'] || !process.env['SAUCE_ACCESS_KEY'] ||
       !process.env['SAUCE_TUNNEL_IDENTIFIER']) {
-    try {
-      // The following path comes from /tools/saucelabs/sauce-service.sh.
-      // We setup the required saucelabs environment variables here for the karma test
-      // from a json file under /tmp/angular/sauce-service  so that we don't break the
-      // test cache with a changing SAUCE_TUNNEL_IDENTIFIER provided through --test_env
-      const scParams = require('/tmp/angular/sauce-service/sauce-connect-params.json');
-      process.env['SAUCE_USERNAME'] = scParams.SAUCE_USERNAME;
-      process.env['SAUCE_ACCESS_KEY'] = scParams.SAUCE_ACCESS_KEY;
-      process.env['SAUCE_TUNNEL_IDENTIFIER'] = scParams.SAUCE_TUNNEL_IDENTIFIER;
-    } catch (e) {
-      console.error(e.stack || e);
-      console.error(
-          `!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // We print a helpful error message below if the required Saucelabs parameters have not
+    // been specified in test environment, and the `sauce-service` params file has not been
+    // created either.
+    if (saucelabsParams === null) {
+      console.error(`
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!! Make sure that you have run "yarn bazel run //tools/saucelabs:sauce_service_setup"
 !!! (or "./tools/saucelabs/sauce-service.sh setup") before the test target. Alternately
 !!! you can provide the required SAUCE_* environment variables (SAUCE_USERNAME, SAUCE_ACCESS_KEY &
@@ -45,6 +41,16 @@ try {
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
       process.exit(1);
     }
+    process.env['SAUCE_USERNAME'] = saucelabsParams.SAUCE_USERNAME;
+    process.env['SAUCE_ACCESS_KEY'] = saucelabsParams.SAUCE_ACCESS_KEY;
+    process.env['SAUCE_TUNNEL_IDENTIFIER'] = saucelabsParams.SAUCE_TUNNEL_IDENTIFIER;
+    process.env['SAUCE_LOCALHOST_ALIAS_DOMAIN'] = saucelabsParams.SAUCE_LOCALHOST_ALIAS_DOMAIN;
+  }
+
+  // Pass through the optional `SAUCE_LOCALHOST_ALIAS_DOMAIN` environment variable. The
+  // variable is usually specified on CI, but is not required for testing with Saucelabs.
+  if (!process.env['SAUCE_LOCALHOST_ALIAS_DOMAIN'] && saucelabsParams !== null) {
+    process.env['SAUCE_LOCALHOST_ALIAS_DOMAIN'] = saucelabsParams.SAUCE_LOCALHOST_ALIAS_DOMAIN;
   }
 
   const scStart = `${sauceService} start-ready-wait`;
@@ -59,4 +65,16 @@ try {
 } catch (e) {
   console.error(e.stack || e);
   process.exit(1);
+}
+
+function readLocalSauceConnectParams() {
+  try {
+    // The following path comes from /tools/saucelabs/sauce-service.sh.
+    // We setup the required saucelabs environment variables here for the karma test
+    // from a json file under /tmp/angular/sauce-service  so that we don't break the
+    // test cache with a changing SAUCE_TUNNEL_IDENTIFIER provided through --test_env
+    return require('/tmp/angular/sauce-service/sauce-connect-params.json');
+  } catch {
+    return null;
+  }
 }
