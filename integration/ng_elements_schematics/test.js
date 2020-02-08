8a0dc34e5686c8effe0878e429cde7bc60eca36b
Alan Agius <alan.agius4@gmail.com>
2020-02-08 13:25:39 +0000

    fixup! fix(elements): schematics fails with schema.json not found error

---
+++ integration/ng_elements_schematics/test.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const shx = __importStar(require("shelljs"));
+// Install Angular packages that are built locally from HEAD.
+// This also gets around the bug whereby yarn caches local `file://` urls.
+// See https://github.com/yarnpkg/yarn/issues/2165
+const pwd = process.cwd();
+const ngPackages = [
+    'animations',
+    'common',
+    'compiler',
+    'forms',
+    'platform-browser',
+    'platform-browser-dynamic',
+    'router',
+    'elements',
+    'compiler-cli',
+    'language-service',
+].map(p => `"@angular/${p}@file:${pwd}/../../../dist/packages-dist/${p}"`);
+// Keep typescript, tslib, and @types/node versions in sync with the ones used in this repo
+const nodePackages = [
+    '@types/node',
+    'tslib',
+    'typescript',
+].map(p => `"${p}@file:${pwd}/../../../node_modules/${p}"`);
+const packages = [
+    ...ngPackages,
+    ...nodePackages,
+].join(' ');
+// Clean up previously run test
+shx.rm('-rf', 'demo');
+// Set up demo project.
+shx.exec('ng version');
+shx.exec('ng new demo --skip-git --skip-install --style=css');
+shx.cd('demo');
+// Add @angular/elements
+shx.exec('ng add "../../../dist/packages-dist/elements"');
+// Install Angular packages that are built locally from HEAD.
+shx.exec(`yarn add --ignore-scripts --silent ${packages}`);
+// Test that build is successful after adding elements.
+shx.exec('ng build --no-source-map');
--- integration/ng_elements_schematics/test.ts
+++ integration/ng_elements_schematics/test.ts
@@ -17,24 +17,33 @@ const ngPackages = [
   'elements',
   'compiler-cli',
   'language-service',
-].map(p => `"@angular/${p}@file:${pwd}/../../dist/packages-dist/${p}"`);
+].map(p => `"@angular/${p}@file:${pwd}/../../../dist/packages-dist/${p}"`);
 
 // Keep typescript, tslib, and @types/node versions in sync with the ones used in this repo
 const nodePackages = [
   '@types/node',
   'tslib',
   'typescript',
-].map(p => `"${p}@file:${pwd}/../../node_modules/${p}"`);
+].map(p => `"${p}@file:${pwd}/../../../node_modules/${p}"`);
 
 const packages = [
   ...ngPackages,
   ...nodePackages,
 ].join(' ');
 
-shx.exec('ng version');
+// Clean up previously run test
 shx.rm('-rf', 'demo');
+
+// Set up demo project.
+shx.exec('ng version');
 shx.exec('ng new demo --skip-git --skip-install --style=css');
 shx.cd('demo');
-shx.exec('ng add @angular/elements');
+
+// Add @angular/elements
+shx.exec('ng add "../../../dist/packages-dist/elements"');
+
+// Install Angular packages that are built locally from HEAD.
 shx.exec(`yarn add --ignore-scripts --silent ${packages}`);
-shx.exec('ng build');
+
+// Test that build is successful after adding elements.
+shx.exec('ng build --no-source-map');
