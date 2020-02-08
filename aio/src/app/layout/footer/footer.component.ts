60a2fae9c9d2f0ae0bc708ce6deecc3da8bc4c19
ajitsinghkaler <ajitsinghkaler0@gmail.com>
2020-02-08 16:55:09 +0530

    feat(docs-infra): feature to download docs for offline viewing
    added feature to download docs for offline viewing featching all files tat need to be downloaded from ngsw.json files and downloading them all using fetch which leads to cacheing via service worker
    
    Fixes #27492

--- aio/src/app/layout/footer/footer.component.ts
+++ aio/src/app/layout/footer/footer.component.ts
@@ -9,4 +9,25 @@ import { NavigationNode, VersionInfo } from 'app/navigation/navigation.service';
 export class FooterComponent {
   @Input() nodes: NavigationNode[];
   @Input() versionInfo: VersionInfo;
+
+  async downloadDocs(){
+    try {
+      const manifest = await fetch('ngsw.json').then(res => res.json());
+      const lazyUrls = manifest.assetGroups.find((g:any) => g.name === 'docs-lazy').urls;
+      const confirmed = confirm(`Download all ${lazyUrls.length} URLs?`);
+
+      if (confirmed) {
+        const start = performance.now();
+        const responses = await Promise.all(lazyUrls.map((u:any) => fetch(u)));
+        const failed = responses.filter((r:any) => !r.ok);
+        const failedCount = failed.length;
+        const duration = Math.round((performance.now() - start) / 1000);
+        failed.forEach((failure:any)=> console.log(`Failed to fetch ${failure.url}.`))
+        alert(`Downloaded ${lazyUrls.length} URLs in ${duration}s. (${failedCount} failed.)`);
+      }
+    } catch (err) {
+      console.error(err);
+      alert(`An error occurred:\n\n${err.stack}`);
+    }
+  }
 }
