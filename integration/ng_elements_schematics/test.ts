862370f8d6b0c2209231e7315bdae2901b10cacd
Alan Agius <alan.agius4@gmail.com>
2020-02-10 12:43:29 +0100

    fixup! fix(elements): schematics fails with schema.json not found error

--- integration/ng_elements_schematics/test.ts
+++ integration/ng_elements_schematics/test.ts
@@ -1,30 +1,32 @@
 #!/usr/bin/env node
 
-import * as shx from 'shelljs';
+import {cd, exec, set, rm} from 'shelljs';
+
+// Fail on first error
+set('-e');
 
 // Install Angular packages that are built locally from HEAD.
 // This also gets around the bug whereby yarn caches local `file://` urls.
 // See https://github.com/yarnpkg/yarn/issues/2165
-const pwd = process.cwd();
 const ngPackages = [
   'animations',
+  'core',
   'common',
   'compiler',
   'forms',
   'platform-browser',
   'platform-browser-dynamic',
   'router',
-  'elements',
   'compiler-cli',
   'language-service',
-].map(p => `"@angular/${p}@file:${pwd}/../../dist/packages-dist/${p}"`);
+].map(p => `"@angular/${p}@file:${__dirname}/../../dist/packages-dist/${p}"`);
 
 // Keep typescript, tslib, and @types/node versions in sync with the ones used in this repo
 const nodePackages = [
   '@types/node',
   'tslib',
   'typescript',
-].map(p => `"${p}@file:${pwd}/../../node_modules/${p}"`);
+].map(p => `"${p}@file:${__dirname}/../../node_modules/${p}"`);
 
 const packages = [
   ...ngPackages,
@@ -32,18 +34,18 @@ const packages = [
 ].join(' ');
 
 // Clean up previously run test
-shx.rm('-rf', 'demo');
+rm('-rf', 'demo');
 
-// Set up demo project.
-shx.exec('ng version');
-shx.exec('ng new demo --skip-git --skip-install --style=css');
-shx.cd('demo');
+// Set up demo project
+exec('ng version');
+exec('ng new demo --skip-git --skip-install --style=css --no-interactive');
+cd('demo');
+
+// Install Angular packages that are built locally from HEAD
+exec(`yarn add --ignore-scripts --silent ${packages}`);
 
 // Add @angular/elements
-shx.exec('ng add "../../../dist/packages-dist/elements"');
+exec(`ng add "${__dirname}/../../dist/packages-dist/elements"`);
 
-// Install Angular packages that are built locally from HEAD.
-shx.exec(`yarn add --ignore-scripts --silent ${packages}`);
-
-// Test that build is successful after adding elements.
-shx.exec('ng build --no-source-map');
+// Test that build is successful after adding elements
+exec('ng build --no-source-map');
