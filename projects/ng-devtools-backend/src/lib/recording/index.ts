6cec6cf0cf3829ba981af168bb0fda99c7b48880
AleksanderBodurri <alex.bodurri@mail.utoronto.ca>
2020-02-11 17:44:09 -0500

    refactor(devtools): use factory pattern to clean up ComponentTreeObserver callbacks in recording/index.ts (rangle/angular-devtools#54)

--- projects/ng-devtools-backend/src/lib/recording/index.ts
+++ projects/ng-devtools-backend/src/lib/recording/index.ts
@@ -1,21 +1,12 @@
 import { ComponentTreeObserver, RecorderComponent } from './observer';
-import { getComponentName } from '../highlighter';
 import { AppRecord, ComponentEventType, LifeCycleEventType } from 'protocol';
-import { serializeComponentState } from '../state-serializer';
 import { runOutsideAngular } from '../utils';
+import { createRecord } from './record-factory';
 
 let records: AppRecord[] = [];
 let observer: ComponentTreeObserver;
 let inProgress = false;
 let inChangeDetection = false;
-
-const getChangeDetectionSource = () => {
-  const zone = (window as any).Zone;
-  if (!zone || !zone.currentTask) {
-    return '';
-  }
-  return zone.currentTask.source;
-};
 
 export const start = (): void => {
   if (inProgress) {
@@ -24,56 +15,25 @@ export const start = (): void => {
   inProgress = true;
   observer = new ComponentTreeObserver(
     (component: RecorderComponent) => {
-      records.push({
-        recordType: 'component',
-        timestamp: Date.now(),
-        component: getComponentName(component.component),
-        state: { props: serializeComponentState(component.component, 2) },
-        id: [...component.id],
-        event: ComponentEventType.Create,
-        duration: 0,
-      });
+      records.push(createRecord({ recorderComponent: component, eventType: ComponentEventType.Create }));
     },
     (component: RecorderComponent, duration: number) => {
       if (!inChangeDetection) {
         inChangeDetection = true;
-        records.push({
-          timestamp: Date.now(),
-          recordType: 'lifecycle',
-          event: LifeCycleEventType.ChangeDetectionStart,
-          source: getChangeDetectionSource(),
-        });
+        records.push(createRecord({ eventType: LifeCycleEventType.ChangeDetectionStart }));
         runOutsideAngular(() => {
           setTimeout(() => {
             inChangeDetection = false;
-            records.push({
-              timestamp: Date.now(),
-              recordType: 'lifecycle',
-              event: LifeCycleEventType.ChangeDetectionEnd,
-            });
+            records.push(createRecord({ eventType: LifeCycleEventType.ChangeDetectionEnd }));
           });
         });
       }
-      records.push({
-        recordType: 'component',
-        timestamp: Date.now(),
-        component: getComponentName(component.component),
-        id: [...component.id],
-        event: ComponentEventType.ChangeDetection,
-        state: { props: serializeComponentState(component.component, 1) },
-        duration,
-      });
+      records.push(
+        createRecord({ recorderComponent: component, eventType: ComponentEventType.ChangeDetection, duration })
+      );
     },
     (component: RecorderComponent) => {
-      records.push({
-        recordType: 'component',
-        timestamp: Date.now(),
-        component: getComponentName(component.component),
-        state: { props: serializeComponentState(component.component, 2) },
-        id: [...component.id],
-        event: ComponentEventType.Destroy,
-        duration: 0,
-      });
+      records.push(createRecord({ recorderComponent: component, eventType: ComponentEventType.Destroy }));
     }
   );
   observer.initialize();
---
+++ projects/ng-devtools-backend/src/lib/recording/record-factory.ts
@@ -0,0 +1,84 @@
+import { RecorderComponent } from './observer';
+import { AppRecord, ComponentEventType, LifeCycleEventType } from 'protocol';
+import { getComponentName } from '../highlighter';
+import { serializeComponentState } from '../state-serializer';
+
+export interface RecordFactoryOptions {
+  eventType: ComponentEventType | LifeCycleEventType;
+  duration?: number;
+  recorderComponent?: RecorderComponent;
+}
+
+export const createRecord = (options: RecordFactoryOptions): AppRecord => {
+  let record = {
+    timestamp: Date.now(),
+    event: options.eventType,
+  };
+  if (options.recorderComponent) {
+    record = {
+      ...record,
+      ...createComponentEventTypeRecord(
+        options.recorderComponent,
+        options.duration,
+        options.eventType as ComponentEventType
+      ),
+    };
+  } else {
+    record = {
+      ...record,
+      ...createLifeCycleEventTypeRecord(options.eventType as LifeCycleEventType),
+    };
+  }
+  return record as AppRecord;
+};
+
+const createComponentEventTypeRecord = (
+  recorderComponent: RecorderComponent,
+  duration: number,
+  eventType: ComponentEventType
+) => {
+  const baseComponentEventTypeRecord = {
+    recordType: 'component',
+    component: getComponentName(recorderComponent.component),
+    id: [...recorderComponent.id],
+    state: { props: serializeComponentState(recorderComponent.component, 1) },
+    duration: 0,
+  };
+
+  switch (eventType) {
+    case ComponentEventType.ChangeDetection:
+      return {
+        ...baseComponentEventTypeRecord,
+        duration,
+      };
+    case ComponentEventType.Destroy:
+    case ComponentEventType.Create:
+      return {
+        ...baseComponentEventTypeRecord,
+      };
+  }
+};
+
+const createLifeCycleEventTypeRecord = (eventType: LifeCycleEventType) => {
+  const baseLifeCycleEventTypeRecord = {
+    recordType: 'lifecycle',
+  };
+
+  switch (eventType) {
+    case LifeCycleEventType.ChangeDetectionStart:
+      return {
+        source: getChangeDetectionSource(),
+        ...baseLifeCycleEventTypeRecord,
+      };
+    case LifeCycleEventType.ChangeDetectionEnd:
+      return { ...baseLifeCycleEventTypeRecord };
+  }
+};
+
+const getChangeDetectionSource = () => {
+  const zone = (window as any).Zone;
+  if (!zone || !zone.currentTask) {
+    return '';
+  }
+  return zone.currentTask.source;
+};
