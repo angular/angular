78211c42ea24937cfccd2741d794d5b2f9db090c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-02-11 18:03:13 +0100

    test: add integration test for undecorated-classes-with-decorated-fields migration (#35339)
    We don't have an integration test for the `undecorated-classes-with-decorated-fields
    migration. For consistency and to cover for the latest changes, we add
    it to the `ng update` integration test.
    
    PR Close #35339

---
+++ integration/ng_update_migrations/src/app/migration-tests/undecorated-classes-with-fields.ts
@@ -0,0 +1,47 @@
+import {Component, ElementRef, HostBinding, HostListener, Input, NgModule} from '@angular/core';
+
+export class NonAngularBaseClass {
+  greet() {}
+}
+
+export class BaseClass extends NonAngularBaseClass {
+  @Input() enabled = true;
+}
+
+export class SecondBaseClass extends BaseClass {
+  toggleEnabled() {
+    this.enabled = !this.enabled;
+  }
+}
+
+export class ThirdBaseClass extends SecondBaseClass {
+  @HostListener('focus') onFocus() {}
+}
+
+export class FourthBaseClass extends ThirdBaseClass {
+  focus() {
+    this.onFocus();
+  }
+}
+
+export class FifthBaseClass {
+  constructor(private _elementRef: ElementRef) {}
+  protected calculatePosition(): any {}
+}
+
+export class MyCompSuperBase {
+  @HostBinding('class.hello') hello = true;
+}
+
+export class MyCompBase extends MyCompSuperBase {}
+
+@Component({
+  selector: 'my-comp',
+  template: '',
+})
+export class MyComp extends MyCompBase {}
+
+export class WrappedMyComp extends MyComp {}
+
+@NgModule({declarations: [MyComp, WrappedMyComp]})
+export class TestModule {}
