f2998980f375c0166da0283aafc4ec552af909ee
Minko Gechev <mgechev@gmail.com>
2020-02-12 11:49:16 -0800

    feat(devtools): show frame drop in the timeline (rangle/angular-devtools#62)

---
+++ projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/profiler/recording/timeline/timeline.component.spec.ts
@@ -0,0 +1,21 @@
+import { TimelineComponent } from './timeline.component';
+
+describe('TimelineComponent', () => {
+  let comp: TimelineComponent;
+
+  beforeEach(() => {
+    comp = new TimelineComponent();
+  });
+
+  it('should calculate the framerate from passed duration', () => {
+    expect(comp.frameRate(0)).toBe(64);
+    expect(comp.frameRate(16)).toBe(64);
+    expect(comp.frameRate(17)).toBe(32);
+    expect(comp.frameRate(31)).toBe(32);
+    expect(comp.frameRate(32)).toBe(32);
+    expect(comp.frameRate(33)).toBe(16);
+    expect(comp.frameRate(48)).toBe(16);
+    expect(comp.frameRate(49)).toBe(8);
+    expect(comp.frameRate(2000)).toBe(0);
+  });
+});
