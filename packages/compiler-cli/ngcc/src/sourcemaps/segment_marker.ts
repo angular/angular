f092c9332659e8a8608bac413e2527bf9bcad1d0
Pete Bacon Darwin <pete@bacondarwin.com>
2020-02-16 21:07:30 +0100

    feat(ngcc): implement source-map flattening
    The library used by ngcc to update the source files (MagicString) is able
    to generate a source-map but it is not able to account for any previous
    source-map that the input text is already associated with.
    
    There have been various attempts to fix this but none have been very
    successful, since it is not a trivial problem to solve.
    
    This commit contains a novel approach that is able to load up a tree of
    source-files connected by source-maps and flatten them down into a single
    source-map that maps directly from the final generated file to the original
    sources referenced by the intermediate source-maps.

---
+++ packages/compiler-cli/ngcc/src/sourcemaps/segment_marker.ts
@@ -0,0 +1,86 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+
+/**
+* A marker that indicates the start of a segment in a mapping.
+*
+* The end of a segment is indicated by the the first segment-marker of another mapping whose start
+* is greater or equal to this one.
+*/
+export interface SegmentMarker {
+  readonly line: number;
+  readonly column: number;
+}
+
+/**
+ * Compare two segment-markers, for use in a search or sorting algorithm.
+ *
+ * @returns a positive number if `a` is after `b`, a negative number if `b` is after `a`
+ * and zero if they are at the same position.
+ */
+export function compareSegments(a: SegmentMarker, b: SegmentMarker): number {
+  return a.line === b.line ? a.column - b.column : a.line - b.line;
+}
+
+// The `1` is to indicate a newline character between the lines
+// Note that in the actual contents there could be more than one character -
+// e.g. \r\n - that indicate a newline but that is not important here.
+const NEWLINE_MARKER_OFFSET = 1;
+
+/**
+ * Compute the difference between two segment markers in a source file.
+ *
+ * @param lineLengths the lengths of each line of content of the source file where we are computing
+ * the difference
+ * @param a the start marker
+ * @param b the end marker
+ * @returns the number of characters between the two segments `a` and `b`
+ */
+export function segmentDiff(lineLengths: number[], a: SegmentMarker, b: SegmentMarker) {
+  let diff = b.column - a.column;
+
+  // Deal with `a` being before `b`
+  for (let lineIndex = a.line; lineIndex < b.line; lineIndex++) {
+    diff += lineLengths[lineIndex] + NEWLINE_MARKER_OFFSET;
+  }
+
+  // Deal with `a` being after `b`
+  for (let lineIndex = a.line - 1; lineIndex >= b.line; lineIndex--) {
+    // The `+ 1` is the newline character between the lines
+    diff -= lineLengths[lineIndex] + NEWLINE_MARKER_OFFSET;
+  }
+  return diff;
+}
+
+/**
+ * Return a new SegmentMarker that is offset by the given number of characters.
+ *
+ * @param lineLengths the lines of the source file whose segment marker we are offsetting
+ * @param marker The segment to offset
+ * @param offset The number of character to offset by
+ */
+export function offsetSegment(lineLengths: number[], marker: SegmentMarker, offset: number) {
+  if (offset === 0) {
+    return marker;
+  }
+
+  let line = marker.line;
+  let column = marker.column + offset;
+
+  while (line < lineLengths.length - 1 && column > lineLengths[line]) {
+    column -= lineLengths[line] + NEWLINE_MARKER_OFFSET;
+    line++;
+  }
+  while (line > 0 && column < 0) {
+    line--;
+    column += lineLengths[line] + NEWLINE_MARKER_OFFSET;
+  }
+
+  return {line, column};
+}
