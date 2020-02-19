f06aedc28c2542aef0010a8ff09dfce65aea5063
Sumit Arora <er.sumitarora@gmail.com>
2020-02-19 12:55:16 -0500

    feat(devtools): adding icon popups displaying app status (rangle/angular-devtools#70)

---
+++ projects/shell-chrome/src/app/ng-validate.ts
@@ -0,0 +1,33 @@
+window.addEventListener('message', (event: MessageEvent) => {
+  if (event.source === window && event.data) {
+    chrome.runtime.sendMessage(event.data);
+  }
+});
+
+function detectAngular(win: Window) {
+  const isDebugMode = Boolean((win as any).ng);
+  const ngVersionElement = document.querySelector('[ng-version]');
+  const isSupportedAngularVersion = ngVersionElement
+    ? +ngVersionElement.getAttribute('ng-version').split('.')[0] >= 9
+    : false;
+
+  win.postMessage(
+    {
+      isDebugMode,
+      isSupportedAngularVersion,
+    },
+    '*'
+  );
+}
+
+function installScript(fn: string) {
+  const source = `;(${fn})(window)`;
+  const script = document.createElement('script');
+  script.textContent = source;
+  document.documentElement.appendChild(script);
+  script.parentNode.removeChild(script);
+}
+
+if (document instanceof HTMLDocument) {
+  installScript(detectAngular.toString());
+}
