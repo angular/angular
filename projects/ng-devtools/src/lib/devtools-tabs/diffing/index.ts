b7b99b8532394bba7e27b7f5704ef4baa83afa7c
mgechev <mgechev@gmail.com>
2020-02-19 15:41:30 -0800

    feat(devtools): implement proper diffing in the explorer

--- projects/ng-devtools/src/lib/devtools-tabs/diffing/index.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/diffing/index.ts
@@ -1,37 +1,44 @@
 import { DefaultIterableDiffer } from '@angular/core';
 
+export interface MovedRecord {
+  currentIndex: number;
+  previousIndex: number;
+}
+
 export const diff = <T>(differ: DefaultIterableDiffer<T>, a: T[], b: T[]) => {
   differ.diff(a);
   differ.diff(b);
 
-  differ.forEachRemovedItem(record => {
-    a[record.previousIndex] = null;
+  // We first have to set the moved items to their correct positions.
+  // Keep in mind that the track by function may not guarantee
+  // that we haven't changed any of the items' props.
+  const movedItems: MovedRecord[] = [];
+  differ.forEachMovedItem(record => {
+    a[record.currentIndex] = a[record.previousIndex];
+    movedItems.push(record);
   });
 
+  // Now we can set the new items and remove the deleted ones.
   const newItems: T[] = [];
-  differ.forEachOperation((record, adjustedPreviousIndex, currentIndex) => {
-    if (currentIndex !== null) {
-      a[currentIndex] = record.item;
+  const alreadySet = [];
+  differ.forEachOperation(record => {
+    if (record.currentIndex !== null && record.previousIndex === null) {
+      a[record.currentIndex] = record.item;
+      alreadySet[record.currentIndex] = true;
     }
-    if (record.previousIndex === null && currentIndex) {
+    if (record.currentIndex === null && !alreadySet[record.currentIndex]) {
+      a[record.previousIndex] = null;
+    }
+    if (record.previousIndex === null && record.currentIndex) {
       newItems.push(record.item);
     }
   });
 
-  let currentIdx = 0;
-  for (const item of a) {
-    if (item !== null) {
-      a[currentIdx] = item;
-      currentIdx++;
-    }
-  }
-
   for (let i = a.length - 1; i >= 0; i--) {
-    if (a[i] !== null) {
-      break;
+    if (a[i] === null) {
+      a.splice(i, 1);
     }
-    a.splice(i, 1);
   }
 
-  return newItems;
+  return { newItems, movedItems };
 };
