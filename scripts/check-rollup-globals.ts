afb3f9a1c53b74aecad86a1862350b48cbc35d83
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-02-20 20:03:30 +0100

    build: remove a11y-demo (#18551)
    The accessibility demo pages aren't used anymore and aren't exposed anywhere. These changes get rid of all of the code.

--- scripts/check-rollup-globals.ts
+++ scripts/check-rollup-globals.ts
@@ -27,7 +27,7 @@ const configFile = ts.readJsonConfigFile(join(projectRoot, 'tsconfig.json'), ts.
 const parsedConfig = ts.parseJsonSourceFileConfigFileContent(configFile, ts.sys, projectRoot);
 const filesToCheckGlob = [
   'src/**/!(*.spec).ts',
-  '!src/+(a11y-demo|e2e-app|universal-app|dev-app)/**/*.ts',
+  '!src/+(e2e-app|universal-app|dev-app)/**/*.ts',
   '!src/**/schematics/**/*.ts',
 ];
 
