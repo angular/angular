be922946206a8362ed22596f1102b7ac57a642c4
Sonu Kapoor <sonukapoor@gmail.com>
2020-02-24 07:20:22 -0500

    fixup! feat(docs-infra): add `searchKeywords` preprocessor

--- aio/tools/transforms/angular-base-package/inline-tag-defs/custom-search-defs/index.js
+++ aio/tools/transforms/angular-base-package/inline-tag-defs/custom-search-defs/index.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'searchKeywords',
     description: 'A shorthand for creating elements with search terms. Usage: `{@searchKeywords term1 term2 termN }`',
     handler: function(doc, tagName, tagDescription) {
-      doc.searchKeywords = tagDescription.replace(/"/g, '');
+      doc.searchKeywords = tagDescription;
       return doc;
     }
   };
