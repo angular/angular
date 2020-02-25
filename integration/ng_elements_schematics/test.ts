184a8493c8c47c77663fe61222329c0d9c73d20e
Greg Magolan <gmagolan@gmail.com>
2020-02-25 11:45:33 -0800

    test: run /integration/ng_elements_schematics test with bazel (#35669)
    PR Close #35669

--- integration/ng_elements_schematics/test.ts
+++ integration/ng_elements_schematics/test.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import {cd, exec, rm, set} from 'shelljs';
+import * as fs from 'fs';
 
 // Fail on first error
 set('-e');
@@ -20,19 +21,29 @@ const ngPackages = [
   'router',
   'compiler-cli',
   'language-service',
-].map(p => `"@angular/${p}@file:${__dirname}/../../dist/packages-dist/${p}"`);
+];
 
 // Keep typescript, tslib, and @types/node versions in sync with the ones used in this repo
 const nodePackages = [
   '@types/node',
   'tslib',
   'typescript',
-].map(p => `"${p}@file:${__dirname}/../../node_modules/${p}"`);
+];
 
-const packages = [
-  ...ngPackages,
-  ...nodePackages,
-].join(' ');
+// Under Bazel integration tests are sand-boxed and cannot reference
+// reference `../../dist/*` packages and should not do so as these are not
+// inputs to the test. The npm_integeration_test rule instead provides a manifest
+// file that contains all of the npm package mappings available to the test.
+const bazelMappings: { [key: string]: string } = fs.existsSync('NPM_PACKAGE_MANIFEST.json') ? require('./NPM_PACKAGE_MANIFEST.json') : {};
+
+const packages: { [key: string]: string } = {};
+for (let p of ngPackages) {
+  const n = `@angular/${p}`;
+  packages[n] = `file:${bazelMappings[n]}` || `file:${__dirname}/../../dist/packages-dist/${p}`;
+}
+for (let p of nodePackages) {
+  packages[p] = `file:${bazelMappings[p]}` || `file:${__dirname}/../../node_modules/${p}`;
+}
 
 // Clean up previously run test
 cd(__dirname);
@@ -43,11 +54,13 @@ exec('ng version');
 exec('ng new demo --skip-git --skip-install --style=css --no-interactive');
 cd('demo');
 
-// Install Angular packages that are built locally from HEAD
-exec(`yarn add --ignore-scripts --silent ${packages}`);
+// Install Angular packages that are built locally from HEAD and npm packages
+// from root node modules that are to be kept in sync
+const packageList = Object.keys(packages).map(p => `${p}@${packages[p]}`).join(' ');
+exec(`yarn add --ignore-scripts --silent ${packageList}`);
 
 // Add @angular/elements
-exec(`ng add "${__dirname}/../../dist/packages-dist/elements"`);
+exec(bazelMappings ? `ng add "${bazelMappings['@angular/elements']}"` : `ng add "${__dirname}/../../dist/packages-dist/elements"`);
 
 // Test that build is successful after adding elements
 exec('ng build --no-source-map');
