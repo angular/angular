2025717a130c1f484db0daa6dfbd5c6981d7e0d0
AleksanderBodurri <alex.bodurri@mail.utoronto.ca>
2020-02-26 10:17:27 -0500

    fix(devtools): check if node.nativeelement is an instance of an HTMLElement before calling ng.getComponent on it (rangle/angular-devtools#93)

--- projects/ng-devtools-backend/src/lib/selected-component.ts
+++ projects/ng-devtools-backend/src/lib/selected-component.ts
@@ -7,7 +7,7 @@ const SELECTED_COMPONENT_PROPERTY_KEY_BASE = '$ng0';
 export const setConsoleReference = (node: ComponentTreeNode) => {
   Object.defineProperty(window, SELECTED_COMPONENT_PROPERTY_KEY_BASE, {
     get: () => {
-      if (node) {
+      if (node && node.nativeElement instanceof HTMLElement) {
         return ng.getComponent(node.nativeElement) || node;
       }
       return node.nativeElement;
