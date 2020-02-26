f6a497c87dd2f3f0d8ba5f291afbf76dd68be14c
David Taylor <davidt-google-cla@yadt.uk>
2020-02-26 09:00:54 +0000

    docs(core): fix ViewChild/ContentChild API docs for the `read` property
    Previously, the API docs for ViewChild and ContentChild erroneously
    implied that the `read` property was a boolean. Since `read` actually
    accepts a token, this commit updates the docs to reflect that.

--- packages/core/src/metadata/di.ts
+++ packages/core/src/metadata/di.ts
@@ -140,7 +140,7 @@ export interface ContentChildrenDecorator {
    *
    * * **selector** - The directive type or the name used for querying.
    * * **descendants** - True to include all descendants, otherwise include only direct children.
-   * * **read** - True to read a different token from the queried elements.
+   * * **read** - The token to read from the queried elements.
    *
    * @usageNotes
    *
@@ -204,7 +204,7 @@ export interface ContentChildDecorator {
    * **Metadata Properties**:
    *
    * * **selector** - The directive type or the name used for querying.
-   * * **read** - True to read a different token from the queried element.
+   * * **read** - The token to read from the queried element.
    * * **static** - True to resolve query results before change detection runs,
    * false to resolve after change detection. Defaults to false.
    *
@@ -262,7 +262,7 @@ export interface ViewChildrenDecorator {
    * **Metadata Properties**:
    *
    * * **selector** - The directive type or the name used for querying.
-   * * **read** - True to read a different token from the queried elements.
+   * * **read** - The token to read from the queried elements.
    *
    * @usageNotes
    *
@@ -315,7 +315,7 @@ export interface ViewChildDecorator {
    * **Metadata Properties**:
    *
    * * **selector** - The directive type or the name used for querying.
-   * * **read** - True to read a different token from the queried elements.
+   * * **read** - The token to read from the queried elements.
    * * **static** - True to resolve query results before change detection runs,
    * false to resolve after change detection. Defaults to false.
    *
