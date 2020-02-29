82a6fc5ef9d51e25ce64b7c9c36f2693ab233cc2
George Kalpakas <kalpakas.g@gmail.com>
2020-02-29 21:05:23 +0200

    build(docs-infra): use local version of Zone.js when testing against local packages (#35858)
    In some cases, we want to test the AIO app or docs examples against the
    locally built Angular packages (for example to ensure that the changes
    in a commit do not introduce a breaking change). In order to achieve
    this, we have the `ng-packages-installer` script that handles updating
    a project's `package.json` file to use the locally built Angular
    packages (and appropriate versions for their (dev-/peer-)dependencies).
    
    Previously, `ng-packages-installer` would only consider the locally
    built Angular packages (from `dist/packages-dist/`). However, given that
    Zone.js is now part of the `angular/angular` repo, it makes sense to
    also use the locally built Zone.js package (from `dist/zone.js-dist/`).
    Otherwise, the tests might fail for commits that update both the Angular
    packages (and related docs examples) and the Zone.js package. An example
    of such a simultaneous change (that would have broken tests) is #33838.
    
    This commit updates the script to install the locally built Zone.js
    package (in addition to the Angular ones). The commit ensures that the
    Zone.js package will always be available alongside the Angular packages
    (i.e. that the Zone.js package will be built by the same script that
    builds the Angular packages and that the `dist/zone.js-dist/` directory
    will be cached on CI).
    
    Note: This problem was discovered while enabling docs examples unit
    tests in #34374.
    
    PR Close #35858

--- scripts/build/build-ivy-npm-packages.js
+++ scripts/build/build-ivy-npm-packages.js
@@ -9,8 +9,18 @@
 
 'use strict';
 
+const {buildZoneJsPackage} = require('./zone-js-builder');
 const {buildTargetPackages} = require('./package-builder');
 
 
 // Build the ivy packages into `dist/packages-dist-ivy-aot/`.
 buildTargetPackages('dist/packages-dist-ivy-aot', true, 'Ivy AOT');
+
+// Build the `zone.js` npm package into `dist/zone.js-dist-ivy-aot/`, because it might be needed by
+// other scripts/tests.
+//
+// NOTE:
+// The `-ivy-aot` suffix is only used to differentiate from the packages built by the
+// `build-packages-dist.js` script, so that there is no conflict when persisting them to the
+// workspace on CI.
+buildZoneJsPackage('dist/zone.js-dist-ivy-aot');
