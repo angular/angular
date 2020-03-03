e0a35e13d57f5c4ba3d6c87231e0dd6e0cafd923
Alan Agius <alan.agius4@gmail.com>
2020-03-03 09:50:15 +0100

    perf(ngcc): reduce directory traversing (#35756)
    This reduces the time that `findEntryPoints` takes from 9701.143ms to 4177.278ms, by reducing the file operations done.
    
    Reference: #35717
    
    PR Close #35756

--- packages/compiler-cli/ngcc/src/entry_point_finder/directory_walker_entry_point_finder.ts
+++ packages/compiler-cli/ngcc/src/entry_point_finder/directory_walker_entry_point_finder.ts
@@ -9,8 +9,9 @@ import {AbsoluteFsPath, FileSystem, join, resolve} from '../../../src/ngtsc/file
 import {DependencyResolver, SortedEntryPointsInfo} from '../dependencies/dependency_resolver';
 import {Logger} from '../logging/logger';
 import {NgccConfiguration} from '../packages/configuration';
-import {EntryPoint, getEntryPointInfo} from '../packages/entry_point';
+import {EntryPoint, INVALID_ENTRY_POINT, NO_ENTRY_POINT, getEntryPointInfo} from '../packages/entry_point';
 import {PathMappings} from '../utils';
+import {NGCC_DIRECTORY} from '../writing/new_entry_point_file_writer';
 import {EntryPointFinder} from './interface';
 import {getBasePaths} from './utils';
 
@@ -40,10 +41,24 @@ export class DirectoryWalkerEntryPointFinder implements EntryPointFinder {
    * The function will recurse into directories that start with `@...`, e.g. `@angular/...`.
    * @param sourceDirectory An absolute path to the root directory where searching begins.
    */
-  private walkDirectoryForEntryPoints(sourceDirectory: AbsoluteFsPath): EntryPoint[] {
+  walkDirectoryForEntryPoints(sourceDirectory: AbsoluteFsPath): EntryPoint[] {
     const entryPoints = this.getEntryPointsForPackage(sourceDirectory);
+    if (entryPoints === null) {
+      return [];
+    }
+
     if (entryPoints.length > 0) {
-      // The `sourceDirectory` is an entry-point itself so no need to search its sub-directories.
+      // The `sourceDirectory` is an entry point itself so no need to search its sub-directories.
+      // Also check for any nested node_modules in this package but only if it was compiled by
+      // Angular.
+      // It is unlikely that a non Angular entry point has a dependency on an Angular library.
+      if (entryPoints.some(e => e.compiledByAngular)) {
+        const nestedNodeModulesPath = this.fs.join(sourceDirectory, 'node_modules');
+        if (this.fs.exists(nestedNodeModulesPath)) {
+          entryPoints.push(...this.walkDirectoryForEntryPoints(nestedNodeModulesPath));
+        }
+      }
+
       return entryPoints;
     }
 
@@ -52,23 +67,16 @@ export class DirectoryWalkerEntryPointFinder implements EntryPointFinder {
         // Not interested in hidden files
         .filter(p => !p.startsWith('.'))
         // Ignore node_modules
-        .filter(p => p !== 'node_modules')
+        .filter(p => p !== 'node_modules' && p !== NGCC_DIRECTORY)
         // Only interested in directories (and only those that are not symlinks)
         .filter(p => {
           const stat = this.fs.lstat(resolve(sourceDirectory, p));
           return stat.isDirectory() && !stat.isSymbolicLink();
         })
         .forEach(p => {
-          // Either the directory is a potential package or a namespace containing packages (e.g
-          // `@angular`).
+          // Package is a potential namespace containing packages (e.g `@angular`).
           const packagePath = join(sourceDirectory, p);
           entryPoints.push(...this.walkDirectoryForEntryPoints(packagePath));
-
-          // Also check for any nested node_modules in this package
-          const nestedNodeModulesPath = join(packagePath, 'node_modules');
-          if (this.fs.exists(nestedNodeModulesPath)) {
-            entryPoints.push(...this.walkDirectoryForEntryPoints(nestedNodeModulesPath));
-          }
         });
     return entryPoints;
   }
@@ -76,9 +84,9 @@ export class DirectoryWalkerEntryPointFinder implements EntryPointFinder {
   /**
    * Recurse the folder structure looking for all the entry points
    * @param packagePath The absolute path to an npm package that may contain entry points
-   * @returns An array of entry points that were discovered.
+   * @returns An array of entry points that were discovered or null when it's not a valid entrypoint
    */
-  private getEntryPointsForPackage(packagePath: AbsoluteFsPath): EntryPoint[] {
+  private getEntryPointsForPackage(packagePath: AbsoluteFsPath): EntryPoint[]|null {
     const entryPoints: EntryPoint[] = [];
 
     // Try to get an entry point from the top level package directory
@@ -86,20 +94,30 @@ export class DirectoryWalkerEntryPointFinder implements EntryPointFinder {
         getEntryPointInfo(this.fs, this.config, this.logger, packagePath, packagePath);
 
     // If there is no primary entry-point then exit
-    if (topLevelEntryPoint === null) {
+    if (topLevelEntryPoint === NO_ENTRY_POINT) {
       return [];
+    }
+
+    if (topLevelEntryPoint === INVALID_ENTRY_POINT) {
+      return null;
     }
 
     // Otherwise store it and search for secondary entry-points
     entryPoints.push(topLevelEntryPoint);
     this.walkDirectory(packagePath, packagePath, (path, isDirectory) => {
+      if (!path.endsWith('.js') && !isDirectory) {
+        return false;
+      }
+
       // If the path is a JS file then strip its extension and see if we can match an entry-point.
       const possibleEntryPointPath = isDirectory ? path : stripJsExtension(path);
       const subEntryPoint =
           getEntryPointInfo(this.fs, this.config, this.logger, packagePath, possibleEntryPointPath);
-      if (subEntryPoint !== null) {
-        entryPoints.push(subEntryPoint);
+      if (subEntryPoint === NO_ENTRY_POINT || subEntryPoint === INVALID_ENTRY_POINT) {
+        return false;
       }
+      entryPoints.push(subEntryPoint);
+      return true;
     });
 
     return entryPoints;
@@ -113,26 +131,25 @@ export class DirectoryWalkerEntryPointFinder implements EntryPointFinder {
    */
   private walkDirectory(
       packagePath: AbsoluteFsPath, dir: AbsoluteFsPath,
-      fn: (path: AbsoluteFsPath, isDirectory: boolean) => void) {
+      fn: (path: AbsoluteFsPath, isDirectory: boolean) => boolean) {
     return this.fs
         .readdir(dir)
         // Not interested in hidden files
         .filter(path => !path.startsWith('.'))
         // Ignore node_modules
-        .filter(path => path !== 'node_modules')
-        .map(path => resolve(dir, path))
+        .filter(path => path !== 'node_modules' && path !== NGCC_DIRECTORY)
         .forEach(path => {
-          const stat = this.fs.lstat(path);
+          const absolutePath = resolve(dir, path);
+          const stat = this.fs.lstat(absolutePath);
 
           if (stat.isSymbolicLink()) {
             // We are not interested in symbolic links
             return;
           }
 
-          fn(path, stat.isDirectory());
-
-          if (stat.isDirectory()) {
-            this.walkDirectory(packagePath, path, fn);
+          const containsEntryPoint = fn(absolutePath, stat.isDirectory());
+          if (containsEntryPoint) {
+            this.walkDirectory(packagePath, absolutePath, fn);
           }
         });
   }
--- packages/compiler-cli/ngcc/test/entry_point_finder/directory_walker_entry_point_finder_spec.ts
+++ packages/compiler-cli/ngcc/test/entry_point_finder/directory_walker_entry_point_finder_spec.ts
@@ -136,6 +136,71 @@ runInEachFileSystem(() => {
             ]);
       });
 
+      it('should handle try to process nested node_modules of non Angular packages', () => {
+        const basePath = _Abs('/nested_node_modules/node_modules');
+        loadTestFiles([
+          ...createPackage(basePath, 'outer', ['inner'], false),
+          ...createPackage(_Abs(`${basePath}/outer/node_modules`), 'inner', undefined, false),
+        ]);
+
+        const finder = new DirectoryWalkerEntryPointFinder(
+            fs, config, logger, resolver, _Abs('/nested_node_modules/node_modules'), undefined);
+        const spy = spyOn(finder, 'walkDirectoryForEntryPoints').and.callThrough();
+        const {entryPoints} = finder.findEntryPoints();
+        expect(spy.calls.allArgs()).toEqual([
+          [_Abs(basePath)],
+          [_Abs(`${basePath}/outer`)],
+        ]);
+
+        expect(entryPoints).toEqual([]);
+      });
+
+      it('should not try to process deeply nested folders of non TypeScript packages', () => {
+        const basePath = _Abs('/namespaced/node_modules');
+        loadTestFiles([
+          ...createNonTsPackage(_Abs(`${basePath}/@schematics`), 'angular'),
+          {
+            name: _Abs(`${basePath}/@schematics/angular/src/nested/index.js`),
+            contents: 'index',
+          },
+        ]);
+
+        const finder =
+            new DirectoryWalkerEntryPointFinder(fs, config, logger, resolver, basePath, undefined);
+        const spy = spyOn(finder, 'walkDirectoryForEntryPoints').and.callThrough();
+        const {entryPoints} = finder.findEntryPoints();
+        expect(spy.calls.allArgs()).toEqual([
+          [_Abs(basePath)],
+          [_Abs(`${basePath}/@schematics`)],
+          [_Abs(`${basePath}/@schematics/angular`)],
+        ]);
+
+        expect(entryPoints).toEqual([]);
+      });
+
+      it('should not try to process nested node_modules of non TypeScript packages', () => {
+        const basePath = _Abs('/namespaced/node_modules');
+        loadTestFiles([
+          ...createNonTsPackage(_Abs(`${basePath}/@schematics`), 'angular'),
+          ...createNonTsPackage(_Abs(`${basePath}/@schematics/angular/node_modules`), 'test'),
+          {
+            name: _Abs(`${basePath}/@schematics/angular/src/nested/index.js`),
+            contents: 'index',
+          },
+        ]);
+
+        const finder =
+            new DirectoryWalkerEntryPointFinder(fs, config, logger, resolver, basePath, undefined);
+        const spy = spyOn(finder, 'walkDirectoryForEntryPoints').and.callThrough();
+        const {entryPoints} = finder.findEntryPoints();
+        expect(spy.calls.allArgs()).toEqual([
+          [_Abs(basePath)],
+          [_Abs(`${basePath}/@schematics`)],
+          [_Abs(`${basePath}/@schematics/angular`)],
+        ]);
+
+        expect(entryPoints).toEqual([]);
+      });
 
       it('should handle dependencies via pathMappings', () => {
         const basePath = _Abs('/path_mapped/node_modules');
@@ -195,8 +260,9 @@ runInEachFileSystem(() => {
       });
 
       function createPackage(
-          basePath: AbsoluteFsPath, packageName: string, deps: string[] = []): TestFile[] {
-        return [
+          basePath: AbsoluteFsPath, packageName: string, deps: string[] = [],
+          isCompiledByAngular = true): TestFile[] {
+        const files: TestFile[] = [
           {
             name: _Abs(`${basePath}/${packageName}/package.json`),
             contents: JSON.stringify({
@@ -205,8 +271,29 @@ runInEachFileSystem(() => {
             })
           },
           {
+            name: _Abs(`${basePath}/${packageName}/fesm2015/${packageName}.js`),
+            contents: deps.map((dep, i) => `import * as i${i} from '${dep}';`).join('\n'),
+          },
+        ];
+
+        if (isCompiledByAngular) {
+          files.push({
             name: _Abs(`${basePath}/${packageName}/${packageName}.metadata.json`),
             contents: 'metadata info'
+          });
+        }
+
+        return files;
+      }
+
+      function createNonTsPackage(
+          basePath: AbsoluteFsPath, packageName: string, deps: string[] = []): TestFile[] {
+        return [
+          {
+            name: _Abs(`${basePath}/${packageName}/package.json`),
+            contents: JSON.stringify({
+              fesm2015: `./fesm2015/${packageName}.js`,
+            })
           },
           {
             name: _Abs(`${basePath}/${packageName}/fesm2015/${packageName}.js`),
