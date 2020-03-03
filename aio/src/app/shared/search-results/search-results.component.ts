ac4a480280e285c6b01bcc2d1b390c587af5a9cc
Sonu Kapoor <sonukapoor@gmail.com>
2020-03-02 21:26:03 -0500

    fix(docs-infra): add top level folders into `topLevelFolders`
    Closes #35676

--- aio/src/app/shared/search-results/search-results.component.ts
+++ aio/src/app/shared/search-results/search-results.component.ts
@@ -30,7 +30,7 @@ export class SearchResultsComponent implements OnChanges {
 
   readonly defaultArea = 'other';
   searchState: SearchState = SearchState.InProgress;
-  readonly topLevelFolders = ['guide', 'tutorial'];
+  readonly topLevelFolders = ['api', 'cli', 'guide', 'start', 'tutorial'];
   searchAreas: SearchArea[] = [];
 
   ngOnChanges() {
