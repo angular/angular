7835c5c403a404c70c8f6fd5ee4b2673661fcde3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-05 13:01:41 -0800

    refactor(compiler): split core/api.ts into multiple files
    This commit splits the ngtsc `core` package's api entrypoint, which
    previously was a single `api.ts` file, into an api/ directory with multiple
    files. This is done to isolate the parts of the API definitions pertaining
    to the public-facing `angularCompilerOptions` field in tsconfig.json into a
    single file, which will enable a public API guard test to be added in a
    future commit.

---
+++ packages/compiler-cli/src/ngtsc/core/api/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+export * from './src/interfaces';
+export * from './src/options';
+export * from './src/public_options';
