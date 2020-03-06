e1e7e7c471ebd30222896f35fcc1f73684985ed2
Hadrien Milano <hadrien.milano@appdynamics.com>
2020-03-06 14:46:08 +0100

    refactor: regenerate golden

--- tools/public_api_guard/core/core.d.ts
+++ tools/public_api_guard/core/core.d.ts
@@ -328,7 +328,7 @@ export declare class ErrorHandler {
     handleError(error: any): void;
 }
 
-export declare class EventEmitter<T extends any> extends Subject<T> {
+export declare class EventEmitter<T extends any = void> extends Subject<T> {
     constructor(isAsync?: boolean);
     emit(value: T): void;
     subscribe(generatorOrNext?: any, error?: any, complete?: any): Subscription;
