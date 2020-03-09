75739d569f8d281980c2baac509cd5c42a32c0d1
Andrew Kushnir <akushnir@google.com>
2020-03-09 13:51:21 -0700

    Revert "feat: Monkey patches MessagePort onproperties (onmessage/onmessageerror) (#34610)"
    This reverts commit 1882451ec03daeebe32f2355030faf7e2f90ae8a.
    
    Reason: breaks some g3 targets.

--- packages/zone.js/test/npm_package/npm_package.spec.ts
+++ packages/zone.js/test/npm_package/npm_package.spec.ts
@@ -123,8 +123,6 @@ describe('Zone.js npm_package', () => {
           'zone-patch-fetch.min.js',
           'zone-patch-jsonp.js',
           'zone-patch-jsonp.min.js',
-          'zone-patch-message-port.js',
-          'zone-patch-message-port.min.js',
           'zone-patch-promise-test.js',
           'zone-patch-promise-test.min.js',
           'zone-patch-resize-observer.js',
