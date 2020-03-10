f699c962bba7dffa9bbf23852a8c44691d0630a5
Andrew Scott <atscott01@gmail.com>
2020-03-10 15:46:09 -0700

    test(core): Add benchpress test for transplanted view change detection (#36001)
    Adds a performance benchmark for transplanted views (those which are declared in one component and inserted into another).
    
    PR Close #36001

---
+++ modules/benchmarks/src/change_detection/change_detection.e2e-spec.ts
@@ -0,0 +1,30 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {$} from 'protractor';
+
+import {openBrowser, verifyNoBrowserErrors} from '../../../e2e_util/e2e_util';
+
+describe('change detection benchmark', () => {
+  afterEach(verifyNoBrowserErrors);
+
+  it(`should render and update`, async() => {
+    openBrowser({
+      url: '',
+      ignoreBrowserSynchronization: true,
+      params: [{name: 'viewCount', value: 1}],
+    });
+    expect($('#root').getText()).toContain('1');
+    await $('#detectChanges').click();
+    expect($('#root').getText()).toContain('2');
+    await $('#detectChanges').click();
+    expect($('#root').getText()).toContain('3');
+    await $('#destroyDom').click();
+    expect(await $('#root').getText()).toEqual('');
+  });
+});
