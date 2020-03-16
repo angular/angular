839520cf14acf177876dc2ae3b60431b20988ada
JoostK <joost.koehoorn@gmail.com>
2020-03-16 22:48:45 +0100

    fix(common): let `KeyValuePipe` accept type unions with `null`
    `KeyValuePipe` currently accepts `null` values as well as `Map`s and a
    few others. However, due to the way in which TS overloads work, a type
    of `T|null` will not be accepted by `KeyValuePipe`'s signatures, even
    though both `T` and `null` individually would be.
    
    To make this work, each signature that accepts some type `T` has been
    duplicated with a second one below it that accepts a `T|null` and
    includes `null` in its return type.
    
    Fixes #35743

--- packages/common/src/pipes/keyvalue_pipe.ts
+++ packages/common/src/pipes/keyvalue_pipe.ts
@@ -56,11 +56,22 @@ export class KeyValuePipe implements PipeTransform {
       compareFn?: (a: KeyValue<string, V>, b: KeyValue<string, V>) => number):
       Array<KeyValue<string, V>>;
   transform<V>(
+      input: {[key: string]: V}|Map<string, V>|null,
+      compareFn?: (a: KeyValue<string, V>, b: KeyValue<string, V>) => number):
+      Array<KeyValue<string, V>>|null;
+  transform<V>(
       input: {[key: number]: V}|Map<number, V>,
       compareFn?: (a: KeyValue<number, V>, b: KeyValue<number, V>) => number):
       Array<KeyValue<number, V>>;
+  transform<V>(
+      input: {[key: number]: V}|Map<number, V>|null,
+      compareFn?: (a: KeyValue<number, V>, b: KeyValue<number, V>) => number):
+      Array<KeyValue<number, V>>|null;
   transform<K, V>(input: Map<K, V>, compareFn?: (a: KeyValue<K, V>, b: KeyValue<K, V>) => number):
       Array<KeyValue<K, V>>;
+  transform<K, V>(
+      input: Map<K, V>|null,
+      compareFn?: (a: KeyValue<K, V>, b: KeyValue<K, V>) => number): Array<KeyValue<K, V>>|null;
   transform<K, V>(
       input: null|{[key: string]: V, [key: number]: V}|Map<K, V>,
       compareFn: (a: KeyValue<K, V>, b: KeyValue<K, V>) => number = defaultComparator):
