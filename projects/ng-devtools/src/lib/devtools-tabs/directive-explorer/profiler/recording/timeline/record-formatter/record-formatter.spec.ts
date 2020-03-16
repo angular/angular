8b93cf3a259050a1e38f6ccd0cfbbe93de44f577
AleksanderBodurri <alex.bodurri@mail.utoronto.ca>
2020-03-16 14:35:43 -0400

    refactor(devtools): create mock formatter to sepearte getLabel and getValue tests from flamegraph;

---
+++ projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/profiler/recording/timeline/record-formatter/record-formatter.spec.ts
@@ -0,0 +1,124 @@
+import { FlamegraphFormatter } from './flamegraph-formatter';
+import { ElementProfile, ProfilerFrame } from 'protocol';
+import { AppEntry, RecordFormatter, TimelineView } from './record-formatter';
+
+class MockFormatter extends RecordFormatter<any> {
+  addFrame(nodes: any[], elements: ElementProfile[], prev?: any): void {
+    return;
+  }
+
+  format(records: ProfilerFrame[]): any {
+    return;
+  }
+
+  insertTimelineRecord(result: AppEntry<any>[], record: ProfilerFrame): void {}
+}
+
+const formatter = new MockFormatter();
+
+describe('getValue cases', () => {
+  let element;
+
+  it('calculates value with  no lifecycle hooks', () => {
+    element = {
+      children: [],
+      directives: [{ changeDetection: 10, isElement: false, isComponent: true, lifecycle: {}, name: 'AppComponent' }],
+    };
+    expect(formatter.getValue(element)).toBe(10);
+  });
+
+  it('calculates value with 0 change detection and existing lifecycle hooks', () => {
+    element = {
+      children: [],
+      directives: [
+        { isComponent: false, isElement: false, name: 'NgForOf', lifecycle: { ngDoCheck: 5 }, changeDetection: 0 },
+      ],
+    };
+    expect(formatter.getValue(element)).toBe(5);
+  });
+
+  it('calculates value with non 0 change detection and one lifecycle hook', () => {
+    element = {
+      children: [],
+      directives: [
+        { isComponent: false, isElement: false, name: 'NgForOf', lifecycle: { ngDoCheck: 5 }, changeDetection: 10 },
+      ],
+    };
+    expect(formatter.getValue(element)).toBe(15);
+  });
+
+  it('calculates value with non 0 change detection and multiple lifecycle hooks', () => {
+    element = {
+      children: [],
+      directives: [
+        {
+          isComponent: false,
+          isElement: false,
+          name: 'NgForOf',
+          lifecycle: { ngDoCheck: 5, ngAfterViewInit: 100 },
+          changeDetection: 10,
+        },
+      ],
+    };
+    expect(formatter.getValue(element)).toBe(115);
+  });
+});
+
+describe('getLabel cases', () => {
+  let element: ElementProfile;
+
+  it('has only components', () => {
+    element = {
+      children: [],
+      directives: [
+        {
+          changeDetection: 10,
+          isElement: false,
+          isComponent: true,
+          lifecycle: {},
+          name: 'AppComponent',
+        },
+      ],
+    };
+    expect(formatter.getLabel(element)).toBe('AppComponent');
+  });
+
+  it('has only directives', () => {
+    element = {
+      children: [],
+      directives: [
+        {
+          isComponent: false,
+          isElement: false,
+          name: 'RouterOutlet',
+          lifecycle: {},
+          changeDetection: 0,
+        },
+      ],
+    };
+    expect(formatter.getLabel(element)).toBe('[RouterOutlet]');
+  });
+
+  it('has a component and a directive', () => {
+    element = {
+      children: [],
+      directives: [
+        { isComponent: false, isElement: false, name: 'TooltipDirective', lifecycle: {}, changeDetection: 0 },
+        { changeDetection: 0, isElement: false, isComponent: true, lifecycle: {}, name: 'TodoComponent' },
+      ],
+    };
+    expect(formatter.getLabel(element)).toBe('TodoComponent[TooltipDirective]');
+  });
+
+  it('has a component and multiple directives', () => {
+    element = {
+      children: [],
+      directives: [
+        { isComponent: false, isElement: false, name: 'TooltipDirective', lifecycle: {}, changeDetection: 0 },
+        { isComponent: false, isElement: false, name: 'RandomDirective', lifecycle: {}, changeDetection: 0 },
+        { changeDetection: 0, isElement: false, isComponent: true, lifecycle: {}, name: 'TodoComponent' },
+      ],
+    };
+    expect(formatter.getLabel(element)).toBe('TodoComponent[TooltipDirective, RandomDirective]');
+  });
+});
