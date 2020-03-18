a776c85f575b28ec5d908b50b72e06df42c388ba
danranVm <danranvm@gmail.com>
2020-03-18 15:28:25 +0800

    feat(core): add `isPromise` generic
    This commit adds generic to `isPromise` function to help with type inference.

--- packages/core/src/util/lang.ts
+++ packages/core/src/util/lang.ts
@@ -11,7 +11,7 @@ import {Observable} from 'rxjs';
 /**
  * Determine if the argument is shaped like a Promise
  */
-export function isPromise(obj: any): obj is Promise<any> {
+export function isPromise<T = any>(obj: any): obj is Promise<T> {
   // allow any Promise/A+ compliant thenable.
   // It's up to the caller to ensure that obj.then conforms to the spec
   return !!obj && typeof obj.then === 'function';
