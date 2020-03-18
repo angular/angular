24912000e6cab00875d3d7d3834167fb75257d38
Sumit Arora <er.sumitarora@gmail.com>
2020-03-18 10:40:43 -0400

    feat(devtools): adding piechart for profiler

--- projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/profiler/recording/timeline/recording-visualizer/flamegraph-visualizer/flamegraph-visualizer.component.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/profiler/recording/timeline/recording-visualizer/flamegraph-visualizer/flamegraph-visualizer.component.ts
@@ -2,6 +2,11 @@ import { Component, ElementRef, Input } from '@angular/core';
 import { FlamegraphNode } from '../../record-formatter/flamegraph-formatter/flamegraph-formatter';
 import { RawData } from 'ngx-flamegraph/lib/utils';
 
+export interface GraphNode {
+  name: string;
+  value: number;
+}
+
 @Component({
   selector: 'ng-flamegraph-visualizer',
   templateUrl: './flamegraph-visualizer.component.html',
@@ -10,6 +15,13 @@ import { RawData } from 'ngx-flamegraph/lib/utils';
 export class FlamegraphVisualizerComponent {
   profilerBars: FlamegraphNode[] = [];
   selectedEntry: FlamegraphNode = null;
+
+  // graph options
+  graphData = [];
+  view: [number, number] = [235, 200];
+  colorScheme = {
+    domain: ['#E71D36', '#2EC4B6', '#FF9F1C', '#011627'],
+  };
 
   @Input() set bars(data: FlamegraphNode[]) {
     this.selectedEntry = null;
@@ -20,9 +32,35 @@ export class FlamegraphVisualizerComponent {
 
   selectFrame(frame: RawData): void {
     this.selectedEntry = frame as FlamegraphNode;
+    this.renderGraph(this.selectedEntry);
   }
 
   get availableWidth(): number {
     return this._el.nativeElement.querySelector('.level-profile-wrapper').offsetWidth;
+  }
+
+  formatPieChartData(flameGraphNode: FlamegraphNode): GraphNode[] {
+    const graphData: GraphNode[] = [];
+    flameGraphNode.original.directives.forEach(node => {
+      graphData.push({
+        name: `${node.name} changeDetection`,
+        value: +node.changeDetection.toFixed(2),
+      });
+      Object.keys(node.lifecycle).forEach(key => {
+        graphData.push({
+          name: `${node.name} ${key}`,
+          value: +node.lifecycle[key].toFixed(2),
+        });
+      });
+    });
+    return graphData;
+  }
+
+  renderGraph(node: FlamegraphNode): void {
+    this.graphData = this.formatPieChartData(node);
+  }
+
+  formatToolTip(data: any): string {
+    return `${data.data.name} ${data.data.value}ms`;
   }
 }
--- projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/profiler/recording/timeline/recording-visualizer/recording-visualizer.module.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/profiler/recording/timeline/recording-visualizer/recording-visualizer.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { TimelineVisualizerComponent } from './timeline-visualizer.component';
 import { CommonModule } from '@angular/common';
 import { NgxFlamegraphModule } from 'ngx-flamegraph';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { FlamegraphVisualizerComponent } from './flamegraph-visualizer/flamegraph-visualizer.component';
 import { WebtreegraphVisualizerComponent } from './webtreegraph-visualizer/webtreegraph-visualizer.component';
 
 @NgModule({
   declarations: [TimelineVisualizerComponent, FlamegraphVisualizerComponent, WebtreegraphVisualizerComponent],
-  imports: [CommonModule, NgxFlamegraphModule],
+  imports: [CommonModule, NgxFlamegraphModule, NgxChartsModule],
   exports: [TimelineVisualizerComponent],
 })
 export class RecordingVisualizerModule {}
