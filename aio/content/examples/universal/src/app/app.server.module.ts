0d84b077febb4b1dd20c46da71868aaf7da02ffd
George Kalpakas <kalpakas.g@gmail.com>
2020-03-19 16:33:26 +0200

    docs: remove unneeded code from `universal` example/guide
    In the past, server-side rendered apps needed to convert URLs used in
    API requests to absolute when rendering on the server. Originally, this
    was handled in the `universal` guide and corresponding example app by
    modifying the `HeroService` to use `APP_BASE_HREF` to derive the
    absolute URL.
    
    In #28956, the guide was updated to show an improved method: Specifying
    an `HttpInterceptor` that took care of converting the URLs to absolute.
    That interceptor was only provided when rendering the app on the server.
    By mistake, the corresponding example app was not updated along with the
    guide.
    
    Since `@nguniversal/*` v7.1.0, it is no longer necessary to convert the
    URLs to absolute inside the app. This is handled in the `@nguniversal`
    libs (see angular/universal#897).
    
    This commit updates the example app to emove unnecessary code and
    modifies the guide to mention the issue with absolute URLs, but explain
    that developers only need to worry about it when not using one of the
    `@nguniversal/*-engine` packages.

--- aio/content/examples/universal/src/app/app.server.module.ts
+++ aio/content/examples/universal/src/app/app.server.module.ts
@@ -1,17 +1,45 @@
+// #docplaster
+// #docregion absolute-url-interceptor
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+// #enddocregion absolute-url-interceptor
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
+// #docregion absolute-url-interceptor
+import {AbsoluteUrlInterceptor} from './absolute-url.interceptor';
+// #enddocregion absolute-url-interceptor
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
+// #docplaster ...
+// #docregion absolute-url-interceptor
 @NgModule({
+  // #enddocregion absolute-url-interceptor
   imports: [
     AppModule,
     ServerModule,
   ],
+  // #docplaster
+  // #docregion absolute-url-interceptor
   providers: [
-    // Add universal-only providers here
+    // Add server-only providers here.
+    // #docplaster
+    // #enddocregion absolute-url-interceptor
+    //
+    // NOTE:
+    // This interceptor is not needed for this example app. It is only included here as an
+    // illustration of how to include server-only providers to an app.
+    // #docregion absolute-url-interceptor
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AbsoluteUrlInterceptor,
+      multi: true,
+    },
   ],
+  // #enddocregion absolute-url-interceptor
   bootstrap: [AppComponent],
+// #docplaster ...
+// #docregion absolute-url-interceptor
 })
 export class AppServerModule {}
+// #enddocregion absolute-url-interceptor
