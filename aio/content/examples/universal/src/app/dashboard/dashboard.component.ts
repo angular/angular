dab5da977f95dffb02843ed7b18cee8574696012
George Kalpakas <kalpakas.g@gmail.com>
2020-03-19 16:33:20 +0200

    fix(docs-infra): align `universal` example with `toh-pt6`
    As mentioned in the `universal` guide, the `toh-pt6` examples is the
    starting poitn for the `universal` example. However, the two examples
    had become out-of-sync, because some fixes/changes were made to the
    Tour-of-Heroes examples.
    
    This commit ports these changes to the `universal` example.

--- aio/content/examples/universal/src/app/dashboard/dashboard.component.ts
+++ aio/content/examples/universal/src/app/dashboard/dashboard.component.ts
@@ -18,6 +18,6 @@ export class DashboardComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
