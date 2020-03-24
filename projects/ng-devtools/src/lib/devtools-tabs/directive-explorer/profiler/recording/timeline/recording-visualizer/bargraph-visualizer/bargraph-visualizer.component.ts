2b48785931895a4d1d43ada8089501a2d2104d06
Sumit Arora <er.sumitarora@gmail.com>
2020-03-24 12:32:00 -0400

    feat(devtools): adding bargraph visualizer

---
+++ projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/profiler/recording/timeline/recording-visualizer/bargraph-visualizer/bargraph-visualizer.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { BargraphNode } from '../../record-formatter/bargraph-formatter';
+
+@Component({
+  selector: 'ng-bargraph-visualizer',
+  templateUrl: './bargraph-visualizer.component.html',
+  styleUrls: ['./bargraph-visualizer.component.css'],
+})
+export class BargraphVisualizerComponent {
+  view: [number, number] = [600, 600];
+  colorScheme = {
+    domain: ['rgb(237, 213, 94)', 'rgb(240, 117, 117)', 'rgb(238, 189, 99)'],
+  };
+  profileRecords: BargraphNode[];
+
+  @Input() set records(data: BargraphNode[]) {
+    this.profileRecords = data;
+    this.view = [1000, data.length * 30];
+  }
+}
