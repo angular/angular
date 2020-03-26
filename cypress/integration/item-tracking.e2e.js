5cd739159dab96d79dda8ffb4bd86ecaf450d29f
Sumit Arora <er.sumitarora@gmail.com>
2020-03-26 18:57:01 -0400

    test(devtools): fixing item-tracking e2e tests (rangle/angular-devtools#201)

--- cypress/integration/item-tracking.e2e.js
+++ cypress/integration/item-tracking.e2e.js
@@ -4,40 +4,31 @@ describe('Tracking items from application to component tree', () => {
   });
 
   it('should have only one todo item on start', () => {
-    cy.enter('#sample-app').then(getBody => {
-      getBody()
-        .find('app-todo')
-        .contains('Buy milk');
+    cy.enter('#sample-app').then((getBody) => {
+      getBody().find('app-todo').contains('Buy milk');
     });
 
-    cy.get('mat-tree')
-      .find('mat-tree-node:contains("app-todo[TooltipDirective]")')
-      .its('length')
-      .should('eq', 2);
+    cy.get('mat-tree').find('mat-tree-node:contains("app-todo[TooltipDirective]")').its('length').should('eq', 2);
   });
 
   it('should be able to detect a new todo from user and add it to the tree', () => {
     cy.enter('#sample-app')
-      .then(getBody => {
-        getBody()
-          .find('input.new-todo')
-          .type('Buy cookies{enter}');
+      .then((getBody) => {
+        getBody().find('input.new-todo').type('Buy cookies{enter}');
       })
       .then(() => {
-        cy.enter('#sample-app').then(getBody => {
-          getBody()
-            .find('app-todo')
-            .contains('Buy milk');
+        cy.enter('#sample-app')
+          .then((getBody) => {
+            getBody().find('app-todo').contains('Buy milk');
 
-          getBody()
-            .find('app-todo')
-            .contains('Buy cookies');
-        });
-
-        cy.get('mat-tree')
-          .find('mat-tree-node:contains("app-todo[TooltipDirective]")')
-          .its('length')
-          .should('eq', 3);
+            getBody().find('app-todo').contains('Buy cookies');
+          })
+          .then(() => {
+            cy.get('mat-tree')
+              .find('mat-tree-node:contains("app-todo[TooltipDirective]")')
+              .its('length')
+              .should('eq', 3);
+          });
       });
   });
 });
