8ea30b662f975d1aff32c9e069016e54a511d60a
Joey Perrott <josephperrott@gmail.com>
2020-03-26 10:45:09 -0700

    feat(dev-infra): standard CLI commands using yargs (#36326)
    Creates a standard model for CLI commands provided by ng-dev.
    Allows for us to have any of the tools/scripts extend to be
    included in the ng-dev command, or be standalone using the same
    yargs parser.
    
    PR Close #36326

---
+++ dev-infra/commit-message/cli.ts
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import * as yargs from 'yargs';
+import {validateFile} from './validate-file';
+
+/** Build the parser for the commit-message commands. */
+export function buildCommitMessageParser(localYargs: yargs.Argv) {
+  return localYargs.help().strict().command(
+      'pre-commit-validate', 'Validate the most recent commit message', {},
+      () => { validateFile('.git/COMMIT_EDITMSG'); });
+}
+
+if (require.main == module) {
+  buildCommitMessageParser(yargs).parse();
+}
---
+++ dev-infra/commit-message/validate-file.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import {readFileSync} from 'fs';
+import {join} from 'path';
+
+import {getRepoBaseDir} from '../utils/config';
+
+import {validateCommitMessage} from './validate';
+
+/** Validate commit message at the provided file path. */
+export function validateFile(filePath: string) {
+  const commitMessage = readFileSync(join(getRepoBaseDir(), filePath), 'utf8');
+  if (validateCommitMessage(commitMessage)) {
+    console.info('√  Valid commit message');
+  }
+}
