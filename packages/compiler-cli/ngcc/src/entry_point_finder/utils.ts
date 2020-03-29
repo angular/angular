372b9101e26caf463813d688e2a62f463f8846e9
Pete Bacon Darwin <pete@bacondarwin.com>
2020-03-29 17:13:43 +0100

    refactor(ngcc): simplify `DirectoryWalkerEntryPointFinder` (#36305)
    This commit simplifies the `DirectoryWalkerEntryPointFinder` inter-method
    calling to make it easier to follow, and also to support controlling
    walking of a directory based on its children.
    
    PR Close #36305

--- packages/compiler-cli/ngcc/src/entry_point_finder/utils.ts
+++ packages/compiler-cli/ngcc/src/entry_point_finder/utils.ts
@@ -78,3 +78,19 @@ function removeContainedPaths(value: AbsoluteFsPath, index: number, array: Absol
   }
   return true;
 }
+
+/**
+ * Run a task and track how long it takes.
+ *
+ * @param task The task whose duration we are tracking
+ * @param log The function to call with the duration of the task
+ * @returns The result of calling `task`.
+ */
+export function trackDuration<T = void>(
+    task: () => T extends Promise<unknown>? never : T, log: (duration: number) => void): T {
+  const startTime = Date.now();
+  const result = task();
+  const duration = Math.round((Date.now() - startTime) / 100) / 10;
+  log(duration);
+  return result;
+}
