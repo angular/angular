860bc8054130708ff32297cb920277543b1617eb
AleksanderBodurri <alex.bodurri@mail.utoronto.ca>
2020-03-30 15:35:21 -0400

    feat(devtools): set showNode option to show most nodes in the treegraph (rangle/angular-devtools#219)

--- projects/ng-devtools/src/lib/devtools-tabs/profiler/recording/timeline/recording-visualizer/webtreegraph-visualizer/webtreegraph-visualizer.component.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/profiler/recording/timeline/recording-visualizer/webtreegraph-visualizer/webtreegraph-visualizer.component.ts
@@ -44,6 +44,7 @@ export class WebtreegraphVisualizerComponent implements AfterViewInit, OnInit {
     treemap.render(this.tree.nativeElement, this.webTreeRecords as any, {
       padding: [20, 5, 5, 5],
       caption: (node) => `${node.id}: ${node.size.toFixed(3)} ms`,
+      showNode: () => true,
     });
   }
 
