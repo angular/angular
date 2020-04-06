f9f6e2e1b3cc102cfb482f92b36850c2d03d9e6e
JoostK <joost.koehoorn@gmail.com>
2020-04-06 21:47:40 +0200

    style(compiler): reformat partial evaluator source tree (#36461)
    PR Close #36461

--- packages/compiler-cli/src/ngtsc/partial_evaluator/src/result.ts
+++ packages/compiler-cli/src/ngtsc/partial_evaluator/src/result.ts
@@ -21,8 +21,8 @@ import {DynamicValue} from './dynamic';
  * non-primitive value, or a special `DynamicValue` type which indicates the value was not
  * available statically.
  */
-export type ResolvedValue = number | boolean | string | null | undefined | Reference | EnumValue |
-    ResolvedValueArray | ResolvedValueMap | ResolvedModule | KnownFn | DynamicValue<unknown>;
+export type ResolvedValue = number|boolean|string|null|undefined|Reference|EnumValue|
+    ResolvedValueArray|ResolvedValueMap|ResolvedModule|KnownFn|DynamicValue<unknown>;
 
 /**
  * An array of `ResolvedValue`s.
@@ -54,12 +54,14 @@ export class ResolvedModule {
       return undefined;
     }
 
-    return this.evaluate(this.exports.get(name) !);
+    return this.evaluate(this.exports.get(name)!);
   }
 
   getExports(): ResolvedValueMap {
     const map = new Map<string, ResolvedValue>();
-    this.exports.forEach((decl, name) => { map.set(name, this.evaluate(decl)); });
+    this.exports.forEach((decl, name) => {
+      map.set(name, this.evaluate(decl));
+    });
     return map;
   }
 }
