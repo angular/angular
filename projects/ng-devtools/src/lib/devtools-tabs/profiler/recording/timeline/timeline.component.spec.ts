ce571826cae8d2ca5b7327c1d3abad9a339b1607
mgechev <mgechev@gmail.com>
2020-04-06 11:48:57 -0700

    feat(devtools): show timeline bars color dependant on the framerate

--- projects/ng-devtools/src/lib/devtools-tabs/profiler/recording/timeline/timeline.component.spec.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/profiler/recording/timeline/timeline.component.spec.ts
@@ -8,14 +8,14 @@ describe('TimelineComponent', () => {
   });
 
   it('should calculate the framerate from passed duration', () => {
-    expect(comp.frameRate(0)).toBe(64);
-    expect(comp.frameRate(16)).toBe(64);
-    expect(comp.frameRate(17)).toBe(32);
-    expect(comp.frameRate(31)).toBe(32);
-    expect(comp.frameRate(32)).toBe(32);
-    expect(comp.frameRate(33)).toBe(16);
-    expect(comp.frameRate(48)).toBe(16);
-    expect(comp.frameRate(49)).toBe(8);
-    expect(comp.frameRate(2000)).toBe(0);
+    expect(comp.estimateFrameRate(0)).toBe(60);
+    expect(comp.estimateFrameRate(15)).toBe(60);
+    expect(comp.estimateFrameRate(17)).toBe(30);
+    expect(comp.estimateFrameRate(31)).toBe(30);
+    expect(comp.estimateFrameRate(30)).toBe(30);
+    expect(comp.estimateFrameRate(33)).toBe(15);
+    expect(comp.estimateFrameRate(48)).toBe(15);
+    expect(comp.estimateFrameRate(49)).toBe(7);
+    expect(comp.estimateFrameRate(2000)).toBe(0);
   });
 });
