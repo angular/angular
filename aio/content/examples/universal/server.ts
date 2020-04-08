ec5c108b60f5fb12ea6fd4d71bb8099ea62cdc72
George Kalpakas <kalpakas.g@gmail.com>
2020-04-08 15:27:42 +0300

    refactor(docs-infra): update `universal` example to match latest CLI (#36483)
    Update the `universal` example (and related files) to match what would
    be generated by the latest CLI.
    
    Fixes #35289
    
    PR Close #36483

--- aio/content/examples/universal/server.ts
+++ aio/content/examples/universal/server.ts
@@ -6,24 +6,28 @@ import { join } from 'path';
 
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
+import { existsSync } from 'fs';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/express-engine-ivy/browser');
+  const distFolder = join(process.cwd(), 'dist/browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // #docregion ngExpressEngine
+  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
   server.engine('html', ngExpressEngine({
     bootstrap: AppServerModule,
   }));
   // #enddocregion ngExpressEngine
+
   server.set('view engine', 'html');
   server.set('views', distFolder);
 
   // #docregion data-request
   // TODO: implement data requests securely
-  server.get('/api/*', (req, res) => {
-    res.status(404).send('data requests are not supported');
+  server.get('/api/**', (req, res) => {
+    res.status(404).send('data requests are not yet supported');
   });
   // #enddocregion data-request
 
@@ -37,7 +41,7 @@ export function app() {
   // #docregion navigation-request
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
-    res.render('index', { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
   });
   // #enddocregion navigation-request
 
@@ -59,7 +63,8 @@ function run() {
 // The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
-if (mainModule && mainModule.filename === __filename) {
+const moduleFilename = mainModule && mainModule.filename || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
 
