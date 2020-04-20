35d61c11fd52a3f56bc7f2d97ed3f15fbc7bbc0c
Andrew Scott <atscott01@gmail.com>
2020-04-20 10:39:09 -0700

    test(core): Add benchmark for transplanted views when insertion is dirty (#36722)
    The current benchmark for transplanted views only exercises the path
    when the declaration location is dirty and the insertion is not. This
    test adds a benchmark for when both insertion and declaration are dirty.
    
    PR Close #36722

--- modules/benchmarks/src/change_detection/transplanted_views/transplanted_views.ts
+++ modules/benchmarks/src/change_detection/transplanted_views/transplanted_views.ts
@@ -6,28 +6,10 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {ChangeDetectionStrategy, Component, Input, NgModule, TemplateRef} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, NgModule, TemplateRef, ViewChild} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {newArray} from '../util';
-
-@Component({
-  selector: 'declaration-component',
-  template: `
-  <ng-template #template>{{trackTemplateRefresh()}}</ng-template>
-  <insertion-component [template]="template" [viewCount]="viewCount"></insertion-component>
-  `,
-})
-export class DeclarationComponent {
-  @Input() viewCount = 1;
-  // Tracks number of times the template was executed to ensure it was updated during CD.
-  templateRefreshCount = 0;
-
-  trackTemplateRefresh() {
-    this.templateRefreshCount++;
-    return this.templateRefreshCount;
-  }
-}
 
 @Component({
   selector: 'insertion-component',
@@ -44,9 +26,30 @@ export class InsertionComponent {
     this.views = n > 0 ? newArray<any>(n) : [];
   }
 
+  constructor(readonly changeDetector: ChangeDetectorRef) {}
+
   // use trackBy to ensure profile isn't affected by the cost to refresh ngFor.
   trackByIndex(index: number, item: any) {
     return index;
+  }
+}
+
+@Component({
+  selector: 'declaration-component',
+  template: `
+  <ng-template #template>{{trackTemplateRefresh()}}</ng-template>
+  <insertion-component [template]="template" [viewCount]="viewCount"></insertion-component>
+  `,
+})
+export class DeclarationComponent {
+  @Input() viewCount = 1;
+  @ViewChild(InsertionComponent) insertionComponent!: InsertionComponent;
+  // Tracks number of times the template was executed to ensure it was updated during CD.
+  templateRefreshCount = 0;
+
+  trackTemplateRefresh() {
+    this.templateRefreshCount++;
+    return this.templateRefreshCount;
   }
 }
 
