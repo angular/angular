d0e024ec2dcf01a54e2a3f32ffa7f3a60dda5b0f
Andrew Scott <atscott01@gmail.com>
2020-04-20 10:39:09 -0700

    test(core): Add benchmark for transplanted views when insertion is dirty (#36722)
    The current benchmark for transplanted views only exercises the path
    when the declaration location is dirty and the insertion is not. This
    test adds a benchmark for when both insertion and declaration are dirty.
    
    PR Close #36722

--- modules/benchmarks/src/change_detection/change_detection.e2e-spec.ts
+++ modules/benchmarks/src/change_detection/change_detection.e2e-spec.ts
@@ -23,8 +23,14 @@ describe('change detection benchmark', () => {
     expect(await $('#root').getText()).toEqual('');
     await $('#createDom').click();
     expect($('#root').getText()).toContain('1');
+
+    await $('#markInsertionComponentForCheck').click();
     await $('#detectChanges').click();
-    expect($('#root').getText()).toContain('2');
+    // Ivy currently refreshes at *both* declaration and insertion while VE only refreshes at
+    // insertion. Simply assert that the view was updated at least once since the first update.
+    expect(Number(await $('#root').getText())).toBeGreaterThan(1);
+
+    // The button click causes change detection to trigger at the root
     await $('#destroyDom').click();
     expect(await $('#root').getText()).toEqual('');
   });
