decacd5f74c66b7c41367ccadce0c25f5143213f
Joey Perrott <josephperrott@gmail.com>
2020-04-20 14:44:25 -0700

    feat(dev-infra): create environment stamping script in ng-dev (#36844)
    Create a common environment stamping script in the ng-dev tooling
    as to be used in common release tasks.  This is the first step in
    consolidating pieces of the release scripting process into a single
    location to develop a release tool.
    
    PR Close #36844

--- dev-infra/cli.ts
+++ dev-infra/cli.ts
@@ -11,14 +11,16 @@ import {tsCircularDependenciesBuilder} from './ts-circular-dependencies/index';
 import {buildPullapproveParser} from './pullapprove/cli';
 import {buildCommitMessageParser} from './commit-message/cli';
 import {buildFormatParser} from './format/cli';
+import {buildReleaseParser} from './release/cli';
 
 yargs.scriptName('ng-dev')
     .demandCommand()
     .recommendCommands()
-    .command('ts-circular-deps <command>', '', tsCircularDependenciesBuilder)
-    .command('pullapprove <command>', '', buildPullapproveParser)
     .command('commit-message <command>', '', buildCommitMessageParser)
     .command('format <command>', '', buildFormatParser)
+    .command('pullapprove <command>', '', buildPullapproveParser)
+    .command('release <command>', '', buildReleaseParser)
+    .command('ts-circular-deps <command>', '', tsCircularDependenciesBuilder)
     .wrap(120)
     .strict()
     .parse();
