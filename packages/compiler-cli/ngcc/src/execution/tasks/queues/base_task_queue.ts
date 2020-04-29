304816e5738388e2229c3eb5c8c0aa9691638a2c
George Kalpakas <kalpakas.g@gmail.com>
2020-04-29 21:28:09 +0300

    refactor(ngcc): rename `TaskQueue#markTaskCompleted()` to `markAsCompleted()` (#36626)
    Rename the `markTaskCompleted()` method to be consistent with the other
    similar methods of `TaskQueue` (`markAsFailed()` and
    `markAsUnprocessed()`).
    
    PR Close #36626

--- packages/compiler-cli/ngcc/src/execution/tasks/queues/base_task_queue.ts
+++ packages/compiler-cli/ngcc/src/execution/tasks/queues/base_task_queue.ts
@@ -37,7 +37,7 @@ export abstract class BaseTaskQueue implements TaskQueue {
         break;
       }
       // We are skipping this task so mark it as complete
-      this.markTaskCompleted(nextTask);
+      this.markAsCompleted(nextTask);
       const failedTask = this.tasksToSkip.get(nextTask)!;
       this.logger.warn(`Skipping processing of ${nextTask.entryPoint.name} because its dependency ${
           failedTask.entryPoint.name} failed to compile.`);
@@ -46,21 +46,21 @@ export abstract class BaseTaskQueue implements TaskQueue {
     return nextTask;
   }
 
-  markAsFailed(task: Task) {
-    if (this.dependencies.has(task)) {
-      for (const dependentTask of this.dependencies.get(task)!) {
-        this.skipDependentTasks(dependentTask, task);
-      }
-    }
-  }
-
-  markTaskCompleted(task: Task): void {
+  markAsCompleted(task: Task): void {
     if (!this.inProgressTasks.has(task)) {
       throw new Error(
           `Trying to mark task that was not in progress as completed: ${stringifyTask(task)}`);
     }
 
     this.inProgressTasks.delete(task);
+  }
+
+  markAsFailed(task: Task): void {
+    if (this.dependencies.has(task)) {
+      for (const dependentTask of this.dependencies.get(task)!) {
+        this.skipDependentTasks(dependentTask, task);
+      }
+    }
   }
 
   markAsUnprocessed(task: Task): void {
