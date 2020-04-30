40fb6fda159972922365e33652f9bfa506f5557c
mgechev <mgechev@gmail.com>
2020-04-30 15:30:47 -0700

    feat(devtools): introduce timing API support

--- projects/ng-devtools-backend/src/lib/component-tree-identifiers.ts
+++ projects/ng-devtools-backend/src/lib/component-tree-identifiers.ts
@@ -1,13 +1,46 @@
 import { DirectiveForestObserver } from './observer/observer';
+import { getDirectiveName } from './highlighter';
 
 let observer: DirectiveForestObserver;
+
+const markName = (s: string) => `🅰️ ${s}`;
+
+const supportsPerformance = globalThis.performance && typeof globalThis.performance.getEntriesByName === 'function';
+
+const recordMark = (s: string) => {
+  if (supportsPerformance) {
+    performance.mark(markName(s));
+  }
+};
+
+const endMark = (nodeName: string) => {
+  if (supportsPerformance) {
+    const name = markName(nodeName);
+    const start = `${name}_start`;
+    const end = `${name}_end`;
+    if (performance.getEntriesByName(start).length > 0) {
+      performance.mark(end);
+      performance.measure(name, start, end);
+    }
+    performance.clearMarks(start);
+    performance.clearMarks(end);
+    performance.clearMeasures(name);
+  }
+};
 
 export const observeDOM = () => {
   if (observer) {
     console.error('Cannot initialize the DOM observer more than once');
     return;
   }
-  observer = new DirectiveForestObserver({});
+  observer = new DirectiveForestObserver({
+    // onChangeDetectionStart(component: any): void {
+    //   // recordMark(`${getDirectiveName(component)}_start`);
+    // },
+    // onChangeDetectionEnd(component: any): void {
+    //   // endMark(getDirectiveName(component));
+    // },
+  });
   observer.initialize();
 };
 
