b63daeccebf60016b8e0ea70137468ba370023b6
Joey Perrott <josephperrott@gmail.com>
2020-05-05 15:37:31 -0700

    feat(dev-infra): create tool to determine conflicts created by a PR (#37051)
    Creates a tool in ng-dev to determine the PRs which become conflicted
    by merging a specified PR.  Often the question is brought up of how
    many PRs require a rebase as a result of a change.  This script allows
    to determine this impact.
    
    PR Close #37051

---
+++ dev-infra/utils/shelljs.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {exec as _exec, ShellString} from 'shelljs';
+
+/* Run an exec command as silent. */
+export function exec(cmd: string): ShellString {
+  return _exec(cmd, {silent: true});
+}
