35e0d8311ba680b446dd06d81b76c6a619d7d0c4
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-05-06 20:44:55 +0200

    docs(common): remove mention of the deprecated (#36953) web worker platform. Minor grammar/stylistic changes.
    Inline documentation for the PlatformLocation service instead mentions @angular/platform-server.
    
    Typos corrected, minor grammar and stylistic changes.
    
    PR Close #36953

--- packages/common/src/location/platform_location.ts
+++ packages/common/src/location/platform_location.ts
@@ -14,21 +14,21 @@ import {DOCUMENT} from '../dom_tokens';
  * This class should not be used directly by an application developer. Instead, use
  * {@link Location}.
  *
- * `PlatformLocation` encapsulates all calls to DOM apis, which allows the Router to be platform
- * agnostic.
+ * `PlatformLocation` encapsulates all calls to DOM APIs, which allows the Router to be
+ * platform-agnostic.
  * This means that we can have different implementation of `PlatformLocation` for the different
- * platforms that angular supports. For example, `@angular/platform-browser` provides an
- * implementation specific to the browser environment, while `@angular/platform-webworker` provides
- * one suitable for use with web workers.
+ * platforms that Angular supports. For example, `@angular/platform-browser` provides an
+ * implementation specific to the browser environment, while `@angular/platform-server` provides
+ * one suitable for use with server-side rendering.
  *
  * The `PlatformLocation` class is used directly by all implementations of {@link LocationStrategy}
- * when they need to interact with the DOM apis like pushState, popState, etc...
+ * when they need to interact with the DOM APIs like pushState, popState, etc.
  *
  * {@link LocationStrategy} in turn is used by the {@link Location} service which is used directly
  * by the {@link Router} in order to navigate between routes. Since all interactions between {@link
  * Router} /
- * {@link Location} / {@link LocationStrategy} and DOM apis flow through the `PlatformLocation`
- * class they are all platform independent.
+ * {@link Location} / {@link LocationStrategy} and DOM APIs flow through the `PlatformLocation`
+ * class, they are all platform-agnostic.
  *
  * @publicApi
  */
