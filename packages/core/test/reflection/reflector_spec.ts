23d1a899417931f5dfd0c0fd5e183e3420979967
Daniel Seibel Silva <esugoi@users.noreply.github.com>
2020-05-06 17:26:12 -0300

    fix(core): inheritance delegate ctor regex updated to work on minified code (#36962)
    If one component Parent inherit another component Base like the following:
    
    @Component(...)
    class Base {
      constructor(@Inject(InjectionToken) injector: Injector) { }
    }
    
    @Component(...)
    class Parent extends Base {
        // no constructor
    }
    
    When creating Component Parent, the dependency injection should work on delegating ctors like above.
    
    The code Parent code above will be compiled into something like:
    
    class Parent extends Base {
      constructor() {
        super(...arguments);
      }
    }
    
    The angular core isDelegateCtor function will identify the delegation ctor to the base class.
    
    But when the code above is minified (using terser), the minified code will compress the spaces, resulting in something like:
    class Parent extends Base{constructor(){super(...arguments)}}
    
    The regex will stop working, since it wasn't aware of this case. So this fix will allow this to work in minified code cases.
    
    PR Close #36962

--- packages/core/test/reflection/reflector_spec.ts
+++ packages/core/test/reflection/reflector_spec.ts
@@ -201,6 +201,18 @@ class TestObj {
         expect(isDelegateCtor(ChildWithCtor.toString())).toBe(false);
       });
 
+      it('should support ES2015 classes when minified', () => {
+        // These classes are ES2015 in minified form
+        const ChildNoCtorMinified = 'class ChildNoCtor extends Parent{}';
+        const ChildWithCtorMinified = 'class ChildWithCtor extends Parent{constructor(){super()}}';
+        const ChildNoCtorPrivatePropsMinified =
+            'class ChildNoCtorPrivateProps extends Parent{constructor(){super(...arguments);this.x=10}}';
+
+        expect(isDelegateCtor(ChildNoCtorMinified)).toBe(true);
+        expect(isDelegateCtor(ChildNoCtorPrivatePropsMinified)).toBe(true);
+        expect(isDelegateCtor(ChildWithCtorMinified)).toBe(false);
+      });
+
       it('should not throw when no prototype on type', () => {
         // Cannot test arrow function here due to the compilation
         const dummyArrowFn = function() {};
