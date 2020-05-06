dce3903f38757d1da630039a2994faf442eda7f0
Pete Bacon Darwin <pete@bacondarwin.com>
2020-05-06 21:51:08 +0100

    fix(compiler-cli): fix case-sensitivity issues in NgtscCompilerHost
    The `getCanonicalFileName()` method was not actually
    calling the  `useCaseSensitiveFileNames()` method. So
    it always returned a case-sensitive canonical filename.

--- packages/compiler-cli/src/ngtsc/file_system/test/compiler_host_spec.ts
+++ packages/compiler-cli/src/ngtsc/file_system/test/compiler_host_spec.ts
@@ -41,5 +41,27 @@ runInEachFileSystem(() => {
         expect(host.getSourceFile(directory, ts.ScriptTarget.ES2015)).toBe(undefined);
       });
     });
+
+    describe('useCaseSensitiveFileNames()', () => {
+      it('should return the same as `FileSystem.isCaseSensitive()', () => {
+        const directory = absoluteFrom('/a/b/c');
+        const fs = getFileSystem();
+        fs.ensureDir(directory);
+        const host = new NgtscCompilerHost(fs);
+        expect(host.useCaseSensitiveFileNames()).toEqual(fs.isCaseSensitive());
+      });
+    });
+
+    describe('getCanonicalFileName()', () => {
+      it('should return the original filename if FS is case-sensitive or lower case otherwise',
+         () => {
+           const directory = absoluteFrom('/a/b/c');
+           const fs = getFileSystem();
+           fs.ensureDir(directory);
+           const host = new NgtscCompilerHost(fs);
+           expect(host.getCanonicalFileName(('AbCd.ts')))
+               .toEqual(fs.isCaseSensitive() ? 'AbCd.ts' : 'abcd.ts');
+         });
+    });
   });
 });
