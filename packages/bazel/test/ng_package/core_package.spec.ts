9dbb30f884dfedc758df1efeaf79c826c51fd1c7
Igor Minar <igor@angular.io>
2020-05-05 21:35:44 -0700

    feat(bazel): simplify ng_package by dropping esm5 and fesm5 (#36944)
    esm5 and fesm5 are no longer needed and have been deprecated in the past.
    
    https://v9.angular.io/guide/deprecations#esm5-and-fesm5-code-formats-in-angular-npm-packages
    
    This commit modifies ng_package to no longer distribute these two formats in npm packages
    built by ng_package (e.g. @angular/core).
    
    This commit intentionally doesn't fully clean up the ng_package rule to remove all traces of esm5 and fems5
    build artifacts as that is a bigger cleanup and currently we are narrowing down the scope of this change
    to the MVP needed for v10, which in this case is 'do not put esm5 and fesm5' into the npm packages.
    
    More cleanup to follow: https://angular-team.atlassian.net/browse/FW-2143
    
    BREAKING CHANGE: esm5 and fesm5 format is no longer distributed in
    Angular's npm packages e.g. @angular/core
    
    If you are not using Angular CLI to build your application or library,
    and you need to be able to build es5 artifacts, then you will need to
    downlevel the distributed Angular code to es5 on your own.
    
    Angular CLI will automatically downlevel the code to es5 if differential
    loading is enabled in the Angular project, so no action is required from
    Angular CLI users.
    
    PR Close #36944

--- packages/bazel/test/ng_package/core_package.spec.ts
+++ packages/bazel/test/ng_package/core_package.spec.ts
@@ -51,9 +51,8 @@ describe('@angular/core ng_package', () => {
 
       it('should contain module resolution mappings', () => {
         expect(shx.grep('"main":', packageJson)).toContain(`./bundles/core.umd.js`);
-        expect(shx.grep('"module":', packageJson)).toContain(`./fesm5/core.js`);
+        expect(shx.grep('"module":', packageJson)).toContain(`./fesm2015/core.js`);
         expect(shx.grep('"es2015":', packageJson)).toContain(`./fesm2015/core.js`);
-        expect(shx.grep('"esm5":', packageJson)).toContain(`./esm5/core.js`);
         expect(shx.grep('"esm2015":', packageJson)).toContain(`./esm2015/core.js`);
         expect(shx.grep('"typings":', packageJson)).toContain(`./core.d.ts`);
       });
@@ -121,46 +120,6 @@ describe('@angular/core ng_package', () => {
           });
     });
 
-    describe('fesm5', () => {
-      it('should have a fesm5 file in the /fesm5 directory', () => {
-        expect(shx.cat('fesm5/core.js')).toContain(`export {`);
-      });
-
-      it('should have a source map', () => {
-        expect(shx.cat('fesm5/core.js.map')).toContain(`{"version":3,"file":"core.js","sources":`);
-      });
-
-      it('should not be processed by tsickle', () => {
-        expect(shx.cat('fesm5/core.js')).not.toContain('@fileoverview added by tsickle');
-      });
-
-      if (ivyEnabled) {
-        it('should have decorators downleveled to static props e.g. ɵprov', () => {
-          expect(shx.cat('fesm5/core.js')).not.toContain('__decorate');
-          expect(shx.cat('fesm5/core.js')).toContain('.ɵprov = ');
-        });
-      } else {
-        it('should have decorators', () => {
-          expect(shx.cat('fesm5/core.js')).toContain('__decorate');
-        });
-
-        // See: https://github.com/angular/angular/pull/32069
-        it('should retain access to const', () => {
-          expect(shx.cat('fesm5/core.js')).toContain('!ivyEnabled');
-        });
-      }
-
-      it('should load tslib from external bundle', () => {
-        expect(shx.cat('fesm5/core.js')).not.toContain('function __extends');
-        expect(shx.cat('fesm5/core.js')).toMatch('import {.*__extends');
-      });
-
-      obsoleteInIvy('we no longer need to export private symbols')
-          .it('should have been built from the generated bundle index', () => {
-            expect(shx.cat('fesm5/core.js')).toMatch('export {.*makeParamDecorator');
-          });
-    });
-
     describe('esm2015', () => {
       it('should not contain any *.ngfactory.js files', () => {
         expect(shx.find('esm2015').filter(f => f.endsWith('.ngfactory.js'))).toEqual([]);
@@ -168,16 +127,6 @@ describe('@angular/core ng_package', () => {
 
       it('should not contain any *.ngsummary.js files', () => {
         expect(shx.find('esm2015').filter(f => f.endsWith('.ngsummary.js'))).toEqual([]);
-      });
-    });
-
-    describe('esm5', () => {
-      it('should not contain any *.ngfactory.js files', () => {
-        expect(shx.find('esm5').filter(f => f.endsWith('.ngfactory.js'))).toEqual([]);
-      });
-
-      it('should not contain any *.ngsummary.js files', () => {
-        expect(shx.find('esm5').filter(f => f.endsWith('.ngsummary.js'))).toEqual([]);
       });
     });
 
@@ -227,9 +176,8 @@ describe('@angular/core ng_package', () => {
       it('should have its module resolution mappings defined in the nested package.json', () => {
         const packageJson = p`testing/package.json`;
         expect(shx.grep('"main":', packageJson)).toContain(`../bundles/core-testing.umd.js`);
-        expect(shx.grep('"module":', packageJson)).toContain(`../fesm5/testing.js`);
+        expect(shx.grep('"module":', packageJson)).toContain(`../fesm2015/testing.js`);
         expect(shx.grep('"es2015":', packageJson)).toContain(`../fesm2015/testing.js`);
-        expect(shx.grep('"esm5":', packageJson)).toContain(`../esm5/testing/testing.js`);
         expect(shx.grep('"esm2015":', packageJson)).toContain(`../esm2015/testing/testing.js`);
         expect(shx.grep('"typings":', packageJson)).toContain(`./testing.d.ts`);
       });
@@ -277,17 +225,6 @@ describe('@angular/core ng_package', () => {
       it('should have the version info in the header', () => {
         expect(shx.cat('fesm2015/testing.js'))
             .toMatch(/@license Angular v\d+\.\d+\.\d+(?!-PLACEHOLDER)/);
-      });
-    });
-
-    describe('fesm5', () => {
-      it('should have a fesm5 file in the /fesm5 directory', () => {
-        expect(shx.cat('fesm5/testing.js')).toContain(`export {`);
-      });
-
-      it('should have a source map', () => {
-        expect(shx.cat('fesm5/testing.js.map'))
-            .toContain(`{"version":3,"file":"testing.js","sources":`);
       });
     });
 
