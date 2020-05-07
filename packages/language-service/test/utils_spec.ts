0b05052cf43b18acb5e27eaea755ab5e10acf6e1
Keen Yee Liau <kyliau@google.com>
2020-05-07 08:55:33 -0700

    refactor(language-service): move TS utils to separate file (#36984)
    This commit refactors TS-only utility functions to a separate file so
    that they could be shared with Ivy language service.
    A separate ts_library rule is created so that there is no dependency on
    `compiler` and `compiler-cli` to make the compilation fast and
    light-weight.
    The method `getPropertyAssignmentFromValue` is modified slightly to
    improve the ergonomics of the function.
    
    PR Close #36984

--- packages/language-service/test/utils_spec.ts
+++ packages/language-service/test/utils_spec.ts
@@ -9,7 +9,8 @@
 import * as ng from '@angular/compiler';
 import * as ts from 'typescript';
 
-import {getClassDeclFromDecoratorProp, getDirectiveClassLike, getPathToNodeAtPosition} from '../src/utils';
+import {getClassDeclFromDecoratorProp, getDirectiveClassLike} from '../src/ts_utils';
+import {getPathToNodeAtPosition} from '../src/utils';
 import {MockTypescriptHost} from './test_utils';
 
 describe('getDirectiveClassLike', () => {
