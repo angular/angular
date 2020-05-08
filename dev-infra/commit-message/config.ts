4f1b7a4a6a9d921837e6ab947988693edb8b0237
Joey Perrott <josephperrott@gmail.com>
2020-05-08 14:51:29 -0700

    feat(dev-infra): introduce validators for ng-dev config loading (#37049)
    Introduces infrastructure to validate configuration of the ng-dev
    command at run time.  Allowing for errors to be returned to the
    user running the command.
    
    PR Close #37049

--- dev-infra/commit-message/config.ts
+++ dev-infra/commit-message/config.ts
@@ -5,9 +5,27 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+
+import {assertNoErrors, getConfig, NgDevConfig} from '../utils/config';
+
 export interface CommitMessageConfig {
   maxLineLength: number;
   minBodyLength: number;
   types: string[];
   scopes: string[];
+}
+
+/** Retrieve and validate the config as `CommitMessageConfig`. */
+export function getCommitMessageConfig() {
+  // List of errors encountered validating the config.
+  const errors: string[] = [];
+  // The unvalidated config object.
+  const config: Partial<NgDevConfig<{commitMessage: CommitMessageConfig}>> = getConfig();
+
+  if (config.commitMessage === undefined) {
+    errors.push(`No configuration defined for "commitMessage"`);
+  }
+
+  assertNoErrors(errors);
+  return config as Required<typeof config>;
 }
