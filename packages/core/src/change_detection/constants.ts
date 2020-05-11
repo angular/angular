bb535845cd2829a49441343bcd8ed8412d327a46
Judy Bogart <jbogart@gmail.com>
2020-05-11 14:57:23 -0700

    docs: clean up api doc in core (#37053)
    Add introductions to usage examples and edit descriptions to be more complete and consistent with current API reference styles
    
    PR Close #37053

--- packages/core/src/change_detection/constants.ts
+++ packages/core/src/change_detection/constants.ts
@@ -11,6 +11,8 @@
  * The strategy that the default change detector uses to detect changes.
  * When set, takes effect the next time change detection is triggered.
  *
+ * @see {@link ChangeDetectorRef#usage-notes Change detection usage}
+ *
  * @publicApi
  */
 export enum ChangeDetectionStrategy {
