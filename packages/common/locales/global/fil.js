7de133cdc7287168c161c97abdbb0d989861fab9
Misko Hevery <misko@hevery.com>
2020-05-12 11:10:01 -0700

    Revert "fix(common): `locales/global/*.js` are not ES5 compliant (#36342)"
    This reverts commit 078b0be4dc69ef4a8cdf4eb15caedde1114950c7.
    
    The original commit was a work around for a bug in CLI. That bug was fixed in the CLI, as a result this change is no longer needed and is being reverted.

--- packages/common/locales/global/fil.js
+++ packages/common/locales/global/fil.js
@@ -13,9 +13,9 @@
   global.ng = global.ng || {};
   global.ng.common = global.ng.common || {};
   global.ng.common.locales = global.ng.common.locales || {};
-  var u = undefined;
+  const u = undefined;
   function plural(n) {
-    var i = Math.floor(Math.abs(n)), v = n.toString().replace(/^[^.]*\.?/, '').length,
+    let i = Math.floor(Math.abs(n)), v = n.toString().replace(/^[^.]*\.?/, '').length,
         f = parseInt(n.toString().replace(/^[^.]*\.?/, ''), 10) || 0;
     if (v === 0 && (i === 1 || i === 2 || i === 3) ||
         v === 0 && !(i % 10 === 4 || i % 10 === 6 || i % 10 === 9) ||
