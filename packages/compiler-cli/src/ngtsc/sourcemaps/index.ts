799b6209990097c0d7f789f7626bd3262ffc1755
Pete Bacon Darwin <pete@bacondarwin.com>
2020-05-14 21:12:35 +0100

    refactor(ngcc): move `sourcemaps` into `ngtsc` (#37114)
    The `SourceFile` and associated code is general and reusable in
    other projects (such as `@angular/localize`). Moving it to `ngtsc`
    makes it more easily shared.
    
    PR Close #37114

---
+++ packages/compiler-cli/src/ngtsc/sourcemaps/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+export {RawSourceMap} from './src/raw_source_map';
+export {Mapping, SourceFile} from './src/source_file';
+export {SourceFileLoader} from './src/source_file_loader';
