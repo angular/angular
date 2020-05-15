d0a1db6f386e92acfaa8196b66dc629c19b013cc
Sonu Kapoor <sonukapoor@gmail.com>
2020-05-15 11:17:48 -0400

    refactor(docs-infra): refactors `extractDecoratedClasses`
    This commit removes the dependency on the `lodash` module and refactors
    the `extractDecoratedClasses` method.

--- aio/tools/transforms/angular-api-package/processors/extractDecoratedClasses.js
+++ aio/tools/transforms/angular-api-package/processors/extractDecoratedClasses.js
@@ -1,5 +1,3 @@
-var _ = require('lodash');
-
 module.exports = function extractDecoratedClassesProcessor(EXPORT_DOC_TYPES) {
 
   // Add the "directive" docType into those that can be exported from a module
@@ -10,12 +8,9 @@ module.exports = function extractDecoratedClassesProcessor(EXPORT_DOC_TYPES) {
     $runBefore: ['docs-processed'],
     decoratorTypes: ['Directive', 'Component', 'Pipe', 'NgModule'],
     $process: function(docs) {
-      var decoratorTypes = this.decoratorTypes;
-
-      _.forEach(docs, function(doc) {
-
-        _.forEach(doc.decorators, function(decorator) {
-
+      const decoratorTypes = this.decoratorTypes;
+      docs.forEach(doc => {
+        (doc.decorators || []).forEach(decorator => {
           if (decoratorTypes.indexOf(decorator.name) !== -1) {
             doc.docType = decorator.name.toLowerCase();
             // Directives do not always have an argument (i.e. abstract directives).
