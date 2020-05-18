c4234577db4cd0b76d5fedae72831787710b6955
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-05-19 02:40:29 +0430

    docs(router): corrected minor typo (#37166)
    This commit corrects a typo and rephrases the first sentence of the `redirectTo`
    description to be more understandable.
    
    PR Close #37166

--- packages/router/src/config.ts
+++ packages/router/src/config.ts
@@ -420,7 +420,7 @@ export interface Route {
    */
   component?: Type<any>;
   /**
-   * A URL to which to redirect when a the path matches.
+   * A URL to redirect to when the path matches.
    * Absolute if the URL begins with a slash (/), otherwise relative to the path URL.
    * When not present, router does not redirect.
    */
