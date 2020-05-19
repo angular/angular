8adb72930b2109c42c15a0f061ba3dc81622d9ec
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-05-19 10:04:04 +0200

    build: use merge script from shared dev-infra package
    We recently moved the merge script over to the framework
    repository. This commit removes our version of the merge
    script and wires up the one from `ng-dev pr merge`.

--- .ng-dev-config.ts
+++ .ng-dev-config.ts
@@ -1,14 +1,25 @@
-module.exports = () => {
+import {MergeConfig} from '@angular/dev-infra-private/pr/merge/config';
+import {GithubConfig} from '@angular/dev-infra-private/utils/config';
+
+/**
+ * Github configuration for the ng-dev command. This repository is
+ * uses as remote for the merge script.
+ */
+const github: GithubConfig = {
+  owner: 'angular',
+  name: 'components'
+};
+
+/**
+ * Configuration for the merge tool in `ng-dev`. This sets up the labels which
+ * are respected by the merge script (e.g. the target labels).
+ */
+const merge = (): MergeConfig => {
   const {major, minor} = parseVersion(require('./package').version);
   const patchBranch = `${major}.${minor}.x`;
   const minorBranch = `${major}.x`;
 
   return {
-    projectRoot: __dirname,
-    repository: {
-      user: 'angular',
-      name: 'components',
-    },
     // By default, the merge script merges locally with `git cherry-pick` and autosquash.
     // This has the downside of pull requests showing up as `Closed` instead of `Merged`.
     // In the components repository, since we don't use fixup or squash commits, we can
@@ -42,11 +53,16 @@ module.exports = () => {
         branches: (target) => [target],
       }
     ],
-  }
+  };
+};
+
+module.exports = {
+  github,
+  merge,
 };
 
 /** Converts a version string into an object. */
-function parseVersion(version) {
+function parseVersion(version: string) {
   const [major = 0, minor = 0, patch = 0] = version.split('.').map(segment => Number(segment));
   return {major, minor, patch};
 }
--- scripts/merge-script/string-pattern.ts
+++
@@ -1,4 +0,0 @@
-/** Checks whether the specified value matches the given pattern. */
-export function matchesPattern(value: string, pattern: RegExp|string): boolean {
-  return typeof pattern === 'string' ? value === pattern : pattern.test(value);
-}
