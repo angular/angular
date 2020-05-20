0b1fe1ab25e0f5efca8f199ca3e15a5422fd2677
Joey Perrott <josephperrott@gmail.com>
2020-05-20 14:19:17 -0700

    feat(dev-infra): migrate format tool to use new logging system (#37232)
    Migrate the formatting tool in ng-dev to use new logging system rather
    than directly calling console.* to create a better experience
    for users.
    
    PR Close #37232

--- dev-infra/format/formatters/buildifier.ts
+++ dev-infra/format/formatters/buildifier.ts
@@ -9,6 +9,7 @@
 import {join} from 'path';
 
 import {getRepoBaseDir} from '../../utils/config';
+import {error} from '../../utils/console';
 
 import {Formatter} from './base-formatter';
 
@@ -35,9 +36,9 @@ export class Buildifier extends Formatter {
       callback:
           (file: string, code: number, _: string, stderr: string) => {
             if (code !== 0) {
-              console.error(`Error running buildifier on: ${file}`);
-              console.error(stderr);
-              console.error();
+              error(`Error running buildifier on: ${file}`);
+              error(stderr);
+              error();
               return true;
             }
             return false;
