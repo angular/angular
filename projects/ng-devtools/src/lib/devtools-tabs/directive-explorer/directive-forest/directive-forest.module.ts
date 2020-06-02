c29c041ec8131cbf408e9062b45cc479236df1d9
Sumit Arora <er.sumitarora@gmail.com>
2020-06-02 13:04:47 -0400

    feat(devtools): themeing breadcrums

--- projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/directive-forest/directive-forest.module.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/directive-forest/directive-forest.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreadcrumbsComponent } from '../breadcrumbs/breadcrumbs.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [BreadcrumbsComponent],
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   exports: [BreadcrumbsComponent],
 })
 export class DirectiveForestModule {}
