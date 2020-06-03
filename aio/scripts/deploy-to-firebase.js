9eab4f11a913b197a2ff818f654959373aa7e600
George Kalpakas <kalpakas.g@gmail.com>
2020-06-04 02:24:19 +0300

    refactor(docs-infra): break up `deploy-to-firebase.js` script into functions
    This commit breaks up the code in `deploy-to-firebase.js` script, that
    we use for deploying angular.io to production, to smaller functions
    (instead of a monolithic block). This makes the script easier to
    maintain and also makes testing individual operations easier.
    
    The commit also updates the `deploy-to-firebase.spec.js` spec file to
    take advantage of the standalone functions to speed up testing by
    calling the corresponding function instead of having to spawn a new
    process and run the `deploy-to-firebase.js` script with the `--dry-run`
    flag.
    
    NOTE: Before updating the tests, I verified that the updated
          `deploy-to-firebase.js` script passed the old tests.

--- aio/scripts/deploy-to-firebase.js
+++ aio/scripts/deploy-to-firebase.js
@@ -7,148 +7,188 @@ const {cd, cp, exec: _exec, set} = require('shelljs');
 set('-e');
 
 
-// Arguments and environment variables
-const args = process.argv.slice(2);
-const dryRun = args[0] === '--dry-run';
-
-const {
-  CI_AIO_MIN_PWA_SCORE,
-  CI_BRANCH,
-  CI_COMMIT,
-  CI_PULL_REQUEST,
-  CI_REPO_NAME,
-  CI_REPO_OWNER,
-  CI_SECRET_AIO_DEPLOY_FIREBASE_TOKEN,
-  CI_STABLE_BRANCH,
-} = process.env;
-
-// Do not deploy if we are running in a fork.
-if (`${CI_REPO_OWNER}/${CI_REPO_NAME}` !== 'angular/angular') {
-  console.log('Skipping deploy because this is not angular/angular.');
-  process.exit(0);
-}
-
-// Do not deploy if this is a PR. PRs are deployed in the `aio_preview` CircleCI job.
-if (CI_PULL_REQUEST !== 'false') {
-  console.log('Skipping deploy because this is a PR build.');
-  process.exit(0);
-}
-
-// Do not deploy if the current commit is not the latest on its branch.
-const latestCommit = exec(`git ls-remote origin ${CI_BRANCH}`).slice(0, 40);
-if (CI_COMMIT !== latestCommit) {
-  console.log(`Skipping deploy because ${CI_COMMIT} is not the latest commit (${latestCommit}).`);
-  process.exit(0);
-}
-
-// The deployment mode is computed based on the branch we are building.
-const currentBranchMajorVersion = computeMajorVersion(CI_BRANCH);
-const deployInfoPerTarget = {
-  next: {
-    deployEnv: 'next',
-    projectId: 'next-angular-io',
-    deployedUrl: 'https://next.angular.io/',
-  },
-  rc: {
-    deployEnv: 'rc',
-    projectId: 'rc-angular-io',
-    deployedUrl: 'https://rc.angular.io/',
-  },
-  stable: {
-    deployEnv: 'stable',
-    projectId: `v${currentBranchMajorVersion}-angular-io`,
-    deployedUrl: 'https://angular.io/',
-  },
-  archive: {
-    deployEnv: 'archive',
-    projectId: `v${currentBranchMajorVersion}-angular-io`,
-    deployedUrl: `https://v${currentBranchMajorVersion}.angular.io/`,
-  },
+// Exports
+module.exports = {
+  computeDeploymentInfo,
+  computeInputVars,
+  getLatestCommit,
 };
-let deployInfo = null;
 
-if (CI_BRANCH === 'master') {
-  deployInfo = deployInfoPerTarget.next;
-} else if (CI_BRANCH === CI_STABLE_BRANCH) {
-  deployInfo = deployInfoPerTarget.stable;
-} else {
-  const stableBranchMajorVersion = computeMajorVersion(CI_STABLE_BRANCH);
+// Run
+if (require.main === module) {
+  const inputVars = computeInputVars(process.env);
+  const deploymentInfo = computeDeploymentInfo(inputVars);
 
-  // Find the branch that has highest minor version for the given `currentBranchMajorVersion`.
-  const mostRecentMinorVersionBranch =
-    // List the branches that start with the major version.
-    exec(`git ls-remote origin refs/heads/${currentBranchMajorVersion}.*.x`).split('\n')
-        // Extract the version number.
-        .map(line => line.split('/')[2])
-        // Sort by the minor version.
-        .sort((a, b) => a.split('.')[1] - b.split('.')[1])
-        // Get the highest version.
-        .pop();
-
-  // Do not deploy if it is not the latest branch for the given major version.
-  // NOTE: At this point, we know the current branch is not the stable branch.
-  if (CI_BRANCH !== mostRecentMinorVersionBranch) {
-    console.log(
-        `Skipping deploy of branch "${CI_BRANCH}" to Firebase.\n` +
-        'There is a more recent branch with the same major version: ' +
-        `"${mostRecentMinorVersionBranch}"`);
-    process.exit(0);
+  if (deploymentInfo.skipped) {
+    console.log(deploymentInfo.reason);
+  } else {
+    const dryRun = process.argv[2] === '--dry-run';
+    deploy({...inputVars, ...deploymentInfo}, dryRun);
   }
-
-  deployInfo = (currentBranchMajorVersion < stableBranchMajorVersion) ?
-      // This is the latest minor version for a major that is less than the stable major version:
-      // Deploy as `archive`.
-      deployInfoPerTarget.archive :
-      // This is the latest minor version for a major that is equal or greater than the stable major
-      // version, but not the stable version itself:
-      // Deploy as `rc`.
-      deployInfoPerTarget.rc;
 }
-
-const {deployEnv, projectId, deployedUrl} = deployInfo;
-console.log(
-    `Git branch        : ${CI_BRANCH}\n` +
-    `Build/deploy mode : ${deployEnv}\n` +
-    `Firebase project  : ${projectId}\n` +
-    `Deployment URL    : ${deployedUrl}\n`);
-
-if (dryRun) {
-  process.exit(0);
-}
-
-// Deploy
-cd(`${__dirname}/..`);
-
-// Build the app.
-yarn(`build --configuration=${deployEnv} --progress=false`);
-
-// Include any mode-specific files.
-cp('-rf', `src/extra-files/${deployEnv}/.`, 'dist/');
-
-// Set `deployedUrl` as parameter in the opensearch description.
-// `deployedUrl` must end with `/`.
-yarn(`set-opensearch-url ${deployedUrl}`);
-
-// Check payload size.
-yarn('payload-size');
-
-// Deploy to Firebase.
-yarn(`firebase use "${projectId}" --token "${CI_SECRET_AIO_DEPLOY_FIREBASE_TOKEN}"`);
-yarn(
-    `firebase deploy --message "Commit: ${CI_COMMIT}" --non-interactive --token ` +
-    CI_SECRET_AIO_DEPLOY_FIREBASE_TOKEN);
-
-// Run PWA-score tests.
-yarn(`test-pwa-score "${deployedUrl}" "${CI_AIO_MIN_PWA_SCORE}"`);
-
 
 // Helpers
+function computeDeploymentInfo(
+    {currentBranch, currentCommit, isPullRequest, repoName, repoOwner, stableBranch}) {
+  // Do not deploy if we are running in a fork.
+  if (`${repoOwner}/${repoName}` !== 'angular/angular') {
+    return skipDeployment('Skipping deploy because this is not angular/angular.');
+  }
+
+  // Do not deploy if this is a PR. PRs are deployed in the `aio_preview` CircleCI job.
+  if (isPullRequest) {
+    return skipDeployment('Skipping deploy because this is a PR build.');
+  }
+
+  // Do not deploy if the current commit is not the latest on its branch.
+  const latestCommit = getLatestCommit(currentBranch);
+  if (currentCommit !== latestCommit) {
+    return skipDeployment(
+        `Skipping deploy because ${currentCommit} is not the latest commit (${latestCommit}).`);
+  }
+
+  // The deployment mode is computed based on the branch we are building.
+  const currentBranchMajorVersion = computeMajorVersion(currentBranch);
+  const deploymentInfoPerTarget = {
+    next: {
+      deployEnv: 'next',
+      projectId: 'next-angular-io',
+      deployedUrl: 'https://next.angular.io/',
+    },
+    rc: {
+      deployEnv: 'rc',
+      projectId: 'rc-angular-io',
+      deployedUrl: 'https://rc.angular.io/',
+    },
+    stable: {
+      deployEnv: 'stable',
+      projectId: `v${currentBranchMajorVersion}-angular-io`,
+      deployedUrl: 'https://angular.io/',
+    },
+    archive: {
+      deployEnv: 'archive',
+      projectId: `v${currentBranchMajorVersion}-angular-io`,
+      deployedUrl: `https://v${currentBranchMajorVersion}.angular.io/`,
+    },
+  };
+
+  if (currentBranch === 'master') {
+    return deploymentInfoPerTarget.next;
+  } else if (currentBranch === stableBranch) {
+    return deploymentInfoPerTarget.stable;
+  } else {
+    const stableBranchMajorVersion = computeMajorVersion(stableBranch);
+
+    // Find the branch that has highest minor version for the given `currentBranchMajorVersion`.
+    const mostRecentMinorVersionBranch =
+      // List the branches that start with the major version.
+      getRemoteRefs(`refs/heads/${currentBranchMajorVersion}.*.x`)
+          // Extract the version number.
+          .map(line => line.split('/')[2])
+          // Sort by the minor version.
+          .sort((a, b) => a.split('.')[1] - b.split('.')[1])
+          // Get the highest version.
+          .pop();
+
+    // Do not deploy if it is not the latest branch for the given major version.
+    // NOTE: At this point, we know the current branch is not the stable branch.
+    if (currentBranch !== mostRecentMinorVersionBranch) {
+      return skipDeployment(
+          `Skipping deploy of branch "${currentBranch}" to Firebase.\n` +
+          'There is a more recent branch with the same major version: ' +
+          `"${mostRecentMinorVersionBranch}"`);
+    }
+
+    return (currentBranchMajorVersion < stableBranchMajorVersion) ?
+        // This is the latest minor version for a major that is less than the stable major version:
+        // Deploy as `archive`.
+        deploymentInfoPerTarget.archive :
+        // This is the latest minor version for a major that is equal or greater than the stable major
+        // version, but not the stable version itself:
+        // Deploy as `rc`.
+        deploymentInfoPerTarget.rc;
+  }
+
+  // We should never get here.
+  throw new Error('Failed to determine deployment info.');
+}
+
 function computeMajorVersion(branchName) {
   return +branchName.split('.', 1)[0];
 }
 
+function computeInputVars({
+  CI_AIO_MIN_PWA_SCORE: minPwaScore,
+  CI_BRANCH: currentBranch,
+  CI_COMMIT: currentCommit,
+  CI_PULL_REQUEST,
+  CI_REPO_NAME: repoName,
+  CI_REPO_OWNER: repoOwner,
+  CI_SECRET_AIO_DEPLOY_FIREBASE_TOKEN: firebaseToken,
+  CI_STABLE_BRANCH: stableBranch,
+}) {
+  return {
+    currentBranch,
+    currentCommit,
+    firebaseToken,
+    isPullRequest: CI_PULL_REQUEST !== 'false',
+    minPwaScore,
+    repoName,
+    repoOwner,
+    stableBranch,
+  };
+}
+
+function deploy(
+    {currentBranch, currentCommit, deployedUrl, deployEnv, firebaseToken, minPwaScore, projectId},
+    dryRun) {
+  console.log(
+      `Git branch        : ${currentBranch}\n` +
+      `Build/deploy mode : ${deployEnv}\n` +
+      `Firebase project  : ${projectId}\n` +
+      `Deployment URL    : ${deployedUrl}\n`);
+
+  if (dryRun) return;
+
+  cd(`${__dirname}/..`);
+
+  // Build the app.
+  yarn(`build --configuration=${deployEnv} --progress=false`);
+
+  // Include any mode-specific files.
+  cp('-rf', `src/extra-files/${deployEnv}/.`, 'dist/');
+
+  // Set `deployedUrl` as parameter in the opensearch description.
+  // `deployedUrl` must end with `/`.
+  yarn(`set-opensearch-url ${deployedUrl}`);
+
+  // Check payload size.
+  yarn('payload-size');
+
+  // Deploy to Firebase.
+  yarn(`firebase use "${projectId}" --token "${firebaseToken}"`);
+  yarn(
+      `firebase deploy --message "Commit: ${currentCommit}" --non-interactive --token ` +
+      `"${firebaseToken}"`);
+
+  // Run PWA-score tests.
+  yarn(`test-pwa-score "${deployedUrl}" "${minPwaScore}"`);
+}
+
 function exec(cmd, opts) {
   return _exec(cmd, {silent: true, ...opts}).trim();
+}
+
+function getRemoteRefs(refOrPattern, remote = 'origin') {
+  return exec(`git ls-remote ${remote} ${refOrPattern}`).split('\n');
+}
+
+function getLatestCommit(branchName, remote = 'origin') {
+  return getRemoteRefs(branchName, remote)[0].slice(0, 40);
+}
+
+function skipDeployment(reason) {
+  return {reason, skipped: true};
 }
 
 function yarn(cmd) {
