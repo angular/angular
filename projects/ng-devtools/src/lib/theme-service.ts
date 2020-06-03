f7ade259fe6814d11912972571f37665fa17dba1
Sumit Arora <er.sumitarora@gmail.com>
2020-06-03 15:19:38 -0400

    fix(devtools): fixing code review comments

--- projects/ng-devtools/src/lib/theme-service.ts
+++ projects/ng-devtools/src/lib/theme-service.ts
@@ -1,12 +1,14 @@
 import { Subject } from 'rxjs';
 import { Injectable, RendererFactory2, Renderer2 } from '@angular/core';
 
+export type Themes = 'dark' | 'light';
+
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeService {
   private renderer: Renderer2;
-  currentTheme: Subject<string> = new Subject();
+  currentTheme: Subject<Themes> = new Subject();
 
   constructor(private _rendererFactory: RendererFactory2) {
     this.renderer = this._rendererFactory.createRenderer(null, null);
@@ -14,8 +16,8 @@ export class ThemeService {
   }
 
   toggleDarkMode(isDark: boolean) {
-    const removeClass = isDark ? 'color-scheme-light' : 'color-scheme-dark';
-    const addClass = !isDark ? 'color-scheme-light' : 'color-scheme-dark';
+    const removeClass = isDark ? 'light' : 'dark';
+    const addClass = !isDark ? 'light' : 'dark';
     this.renderer.removeClass(document.body, removeClass);
     this.renderer.addClass(document.body, addClass);
     this.currentTheme.next(addClass);
