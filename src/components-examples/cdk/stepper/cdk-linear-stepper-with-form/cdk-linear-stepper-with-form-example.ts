84bfa7cdf061bb7fae6be25ffd745b6baca7b362
nielsr98 <54564567+nielsr98@users.noreply.github.com>
2020-06-04 22:57:19 -0400

    docs(cdk/stepper): add linear stepper example  (#19518)

---
+++ src/components-examples/cdk/stepper/cdk-linear-stepper-with-form/cdk-linear-stepper-with-form-example.ts
@@ -0,0 +1,41 @@
+import {Component} from '@angular/core';
+import {CdkStepper} from '@angular/cdk/stepper';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+/** @title A custom CDK linear stepper with forms */
+@Component({
+  selector: 'cdk-linear-stepper-with-form',
+  templateUrl: './cdk-linear-stepper-with-form-example.html',
+  styleUrls: ['./cdk-linear-stepper-with-form-example.css']
+})
+export class CdkLinearStepperWithFormExample {
+  isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private readonly _formBuilder: FormBuilder) {
+      this.firstFormGroup = this._formBuilder.group({
+          firstControl: ['', Validators.required]
+      });
+      this.secondFormGroup = this._formBuilder.group({
+          secondControl: ['', Validators.required]
+      });
+  }
+
+  toggleLinearity() {
+      this.isLinear = !this.isLinear;
+  }
+}
+
+/** Custom CDK linear stepper component */
+@Component({
+  selector: 'example-custom-linear-stepper',
+  templateUrl: './example-custom-linear-stepper.html',
+  styleUrls: ['./example-custom-linear-stepper.css'],
+  providers: [{provide: CdkStepper, useExisting: CustomLinearStepper}]
+})
+export class CustomLinearStepper extends CdkStepper {
+  selectStepByIndex(index: number): void {
+    this.selectedIndex = index;
+  }
+}
--- src/components-examples/cdk/stepper/index.ts
+++ src/components-examples/cdk/stepper/index.ts
@@ -5,22 +5,32 @@ import {
   CdkCustomStepperWithoutFormExample,
   CustomStepper
 } from './cdk-custom-stepper-without-form/cdk-custom-stepper-without-form-example';
+import {
+    CdkLinearStepperWithFormExample,
+    CustomLinearStepper
+} from './cdk-linear-stepper-with-form/cdk-linear-stepper-with-form-example';
+import {ReactiveFormsModule} from '@angular/forms';
 
 export {
   CdkCustomStepperWithoutFormExample,
   CustomStepper,
+  CdkLinearStepperWithFormExample,
+  CustomLinearStepper
 };
 
 const EXAMPLES = [
   CdkCustomStepperWithoutFormExample,
   CustomStepper,
+  CdkLinearStepperWithFormExample,
+  CustomLinearStepper
 ];
 
 @NgModule({
-  imports: [
-    CdkStepperModule,
-    CommonModule,
-  ],
+    imports: [
+        CdkStepperModule,
+        CommonModule,
+        ReactiveFormsModule,
+    ],
   declarations: EXAMPLES,
   exports: EXAMPLES,
   entryComponents: EXAMPLES,
