31afc80cfe43d1fcc5582ca1fcf196399419b41f
RN Lee <rnlee0054@gmail.com>
2020-06-04 19:58:40 -0700

    docs(material/expansion): Fix expansion-expand-collapse-all-example selector (#19499)

--- src/components-examples/material/expansion/expansion-expand-collapse-all/expansion-expand-collapse-all-example.ts
+++ src/components-examples/material/expansion/expansion-expand-collapse-all/expansion-expand-collapse-all-example.ts
@@ -5,7 +5,7 @@ import {MatAccordion} from '@angular/material/expansion';
  * @title Accordion with expand/collapse all toggles
  */
 @Component({
-  selector: 'expansion-toggle-all-example',
+  selector: 'expansion-expand-collapse-all-example',
   templateUrl: 'expansion-expand-collapse-all-example.html',
   styleUrls: ['expansion-expand-collapse-all-example.css'],
 })
