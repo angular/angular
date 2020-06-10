f0a925773c1dfaf082d8e32e1b0244cff9af23a4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-10 15:01:49 +0200

    build: add size test for standalone button-toggle

---
+++ integration/size-test/material/button-toggle/standalone.ts
@@ -0,0 +1,23 @@
+import {Component, NgModule} from '@angular/core';
+import {platformBrowser} from '@angular/platform-browser';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
+/**
+ * Basic component using a standalone `MatButtonToggle`. Other parts of the button-toggle
+ * module such as `MatButtonToggleGroup` are not used and should be tree-shaken away.
+ */
+@Component({
+  template: `
+    <mat-button-toggle>Center text</mat-button-toggle>
+  `,
+})
+export class TestComponent {}
+
+@NgModule({
+  imports: [MatButtonToggleModule],
+  declarations: [TestComponent],
+  bootstrap: [TestComponent],
+})
+export class AppModule {}
+
+platformBrowser().bootstrapModule(AppModule);
