c14eed662dac3fac514e7adb8e6a4ef1c3180143
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-10 15:48:58 +0200

    build: add size-test for date-range-picker without form-field

---
+++ integration/size-test/material/datepicker/range-picker/without-form-field.ts
@@ -0,0 +1,30 @@
+import {Component, NgModule} from '@angular/core';
+import {platformBrowser} from '@angular/platform-browser';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+/**
+ * Basic component using `MatDateRangePicker` with `MatDatepickerToggle`. Form fields
+ * are optionally used in combination with the date-range input, so we expect that to
+ * be tree-shaken away in this example. Similarly the standard datepicker input directive
+ * should be omitted.
+ */
+@Component({
+  template: `
+    <mat-date-range-input [rangePicker]="picker">
+      <input matStartDate>
+      <input matEndDate>
+    </mat-date-range-input>
+    <mat-datepicker-toggle [for]="picker" matSuffix></mat-datepicker-toggle>
+    <mat-date-range-picker #picker></mat-date-range-picker>
+  `,
+})
+export class TestComponent {}
+
+@NgModule({
+  imports: [MatDatepickerModule],
+  declarations: [TestComponent],
+  bootstrap: [TestComponent],
+})
+export class AppModule {}
+
+platformBrowser().bootstrapModule(AppModule);
