904ca6c88d8e4559db78b1f9551b42c0da51bab6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-12 19:12:43 +0200

    build: use formatting tool from shared dev-infra package (#19592)
    Removes our custom script/buildifier configuration. We will
    be using the `ng-dev format` command that has builtin support
    for buildifier (e.g with configured rules). Also it allows us
    to format changed files
    
    The motiviation is that we don't need to maintain multiple buildifier
    configurations if there is one canonical one now available in the
    shared dev-infra package.

--- .ng-dev/config.ts
+++ .ng-dev/config.ts
@@ -1,7 +1,9 @@
+import {format} from './format';
 import {github} from './github';
 import {merge} from './merge';
 
 module.exports = {
   github,
   merge,
+  format,
 };
---
+++ .ng-dev/format.ts
@@ -0,0 +1,9 @@
+import {FormatConfig} from '@angular/dev-infra-private/format/config';
+
+/**
+ * Configuration for the ng-dev format command. We currently only use the buildifier
+ * formatter that is responsible for formatting Bazel build and `.bzl` files.
+ */
+export const format: FormatConfig = {
+  buildifier: true,
+};
