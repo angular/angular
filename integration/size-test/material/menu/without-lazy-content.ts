db1efcf373e58ecb4801df077e313cb5d7f23469
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-15 15:19:41 +0200

    build: add size-test for menu without lazy-content

---
+++ integration/size-test/material/menu/without-lazy-content.ts
@@ -0,0 +1,24 @@
+import {Component, NgModule} from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {platformBrowser} from '@angular/platform-browser';
+
+/**
+ * Basic component using `MatMenu` and `MatMenuTrigger`. No lazy `MatMenuContent` is
+ * specified, so it should be tree-shaken away.
+ */
+@Component({
+  template: `
+    <button [matMenuTriggerFor]="menu">Open</button>
+    <mat-menu #menu="matMenu"></mat-menu>
+  `,
+})
+export class TestComponent {}
+
+@NgModule({
+  imports: [MatMenuModule],
+  declarations: [TestComponent],
+  bootstrap: [TestComponent],
+})
+export class AppModule {}
+
+platformBrowser().bootstrapModule(AppModule);
