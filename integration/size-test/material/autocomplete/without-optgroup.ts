ca2475680f2257e97babfdea5526f8d47641cbcb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-15 13:28:53 +0200

    build: create size test for autocomplete without optgroup

---
+++ integration/size-test/material/autocomplete/without-optgroup.ts
@@ -0,0 +1,26 @@
+import {Component, NgModule} from '@angular/core';
+import {platformBrowser} from '@angular/platform-browser';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+/**
+ * Basic component using `MatAutocomplete` and `MatOption`. Other supported parts of the
+ * autocomplete like `MatOptgroup` are not used and should be tree-shaken away.
+ */
+@Component({
+  template: `
+    <input [matAutocomplete]="myAutocomplete">
+    <mat-autocomplete #myAutocomplete>
+      <mat-option value="First">First</mat-option>
+    </mat-autocomplete>
+  `,
+})
+export class TestComponent {}
+
+@NgModule({
+  imports: [MatAutocompleteModule],
+  declarations: [TestComponent],
+  bootstrap: [TestComponent],
+})
+export class AppModule {}
+
+platformBrowser().bootstrapModule(AppModule);
