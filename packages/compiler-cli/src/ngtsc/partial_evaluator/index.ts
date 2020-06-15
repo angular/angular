712f1bd0b7fd215c734d1eaa506cfd1847ec2432
JoostK <joost.koehoorn@gmail.com>
2020-06-15 12:48:34 +0200

    feat(compiler-cli): explain why an expression cannot be used in AOT compilations (#37587)
    During AOT compilation, the value of some expressions need to be known at
    compile time. The compiler has the ability to statically evaluate expressions
    the best it can, but there can be occurrences when an expression cannot be
    evaluated statically. For instance, the evaluation could depend on a dynamic
    value or syntax is used that the compiler does not understand. Alternatively,
    it is possible that an expression could be statically evaluated but the
    resulting value would be of an incorrect type.
    
    In these situations, it would be helpful if the compiler could explain why it
    is unable to evaluate an expression. To this extend, the static interpreter
    in Ivy keeps track of a trail of `DynamicValue`s which follow the path of nodes
    that were considered all the way to the node that causes an expression to be
    considered dynamic. Up until this commit, this rich trail of information was
    not surfaced to a developer so the compiler was of little help to explain
    why static evaluation failed, resulting in situations that are hard to debug
    and resolve.
    
    This commit adds much more insight to the diagnostic that is produced for static
    evaluation errors. For dynamic values, the trail of `DynamicValue` instances
    is presented to the user in a meaningful way. If a value is available but not
    of the correct type, the type of the resolved value is shown.
    
    Resolves FW-2155
    
    PR Close #37587

--- packages/compiler-cli/src/ngtsc/partial_evaluator/index.ts
+++ packages/compiler-cli/src/ngtsc/partial_evaluator/index.ts
@@ -6,6 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+export {describeResolvedType, traceDynamicValue} from './src/diagnostics';
 export {DynamicValue} from './src/dynamic';
 export {ForeignFunctionResolver, PartialEvaluator} from './src/interface';
 export {EnumValue, KnownFn, ResolvedValue, ResolvedValueArray, ResolvedValueMap} from './src/result';
