7f93f7ef478e42f22df5824ef134c3a6f3cf882b
Joey Perrott <josephperrott@gmail.com>
2020-06-17 05:34:41 -0700

    build: move shims_for_IE to third_party directory (#37624)
    The shims_for_IE.js file contains vendor code that predates the third_party
    directory. This file is currently used for internal karma testing setup. This
    change corrects this by moving the shims_for_IE file to //third_part/
    
    PR Close #37624

--- karma-js.conf.js
+++ karma-js.conf.js
@@ -42,7 +42,7 @@ module.exports = function(config) {
 
       // Including systemjs because it defines `__eval`, which produces correct stack traces.
       'test-events.js',
-      'shims_for_IE.js',
+      'third_party/shims_for_IE.js',
       'node_modules/systemjs/dist/system.src.js',
 
       // Serve polyfills necessary for testing the `elements` package.
