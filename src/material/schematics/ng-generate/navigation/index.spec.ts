31723b858775417b4d5e9d98ed0d052e932645cd
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-06-19 05:44:50 +0530

    feat(material/schematics): enable routing option (#19439)

--- src/material/schematics/ng-generate/navigation/index.spec.ts
+++ src/material/schematics/ng-generate/navigation/index.spec.ts
@@ -155,4 +155,26 @@ describe('material-navigation-schematic', () => {
       expect(tree.files).not.toContain('/projects/material/src/app/foo/foo.component.spec.ts');
     });
   });
+  describe('router option', () => {
+    it('should respect the option value if routing true', async () => {
+      const tree =
+        await runner
+          .runSchematicAsync(
+            'navigation', { routing: true, ...baseOptions }, await createTestApp(runner))
+          .toPromise();
+      const template = tree
+          .readContent('/projects/material/src/app/foo/foo.component.html');
+      expect(template).toContain('<a mat-list-item routerLink="/">Link 1</a>');
+    });
+    it('should respect the option value if routing false', async () => {
+      const tree =
+        await runner
+          .runSchematicAsync(
+            'navigation', { routing: false, ...baseOptions }, await createTestApp(runner))
+          .toPromise();
+      const template = tree
+          .readContent('/projects/material/src/app/foo/foo.component.html');
+      expect(template).toContain('<a mat-list-item href="#">Link 1</a>');
+    });
+  });
 });
