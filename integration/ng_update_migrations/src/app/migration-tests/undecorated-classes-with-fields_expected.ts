73b9455888697d938c2596f71c37d19e89884907
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-25 21:08:01 +0200

    fixup! fix(migrations): do not incorrectly add todo for @Injectable or @Pipe
    Add comment to integration test for clarity

--- integration/ng_update_migrations/src/app/migration-tests/undecorated-classes-with-fields_expected.ts
+++ integration/ng_update_migrations/src/app/migration-tests/undecorated-classes-with-fields_expected.ts
@@ -90,11 +90,15 @@ export class WithDirectiveLifecycleHook {
   ngOnInit() {}
 }
 
+// This class is already decorated and should not be migrated. i.e. no TODO
+// or Angular decorator should be added. `@Injectable` is sufficient.
 @Injectable()
 export class MyService {
   ngOnDestroy() {}
 }
 
+// This class is already decorated and should not be migrated. i.e. no TODO
+// or Angular decorator should be added. `@Injectable` is sufficient.
 @Pipe({name: 'my-pipe'})
 export class MyPipe {
   ngOnDestroy() {}
