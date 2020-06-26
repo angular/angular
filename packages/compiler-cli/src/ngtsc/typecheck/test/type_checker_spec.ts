49f27e31ed8c9b563b92a193a1a9eed366e069de
JoostK <joost.koehoorn@gmail.com>
2020-06-26 23:06:36 +0200

    test(compiler-cli): re-enable dynamic value diagnostic tests on Windows CI (#37782)
    This commit re-enables some tests that were temporarily disabled on Windows,
    as they failed on native Windows CI. The Windows filesystem emulation has
    been corrected in an earlier commit, such that the original failure would
    now also occur during emulation on Linux CI.
    
    PR Close #37782

--- packages/compiler-cli/src/ngtsc/typecheck/test/type_checker_spec.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/test/type_checker_spec.ts
@@ -6,8 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {platform} from 'os';
-
 import {ErrorCode, ngErrorCode} from '../../diagnostics';
 import {absoluteFrom, absoluteFromSourceFile, getSourceFileOrError} from '../../file_system';
 import {runInEachFileSystem} from '../../file_system/testing';
@@ -15,7 +13,7 @@ import {OptimizeFor} from '../api';
 
 import {getClass, setup, TestDeclaration} from './test_utils';
 
-runInEachFileSystem(os => {
+runInEachFileSystem(() => {
   describe('TemplateTypeChecker', () => {
     it('should batch diagnostic operations when requested in WholeProgram mode', () => {
       const file1 = absoluteFrom('/file1.ts');
@@ -172,50 +170,47 @@ runInEachFileSystem(os => {
         expect(diags[0].code).toBe(ngErrorCode(ErrorCode.INLINE_TCB_REQUIRED));
       });
 
-      // These tests are currently disabled when running in Windows mode as the assertions involving
-      // the filename attached to the diagnostic are suffering from a case-sensitivity issue.
-      if (os !== 'Windows' && platform() !== 'win32') {
-        it('should produce errors for components that require type constructor inlining', () => {
-          const fileName = absoluteFrom('/main.ts');
-          const dirFile = absoluteFrom('/dir.ts');
-          const {program, templateTypeChecker} = setup(
-              [
-                {
-                  fileName,
-                  source: `export class Cmp {}`,
-                  templates: {'Cmp': '<div dir></div>'},
-                  declarations: [{
-                    name: 'TestDir',
-                    selector: '[dir]',
-                    file: dirFile,
-                    type: 'directive',
-                    isGeneric: true,
-                  }]
-                },
-                {
-                  fileName: dirFile,
-                  source: `
-                  // A non-exported interface used as a type bound for a generic directive causes
-                  // an inline type constructor to be required.
-                  interface NotExported {}
-                  export abstract class TestDir<T extends NotExported> {}`,
-                  templates: {},
-                }
-              ],
-              {inlining: false});
-          const sf = getSourceFileOrError(program, fileName);
-          const diags = templateTypeChecker.getDiagnosticsForFile(sf, OptimizeFor.WholeProgram);
-          expect(diags.length).toBe(1);
-          expect(diags[0].code).toBe(ngErrorCode(ErrorCode.INLINE_TYPE_CTOR_REQUIRED));
+      it('should produce errors for components that require type constructor inlining', () => {
+        const fileName = absoluteFrom('/main.ts');
+        const dirFile = absoluteFrom('/dir.ts');
+        const {program, templateTypeChecker} = setup(
+            [
+              {
+                fileName,
+                source: `export class Cmp {}`,
+                templates: {'Cmp': '<div dir></div>'},
+                declarations: [{
+                  name: 'TestDir',
+                  selector: '[dir]',
+                  file: dirFile,
+                  type: 'directive',
+                  isGeneric: true,
+                }]
+              },
+              {
+                fileName: dirFile,
+                source: `
+                // A non-exported interface used as a type bound for a generic directive causes
+                // an inline type constructor to be required.
+                interface NotExported {}
+                export class TestDir<T extends NotExported> {}`,
+                templates: {},
+              }
+            ],
+            {inlining: false});
+        const sf = getSourceFileOrError(program, fileName);
+        const diags = templateTypeChecker.getDiagnosticsForFile(sf, OptimizeFor.WholeProgram);
+        expect(diags.length).toBe(1);
+        expect(diags[0].code).toBe(ngErrorCode(ErrorCode.INLINE_TYPE_CTOR_REQUIRED));
 
-          // The relatedInformation of the diagnostic should point to the directive which required
-          // the inline type constructor.
-          expect(diags[0].relatedInformation).not.toBeUndefined();
-          expect(diags[0].relatedInformation!.length).toBe(1);
-          expect(diags[0].relatedInformation![0].file).not.toBeUndefined();
-          expect(absoluteFromSourceFile(diags[0].relatedInformation![0].file!)).toBe(dirFile);
-        });
-      }
+        // The relatedInformation of the diagnostic should point to the directive which required
+        // the inline type constructor.
+        const dirSf = getSourceFileOrError(program, dirFile);
+        expect(diags[0].relatedInformation).not.toBeUndefined();
+        expect(diags[0].relatedInformation!.length).toBe(1);
+        expect(diags[0].relatedInformation![0].file).not.toBeUndefined();
+        expect(absoluteFromSourceFile(diags[0].relatedInformation![0].file!)).toBe(dirSf.fileName);
+      });
     });
 
     describe('template overrides', () => {
