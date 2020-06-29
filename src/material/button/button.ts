c2abcadb027f79d6cd5404c2bc857a7770df95c1
Nick Walther <61717337+nickwalther@users.noreply.github.com>
2020-06-29 08:17:16 -0700

    refactor: style disabled button with class instead of `:disabled` (#19406)
    Uses a new class, `.mat-button-disabled`, for applying disabled button styles. This change request comes from GCP, which wants to avoid _actually_ disabling buttons so that they can have tooltips.

--- src/material/button/button.ts
+++ src/material/button/button.ts
@@ -71,6 +71,10 @@ const _MatButtonMixinBase: CanDisableRippleCtor & CanDisableCtor & CanColorCtor 
   host: {
     '[attr.disabled]': 'disabled || null',
     '[class._mat-animation-noopable]': '_animationMode === "NoopAnimations"',
+    // Add a class for disabled button styling instead of the using attribute
+    // selector or pseudo-selector.  This allows users to create focusabled
+    // disabled buttons without recreating the styles.
+    '[class.mat-button-disabled]': 'disabled',
     'class': 'mat-focus-indicator',
   },
   templateUrl: 'button.html',
@@ -160,6 +164,7 @@ export class MatButton extends _MatButtonMixinBase
     '[attr.aria-disabled]': 'disabled.toString()',
     '(click)': '_haltDisabledEvents($event)',
     '[class._mat-animation-noopable]': '_animationMode === "NoopAnimations"',
+    '[class.mat-button-disabled]': 'disabled',
     'class': 'mat-focus-indicator',
   },
   inputs: ['disabled', 'disableRipple', 'color'],
