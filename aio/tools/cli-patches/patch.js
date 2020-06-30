11aaea14dfafc74ffd5bf1bcca5c647d684cb73e
George Kalpakas <kalpakas.g@gmail.com>
2020-06-30 18:31:43 +0300

    TEST - `@angular-devkit/build-angular` v#8752571a9 + disabled `DedupeModuleResolvePlugin`

--- aio/tools/cli-patches/patch.js
+++ aio/tools/cli-patches/patch.js
@@ -1,9 +1,11 @@
-const fs = require('fs');
 const sh = require('shelljs');
 
 const PATCH_LOCK = 'node_modules/@angular/cli/.patched';
 
-if (!fs.existsSync(PATCH_LOCK)) {
+sh.set('-e');
+sh.cd(`${__dirname}/../../`);
+
+if (!sh.test('-f', PATCH_LOCK)) {
+  sh.ls(`${__dirname}/*.patch`).forEach(patchFile => sh.exec(`patch -p0 -i "${patchFile}"`));
   sh.touch(PATCH_LOCK);
 }
-
