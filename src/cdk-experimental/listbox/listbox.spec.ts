d9b223879bddbdf8e2d352e925527945f8b32bc2
nielsr98 <54564567+nielsr98@users.noreply.github.com>
2020-07-08 13:42:16 -0400

    Cdk listbox key management (#19810)

--- src/cdk-experimental/listbox/listbox.spec.ts
+++ src/cdk-experimental/listbox/listbox.spec.ts
@@ -1,20 +1,28 @@
 import {
   ComponentFixture,
   async,
-  TestBed,
+  TestBed, tick, fakeAsync,
 } from '@angular/core/testing';
 import {Component, DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
 import {
   CdkOption,
-  CdkListboxModule, ListboxSelectionChangeEvent
+  CdkListboxModule, ListboxSelectionChangeEvent, CdkListbox
 } from './index';
-import {dispatchMouseEvent} from '@angular/cdk/testing/private';
+import {dispatchKeyboardEvent, dispatchMouseEvent} from '@angular/cdk/testing/private';
+import {A, DOWN_ARROW, END, HOME, SPACE} from '@angular/cdk/keycodes';
 
 describe('CdkOption', () => {
 
   describe('selection state change', () => {
     let fixture: ComponentFixture<ListboxWithOptions>;
+
+    let testComponent: ListboxWithOptions;
+
+    let listbox: DebugElement;
+    let listboxInstance: CdkListbox;
+    let listboxElement: HTMLElement;
+
     let options: DebugElement[];
     let optionInstances: CdkOption[];
     let optionElements: HTMLElement[];
@@ -29,6 +37,12 @@ describe('CdkOption', () => {
     beforeEach(async(() => {
       fixture = TestBed.createComponent(ListboxWithOptions);
       fixture.detectChanges();
+
+      testComponent = fixture.debugElement.componentInstance;
+
+      listbox = fixture.debugElement.query(By.directive(CdkListbox));
+      listboxInstance = listbox.injector.get<CdkListbox>(CdkListbox);
+      listboxElement = listbox.nativeElement;
 
       options = fixture.debugElement.queryAll(By.directive(CdkOption));
       optionInstances = options.map(o => o.injector.get<CdkOption>(CdkOption));
@@ -52,7 +66,7 @@ describe('CdkOption', () => {
     });
 
     it('should update aria-selected when selected is changed programmatically', () => {
-      expect(optionElements[0].getAttribute('aria-selected')).toBeNull();
+      expect(optionElements[0].hasAttribute('aria-selected')).toBeFalse();
       optionInstances[1].selected = true;
       fixture.detectChanges();
 
@@ -63,7 +77,7 @@ describe('CdkOption', () => {
       let selectedOptions = optionInstances.filter(option => option.selected);
 
       expect(selectedOptions.length).toBe(0);
-      expect(optionElements[0].getAttribute('aria-selected')).toBeNull();
+      expect(optionElements[0].hasAttribute('aria-selected')).toBeFalse();
       expect(optionInstances[0].selected).toBeFalse();
       expect(fixture.componentInstance.changedOption).toBeUndefined();
 
@@ -77,21 +91,247 @@ describe('CdkOption', () => {
       expect(fixture.componentInstance.changedOption).toBeDefined();
       expect(fixture.componentInstance.changedOption.id).toBe(optionInstances[0].id);
     });
+
+    it('should update selected option on space or enter key press', () => {
+      let selectedOptions = optionInstances.filter(option => option.selected);
+
+      expect(selectedOptions.length).toBe(0);
+      expect(optionElements[0].hasAttribute('aria-selected')).toBeFalse();
+      expect(optionInstances[0].selected).toBeFalse();
+      expect(fixture.componentInstance.changedOption).toBeUndefined();
+
+      listboxInstance.setActiveOption(optionInstances[0]);
+      dispatchKeyboardEvent(listboxElement, 'keydown', SPACE);
+      fixture.detectChanges();
+
+      selectedOptions = optionInstances.filter(option => option.selected);
+      expect(selectedOptions.length).toBe(1);
+      expect(optionElements[0].getAttribute('aria-selected')).toBe('true');
+      expect(optionInstances[0].selected).toBeTrue();
+      expect(fixture.componentInstance.changedOption).toBeDefined();
+      expect(fixture.componentInstance.changedOption.id).toBe(optionInstances[0].id);
+    });
+
+    it('should update active option on home and end key press', () => {
+      listboxInstance.setActiveOption(optionInstances[1]);
+      fixture.detectChanges();
+
+      expect(listboxInstance._listKeyManager.activeItem).toBe(optionInstances[1]);
+
+      dispatchKeyboardEvent(listboxElement, 'keydown', HOME);
+      fixture.detectChanges();
+
+      expect(listboxInstance._listKeyManager.activeItem).toBe(optionInstances[0]);
+
+      dispatchKeyboardEvent(listboxElement, 'keydown', END);
+      fixture.detectChanges();
+
+      expect(listboxInstance._listKeyManager.activeItem).toBe(optionInstances[3]);
+    });
+
+    it('should be able to toggle listbox disabled state', () => {
+      expect(listboxInstance.disabled).toBeFalse();
+      expect(listboxElement.getAttribute('aria-disabled')).toBe('false');
+
+      testComponent.isListboxDisabled = true;
+      fixture.detectChanges();
+
+      expect(listboxInstance.disabled).toBeTrue();
+      expect(listboxElement.getAttribute('aria-disabled')).toBe('true');
+    });
+
+    it('should toggle option disabled state', () => {
+      expect(optionInstances[0].disabled).toBeFalse();
+      expect(optionElements[0].getAttribute('aria-disabled')).toBe('false');
+
+      testComponent.isPurpleDisabled = true;
+      fixture.detectChanges();
+
+      expect(optionInstances[0].disabled).toBeTrue();
+      expect(optionElements[0].getAttribute('aria-disabled')).toBe('true');
+    });
+
+    it('should toggle option aria-disabled state on listbox disabled state change', () => {
+      optionInstances[0].disabled = true;
+      fixture.detectChanges();
+
+      expect(listboxInstance.disabled).toBeFalse();
+      expect(optionInstances[0].disabled).toBeTrue();
+      expect(optionElements[0].hasAttribute('tabindex')).toBeFalse();
+      expect(optionElements[1].getAttribute('aria-disabled')).toBe('false');
+      expect(optionElements[1].getAttribute('tabindex')).toBe('-1');
+
+      testComponent.isListboxDisabled = true;
+      fixture.detectChanges();
+
+      expect(listboxInstance.disabled).toBeTrue();
+      expect(optionInstances[0].disabled).toBeTrue();
+      expect(optionElements[0].hasAttribute('tabindex')).toBeFalse();
+      expect(optionElements[1].getAttribute('aria-disabled')).toBe('true');
+      expect(optionElements[1].hasAttribute('tabindex')).toBeFalse();
+    });
+
+    it('should not toggle selected on click of a disabled option', () => {
+      let selectedOptions = optionInstances.filter(option => option.selected);
+
+      expect(selectedOptions.length).toBe(0);
+      expect(optionElements[0].hasAttribute('aria-selected')).toBeFalse();
+      expect(optionInstances[0].selected).toBeFalse();
+      expect(fixture.componentInstance.changedOption).toBeUndefined();
+
+      testComponent.isPurpleDisabled = true;
+      fixture.detectChanges();
+      dispatchMouseEvent(optionElements[0], 'click');
+      fixture.detectChanges();
+
+      selectedOptions = optionInstances.filter(option => option.selected);
+      expect(selectedOptions.length).toBe(0);
+      expect(optionElements[0].hasAttribute('aria-selected')).toBeFalse();
+      expect(optionInstances[0].selected).toBeFalse();
+      expect(fixture.componentInstance.changedOption).toBeUndefined();
+    });
+
+    it('should not toggle selected on click in a disabled listbox', () => {
+      let selectedOptions = optionInstances.filter(option => option.selected);
+
+      expect(selectedOptions.length).toBe(0);
+      expect(optionElements[0].hasAttribute('aria-selected')).toBeFalse();
+      expect(optionInstances[0].selected).toBeFalse();
+      expect(fixture.componentInstance.changedOption).toBeUndefined();
+
+      testComponent.isListboxDisabled = true;
+      fixture.detectChanges();
+      dispatchMouseEvent(optionElements[0], 'click');
+      fixture.detectChanges();
+
+      selectedOptions = optionInstances.filter(option => option.selected);
+      expect(selectedOptions.length).toBe(0);
+      expect(optionElements[0].hasAttribute('aria-selected')).toBeFalse();
+      expect(optionInstances[0].selected).toBeFalse();
+      expect(fixture.componentInstance.changedOption).toBeUndefined();
+    });
+
+    it('should change active item using type ahead', fakeAsync(() => {
+      expect(listboxInstance._listKeyManager.activeItem).toBeNull();
+      expect(listboxInstance._listKeyManager.activeItemIndex).toBe(-1);
+
+      dispatchKeyboardEvent(listboxElement, 'keydown', A);
+      fixture.detectChanges();
+      tick(200);
+
+      expect(listboxInstance._listKeyManager.activeItem).toEqual(optionInstances[2]);
+      expect(listboxInstance._listKeyManager.activeItemIndex).toBe(2);
+    }));
+
+    it('should not handle space or enter on a disabled listbox', () => {
+      let selectedOptions = optionInstances.filter(option => option.selected);
+
+      expect(selectedOptions.length).toBe(0);
+      expect(fixture.componentInstance.changedOption).toBeUndefined();
+
+      listboxInstance.setActiveOption(optionInstances[0]);
+      testComponent.isListboxDisabled = true;
+      fixture.detectChanges();
+
+      dispatchKeyboardEvent(listboxElement, 'keydown', SPACE);
+      fixture.detectChanges();
+
+      selectedOptions = optionInstances.filter(option => option.selected);
+      expect(selectedOptions.length).toBe(0);
+      expect(fixture.componentInstance.changedOption).toBeUndefined();
+    });
+
+    it('should not handle type ahead on a disabled listbox', fakeAsync(() => {
+      expect(listboxInstance._listKeyManager.activeItem).toBeNull();
+      expect(listboxInstance._listKeyManager.activeItemIndex).toBe(-1);
+
+      testComponent.isListboxDisabled = true;
+      fixture.detectChanges();
+
+      dispatchKeyboardEvent(listboxElement, 'keydown', A);
+      fixture.detectChanges();
+      tick(200);
+
+      expect(listboxInstance._listKeyManager.activeItem).toBeNull();
+      expect(listboxInstance._listKeyManager.activeItemIndex).toBe(-1);
+    }));
+
+    it('should not select a disabled option using space or enter', () => {
+      let selectedOptions = optionInstances.filter(option => option.selected);
+
+      expect(selectedOptions.length).toBe(0);
+      expect(fixture.componentInstance.changedOption).toBeUndefined();
+
+      listboxInstance.setActiveOption(optionInstances[0]);
+      testComponent.isPurpleDisabled = true;
+      fixture.detectChanges();
+
+      dispatchKeyboardEvent(listboxElement, 'keydown', SPACE);
+      fixture.detectChanges();
+
+      selectedOptions = optionInstances.filter(option => option.selected);
+      expect(selectedOptions.length).toBe(0);
+      expect(fixture.componentInstance.changedOption).toBeUndefined();
+    });
+
+    it('should update active item upon arrow key presses', () => {
+      expect(listboxInstance._listKeyManager.activeItem).toBeNull();
+      expect(listboxInstance._listKeyManager.activeItemIndex).toBe(-1);
+
+      dispatchKeyboardEvent(listboxElement, 'keydown', DOWN_ARROW);
+      fixture.detectChanges();
+
+      expect(listboxInstance._listKeyManager.activeItem).toEqual(optionInstances[0]);
+      expect(listboxInstance._listKeyManager.activeItemIndex).toBe(0);
+
+      dispatchKeyboardEvent(listboxElement, 'keydown', DOWN_ARROW);
+      fixture.detectChanges();
+
+      expect(listboxInstance._listKeyManager.activeItem).toEqual(optionInstances[1]);
+      expect(listboxInstance._listKeyManager.activeItemIndex).toBe(1);
+    });
+
+    it('should skip disabled options when navigating with arrow keys', () => {
+      expect(listboxInstance._listKeyManager.activeItem).toBeNull();
+      expect(listboxInstance._listKeyManager.activeItemIndex).toBe(-1);
+
+      testComponent.isSolarDisabled = true;
+      dispatchKeyboardEvent(listboxElement, 'keydown', DOWN_ARROW);
+      fixture.detectChanges();
+
+      expect(listboxInstance._listKeyManager.activeItem).toEqual(optionInstances[0]);
+      expect(listboxInstance._listKeyManager.activeItemIndex).toBe(0);
+
+      dispatchKeyboardEvent(listboxElement, 'keydown', DOWN_ARROW);
+      fixture.detectChanges();
+
+      expect(listboxInstance._listKeyManager.activeItem).toEqual(optionInstances[2]);
+      expect(listboxInstance._listKeyManager.activeItemIndex).toBe(2);
+    });
   });
 
 });
 
 @Component({
   template: `
-  <div cdkListbox (selectionChange)="onSelectionChange($event)">
-    <div cdkOption>Void</div>
-    <div cdkOption>Solar</div>
-    <div cdkOption>Arc</div>
-    <div cdkOption>Stasis</div>
-  </div>`
+    <div cdkListbox
+        [disabled]="isListboxDisabled"
+        (selectionChange)="onSelectionChange($event)">
+      <div cdkOption
+          [disabled]="isPurpleDisabled">
+        Purple</div>
+      <div cdkOption
+           [disabled]="isSolarDisabled">
+        Solar</div>
+      <div cdkOption>Arc</div>
+      <div cdkOption>Stasis</div>
+    </div>`
 })
 class ListboxWithOptions {
   changedOption: CdkOption;
+  isListboxDisabled: boolean = false;
+  isPurpleDisabled: boolean = false;
+  isSolarDisabled: boolean = false;
 
   onSelectionChange(event: ListboxSelectionChangeEvent) {
     this.changedOption = event.option;
--- src/cdk-experimental/listbox/listbox.ts
+++ src/cdk-experimental/listbox/listbox.ts
@@ -7,98 +7,214 @@
  */
 
 import {
+  AfterContentInit,
   ContentChildren,
   Directive,
-  EventEmitter, forwardRef, Inject,
-  Input, Output,
+  ElementRef, EventEmitter, forwardRef,
+  Inject,
+  Input, OnDestroy, Output,
   QueryList
 } from '@angular/core';
+import {ActiveDescendantKeyManager, Highlightable, ListKeyManagerOption} from '@angular/cdk/a11y';
+import {END, ENTER, HOME, SPACE} from '@angular/cdk/keycodes';
 import {BooleanInput, coerceBooleanProperty} from '@angular/cdk/coercion';
 
 let nextId = 0;
 
-/**
- * Directive that applies interaction patterns to an element following the aria role of option.
- * Typically meant to be placed inside a listbox. Logic handling selection, disabled state, and
- * value is built in.
- */
 @Directive({
   selector: '[cdkOption]',
   exportAs: 'cdkOption',
   host: {
-    role: 'option',
+    'role': 'option',
     '(click)': 'toggle()',
-    '[attr.aria-selected]': 'selected || null',
+    '(focus)': 'activate()',
+    '(blur)': 'deactivate()',
     '[id]': 'id',
+    '[attr.aria-selected]': '_selected || null',
+    '[attr.tabindex]': '_getTabIndex()',
+    '[attr.aria-disabled]': '_isInteractionDisabled()',
+    '[class.cdk-option-disabled]': '_isInteractionDisabled()',
+    '[class.cdk-option-active]': '_active'
+
   }
 })
-export class CdkOption {
+export class CdkOption implements ListKeyManagerOption, Highlightable {
   private _selected: boolean = false;
+  private _disabled: boolean = false;
+  _active: boolean = false;
 
-  /** Whether the option is selected or not */
   @Input()
   get selected(): boolean {
     return this._selected;
   }
   set selected(value: boolean) {
-    this._selected = coerceBooleanProperty(value);
+    if (!this._disabled) {
+      this._selected = coerceBooleanProperty(value);
+    }
   }
 
-  /** The id of the option, set to a uniqueid if the user does not provide one */
+  /** The id of the option, set to a uniqueid if the user does not provide one. */
   @Input() id = `cdk-option-${nextId++}`;
 
-  constructor(@Inject(forwardRef(() => CdkListbox)) public listbox: CdkListbox) {}
+  @Input()
+  get disabled(): boolean {
+    return this._disabled;
+  }
+  set disabled(value: boolean) {
+    this._disabled = coerceBooleanProperty(value);
+  }
 
-  /** Toggles the selected state, emits a change event through the injected listbox */
+  constructor(private _elementRef: ElementRef,
+              @Inject(forwardRef(() => CdkListbox)) public listbox: CdkListbox) {
+  }
+
+  /** Toggles the selected state, emits a change event through the injected listbox. */
   toggle() {
-    this.selected = !this.selected;
-    this.listbox._emitChangeEvent(this);
+    if (!this._isInteractionDisabled()) {
+      this.selected = !this.selected;
+      this.listbox._emitChangeEvent(this);
+    }
+  }
+
+  /** Sets the active property true if the option and listbox aren't disabled. */
+  activate() {
+    if (!this._isInteractionDisabled()) {
+      this._active = true;
+      this.listbox.setActiveOption(this);
+    }
+  }
+
+  /** Sets the active property false. */
+  deactivate() {
+    this._active = false;
+  }
+
+  /** Returns true if the option or listbox are disabled, and false otherwise. */
+  _isInteractionDisabled(): boolean {
+    return (this.listbox.disabled || this._disabled);
+  }
+
+  /** Returns the tab index which depends on the disabled property. */
+  _getTabIndex(): string | null {
+    return (this.listbox.disabled || this._disabled) ? null : '-1';
+  }
+
+  getLabel(): string {
+    // TODO: improve to method to handle more complex combinations of elements and text
+    return this._elementRef.nativeElement.textContent;
+  }
+
+  setActiveStyles() {
+    this._active = true;
+  }
+
+  setInactiveStyles() {
+    this._active = false;
   }
 
   static ngAcceptInputType_selected: BooleanInput;
+  static ngAcceptInputType_disabled: BooleanInput;
 }
 
-/**
- * Directive that applies interaction patterns to an element following the aria role of listbox.
- * Typically CdkOption elements are placed inside the listbox. Logic to handle keyboard navigation,
- * selection of options, active options, and disabled states is built in.
- */
 @Directive({
-  selector: '[cdkListbox]',
-  exportAs: 'cdkListbox',
-  host: {
-    role: 'listbox',
-  }
+    selector: '[cdkListbox]',
+    exportAs: 'cdkListbox',
+    host: {
+      'role': 'listbox',
+      '(keydown)': '_keydown($event)',
+      '[attr.aria-disabled]': '_disabled',
+    }
 })
-export class CdkListbox {
+export class CdkListbox implements AfterContentInit, OnDestroy {
 
-  /** A query list containing all CdkOption elements within this listbox */
+  _listKeyManager: ActiveDescendantKeyManager<CdkOption>;
+  private _disabled: boolean = false;
+
   @ContentChildren(CdkOption, {descendants: true}) _options: QueryList<CdkOption>;
 
   @Output() readonly selectionChange: EventEmitter<ListboxSelectionChangeEvent> =
       new EventEmitter<ListboxSelectionChangeEvent>();
 
-  /** Emits a selection change event, called when an option has its selected state changed */
+  @Input()
+  get disabled(): boolean {
+    return this._disabled;
+  }
+  set disabled(value: boolean) {
+    this._disabled = coerceBooleanProperty(value);
+  }
+
+  ngAfterContentInit() {
+    this._listKeyManager = new ActiveDescendantKeyManager(this._options)
+      .withWrap().withVerticalOrientation().withTypeAhead();
+  }
+
+  ngOnDestroy() {
+    this._listKeyManager.change.complete();
+  }
+
+  _keydown(event: KeyboardEvent) {
+    if (this._disabled) {
+      return;
+    }
+
+    const manager = this._listKeyManager;
+    const keyCode = event.keyCode;
+
+    if (keyCode === HOME || keyCode === END) {
+      event.preventDefault();
+      keyCode === HOME ? manager.setFirstItemActive() : manager.setLastItemActive();
+
+    } else if (keyCode === SPACE || keyCode === ENTER) {
+      if (manager.activeItem && !manager.isTyping()) {
+        this._toggleActiveOption();
+      }
+
+    } else {
+      manager.onKeydown(event);
+    }
+
+  }
+
+  /** Emits a selection change event, called when an option has its selected state changed. */
   _emitChangeEvent(option: CdkOption) {
     this.selectionChange.emit(new ListboxSelectionChangeEvent(this, option));
   }
 
-  /** Sets the given option's selected state to true */
-  select(option: CdkOption) {
-    option.selected = true;
+  private _toggleActiveOption() {
+    const activeOption = this._listKeyManager.activeItem;
+    if (activeOption && !activeOption.disabled) {
+      activeOption.toggle();
+      this._emitChangeEvent(activeOption);
+    }
   }
 
-  /** Sets the given option's selected state to null. Null is preferable for screen readers */
-  deselect(option: CdkOption) {
-    option.selected = false;
+  /** Selects the given option if the option and listbox aren't disabled. */
+  select(option: CdkOption) {
+    if (!this.disabled && !option.disabled) {
+      option.selected = true;
+    }
   }
+
+  /** Deselects the given option if the option and listbox aren't disabled. */
+  deselect(option: CdkOption) {
+    if (!this.disabled && !option.disabled) {
+      option.selected = false;
+    }
+  }
+
+  /** Updates the key manager's active item to the given option. */
+  setActiveOption(option: CdkOption) {
+    this._listKeyManager.updateActiveItem(option);
+  }
+
+  static ngAcceptInputType_disabled: BooleanInput;
 }
 
 /** Change event that is being fired whenever the selected state of an option changes. */
 export class ListboxSelectionChangeEvent {
   constructor(
-    /** Reference to the listbox that emitted the event. */
-    public source: CdkListbox,
-    /** Reference to the option that has been changed. */
-    public option: CdkOption) {}
+      /** Reference to the listbox that emitted the event. */
+      public source: CdkListbox,
+      /** Reference to the option that has been changed. */
+      public option: CdkOption) {}
 }
