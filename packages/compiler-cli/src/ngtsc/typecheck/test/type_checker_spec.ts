271e0f98968f311364f2e202088f61c572bebdda
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-14 13:20:26 -0700

    refactor(compiler-cli): allow program strategies to opt out of inlining (#38105)
    The template type-checking engine relies on the abstraction interface
    `TypeCheckingProgramStrategy` to create updated `ts.Program`s for
    template type-checking. The basic API is that the type-checking engine
    requests changes to certain files in the program, and the strategy provides
    an updated `ts.Program`.
    
    Typically, such changes are made to 'ngtypecheck' shim files, but certain
    conditions can cause template type-checking to require "inline" operations,
    which change user .ts files instead. The strategy used by 'ngc' (the
    `ReusedProgramStrategy`) supports these kinds of updates, but other clients
    such as the language service might not always support modifying user files.
    
    To accommodate this, the `TypeCheckingProgramStrategy` interface was
    modified to include a `supportsInlineOperations` flag. If an implementation
    specifies `false` for inline support, the template type-checking system will
    return diagnostics on components which would otherwise require inline
    operations.
    
    Closes #38059
    
    PR Close #38105

--- packages/compiler-cli/src/ngtsc/typecheck/test/type_checker_spec.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/test/type_checker_spec.ts
@@ -6,7 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {absoluteFrom, getSourceFileOrError} from '../../file_system';
+import {ErrorCode, ngErrorCode} from '../../diagnostics';
+import {absoluteFrom, absoluteFromSourceFile, getSourceFileOrError} from '../../file_system';
 import {runInEachFileSystem} from '../../file_system/testing';
 
 import {getClass, setup} from './test_utils';
@@ -42,6 +43,91 @@ runInEachFileSystem(() => {
       expect(block).not.toBeNull();
       expect(block!.getText()).toMatch(/: i[0-9]\.Cmp1/);
       expect(block!.getText()).toContain(`document.createElement("div")`);
+    });
+
+    describe('when inlining is unsupported', () => {
+      it('should not produce errors for components that do not require inlining', () => {
+        const fileName = absoluteFrom('/main.ts');
+        const dirFile = absoluteFrom('/dir.ts');
+        const {program, templateTypeChecker} = setup(
+            [
+              {
+                fileName,
+                source: `export class Cmp {}`,
+                templates: {'Cmp': '<div dir></div>'},
+                declarations: [{
+                  name: 'TestDir',
+                  selector: '[dir]',
+                  file: dirFile,
+                  type: 'directive',
+                }]
+              },
+              {
+                fileName: dirFile,
+                source: `export class TestDir {}`,
+                templates: {},
+              }
+            ],
+            {inlining: false});
+        const sf = getSourceFileOrError(program, fileName);
+        const diags = templateTypeChecker.getDiagnosticsForFile(sf);
+        expect(diags.length).toBe(0);
+      });
+
+      it('should produce errors for components that require TCB inlining', () => {
+        const fileName = absoluteFrom('/main.ts');
+        const {program, templateTypeChecker} = setup(
+            [{
+              fileName,
+              source: `class Cmp {} // not exported, so requires inline`,
+              templates: {'Cmp': '<div></div>'}
+            }],
+            {inlining: false});
+        const sf = getSourceFileOrError(program, fileName);
+        const diags = templateTypeChecker.getDiagnosticsForFile(sf);
+        expect(diags.length).toBe(1);
+        expect(diags[0].code).toBe(ngErrorCode(ErrorCode.INLINE_TCB_REQUIRED));
+      });
+
+      it('should produce errors for components that require type constructor inlining', () => {
+        const fileName = absoluteFrom('/main.ts');
+        const dirFile = absoluteFrom('/dir.ts');
+        const {program, templateTypeChecker} = setup(
+            [
+              {
+                fileName,
+                source: `export class Cmp {}`,
+                templates: {'Cmp': '<div dir></div>'},
+                declarations: [{
+                  name: 'TestDir',
+                  selector: '[dir]',
+                  file: dirFile,
+                  type: 'directive',
+                }]
+              },
+              {
+                fileName: dirFile,
+                source: `
+                  // A non-exported interface used as a type bound for a generic directive causes
+                  // an inline type constructor to be required.
+                  interface NotExported {}
+                  export class TestDir<T extends NotExported> {}`,
+                templates: {},
+              }
+            ],
+            {inlining: false});
+        const sf = getSourceFileOrError(program, fileName);
+        const diags = templateTypeChecker.getDiagnosticsForFile(sf);
+        expect(diags.length).toBe(1);
+        expect(diags[0].code).toBe(ngErrorCode(ErrorCode.INLINE_TYPE_CTOR_REQUIRED));
+
+        // The relatedInformation of the diagnostic should point to the directive which required the
+        // inline type constructor.
+        expect(diags[0].relatedInformation).not.toBeUndefined();
+        expect(diags[0].relatedInformation!.length).toBe(1);
+        expect(diags[0].relatedInformation![0].file).not.toBeUndefined();
+        expect(absoluteFromSourceFile(diags[0].relatedInformation![0].file!)).toBe(dirFile);
+      });
     });
   });
 });
