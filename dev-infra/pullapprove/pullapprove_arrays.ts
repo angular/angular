f8a82ced9364fe84a39c6165ed73a54a730b3077
Andrew Scott <atscott01@gmail.com>
2020-07-20 11:53:26 -0700

    refactor(dev-infra): Add support for groups in the conditions evaluator
    Conditions can refer to the groups array that is a list of the preceding groups.
    This commit adds support to the verification for those conditions.
    
    This commit also adds some tests to the parsing and condition matching
    to ensure everything works as expected.

---
+++ dev-infra/pullapprove/pullapprove_arrays.ts
@@ -0,0 +1,86 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import {PullApproveGroup} from './group';
+import {getOrCreateGlob} from './utils';
+
+export class PullApproveGroupStateDependencyError extends Error {
+  constructor(message?: string) {
+    super(message);
+    // see: typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html
+    Object.setPrototypeOf(this, new.target.prototype);      // restore prototype chain
+    this.name = PullApproveGroupStateDependencyError.name;  // stack traces display correctly now
+  }
+}
+
+/**
+ * Superset of a native array. The superset provides methods which mimic the
+ * list data structure used in PullApprove for files in conditions.
+ */
+export class PullApproveGroupArray extends Array<PullApproveGroup> {
+  constructor(...elements: PullApproveGroup[]) {
+    super(...elements);
+
+    // Set the prototype explicitly because in ES5, the prototype is accidentally
+    // lost due to a limitation in down-leveling.
+    // https://github.com/Microsoft/TypeScript/wiki/FAQ#why-doesnt-extending-built-ins-like-error-array-and-map-work.
+    Object.setPrototypeOf(this, PullApproveGroupArray.prototype);
+  }
+
+  get pending() {
+    throw new PullApproveGroupStateDependencyError();
+  }
+
+  get active() {
+    throw new PullApproveGroupStateDependencyError();
+  }
+
+  get inactive() {
+    throw new PullApproveGroupStateDependencyError();
+  }
+
+  get rejected() {
+    throw new PullApproveGroupStateDependencyError();
+  }
+
+  get names() {
+    return this.map(g => g.groupName);
+  }
+
+  include(pattern: string): PullApproveGroupArray {
+    return new PullApproveGroupArray(...this.filter(g => g.groupName.match(pattern)));
+  }
+
+  exclude(pattern: string): PullApproveGroupArray {
+    return new PullApproveGroupArray(...this.filter(g => !g.groupName.match(pattern)));
+  }
+}
+
+/**
+ * Superset of a native array. The superset provides methods which mimic the
+ * list data structure used in PullApprove for files in conditions.
+ */
+export class PullApproveArray extends Array<string> {
+  constructor(...elements: string[]) {
+    super(...elements);
+
+    // Set the prototype explicitly because in ES5, the prototype is accidentally
+    // lost due to a limitation in down-leveling.
+    // https://github.com/Microsoft/TypeScript/wiki/FAQ#why-doesnt-extending-built-ins-like-error-array-and-map-work.
+    Object.setPrototypeOf(this, PullApproveArray.prototype);
+  }
+
+  /** Returns a new array which only includes files that match the given pattern. */
+  include(pattern: string): PullApproveArray {
+    return new PullApproveArray(...this.filter(s => getOrCreateGlob(pattern).match(s)));
+  }
+
+  /** Returns a new array which only includes files that did not match the given pattern. */
+  exclude(pattern: string): PullApproveArray {
+    return new PullApproveArray(...this.filter(s => !getOrCreateGlob(pattern).match(s)));
+  }
+}
