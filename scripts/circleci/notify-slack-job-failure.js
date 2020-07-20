14758054c48389527053423a4e8eef16e59bdb6f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-07-20 16:29:16 +0200

    ci: report size-test failures to separate slack channel (#20033)
    No longer reports size-test failures to the #components-ci-failures
    Slack channel. This channel should only receive messages for actual
    job failures. The size tests do not cause CI to turn red, so they
    should be handled in a separate channel.

---
+++ scripts/circleci/notify-slack-job-failure.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+/**
+ * Script that notifies Slack about the currently failing job. This script
+ * will be a noop when running for forked builds (i.e. PRs).
+ */
+
+if (process.env.CIRCLE_PR_NUMBER) {
+  console.info('Skipping notifications for pull requests.');
+  process.exit(0);
+}
+
+const {echo, set} = require('shelljs');
+const {
+  CIRCLE_JOB: jobName,
+  CIRCLE_BRANCH: branchName,
+  CIRCLE_BUILD_URL: jobUrl,
+  SLACK_COMPONENTS_CI_FAILURES_WEBHOOK_URL: webhookUrl,
+} = process.env;
+
+const text = `\`${jobName}\` failed in branch: ${branchName}: ${jobUrl}`;
+const payload = {text};
+const [channelName] = process.argv.slice(2);
+
+set('-e');
+
+// If an explicit channel has been specified, override the default
+// webhook channel to the specified one.
+if (channelName !== undefined) {
+  payload.channel = channelName;
+}
+
+echo(JSON.stringify(payload, null, 2))
+  .exec(`curl -d@- -H "Content-Type: application/json" ${webhookUrl}`);
+console.info('Notified Slack about job failure.');
