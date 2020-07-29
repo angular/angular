398118f708939dd9345c1fb3aa6b46412796f532
Joey Perrott <josephperrott@gmail.com>
2020-07-29 14:19:15 -0700

    feat(dev-infra): create a wizard for building commit messages (#38457)
    Creates a wizard to walk through creating a commit message in the correct
    template for commit messages in Angular repositories.
    
    PR Close #38457

--- dev-infra/commit-message/config.ts
+++ dev-infra/commit-message/config.ts
@@ -39,36 +39,56 @@ export enum ScopeRequirement {
 
 /** A commit type */
 export interface CommitType {
+  description: string;
+  name: string;
   scope: ScopeRequirement;
 }
 
 /** The valid commit types for Angular commit messages. */
 export const COMMIT_TYPES: {[key: string]: CommitType} = {
   build: {
+    name: 'build',
+    description: 'Changes to local repository build system and tooling',
     scope: ScopeRequirement.Forbidden,
   },
   ci: {
+    name: 'ci',
+    description: 'Changes to CI configuration and CI specific tooling',
     scope: ScopeRequirement.Forbidden,
   },
   docs: {
+    name: 'docs',
+    description: 'Changes which exclusively affects documentation.',
     scope: ScopeRequirement.Optional,
   },
   feat: {
+    name: 'feat',
+    description: 'Creates a new feature',
     scope: ScopeRequirement.Required,
   },
   fix: {
+    name: 'fix',
+    description: 'Fixes a previously discovered failure/bug',
     scope: ScopeRequirement.Required,
   },
   perf: {
+    name: 'perf',
+    description: 'Improves performance without any change in functionality or API',
     scope: ScopeRequirement.Required,
   },
   refactor: {
+    name: 'refactor',
+    description: 'Refactor without any change in functionality or API (includes style changes)',
     scope: ScopeRequirement.Required,
   },
   release: {
+    name: 'release',
+    description: 'A release point in the repository',
     scope: ScopeRequirement.Forbidden,
   },
   test: {
+    name: 'test',
+    description: 'Improvements or corrections made to the project\'s test suite',
     scope: ScopeRequirement.Required,
   },
 };
