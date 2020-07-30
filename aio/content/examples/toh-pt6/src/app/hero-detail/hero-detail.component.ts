e97e3a49a2431b4b1db2ab1044264dcad152c43d
Sergey Falinsky <sergey@falinsky.com>
2020-07-31 01:30:57 +0300

    docs: remove unused Input decorator
    In the part "5. Add In-app Navigation" of the tutorial it was already removed

--- aio/content/examples/toh-pt6/src/app/hero-detail/hero-detail.component.ts
+++ aio/content/examples/toh-pt6/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -11,7 +11,7 @@ import { HeroService }  from '../hero.service';
   styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
   constructor(
     private route: ActivatedRoute,
