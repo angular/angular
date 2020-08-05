df40a69bd7ef3fbeaa0e822ff1383bf061e072aa
Wagner Maciel <wagnermaciel@google.com>
2020-08-05 10:59:20 -0700

    test(mdc-button): add performance tests for mdc-button (#20032)
    * test(mdc-button): add performance tests for mdc-button
    
    * fix(mdc-indigo-pink): add mat-core and mat-core-theme to indigo-pink
    
    * fixup! fix(mdc-indigo-pink): add mat-core and mat-core-theme to indigo-pink
    
    * fixup! test(mdc-button): add performance tests for mdc-button

---
+++ test/benchmarks/mdc/button/app.module.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Component, NgModule, ViewEncapsulation} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material-experimental/mdc-button';
+
+/** component: mdc-raised-button */
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <button id="show" (click)="show()">Show</button>
+    <button id="hide" (click)="hide()">Hide</button>
+    <button *ngIf="isVisible" mat-raised-button>Basic</button>
+  `,
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['//src/material-experimental/mdc-theming/prebuilt/indigo-pink.css'],
+})
+export class ButtonBenchmarkApp {
+  isChecked = false;
+  isVisible = false;
+
+  show() { this.isVisible = true; }
+  hide() { this.isVisible = false; }
+}
+
+
+@NgModule({
+  declarations: [ButtonBenchmarkApp],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [ButtonBenchmarkApp],
+})
+export class AppModule {}
---
+++ test/benchmarks/mdc/button/button.perf-spec.ts
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {$, browser} from 'protractor';
+import {runBenchmark} from '@angular/dev-infra-private/benchmark/driver-utilities';
+
+describe('button performance benchmarks', () => {
+  beforeAll(() => {
+    browser.rootEl = '#root';
+  });
+
+  it('renders a basic raised button', async() => {
+    await runBenchmark({
+      id: 'button-render',
+      url: '',
+      ignoreBrowserSynchronization: true,
+      params: [],
+      prepare: async () => await $('#hide').click(),
+      work: async () => await $('#show').click(),
+    });
+  });
+
+  it('clicks a basic raised button', async() => {
+    await runBenchmark({
+      id: 'button-click',
+      url: '',
+      ignoreBrowserSynchronization: true,
+      params: [],
+      setup: async () => await $('#show').click(),
+      work: async () => await $('.mat-mdc-raised-button').click(),
+    });
+  });
+});
