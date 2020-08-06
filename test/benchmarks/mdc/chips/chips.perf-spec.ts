cd433a41f7815cf35af0127ae0c84577916e4ca5
Wagner Maciel <wagnermaciel@google.com>
2020-08-06 10:20:08 -0700

    test(mdc-chips): add performance tests for mdc-chips (#20131)
    * fix(mdc-indigo-pink): add mat-core and mat-core-theme to indigo-pink
    
    * fixup! fix(mdc-indigo-pink): add mat-core and mat-core-theme to indigo-pink
    
    * test(mdc-chips): add performance tests for mdc-chips
    
    * fix(mdc-chips-benchmark): remove unnecessary providers
    
    * fix(mdc-chips-benchmark): add tests for grid and listbox
    
    * fixup! fix(mdc-chips-benchmark): add tests for grid and listbox

---
+++ test/benchmarks/mdc/chips/chips.perf-spec.ts
@@ -0,0 +1,54 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {$, browser} from 'protractor';
+import {runBenchmark} from '@angular/dev-infra-private/benchmark/driver-utilities';
+
+async function runRenderBenchmark(testId: string, showBtnId: string) {
+  return runBenchmark({
+    id: testId,
+    url: '',
+    ignoreBrowserSynchronization: true,
+    params: [],
+    prepare: async () => await $('#hide').click(),
+    work: async () => await $(showBtnId).click(),
+  });
+}
+
+describe('chip performance benchmarks', () => {
+  beforeAll(() => {
+    browser.rootEl = '#root';
+  });
+
+  it('renders a single chip', async() => {
+    await runRenderBenchmark('single-chip-render', '#show-single-chip');
+  });
+
+  it('renders a set', async() => {
+    await runRenderBenchmark('chip-set-render', '#show-chip-set');
+  });
+
+  it('renders a grid', async() => {
+    await runRenderBenchmark('chip-grid-render', '#show-chip-grid');
+  });
+
+  it('renders a listbox', async() => {
+    await runRenderBenchmark('chip-listbox-render', '#show-chip-listbox');
+  });
+
+  it('clicks a chip', async() => {
+    await runBenchmark({
+      id: 'chip-click',
+      url: '',
+      ignoreBrowserSynchronization: true,
+      params: [],
+      setup: async() => await $('#show-single-chip').click(),
+      work: async () => await $('#single-chip').click(),
+    });
+  });
+});
