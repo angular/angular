25ce323fbceb78392d8468ff6eab67b0b8d717b1
Wagner Maciel <wagnermaciel@google.com>
2020-08-06 10:20:48 -0700

    test(mdc-form-field): add performance tests for mdc-form-field (#20135)
    * test(mdc-button): add performance tests for mdc-button
    
    * fix(mdc-indigo-pink): add mat-core and mat-core-theme to indigo-pink
    
    * fixup! fix(mdc-indigo-pink): add mat-core and mat-core-theme to indigo-pink
    
    * fixup! test(mdc-button): add performance tests for mdc-button
    
    * test(mdc-form-field): add performance tests for mdc-form-field
    
    * fix(mdc-form-field-benchmark): remove unnecessary provider

---
+++ test/benchmarks/mdc/form-field/form-field.perf-spec.ts
@@ -0,0 +1,35 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {$, browser} from 'protractor';
+import {runBenchmark} from '@angular/dev-infra-private/benchmark/driver-utilities';
+
+function runFormFieldRenderBenchmark(testId: string, showBtnId: string) {
+  return runBenchmark({
+    id: testId,
+    url: '',
+    ignoreBrowserSynchronization: true,
+    params: [],
+    prepare: async () => await $('#hide').click(),
+    work: async () => await $(showBtnId).click()
+  });
+}
+
+describe('form field performance benchmarks', () => {
+  beforeAll(() => {
+    browser.rootEl = '#root';
+  });
+
+  it('renders an input in a form field', async() => {
+    await runFormFieldRenderBenchmark('input-form-field-render', '#show-input');
+  });
+
+  it('renders an textarea in a form field', async() => {
+    await runFormFieldRenderBenchmark('textarea-form-field-render', '#show-textarea');
+  });
+});
