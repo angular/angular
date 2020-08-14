9712bae366e207d4c78dc21a37ff698094b5fd23
Joey Perrott <josephperrott@gmail.com>
2020-08-14 16:49:07 -0700

    feat(dev-infra): tooling to check out pending PR
    Creates a tool within ng-dev to checkout a pending PR from the upstream repository.  This automates
    an action that many developers on the Angular team need to do periodically in the process of testing
    and reviewing incoming PRs.
    
    Example usage:
      ng-dev pr checkout <pr-number>

--- dev-infra/pr/common/checkout-pr.ts
+++ dev-infra/pr/common/checkout-pr.ts
@@ -9,12 +9,11 @@
 import {types as graphQLTypes} from 'typed-graphqlify';
 import {URL} from 'url';
 
-import {getConfig, NgDevConfig} from '../../utils/config';
-import {error, info, promptConfirm} from '../../utils/console';
+import {info} from '../../utils/console';
 import {GitClient} from '../../utils/git';
 import {getPr} from '../../utils/github';
 
-/* GraphQL schema for the response body for each pending PR. */
+/* GraphQL schema for the response body for a pending PR. */
 const PR_SCHEMA = {
   state: graphQLTypes.string,
   maintainerCanModify: graphQLTypes.boolean,
@@ -36,17 +35,39 @@ const PR_SCHEMA = {
   },
 };
 
+export class HasLocalChangesError extends Error {
+  constructor(msg: string) {
+    super(msg);
+  }
+}
+
+export class MaintainerModifyAccessError extends Error {
+  constructor(msg: string) {
+    super(msg);
+  }
+}
+
+/** Options for checking out a PR */
+export interface PrCheckoutOptions {
+  /** The local name for the branch when checking out a pr. */
+  branchName?: string;
+  /** Whether the PR should be checked out if the maintainer cannot modify. */
+  allowIfMaintainerCannotModify?: boolean;
+}
+
 /**
  * Rebase the provided PR onto its merge target branch, and push up the resulting
  * commit to the PRs repository.
  */
-export async function rebasePr(
-    prNumber: number, githubToken: string, config: Pick<NgDevConfig, 'github'> = getConfig()) {
+export async function checkOutPrLocally(
+    prNumber: number, githubToken: string, opts: PrCheckoutOptions = {}) {
+  /** Authenticated Git client for git and Github interactions. */
   const git = new GitClient(githubToken);
-  // TODO: Rely on a common assertNoLocalChanges function.
+
+  // In order to preserve local changes, checkouts cannot occur if local changes are present in the
+  // git environment.  Checked before retrieving the PR to fail fast.
   if (git.hasLocalChanges()) {
-    error('Cannot perform rebase of PR with local changes.');
-    process.exit(1);
+    throw new HasLocalChangesError('Cannot checkout requested PR because of local changes in git');
   }
 
   /**
@@ -54,81 +75,52 @@ export async function rebasePr(
    * any Git operations that may change the working branch.
    */
   const previousBranchOrRevision = git.getCurrentBranchOrRevision();
-  /* Get the PR information from Github. */
+  /* The PR information from Github. */
   const pr = await getPr(PR_SCHEMA, prNumber, git);
-
+  /** The branch name of the PR from the repository the PR came from. */
   const headRefName = pr.headRef.name;
-  const baseRefName = pr.baseRef.name;
+  /** The full ref for the repository and branch the PR came from. */
   const fullHeadRef = `${pr.headRef.repository.nameWithOwner}:${headRefName}`;
-  const fullBaseRef = `${pr.baseRef.repository.nameWithOwner}:${baseRefName}`;
+  /** The full URL path of the repository the PR came from with github token as authentication. */
   const headRefUrl = addAuthenticationToUrl(pr.headRef.repository.url, githubToken);
-  const baseRefUrl = addAuthenticationToUrl(pr.baseRef.repository.url, githubToken);
-
   // Note: Since we use a detached head for rebasing the PR and therefore do not have
   // remote-tracking branches configured, we need to set our expected ref and SHA. This
   // allows us to use `--force-with-lease` for the detached head while ensuring that we
   // never accidentally override upstream changes that have been pushed in the meanwhile.
   // See:
   // https://git-scm.com/docs/git-push#Documentation/git-push.txt---force-with-leaseltrefnamegtltexpectgt
+  /** Flag for a force push with leage back to upstream. */
   const forceWithLeaseFlag = `--force-with-lease=${headRefName}:${pr.headRefOid}`;
 
   // If the PR does not allow maintainers to modify it, exit as the rebased PR cannot
   // be pushed up.
-  if (!pr.maintainerCanModify && !pr.viewerDidAuthor) {
-    error(
-        `Cannot rebase as you did not author the PR and the PR does not allow maintainers` +
-        `to modify the PR`);
-    process.exit(1);
+  if (!pr.maintainerCanModify && !pr.viewerDidAuthor && !opts.allowIfMaintainerCannotModify) {
+    throw new MaintainerModifyAccessError('PR is not set to allow maintainers to modify the PR');
   }
 
   try {
     // Fetch the branch at the commit of the PR, and check it out in a detached state.
     info(`Checking out PR #${prNumber} from ${fullHeadRef}`);
     git.run(['fetch', headRefUrl, headRefName]);
-    git.run(['checkout', '--detach', 'FETCH_HEAD']);
-
-    // Fetch the PRs target branch and rebase onto it.
-    info(`Fetching ${fullBaseRef} to rebase #${prNumber} on`);
-    git.run(['fetch', baseRefUrl, baseRefName]);
-    info(`Attempting to rebase PR #${prNumber} on ${fullBaseRef}`);
-    const rebaseResult = git.runGraceful(['rebase', 'FETCH_HEAD']);
-
-    // If the rebase was clean, push the rebased PR up to the authors fork.
-    if (rebaseResult.status === 0) {
-      info(`Rebase was able to complete automatically without conflicts`);
-      info(`Pushing rebased PR #${prNumber} to ${fullHeadRef}`);
-      git.run(['push', headRefUrl, `HEAD:${headRefName}`, forceWithLeaseFlag]);
-      info(`Rebased and updated PR #${prNumber}`);
-      cleanUpGitState();
-      process.exit(0);
+    if (opts.branchName) {
+      git.run(['checkout', '-b', opts.branchName, 'FETCH_HEAD']);
+    } else {
+      git.run(['checkout', '--detach', 'FETCH_HEAD']);
     }
   } catch (err) {
-    error(err.message);
     cleanUpGitState();
-    process.exit(1);
+    throw Error('git didnt work');
   }
 
-  // On automatic rebase failures, prompt to choose if the rebase should be continued
-  // manually or aborted now.
-  info(`Rebase was unable to complete automatically without conflicts.`);
-  // If the command is run in a non-CI environment, prompt to format the files immediately.
-  const continueRebase =
-      process.env['CI'] === undefined && await promptConfirm('Manually complete rebase?');
-
-  if (continueRebase) {
-    info(`After manually completing rebase, run the following command to update PR #${prNumber}:`);
-    info(` $ git push ${pr.headRef.repository.url} HEAD:${headRefName} ${forceWithLeaseFlag}`);
-    info();
-    info(`To abort the rebase and return to the state of the repository before this command`);
-    info(`run the following command:`);
-    info(` $ git rebase --abort && git reset --hard && git checkout ${previousBranchOrRevision}`);
-    process.exit(1);
-  } else {
-    info(`Cleaning up git state, and restoring previous state.`);
-  }
-
-  cleanUpGitState();
-  process.exit(1);
+  return {
+    pushToUpstream: () => {
+      git.run(['push', headRefUrl, `HEAD:${headRefName}`, forceWithLeaseFlag]);
+      cleanUpGitState();
+    },
+    reset: () => {
+      cleanUpGitState();
+    }
+  };
 
   /** Reset git back to the original branch. */
   function cleanUpGitState() {
@@ -138,6 +130,10 @@ export async function rebasePr(
     git.runGraceful(['reset', '--hard'], {stdio: 'ignore'});
     // Checkout the original branch from before the run began.
     git.runGraceful(['checkout', previousBranchOrRevision], {stdio: 'ignore'});
+    // If a named branch was created, delete the branch.
+    if (opts.branchName) {
+      git.runGraceful(['branch', '-D', opts.branchName]);
+    }
   }
 }
 
