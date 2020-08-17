3bda78e69087e804357c917bc8d070c2cf1aa611
Joey Perrott <josephperrott@gmail.com>
2020-08-17 15:40:40 -0700

    refactor(dev-infra): update yargs and typings for yargs
    Updating yargs and typings for the updated yargs module.

--- dev-infra/pr/merge/cli.ts
+++ dev-infra/pr/merge/cli.ts
@@ -12,17 +12,26 @@ import {error, red, yellow} from '../../utils/console';
 
 import {GITHUB_TOKEN_GENERATE_URL, mergePullRequest} from './index';
 
+/** The options available to the merge command via CLI. */
+export interface MergeCommandOptions {
+  'github-token'?: string;
+  'pr-number': number;
+}
+
 /** Builds the options for the merge command. */
-export function buildMergeCommand(yargs: Argv) {
-  return yargs.help().strict().option('github-token', {
-    type: 'string',
-    description: 'Github token. If not set, token is retrieved from the environment variables.'
-  });
+export function buildMergeCommand(yargs: Argv): Argv<MergeCommandOptions> {
+  return yargs.help()
+      .strict()
+      .positional('pr-number', {demandOption: true, type: 'number'})
+      .option('github-token', {
+        type: 'string',
+        description: 'Github token. If not set, token is retrieved from the environment variables.'
+      });
 }
 
 /** Handles the merge command. i.e. performs the merge of a specified pull request. */
-export async function handleMergeCommand(args: Arguments) {
-  const githubToken = args.githubToken || process.env.GITHUB_TOKEN || process.env.TOKEN;
+export async function handleMergeCommand(args: Arguments<MergeCommandOptions>) {
+  const githubToken = args['github-token'] || process.env.GITHUB_TOKEN || process.env.TOKEN;
   if (!githubToken) {
     error(red('No Github token set. Please set the `GITHUB_TOKEN` environment variable.'));
     error(red('Alternatively, pass the `--github-token` command line flag.'));
@@ -30,5 +39,5 @@ export async function handleMergeCommand(args: Arguments) {
     process.exit(1);
   }
 
-  await mergePullRequest(args.prNumber, githubToken);
+  await mergePullRequest(args['pr-number'], githubToken);
 }
--- dev-infra/pr/rebase/cli.ts
+++ dev-infra/pr/rebase/cli.ts
@@ -15,17 +15,26 @@ import {rebasePr} from './index';
 /** URL to the Github page where personal access tokens can be generated. */
 export const GITHUB_TOKEN_GENERATE_URL = `https://github.com/settings/tokens`;
 
-/** Builds the rebase pull request command. */
-export function buildRebaseCommand(yargs: Argv) {
-  return yargs.option('github-token', {
-    type: 'string',
-    description: 'Github token. If not set, token is retrieved from the environment variables.'
-  });
+/** The options available to the rebase command via CLI. */
+export interface RebaseCommandOptions {
+  'github-token'?: string;
+  prNumber: number;
 }
 
+/** Builds the rebase pull request command. */
+export function buildRebaseCommand(yargs: Argv): Argv<RebaseCommandOptions> {
+  return yargs
+      .option('github-token', {
+        type: 'string',
+        description: 'Github token. If not set, token is retrieved from the environment variables.'
+      })
+      .positional('prNumber', {type: 'number', demandOption: true});
+}
+
+
 /** Handles the rebase pull request command. */
-export async function handleRebaseCommand(args: Arguments) {
-  const githubToken = args.githubToken || process.env.GITHUB_TOKEN || process.env.TOKEN;
+export async function handleRebaseCommand(args: Arguments<RebaseCommandOptions>) {
+  const githubToken = args['github-token'] || process.env.GITHUB_TOKEN || process.env.TOKEN;
   if (!githubToken) {
     error('No Github token set. Please set the `GITHUB_TOKEN` environment variable.');
     error('Alternatively, pass the `--github-token` command line flag.');
