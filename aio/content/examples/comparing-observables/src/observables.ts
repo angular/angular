cc8ad7ba11ce530f73a43a70dd9a7eb7705bd21b
Sonu Kapoor <sonukapoor@gmail.com>
2020-08-22 13:16:16 +0300

    test(docs-infra): add missing tests for obserables and promises
    This commit adds missing tests for obserables and promises which
    are both stand-alone mini-apps.

--- aio/content/examples/comparing-observables/src/observables.ts
+++ aio/content/examples/comparing-observables/src/observables.ts
@@ -1,40 +1,72 @@
-import { map } from 'rxjs/operators';
+// #docplaster
+
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-// #docregion observable
+export function docRegionObservable(console: Console) {
+  // #docregion observable
 
-// declare a publishing operation
-const observable = new Observable<number>(observer => {
-  // Subscriber fn...
-});
+  // declare a publishing operation
+  const observable = new Observable<number>(observer => {
+    // Subscriber fn...
+    // #enddocregion observable
+    // The below code is used for unit testing only
+    observer.next(2);
+    // #docregion observable
+  });
 
-// initiate execution
-observable.subscribe(() => {
-  // observer handles notifications
-});
+  // initiate execution
+  observable.subscribe(value => {
+    // observer handles notifications
+    // #enddocregion observable
+    // The below code is used for unit testing only
+    console.log(value);
+    // #docregion observable
+  });
 
-// #enddocregion observable
+  // #enddocregion observable
+  return observable;
+}
 
-// #docregion unsubscribe
+export function docRegionUnsubscribe() {
+  const observable = new Observable<number>(() => {
+    // Subscriber fn...
+  });
+  // #docregion unsubscribe
 
-const subscription = observable.subscribe(() => {
-  // observer handles notifications
-});
+  const subscription = observable.subscribe(() => {
+    // observer handles notifications
+  });
 
-subscription.unsubscribe();
+  subscription.unsubscribe();
 
-// #enddocregion unsubscribe
+  // #enddocregion unsubscribe
+  return subscription;
+}
 
-// #docregion error
+export function docRegionError() {
+  const observable = new Observable<number>(() => {
+    // Subscriber fn...
+  });
 
-observable.subscribe(() => {
-  throw Error('my error');
-});
+  // #docregion error
+  observable.subscribe(() => {
+    throw new Error('my error');
+  });
+  // #enddocregion error
+}
 
-// #enddocregion error
+export function docRegionChain() {
+  let observable = new Observable<number>(observer => {
+    // Subscriber fn...
+    observer.next(2);
+  });
 
-// #docregion chain
+  observable =
+  // #docregion chain
 
-observable.pipe(map(v => 2 * v));
+  observable.pipe(map(v => 2 * v));
 
-// #enddocregion chain
+  // #enddocregion chain
+  return observable;
+}
