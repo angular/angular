184f7781a1462b28a58025ee9b6c8b8c48b5ec7a
mgechev <mgechev@gmail.com>
2020-08-27 18:49:41 +0300

    feat(devtools): add demo app which does not use zones

---
+++ projects/demo-no-zone/e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo(): Promise<any> {
+    return browser.get(browser.baseUrl) as Promise<any>;
+  }
+
+  getTitleText(): Promise<any> {
+    return element(by.css('app-root h1')).getText() as Promise<string>;
+  }
+}
