4f28192d62b59a4a45f7f27890e9fb356e2cf885
Joey Perrott <josephperrott@gmail.com>
2020-08-28 11:45:01 -0700

    refactor(dev-infra): use a mixin to require a `github-token` for an ng-dev command (#38630)
    Creates a mixin for requiring a github token to be provided to a command.  This mixin
    allows for a centralized management of the requirement and handling of the github-token.
    
    PR Close #38630

--- dev-infra/pr/checkout/cli.ts
+++ dev-infra/pr/checkout/cli.ts
@@ -8,35 +8,21 @@
 
 import {Arguments, Argv, CommandModule} from 'yargs';
 
-import {error} from '../../utils/console';
+import {addGithubTokenFlag} from '../../utils/yargs';
 import {checkOutPullRequestLocally} from '../common/checkout-pr';
 
 export interface CheckoutOptions {
   prNumber: number;
-  'github-token'?: string;
+  githubToken: string;
 }
-
-/** URL to the Github page where personal access tokens can be generated. */
-export const GITHUB_TOKEN_GENERATE_URL = `https://github.com/settings/tokens`;
 
 /** Builds the checkout pull request command. */
 function builder(yargs: Argv) {
-  return yargs.positional('prNumber', {type: 'number', demandOption: true}).option('github-token', {
-    type: 'string',
-    description: 'Github token. If not set, token is retrieved from the environment variables.'
-  });
+  return addGithubTokenFlag(yargs).positional('prNumber', {type: 'number', demandOption: true});
 }
 
 /** Handles the checkout pull request command. */
-async function handler({prNumber, 'github-token': token}: Arguments<CheckoutOptions>) {
-  const githubToken = token || process.env.GITHUB_TOKEN || process.env.TOKEN;
-  if (!githubToken) {
-    error('No Github token set. Please set the `GITHUB_TOKEN` environment variable.');
-    error('Alternatively, pass the `--github-token` command line flag.');
-    error(`You can generate a token here: ${GITHUB_TOKEN_GENERATE_URL}`);
-    process.exitCode = 1;
-    return;
-  }
+async function handler({prNumber, githubToken}: Arguments<CheckoutOptions>) {
   const prCheckoutOptions = {allowIfMaintainerCannotModify: true, branchName: `pr-${prNumber}`};
   await checkOutPullRequestLocally(prNumber, githubToken, prCheckoutOptions);
 }
