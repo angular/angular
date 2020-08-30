478b122504f4e30a47eedaf2c9b292dab933181a
Juan José Arboleda <soyjuanarbol@gmail.com>
2020-08-30 00:35:22 -0500

    refactor(forms): remove extra space in error message
    Remove extra whitespace at package/forms/model.ts error messages

--- packages/forms/src/model.ts
+++ packages/forms/src/model.ts
@@ -1659,7 +1659,7 @@ export class FormGroup extends AbstractControl {
   _throwIfControlMissing(name: string): void {
     if (!Object.keys(this.controls).length) {
       throw new Error(`
-        There are no form controls registered with this group yet.  If you're using ngModel,
+        There are no form controls registered with this group yet. If you're using ngModel,
         you may want to check next tick (e.g. use setTimeout).
       `);
     }
@@ -2101,7 +2101,7 @@ export class FormArray extends AbstractControl {
   _throwIfControlMissing(index: number): void {
     if (!this.controls.length) {
       throw new Error(`
-        There are no form controls registered with this array yet.  If you're using ngModel,
+        There are no form controls registered with this array yet. If you're using ngModel,
         you may want to check next tick (e.g. use setTimeout).
       `);
     }
