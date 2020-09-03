5672aba2f974e00b167e2c9cb616e4c4027ec3b6
Joey Perrott <josephperrott@gmail.com>
2020-09-03 14:54:31 -0700

    refactor(dev-infra): update commit message validation to return validation result (#38703)
    Previously, the validateCommitMessage function ran validation and logged the results.
    The validateCommitMessage function now returns an object containing the validation
    results and the cli action functions are instead responsible for logging the results.
    
    This is being done as a prefactor for a change which allows for commit message
    hook validation to be either a blocking error or a warning.
    
    PR Close #38703

--- dev-infra/commit-message/validate-file.ts
+++ dev-infra/commit-message/validate-file.ts
@@ -9,19 +9,25 @@ import {readFileSync} from 'fs';
 import {resolve} from 'path';
 
 import {getRepoBaseDir} from '../utils/config';
-import {info} from '../utils/console';
+import {error, green, info, red} from '../utils/console';
 
 import {deleteCommitMessageDraft, saveCommitMessageDraft} from './commit-message-draft';
-import {validateCommitMessage} from './validate';
+import {printValidationErrors, validateCommitMessage} from './validate';
 
 /** Validate commit message at the provided file path. */
 export function validateFile(filePath: string) {
   const commitMessage = readFileSync(resolve(getRepoBaseDir(), filePath), 'utf8');
-  if (validateCommitMessage(commitMessage)) {
-    info('√  Valid commit message');
+  const {valid, errors} = validateCommitMessage(commitMessage);
+  if (valid) {
+    info(`${green('√')}  Valid commit message`);
     deleteCommitMessageDraft(filePath);
     return;
   }
+
+  error(`${red('✘')}  Invalid commit message`);
+  printValidationErrors(errors);
+  error('Aborting commit attempt due to invalid commit message.');
+
   // On all invalid commit messages, the commit message should be saved as a draft to be
   // restored on the next commit attempt.
   saveCommitMessageDraft(filePath, commitMessage);
