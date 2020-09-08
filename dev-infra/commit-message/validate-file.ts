898ef085cd24fc91161063e9c00a954f0b774348
Joey Perrott <josephperrott@gmail.com>
2020-09-08 14:38:04 -0700

    feat(dev-infra): allow local ng-dev configuration to error on invalid commit messages
    As part of the commit message conformance check, local commit message checks are
    made to be warnings rather than failures. An additional local option is also in
    place to allow for the commit message validation failures to be considered errors
    instead.

--- dev-infra/commit-message/validate-file.ts
+++ dev-infra/commit-message/validate-file.ts
@@ -8,8 +8,8 @@
 import {readFileSync} from 'fs';
 import {resolve} from 'path';
 
-import {getRepoBaseDir} from '../utils/config';
-import {error, green, info, red} from '../utils/console';
+import {getRepoBaseDir, getUserConfig} from '../utils/config';
+import {error, green, info, log, red, yellow} from '../utils/console';
 
 import {deleteCommitMessageDraft, saveCommitMessageDraft} from './commit-message-draft';
 import {printValidationErrors, validateCommitMessage} from './validate';
@@ -21,16 +21,33 @@ export function validateFile(filePath: string) {
   if (valid) {
     info(`${green('√')}  Valid commit message`);
     deleteCommitMessageDraft(filePath);
+    process.exitCode = 0;
     return;
   }
 
-  error(`${red('✘')}  Invalid commit message`);
-  printValidationErrors(errors);
-  error('Aborting commit attempt due to invalid commit message.');
+  /**
+   * Whether the validation failure should be treated as an error, otherwise treated as a warning.
+   *
+   * The tool running on CI is set to always run treating validation failures as an error.
+   */
+  const isErrorMode = !!getUserConfig().commitMessage?.errorOnInvalidMessage || !!process.env['CI'];
+  /** Function used to print to the console log. */
+  let printFn = isErrorMode ? error : log;
+
+  printFn(`${isErrorMode ? red('✘') : yellow('!')}  Invalid commit message`);
+  printValidationErrors(errors, printFn);
+  if (isErrorMode) {
+    printFn(red('Aborting commit attempt due to invalid commit message.'));
+    printFn(
+        red('Commit message aborted as failure rather than warning due to local configuration.'));
+  } else {
+    printFn(yellow('Before this commit can be merged into the upstream repository, it must be'));
+    printFn(yellow('amended to follow commit message guidelines.'));
+  }
 
   // On all invalid commit messages, the commit message should be saved as a draft to be
   // restored on the next commit attempt.
   saveCommitMessageDraft(filePath, commitMessage);
-  // If the validation did not return true, exit as a failure.
-  process.exit(1);
+  // Set the correct exit code based on if invalid commit message is an error.
+  process.exitCode = isErrorMode ? 1 : 0;
 }
