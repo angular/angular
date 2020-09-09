3f2145fca2603a6e19bd1f71fe028947f547f8db
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-09-09 14:53:13 +0200

    refactor(dev-infra): move existing env-stamp command into subfolder
    Moves the existing `ng-dev release env-stamp` command into a
    subfolder so that the staging/publish tool can have its own
    dedicated folder (without being polluted by the env-stamp logic).
    
    Every subcommand should be in its own folder.

--- dev-infra/release/stamping/env-stamp.ts
+++ dev-infra/release/stamping/env-stamp.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {exec as _exec} from '../utils/shelljs';
+import {exec as _exec} from '../../utils/shelljs';
 
 /**
  * Log the environment variables expected by bazel for stamping.
