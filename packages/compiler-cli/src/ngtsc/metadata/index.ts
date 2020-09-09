c54161098d708b01d7f5de852fe7693acb7b8db4
Andrew Scott <atscott@google.com>
2020-09-09 11:26:44 -0700

    Revert "perf(compiler-cli): optimize computation of type-check scope information (#38749)" (#38767)
    This reverts commit e156e29eddcc85b2197f3b052ed76dd132671401.
    internal failure:
    https://test.corp.google.com/ui#id=OCL:329948619:BASE:329967516:1599160428139:d63165ae
    
    PR Close #38767

--- packages/compiler-cli/src/ngtsc/metadata/index.ts
+++ packages/compiler-cli/src/ngtsc/metadata/index.ts
@@ -8,7 +8,6 @@
 
 export * from './src/api';
 export {DtsMetadataReader} from './src/dts';
-export {flattenInheritedDirectiveMetadata} from './src/inheritance';
 export {CompoundMetadataRegistry, LocalMetadataRegistry, InjectableClassRegistry} from './src/registry';
 export {extractDirectiveTypeCheckMeta, CompoundMetadataReader} from './src/util';
 export {BindingPropertyName, ClassPropertyMapping, ClassPropertyName, InputOrOutput} from './src/property_mapping';
