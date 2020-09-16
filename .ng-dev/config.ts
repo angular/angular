42be968d7f0682ca572ae79ca6168efb2d65a333
Joey Perrott <josephperrott.github@gmail.com>
2020-09-16 16:43:42 -0700

    ci: set up commit message conformance rules using ng-dev (#20580)

--- .ng-dev/config.ts
+++ .ng-dev/config.ts
@@ -1,9 +1,11 @@
 import {format} from './format';
 import {github} from './github';
 import {merge} from './merge';
+import {commitMessage} from './commit-message';
 
 module.exports = {
+  commitMessage,
+  format,
   github,
   merge,
-  format,
 };
