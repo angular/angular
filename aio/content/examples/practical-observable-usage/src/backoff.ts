8422633b8ab070d3321b7f9d557677773695e8aa
Sonu Kapoor <sonukapoor@gmail.com>
2020-09-19 12:41:59 -0400

    test(docs-infra): add missing unit tests for backoff example (#38896)
    This commit adds the missing unit test for the backoff example.
    
    PR Close #38896

--- aio/content/examples/practical-observable-usage/src/backoff.ts
+++ aio/content/examples/practical-observable-usage/src/backoff.ts
@@ -1,9 +1,10 @@
-// TODO: Add unit tests for this file.
+// #docplaster
+// #docregion
 import { of, pipe, range, throwError, timer, zip } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 import { map, mergeMap, retryWhen } from 'rxjs/operators';
 
-function backoff(maxTries, delay) {
+export function backoff(maxTries, delay) {
   return pipe(
     retryWhen(attempts =>
       zip(range(1, maxTries + 1), attempts).pipe(
@@ -15,10 +16,17 @@ function backoff(maxTries, delay) {
   );
 }
 
+// #enddocregion
+/*
+  This function declaration is necessary to ensure that it does not get called
+  when running the unit tests. It will not get rendered into the docs.
+  The indentation needs to start in the leftmost level position as well because of how
+  the docplaster combines the different regions together.
+*/
+function docRegionAjaxCall() {
+// #docregion
 ajax('/api/endpoint')
   .pipe(backoff(3, 250))
-  .subscribe(data => handleData(data));
-
-function handleData(data) {
-  // ...
+  .subscribe(function handleData(data) { /* ... */ });
+// #enddocregion
 }
