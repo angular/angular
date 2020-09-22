2967ad0c2e4ad951eada231214a2ccedd6c2f04c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-22 18:55:05 +0300

    docs(material/paginator): add explicit type for better formatting in docs (#20608)
    `MatPaginatorIntl.getRangeLabel` didn't have an explicit type which was causing
    dgeni to output the entire initializer.

--- src/material/paginator/paginator-intl.ts
+++ src/material/paginator/paginator-intl.ts
@@ -38,20 +38,21 @@ export class MatPaginatorIntl {
   lastPageLabel: string = 'Last page';
 
   /** A label for the range of items within the current page and the length of the whole list. */
-  getRangeLabel = (page: number, pageSize: number, length: number) => {
-    if (length == 0 || pageSize == 0) { return `0 of ${length}`; }
+  getRangeLabel: (page: number, pageSize: number, length: number) => string =
+    (page: number, pageSize: number, length: number) => {
+      if (length == 0 || pageSize == 0) { return `0 of ${length}`; }
 
-    length = Math.max(length, 0);
+      length = Math.max(length, 0);
 
-    const startIndex = page * pageSize;
+      const startIndex = page * pageSize;
 
-    // If the start index exceeds the list length, do not try and fix the end index to the end.
-    const endIndex = startIndex < length ?
-        Math.min(startIndex + pageSize, length) :
-        startIndex + pageSize;
+      // If the start index exceeds the list length, do not try and fix the end index to the end.
+      const endIndex = startIndex < length ?
+          Math.min(startIndex + pageSize, length) :
+          startIndex + pageSize;
 
-    return `${startIndex + 1} – ${endIndex} of ${length}`;
-  }
+      return `${startIndex + 1} – ${endIndex} of ${length}`;
+    }
 }
 
 /** @docs-private */
