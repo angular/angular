62a2fc898157d373de41129749068cfeda1d638f
Sonu Kapoor <sonukapoor@gmail.com>
2020-09-28 17:56:57 +0300

    fix(docs-infra): fix the retry-on-error example (#38905)
    Previously, the `retry` example did not work as intended. The `retry`
    operator was called before the exception occured, thus not retrying the
    `ajax` request.
    
    This commit moves the `retry` operator into the correct order to ensure
    that the failed request is retried.
    
    PR Close #38905

--- aio/content/examples/rx-library/src/retry-on-error.ts
+++ aio/content/examples/rx-library/src/retry-on-error.ts
@@ -8,13 +8,13 @@ import { ajax } from 'rxjs/ajax';
 import { map, retry, catchError } from 'rxjs/operators';
 
 const apiData = ajax('/api/data').pipe(
-  retry(3), // Retry up to 3 times before failing
   map(res => {
     if (!res.response) {
       throw new Error('Value expected!');
     }
     return res.response;
   }),
+  retry(3), // Retry up to 3 times before failing
   catchError(err => of([]))
 );
 
