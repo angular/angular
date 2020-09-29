d80c4890be1ab528a5caf8244477aa1f7370cb73
George Kalpakas <kalpakas.g@gmail.com>
2020-09-29 18:26:20 +0300

    build(docs-infra): remove the `only-arrow-functions` tslint rule to align with CLI (#39018)
    This commit removes the `only-arrow-functions: false` tslint rule to
    more closely align `tslint.json` with the one generated by the latest
    Angular CLI for new apps.
    
    PR Close #39018

--- aio/src/app/search/search.worker.ts
+++ aio/src/app/search/search.worker.ts
@@ -44,7 +44,7 @@ function handleMessage(message: { data: WebWorkerMessage }): void {
   const payload = message.data.payload;
   switch (type) {
     case 'load-index':
-      makeRequest(SEARCH_TERMS_URL, function(searchInfo: PageInfo[]) {
+      makeRequest(SEARCH_TERMS_URL, (searchInfo: PageInfo[]) => {
         index = createIndex(loadIndex(searchInfo));
         postMessage({ type, id, payload: true });
       });
@@ -94,7 +94,7 @@ function queryIndex(query: string): PageInfo[] {
         results = index.search(query + ' ' + titleQuery);
       }
       // Map the hits into info about each page to be returned as results
-      return results.map(function(hit) { return pages[hit.ref]; });
+      return results.map(hit => pages[hit.ref]);
     }
   } catch (e) {
     // If the search query cannot be parsed the index throws an error
