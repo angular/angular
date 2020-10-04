64a1026e3731c301cdbf83f57384568799bdd225
JiaLiPassion <JiaLi.Passion@gmail.com>
2020-10-05 06:54:20 +0900

    test(zone.js): add basic test for zone.js bundle size
    Zone.js bundle size impacts the `polyfills` size very much, so in this PR,
    the basic test for legacy bundle `zone.umd.js` and evergreen bundle `zone.js`.
    
    The script checks the unzipped and gzipped version of the bundles.

--- packages/zone.js/check-file-size.js
+++ packages/zone.js/check-file-size.js
@@ -6,23 +6,47 @@
  * found in the LICENSE file at https://angular.io/license
  */
 const fs = require('fs');
+const {createGzip} = require('zlib');
+const {pipeline} = require('stream');
+const {promisify} = require('util');
+const pipe = promisify(pipeline);
+const remove = promisify(fs.unlink);
 
-module.exports = function(config) {
+async function do_gzip(input, output) {
+  const gzip = createGzip();
+  const source = fs.createReadStream(input);
+  const destination = fs.createWriteStream(output);
+  await pipe(source, gzip, destination);
+}
+
+function checkSize(file, limit) {
+  try {
+    const stats = fs.statSync(file);
+    if (stats.size > limit) {
+      console.error(`file ${file} size over limit, limit is ${limit}, actual is ${stats.size}`);
+      return false;
+    }
+  } catch (err) {
+    console.error(`failed to get filesize: ${file}`);
+    return false;
+  }
+  return true;
+}
+
+const checker = function(config) {
   let chkResult = true;
-  config.targets.forEach(target => {
+  config.targets.forEach(async target => {
     if (target.checkTarget) {
-      try {
-        const stats = fs.statSync(target.path);
-        if (stats.size > target.limit) {
-          console.error(`file ${target.path} size over limit, limit is ${target.limit}, actual is ${
-              stats.size}`);
-          chkResult = false;
-        }
-      } catch (err) {
-        console.error(`failed to get filesize: ${target.path}`);
-        chkResult = false;
+      if (target.gzipLimit) {
+        // gzip target
+        await do_gzip(target.path, `${target.path}.gz`);
       }
+      chkResult =
+          checkSize(target.path, target.limit) && checkSize(`${target.path}.gz`, target.gzipLimit);
+      await remove(`${target.path}.gz`);
     }
   });
   return chkResult;
 };
+
+process.exitCode = checker(require('./file-size-limit.json')) ? 0 : 1;
