753b36784f325a55d006aa2363a752da359924c5
Andrew Kushnir <akushnir@google.com>
2020-10-05 18:06:58 -0700

    test(core): add micro benchmarks for i18n scenarios
    This commit adds micro benchmarks to run micro benchmarks for i18n-related logic in the
    following scenarios:
    
    - i18n static attributes
    - i18n attributes with interpolations
    - i18n blocks of static text
    - i18n blocks of text + interpolations
    - simple ICUs
    - nested ICUs
    
    First 4 scenarios also have baseline scenarios (non-i18n) so that we can compare i18n perf with
    non-i18n logic.

--- packages/core/src/render3/instructions/all.ts
+++ packages/core/src/render3/instructions/all.ts
@@ -48,3 +48,4 @@ export * from './class_map_interpolation';
 export * from './style_map_interpolation';
 export * from './style_prop_interpolation';
 export * from './host_property';
+export * from './i18n';
