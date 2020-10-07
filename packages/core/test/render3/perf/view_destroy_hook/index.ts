a83693ddd99abe5152eb3cc393fd4f4073218176
Andrew Kushnir <akushnir@google.com>
2020-10-06 21:45:28 -0700

    refactor(core): use relative import paths in micro benchmarks (#39142)
    This commit updates micro benchmarks to use relative path to Ivy runtime code. Keeping absolute
    locations caused issues with build optimizer that retained certain symbols and they appeared in the
    output twice.
    
    PR Close #39142

--- packages/core/test/render3/perf/view_destroy_hook/index.ts
+++ packages/core/test/render3/perf/view_destroy_hook/index.ts
@@ -6,12 +6,12 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import {OnDestroy} from '@angular/core';
-import {LViewFlags, TViewType} from '@angular/core/src/render3/interfaces/view';
 
 import {ɵɵdefineDirective, ɵɵelement, ɵɵelementEnd, ɵɵelementStart} from '../../../../src/render3/index';
 import {createLView, createTNode, createTView} from '../../../../src/render3/instructions/shared';
 import {RenderFlags} from '../../../../src/render3/interfaces/definition';
 import {TNodeType} from '../../../../src/render3/interfaces/node';
+import {LViewFlags, TViewType} from '../../../../src/render3/interfaces/view';
 import {destroyLView} from '../../../../src/render3/node_manipulation';
 import {createBenchmark} from '../micro_bench';
 import {createAndRenderLView} from '../setup';
