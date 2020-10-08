2a0a287121480efcc71d3bedd2b437b9383ca95f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-08 12:56:05 -0700

    refactor(compiler-cli): move global completion into new CompletionEngine (#39278)
    This commit refactors the previously introduced `getGlobalCompletions()` API
    for the template type-checker in a couple ways:
    
     * The return type is adjusted to use a `Map` instead of an array, and
       separate out the component context completion position. This allows for a
       cleaner integration in the language service.
     * A new `CompletionEngine` class is introduced which powers autocompletion
       for a single component, and can cache completion results.
     * The `CompletionEngine` for each component is itself cached on the
       `TemplateTypeCheckerImpl` and is invalidated when the component template
       is overridden or reset.
    
    This refactoring simplifies the `TemplateTypeCheckerImpl` class by
    extracting the autocompletion logic, enables caching for better performance,
    and prepares for the introduction of other autocompletion APIs.
    
    PR Close #39278

--- packages/compiler-cli/src/ngtsc/typecheck/api/completion.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/api/completion.ts
@@ -13,34 +13,21 @@ import {ShimLocation} from './symbols';
 /**
  * An autocompletion source of any kind.
  */
-export type Completion = CompletionContextComponent|CompletionReference|CompletionVariable;
-
-/**
- * An autocompletion source that drives completion in a global context.
- */
-export type GlobalCompletion = CompletionContextComponent|CompletionReference|CompletionVariable;
+export type Completion = ReferenceCompletion|VariableCompletion;
 
 /**
  * Discriminant of an autocompletion source (a `Completion`).
  */
+
 export enum CompletionKind {
-  ContextComponent,
   Reference,
   Variable,
 }
 
 /**
- * An autocompletion source backed by a shim file position where TS APIs can be used to retrieve
- * completions for the context component of a template.
- */
-export interface CompletionContextComponent extends ShimLocation {
-  kind: CompletionKind.ContextComponent;
-}
-
-/**
  * An autocompletion result representing a local reference declared in the template.
  */
-export interface CompletionReference {
+export interface ReferenceCompletion {
   kind: CompletionKind.Reference;
 
   /**
@@ -52,11 +39,36 @@ export interface CompletionReference {
 /**
  * An autocompletion result representing a variable declared in the template.
  */
-export interface CompletionVariable {
+export interface VariableCompletion {
   kind: CompletionKind.Variable;
 
   /**
    * The `TmplAstVariable` from the template which should be available as a completion.
    */
   node: TmplAstVariable;
+}
+
+/**
+ * Autocompletion data for an expression in the global scope.
+ *
+ * Global completion is accomplished by merging data from two sources:
+ *  * TypeScript completion of the component's class members.
+ *  * Local references and variables that are in scope at a given template level.
+ */
+export interface GlobalCompletion {
+  /**
+   * A location within the type-checking shim where TypeScript's completion APIs can be used to
+   * access completions for the template's component context (component class members).
+   */
+  componentContext: ShimLocation;
+
+  /**
+   * `Map` of local references and variables that are visible at the requested level of the
+   * template.
+   *
+   * Shadowing of references/variables from multiple levels of the template has already been
+   * accounted for in the preparation of `templateContext`. Entries here shadow component members of
+   * the same name (from the `componentContext` completions).
+   */
+  templateContext: Map<string, ReferenceCompletion|VariableCompletion>;
 }
