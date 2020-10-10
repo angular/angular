e8d47c2d41ede1bf2f0392a0196f09e1858e2276
Bjarki <bjarki@google.com>
2020-10-10 01:13:32 +0000

    feat(core): allow returning Trusted Types from SanitizerFn (#39218)
    Sanitizers in Angular currently return strings, which will then
    eventually make their way down to the DOM, e.g. as the value of an
    attribute or property. This may cause a Trusted Types violation. As a
    step towards fixing that, make it possible to return Trusted Types from
    the SanitizerFn interface, which represents the internal sanitization
    pipeline. DOM renderer interfaces are also updated to reflect the fact
    that setAttribute and setAttributeNS must be able to accept Trusted
    Types.
    
    PR Close #39218

--- packages/core/src/render3/interfaces/renderer.ts
+++ packages/core/src/render3/interfaces/renderer.ts
@@ -16,6 +16,7 @@
  */
 
 import {RendererStyleFlags2, RendererType2} from '../../render/api';
+import {TrustedHTML, TrustedScript, TrustedScriptURL} from '../../util/security/trusted_type_defs';
 import {getDocument} from './document';
 
 // TODO: cleanup once the code is merged in angular/angular
@@ -80,7 +81,9 @@ export interface ProceduralRenderer3 {
   parentNode(node: RNode): RElement|null;
   nextSibling(node: RNode): RNode|null;
 
-  setAttribute(el: RElement, name: string, value: string, namespace?: string|null): void;
+  setAttribute(
+      el: RElement, name: string, value: string|TrustedHTML|TrustedScript|TrustedScriptURL,
+      namespace?: string|null): void;
   removeAttribute(el: RElement, name: string, namespace?: string|null): void;
   addClass(el: RElement, name: string): void;
   removeClass(el: RElement, name: string): void;
@@ -157,9 +160,11 @@ export interface RElement extends RNode {
   classList: RDomTokenList;
   className: string;
   textContent: string|null;
-  setAttribute(name: string, value: string): void;
+  setAttribute(name: string, value: string|TrustedHTML|TrustedScript|TrustedScriptURL): void;
   removeAttribute(name: string): void;
-  setAttributeNS(namespaceURI: string, qualifiedName: string, value: string): void;
+  setAttributeNS(
+      namespaceURI: string, qualifiedName: string,
+      value: string|TrustedHTML|TrustedScript|TrustedScriptURL): void;
   addEventListener(type: string, listener: EventListener, useCapture?: boolean): void;
   removeEventListener(type: string, listener?: EventListener, options?: boolean): void;
 
