10db99588f92ba02cad63e7549aed6290055ae0c
Vlasis Charalampous <vlasischar@gmail.com>
2020-10-13 22:02:45 +0300

    refactor(docs-infra): fix `strictTemplates` failures in `accessibility` docs example (#39248)
    fix `strictTemplates` failures in `accessibility` docs example
    
    PR Close #39248

--- aio/content/examples/accessibility/e2e/src/app.e2e-spec.ts
+++ aio/content/examples/accessibility/e2e/src/app.e2e-spec.ts
@@ -12,7 +12,7 @@ describe('Accessibility example e2e tests', () => {
 
   it('should take a number and change progressbar width', () => {
     element(by.css('input')).sendKeys('16');
-    expect(element(by.css('input')).getAttribute('value')).toEqual('016');
+    expect(element(by.css('input')).getAttribute('value')).toEqual('16');
     expect(element(by.css('app-example-progressbar div')).getCssValue('width')).toBe('48px');
   });
 
