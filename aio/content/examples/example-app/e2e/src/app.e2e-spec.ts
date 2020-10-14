ed74e640a5c3cb37b44bd6ed8df933ac4f23da22
Kapunahele Wong <kapunahelewong@gmail.com>
2020-10-14 15:22:58 -0400

    docs: add an example app to act as a template
    This commit adds a minimal example app that
    contributors can use a template.

---
+++ aio/content/examples/example-app/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,9 @@
+import { browser, element, by } from 'protractor';
+
+describe('Example app e2e tests', () => {
+
+  beforeEach(() => browser.get(''));
+
+  // Add your e2e tests here
+  
+});
---
+++ aio/content/examples/example-app/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'example'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('example');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('example app is running!');
+  });
+});
