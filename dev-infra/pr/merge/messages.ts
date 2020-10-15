767fdccf593bf2aaa77e2bc2e409c175f0873fd4
Joey Perrott <josephperrott@gmail.com>
2020-10-15 10:58:51 -0700

    feat(dev-infra): prompt caretaker to confirm the merge branches on merge (#39333)
    Perviously, it was not immediately clear what branches a PR would merge
    into during the merge process.  This prompt allows for caretakers to
    understand and acknowledge where the PR will merge to.
    
    PR Close #39333

--- dev-infra/pr/merge/messages.ts
+++ dev-infra/pr/merge/messages.ts
@@ -11,5 +11,11 @@ import {PullRequest} from './pull-request';
 
 export function getCaretakerNotePromptMessage(pullRequest: PullRequest): string {
   return red('Pull request has a caretaker note applied. Please make sure you read it.') +
-      `\nQuick link to PR: ${pullRequest.url}`;
+      `\nQuick link to PR: ${pullRequest.url}\nDo you want to proceed merging?`;
+}
+
+export function getTargettedBranchesConfirmationPromptMessage(pullRequest: PullRequest): string {
+  const targetBranchListAsString = pullRequest.targetBranches.map(b => ` - ${b}\n`).join('');
+  return `Pull request #${pullRequest.prNumber} will merge into:\n${
+      targetBranchListAsString}\nDo you want to proceed merging?`;
 }
