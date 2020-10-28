63631012bad365773e8a7b03adcbb618e5f0eea5
Pete Bacon Darwin <pete@bacondarwin.com>
2020-10-28 21:35:06 +0000

    refactor(compiler): store the `fullStart` location on `ParseSourceSpan`s
    The lexer is able to skip leading trivia in the `start` location of tokens.
    This makes the source-span more friendly since things like elements
    appear to begin at the start of the opening tag, rather than at the
    start of any leading whitespace, which could include newlines.
    
    But some tooling requires the full source-span to be available, such
    as when tokenizing a text span into an Angular expression.
    
    This commit simply adds the `fullStart` location to the `ParseSourceSpan`
    class, and ensures that places where such spans are cloned, this
    property flows through too.

--- packages/compiler/src/parse_util.ts
+++ packages/compiler/src/parse_util.ts
@@ -100,8 +100,32 @@ export class ParseSourceFile {
 }
 
 export class ParseSourceSpan {
+  /**
+   * Create an object that holds information about spans of tokens/nodes captured during
+   * lexing/parsing of text.
+   *
+   * @param start
+   * The location of the start of the span (having skipped leading trivia).
+   * Skipping leading trivia makes source-spans more "user friendly", since things like HTML
+   * elements will appear to begin at the start of the opening tag, rather than at the start of any
+   * leading trivia, which could include newlines.
+   *
+   * @param end
+   * The location of the end of the span.
+   *
+   * @param fullStart
+   * The start of the token without skipping the leading trivia.
+   * This is used by tooling that splits tokens further, such as extracting Angular interpolations
+   * from text tokens. Such tooling creates new source-spans relative to the original token's
+   * source-span. If leading trivia characters have been skipped then the new source-spans may be
+   * incorrectly offset.
+   *
+   * @param details
+   * Additional information (such as identifier names) that should be associated with the span.
+   */
   constructor(
-      public start: ParseLocation, public end: ParseLocation, public details: string|null = null) {}
+      public start: ParseLocation, public end: ParseLocation,
+      public fullStart: ParseLocation = start, public details: string|null = null) {}
 
   toString(): string {
     return this.start.file.content.substring(this.start.offset, this.end.offset);
