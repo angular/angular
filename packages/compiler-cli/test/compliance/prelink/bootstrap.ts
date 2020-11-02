b94f30b3bd27d0985fba2b3294ff31a785e26c65
JoostK <joost.koehoorn@gmail.com>
2020-11-02 18:20:40 +0100

    fixup! test(compiler-cli): run the linker transform in the compliance test's prelink target

--- packages/compiler-cli/test/compliance/prelink/bootstrap.ts
+++ packages/compiler-cli/test/compliance/prelink/bootstrap.ts
@@ -36,7 +36,7 @@ const linkedCompile: CompileFn = (data, angularFiles, options) => {
  *
  * @param file The file name and its source to be transformed using the linker.
  * @param linkerPlugin The linker plugin to apply.
- * @returns The file's source content which has been transformed using the linker.
+ * @returns The file's source content, which has been transformed using the linker if necessary.
  */
 function applyLinker(file: {fileName: string; source: string}, linkerPlugin: PluginObj): string {
   if (!file.fileName.endsWith('.js')) {
