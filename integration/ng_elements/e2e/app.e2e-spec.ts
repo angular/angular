ff925af90811376574c9ebb2408abeeeb978cef1
George Kalpakas <kalpakas.g@gmail.com>
2020-11-04 20:45:33 +0200

    test(elements): add integration tests for Angular Elements using `ShadowDom` (#39452)
    Previously, the project used for running integration tests for Angular
    Elements declared a component that used `ShadowDom` for view
    encopsulation, but it did not include any tests to verify that the view
    was updated correctly.
    
    This commit adds the missing tests.
    
    PR Close #39452

--- integration/ng_elements/e2e/app.e2e-spec.ts
+++ integration/ng_elements/e2e/app.e2e-spec.ts
@@ -1,22 +1,42 @@
-import { browser, element, ExpectedConditions as EC, by } from 'protractor';
+import {browser, by, element, ElementFinder, ExpectedConditions as EC} from 'protractor';
 
 browser.waitForAngularEnabled(false);
 describe('Element E2E Tests', function () {
   describe('Hello World Elements', () => {
-    const helloWorldEl = element(by.css('hello-world-el'));
-
     beforeEach(() => browser.get('hello-world.html'));
 
-    it('should display "Hello World!"', function () {
-      expect(helloWorldEl.getText()).toEqual('Hello World!');
+    describe('(with default view encapsulation)', () => {
+      const helloWorldEl = element(by.css('hello-world-el'));
+
+      it('should display "Hello World!"', function () {
+        expect(helloWorldEl.getText()).toBe('Hello World!');
+      });
+
+      it('should display "Hello Foo!" via name attribute', function () {
+        const input = element(by.css('input[type=text]'));
+        input.sendKeys('Foo');
+
+        // Make tests less flaky on CI by waiting up to 5s for the element text to be updated.
+        browser.wait(EC.textToBePresentInElement(helloWorldEl, 'Hello Foo!'), 5000);
+      });
     });
 
-    it('should display "Hello Foo!" via name attribute', function () {
-      const input = element(by.css('input[type=text]'));
-      input.sendKeys('Foo');
+    describe('(with `ShadowDom` view encapsulation)', () => {
+      const helloWorldShadowEl = element(by.css('hello-world-shadow-el'));
+      const getShadowDomText = (el: ElementFinder) =>
+        browser.executeScript('return arguments[0].shadowRoot.textContent', el);
 
-      // Make tests less flaky on CI by waiting up to 5s for the element text to be updated.
-      browser.wait(EC.textToBePresentInElement(helloWorldEl, 'Hello Foo!'), 5000);
+      it('should display "Hello World!"', function () {
+        expect(getShadowDomText(helloWorldShadowEl)).toBe('Hello World!');
+      });
+
+      it('should display "Hello Foo!" via name attribute', function () {
+        const input = element(by.css('input[type=text]'));
+        input.sendKeys('Foo');
+
+        // Make tests less flaky on CI by waiting up to 5s for the element text to be updated.
+        browser.wait(async () => await getShadowDomText(helloWorldShadowEl) === 'Hello Foo!', 5000);
+      });
     });
   });
 });
