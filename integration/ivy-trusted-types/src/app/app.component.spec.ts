123656a727bdc1119cb44c0016708f96df7b3d0c
Bjarki <bjarki@google.com>
2020-11-09 11:25:43 +0000

    test(core): create ivy-trusted-types integration test (#39614)
    Create an integration test for testing support for Trusted Types in Ivy.
    Generated with:
    
    ```
    yarn ng new ivy-trusted-types -g --skip-install --style css --routing --strict
    ```
    
    PR Close #39614

---
+++ integration/ivy-trusted-types/src/app/app.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'ivy-trusted-types'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ivy-trusted-types');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('ivy-trusted-types app is running!');
+  });
+});
