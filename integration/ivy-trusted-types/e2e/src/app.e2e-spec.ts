569086a97455f37200138280a4c3ef8c9275bca9
Bjarki <bjarki@google.com>
2020-11-09 18:36:16 +0000

    test(core): add e2e tests to ivy-trusted-types integration test (#39614)
    Make sure that all Trusted Types-relevant sinks that can appear in an
    Angular template (e.g. not <script>, since it's forbidden) continue to
    work as expected with correct sanitization semantics, without
    introducing Trusted Types violations.
    
    PR Close #39614

--- integration/ivy-trusted-types/e2e/src/app.e2e-spec.ts
+++ integration/ivy-trusted-types/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {browser, logging} from 'protractor';
+import {AppPage} from './app.po';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -13,7 +13,49 @@ describe('workspace-project App', () => {
     expect(await page.getTitleText()).toEqual('ivy-trusted-types app is running!');
   });
 
+  it('should sanitize and inject bound innerHTML', async () => {
+    await page.navigateTo();
+    expect(await page.getBoundHtmlText()).toEqual('Hello from bound HTML');
+    expect(await page.boundHtmlIframeIsPresent()).toBe(false);
+  });
+
+  it('should directly inject SafeHtml bound to innerHTML', async () => {
+    await page.navigateTo();
+    expect(await page.getBoundSafeHtmlText()).toEqual('Hello from bound SafeHtml');
+    expect(await page.boundSafeHtmlIframeIsPresent()).toBe(true);
+  });
+
+  it('should replace element with outerHTML contents', async () => {
+    await page.navigateTo();
+    expect(await page.getOuterHTMLText()).toBe('Hello from second outerHTML');
+  });
+
+  it('should load iframe', async () => {
+    await page.navigateTo();
+    await browser.waitForAngularEnabled(false);
+    await page.switchToIframe();
+    expect(await page.getHeaderText()).toEqual('Hello from iframe');
+  });
+
+  it('should load embed', async () => {
+    await page.navigateTo();
+    await browser.waitForAngularEnabled(false);
+    await page.switchToEmbed();
+    expect(await page.getHeaderText()).toEqual('Hello from embed');
+  });
+
+  it('should load object', async () => {
+    await page.navigateTo();
+    await browser.waitForAngularEnabled(false);
+    await page.switchToObject();
+    expect(await page.getHeaderText()).toEqual('Hello from object');
+  });
+
   afterEach(async () => {
+    // Re-enable waiting for Angular in case we disabled it to navigate to a
+    // non-Angular page
+    await browser.waitForAngularEnabled(true);
+
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
     expect(logs).not.toContain(jasmine.objectContaining({
