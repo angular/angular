fc1115e4fbada53e5b75b2c46bdb6bb7915ed13b
Joey Perrott <josephperrott@gmail.com>
2020-11-12 11:01:01 -0800

    build: define the package building function for ng-dev release tooling
    The packages are built during the release process on demand via a function
    defined locally in the ng-dev config.

--- .ng-dev/release.ts
+++ .ng-dev/release.ts
@@ -2,30 +2,42 @@ import {join} from 'path';
 import {exec} from 'shelljs';
 import {ReleaseConfig} from '../dev-infra/release/config';
 
+
+const packages = [
+  'animations',
+  'bazel',
+  'common',
+  'compiler',
+  'compiler-cli',
+  'core',
+  'elements',
+  'forms',
+  'language-service',
+  'localize',
+  'platform-browser',
+  'platform-browser-dynamic',
+  'platform-server',
+  'router',
+  'service-worker',
+  'upgrade',
+];
+
 /** Configuration for the `ng-dev release` command. */
 export const release: ReleaseConfig = {
-  npmPackages: [
-    '@angular/animations',
-    '@angular/bazel',
-    '@angular/common',
-    '@angular/compiler',
-    '@angular/compiler-cli',
-    '@angular/core',
-    '@angular/elements',
-    '@angular/forms',
-    '@angular/language-service',
-    '@angular/localize',
-    '@angular/platform-browser',
-    '@angular/platform-browser-dynamic',
-    '@angular/platform-server',
-    '@angular/platform-webworker',
-    '@angular/platform-webworker-dynamic',
-    '@angular/router',
-    '@angular/service-worker',
-    '@angular/upgrade',
-  ],
-  // TODO: Implement release package building here.
-  buildPackages: async () => [],
+  npmPackages: packages.map(pkg => `@angular/${pkg}`),
+  buildPackages: async () => {
+    const packageTargets = packages.map(pkg => `//packages/${pkg}:npm_package`).join(' ');
+    const buildResult = exec(`yarn -s bazel build --stamp ${packageTargets}`);
+
+    if (buildResult.code !== 0) {
+      throw new Error(`Error occured while building packages:\n${buildResult.stderr}`);
+    }
+
+    return packages.map(pkg => ({
+                          name: `@angular/${pkg}`,
+                          outputPath: `dist/bin/packages/${pkg}/npm_package`,
+                        }))
+  },
   // TODO: This can be removed once there is an org-wide tool for changelog generation.
   generateReleaseNotesForHead: async () => {
     exec('yarn -s gulp changelog', {cwd: join(__dirname, '../')});
