a41d927ace47971f66145b102182c10d0009e1b3
Andrew Kushnir <akushnir@google.com>
2020-11-19 13:05:34 -0800

    Revert "test(core): rename ivy-trusted-types integration test to trusted-types (#39614)"
    This reverts commit 29fdbf250b2c34212c181a66ec973819694ac005.

--- integration/ivy-trusted-types/src/app/app.component.ts
+++ integration/ivy-trusted-types/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {DomSanitizer, SafeHtml, SafeResourceUrl} from '@angular/platform-browser
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trusted-types';
+  title = 'ivy-trusted-types';
   html = `<span>Hello from bound HTML</span><iframe id="bound-html-iframe"></iframe>`;
   iframeHtml = `<h1>Hello from iframe</h1>`;
   replace = `<span>Hello from second outerHTML</span>`;
