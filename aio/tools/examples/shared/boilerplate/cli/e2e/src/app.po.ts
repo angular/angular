b8499927d319fdc816ca5e65dd3ee77c6a1d6faf
George Kalpakas <kalpakas.g@gmail.com>
2020-11-23 20:16:57 +0200

    build(docs-infra): update docs examples to Angular v11.0.1
    This commit updates the docs examples to Angular v11.0.1. In addition to
    updating the dependencies versions, it also updates the project's
    structure and config to more closely match what a new v11 CLI app would
    look like. See, also, the [diff][1] between a basic v10.1.3 CLI app and a
    v11.0.2 one.
    
    NOTE:
    I refrained from disabling the Selenium Promise Manager (as seen
    [here][2]) and switching all e2e tests to `async/await`, because that is
    a big change and should be done in a separate commit/PR.
    
    [1]: https://github.com/cexbrayat/angular-cli-diff/compare/10.1.3..11.0.2
    [2]:
    https://github.com/cexbrayat/angular-cli-diff/compare/10.1.3...11.0.2#diff-dbd675d74087d57cd084d6dd6ae24ae2eeff2ff0122680e12916052f8a843a29

--- aio/tools/examples/shared/boilerplate/cli/e2e/src/app.po.ts
+++ aio/tools/examples/shared/boilerplate/cli/e2e/src/app.po.ts
@@ -1,11 +1,11 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+  async navigateTo(): Promise<unknown> {
+    return browser.get(browser.baseUrl);
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  async getTitleText(): Promise<string> {
+    return element(by.css('app-root h1')).getText();
   }
 }
