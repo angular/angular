d9552f29595146ec3cb97e64c93019ccffa6f2e7
George Kalpakas <kalpakas.g@gmail.com>
2020-11-28 17:46:28 +0200

    Try with larger jasmine timeout (60000)

--- aio/tests/e2e/protractor.conf.js
+++ aio/tests/e2e/protractor.conf.js
@@ -26,7 +26,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: 60000,
     print: function() {}
   },
   onPrepare() {
