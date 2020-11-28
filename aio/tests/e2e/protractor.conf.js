37963c7276923ae759f778fbbddeb7359d9258fd
George Kalpakas <kalpakas.g@gmail.com>
2020-11-29 00:05:43 +0200

    Try with `webdriver-manager` v12.0.6

--- aio/tests/e2e/protractor.conf.js
+++ aio/tests/e2e/protractor.conf.js
@@ -21,7 +21,7 @@ exports.config = {
     },
   },
   directConnect: true,
-  SELENIUM_PROMISE_MANAGER: true,
+  SELENIUM_PROMISE_MANAGER: false,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
