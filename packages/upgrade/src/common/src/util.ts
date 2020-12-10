bed2e8a23cf70e307da6f38ba81e2343955395c1
George Kalpakas <kalpakas.g@gmail.com>
2020-12-10 19:13:32 +0200

    refactor(upgrade): create a helper for cleaning jqLite/jQuery data (#40045)
    This commit moves the code for cleaning jqLite/jQuery data on an element
    to a re-usable helper function. This way it is easier to keep the code
    consistent across all places where we need to clean data (now and in the
    future).
    
    PR Close #40045

--- packages/upgrade/src/common/src/util.ts
+++ packages/upgrade/src/common/src/util.ts
@@ -8,7 +8,7 @@
 
 import {Injector, Type} from '@angular/core';
 
-import {IInjectorService, INgModelController} from './angular1';
+import {element as angularElement, IInjectorService, INgModelController} from './angular1';
 import {DOWNGRADED_MODULE_COUNT_KEY, UPGRADE_APP_TYPE_KEY} from './constants';
 
 const DIRECTIVE_PREFIX_REGEXP = /^(?:x|data)[:\-_]/i;
@@ -23,6 +23,25 @@ export function onError(e: any) {
     console.log(e, e.stack);
   }
   throw e;
+}
+
+/**
+ * Clean the jqLite/jQuery data on the element and all its descendants.
+ * Equivalent to how jqLite/jQuery invoke `cleanData()` on an Element when removed:
+ *   https://github.com/angular/angular.js/blob/2e72ea13fa98bebf6ed4b5e3c45eaf5f990ed16f/src/jqLite.js#L349-L355
+ *   https://github.com/jquery/jquery/blob/6984d1747623dbc5e87fd6c261a5b6b1628c107c/src/manipulation.js#L182
+ *
+ * NOTE:
+ * `cleanData()` will also invoke the AngularJS `$destroy` DOM event on the element:
+ *   https://github.com/angular/angular.js/blob/2e72ea13fa98bebf6ed4b5e3c45eaf5f990ed16f/src/Angular.js#L1932-L1945
+ *
+ * @param node The DOM node whose data needs to be cleaned.
+ */
+export function cleanData(node: Node): void {
+  angularElement.cleanData([node]);
+  if (isParentNode(node)) {
+    angularElement.cleanData(node.querySelectorAll('*'));
+  }
 }
 
 export function controllerKey(name: string): string {
@@ -51,6 +70,10 @@ export function getUpgradeAppType($injector: IInjectorService): UpgradeAppType {
 
 export function isFunction(value: any): value is Function {
   return typeof value === 'function';
+}
+
+function isParentNode(node: Node|ParentNode): node is ParentNode {
+  return isFunction((node as unknown as ParentNode).querySelectorAll);
 }
 
 export function validateInjectionKey(
