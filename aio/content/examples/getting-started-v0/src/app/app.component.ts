1fae285e04592cf5169074d0a254debde3374c01
George Kalpakas <kalpakas.g@gmail.com>
2021-01-06 23:05:29 +0200

    docs: more closely align the `getting-started-v0` and `getting-started` docs examples (#40197)
    This commit aligns the code and style in the `getting-started-v0` docs
    example with that of the `getting-started` example more closely (except
    for the the parts that are intentionally different between the two).
    
    PR Close #40197

--- aio/content/examples/getting-started-v0/src/app/app.component.ts
+++ aio/content/examples/getting-started-v0/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {}
+export class AppComponent {
+}
