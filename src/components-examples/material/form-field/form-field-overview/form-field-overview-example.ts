154ef43baad9b12e7d842d09ab101d866d8cd47c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-06 13:54:14 +0200

    docs: fix form field example styles (#21124)
    * Fixes an example that was referring to the `example-container` class, but wasn't actually using it.
    * Uses flexbox to layout the example, instead of `br` tags which are an anti-pattern.
    
    Fixes https://github.com/angular/material.angular.io/issues/889.

--- src/components-examples/material/form-field/form-field-overview/form-field-overview-example.ts
+++ src/components-examples/material/form-field/form-field-overview/form-field-overview-example.ts
@@ -4,5 +4,6 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'form-field-overview-example',
   templateUrl: 'form-field-overview-example.html',
+  styleUrls: ['form-field-overview-example.css']
 })
 export class FormFieldOverviewExample {}
