5f68edd810884155eac1ecc133aa6284162c9669
George Kalpakas <kalpakas.g@gmail.com>
2021-01-06 23:05:29 +0200

    docs: more closely align the `getting-started-v0` and `getting-started` docs examples
    This commit aligns the code and style in the `getting-started-v0` docs
    example with that of the `getting-started` example more closely (except
    for the the parts that are intentionally different between the two).

--- aio/content/examples/getting-started-v0/src/main.ts
+++ aio/content/examples/getting-started-v0/src/main.ts
@@ -1,11 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { environment } from './environments/environment';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
