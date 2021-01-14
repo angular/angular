566206b51979301d40f94c293a4fe3baa0c973f7
Pete Bacon Darwin <pete@bacondarwin.com>
2021-01-14 20:49:10 +0000

    fix(compiler-cli): handle pseudo cycles in inline source-maps (#40435)
    When a source-map has an inline source, any source-map linked from
    that source should only be loaded if itself is also inline; it should not
    attempt to load a source-map from the file-system. Otherwise we can
    find ourselves with inadvertent infinite cyclic dependencies.
    
    For example, if a transpiler takes a file (e.g. index.js) and generates
    a new file overwriting the original file - capturing the original
    source inline in the new source-map (index.js.map) - the source
    file loader might read the inline original file (also index.js) and
    then try to load the `index.js.map` file from disk - ad infinitum.
    
    Note that the first call to `loadSourceFile()` is special, since you can
    pass in the source-file and source-map contents directly as in-memory
    strrngs. This is common if the transpiler has just generated these and has
    not yet written them to disk.
    When the contents are passed into `loadSourceFile()` directly, they are
    not treated as "inline" for the purposes described above since there is
    no chance of these "in-memory" source and source-map contents being caught
    up in a cyclic dependency.
    
    Fixes #40408
    
    PR Close #40435

--- packages/compiler-cli/src/ngtsc/sourcemaps/src/raw_source_map.ts
+++ packages/compiler-cli/src/ngtsc/sourcemaps/src/raw_source_map.ts
@@ -5,6 +5,8 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+import {AbsoluteFsPath} from '../../file_system';
+import {ContentOrigin} from './content_origin';
 
 /**
  * This interface is the basic structure of the JSON in a raw source map that one might load from
@@ -18,4 +20,23 @@ export interface RawSourceMap {
   names: string[];
   sourcesContent?: (string|null)[];
   mappings: string;
+}
+
+
+/**
+ * The path and content of a source-map.
+ */
+export interface MapAndPath {
+  /** The path to the source map if it was external or `null` if it was inline. */
+  mapPath: AbsoluteFsPath|null;
+  /** The raw source map itself. */
+  map: RawSourceMap;
+}
+
+/**
+ * Information about a loaded source-map.
+ */
+export interface SourceMapInfo extends MapAndPath {
+  /** From where the content for this source-map came. */
+  origin: ContentOrigin;
 }
