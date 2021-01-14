24413bd41e3f506b8d84f8ffa4d2636be8243088
Pete Bacon Darwin <pete@bacondarwin.com>
2021-01-14 17:04:47 +0000

    fixup! fix(ngcc): copy (and update) source-maps for unmodified source files

--- packages/compiler-cli/ngcc/src/writing/new_entry_point_file_writer.ts
+++ packages/compiler-cli/ngcc/src/writing/new_entry_point_file_writer.ts
@@ -112,7 +112,7 @@ export class NewEntryPointFileWriter extends InPlaceFileWriter {
         this.fs.writeFile(newSourceMapPath, JSON.stringify(sourceMap));
       } catch (e) {
         this.logger.warn(`Failed to process source-map at ${sourceMapPath}`);
-        this.logger.warn(e.message);
+        this.logger.warn(e.message ?? e);
       }
     }
   }
