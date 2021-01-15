0911820f90d2262eea9ce2e92c0984b03db4a690
Joey Perrott <josephperrott.github@gmail.com>
2021-01-15 16:02:13 -0700

    build: configure the ng-dev caretaker check command (#21611)
    Create the configuration for the caretaker check command in the repo.

---
+++ .ng-dev/caretaker.ts
@@ -0,0 +1,15 @@
+import {CaretakerConfig} from '@angular/dev-infra-private/caretaker/config';
+
+/** The configuration for `ng-dev caretaker` commands. */
+export const caretaker: CaretakerConfig = {
+  githubQueries: [
+    {
+      name: 'Merge Queue',
+      query: `is:pr is:open status:success label:"pr: merge ready"`,
+    },
+    {
+      name: 'Triage Queue',
+      query: `is:open label:"needs triage"`,
+    }
+  ]
+};
--- .ng-dev/config.ts
+++ .ng-dev/config.ts
@@ -2,10 +2,12 @@ import {format} from './format';
 import {github} from './github';
 import {merge} from './merge';
 import {commitMessage} from './commit-message';
+import {caretaker} from './caretaker';
 
 module.exports = {
   commitMessage,
   format,
   github,
   merge,
+  caretaker,
 };
