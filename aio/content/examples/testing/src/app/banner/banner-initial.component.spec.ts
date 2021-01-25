2abdf5c75c10621dfa043afe9fd9de08a8aab807
S. Iftekhar Hossain <ifteeh01@gmail.com>
2021-01-26 04:26:09 +0600

    docs: fixed issue with missing code sample by rearranging import to proper docregion (#40565)
    Fixes #40558
    
    PR Close #40565

--- aio/content/examples/testing/src/app/banner/banner-initial.component.spec.ts
+++ aio/content/examples/testing/src/app/banner/banner-initial.component.spec.ts
@@ -1,14 +1,14 @@
 // #docplaster
-// #docregion import-by
-// #enddocregion import-by
 // #docregion import-debug-element
 import { DebugElement } from '@angular/core';
 // #enddocregion import-debug-element
 // #docregion v1
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 
 // #enddocregion v1
+// #docregion import-by
+import { By } from '@angular/platform-browser';
+// #enddocregion import-by
 import { BannerComponent } from './banner-initial.component';
 
 /*
