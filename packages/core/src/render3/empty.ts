552009ec07da88ce9ae188377e883e4f19a03fd6
Misko Hevery <misko@hevery.com>
2021-01-28 14:35:58 -0800

    Revert "fix(core): remove duplicated EMPTY_ARRAY constant (#40587)"
    This reverts commit cabe8bec40ff4e40fc8c88110c2c8d4a64d4b2b1.

--- packages/core/src/render3/empty.ts
+++ packages/core/src/render3/empty.ts
@@ -15,6 +15,7 @@ import {initNgDevMode} from '../util/ng_dev_mode';
  */
 
 export const EMPTY_OBJ: {} = {};
+export const EMPTY_ARRAY: any[] = [];
 
 // freezing the values prevents any code from accidentally inserting new values in
 if ((typeof ngDevMode === 'undefined' || ngDevMode) && initNgDevMode()) {
@@ -22,4 +23,6 @@ if ((typeof ngDevMode === 'undefined' || ngDevMode) && initNgDevMode()) {
   // when optimizing code and the whole if statement will be dropped.
   // tslint:disable-next-line:no-toplevel-property-access
   Object.freeze(EMPTY_OBJ);
+  // tslint:disable-next-line:no-toplevel-property-access
+  Object.freeze(EMPTY_ARRAY);
 }
