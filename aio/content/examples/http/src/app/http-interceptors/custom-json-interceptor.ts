9e5d9e686cb8e94ba2528cda7d44241f9521bb7d
Pete Bacon Darwin <pete@bacondarwin.com>
2021-02-02 15:25:39 +0000

    fixup! docs(http): add custom JSONParser example

--- aio/content/examples/http/src/app/http-interceptors/custom-json-interceptor.ts
+++ aio/content/examples/http/src/app/http-interceptors/custom-json-interceptor.ts
@@ -1,6 +1,6 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
-import {Injectable} from '@angular/core';
-import {map} from 'rxjs/operators';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 // #docregion custom-json-interceptor
 @Injectable()
