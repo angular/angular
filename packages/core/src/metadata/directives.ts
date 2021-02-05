13d82ce248ae65a214df5b1596ce4a4ba99e65ce
abarghoud <abarghoud@gmail.com>
2021-02-05 11:49:41 +0100

    docs(core): add documentation for HostListener (#40720)
    add return value documentations for HostListener callbacks
    
    Closes #40708
    
    PR Close #40720

--- packages/core/src/metadata/directives.ts
+++ packages/core/src/metadata/directives.ts
@@ -823,6 +823,11 @@ export interface HostListenerDecorator {
   /**
    * Decorator that declares a DOM event to listen for,
    * and provides a handler method to run when that event occurs.
+   *
+   * Angular invokes the supplied handler method when the host element emits the specified event,
+   * and updates the bound element with the result.
+   *
+   * If the handler method returns false, applies `preventDefault` on the bound element.
    */
   (eventName: string, args?: string[]): any;
   new(eventName: string, args?: string[]): any;
