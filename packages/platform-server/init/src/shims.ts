a67a7200bb965117241bf99e726dbf9a9aedeba0
George Kalpakas <kalpakas.g@gmail.com>
2021-02-11 19:26:00 +0200

    fixup! feat(platform-server): allow shimming the global env sooner

--- packages/platform-server/init/src/shims.ts
+++ packages/platform-server/init/src/shims.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import {setDomTypes} from '@angular/platform-server/src/domino_adapter';
+import {setDomTypes} from '../../src/domino_adapter';
 
 /**
  * Apply the necessary shims to make DOM globals (such as `Element`, `HTMLElement`, etc.) available
