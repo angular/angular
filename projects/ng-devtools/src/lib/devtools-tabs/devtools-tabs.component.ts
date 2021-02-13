8df20a7f864af42fd15037dbdb6026d93d1cdb45
AleksanderBodurri <alex.bodurri@mail.utoronto.ca>
2021-02-13 05:22:04 -0500

    refactor(devtools): clean up mat-tab csss

--- projects/ng-devtools/src/lib/devtools-tabs/devtools-tabs.component.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/devtools-tabs.component.ts
@@ -1,22 +1,25 @@
-import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Events, MessageBus } from 'protocol';
-import { MatTabGroup } from '@angular/material/tabs';
 import { DirectiveExplorerComponent } from './directive-explorer/directive-explorer.component';
 import { ApplicationEnvironment } from '../application-environment';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { TabUpdate } from './tab-update';
 import { Theme, ThemeService } from '../theme-service';
 import { Subscription } from 'rxjs';
+import { MatTabNav } from '@angular/material/tabs';
 
 @Component({
   selector: 'ng-devtools-tabs',
   templateUrl: './devtools-tabs.component.html',
   styleUrls: ['./devtools-tabs.component.scss'],
 })
-export class DevToolsTabsComponent implements OnInit, OnDestroy {
+export class DevToolsTabsComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() angularVersion: string | undefined = undefined;
-  @ViewChild(MatTabGroup) tabGroup: MatTabGroup;
   @ViewChild(DirectiveExplorerComponent) directiveExplorer: DirectiveExplorerComponent;
+  @ViewChild('navBar', { static: true }) navbar: MatTabNav;
+
+  tabs = ['Components', 'Profiler'];
+  activeTab: 'Components' | 'Profiler' = 'Components';
 
   inspectorRunning = false;
 
@@ -34,12 +37,21 @@ export class DevToolsTabsComponent implements OnInit, OnDestroy {
     this._currentThemeSubscription = this.themeService.currentTheme.subscribe((theme) => (this.currentTheme = theme));
   }
 
+  ngAfterViewInit(): void {
+    this.navbar.disablePagination = true;
+  }
+
   ngOnDestroy(): void {
     this._currentThemeSubscription.unsubscribe();
   }
 
   get latestSHA(): string {
     return this._applicationEnvironment.environment.process.env.LATEST_SHA;
+  }
+
+  onTabChange(tab: 'Profiler' | 'Components'): void {
+    this.activeTab = tab;
+    this.tabUpdate.notify();
   }
 
   toggleInspector(): void {
@@ -50,7 +62,7 @@ export class DevToolsTabsComponent implements OnInit, OnDestroy {
   emitInspectorEvent(): void {
     if (this.inspectorRunning) {
       this._messageBus.emit('inspectorStart');
-      this.tabGroup.selectedIndex = 0;
+      this.activeTab = 'Components';
     } else {
       this._messageBus.emit('inspectorEnd');
       this._messageBus.emit('removeHighlightOverlay');
