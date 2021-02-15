e15aba9230aa0c7f284de6a815f3767ba11dc89e
Alexey Elin <alexey.elin@list.ru>
2021-02-15 12:34:51 +0300

    docs: format null as code fragment + fix description of returned value

--- packages/core/src/di/metadata.ts
+++ packages/core/src/di/metadata.ts
@@ -71,14 +71,14 @@ export interface OptionalDecorator {
   /**
    * Parameter decorator to be used on constructor parameters,
    * which marks the parameter as being an optional dependency.
-   * The DI framework provides null if the dependency is not found.
+   * The DI framework provides `null` if the dependency is not found.
    *
    * Can be used together with other parameter decorators
    * that modify how dependency injection operates.
    *
    * @usageNotes
    *
-   * The following code allows the possibility of a null result:
+   * The following code allows the possibility of a `null` result:
    *
    * <code-example path="core/di/ts/metadata_spec.ts" region="Optional">
    * </code-example>
@@ -118,7 +118,7 @@ export interface SelfDecorator {
    * which tells the DI framework to start dependency resolution from the local injector.
    *
    * Resolution works upward through the injector hierarchy, so the children
-   * of this class must configure their own providers or be prepared for a null result.
+   * of this class must configure their own providers or be prepared for a `null` result.
    *
    * @usageNotes
    *
@@ -216,7 +216,7 @@ export interface HostDecorator {
    *
    * @usageNotes
    *
-   * The following shows use with the `@Optional` decorator, and allows for a null result.
+   * The following shows use with the `@Optional` decorator, and allows for a `null` result.
    *
    * <code-example path="core/di/ts/metadata_spec.ts" region="Host">
    * </code-example>
