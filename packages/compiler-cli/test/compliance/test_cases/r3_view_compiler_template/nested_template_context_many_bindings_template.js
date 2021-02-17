cdf1ea1951fb7187b1f6c9bb8a847c859c41e0b8
Kristiyan Kostadinov <crisbeto@abv.bg>
2021-02-17 11:23:18 +0100

    refactor(compiler): retrieve variables from context inside nested template listener (#40833)
    This is a pre-requisite for #40360. Given the following template which has a listener
    that references a variable from a parent template (`name`):
    
    ```
    <ng-template let-name="name">
      <button (click)="hello(name)"></button>
    </ng-template>
    ```
    
    We generate code that looks that looks like. Note how we access `name` through `ctx`:
    
    ```js
    function template(rf, ctx) {
      if (rf & 1) {
        const r0 = ɵɵgetCurrentView();
        ɵɵelementStart(0, "button", 2);
        ɵɵlistener("click", function() {
          ɵɵrestoreView(r0);
          const name_r0 = ctx.name; // Note the `ctx.name` access here.
          const ctx_r1 = ɵɵnextContext();
          return ctx_r1.log(name_r0);
        });
        ɵɵelementEnd();
      }
    }
    ```
    
    This works fine at the moment, because the template context object can't be changed after creation.
    The changes in #40360 allow for the object to be changed, which means that the `ctx` reference
    inside the listener will be out of date, because it was bound during creation mode.
    
    This PR aims to address the issue by accessing the context inside listeners through the saved
    view reference. With the new code, the generated code from above will look as follows:
    
    ```js
    function template(rf, ctx) {
      if (rf & 1) {
        const r0 = ɵɵgetCurrentView();
        ɵɵelementStart(0, "button", 2);
        ɵɵlistener("click", function() {
          const restoredCtx = ɵɵrestoreView(r0);
          const name_r0 = restoredCtx.name;
          const ctx_r1 = ɵɵnextContext();
          return ctx_r1.log(name_r0);
        });
        ɵɵelementEnd();
      }
    }
    ```
    
    PR Close #40833

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_template/nested_template_context_many_bindings_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_template/nested_template_context_many_bindings_template.js
@@ -3,9 +3,9 @@ function MyComponent_div_0_Template(rf, ctx) {
     const $s$ = $r3$.ɵɵgetCurrentView();
     $r3$.ɵɵelementStart(0, "div", 1);
     $r3$.ɵɵlistener("click", function MyComponent_div_0_Template_div_click_0_listener() {
-      $r3$.ɵɵrestoreView($s$);
-      const $d$ = ctx.$implicit;
-      const $i$ = ctx.index;
+      const $sr$ = $r3$.ɵɵrestoreView($s$);
+      const $d$ = $sr$.$implicit;
+      const $i$ = $sr$.index;
       const $comp$ = $r3$.ɵɵnextContext();
       return $comp$._handleClick($d$, $i$);
     });
