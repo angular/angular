3d8ac06e4b3d50170cc2a1237db1d622c394a77b
Keen Yee Liau <kyliau@umich.edu>
2021-02-22 14:13:43 -0800

    refactor(language-service): Clean up getTcb interface (#40954)
    `GetTcbResponse` should not be a union type with `undefined`.
    
    PR Close #40954

--- packages/language-service/api.ts
+++ packages/language-service/api.ts
@@ -31,7 +31,7 @@ export type GetTcbResponse = {
    * code, `selections` is empty.
    */
   selections: ts.TextSpan[],
-}|undefined;
+};
 
 export type GetComponentLocationsForTemplateResponse = ts.DocumentSpan[];
 
@@ -40,6 +40,6 @@ export type GetComponentLocationsForTemplateResponse = ts.DocumentSpan[];
  * whose API surface is a strict superset of TypeScript's language service.
  */
 export interface NgLanguageService extends ts.LanguageService {
-  getTcb(fileName: string, position: number): GetTcbResponse;
+  getTcb(fileName: string, position: number): GetTcbResponse|undefined;
   getComponentLocationsForTemplate(fileName: string): GetComponentLocationsForTemplateResponse;
 }
