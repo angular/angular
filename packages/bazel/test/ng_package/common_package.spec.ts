596dfb88a62a7632d975654f96e88146bc2de94e
Joey Perrott <josephperrott@gmail.com>
2021-02-22 14:27:30 -0800

    test(bazel): use bazel runfiles helper for resolving file names (#40955)
    Use bazel's runfiles helper to resolve file name paths rather than
    require.
    
    PR Close #40955

--- packages/bazel/test/ng_package/common_package.spec.ts
+++ packages/bazel/test/ng_package/common_package.spec.ts
@@ -11,10 +11,13 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as shx from 'shelljs';
 
+/** Runfiles helper from bazel to resolve file name paths.  */
+const runfiles = require(process.env['BAZEL_NODE_RUNFILES_HELPER']!);
+
 // Resolve the "npm_package" directory by using the runfile resolution. Note that we need to
 // resolve the "package.json" of the package since otherwise NodeJS would resolve the "main"
 // file, which is not necessarily at the root of the "npm_package".
-shx.cd(path.dirname(require.resolve('angular/packages/common/npm_package/package.json')));
+shx.cd(path.dirname(runfiles.resolve('angular/packages/common/npm_package/package.json')));
 
 describe('@angular/common ng_package', () => {
   describe('should have the locales files', () => {
