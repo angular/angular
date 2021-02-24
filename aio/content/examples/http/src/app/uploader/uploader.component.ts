e1bc93fdb499b1abe81a3f8698a31117e1dff5ab
Kapunahele Wong <kapunahelewong@gmail.com>
2021-02-24 10:20:28 -0500

    docs: improve accessibility of http example (#41057)
    PR Close #41057

--- aio/content/examples/http/src/app/uploader/uploader.component.ts
+++ aio/content/examples/http/src/app/uploader/uploader.component.ts
@@ -4,6 +4,7 @@ import { UploaderService } from './uploader.service';
 @Component({
   selector: 'app-uploader',
   templateUrl: './uploader.component.html',
+  styles: ['input[type=file] { font-size: 1.2rem; margin-top: 1rem; display: block; }'],
   providers: [ UploaderService ]
 })
 export class UploaderComponent {
