be8893fd1dbe1620c880bd067c02f168d5809d62
twerske <pokepsi@gmail.com>
2021-02-26 09:21:54 -0800

    docs: add youtube to social icons (#40987)
    PR Close #40987

--- aio/src/app/app.module.ts
+++ aio/src/app/app.module.ts
@@ -127,6 +127,20 @@ export const svgIconProviders = [
     },
     multi: true,
   },
+  {
+    provide: SVG_ICONS,
+    useValue: {
+      namespace: 'logos',
+      name: 'youtube',
+      svgSource:
+        '<svg focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">' +
+          '<path d="M21.58 7.19c-.23-.86-.91-1.54-1.77-1.77C18.25 5 12 5 12 5s-6.25 0-7.81.42c-.86.23-1.54.91-1.77 1.77' +
+            'C2 8.75 2 12 2 12s0 3.25.42 4.81c.23.86.91 1.54 1.77 1.77C5.75 19 12 19 12 19s6.25 0 7.81-.42' +
+            'c.86-.23 1.54-.91 1.77-1.77C22 15.25 22 12 22 12s0-3.25-.42-4.81zM10 15V9l5.2 3-5.2 3z" />' +
+        '</svg>',
+    },
+    multi: true,
+  },
 ];
 // tslint:enable: max-line-length
 
