4637df55516c3fbc82397c79b61b5c96f2af01c4
Andrew Kushnir <akushnir@google.com>
2021-03-01 19:06:37 -0800

    test(forms): add integration test app to keep track of payload size (#41045)
    This commit adds a Forms-based test app into the `integration` folder to have an ability to measure and keep
    track of payload size for the changes in Forms package.
    
    PR Close #41045

---
+++ integration/forms/src/test.ts
@@ -0,0 +1,20 @@
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+declare const require: any;
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
+// Then we find all the tests.
+const context = require.context('./', true, /\.spec\.ts$/);
+// And load the modules.
+context.keys().map(context);
