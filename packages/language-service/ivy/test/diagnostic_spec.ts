9322e6a076228cc5ea212f3cbe3bbdb6bb8cea82
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-03 11:54:40 -0800

    fix(language-service): don't show external template diagnostics in ts files (#41070)
    The compiler considers template diagnostics to "belong" to the source file
    of the component using the template. This means that when diagnostics for
    a source file are reported, it returns diagnostics of TS structures in the
    actual source file, diagnostics for any inline templates, and diagnostics of
    any external templates.
    
    The Language Service uses a different model, and wants to show template
    diagnostics in the actual .html file. Thus, it's not necessary (and in fact
    incorrect) to include such diagnostics for the actual .ts file as well.
    Doing this currently causes a bug where external diagnostics appear in the
    TS file with "random" source spans.
    
    This commit changes the Language Service to filter the set of diagnostics
    returned by the compiler and only include those diagnostics with spans
    actually within the .ts file itself.
    
    Fixes #41032
    
    PR Close #41070

--- packages/language-service/ivy/test/diagnostic_spec.ts
+++ packages/language-service/ivy/test/diagnostic_spec.ts
@@ -75,6 +75,44 @@ describe('getSemanticDiagnostics', () => {
     expect(diags).toEqual([]);
   });
 
+  it('should not report external template diagnostics on the TS file', () => {
+    const files = {
+      'app.ts': `
+        import {Component, NgModule} from '@angular/core';
+
+        @Component({
+          templateUrl: './app.html'
+        })
+        export class AppComponent {}
+      `,
+      'app.html': '{{nope}}'
+    };
+
+    const project = createModuleAndProjectWithDeclarations(env, 'test', files);
+    const diags = project.getDiagnosticsForFile('app.ts');
+    expect(diags).toEqual([]);
+  });
+
+  it('should report diagnostics in inline templates', () => {
+    const files = {
+      'app.ts': `
+        import {Component, NgModule} from '@angular/core';
+
+        @Component({
+          template: '{{nope}}',
+        })
+        export class AppComponent {}
+      `
+    };
+    const project = createModuleAndProjectWithDeclarations(env, 'test', files);
+    const diags = project.getDiagnosticsForFile('app.ts');
+    expect(diags.length).toBe(1);
+    const {category, file, messageText} = diags[0];
+    expect(category).toBe(ts.DiagnosticCategory.Error);
+    expect(file?.fileName).toBe('/test/app.ts');
+    expect(messageText).toBe(`Property 'nope' does not exist on type 'AppComponent'.`);
+  });
+
   it('should report member does not exist in external template', () => {
     const files = {
       'app.ts': `
@@ -179,11 +217,17 @@ describe('getSemanticDiagnostics', () => {
     };
 
     const project = env.addProject('test', files);
-    const diags = project.getDiagnosticsForFile('app.ts');
-    expect(diags.map(x => x.messageText).sort()).toEqual([
-      'Parser Error: Bindings cannot contain assignments at column 8 in [{{nope = false}}] in /test/app1.html@0:0',
-      'Parser Error: Bindings cannot contain assignments at column 8 in [{{nope = true}}] in /test/app2.html@0:0'
-    ]);
+    const diags1 = project.getDiagnosticsForFile('app1.html');
+    expect(diags1.length).toBe(1);
+    expect(diags1[0].messageText)
+        .toBe(
+            'Parser Error: Bindings cannot contain assignments at column 8 in [{{nope = false}}] in /test/app1.html@0:0');
+
+    const diags2 = project.getDiagnosticsForFile('app2.html');
+    expect(diags2.length).toBe(1);
+    expect(diags2[0].messageText)
+        .toBe(
+            'Parser Error: Bindings cannot contain assignments at column 8 in [{{nope = true}}] in /test/app2.html@0:0');
   });
 
   it('reports a diagnostic for a component without a template', () => {
