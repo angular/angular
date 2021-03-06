f274b5b1c1d3d3d2045f4834f465d8cd8a9d1e53
Igor Minar <igor@angular.io>
2021-03-05 20:02:21 -0800

    refactor(core): don't use innerHTML in DOMTestComponentRenderer
    The use of innerHTML is unnecessary and causes TrustedType violations.

--- packages/platform-browser-dynamic/testing/src/dom_test_component_renderer.ts
+++ packages/platform-browser-dynamic/testing/src/dom_test_component_renderer.ts
@@ -20,16 +20,15 @@ export class DOMTestComponentRenderer extends TestComponentRenderer {
   }
 
   insertRootElement(rootElId: string) {
-    const template = getDOM().getDefaultDocument().createElement('template');
-    template.innerHTML = `<div id="${rootElId}"></div>`;
-    const rootEl = <HTMLElement>getContent(template).firstChild;
+    const rootElement = getDOM().getDefaultDocument().createElement('div');
+    rootElement.setAttribute('id', rootElId);
 
     // TODO(juliemr): can/should this be optional?
     const oldRoots = this._doc.querySelectorAll('[id^=root]');
     for (let i = 0; i < oldRoots.length; i++) {
       getDOM().remove(oldRoots[i]);
     }
-    this._doc.body.appendChild(rootEl);
+    this._doc.body.appendChild(rootElement);
   }
 }
 
