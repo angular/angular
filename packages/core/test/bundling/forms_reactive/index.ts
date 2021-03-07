fa3689f43281a42a4e557d5ee7835149619144d9
Andrew Kushnir <akushnir@google.com>
2021-03-07 14:33:10 -0800

    test(forms): split Forms example app into Reactive and Template-driven ones (#41108)
    One of the main goals of the bundling tests is to verify that unused symbols are tree-shaken away in prod bundles.
    Currently both Reactive and Template-driven test apps are merged into one. In order to make these tree-shaking
    tests even more useful, this commit splits exiting test app into two, so that we can further optimize sets of
    symbols that should be retained in both scenarios.
    
    PR Close #41108

--- packages/core/test/bundling/forms_reactive/index.ts
+++ packages/core/test/bundling/forms_reactive/index.ts
@@ -6,76 +6,36 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import {Component, NgModule, ɵNgModuleFactory as NgModuleFactory} from '@angular/core';
-import {FormArray, FormBuilder, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {BrowserModule, platformBrowser} from '@angular/platform-browser';
-
-@Component({
-  selector: 'app-template-forms',
-  template: `
-  <form novalidate>
-    <div ngModelGroup="profileForm">
-      <div>
-        First Name:
-        <input name="first" ngModel required />
-      </div>
-      <div>
-        Last Name:
-        <input name="last" ngModel />
-      </div>
-      <div>
-        Subscribe:
-        <input name="subscribed" type="checkbox" ngModel />
-      </div>
-
-      <div>Disabled: <input name="foo" ngModel disabled /></div>
-
-      <div *ngFor="let city of addresses; let i = index">
-        City <input [(ngModel)]="addresses[i].city" name="name" />
-      </div>
-
-      <button (click)="addCity()">Add City</button>
-    </div>
-  </form>`,
-})
-class TemplateFormsComponent {
-  name = {first: 'Nancy', last: 'Drew', subscribed: true};
-  addresses = [{city: 'Toronto'}];
-  constructor() {
-    // We use this reference in our test
-    (window as any).templateFormsComponent = this;
-  }
-
-  addCity() {
-    this.addresses.push(({city: ''}));
-  }
-}
 
 @Component({
   selector: 'app-reactive-forms',
   template: `
-  <form [formGroup]="profileForm">
-    <div>
-      First Name:
-      <input type="text" formControlName="firstName" />
-    </div>
-    <div>
-      Last Name:
-      <input type="text" formControlName="lastName" />
-    </div>
-
-    <div>
-      Subscribe:
-      <input type="checkbox" formControlName="subscribed" />
-    </div>
-
-    <div>Disabled: <input formControlName="disabledInput" /></div>
-    <div formArrayName="addresses">
-      <div *ngFor="let item of itemControls; let i = index" [formGroupName]="i">
-        <div>City: <input formControlName="city" /></div>
+    <form [formGroup]="profileForm">
+      <div>
+        First Name:
+        <input type="text" formControlName="firstName" />
       </div>
-    </div>
-    <button (click)="addCity()">Add City</button>
-  </form>`,
+      <div>
+        Last Name:
+        <input type="text" formControlName="lastName" />
+      </div>
+
+      <div>
+        Subscribe:
+        <input type="checkbox" formControlName="subscribed" />
+      </div>
+
+      <div>Disabled: <input formControlName="disabledInput" /></div>
+      <div formArrayName="addresses">
+        <div *ngFor="let item of itemControls; let i = index" [formGroupName]="i">
+          <div>City: <input formControlName="city" /></div>
+        </div>
+      </div>
+      <button (click)="addCity()">Add City</button>
+    </form>
+  `
 })
 class ReactiveFormsComponent {
   profileForm!: FormGroup;
@@ -117,7 +77,6 @@ class ReactiveFormsComponent {
 @Component({
   selector: 'app-root',
   template: `
-    <app-template-forms></app-template-forms>
     <app-reactive-forms></app-reactive-forms>
   `
 })
@@ -125,8 +84,8 @@ class RootComponent {
 }
 
 @NgModule({
-  declarations: [RootComponent, TemplateFormsComponent, ReactiveFormsComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule]
+  declarations: [RootComponent, ReactiveFormsComponent],
+  imports: [BrowserModule, ReactiveFormsModule]
 })
 class FormsExampleModule {
   ngDoBootstrap(app: any) {
