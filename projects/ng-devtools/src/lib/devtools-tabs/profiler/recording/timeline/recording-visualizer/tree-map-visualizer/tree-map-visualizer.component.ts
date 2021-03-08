794d06ab1b963e06c11d6670cddda0b872c47f9a
AleksanderBodurri <alex.bodurri@mail.utoronto.ca>
2021-03-08 02:44:27 -0500

    refactor(devtools): refactor tree-map visualizer to use ResizeObserver instead of window resize listener

--- projects/ng-devtools/src/lib/devtools-tabs/profiler/recording/timeline/recording-visualizer/tree-map-visualizer/tree-map-visualizer.component.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/profiler/recording/timeline/recording-visualizer/tree-map-visualizer/tree-map-visualizer.component.ts
@@ -1,62 +1,61 @@
-import { AfterViewInit, Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+/// <reference types="resize-observer-browser" />
+import { Component, ElementRef, Input, NgZone, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as treemap from 'webtreemap/build/webtreemap';
 import { TreeMapNode, TreeMapFormatter } from '../../record-formatter/tree-map-formatter';
-import { Subject } from 'rxjs';
-import { throttleTime } from 'rxjs/operators';
 import { ProfilerFrame } from 'protocol';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'ng-tree-map-visualizer',
   templateUrl: './tree-map-visualizer.component.html',
   styleUrls: ['./tree-map-visualizer.component.scss'],
 })
-export class TreeMapVisualizerComponent implements AfterViewInit, OnInit {
+export class TreeMapVisualizerComponent implements OnInit, OnDestroy {
   private _formatter = new TreeMapFormatter();
 
   @Input() set frame(frame: ProfilerFrame) {
     // first element in data is the Application node
     this.treeMapRecords = this._formatter.formatFrame(frame);
     if (this.tree) {
-      this.updateTree();
+      this._renderTree();
     }
   }
 
-  private resize$ = new Subject<void>();
+  constructor(private _ngZone: NgZone) {}
 
+  private resize$ = new Subject<void>();
+  private _throttledResizeSubscription: Subscription;
+
+  private _resizeObserver: ResizeObserver = new ResizeObserver(() => this._ngZone.run(() => this.resize$.next()));
   treeMapRecords: TreeMapNode;
 
-  @ViewChild('webTree') tree: ElementRef;
-
-  ngAfterViewInit(): void {
-    this.updateTree();
-  }
+  @ViewChild('webTree', { static: true }) tree: ElementRef;
 
   ngOnInit(): void {
-    this.resize$
-      .asObservable()
-      .pipe(throttleTime(100))
-      .subscribe((_) => this.updateTree());
+    this._throttledResizeSubscription = this.resize$.pipe(debounceTime(100)).subscribe(() => this._renderTree());
+    this._resizeObserver.observe(this.tree.nativeElement);
   }
 
-  updateTree(): void {
-    this.removeTree();
-    this.createTree();
+  ngOnDestroy(): void {
+    this._throttledResizeSubscription.unsubscribe();
+    this._resizeObserver.unobserve(this.tree.nativeElement);
   }
 
-  createTree(): void {
-    treemap.render(this.tree.nativeElement, this.treeMapRecords as any, {
+  private _renderTree(): void {
+    this._removeTree();
+    this._createTree();
+  }
+
+  private _removeTree(): void {
+    Array.from(this.tree.nativeElement.children).forEach((child: HTMLElement) => child.remove());
+  }
+
+  private _createTree(): void {
+    treemap.render(this.tree.nativeElement, this.treeMapRecords, {
       padding: [20, 5, 5, 5],
       caption: (node) => `${node.id}: ${node.size.toFixed(3)} ms`,
       showNode: () => true,
     });
-  }
-
-  removeTree(): void {
-    Array.from(this.tree.nativeElement.children).forEach((child: HTMLElement) => child.remove());
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(): void {
-    this.resize$.next();
   }
 }
