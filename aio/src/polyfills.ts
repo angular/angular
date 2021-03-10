77ec678010044abf9cfc42a9cc203e9274aa5e32
George Kalpakas <kalpakas.g@gmail.com>
2021-03-10 21:53:25 +0200

    fix(docs-infra): avoid unnecessarily loading Custom Elements ES5 shim
    The custom elements spec is not compatible with ES5 style classes. This
    means ES2015 code compiled to ES5 will not work with a native
    implementation of Custom Elements. To support browsers that natively
    support Custom Elements but not ES2015 modules, we load
    `@webcomponents/custom-elements/src/native-shim.js`, which minimally
    augments the native implementation to be compatible with ES5 code.
    (See [here][1] for more details.)
    
    Previously, the shim was included in `polyfills.ts`, which meant it was
    loaded in all browsers (even those supporting ES2015 modules and thus
    not needing the shim).
    
    This commit moves the shim from `polyfills.ts` to a `nomodule` script
    tag in `index.html`. This will ensure that it is only loaded in browsers
    that do not support ES2015 modules and thus do not needed the shim.
    
    NOTE:
    This commit also reduces size of the polyfills bundle by ~400B
    (52609B --> 52215B).
    
    [1]: https://www.npmjs.com/package/@webcomponents/custom-elements#es5-vs-es2015

--- aio/src/polyfills.ts
+++ aio/src/polyfills.ts
@@ -65,11 +65,3 @@ import 'zone.js';  // Included with Angular CLI.
  */
 // Custom Elements polyfill. Required for browsers that do not natively support Custom Elements.
 import '@webcomponents/custom-elements';
-// Custom Element ES5 shim. Required for browsers that natively support Custom Elements, but do not
-// support ES2015 modules.
-// NOTE: Chrome, Firefox and Safari should not need this, because they added support for ES2015
-//       modules before Custom Elements. It is still required for some other (less common) browsers:
-//       - UC browser for android 11.8 (~3.5% global usage)
-//       - Samsung browser 5.0-8.1 (~0.43% global usage)
-//       - Opera 41-47 (~0.02% global usage)
-import '@webcomponents/custom-elements/src/native-shim';
