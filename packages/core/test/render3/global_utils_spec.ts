a1e0f49e62d78d7ade080cbd82066cefd311e3ee
mgechev <mgechev@gmail.com>
2021-03-11 18:43:23 -0800

    perf(core): add private hooks around user code executed by the runtime (#41255)
    Introduces an **internal**, **experimental** `profiler` function, which
    the runtime invokes around user code, including before and after:
    - Running the template function of a component
    - Executing a lifecycle hook
    - Evaluating an output handler
    
    The `profiler` function invokes a callback set with the global
    `ng.ɵsetProfiler`. This API is **private** and **experimental** and
    could be removed or changed at any time.
    
    This implementation is cheap and available in production. It's cheap
    because the `profiler` function is simple, which allows the JiT compiler
    to inline it in the callsites. It also doesn't add up much to the
    production bundle.
    
    To listen for profiler events:
    
    ```ts
    ng.ɵsetProfiler((event, ...args) => {
      // monitor user code execution
    });
    ```
    
    PR Close #41255

--- packages/core/test/render3/global_utils_spec.ts
+++ packages/core/test/render3/global_utils_spec.ts
@@ -6,6 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {setProfiler} from '@angular/core/src/render3/profiler';
 import {applyChanges} from '../../src/render3/util/change_detection_utils';
 import {getComponent, getContext, getDirectives, getHostElement, getInjector, getListeners, getOwningComponent, getRootComponents} from '../../src/render3/util/discovery_utils';
 import {GLOBAL_PUBLISH_EXPANDO_KEY, GlobalDevModeContainer, publishDefaultGlobalUtils, publishGlobalUtil} from '../../src/render3/util/global_utils';
@@ -59,6 +60,10 @@ describe('global utils', () => {
 
     it('should publish applyChanges', () => {
       assertPublished('applyChanges', applyChanges);
+    });
+
+    it('should publish ɵsetProfiler', () => {
+      assertPublished('ɵsetProfiler', setProfiler);
     });
   });
 });
