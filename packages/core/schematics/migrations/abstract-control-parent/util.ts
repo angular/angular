190fa07b9a416defb581d2bf76d1deef7baefce6
Kristiyan Kostadinov <crisbeto@abv.bg>
2021-03-15 18:16:48 +0100

    feat(router): add migration for ActivatedRouteSnapshot.fragment (#41092)
    Adds a migration that casts the value of `ActivatedRouteSnapshot.fragment` to be non-nullable.
    
    Also moves some code from the `AbstractControl.parent` migration so that it can be reused.
    
    Relates to #37336.
    
    PR Close #41092

--- packages/core/schematics/migrations/abstract-control-parent/util.ts
+++ packages/core/schematics/migrations/abstract-control-parent/util.ts
@@ -8,14 +8,11 @@
 
 import {normalize} from 'path';
 import * as ts from 'typescript';
+import {isNullCheck, isSafeAccess} from '../../utils/typescript/nodes';
+import {hasOneOfTypes, isNullableType} from '../../utils/typescript/symbol';
 
 /** Names of symbols from `@angular/forms` whose `parent` accesses have to be migrated. */
-const abstractControlSymbols = new Set<string>([
-  'AbstractControl',
-  'FormArray',
-  'FormControl',
-  'FormGroup',
-]);
+const abstractControlSymbols = ['AbstractControl', 'FormArray', 'FormControl', 'FormGroup'];
 
 /**
  * Finds the `PropertyAccessExpression`-s that are accessing the `parent` property in
@@ -38,78 +35,6 @@ export function findParentAccesses(
   return results;
 }
 
-/** Checks whether a node's type is nullable (`null`, `undefined` or `void`). */
-function isNullableType(typeChecker: ts.TypeChecker, node: ts.Node) {
-  // Skip expressions in the form of `foo.bar!.baz` since the `TypeChecker` seems
-  // to identify them as null, even though the user indicated that it won't be.
-  if (node.parent && ts.isNonNullExpression(node.parent)) {
-    return false;
-  }
-
-  const type = typeChecker.getTypeAtLocation(node);
-  const typeNode = typeChecker.typeToTypeNode(type, undefined, ts.NodeBuilderFlags.None);
-  let hasSeenNullableType = false;
-
-  // Trace the type of the node back to a type node, walk
-  // through all of its sub-nodes and look for nullable tyes.
-  if (typeNode) {
-    (function walk(current: ts.Node) {
-      if (current.kind === ts.SyntaxKind.NullKeyword ||
-          current.kind === ts.SyntaxKind.UndefinedKeyword ||
-          current.kind === ts.SyntaxKind.VoidKeyword) {
-        hasSeenNullableType = true;
-        // Note that we don't descend into type literals, because it may cause
-        // us to mis-identify the root type as nullable, because it has a nullable
-        // property (e.g. `{ foo: string | null }`).
-      } else if (!hasSeenNullableType && !ts.isTypeLiteralNode(current)) {
-        current.forEachChild(walk);
-      }
-    })(typeNode);
-  }
-
-  return hasSeenNullableType;
-}
-
-/**
- * Checks whether a particular node is part of a null check. E.g. given:
- * `control.parent ? control.parent.value : null` the null check would be `control.parent`.
- */
-function isNullCheck(node: ts.PropertyAccessExpression): boolean {
-  if (!node.parent) {
-    return false;
-  }
-
-  // `control.parent && control.parent.value` where `node` is `control.parent`.
-  if (ts.isBinaryExpression(node.parent) && node.parent.left === node) {
-    return true;
-  }
-
-  // `control.parent && control.parent.parent && control.parent.parent.value`
-  // where `node` is `control.parent`.
-  if (node.parent.parent && ts.isBinaryExpression(node.parent.parent) &&
-      node.parent.parent.left === node.parent) {
-    return true;
-  }
-
-  // `if (control.parent) {...}` where `node` is `control.parent`.
-  if (ts.isIfStatement(node.parent) && node.parent.expression === node) {
-    return true;
-  }
-
-  // `control.parent ? control.parent.value : null` where `node` is `control.parent`.
-  if (ts.isConditionalExpression(node.parent) && node.parent.condition === node) {
-    return true;
-  }
-
-  return false;
-}
-
-/** Checks whether a property access is safe (e.g. `foo.parent?.value`). */
-function isSafeAccess(node: ts.PropertyAccessExpression): boolean {
-  return node.parent != null && ts.isPropertyAccessExpression(node.parent) &&
-      node.parent.expression === node && node.parent.questionDotToken != null;
-}
-
 /** Checks whether a property access is on an `AbstractControl` coming from `@angular/forms`. */
 function isAbstractControlReference(
     typeChecker: ts.TypeChecker, node: ts.PropertyAccessExpression): boolean {
@@ -119,37 +44,14 @@ function isAbstractControlReference(
   // If such a node is found, we check whether the type is one of the `AbstractControl` symbols
   // and whether it comes from the `@angular/forms` directory in the `node_modules`.
   while (ts.isPropertyAccessExpression(current)) {
-    const type = typeChecker.getTypeAtLocation(current.expression);
-    const symbol = type.getSymbol();
-    if (symbol && type) {
+    const symbol = typeChecker.getTypeAtLocation(current.expression)?.getSymbol();
+    if (symbol) {
       const sourceFile = symbol.valueDeclaration?.getSourceFile();
       return sourceFile != null &&
           formsPattern.test(normalize(sourceFile.fileName).replace(/\\/g, '/')) &&
-          hasAbstractControlType(typeChecker, type);
+          hasOneOfTypes(typeChecker, current.expression, abstractControlSymbols);
     }
     current = current.expression;
   }
   return false;
-}
-
-/**
- * Walks through the sub-types of a type, looking for a type that
- * has the same name as one of the `AbstractControl` types.
- */
-function hasAbstractControlType(typeChecker: ts.TypeChecker, type: ts.Type): boolean {
-  const typeNode = typeChecker.typeToTypeNode(type, undefined, ts.NodeBuilderFlags.None);
-  let hasMatch = false;
-  if (typeNode) {
-    (function walk(current: ts.Node) {
-      if (ts.isIdentifier(current) && abstractControlSymbols.has(current.text)) {
-        hasMatch = true;
-        // Note that we don't descend into type literals, because it may cause
-        // us to mis-identify the root type as nullable, because it has a nullable
-        // property (e.g. `{ foo: FormControl }`).
-      } else if (!hasMatch && !ts.isTypeLiteralNode(current)) {
-        current.forEachChild(walk);
-      }
-    })(typeNode);
-  }
-  return hasMatch;
 }
--- packages/core/schematics/utils/typescript/nodes.ts
+++ packages/core/schematics/utils/typescript/nodes.ts
@@ -26,3 +26,43 @@ export function closestNode<T extends ts.Node>(node: ts.Node, kind: ts.SyntaxKin
 
   return null;
 }
+
+/**
+ * Checks whether a particular node is part of a null check. E.g. given:
+ * `foo.bar ? foo.bar.value : null` the null check would be `foo.bar`.
+ */
+export function isNullCheck(node: ts.Node): boolean {
+  if (!node.parent) {
+    return false;
+  }
+
+  // `foo.bar && foo.bar.value` where `node` is `foo.bar`.
+  if (ts.isBinaryExpression(node.parent) && node.parent.left === node) {
+    return true;
+  }
+
+  // `foo.bar && foo.bar.parent && foo.bar.parent.value`
+  // where `node` is `foo.bar`.
+  if (node.parent.parent && ts.isBinaryExpression(node.parent.parent) &&
+      node.parent.parent.left === node.parent) {
+    return true;
+  }
+
+  // `if (foo.bar) {...}` where `node` is `foo.bar`.
+  if (ts.isIfStatement(node.parent) && node.parent.expression === node) {
+    return true;
+  }
+
+  // `foo.bar ? foo.bar.value : null` where `node` is `foo.bar`.
+  if (ts.isConditionalExpression(node.parent) && node.parent.condition === node) {
+    return true;
+  }
+
+  return false;
+}
+
+/** Checks whether a property access is safe (e.g. `foo.parent?.value`). */
+export function isSafeAccess(node: ts.Node): boolean {
+  return node.parent != null && ts.isPropertyAccessExpression(node.parent) &&
+      node.parent.expression === node && node.parent.questionDotToken != null;
+}
