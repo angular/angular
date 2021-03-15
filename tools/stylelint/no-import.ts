b2622b4d19c1840857bcddd46c1ffc7ebd334367
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-15 18:57:55 +0100

    build: add lint rule to prevent @import usage (#22222)
    Adds a custom lint rule that doesn't allow for `@import` to be used.

---
+++ tools/stylelint/no-import.ts
@@ -0,0 +1,37 @@
+import {createPlugin, utils} from 'stylelint';
+import {basename} from 'path';
+
+const ruleName = 'material/no-import';
+const messages = utils.ruleMessages(ruleName, {
+  expected: () => '@import is not allowed. Use @use instead.',
+});
+
+/** Stylelint plugin that doesn't allow `@import` to be used. */
+const plugin = createPlugin(ruleName, (isEnabled: boolean, options?: {exclude?: string}) => {
+  return (root, result) => {
+    if (!isEnabled) {
+      return;
+    }
+
+    const excludePattern = options?.exclude ? new RegExp(options.exclude) : null;
+
+    if (excludePattern?.test(basename(root.source!.input.file!))) {
+      return;
+    }
+
+    root.walkAtRules(rule => {
+      if (rule.name === 'import') {
+        utils.report({
+          result,
+          ruleName,
+          message: messages.expected(),
+          node: rule
+        });
+       }
+    });
+  };
+});
+
+plugin.ruleName = ruleName;
+plugin.messages = messages;
+export default plugin;
