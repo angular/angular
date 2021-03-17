ff8c1f4f566f77f01e8f42f3961f3fe72ab25378
George Kalpakas <kalpakas.g@gmail.com>
2021-03-17 22:00:19 +0200

    fixup! docs: make `HeroService#gerHero()` in `toh-pt5` compatible with TS strict mode

--- aio/content/examples/toh-pt5/src/app/hero.service.ts
+++ aio/content/examples/toh-pt5/src/app/hero.service.ts
@@ -19,7 +19,8 @@ export class HeroService {
 
   // #docregion getHero
   getHero(id: number): Observable<Hero> {
-    // TODO: Handle failing to find a hero with the specified `id`.
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
     const hero = HEROES.find(h => h.id === id) as Hero;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
