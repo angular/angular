37dea7ccd73fe766bc3f24a723cac4eac0018f68
AleksanderBodurri <alex.bodurri@mail.utoronto.ca>
2021-03-18 15:30:01 -0400

    fix(devtools): sort array indexes in ascending order

--- projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/property-resolver/property-data-source.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/property-resolver/property-data-source.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
 import { DefaultIterableDiffer } from '@angular/core';
 import { diff } from '../../diffing';
 import { FlatNode, Property } from './element-property-resolver';
-import { arrayify } from './arrayify';
+import { arrayifyProps } from './arrayify-props';
 
 const trackBy = (_: number, item: FlatNode) => {
   return `#${item.prop.name}#${item.level}`;
@@ -27,7 +27,7 @@ export class PropertyDataSource extends DataSource<FlatNode> {
     private _messageBus: MessageBus<Events>
   ) {
     super();
-    this._data.next(this._treeFlattener.flattenNodes(arrayify(props)));
+    this._data.next(this._treeFlattener.flattenNodes(arrayifyProps(props)));
   }
 
   get data(): FlatNode[] {
@@ -39,7 +39,7 @@ export class PropertyDataSource extends DataSource<FlatNode> {
   }
 
   update(props: { [prop: string]: Descriptor }): void {
-    const newData = this._treeFlattener.flattenNodes(arrayify(props));
+    const newData = this._treeFlattener.flattenNodes(arrayifyProps(props));
     diff(this._differ, this.data, newData);
     this._data.next(this.data);
   }
@@ -99,7 +99,7 @@ export class PropertyDataSource extends DataSource<FlatNode> {
     this._messageBus.once('nestedProperties', (position: DirectivePosition, data: Properties, _path: string[]) => {
       node.prop.descriptor.value = data.props;
       this._treeControl.expand(node);
-      const props = arrayify(data.props, node.prop);
+      const props = arrayifyProps(data.props, node.prop);
       const flatNodes = this._treeFlattener.flattenNodes(props);
       flatNodes.forEach((f) => (f.level += node.level + 1));
       this.data.splice(index + 1, 0, ...flatNodes);
