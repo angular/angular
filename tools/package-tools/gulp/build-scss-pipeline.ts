902e946dc95c170f7860c48e990742d8f129aa08
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-19 19:26:37 +0100

    build: avoid installing node-sass (#22283)
    We no longer use `node-sass`, but we were still installing it, because `gulp-sass`
    depends upon it. These changes switch to a fork of `gulp-sass` that depends on
    the Dart version of Sass.

--- tools/package-tools/gulp/build-scss-pipeline.ts
+++ tools/package-tools/gulp/build-scss-pipeline.ts
@@ -3,14 +3,11 @@ import {join} from 'path';
 import {buildConfig} from '../build-config';
 
 // These imports lack of type definitions.
-const gulpSass = require('gulp-sass');
+const gulpSass = require('gulp-dart-sass');
 const sass = require('sass');
+const sassIncludePaths = [join(buildConfig.projectDir, 'node_modules/')];
 
-const sassIncludePaths = [
-  join(buildConfig.projectDir, 'node_modules/')
-];
-
-// Set the compiler to our version of `sass`, rather than the one that `gulp-sass` depends on.
+// Set the compiler to our version of `sass`, rather than the one that `gulp-dart-sass` depends on.
 gulpSass.compiler = sass;
 
 /** Create a gulp task that builds SCSS files. */
