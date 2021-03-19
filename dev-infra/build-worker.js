70625772c01cdcd4a0cf110a89f96598e8f6f0f7
Joey Perrott <josephperrott@gmail.com>
2021-03-19 10:01:11 -0700

    fix(dev-infra): remove commit message wizard and builder
    Removing commit message builder and wizard as they are unused and
    unneeded.

--- dev-infra/build-worker.js
+++ dev-infra/build-worker.js
@@ -7,7 +7,6 @@ var fs = require('fs');
 var path = require('path');
 var chalk = _interopDefault(require('chalk'));
 require('inquirer');
-require('inquirer-autocomplete-prompt');
 var shelljs = require('shelljs');
 
 /**
