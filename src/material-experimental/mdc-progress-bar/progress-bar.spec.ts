5b7b03ea7d3f2e923e24b6d9286b796173b72582
Dominik Rabij <dominik.rabij@gmail.com>
2021-03-19 19:57:00 +0100

    fix(material/progress-bar): Support ChromeVox (#22166)
    * fix(material/progress-bar): Support ChromeVox
    
    * refactor(material/progress-bar): wrap elements with aria-hidden div
    
    * fix(material-experimental/mdc-progress-bar): Support ChromeVox #22165
    
    * test(material/progress-bar): add ChromeVox support tests
    
    * test(material-experimental/mdc-progress-bar): add ChromeVox support test

--- src/material-experimental/mdc-progress-bar/progress-bar.spec.ts
+++ src/material-experimental/mdc-progress-bar/progress-bar.spec.ts
@@ -17,6 +17,19 @@ describe('MDC-based MatProgressBar', () => {
     return TestBed.createComponent<T>(componentType);
   }
 
+  // All children need to be hidden for screen readers in order to support ChromeVox.
+  // More context in the issue: https://github.com/angular/components/issues/22165.
+  it('should have elements wrapped in aria-hidden div', () => {
+    const fixture = createComponent(BasicProgressBar);
+    const host = fixture.nativeElement as Element;
+    const element = host.children[0];
+    expect(element.children.length).toBe(1);
+
+    const div = element.querySelector('div')!;
+    expect(div).toBeTruthy();
+    expect(div.getAttribute('aria-hidden')).toBe('true');
+  });
+
   describe('with animation', () => {
     describe('basic progress-bar', () => {
       it('should apply a mode of "determinate" if no mode is provided.', () => {
