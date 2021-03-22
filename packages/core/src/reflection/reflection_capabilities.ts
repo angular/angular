c772708c9db81b9dfd27d2e4d8721d0b56bd8718
JoostK <joost.koehoorn@gmail.com>
2021-03-22 18:49:49 +0100

    fixup! fix(core): detect synthesized constructors that have been downleveled using TS 4.2

--- packages/core/src/reflection/reflection_capabilities.ts
+++ packages/core/src/reflection/reflection_capabilities.ts
@@ -49,7 +49,7 @@ import {GetterFn, MethodFn, SetterFn} from './types';
  * More details can be found in: https://github.com/angular/angular/issues/38453.
  */
 export const ES5_DELEGATE_CTOR =
-    /^function\s+\S+\(\)\s*{[\s\S]+\.apply\(this,\s*(arguments|(?:[^()]+\(\[\])?[^()]+\(arguments\))\)/;
+    /^function\s+\S+\(\)\s*{[\s\S]+\.apply\(this,\s*(arguments|(?:[^()]+\(\[\],)?[^()]+\(arguments\))\)/;
 /** Regular expression that detects ES2015 classes which extend from other classes. */
 export const ES2015_INHERITED_CLASS = /^class\s+[A-Za-z\d$_]*\s*extends\s+[^{]+{/;
 /**
