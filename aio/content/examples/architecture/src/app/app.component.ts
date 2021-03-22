01e546f11633e081148ee8c0bcfc0a03a9e0ec97
Kapunahele Wong <kapunahelewong@gmail.com>
2021-03-22 11:01:38 -0400

    docs: improve accessibility of architecture example (#41314)
    PR Close #41314

--- aio/content/examples/architecture/src/app/app.component.ts
+++ aio/content/examples/architecture/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <h1>Architecture Example</h1>
     <app-hero-list></app-hero-list>
     <app-sales-tax></app-sales-tax>
   `
