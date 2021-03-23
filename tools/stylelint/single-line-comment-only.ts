3c975e9304d60a377eb1ca7a4bee08a82c3fbd1d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-23 16:35:48 +0100

    build: enforce that we use comments that won't show up in the generated output (#22309)
    Adds a custom stylelint rule that will prevent multi-line comments (`/* */`) from being used in .scss files. The problem with such comments are preserved by Sass and will show up in the user's output, whereas single-line comments (`//`) will not.
    
    **Note:** while Stylelint has a `comment-pattern` rule already, we can't use it because the rule works on the comment text which doesn't include the syntax.

---
+++ tools/stylelint/single-line-comment-only.ts
@@ -0,0 +1,43 @@
+import {createPlugin, utils} from 'stylelint';
+import {basename} from 'path';
+
+const ruleName = 'material/single-line-comment-only';
+const messages = utils.ruleMessages(ruleName, {
+  expected: () => 'Multi-line comments are not allowed (e.g. /* */). ' +
+                  'Use single-line comments instead (//).',
+});
+
+/**
+ * Stylelint plugin that doesn't allow multi-line comments to
+ * be used, because they'll show up in the user's output.
+ */
+const plugin = createPlugin(ruleName, (isEnabled: boolean, options?: {filePattern?: string}) => {
+  return (root, result) => {
+    if (!isEnabled) {
+      return;
+    }
+
+    const filePattern = options?.filePattern ? new RegExp(options.filePattern) : null;
+
+    if (filePattern && !filePattern?.test(basename(root.source!.input.file!))) {
+      return;
+    }
+
+    root.walkComments(comment => {
+      // The `raws.inline` property isn't in the typing so we need to cast to any. Also allow
+      // comments starting with `!` since they're used to tell minifiers to preserve the comment.
+      if (!(comment.raws as any).inline && !comment.text.startsWith('!')) {
+        utils.report({
+          result,
+          ruleName,
+          message: messages.expected(),
+          node: comment
+        });
+      }
+    });
+  };
+});
+
+plugin.ruleName = ruleName;
+plugin.messages = messages;
+export default plugin;
