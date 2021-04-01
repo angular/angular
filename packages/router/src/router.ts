d4386fef457be82bed3032523a432904f5fa0903
josemontespg <64992551+josemontespg@users.noreply.github.com>
2021-04-01 15:57:58 -0300

    docs(router): make getCurrentNavigation() jsdoc more explicit
    Add additional clarification to the documentation for
    `getCurrentNavigation`.

--- packages/router/src/router.ts
+++ packages/router/src/router.ts
@@ -1027,7 +1027,10 @@ export class Router {
     return this.serializeUrl(this.currentUrlTree);
   }
 
-  /** The current Navigation object if one exists */
+  /**
+   * Returns the current `Navigation` object when the router is navigating,
+   * and `null` when idle.
+   */
   getCurrentNavigation(): Navigation|null {
     return this.currentNavigation;
   }
