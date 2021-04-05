2e03a591a482f1c83866bd19b23cf17764912c4e
George Kalpakas <kalpakas.g@gmail.com>
2021-04-05 19:05:54 +0300

    build: make the build scripts for the various packages consistent
    This commit makes the build scripts for the various packages (framework,
    `@angular/dev-infra-private`, `angular-in-memory-web-api`, `zone.js`)
    consistent. This makes it easier to maintain them (e.g. make similar
    changes across all build scripts).

--- scripts/build/build-packages-dist.js
+++ scripts/build/build-packages-dist.js
@@ -9,22 +9,22 @@
 
 'use strict';
 
-const {buildZoneJsPackage} = require('./zone-js-builder');
+const {buildAngularInMemoryWebApiPackage} = require('./angular-in-memory-web-api');
 const {buildDevInfraPackage} = require('./dev-infra-builder');
 const {buildTargetPackages} = require('./package-builder');
-const {buildAngularInMemoryWebAPIPackage} = require('./angular-in-memory-web-api');
+const {buildZoneJsPackage} = require('./zone-js-builder');
 
 
 // Build the legacy (view engine) npm packages into `dist/packages-dist/`.
 buildTargetPackages('dist/packages-dist', false, 'Production');
 
+// Build the `angular-dev-infra` npm package into `dist/packages-dist/`.
+buildDevInfraPackage('dist/packages-dist');
+
+// Build the `angular-in-memory-web-api` npm package into `dist/packages-dist/misc/`, because it
+// might be needed by other scripts/targets.
+buildAngularInMemoryWebApiPackage('dist/packages-dist/misc');
+
 // Build the `zone.js` npm package into `dist/zone.js-dist/`, because it might be needed by other
 // scripts/tests.
 buildZoneJsPackage('dist/zone.js-dist');
-
-// Build the `angular-dev-infra` npm package into `dist/packages-dist/@angular/dev-infra-private`
-buildDevInfraPackage();
-
-// Build the `angular-in-memory-web-api` npm package into
-// `dist/packages-dist/misc/angular-in-memory-web-api`
-buildAngularInMemoryWebAPIPackage();
