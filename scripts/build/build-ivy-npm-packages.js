e7179893f92c626e471971324e5087a26d72b616
George Kalpakas <kalpakas.g@gmail.com>
2021-04-05 19:05:54 +0300

    build: build `angular-in-memory-web-api` when building packages with Ivy (#41429)
    This commit updates the `build-ivy-npm-packages.js` script to also build
    the `angular-in-memory-web-api` package. This could be used if someone
    wanted to test the Ivy packages in docs examples or an integration test.
    
    PR Close #41429

--- scripts/build/build-ivy-npm-packages.js
+++ scripts/build/build-ivy-npm-packages.js
@@ -9,12 +9,22 @@
 
 'use strict';
 
-const {buildZoneJsPackage} = require('./zone-js-builder');
+const {buildAngularInMemoryWebApiPackage} = require('./angular-in-memory-web-api');
 const {buildTargetPackages} = require('./package-builder');
+const {buildZoneJsPackage} = require('./zone-js-builder');
 
 
 // Build the ivy packages into `dist/packages-dist-ivy-aot/`.
 buildTargetPackages('dist/packages-dist-ivy-aot', true, 'Ivy AOT');
+
+// Build the `angular-in-memory-web-api` npm package into `dist/packages-dist-ivy-aot/misc/`,
+// because it might be needed by other scripts/targets.
+//
+// NOTE:
+// The `-ivy-aot` suffix is only used to differentiate from the packages built by the
+// `build-packages-dist.js` script, so that there is no conflict when persisting them to the
+// workspace on CI.
+buildAngularInMemoryWebApiPackage('dist/packages-dist-ivy-aot/misc');
 
 // Build the `zone.js` npm package into `dist/zone.js-dist-ivy-aot/`, because it might be needed by
 // other scripts/tests.
