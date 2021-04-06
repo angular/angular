5519131a727abd72f900fd1c4774d0b43cacc184
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-04-05 21:05:18 -0600

    fix(material-experimental/mdc-paginator): rename module file (#22387)

--- src/material-experimental/mdc-paginator/public-api.ts
+++ src/material-experimental/mdc-paginator/public-api.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-export * from './paginator-module';
+export * from './module';
 export * from './paginator';
 export {
   MatPaginatorIntl,
