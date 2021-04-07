d19d46d776a3837d5e1e592684cf9ea2bc478766
Joey Perrott <josephperrott@gmail.com>
2021-04-07 10:56:10 -0700

    fixup! test(dev-infra): extract commit message build function into testing util function

--- dev-infra/commit-message/test-util.ts
+++ dev-infra/commit-message/test-util.ts
@@ -21,7 +21,7 @@ export interface CommitMessageParts {
  * Generate a commit message builder function, using the provided defaults.
  */
 export function commitMessageBuilder(defaults: CommitMessageParts) {
-  return (params: Partial<CommitMessageParts> = defaults) => {
+  return (params: Partial<CommitMessageParts> = {}) => {
     const {prefix, type, npmScope, scope, summary, body, footer} = {...defaults, ...params};
     const scopeSlug = npmScope ? `${npmScope}/${scope}` : scope;
     return `${prefix}${type}${scopeSlug ? '(' + scopeSlug + ')' : ''}: ${summary}\n\n${body}\n\n${
