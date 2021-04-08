0f739dbc9b717e1a2e9bf93178f4f602b2af7a04
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-08 18:46:06 +0200

    refactor(form-field): remove unnecessary bindings (#16941)
    Removes the `mat-accent` and `mat-warn` host bindings from the `mat-form-field` since they're already handled by the `Color` mixin.

--- src/material/form-field/form-field.ts
+++ src/material/form-field/form-field.ts
@@ -137,8 +137,6 @@ export const MAT_FORM_FIELD = new InjectionToken<MatFormField>('MatFormField');
     '[class.mat-form-field-disabled]': '_control.disabled',
     '[class.mat-form-field-autofilled]': '_control.autofilled',
     '[class.mat-focused]': '_control.focused',
-    '[class.mat-accent]': 'color == "accent"',
-    '[class.mat-warn]': 'color == "warn"',
     '[class.ng-untouched]': '_shouldForward("untouched")',
     '[class.ng-touched]': '_shouldForward("touched")',
     '[class.ng-pristine]': '_shouldForward("pristine")',
