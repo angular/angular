b006630583f4e9ff41126ec9cbeafd86440d0d15
iRealNirmal <nil4u.you@gmail.com>
2021-04-11 11:29:20 +0530

    feature(compiler-cli): Add support for $ in attribute

--- packages/compiler/src/selector.ts
+++ packages/compiler/src/selector.ts
@@ -13,7 +13,7 @@ const _SELECTOR_REGEXP = new RegExp(
         '(([\\.\\#]?)[-\\w]+)|' +  // 2: "tag"; 3: "."/"#";
         // "-" should appear first in the regexp below as FF31 parses "[.-\w]" as a range
         // 4: attribute; 5: attribute_string; 6: attribute_value
-        '(?:\\[([-.\\w*]+)(?:=([\"\']?)([^\\]\"\']*)\\5)?\\])|' +  // "[name]", "[name=value]",
+        '(?:\\[([-.\\w$*]+)(?:=([\"\']?)([^\\]\"\']*)\\5)?\\])|' +  // "[name]", "[name=value]",
                                                                    // "[name="value"]",
                                                                    // "[name='value']"
         '(\\))|' +                                                 // 7: ")"
