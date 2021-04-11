1e7e17702ff30d707cebf8d79cdaca2640fe60f9
Alan Cohen <acohen@opentext.com>
2021-04-10 21:26:09 -0400

    docs: change ActivatedRouteStub code sample to work with strictNullChecks on
    convertToParamMap() does not accept undefined.
    Fix code sample so it can be used in strict mode without requiring changes.

--- aio/content/examples/testing/src/testing/activated-route-stub.ts
+++ aio/content/examples/testing/src/testing/activated-route-stub.ts
@@ -15,14 +15,16 @@ export class ActivatedRouteStub {
   private subject = new ReplaySubject<ParamMap>();
 
   constructor(initialParams?: Params) {
-    this.setParamMap(initialParams);
+    if (initialParams) {
+      this.setParamMap(initialParams);
+    }
   }
 
   /** The mock paramMap observable */
   readonly paramMap = this.subject.asObservable();
 
-  /** Set the paramMap observables's next value */
-  setParamMap(params?: Params) {
+  /** Set the paramMap observable's next value */
+  setParamMap(params: Params) {
     this.subject.next(convertToParamMap(params));
   }
 }
