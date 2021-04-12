5f6774bfdebb2da4a9decd3a820c1302565f29d7
Alan Cohen <acohen@opentext.com>
2021-04-12 18:35:06 -0400

    fixup! docs: change ActivatedRouteStub code sample to work with strictNullChecks on

--- aio/content/examples/testing/src/testing/activated-route-stub.ts
+++ aio/content/examples/testing/src/testing/activated-route-stub.ts
@@ -15,16 +15,14 @@ export class ActivatedRouteStub {
   private subject = new ReplaySubject<ParamMap>();
 
   constructor(initialParams?: Params) {
-    if (initialParams) {
       this.setParamMap(initialParams);
-    }
   }
 
   /** The mock paramMap observable */
   readonly paramMap = this.subject.asObservable();
 
   /** Set the paramMap observable's next value */
-  setParamMap(params: Params) {
+  setParamMap(params: Params = {}) {
     this.subject.next(convertToParamMap(params));
   }
 }
