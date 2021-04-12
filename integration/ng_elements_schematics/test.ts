d64a377132817ec836835aebed4f7751c3a415f1
Alan Agius <alan.agius4@gmail.com>
2021-04-12 11:22:24 +0200

    test: update CLI integration tests to run only for Ivy
    In Angular CLI 12, application can only be compiled using Ivy, therefore we shouldn't run these tests when Bazel runs with View Engine context.

--- integration/ng_elements_schematics/test.ts
+++ integration/ng_elements_schematics/test.ts
@@ -46,7 +46,7 @@ for (let p of nodePackages) {
 }
 
 // TODO(alan-agius4): remove once the main repo dependency on `@angular-devkut/build-angular` is `0.1200.0-next.9` or greater.
-packages['@angular-devkit/build-angular'] = 'file:../../../node_modules/@angular-devkit/build-angular';
+packages['@angular-devkit/build-angular'] = `file:${__dirname}/../../node_modules/@angular-devkit/build-angular`;
 
 // Clean up previously run test
 cd(__dirname);
