9a3b82f19d26819c95c340d702c1c32787f2931e
Kristiyan Kostadinov <crisbeto@abv.bg>
2021-04-12 17:36:31 +0200

    perf(compiler): reduce amount of generated code for safe accesses and nullish coalescing (#41563)
    This is follow-up from #41437 and it reduces the amount of code we generate for safe property accesses (`a?.b`) and nullish coalescing (`a ?? b`) by:
    1. Reusing variables in nested nullish coalescing expressions.
    2. Not initializing temporary variables to `null`. The way our code is generated means that the value will always be overwritten before we compare against it so the initializer didn't really matter.
    
    Fixes #41491.
    
    PR Close #41563

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler/nullish_coalescing/nullish_coalescing_host_bindings.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler/nullish_coalescing/nullish_coalescing_host_bindings.js
@@ -1,13 +1,12 @@
 hostBindings: function MyApp_HostBindings(rf, ctx) {
   if (rf & 1) {
     i0.ɵɵlistener("click", function MyApp_click_HostBindingHandler() {
-      let tmp_b_0 = null;
-      let tmp_b_1 = null;
-      return ctx.logLastName((tmp_b_0 = (tmp_b_1 = ctx.lastName) !== null && tmp_b_1 !== undefined ? tmp_b_1 : ctx.lastNameFallback) !== null && tmp_b_0 !== undefined ? tmp_b_0 : "unknown");
+      let $tmp$;
+      return ctx.logLastName(($tmp$ = ($tmp$ = ctx.lastName) !== null && $tmp$ !== undefined ? $tmp$ : ctx.lastNameFallback) !== null && $tmp$ !== undefined ? $tmp$ : "unknown");
     });
   }
   if (rf & 2) {
-    let tmp_b_0 = null;
-    i0.ɵɵattribute("first-name", "Hello, " + ((tmp_b_0 = ctx.firstName) !== null && tmp_b_0 !== undefined ? tmp_b_0 : "Frodo") + "!");
+    let $tmp$;
+    i0.ɵɵattribute("first-name", "Hello, " + (($tmp$ = ctx.firstName) !== null && $tmp$ !== undefined ? $tmp$ : "Frodo") + "!");
   }
 }
