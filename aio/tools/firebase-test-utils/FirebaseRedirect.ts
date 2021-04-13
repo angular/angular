37cedd37fa5bdb4476a7b0c528c773df8c4d6dea
Alan Agius <alan.agius4@gmail.com>
2021-04-13 15:49:43 +0200

    build(docs-infra): add correct types to tuples
    This is to fix the below error:
    ```
    tools/firebase-test-utils/FirebaseRedirect.ts:17:50 - error TS2345: Argument of type '(string | RegExp)[][]' is not assignable to parameter of type 'ReplacementDetail[]'.
      Type '(string | RegExp)[]' is missing the following properties from type 'ReplacementDetail': 0, 1
    
    17     return XRegExp.replaceEach(this.destination, [...paramReplacers, ...restReplacers]);
    ```
    
    https://app.circleci.com/pipelines/github/angular/angular/31076/workflows/5fd3851e-ae9e-4d77-b0ef-366ba38a9088/jobs/961795

--- aio/tools/firebase-test-utils/FirebaseRedirect.ts
+++ aio/tools/firebase-test-utils/FirebaseRedirect.ts
@@ -12,8 +12,9 @@ export class FirebaseRedirect {
       return undefined;
     }
 
-    const paramReplacers = Object.keys(this.glob.namedParams).map(name => [ XRegExp(`:${name}`, 'g'), match[name] ]);
-    const restReplacers = Object.keys(this.glob.restParams).map(name => [ XRegExp(`:${name}\\*`, 'g'), match[name] ]);
+    XRegExp.uninstall('namespacing');
+    const paramReplacers = Object.keys(this.glob.namedParams).map<[RegExp, string]>(name => [ XRegExp(`:${name}`, 'g'), match[name] ]);
+    const restReplacers = Object.keys(this.glob.restParams).map<[RegExp, string]>(name => [ XRegExp(`:${name}\\*`, 'g'), match[name] ]);
     return XRegExp.replaceEach(this.destination, [...paramReplacers, ...restReplacers]);
   }
 }
