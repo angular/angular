70341bcfedb0744149d0794ef5482ee28b997182
Pete Bacon Darwin <pete@bacondarwin.com>
2021-04-18 11:45:29 +0100

    build(docs-infra): update docs examples to Angular v11.2.10 (#41689)
    This commit updates the docs examples to Angular v11.2.10. See the [diff between 11.0.1 and 11.2.10 (FW) and 11.2.9 (CLI)][1].
    
    The changes are fairly trivial including:
    
    - Removal of `emitDecoratorMetadata` from tsconfig.json files, where no JIT compilation is required.
    - Setting `enableI18nLegacyMessageIdFormat` to `false` for CLI based applications - the i18n example was already migrated away from legacy message IDs.
    
    [1]: https://github.com/cexbrayat/angular-cli-diff/compare/11.2.9..11.0.1
    
    PR Close #41689

--- aio/tools/examples/shared/boilerplate/cli/karma.conf.js
+++ aio/tools/examples/shared/boilerplate/cli/karma.conf.js
@@ -13,6 +13,12 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
