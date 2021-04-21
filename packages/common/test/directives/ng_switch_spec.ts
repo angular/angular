640ec7828fb0cfd15a0767f33228f0f79ed043f3
va-stefanek <mateusz.stefanczyk@valueadd.pl>
2021-04-21 21:34:34 +0200

    refactor(common): change missing NgSwitch provider error message (#41704)
    change error message of invalid NgSwitch use and add corner cases tests
    
    PR Close #41704

--- packages/common/test/directives/ng_switch_spec.ts
+++ packages/common/test/directives/ng_switch_spec.ts
@@ -7,8 +7,8 @@
  */
 
 import {CommonModule} from '@angular/common';
-import {Attribute, Component, Directive, TemplateRef, ViewChild} from '@angular/core';
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {Attribute, Component, Directive, TemplateRef, ViewChild,} from '@angular/core';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {expect} from '@angular/platform-browser/testing/src/matchers';
 
 {
@@ -175,6 +175,24 @@ import {expect} from '@angular/platform-browser/testing/src/matchers';
         getComponent().switchValue = 'b';
         detectChangesAndExpectText('when b1;when b2;');
       });
+
+      it('should throw error when ngSwitchCase is used outside of ngSwitch', waitForAsync(() => {
+           const template = '<div [ngSwitch]="switchValue"></div>' +
+               '<div *ngSwitchCase="\'a\'"></div>';
+
+           expect(() => createTestComponent(template))
+               .toThrowError(
+                   'NG0305: An element with the "ngSwitchCase" attribute (matching the "NgSwitchCase" directive) must be located inside an element with the "ngSwitch" attribute (matching "NgSwitch" directive)');
+         }));
+
+      it('should throw error when ngSwitchDefault is used outside of ngSwitch', waitForAsync(() => {
+           const template = '<div [ngSwitch]="switchValue"></div>' +
+               '<div *ngSwitchDefault></div>';
+
+           expect(() => createTestComponent(template))
+               .toThrowError(
+                   'NG0305: An element with the "ngSwitchDefault" attribute (matching the "NgSwitchDefault" directive) must be located inside an element with the "ngSwitch" attribute (matching "NgSwitch" directive)');
+         }));
 
       it('should support nested NgSwitch on ng-container with ngTemplateOutlet', () => {
         fixture = TestBed.createComponent(ComplexComponent);
