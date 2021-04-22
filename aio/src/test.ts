4f20cd16b424fea3cad6398be1901c4197fde87e
Alan Agius <alan.agius4@gmail.com>
2021-04-22 13:16:46 +0200

    build(docs-infra): add `assert` polyfill (#41764)
    With this change we add the `assert` polyfill which is required because `timezone-mock` is a Node.JS library which is being used in Browser.
    
    PR Close #41764

--- aio/src/test.ts
+++ aio/src/test.ts
@@ -7,6 +7,13 @@ import {
     platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
+// Needed for `assert` polyfill uses `process`.
+// See: https://github.com/browserify/commonjs-assert/blob/bba838e9ba9e28edf3127ce6974624208502f6bc/internal/assert/assertion_error.js#L138
+// The `assert` polyfill is needed because of `timezone-mock` which is a Node.JS library but in being used in Browser.
+(globalThis as any).process = {
+    env: {},
+};
+
 declare const require: any;
 
 // First, initialize the Angular testing environment.
