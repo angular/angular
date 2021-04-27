8ac1b48fb3558c193c04dc3e03b481b63505a387
George Kalpakas <kalpakas.g@gmail.com>
2021-04-27 22:50:12 +0300

    test(docs-infra): update `firebase-test-utils` to support regex-based redirects
    This commit updates the utilities in `firebase-test-utils/` to also
    support testing Firebase redirects that are configured using regular
    expressions (via the `regex` property).
    See the [Firebase docs][1] for more details.
    
    [1]: https://firebase.google.com/docs/hosting/full-config#redirects

--- aio/tools/firebase-test-utils/FirebaseRedirect.spec.ts
+++ aio/tools/firebase-test-utils/FirebaseRedirect.spec.ts
@@ -3,18 +3,25 @@ import { FirebaseRedirect } from './FirebaseRedirect';
 describe('FirebaseRedirect', () => {
   describe('replace', () => {
     it('should return undefined if the redirect does not match the url', () => {
-      const redirect = new FirebaseRedirect({source: '/a/b/c', destination: '/x/y/z'});
-      expect(redirect.replace('/1/2/3')).toBe(undefined);
+      const [globRedirect, regexRedirect] = getRedirects('/a/b/c', '^/a/b/c$', '/x/y/z');
+
+      expect(globRedirect.replace('/1/2/3')).toBe(undefined);
+      expect(regexRedirect.replace('/1/2/3')).toBe(undefined);
     });
 
     it('should return the destination if there is a match', () => {
-      const redirect = new FirebaseRedirect({source: '/a/b/c', destination: '/x/y/z'});
-      expect(redirect.replace('/a/b/c')).toBe('/x/y/z');
+      const [globRedirect, regexRedirect] = getRedirects('/a/b/c', '^/a/b/c$', '/x/y/z');
+
+      expect(globRedirect.replace('/a/b/c')).toBe('/x/y/z');
+      expect(regexRedirect.replace('/a/b/c')).toBe('/x/y/z');
     });
 
     it('should inject name params into the destination', () => {
-      const redirect = new FirebaseRedirect({source: '/api/:package/:api-*', destination: '<:package><:api>'});
-      expect(redirect.replace('/api/common/NgClass-directive')).toEqual('<common><NgClass>');
+      const [globRedirect, regexRedirect] =
+          getRedirects('/api/:package/:api-*', '^/api/(?P<package>[^/]+)/(?P<api>[^/]+)-.*$', '<:package><:api>');
+
+      expect(globRedirect.replace('/api/common/NgClass-directive')).toBe('<common><NgClass>');
+      expect(regexRedirect.replace('/api/common/NgClass-directive')).toBe('<common><NgClass>');
     });
 
     it('should inject rest params into the destination', () => {
@@ -28,3 +35,10 @@ describe('FirebaseRedirect', () => {
     });
   });
 });
+
+function getRedirects(globPattern: string, regexPattern: string, destination: string) {
+  return [
+    new FirebaseRedirect({source: globPattern, destination}),
+    new FirebaseRedirect({regex: regexPattern, destination}),
+  ];
+}
