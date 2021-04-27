dc708c12ad65f2a313e11ad14bd46395c3c417c9
George Kalpakas <kalpakas.g@gmail.com>
2021-04-27 22:50:11 +0300

    refactor(docs-infra): prepare `firebase-test-utils` for accepting regex-based redirects (#41842)
    Currently, the utilities for testing Firebase redirects assume that the
    redirects are configured using the glob-based `source` property.
    However, Firebase also supports configuring redirects using regular
    expressions (via the `regex` property).
    See the [Firebase docs][1] for more details.
    
    This commit refactors the utilities in `firebase-test-utils/` to make it
    easy to add support for such regex-based redirect configurations.
    
    [1]: https://firebase.google.com/docs/hosting/full-config#redirects
    
    PR Close #41842

--- aio/tools/firebase-test-utils/FirebaseRedirect.ts
+++ aio/tools/firebase-test-utils/FirebaseRedirect.ts
@@ -1,19 +1,25 @@
 import * as XRegExp from 'xregexp';
-import { FirebaseGlob } from './FirebaseGlob';
+import { FirebaseRedirectConfig } from './FirebaseRedirector';
+import { FirebaseRedirectSource } from './FirebaseRedirectSource';
 
 export class FirebaseRedirect {
-  glob = new FirebaseGlob(this.source);
-  constructor(public source: string, public destination: string) {}
+  source: FirebaseRedirectSource;
+  destination: string;
+
+  constructor({source, destination}: FirebaseRedirectConfig) {
+    this.source = FirebaseRedirectSource.fromGlobPattern(source);
+    this.destination = destination;
+  }
 
   replace(url: string): string | undefined {
-    const match = this.glob.match(url);
+    const match = this.source.match(url);
 
     if (!match) {
       return undefined;
     }
 
-    const paramReplacers = Object.keys(this.glob.namedParams).map<[RegExp, string]>(name => [ XRegExp(`:${name}`, 'g'), match[name] ]);
-    const restReplacers = Object.keys(this.glob.restParams).map<[RegExp, string]>(name => [ XRegExp(`:${name}\\*`, 'g'), match[name] ]);
-    return XRegExp.replaceEach(this.destination, [...paramReplacers, ...restReplacers]);
+    const namedReplacers = this.source.namedGroups.map<[RegExp, string]>(name => [ XRegExp(`:${name}`, 'g'), match[name] ]);
+    const restReplacers = this.source.restNamedGroups.map<[RegExp, string]>(name => [ XRegExp(`:${name}\\*`, 'g'), match[name] ]);
+    return XRegExp.replaceEach(this.destination, [...namedReplacers, ...restReplacers]);
   }
 }
