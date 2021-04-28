9d9518e67e81f11ae364117c11b7ef4d68d4053c
mgechev <mgechev@gmail.com>
2021-04-27 17:49:38 -0700

    feat(devtools): implement negation in the filtering query language

--- projects/ng-devtools/src/lib/devtools-tabs/profiler/recording/timeline/filter.spec.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/profiler/recording/timeline/filter.spec.ts
@@ -15,32 +15,33 @@ describe('filtering', () => {
     it('should return proper result with duration', () => {
       const result = parseFilter('duration  :  =2');
       expect(result.length).toBe(1);
-      expect(result[0][0]).toBe('duration');
-      expect(result[0][1]).toEqual(['=', 2]);
+      expect(result[0][0]).toBe(true);
+      expect(result[0][1]).toBe('duration');
+      expect(result[0][2]).toEqual(['=', 2]);
     });
 
     it('should return proper result with source', () => {
       const result = parseFilter('source  :  foobar');
       expect(result.length).toBe(1);
-      expect(result[0][0]).toBe('source');
-      expect(result[0][1]).toEqual('foobar');
+      expect(result[0][1]).toBe('source');
+      expect(result[0][2]).toEqual('foobar');
     });
 
     it('should return proper result with composite filters', () => {
       const result = parseFilter('source  : foobar  baz duration: >= 4200');
       expect(result.length).toBe(2);
-      expect(result[0][0]).toBe('source');
-      expect(result[0][1]).toEqual('foobar  baz');
+      expect(result[0][1]).toBe('source');
+      expect(result[0][2]).toEqual('foobar  baz');
 
-      expect(result[1][0]).toBe('duration');
-      expect(result[1][1]).toEqual(['>=', 4200]);
+      expect(result[1][1]).toBe('duration');
+      expect(result[1][2]).toEqual(['>=', 4200]);
     });
 
     it('should ignore invalid operators', () => {
       const result1 = parseFilter('sorce  : foobar  baz duration: >= 4200');
       expect(result1.length).toBe(1);
-      expect(result1[0][0]).toBe('duration');
-      expect(result1[0][1]).toEqual(['>=', 4200]);
+      expect(result1[0][1]).toBe('duration');
+      expect(result1[0][2]).toEqual(['>=', 4200]);
 
       const result2 = parseFilter('sorce  : foobar  baz dration: >= 4200');
       expect(result2.length).toBe(0);
@@ -49,11 +50,27 @@ describe('filtering', () => {
     it('should consider decimal values in durations queries with a ms suffix', () => {
       const result = parseFilter('source: foobar duration: >=42.42ms');
       expect(result.length).toBe(2);
-      expect(result[0][0]).toBe('source');
-      expect(result[0][1]).toEqual('foobar');
+      expect(result[0][1]).toBe('source');
+      expect(result[0][2]).toEqual('foobar');
 
-      expect(result[1][0]).toBe('duration');
-      expect(result[1][1]).toEqual(['>=', 42.42]);
+      expect(result[1][1]).toBe('duration');
+      expect(result[1][2]).toEqual(['>=', 42.42]);
+    });
+
+    it('should consider negation', () => {
+      const result = parseFilter(' ! source: foobar !duration: >=42.42ms duration: >=42.42ms');
+      expect(result.length).toBe(3);
+      expect(result[0][0]).toBe(false);
+      expect(result[0][1]).toBe('source');
+      expect(result[0][2]).toEqual('foobar');
+
+      expect(result[1][0]).toBe(false);
+      expect(result[1][1]).toBe('duration');
+      expect(result[1][2]).toEqual(['>=', 42.42]);
+
+      expect(result[2][0]).toBe(true);
+      expect(result[2][1]).toBe('duration');
+      expect(result[2][2]).toEqual(['>=', 42.42]);
     });
   });
 
@@ -183,6 +200,62 @@ describe('filtering', () => {
           frame: {
             directives: [],
             duration: 15,
+            source: 'click',
+          },
+          style: {},
+          toolTip: '',
+        })
+      ).toBeTrue();
+    });
+
+    it('should work with negation', () => {
+      const filter = createFilter('!source:message');
+
+      expect(
+        filter({
+          frame: {
+            directives: [],
+            duration: 15,
+            source: 'message',
+          },
+          style: {},
+          toolTip: '',
+        })
+      ).toBeFalse();
+    });
+
+    it('should work with negation and composite expressions', () => {
+      const filter = createFilter('!duration:=15 !source:message source:click');
+
+      expect(
+        filter({
+          frame: {
+            directives: [],
+            duration: 15,
+            source: 'click',
+          },
+          style: {},
+          toolTip: '',
+        })
+      ).toBeFalse();
+
+      expect(
+        filter({
+          frame: {
+            directives: [],
+            duration: 10,
+            source: 'message',
+          },
+          style: {},
+          toolTip: '',
+        })
+      ).toBeFalse();
+
+      expect(
+        filter({
+          frame: {
+            directives: [],
+            duration: 14,
             source: 'click',
           },
           style: {},
