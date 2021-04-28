75bb931889b946c243161a6ce0503bc7d08a6565
JoostK <joost.koehoorn@gmail.com>
2021-04-28 22:14:38 +0200

    fix(compiler-cli): prefer non-aliased exports in reference emitters (#41866)
    This commit changes the reference emitters in the Ivy compiler to prefer
    non-aliased exports if they exist. This avoids selecting "private
    exports" that may not be stable, e.g. the reexports that have been added
    by the View Engine compiler. Such reexports are not stable and are
    therefore not suitable to be emitted into partial compilations, as the
    output of partial compilations should only reference stable symbols
    from upstream libraries.
    
    An alternative solution has been considered where ViewEngine-generated
    exports would gain a certain prefix, such that the Ivy compiler could
    just exclude those exports (see #41443). However, that solution would
    be insufficient in case a library is built using partial compilation and
    while depending itself on a VE-compiled library from earlier versions of
    Angular, where the magic prefix would be missing. For such libraries,
    ngcc would have generated reexports using the declared name if not already
    present so this change does result in choosing the correct export.
    
    Because ngcc always generates reexports using the declared name even if
    an aliased export is present, this change causes those ngcc-generated
    exports to be chosen in downstream libraries using partial compilation.
    This is unfortunate as it means that the declared names become
    effectively public even if the library author was intentionally
    exporting it using an alias. This commit does not address this problem;
    it is expected that this should not result in widespread issues across
    the library ecosystem.
    
    Fixes #41277
    
    PR Close #41866

--- packages/compiler-cli/src/ngtsc/imports/test/emitter_spec.ts
+++ packages/compiler-cli/src/ngtsc/imports/test/emitter_spec.ts
@@ -7,12 +7,13 @@
  */
 import {ExternalExpr} from '@angular/compiler';
 import * as ts from 'typescript';
+import {UnifiedModulesHost} from '../../core/api';
 
-import {absoluteFrom as _, LogicalFileSystem} from '../../file_system';
+import {absoluteFrom as _, basename, LogicalFileSystem} from '../../file_system';
 import {runInEachFileSystem, TestFile} from '../../file_system/testing';
 import {Declaration, TypeScriptReflectionHost} from '../../reflection';
 import {getDeclaration, makeProgram} from '../../testing';
-import {AbsoluteModuleStrategy, ImportFlags, LogicalProjectStrategy} from '../src/emitter';
+import {AbsoluteModuleStrategy, ImportFlags, LogicalProjectStrategy, RelativePathStrategy, UnifiedModulesStrategy} from '../src/emitter';
 import {Reference} from '../src/references';
 import {ModuleResolver} from '../src/resolver';
 
@@ -47,6 +48,41 @@ runInEachFileSystem(() => {
       const reference = new Reference(decl);
       const emitted = strategy.emit(reference, context, ImportFlags.None);
       expect(emitted).toBeNull();
+    });
+
+    it('should prefer non-aliased exports', () => {
+      const {strategy, program} = makeStrategy([
+        {
+          name: _('/node_modules/external.d.ts'),
+          contents: `
+             declare class Foo {}
+             export {Foo as A};
+             export {Foo};
+             export {Foo as B};
+           `,
+        },
+        {
+          name: _('/context.ts'),
+          contents: 'export class Context {}',
+        },
+      ]);
+      const decl =
+          getDeclaration(program, _('/node_modules/external.d.ts'), 'Foo', ts.isClassDeclaration);
+      const context = program.getSourceFile(_('/context.ts'))!;
+
+      const reference = new Reference(decl, {
+        specifier: 'external',
+        resolutionContext: context.fileName,
+      });
+      const emitted = strategy.emit(reference, context, ImportFlags.None);
+      if (emitted === null) {
+        return fail('Reference should be emitted');
+      }
+      if (!(emitted.expression instanceof ExternalExpr)) {
+        return fail('Reference should be emitted as ExternalExpr');
+      }
+      expect(emitted.expression.value.name).toEqual('Foo');
+      expect(emitted.expression.value.moduleName).toEqual('external');
     });
 
     it('should generate an import using the exported name of the declaration', () => {
@@ -172,6 +208,109 @@ runInEachFileSystem(() => {
 
       // Expect the prefixed name from the TestHost.
       expect((ref!.expression as ExternalExpr).value.name).toEqual('testFoo');
+    });
+
+    it('should prefer non-aliased exports', () => {
+      const {program, host} = makeProgram([
+        {
+          name: _('/index.ts'),
+          contents: `
+             declare class Foo {}
+             export {Foo as A};
+             export {Foo};
+             export {Foo as B};
+           `,
+        },
+        {
+          name: _('/context.ts'),
+          contents: 'export class Context {}',
+        }
+      ]);
+      const checker = program.getTypeChecker();
+      const logicalFs = new LogicalFileSystem([_('/')], host);
+      const strategy = new LogicalProjectStrategy(new TypeScriptReflectionHost(checker), logicalFs);
+      const decl = getDeclaration(program, _('/index.ts'), 'Foo', ts.isClassDeclaration);
+      const context = program.getSourceFile(_('/context.ts'))!;
+      const emitted = strategy.emit(new Reference(decl), context);
+      if (emitted === null) {
+        return fail('Reference should be emitted');
+      }
+      if (!(emitted.expression instanceof ExternalExpr)) {
+        return fail('Reference should be emitted as ExternalExpr');
+      }
+      expect(emitted.expression.value.name).toEqual('Foo');
+      expect(emitted.expression.value.moduleName).toEqual('./index');
+    });
+  });
+
+  describe('RelativePathStrategy', () => {
+    it('should prefer non-aliased exports', () => {
+      const {program} = makeProgram([
+        {
+          name: _('/index.ts'),
+          contents: `
+             declare class Foo {}
+             export {Foo as A};
+             export {Foo};
+             export {Foo as B};
+           `,
+        },
+        {
+          name: _('/context.ts'),
+          contents: 'export class Context {}',
+        }
+      ]);
+      const checker = program.getTypeChecker();
+      const strategy = new RelativePathStrategy(new TypeScriptReflectionHost(checker));
+      const decl = getDeclaration(program, _('/index.ts'), 'Foo', ts.isClassDeclaration);
+      const context = program.getSourceFile(_('/context.ts'))!;
+      const emitted = strategy.emit(new Reference(decl), context);
+      if (emitted === null) {
+        return fail('Reference should be emitted');
+      }
+      if (!(emitted.expression instanceof ExternalExpr)) {
+        return fail('Reference should be emitted as ExternalExpr');
+      }
+      expect(emitted.expression.value.name).toEqual('Foo');
+      expect(emitted.expression.value.moduleName).toEqual('./index');
+    });
+  });
+
+  describe('UnifiedModulesStrategy', () => {
+    it('should prefer non-aliased exports', () => {
+      const {program} = makeProgram([
+        {
+          name: _('/index.ts'),
+          contents: `
+             declare class Foo {}
+             export {Foo as A};
+             export {Foo};
+             export {Foo as B};
+           `,
+        },
+        {
+          name: _('/context.ts'),
+          contents: 'export class Context {}',
+        }
+      ]);
+      const checker = program.getTypeChecker();
+      const host: UnifiedModulesHost = {
+        fileNameToModuleName(importedFilePath): string {
+          return basename(importedFilePath, '.ts');
+        }
+      };
+      const strategy = new UnifiedModulesStrategy(new TypeScriptReflectionHost(checker), host);
+      const decl = getDeclaration(program, _('/index.ts'), 'Foo', ts.isClassDeclaration);
+      const context = program.getSourceFile(_('/context.ts'))!;
+      const emitted = strategy.emit(new Reference(decl), context);
+      if (emitted === null) {
+        return fail('Reference should be emitted');
+      }
+      if (!(emitted.expression instanceof ExternalExpr)) {
+        return fail('Reference should be emitted as ExternalExpr');
+      }
+      expect(emitted.expression.value.name).toEqual('Foo');
+      expect(emitted.expression.value.moduleName).toEqual('index');
     });
   });
 });
