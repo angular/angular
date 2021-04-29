6eafaa7b5e2b85e53b4213f9f7756474609a0fa3
JoostK <joost.koehoorn@gmail.com>
2021-04-29 23:22:35 +0200

    fix(compiler-cli): expose the linker as a Babel plugin (#41918)
    This allows the linker to be used as a true Babel plugin. In a Babel
    configuration file, include the linker as follows:
    
    ```js
    {
      plugins: [
        '@angular/compiler-cli/linker/babel',
      ]
    }
    ```
    
    or, if you need to specify configuration options:
    
    ```js
    {
      plugins: [
        ['@angular/compiler-cli/linker/babel', {linkerJitMode: true}],
      ]
    }
    ```
    
    PR Close #41918

--- packages/compiler-cli/linker/babel/index.ts
+++ packages/compiler-cli/linker/babel/index.ts
@@ -5,4 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+import {defaultLinkerPlugin} from './src/babel_plugin';
+
 export {createEs2015LinkerPlugin} from './src/es2015_linker_plugin';
+export default defaultLinkerPlugin;
