4fac1029668285b80625f83791c266e5063760e5
Joey Perrott <josephperrott@gmail.com>
2021-04-30 11:13:25 -0700

    build: update release configuration to use new release notes system (#41905)
    Update the ng-dev config for release to use the new release notes system
    for generating release notes.
    
    PR Close #41905

--- .ng-dev/release.ts
+++ .ng-dev/release.ts
@@ -1,5 +1,4 @@
 import {join} from 'path';
-import {exec} from 'shelljs';
 import {ReleaseConfig} from '../dev-infra/release/config';
 
 /** Configuration for the `ng-dev release` command. */
@@ -29,9 +28,8 @@ export const release: ReleaseConfig = {
     const {buildTargetPackages} = require(join(__dirname, '../scripts/build/package-builder'));
     return buildTargetPackages('dist/release-output', false, 'Release', true);
   },
-  // TODO: This can be removed once there is an org-wide tool for changelog generation.
-  generateReleaseNotesForHead: async () => {
-    exec('yarn -s gulp changelog', {cwd: join(__dirname, '../')});
+  releaseNotes: {
+    hiddenScopes: ['aio', 'dev-infra', 'docs-infra', 'zone.js'],
   },
   releasePrLabels: ['comp: build & ci', 'action: merge', 'PullApprove: disable'],
 };
