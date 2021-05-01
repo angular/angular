89e495dfff624e71c69e431aafc649829042a6bb
mgechev <mgechev@gmail.com>
2021-04-30 17:41:18 -0700

    build(devtools): update scripts and manifest

--- projects/shell-chrome/set-version.js
+++ projects/shell-chrome/set-version.js
@@ -8,27 +8,44 @@ const MANIFEST_PATH = join(__dirname, 'src/manifest.json');
 
 const manifest = JSON.parse(readFileSync(MANIFEST_PATH).toString());
 
+console.log('Current version', chalk.yellow(manifest.version));
+console.log('Current version name', chalk.yellow(manifest.version_name));
+
+const setVersion = (nextVersion) => {
+  manifest.version = nextVersion;
+  manifest.version_name = nextVersion;
+
+  writeFileSync(MANIFEST_PATH, JSON.stringify(manifest, null, 2));
+};
+
+const answerMap = {
+  yes: true,
+  y: true,
+  no: false,
+  n: false,
+};
+
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-console.log('Current version', chalk.yellow(manifest.version));
-console.log('Current version name', chalk.yellow(manifest.version_name));
-
 rl.question(chalk.yellowBright('Set the current version: '), (nextVersion) => {
-  rl.close();
   if (!semver.valid(nextVersion)) {
     console.error(chalk.red('Invalid version'));
-    process.exit(1);
   }
-  if (semver.lte(nextVersion, manifest.version)) {
-    console.error(chalk.red('Next version cant be smaller than the previous one'));
-    process.exit(1);
+  if (semver.gt(nextVersion, manifest.version)) {
+    rl.close();
+    setVersion(nextVersion);
+    return;
   }
-
-  manifest.version = nextVersion;
-  manifest.version_name = nextVersion;
-
-  writeFileSync(MANIFEST_PATH, JSON.stringify(manifest, null, 2));
+  console.error(chalk.yellow('Next version cannot be smaller or equal to the previous one'));
+  rl.question('Are you sure you want to continue? (y/n) ', (answer) => {
+    rl.close();
+    answer = answer.toLowerCase();
+    if (!answerMap[answer]) {
+      throw new Error('Exiting');
+    }
+    setVersion(nextVersion);
+  });
 });
