958238ca127cd5c44227f1056a65821201839cd2
Kapunahele Wong <kapunahelewong@gmail.com>
2021-05-06 16:11:49 +0300

    feat(docs-infra): add support for unformatted code-snippets
    This commit adds support for skipping formatting in `<aio-code>`
    elements (which are used by `<code-example>` and `<code-pane>` elements
    under the hood) by specifying the `language` option as `'root'`.
    
    This is useful for code-snippets that include plain text.

--- aio/src/app/custom-elements/code/code.component.spec.ts
+++ aio/src/app/custom-elements/code/code.component.spec.ts
@@ -117,6 +117,15 @@ describe('CodeComponent', () => {
       expect(getFormattedCode()).toBe(
           `Formatted code (language: auto, linenums: true): ${bigMultiLineCode}`);
     });
+
+    it('should skip prettify if language is `\'none\'`', () => {
+      hostComponent.setCode(bigMultiLineCode);
+      hostComponent.language = 'none';
+
+      fixture.detectChanges();
+
+      expect(getFormattedCode()).toBe(bigMultiLineCode);
+    });
   });
 
   describe('whitespace handling', () => {
