fab8d018b46b27ed58e8d8e3a009ea224ddde2cb
Minko Gechev <mgechev@gmail.com>
2021-05-10 14:29:16 -0700

    refactor(devtools): better naming and removal of log statement (rangle/angular-devtools#753)

--- projects/ng-devtools/src/lib/devtools-tabs/profiler/recording/timeline/timeline.component.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/profiler/recording/timeline/timeline.component.ts
@@ -29,7 +29,6 @@ export class TimelineComponent implements OnDestroy {
     this._maxDuration = -Infinity;
     this._subscription = data.subscribe({
       next: (frames: ProfilerFrame[]): void => {
-        console.log(frames);
         this._processFrames(frames);
       },
       complete: (): void => {
