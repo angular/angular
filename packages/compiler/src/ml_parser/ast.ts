8ed452bbe170f7316b974b68780920eadc65db75
Pete Bacon Darwin <pete@bacondarwin.com>
2021-05-15 20:37:08 +0100

    refactor(compiler): expose token parts in Text nodes (#43132)
    When it was tokenized, text content is split into parts that can include
    interpolations and encoded entities tokens.
    
    To make this information available to downstream processing, this commit
    adds these tokens to the `Text` AST nodes, with suitable processing.
    
    PR Close #43132

--- packages/compiler/src/ml_parser/ast.ts
+++ packages/compiler/src/ml_parser/ast.ts
@@ -9,6 +9,7 @@
 import {AstPath} from '../ast_path';
 import {I18nMeta} from '../i18n/i18n_ast';
 import {ParseSourceSpan} from '../parse_util';
+import {Token} from './lexer';
 
 interface BaseNode {
   sourceSpan: ParseSourceSpan;
@@ -23,7 +24,8 @@ export abstract class NodeWithI18n implements BaseNode {
 }
 
 export class Text extends NodeWithI18n {
-  constructor(public value: string, sourceSpan: ParseSourceSpan, i18n?: I18nMeta) {
+  constructor(
+      public value: string, sourceSpan: ParseSourceSpan, public tokens: Token[], i18n?: I18nMeta) {
     super(sourceSpan, i18n);
   }
   override visit(visitor: Visitor, context: any): any {
@@ -55,8 +57,8 @@ export class ExpansionCase implements BaseNode {
 export class Attribute extends NodeWithI18n {
   constructor(
       public name: string, public value: string, sourceSpan: ParseSourceSpan,
-      readonly keySpan: ParseSourceSpan|undefined, public valueSpan?: ParseSourceSpan,
-      i18n?: I18nMeta) {
+      readonly keySpan: ParseSourceSpan|undefined, public valueSpan: ParseSourceSpan|undefined,
+      public valueTokens: Token[]|undefined, i18n: I18nMeta|undefined) {
     super(sourceSpan, i18n);
   }
   override visit(visitor: Visitor, context: any): any {
