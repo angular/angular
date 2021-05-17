58be6d7b95d729464c492b94bfda269c2b84131f
David Shevitz <dshevitz@google.com>
2021-05-17 23:04:08 +0000

    docs: add new tutorial that explains how to use the UrlMatcher for custom route matching (#42138)
    PR Close #42138

---
+++ aio/content/examples/routing-with-urlmatcher/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+}
