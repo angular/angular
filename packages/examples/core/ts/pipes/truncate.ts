640c59b9019827dec0069e21dd64903ef48bdb41
Pete Bacon Darwin <pete@bacondarwin.com>
2021-05-22 12:50:10 +0100

    docs(core): tighten up PipeTransform example types (#42240)
    The example was using `any` type but then assuming that the value was a string.
    
    Fixes #42239
    
    PR Close #42240

--- packages/examples/core/ts/pipes/truncate.ts
+++ packages/examples/core/ts/pipes/truncate.ts
@@ -5,12 +5,12 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-
+// #docregion
 import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({name: 'truncate'})
 export class TruncatePipe implements PipeTransform {
-  transform(value: any, length: number, symbol: string) {
+  transform(value: string, length: number, symbol: string) {
     return value.split(' ').slice(0, length).join(' ') + symbol;
   }
 }
