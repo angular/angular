5b0d3356a64c35f6308b95501ad81e89fe3ad7a7
Amir Rustamzadeh <334337+amirrustam@users.noreply.github.com>
2021-05-23 14:39:48 -0400

    ci(devtools): fix flaky tests using cypress orb (rangle/angular-devtools#733)
    * ci: refactor circle config to use cypress orb
    
    * ci: skipping unit tests while validating e2e runs
    
    * ci: fix karma chrome launches
    
    * ci: investigating karma headless failure
    
    * ci: iterating on failure
    
    * ci: move environment to executor
    
    * build: revert karma config changes
    
    * ci: update karma config
    
    Co-authored-by: mgechev <mgechev@gmail.com>

--- projects/shared-utils/karma.conf.js
+++ projects/shared-utils/karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox'],
+      },
+    },
     singleRun: false,
     restartOnFileChange: true,
   });
