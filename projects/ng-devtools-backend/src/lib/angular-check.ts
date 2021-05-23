dbe79f410470942fd45380a5d36515a050a99805
mgechev <mgechev@gmail.com>
2021-05-23 10:48:21 -0700

    fix(devtools): update the prod mode detection to also consider VE apps
    Fix rangle/angular-devtools#798

--- projects/ng-devtools-backend/src/lib/angular-check.ts
+++ projects/ng-devtools-backend/src/lib/angular-check.ts
@@ -23,14 +23,18 @@ export const appIsSupportedAngularVersion = (): boolean => {
 
 /**
  * We check if the global `window.ng` is an object and if this object
- * has the `getComponent` method attached to it. In some g3 apps processed
- * with Closure, `ng` is a function, which means that `typeof ng !== 'undefined'`
- * is not a sufficient check.
+ * has the `getComponent` or `probe` methods attached to it.
+ *
+ * `ng.probe` is a view engine method, but to ensure that we correctly
+ * detect development mode we need to consider older rendering engines.
+ *
+ * In some g3 apps processed with Closure, `ng` is a function,
+ * which means that `typeof ng !== 'undefined'` is not a sufficient check.
  *
  * @returns if the app has global ng debug object
  */
 const appHasGlobalNgDebugObject = (): boolean => {
-  return typeof ng === 'object' && typeof ng.getComponent === 'function';
+  return typeof ng === 'object' && (typeof ng.getComponent === 'function' || typeof ng.probe === 'function');
 };
 
 export const getAngularVersion = (): string | null => {
