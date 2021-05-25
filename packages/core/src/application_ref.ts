21a80ee41ecbaed28cfb04a74f4426e25f655617
Umair Hafeez <umairhafeez.ca@gmail.com>
2021-05-25 01:29:30 -0400

    docs(core): improve applicationref.bootstrap docs
    add proper examples and update usage notes
    add references to the method at relevant places in the docs

--- packages/core/src/application_ref.ts
+++ packages/core/src/application_ref.ts
@@ -682,20 +682,51 @@ export class ApplicationRef {
   }
 
   /**
-   * Bootstrap a new component at the root level of the application.
+   * Bootstrap a component onto the element identified by its selector or, optionally, to a
+   specified
+   * element
    *
    * @usageNotes
    * ### Bootstrap process
    *
-   * When bootstrapping a new root component into an application, Angular mounts the
-   * specified application component onto DOM elements identified by the componentType's
-   * selector and kicks off automatic change detection to finish initializing the component.
+   * When bootstrapping an application with a component, Angular mounts the specified application
+   * component onto a DOM element, and kicks off automatic change detection to finish initializing
+   * the component.
+   *
+   * If not provided in the `rootSelectorOrNode` argument, the selector of the provided
+   * `componentOrFactory` will be used to match the DOM elements.
+   *
+   * Please note that if multiple elements match the selector, the first matched element will be
+   * used as a host element.
+   *
+   * ### Example
+   *
+   * Generally, we define the component to bootstrap in the bootstrap array of `NgModule`,
+   * but it requires us to know the component while writing the application code.
+   *
+   * Imagine a situation where we have to wait for an API call to decide about the component to
+   * bootstrap. We can use the `ngDoBootstrap` hook of the `NgModule` and call this method to
+   * dynamically bootstrap a component.
+   *
+   * {@example core/ts/platform/platform.ts region='componentSelector'}
    *
    * Optionally, a component can be mounted onto a DOM element that does not match the
    * componentType's selector.
    *
-   * ### Example
-   * {@example core/ts/platform/platform.ts region='longform'}
+   * ```typescript
+   * ngDoBootstrap(appRef: ApplicationRef) {
+   *   // it can be a CSS selector
+   *   . . .
+   *   appRef.bootstrap(this.componentMap[componentName], '#root-element');
+   *   . . .
+
+   *   // or it can be a reference to a DOM node
+   *   . . .
+   *   const element = document.querySelector('#root-element');
+   *   appRef.bootstrap(this.componentMap[componentName], element);
+   *   . . .
+   * }
+   * ```
    */
   bootstrap<C>(componentOrFactory: ComponentFactory<C>|Type<C>, rootSelectorOrNode?: string|any):
       ComponentRef<C> {
