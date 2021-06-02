8f24d71142ef7443eeb6614bfda5db3415a43c8b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-02 15:50:07 +0200

    build: simplify generation of closure locale file (#42230)
    In the past, the closure file has been generated so that all individual
    locale files were imported individually. This resulted in a huge
    slow-down in g3 due to the large amount of imports.
    
    With 90bd984ff74f7605d7c08fd9fdbf610ba7fa67a5 this changed so that we
    inline the locale data for the g3 closure locale file. Also the file
    only contained data for locales being supported by Closure. For this a
    list of locales has been extracted from Closure Compiler, as well as a
    list of locale aliases.
    
    This logic is prone to CLDR version updates, and also broke as part of
    the Gulp -> Bazel migration where this logic has been slightly modified
    but caused issues in G3. e.g. a locale `zh-Hant` was requested in g3,
    but the locale data had the name of the alias locale that provided the
    data at index zero (which represents the locale name). Note that the
    locale names at index zero always could differentiate from the requested
    `goog.LOCALE` due to the aliasing logic. This just didn't come up before.
    
    We simplify this logic by generating a `goog.LOCALE` case for all
    locales CLDR provides data for. We don't need to bother about aliasing
    because with the refactorings to the CLDR generation tool, all locales
    are built (which also captures the aliases), and we can generate the locale
    file on the fly (which has not been done before).
    
    PR Close #42230

--- packages/common/locales/generate-locales-tool/bin/write-locale-files-to-dist.ts
+++ packages/common/locales/generate-locales-tool/bin/write-locale-files-to-dist.ts
@@ -31,22 +31,8 @@ function main(outputDir: string) {
   console.info(`Writing locales to: ${outputDir}`);
 
   // Generate locale files for all locales we have data for.
-  cldrData.availableLocales.forEach((locale: string) => {
-    const localeData = cldrData.getLocaleData(locale);
-
-    // If `cldrjs` is unable to resolve a `bundle` for the current locale, then there is no data
-    // for this locale, and it should not be generated. This can happen as with older versions of
-    // CLDR where `availableLocales.json` specifies locales for which no data is available
-    // (even within the `full` tier packages). See:
-    // http://cldr.unicode.org/development/development-process/design-proposals/json-packaging.
-    // TODO(devversion): Remove if we update to CLDR v39 where this seems fixed. Note that this
-    //  worked before in the Gulp tooling without such a check because the `cldr-data-downloader`
-    //  overwrote the `availableLocales` to only capture locales with data.
-    if (localeData && !(localeData.attributes as any).bundle) {
-      console.info(`Skipping generation of ${locale} as there is no data.`);
-      return;
-    }
-
+  cldrData.availableLocales.forEach(localeData => {
+    const locale = localeData.locale;
     const localeFile = generateLocale(locale, localeData, baseCurrencies);
     const localeExtraFile = generateLocaleExtra(locale, localeData);
     const localeGlobalFile = generateLocaleGlobalFile(locale, localeData, baseCurrencies);
