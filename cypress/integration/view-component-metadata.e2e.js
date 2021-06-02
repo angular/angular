5c298d7a936ba6949900b02143df780c2fe35bf2
Sumit Arora <er.sumitarora@gmail.com>
2021-06-01 23:40:38 -0400

    test(devtools): adding e2e tests for inputs, outputs & properties

--- cypress/integration/view-component-metadata.e2e.js
+++ cypress/integration/view-component-metadata.e2e.js
@@ -36,5 +36,71 @@ describe('Viewing component metadata', () => {
         .last()
         .should('have.text', ' Change Detection Strategy: Default');
     });
+
+    it('should display correct set of inputs', () => {
+      cy.get('mat-expansion-panel')
+        .eq(1)
+        .find('mat-expansion-panel-header')
+        .first()
+        .should('have.text', ' @Inputs open_in_new');
+
+      cy.get('mat-expansion-panel')
+        .eq(1)
+        .find('mat-tree-node')
+        .eq(0)
+        .find('span')
+        .eq(0)
+        .should('have.text', ' inputOne ');
+
+      cy.get('mat-expansion-panel')
+        .eq(1)
+        .find('mat-tree-node')
+        .eq(1)
+        .find('span')
+        .eq(0)
+        .should('have.text', ' inputTwo ');
+    });
+
+    it('should display correct set of outputs', () => {
+      cy.get('mat-expansion-panel')
+        .eq(2)
+        .find('mat-expansion-panel-header')
+        .first()
+        .should('have.text', ' @Outputs open_in_new');
+
+      cy.get('mat-expansion-panel')
+        .eq(2)
+        .find('mat-tree-node')
+        .eq(0)
+        .find('span')
+        .eq(0)
+        .should('have.text', ' outputOne ');
+
+      cy.get('mat-expansion-panel')
+        .eq(2)
+        .find('mat-tree-node')
+        .eq(1)
+        .find('span')
+        .eq(0)
+        .should('have.text', ' outputTwo ');
+    });
+
+    it('should display correct set of properties', () => {
+      cy.get('mat-expansion-panel')
+        .eq(3)
+        .find('mat-expansion-panel-header')
+        .first()
+        .should('have.text', ' Properties open_in_new');
+
+      cy.get('mat-expansion-panel')
+        .eq(3)
+        .find('mat-tree-node')
+        .eq(0)
+        .find('span')
+        .eq(0)
+        .should('have.text', ' elementRef ');
+
+      cy.get('mat-expansion-panel').eq(3).find('mat-tree-node').eq(1).find('span').eq(0).should('have.text', ' zippy ');
+    });
   });
 });
