4efd061eafba6f637e1965f924b92c560547a375
MrJithil <jithil@outlook.com>
2021-06-06 23:49:05 +0530

    docs: add links to examples for ComponentFactoryResolver
    Fixes #21705

--- packages/core/src/linker/component_factory_resolver.ts
+++ packages/core/src/linker/component_factory_resolver.ts
@@ -40,6 +40,9 @@ class _NullComponentFactoryResolver implements ComponentFactoryResolver {
  * then use the factory's `create()` method to create a component of that type.
  *
  * @see [Dynamic Components](guide/dynamic-component-loader)
+ * @see [Usage Example](guide/dynamic-component-loader#resolving-components)
+ * @see <live-example name="dynamic-component-loader" noDownload></live-example>
+of the code in this cookbook
  * @publicApi
  */
 export abstract class ComponentFactoryResolver {
