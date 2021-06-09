60dbf017fbca5de55f9e5e95044d780740a0e4b8
JoostK <joost.koehoorn@gmail.com>
2021-06-09 21:25:43 +0200

    fix(compiler-cli): prevent prior compilations from being retained in watch builds (#42537)
    In watch builds, the compiler attempts to reuse as much information from
    a prior compilation as possible. To accomplish this, it keeps a
    reference to the most recently succeeded `TraitCompiler`, which contains
    all analysis data for the program. However, `TraitCompiler` has an
    internal reference to an `IncrementalBuild`, which is itself built on
    top of its prior state. Consequently, all prior compilations continued
    to be referenced, preventing garbage collection from cleaning up these
    instances.
    
    This commit changes the `AnalyzedIncrementalState` to no longer retain
    a `TraitCompiler` instance, but only the analysis data it contains. This
    breaks the retainer path to the prior incremental state, allowing it to
    be garbage collected.
    
    PR Close #42537

--- packages/compiler-cli/src/ngtsc/incremental/test/incremental_spec.ts
+++ packages/compiler-cli/src/ngtsc/incremental/test/incremental_spec.ts
@@ -10,6 +10,7 @@ import {absoluteFrom, getSourceFileOrError} from '../../file_system';
 import {runInEachFileSystem} from '../../file_system/testing';
 import {NOOP_PERF_RECORDER} from '../../perf';
 import {makeProgram} from '../../testing';
+import {TraitCompiler} from '../../transform';
 import {IncrementalCompilation} from '../src/incremental';
 
 runInEachFileSystem(() => {
@@ -20,13 +21,14 @@ runInEachFileSystem(() => {
         {name: FOO_PATH, contents: `export const FOO = true;`},
       ]);
       const fooSf = getSourceFileOrError(program, FOO_PATH);
+      const traitCompiler = {getAnalyzedRecords: () => new Map()} as TraitCompiler;
 
       const versionMapFirst = new Map([[FOO_PATH, 'version.1']]);
       const firstCompilation = IncrementalCompilation.fresh(
           program,
           versionMapFirst,
       );
-      firstCompilation.recordSuccessfulAnalysis(null!);
+      firstCompilation.recordSuccessfulAnalysis(traitCompiler);
       firstCompilation.recordSuccessfulEmit(fooSf);
 
       const versionMapSecond = new Map([[FOO_PATH, 'version.2']]);
@@ -34,7 +36,7 @@ runInEachFileSystem(() => {
           program, versionMapSecond, program, firstCompilation.state, new Set(),
           NOOP_PERF_RECORDER);
 
-      secondCompilation.recordSuccessfulAnalysis(null!);
+      secondCompilation.recordSuccessfulAnalysis(traitCompiler);
       expect(secondCompilation.safeToSkipEmit(fooSf)).toBeFalse();
     });
   });
