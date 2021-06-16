8419cb9266ee0594162208cb7e3acdf7b396f731
Pete Bacon Darwin <pete@bacondarwin.com>
2021-06-16 12:25:13 +0100

    refactor(compiler-cli): ngcc entry-point manifest hash is now configurable (#42582)
    The hash algorithm for the entry-point manifest was hardcoded to `md5`.
    This can now be configured by the `hashAlgorithm` property on the
    ngcc.config.js project configuration.
    
    PR Close #42582

--- packages/compiler-cli/ngcc/src/packages/entry_point_manifest.ts
+++ packages/compiler-cli/ngcc/src/packages/entry_point_manifest.ts
@@ -159,7 +159,7 @@ export class EntryPointManifest {
       const lockFilePath = this.fs.resolve(directory, lockFileName);
       if (this.fs.exists(lockFilePath)) {
         const lockFileContents = this.fs.readFile(lockFilePath);
-        return createHash('md5').update(lockFileContents).digest('hex');
+        return createHash(this.config.hashAlgorithm).update(lockFileContents).digest('hex');
       }
     }
     return null;
