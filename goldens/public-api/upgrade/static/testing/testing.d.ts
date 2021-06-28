41a3ce48a92c53dc48a95f79d781b8da0ebbf845
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-28 19:54:20 +0200

    build: update API goldens to reflect new tool
    Updates the TS API guardian goldens with their equivalents
    based on the new shared dev-infra tool.

--- goldens/public-api/upgrade/static/testing/testing.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function createAngularJSTestingModule(angularModules: any[]): string;
-
-export declare function createAngularTestingModule(angularJSModules: string[], strictDi?: boolean): Type<any>;
