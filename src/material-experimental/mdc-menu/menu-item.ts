a1e8e409d528d46aed672bcac365d994f45367d9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-01 04:08:17 +0200

    fix(material-experimental/mdc-menu): align ripple timings with MDC (#23015)
    Aligns the timings for the MDC-based menu item with MDC.

--- src/material-experimental/mdc-menu/menu-item.ts
+++ src/material-experimental/mdc-menu/menu-item.ts
@@ -6,8 +6,24 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {Component, ChangeDetectionStrategy, ViewEncapsulation} from '@angular/core';
-import {MatMenuItem as BaseMatMenuItem} from '@angular/material/menu';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+  Inject,
+  ElementRef,
+  Optional,
+} from '@angular/core';
+import {
+  MAT_RIPPLE_GLOBAL_OPTIONS,
+  RippleAnimationConfig,
+  RippleGlobalOptions,
+} from '@angular/material-experimental/mdc-core';
+import {MatMenuItem as BaseMatMenuItem, MatMenuPanel, MAT_MENU_PANEL} from '@angular/material/menu';
+import {ANIMATION_MODULE_TYPE} from '@angular/platform-browser/animations';
+import {FocusMonitor} from '@angular/cdk/a11y';
+import {DOCUMENT} from '@angular/common';
+import {numbers} from '@material/ripple';
 
 /**
  * Single item inside of a `mat-menu`. Provides the menu item styling and accessibility treatment.
@@ -37,4 +53,22 @@ import {MatMenuItem as BaseMatMenuItem} from '@angular/material/menu';
   ]
 })
 export class MatMenuItem extends BaseMatMenuItem {
+  _rippleAnimation: RippleAnimationConfig;
+  _noopAnimations: boolean;
+
+  constructor(elementRef: ElementRef<HTMLElement>,
+    @Inject(DOCUMENT) document?: any,
+    focusMonitor?: FocusMonitor,
+    @Inject(MAT_MENU_PANEL) @Optional() parentMenu?: MatMenuPanel<unknown>,
+    @Optional() @Inject(MAT_RIPPLE_GLOBAL_OPTIONS)
+      globalRippleOptions?: RippleGlobalOptions,
+    @Optional() @Inject(ANIMATION_MODULE_TYPE) animationMode?: string) {
+    super(elementRef, document, focusMonitor, parentMenu);
+
+    this._noopAnimations = animationMode === 'NoopAnimations';
+    this._rippleAnimation = globalRippleOptions?.animation || {
+      enterDuration: numbers.DEACTIVATION_TIMEOUT_MS,
+      exitDuration: numbers.FG_DEACTIVATION_MS
+    };
+  }
 }
