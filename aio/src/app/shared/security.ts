c329b349de087c08531a0ecca5f76f9394c474be
Bjarki <bjarki@google.com>
2021-07-06 02:25:29 +0000

    fix(docs-infra): make custom icon registry compatible with Trusted Types
    Change SvgIconInfo.svgSource to be a TrustedHTML and update all its
    users accordingly. Also introduce the svg template tag function for
    building TrustedHTML from constant SVG sources.

--- aio/src/app/shared/security.ts
+++ aio/src/app/shared/security.ts
@@ -1,3 +1,4 @@
+import { assertIsTemplateObject } from 'safevalues/implementation/safe_string_literal';
 import { htmlFromStringKnownToSatisfyTypeContract } from 'safevalues/unsafe/reviewed';
 
 export function fromInnerHTML(el: Element): TrustedHTML {
@@ -6,4 +7,13 @@ export function fromInnerHTML(el: Element): TrustedHTML {
 
 export function fromOuterHTML(el: Element): TrustedHTML {
   return htmlFromStringKnownToSatisfyTypeContract(el.outerHTML, 'existing outerHTML content');
+}
+
+export function svg(constantSvg: TemplateStringsArray): TrustedHTML {
+  // Assert that the argument is a template literal with no interpolation
+  // expressions. Note that this check is not fool-proof, but should catch any
+  // mistakes. Additionally, any attempts at spoofing a template literal should
+  // look odd enough to draw attention during a code review.
+  assertIsTemplateObject(constantSvg, false, 'Must only be used for static SVG markup.');
+  return htmlFromStringKnownToSatisfyTypeContract(constantSvg[0], 'static SVG markup');
 }
