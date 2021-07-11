3e8454dbedf48efa6d433e91e4a106700900ac22
George Kalpakas <kalpakas.g@gmail.com>
2021-07-11 15:00:09 +0300

    fixup! docs(core): fix `ViewChildren` code examples to avoid TS error

--- packages/core/src/metadata/di.ts
+++ packages/core/src/metadata/di.ts
@@ -338,8 +338,8 @@ export interface ViewChildrenDecorator {
    * with `@ViewChildren('cmp')`)
    *   * Any provider defined in the child component tree of the current component (e.g.
    * `@ViewChildren(SomeService) someService!: SomeService`)
-   *   * Any provider defined through a string token (e.g. `@ViewChildren('someToken') someTokenVal!:
-   * any`)
+   *   * Any provider defined through a string token (e.g. `@ViewChildren('someToken')
+   * someTokenVal!: any`)
    *   * A `TemplateRef` (e.g. query `<ng-template></ng-template>` with `@ViewChildren(TemplateRef)
    * template;`)
    *
