f070c203b39260b4771601f0551bde2405b13c1b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-07-12 18:15:05 +0200

    build: fix approve-api golden script
    The API goldens have been flattened as part of 766b07ecd9c15b4c110b339b296843328cb27447,
    but the script for updating goldens has not been updated.
    
    This commit fixes the script so that it respects the new file location
    of goldens.

--- scripts/approve-api-golden.js
+++ scripts/approve-api-golden.js
@@ -3,24 +3,29 @@
 const shelljs = require('shelljs');
 const chalk = require('chalk');
 const path = require('path');
-const packageName = process.argv[2];
+const searchPackageName = process.argv[2];
 const {guessPackageName} = require('./util');
 
-if (!packageName) {
+if (!searchPackageName) {
   console.error(chalk.red('No package name has been passed in for API golden approval.'));
   process.exit(1);
 }
 
 const projectDir = path.join(__dirname, '../');
-const packageNameGuess = guessPackageName(packageName, path.join(projectDir, 'src'));
+const packageNameGuess = guessPackageName(searchPackageName, path.join(projectDir, 'src'));
 
 if (!packageNameGuess.result) {
   console.error(chalk.red(`Could not find package for API golden approval called ` +
-    `${chalk.yellow(packageName)}. Looked in packages: \n${packageNameGuess.attempts.join('\n')}`));
+    `${chalk.yellow(searchPackageName)}. Looked in packages:\n` +
+    `${packageNameGuess.attempts.join('\n')}`));
   process.exit(1);
 }
+
+const [packageName, ...entryPointTail] = packageNameGuess.result.split('/');
+const apiGoldenTargetName =
+    `//tools/public_api_guard:${packageName}/${entryPointTail.join('-')}.md_api.accept`;
 
 // ShellJS should exit if any command fails.
 shelljs.set('-e');
 shelljs.cd(projectDir);
-shelljs.exec(`yarn bazel run //tools/public_api_guard:${packageNameGuess.result}.d.ts_api.accept`);
+shelljs.exec(`yarn bazel run ${apiGoldenTargetName}`);
