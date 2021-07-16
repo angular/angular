ng-docs/angular-cn

build: convert CLDR locale extraction from Gulp to Bazel tool (#42230)
Converts the CLDR locale extraction script to a Bazel tool.
This allows us to generate locale files within Bazel, so that
locales don't need to live as sources within the repo. Also
it allows us to get rid of the legacy Gulp tooling.

The migration of the Gulp script to a Bazel tool involved the
following things:

  1. Basic conversion of the `extract.js` script to TypeScript.
     This mostly was about adding explicit types. e.g. adding `locale:
     string` or `localeData: CldrStatic`.

  2. Split-up into separate files. Instead of keeping the large
     `extract.js` file, the tool has been split into separate files.
     The logic remains the same, just that code is more readable and
     maintainable.

  3. Introduction of a new `index.ts` file that is the entry-point
     for the Bazel tool. Previously the Gulp tool just generated
     all locale files, the default locale and base currency files
     at once. The new entry-point accepts a mode to be passed as
     first process argument. based on that argument, either locales
     are generated into a specified directory, or the default locale,
     base currencies or closure file is generated.

     This allows us to generate files with a Bazel genrule where
     we simply run the tool and specify the outputs. Note: It's
     necessary to have multiple modes because files live in separate
     locations. e.g. the default locale in `@angular/core`, but the
     rest in `@angular/common`.

  4. Removal of the `cldr-data-downloader` and custom CLDR resolution
     logic. Within Bazel we cannot run a downloader using network.

     We switch this to something more Bazel idiomatic with better
     caching. For this a new repository rule is introduced that
     downloads the CLDR JSON repository and extracts it. Within
     that rule we determine the supported locales so that they
     can be used to pre-declare outputs (for the locales) within
 

packages/common/locales/generate-locales-tool/locale-base-currencies.ts
@@ -1,0 +1,75 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {CldrStatic} from 'cldrjs';
+
+import {fileHeader} from './file-header';
+import {stringify} from './object-stringify';
+
+export type BaseCurrencySymbols = [
+  string
+]|[string | undefined, string]|[string, undefined, number]|[string | undefined, string, number];
+
+export type BaseCurrencies = {
+  [code: string]: BaseCurrencySymbols|undefined;
+};
+
+/**
+ * Generate a file that contains the list of currencies, their symbols and digits.
+ */
+export function generateBaseCurrenciesFile(baseLocaleData: CldrStatic) {
+  const baseCurrencies = generateBaseCurrencies(baseLocaleData);
+
+  return `${fileHeader}
+export type CurrenciesSymbols = [string] | [string | undefined, string];
+
+/** @internal */
+export const CURRENCIES_EN: {[code: string]: CurrenciesSymbols | [string | undefined, string | undefined, number]} = ${
+      stringify(baseCurrencies)};
+`;
+}
+
+/**
+ * Generate a list of currencies to be used as a base for other currencies
+ * e.g.: {'ARS': [, '$'], 'AUD': ['A$', '$'], ...}
+ */
+export function generateBaseCurrencies(localeData: CldrStatic) {
+  const currenciesData = localeData.main('numbers/currencies');
+  const fractions = localeData.get(`supplemental/currencyData/fractions`);
+  const currencies: BaseCurrencies = {};
+
+  Object.keys(currenciesData).forEach(key => {
+    let symbolsArray = [];
+    const symbol = currenciesData[key].symbol;
+    const symbolNarrow = currenciesData[key]['symbol-alt-narrow'];
+    if (symbol && symbol !== key) {
+      symbolsArray.push(symbol);
+    }
+    if (symbolNarrow && symbolNarrow !== symbol) {
+      if (symbolsArray.length > 0) {
+        symbolsArray.push(symbolNarrow);
+      } else {
+        symbolsArray = [undefined, symbolNarrow];
+      }
+    }
+    if (fractions[key] && fractions[key]['_digits']) {
+      const digits = parseInt(fractions[key]['_digits'], 10);
+      if (symbolsArray.length === 2) {
+        symbolsArray.push(digits);
+      } else if (symbolsArray.length === 1) {
+        symbolsArray = [...symbolsArray, undefined, digits];
+      } else {
+        symbolsArray = [undefined, undefined, digits];
+      }
+    }
+    if (symbolsArray.length > 0) {
+      currencies[key] = symbolsArray as BaseCurrencySymbols;
+    }
+  });
+  return currencies;
+}

packages/common/locales/generate-locales-tool/locale-currencies.ts
@@ -1,0 +1,86 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {CldrStatic} from 'cldrjs';
+import {BaseCurrencies} from './locale-base-currencies';
+
+/**
+ * To minimize the file even more, we only output the differences compared to the base currency
+ */
+export function generateLocaleCurrencies(localeData: CldrStatic, baseCurrencies: BaseCurrencies) {
+  const currenciesData = localeData.main('numbers/currencies');
+  const currencies: any = {};
+
+  Object.keys(currenciesData).forEach(code => {
+    let symbolsArray = [];
+    const symbol = currenciesData[code].symbol;
+    const symbolNarrow = currenciesData[code]['symbol-alt-narrow'];
+    if (symbol && symbol !== code) {
+      symbolsArray.push(symbol);
+    }
+    if (symbolNarrow && symbolNarrow !== symbol) {
+      if (symbolsArray.length > 0) {
+        symbolsArray.push(symbolNarrow);
+      } else {
+        symbolsArray = [undefined, symbolNarrow];
+      }
+    }
+
+    const baseCurrencySymbols = baseCurrencies[code] || [];
+
+    // Jf locale data is equal to the one in the base currencies, skip this currency to
+    // avoid unnecessary locale data that could be inferred from the base currency.
+    if (baseCurrencySymbols && baseCurrencySymbols[0] === symbolsArray[0] &&
+        baseCurrencySymbols[1] === symbolsArray[1]) {
+      return;
+    }
+
+    currencies[code] = symbolsArray;
+  });
+  return currencies;
+}
+
+/**
+ * Returns the currency code, symbol and name for a locale
+ */
+export function getCurrencySettings(localeName: string, localeData: CldrStatic) {
+  const currencyInfo = localeData.main(`numbers/currencies`);
+  let currentCurrency = '';
+
+  // find the currency currently used in this country
+  const currencies: any[] =
+      localeData.get(`supplemental/currencyData/region/${localeData.attributes.territory}`) ||
+      localeData.get(
+          `supplemental/currencyData/region/${localeData.attributes.language.toUpperCase()}`);
+
+  if (currencies) {
+    currencies.some(currency => {
+      const keys = Object.keys(currency);
+      return keys.some(key => {
+        if (currency[key]._from && !currency[key]._to) {
+          return currentCurrency = key;
+        }
+      });
+    });
+
+    if (!currentCurrency) {
+      throw new Error(`Unable to find currency for locale "${localeName}"`);
+    }
+  }
+
+  let currencySettings = [undefined, undefined, undefined];
+
+  if (currentCurrency) {
+    currencySettings = [
+      currentCurrency, currencyInfo[currentCurrency].symbol,
+      currencyInfo[currentCurrency].displayName
+    ];
+  }
+
+  return currencySettings;
+}

packages/common/locales/generate-locales-tool/locale-extra-file.ts
@@ -1,0 +1,58 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {CldrStatic} from 'cldrjs';
+
+import {removeDuplicates} from './array-deduplication';
+import {getDayPeriodRules, getDayPeriodsNoAmPm} from './day-periods';
+import {fileHeader} from './file-header';
+import {stringify} from './object-stringify';
+
+/**
+ * Generate the contents for the extra data file
+ */
+export function generateLocaleExtra(locale: string, localeData: CldrStatic) {
+  return `${fileHeader}
+const u = undefined;
+
+export default ${generateDayPeriodsSupplementalString(locale, localeData)};
+`;
+}
+
+
+/**
+ * Collect up the day period rules, and extended day period data.
+ */
+export function generateDayPeriodsSupplementalString(locale: string, localeData: CldrStatic) {
+  const dayPeriods = getDayPeriodsNoAmPm(localeData);
+  const dayPeriodRules = getDayPeriodRules(localeData);
+
+  let dayPeriodsSupplemental: any[] = [];
+  if (Object.keys(dayPeriods.format.narrow).length) {
+    const keys = Object.keys(dayPeriods.format.narrow);
+
+    if (keys.length !== Object.keys(dayPeriodRules).length) {
+      throw new Error(`Error: locale ${locale} has not the correct number of day period rules`);
+    }
+
+    const dayPeriodsFormat = removeDuplicates([
+      Object.values(dayPeriods.format.narrow), Object.values(dayPeriods.format.abbreviated),
+      Object.values(dayPeriods.format.wide)
+    ]);
+
+    const dayPeriodsStandalone = removeDuplicates([
+      Object.values(dayPeriods['stand-alone'].narrow),
+      Object.values(dayPeriods['stand-alone'].abbreviated),
+      Object.values(dayPeriods['stand-alone'].wide)
+    ]);
+
+    const rules = keys.map(key => dayPeriodRules[key]);
+    dayPeriodsSupplemental = [...removeDuplicates([dayPeriodsFormat, dayPeriodsStandalone]), rules];
+  }
+  return stringify(dayPeriodsSupplemental).replace(/undefined/g, 'u');
+}

packages/common/locales/generate-locales-tool/locale-global-file.ts
@@ -1,0 +1,43 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {CldrStatic} from 'cldrjs';
+
+import {fileHeader} from './file-header';
+import {BaseCurrencies} from './locale-base-currencies';
+import {generateDayPeriodsSupplementalString} from './locale-extra-file';
+import {generateBasicLocaleString} from './locale-file';
+import {getPluralFunction} from './plural-function';
+
+/**
+ * Generated the contents for the global locale file
+ */
+export function generateLocaleGlobalFile(
+    locale: string, localeData: CldrStatic, baseCurrencies: BaseCurrencies) {
+  const basicLocaleData = generateBasicLocaleString(locale, localeData, baseCurrencies);
+  const extraLocaleData = generateDayPeriodsSupplementalString(locale, localeData);
+  const data = basicLocaleData.replace(/\]$/, `, ${extraLocaleData}]`);
+  return `${fileHeader}
+  (function(global) {
+    global.ng = global.ng || {};
+    global.ng.common = global.ng.common || {};
+    global.ng.common.locales = global.ng.common.locales || {};
+    const u = undefined;
+    ${getPluralFunction(locale, false)}
+    global.ng.common.locales['${normalizeLocale(locale)}'] = ${data};
+  })(typeof globalThis !== 'undefined' && globalThis || typeof global !== 'undefined' && global || typeof window !== 'undefined' && window);
+    `;
+}
+
+
+/**
+ * In Angular the locale is referenced by a "normalized" form.
+ */
+function normalizeLocale(locale: string): string {
+  return locale.toLowerCase().replace(/_/g, '-');
+}

angular/angular

packages/common/locales/generate-locales-tool/locale-base-currencies.ts
@@ -1,0 +1,74 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {CldrLocaleData} from './cldr-data';
+import {fileHeader} from './file-header';
+import {stringify} from './object-stringify';
+
+export type BaseCurrencySymbols = [
+  string
+]|[string | undefined, string]|[string, undefined, number]|[string | undefined, string, number];
+
+export type BaseCurrencies = {
+  [code: string]: BaseCurrencySymbols|undefined;
+};
+
+/**
+ * Generate a file that contains the list of currencies, their symbols and digits.
+ */
+export function generateBaseCurrenciesFile(baseLocaleData: CldrLocaleData) {
+  const baseCurrencies = generateBaseCurrencies(baseLocaleData);
+
+  return `${fileHeader}
+export type CurrenciesSymbols = [string] | [string | undefined, string];
+
+/** @internal */
+export const CURRENCIES_EN: {[code: string]: CurrenciesSymbols | [string | undefined, string | undefined, number]} = ${
+      stringify(baseCurrencies)};
+`;
+}
+
+/**
+ * Generate a list of currencies to be used as a base for other currencies
+ * e.g.: {'ARS': [, '$'], 'AUD': ['A$', '$'], ...}
+ */
+export function generateBaseCurrencies(localeData: CldrLocaleData) {
+  const currenciesData = localeData.main('numbers/currencies');
+  const fractions = localeData.get(`supplemental/currencyData/fractions`);
+  const currencies: BaseCurrencies = {};
+
+  Object.keys(currenciesData).forEach(key => {
+    let symbolsArray = [];
+    const symbol = currenciesData[key].symbol;
+    const symbolNarrow = currenciesData[key]['symbol-alt-narrow'];
+    if (symbol && symbol !== key) {
+      symbolsArray.push(symbol);
+    }
+    if (symbolNarrow && symbolNarrow !== symbol) {
+      if (symbolsArray.length > 0) {
+        symbolsArray.push(symbolNarrow);
+      } else {
+        symbolsArray = [undefined, symbolNarrow];
+      }
+    }
+    if (fractions[key] && fractions[key]['_digits']) {
+      const digits = parseInt(fractions[key]['_digits'], 10);
+      if (symbolsArray.length === 2) {
+        symbolsArray.push(digits);
+      } else if (symbolsArray.length === 1) {
+        symbolsArray = [...symbolsArray, undefined, digits];
+      } else {
+        symbolsArray = [undefined, undefined, digits];
+      }
+    }
+    if (symbolsArray.length > 0) {
+      currencies[key] = symbolsArray as BaseCurrencySymbols;
+    }
+  });
+  return currencies;
+}

packages/common/locales/generate-locales-tool/locale-currencies.ts
@@ -1,0 +1,87 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {CldrLocaleData} from './cldr-data';
+import {BaseCurrencies} from './locale-base-currencies';
+
+/**
+ * To minimize the file even more, we only output the differences compared to the base currency
+ */
+export function generateLocaleCurrencies(
+    localeData: CldrLocaleData, baseCurrencies: BaseCurrencies) {
+  const currenciesData = localeData.main('numbers/currencies');
+  const currencies: any = {};
+
+  Object.keys(currenciesData).forEach(code => {
+    let symbolsArray = [];
+    const symbol = currenciesData[code].symbol;
+    const symbolNarrow = currenciesData[code]['symbol-alt-narrow'];
+    if (symbol && symbol !== code) {
+      symbolsArray.push(symbol);
+    }
+    if (symbolNarrow && symbolNarrow !== symbol) {
+      if (symbolsArray.length > 0) {
+        symbolsArray.push(symbolNarrow);
+      } else {
+        symbolsArray = [undefined, symbolNarrow];
+      }
+    }
+
+    const baseCurrencySymbols = baseCurrencies[code] || [];
+
+    // Jf locale data is equal to the one in the base currencies, skip this currency to
+    // avoid unnecessary locale data that could be inferred from the base currency.
+    if (baseCurrencySymbols && baseCurrencySymbols[0] === symbolsArray[0] &&
+        baseCurrencySymbols[1] === symbolsArray[1]) {
+      return;
+    }
+
+    currencies[code] = symbolsArray;
+  });
+  return currencies;
+}
+
+/**
+ * Returns the currency code, symbol and name for a locale
+ */
+export function getCurrencySettings(localeName: string, localeData: CldrLocaleData) {
+  const currencyInfo = localeData.main(`numbers/currencies`);
+  let currentCurrency = '';
+
+  // find the currency currently used in this country
+  const currencies: any[] =
+      localeData.get(`supplemental/currencyData/region/${localeData.attributes.territory}`) ||
+      localeData.get(
+          `supplemental/currencyData/region/${localeData.attributes.language.toUpperCase()}`);
+
+  if (currencies) {
+    currencies.some(currency => {
+      const keys = Object.keys(currency);
+      return keys.some(key => {
+        if (currency[key]._from && !currency[key]._to) {
+          return currentCurrency = key;
+        }
+      });
+    });
+
+    if (!currentCurrency) {
+      throw new Error(`Unable to find currency for locale "${localeName}"`);
+    }
+  }
+
+  let currencySettings = [undefined, undefined, undefined];
+
+  if (currentCurrency) {
+    currencySettings = [
+      currentCurrency, currencyInfo[currentCurrency].symbol,
+      currencyInfo[currentCurrency].displayName
+    ];
+  }
+
+  return currencySettings;
+}

packages/common/locales/generate-locales-tool/locale-extra-file.ts
@@ -1,0 +1,57 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {removeDuplicates} from './array-deduplication';
+import {CldrLocaleData} from './cldr-data';
+import {getDayPeriodRules, getDayPeriodsNoAmPm} from './day-periods';
+import {fileHeader} from './file-header';
+import {stringify} from './object-stringify';
+
+/**
+ * Generate the contents for the extra data file
+ */
+export function generateLocaleExtra(locale: string, localeData: CldrLocaleData) {
+  return `${fileHeader}
+const u = undefined;
+
+export default ${generateDayPeriodsSupplementalString(locale, localeData)};
+`;
+}
+
+
+/**
+ * Collect up the day period rules, and extended day period data.
+ */
+export function generateDayPeriodsSupplementalString(locale: string, localeData: CldrLocaleData) {
+  const dayPeriods = getDayPeriodsNoAmPm(localeData);
+  const dayPeriodRules = getDayPeriodRules(localeData);
+
+  let dayPeriodsSupplemental: any[] = [];
+  if (Object.keys(dayPeriods.format.narrow).length) {
+    const keys = Object.keys(dayPeriods.format.narrow);
+
+    if (keys.length !== Object.keys(dayPeriodRules).length) {
+      throw new Error(`Error: locale ${locale} has not the correct number of day period rules`);
+    }
+
+    const dayPeriodsFormat = removeDuplicates([
+      Object.values(dayPeriods.format.narrow), Object.values(dayPeriods.format.abbreviated),
+      Object.values(dayPeriods.format.wide)
+    ]);
+
+    const dayPeriodsStandalone = removeDuplicates([
+      Object.values(dayPeriods['stand-alone'].narrow),
+      Object.values(dayPeriods['stand-alone'].abbreviated),
+      Object.values(dayPeriods['stand-alone'].wide)
+    ]);
+
+    const rules = keys.map(key => dayPeriodRules[key]);
+    dayPeriodsSupplemental = [...removeDuplicates([dayPeriodsFormat, dayPeriodsStandalone]), rules];
+  }
+  return stringify(dayPeriodsSupplemental).replace(/undefined/g, 'u');
+}

packages/common/locales/generate-locales-tool/locale-global-file.ts
@@ -1,0 +1,42 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {CldrLocaleData} from './cldr-data';
+import {fileHeader} from './file-header';
+import {BaseCurrencies} from './locale-base-currencies';
+import {generateDayPeriodsSupplementalString} from './locale-extra-file';
+import {generateBasicLocaleString} from './locale-file';
+import {getPluralFunction} from './plural-function';
+
+/**
+ * Generated the contents for the global locale file
+ */
+export function generateLocaleGlobalFile(
+    locale: string, localeData: CldrLocaleData, baseCurrencies: BaseCurrencies) {
+  const basicLocaleData = generateBasicLocaleString(locale, localeData, baseCurrencies);
+  const extraLocaleData = generateDayPeriodsSupplementalString(locale, localeData);
+  const data = basicLocaleData.replace(/\]$/, `, ${extraLocaleData}]`);
+  return `${fileHeader}
+  (function(global) {
+    global.ng = global.ng || {};
+    global.ng.common = global.ng.common || {};
+    global.ng.common.locales = global.ng.common.locales || {};
+    const u = undefined;
+    ${getPluralFunction(locale, false)}
+    global.ng.common.locales['${normalizeLocale(locale)}'] = ${data};
+  })(typeof globalThis !== 'undefined' && globalThis || typeof global !== 'undefined' && global || typeof window !== 'undefined' && window);
+    `;
+}
+
+
+/**
+ * In Angular the locale is referenced by a "normalized" form.
+ */
+function normalizeLocale(locale: string): string {
+  return locale.toLowerCase().replace(/_/g, '-');
+}