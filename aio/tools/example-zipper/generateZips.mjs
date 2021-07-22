da0dabc6b4a6387f9f6dc78710a8e36726516584
George Kalpakas <kalpakas.g@gmail.com>
2021-07-22 14:38:18 +0300

    build(docs-infra): switch the `generate-stackblitz/zips` scripts to ESM (#42945)
    Switch the JS scripts used for generating StackBlitz projects and zips
    from CommonJS to ESM format. This is necessary for upgrading the
    `globby` dependency to [version 12.0.0][1] in a subsequent commit.
    
    [1]: https://github.com/sindresorhus/globby/releases/v12.0.0
    
    PR Close #42945

--- aio/tools/example-zipper/generateZips.mjs
+++ aio/tools/example-zipper/generateZips.mjs
@@ -1,6 +1,8 @@
-const ExampleZipper = require('./exampleZipper');
-const path = require('canonical-path');
+import path from 'canonical-path';
+import {fileURLToPath} from 'url';
+import {ExampleZipper} from './exampleZipper.mjs';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const EXAMPLES_PATH = path.join(__dirname, '../../content/examples');
 const ZIPS_PATH = path.join(__dirname, '../../src/generated/zips');
 
