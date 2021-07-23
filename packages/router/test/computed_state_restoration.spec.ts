fab0630976b54124786b243f85269b13db93270a
Andrew Scott <atscott@google.com>
2021-07-23 11:58:59 -0700

    fixup! test(router): move computed state restoration tests to own file

--- packages/router/test/computed_state_restoration.spec.ts
+++ packages/router/test/computed_state_restoration.spec.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
 import {CommonModule, Location, LocationStrategy, PlatformLocation} from '@angular/common';
 import {SpyLocation} from '@angular/common/testing';
 import {Component, EventEmitter, Injectable, NgModule} from '@angular/core';
