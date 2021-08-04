357c39676829f2aab435e5b4574568df303e4ab6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-08-04 04:29:53 +0200

    fix(cdk/overlay): expand `OverlayReference` interface to cover more properties (#23254)
    We have an interface called `OverlayReference` which is meant to mimic `OverlayRef` while allowing us to break the circular dependency of `ScrollStrategy -> OverlayRef -> OverlayConfig -> ScrollStrategy`. The problem is that it currently doesn't expose some useful members.
    
    These changes expand the interface to cover more of the `OverlayRef` API surface.
    
    Fixes #23234.

--- src/cdk/overlay/overlay-reference.ts
+++ src/cdk/overlay/overlay-reference.ts
@@ -8,7 +8,7 @@
 
 import {Portal} from '@angular/cdk/portal';
 import {Direction, Directionality} from '@angular/cdk/bidi';
-import {Subject} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 
 /**
  * Basic interface for an overlay. Used to avoid circular type references between
@@ -21,12 +21,20 @@ export interface OverlayReference {
   dispose: () => void;
   overlayElement: HTMLElement;
   hostElement: HTMLElement;
+  backdropElement: HTMLElement | null;
   getConfig: () => any;
   hasAttached: () => boolean;
   updateSize: (config: any) => void;
   updatePosition: () => void;
   getDirection: () => Direction;
   setDirection: (dir: Direction | Directionality) => void;
+  backdropClick: () => Observable<MouseEvent>;
+  attachments: () => Observable<void>;
+  detachments: () => Observable<void>;
+  keydownEvents: () => Observable<KeyboardEvent>;
+  outsidePointerEvents: () => Observable<MouseEvent>;
+  addPanelClass: (classes: string | string[]) => void;
+  removePanelClass: (classes: string | string[]) => void;
   readonly _outsidePointerEvents: Subject<MouseEvent>;
   readonly _keydownEvents: Subject<KeyboardEvent>;
 }
