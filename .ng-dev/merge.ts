a64250c24b0810c67ea4e2c22c2803220333174a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-08-23 12:10:53 +0200

    build: update shared dev-infra package
    Updates the shared dev-infra package to take advanatge
    of recent improvements for the release tool.
    
    This also fixes the issue where the merge tooling requires
    the `breaking change` label, while there also is the
    `state: breaking change` label. The ng-dev tool no longer
    accepts custom configuration of that label.
    
    Also the merge configuration no longer accepts custom labels,
    in order to achieve consistent labeling as specified in the
    Angular versioning/branching/labeling spec.

--- .ng-dev/merge.ts
+++ .ng-dev/merge.ts
@@ -1,28 +1,22 @@
-import {DevInfraMergeConfig} from '@angular/dev-infra-private/ng-dev/pr/merge/config';
-import {getDefaultTargetLabelConfiguration} from '@angular/dev-infra-private/ng-dev/pr/merge/defaults';
-import {github} from './github';
-import {release} from './release';
+import {MergeConfig} from '@angular/dev-infra-private/ng-dev/pr/merge/config';
 
 /**
  * Configuration for the merge tool in `ng-dev`. This sets up the labels which
  * are respected by the merge script (e.g. the target labels).
  */
-export const merge: DevInfraMergeConfig['merge'] = async api => {
-  return {
-    // By default, the merge script merges locally with `git cherry-pick` and autosquash.
-    // This has the downside of pull requests showing up as `Closed` instead of `Merged`.
-    // In the components repository, since we don't use fixup or squash commits, we can
-    // use the Github API merge strategy. That way we ensure that PRs show up as `Merged`.
-    githubApiMerge: {
-      default: 'squash',
-      labels: [
-        {pattern: 'preserve commits', method: 'rebase'}
-      ]
-    },
-    claSignedLabel: 'cla: yes',
-    mergeReadyLabel: 'merge ready',
-    commitMessageFixupLabel: 'commit message fixup',
-    caretakerNoteLabel: 'caretaker note',
-    labels: await getDefaultTargetLabelConfiguration(api, github, release),
-  };
+export const merge: MergeConfig = {
+  // By default, the merge script merges locally with `git cherry-pick` and autosquash.
+  // This has the downside of pull requests showing up as `Closed` instead of `Merged`.
+  // In the components repository, since we don't use fixup or squash commits, we can
+  // use the Github API merge strategy. That way we ensure that PRs show up as `Merged`.
+  githubApiMerge: {
+    default: 'squash',
+    labels: [
+      {pattern: 'preserve commits', method: 'rebase'}
+    ]
+  },
+  claSignedLabel: 'cla: yes',
+  mergeReadyLabel: 'merge ready',
+  commitMessageFixupLabel: 'commit message fixup',
+  caretakerNoteLabel: 'caretaker note',
 };
