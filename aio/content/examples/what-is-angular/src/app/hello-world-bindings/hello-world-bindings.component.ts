a7bbd79eb0e36333e10849c0166638989afeb783
Enea Jahollari <ejahollari@optimaalbania.com>
2021-08-24 10:49:38 +0200

    docs: enhance what is angular section spacing

--- aio/content/examples/what-is-angular/src/app/hello-world-bindings/hello-world-bindings.component.ts
+++ aio/content/examples/what-is-angular/src/app/hello-world-bindings/hello-world-bindings.component.ts
@@ -5,13 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './hello-world-bindings.component.html'
 })
 export class HelloWorldBindingsComponent {
-    fontColor = 'blue';
-    sayHelloId = 1;
-    canClick = false;
-    message = 'Hello, World';
+  fontColor = 'blue';
+  sayHelloId = 1;
+  canClick = false;
+  message = 'Hello, World';
+
 // #docregion method
-    sayMessage() {
-        alert(this.message);
-    }
+  sayMessage() {
+    alert(this.message);
+  }
 // #enddocregion
+
 }
