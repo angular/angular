e2934d3ed5a8eeea59cf1974a32dd7d3299aa5b9
Enea Jahollari <ejahollari@optimaalbania.com>
2021-08-24 11:09:39 +0200

    docs: enhance spacings in component overview section examples

--- aio/content/examples/component-overview/src/app/component-overview/component-overview.component.2.ts
+++ aio/content/examples/component-overview/src/app/component-overview/component-overview.component.2.ts
@@ -4,12 +4,12 @@ import { Component } from '@angular/core';
 // #docregion templatebacktick
 @Component({
   selector: 'app-component-overview',
-  template: `<h1>Hello World!</h1>
-             <p>This template definition spans
-              multiple lines.</p>`
+  template: `
+    <h1>Hello World!</h1>
+    <p>This template definition spans multiple lines.</p>
+  `
 })
 // #enddocregion templatebacktick
-
 export class ComponentOverviewComponent {
 
 }
