5472c28fa1ddc002a93803a14ed5c6c865ca4dd8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-09-02 16:29:47 +0200

    build: check .ng-dev tool configuration in CI (#43322)
    Checks the `.ng-dev` tool configuration in CI, compared
    to doing it locally when the caretaker intends to perform
    a merge and then realizes the config is broken.
    
    PR Close #43322

--- .ng-dev/merge.ts
+++ .ng-dev/merge.ts
@@ -1,33 +1,24 @@
-import {DevInfraMergeConfig} from '@angular/dev-infra-private/ng-dev/pr/merge/config';
-import {getDefaultTargetLabelConfiguration} from '@angular/dev-infra-private/ng-dev/pr/merge/defaults';
-import {github} from './github';
-import {release} from './release';
+import {MergeConfig} from '@angular/dev-infra-private/ng-dev/pr/merge/config';
 
 /**
  * Configuration for the merge tool in `ng-dev`. This sets up the labels which
  * are respected by the merge script (e.g. the target labels).
  */
-export const merge: DevInfraMergeConfig['merge'] = async api => {
-  return {
-    githubApiMerge: false,
-    claSignedLabel: 'cla: yes',
-    mergeReadyLabel: /^action: merge(-assistance)?/,
-    caretakerNoteLabel: /^(action: merge-assistance)|(PullApprove: disable)/,
-    commitMessageFixupLabel: 'commit message fixup',
-    breakingChangeLabel: 'flag: breaking change',
-    // We can pick any of the NPM packages as we are in a monorepo where all packages are
-    // published together with the same version and branching.
-    labels: await getDefaultTargetLabelConfiguration(api, github, release),
-    requiredBaseCommits: {
-      // PRs that target either `master` or the patch branch, need to be rebased
-      // on top of the latest commit message validation fix.
-      // These SHAs are the commits that update the required license text in the header.
-      'master': '5aeb9a4124922d8ac08eb73b8f322905a32b0b3a',
-      '10.0.x': '27b95ba64a5d99757f4042073fd1860e20e3ed24',
-    },
-    // `dev-infra` and `docs-infra` are not affecting the public NPM packages. Similarly,
-    // the `bazel` package is not considered part of the public API so that features
-    // can land in patch branches.
-    targetLabelExemptScopes: ['dev-infra', 'docs-infra', 'bazel']
-  };
+export const merge: MergeConfig = {
+  githubApiMerge: false,
+  claSignedLabel: 'cla: yes',
+  mergeReadyLabel: /^action: merge(-assistance)?/,
+  caretakerNoteLabel: /^(action: merge-assistance)|(PullApprove: disable)/,
+  commitMessageFixupLabel: 'commit message fixup',
+  requiredBaseCommits: {
+    // PRs that target either `master` or the patch branch, need to be rebased
+    // on top of the latest commit message validation fix.
+    // These SHAs are the commits that update the required license text in the header.
+    'master': '5aeb9a4124922d8ac08eb73b8f322905a32b0b3a',
+    '10.0.x': '27b95ba64a5d99757f4042073fd1860e20e3ed24',
+  },
+  // `dev-infra` and `docs-infra` are not affecting the public NPM packages. Similarly,
+  // the `bazel` package is not considered part of the public API so that features
+  // can land in patch branches.
+  targetLabelExemptScopes: ['dev-infra', 'docs-infra', 'bazel']
 };
