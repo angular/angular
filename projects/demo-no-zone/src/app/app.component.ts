bd6d18d4b55e83035b0db72c21dc1e93aab6deee
AleksanderBodurri <alexbodurri1117@gmail.com>
2021-09-08 18:34:14 -0400

    build(devtools): migrate demo-no-zone to be built with bazel and allow it to be served with concatjs_devserver
    Allows the demo-no-zone application to be built with bazel and a devserver run with `bazel run projects/demo-no-zone/src:devserver`

--- projects/demo-no-zone/src/app/app.component.ts
+++ projects/demo-no-zone/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component, ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   counter = 0;
 
-  constructor(private _cd: ChangeDetectorRef) {}
+  constructor(private _cd: ChangeDetectorRef) {
+    console.log((window as any).ng);
+  }
 
   increment(): void {
     this.counter++;
