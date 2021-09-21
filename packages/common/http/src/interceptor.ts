9b7cdaefe89e54d8bc0ba2d3cdd2d04b3dbbd8ee
Daniel Díaz <dalejodc@gmail.com>
2021-09-21 16:32:21 -0600

    docs(http): get rid of not needed space at the end of the sentence (#43528)
    PR Close #43528

--- packages/common/http/src/interceptor.ts
+++ packages/common/http/src/interceptor.ts
@@ -36,7 +36,7 @@ import {HttpEvent} from './response';
  * @usageNotes
  *
  * To use the same instance of `HttpInterceptors` for the entire app, import the `HttpClientModule`
- * only in your `AppModule`, and add the interceptors to the root application injector .
+ * only in your `AppModule`, and add the interceptors to the root application injector.
  * If you import `HttpClientModule` multiple times across different modules (for example, in lazy
  * loading modules), each import creates a new copy of the `HttpClientModule`, which overwrites the
  * interceptors provided in the root module.
