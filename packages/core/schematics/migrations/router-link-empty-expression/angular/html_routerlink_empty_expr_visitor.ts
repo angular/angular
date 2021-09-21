9bf01dcac13742b66f377ff47fca2d7fa8cbce3a
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-09-21 16:24:10 -0400

    refactor(migrations): remove most template AST deep imports
    Most of the deep imports into the `@angular/compiler` package in the `@angular/core` migrations are for template AST types that are available as exports from the main entry point of the package (albeit under slightly different names). For the available main entry point exports, the deep imports have been transitioned to no longer use the deep import.

--- packages/core/schematics/migrations/router-link-empty-expression/angular/html_routerlink_empty_expr_visitor.ts
+++ packages/core/schematics/migrations/router-link-empty-expression/angular/html_routerlink_empty_expr_visitor.ts
@@ -6,27 +6,27 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {ASTWithSource, EmptyExpr} from '@angular/compiler';
-import {BoundAttribute, Element, NullVisitor, Template, visitAll} from '@angular/compiler/src/render3/r3_ast';
+import {ASTWithSource, EmptyExpr, TmplAstBoundAttribute, TmplAstElement, TmplAstTemplate} from '@angular/compiler';
+import {NullVisitor, visitAll} from '@angular/compiler/src/render3/r3_ast';
 
 /**
  * HTML AST visitor that traverses the Render3 HTML AST in order to find all
  * undefined routerLink asssignment ([routerLink]="").
  */
 export class RouterLinkEmptyExprVisitor extends NullVisitor {
-  readonly emptyRouterLinkExpressions: BoundAttribute[] = [];
+  readonly emptyRouterLinkExpressions: TmplAstBoundAttribute[] = [];
 
-  override visitElement(element: Element): void {
+  override visitElement(element: TmplAstElement): void {
     visitAll(this, element.inputs);
     visitAll(this, element.children);
   }
 
-  override visitTemplate(t: Template): void {
+  override visitTemplate(t: TmplAstTemplate): void {
     visitAll(this, t.inputs);
     visitAll(this, t.children);
   }
 
-  override visitBoundAttribute(node: BoundAttribute) {
+  override visitBoundAttribute(node: TmplAstBoundAttribute) {
     if (node.name === 'routerLink' && node.value instanceof ASTWithSource &&
         node.value.ast instanceof EmptyExpr) {
       this.emptyRouterLinkExpressions.push(node);
