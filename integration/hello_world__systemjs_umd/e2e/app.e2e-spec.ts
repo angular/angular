31e1f19ac41bd66fbabe31ec670dfde126eac720
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-09-23 12:30:56 +0200

    test: remove hello world SystemJS UMD integration test
    Given that Angular no longer ships UMD bundles as of v13, the
    hello world SystemJS integration test cannot work.

--- integration/hello_world__systemjs_umd/e2e/app.e2e-spec.ts
+++
@@ -1,8 +0,0 @@
-import { browser, element, by } from 'protractor';
-
-describe('Hello world E2E Tests', function () {
-  it('should display: Hello world!', function () {
-    browser.get('');
-    expect(element(by.css('div')).getText()).toEqual('Hello world!');
-  });
-});
