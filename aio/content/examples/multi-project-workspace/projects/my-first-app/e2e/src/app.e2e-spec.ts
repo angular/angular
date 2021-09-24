e97f385480b9662297b809c20f7a26c0ec147b18
Eduard Bondarenko <eduardbcom@gmail.com>
2021-09-24 23:53:38 +0300

    docs: better documentation for multiple projects within one workspace, part 4

--- aio/content/examples/multi-project-workspace/projects/my-first-app/e2e/src/app.e2e-spec.ts
+++ aio/content/examples/multi-project-workspace/projects/my-first-app/e2e/src/app.e2e-spec.ts
@@ -1,23 +1,23 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+// import { AppPage } from './app.po';
+// import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+// describe('workspace-project App', () => {
+//   let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+//   beforeEach(() => {
+//     page = new AppPage();
+//   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-first-app app is running!');
-  });
+//   it('should display welcome message', () => {
+//     page.navigateTo();
+//     expect(page.getTitleText()).toEqual('my-first-app app is running!');
+//   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
-});
+//   afterEach(async () => {
+//     // Assert that there are no errors emitted from the browser
+//     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+//     expect(logs).not.toContain(jasmine.objectContaining({
+//       level: logging.Level.SEVERE,
+//     } as logging.Entry));
+//   });
+// });
