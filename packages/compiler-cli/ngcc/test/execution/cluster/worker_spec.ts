5666ef6f7f8c6bdbdf26614d91ebd45078cc86e1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-09-26 14:48:13 +0200

    refactor(compiler-cli): fix ngcc cluster workers incorrectly being marked as busy with ESM
    Ngcc relies on cluster for distributing work. The master controller
    sends messages to the workers as soon as the worker becomes `online`.
    The online event is sent as part of the NodeJS cluster logic itself.
    
    This does not work well because technically `online` could emit before the
    worker started listening (this seems to be case now with ESM as the
    imports are loaded in a way where `online` emits too early; before the
    worker actually listens for messages).
    
    We fix this by explicitly notifying the master when the worker
    is ready for retrieving IPC messages/or tasks. This is more safe
    anyway as it's not clearly specified when `online` emits.

--- packages/compiler-cli/ngcc/test/execution/cluster/worker_spec.ts
+++ packages/compiler-cli/ngcc/test/execution/cluster/worker_spec.ts
@@ -63,8 +63,18 @@ describe('startWorker()', () => {
       expect(createCompileFnSpy).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function));
     });
 
+    it('should notify the cluster master once ready', () => {
+      startWorker(mockLogger, createCompileFnSpy);
+
+      expect(processSendSpy).toHaveBeenCalledTimes(1);
+      expect(processSendSpy).toHaveBeenCalledWith({type: 'ready'}, jasmine.any(Function));
+    });
+
     it('should set up `compileFn()` to send `transformed-files` messages to master', () => {
       startWorker(mockLogger, createCompileFnSpy);
+
+      expect(processSendSpy).toHaveBeenCalledTimes(1);
+      expect(processSendSpy).toHaveBeenCalledWith({type: 'ready'}, jasmine.any(Function));
 
       const mockTransformedFiles: FileToWrite[] = [
         {path: '/foo' as AbsoluteFsPath, contents: 'FOO'},
@@ -75,7 +85,7 @@ describe('startWorker()', () => {
 
       beforeWritingFiles(mockTransformedFiles);
 
-      expect(processSendSpy).toHaveBeenCalledTimes(1);
+      expect(processSendSpy).toHaveBeenCalledTimes(2);
       expect(processSendSpy)
           .toHaveBeenCalledWith(
               {type: 'transformed-files', files: ['/foo', '/bar']}, jasmine.any(Function));
@@ -84,6 +94,9 @@ describe('startWorker()', () => {
     it('should set up `compileFn()` to send `task-completed` messages to master', () => {
       startWorker(mockLogger, createCompileFnSpy);
       const onTaskCompleted: TaskCompletedCallback = createCompileFnSpy.calls.argsFor(0)[1];
+
+      // Reset as we are not interested in the initial messages.
+      processSendSpy.calls.reset();
 
       onTaskCompleted(null as any, TaskProcessingOutcome.Processed, null);
       expect(processSendSpy).toHaveBeenCalledTimes(1);
@@ -131,7 +144,7 @@ describe('startWorker()', () => {
       cluster.worker.emit('message', {type: 'process-task', task: mockTask});
 
       expect(compileFnSpy).toHaveBeenCalledWith(mockTask);
-      expect(processSendSpy).not.toHaveBeenCalled();
+      expect(processSendSpy).toHaveBeenCalledOnceWith({type: 'ready'}, jasmine.any(Function));
 
       expect(mockLogger.logs.debug[0]).toEqual([
         '[Worker #42] Processing task: {entryPoint: foo, formatProperty: es2015, processDts: Yes}',
@@ -189,7 +202,7 @@ describe('startWorker()', () => {
 
         expect(mockLogger.logs.warn).toEqual([[`[Worker #42] ${noMemError.stack}`]]);
         expect(processExitSpy).toHaveBeenCalledWith(1);
-        expect(processSendSpy).not.toHaveBeenCalled();
+        expect(processSendSpy).toHaveBeenCalledOnceWith({type: 'ready'}, jasmine.any(Function));
       } finally {
         uninstallProcessExitSpies();
       }
