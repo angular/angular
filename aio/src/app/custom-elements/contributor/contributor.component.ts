b068949e0efb9d3e3c991da6cc48be92bf509f13
dario-piotrowicz <dario.piotrowicz@gmail.com>
2021-09-26 15:11:04 +0100

    fix(docs-infra): convert button-like elements to actual buttons
    some elements in the aio application are anchors or divs but behave
    like buttons, it is semantically (and a11y) more correct to convert
    them to actual button elements instead

--- aio/src/app/custom-elements/contributor/contributor.component.ts
+++ aio/src/app/custom-elements/contributor/contributor.component.ts
@@ -8,15 +8,17 @@ import { CONTENT_URL_PREFIX } from 'app/documents/document.service';
   template: `
     <div [ngClass]="{ 'flipped': person.isFlipped }" class="contributor-card">
 
-        <div class="card-front" (click)="flipCard(person)" (keyup.enter)="flipCard(person)">
+        <button class="card-front" (click)="flipCard(person)">
             <h3>{{person.name}}</h3>
 
             <div class="contributor-image"
                  [style.background-image]="'url('+pictureBase+(person.picture || noPicture)+')'">
                  <div class="contributor-info">
-                     <a *ngIf="person.bio" mat-button class="info-item" tabindex="0">
-                         View Bio
-                     </a>
+                     <div *ngIf="person.bio" mat-button class="info-item">
+                         <button mat-button>
+                             View Bio
+                         </button>
+                     </div>
                      <a *ngIf="person.twitter" mat-icon-button class="info-item icon"
                          href="https://twitter.com/{{person.twitter}}"
                          target="_blank" (click)="$event.stopPropagation()">
@@ -28,13 +30,12 @@ import { CONTENT_URL_PREFIX } from 'app/documents/document.service';
                      </a>
                  </div>
             </div>
-        </div>
+        </button>
 
-        <div class="card-back" *ngIf="person.isFlipped" (click)="flipCard(person)" (keyup.enter)="flipCard(person)"
-             tabindex="0">
+        <button class="card-back" *ngIf="person.isFlipped" (click)="flipCard(person)">
             <h3>{{person.name}}</h3>
             <p class="contributor-bio">{{person.bio}}</p>
-        </div>
+        </button>
     </div>
   `
 })
