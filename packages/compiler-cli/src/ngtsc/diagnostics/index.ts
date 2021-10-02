fa7693f78465db0385b136d63980dffcee01580e
JoostK <joost.koehoorn@gmail.com>
2021-10-02 22:09:17 +0200

    refactor(compiler-cli): extract error documentation base url into separate file (#43527)
    Prior refactorings caused unexpected g3 sync issues due to a patch that
    changes the error documentation URL. This commit moves the base url into
    a separate file to make this more apparent.
    
    PR Close #43527

--- packages/compiler-cli/src/ngtsc/diagnostics/index.ts
+++ packages/compiler-cli/src/ngtsc/diagnostics/index.ts
@@ -6,7 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-export {COMPILER_ERRORS_WITH_GUIDES, ERROR_DETAILS_PAGE_BASE_URL} from './src/docs';
+export {COMPILER_ERRORS_WITH_GUIDES} from './src/docs';
 export {FatalDiagnosticError, isFatalDiagnosticError, makeDiagnostic, makeRelatedInformation} from './src/error';
 export {ErrorCode} from './src/error_code';
+export {ERROR_DETAILS_PAGE_BASE_URL} from './src/error_details_base_url';
 export {ngErrorCode, replaceTsWithNgInErrors} from './src/util';
