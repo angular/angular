97fc9de2c48e389c2d64cb66da4c15d727780383
ericcheng2005 <68196762+import-brain@users.noreply.github.com>
2021-10-02 15:46:28 -0400

    refactor(core): made comment structure consistent in index.ts (#43684)
    changed second big block of comments to use multi-line comment structure instead of multiple single-line comments
    
    PR Close #43684

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -6,9 +6,10 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-// This file is not used to build this module. It is only used during editing
-// by the TypeScript language service and during build for verification. `ngc`
-// replaces this file with production index.ts when it rewrites private symbol
-// names.
+/* This file is not used to build this module. It is only used during editing
+ * by the TypeScript language service and during build for verification. `ngc`
+ * replaces this file with production index.ts when it rewrites private symbol
+ * names.
+ */
 
 export * from './public_api';
