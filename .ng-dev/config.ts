0d43148f5871b2a6f39b5981ed6f4d847761f065
Joey Perrott <josephperrott@gmail.com>
2021-10-04 10:25:27 -0700

    build: update to the latest version of @angular/dev-infra-private (#43541)
    Updates to the latest dev-infra version and updates the config as required.
    
    PR Close #43541

--- .ng-dev/config.ts
+++ .ng-dev/config.ts
@@ -2,14 +2,14 @@ import {caretaker} from './caretaker';
 import {commitMessage} from './commit-message';
 import {format} from './format';
 import {github} from './github';
-import {merge} from './merge';
+import {pullRequest} from './pullRequest';
 import {release} from './release';
 
 module.exports = {
   commitMessage,
   format,
   github,
-  merge,
+  pullRequest,
   caretaker,
   release,
 };
--- .ng-dev/pullRequest.ts
+++ .ng-dev/pullRequest.ts
@@ -1,12 +1,11 @@
-import {MergeConfig} from '@angular/dev-infra-private/ng-dev/pr/config';
+import {PullRequestConfig} from '@angular/dev-infra-private/ng-dev/pr/config';
 
 /**
  * Configuration for the merge tool in `ng-dev`. This sets up the labels which
  * are respected by the merge script (e.g. the target labels).
  */
-export const merge: MergeConfig = {
+export const pullRequest: PullRequestConfig = {
   githubApiMerge: false,
-  claSignedLabel: 'cla: yes',
   mergeReadyLabel: /^action: merge(-assistance)?/,
   caretakerNoteLabel: /^(action: merge-assistance)|(PullApprove: disable)/,
   commitMessageFixupLabel: 'commit message fixup',
