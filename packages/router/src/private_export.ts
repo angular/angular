b9aab0c87bcccb61167e92c1e910630afad67648
Andrew Scott <atscott@google.com>
2021-10-14 17:16:33 -0700

    fix(upgrade): Do not trigger duplicate navigation events from Angular Router (#43441)
    This code mimics behavior that Google Analytics has been using to
    prevent duplicate navigations. They set up their own `HybridRoutingService`
    location sync to avoid duplicate navigations that came from the Angular
    router. This would happen because the Angular router would trigger a
    navigation, which would then get picked up by the `$locationShim`, which
    would trigger a `$locationChangeStart`, which would then be picked up by
    the `setUpLocationSync` watcher here, which would again trigger a
    navigation in the Angular Router.
    
    All of this can be prevented by checking if the `navigationId` exists on
    the history state object. This property is added by the Angular router
    during navigations.
    
    fixes #21610
    
    PR Close #43441

--- packages/router/src/private_export.ts
+++ packages/router/src/private_export.ts
@@ -8,5 +8,6 @@
 
 
 export {ɵEmptyOutletComponent} from './components/empty_outlet';
+export {RestoredState as ɵRestoredState} from './router';
 export {assignExtraOptionsToRouter as ɵassignExtraOptionsToRouter, ROUTER_PROVIDERS as ɵROUTER_PROVIDERS} from './router_module';
 export {flatten as ɵflatten} from './utils/collection';
