9ae5e8fc1dbd26e7f90c2d61ebd7a2ff9d673c3e
George Kalpakas <kalpakas.g@gmail.com>
2021-10-19 16:19:03 +0300

    build(docs-infra): update docs examples to Angular v13.0.0-rc.0
    This commit updates the docs examples to Angular v13.0.0-rc.0. In
    addition to updating the dependency versions, it also updates the
    projects' structure and configs to more closely match what a new v13 CLI
    app would look like. See, also, the [diff][1] between a basic v12.1.3
    CLI app and a v13.0.0-rc.0 one.
    (NOTE: v12.1.3 was the last version for which we aligned the projects
    with how a new CLI app would look like.)
    
    Additionlly:
    - Since we now use RxJS v7 in docs examples, this commit removes the
      extra CI steps to explicitly test the docs examples against RxJS v7.
    - Since Angular v13 no longer supports IE, this commit removes an
      IE-specific section from `upgrade-setup.md`.
    
    [1]: https://github.com/cexbrayat/angular-cli-diff/compare/12.1.3..13.0.0-rc.0

--- aio/tools/examples/shared/boilerplate/i18n/src/polyfills.ts
+++ aio/tools/examples/shared/boilerplate/i18n/src/polyfills.ts
@@ -12,8 +12,8 @@ import '@angular/localize/init';
  *      file.
  *
  * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
- * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
- * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
+ * automatically update themselves. This includes recent versions of Safari, Chrome (including
+ * Opera), Edge on the desktop, and iOS and Chrome on mobile.
  *
  * Learn more in https://angular.io/guide/browser-support
  */
@@ -21,16 +21,6 @@ import '@angular/localize/init';
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-
-/** IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/**
- * Web Animations `@angular/platform-browser/animations`
- * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
- * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
