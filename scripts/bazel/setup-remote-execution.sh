7ec01396f4fabcffdc69c5f264fe9615afb464d1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-10-27 05:35:24 +0200

    build: setup preview builds for dev-app (#23825)
    * build: setup preview builds for dev-app
    
    Sets up preview builds for the dev-app. Whenever the `dev-app preview`
    label is applied to pull requests, a Github action will build the
    dev-app using RBE and deploy it to a preview channel within a Firebase
    project.
    
    The deployment and building is split up into two individual workflows
    to guarantee a secure exeuction of these steps. This follows the
    concept as outlined in https://securitylab.github.com/research/github-actions-preventing-pwn-requests/.
    
    In the future, we can try extracting some of this logic into a common
    tool in the dev-infra repository.. allowing preview builds to be used
    for other things, or in other repositories as well (or switching AIO
    away from the rather-complicated docker preview build setup).
    
    * fixup! build: setup preview builds for dev-app
    
    Address feedback
    
    * fixup! build: setup preview builds for dev-app
    
    Update old links

---
+++ scripts/bazel/setup-remote-execution.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# The script should immediately exit if any command in the script fails.
+set -e
+
+if [[ -z "${GCP_DECRYPT_TOKEN}" ]]; then
+  echo "Please specify the \"GCP_DECRYPT_TOKEN\" environment variable when setting up remote " \
+      "execution"
+  exit 1
+fi
+
+# Decode the GCP token that is needed to authenticate the Bazel remote execution.
+openssl aes-256-cbc -d -in scripts/bazel/gcp_token -md md5 -k ${GCP_DECRYPT_TOKEN} \
+  -out $HOME/.gcp_credentials
+
+# Set the "GOOGLE_APPLICATION_CREDENTIALS" environment variable. It should point to the GCP credentials
+# file. Bazel will then automatically picks up the credentials from that variable.
+# https://docs.bazel.build/versions/main/command-line-reference.html#flag--google_default_credentials
+# https://cloud.google.com/docs/authentication/production.
+if [[ ! -z "${BASH_ENV}" ]]; then
+  # CircleCI uses the `BASH_ENV` variable for environment variables.
+  echo "export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/.gcp_credentials" >> ${BASH_ENV}
+elif [[ ! -z "${GITHUB_ENV}" ]]; then
+  # Github actions use the `GITHUB_ENV` variable for environment variables.
+  echo "GOOGLE_APPLICATION_CREDENTIALS=${HOME}/.gcp_credentials" >> ${GITHUB_ENV}
+fi
+
+# Update the project Bazel configuration to always use remote execution.
+echo "build --config=remote" >> .bazelrc
---
+++ scripts/github/fetch-workflow-artifact.mjs
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+/**
+ * Fetches a specified artifact by name from the given workflow and writes
+ * the downloaded zip file to the stdout.
+ *
+ * Command line usage:
+ *   ./fetch-workflow-artifact.js <gh-token> <workflow-id> <artifact-name>
+ */
+
+import octokit from '@octokit/rest';
+
+async function main() {
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/', 2);
+  const [token, workflowId, artifactName] = process.argv.slice(2);
+  const github = new octokit.Octokit({auth: token});
+  const artifacts = await github.actions.listWorkflowRunArtifacts({
+    owner,
+    repo,
+    run_id: workflowId,
+  });
+
+  const matchArtifact = artifacts.data.artifacts.find(
+    artifact => artifact.name === artifactName,
+  );
+
+  const download = await github.actions.downloadArtifact({
+    owner,
+    repo,
+    artifact_id: matchArtifact.id,
+    archive_format: 'zip',
+  });
+
+  process.stdout.write(Buffer.from(download.data));
+}
+
+await main();
