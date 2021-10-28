fce7bb8daff7aec0580172f6e1d0ca0151bc2203
Alan Agius <alan.agius4@gmail.com>
2021-10-28 13:19:05 +0200

    test: remove `document-register-element` from elements tests (#43975)
    All the [browsers](https://angular.io/guide/browser-support) that Angular supports `CustomElementRegistry` APIs
    
    PR Close #43975

--- integration/cli-elements-universal/src/polyfills.ts
+++ integration/cli-elements-universal/src/polyfills.ts
@@ -55,10 +55,8 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import 'document-register-element';
