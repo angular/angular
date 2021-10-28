494d056c48f052d5e22c24cee7ef094e6ef079f0
George Kalpakas <kalpakas.g@gmail.com>
2021-10-28 19:01:44 +0300

    refactor(docs-infra): remove obsolete way of disabling `DocViewer` animations (#43977)
    Previously, there were two ways to disable view transition animations in
    `DocViewerComponent`: Using a `.no-animations` CSS class on the
    `<aio-doc-viewer>` element and setting the static
    `DocViewerComponent.animationsEnabled` property to `false`. The latter
    was only used to disable animations for testing purposes.
    
    With the recent introduction of `AppComponent.reducedMotion`, there is
    now an easy way to disable animations in tests using the
    `.no-animations` class (which is applied by `AppComponent` based on the
    value of `AppComponent.reducedMotion`). Therefore, it is no longer
    necessary to have `DocViewerComponent.animationEnabled`.
    
    This commit removes the now obsolete `animationsEnalbed` property.
    
    PR Close #43977

--- aio/src/app/app.component.spec.ts
+++ aio/src/app/app.component.spec.ts
@@ -76,15 +76,17 @@ describe('AppComponent', () => {
 
 
   describe('with proper DocViewer', () => {
+    const originalReducedMotion = AppComponent.reducedMotion;
 
     beforeEach(async () => {
-      DocViewerComponent.animationsEnabled = false;
+      // Set `reducedMotion` to `true` to disable animations (such as view transitions) for the tests.
+      AppComponent.reducedMotion = true;
 
       createTestingModule('a/b');
       await initializeTest();
     });
 
-    afterEach(() => DocViewerComponent.animationsEnabled = true);
+    afterEach(() => AppComponent.reducedMotion = originalReducedMotion);
 
     it('should create', () => {
       expect(component).toBeDefined();
