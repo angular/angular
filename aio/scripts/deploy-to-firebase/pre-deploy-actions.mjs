4f39c2bf7d392de6b021b521f3d993852c2c5159
George Kalpakas <kalpakas.g@gmail.com>
2021-10-29 14:17:14 +0300

    feat(docs-infra): create undo functions for all pre-deploy actions (#43963)
    Create functions to undo the changes made by any pre-deploy action.
    In a future commit, this will allow deploying a build to multiple
    projects/sites with different small tweaks for each.
    
    PR Close #43963

--- aio/scripts/deploy-to-firebase/pre-deploy-actions.mjs
+++ aio/scripts/deploy-to-firebase/pre-deploy-actions.mjs
@@ -1,6 +1,13 @@
+import fs from 'fs';
 import sh from 'shelljs';
 import u from './utils.mjs';
 
+
+// Constants
+const DIST_DIR = 'dist';
+const FIREBASE_JSON_PATH = 'firebase.json';
+const NGSW_JSON_PATH = `${DIST_DIR}/ngsw.json`;
+const NGSW_JSON_BAK_PATH = `${NGSW_JSON_PATH}.bak`;
 
 // Exports
 const exp = {
@@ -8,6 +15,12 @@ const exp = {
   checkPayloadSize,
   disableServiceWorker,
   redirectToAngularIo,
+  undo: {
+    build: undoBuild,
+    checkPayloadSize: undoCheckPayloadSize,
+    disableServiceWorker: undoDisableServiceWorker,
+    redirectToAngularIo: undoRedirectToAngularIo,
+  },
 };
 export default exp;
 
@@ -17,7 +30,7 @@ function build({deployedUrl, deployEnv}) {
   u.yarn(`build --configuration=${deployEnv} --progress=false`);
 
   u.logSectionHeader('Add any mode-specific files into the AIO distribution.');
-  sh.cp('-rf', `src/extra-files/${deployEnv}/.`, 'dist/');
+  sh.cp('-rf', `src/extra-files/${deployEnv}/.`, DIST_DIR);
 
   u.logSectionHeader('Update opensearch descriptor for AIO with `deployedUrl`.');
   u.yarn(`set-opensearch-url ${deployedUrl.replace(/[^/]$/, '$&/')}`); // The URL must end with `/`.
@@ -33,7 +46,15 @@ function disableServiceWorker() {
 
   // Rename the SW manifest (`ngsw.json`). This will cause the ServiceWorker to unregister itself.
   // See https://angular.io/guide/service-worker-devops#fail-safe.
-  sh.mv('dist/ngsw.json', 'dist/ngsw.json.bak');
+  sh.mv(NGSW_JSON_PATH, NGSW_JSON_BAK_PATH);
+}
+
+function escapeForRegex(str) {
+  return str.replace(/[.?*+\\|^$()[\]{}]/g, '\\$&');
+}
+
+function getFirebaseRedirectRuleTo(origin) {
+  return `{"type": 302, "regex": "^(.*/[^./]*)$", "destination": "${origin}:1"}`;
 }
 
 function redirectToAngularIo() {
@@ -42,7 +63,35 @@ function redirectToAngularIo() {
   // Update the Firebase hosting configuration to redirect all non-file requests (i.e. requests that
   // do not contain a dot in their last path segment) to `angular.io`.
   // See https://firebase.google.com/docs/hosting/full-config#redirects.
-  const redirectRule =
-      '{"type": 302, "regex": "^(.*/[^./]*)$", "destination": "https://angular.io:1"}';
-  sh.sed('-i', /(\s*)"redirects": \[/, `$&\n$1  ${redirectRule},\n`, 'firebase.json');
+  const redirectRule = getFirebaseRedirectRuleTo('https://angular.io');
+  const oldContent = fs.readFileSync(FIREBASE_JSON_PATH, 'utf8');
+  const newContent = oldContent.replace(/( *)"redirects": \[/, `$&\n$1  ${redirectRule},\n`);
+
+  fs.writeFileSync(FIREBASE_JSON_PATH, newContent);
+}
+
+function undoBuild() {
+  u.logSectionHeader('Remove the build artifacts.');
+  sh.rm('-rf', DIST_DIR);
+}
+
+function undoCheckPayloadSize() {
+  // Nothing to undo.
+}
+
+function undoDisableServiceWorker() {
+  u.logSectionHeader('Re-enable the ServiceWorker.');
+  sh.mv(NGSW_JSON_BAK_PATH, NGSW_JSON_PATH);
+}
+
+function undoRedirectToAngularIo() {
+  u.logSectionHeader('Remove Firebase hosting redirect to https://angular.io/.');
+
+  const redirectRule = getFirebaseRedirectRuleTo('https://angular.io');
+  const oldContent = fs.readFileSync(FIREBASE_JSON_PATH, 'utf8');
+  const newContent = oldContent.replace(
+      new RegExp(`(( *)"redirects": \\[)\\n\\2  ${escapeForRegex(redirectRule)},\\n`),
+      '$1');
+
+  fs.writeFileSync(FIREBASE_JSON_PATH, newContent);
 }
