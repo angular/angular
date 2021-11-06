e4475e66f965796b7ad4bb3250fed7b18930d061
dario-piotrowicz <dario.piotrowicz@gmail.com>
2021-11-06 16:57:20 +0000

    refactor(core): move outdated comment to view.ts (#44099)
    remove the comment suggesting to use a const enum for ViewEncapsulation in
    the renderer3/definitions.ts file and add a similar comment in the
    view.ts file in which the enum is defined
    
    Note: the new comment does not contain the suggestion of changing `None`
    to `0` as it is unclear what benefits that would bring (for more info
    see: https://github.com/angular/angular/pull/44099#discussion_r744157686)
    
    PR Close #44099

--- packages/core/src/metadata/view.ts
+++ packages/core/src/metadata/view.ts
@@ -19,6 +19,9 @@
  * @publicApi
  */
 export enum ViewEncapsulation {
+  // TODO: consider making `ViewEncapsulation` a `const enum` instead. See
+  // https://github.com/angular/angular/issues/44119 for additional information.
+
   /**
    * Emulate `Native` scoping of styles by adding an attribute containing surrogate id to the Host
    * Element and pre-processing the style rules provided via {@link Component#styles styles} or
