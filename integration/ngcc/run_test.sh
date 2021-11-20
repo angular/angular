cedab0c34aed0489bbbfc8d8340cafbc1589c44f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-11-20 17:23:07 +0100

    test: switch integration tests from puppeteer/webdriver-manager to Bazel-managed chromium
    Switches the integration tests form Puppeteer/webdriver-manager to the
    Bazel-managed Chromium/Chromedriver. This is now possible with the new
    integration test rule for which we can consult the
    `dev-infra/bazel/browsers` toolchain and setup environment variables.
    This has been configured already in a previous commit.
    
    This commit also includes some additional small cleanups necessary for
    the new integration test rule:
    
    * The `test.sh` scripts have been renamed as they would conflict with
      the `test.sh` scripts generated by the integration test rule.
      Previously this was not an issue because tests were declared at a
      higher-level. As mentioned though this has other downsides and it is
      trivial to rename the file.
    
    * Related to the point above, since tests are now declared witin the
      actual test folder (for perf e.g.), `package.json` files setting
      `"type": "module"` will accidentally cause the `nodejs_test`-generated
      files to be considered ESM. This is not correct and likely needs to be
      fixed upstream in `rules_nodejs` where explicit `.cjs` extensions
      should be used. This is only happening **once** in the `injectable-def`
      test so it is acceptable doing that for now.

--- integration/ngcc/run_test.sh
+++ integration/ngcc/run_test.sh
@@ -214,8 +214,8 @@ ngc -p tsconfig-app.json
 assertSucceeded "Expected the app to successfully compile with the ngcc-processed libraries."
 
 # Did it compile the main.ts correctly (including the ngIf and MatButton directives)?
-  grep "directives: \[.*\.NgIf.*\]" dist/src/main.js
-  assertSucceeded "Expected the compiled app's 'main.ts' to list 'NgIf' in 'directives'."
+grep "directives: \[.*\.NgIf.*\]" dist/src/main.js
+assertSucceeded "Expected the compiled app's 'main.ts' to list 'NgIf' in 'directives'."
 
-  grep "directives: \[.*\.MatButton.*\]" dist/src/main.js
-  assertSucceeded "Expected the compiled app's 'main.ts' to list 'MatButton' in 'directives'."
+grep "directives: \[.*\.MatButton.*\]" dist/src/main.js
+assertSucceeded "Expected the compiled app's 'main.ts' to list 'MatButton' in 'directives'."
