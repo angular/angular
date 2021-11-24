65efda1f9f86d388c7a62b11a753117cf41c6acd
JoostK <joost.koehoorn@gmail.com>
2021-11-24 22:42:28 +0100

    refactor(compiler-cli): dismantle View Engine implementation of ngc
    This commit does a first-pass removal of the View Engine infrastructure
    in compiler-cli. A more in-depth cleanup is necessary and large parts
    of the View Engine compiler infrastructure remain within
    `@angular/compiler`, this is just a first cleanup step.

--- packages/compiler-cli/src/transformers/util.ts
+++ packages/compiler-cli/src/transformers/util.ts
@@ -6,33 +6,14 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {syntaxError} from '@angular/compiler';
-import * as path from 'path';
 import ts from 'typescript';
 
-import {CompilerOptions, DEFAULT_ERROR_CODE, Diagnostic, SOURCE} from './api';
+import {DEFAULT_ERROR_CODE, Diagnostic, SOURCE} from './api';
 
 export const GENERATED_FILES = /(.*?)\.(ngfactory|shim\.ngstyle|ngstyle|ngsummary)\.(js|d\.ts|ts)$/;
-export const DTS = /\.d\.ts$/;
-export const TS = /^(?!.*\.d\.ts$).*\.ts$/;
-
-export const enum StructureIsReused {
-  Not = 0,
-  SafeModules = 1,
-  Completely = 2
-}
-
-// Note: This is an internal property in TypeScript. Use it only for assertions and tests.
-export function tsStructureIsReused(program: ts.Program): StructureIsReused {
-  return (program as any).structureIsReused;
-}
 
 export function error(msg: string): never {
   throw new Error(`Internal error: ${msg}`);
-}
-
-export function userError(msg: string): never {
-  throw syntaxError(msg);
 }
 
 export function createMessageDiagnostic(messageText: string): ts.Diagnostic&Diagnostic {
@@ -45,26 +26,6 @@ export function createMessageDiagnostic(messageText: string): ts.Diagnostic&Diag
     code: DEFAULT_ERROR_CODE,
     source: SOURCE,
   };
-}
-
-export function isInRootDir(fileName: string, options: CompilerOptions) {
-  return !options.rootDir || pathStartsWithPrefix(options.rootDir, fileName);
-}
-
-export function relativeToRootDirs(filePath: string, rootDirs: string[]): string {
-  if (!filePath) return filePath;
-  for (const dir of rootDirs || []) {
-    const rel = pathStartsWithPrefix(dir, filePath);
-    if (rel) {
-      return rel;
-    }
-  }
-  return filePath;
-}
-
-function pathStartsWithPrefix(prefix: string, fullPath: string): string|null {
-  const rel = path.relative(prefix, fullPath);
-  return rel.startsWith('..') ? null : rel;
 }
 
 /**
@@ -93,13 +54,4 @@ export function ngToTsDiagnostic(ng: Diagnostic): ts.Diagnostic {
     start,
     length,
   };
-}
-
-/**
- * Strip multiline comment start and end markers from the `commentText` string.
- *
- * This will also strip the JSDOC comment start marker (`/**`).
- */
-export function stripComment(commentText: string): string {
-  return commentText.replace(/^\/\*\*?/, '').replace(/\*\/$/, '').trim();
 }
