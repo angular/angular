907da3977a38ab62ca7be9d388ff123e463d3581
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-11-26 19:28:39 +0100

    fix(core): destroy hooks not set up for `useClass` provider using `forwardRef` (#44281)
    Fixes an issue where destroy hooks are not set up for `useClass` providers
    that rely on `forwardRef` for passing the actual class reference.
    
    Currently the destroy hooks are not captured because forward refs are
    not resolved for class providers. In ES2015+ this issue becomes even
    more critical if arrow-functions are used for
    
    e.g. `useClass: () => forwardRef(..)`.
    
    This is because arrow functions do not have a prototype, unlike in ES5
    where the function assigned to `useClass` would always have a prototype.
    In ES2015+ a runtime exception as followed is returned (and doesn't give
    any useful indication of the forwardRef issue):
    
    ```
    TypeError: Cannot read properties of undefined (reading 'ngOnDestroy')
      at registerDestroyHooksIfSupported (dist/packages/core/src/render3/di_setup.ts:196:35 <- dist/legacy-test-bundle.spec.js:41508:37)
      at resolveProvider (dist/packages/core/src/render3/di_setup.ts:153:9 <- dist/legacy-test-bundle.spec.js:41485:11)
      at resolveProvider (dist/packages/core/src/render3/di_setup.ts:70:7 <- dist/legacy-test-bundle.spec.js:41445:9)
      at providersResolver (dist/packages/core/src/render3/di_setup.ts:54:5 <- dist/legacy-test-bundle.spec.js:41438:7)
      at Object.definition.providersResolver (dist/packages/core/src/render3/features/providers_feature.ts:48:18 <- dist/legacy-test-bundle.spec.js:41600:16)
      at resolveDirectives (dist/packages/core/src/render3/instructions/shared.ts:1179:40 <- dist/legacy-test-bundle.spec.js:35611:17)
      at elementStartFirstCreatePass (dist/packages/core/src/render3/instructions/element.ts:40:7 <- dist/legacy-test-bundle.spec.js:38432:27)
      at ɵɵelementStart (dist/packages/core/src/render3/instructions/element.ts:87:7 <- dist/legacy-test-bundle.spec.js:38453:43)
      at ɵɵelement (dist/packages/core/src/render3/instructions/element.ts:180:3 <- dist/legacy-test-bundle.spec.js:38510:5)
      at MultipleFormControls_Template (ng:///MultipleFormControls.js:9:9)
    ```
    
    PR Close #44281

---
+++ packages/core/test/render3/providers_helper.ts
@@ -0,0 +1,178 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {InjectorType, Provider, ɵɵdefineComponent, ɵɵdefineDirective, ɵɵelement, ɵɵelementEnd, ɵɵelementStart, ɵɵProvidersFeature, ɵɵtext} from '@angular/core';
+import {createInjector} from '@angular/core/src/di/r3_injector';
+import {RenderFlags} from '@angular/core/src/render3';
+import {ComponentFixture} from '@angular/core/test/render3/render_util';
+
+export interface ComponentTest {
+  providers?: Provider[];
+  viewProviders?: Provider[];
+  directiveProviders?: Provider[];
+  directive2Providers?: Provider[];
+  directiveAssertion?: () => void;
+  componentAssertion?: () => void;
+}
+
+export function expectProvidersScenario(defs: {
+  app?: ComponentTest,
+  parent?: ComponentTest,
+  viewChild?: ComponentTest,
+  contentChild?: ComponentTest,
+  ngModule?: InjectorType<any>,
+}): void {
+  function testComponentInjection<T>(def: ComponentTest|undefined, instance: T): T {
+    if (def) {
+      def.componentAssertion && def.componentAssertion();
+    }
+    return instance;
+  }
+
+  function testDirectiveInjection<T>(def: ComponentTest|undefined, instance: T): T {
+    if (def) {
+      def.directiveAssertion && def.directiveAssertion();
+    }
+    return instance;
+  }
+
+  class ViewChildComponent {
+    static ɵfac = () => testComponentInjection(defs.viewChild, new ViewChildComponent());
+    static ɵcmp = ɵɵdefineComponent({
+      type: ViewChildComponent,
+      selectors: [['view-child']],
+      decls: 1,
+      vars: 0,
+      template:
+          function(fs: RenderFlags, ctx: ViewChildComponent) {
+            if (fs & RenderFlags.Create) {
+              ɵɵtext(0, 'view-child');
+            }
+          },
+      features: defs.viewChild &&
+          [ɵɵProvidersFeature(defs.viewChild.providers || [], defs.viewChild.viewProviders || [])]
+    });
+  }
+
+  class ViewChildDirective {
+    static ɵfac = () => testDirectiveInjection(defs.viewChild, new ViewChildDirective());
+    static ɵdir = ɵɵdefineDirective({
+      type: ViewChildDirective,
+      selectors: [['view-child']],
+      features: defs.viewChild && [ɵɵProvidersFeature(defs.viewChild.directiveProviders || [])],
+    });
+  }
+
+  class ContentChildComponent {
+    static ɵfac =
+        () => {
+          return testComponentInjection(defs.contentChild, new ContentChildComponent());
+        }
+
+    static ɵcmp = ɵɵdefineComponent({
+      type: ContentChildComponent,
+      selectors: [['content-child']],
+      decls: 1,
+      vars: 0,
+      template:
+          function(fs: RenderFlags, ctx: ParentComponent) {
+            if (fs & RenderFlags.Create) {
+              ɵɵtext(0, 'content-child');
+            }
+          },
+      features: defs.contentChild &&
+          [ɵɵProvidersFeature(
+              defs.contentChild.providers || [], defs.contentChild.viewProviders || [])],
+    });
+  }
+
+  class ContentChildDirective {
+    static ɵfac =
+        () => {
+          return testDirectiveInjection(defs.contentChild, new ContentChildDirective());
+        }
+
+    static ɵdir = ɵɵdefineDirective({
+      type: ContentChildDirective,
+      selectors: [['content-child']],
+      features: defs.contentChild &&
+          [ɵɵProvidersFeature(defs.contentChild.directiveProviders || [])],
+    });
+  }
+
+
+  class ParentComponent {
+    static ɵfac = () => testComponentInjection(defs.parent, new ParentComponent());
+    static ɵcmp = ɵɵdefineComponent({
+      type: ParentComponent,
+      selectors: [['parent']],
+      decls: 1,
+      vars: 0,
+      template:
+          function(fs: RenderFlags, ctx: ParentComponent) {
+            if (fs & RenderFlags.Create) {
+              ɵɵelement(0, 'view-child');
+            }
+          },
+      features: defs.parent &&
+          [ɵɵProvidersFeature(defs.parent.providers || [], defs.parent.viewProviders || [])],
+      directives: [ViewChildComponent, ViewChildDirective]
+    });
+  }
+
+  class ParentDirective {
+    static ɵfac = () => testDirectiveInjection(defs.parent, new ParentDirective());
+    static ɵdir = ɵɵdefineDirective({
+      type: ParentDirective,
+      selectors: [['parent']],
+      features: defs.parent && [ɵɵProvidersFeature(defs.parent.directiveProviders || [])],
+    });
+  }
+
+  class ParentDirective2 {
+    static ɵfac = () => testDirectiveInjection(defs.parent, new ParentDirective2());
+    static ɵdir = ɵɵdefineDirective({
+      type: ParentDirective2,
+      selectors: [['parent']],
+      features: defs.parent && [ɵɵProvidersFeature(defs.parent.directive2Providers || [])],
+    });
+  }
+
+
+  class App {
+    static ɵfac = () => testComponentInjection(defs.app, new App());
+    static ɵcmp = ɵɵdefineComponent({
+      type: App,
+      selectors: [['app']],
+      decls: 2,
+      vars: 0,
+      template:
+          function(fs: RenderFlags, ctx: App) {
+            if (fs & RenderFlags.Create) {
+              ɵɵelementStart(0, 'parent');
+              ɵɵelement(1, 'content-child');
+              ɵɵelementEnd();
+            }
+          },
+      features: defs.app &&
+          [ɵɵProvidersFeature(defs.app.providers || [], defs.app.viewProviders || [])],
+      directives:
+          [
+            ParentComponent, ParentDirective2, ParentDirective, ContentChildComponent,
+            ContentChildDirective
+          ]
+    });
+  }
+
+
+  const fixture = new ComponentFixture(
+      App, {injector: defs.ngModule ? createInjector(defs.ngModule) : undefined});
+  expect(fixture.html).toEqual('<parent><view-child>view-child</view-child></parent>');
+
+  fixture.destroy();
+}
