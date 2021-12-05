angular/angular

commit 3c8a5d1c706fe0ebff621f76eb9e98e0f5ec0f77

fix(ngcc): support the UMD wrapper function format emitted by Webpack (#44245)
Previously, ngcc could only handle UMD modules whose wrapper function
was implemented as a `ts.ConditionalExpression` (i.e. using a ternary
operator). This is the format emitted by popular bundlers, such as
Rollup.

This commit adds support for a different format, that uses `if/else`
statements, which is what is [emitted by Webpack][1].

[1]: https://webpack.js.org/configuration/output/#type-umd

Fixes #44019

PR Close #44245

packages/compiler-cli/ngcc/src/host/umd_host.ts
@@ -567,10 +567,15 @@ function getUmdWrapper(statement: ts.Statement):
 
 /**
  * Parse the wrapper function of a UMD module and extract info about the factory function calls for
- * the various formats (CommonJS, AMD, global).
+ * the various formats (CommonJS, CommonJS2, AMD, global).
  *
- * The supported format for the UMD wrapper function body is a single statement which is a
- * `ts.ConditionalExpression` (i.e. using a ternary operator). For example:
+ * NOTE:
+ * For more info on the distinction between CommonJS and CommonJS2 see
+ * https://github.com/webpack/webpack/issues/1114.
+ *
+ * The supported format for the UMD wrapper function body is a single statement which is either a
+ * `ts.ConditionalExpression` (i.e. using a ternary operator) (typically emitted by Rollup) or a
+ * `ts.IfStatement` (typically emitted by Webpack). For example:
  *
  * ```js
  * // Using a conditional expression:
@@ -587,6 +592,28 @@ function getUmdWrapper(statement: ts.Statement):
  *   // ...
  * }));
  * ```
+ *
+ * or
+ *
+ * ```js
+ * // Using an `if` statement:
+ * (function (root, factory) {
+ *   if (typeof exports === 'object' && typeof module === 'object')
+ *     // CommonJS2 factory call.
+ *     module.exports = factory(require('foo'), require('bar'));
+ *   else if (typeof define === 'function' && define.amd)
+ *     // AMD factory call.
+ *     define(['foo', 'bar'], factory);
+ *   else if (typeof exports === 'object')
+ *     // CommonJS factory call.
+ *     exports['my-lib'] = factory(require('foo'), require('bar'));
+ *   else
+ *     // Global factory call.
+ *     root['my-lib'] = factory(root['foo'], root['bar']);
+ * })(global, function (foo, bar) {
+ *   // ...
+ * });
+ * ```
  */
 function parseUmdWrapperFunction(wrapperFn: ts.FunctionExpression): UmdModule['factoryCalls'] {
   const stmt = wrapperFn.body.statements[0];
@@ -594,25 +621,27 @@ function parseUmdWrapperFunction(wrapperFn: ts.FunctionExpression): UmdModule['f
 
   if (ts.isExpressionStatement(stmt) && ts.isConditionalExpression(stmt.expression)) {
     conditionalFactoryCalls = extractFactoryCallsFromConditionalExpression(stmt.expression);
+  } else if (ts.isIfStatement(stmt)) {
+    conditionalFactoryCalls = extractFactoryCallsFromIfStatement(stmt);
   } else {
     throw new Error(
-        'UMD wrapper body is not in a supported format (expected a conditional expression):\n' +
-        wrapperFn.body.getText());
+        'UMD wrapper body is not in a supported format (expected a conditional expression or if ' +
+        'statement):\n' + wrapperFn.body.getText());
   }
 
-  const factoryCalls = {
-    amdDefine: getAmdDefineCall(conditionalFactoryCalls),
-    commonJs: getCommonJsFactoryCall(conditionalFactoryCalls),
-    global: getGlobalFactoryCall(conditionalFactoryCalls),
-  };
+  const amdDefine = getAmdDefineCall(conditionalFactoryCalls);
+  const commonJs = getCommonJsFactoryCall(conditionalFactoryCalls);
+  const commonJs2 = getCommonJs2FactoryCall(conditionalFactoryCalls);
+  const global = getGlobalFactoryCall(conditionalFactoryCalls);
+  const cjsCallForImports = commonJs2 || commonJs;
 
-  if (factoryCalls.commonJs === null) {
+  if (cjsCallForImports === null) {
     throw new Error(
-        'Unable to find a CommonJS factory call inside the UMD wrapper function:\n' +
+        'Unable to find a CommonJS or CommonJS2 factory call inside the UMD wrapper function:\n' +
         stmt.getText());
   }
 
-  return factoryCalls as (typeof factoryCalls&{commonJs: ts.CallExpression});
+  return {amdDefine, commonJs, commonJs2, global, cjsCallForImports};
 }
 
 /**
@@ -657,6 +686,64 @@ function extractFactoryCallsFromConditionalExpression(node: ts.ConditionalExpres
   return factoryCalls;
 }
 
+/**
+ * Extract `UmdConditionalFactoryCall`s from a `ts.IfStatement` of the form:
+ *
+ * ```js
+ * if (typeof exports === 'object' && typeof module === 'object')
+ *   // CommonJS2 factory call.
+ *   module.exports = factory(require('foo'), require('bar'));
+ * else if (typeof define === 'function' && define.amd)
+ *   // AMD factory call.
+ *   define(['foo', 'bar'], factory);
+ * else if (typeof exports === 'object')
+ *   // CommonJS factory call.
+ *   exports['my-lib'] = factory(require('foo'), require('bar'));
+ * else
+ *   // Global factory call.
+ *   root['my-lib'] = factory(root['foo'], root['bar']);
+ * ```
+ */
+function extractFactoryCallsFromIfStatement(node: ts.IfStatement): UmdConditionalFactoryCall[] {
+  const factoryCalls: UmdConditionalFactoryCall[] = [];
+  let currentNode: ts.Statement|undefined = node;
+
+  while (currentNode && ts.isIfStatement(currentNode)) {
+    if (!ts.isBinaryExpression(currentNode.expression)) {
+      throw new Error(
+          'Condition inside UMD wrapper is not a binary expression:\n' +
+          currentNode.expression.getText());
+    }
+    if (!ts.isExpressionStatement(currentNode.thenStatement)) {
+      throw new Error(
+          'Then-statement inside UMD wrapper is not an expression statement:\n' +
+          currentNode.thenStatement.getText());
+    }
+
+    factoryCalls.push({
+      condition: currentNode.expression,
+      factoryCall: getFunctionCallFromExpression(currentNode.thenStatement.expression),
+    });
+
+    currentNode = currentNode.elseStatement;
+  }
+
+  if (currentNode) {
+    if (!ts.isExpressionStatement(currentNode)) {
+      throw new Error(
+          'Else-statement inside UMD wrapper is not an expression statement:\n' +
+          currentNode.getText());
+    }
+
+    factoryCalls.push({
+      condition: null,
+      factoryCall: getFunctionCallFromExpression(currentNode.expression),
+    });
+  }
+
+  return factoryCalls;
+}
+
 function getFunctionCallFromExpression(node: ts.Expression): ts.CallExpression {
   // Be resilient to `node` being inside parenthesis.
   if (ts.isParenthesizedExpression(node)) {
@@ -665,8 +752,9 @@ function getFunctionCallFromExpression(node: ts.Expression): ts.CallExpression {
     return getFunctionCallFromExpression(node.expression);
   }
 
-  // Be resilient to `node` being part of a comma expression.
-  if (ts.isBinaryExpression(node) && node.operatorToken.kind === ts.SyntaxKind.CommaToken) {
+  // Be resilient to `node` being part of an assignment or comma expression.
+  if (ts.isBinaryExpression(node) &&
+      [ts.SyntaxKind.CommaToken, ts.SyntaxKind.EqualsToken].includes(node.operatorToken.kind)) {
     // NOTE:
     // Since we are going further down the AST, there is no risk of infinite recursion.
     return getFunctionCallFromExpression(node.right);
@@ -698,15 +786,29 @@ function getAmdDefineCall(calls: UmdConditionalFactoryCall[]): ts.CallExpression
  * Get the factory call for setting up the CommonJS dependencies in the UMD wrapper.
  */
 function getCommonJsFactoryCall(calls: UmdConditionalFactoryCall[]): ts.CallExpression|null {
-  // The factory call for CommonJS dependencies is the one that is guarded with a `&&` expression
-  // whose one side is a `typeof exports` or `typeof module` condition.
+  // The factory call for CommonJS dependencies is the one that is guarded with a `typeof exports`
+  // condition.
   const cjsConditionalCall = calls.find(
+      call => call.condition?.operatorToken.kind === ts.SyntaxKind.EqualsEqualsEqualsToken &&
+          isTypeOf(call.condition, 'exports') && ts.isIdentifier(call.factoryCall.expression) &&
+          call.factoryCall.expression.text === 'factory');
+
+  return cjsConditionalCall?.factoryCall ?? null;
+}
+
+/**
+ * Get the factory call for setting up the CommonJS2 dependencies in the UMD wrapper.
+ */
+function getCommonJs2FactoryCall(calls: UmdConditionalFactoryCall[]): ts.CallExpression|null {
+  // The factory call for CommonJS2 dependencies is the one that is guarded with a `&&` expression
+  // whose one side is a `typeof exports` or `typeof module` condition.
+  const cjs2ConditionalCall = calls.find(
       call => call.condition?.operatorToken.kind === ts.SyntaxKind.AmpersandAmpersandToken &&
           oneOfBinaryConditions(call.condition, exp => isTypeOf(exp, 'exports', 'module')) &&
           ts.isIdentifier(call.factoryCall.expression) &&
           call.factoryCall.expression.text === 'factory');
 
-  return cjsConditionalCall?.factoryCall ?? null;
+  return cjs2ConditionalCall?.factoryCall ?? null;
 }
 
 /**
@@ -733,13 +835,19 @@ function isTypeOf(node: ts.Expression, ...types: string[]): boolean {
 export function getImportsOfUmdModule(umdModule: UmdModule):
     {parameter: ts.ParameterDeclaration, path: string}[] {
   const imports: {parameter: ts.ParameterDeclaration, path: string}[] = [];
-  const cjsFactoryCall = umdModule.factoryCalls.commonJs;
+  const cjsFactoryCall = umdModule.factoryCalls.cjsCallForImports;
 
-  for (let i = 1; i < umdModule.factoryFn.parameters.length; i++) {
-    imports.push({
-      parameter: umdModule.factoryFn.parameters[i],
-      path: getRequiredModulePath(cjsFactoryCall, i),
-    });
+  // Some UMD formats pass `exports` as the first argument to the factory call, while others don't.
+  // Compute the index at which the dependencies start (i.e. the index of the first `require` call).
+  const depStartIndex = cjsFactoryCall.arguments.findIndex(arg => isRequireCall(arg));
+
+  if (depStartIndex !== -1) {
+    for (let i = depStartIndex; i < umdModule.factoryFn.parameters.length; i++) {
+      imports.push({
+        parameter: umdModule.factoryFn.parameters[i],
+        path: getRequiredModulePath(cjsFactoryCall, i),
+      });
+    }
   }
 
   return imports;
@@ -748,9 +856,8 @@ export function getImportsOfUmdModule(umdModule: UmdModule):
 interface UmdModule {
   wrapperFn: ts.FunctionExpression;
   factoryFn: ts.FunctionExpression;
-  factoryCalls: {
-    commonJs: ts.CallExpression; amdDefine: ts.CallExpression | null;
-    global: ts.CallExpression | null;
+  factoryCalls: Record<'amdDefine'|'commonJs'|'commonJs2'|'global', ts.CallExpression|null>&{
+    cjsCallForImports: ts.CallExpression;
   };
 }
 

packages/compiler-cli/ngcc/test/dependencies/umd_dependency_host_spec.ts
@@ -33,6 +33,15 @@ runInEachFileSystem(() => {
         expect(ts.createSourceFile).not.toHaveBeenCalled();
       });
 
+      it('should correctly process files with no imports', () => {
+        const {dependencies, missing, deepImports} = createDependencyInfo();
+        host.collectDependencies(
+            _('/no/imports/but/cannot/skip/index.js'), {dependencies, missing, deepImports});
+        expect(dependencies.size).toBe(0);
+        expect(missing.size).toBe(0);
+        expect(deepImports.size).toBe(0);
+      });
+
       it('should resolve all the external imports of the source file', () => {
         const {dependencies, missing, deepImports} = createDependencyInfo();
         host.collectDependencies(
@@ -143,6 +152,18 @@ runInEachFileSystem(() => {
         },
         {name: _('/no/imports/or/re-exports/package.json'), contents: '{"esm2015": "./index.js"}'},
         {name: _('/no/imports/or/re-exports/index.metadata.json'), contents: 'MOCK METADATA'},
+        {
+          name: _('/no/imports/but/cannot/skip/index.js'),
+          contents:
+              '// A file containing text that looks like a `require("call")` to trick the host\n' +
+              '// into processing it even if it has no actual dependencies.\n' +
+              umd('no_imports_but_cannot_skip', []),
+        },
+        {
+          name: _('/no/imports/but/cannot/skip/package.json'),
+          contents: '{"esm2015": "./index.js"}'
+        },
+        {name: _('/no/imports/but/cannot/skip/index.metadata.json'), contents: 'MOCK METADATA'},
         {
           name: _('/external/imports/index.js'),
           contents: umd('imports_index', ['lib_1', 'lib_1/sub_1'])

packages/compiler-cli/ngcc/src/rendering/umd_rendering_formatter.ts
@@ -62,6 +62,7 @@ export class UmdRenderingFormatter extends Esm5RenderingFormatter {
 
     // We need to add new `require()` calls for each import in the CommonJS initializer
     renderCommonJsDependencies(output, factoryCalls.commonJs, imports);
+    renderCommonJsDependencies(output, factoryCalls.commonJs2, imports);
     renderAmdDependencies(output, factoryCalls.amdDefine, imports);
     renderGlobalDependencies(output, factoryCalls.global, imports);
     renderFactoryParameters(output, factoryFn, imports);
@@ -134,7 +135,7 @@ export class UmdRenderingFormatter extends Esm5RenderingFormatter {
 }
 
 /**
- * Add dependencies to the CommonJS part of the UMD wrapper function.
+ * Add dependencies to the CommonJS/CommonJS2 part of the UMD wrapper function.
  */
 function renderCommonJsDependencies(
     output: MagicString, factoryCall: ts.CallExpression|null, imports: Import[]) {

commit 86d5c2e75849c6c9f0c724871154b328358e4ab8

fix(ngcc): correctly resolve UMD dependencies
Previously, when processing UMD, ngcc assumed that the `exports`
argument of the CommonJS factory call (if present) would be the first
argument of the call. This is generally true for the supported UMD
formats, but can change if ngcc prepends more imports (and thus factory
arguments) while processing the module. This could lead to errors when
trying to collect dependencies of an already processed module.
(This was accidentally broken in #44245 (commit 2bc3522e167).)

This commit fixes it by not making any assumptions about the position of
an `exports` argument in the CommonJS factory call.

Fixes #44380

packages/compiler-cli/ngcc/src/host/umd_host.ts
@@ -837,17 +837,32 @@ export function getImportsOfUmdModule(umdModule: UmdModule):
   const imports: {parameter: ts.ParameterDeclaration, path: string}[] = [];
   const cjsFactoryCall = umdModule.factoryCalls.cjsCallForImports;
 
-  // Some UMD formats pass `exports` as the first argument to the factory call, while others don't.
-  // Compute the index at which the dependencies start (i.e. the index of the first `require` call).
-  const depStartIndex = cjsFactoryCall.arguments.findIndex(arg => isRequireCall(arg));
+  for (let i = 0; i < umdModule.factoryFn.parameters.length; i++) {
+    const arg = cjsFactoryCall.arguments[i];
 
-  if (depStartIndex !== -1) {
-    for (let i = depStartIndex; i < umdModule.factoryFn.parameters.length; i++) {
-      imports.push({
-        parameter: umdModule.factoryFn.parameters[i],
-        path: getRequiredModulePath(cjsFactoryCall, i),
-      });
+    if (arg === undefined) {
+      throw new Error(
+          `Missing argument at index ${i} from UMD factory call:\n` + cjsFactoryCall.getText());
     }
+
+    if (ts.isIdentifier(arg) && arg.text === 'exports') {
+      // Some UMD formats pass `exports` as the first argument to the factory call, while others
+      // don't. Also, after ngcc has added new imports, `exports` might not be the first argument
+      // any more.
+      // Ignore `exports` and only look at other arguments for imports.
+      continue;
+    }
+
+    if (!isRequireCall(arg)) {
+      throw new Error(
+          `Argument at index ${i} of UMD factory call is not a \`require\` call with a single ` +
+          'string argument:\n' + cjsFactoryCall.getText());
+    }
+
+    imports.push({
+      parameter: umdModule.factoryFn.parameters[i],
+      path: arg.arguments[0].text,
+    });
   }
 
   return imports;
@@ -870,18 +885,6 @@ interface UmdModule {
 interface UmdConditionalFactoryCall {
   condition: ts.BinaryExpression|null;
   factoryCall: ts.CallExpression;
-}
-
-function getRequiredModulePath(cjsFactoryCall: ts.CallExpression, paramIndex: number): string {
-  const requireCall = cjsFactoryCall.arguments[paramIndex];
-
-  if (requireCall !== undefined && !isRequireCall(requireCall)) {
-    throw new Error(
-        `Argument at index ${paramIndex} of UMD factory call is not a \`require\` call with a ` +
-        'single string argument:\n' + cjsFactoryCall.getText());
-  }
-
-  return requireCall.arguments[0].text;
 }
 
 /**

packages/compiler-cli/ngcc/test/dependencies/umd_dependency_host_spec.ts
@@ -13,7 +13,7 @@ import {loadTestFiles} from '../../../src/ngtsc/testing';
 import {createDependencyInfo} from '../../src/dependencies/dependency_host';
 import {ModuleResolver} from '../../src/dependencies/module_resolver';
 import {UmdDependencyHost} from '../../src/dependencies/umd_dependency_host';
-import {testForEachUmdFormat} from '../helpers/umd_utils';
+import {AdditionalFormatOptions, testForEachUmdFormat} from '../helpers/umd_utils';
 
 runInEachFileSystem(() => {
   describe(
@@ -50,6 +50,17 @@ runInEachFileSystem(() => {
             const {dependencies, missing, deepImports} = createDependencyInfo();
             host.collectDependencies(
                 _('/external/imports/index.js'), {dependencies, missing, deepImports});
+            expect(dependencies.size).toBe(2);
+            expect(missing.size).toBe(0);
+            expect(deepImports.size).toBe(0);
+            expect(dependencies.has(_('/node_modules/lib_1'))).toBe(true);
+            expect(dependencies.has(_('/node_modules/lib_1/sub_1'))).toBe(true);
+          });
+
+          it('should resolve imports preceeding the `exports` parameter', () => {
+            const {dependencies, missing, deepImports} = createDependencyInfo();
+            host.collectDependencies(
+                _('/external/preceeding/index.js'), {dependencies, missing, deepImports});
             expect(dependencies.size).toBe(2);
             expect(missing.size).toBe(0);
             expect(deepImports.size).toBe(0);
@@ -196,6 +207,13 @@ runInEachFileSystem(() => {
             {name: _('/external/imports/package.json'), contents: '{"main": "./index.js"}'},
             {name: _('/external/imports/index.metadata.json'), contents: 'MOCK METADATA'},
             {
+              name: _('/external/preceeding/index.js'),
+              contents:
+                  umd('imports_index', ['lib_1', 'lib_1/sub_1'], undefined, {exportsParamIndex: 1})
+            },
+            {name: _('/external/preceeding/package.json'), contents: '{"main": "./index.js"}'},
+            {name: _('/external/preceeding/index.metadata.json'), contents: 'MOCK METADATA'},
+            {
               name: _('/external/re-exports/index.js'),
               contents: umd('imports_index', ['lib_1', 'lib_1/sub_1'], ['lib_1.X', 'lib_1sub_1.Y'])
             },
@@ -293,10 +311,12 @@ runInEachFileSystem(() => {
           ]);
         }
 
-        function umd(moduleName: string, importPaths: string[], exportNames: string[] = []) {
+        function umd(
+            moduleName: string, importPaths: string[], exportNames: string[] = [],
+            additionalOptions?: AdditionalFormatOptions) {
           const exportStatements =
               exportNames.map(e => `  exports.${e.replace(/.+\./, '')} = ${e};`).join('\n');
-          return createUmdModule(moduleName, importPaths, exportStatements);
+          return createUmdModule(moduleName, importPaths, exportStatements, additionalOptions);
         }
       }));
 });

packages/compiler-cli/ngcc/test/helpers/umd_utils.ts
@@ -7,8 +7,26 @@
  */
 
 export interface AdditionalFormatOptions {
+  /**
+   * The index of the `exports` parameter index in the factory function (and thus the corresponding
+   * argument in the various factory calls). (Only affects the `Rollup` format.)
+   *
+   * Defaults to 0 (i.e. `exports` being the first argument).
+   */
+  exportsParamIndex?: number;
+
+  /**
+   * Whether to include an initializer for the `global` variable (`global = global || self`) before
+   * the global factory call. (Only affects the `Rollup` format.)
+   */
   hasGlobalInitializer?: boolean;
-  omitExports?: boolean;
+
+  /**
+   * A set of dependencies that are not used in the factory function (i.e. for which factory
+   * function parameters are omitted).
+   * Unused dependencies must always follow used ones (if any). In other words, unused dependencies
+   * can only appear at the end of the dependency list.
+   */
   unusedDependencies?: Set<String>;
 }
 
@@ -27,7 +45,7 @@ export function createUmdModuleFactory(
     parenthesisFormat: ParenthesisFormat = ParenthesisFormat.AroundIife) {
   return function createUmdModule(
       moduleName: string, dependencies: string[], factoryBody: string,
-      {hasGlobalInitializer = false, omitExports = false, unusedDependencies = new Set()}:
+      {exportsParamIndex = 0, hasGlobalInitializer = false, unusedDependencies = new Set()}:
           AdditionalFormatOptions = {}) {
     // Ensure that any unused dependencies are at the end.
     const firstUnusedDepIndex = dependencies.findIndex(dep => unusedDependencies.has(dep));
@@ -48,14 +66,16 @@ export function createUmdModuleFactory(
                    .replace(/[-/](.?)/g, (_, g1) => g1.toUpperCase()));
     const depsParamNames = dependencies.filter(dep => !unusedDependencies.has(dep))
                                .map((dep, i) => depsIdentifiers[i].replace(/^.*\./, ''));
-    const maybePrepend = (item: string, arr: string[]) => omitExports ? arr : [item, ...arr];
+    const insertItem = (item: string, arr: string[]) => (exportsParamIndex < 0) ?
+        arr :
+        [...arr.slice(0, exportsParamIndex), item, ...arr.slice(exportsParamIndex)];
 
     switch (wrapperFunctionFormat) {
       case WrapperFunctionFormat.Rollup:
-        const cjsArgs = maybePrepend('exports', dependencies.map(d => `require('${d}')`));
-        const amdArgs = maybePrepend('exports', dependencies).map(d => `'${d}'`);
-        const globalArgs = maybePrepend(moduleName, depsIdentifiers).map(id => `global.${id}`);
-        const factoryParams = maybePrepend('exports', depsParamNames);
+        const cjsArgs = insertItem('exports', dependencies.map(d => `require('${d}')`));
+        const amdArgs = insertItem('exports', dependencies).map(d => `'${d}'`);
+        const globalArgs = insertItem(moduleName, depsIdentifiers).map(id => `global.${id}`);
+        const factoryParams = insertItem('exports', depsParamNames);
 
         wrapperFunctionDeclaration = stripIndentation(`
           function (global, factory) {

packages/compiler-cli/ngcc/test/rendering/umd_rendering_formatter_spec.ts
@@ -351,7 +351,7 @@ exports.BadIife = BadIife;
                 factoryBody: `
                     var index = '';
                     return index;`,
-                additionalOptions: {omitExports: true},
+                additionalOptions: {exportsParamIndex: -1},
               },
             });
             const {renderer, program} = setup(PROGRAM);