991377aae1913cd410d0ed0e8d1940c36c3fad25
Andrew Kushnir <akushnir@google.com>
2021-12-07 17:24:46 -0800

    refactor(core): make RuntimeError reusable across packages (#44398)
    This commit updates the code around the `RuntimeError` class to make it more reusable between packages (currently it's only usable inside the `core` package). Specifically:
    - the error formatting logic was updated to handle cases when there is no error message provided
    - there is no special Set that contains a set of error codes for which we have guides on angular.io. Instead, this is now encoded into the error code itself (making such codes negative integers). Having a separate Set makes it non-tree-shakable, which we want to avoid.
    
    This change should allow to employ the `RuntimeError` class in other packages to further standardize this subsystem and make the errors thrown by the framework consistent.
    
    As a part of the refactoring, the `common` package code was also updated to follow the same logic as `core`, since the `RuntimeError` class was used there as well.
    
    PR Close #44398

---
+++ packages/core/test/runtime_error_spec.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {RuntimeError, RuntimeErrorCode} from '../src/render3/error_code';
+
+describe('RuntimeError utils', () => {
+  it('should format the error message correctly', () => {
+    // Error with a guide, but without an error message.
+    let errorInstance = new RuntimeError(RuntimeErrorCode.EXPORT_NOT_FOUND, '');
+    expect(errorInstance.toString())
+        .toBe('Error: NG0301. Find more at https://angular.io/errors/NG0301');
+
+    // Error without a guide and an error message.
+    errorInstance = new RuntimeError(RuntimeErrorCode.TEMPLATE_STRUCTURE_ERROR, '');
+    expect(errorInstance.toString()).toBe('Error: NG0305');
+
+    // Error without a guide, but with an error message.
+    errorInstance =
+        new RuntimeError(RuntimeErrorCode.TEMPLATE_STRUCTURE_ERROR, 'Some error message');
+    expect(errorInstance.toString()).toBe('Error: NG0305: Some error message');
+
+    // Error with both a guide and an error message.
+    errorInstance = new RuntimeError(RuntimeErrorCode.EXPORT_NOT_FOUND, 'Some error message');
+    expect(errorInstance.toString())
+        .toBe('Error: NG0301: Some error message. Find more at https://angular.io/errors/NG0301');
+  });
+});
