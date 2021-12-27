a7af786747aad1cbb91f83b26135f4ee58ce517c
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-12-27 17:54:27 +0100

    refactor(docs-infra): remove IE11 code
    since IE11 is no longer a supported browser, remove angular.io code
    implemented specifically for IE11 support
    
    this change reverts the following PRs:
     - #26059 (slightly)
     - #28188 (fully)
     - #32871 (fully)
     - #41183 (partially)

--- aio/src/app/custom-elements/code/code-tabs.component.ts
+++ aio/src/app/custom-elements/code/code-tabs.component.ts
@@ -59,15 +59,13 @@ export class CodeTabsComponent implements OnInit, AfterViewInit {
     const contentElem = this.content.nativeElement;
     const codeExamples = Array.from(contentElem.querySelectorAll('code-pane'));
 
+    // Remove DOM nodes that are no longer needed.
+    contentElem.innerHTML = '' as unknown as TrustedHTML as unknown as string;
+
     for (const tabContent of codeExamples) {
       this.tabs.push(this.getTabInfo(tabContent));
     }
 
-    // Remove DOM nodes that are no longer needed.
-    //
-    // NOTE:
-    // In IE11, doing this also empties the `<code-pane>` nodes captured in `codeExamples` ¯\_(ツ)_/¯
-    // Only remove the unnecessary nodes after having captured the `<code-pane>` contents.
     contentElem.textContent = '';
   }
 
