1028a4eb6c805da9e841ebbbcbbae579901e4a62
Doug Parker <dgp1130@users.noreply.github.com>
2022-01-06 11:14:52 -0800

    refactor(compiler-cli): update template typechecking link to latest angular.io version (#44649)
    This page exists in the most recent angular.io version (v13 currently), so there's no need to link to an old version. The hash also refers to the title section of the page, which isn't necessary and is now dropped.
    
    PR Close #44649

--- packages/compiler-cli/src/ngtsc/core/src/compiler.ts
+++ packages/compiler-cli/src/ngtsc/core/src/compiler.ts
@@ -1160,7 +1160,7 @@ One of the following actions is required:
 2. Remove "strictTemplates" or set it to 'false'.
 
 More information about the template type checking compiler options can be found in the documentation:
-https://v9.angular.io/guide/template-typecheck#template-type-checking`,
+https://angular.io/guide/template-typecheck`,
     };
   }
 
