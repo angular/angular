b0fb64ea14d2d70cdb9597c2851723f6bcbae8a6
dario-piotrowicz <dario.piotrowicz@gmail.com>
2022-01-12 21:37:37 +0000

    refactor(elements): remove the createCustomEvent function
    the createCustomEvent function's pupose is to create customEvents
    even on browsers where `CustomEvent` is not a constructor,
    `CustomEvent` is currently available in all supported browsers (since
    IE11 support has ended), so remove the function as it is no longer
    needed

--- packages/elements/src/utils.ts
+++ packages/elements/src/utils.ts
@@ -53,23 +53,6 @@ export function camelToDashCase(input: string): string {
 }
 
 /**
- * Create a `CustomEvent` (even on browsers where `CustomEvent` is not a constructor).
- */
-export function createCustomEvent(doc: Document, name: string, detail: any): CustomEvent {
-  const bubbles = false;
-  const cancelable = false;
-
-  // On IE11, `CustomEvent` is not a constructor.
-  if (typeof CustomEvent !== 'function') {
-    const event = doc.createEvent('CustomEvent');
-    event.initCustomEvent(name, bubbles, cancelable, detail);
-    return event;
-  }
-
-  return new CustomEvent(name, {bubbles, cancelable, detail});
-}
-
-/**
  * Check whether the input is an `Element`.
  */
 export function isElement(node: Node|null): node is Element {
