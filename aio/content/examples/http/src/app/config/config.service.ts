9f3feb8e880684f201e3c943107be3f4f81a0869
Srdjan Milic <srdjanrmilic@gmail.com>
2022-01-16 08:04:51 +0100

    refactor(http): code styling issues
    Fixed long line code issue that CircleCi test complain about.

--- aio/content/examples/http/src/app/config/config.service.ts
+++ aio/content/examples/http/src/app/config/config.service.ts
@@ -82,7 +82,9 @@ export class ConfigService {
         `Backend returned code ${error.status}, body was: `, error.error);
     }
     // Return an observable with a user-facing error message.
-    return throwError(() => new Error('Something bad happened; please try again later.'));
+    return throwError(() => {
+      new Error('Something bad happened; please try again later.');
+    });
   }
   // #enddocregion handleError
 
