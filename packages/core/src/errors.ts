2916ac9e1af827bca363638507f5ed51cd72f09d
Andrew Kushnir <akushnir@google.com>
2022-01-21 13:02:40 -0800

    refactor(core): the `RuntimeError` class should support more compact syntax (#44783)
    This commit refactors the `RuntimeError` class to support a short version of providing error messages:
    ```
    throw new RuntimeError(
      RuntimeErrorCode.INJECTOR_ALREADY_DESTROYED,
      ngDevMode && 'Injector has already been destroyed.');
    ```
    In prod mode, the second argument becomes `false` andn this commit extends the typings to support that.
    
    This commit also contains a couple places were the `RuntimeError` class is used to demostrate the compact form.
    
    PR Close #44783

--- packages/core/src/errors.ts
+++ packages/core/src/errors.ts
@@ -65,22 +65,40 @@ export const enum RuntimeErrorCode {
   UNSAFE_VALUE_IN_SCRIPT = 905
 }
 
-export class RuntimeError<T = RuntimeErrorCode> extends Error {
-  constructor(public code: T, message: string) {
+/**
+ * Class that represents a runtime error.
+ * Formats and outputs the error message in a consistent way.
+ *
+ * Example:
+ * ```
+ *  throw new RuntimeError(
+ *    RuntimeErrorCode.INJECTOR_ALREADY_DESTROYED,
+ *    ngDevMode && 'Injector has already been destroyed.');
+ * ```
+ *
+ * Note: the `message` argument contains a descriptive error message as a string in development
+ * mode (when the `ngDevMode` is defined). In production mode (after tree-shaking pass), the
+ * `message` argument becomes `false`, thus we account for it in the typings and the runtime logic.
+ */
+export class RuntimeError<T extends number = RuntimeErrorCode> extends Error {
+  constructor(public code: T, message: null|false|string) {
     super(formatRuntimeError<T>(code, message));
   }
 }
 
-/** Called to format a runtime error */
-export function formatRuntimeError<T = RuntimeErrorCode>(code: T, message: string): string {
-  const codeAsNumber = code as unknown as number;
+/**
+ * Called to format a runtime error.
+ * See additional info on the `message` argument type in the `RuntimeError` class description.
+ */
+export function formatRuntimeError<T extends number = RuntimeErrorCode>(
+    code: T, message: null|false|string): string {
   // Error code might be a negative number, which is a special marker that instructs the logic to
   // generate a link to the error details page on angular.io.
-  const fullCode = `NG0${Math.abs(codeAsNumber)}`;
+  const fullCode = `NG0${Math.abs(code)}`;
 
   let errorMessage = `${fullCode}${message ? ': ' + message : ''}`;
 
-  if (ngDevMode && codeAsNumber < 0) {
+  if (ngDevMode && code < 0) {
     errorMessage = `${errorMessage}. Find more at ${ERROR_DETAILS_PAGE_BASE_URL}/${fullCode}`;
   }
   return errorMessage;
