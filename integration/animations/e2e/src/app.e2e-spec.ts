fcc6528159cd43337fcbdbfded924a19fa8625a2
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-25 15:31:15 -0600

    test(animations): Add bundling size regression test for animations package (#44826)
    This adds a size regression test for the animations package to the test suite.
    
    PR Close #44826

---
+++ integration/animations/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,24 @@
+import {browser, logging} from 'protractor';
+
+import {AppPage} from './app.po';
+
+describe('animations app', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display title', () => {
+    page.navigateTo();
+    expect(page.getTitleText()).toEqual('Animations app');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
