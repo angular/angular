c8d6f2f31d62fdfca0537a75c70ac006a7292283
Andrew Kushnir <akushnir@google.com>
2022-01-30 19:50:11 -0800

    perf(animations): remove no longer needed CssKeyframes classes (#44903)
    This commit removes CSS keyframes-based code that was used to support animations in old browsers. With IE11 deprecation, all supported browsers have native WebAnimations support, so the old code can be removed. This results in ~7KB decrease of the animations package bundle size, since most of the code was non-tree-shakable.
    
    Closes #44520.

--- packages/animations/browser/test/render/web_animations/web_animations_driver_spec.ts
+++ packages/animations/browser/test/render/web_animations/web_animations_driver_spec.ts
@@ -7,7 +7,6 @@
  */
 import {browserDetection} from '@angular/platform-browser/testing/src/browser_util';
 
-import {CssKeyframesPlayer} from '../../../src/render/css_keyframes/css_keyframes_player';
 import {WebAnimationsDriver} from '../../../src/render/web_animations/web_animations_driver';
 import {WebAnimationsPlayer} from '../../../src/render/web_animations/web_animations_player';
 
@@ -15,38 +14,11 @@ import {WebAnimationsPlayer} from '../../../src/render/web_animations/web_animat
   describe('WebAnimationsDriver', () => {
     if (isNode) return;
 
-    describe('when web-animations are not supported natively', () => {
-      it('should return an instance of a CssKeyframePlayer if scrubbing is not requested', () => {
-        const element = createElement();
-        const driver = makeDriver();
-        driver.overrideWebAnimationsSupport(false);
-        const player = driver.animate(element, [], 1000, 1000, '', [], false);
-        expect(player instanceof CssKeyframesPlayer).toBeTruthy();
-      });
-
-      it('should return an instance of a WebAnimationsPlayer if scrubbing is not requested', () => {
-        const element = createElement();
-        const driver = makeDriver();
-        driver.overrideWebAnimationsSupport(false);
-        const player = driver.animate(element, [], 1000, 1000, '', [], true);
-        expect(player instanceof WebAnimationsPlayer).toBeTruthy();
-      });
-    });
-
     describe('when web-animations are supported natively', () => {
       it('should return an instance of a WebAnimationsPlayer if scrubbing is not requested', () => {
         const element = createElement();
         const driver = makeDriver();
-        driver.overrideWebAnimationsSupport(true);
-        const player = driver.animate(element, [], 1000, 1000, '', [], false);
-        expect(player instanceof WebAnimationsPlayer).toBeTruthy();
-      });
-
-      it('should return an instance of a WebAnimationsPlayer if scrubbing is requested', () => {
-        const element = createElement();
-        const driver = makeDriver();
-        driver.overrideWebAnimationsSupport(true);
-        const player = driver.animate(element, [], 1000, 1000, '', [], true);
+        const player = driver.animate(element, [], 1000, 1000, '', []);
         expect(player instanceof WebAnimationsPlayer).toBeTruthy();
       });
     });
