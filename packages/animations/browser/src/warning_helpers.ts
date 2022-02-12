6c61d20476bf214176994dff4e46d6452bd821cf
dario-piotrowicz <dario.piotrowicz@gmail.com>
2022-02-12 22:32:03 +0000

    fix(animations): allow animations with unsupported CSS properties (#45185)
    currently animations with unsupported CSS properties cause a hard error
    and the crash of the animation itself, instead of this behaviour just
    ignore such properties and provide a warning for the developer in
    the console (only in dev mode)
    
    this change also introduces a general way to present warnings
    in the animations code
    
    resolves #23195
    
    PR Close #45185

---
+++ packages/animations/browser/src/warning_helpers.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+const NG_DEV_MODE = typeof ngDevMode === 'undefined' || !!ngDevMode;
+
+function createListOfWarnings(warnings: string[]): string {
+  const LINE_START = '\n - ';
+  return `${LINE_START}${warnings.filter(Boolean).map(warning => warning).join(LINE_START)}`;
+}
+
+export function warnValidation(warnings: string[]): void {
+  NG_DEV_MODE && console.warn(`animation validation warnings:${createListOfWarnings(warnings)}`);
+}
+
+export function warnTriggerBuild(name: string, warnings: string[]): void {
+  NG_DEV_MODE &&
+      console.warn(`The animation trigger "${name}" has built with the following warnings:${
+          createListOfWarnings(warnings)}`);
+}
+
+export function warnRegister(warnings: string[]): void {
+  NG_DEV_MODE &&
+      console.warn(`Animation built with the following warnings:${createListOfWarnings(warnings)}`);
+}
+
+export function triggerParsingWarnings(name: string, warnings: string[]): void {
+  NG_DEV_MODE &&
+      console.warn(`Animation parsing for the ${name} trigger presents the following warnings:${
+          createListOfWarnings(warnings)}`);
+}
+
+export function pushUnrecognizedPropertiesWarning(warnings: string[], props: string[]): void {
+  if (ngDevMode && props.length) {
+    warnings.push(
+        `The provided CSS properties are not recognized properties supported for animations: ${
+            props.join(', ')}`);
+  }
+}
