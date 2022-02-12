7ad52c4c7e0b7aacb0545f0f021b5d3696b858c2
dario-piotrowicz <dario.piotrowicz@gmail.com>
2022-02-12 22:32:03 +0000

    fix(animations): allow animations with unsupported CSS properties
    currently animations with unsupported CSS properties cause a hard error
    and the crash of the animation itself, instead of this behaviour just
    ignore such properties and provide a warning for the developer in
    the console (only in dev mode)
    
    this change also introduces a general way to present warnings
    in the animations code
    
    resolves #23195

--- packages/animations/browser/src/dsl/animation_ast_builder.ts
+++ packages/animations/browser/src/dsl/animation_ast_builder.ts
@@ -11,6 +11,7 @@ import {invalidDefinition, invalidKeyframes, invalidOffset, invalidParallelAnima
 import {AnimationDriver} from '../render/animation_driver';
 import {getOrSetAsInMap} from '../render/shared';
 import {copyObj, extractStyleParams, iteratorToArray, NG_ANIMATING_SELECTOR, NG_TRIGGER_SELECTOR, normalizeAnimationEntry, resolveTiming, SUBSTITUTION_EXPR_START, validateStyleParams, visitDslNode} from '../util';
+import {pushUnrecognizedPropertiesWarning} from '../warning_helpers';
 
 import {AnimateAst, AnimateChildAst, AnimateRefAst, Ast, DynamicTimingAst, GroupAst, KeyframesAst, QueryAst, ReferenceAst, SequenceAst, StaggerAst, StateAst, StyleAst, TimingAst, TransitionAst, TriggerAst} from './animation_ast';
 import {AnimationDslVisitor} from './animation_dsl_visitor';
@@ -56,9 +57,9 @@ const SELF_TOKEN_REGEX = new RegExp(`\s*${SELF_TOKEN}\s*,?`, 'g');
  * Otherwise an error will be thrown.
  */
 export function buildAnimationAst(
-    driver: AnimationDriver, metadata: AnimationMetadata|AnimationMetadata[],
-    errors: Error[]): Ast<AnimationMetadataType> {
-  return new AnimationAstBuilderVisitor(driver).build(metadata, errors);
+    driver: AnimationDriver, metadata: AnimationMetadata|AnimationMetadata[], errors: Error[],
+    warnings: string[]): Ast<AnimationMetadataType> {
+  return new AnimationAstBuilderVisitor(driver).build(metadata, errors, warnings);
 }
 
 const ROOT_SELECTOR = '';
@@ -66,12 +67,20 @@ const ROOT_SELECTOR = '';
 export class AnimationAstBuilderVisitor implements AnimationDslVisitor {
   constructor(private _driver: AnimationDriver) {}
 
-  build(metadata: AnimationMetadata|AnimationMetadata[], errors: Error[]):
+  build(metadata: AnimationMetadata|AnimationMetadata[], errors: Error[], warnings: string[]):
       Ast<AnimationMetadataType> {
     const context = new AnimationAstBuilderContext(errors);
     this._resetContextStyleTimingState(context);
-    return <Ast<AnimationMetadataType>>visitDslNode(
-        this, normalizeAnimationEntry(metadata), context);
+    const ast =
+        <Ast<AnimationMetadataType>>visitDslNode(this, normalizeAnimationEntry(metadata), context);
+
+    if (context.unsupportedCSSPropertiesFound.size) {
+      pushUnrecognizedPropertiesWarning(
+          warnings,
+          [...context.unsupportedCSSPropertiesFound.keys()],
+      );
+    }
+    return ast;
   }
 
   private _resetContextStyleTimingState(context: AnimationAstBuilderContext) {
@@ -303,7 +312,8 @@ export class AnimationAstBuilderVisitor implements AnimationDslVisitor {
 
       Object.keys(tuple).forEach(prop => {
         if (!this._driver.validateStyleProperty(prop)) {
-          context.errors.push(invalidProperty(prop));
+          delete tuple[prop];
+          context.unsupportedCSSPropertiesFound.add(prop);
           return;
         }
 
@@ -507,6 +517,7 @@ export class AnimationAstBuilderContext {
   public currentTime: number = 0;
   public collectedStyles: {[selectorName: string]: {[propName: string]: StyleTimeTuple}} = {};
   public options: AnimationOptions|null = null;
+  public unsupportedCSSPropertiesFound: Set<string> = new Set<string>();
   constructor(public errors: Error[]) {}
 }
 
