da07c36e7e633f592fcbb7972eb0c132c2737633
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2022-02-14 22:25:25 +0000

    feat(material/schematics): add radio styles migrator and tests

---
+++ src/material/schematics/ng-generate/mdc-migration/rules/components/radio/radio-styles.ts
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {ClassNameChange, StyleMigrator} from '../../style-migrator';
+
+export class RadioStylesMigrator extends StyleMigrator {
+  component = 'radio';
+
+  deprecatedPrefix = 'mat-radio';
+
+  mixinChanges = [
+    {
+      old: 'radio-theme',
+      new: ['mdc-radio-theme', 'mdc-radio-typography'],
+    },
+  ];
+
+  classChanges: ClassNameChange[] = [
+    {old: '.mat-radio-group', new: '.mat-mdc-radio-group'},
+    {old: '.mat-radio-button', new: '.mat-mdc-radio-button'},
+  ];
+}
--- src/material/schematics/ng-generate/mdc-migration/rules/index.ts
+++ src/material/schematics/ng-generate/mdc-migration/rules/index.ts
@@ -9,6 +9,7 @@
 import {ButtonStylesMigrator} from './components/button/button-styles';
 import {CheckboxStylesMigrator} from './components/checkbox/checkbox-styles';
 import {ProgressBarStylesMigrator} from './components/progress-bar/progress-bar-styles';
+import {RadioStylesMigrator} from './components/radio/radio-styles';
 import {SlideToggleStylesMigrator} from './components/slide-toggle/slide-toggle-styles';
 import {StyleMigrator} from './style-migrator';
 
@@ -16,5 +17,6 @@ export const MIGRATORS: StyleMigrator[] = [
   new ButtonStylesMigrator(),
   new CheckboxStylesMigrator(),
   new ProgressBarStylesMigrator(),
+  new RadioStylesMigrator(),
   new SlideToggleStylesMigrator(),
 ];
