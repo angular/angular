d8428ca598be670a41abc0adea8453d0dd6d4835
Andrew Scott <atscott@google.com>
2022-02-22 16:26:53 -0800

    refactor(router): Update match result to be more explicit (#45170)
    The `lastChild` property in the match result is only used to compute the
    remaining segments (i.e. the ones which were not consumed). The updated
    type here makes it easier to use and more clear.
    
    PR Close #45170

--- packages/router/src/apply_redirects.ts
+++ packages/router/src/apply_redirects.ts
@@ -258,7 +258,7 @@ class ApplyRedirects {
   private expandRegularSegmentAgainstRouteUsingRedirect(
       ngModule: NgModuleRef<any>, segmentGroup: UrlSegmentGroup, routes: Route[], route: Route,
       segments: UrlSegment[], outlet: string): Observable<UrlSegmentGroup> {
-    const {matched, consumedSegments, lastChild, positionalParamSegments} =
+    const {matched, consumedSegments, remainingSegments, positionalParamSegments} =
         match(segmentGroup, route, segments);
     if (!matched) return noMatch(segmentGroup);
 
@@ -270,8 +270,7 @@ class ApplyRedirects {
 
     return this.lineralizeSegments(route, newTree).pipe(mergeMap((newSegments: UrlSegment[]) => {
       return this.expandSegment(
-          ngModule, segmentGroup, routes, newSegments.concat(segments.slice(lastChild)), outlet,
-          false);
+          ngModule, segmentGroup, routes, newSegments.concat(remainingSegments), outlet, false);
     }));
   }
 
@@ -291,10 +290,9 @@ class ApplyRedirects {
       return of(new UrlSegmentGroup(segments, {}));
     }
 
-    const {matched, consumedSegments, lastChild} = match(rawSegmentGroup, route, segments);
+    const {matched, consumedSegments, remainingSegments} = match(rawSegmentGroup, route, segments);
     if (!matched) return noMatch(rawSegmentGroup);
 
-    const rawSlicedSegments = segments.slice(lastChild);
     const childConfig$ = this.getChildConfig(ngModule, route, segments);
 
     return childConfig$.pipe(mergeMap((routerConfig: LoadedRouterConfig) => {
@@ -302,7 +300,7 @@ class ApplyRedirects {
       const childConfig = routerConfig.routes;
 
       const {segmentGroup: splitSegmentGroup, slicedSegments} =
-          split(rawSegmentGroup, consumedSegments, rawSlicedSegments, childConfig);
+          split(rawSegmentGroup, consumedSegments, remainingSegments, childConfig);
       // See comment on the other call to `split` about why this is necessary.
       const segmentGroup =
           new UrlSegmentGroup(splitSegmentGroup.segments, splitSegmentGroup.children);
