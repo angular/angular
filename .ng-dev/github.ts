7ceb20c953cb9eac2203fe80ed7d3139be9d193b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-02-26 21:59:18 +0100

    build: update tooling to use new entry-point for dev-infra exports
    Previously, dev-infra only bundled the CLI with all its dependencies, while
    we still also kept all bundled dependencies in the `dependencies`. This basically
    meant that the CLI bundling did not provide any value at all.
    
    We fixed this upstream and now dependencies are still bundled, but no longer
    also declared as `dependencies`. We also stopped shipping deep JS files, only
    shipping the bundles now.
    
    As part of this deep file removal, we introduced a runtime entry-point for
    exports/types. This one will also benefit from the bundling and it will be
    predictable what symbols dev-infra consumer projects rely on (allowing us to
    also have an API guard or something in the future).

--- .ng-dev/github.ts
+++ .ng-dev/github.ts
@@ -1,4 +1,4 @@
-import {GithubConfig} from '@angular/dev-infra-private/ng-dev/utils/config';
+import {GithubConfig} from '@angular/dev-infra-private/ng-dev';
 
 /**
  * Github configuration for the `ng-dev` command. This repository is used as
