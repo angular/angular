7a408378272e74c6c1dab612236fadb9ca5f70e0
JiaLiPassion <JiaLi.Passion@gmail.com>
2022-02-27 14:13:41 +0900

    feat(core): add callback for waitForAsync() function

--- packages/core/testing/src/async.ts
+++ packages/core/testing/src/async.ts
@@ -50,3 +50,76 @@ export function waitForAsync(fn: Function): (done: any) => any {
 export function async(fn: Function): (done: any) => any {
   return waitForAsync(fn);
 }
+
+/**
+ * Register a callback function when all the async tasks finished inside
+ * the waitForAsync() test function.
+ *
+ * Example:
+ *
+ * ```
+ * it('...', waitForAsync(() => {
+ *   let timeoutCalled = false;
+ *   setTimeout(() => {
+ *     timeoutCalled = true;
+ *   });
+ *   onWaitForAsyncFinished(() => {
+ *     expect(timeoutCalled).toBe(true);
+ *   });
+ * });
+ * ```
+ *
+ * @publicApi
+ */
+export function onWaitForAsyncFinished(fn: Function): void {
+  const _Zone: any = typeof Zone !== 'undefined' ? Zone : null;
+  if (!_Zone) {
+    throw new Error(
+        'Zone is needed for the waitForAsync() test helper but could not be found. ' +
+        'Please make sure that your environment includes zone.js');
+  };
+  const onWaitForAsyncFinished = _Zone && _Zone[_Zone.__symbol__('onWaitForAsyncFinished')];
+  if (typeof onWaitForAsyncFinished === 'function') {
+    onWaitForAsyncFinished(fn);
+  } else {
+    throw new Error(
+        'zone-testing is needed for the onWaitForAsyncFinished() test helper but could not be found. ' +
+        'Please make sure that your environment includes zone-testing.js');
+  }
+}
+
+/**
+ * Register a callback function when error thrown in the async tasks inside
+ * the waitForAsync() test function.
+ *
+ * Example:
+ *
+ * ```
+ * it('...', waitForAsync(() => {
+ *   setTimeout(() => {
+ *     throw new Error('test');
+ *   });
+ *   onWaitForAsyncThrowError((err) => {
+ *     expect(err.message).toEqual('test');
+ *   });
+ * });
+ * ```
+ *
+ * @publicApi
+ */
+export function onWaitForAsyncThrowError(fn: (error: any) => void): void {
+  const _Zone: any = typeof Zone !== 'undefined' ? Zone : null;
+  if (!_Zone) {
+    throw new Error(
+        'Zone is needed for the waitForAsync() test helper but could not be found. ' +
+        'Please make sure that your environment includes zone.js');
+  };
+  const onWaitForAsyncThrowError = _Zone && _Zone[_Zone.__symbol__('onWaitForAsyncThrowError')];
+  if (typeof onWaitForAsyncThrowError === 'function') {
+    onWaitForAsyncThrowError(fn);
+  } else {
+    throw new Error(
+        'zone-testing is needed for the onWaitForAsyncThrowError() test helper but could not be found. ' +
+        'Please make sure that your environment includes zone-testing.js');
+  }
+}
