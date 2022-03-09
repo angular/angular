26fc03e76c57c330afa0e460d18cbd602809db9b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-03-09 22:01:29 +0100

    build: do not reserve `GOOGLE_APPLICATION_CREDENTIALS` for RBE auth
    We currently configure RBE by setting `GOOGLE_APPLICATION_CREDENTIALS`
    into the `$BASH_ENV` variable, ensuring RBE is configured everywhere on CI.
    
    This worked nicely but now with automatic docs deployment turned out to
    be problematic since it prevents scripts from defining `GOOGLE_APPLICATION_CREDENTIALS`
    themselves/overriding it. the reason is that `$BASH_ENV` always runs in new
    child processes (like when firebase is initialized) and then overrides the credentials
    back to the RBE service key.
    
    We can simplify this code by using a dedicated Bazel flag.

--- scripts/bazel/setup-remote-execution.sh
+++ scripts/bazel/setup-remote-execution.sh
@@ -11,21 +11,10 @@ fi
 
 # Decode the GCP token that is needed to authenticate the Bazel remote execution.
 openssl aes-256-cbc -d -in scripts/bazel/gcp_token -md md5 -k ${GCP_DECRYPT_TOKEN} \
-  -out $HOME/.gcp_credentials
-
-# Set the "GOOGLE_APPLICATION_CREDENTIALS" environment variable. It should point to the GCP credentials
-# file. Bazel will then automatically picks up the credentials from that variable.
-# https://docs.bazel.build/versions/main/command-line-reference.html#flag--google_default_credentials
-# https://cloud.google.com/docs/authentication/production.
-if [[ ! -z "${BASH_ENV}" ]]; then
-  # CircleCI uses the `BASH_ENV` variable for environment variables.
-  echo "export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/.gcp_credentials" >> ${BASH_ENV}
-elif [[ ! -z "${GITHUB_ENV}" ]]; then
-  # Github actions use the `GITHUB_ENV` variable for environment variables.
-  echo "GOOGLE_APPLICATION_CREDENTIALS=${HOME}/.gcp_credentials" >> ${GITHUB_ENV}
-fi
+  -out $HOME/.gcp_rbe_credentials
 
 # Update the project Bazel configuration to always use remote execution.
 # Note: We add the remote config flag to the user bazelrc file that is not tracked
 # by Git. This is necessary to avoid stamping builds with `.with-local-changes`.
 echo "build --config=remote" >> .bazelrc.user
+echo "build:remote --google_credentials=\"$HOME/.gcp_rbe_credentials\"" >> .bazelrc.user
