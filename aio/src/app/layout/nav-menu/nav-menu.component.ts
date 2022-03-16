0fc4571b8564ab22b4861637ea3779fbb4dcfd94
dario-piotrowicz <dario.piotrowicz@gmail.com>
2022-03-16 21:47:16 +0000

    fix(docs-infra): assign different aria labels to main aio navs (#45209)
    assign different aria labels to the primary nav and the one used for
    guides and docs, so that impaired users can more easily distinguish the
    two
    
    PR Close #45209

--- aio/src/app/layout/nav-menu/nav-menu.component.ts
+++ aio/src/app/layout/nav-menu/nav-menu.component.ts
@@ -4,12 +4,18 @@ import { CurrentNode, NavigationNode } from 'app/navigation/navigation.service';
 @Component({
   selector: 'aio-nav-menu',
   template: `
-  <aio-nav-item *ngFor="let node of filteredNodes" [node]="node" [selectedNodes]="currentNode?.nodes" [isWide]="isWide">
-  </aio-nav-item>`
+  <nav [attr.aria-label]="navLabel || null">
+    <aio-nav-item *ngFor="let node of filteredNodes"
+      [node]="node"
+      [selectedNodes]="currentNode?.nodes"
+      [isWide]="isWide">
+    </aio-nav-item>
+  </nav>`
 })
 export class NavMenuComponent {
   @Input() currentNode: CurrentNode | undefined;
   @Input() isWide = false;
   @Input() nodes: NavigationNode[];
+  @Input() navLabel: string;
   get filteredNodes() { return this.nodes ? this.nodes.filter(n => !n.hidden) : []; }
 }
