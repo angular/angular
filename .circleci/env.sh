a15c7e8f98f80bcc830f81db5958bc4b55745bcf
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-03-24 20:23:53 +0100

    ci: improve stability of windows bazel CI job (#45443)
    Improves stability of the Windows Bazel CI job by
    installing Bazelisk globally.
    
    Also makes the environment helpers more convenient by
    evaluating the variable assignments directly, simplifying
    some Bash logic.
    
    PR Close #45443

--- .circleci/env.sh
+++ .circleci/env.sh
@@ -92,14 +92,6 @@ echo "export CI_REPO_OWNER=\"${CI_REPO_OWNER}\";" >> $bazelVarEnv
 echo "export CI_SECRET_PAYLOAD_FIREBASE_TOKEN=\"${CI_SECRET_PAYLOAD_FIREBASE_TOKEN}\";" >> $bazelVarEnv
 
 ####################################################################################################
-####################################################################################################
-##                  Source `$BASH_ENV` to make the variables available immediately.               ##
-##                  ***NOTE: This must remain the last variable definition in this script***      ##
-####################################################################################################
-####################################################################################################
-source $BASH_ENV;
-
-####################################################################################################
 # Platform-specific environment setup (which can leverage the base variables from here)
 ####################################################################################################
 
