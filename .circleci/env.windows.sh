b18792f0f62039554c89c325772ed58e2ef50126
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-03-24 20:23:53 +0100

    ci: improve stability of windows bazel CI job
    Improves stability of the Windows Bazel CI job by
    installing Bazelisk globally.
    
    Also makes the environment helpers more convenient by
    evaluating the variable assignments directly, simplifying
    some Bash logic.

--- .circleci/env.windows.sh
+++ .circleci/env.windows.sh
@@ -9,3 +9,9 @@ openssl aes-256-cbc -d -in "${PROJECT_ROOT}/.circleci/gcp_token" \
 # Set bazel configuration for CircleCI runs.
 ####################################################################################################
 cp "${PROJECT_ROOT}/.circleci/bazel.windows.rc" "${USERPROFILE}/.bazelrc";
+
+
+# Expose the Bazelisk version. We need to run Bazelisk globally since Windows has problems launching
+# Bazel from a node modules directoy that might be modified by the Bazel Yarn install then.
+setPublicVar BAZELISK_VERSION \
+    "$(cd ${PROJECT_ROOT}; node -p 'require("./package.json").devDependencies["@bazel/bazelisk"]')"
