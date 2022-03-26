fa90c74a73766060e824822dad71012adbc6c4cb
JoostK <joost.koehoorn@gmail.com>
2022-03-26 22:10:33 +0100

    refactor(compiler-cli): rename `ShimLocation` to `TcbLocation` (#45478)
    Inline type check blocks (TCBs) are emitted into the original source file, but
    node positions would still be represented as a `ShimLocation` with a `shimPath`
    corresponding with the type-checking shim file. This results in inconsistencies,
    as the `positionInShimFile` field of `ShimLocation` would not correspond with
    the `shimPath` of that `ShimLocation`.
    
    This commit is a precursor to letting `ShimLocation` also represent the correct
    location for inline type check blocks, by renaming the interface to
    `TcbLocation`. A followup commit addresses the actual inconsistency.
    
    PR Close #45478

--- packages/compiler-cli/src/ngtsc/typecheck/api/checker.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/api/checker.ts
@@ -15,7 +15,7 @@ import {ErrorCode} from '../../diagnostics';
 import {FullTemplateMapping, NgTemplateDiagnostic, TypeCheckableDirectiveMeta} from './api';
 import {GlobalCompletion} from './completion';
 import {DirectiveInScope, PipeInScope} from './scope';
-import {ElementSymbol, ShimLocation, Symbol, TemplateSymbol} from './symbols';
+import {ElementSymbol, Symbol, TcbLocation, TemplateSymbol} from './symbols';
 
 /**
  * Interface to the Angular Template Type Checker to extract diagnostics and intelligence from the
@@ -56,7 +56,7 @@ export interface TemplateTypeChecker {
    * Given a `shim` and position within the file, returns information for mapping back to a template
    * location.
    */
-  getTemplateMappingAtShimLocation(shimLocation: ShimLocation): FullTemplateMapping|null;
+  getTemplateMappingAtTcbLocation(tcbLocation: TcbLocation): FullTemplateMapping|null;
 
   /**
    * Get all `ts.Diagnostic`s currently available that pertain to the given component.
@@ -113,19 +113,19 @@ export interface TemplateTypeChecker {
 
 
   /**
-   * For the given expression node, retrieve a `ShimLocation` that can be used to perform
+   * For the given expression node, retrieve a `TcbLocation` that can be used to perform
    * autocompletion at that point in the expression, if such a location exists.
    */
   getExpressionCompletionLocation(
-      expr: PropertyRead|SafePropertyRead, component: ts.ClassDeclaration): ShimLocation|null;
+      expr: PropertyRead|SafePropertyRead, component: ts.ClassDeclaration): TcbLocation|null;
 
   /**
    * For the given node represents a `LiteralPrimitive`(the `TextAttribute` represents a string
-   * literal), retrieve a `ShimLocation` that can be used to perform autocompletion at that point in
+   * literal), retrieve a `TcbLocation` that can be used to perform autocompletion at that point in
    * the node, if such a location exists.
    */
   getLiteralCompletionLocation(
-      strNode: LiteralPrimitive|TmplAstTextAttribute, component: ts.ClassDeclaration): ShimLocation
+      strNode: LiteralPrimitive|TmplAstTextAttribute, component: ts.ClassDeclaration): TcbLocation
       |null;
 
   /**
