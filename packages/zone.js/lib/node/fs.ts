9a52aa101be6021b8cd6aac7dffbd9a92376f66d
Lam Wei Li <lam_wei_li@hotmail.com>
2022-04-07 02:31:51 +0800

    fix(zone.js): patch node for nested fs functions
    Closes angular#45546
    
    It patches the nested functions `fs.realpath.native` and `fs.realpathSync.native` first, storing them in a temporary space, before patching the usual `fs` functions (which will cause the nested functions to be `undefined`). Lastly, it populates back the patched nested functions from the temporary space back to `fs`.

--- packages/zone.js/lib/node/fs.ts
+++ packages/zone.js/lib/node/fs.ts
@@ -17,6 +17,10 @@ Zone.__load_patch('fs', () => {
 
   // watch, watchFile, unwatchFile has been patched
   // because EventEmitter has been patched
+  const TO_PATCH_MACROTASK_NESTED = [
+    {'parent': 'realpath', 'nested': 'native'},
+    {'parent': 'realpathSync', 'nested': 'native'},
+  ];
   const TO_PATCH_MACROTASK_METHODS = [
     'access',  'appendFile', 'chmod',    'chown',    'close',     'exists',    'fchmod',
     'fchown',  'fdatasync',  'fstat',    'fsync',    'ftruncate', 'futimes',   'lchmod',
@@ -26,6 +30,26 @@ Zone.__load_patch('fs', () => {
   ];
 
   if (fs) {
+    // store the patched nested function temporarily first
+    const tempNestedResults: {[key: string]: any} = {};
+    TO_PATCH_MACROTASK_NESTED
+        .filter(
+            method => !!fs?.[method.parent][method.nested] &&
+                typeof fs[method.parent][method.nested] === 'function')
+        .forEach(function(method) {
+          patchMacroTask(fs[method.parent], method.nested, function(self, args) {
+            return {
+              name: 'fs.' + method.parent + '.' + method.nested,
+              args: args,
+              cbIdx: args.length > 0 ? args.length - 1 : -1,
+              target: self
+            };
+          });
+          tempNestedResults[method.parent] = tempNestedResults[method.parent] || {};
+          tempNestedResults[method.parent][method.nested] = fs[method.parent][method.nested];
+        });
+
+    // patching of direct fs functions will override nested functions
     TO_PATCH_MACROTASK_METHODS.filter(name => !!fs[name] && typeof fs[name] === 'function')
         .forEach(name => {
           patchMacroTask(fs, name, (self: any, args: any[]) => {
@@ -37,5 +61,10 @@ Zone.__load_patch('fs', () => {
             };
           });
         });
+
+    // re-populate the nested function as it became undefined
+    for (let method of TO_PATCH_MACROTASK_NESTED) {
+      fs[method.parent][method.nested] = tempNestedResults[method.parent][method.nested];
+    }
   }
 });
