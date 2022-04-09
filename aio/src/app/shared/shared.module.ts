7085424f35c064096747ba8744107c1c83592f81
dario-piotrowicz <dario.piotrowicz@gmail.com>
2022-04-09 20:33:44 +0100

    feat(docs-infra): add close button to search-results aio panel
    add a close button to the search-results aio panel so that the user can
    conveniently close it via keyboard
    
    this complements the focus trap implemented in PR #44989
    (more here: https://github.com/angular/angular/pull/44989#issuecomment-1037287678)

--- aio/src/app/shared/shared.module.ts
+++ aio/src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { SelectComponent } from './select/select.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
   ],
   exports: [
     SearchResultsComponent,
