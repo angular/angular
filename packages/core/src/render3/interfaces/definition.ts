9e4c4bc42adde5bcc1e847e335f96eee029e8a66
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-04-19 13:45:27 +0200

    docs(core): add API documentation to standalone-related classes (#45687)
    This commit adds some internal documentation.
    
    PR Close #45687

--- packages/core/src/render3/interfaces/definition.ts
+++ packages/core/src/render3/interfaces/definition.ts
@@ -322,6 +322,9 @@ export interface ComponentDef<T> extends DirectiveDef<T> {
    */
   tView: TView|null;
 
+  /**
+   * A function added by the {@see ɵɵStandaloneFeature} and used by the framework to create standalone injectors.
+   */
   getStandaloneInjector: ((parentInjector: EnvironmentInjector) => EnvironmentInjector | null)|null;
 
   /**
