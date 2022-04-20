2a5508cee6b2697770c23fb64c8bed17afc88608
Andrew Kushnir <akushnir@google.com>
2022-04-19 18:54:29 -0700

    test(common): add a test app for the `NgImage` directive
    This commit adds a simple app that uses the `NgImage` directive to simplify further investigation and tests.

--- packages/common/src/private_export.ts
+++ packages/common/src/private_export.ts
@@ -6,5 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+export {IMAGE_LOADER as ɵIMAGE_LOADER, NgImage as ɵNgImage, NgImageModule as ɵNgImageModule} from './directives/ng_image';
 export {DomAdapter as ɵDomAdapter, getDOM as ɵgetDOM, setRootDomAdapter as ɵsetRootDomAdapter} from './dom_adapter';
 export {BrowserPlatformLocation as ɵBrowserPlatformLocation} from './location/platform_location';
