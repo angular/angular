b60f34c7c1504e25abd9dd3ed87503af854f0133
Andrew Kushnir <akushnir@google.com>
2022-04-20 19:32:33 -0700

    test(core): add integration test apps for the `bootstrapApplication` API
    This commit adds 2 integration apps to verify the `bootstrapApplication` API behavior as well as keep track of the bundle size and retained symbols (tree-shaking).

---
+++ integration/standalone-bootstrap/e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import {browser, by, element} from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get(browser.baseUrl) as Promise<any>;
+  }
+
+  getTitleText() {
+    return element(by.css('app-root h1')).getText() as Promise<string>;
+  }
+}
