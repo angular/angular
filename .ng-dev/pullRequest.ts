839392523e0cb121bd00ca2ed71cf351bf3d4bda
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-04-21 13:45:42 +0000

    build: updates to account for github primary branch rename
    This is the commit accounting for the Github primary branch
    rename when we actually perform the update.
    
    We have three change phases: Prepare, Direct, Cleanup. This commit
    is for the `direct` phase.

--- .ng-dev/pullRequest.ts
+++ .ng-dev/pullRequest.ts
@@ -10,10 +10,10 @@ export const pullRequest: PullRequestConfig = {
   caretakerNoteLabel: /^(action: merge-assistance)|(PullApprove: disable)/,
   commitMessageFixupLabel: 'commit message fixup',
   requiredBaseCommits: {
-    // PRs that target either `master` or the patch branch, need to be rebased
+    // PRs that target either `main` or the patch branch, need to be rebased
     // on top of the latest commit message validation fix.
     // These SHAs are the commits that update the required license text in the header.
-    'master': '5aeb9a4124922d8ac08eb73b8f322905a32b0b3a',
+    'main': '5aeb9a4124922d8ac08eb73b8f322905a32b0b3a',
     '10.0.x': '27b95ba64a5d99757f4042073fd1860e20e3ed24',
   },
   // `dev-infra` and `docs-infra` are not affecting the public NPM packages. Similarly,
--- aio/tests/e2e/src/app.e2e-spec.ts
+++ aio/tests/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
-import { browser, by, element, ElementFinder } from 'protractor';
+import { ElementFinder, browser, by, element } from 'protractor';
+
 import { SitePage } from './app.po';
 
 describe('site App', () => {
@@ -232,12 +233,12 @@ describe('site App', () => {
       expect(await page.ghLinks.count()).toEqual(1);
       /* eslint-disable max-len */
       expect(await page.ghLinks.get(0).getAttribute('href'))
-        .toMatch(/https:\/\/github\.com\/angular\/angular\/edit\/master\/aio\/content\/tutorial\/toh-pt1\.md\?message=docs%3A%20describe%20your%20change\.\.\./);
+        .toMatch(/https:\/\/github\.com\/angular\/angular\/edit\/main\/aio\/content\/tutorial\/toh-pt1\.md\?message=docs%3A%20describe%20your%20change\.\.\./);
 
       await page.navigateTo('guide/router');
       expect(await page.ghLinks.count()).toEqual(1);
       expect(await page.ghLinks.get(0).getAttribute('href'))
-        .toMatch(/https:\/\/github\.com\/angular\/angular\/edit\/master\/aio\/content\/guide\/router\.md\?message=docs%3A%20describe%20your%20change\.\.\./);
+        .toMatch(/https:\/\/github\.com\/angular\/angular\/edit\/main\/aio\/content\/guide\/router\.md\?message=docs%3A%20describe%20your%20change\.\.\./);
       /* eslint-enable max-len */
     });
 
