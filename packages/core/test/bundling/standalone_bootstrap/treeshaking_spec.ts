3e46a426c47bf6589952c7f2a17d914dd6f38dfe
Andrew Kushnir <akushnir@google.com>
2022-04-20 19:32:33 -0700

    test(core): add integration test apps for the `bootstrapApplication` API (#45674)
    This commit adds 2 integration apps to verify the `bootstrapApplication` API behavior as well as keep track of the bundle size and retained symbols (tree-shaking).
    
    PR Close #45674

---
+++ packages/core/test/bundling/standalone_bootstrap/treeshaking_spec.ts
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import '@angular/compiler';
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+const PACKAGE = 'angular/packages/core/test/bundling/standalone_bootstrap';
+
+describe('treeshaking with uglify', () => {
+  let content: string;
+  // We use the debug version as otherwise symbols/identifiers would be mangled (and the test would
+  // always pass)
+  const contentPath = require.resolve(path.join(PACKAGE, 'bundle.debug.min.js'));
+  beforeAll(() => {
+    content = fs.readFileSync(contentPath, {encoding: 'utf-8'});
+  });
+
+  it('should drop unused TypeScript helpers', () => {
+    expect(content).not.toContain('__asyncGenerator');
+  });
+
+  it('should not contain rxjs from commonjs distro', () => {
+    expect(content).not.toContain('commonjsGlobal');
+    expect(content).not.toContain('createCommonjsModule');
+  });
+});
