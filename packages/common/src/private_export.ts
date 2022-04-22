547ec4e5b50ccf890879318ef427f5bc79eef5d5
Kara Erickson <karakara@google.com>
2022-04-22 15:45:59 -0700

    refactor(common): change name of directive from NgImage => NgOptimizedImage
    We want it to be clear what benefits the image directive
    confers over a normal <img> tag, and the `NgImage` name
    didn't provide much information. `NgOptimizedImage` makes
    it obvious that the new directive is intended to improve
    performance.

--- packages/common/src/private_export.ts
+++ packages/common/src/private_export.ts
@@ -6,6 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-export {IMAGE_LOADER as ɵIMAGE_LOADER, NgImage as ɵNgImage, NgImageModule as ɵNgImageModule} from './directives/ng_image';
+export {IMAGE_LOADER as ɵIMAGE_LOADER, NgOptimizedImage as ɵNgOptimizedImage, NgOptimizedImageModule as ɵNgOptimizedImageModule} from './directives/ng_optimized_image';
 export {DomAdapter as ɵDomAdapter, getDOM as ɵgetDOM, setRootDomAdapter as ɵsetRootDomAdapter} from './dom_adapter';
 export {BrowserPlatformLocation as ɵBrowserPlatformLocation} from './location/platform_location';
