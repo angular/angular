71f64f2875ad7e451e9adc84cebdcb4acdd23a10
George Kalpakas <kalpakas.g@gmail.com>
2022-04-27 13:32:09 +0300

    docs(upgrade): move import back into docregion in `rollup-config.js` (#45778)
    This was accidentally broken in #45405.
    
    PR Close #45778

--- aio/content/examples/upgrade-phonecat-2-hybrid/rollup-config.js
+++ aio/content/examples/upgrade-phonecat-2-hybrid/rollup-config.js
@@ -1,5 +1,5 @@
-import commonjs from '@rollup/plugin-commonjs';
 // #docregion
+import commonjs from '@rollup/plugin-commonjs';
 import {nodeResolve} from '@rollup/plugin-node-resolve'
 import {terser} from 'rollup-plugin-terser'
 
