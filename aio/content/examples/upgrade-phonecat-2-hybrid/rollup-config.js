angular/angular

commit 067e4c26beaed7ba5fc7e29703b22fb3fd024f70

build: update systemjs aio boilerplate to work with APF v13+ `exports` field (#45405)
The SystemJS examples were using an outdated version of rollup that did
not support export fields. Now with the recent changes where we removed
secondary package.json files, the rather old/somewhat outdated SystemJS
examples failed bundling since exports were not considered.

PR Close #45405

aio/content/examples/upgrade-phonecat-2-hybrid/rollup-config.js
@@ -1,6 +1,6 @@
+import commonjs from '@rollup/plugin-commonjs';
 // #docregion
-import nodeResolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs';
+import {nodeResolve} from '@rollup/plugin-node-resolve'
 import {terser} from 'rollup-plugin-terser'
 
 //paths are relative to the execution path
@@ -13,7 +13,7 @@ export default {
     sourcemapFile: 'aot/dist/build.js.map'
   },
   plugins: [
-    nodeResolve({jsnext: true, module: true}),
+    nodeResolve(),
     commonjs({
       include: ['node_modules/rxjs/**']
     }),

commit de528b4ee03c36f9db374df282f810281f901830

docs(upgrade): move import back into docregion in `rollup-config.js`
This was accidentally broken in #45405.

aio/content/examples/upgrade-phonecat-2-hybrid/rollup-config.js
@@ -1,5 +1,5 @@
-import commonjs from '@rollup/plugin-commonjs';
 // #docregion
+import commonjs from '@rollup/plugin-commonjs';
 import {nodeResolve} from '@rollup/plugin-node-resolve'
 import {terser} from 'rollup-plugin-terser'
 