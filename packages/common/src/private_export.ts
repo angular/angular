b158163c63322d35e517f50e242d7d8e0c947867
Kara Erickson <karakara@google.com>
2022-05-11 11:48:40 -0700

    fix(common): remove default for image width
    Previously NgOptimizedImage would default to requesting
    an image at the width matching the width attribute in
    the image HTML. While this works for width attrs that
    match the intrinsic size of the image (e.g. responsive
    images or images sized with CSS), this can be a sharp
    edge for developers who use the width/height attributes
    to set rendered size (i.e. instead of CSS, which one can
    do for a fixed size image). In this case, defaulting to
    the width attribute for the requested image would mean
    requesting always at 1x quality, so screens with a DPR
     of 2+ get an image that is too small. Without a default
    request width, the image served by the CDN would likely
    be at intrinsic size, so 2x images would look correct.
    
    This PR also updates the NgOptimizedImage sandbox and tests.

--- packages/common/src/private_export.ts
+++ packages/common/src/private_export.ts
@@ -6,6 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-export {IMAGE_LOADER as ɵIMAGE_LOADER, NgOptimizedImage as ɵNgOptimizedImage} from './directives/ng_optimized_image';
+export {IMAGE_LOADER as ɵIMAGE_LOADER, ImageLoaderConfig as ɵImageLoaderConfig, NgOptimizedImage as ɵNgOptimizedImage} from './directives/ng_optimized_image';
 export {DomAdapter as ɵDomAdapter, getDOM as ɵgetDOM, setRootDomAdapter as ɵsetRootDomAdapter} from './dom_adapter';
 export {BrowserPlatformLocation as ɵBrowserPlatformLocation} from './location/platform_location';
