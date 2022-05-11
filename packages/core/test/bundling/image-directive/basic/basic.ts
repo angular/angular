a4b4bf967ca57ac6b5ccae7b493b7e3904555607
Kara Erickson <karakara@google.com>
2022-05-11 11:48:40 -0700

    fix(common): remove default for image width
    Previously NgOptimizedImage would default to requesting
    an image at the width matching the width attribute in
    the image HTML. While this works for width attrs that
    match the intrinsic size of the image (e.g. responsive
    images or images sized with CSS), this can be a sharp
    edge for developers who use the width/height attributes
    to set rendered size (i.e. instead of CSS, which one can
    do for a fixed size image). In this case, defaulting to
    the width attribute for the requested image would mean
    requesting always at 1x quality, so screens with a DPR
     of 2+ get an image that is too small. Without a default
    request width, the image served by the CDN would likely
    be at intrinsic size, so 2x images would look correct.
    
    This PR also updates the NgOptimizedImage sandbox and tests.

--- packages/core/test/bundling/image-directive/basic/basic.ts
+++ packages/core/test/bundling/image-directive/basic/basic.ts
@@ -6,15 +6,51 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {ɵIMAGE_LOADER as IMAGE_LOADER, ɵNgOptimizedImage as NgOptimizedImage} from '@angular/common';
+import {ɵIMAGE_LOADER as IMAGE_LOADER, ɵImageLoaderConfig as ImageLoaderConfig, ɵNgOptimizedImage as NgOptimizedImage} from '@angular/common';
 import {Component} from '@angular/core';
+
+const CUSTOM_IMGIX_LOADER = (config: ImageLoaderConfig) => {
+  const widthStr = config.width ? `?w=${config.width}` : ``;
+  return `https://aurora-project.imgix.net/${config.src}${widthStr}`;
+};
+
 
 @Component({
   selector: 'basic',
+  styles: [`
+    h1 {
+      display: flex;
+      align-items: center;
+    }
+
+    main {
+      border: 1px solid blue;
+      margin: 16px;
+      padding: 16px;
+    }
+
+    .spacer {
+      height: 3000px;
+    }
+
+    main img {
+      width: 100%;
+      height: auto;
+    }
+  `],
+  template: `
+    <h1> 
+      <img rawSrc="a.png" width="50" height="50" priority>
+      <span>Angular image app</span>
+    </h1>
+    <main>
+      <div class="spacer"></div>
+      <img rawSrc="hermes.jpeg" width="4030" height="3020">
+    </main>
+  `,
   standalone: true,
   imports: [NgOptimizedImage],
-  template: `<img rawSrc="./a.png" width="150" height="150" priority>`,
-  providers: [{provide: IMAGE_LOADER, useValue: () => 'b.png'}],
+  providers: [{provide: IMAGE_LOADER, useValue: CUSTOM_IMGIX_LOADER}],
 })
 export class BasicComponent {
 }
