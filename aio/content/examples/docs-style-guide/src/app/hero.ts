274c6fa6438d2749b8d64557cc28b3ae28d186fd
Bob Watson <bobwatson@google.com>
2022-05-16 11:12:09 -0700

    docs: remove Narco from hero list in code, text, and images (#46008)
    Fixes issue #45769
    
    Removes the malevolent comic book character, "Narco," from the list of heroes. This change removes the reference from the code, text and screenshots used in the documentation and makes other minor edits as necessary.
    
    PR Close #46008

--- aio/content/examples/docs-style-guide/src/app/hero.ts
+++ aio/content/examples/docs-style-guide/src/app/hero.ts
@@ -4,8 +4,8 @@ export interface Hero {
 }
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
+  { id: 12, name: 'Dr. Nice' },
   { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' }
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' }
 ];
