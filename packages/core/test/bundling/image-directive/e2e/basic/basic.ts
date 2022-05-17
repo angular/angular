d578f3922a9256eb714fc82a0e217a8a33b83c51
Andrew Kushnir <akushnir@google.com>
2022-05-16 19:11:27 -0700

    test(common): reorg e2e folders for the `NgOptimizedImage` directive tests
    This commit updates the e2e folder to separate e2e tests and playground scripts (so it's clear where files are used).

---
+++ packages/core/test/bundling/image-directive/e2e/basic/basic.ts
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {ɵIMAGE_LOADER as IMAGE_LOADER, ɵNgOptimizedImage as NgOptimizedImage} from '@angular/common';
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'basic',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  template: `<img rawSrc="/e2e/a.png" width="150" height="150" priority>`,
+  providers: [{provide: IMAGE_LOADER, useValue: () => '/e2e/b.png'}],
+})
+export class BasicComponent {
+}
