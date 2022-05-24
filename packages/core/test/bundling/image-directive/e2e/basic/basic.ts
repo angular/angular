3c878c0bfc5fc9e4b94a2a2d18368e793b4ed20e
Andrew Kushnir <akushnir@google.com>
2022-05-23 17:16:56 -0700

    refactor(common): create an NgModule for the `NgOptimizedImage` directive
    This commit updates the `NgOptimizedImage` directive to drop the `standalone` flag and create a new NgModule which declares and exports it, so that the directive can be used in apps that use pre-v14 version of Angular.

--- packages/core/test/bundling/image-directive/e2e/basic/basic.ts
+++ packages/core/test/bundling/image-directive/e2e/basic/basic.ts
@@ -6,13 +6,13 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {ɵIMAGE_LOADER as IMAGE_LOADER, ɵNgOptimizedImage as NgOptimizedImage} from '@angular/common';
+import {ɵIMAGE_LOADER as IMAGE_LOADER, ɵNgOptimizedImageModule as NgOptimizedImageModule} from '@angular/common';
 import {Component} from '@angular/core';
 
 @Component({
   selector: 'basic',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImageModule],
   template: `<img rawSrc="/e2e/a.png" width="150" height="150" priority>`,
   providers: [{provide: IMAGE_LOADER, useValue: () => '/e2e/b.png'}],
 })
