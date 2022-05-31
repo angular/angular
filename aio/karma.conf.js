9fe24b246fa1e720adf59ac80969bf05d1e6c1d2
Derek Cormier <derek@aspect.dev>
2022-05-31 13:39:59 -0700

    build(bazel): run aio app tests with bazel-provided chromium
    Similar to the e2e protractor tests

--- aio/karma.conf.js
+++ aio/karma.conf.js
@@ -1,6 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
