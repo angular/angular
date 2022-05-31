angular/angular

commit 61894570f1e15bdf7f99f9efefd19d3e341dfb07

docs: improve markdown (#45590)
improve markdown for documentation. Duplicate of #45325 that targets 13.3.x.

PR Close #45590

aio/content/examples/router/src/app/app-routing.module.10.ts
@@ -1,0 +1,65 @@
+// #docplaster
+import {Injectable, NgModule} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {
+  Resolve,
+  RouterModule,
+  RouterStateSnapshot,
+  Routes,
+  TitleStrategy,
+} from '@angular/router';  // CLI imports router
+
+// #docregion page-title
+const routes: Routes = [
+  {
+    path: 'first-component',
+    title: 'First component',
+    component: FirstComponent,  // this is the component with the <router-outlet> in the template
+    children: [
+      {
+        path: 'child-a',  // child route path
+        title: ResolvedChildATitle,
+        component: ChildAComponent,  // child route component that the router renders
+      },
+      {
+        path: 'child-b',
+        title: 'child b',
+        component: ChildBComponent,  // another child route component that the router renders
+      },
+    ],
+  },
+];
+
+@Injectable({providedIn: 'root'})
+export class ResolvedChildATitle implements Resolve<string> {
+  resolve() {
+    return Promise.resolve('child a');
+  }
+}
+// #enddocregion page-title
+
+
+// #docregion custom-page-title
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`My Application | ${title}`);
+    }
+  }
+}
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    {provide: TitleStrategy, useClass: TemplatePageTitleStrategy},
+  ]
+})
+export class AppRoutingModule {
+}
+// #enddocregion custom-page-title

commit d36e5fc69f07e1d6868854bf08ad450782d07235

docs: remove router title docs from 13.3.x
The router title service is only available in 14+.
Docs refactoring in https://github.com/angular/angular/commit/61894570f1e15bdf7f99f9efefd19d3e341dfb07
accidentally added documentation for it to 13.3.x

aio/content/examples/router/src/app/app-routing.module.10.ts
@@ -1,65 +1,0 @@
-// #docplaster
-import {Injectable, NgModule} from '@angular/core';
-import {Title} from '@angular/platform-browser';
-import {
-  Resolve,
-  RouterModule,
-  RouterStateSnapshot,
-  Routes,
-  TitleStrategy,
-} from '@angular/router';  // CLI imports router
-
-// #docregion page-title
-const routes: Routes = [
-  {
-    path: 'first-component',
-    title: 'First component',
-    component: FirstComponent,  // this is the component with the <router-outlet> in the template
-    children: [
-      {
-        path: 'child-a',  // child route path
-        title: ResolvedChildATitle,
-        component: ChildAComponent,  // child route component that the router renders
-      },
-      {
-        path: 'child-b',
-        title: 'child b',
-        component: ChildBComponent,  // another child route component that the router renders
-      },
-    ],
-  },
-];
-
-@Injectable({providedIn: 'root'})
-export class ResolvedChildATitle implements Resolve<string> {
-  resolve() {
-    return Promise.resolve('child a');
-  }
-}
-// #enddocregion page-title
-
-
-// #docregion custom-page-title
-export class TemplatePageTitleStrategy extends TitleStrategy {
-  constructor(private readonly title: Title) {
-    super();
-  }
-
-  override updateTitle(routerState: RouterStateSnapshot) {
-    const title = this.buildTitle(routerState);
-    if (title !== undefined) {
-      this.title.setTitle(`My Application | ${title}`);
-    }
-  }
-}
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [
-    {provide: TitleStrategy, useClass: TemplatePageTitleStrategy},
-  ]
-})
-export class AppRoutingModule {
-}
-// #enddocregion custom-page-title