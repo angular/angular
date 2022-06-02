4e741eefa7b5f19da98658626b92f01e12a87e35
Derek Cormier <derek.m.cormier@gmail.com>
2022-06-02 15:18:34 -0700

    build(bazel): incrementally build aio example zips and live examples
    The existing scripts were amended to work on one example at a time

--- aio/tools/examples/constants.js
+++ aio/tools/examples/constants.js
@@ -1,13 +1,12 @@
 const path = require('canonical-path');
 
 // Bazel sets the environment variable BUILD_WORKSPACE_DIRECTORY when calling bazel run,
-// which points to the root of the source tree. In the create example case we want to
-// point to the source tree rather than the output tree.
-exports.EXAMPLES_BASE_PATH = process.env.BUILD_WORKSPACE_DIRECTORY
-    ? path.resolve(process.env.BUILD_WORKSPACE_DIRECTORY, 'aio', 'content', 'examples')
-    : path.resolve(__dirname, '../../content/examples');
+// which points to the root of the source tree. When running create-example we want to
+// point to the source tree. In build and test cases we want to point to the output tree.
+exports.PROJECT_ROOT = process.env.BUILD_WORKSPACE_DIRECTORY || path.resolve(__dirname, '../../../');
+exports.EXAMPLES_BASE_PATH = path.resolve(this.PROJECT_ROOT, 'aio', 'content', 'examples');
+exports.SHARED_PATH = path.resolve(this.PROJECT_ROOT, 'aio', 'tools', 'examples', 'shared');
 
 exports.EXAMPLE_CONFIG_FILENAME = 'example-config.json';
-exports.SHARED_PATH = path.resolve(__dirname, 'shared');
 exports.STACKBLITZ_CONFIG_FILENAME = 'stackblitz.json';
 exports.BAZEL_EXAMPLE_BOILERPLATE_OUTPUT_PATH = process.env.BAZEL_EXAMPLE_BOILERPLATE_OUTPUT_PATH || 'bazel-test-dist';
