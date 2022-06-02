9a27c4cb509d802c3ed1fac5805c08f4cddcbd4e
Derek Cormier <derek.m.cormier@gmail.com>
2022-06-02 15:18:34 -0700

    build(bazel): incrementally build aio example zips and live examples
    The existing scripts were amended to work on one example at a time

--- aio/tools/stackblitz-builder/generateStackblitz.mjs
+++ aio/tools/stackblitz-builder/generateStackblitz.mjs
@@ -1,14 +1,12 @@
-import {join} from 'path';
 import {StackblitzBuilder} from './builder.mjs';
 
 const argv = process.argv.slice(2);
 if (argv.length !== 2) {
-    console.error("Usage: node generateStackblitz.mjs [examples-path] [output-path]");
+    console.error("Usage: node generateStackblitz.mjs [example-path] [output-path]");
     process.exit(1);
 }
 
-const EXAMPLES_PATH = argv[0];
+const EXAMPLE_PATH = argv[0];
 const OUTPUT_PATH = argv[1];
-const LIVE_EXAMPLES_PATH = join(OUTPUT_PATH, 'generated', 'live-examples');
 
-new StackblitzBuilder(EXAMPLES_PATH, LIVE_EXAMPLES_PATH).build();
+new StackblitzBuilder(EXAMPLE_PATH, OUTPUT_PATH).build();
