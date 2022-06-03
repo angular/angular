acc5a083a3df00680c892ac4a41df50b0d47e085
dario-piotrowicz <dario.piotrowicz@gmail.com>
2022-06-03 10:43:56 +0100

    fixup! refactor(core): improve code around element validation
    fix metadata circular dependency

--- packages/core/src/render3/instructions/element_validation.ts
+++ packages/core/src/render3/instructions/element_validation.ts
@@ -8,7 +8,7 @@
 
 import {formatRuntimeError, RuntimeError, RuntimeErrorCode} from '../../errors';
 import {Type} from '../../interface/type';
-import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, SchemaMetadata} from '../../metadata';
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, SchemaMetadata} from '../../metadata/schema';
 import {throwError} from '../../util/assert';
 import {getComponentDef} from '../definition';
 import {ComponentDef} from '../interfaces/definition';
