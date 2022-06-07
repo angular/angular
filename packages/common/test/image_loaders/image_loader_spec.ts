da805c79c1a39452f050ea3e45706f39c3ea4f10
Andrew Kushnir <akushnir@google.com>
2022-06-07 15:15:45 -0700

    test(common): optimize image loader tests of the NgOptimizedImage directive
    This commit updates the image loader tests to avoid creating the TestBed environment, since it's not really needed for the tests. Instead, the loader functions are invoked directly and the output is verified.

--- packages/common/test/image_loaders/image_loader_spec.ts
+++ packages/common/test/image_loaders/image_loader_spec.ts
@@ -6,57 +6,68 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {CommonModule} from '@angular/common';
+import {IMAGE_LOADER, ImageLoader, PRECONNECT_CHECK_BLOCKLIST} from '@angular/common/src/directives/ng_optimized_image';
 import {provideImgixLoader} from '@angular/common/src/directives/ng_optimized_image/image_loaders/imgix_loader';
-import {NgOptimizedImageModule} from '@angular/common/src/directives/ng_optimized_image/ng_optimized_image';
-import {PRECONNECT_CHECK_BLOCKLIST} from '@angular/common/src/directives/ng_optimized_image/preconnect_link_checker';
 import {RuntimeErrorCode} from '@angular/common/src/errors';
-import {Component, ValueProvider} from '@angular/core';
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {expect} from '@angular/platform-browser/testing/src/matchers';
+import {createEnvironmentInjector, ValueProvider} from '@angular/core';
 
 describe('Built-in image directive loaders', () => {
   describe('Imgix loader', () => {
+    function createImgixLoader(path: string): ImageLoader {
+      const injector = createEnvironmentInjector([provideImgixLoader(path)]);
+      return injector.get(IMAGE_LOADER);
+    }
+
+    function invalidPathError(path: string): string {
+      return `NG0${RuntimeErrorCode.INVALID_INPUT}: ImgixLoader has detected ` +
+          `an invalid path: expecting a path like https://somepath.imgix.net/` +
+          `but got: \`${path}\``;
+    }
+
     describe('invalid paths', () => {
-      it('should throw if path is empty', () => {
-        expect(() => {
-          setupTestingModule([provideImgixLoader('')]);
-        })
-            .toThrowError(
-                `NG0${RuntimeErrorCode.INVALID_INPUT}: ImgixLoader has detected an invalid path: ` +
-                `expecting a path like https://somepath.imgix.net/` +
-                `but got: \`\``);
+      it('should throw when a path is empty', () => {
+        const path = '';
+        expect(() => provideImgixLoader(path)).toThrowError(invalidPathError(path));
       });
 
-      it('should throw if not a path', () => {
-        expect(() => {
-          setupTestingModule([provideImgixLoader('wellhellothere')]);
-        })
-            .toThrowError(
-                `NG0${RuntimeErrorCode.INVALID_INPUT}: ImgixLoader has detected an invalid path: ` +
-                `expecting a path like https://somepath.imgix.net/` +
-                `but got: \`wellhellothere\``);
+      it('should throw when a path is not a URL', () => {
+        const path = 'wellhellothere';
+        expect(() => provideImgixLoader(path)).toThrowError(invalidPathError(path));
       });
 
-      it('should throw if path is missing a scheme', () => {
-        expect(() => {
-          setupTestingModule([provideImgixLoader('somepath.imgix.net')]);
-        })
-            .toThrowError(
-                `NG0${RuntimeErrorCode.INVALID_INPUT}: ImgixLoader has detected an invalid path: ` +
-                `expecting a path like https://somepath.imgix.net/` +
-                `but got: \`somepath.imgix.net\``);
+      it('should throw when a path is missing a scheme', () => {
+        const path = 'somepath.imgix.net';
+        expect(() => provideImgixLoader(path)).toThrowError(invalidPathError(path));
       });
 
-      it('should throw if path is malformed', () => {
-        expect(() => {
-          setupTestingModule([provideImgixLoader('somepa\th.imgix.net? few')]);
-        })
-            .toThrowError(
-                `NG0${RuntimeErrorCode.INVALID_INPUT}: ImgixLoader has detected an invalid path: ` +
-                `expecting a path like https://somepath.imgix.net/` +
-                `but got: \`somepa\th.imgix.net? few\``);
+      it('should throw when a path is malformed', () => {
+        const path = 'somepa\th.imgix.net? few';
+        expect(() => provideImgixLoader(path)).toThrowError(invalidPathError(path));
       });
+    });
+
+    it('should construct an image loader with the given path', () => {
+      const loader = createImgixLoader('https://somesite.imgix.net');
+      const config = {src: 'img.png'};
+      expect(loader(config)).toBe('https://somesite.imgix.net/img.png?auto=format');
+    });
+
+    it('should handle a trailing forward slash on the path', () => {
+      const loader = createImgixLoader('https://somesite.imgix.net/');
+      const config = {src: 'img.png'};
+      expect(loader(config)).toBe('https://somesite.imgix.net/img.png?auto=format');
+    });
+
+    it('should handle a leading forward slash on the src', () => {
+      const loader = createImgixLoader('https://somesite.imgix.net');
+      const config = {src: '/img.png'};
+      expect(loader(config)).toBe('https://somesite.imgix.net/img.png?auto=format');
+    });
+
+    it('should construct an image loader with the given path', () => {
+      const loader = createImgixLoader('https://somesite.imgix.net');
+      const config = {src: 'img.png', width: 100};
+      expect(loader(config)).toBe('https://somesite.imgix.net/img.png?auto=format&w=100');
     });
 
     describe('options', () => {
@@ -89,91 +100,5 @@ describe('Built-in image directive loaders', () => {
            expect(providers.length).toBe(1);
          });
     });
-
-    it('should construct an image loader with the given path', () => {
-      setupTestingModule([provideImgixLoader('https://somesite.imgix.net')]);
-
-      const template = `
-      <img rawSrc="img.png" width="150" height="50">
-      <img rawSrc="img-2.png" width="150" height="50">
-    `;
-      const fixture = createTestComponent(template);
-      fixture.detectChanges();
-
-      const nativeElement = fixture.nativeElement as HTMLElement;
-      const imgs = nativeElement.querySelectorAll('img')!;
-      expect(imgs[0].src).toBe('https://somesite.imgix.net/img.png?auto=format');
-      expect(imgs[1].src).toBe('https://somesite.imgix.net/img-2.png?auto=format');
-    });
-
-    it('should handle a trailing forward slash on the path', () => {
-      setupTestingModule([provideImgixLoader('https://somesite.imgix.net/')]);
-
-      const template = `
-      <img rawSrc="img.png" width="150" height="50">
-    `;
-      const fixture = createTestComponent(template);
-      fixture.detectChanges();
-
-      const nativeElement = fixture.nativeElement as HTMLElement;
-      const img = nativeElement.querySelector('img')!;
-      expect(img.src).toBe('https://somesite.imgix.net/img.png?auto=format');
-    });
-
-    it('should handle a leading forward slash on the src', () => {
-      setupTestingModule([provideImgixLoader('https://somesite.imgix.net/')]);
-
-      const template = `
-      <img rawSrc="/img.png" width="150" height="50">
-    `;
-      const fixture = createTestComponent(template);
-      fixture.detectChanges();
-
-      const nativeElement = fixture.nativeElement as HTMLElement;
-      const img = nativeElement.querySelector('img')!;
-      expect(img.src).toBe('https://somesite.imgix.net/img.png?auto=format');
-    });
-
-    it('should be compatible with rawSrcset', () => {
-      setupTestingModule([provideImgixLoader('https://somesite.imgix.net')]);
-
-      const template = `
-      <img rawSrc="img.png" rawSrcset="100w, 200w" width="100" height="50">
-    `;
-      const fixture = createTestComponent(template);
-      fixture.detectChanges();
-
-      const nativeElement = fixture.nativeElement as HTMLElement;
-      const img = nativeElement.querySelector('img')!;
-      expect(img.src).toBe('https://somesite.imgix.net/img.png?auto=format');
-      expect(img.srcset)
-          .toBe(
-              'https://somesite.imgix.net/img.png?auto=format&w=100 100w, https://somesite.imgix.net/img.png?auto=format&w=200 200w');
-    });
   });
 });
-
-
-// Helpers
-
-@Component({
-  selector: 'test-cmp',
-  template: '',
-})
-class TestComponent {
-}
-
-function setupTestingModule(providers: any[]) {
-  TestBed.configureTestingModule({
-    declarations: [TestComponent],
-    // Note: the `NgOptimizedImage` directive is experimental and is not a part of the
-    // `CommonModule` yet, so it's imported separately.
-    imports: [CommonModule, NgOptimizedImageModule],
-    providers,
-  });
-}
-
-function createTestComponent(template: string): ComponentFixture<TestComponent> {
-  return TestBed.overrideComponent(TestComponent, {set: {template: template}})
-      .createComponent(TestComponent);
-}
