2c8fd2b57b184faddd75634b073f82a864040b46
Derek Cormier <derek@aspect.dev>
2022-06-08 15:00:58 -0700

    build(bazel): enable runfiles (#46313)
    Patches are required for tsec and rules_webtesting. The fix for
    rules_webtesting was merged to that repo
    (https://github.com/bazelbuild/rules_webtesting/commit/581b1557e382f93419da6a03b91a45c2ac9a9ec8)
    but it's unclear when a release will be cut.
    
    PR Close #46313

--- tools/testing/init_browser_tests.ts
+++ tools/testing/init_browser_tests.ts
@@ -7,6 +7,7 @@
  */
 
 import '@angular/compiler'; // For JIT mode. Must be in front of any other @angular/* imports.
+
 import {TestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
