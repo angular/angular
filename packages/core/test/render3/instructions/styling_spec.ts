fd8b9cf8e3a1a38eb0ce72fd84d46341611af0f7
Andrew Scott <atscott@google.com>
2022-06-29 13:48:48 -0700

    Revert "test(core): remove enableRenderer3 and Renderer3 from tests (#46612)"
    This reverts commit a183a2df50129228928bdae78faaf19678215a81.

--- packages/core/test/render3/instructions/styling_spec.ts
+++ packages/core/test/render3/instructions/styling_spec.ts
@@ -10,6 +10,7 @@ import {DirectiveDef} from '@angular/core/src/render3';
 import {ɵɵdefineDirective} from '@angular/core/src/render3/definition';
 import {classStringParser, styleStringParser, toStylingKeyValueArray, ɵɵclassProp, ɵɵstyleMap, ɵɵstyleProp} from '@angular/core/src/render3/instructions/styling';
 import {AttributeMarker, TAttributes} from '@angular/core/src/render3/interfaces/node';
+import {enableRenderer3} from '@angular/core/src/render3/interfaces/renderer';
 import {getTStylingRangeNext, getTStylingRangeNextDuplicate, getTStylingRangePrev, getTStylingRangePrevDuplicate, setTStylingRangeNext, setTStylingRangePrev, StylingRange, toTStylingRange, TStylingKey, TStylingRange} from '@angular/core/src/render3/interfaces/styling';
 import {HEADER_OFFSET, TVIEW} from '@angular/core/src/render3/interfaces/view';
 import {getLView, leaveView, setBindingRootForHostBindings} from '@angular/core/src/render3/state';
@@ -21,6 +22,7 @@ import {getElementClasses, getElementStyles} from '@angular/core/testing/src/sty
 import {clearFirstUpdatePass, enterViewWithOneDiv, rewindBindingIndex} from './shared_spec';
 
 describe('styling', () => {
+  beforeAll(enableRenderer3);
   beforeEach(enterViewWithOneDiv);
   afterEach(leaveView);
 
--- packages/core/test/render3/perf/setup.ts
+++ packages/core/test/render3/perf/setup.ts
@@ -8,14 +8,16 @@
 import {addToViewTree, createLContainer, createLView, createTNode, createTView, getOrCreateTNode, refreshView, renderView} from '../../../src/render3/instructions/shared';
 import {ComponentTemplate, DirectiveDefList} from '../../../src/render3/interfaces/definition';
 import {TAttributes, TElementNode, TNodeType} from '../../../src/render3/interfaces/node';
+import {domRendererFactory3, enableRenderer3, RendererFactory3} from '../../../src/render3/interfaces/renderer';
 import {LView, LViewFlags, TVIEW, TView, TViewType} from '../../../src/render3/interfaces/view';
 import {insertView} from '../../../src/render3/node_manipulation';
 
-import {MicroBenchmarkDomRendererFactory, MicroBenchmarkRendererFactory} from './noop_renderer';
+import {MicroBenchmarkRendererFactory} from './noop_renderer';
 
 const isBrowser = typeof process === 'undefined';
-const rendererFactory =
-    isBrowser ? new MicroBenchmarkDomRendererFactory() : new MicroBenchmarkRendererFactory();
+enableRenderer3();
+const rendererFactory: RendererFactory3 =
+    isBrowser ? domRendererFactory3 : new MicroBenchmarkRendererFactory;
 const renderer = rendererFactory.createRenderer(null, null);
 
 export function createAndRenderLView(
