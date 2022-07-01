a4ec1096a1e2b750b03aa9359ff9e437f427118e
dario-piotrowicz <dario.piotrowicz@gmail.com>
2022-07-01 15:23:57 +0100

    refactor(animations): refactor non-animatable check to be timeline based (#46666)
    move the check for non-animatable properties from the animation building
    phase to the application of the animation's transition instead, in such
    a way we can check it against the keyframes of the transition's timeline
    in order to only provide warnings for properties which are being
    animated, thus not providing any warning for non-animatable properties
    being applied to elements via the style function
    
    this change has the benfit just mentioned above but it comes with two
    drawbacks:
     - the warning handling is not done in the building time so it is a bit
    inconsistent with other type of validations (such as the unsupported css
    properties one for example)
     - before the warning was being applied only when the animation's data
    was being parsed, so it happed only once but now since it is applied
    when the animation is actually being prepared to be played, it happens
    each time the animation runs
    
    resolves #46602
    
    PR Close #46666

--- packages/animations/browser/src/dsl/animation_ast_builder.ts
+++ packages/animations/browser/src/dsl/animation_ast_builder.ts
@@ -11,7 +11,7 @@ import {invalidDefinition, invalidKeyframes, invalidOffset, invalidParallelAnima
 import {AnimationDriver} from '../render/animation_driver';
 import {getOrSetDefaultValue} from '../render/shared';
 import {convertToMap, copyObj, extractStyleParams, iteratorToArray, NG_ANIMATING_SELECTOR, NG_TRIGGER_SELECTOR, normalizeAnimationEntry, resolveTiming, SUBSTITUTION_EXPR_START, validateStyleParams, visitDslNode} from '../util';
-import {pushNonAnimatablePropertiesWarning, pushUnrecognizedPropertiesWarning} from '../warning_helpers';
+import {pushUnrecognizedPropertiesWarning} from '../warning_helpers';
 
 import {AnimateAst, AnimateChildAst, AnimateRefAst, Ast, DynamicTimingAst, GroupAst, KeyframesAst, QueryAst, ReferenceAst, SequenceAst, StaggerAst, StateAst, StyleAst, TimingAst, TransitionAst, TriggerAst} from './animation_ast';
 import {AnimationDslVisitor} from './animation_dsl_visitor';
@@ -79,13 +79,6 @@ export class AnimationAstBuilderVisitor implements AnimationDslVisitor {
         pushUnrecognizedPropertiesWarning(
             warnings,
             [...context.unsupportedCSSPropertiesFound.keys()],
-        );
-      }
-
-      if (context.nonAnimatableCSSPropertiesFound.size) {
-        pushNonAnimatablePropertiesWarning(
-            warnings,
-            [...context.nonAnimatableCSSPropertiesFound.keys()],
         );
       }
     }
@@ -321,15 +314,6 @@ export class AnimationAstBuilderVisitor implements AnimationDslVisitor {
             context.unsupportedCSSPropertiesFound.add(prop);
             return;
           }
-
-          if (this._driver.validateAnimatableStyleProperty) {
-            if (!this._driver.validateAnimatableStyleProperty(prop)) {
-              context.nonAnimatableCSSPropertiesFound.add(prop);
-              // note: non animatable properties are not removed for the tuple just in case they are
-              //       categorized as non animatable but can actually be animated
-              return;
-            }
-          }
         }
 
         // This is guaranteed to have a defined Map at this querySelector location making it
@@ -535,7 +519,6 @@ export class AnimationAstBuilderContext {
   public collectedStyles = new Map<string, Map<string, StyleTimeTuple>>();
   public options: AnimationOptions|null = null;
   public unsupportedCSSPropertiesFound: Set<string> = new Set<string>();
-  public readonly nonAnimatableCSSPropertiesFound: Set<string> = new Set<string>();
   constructor(public errors: Error[]) {}
 }
 
