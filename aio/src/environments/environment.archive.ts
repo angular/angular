963cfc622a84741c8b5a2ca3a5ab39d9be70edbe
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-07-05 17:57:00 +0000

    feat(docs-infra): set up Google Analytics 4 along with keeping legacy Universal Analytics
    We currently use Universal Analytics. This is deprecated in favor of
    Google Analytics 4 and UA will stop processing hits in October 2023.
    
    This change intends to prepare us for this migration, and to already
    pre-populate our GA4 property (there is no way to migrate existing data
    /properties into a GA4 property -- a new one needs to be created).
    
    This will help us minimize the data gap so that we can:
    
    * Continue to look at the UA property with the full time span until
      October 2023
    * Can start using the GA4 property long-term in the future, starting
      with data even before Universal Analytics stops processing new data.
    
    We need to keep the existing `analytics.js` setup. Initially we have
    considered using `gtag.js` for both the UA and GA4 properties, as it
    supports that, but that doesn't work with our strict trusted types
    enforcement because it results in multiple `gtag.js` scripts (specific
    versions for UA or GA4) that recreate the same trusted type policies.
    This causes runtime errors and breaks the setup.
    
    Instead, with continued use of `analytics.js` we have the benefit of
    a good separation of trusted types + events and configuration. There is
    some problematic with translation of Universal Analytics Events to GA4,
    or the other way around (even though we don't use custom events
    currenlty)
    
    We also do not need to send page views for our GA4 property because GA4
    with gtag supports this automatically (respecting the history state --
    using the `Enhanced measurement events` setting in the UI).
    For our UA legacy instance we continue to dispatch events manually. This
    logic can be removed in the future.
    
    More details can be found here:
    https://docs.google.com/document/d/1aK8u4ZlXbqQ2wMqmgSX7Ces8iLgamC13oCoG6VeBruA/edit?usp=sharing&resourcekey=0-EVe-Rhnme3bj_pkz2RcOmw.

--- aio/src/environments/environment.archive.ts
+++ aio/src/environments/environment.archive.ts
@@ -1,6 +1,8 @@
 // This is for archived sites, which are hosted at https://vX.angular.io/, where X is the major Angular version.
 export const environment = {
-  gaId: 'UA-8594346-15', // Production id (since it is linked from the main site)
+  legacyUniversalAnalyticsId: 'UA-8594346-15', // Production id (since it is linked from the main site)
+  googleAnalyticsId: 'G-BVV0RDSG7F', // Production id (since it is linked from the main site)
+
   production: true,
   mode: 'archive',
 };
