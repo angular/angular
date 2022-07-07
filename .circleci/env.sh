bd927236df1a7e4e29d887f3c95f2d79937fb466
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-07-07 22:41:23 +0200

    fixup! ci: use actual main branch for components repo unit test job (#46743)
    Remove todo
    PR Close #46743

--- .circleci/env.sh
+++ .circleci/env.sh
@@ -71,7 +71,6 @@ setPublicVar SAUCE_READY_FILE_TIMEOUT 120
 # the `save_cache` path configuration in `config.yml`
 setPublicVar COMPONENTS_REPO_TMP_DIR "/tmp/angular-components-repo"
 setPublicVar COMPONENTS_REPO_URL "https://github.com/angular/components.git"
-# TODO(BRANCH_RENAME_CLEANUP): fixup branch name to point to main branch
 setPublicVar COMPONENTS_REPO_BRANCH "main"
 # **NOTE**: When updating the commit SHA, also update the cache key in the CircleCI `config.yml`.
 setPublicVar COMPONENTS_REPO_COMMIT "7a24e95bafbdeb697f74a48e275c2442bcbefc74"
