3004e2ad3a267eeae79d91ee487cfd33efbf9fb0
Cédric Exbrayat <cedric@ninja-squad.com>
2022-07-15 09:37:52 +0200

    fix(compiler-cli): improve the missingControlFlowDirective message
    The extended diagnostics about missing control flow directive was only mentioning that the `CommonModule` should be imported.
    Now that the control flow directives are available as standalone, the message mentions that directive itself can be imported.
    
    The message now also mentions which import should be used for the directive (as it can be tricky to figure out that `NgForOf` is the directive corresponding to `*ngFor`).

--- packages/compiler-cli/src/ngtsc/typecheck/extended/checks/missing_control_flow_directive/index.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/extended/checks/missing_control_flow_directive/index.ts
@@ -15,14 +15,18 @@ import {NgTemplateDiagnostic} from '../../../api';
 import {TemplateCheckFactory, TemplateCheckWithVisitor, TemplateContext} from '../../api';
 
 /**
- * The list of known control flow directives present in the `CommonModule`.
+ * The list of known control flow directives present in the `CommonModule`,
+ * and their corresponding imports.
  *
  * Note: there is no `ngSwitch` here since it's typically used as a regular
  * binding (e.g. `[ngSwitch]`), however the `ngSwitchCase` and `ngSwitchDefault`
  * are used as structural directives and a warning would be generated. Once the
  * `CommonModule` is included, the `ngSwitch` would also be covered.
  */
-const KNOWN_CONTROL_FLOW_DIRECTIVES = new Set(['ngIf', 'ngFor', 'ngSwitchCase', 'ngSwitchDefault']);
+export const KNOWN_CONTROL_FLOW_DIRECTIVES = new Map([
+  ['ngIf', 'NgIf'], ['ngFor', 'NgForOf'], ['ngSwitchCase', 'NgSwitchCase'],
+  ['ngSwitchDefault', 'NgSwitchDefault']
+]);
 
 /**
  * Ensures that there are no known control flow directives (such as *ngIf and *ngFor)
@@ -64,9 +68,14 @@ class MissingControlFlowDirectiveCheck extends
     }
 
     const sourceSpan = controlFlowAttr.keySpan || controlFlowAttr.sourceSpan;
-    const errorMessage = `The \`*${controlFlowAttr.name}\` directive was used in the template, ` +
-        `but the \`CommonModule\` was not imported. Please make sure that the \`CommonModule\` ` +
-        `is included into the \`@Component.imports\` array of this component.`;
+    const correspondingImport = KNOWN_CONTROL_FLOW_DIRECTIVES.get(controlFlowAttr.name);
+    const errorMessage =
+        `The \`*${controlFlowAttr.name}\` directive was used in the template, ` +
+        `but neither the \`${
+            correspondingImport}\` directive nor the \`CommonModule\` was imported. ` +
+        `Please make sure that either the \`${
+            correspondingImport}\` directive or the \`CommonModule\` ` +
+        `is included in the \`@Component.imports\` array of this component.`;
     const diagnostic = ctx.makeTemplateDiagnostic(sourceSpan, errorMessage);
     return [diagnostic];
   }
