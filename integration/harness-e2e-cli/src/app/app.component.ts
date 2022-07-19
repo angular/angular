29056380e26a2a9501c32194de84a97b32a56fba
Alka Vats <76740397+alkavats1@users.noreply.github.com>
2022-07-19 19:40:04 +0530

    refactor: remove empty harness e2e stylesheet (#25304)
    removed the empty files to remove the code smells from the code

--- integration/harness-e2e-cli/src/app/app.component.ts
+++ integration/harness-e2e-cli/src/app/app.component.ts
@@ -3,7 +3,6 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'harness-e2e-cli';
