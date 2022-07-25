f23232ff66559ddc28aec26d461355568c25530d
Victor Porof <victorporof@chromium.org>
2022-07-25 09:04:41 +0000

    feat(zone.js): Update to the simpler Async Stack Tagging v2 API (#46958)
    Signed-off-by: Victor Porof <victorporof@chromium.org>
    
    PR Close #46958

--- packages/zone.js/test/zone-spec/async-tagging-console.spec.ts
+++ packages/zone.js/test/zone-spec/async-tagging-console.spec.ts
@@ -12,51 +12,55 @@ describe('AsyncTaggingConsoleTest', () => {
   const AsyncStackTaggingZoneSpec = (Zone as any)['AsyncStackTaggingZoneSpec'];
 
   describe('should call console async stack tagging API', () => {
-    let idx = 1;
-    const scheduleAsyncTaskSpy = jasmine.createSpy('scheduleAsyncTask').and.callFake(() => {
-      return idx++;
-    });
     const startAsyncTaskSpy = jasmine.createSpy('startAsyncTask');
     const finishAsyncTaskSpy = jasmine.createSpy('finishAsyncTask');
-    const cancelAsyncTaskSpy = jasmine.createSpy('cancelAsyncTask');
+    const scheduleAsyncTaskSpy = jasmine.createSpy('scheduleAsyncTask').and.callFake(() => {
+      return {
+        run: (f: () => unknown) => {
+          startAsyncTaskSpy();
+          const retval = f();
+          finishAsyncTaskSpy();
+          return retval;
+        },
+      };
+    });
+
     let asyncStackTaggingZone: Zone;
 
     beforeEach(() => {
       scheduleAsyncTaskSpy.calls.reset();
       startAsyncTaskSpy.calls.reset();
       finishAsyncTaskSpy.calls.reset();
-      cancelAsyncTaskSpy.calls.reset();
       asyncStackTaggingZone = Zone.current.fork(new AsyncStackTaggingZoneSpec('test', {
-        scheduleAsyncTask: scheduleAsyncTaskSpy,
-        startAsyncTask: startAsyncTaskSpy,
-        finishAsyncTask: finishAsyncTaskSpy,
-        cancelAsyncTask: cancelAsyncTaskSpy,
+        createTask: scheduleAsyncTaskSpy,
       }));
     });
+
     it('setTimeout', (done: DoneFn) => {
       asyncStackTaggingZone.run(() => {
         setTimeout(() => {});
       });
       setTimeout(() => {
-        expect(scheduleAsyncTaskSpy).toHaveBeenCalledWith('setTimeout', false);
+        expect(scheduleAsyncTaskSpy).toHaveBeenCalledWith('Zone - setTimeout');
         expect(startAsyncTaskSpy.calls.count()).toBe(1);
         expect(finishAsyncTaskSpy.calls.count()).toBe(1);
         done();
       });
     });
+
     it('clearTimeout', (done: DoneFn) => {
       asyncStackTaggingZone.run(() => {
         const id = setTimeout(() => {});
         clearTimeout(id);
       });
       setTimeout(() => {
-        expect(scheduleAsyncTaskSpy).toHaveBeenCalledWith('setTimeout', false);
+        expect(scheduleAsyncTaskSpy).toHaveBeenCalledWith('Zone - setTimeout');
         expect(startAsyncTaskSpy).not.toHaveBeenCalled();
         expect(finishAsyncTaskSpy).not.toHaveBeenCalled();
-        expect(cancelAsyncTaskSpy.calls.count()).toBe(1);
         done();
       });
     });
+
     it('setInterval', (done: DoneFn) => {
       asyncStackTaggingZone.run(() => {
         let count = 0;
@@ -68,19 +72,19 @@ describe('AsyncTaggingConsoleTest', () => {
         }, 10);
       });
       setTimeout(() => {
-        expect(scheduleAsyncTaskSpy).toHaveBeenCalledWith('setInterval', true);
+        expect(scheduleAsyncTaskSpy).toHaveBeenCalledWith('Zone - setInterval');
         expect(startAsyncTaskSpy.calls.count()).toBe(2);
-        expect(finishAsyncTaskSpy.calls.count()).toBe(1);
-        expect(cancelAsyncTaskSpy.calls.count()).toBe(1);
+        expect(finishAsyncTaskSpy.calls.count()).toBe(2);
         done();
       }, 50);
     });
+
     it('Promise', (done: DoneFn) => {
       asyncStackTaggingZone.run(() => {
         Promise.resolve(1).then(() => {});
       });
       setTimeout(() => {
-        expect(scheduleAsyncTaskSpy).toHaveBeenCalledWith('Promise.then', false);
+        expect(scheduleAsyncTaskSpy).toHaveBeenCalledWith('Zone - Promise.then');
         expect(startAsyncTaskSpy.calls.count()).toBe(1);
         expect(finishAsyncTaskSpy.calls.count()).toBe(1);
         done();
@@ -94,12 +98,10 @@ describe('AsyncTaggingConsoleTest', () => {
              Zone.root.run(() => {
                setTimeout(() => {
                  expect(scheduleAsyncTaskSpy.calls.all()[0].args).toEqual([
-                   'XMLHttpRequest.addEventListener:load',
-                   true,
+                   'Zone - XMLHttpRequest.addEventListener:load',
                  ]);
                  expect(scheduleAsyncTaskSpy.calls.all()[1].args).toEqual([
-                   'XMLHttpRequest.send',
-                   false,
+                   'Zone - XMLHttpRequest.send',
                  ]);
                  expect(startAsyncTaskSpy.calls.count()).toBe(2);
                  expect(finishAsyncTaskSpy.calls.count()).toBe(2);
@@ -124,10 +126,9 @@ describe('AsyncTaggingConsoleTest', () => {
            button.dispatchEvent(clickEvent);
            button.removeEventListener('click', handler);
            expect(scheduleAsyncTaskSpy)
-               .toHaveBeenCalledWith('HTMLButtonElement.addEventListener:click', true);
+               .toHaveBeenCalledWith('Zone - HTMLButtonElement.addEventListener:click');
            expect(startAsyncTaskSpy.calls.count()).toBe(2);
            expect(finishAsyncTaskSpy.calls.count()).toBe(2);
-           expect(cancelAsyncTaskSpy.calls.count()).toBe(1);
          });
        }));
   });
