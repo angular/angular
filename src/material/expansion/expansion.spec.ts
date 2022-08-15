2615b5f128aef9ea6e4f662b7f0d164b04f8165b
swseverance <swseverance@gmail.com>
2022-08-15 15:34:06 -0400

    fix(expansion): fix expansion `.mat-expansion-panel-header` styles (#20019)
    * Add margin to `.mat-content` element to account for consumer using
      `mat-expansion-panel` components with the toggle hidden alongside
      `mat-expansion-panel` components where the toggle is visible
    * Adjust sizing of `.mat-expansion-panel-header-title` and
      `.mat-expansion-panel-header-description` elements so that the width
      of each respective element is uniform across multiple adjacent
      `mat-expansion-panel` elements
    * Fixes #20002

--- src/material/expansion/expansion.spec.ts
+++ src/material/expansion/expansion.spec.ts
@@ -61,9 +61,8 @@ describe('MatExpansionPanel', () => {
 
   it('should be able to render panel content lazily', fakeAsync(() => {
     const fixture = TestBed.createComponent(LazyPanelWithContent);
-    const content = fixture.debugElement.query(
-      By.css('.mat-expansion-panel-content'),
-    )!.nativeElement;
+    const content = fixture.debugElement.query(By.css('.mat-expansion-panel-content'))!
+      .nativeElement;
     fixture.detectChanges();
 
     expect(content.textContent.trim())
@@ -80,9 +79,8 @@ describe('MatExpansionPanel', () => {
 
   it('should render the content for a lazy-loaded panel that is opened on init', fakeAsync(() => {
     const fixture = TestBed.createComponent(LazyPanelOpenOnLoad);
-    const content = fixture.debugElement.query(
-      By.css('.mat-expansion-panel-content'),
-    )!.nativeElement;
+    const content = fixture.debugElement.query(By.css('.mat-expansion-panel-content'))!
+      .nativeElement;
     fixture.detectChanges();
 
     expect(content.textContent.trim())
@@ -312,12 +310,14 @@ describe('MatExpansionPanel', () => {
   it('should be able to hide the toggle', () => {
     const fixture = TestBed.createComponent(PanelWithContent);
     const header = fixture.debugElement.query(By.css('.mat-expansion-panel-header'))!.nativeElement;
+    const content = fixture.debugElement.query(By.css('.mat-content'))!.nativeElement;
 
     fixture.detectChanges();
 
     expect(header.querySelector('.mat-expansion-indicator'))
       .withContext('Expected indicator to be shown.')
       .toBeTruthy();
+    expect(content.classList).not.toContain('mat-content-hide-toggle');
 
     fixture.componentInstance.hideToggle = true;
     fixture.detectChanges();
@@ -325,6 +325,7 @@ describe('MatExpansionPanel', () => {
     expect(header.querySelector('.mat-expansion-indicator'))
       .withContext('Expected indicator to be hidden.')
       .toBeFalsy();
+    expect(content.classList).toContain('mat-content-hide-toggle');
   });
 
   it('should update the indicator rotation when the expanded state is toggled programmatically', fakeAsync(() => {
