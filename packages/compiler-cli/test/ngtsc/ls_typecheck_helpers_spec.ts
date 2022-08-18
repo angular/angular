baaf1d14b5014c175a58e81591136915d013b0f4
Dylan Hunn <dylhunn@gmail.com>
2022-08-18 15:35:08 -0700

    refactor(compiler): Add a new helper method `getOwningNgModule`. (#47166)
    This helper accepts a class for an Angular trait, and returns the NgModule which owns that trait. This will be useful for the language service import project, which needs to edit import arrays on the module.
    
    PR Close #47166

--- packages/compiler-cli/test/ngtsc/ls_typecheck_helpers_spec.ts
+++ packages/compiler-cli/test/ngtsc/ls_typecheck_helpers_spec.ts
@@ -30,19 +30,18 @@ runInEachFileSystem(() => {
       env.tsconfig({strictTemplates: true, _enableTemplateTypeChecker: true});
     });
 
-
     describe('supports `getPrimaryAngularDecorator()` ', () => {
       it('for components', () => {
         env.write('test.ts', `
-		import {Component} from '@angular/core';
-
-		@Component({
-			standalone: true,
-			selector: 'test-cmp',
-			template: '<div></div>',
-		})
-		export class TestCmp {}
-		`);
+		 import {Component} from '@angular/core';
+ 
+		 @Component({
+			 standalone: true,
+			 selector: 'test-cmp',
+			 template: '<div></div>',
+		 })
+		 export class TestCmp {}
+		 `);
         const {program, checker} = env.driveTemplateTypeChecker();
         const sf = program.getSourceFile(_('/test.ts'));
         expect(sf).not.toBeNull();
@@ -52,15 +51,15 @@ runInEachFileSystem(() => {
 
       it('for pipes', () => {
         env.write('test.ts', `
-		import {Pipe, PipeTransform} from '@angular/core';
-
-		@Pipe({name: 'expPipe'})
-		export class ExpPipe implements PipeTransform {
-			transform(value: number, exponent = 1): number {
-				return Math.pow(value, exponent);
-			}
-		}
-		`);
+		 import {Pipe, PipeTransform} from '@angular/core';
+ 
+		 @Pipe({name: 'expPipe'})
+		 export class ExpPipe implements PipeTransform {
+			 transform(value: number, exponent = 1): number {
+				 return Math.pow(value, exponent);
+			 }
+		 }
+		 `);
         const {program, checker} = env.driveTemplateTypeChecker();
         const sf = program.getSourceFile(_('/test.ts'));
         expect(sf).not.toBeNull();
@@ -70,21 +69,105 @@ runInEachFileSystem(() => {
 
       it('for NgModules', () => {
         env.write('test.ts', `
-			import {NgModule} from '@angular/core';
-
-			@NgModule({
-				declarations: [],
-				imports: [],
-				providers: [],
-				bootstrap: []
-			})
-			export class AppModule {}
-		  `);
+			 import {NgModule} from '@angular/core';
+ 
+			 @NgModule({
+				 declarations: [],
+				 imports: [],
+				 providers: [],
+				 bootstrap: []
+			 })
+			 export class AppModule {}
+		   `);
         const {program, checker} = env.driveTemplateTypeChecker();
         const sf = program.getSourceFile(_('/test.ts'));
         expect(sf).not.toBeNull();
         const decorator = checker.getPrimaryAngularDecorator(getClass(sf!, 'AppModule'));
         expect(decorator?.getText()).toContain(`declarations: []`);
+      });
+    });
+
+    describe('supports `getOwningNgModule()` ', () => {
+      it('for components', () => {
+        env.write('test.ts', `
+			  import {Component, NgModule} from '@angular/core';
+  
+			  @NgModule({
+				  declarations: [AppCmp],
+				  imports: [],
+				  providers: [],
+				  bootstrap: [AppCmp]
+			  })
+			  export class AppModule {}
+	
+			  @Component({
+				  selector: 'app-cmp',
+				  template: '<div></div>',
+			  })
+			  export class AppCmp {}
+			`);
+        const {program, checker} = env.driveTemplateTypeChecker();
+        const sf = program.getSourceFile(_('/test.ts'));
+        expect(sf).not.toBeNull();
+        const ngModuleKnownClass = getClass(sf!, 'AppModule');
+        expect(ngModuleKnownClass).not.toBeNull();
+        const ngModuleRetrievedClass = checker.getOwningNgModule(getClass(sf!, 'AppCmp'));
+        expect(ngModuleRetrievedClass).toEqual(ngModuleKnownClass);
+      });
+
+      it('for standalone components (which should be null)', () => {
+        env.write('test.ts', `
+			  import {Component, NgModule} from '@angular/core';
+  
+			  @NgModule({
+				  declarations: [AppCmp],
+				  imports: [],
+				  providers: [],
+				  bootstrap: [AppCmp]
+			  })
+			  export class AppModule {}
+	
+			  @Component({
+				  selector: 'app-cmp',
+				  template: '<div></div>',
+				  standalone: true,
+			  })
+			  export class AppCmp {}
+			`);
+        const {program, checker} = env.driveTemplateTypeChecker();
+        const sf = program.getSourceFile(_('/test.ts'));
+        expect(sf).not.toBeNull();
+        const ngModuleKnownClass = getClass(sf!, 'AppModule');
+        expect(ngModuleKnownClass).not.toBeNull();
+        const ngModuleRetrievedClass = checker.getOwningNgModule(getClass(sf!, 'AppCmp'));
+        expect(ngModuleRetrievedClass).toBe(null);
+      });
+
+      it('for pipes', () => {
+        env.write('test.ts', `
+			  import {Component, NgModule, Pipe, PipeTransform} from '@angular/core';
+  
+			  @NgModule({
+				  declarations: [ExpPipe],
+				  imports: [],
+				  providers: [],
+			  })
+			  export class PipeModule {}
+	
+			  @Pipe({name: 'expPipe'})
+			  export class ExpPipe implements PipeTransform {
+				  transform(value: number, exponent = 1): number {
+					  return Math.pow(value, exponent);
+				  }
+			  }
+			`);
+        const {program, checker} = env.driveTemplateTypeChecker();
+        const sf = program.getSourceFile(_('/test.ts'));
+        expect(sf).not.toBeNull();
+        const ngModuleKnownClass = getClass(sf!, 'PipeModule');
+        expect(ngModuleKnownClass).not.toBeNull();
+        const ngModuleRetrievedClass = checker.getOwningNgModule(getClass(sf!, 'ExpPipe'));
+        expect(ngModuleRetrievedClass).toEqual(ngModuleKnownClass);
       });
     });
   });
