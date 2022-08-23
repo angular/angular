834927d0e7e3f5432783cc2927e75e9558b6ff60
Jeremy Elbourn <jelbourn@google.com>
2022-08-23 15:45:12 -0700

    refactor(animations): remove unnecesary `null` (#47238)
    These null values are unused and unecessary. I suspect it's a remnant from when the codebase was transpiled to Dart.
    
    PR Close #47238

--- packages/animations/src/util.ts
+++ packages/animations/src/util.ts
@@ -6,5 +6,5 @@
  * found in the LICENSE file at https://angular.io/license
  */
 export function scheduleMicroTask(cb: () => any) {
-  Promise.resolve(null).then(cb);
+  Promise.resolve().then(cb);
 }
