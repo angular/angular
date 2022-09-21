d7683a611a6f0331c5aa03d83b994741fe8cf9bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-20 17:02:30 -0700

    refactor(http): introduce the functional interceptors API (#47502)
    This commit introduces new types and symbols related to functional HTTP
    interceptors - interceptors which are plain functions with access to DI via
    the `inject()` operation.
    
    This new form of interceptor is not exposed publically in this commit, but
    the legacy class-based interceptors are refactored to be built on top of the
    new API internally.
    
    PR Close #47502

--- packages/common/http/public_api.ts
+++ packages/common/http/public_api.ts
@@ -31,11 +31,14 @@ export {HttpBackend, HttpHandler} from './src/backend';
 export {HttpClient} from './src/client';
 export {HttpContext, HttpContextToken} from './src/context';
 export {HttpHeaders} from './src/headers';
-export {HTTP_INTERCEPTORS, HttpInterceptor} from './src/interceptor';
+export {HTTP_INTERCEPTORS, HttpInterceptor, HttpInterceptorHandler as ɵHttpInterceptorHandler} from './src/interceptor';
 export {JsonpClientBackend, JsonpInterceptor} from './src/jsonp';
-export {HttpClientJsonpModule, HttpClientModule, HttpClientXsrfModule, HttpInterceptingHandler as ɵHttpInterceptingHandler} from './src/module';
+export {HttpClientJsonpModule, HttpClientModule, HttpClientXsrfModule} from './src/module';
 export {HttpParameterCodec, HttpParams, HttpParamsOptions, HttpUrlEncodingCodec} from './src/params';
 export {HttpRequest} from './src/request';
 export {HttpDownloadProgressEvent, HttpErrorResponse, HttpEvent, HttpEventType, HttpHeaderResponse, HttpProgressEvent, HttpResponse, HttpResponseBase, HttpSentEvent, HttpStatusCode, HttpUploadProgressEvent, HttpUserEvent} from './src/response';
 export {HttpXhrBackend} from './src/xhr';
 export {HttpXsrfTokenExtractor} from './src/xsrf';
+
+// This re-export exists because g3 depends on this old private name.
+export {HttpInterceptorHandler as ɵHttpInterceptingHandler} from './src/interceptor';
