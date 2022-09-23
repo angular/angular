4864d2e6dd3520b5e0ca1ba221acf93be6527b66
Derek Cormier <derek@aspect.dev>
2022-09-22 18:09:33 -0700

    build(bazel): fix broken aio github links on Windows
    Caused the aio e2e tests to fail.

--- aio/tools/transforms/angular-api-package/index.js
+++ aio/tools/transforms/angular-api-package/index.js
@@ -42,7 +42,7 @@ module.exports =
         .processor(require('./processors/collectPackageContentDocs'))
         .processor(require('./processors/processPackages'))
         .processor(require('./processors/processNgModuleDocs'))
-        .processor(require('./processors/fixupRealProjectRelativePath'))
+        .processor(require('./processors/fixupProjectRelativePath'))
         .processor(require('./processors/processAliasDocs'))
         .processor(require('./processors/mergeOverriddenImplementation'))
 
