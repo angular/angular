133beab0831427711fda37064d0b16fe5544295a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-09-24 09:09:53 +0000

    build: setup remote execution through shared command
    Sets up remote execution through a shared command.

--- .circleci/env.linux.sh
+++ .circleci/env.linux.sh
@@ -1,11 +1,4 @@
 ####################################################################################################
-# Decrypt GCP Credentials and store them as the Google default credentials.
-####################################################################################################
-mkdir -p "$HOME/.config/gcloud";
-openssl aes-256-cbc -d -in "${PROJECT_ROOT}/.circleci/gcp_token" \
-        -md md5 -k "$CIRCLE_PROJECT_REPONAME" -out "$HOME/.config/gcloud/application_default_credentials.json"
-
-####################################################################################################
 # Set bazel configuration for CircleCI runs.
 ####################################################################################################
-cp "${PROJECT_ROOT}/.circleci/bazel.linux.rc" "$HOME/.bazelrc";
+cp "${PROJECT_ROOT}/.circleci/bazel.linux.rc" ".bazelrc.user";
--- scripts/ci/run_angular_components_unit_tests.sh
+++ scripts/ci/run_angular_components_unit_tests.sh
@@ -13,6 +13,11 @@ cd ${COMPONENTS_REPO_TMP_DIR}
 # Create a symlink for the Bazel binary installed through NPM, as running through Yarn introduces OOM errors.
 ./scripts/circleci/setup_bazel_binary.sh
 
+# Properly wire up the components repo CircleCI Bazel configuration
+# Also enable RBE. The credentials are expected to be set up already.
+echo "import %workspace%/.circleci/bazel.rc" >> ./.bazelrc.user
+echo "build --config=remote" >> ./.bazelrc.user
+
 # Now actually run the tests.
 bazel test \
   --build_tag_filters=-docs-package,-e2e,-browser:firefox \
