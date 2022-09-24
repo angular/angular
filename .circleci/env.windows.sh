b797dabfd16680cd8785fbc7ff11b1f6e62a64a8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-09-24 09:09:53 +0000

    build: setup remote execution through shared command
    Sets up remote execution through a shared command.

--- .circleci/env.windows.sh
+++ .circleci/env.windows.sh
@@ -1,14 +1,7 @@
 ####################################################################################################
-# Decrypt GCP Credentials and store them as the Google default credentials.
-####################################################################################################
-mkdir -p "${APPDATA}/gcloud/";
-openssl aes-256-cbc -d -in "${PROJECT_ROOT}/.circleci/gcp_token" \
-        -md md5 -k "$CIRCLE_PROJECT_REPONAME" -out "${APPDATA}/gcloud/application_default_credentials.json"
-
-####################################################################################################
 # Set bazel configuration for CircleCI runs.
 ####################################################################################################
-cp "${PROJECT_ROOT}/.circleci/bazel.windows.rc" "${USERPROFILE}/.bazelrc";
+cp "${PROJECT_ROOT}/.circleci/bazel.windows.rc" ".bazelrc.user";
 
 
 # Expose the Bazelisk version. We need to run Bazelisk globally since Windows has problems launching
