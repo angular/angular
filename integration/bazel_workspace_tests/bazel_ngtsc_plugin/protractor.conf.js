0a360943b41ee499c4b71d2dc529a31e7e6cd140
Alan Agius <alanagius@google.com>
2022-09-29 16:54:26 +0000

    test: delete bazel_ngtsc_plugin integration test (#47584)
    This does not provide any added value.
    
    PR Close #47584

--- integration/bazel_workspace_tests/bazel_ngtsc_plugin/protractor.conf.js
+++
@@ -1,4 +0,0 @@
-exports.config = {
-  getPageTimeout: 60 * 1000,
-  allScriptsTimeout: 60 * 1000,
-};
--- integration/bazel_workspace_tests/bazel_ngtsc_plugin/src/hello-world/hello-world.module.ts
+++
@@ -1,8 +0,0 @@
-import {HelloWorldComponent} from './hello-world.component';
-import {NgModule} from '@angular/core';
-
-@NgModule({
-  declarations: [HelloWorldComponent],
-  exports: [HelloWorldComponent],
-})
-export class HelloWorldModule {}
