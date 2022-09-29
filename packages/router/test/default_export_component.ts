942d6c58c6c1ae2d526f5eef5a8aefeecaf9c0bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-29 14:17:15 -0700

    feat(router): auto-unwrap default exports when lazy loading
    When using `loadChildren` or `loadComponent`, a common pattern is to pass
    a function that returns a `Promise` from a dynamic import:
    
    ```typescript
    {
      path: 'lazy',
      loadComponent: () => import('./lazy-file').then(m => m.LazyCmp),
    }
    ```
    
    The `.then` part of the expression selects the particular exported
    component symbol from the dynamically imported ES module.
    
    ES modules can have a "default export", created with the `export default`
    modifier:
    
    ```typescript
    @Component({...})
    export default class LazyCmp { ... }
    ```
    
    This default export is made available to dynamic imports under the well-
    known key of `'default'`, per the ES module spec:
    https://tc39.es/ecma262/#table-export-forms-mapping-to-exportentry-records
    
    This commit adds a feature to the router to automatically dereference such
    default exports. With this logic, when `export default` is used, a `.then`
    operation to select the particular exported symbol is no longer required:
    
    ```typescript
    {
      path: 'lazy',
      loadComponent: () => import('./lazy-file'),
    }
    ```
    
    The above `loadComponent` operation will automatically use the `default`
    export of the `lazy-file` ES module.
    
    This functionality works for `loadChildren` as well.

---
+++ packages/router/test/default_export_component.ts
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+  standalone: true,
+  template: 'default exported',
+  selector: 'test-route',
+})
+export default class TestRoute {
+}
