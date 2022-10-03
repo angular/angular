a2a066d3258df2f45538b3aa0a061428fb40e3c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-10-03 13:51:19 -0700

    refactor(router): organize deprecated loadChildren code (#47586)
    This commit breaks out the code in @angular/router needed to support the
    deprecated `loadChildren` signature into a separate file, which reduces the
    opportunity for conflicts when patching that behavior in g3.
    
    PR Close #47586

---
+++ packages/router/src/deprecated_load_children.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Injector, NgModuleFactory} from '@angular/core';
+import {Observable} from 'rxjs';
+
+// This file exists to support the legacy `loadChildren: string` behavior being patched back into
+// Angular.
+
+/**
+ * Deprecated `loadChildren` value types.
+ *
+ * @publicApi
+ * @deprecated represents the deprecated type side of `LoadChildren`.
+ */
+export type DeprecatedLoadChildren = never;
+
+export function deprecatedLoadChildrenString(
+    injector: Injector, loadChildren: unknown): Observable<NgModuleFactory<any>>|null {
+  return null;
+}
