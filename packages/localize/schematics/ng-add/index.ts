301a8b502ed4a4e0881460f422d651493c539a19
Alan Agius <alanagius@google.com>
2022-10-13 14:09:57 +0000

    fix(localize): update ng add schematic to support Angular CLI version 15
    Prior to this, the `@angular/localize/init` was added as a polyfill which caused the `@angular/localize` types not to be included in the TypeScript program which caused errors such as the below:
    
    ```
    Error: src/app/app.component.ts:9:11 - error TS2304: Cannot find name '$localize'.
    ```
    
    With the recent changes in the CLI (https://github.com/angular/angular-cli/pull/24032), adding `@angular/localize/init` as polyfil or in the `main.server.ts` is no longer necessary. Instead we add this as a TypeScript type. When users are running in JIT mode, we add `@angular/localize/init` as an additional entrypoint.
    
    This change also exposes the `$localize` method as a global when importing `@angular/localize`.
    
    Closes #47677

--- packages/localize/schematics/ng-add/index.ts
+++ packages/localize/schematics/ng-add/index.ts
@@ -5,105 +5,85 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  *
- * @fileoverview Schematics for ng-new project that builds with Bazel.
+ * @fileoverview Schematics for `ng add @angular/localize` schematic.
  */
 
-import {tags} from '@angular-devkit/core';
 import {chain, noop, Rule, SchematicContext, SchematicsException, Tree,} from '@angular-devkit/schematics';
 import {NodePackageInstallTask} from '@angular-devkit/schematics/tasks';
 import {addPackageJsonDependency, NodeDependencyType, removePackageJsonDependency,} from '@schematics/angular/utility/dependencies';
-import {allTargetOptions, getWorkspace, updateWorkspace,} from '@schematics/angular/utility/workspace';
+import {JSONFile, JSONPath} from '@schematics/angular/utility/json-file';
+import {getWorkspace} from '@schematics/angular/utility/workspace';
 import {Builders} from '@schematics/angular/utility/workspace-models';
 
 import {Schema} from './schema';
 
-export const localizePolyfill = `@angular/localize/init`;
+const localizeType = `@angular/localize`;
 
-function prependToMainFiles(projectName: string): Rule {
+function addTypeScriptConfigTypes(projectName: string): Rule {
   return async (host: Tree) => {
     const workspace = await getWorkspace(host);
     const project = workspace.projects.get(projectName);
     if (!project) {
-      throw new SchematicsException(`Invalid project name (${projectName})`);
+      throw new SchematicsException(`Invalid project name '${projectName}'.`);
     }
 
-    const fileList = new Set<string>();
+    // We add the root workspace tsconfig for better IDE support.
+    const tsConfigFiles = new Set<string>(['tsconfig.json']);
     for (const target of project.targets.values()) {
-      if (target.builder !== Builders.Server) {
-        continue;
-      }
+      switch (target.builder) {
+        case Builders.Karma:
+        case Builders.Server:
+        case Builders.Browser:
+          const value = target.options?.['tsConfig'];
+          if (typeof value === 'string') {
+            tsConfigFiles.add(value);
+          }
 
-      for (const [, options] of allTargetOptions(target)) {
-        const value = options['main'];
-        if (typeof value === 'string') {
-          fileList.add(value);
-        }
+          break;
       }
     }
 
-    for (const path of fileList) {
-      const content = host.readText(path);
-      if (content.includes(localizePolyfill)) {
-        // If the file already contains the polyfill (or variations), ignore it too.
+    const typesJsonPath: JSONPath = ['compilerOptions', 'types'];
+    for (const path of tsConfigFiles) {
+      if (!host.exists(path)) {
         continue;
       }
 
-      // Add string at the start of the file.
-      const recorder = host.beginUpdate(path);
+      const json = new JSONFile(host, path);
+      const types = json.get(typesJsonPath) ?? [];
+      if (!Array.isArray(types)) {
+        throw new SchematicsException(`TypeScript configuration file '${
+            path}' has an invalid 'types' property. It must be an array.`);
+      }
 
-      const localizeStr =
-          tags.stripIndents`/***************************************************************************************************
-     * Load \`$localize\` onto the global scope - used if i18n tags appear in Angular templates.
-     */
-     import '${localizePolyfill}';
-    `;
-      recorder.insertLeft(0, localizeStr);
-      host.commitUpdate(recorder);
+      const hasLocalizeType =
+          types.some((t) => t === localizeType || t === '@angular/localize/init');
+      if (hasLocalizeType) {
+        // Skip has already localize type.
+        continue;
+      }
+
+      json.modify(typesJsonPath, [...types, localizeType]);
     }
   };
 }
 
-function addToPolyfillsOption(projectName: string): Rule {
-  return updateWorkspace((workspace) => {
-    const project = workspace.projects.get(projectName);
-    if (!project) {
-      throw new SchematicsException(`Invalid project name (${projectName})`);
-    }
-
-    for (const target of project.targets.values()) {
-      if (target.builder !== Builders.Browser && target.builder !== Builders.Karma) {
-        continue;
-      }
-
-      target.options ??= {};
-      target.options['polyfills'] ??= [localizePolyfill];
-
-      for (const [, options] of allTargetOptions(target)) {
-        // Convert polyfills option to array.
-        const polyfillsValue = typeof options['polyfills'] === 'string' ? [options['polyfills']] :
-                                                                          options['polyfills'];
-        if (Array.isArray(polyfillsValue) && !polyfillsValue.includes(localizePolyfill)) {
-          options['polyfills'] = [...polyfillsValue, localizePolyfill];
-        }
-      }
-    }
-  });
-}
-
 function moveToDependencies(host: Tree, context: SchematicContext): void {
-  if (host.exists('package.json')) {
-    // Remove the previous dependency and add in a new one under the desired type.
-    removePackageJsonDependency(host, '@angular/localize');
-    addPackageJsonDependency(host, {
-      name: '@angular/localize',
-      type: NodeDependencyType.Default,
-      version: `~0.0.0-PLACEHOLDER`,
-    });
-
-    // Add a task to run the package manager. This is necessary because we updated
-    // "package.json" and we want lock files to reflect this.
-    context.addTask(new NodePackageInstallTask());
+  if (!host.exists('package.json')) {
+    return;
   }
+
+  // Remove the previous dependency and add in a new one under the desired type.
+  removePackageJsonDependency(host, '@angular/localize');
+  addPackageJsonDependency(host, {
+    name: '@angular/localize',
+    type: NodeDependencyType.Default,
+    version: `~0.0.0-PLACEHOLDER`,
+  });
+
+  // Add a task to run the package manager. This is necessary because we updated
+  // "package.json" and we want lock files to reflect this.
+  context.addTask(new NodePackageInstallTask());
 }
 
 export default function(options: Schema): Rule {
@@ -117,8 +97,7 @@ export default function(options: Schema): Rule {
     }
 
     return chain([
-      prependToMainFiles(projectName),
-      addToPolyfillsOption(projectName),
+      addTypeScriptConfigTypes(projectName),
       // If `$localize` will be used at runtime then must install `@angular/localize`
       // into `dependencies`, rather than the default of `devDependencies`.
       options.useAtRuntime ? moveToDependencies : noop(),
