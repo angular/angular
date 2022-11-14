b6715973238937308c71d7a8e7d66edf33b96747
ced <cedric@ninja-squad.com>
2022-11-14 19:00:55 +0100

    fix(common): export the IMAGE_CONFIG token (#48051)
    The token introduced in 4fde292bb58f5d5bc3cf6e634f7cff9eb0d13d84 was not exported publicly.
    
    PR Close #48051

--- packages/common/src/common.ts
+++ packages/common/src/common.ts
@@ -27,4 +27,4 @@ export {PLATFORM_BROWSER_ID as ɵPLATFORM_BROWSER_ID, PLATFORM_SERVER_ID as ɵPL
 export {VERSION} from './version';
 export {ViewportScroller, NullViewportScroller as ɵNullViewportScroller} from './viewport_scroller';
 export {XhrFactory} from './xhr';
-export {IMAGE_LOADER, ImageLoader, ImageLoaderConfig, NgOptimizedImage, PRECONNECT_CHECK_BLOCKLIST, provideCloudflareLoader, provideCloudinaryLoader, provideImageKitLoader, provideImgixLoader} from './directives/ng_optimized_image';
+export {IMAGE_CONFIG, ImageConfig, IMAGE_LOADER, ImageLoader, ImageLoaderConfig, NgOptimizedImage, PRECONNECT_CHECK_BLOCKLIST, provideCloudflareLoader, provideCloudinaryLoader, provideImageKitLoader, provideImgixLoader} from './directives/ng_optimized_image';
