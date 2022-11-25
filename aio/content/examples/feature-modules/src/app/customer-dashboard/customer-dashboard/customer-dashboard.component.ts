ebb3f58f4c8455eff657c234bb5e231d38d66455
piyush132000 <piyush132000@gmail.com>
2022-11-25 09:43:46 +0530

    docs: removed empty methods (#48230)
    I removed empty methods from a file and unnecessary import
    
    PR Close #48230

--- aio/content/examples/feature-modules/src/app/customer-dashboard/customer-dashboard/customer-dashboard.component.ts
+++ aio/content/examples/feature-modules/src/app/customer-dashboard/customer-dashboard/customer-dashboard.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-customer-dashboard',
   templateUrl: './customer-dashboard.component.html',
   styleUrls: ['./customer-dashboard.component.css']
 })
-export class CustomerDashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class CustomerDashboardComponent {
 
 }
