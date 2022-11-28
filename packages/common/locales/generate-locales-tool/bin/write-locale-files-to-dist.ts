d9bf723cc692d0d364bffab2ec22f4e66167a0d7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-11-28 15:28:06 +0000

    build: refactor `generate-locales-tool` to ESM
    The `generate-locales-tool` now needs to run as ESM because we changed
    the `ts_library` rule. This commit accounts for the ESM syntax but
    removing CJS code parts like `require.main === module`.
    
    Also imports using `require` need to be changed to their ESM
    equivalents.

--- packages/common/locales/generate-locales-tool/bin/write-locale-files-to-dist.ts
+++ packages/common/locales/generate-locales-tool/bin/write-locale-files-to-dist.ts
@@ -21,7 +21,11 @@ import {BASE_LOCALE} from './base-locale';
  * Generates locale files for each available CLDR locale and writes it to the
  * specified directory.
  */
-function main(outputDir: string) {
+function main(outputDir: string|undefined) {
+  if (outputDir === undefined) {
+    throw Error('No output directory specified.');
+  }
+
   const cldrData = new CldrData();
   const baseLocaleData = cldrData.getLocaleData(BASE_LOCALE)!;
   const baseCurrencies = generateBaseCurrencies(baseLocaleData);
@@ -31,7 +35,7 @@ function main(outputDir: string) {
   console.info(`Writing locales to: ${outputDir}`);
 
   // Generate locale files for all locales we have data for.
-  cldrData.availableLocales.forEach(localeData => {
+  cldrData.availableLocales.forEach((localeData) => {
     const locale = localeData.locale;
     const localeFile = generateLocale(locale, localeData, baseCurrencies);
     const localeExtraFile = generateLocaleExtra(locale, localeData);
@@ -43,15 +47,4 @@ function main(outputDir: string) {
   });
 }
 
-
-if (require.main === module) {
-  // The first argument is expected to be a path resolving to a directory
-  // where all locales should be generated into.
-  const outputDir = process.argv[2];
-
-  if (outputDir === undefined) {
-    throw Error('No output directory specified.');
-  }
-
-  main(outputDir);
-}
+main(process.argv[2]);
