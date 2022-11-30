861f75591873d6038daa448ba07bd0e1a4d7eafd
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-11-30 15:43:23 +0000

    build: remove docker-based AIO preview deploy setup (#48345)
    The docker-based AIO preview deploy setup is replaced with
    significantly simpler setup using GitHub actions provided by
    dev-infra (also something dev-infra can maintain better then).
    
    The actions under the hood leverage Firebase preview channels.
    
    PR Close #48345

--- aio/aio-builds-setup/dockerbuild/scripts-js/lib/preview-server/preview-error.ts
+++
@@ -1,8 +0,0 @@
-// Classes
-export class PreviewServerError extends Error {
-  // Constructor
-  constructor(public status: number = 500, message?: string) {
-    super(message);
-    Object.setPrototypeOf(this, PreviewServerError.prototype);
-  }
-}
--- aio/aio-builds-setup/dockerbuild/scripts-js/test/index.ts
+++
@@ -1,6 +0,0 @@
-// Imports
-import {runTests} from '../lib/common/run-tests';
-
-// Run
-const specFiles = [`${__dirname}/**/*.spec.js`];
-runTests(specFiles);
