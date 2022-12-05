8ed0178268f0742c194f3e87efe2dbf7499700e0
Dylan Hunn <dylhunn@gmail.com>
2022-12-04 17:20:00 -0800

    refactor(language-service): Improve the quick fix auto-import tests.
    Currently, the auto-import tests are very difficult to debug, because the `expect`ations are buried several function calls deep, and also because the multi-line replacements are hard to deal with. The auto-import tests now use a different approach, which can be easily debugged from the failure message. In addition, several new tests have been added to cover more cases.

--- packages/language-service/test/code_fixes_spec.ts
+++ packages/language-service/test/code_fixes_spec.ts
@@ -7,6 +7,8 @@
  */
 
 import {initMockFileSystem} from '@angular/compiler-cli/src/ngtsc/file_system/testing';
+import {spawn} from 'child_process';
+import {CodeAction} from 'typescript';
 
 import {FixIdForCodeFixesAll} from '../src/codefixes/utils';
 import {createModuleAndProjectWithDeclarations, LanguageServiceTestEnv} from '../testing';
@@ -21,15 +23,15 @@ describe('code fixes', () => {
   it('should fix error when property does not exist on type', () => {
     const files = {
       'app.ts': `
-      import {Component, NgModule} from '@angular/core';
-
-      @Component({
-        templateUrl: './app.html'
-      })
-      export class AppComponent {
-        title1 = '';
-      }
-    `,
+       import {Component, NgModule} from '@angular/core';
+ 
+       @Component({
+         templateUrl: './app.html'
+       })
+       export class AppComponent {
+         title1 = '';
+       }
+     `,
       'app.html': `{{title}}`
     };
 
@@ -56,14 +58,14 @@ describe('code fixes', () => {
   it('should fix a missing method when property does not exist on type', () => {
     const files = {
       'app.ts': `
-      import {Component, NgModule} from '@angular/core';
-
-      @Component({
-        templateUrl: './app.html'
-      })
-      export class AppComponent {
-      }
-    `,
+       import {Component, NgModule} from '@angular/core';
+ 
+       @Component({
+         templateUrl: './app.html'
+       })
+       export class AppComponent {
+       }
+     `,
       'app.html': `{{title('Angular')}}`
     };
 
@@ -88,15 +90,15 @@ describe('code fixes', () => {
      () => {
        const files = {
          'app.ts': `
-        import {Component, NgModule} from '@angular/core';
-  
-        @Component({
-          templateUrl: './app.html'
-        })
-        export class AppComponent {
-          title1 = '';
-        }
-      `,
+         import {Component, NgModule} from '@angular/core';
+   
+         @Component({
+           templateUrl: './app.html'
+         })
+         export class AppComponent {
+           title1 = '';
+         }
+       `,
          'app.html': `<div *ngIf="title" />`
        };
 
@@ -112,16 +114,16 @@ describe('code fixes', () => {
   it('should fix all errors when property does not exist on type', () => {
     const files = {
       'app.ts': `
-      import {Component, NgModule} from '@angular/core';
-
-      @Component({
-        template: '{{tite}}{{bannr}}',
-      })
-      export class AppComponent {
-        title = '';
-        banner = '';
-      }
-    `,
+       import {Component, NgModule} from '@angular/core';
+ 
+       @Component({
+         template: '{{tite}}{{bannr}}',
+       })
+       export class AppComponent {
+         title = '';
+         banner = '';
+       }
+     `,
     };
 
     const project = createModuleAndProjectWithDeclarations(env, 'test', files);
@@ -162,15 +164,15 @@ describe('code fixes', () => {
   it('should fix invalid banana-in-box error', () => {
     const files = {
       'app.ts': `
-      import {Component, NgModule} from '@angular/core';
-
-      @Component({
-        templateUrl: './app.html'
-      })
-      export class AppComponent {
-        title = '';
-      }
-    `,
+       import {Component, NgModule} from '@angular/core';
+ 
+       @Component({
+         templateUrl: './app.html'
+       })
+       export class AppComponent {
+         title = '';
+       }
+     `,
       'app.html': `<input ([ngModel])="title">`,
     };
 
@@ -194,16 +196,16 @@ describe('code fixes', () => {
   it('should fix all invalid banana-in-box errors', () => {
     const files = {
       'app.ts': `
-      import {Component, NgModule} from '@angular/core';
-
-      @Component({
-        template: '<input ([ngModel])="title"><input ([value])="title">',
-      })
-      export class AppComponent {
-        title = '';
-        banner = '';
-      }
-    `,
+       import {Component, NgModule} from '@angular/core';
+ 
+       @Component({
+         template: '<input ([ngModel])="title"><input ([value])="title">',
+       })
+       export class AppComponent {
+         title = '';
+         banner = '';
+       }
+     `,
     };
 
     const project = createModuleAndProjectWithDeclarations(env, 'test', files);
@@ -228,61 +230,177 @@ describe('code fixes', () => {
   });
 
   describe('should fix missing selector imports', () => {
-    const files = {};
-    const standaloneFiles = {
-      'foo.ts': ` import {CommonModule} from '@angular/common';
-      import {Component} from '@angular/core';
-
-      @Component({
-        selector: 'foo',
-        template: '<bar></bar>',
-        standalone: true
-      })
-      export class FooComponent {}`,
-      'bar.ts': `
-      import {CommonModule} from '@angular/common';
-      import {Component} from '@angular/core';
-
-      @Component({
-        selector: 'bar',
-        template: '<div>bar</div>',
-        standalone: true
-      })
-      export class BarComponent {}`,
-    };
-
     it('for a new standalone component import', () => {
-      const project =
-          createModuleAndProjectWithDeclarations(env, 'test', files, {}, standaloneFiles);
+      const standaloneFiles = {
+        'foo.ts': `
+         import {Component} from '@angular/core';
+         @Component({
+           selector: 'foo',
+           template: '<bar></bar>',
+           standalone: true
+         })
+         export class FooComponent {}
+         `,
+        'bar.ts': `
+         import {Component} from '@angular/core';
+         @Component({
+           selector: 'bar',
+           template: '<div>bar</div>',
+           standalone: true
+         })
+         export class BarComponent {}
+         `,
+      };
+
+      const project = createModuleAndProjectWithDeclarations(env, 'test', {}, {}, standaloneFiles);
       const diags = project.getDiagnosticsForFile('foo.ts');
       const fixFile = project.openFile('foo.ts');
       fixFile.moveCursorToText('<¦bar>');
 
       const codeActions =
           project.getCodeFixesAtPosition('foo.ts', fixFile.cursor, fixFile.cursor, [diags[0].code]);
-      // TODO(dylhunn): These integration test helpers are hard to debug, and somewhat brittle
-      // against formatting. They can be refactored more thoroughly to simplify multiline tests
-      // like these, using Jasime expects all the way down.
-      expectIncludeReplacementText({
-        codeActions,
-        content: fixFile.contents,
-        text: null,
-        newText:
-            `{\n    selector: 'foo',\n    template: '<bar></bar>',\n    standalone: true,\n    imports: [BarComponent]\n}`,
-        fileName: 'foo.ts',
-        description: `Import BarComponent from './bar' on FooComponent`,
-        removeWhitespace: false,  // Include whitespace to check multiline formatting
-      });
-      expectIncludeAddText({
-        codeActions,
-        position: null,
-        text: `import{BarComponent}from"./bar";`,
-        fileName: 'foo.ts',
-        removeWhitespace: true,
-      });
+      const actionChanges = allChangesForCodeActions(fixFile.contents, codeActions);
+      actionChangesMatch(actionChanges, `Import BarComponent from './bar' on FooComponent`, [
+        [
+          ``,
+          `import { BarComponent } from "./bar";`,
+        ],
+        [
+          `{`,
+          `{ selector: 'foo', template: '<bar></bar>', standalone: true, imports: [BarComponent] }`,
+        ]
+      ]);
+    });
+
+    it('for a new NgModule-based component import', () => {
+      const standaloneFiles = {
+        'foo.ts': `
+         import {Component} from '@angular/core';
+         @Component({
+           selector: 'foo',
+           template: '<bar></bar>',
+           standalone: true
+         })
+         export class FooComponent {}
+         `,
+        'bar.ts': `
+         import {Component, NgModule} from '@angular/core';
+         @Component({
+           selector: 'bar',
+           template: '<div>bar</div>',
+         })
+         export class BarComponent {}
+         @NgModule({
+           declarations: [BarComponent],
+           exports: [BarComponent],
+           imports: []
+         })
+         export class BarModule {}
+         `,
+      };
+
+      const project = createModuleAndProjectWithDeclarations(env, 'test', {}, {}, standaloneFiles);
+      const diags = project.getDiagnosticsForFile('foo.ts');
+      const fixFile = project.openFile('foo.ts');
+      fixFile.moveCursorToText('<¦bar>');
+
+      const codeActions =
+          project.getCodeFixesAtPosition('foo.ts', fixFile.cursor, fixFile.cursor, [diags[0].code]);
+      const actionChanges = allChangesForCodeActions(fixFile.contents, codeActions);
+      actionChangesMatch(actionChanges, `Import BarModule from './bar' on FooComponent`, [
+        [
+          ``,
+          `import { BarModule } from "./bar";`,
+        ],
+        [
+          `{`,
+          `{ selector: 'foo', template: '<bar></bar>', standalone: true, imports: [BarModule] }`,
+        ]
+      ]);
+    });
+
+    it('for an import of a component onto an ngModule', () => {
+      const standaloneFiles = {
+        'foo.ts': `
+         import {Component, NgModule} from '@angular/core';
+         @Component({
+           selector: 'foo',
+           template: '<bar></bar>',
+         })
+         export class FooComponent {}
+         @NgModule({
+           declarations: [FooComponent],
+           exports: [],
+           imports: []
+         })
+         export class FooModule {}
+         `,
+        'bar.ts': `
+         import {Component} from '@angular/core';
+         @Component({
+           selector: 'bar',
+           template: '<div>bar</div>',
+           standalone: true,
+         })
+         export class BarComponent {}
+         `,
+      };
+
+      const project = createModuleAndProjectWithDeclarations(env, 'test', {}, {}, standaloneFiles);
+      const diags = project.getDiagnosticsForFile('foo.ts');
+      const fixFile = project.openFile('foo.ts');
+      fixFile.moveCursorToText('<¦bar>');
+
+      const codeActions =
+          project.getCodeFixesAtPosition('foo.ts', fixFile.cursor, fixFile.cursor, [diags[0].code]);
+      const actionChanges = allChangesForCodeActions(fixFile.contents, codeActions);
+      actionChangesMatch(actionChanges, `Import BarComponent from './bar' on FooModule`, [
+        [
+          ``,
+          `import { BarComponent } from "./bar";`,
+        ],
+        [
+          `{`,
+          `{ declarations: [FooComponent], exports: [], imports: [BarComponent] }`,
+        ]
+      ]);
     });
   });
 });
+
+type ActionChanges = {
+  [description: string]: Array<readonly[string, string]>
+};
+
+function actionChangesMatch(
+    actionChanges: ActionChanges, description: string,
+    substitutions: Array<readonly[string, string]>) {
+  expect(Object.keys(actionChanges)).toContain(description);
+  for (const substitution of substitutions) {
+    expect(actionChanges[description]).toContain([substitution[0], substitution[1]]);
+  }
+}
+
+// Returns the ActionChanges for all changes in the given code actions, collapsing whitespace into a
+// single space and trimming at the ends.
+function allChangesForCodeActions(
+    fileContents: string, codeActions: readonly CodeAction[]): ActionChanges {
+  // Replace all whitespace characters with a single space, then deduplicate spaces and trim.
+  const collapse = (s: string) => s.replace(/\s/g, ' ').replace(/\s{2,}/g, ' ').trim();
+  let allActionChanges: ActionChanges = {};
+  // For all code actions, construct a map from descriptions to [oldText, newText] pairs.
+  for (const action of codeActions) {
+    const actionChanges = action.changes.flatMap(change => {
+      return change.textChanges.map(tc => {
+        const oldText = collapse(fileContents.slice(tc.span.start, tc.span.start + spawn.length));
+        const newText = collapse(tc.newText);
+        return [oldText, newText] as const;
+      });
+    });
+    allActionChanges[collapse(action.description)] = actionChanges;
+  }
+  return allActionChanges;
+}
 
 function expectNotIncludeFixAllInfo(codeActions: readonly ts.CodeFixAction[]) {
   for (const codeAction of codeActions) {
@@ -296,22 +414,16 @@ function expectNotIncludeFixAllInfo(codeActions: readonly ts.CodeFixAction[]) {
  * check it.
  */
 function expectIncludeReplacementText(
-    {codeActions, content, text, newText, fileName, description, removeWhitespace = false}: {
+    {codeActions, content, text, newText, fileName, description}: {
       codeActions: readonly ts.CodeAction[]; content: string; text: string | null; newText: string;
       fileName: string;
       description?: string;
-      removeWhitespace?: boolean;
     }) {
   let includeReplacementText = false;
   for (const codeAction of codeActions) {
-    includeReplacementText = includeReplacementTextInChanges({
-                               fileTextChanges: codeAction.changes,
-                               content,
-                               text,
-                               newText,
-                               fileName,
-                               removeWhitespace
-                             }) &&
+    includeReplacementText =
+        includeReplacementTextInChanges(
+            {fileTextChanges: codeAction.changes, content, text, newText, fileName}) &&
         (description === undefined ? true : (description === codeAction.description));
     if (includeReplacementText) {
       return;
@@ -320,14 +432,13 @@ function expectIncludeReplacementText(
   expect(includeReplacementText).toBeTruthy();
 }
 
-function expectIncludeAddText({codeActions, position, text, fileName, removeWhitespace = false}: {
+function expectIncludeAddText({codeActions, position, text, fileName}: {
   codeActions: readonly ts.CodeAction[]; position: number | null; text: string; fileName: string;
-  removeWhitespace?: boolean;
 }) {
   let includeAddText = false;
   for (const codeAction of codeActions) {
-    includeAddText = includeAddTextInChanges(
-        {fileTextChanges: codeAction.changes, position, text, fileName, removeWhitespace});
+    includeAddText =
+        includeAddTextInChanges({fileTextChanges: codeAction.changes, position, text, fileName});
     if (includeAddText) {
       return;
     }
@@ -336,34 +447,26 @@ function expectIncludeAddText({codeActions, position, text, fileName, removeWhit
 }
 
 function expectIncludeReplacementTextForFileTextChange(
-    {fileTextChanges, content, text, newText, fileName, removeWhitespace = false}: {
+    {fileTextChanges, content, text, newText, fileName}: {
       fileTextChanges: readonly ts.FileTextChanges[]; content: string; text: string;
       newText: string;
       fileName: string;
-      removeWhitespace?: boolean;
     }) {
-  expect(includeReplacementTextInChanges(
-             {fileTextChanges, content, text, newText, fileName, removeWhitespace}))
+  expect(includeReplacementTextInChanges({fileTextChanges, content, text, newText, fileName}))
       .toBeTruthy();
 }
 
-function expectIncludeAddTextForFileTextChange(
-    {fileTextChanges, position, text, fileName, removeWhitespace = false}: {
-      fileTextChanges: readonly ts.FileTextChanges[]; position: number; text: string;
-      fileName: string;
-      removeWhitespace?: boolean;
-    }) {
-  expect(includeAddTextInChanges({fileTextChanges, position, text, fileName, removeWhitespace}))
-      .toBeTruthy();
+function expectIncludeAddTextForFileTextChange({fileTextChanges, position, text, fileName}: {
+  fileTextChanges: readonly ts.FileTextChanges[]; position: number; text: string; fileName: string;
+}) {
+  expect(includeAddTextInChanges({fileTextChanges, position, text, fileName})).toBeTruthy();
 }
 
-function includeReplacementTextInChanges(
-    {fileTextChanges, content, text, newText, fileName, removeWhitespace = false}: {
-      fileTextChanges: readonly ts.FileTextChanges[]; content: string; text: string | null;
-      newText: string;
-      fileName: string;
-      removeWhitespace?: boolean;
-    }) {
+function includeReplacementTextInChanges({fileTextChanges, content, text, newText, fileName}: {
+  fileTextChanges: readonly ts.FileTextChanges[]; content: string; text: string | null;
+  newText: string;
+  fileName: string;
+}) {
   for (const change of fileTextChanges) {
     if (!change.fileName.endsWith(fileName)) {
       continue;
@@ -374,9 +477,8 @@ function includeReplacementTextInChanges(
       }
       const textChangeOldText =
           content.slice(textChange.span.start, textChange.span.start + textChange.span.length);
-      let preprocess = removeWhitespace ? deleteWhitespace : (s: string) => s;
-      const oldTextMatches = text === null || (preprocess(textChangeOldText) === preprocess(text));
-      const newTextMatches = preprocess(newText) === preprocess(textChange.newText);
+      const oldTextMatches = text === null || textChangeOldText === text;
+      const newTextMatches = newText === textChange.newText;
       if (oldTextMatches && newTextMatches) {
         return true;
       }
@@ -385,12 +487,10 @@ function includeReplacementTextInChanges(
   return false;
 }
 
-function includeAddTextInChanges(
-    {fileTextChanges, position, text, fileName, removeWhitespace = false}: {
-      fileTextChanges: readonly ts.FileTextChanges[]; position: number | null; text: string;
-      fileName: string;
-      removeWhitespace?: boolean;
-    }) {
+function includeAddTextInChanges({fileTextChanges, position, text, fileName}: {
+  fileTextChanges: readonly ts.FileTextChanges[]; position: number | null; text: string;
+  fileName: string;
+}) {
   for (const change of fileTextChanges) {
     if (!change.fileName.endsWith(fileName)) {
       continue;
@@ -399,21 +499,12 @@ function includeAddTextInChanges(
       if (textChange.span.length > 0) {
         continue;
       }
-      let preprocess = removeWhitespace ? deleteWhitespace : (s: string) => s;
-      const includeAddText = (position === null || position === textChange.span.start) &&
-          preprocess(text) === preprocess(textChange.newText);
+      const includeAddText =
+          (position === null || position === textChange.span.start) && text === textChange.newText;
       if (includeAddText) {
         return true;
       }
     }
   }
   return false;
-}
-
-/**
- * For many test input files, there will be extra whitespace from the file formatting,
- * which causes unnecessarily fragile tests.
- */
-function deleteWhitespace(str: string) {
-  return str.replace(/\s/g, '');
 }
