7cec8768122cffd680a7be9d8d3a1ac22a89369b
Matthieu Riegler <kyro38@gmail.com>
2022-12-07 00:11:25 +0100

    refactor: cleanup polyfills by importing zone.js directly in angular.json (#48380)
    Since v15, polyfills can be passed directly into angular.json. Let's use this opportunity to reduce the number of unecessary files.
    
    PR Close #48380

--- integration/ivy-i18n/src/polyfills.ts
+++ integration/ivy-i18n/src/polyfills.ts
@@ -18,9 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-/** IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
@@ -45,7 +42,7 @@
  * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
  * (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
  *
- *  in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
+ *  in Edge developer tools, the addEventListener will also be wrapped by zone.js
  *  with the following flag, it will bypass `zone.js` patch for IE/Edge
  *
  *  (window as any).__Zone_enable_cross_context_check = true;
