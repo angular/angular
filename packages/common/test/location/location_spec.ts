134d8010ab3d3a672be87caf3b733520b5e9754c
Konstantin Kharitonov <constantant1@gmail.com>
2022-12-06 17:22:45 +0100

    fix(common): Update `Location` to support base href containing `origin`
    In case `APP_BASE_HREF` is set including `origin` the further usage of it might cause failure
    
    e.g.
    If an app is placed on `https://example.com` and bundles are on `https://cdn-example.com` you have to set `APP_BASE_HREF` up as `https://example.com/` and build the app with `--base-href` as `https://cdn-example.com/` but it does not work because of the bug
    
    Fixes #48175

--- packages/common/test/location/location_spec.ts
+++ packages/common/test/location/location_spec.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {CommonModule, Location, LocationStrategy, PathLocationStrategy, PlatformLocation} from '@angular/common';
+import {APP_BASE_HREF, CommonModule, Location, LocationStrategy, PathLocationStrategy, PlatformLocation} from '@angular/common';
 import {MockLocationStrategy, MockPlatformLocation} from '@angular/common/testing';
 import {TestBed} from '@angular/core/testing';
 
@@ -55,7 +55,7 @@ describe('Location Class', () => {
               return new MockPlatformLocation();
             }
           },
-          {provide: Location, useClass: Location, deps: [LocationStrategy, PlatformLocation]},
+          {provide: Location, useClass: Location, deps: [LocationStrategy]},
         ]
       });
 
@@ -147,7 +147,7 @@ describe('Location Class', () => {
               return new MockPlatformLocation();
             }
           },
-          {provide: Location, useClass: Location, deps: [LocationStrategy, PlatformLocation]},
+          {provide: Location, useClass: Location, deps: [LocationStrategy]},
         ]
       });
 
@@ -213,6 +213,57 @@ describe('Location Class', () => {
       locationStrategy.simulatePopState('/test');
 
       expect(notificationCount).toBe(1);
+    });
+  });
+
+  describe('location.normalize(url) should return only route', () => {
+    const basePath = '/en';
+    const route = '/go/to/there';
+    const url = basePath + route;
+    const getBaseHref = (origin: string) => origin + basePath + '/';
+
+    it('in case APP_BASE_HREF starts with http:', () => {
+      const origin = 'http://example.com';
+      const baseHref = getBaseHref(origin);
+
+      TestBed.configureTestingModule({providers: [{provide: APP_BASE_HREF, useValue: baseHref}]});
+
+      const location = TestBed.inject(Location);
+
+      expect(location.normalize(url)).toBe(route);
+    });
+
+    it('in case APP_BASE_HREF starts with https:', () => {
+      const origin = 'https://example.com';
+      const baseHref = getBaseHref(origin);
+
+      TestBed.configureTestingModule({providers: [{provide: APP_BASE_HREF, useValue: baseHref}]});
+
+      const location = TestBed.inject(Location);
+
+      expect(location.normalize(url)).toBe(route);
+    });
+
+    it('in case APP_BASE_HREF starts with no protocol', () => {
+      const origin = '//example.com';
+      const baseHref = getBaseHref(origin);
+
+      TestBed.configureTestingModule({providers: [{provide: APP_BASE_HREF, useValue: baseHref}]});
+
+      const location = TestBed.inject(Location);
+
+      expect(location.normalize(url)).toBe(route);
+    });
+
+    it('in case APP_BASE_HREF starts with no origin', () => {
+      const origin = '';
+      const baseHref = getBaseHref(origin);
+
+      TestBed.configureTestingModule({providers: [{provide: APP_BASE_HREF, useValue: baseHref}]});
+
+      const location = TestBed.inject(Location);
+
+      expect(location.normalize(url)).toBe(route);
     });
   });
 });
