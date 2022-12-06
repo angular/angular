bf44dc234a32069cb297ef7d3a87c2004cad8b00
Konstantin Kharitonov <constantant1@gmail.com>
2022-12-06 17:22:45 +0100

    fix(common): Update `Location` to support base href containing `origin` (#48327)
    In case `APP_BASE_HREF` is set including `origin` the further usage of it might cause failure
    
    e.g.
    If an app is placed on `https://example.com` and bundles are on `https://cdn-example.com` you have to set `APP_BASE_HREF` up as `https://example.com/` and build the app with `--base-href` as `https://cdn-example.com/` but it does not work because of the bug
    
    Fixes #48175
    
    PR Close #48327

--- packages/common/testing/src/location_mock.ts
+++ packages/common/testing/src/location_mock.ts
@@ -25,7 +25,7 @@ export class SpyLocation implements Location {
   /** @internal */
   _subject: EventEmitter<any> = new EventEmitter();
   /** @internal */
-  _baseHref: string = '';
+  _basePath: string = '';
   /** @internal */
   _locationStrategy: LocationStrategy = null!;
   /** @internal */
@@ -43,7 +43,7 @@ export class SpyLocation implements Location {
   }
 
   setBaseHref(url: string) {
-    this._baseHref = url;
+    this._basePath = url;
   }
 
   path(): string {
@@ -81,7 +81,7 @@ export class SpyLocation implements Location {
     if (url.length > 0 && !url.startsWith('/')) {
       url = '/' + url;
     }
-    return this._baseHref + url;
+    return this._basePath + url;
   }
 
   go(path: string, query: string = '', state: any = null) {
