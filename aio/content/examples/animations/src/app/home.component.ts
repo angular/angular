0b799220ca1bb8434f65b29e15cc3526e1ba80f8
piyush132000 <piyush132000@gmail.com>
2022-12-07 13:05:07 +0530

    docs: cleanup the components (#48382)
    Removed empty methods and import from files
    
    PR Close #48382

--- aio/content/examples/animations/src/app/home.component.ts
+++ aio/content/examples/animations/src/app/home.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class HomeComponent {}
--- aio/content/examples/lazy-loading-ngmodules/src/app/customers/customers.component.ts
+++ aio/content/examples/lazy-loading-ngmodules/src/app/customers/customers.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css']
 })
-export class CustomersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class CustomersComponent {}
