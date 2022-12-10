f268a0c9f09389e2b5972cac5978e5341e365014
piyush132000 <piyush132000@gmail.com>
2022-12-10 08:28:29 +0530

    docs: updated docs removed unnecessary imports and empty methods
    Removed empty methods and unnecessary imports in files

--- aio/content/examples/ngmodules/src/app/items/items.component.ts
+++ aio/content/examples/ngmodules/src/app/items/items.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
-export class ItemsComponent implements OnInit {
+export class ItemsComponent {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
