327aa7e28b2fac68d577b9624b2e6381270d263e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-13 17:31:50 +0000

    refactor: update compiler-cli babel linker to be ESM only
    Since the linker is no longer being tested with CommonJS, we can remove
    most of the CJS/ESM interop trickery.

--- packages/compiler-cli/linker/babel/src/babel_plugin.ts
+++ packages/compiler-cli/linker/babel/src/babel_plugin.ts
@@ -5,11 +5,12 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+import {ConfigAPI, PluginObj} from '@babel/core';
+
 import {NodeJSFileSystem} from '../../../src/ngtsc/file_system';
 import {ConsoleLogger, LogLevel} from '../../../src/ngtsc/logging';
 import {LinkerOptions} from '../../src/file_linker/linker_options';
 
-import {ConfigAPI, PluginObj} from './babel_core';
 import {createEs2015LinkerPlugin} from './es2015_linker_plugin';
 
 /**
