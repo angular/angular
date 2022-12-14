d3223010b0bc806d5784141ca9f4a02d02c695f9
Bob Watson <bobwatson@google.com>
2022-12-14 15:48:16 -0800

    docs: add first-app-lesson-10 example (#48512)
    PR Close #48512

---
+++ aio/content/examples/first-app-lesson-10/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  template: `
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    </section>
+  `,
+  styleUrls: ['./housing-location.component.css'],
+})
+
+export class HousingLocationComponent {
+
+  @Input() housingLocation!: HousingLocation;
+
+}
