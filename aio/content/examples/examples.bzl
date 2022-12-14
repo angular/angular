c34174f3bb4ed3ab96a731d4fd17d275bc348eb5
Bob Watson <bobwatson@google.com>
2022-12-14 15:08:32 -0800

    docs: add first-app-lesson-09 example (#48512)
    PR Close #48512

--- aio/content/examples/examples.bzl
+++ aio/content/examples/examples.bzl
@@ -51,6 +51,7 @@ EXAMPLES = {
     "first-app-lesson-06": {"stackblitz": True, "zip": True},
     "first-app-lesson-07": {"stackblitz": True, "zip": True},
     "first-app-lesson-08": {"stackblitz": True, "zip": True},
+    "first-app-lesson-09": {"stackblitz": True, "zip": True},
     "form-validation": {"stackblitz": True, "zip": True},
     "forms": {"stackblitz": True, "zip": True},
     "forms-overview": {"stackblitz": True, "zip": True},
---
+++ aio/content/examples/first-app-lesson-09/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { browser, element, by, logging } from 'protractor';
+
+describe('first-app-lesson-09 app', () => {
+
+  beforeEach(() => browser.get(''));
+
+  it('should display correct title', async () => {
+    expect(await element.all(by.css('.brand-logo')).get(0).getAttribute('src')).toContain('/assets/logo.svg');
+  });
+
+  it('should have a filter string input', async () => {
+    expect(await element.all(by.css('input')).get(0).getAttribute('placeholder')).toEqual('Filter by city');
+    expect(await element.all(by.css('input')).get(0).getAttribute('type')).toEqual('text');
+  });
+
+  it('should have a search button', async () => {
+    expect(await element.all(by.css('button')).get(0).getText()).toEqual('Search');
+  });
+
+  it('should have a housing-location component', async () => {
+    expect(await element.all(by.css('.listing > .listing-location')).get(0).getText()).toEqual('Chicago, IL');
+  });
+
+  it('should have a title in the housing-location component', async () => {
+    expect(await element.all(by.css('.listing > h2')).get(0).getText()).toEqual('Acme Fresh Start Housing');
+  });
+
+  it('should have 10 housing-location components', async () => {
+    expect(await element.all(by.css('app-housing-location'))['length'] === 10);
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
