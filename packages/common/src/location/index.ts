60f46aa677f75111ca2e97d4090b57d84af39831
Andrew Scott <atscott@google.com>
2022-12-14 08:04:43 -0800

    feat(common): Add BrowserPlatformLocation to the public API (#48488)
    `PlatformLocation` is already part of the public API so developers can
    create their own. This means that developers would already be able to
    access the existing `BrowserPlatformLocation` at runtime by injecting
    it. The motivation for adding `BrowserPlatformLocation` to the public
    API is because of those facts, but driven more by the fact that we are
    looking to include `MockPlatformLocation` by default in TestBed.
    Developers would need a way to revert back to the current behavior for
    some tests that rely directly on browser interaction.
    
    PR Close #48488

--- packages/common/src/location/index.ts
+++ packages/common/src/location/index.ts
@@ -9,4 +9,4 @@
 export {HashLocationStrategy} from './hash_location_strategy';
 export {Location, PopStateEvent} from './location';
 export {APP_BASE_HREF, LocationStrategy, PathLocationStrategy} from './location_strategy';
-export {LOCATION_INITIALIZED, LocationChangeEvent, LocationChangeListener, PlatformLocation} from './platform_location';
+export {BrowserPlatformLocation, LOCATION_INITIALIZED, LocationChangeEvent, LocationChangeListener, PlatformLocation} from './platform_location';
