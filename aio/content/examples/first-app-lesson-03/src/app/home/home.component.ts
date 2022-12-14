e51df686a59703417fcd0f8837f7afbe7f9b6c29
Bob Watson <bobwatson@google.com>
2022-12-14 08:12:42 -0800

    docs: add first-app-lesson-03 example (#48512)
    PR Close #48512

---
+++ aio/content/examples/first-app-lesson-03/src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
+})
+
+export class HomeComponent {
+
+}
