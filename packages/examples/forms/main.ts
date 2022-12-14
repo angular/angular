cd43584fe4d8b4b70c14d53664368f61da7e5a94
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-14 11:38:07 +0000

    refactor: switch `packages/examples` to ESM-compatible http server (#48521)
    * Switches all examples to use dev-infra's canonical ESM-compatible
      `http_server`.
    * Uses ESBuild for bundling ESM into a single file, compared to having
      to load hundreds of individual ESM files in the browser (potential
      source of flakiness & slowness).
    
    PR Close #48521

--- packages/examples/forms/main.ts
+++ packages/examples/forms/main.ts
@@ -6,6 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import 'zone.js/lib/browser/rollup-main';
+
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {TestsAppModule} from './test_module';
