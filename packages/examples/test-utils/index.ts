ca9b78a5b945ee6c62f40d4d78abbf90632544a1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-15 20:50:19 +0000

    build: remove remaining usages of deprecated `ts_devserver` (#48521)
    * Switches all remaining targets (even if not tested and failing as per
      build) away from `ts_devserver` to the canonical `http_server` from
      dev-infra.
    
    PR Close #48521

--- packages/examples/test-utils/index.ts
+++ packages/examples/test-utils/index.ts
@@ -19,19 +19,6 @@ export async function verifyNoBrowserErrors() {
   browserLog.forEach(logEntry => {
     const msg = logEntry.message;
 
-    // Since we currently use the `ts_devserver` from the Bazel TypeScript rules, which does
-    // fallback to the "index.html" file for HTML5 pushState routing but does always serve the
-    // expected fallback with a 404 status code, the browser will print a message about the 404,
-    // while the page loaded properly. Ideally the "ts_devserver" would allow us to opt-in for
-    // just returning a 200 status code, but the devserver is intended to be kept manually, so
-    // we manually filter this error before ensuring there are no console errors.
-    // TODO: This is a current limitation of using the "ts_devserver" with Angular routing.
-    // Tracked with: TOOL-629
-    if (msg.includes(
-            `Failed to load resource: the server responded with a status of 404 (Not Found)`)) {
-      return;
-    }
-
     console.log('>> ' + msg, logEntry);
 
     if (logEntry.level.value >= logging.Level.INFO.value) {
