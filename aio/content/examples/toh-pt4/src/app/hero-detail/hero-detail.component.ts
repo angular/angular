c308bc11535ebee1abc8500340350a4db43831e9
piyush132000 <piyush132000@gmail.com>
2022-12-20 08:33:53 +0530

    docs: updated docs removed unnecessary imports and empty methods
    Removed empty methods and unnecessary imports in files

--- aio/content/examples/toh-pt4/src/app/hero-detail/hero-detail.component.ts
+++ aio/content/examples/toh-pt4/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
@@ -6,13 +6,8 @@ import { Hero } from '../hero';
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
+export class HeroDetailComponent {
 
   @Input() hero: Hero | undefined;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
