43ced455d687c13ade2c5f61bf1ededad098c05a
Matthieu Riegler <kyro38@gmail.com>
2022-12-24 13:51:26 +0100

    docs: remove empty constructors & ngOnInit() (#48576)
    PR Close #48576

--- aio/content/examples/two-way-binding/src/app/app.component.ts
+++ aio/content/examples/two-way-binding/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor() { }
   // #docregion font-size
   fontSizePx = 16;
   // #enddocregion font-size
