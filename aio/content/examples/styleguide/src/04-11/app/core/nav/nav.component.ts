505d0bec2f419d56c424008f57f082bc4e76453f
piyush132000 <piyush132000@gmail.com>
2023-01-03 13:06:49 +0530

    docs: updated docs removed unnecessary imports and empty methods
    Removed empty methods and unnecessary imports in files

--- aio/content/examples/styleguide/src/04-11/app/core/nav/nav.component.ts
+++ aio/content/examples/styleguide/src/04-11/app/core/nav/nav.component.ts
@@ -1,15 +1,19 @@
 // #docregion
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'toh-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
   menuItems = [
     'Heroes',
     'Villains',
     'Other'
   ];
+
+  ngOnInit() {  }
+
+  constructor() { }
 }
