7c27eaa00a99b7059211d4a706729bfd588b04ec
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-04 21:09:58 +0000

    test(docs-infra): increase jasmine default timeout for e2e specs
    Similar to the Saucelabs job, the jasmine default timeout can be
    increased to avoid the common jasmine timeouts. We cannot control
    how fast Selenium e.g. loads a page or not.

---
+++ aio/tests/e2e/src/test-init.e2e-spec.ts
@@ -0,0 +1,8 @@
+/**
+ * @file Spec file loaded by Protractor. It is not guaranteed
+ *   to load before other spec files. Jasmine tests execute after all
+ *   files are loaded, so this file can be used for initialization.
+ */
+
+// Increase the timeout for specs as Selenium & Protractor is slow.
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
