9ed51d33f847a793c576ace5c9790b2f03eaa77c
Dylan Hunn <dylhunn@gmail.com>
2023-01-04 17:40:36 -0800

    feat(language-service): Introduce a new NgModuleIndex, and use it to suggest re-exports. (#48354)
    NgModules can re-export other NgModules, which transitively includes all traits exported by the re-exported NgModule. We want to be able to suggest *all* re-exports of a component when trying to auto-import it.
    
    Previously, we used an approximation when importing from NgModules: we looked at a Component's metadata, and just imported the declaring NgModule. However, this is not technically correct -- the declaring NgModule it is not necessarily the same one that exports it for the current scope. (Indeed, there could be multiple re-exports!) As a replacement, I have implemented a more general solution.
    
    This PR introduces a new class on the template type checker, called `NgModuleIndex`. When queried, it conducts a depth-first-search over the NgModule import/export graph, in order to find all NgModules anywhere in the current dependency graph, as well as all exports of those NgModules. This allows the language service to suggest all of the re-exports, in addition to the original export.
    
    PR Close #48354

--- packages/compiler-cli/src/ngtsc/typecheck/api/checker.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/api/checker.ts
@@ -149,8 +149,9 @@ export interface TemplateTypeChecker {
   /**
    * In the context of an Angular trait, generate potential imports for a directive.
    */
-  getPotentialImportsFor(directive: PotentialDirective, inComponent: ts.ClassDeclaration):
-      ReadonlyArray<PotentialImport>;
+  getPotentialImportsFor(
+      toImport: PotentialDirective|PotentialPipe,
+      inComponent: ts.ClassDeclaration): ReadonlyArray<PotentialImport>;
 
   /**
    * Get the primary decorator for an Angular class (such as @Component). This does not work for
