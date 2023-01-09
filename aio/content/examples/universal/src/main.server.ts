a29203ff1f2faf0a757d0de07bfcfefc69a66303
Alan Agius <alanagius@google.com>
2023-01-09 09:42:35 +0000

    docs: update universal example (#48665)
    This change updates the universal example to align with latest CLI changes.
    
    PR Close #48665

--- aio/content/examples/universal/src/main.server.ts
+++ aio/content/examples/universal/src/main.server.ts
@@ -1,9 +1,1 @@
-import { enableProdMode } from '@angular/core';
-
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
 export { AppServerModule } from './app/app.server.module';
