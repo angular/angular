128c4a604bb3eb70899f14647ff57716df9a6929
Alan Agius <alanagius@google.com>
2023-01-09 09:42:35 +0000

    docs: update universal example (#48665)
    This change updates the universal example to align with latest CLI changes.
    
    PR Close #48665

--- aio/content/examples/universal/server.ts
+++ aio/content/examples/universal/server.ts
@@ -1,15 +1,15 @@
 import 'zone.js/node';
 
+import { APP_BASE_HREF } from '@angular/common';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
+import { existsSync } from 'fs';
 import { join } from 'path';
 
 import { AppServerModule } from './src/main.server';
-import { APP_BASE_HREF } from '@angular/common';
-import { existsSync } from 'fs';
 
 // The Express app is exported so that it can be used by serverless Functions.
-export function app() {
+export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
@@ -48,8 +48,8 @@ export function app() {
   return server;
 }
 
-function run() {
-  const port = process.env.PORT || 4000;
+function run(): void {
+  const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
   const server = app();
