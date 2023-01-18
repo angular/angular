b839b5fc803114ab3fce8b2cf3740f0f06e9649b
Bob Watson <bobwatson@google.com>
2023-01-17 17:18:50 -0800

    docs: add first-app tutorial lesson-01

--- aio/content/examples/first-app-lesson-01/src/app/app.component.ts
+++ aio/content/examples/first-app-lesson-01/src/app/app.component.ts
@@ -4,9 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   imports: [],
+  // #docregion app-comp-template
   template: `<h1>Hello world!</h1>`,
+  // #enddocregion
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // #docregion app-comp-title
   title = 'homes';
+  // #enddocregion
 }
