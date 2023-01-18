6b35bf85d3e4423a536be8fda926346d4d7846f3
De Wildt <dewildtvr@gmail.com>
2023-01-17 21:05:41 -0500

    revert "fixup! fix(router): fix = not parsed in router segment name"
    This reverts commit 2279f4d4620eba083a9832ed096890b69a25ec42.
    
    Reverting that commit based off PR feedback that this change should only affect the parsing of sergments and node encoding of the url

--- packages/router/src/url_tree.ts
+++ packages/router/src/url_tree.ts
@@ -445,8 +445,7 @@ function encodeUriString(s: string): string {
       .replace(/%40/g, '@')
       .replace(/%3A/gi, ':')
       .replace(/%24/g, '$')
-      .replace(/%2C/gi, ',')
-      .replace(/%3D/gi, '=');
+      .replace(/%2C/gi, ',');
 }
 
 /**
