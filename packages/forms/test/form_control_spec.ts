d4926f386145b457deb081de56c746e59ae595a7
Matthieu Riegler <kyro38@gmail.com>
2023-02-01 23:27:06 +0100

    refactor: remove todos on forms tests (#48894)
    Removing the todos implied using fakeAsync and passing an async validator as async (3rd parameter).
    
    PR Close #48894

--- packages/forms/test/form_control_spec.ts
+++ packages/forms/test/form_control_spec.ts
@@ -1131,26 +1131,24 @@ describe('FormControl', () => {
          ]);
        }));
 
-    // TODO: remove the if statement after making observable delivery sync
-    it('should update set errors and status before emitting an event', done => {
-      c.valueChanges.subscribe(() => {
-        expect(c.valid).toEqual(false);
-        expect(c.errors).toEqual({'required': true});
-        done();
-      });
-      c.setValue('');
-    });
+    it('should update set errors and status before emitting an event', fakeAsync(() => {
+         c.setValue('');
 
-    it('should return a cold observable', done => {
-      c.setValue('will be ignored');
-      c.valueChanges.subscribe({
-        next: (value: any) => {
-          expect(value).toEqual('new');
-          done();
-        }
-      });
-      c.setValue('new');
-    });
+         tick();
+         expect(c.valid).toEqual(false);
+         expect(c.errors).toEqual({'required': true});
+       }));
+
+    it('should return a cold observable', fakeAsync(() => {
+         let value: string|null = null;
+         c.setValue('will be ignored');
+         c.valueChanges.subscribe((v) => value = v);
+         c.setValue('new');
+         tick();
+
+         // @ts-expect-error see microsoft/TypeScript#9998
+         expect(value).toEqual('new');
+       }));
   });
 
   describe('setErrors', () => {
