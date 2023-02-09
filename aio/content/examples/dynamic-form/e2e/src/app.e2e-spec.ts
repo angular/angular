31ce526c19c0b147d1e86962b9c50c3fbf39d92f
an-gu99 <andreas.guggenberger@kabelmail.de>
2023-02-09 17:15:36 +0100

    fix(docs-infra): default type attribute of input element to 'text' instead of '' and overshadow global CSS rule for input[type='text']
    missing value for type attribute seems unintended but only defaulting it to 'text' without changing the CSS would have messed up the style for this example
    
    Fixes #48895

--- aio/content/examples/dynamic-form/e2e/src/app.e2e-spec.ts
+++ aio/content/examples/dynamic-form/e2e/src/app.e2e-spec.ts
@@ -1,21 +1,34 @@
-import { browser, element, by } from 'protractor';
+import {browser, by, element} from 'protractor';
 
 describe('Dynamic Form', () => {
+  beforeAll(() => browser.get(''));
 
-    beforeAll(() => browser.get(''));
+  it('should submit form', async () => {
+    const firstNameElement = element.all(by.css('input[id=firstName]')).get(0);
+    expect(await firstNameElement.getAttribute('value')).toEqual('Bombasto');
 
-    it('should submit form', async () => {
-      const firstNameElement = element.all(by.css('input[id=firstName]')).get(0);
-      expect(await firstNameElement.getAttribute('value')).toEqual('Bombasto');
+    const emailElement = element.all(by.css('input[id=emailAddress]')).get(0);
+    const email = 'test@test.com';
+    await emailElement.sendKeys(email);
+    expect(await emailElement.getAttribute('value')).toEqual(email);
 
-      const emailElement = element.all(by.css('input[id=emailAddress]')).get(0);
-      const email = 'test@test.com';
-      await emailElement.sendKeys(email);
-      expect(await emailElement.getAttribute('value')).toEqual(email);
-
-      await element(by.css('select option[value="solid"]')).click();
-      await element.all(by.css('button')).get(0).click();
-      expect(await element(by.cssContainingText('strong', 'Saved the following values')).isPresent()).toBe(true);
+    await element(by.css('select option[value="solid"]')).click();
+    await element.all(by.css('button')).get(0).click();
+    expect(await element(by.cssContainingText('strong', 'Saved the following values')).isPresent())
+        .toBe(true);
   });
 
+  it('should have both input fields with the same size', async () => {
+    const firstNameElement = element.all(by.css('input[id=firstName]')).get(0);
+    expect(await firstNameElement.getAttribute('type')).toEqual('text');
+    const widthFirstNameElement = await firstNameElement.getCssValue('width');
+    const paddingFirstNameElement = await firstNameElement.getCssValue('padding');
+
+    const emailElement = element.all(by.css('input[id=emailAddress]')).get(0);
+    const widthEmailElement = await emailElement.getCssValue('width');
+    const paddingEmailElement = await emailElement.getCssValue('padding');
+
+    expect(widthFirstNameElement).toEqual(widthEmailElement);
+    expect(paddingFirstNameElement).toEqual(paddingEmailElement);
+  });
 });
--- aio/content/examples/dynamic-form/src/app/question-textbox.ts
+++ aio/content/examples/dynamic-form/src/app/question-textbox.ts
@@ -3,4 +3,5 @@ import { QuestionBase } from './question-base';
 
 export class TextboxQuestion extends QuestionBase<string> {
   override controlType = 'textbox';
+  override type: string = this.type !== '' ? this.type : 'text';
 }
