31a71a0f8942486a114bc7875a8a6ba9c5c3872a
Matthieu Riegler <kyro38@gmail.com>
2023-02-12 21:59:01 +0100

    refactor: remove unnecessary file (#49042)
    view had a single export from a file located in the same package, no need to keep it.
    
    PR Close #49042

--- packages/core/src/di/provider_collection.ts
+++ packages/core/src/di/provider_collection.ts
@@ -13,9 +13,9 @@ import {getFactoryDef} from '../render3/definition_factory';
 import {throwCyclicDependencyError, throwInvalidProviderError} from '../render3/errors_di';
 import {stringifyForError} from '../render3/util/stringify_utils';
 import {deepForEach} from '../util/array_utils';
+import {EMPTY_ARRAY} from '../util/empty';
 import {getClosureSafeProperty} from '../util/property';
 import {stringify} from '../util/stringify';
-import {EMPTY_ARRAY} from '../view';
 
 import {resolveForwardRef} from './forward_ref';
 import {ENVIRONMENT_INITIALIZER} from './initializer_token';
