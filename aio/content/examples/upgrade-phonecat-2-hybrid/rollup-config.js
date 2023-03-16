842d569a9471937b89f8f20f130356c926f4697b
Alan Agius <alanagius@google.com>
2023-03-16 10:05:31 +0000

    feat(bazel): (APF) Angular Package Format updates (#49332)
    Several updates to Angular Package Format.
    
    BREAKING CHANGE:
    
    Several changes to the Angular Package Format (APF)
    - Removal of FESM2015
    - Replacing ES2020 with ES2022
    - Replacing FESM2020 with FESM2022
    
    PR Close #49332

--- aio/content/examples/upgrade-phonecat-2-hybrid/rollup-config.js
+++ aio/content/examples/upgrade-phonecat-2-hybrid/rollup-config.js
@@ -12,6 +12,9 @@ export default {
     sourcemap: true,
     sourcemapFile: 'aot/dist/build.js.map'
   },
+  // Rollup treeshaking has issues with https://github.com/angular/angular/blob/addd7f6249d54e258109f139fad1db0d0250352c/packages/core/src/linker/query_list.ts#L192
+  // see: https://github.com/rollup/rollup/issues/4895
+  treeshake: false,
   plugins: [
     nodeResolve(),
     commonjs({
