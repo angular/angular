439bbf4ad9b2f70c169775751faa63464ac07f44
Keith Li <hereiskeith@gmail.com>
2023-03-19 23:50:53 -0400

    feat(devtools): Display getters and setters in devtools property viewer (#49695)
    Display the function representations of get/set properties in the property viewer just like the existing UI for function properties
    
    PR Close #49695

--- devtools/projects/ng-devtools-backend/src/lib/state-serializer/object-utils.ts
+++ devtools/projects/ng-devtools-backend/src/lib/state-serializer/object-utils.ts
@@ -6,8 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-// Intentionally do not include the prototype because it contains
-// inherited methods (hasOwnProperty, etc.). Also ignore symbols
+// Intentionally do not include all the prototype (Except for getters and setters)
+// because it contains inherited methods (hasOwnProperty, etc.). Also ignore symbols
 // because it is tricky to pass a path to a symbol.
 //
 // We'd have to go through a serialization and deserialization logic
@@ -16,5 +16,29 @@ export const getKeys = (obj: {}): string[] => {
   if (!obj) {
     return [];
   }
-  return Object.getOwnPropertyNames(obj);
+  const properties = Object.getOwnPropertyNames(obj);
+
+  const prototypeMembers = Object.getOwnPropertyDescriptors(Object.getPrototypeOf(obj));
+
+  const ignoreList = ['__proto__'];
+  const gettersAndSetters = Object.keys(prototypeMembers).filter(methodName => {
+    if (ignoreList.includes(methodName)) {
+      return false;
+    }
+    const targetMethod = prototypeMembers[methodName];
+
+    return targetMethod.get || targetMethod.set;
+  });
+
+  return properties.concat(gettersAndSetters);
 };
+
+/**
+ * This helper function covers the common scenario as well as the getters and setters
+ * @param instance The target object
+ * @param propName The string representation of the target property name
+ * @returns The Descriptor object of the property
+ */
+export const getDescriptor = (instance: any, propName: string) =>
+    Object.getOwnPropertyDescriptor(instance, propName) ||
+    Object.getOwnPropertyDescriptor(Object.getPrototypeOf(instance), propName);
