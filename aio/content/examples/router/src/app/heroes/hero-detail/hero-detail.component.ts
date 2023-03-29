7cd12bf25cdaf8540f6bc9afef3f004a80d7ab63
Andrew Scott <atscott@google.com>
2023-03-29 15:45:05 -0700

    fixup! docs(router): Update common tasks guide to use input binding instead of ActivatedRoute

--- aio/content/examples/router/src/app/heroes/hero-detail/hero-detail.component.ts
+++ aio/content/examples/router/src/app/heroes/hero-detail/hero-detail.component.ts
@@ -1,14 +1,12 @@
 // #docplaster
 // #docregion
-import { switchMap } from 'rxjs/operators';
-import { Component, OnInit } from '@angular/core';
-// #docregion imports-route-info
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-// #enddocregion imports-route-info
-import { Observable } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {switchMap} from 'rxjs/operators';
 
-import { HeroService } from '../hero.service';
-import { Hero } from '../hero';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -18,22 +16,13 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
   hero$!: Observable<Hero>;
 
-  // #docregion activated-route
-  constructor(
-    private route: ActivatedRoute,
-  // #enddocregion activated-route
-    private router: Router,
-    private service: HeroService
-  // #docregion activated-route
-  ) {}
-  // #enddocregion activated-route
+  constructor(private route: ActivatedRoute, private router: Router, private service: HeroService) {
+  }
 
 
   ngOnInit() {
     this.hero$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) =>
-        this.service.getHero(params.get('id')!))
-    );
+        switchMap((params: ParamMap) => this.service.getHero(params.get('id')!)));
   }
 
   // #docregion redirect
@@ -42,7 +31,7 @@ export class HeroDetailComponent implements OnInit {
     // Pass along the hero id if available
     // so that the HeroList component can select that hero.
     // Include a junk 'foo' property for fun.
-    this.router.navigate(['/superheroes', { id: heroId, foo: 'foo' }]);
+    this.router.navigate(['/superheroes', {id: heroId, foo: 'foo'}]);
   }
   // #enddocregion redirect
 }
