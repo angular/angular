angular/angular

commit 07a1aa300404969155ed1eb3cd02f4a766e07963

feat(forms): Improve typings form (async)Validators (#48679)
With this commit, AsyncValidatorFn cannot be passed as ValidatorFn  anymore in FormControl.

fixes: #48676

PR Close #48679

packages/forms/src/directives/validators.ts
@@ -482,10 +482,13 @@ export class EmailValidator extends AbstractValidatorDirective {
  * A function that receives a control and synchronously returns a map of
  * validation errors if present, otherwise null.
  *
+ * Error objects with a `then` key or a `subscribe` key are not valid.
+ * They describe an async validator returning respectively a Promise or an Observable.
+ *
  * @publicApi
  */
 export interface ValidatorFn {
-  (control: AbstractControl): ValidationErrors|null;
+  (control: AbstractControl): (ValidationErrors&{then?: never, subscribe?: never})|null;
 }
 
 /**

packages/forms/src/form_builder.ts
@@ -8,7 +8,7 @@
 
 import {inject, Injectable} from '@angular/core';
 
-import {AsyncValidatorFn, ValidatorFn} from './directives/validators';
+import {AsyncValidatorFn, ValidationErrors, ValidatorFn} from './directives/validators';
 import {AbstractControl, AbstractControlOptions, FormHooks} from './model/abstract_model';
 import {FormArray, UntypedFormArray} from './model/form_array';
 import {FormControl, FormControlOptions, FormControlState, UntypedFormControl} from './model/form_control';
@@ -22,10 +22,17 @@ function isAbstractControlOptions(options: AbstractControlOptions|{[key: string]
        (options as AbstractControlOptions).updateOn !== undefined);
 }
 
+// Sometimes we might need this base signature for ValidatorFn because of compiler limitations
+// see https://github.com/microsoft/TypeScript/issues/32608
+export interface UnsafeValidatorFn {
+  (control: AbstractControl): ValidationErrors|null;
+}
+
 /**
  * The union of all validator types that can be accepted by a ControlConfig.
  */
-type ValidatorConfig = ValidatorFn|AsyncValidatorFn|ValidatorFn[]|AsyncValidatorFn[];
+
+type ValidatorConfig = UnsafeValidatorFn|AsyncValidatorFn|UnsafeValidatorFn[]|AsyncValidatorFn[];
 
 /**
  * The compiler may not always be able to prove that the elements of the control config are a tuple

packages/forms/test/form_control_spec.ts
@@ -398,6 +398,15 @@ describe('FormControl', () => {
          expect(c.hasValidator(minValidator)).toEqual(true);
          expect(c.hasValidator(Validators.min(5))).toEqual(false);
        });
+
+    it('should error if passed an async validator instead of a validator', fakeAsync(() => {
+         // @ts-expect-error
+         const c = new FormControl('value', asyncValidator('expected'));
+         tick();
+
+         expect(c.valid).toEqual(false);
+         expect(c.errors).not.toEqual({'async': true});
+       }));
   });
 
   describe('asyncValidator', () => {
@@ -590,16 +599,16 @@ describe('FormControl', () => {
        }));
 
     it('should clear async validators', fakeAsync(() => {
-         const c = new FormControl('value', [asyncValidator('expected'), otherAsyncValidator]);
+         const c = new FormControl('value', [], [asyncValidator('expected'), otherAsyncValidator]);
 
-         c.clearValidators();
+         c.clearAsyncValidators();
 
          expect(c.asyncValidator).toEqual(null);
        }));
 
     it('should not change validity state if control is disabled while async validating',
        fakeAsync(() => {
-         const c = new FormControl('value', [asyncValidator('expected')]);
+         const c = new FormControl('value', [], [asyncValidator('expected')]);
          c.disable();
          tick();
          expect(c.status).toEqual('DISABLED');

packages/forms/test/form_group_spec.ts
@@ -816,7 +816,7 @@ describe('FormGroup', () => {
     let group: FormGroup;
 
     beforeEach(waitForAsync(() => {
-      control = new FormControl('', null, asyncValidatorReturningObservable);
+      control = new FormControl('', [], asyncValidatorReturningObservable);
       group = new FormGroup({'one': control});
     }));
 

commit b18dd0c9681b6dec7406ee07bdc4f0d966fd57fa

refactor(forms): Add a test that uses ControlConfig with a non-static validator.
Previously, this PR cleaned up a bug introduced by #48679. However, since that PR needed to be rolled back, this PR now just checks in the test, to prevent that issue from re-occurring in the future.

packages/forms/test/typed_integration_spec.ts
@@ -1105,6 +1105,15 @@ describe('Typed Class', () => {
           let t1 = c3.controls;
           t1 = null as unknown as ControlsType;
         }
+
+        const c4 = fb.group({foo: [[1, 2, 3], Validators.maxLength(4)]});
+        {
+          type ControlsType = {foo: FormControl<number[]|null>};
+          let t: ControlsType = c4.controls;
+          let t1 = c4.controls;
+          t1 = null as unknown as ControlsType;
+        }
+        expect(c4.controls.foo.value).toEqual([1, 2, 3]);
       });
 
       it('from objects with ControlConfigs and validator lists', () => {