54a3eb401f9da081151aa337230ca63cea560b2c
Matthieu Riegler <kyro38@gmail.com>
2023-04-08 01:12:33 +0200

    ci: Remove environment files and EnableProdMode on integration tests
    Since v15, prod mode is handled by the CLI with the `optimization` flag. We can remove the environement files and `enableProdMode` when the tests use the CLI.

--- integration/cli-elements-universal/src/main.server.ts
+++ integration/cli-elements-universal/src/main.server.ts
@@ -1,10 +1,3 @@
 import '@angular/platform-server/init';
-import { enableProdMode } from '@angular/core';
-
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 export { AppServerModule } from './app/app.server.module';
--- integration/cli-hello-world-lazy/src/main.ts
+++ integration/cli-hello-world-lazy/src/main.ts
@@ -1,14 +1,8 @@
-import {enableProdMode} from '@angular/core';
 import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
 import {provideRouter} from '@angular/router';
 
 import {AppComponent} from './app/app.component';
 import {appRoutes} from './app/app.routes';
-import {environment} from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
   providers: [
