c2f270cf147e6434957e3b259ddc54a54a2a590e
Matthieu Riegler <kyro38@gmail.com>
2023-04-16 11:14:56 +0200

    refactor(compiler): Delete the @angular/compiler/testing module. (#49872)
    The package was removed in components, it can now be deleted here also.
    
    PR Close #49872

--- aio/content/examples/http/src/browser-test-shim.js
+++ aio/content/examples/http/src/browser-test-shim.js
@@ -25,7 +25,6 @@
       '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
       '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
       '@angular/common/http/testing': 'npm:@angular/common/bundles/common-http-testing.umd.js',
-      '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
       '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
       '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
       '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
