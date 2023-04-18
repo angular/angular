c029c678d9587d3bbeeb60720b226c83ec52bedf
skrtheboss <denisfrenademetz97@gmail.com>
2023-04-18 11:37:04 +0200

    fix(core): ensure takeUntilDestroyed unregisters onDestroy listener on unsubscribe (#49901)
    The takeUntilDestroyed must always remove the onDestroy listener,
    in the teardown logic.
    
    PR Close #49901

--- packages/core/rxjs-interop/src/take_until_destroyed.ts
+++ packages/core/rxjs-interop/src/take_until_destroyed.ts
@@ -27,7 +27,8 @@ export function takeUntilDestroyed<T>(destroyRef?: DestroyRef): MonoTypeOperator
   }
 
   const destroyed$ = new Observable<void>(observer => {
-    destroyRef!.onDestroy(observer.next.bind(observer));
+    const unregisterFn = destroyRef!.onDestroy(observer.next.bind(observer));
+    return unregisterFn;
   });
 
   return <T>(source: Observable<T>) => {
