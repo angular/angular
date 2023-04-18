d3a55291e9098d316bdb2daa409bd5602d89f267
Mark Thompson <2554588+MarkTechson@users.noreply.github.com>
2023-04-18 15:03:28 -0700

    docs: update first-app lesson 12 (#49980)
    Add content for tutorial lesson 12 and update the code examples.
    
    PR Close #49980

--- aio/content/examples/first-app-lesson-12/src/app/details/details.component.ts
+++ aio/content/examples/first-app-lesson-12/src/app/details/details.component.ts
@@ -70,8 +70,11 @@ export class DetailsComponent {
   }
 // #docregion form-submit
   submitApplication() {
-    const { firstName, lastName, email } = this.applyForm.value;
-      this.housingService.submitApplication(firstName ?? '', lastName ?? '', email ?? '');
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 // #enddocregion
 }
