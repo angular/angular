38661a6f2b7aeebf6a17b000e78ded7ca8dc7f0e
Alan Agius <alanagius@google.com>
2023-04-19 08:36:49 +0000

    test: update platform-server tests to test both ngmodule and standalone application (#49927)
    This commits update the platform-server test structure to add 2 applications in the Angular CLI workspace one based on ngmodules and another standalone. The same E2E suit is ran on both apps.
    
    PR Close #49927

---
+++ integration/platform-server/projects/standalone/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: '<router-outlet></router-outlet>',
+
+})
+export class AppComponent {
+}
