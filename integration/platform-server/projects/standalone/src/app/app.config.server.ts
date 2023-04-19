b5045bbf8dbd3e99fb67cef6c2cfeb2919cbc7cb
Alan Agius <alanagius@google.com>
2023-04-19 08:36:49 +0000

    test: update platform-server tests to test both ngmodule and standalone application
    This commits update the platform-server test structure to add 2 applications in the Angular CLI workspace one based on ngmodules and another standalone. The same E2E suit is ran on both apps.

---
+++ integration/platform-server/projects/standalone/src/app/app.config.server.ts
@@ -0,0 +1,11 @@
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
+import { appConfig } from './app.config';
+
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering()
+  ]
+};
+
+export const config = mergeApplicationConfig(appConfig, serverConfig);
