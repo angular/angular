207308e2031a257b063bfcc50d8e3ec16086ed5d
Joey Perrott <josephperrott@gmail.com>
2023-04-24 13:16:26 +0000

    ci: update gcp token
    Update to the new gcp token after rotation

--- integration/side-effects/snapshots/core/esm2015.js
+++ integration/side-effects/snapshots/core/esm2015.js
@@ -2,16 +2,16 @@ import "rxjs";
 
 import "rxjs/operators";
 
-const __globalThis = "undefined" !== typeof globalThis && globalThis;
+const __globalThis = typeof globalThis !== "undefined" && globalThis;
 
-const __window = "undefined" !== typeof window && window;
+const __window = typeof window !== "undefined" && window;
 
-const __self = "undefined" !== typeof self && "undefined" !== typeof WorkerGlobalScope && self instanceof WorkerGlobalScope && self;
+const __self = typeof self !== "undefined" && typeof WorkerGlobalScope !== "undefined" && self instanceof WorkerGlobalScope && self;
 
-const __global = "undefined" !== typeof global && global;
+const __global = typeof global !== "undefined" && global;
 
 const _global = __globalThis || __global || __window || __self;
 
-if ("undefined" !== typeof ngDevMode && ngDevMode) _global.$localize = _global.$localize || function() {
+if (typeof ngDevMode !== "undefined" && ngDevMode) _global.$localize = _global.$localize || function() {
     throw new Error("It looks like your application or one of its dependencies is using i18n.\n" + "Angular 9 introduced a global `$localize()` function that needs to be loaded.\n" + "Please run `ng add @angular/localize` from the Angular CLI.\n" + "(For non-CLI projects, add `import '@angular/localize/init';` to your `polyfills.ts` file.\n" + "For server-side rendering applications add the import to your `main.server.ts` file.)");
 };
