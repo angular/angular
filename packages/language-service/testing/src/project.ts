ce00738f9898527a6da2cd577a469e2683c42eff
Andrew Scott <atscott@google.com>
2023-04-27 09:31:07 -0700

    fix(compiler-cli): catch fatal diagnostic when getting diagnostics for components (#50046)
    This commit adds similar handling to what was done in https://github.com/angular/angular/commit/ed817e32fe0239c0f08ce342c7ad224055d56f84.
    The language service calls the `getDiagnosticsForComponent` function
    when the file is not a typescript file.
    
    fixes https://github.com/angular/vscode-ng-language-service/issues/1881
    
    PR Close #50046

--- packages/language-service/testing/src/project.ts
+++ packages/language-service/testing/src/project.ts
@@ -21,7 +21,7 @@ export type ProjectFiles = {
 
 function writeTsconfig(
     fs: FileSystem, tsConfigPath: AbsoluteFsPath, entryFiles: AbsoluteFsPath[],
-    options: TestableOptions): void {
+    angularCompilerOptions: TestableOptions, tsCompilerOptions: {}): void {
   fs.writeFile(
       tsConfigPath,
       JSON.stringify(
@@ -36,11 +36,12 @@ function writeTsconfig(
                 'dom',
                 'es2015',
               ],
+              ...tsCompilerOptions,
             },
             files: entryFiles,
             angularCompilerOptions: {
               strictTemplates: true,
-              ...options,
+              ...angularCompilerOptions,
             }
           },
           null, 2));
@@ -57,7 +58,7 @@ export class Project {
 
   static initialize(
       projectName: string, projectService: ts.server.ProjectService, files: ProjectFiles,
-      options: TestableOptions = {}): Project {
+      angularCompilerOptions: TestableOptions = {}, tsCompilerOptions = {}): Project {
     const fs = getFileSystem();
     const tsConfigPath = absoluteFrom(`/${projectName}/tsconfig.json`);
 
@@ -73,7 +74,7 @@ export class Project {
       }
     }
 
-    writeTsconfig(fs, tsConfigPath, entryFiles, options);
+    writeTsconfig(fs, tsConfigPath, entryFiles, angularCompilerOptions, tsCompilerOptions);
 
     // Ensure the project is live in the ProjectService.
     projectService.openClientFile(entryFiles[0]);
