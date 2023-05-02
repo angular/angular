6a0ebc6fa33c2d064806ddcf8f78f63bcd812377
Matthieu Riegler <kyro38@gmail.com>
2023-05-02 21:43:55 +0200

    docs(animations): Make links out of @see tags
    This commit is part of the work for #50097 to improve the linking on the online documentation.

--- packages/animations/src/animation_metadata.ts
+++ packages/animations/src/animation_metadata.ts
@@ -20,7 +20,7 @@ export type ɵStyleDataMap = Map<string, string|number>;
 
 /**
  * Represents animation-step timing parameters for an animation step.
- * @see `animate()`
+ * @see {@link animate}
  *
  * @publicApi
  */
@@ -84,7 +84,7 @@ export declare interface AnimationOptions {
 /**
  * Adds duration options to control animation styling and timing for a child animation.
  *
- * @see `animateChild()`
+ * @see {@link animateChild}
  *
  * @publicApi
  */
