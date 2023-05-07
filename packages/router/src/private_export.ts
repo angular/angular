4e99439e3fe3ad8a3380b888d9ad4507637e69e4
Matthieu Riegler <kyro38@gmail.com>
2023-05-07 17:35:28 +0200

    refactor(router): remove private export of `withPreloading` (#50194)
    `withPreloading` is part of the public API.
    
    PR Close #50194

--- packages/router/src/private_export.ts
+++ packages/router/src/private_export.ts
@@ -9,6 +9,5 @@
 
 export {ɵEmptyOutletComponent} from './components/empty_outlet';
 export {RestoredState as ɵRestoredState} from './navigation_transition';
-export {withPreloading as ɵwithPreloading} from './provide_router';
 export {ROUTER_PROVIDERS as ɵROUTER_PROVIDERS} from './router_module';
 export {afterNextNavigation as ɵafterNextNavigation} from './utils/navigations';
