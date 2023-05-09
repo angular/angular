cc286be7af31c7f999d6c469d3fd8f9becb28ce7
Matthieu Riegler <kyro38@gmail.com>
2023-05-09 10:40:57 +0200

    docs(docs-infra): Remove warning for `@Annotation`. (#50218)
    Per #50206, `@Annotation` is needed for tsickle. This commit removes the warning "Invalid tags found" produced by dgeni for the annotation decorator.
    
    PR Close #50218

---
+++ aio/tools/transforms/angular-api-package/tag-defs/annotation.js
@@ -0,0 +1,5 @@
+// @Annotation is load-bearing for tsickle's decorator downleveling transformation
+// We must provide a tag-def for it or the doc-gen will produce a warning.
+module.exports = function() {
+  return {name: 'Annotation'};
+};
