7624701daca02b60b72ba53f8ce5cab896ec34f5
gdarnell <12160067+garrettld@users.noreply.github.com>
2023-05-11 08:46:30 -0400

    fixup! fix(core): allow onDestroy unregistration while destroying

--- packages/core/test/acceptance/environment_injector_spec.ts
+++ packages/core/test/acceptance/environment_injector_spec.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {Component, createComponent, createEnvironmentInjector, ENVIRONMENT_INITIALIZER, EnvironmentInjector, inject, InjectFlags, InjectionToken, INJECTOR, Injector, NgModuleRef, ViewContainerRef} from '@angular/core';
+import {Component, createComponent, createEnvironmentInjector, DestroyRef, ENVIRONMENT_INITIALIZER, EnvironmentInjector, inject, InjectFlags, InjectionToken, INJECTOR, Injector, NgModuleRef, ViewContainerRef} from '@angular/core';
 import {R3Injector} from '@angular/core/src/di/r3_injector';
 import {RuntimeError, RuntimeErrorCode} from '@angular/core/src/errors';
 import {TestBed} from '@angular/core/testing';
@@ -25,6 +25,27 @@ describe('environment injector', () => {
 
     envInjector.destroy();
     expect(destroyed).toBeTrue();
+  });
+
+  it('should allow unregistration while destroying', () => {
+    const destroyedLog: string[] = [];
+
+    const parentEnvInjector = TestBed.inject(EnvironmentInjector);
+    const envInjector = createEnvironmentInjector([], parentEnvInjector);
+    const destroyRef = envInjector.get(DestroyRef);
+
+    const unregister = destroyRef.onDestroy(() => {
+      destroyedLog.push('first');
+      unregister();
+    });
+    destroyRef.onDestroy(() => {
+      destroyedLog.push('second');
+    });
+
+    expect(destroyedLog).toEqual([]);
+
+    envInjector.destroy();
+    expect(destroyedLog).toEqual(['first', 'second']);
   });
 
   it('should see providers from a parent EnvInjector', () => {
