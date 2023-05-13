63f5dfdf52bf7e18b789f948303cc48950254c04
Matthieu Riegler <kyro38@gmail.com>
2023-05-13 12:55:40 +0200

    docs: fix inconsistencies in getting started (#50275)
    Fixes #50274
    
    PR Close #50275

--- aio/content/examples/getting-started-v0/src/app/product-list/product-list.component.ts
+++ aio/content/examples/getting-started-v0/src/app/product-list/product-list.component.ts
@@ -8,7 +8,7 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = products;
+  products = [...products];
 
   share() {
     window.alert('The product has been shared!');
