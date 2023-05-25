0caee5ea7b466eff706296557024a1d630184c05
alkavats1 <alka.vats@nashtechglobal.com>
2023-05-26 00:25:44 +0530

    docs: correcting existing import for the commonHttpTesting (#50473)
    PR Close #50473

--- integration/typings_test_ts50/include-all.ts
+++ integration/typings_test_ts50/include-all.ts
@@ -14,7 +14,7 @@ import * as animationsBrowserTesting from '@angular/animations/browser/testing';
 import * as common from '@angular/common';
 import * as commonHttp from '@angular/common/http';
 import * as commonTesting from '@angular/common/testing';
-import * as commonHttpTesting from '@angular/common/testing';
+import * as commonHttpTesting from '@angular/common/http/testing';
 import * as compiler from '@angular/compiler';
 import * as compilerTesting from '@angular/compiler/testing';
 import * as core from '@angular/core';
