e0dae0abbbd3e20f08d651108d2aee4361b737e6
Matthieu Riegler <kyro38@gmail.com>
2023-05-27 15:48:25 +0200

    docs(docs-infra): rimraf requires the globflag. (#50493)
    On windows rimraf requires since v4 the glob flag to support wildcards in path.
    
    fixes #49806
    
    PR Close #50493

--- aio/tools/transforms/angular.io-package/processors/cleanGeneratedFiles.js
+++ aio/tools/transforms/angular.io-package/processors/cleanGeneratedFiles.js
@@ -1,10 +1,10 @@
-const rimraf = require('rimraf');
+const {rimrafSync} = require('rimraf');
 module.exports = function cleanGeneratedFiles() {
   return {
     $runAfter: ['writing-files'],
     $runBefore: ['writeFilesProcessor'],
     $process: function() {
-      rimraf.sync('src/generated/{docs,*.json}');
+      rimrafSync('src/generated/{docs,*.json}', {glob: true});
     }
   };
 };
