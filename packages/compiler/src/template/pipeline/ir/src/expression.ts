993605ff38e180793d6279709f2fae70f0b88e0d
Dylan Hunn <dylhunn@gmail.com>
2023-06-01 11:59:58 -0700

    refactor(compiler): Refactor expression.ts to use the Visitor pattern.
    `expression.ts` currently exports helper functions `transformExpressionsInOp` and `visitExpressionsInOp`, which take a transformation function from one `Expression` to another, and recursively apply it to every expression in the AST. This API is useful for most cases.
    
    However, certain cases require us to *conditionally* apply different transformations depending on our exact position in the tree. Moreover, determining the transformation sometimes requires a *second, nested* recursive exploration of the AST. This makes the above helpers unsuitable for every purpose.
    
    Consider the example of transforming all null-safe accesses in the AST. When we reach an AST node representing a simple property read, we need to determine its left-most null-safe-read descendant, so that we can hoist it to a null check at the top. This requires an additional inline recursive pass, but the inline pass doesn't visit every node! (To be precise, it skips anything other than left-hand recievers of keyed reads, property reads, and function calls.) Unfortunately, we cannot implement this correctly using the existing helper functions which visit all nodes in a subtree -- although we could get the same functional behavior, we would visit all sub-descendants time, instead of just relevant sub-descendants in O(log n) time.
    
    Therefore, I have extended the `o.Visitor` pattern already used in the output AST, by defining `ir.ExpressionVisitor`. An `ir.ExpressionVisitor` class works in exactly the same manner, except it can also visit IR nodes. Furthermore, I have added implementations of `visitExpression` to every IR expression, allowing them to be navigated by the visitor.
    
    For the purpose of convenience, I also added two base visitors that can be extended: an `EverythingVisitor`, which explores the entire tree and applies a transformation to every node, and a `NullVisitor`, which returns null for every kind of expression. Finally, I created a `visitTopLevelExpressionsInOp` function, which takes a Visitor class, and applies it to every expression directly exposed by an op.
    
    Soon, I will port the existing APIs to use the new Visitor implementation. However, this involves enough subtle changes that it ought to be completed in a separate commit.

--- packages/compiler/src/template/pipeline/ir/src/expression.ts
+++ packages/compiler/src/template/pipeline/ir/src/expression.ts
@@ -17,14 +17,159 @@ import type {CreateOp} from './ops/create';
 import type {UpdateOp} from './ops/update';
 
 /**
- * An `o.Expression` subtype representing a logical expression in the intermediate representation.
+ * Expressions defined by the intermediate representation, as opposed to an `o.Expression`.
  */
 export type Expression = LexicalReadExpr|ReferenceExpr|ContextExpr|NextContextExpr|
     GetCurrentViewExpr|RestoreViewExpr|ResetViewExpr|ReadVariableExpr|PureFunctionExpr|
     PureFunctionParameterExpr|PipeBindingExpr|PipeBindingVariadicExpr;
 
 /**
- * Transformer type which converts expressions into general `o.Expression`s (which may be an
+ * A supplemented version of the output expression visitor, which also allows us to visit IR
+ * expressions.
+ */
+export interface ExpressionVisitor extends o.ExpressionVisitor {
+  visitLexicalReadExpr(ast: LexicalReadExpr, context: any): any;
+  visitReferenceExpr(ast: ReferenceExpr, context: any): any;
+  visitContextExpr(ast: ContextExpr, context: any): any;
+  visitNextContextExpr(ast: NextContextExpr, context: any): any;
+  visitGetCurrentViewExpr(ast: GetCurrentViewExpr, context: any): any;
+  visitRestoreViewExpr(ast: RestoreViewExpr, context: any): any;
+  visitResetViewExpr(ast: ResetViewExpr, context: any): any;
+  visitReadVariableExpr(ast: ReadVariableExpr, context: any): any;
+  visitPureFunctionExpr(ast: PureFunctionExpr, context: any): any;
+  visitPureFunctionParameterExpr(ast: PureFunctionParameterExpr, context: any): any;
+  visitPipeBindingExpr(ast: PipeBindingExpr, context: any): any;
+  visitPipeBindingVariadicExpr(ast: PipeBindingVariadicExpr, context: any): any;
+}
+
+/**
+ * The following type assertion statically ensures that the `ExpressionVisitor` supports all of the
+ * expressions described by the `Expression` type.
+ */
+type AllVisitableTypes = {
+  [method in keyof ExpressionVisitor]: Parameters<ExpressionVisitor[method]>[0]
+}[keyof ExpressionVisitor];
+type VisitorComplete = Expression extends AllVisitableTypes ? true : false;
+const visitorSupportsAllTypes = true satisfies VisitorComplete;
+
+/**
+ * An IR expression visitor where the result of visiting any AST node is null.
+ */
+export abstract class NullExpressionVisitor implements ExpressionVisitor {
+  visitReadVarExpr = (ast: o.ReadVarExpr, context: any) => null;
+  visitWriteVarExpr = (ast: o.WriteVarExpr, context: any) => null;
+  visitWriteKeyExpr = (ast: o.WriteKeyExpr, context: any) => null;
+  visitWritePropExpr = (ast: o.WritePropExpr, context: any) => null;
+  visitInvokeFunctionExpr = (ast: o.InvokeFunctionExpr, context: any) => null;
+  visitTaggedTemplateExpr = (ast: o.TaggedTemplateExpr, context: any) => null;
+  visitInstantiateExpr = (ast: o.InstantiateExpr, context: any) => null;
+  visitLiteralExpr = (ast: o.LiteralExpr, context: any) => null;
+  visitLocalizedString = (ast: o.LocalizedString, context: any) => null;
+  visitExternalExpr = (ast: o.ExternalExpr, context: any) => null;
+  visitConditionalExpr = (ast: o.ConditionalExpr, context: any) => null;
+  visitNotExpr = (ast: o.NotExpr, context: any) => null;
+  visitFunctionExpr = (ast: o.FunctionExpr, context: any) => null;
+  visitUnaryOperatorExpr = (ast: o.UnaryOperatorExpr, context: any) => null;
+  visitBinaryOperatorExpr = (ast: o.BinaryOperatorExpr, context: any) => null;
+  visitReadPropExpr = (ast: o.ReadPropExpr, context: any) => null;
+  visitReadKeyExpr = (ast: o.ReadKeyExpr, context: any) => null;
+  visitLiteralArrayExpr = (ast: o.LiteralArrayExpr, context: any) => null;
+  visitLiteralMapExpr = (ast: o.LiteralMapExpr, context: any) => null;
+  visitCommaExpr = (ast: o.CommaExpr, context: any) => null;
+  visitWrappedNodeExpr = (ast: o.WrappedNodeExpr<any>, context: any) => null;
+  visitTypeofExpr = (ast: o.TypeofExpr, context: any) => null;
+  visitLexicalReadExpr = (ast: LexicalReadExpr, context: any) => null;
+  visitReferenceExpr = (ast: ReferenceExpr, context: any) => null;
+  visitContextExpr = (ast: ContextExpr, context: any) => null;
+  visitNextContextExpr = (ast: NextContextExpr, context: any) => null;
+  visitGetCurrentViewExpr = (ast: GetCurrentViewExpr, context: any) => null;
+  visitRestoreViewExpr = (ast: RestoreViewExpr, context: any) => null;
+  visitResetViewExpr = (ast: ResetViewExpr, context: any) => null;
+  visitReadVariableExpr = (ast: ReadVariableExpr, context: any) => null;
+  visitPureFunctionExpr = (ast: PureFunctionExpr, context: any) => null;
+  visitPureFunctionParameterExpr = (ast: PureFunctionParameterExpr, context: any) => null;
+  visitPipeBindingExpr = (ast: PipeBindingExpr, context: any) => null;
+  visitPipeBindingVariadicExpr = (ast: PipeBindingVariadicExpr, context: any) => null;
+}
+
+/**
+ * An IR expression visitor which recursively visits the entire tree. By default, returns the
+ * identity expression. Accepts an optional argument, which causes each expression to be transformed
+ * before it is returned.
+ */
+export class EverythingVisitor implements ExpressionVisitor {
+  constructor(
+      private transform: (e: o.Expression|Expression, ctx: any) => (o.Expression | Expression) =
+          (e, context) => e) {}
+
+  private visitAll(
+      ast: Expression|o.Expression, context: any,
+      ...subexpressions: Array<Expression|o.Expression|null>): Expression|o.Expression {
+    for (const sub of subexpressions) {
+      if (sub != null) {
+        sub.visitExpression(this, context);
+      }
+    }
+    return this.transform(ast, context);
+  }
+  // Output expression types.
+  visitReadVarExpr = (ast: o.ReadVarExpr, context: any) => this.visitAll(ast, context);
+  visitWriteVarExpr = (ast: o.WriteVarExpr, context: any) => this.visitAll(ast, context, ast.value);
+  visitWriteKeyExpr = (ast: o.WriteKeyExpr, context: any) =>
+      this.visitAll(ast, context, ast.receiver, ast.index, ast.value);
+  visitWritePropExpr = (ast: o.WritePropExpr, context: any) =>
+      this.visitAll(ast, context, ast.receiver, ast.value);
+  visitInvokeFunctionExpr = (ast: o.InvokeFunctionExpr, context: any) =>
+      this.visitAll(ast, context, ast.fn, ...ast.args);
+  visitTaggedTemplateExpr = (ast: o.TaggedTemplateExpr, context: any) =>
+      this.visitAll(ast, context, ast.tag, ...ast.template.expressions);  // TODO: this is correct?
+  visitInstantiateExpr = (ast: o.InstantiateExpr, context: any) =>
+      this.visitAll(ast, context, ast.classExpr, ...ast.args);
+  visitLiteralExpr = (ast: o.LiteralExpr, context: any) => this.visitAll(ast, context);
+  visitLocalizedString = (ast: o.LocalizedString, context: any) =>
+      this.visitAll(ast, context, ...ast.expressions);
+  visitExternalExpr = (ast: o.ExternalExpr, context: any) => this.visitAll(ast, context);
+  visitConditionalExpr = (ast: o.ConditionalExpr, context: any) =>
+      this.visitAll(ast, context, ast.condition, ast.trueCase, ast.falseCase);
+  visitNotExpr = (ast: o.NotExpr, context: any) => this.visitAll(ast, context, ast.condition);
+  visitFunctionExpr = (ast: o.FunctionExpr, context: any) => this.visitAll(ast, context);
+  visitUnaryOperatorExpr = (ast: o.UnaryOperatorExpr, context: any) =>
+      this.visitAll(ast, context, ast.expr);
+  visitBinaryOperatorExpr = (ast: o.BinaryOperatorExpr, context: any) =>
+      this.visitAll(ast, context, ast.lhs, ast.rhs);
+  visitReadPropExpr = (ast: o.ReadPropExpr, context: any) =>
+      this.visitAll(ast, context, ast.receiver);
+  visitReadKeyExpr = (ast: o.ReadKeyExpr, context: any) =>
+      this.visitAll(ast, context, ast.receiver, ast.index);
+  visitLiteralArrayExpr = (ast: o.LiteralArrayExpr, context: any) =>
+      this.visitAll(ast, context, ...ast.entries);
+  visitLiteralMapExpr = (ast: o.LiteralMapExpr, context: any) =>
+      this.visitAll(ast, context, ...ast.entries.map(e => e.value));
+  visitCommaExpr = (ast: o.CommaExpr, context: any) => this.visitAll(ast, context, ...ast.parts);
+  visitWrappedNodeExpr = (ast: o.WrappedNodeExpr<any>, context: any) => this.visitAll(ast, context);
+  visitTypeofExpr = (ast: o.TypeofExpr, context: any) => this.visitAll(ast, context, ast.expr);
+  // IR expression types.
+  visitLexicalReadExpr = (ast: LexicalReadExpr, context: any) => this.visitAll(ast, context);
+  visitReferenceExpr = (ast: ReferenceExpr, context: any) => this.visitAll(ast, context);
+  visitContextExpr = (ast: ContextExpr, context: any) => this.visitAll(ast, context);
+  visitNextContextExpr = (ast: NextContextExpr, context: any) => this.visitAll(ast, context);
+  visitGetCurrentViewExpr = (ast: GetCurrentViewExpr, context: any) => this.visitAll(ast, context);
+  visitRestoreViewExpr = (ast: RestoreViewExpr, context: any) =>
+      typeof ast.view === 'number' ? ast : this.visitAll(ast, context, ast.view);
+  visitResetViewExpr = (ast: ResetViewExpr, context: any) => this.visitAll(ast, context, ast.expr);
+  visitReadVariableExpr = (ast: ReadVariableExpr, context: any) => this.visitAll(ast, context);
+  visitPureFunctionExpr = (ast: PureFunctionExpr, context: any) =>
+      this.visitAll(ast, context, ast.body, ...ast.args);
+  visitPureFunctionParameterExpr = (ast: PureFunctionParameterExpr, context: any) =>
+      this.visitAll(ast, context);
+  visitPipeBindingExpr = (ast: PipeBindingExpr, context: any) =>
+      this.visitAll(ast, context, ...ast.args);
+  visitPipeBindingVariadicExpr = (ast: PipeBindingVariadicExpr, context: any) =>
+      this.visitAll(ast, context, ast.args);
+}
+
+/**
+ * Transformer type which converts `o.Expression`s into other `o.Expression`s (which may be an
  * identity transformation).
  */
 export type ExpressionTransform = (expr: o.Expression, flags: VisitorContextFlag) => o.Expression;
@@ -52,6 +197,8 @@ export abstract class ExpressionBase extends o.Expression {
    */
   abstract transformInternalExpressions(transform: ExpressionTransform, flags: VisitorContextFlag):
       void;
+
+  abstract override visitExpression(visitor: ExpressionVisitor, context: any): any;
 }
 
 /**
@@ -64,7 +211,9 @@ export class LexicalReadExpr extends ExpressionBase {
     super();
   }
 
-  override visitExpression(visitor: o.ExpressionVisitor, context: any): void {}
+  override visitExpression(visitor: ExpressionVisitor, context: any): any {
+    return visitor.visitLexicalReadExpr(this, context);
+  }
 
   override isEquivalent(): boolean {
     return false;
@@ -91,7 +240,9 @@ export class ReferenceExpr extends ExpressionBase implements UsesSlotIndexTrait 
     super();
   }
 
-  override visitExpression(): void {}
+  override visitExpression(visitor: ExpressionVisitor, context: any): any {
+    return visitor.visitReferenceExpr(this, context);
+  }
 
   override isEquivalent(e: o.Expression): boolean {
     return e instanceof ReferenceExpr && e.target === this.target;
@@ -114,7 +265,9 @@ export class ContextExpr extends ExpressionBase {
     super();
   }
 
-  override visitExpression(): void {}
+  override visitExpression(visitor: ExpressionVisitor, context: any): any {
+    return visitor.visitContextExpr(this, context);
+  }
 
   override isEquivalent(e: o.Expression): boolean {
     return e instanceof ContextExpr && e.view === this.view;
@@ -139,7 +292,9 @@ export class NextContextExpr extends ExpressionBase {
     super();
   }
 
-  override visitExpression(): void {}
+  override visitExpression(visitor: ExpressionVisitor, context: any): any {
+    return visitor.visitNextContextExpr(this, context);
+  }
 
   override isEquivalent(e: o.Expression): boolean {
     return e instanceof NextContextExpr && e.steps === this.steps;
@@ -165,7 +320,9 @@ export class GetCurrentViewExpr extends ExpressionBase {
     super();
   }
 
-  override visitExpression(): void {}
+  override visitExpression(visitor: ExpressionVisitor, context: any): any {
+    return visitor.visitGetCurrentViewExpr(this, context);
+  }
 
   override isEquivalent(e: o.Expression): boolean {
     return e instanceof GetCurrentViewExpr;
@@ -188,10 +345,8 @@ export class RestoreViewExpr extends ExpressionBase {
     super();
   }
 
-  override visitExpression(visitor: o.ExpressionVisitor, context: any): void {
-    if (typeof this.view !== 'number') {
-      this.view.visitExpression(visitor, context);
-    }
+  override visitExpression(visitor: ExpressionVisitor, context: any): any {
+    return visitor.visitRestoreViewExpr(this, context);
   }
 
   override isEquivalent(e: o.Expression): boolean {
@@ -228,8 +383,8 @@ export class ResetViewExpr extends ExpressionBase {
     super();
   }
 
-  override visitExpression(visitor: o.ExpressionVisitor, context: any): any {
-    this.expr.visitExpression(visitor, context);
+  override visitExpression(visitor: ExpressionVisitor, context: any): any {
+    return visitor.visitResetViewExpr(this, context);
   }
 
   override isEquivalent(e: o.Expression): boolean {
@@ -256,7 +411,9 @@ export class ReadVariableExpr extends ExpressionBase {
     super();
   }
 
-  override visitExpression(): void {}
+  override visitExpression(visitor: ExpressionVisitor, context: any): any {
+    return visitor.visitReadVariableExpr(this, context);
+  }
 
   override isEquivalent(other: o.Expression): boolean {
     return other instanceof ReadVariableExpr && other.xref === this.xref;
@@ -303,11 +460,8 @@ export class PureFunctionExpr extends ExpressionBase implements ConsumesVarsTrai
     this.args = args;
   }
 
-  override visitExpression(visitor: o.ExpressionVisitor, context: any) {
-    this.body?.visitExpression(visitor, context);
-    for (const arg of this.args) {
-      arg.visitExpression(visitor, context);
-    }
+  override visitExpression(visitor: ExpressionVisitor, context: any) {
+    visitor.visitPureFunctionExpr(this, context);
   }
 
   override isEquivalent(other: o.Expression): boolean {
@@ -346,7 +500,9 @@ export class PureFunctionParameterExpr extends ExpressionBase {
     super();
   }
 
-  override visitExpression(): void {}
+  override visitExpression(visitor: ExpressionVisitor, context: any) {
+    visitor.visitPureFunctionParameterExpr(this, context);
+  }
 
   override isEquivalent(other: o.Expression): boolean {
     return other instanceof PureFunctionParameterExpr && other.index === this.index;
@@ -374,10 +530,8 @@ export class PipeBindingExpr extends ExpressionBase implements UsesSlotIndexTrai
     super();
   }
 
-  override visitExpression(visitor: o.ExpressionVisitor, context: any): void {
-    for (const arg of this.args) {
-      arg.visitExpression(visitor, context);
-    }
+  override visitExpression(visitor: ExpressionVisitor, context: any) {
+    visitor.visitPipeBindingExpr(this, context);
   }
 
   override isEquivalent(): boolean {
@@ -413,8 +567,8 @@ export class PipeBindingVariadicExpr extends ExpressionBase implements UsesSlotI
     super();
   }
 
-  override visitExpression(visitor: o.ExpressionVisitor, context: any): void {
-    this.args.visitExpression(visitor, context);
+  override visitExpression(visitor: ExpressionVisitor, context: any) {
+    visitor.visitPipeBindingVariadicExpr(this, context);
   }
 
   override isEquivalent(): boolean {
@@ -431,8 +585,67 @@ export class PipeBindingVariadicExpr extends ExpressionBase implements UsesSlotI
   }
 }
 
+export enum VisitorContextFlag {
+  None = 0b0000,
+  InChildOperation = 0b0001,
+}
+
 /**
- * Visits all `Expression`s in the AST of `op` with the `visitor` function.
+ * Using a full-blown `Visitor` class, visit only the top-level expressions of this op.
+ * Note that unlike the other functions exported from this file, this function accepts a Visitor
+ * *class*, not a visitor *function*. This provides more customizable behavior than a plain
+ * `ExpressionTransform`, but unlike the other exported functions, you are responsible for
+ * recursively exploring the tree of expressions yourself.
+ */
+export function visitTopLevelExpressionsInOp(
+    op: CreateOp|UpdateOp, visitor: ExpressionVisitor, ctx: any) {
+  switch (op.kind) {
+    case OpKind.Property:
+      op.expression = op.expression.visitExpression(visitor, ctx);
+      break;
+    case OpKind.Statement:
+      if (op instanceof o.ExpressionStatement) {
+        op.expr = op.expr.visitExpression(visitor, ctx);
+      } else if (op instanceof o.ReturnStatement) {
+        op.value = op.value.visitExpression(visitor, ctx);
+      } else {
+        throw new Error(`Unhandled statement kind: ${op.constructor.name}`);
+      }
+      break;
+    case OpKind.Variable:
+      op.initializer = op.initializer.visitExpression(visitor, ctx);
+      break;
+    case OpKind.InterpolateText:
+      for (let i = 0; i < op.expressions.length; i++) {
+        op.expressions[i] = op.expressions[i].visitExpression(visitor, ctx);
+      }
+      break;
+    case OpKind.Listener:
+      let currFlags: VisitorContextFlag = ctx.flags ?
+          ctx.flags | VisitorContextFlag.InChildOperation :
+          VisitorContextFlag.InChildOperation;
+      for (const innerOp of op.handlerOps) {
+        visitTopLevelExpressionsInOp(innerOp, visitor, {flags: currFlags});
+      }
+      break;
+    case OpKind.Element:
+    case OpKind.ElementStart:
+    case OpKind.ElementEnd:
+    case OpKind.Container:
+    case OpKind.ContainerStart:
+    case OpKind.ContainerEnd:
+    case OpKind.Template:
+    case OpKind.Text:
+    case OpKind.Advance:
+      // These operations contain no expressions.
+      break;
+    default:
+      throw new Error(`AssertionError: expressionsInOp doesn't handle ${OpKind[op.kind]}`);
+  }
+}
+
+/**
+ * Applies a `visitor` function to all `Expression`s in the AST.
  */
 export function visitExpressionsInOp(
     op: CreateOp|UpdateOp, visitor: (expr: o.Expression, flags: VisitorContextFlag) => void): void {
@@ -440,11 +653,6 @@ export function visitExpressionsInOp(
     visitor(expr, flags);
     return expr;
   }, VisitorContextFlag.None);
-}
-
-export enum VisitorContextFlag {
-  None = 0b0000,
-  InChildOperation = 0b0001,
 }
 
 /**
