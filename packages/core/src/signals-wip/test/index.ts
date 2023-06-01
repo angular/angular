37d52e409141bc3f4b55c5b0c3e6cf02fa588460
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-01 13:36:34 +0000

    WIP: implementation of `input` function signature

--- packages/core/src/signals-wip/test/index.ts
+++ packages/core/src/signals-wip/test/index.ts
@@ -1,19 +1,38 @@
-import {Component, input} from '@angular/core';
+import {Component, input, Input} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 
 @Component({
   selector: 'greet',
   standalone: true,
   signals: true,
-  template: `{{ nameInput() }}`,
+  template: `{{ counter() }}`,
 })
-class Greet {
-  nameInput = input('Jeff');
+export class Greet<T> {
+  counter = input(0);
+  bla = input();  // TODO: should be a diagnostic. no type & no value
+  bla2 = input<string>()
+  bla3 = input({initialValue: 3, required: true});
+  bla4 = input(3, {required: true, alias: 'bla4Public'})
+  gen = input<T>({required: true});
+  gen2 = input<T>();
+
+  works(): T {
+    return this.gen();
+  }
+
+  // TODO: should break, but still supported early prototype.
+  @Input() oldInput: string|undefined;
 }
 
 @Component({
   standalone: true,
-  template: `Hello <greet [nameInput]="'Bla'" />`,
+  template: `
+    Hello <greet [counter]="3" [bla4Public]="10" #ok
+      [bla3]="-10" [gen]="{yes: true}"
+    />
+
+    <button (click)="ok.works().yes">Click</button>
+  `,
   imports: [Greet],
 })
 export class MyApp {
