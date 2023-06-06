0875b519b9dcf15703039b20ef7398b0c964ba0c
Alan Agius <alanagius@google.com>
2023-06-06 10:32:39 +0000

    fix(platform-server): surface errors during rendering (#50587)
    Prior to this change in some cases errors tht happen during routing were not being surfaced. This is due to the fact that the router has floating promises, and the platform was being destroyed prior to these being settled.
    
    PR Close #50587

--- packages/platform-server/test/integration_spec.ts
+++ packages/platform-server/test/integration_spec.ts
@@ -532,7 +532,11 @@ if (getDOM().supportsDOMEvents) return;  // NODE only
 
 describe('platform-server integration', () => {
   beforeEach(() => {
-    if (getPlatform()) destroyPlatform();
+    destroyPlatform();
+  });
+
+  afterAll(() => {
+    destroyPlatform();
   });
 
   it('should bootstrap', async () => {
@@ -994,12 +998,11 @@ describe('platform-server integration', () => {
             renderApplication(
                 getStandaloneBoostrapFn(MyServerAppStandalone, RenderHookProviders), options) :
             renderModule(RenderHookModule, options);
-        bootstrap.then(output => {
-          // title should be added by the render hook.
-          expect(output).toBe(
-              '<html><head><title>RenderHook</title></head><body>' +
-              '<app ng-version="0.0.0-PLACEHOLDER" ng-server-context="other">Works!</app></body></html>');
-        });
+        const output = await bootstrap;
+        // title should be added by the render hook.
+        expect(output).toBe(
+            '<html><head><title>RenderHook</title></head><body>' +
+            '<app ng-version="0.0.0-PLACEHOLDER" ng-server-context="other">Works!</app></body></html>');
       });
 
       it('should call multiple render hooks', async () => {
