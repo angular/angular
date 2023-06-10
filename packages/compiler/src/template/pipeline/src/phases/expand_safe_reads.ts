f27ddc08e176110d993578282a5f97662db7b046
Dylan Hunn <dylhunn@gmail.com>
2023-06-10 13:37:36 -0700

    refactor(compiler): Support safe property reads and keyed reads. Angular's null-safe access operators differ from Javascript's built-in semantics, in that they short-circuit to `null` instead of `undefined`. This necessitates providing a custom transformation, instead of relying on Typescript or Javascript itseld.
    The old TemplateDefinitionBuilder uses a top-down approach based on the Visitor pattern, in which it recursively extracts the left-most safe access, and hoists it to a null check at the top. See `expression_converter.ts` for details.
    
    In this commit, we replace that approach with a new bottom-up algorithm, as part of the template pipeline. This requires an intermediate expression type to represent the not-yet-expanded ternary operators, and is split into its own pass.
    
    Null-safe function calls are not yet implemented, since they will rely on a future temporary variable allocation pass.
    
    Co-authored-by: Alex Rickabaugh <alxhub@users.noreply.github.com>

---
+++ packages/compiler/src/template/pipeline/src/phases/expand_safe_reads.ts
@@ -0,0 +1,97 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import * as o from '../../../../output/output_ast';
+import * as ir from '../../ir';
+import {ComponentCompilation} from '../compilation';
+
+/**
+ * Finds all unresolved safe read expressions, and converts them into the appropriate output AST
+ * reads, guarded by null checks.
+ */
+export function phaseExpandSafeReads(cpl: ComponentCompilation): void {
+  for (const [_, view] of cpl.views) {
+    for (const op of view.ops()) {
+      ir.transformExpressionsInOp(op, safeTransform, ir.VisitorContextFlag.None);
+      ir.transformExpressionsInOp(op, ternaryTransform, ir.VisitorContextFlag.None);
+    }
+  }
+}
+
+function isAccessExpression(e: o.Expression): e is o.ReadPropExpr|ir.SafePropertyReadExpr|
+    o.ReadKeyExpr|ir.SafeKeyedReadExpr|o.InvokeFunctionExpr|ir.SafeInvokeFunctionExpr {
+  return e instanceof o.ReadPropExpr || e instanceof ir.SafePropertyReadExpr ||
+      e instanceof o.ReadKeyExpr || e instanceof ir.SafeKeyedReadExpr ||
+      e instanceof o.InvokeFunctionExpr || e instanceof ir.SafeInvokeFunctionExpr;
+}
+
+// TODO: When strict compatibility with TemplateDefinitionBuilder is not required, we can use `&&`
+// instead.
+function safeTransform(e: o.Expression): o.Expression {
+  if (!isAccessExpression(e)) {
+    return e;
+  }
+  if (e instanceof o.ReadPropExpr && e.receiver instanceof ir.SafeTernaryExpr) {
+    let st = e.receiver;
+    while (st.expr instanceof ir.SafeTernaryExpr) {
+      st = st.expr;
+    }
+    st.expr = new o.ReadPropExpr(st.expr, e.name);
+    return e.receiver;
+  }
+  if (e instanceof o.ReadKeyExpr && e.receiver instanceof ir.SafeTernaryExpr) {
+    let st = e.receiver;
+    while (st.expr instanceof ir.SafeTernaryExpr) {
+      st = st.expr;
+    }
+    st.expr = new o.ReadKeyExpr(st.expr, e.index);
+    return e.receiver;
+  }
+
+  if (e instanceof ir.SafeKeyedReadExpr || e instanceof ir.SafePropertyReadExpr) {
+    if (e.receiver instanceof ir.SafeTernaryExpr) {
+      let st = e.receiver;
+      while (st.expr instanceof ir.SafeTernaryExpr) {
+        st = st.expr;
+      }
+      let newExpr: o.Expression;
+      if (e instanceof ir.SafePropertyReadExpr) {
+        newExpr = new o.ReadPropExpr(st.expr, e.name);
+      } else {
+        newExpr = new o.ReadKeyExpr(st.expr, e.index);
+      }
+      st.expr = new ir.SafeTernaryExpr(st.expr.clone(), newExpr);
+      return e.receiver;
+    } else {
+      let newExpr: o.Expression;
+      if (e instanceof ir.SafePropertyReadExpr) {
+        newExpr = new o.ReadPropExpr(e.receiver, e.name);
+      } else {
+        newExpr = new o.ReadKeyExpr(e.receiver, e.index);
+      }
+      return new ir.SafeTernaryExpr(e.receiver.clone(), newExpr);
+    }
+  }
+
+  if (e instanceof ir.SafeInvokeFunctionExpr) {
+    // TODO: Implement safe function calls in a subsequent commit.
+    return new o.InvokeFunctionExpr(e.receiver, e.args);
+  }
+  return e;
+}
+
+function ternaryTransform(e: o.Expression): o.Expression {
+  if (!(e instanceof ir.SafeTernaryExpr)) {
+    return e;
+  }
+  return new o.ConditionalExpr(
+      new o.BinaryOperatorExpr(o.BinaryOperator.Equals, e.guard, o.NULL_EXPR),
+      o.NULL_EXPR,
+      e.expr,
+  );
+}
