2f7072dc3f51c2d3dddd88819e040ea2b5ccc67f
Miles Malerba <mmalerba@google.com>
2023-06-20 14:57:22 +0900

    refactor(compiler): add support for class property bindings (#50805)
    Adds support for bindings of the form `[class.some-class]="isActive"`
    
    PR Close #50805

--- packages/compiler/src/template/pipeline/ir/src/enums.ts
+++ packages/compiler/src/template/pipeline/ir/src/enums.ts
@@ -89,6 +89,11 @@ export enum OpKind {
   StyleProp,
 
   /**
+   * An operation to bind an expression to a class property of an element.
+   */
+  ClassProp,
+
+  /**
    * An operation to bind an expression to the styles of an element.
    */
   StyleMap,
