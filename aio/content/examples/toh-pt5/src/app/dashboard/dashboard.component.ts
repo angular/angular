d131add0a859bcec7fc47fbe7b3b3873d107ff04
sunilbaba <sunilbaba.villuri@gmail.com>
2023-06-23 16:02:39 +0530

    In the Dashboard component, in the getHeroes method, there is a problem. The method should display the first four elements of the heroes array, but when using the slice method, it is written as (1, 5) instead of (0, 4)

--- aio/content/examples/toh-pt5/src/app/dashboard/dashboard.component.ts
+++ aio/content/examples/toh-pt5/src/app/dashboard/dashboard.component.ts
@@ -20,7 +20,7 @@ export class DashboardComponent implements OnInit {
   // #docregion getHeroes
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+      .subscribe(heroes => this.heroes = heroes.slice(0, 4));
   }
   // #enddocregion getHeroes
 }
