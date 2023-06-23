5bd530ab322d958ada82d615f168df75d87163fc
Charles Lyding <19598772+clydin@users.noreply.github.com>
2023-06-23 18:31:07 -0400

    refactor(compiler-cli): add internal compiler option to control NgModule selector scope emit (#51007)
    An internal compiler option named `supportJitMode` is now available for use by the Angular CLI.
    This option currently controls the emit of NgModule selector scope information. This emitted
    information is only needed in AOT mode when an application also uses JIT. However, AOT mode
    combined with JIT mode is not currently supported nor will work in the Angular CLI. With
    the Angular CLI, JIT mode is only supported if the entire application is built in JIT mode.
    Without this option, the CLI needs to manually perform a code transform to remove the information
    and also replicate TypeScript's import eliding. This is can be a complicated operation and must
    be continually kept up to date with any changes to both the Angular compiler and TypeScript.
    The introduction of this new option alleviates these concerns while also removing several build
    time actions that would otherwise need to be performed on every application build.
    
    PR Close #51007

--- packages/compiler-cli/test/ngtsc/ngtsc_spec.ts
+++ packages/compiler-cli/test/ngtsc/ngtsc_spec.ts
@@ -4909,6 +4909,60 @@ function allTests(os: string) {
               trim('MyModule.ɵmod = /*@__PURE__*/ i0.ɵɵdefineNgModule({ type: MyModule });'));
     });
 
+    it('should emit setNgModuleScope calls for NgModules by default', () => {
+      env.write('test.ts', `
+      import {Component, Directive, Injectable, NgModule, Pipe} from '@angular/core';
+
+      @Component({selector: 'cmp', template: 'I am a component!'}) class TestComponent {}
+      @Directive({selector: 'dir'}) class TestDirective {}
+      @Injectable() class TestInjectable {}
+      @NgModule({declarations: [TestComponent, TestDirective]}) class TestNgModule {}
+      @Pipe({name: 'pipe'}) class TestPipe {}
+    `);
+
+      env.driveMain();
+      const jsContents = env.getContents('test.js');
+      expect(jsContents).toContain('\u0275setNgModuleScope(TestNgModule, ');
+    });
+
+    it('should emit setNgModuleScope calls for NgModules when supportJitMode is true', () => {
+      env.tsconfig({
+        'supportJitMode': true,
+      });
+      env.write('test.ts', `
+      import {Component, Directive, Injectable, NgModule, Pipe} from '@angular/core';
+
+      @Component({selector: 'cmp', template: 'I am a component!'}) class TestComponent {}
+      @Directive({selector: 'dir'}) class TestDirective {}
+      @Injectable() class TestInjectable {}
+      @NgModule({declarations: [TestComponent, TestDirective]}) class TestNgModule {}
+      @Pipe({name: 'pipe'}) class TestPipe {}
+    `);
+
+      env.driveMain();
+      const jsContents = env.getContents('test.js');
+      expect(jsContents).toContain('\u0275setNgModuleScope(TestNgModule, ');
+    });
+
+    it('should not emit setNgModuleScope calls for NgModules when supportJitMode is false', () => {
+      env.tsconfig({
+        'supportJitMode': false,
+      });
+      env.write('test.ts', `
+      import {Component, Directive, Injectable, NgModule, Pipe} from '@angular/core';
+
+      @Component({selector: 'cmp', template: 'I am a component!'}) class TestComponent {}
+      @Directive({selector: 'dir'}) class TestDirective {}
+      @Injectable() class TestInjectable {}
+      @NgModule({declarations: [TestComponent, TestDirective]}) class TestNgModule {}
+      @Pipe({name: 'pipe'}) class TestPipe {}
+    `);
+
+      env.driveMain();
+      const jsContents = env.getContents('test.js');
+      expect(jsContents).not.toContain('\u0275setNgModuleScope(');
+    });
+
     it('should emit setClassMetadata calls for all types by default', () => {
       env.write('test.ts', `
       import {Component, Directive, Injectable, NgModule, Pipe} from '@angular/core';
