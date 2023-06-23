a50836687bcd7d2a136ff6108fcdf3e5e7f8c5e8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-23 11:55:44 +0000

    WIP: prepare tests for failing scenarios discovered during collab
    * Local refs needs more testing
    * Basic variable read failed
    * We do not have any tests for listeners? there was some restore view
      issue: https://github.com/angular/angular/commit/3acfdf4861d5eeed5b2ad42978547da929cc3567

---
+++ packages/core/test/acceptance/signal-components/listeners.spec.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+describe('listeners', () => {
+  it('should be able to invoke a listener',
+     () => {
+         // todo:  listener restore view was missing.
+         // in template pipeline
+     });
+});
