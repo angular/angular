758869d37fc7c60b5dc0b1be17acdc3f42b00d2f
sunilbaba <sunilbaba.villuri@gmail.com>
2023-06-23 15:28:49 +0530

    docs: updating the method to display first four items from dashboard component

--- aio/content/examples/toh-pt6/src/app/dashboard/dashboard.component.ts
+++ aio/content/examples/toh-pt6/src/app/dashboard/dashboard.component.ts
@@ -19,6 +19,6 @@ export class DashboardComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+      .subscribe(heroes => this.heroes = heroes.slice(0, 4));
   }
 }
