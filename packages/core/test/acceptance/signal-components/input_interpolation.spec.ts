0eced157a9969f1bcd9313aa74d6cd69f5753fa5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-26 12:10:42 +0200

    test: add failing tests for property interpolation
    Adding failing tests for the property interpolation so we
    keep those instructions on our radar.

---
+++ packages/core/test/acceptance/signal-components/input_interpolation.spec.ts
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {USE_TEMPLATE_PIPELINE} from '@angular/compiler/src/template/pipeline/switch/index';
+import {Component, input, signal} from '@angular/core';
+import {TestBed} from '@angular/core/testing';
+
+import {Input} from '../../../src/metadata';
+
+
+if (!USE_TEMPLATE_PIPELINE) {
+  console.error(
+      'ERROR: Cannot run this test target without: --//packages/compiler:use_template_pipeline');
+  process.exit(1);
+}
+
+describe('Signal component input interpolations', () => {
+  @Component({
+    selector: 'print',
+    signals: true,
+    template: `{{text()}}`,
+    standalone: true,
+  })
+  class Print {
+    @Input() text = input('');
+  }
+
+  // TODO: compiler needs to generate the ɵɵpropertyInterpolationCreate instruction
+  xit('should bind interpolated values', () => {
+    @Component({
+      signals: true,
+      template: `<print text="Hello, {{name}}!">`,
+      imports: [Print],
+      standalone: true,
+    })
+    class App {
+      name = signal('Angular');
+    }
+
+    const fixture = TestBed.createComponent(App);
+    debugger;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.textContent).toBe('Hello, Angular!');
+  });
+});
