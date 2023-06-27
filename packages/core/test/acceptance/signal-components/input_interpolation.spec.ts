3bd2fcd0440db57166cf39178d770c3e2265d3b4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-27 11:36:42 +0200

    Revert "feat(core): propertyInterpolationCreate"
    This reverts commit 2e3fe0747990647c5cfbba5429e90ed621b18e6e.
    
    Based on the recent design discussion we are going to express property
    interpolation as a computed function and reuse the propertyCreate
    infrastructure.

--- packages/core/test/acceptance/signal-components/input_interpolation.spec.ts
+++ packages/core/test/acceptance/signal-components/input_interpolation.spec.ts
@@ -34,7 +34,7 @@ describe('Signal component input interpolations', () => {
   xit('should bind interpolated values', () => {
     @Component({
       signals: true,
-      template: `<print text="a{{1}}b{{2}}c{{3}}d{{4}}e{{5}}f{{6}}g{{7}}h{{8}}i{{9}}j"></print>`,
+      template: `<print text="Hello, {{name}}!">`,
       imports: [Print],
       standalone: true,
     })
