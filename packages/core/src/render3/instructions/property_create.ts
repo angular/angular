183c92baedce01aa45a84e5665d70de17e1e161a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-27 11:36:42 +0200

    Revert "feat(core): propertyInterpolationCreate"
    This reverts commit 2e3fe0747990647c5cfbba5429e90ed621b18e6e.
    
    Based on the recent design discussion we are going to express property
    interpolation as a computed function and reuse the propertyCreate
    infrastructure.

--- packages/core/src/render3/instructions/property_create.ts
+++ packages/core/src/render3/instructions/property_create.ts
@@ -5,14 +5,23 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import {computed} from '../../signals';
-import {assertDefined} from '../../util/assert';
+import {validateAgainstEventProperties} from '../../sanitization/sanitization';
+import {computed, SIGNAL} from '../../signals';
+import {assertDefined, assertIndexInRange} from '../../util/assert';
 import {EMPTY_ARRAY} from '../../util/empty';
+import {bindingUpdated} from '../bindings';
+import {DirectiveDef} from '../interfaces/definition';
+import {PropertyAliasValue, TNode} from '../interfaces/node';
+import {InternalInputSignal} from '../interfaces/reactivity';
+import {RComment, RElement} from '../interfaces/renderer_dom';
 import {SanitizerFn} from '../interfaces/sanitization';
-import {HEADER_OFFSET} from '../interfaces/view';
-import {getCurrentTNode, getLView, getTView} from '../state';
+import {isComponentHost} from '../interfaces/type_checks';
+import {HEADER_OFFSET, RENDERER} from '../interfaces/view';
+import {getCurrentTNode, getLView, getSelectedTNode, getTView, nextBindingIndex} from '../state';
+import {getNativeByTNode} from '../util/view_utils';
 
-import {determineInputTargets, propertyUpdateDom, propertyUpdateInput} from './property_create_shared';
+import {handleUnknownPropertyError, isPropertyValid} from './element_validation';
+import {mapPropName, markDirtyIfOnPush, setNgReflectProperties, writeToDirectiveInput} from './shared';
 
 /**
  * TODO
@@ -30,7 +39,31 @@ export function ɵɵpropertyCreate<T>(
   assertDefined(tNode, `propertyCreate() must follow an actual element`);
 
   const inputData = tNode.inputs?.[propName] ?? EMPTY_ARRAY;
-  const {zoneTargets, signalInputs} = determineInputTargets(inputData, tView, lView);
+
+  let signalInputs: InternalInputSignal[]|null = null;
+
+  // PERF: the fact that we need to iterate over all the inputs here isn't great.
+  // We might consider storing more info on TView
+  let zoneTargets: PropertyAliasValue|null = null;
+  for (let i = 0; i < inputData.length;) {
+    const directiveIndex = inputData[i++] as number;
+    const privateName = inputData[i++] as string;
+    const def = tView.data[directiveIndex] as DirectiveDef<any>;
+    if (!def.signals) {
+      // TODO(pk): refactor - code flow with all those firstCreatePass checks becomes hard to follow
+      if (tView.firstCreatePass) {
+        (zoneTargets ??= []).push(directiveIndex, privateName);
+      }
+    } else {
+      ngDevMode && assertIndexInRange(lView, directiveIndex);
+      // PERF: megamorphic read on [privateName] access
+      const inputSignal = lView[directiveIndex][privateName][SIGNAL] as InternalInputSignal;
+      (signalInputs ??= []).push(inputSignal);
+    }
+  }
+
+  zoneTargets ??= EMPTY_ARRAY;
+  signalInputs ??= EMPTY_ARRAY;
 
   // If there are multiple signal targets, or any zone targets, then wrap `expr` in a computed. This
   // ensures that the expression is only evaluated once, even if it has multiple consumers. Zone
@@ -65,4 +98,70 @@ export function ɵɵpropertyCreate<T>(
   }
 
   return ɵɵpropertyCreate;
+}
+
+export function propertyUpdateDom(
+    nodeSlot: number, propName: string, expressionSlot: number, sanitizer: SanitizerFn|null): void {
+  const lView = getLView();
+  const expr = lView[expressionSlot];
+
+  let value = expr();
+  const bindingIndex = nextBindingIndex();
+  if (!bindingUpdated(lView, bindingIndex, value)) {
+    return;
+  }
+
+  const tView = getTView();
+  const tNode = tView.data[nodeSlot] as TNode;
+  const element = getNativeByTNode(tNode, lView) as RElement | RComment;
+
+  propName = mapPropName(propName);
+
+  if (ngDevMode) {
+    validateAgainstEventProperties(propName);
+    if (!isPropertyValid(element, propName, tNode.value, tView.schemas)) {
+      handleUnknownPropertyError(propName, tNode.value, tNode.type, lView);
+    }
+    ngDevMode.rendererSetProperty++;
+  }
+
+  // It is assumed that the sanitizer is only added when the compiler determines that the
+  // property is risky, so sanitization can be done without further checks.
+  value = sanitizer != null ? (sanitizer(value, tNode.value || '', propName) as any) : value;
+  lView[RENDERER].setProperty(element as RElement, propName, value);
+}
+
+export function propertyUpdateInput(
+    propName: string, expressionSlot: number, targets: PropertyAliasValue): void {
+  const lView = getLView();
+  const expr = lView[expressionSlot];
+  const value = expr();
+
+  const tNode = getSelectedTNode();
+  const tView = getTView();
+
+  ngDevMode && assertDefined(tNode.inputs, `Expected tNode to have inputs`);
+
+  const bindingIndex = nextBindingIndex();
+  if (!bindingUpdated(lView, bindingIndex, value)) {
+    return;
+  }
+
+  for (let i = 0; i < targets.length;) {
+    const index = targets[i++] as number;
+    const privateName = targets[i++] as string;
+    ngDevMode && assertIndexInRange(lView, index);
+    const instance = lView[index];
+    const def = tView.data[index] as DirectiveDef<any>;
+
+    writeToDirectiveInput(def, instance, propName, privateName, value);
+  }
+
+  const element = getNativeByTNode(tNode, lView) as RElement | RComment;
+  if (isComponentHost(tNode)) {
+    markDirtyIfOnPush(lView, tNode.index);
+  }
+  if (ngDevMode) {
+    setNgReflectProperties(lView, element, tNode.type, targets, value);
+  }
 }
