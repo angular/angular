48f4f8683577e3b85fb9708802565087f6858966
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-28 11:24:06 +0200

    test: add basic test for event listeners
    Adding a passing test for a listener invocation
    that changes signal value.

--- packages/core/test/acceptance/signal-components/listeners.spec.ts
+++ packages/core/test/acceptance/signal-components/listeners.spec.ts
@@ -6,10 +6,29 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {Component, signal} from '@angular/core';
+import {TestBed} from '@angular/core/testing';
+
 describe('listeners', () => {
-  it('should be able to invoke a listener',
-     () => {
-         // todo:  listener restore view was missing.
-         // in template pipeline
-     });
+  it('should invoke listeners that modify context data', () => {
+    @Component({
+      signals: true,
+      standalone: true,
+      template: `<button (click)="counter.set(1)">{{counter()}}</button>`,
+    })
+    class App {
+      counter = signal(0);
+    }
+
+    const fixture = TestBed.createComponent(App);
+
+    fixture.detectChanges();
+    expect(fixture.nativeElement.textContent).toBe('0');
+
+    fixture.nativeElement.firstChild.click();
+    fixture.detectChanges();
+    expect(fixture.nativeElement.textContent).toBe('1');
+  });
 });
+
+// todo:  listener restore view was missing in template pipeline
