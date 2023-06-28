e5e405ee6e180cf62befdef12ac43a91ea484bc2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-28 13:17:09 +0200

    refactor(compiler): add required file header
    Add required file header.

--- packages/compiler/src/template/pipeline/src/phases/creation_var_colocation.ts
+++ packages/compiler/src/template/pipeline/src/phases/creation_var_colocation.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
 import * as o from '../../../../output/output_ast';
 import * as ir from '../../ir';
 import type {CompilationJob, CompilationUnit} from '../compilation';
