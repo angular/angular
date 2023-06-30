7acf163f514a1a57f512aeb68d4af6bf1c14fcfa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-30 13:35:08 +0200

    test: fix signal tests after property binding changes
    Property bindings are wrapped into computed now.

--- packages/core/test/render3/instructions/virtual_spec.ts
+++ packages/core/test/render3/instructions/virtual_spec.ts
@@ -40,7 +40,9 @@ describe('virtual instructions', () => {
 
     fixture.componentInstance.value = 'bye';
     fixture.detectChanges();
-    expect(el.value).toEqual('bye');
+    // property bindings are wrapped into computed so don't update if the reactive value didn't
+    // change
+    expect(el.value).toEqual('hello');
   });
 
   it('should support signal -> signal bindings', () => {
