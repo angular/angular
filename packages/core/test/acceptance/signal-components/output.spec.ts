ffc61749ffaa8a2116b89ebbee7344d033188a6a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-07-05 13:21:17 +0000

    test: rework and enable outputs test to verify functionality

--- packages/core/test/acceptance/signal-components/output.spec.ts
+++ packages/core/test/acceptance/signal-components/output.spec.ts
@@ -10,7 +10,7 @@ import {Component, Output, output, signal} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
 
 describe('outputs', () => {
-  xit('should subscribe to signal directive outputs', () => {
+  it('should subscribe to signal directive outputs', () => {
     @Component({
       selector: 'with-output-cmp',
       standalone: true,
@@ -26,10 +26,11 @@ describe('outputs', () => {
       signals: true,
       standalone: true,
       imports: [WithOutputCmp],
-      template: `<with-output-cmp (increment)="bumpUp()" />{{inc()}}`,
+      template: `<with-output-cmp (increment)="onIncrement()" />{{inc()}}`,
     })
     class App {
       inc = signal(0);
+
       onIncrement() {
         this.inc.update(c => c + 1);
       }
@@ -37,6 +38,10 @@ describe('outputs', () => {
 
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
-    expect(fixture.nativeElement.textContent).toBe('0');
+
+    fixture.nativeElement.querySelector('button').click();
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.textContent).toBe('1');
   });
 });
