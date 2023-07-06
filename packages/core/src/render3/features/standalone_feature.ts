360f2e2ffb8988d6e69ecd9fe40b2de5b8bb0a6f
Andrew Kushnir <akushnir@google.com>
2023-07-05 22:19:37 -0700

    fix(core): ensure that standalone components get correct injector instances (#50954)
    Prior to this change, we've used `componentDef.id` as a key in a Map that acts as a cache to avoid re-creating injector instances for standalone components. In v16, the logic that generates the id has changed from an auto-incremental to a generation based on metadata. If multiple components have similar metadata, their ids might overlap.
    
    This commit updates the logic to stop using `componentDef.id` as a key and instead, use the `componentDef` itself. This would ensure that we always have a correct instance of an injector associated with a standalone component instance.
    
    Resolves #50724.
    
    PR Close #50954

--- packages/core/src/render3/features/standalone_feature.ts
+++ packages/core/src/render3/features/standalone_feature.ts
@@ -19,7 +19,7 @@ import {createEnvironmentInjector} from '../ng_module_ref';
  * collected from the imports graph rooted at a given standalone component.
  */
 class StandaloneService implements OnDestroy {
-  cachedInjectors = new Map<string, EnvironmentInjector|null>();
+  cachedInjectors = new Map<ComponentDef<unknown>, EnvironmentInjector|null>();
 
   constructor(private _injector: EnvironmentInjector) {}
 
@@ -28,16 +28,16 @@ class StandaloneService implements OnDestroy {
       return null;
     }
 
-    if (!this.cachedInjectors.has(componentDef.id)) {
+    if (!this.cachedInjectors.has(componentDef)) {
       const providers = internalImportProvidersFrom(false, componentDef.type);
       const standaloneInjector = providers.length > 0 ?
           createEnvironmentInjector(
               [providers], this._injector, `Standalone[${componentDef.type.name}]`) :
           null;
-      this.cachedInjectors.set(componentDef.id, standaloneInjector);
+      this.cachedInjectors.set(componentDef, standaloneInjector);
     }
 
-    return this.cachedInjectors.get(componentDef.id)!;
+    return this.cachedInjectors.get(componentDef)!;
   }
 
   ngOnDestroy() {
