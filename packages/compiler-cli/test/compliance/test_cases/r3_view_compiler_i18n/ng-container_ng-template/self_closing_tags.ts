c1f06d3dd7fe8a115fff127d41ce9dcb75d1d918
alkavats1 <alka.vats@nashtechglobal.com>
2023-07-19 22:51:17 +0530

    docs: added the alt attribute in the img tag (#51102)
    PR Close #51102

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_i18n/ng-container_ng-template/self_closing_tags.ts
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_i18n/ng-container_ng-template/self_closing_tags.ts
@@ -4,10 +4,10 @@ import {Component, NgModule} from '@angular/core';
   selector: 'my-component',
   template: `
   <ng-container i18n>
-    <img src="logo.png" title="Logo" /> is my logo #1
+    <img src="logo.png" title="Logo"  alt="i18_logo_1"/> is my logo #1
   </ng-container>
   <ng-template i18n>
-    <img src="logo.png" title="Logo" /> is my logo #2
+    <img src="logo.png" title="Logo" alt="i18_logo_2"/> is my logo #2
   </ng-template>
 `,
 })
