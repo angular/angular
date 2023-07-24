a3acbeeb1c1a2a1e8ae36e009efbf0a9c6fefc90
Matthieu Riegler <kyro38@gmail.com>
2023-07-24 15:40:44 +0200

    refactor(core): Better use of Object.keys()
    Code cleaning arround the `Object.keys()` in the core tests.

--- packages/core/test/render3/is_shape_of.ts
+++ packages/core/test/render3/is_shape_of.ts
@@ -61,8 +61,7 @@ export type ShapeOf<T> = {
  */
 export function isShapeOf<T>(obj: any, shapeOf: ShapeOf<T>): obj is T {
   if (typeof obj === 'object' && obj) {
-    return Object.keys(shapeOf).reduce(
-        (prev, key) => prev && obj.hasOwnProperty(key), true as boolean);
+    return Object.keys(shapeOf).every((key) => obj.hasOwnProperty(key));
   }
   return false;
 }
