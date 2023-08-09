8888ab1558a7e76d6a5f537a700fd36bf5ad85e0
Payam Valadkhan <payamv@google.com>
2023-08-09 15:25:59 -0400

    refactor(core): implement runtime logic to compute component dependencies in local compilation mode
    The runtime `ɵɵsetNgModuleScope` is modified to accept raw scope info as passed to it in local compilation mode. The runtime further registers the ng-module in the deps tracker. Then the runtime `ɵɵgetComponentDepsFactory` is implemented to use the deps tracker to get the component dependencies which leads to a valid and working Angular code.

--- packages/core/src/render3/local_compilation.ts
+++ packages/core/src/render3/local_compilation.ts
@@ -6,12 +6,12 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {Type} from '../interface/type';
-
-import {DependencyTypeList, RawScopeInfoFromDecorator} from './interfaces/definition';
+import {depsTracker} from './deps_tracker/deps_tracker';
+import {ComponentType, DependencyTypeList, RawScopeInfoFromDecorator} from './interfaces/definition';
 
 export function ɵɵgetComponentDepsFactory(
-    type: Type<any>, rawImports?: RawScopeInfoFromDecorator): () => DependencyTypeList {
-  // TODO(pmvald): Implement this runtime using deps tracker.
-  return () => [];
+    type: ComponentType<any>, rawImports?: RawScopeInfoFromDecorator[]): () => DependencyTypeList {
+  return () => {
+    return depsTracker.getComponentDependencies(type, rawImports).dependencies;
+  };
 }
