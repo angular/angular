9cc0cbed0ce0d01ce569a1963902e288e23aaccd
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-08-16 10:16:08 +0200

    refactor(compiler): generate if block instructions (#51380)
    Adds the logic to generate the instructions for `if` blocks. There are two primary use cases we need to account for:
    
    A conditional that doesn't use the `as` parameter of the `if` block. To support it we generate a nested ternary expression that evaluates to the index of the template whose condition is truthy. If the block doesn't have an `else` branch, we pass in a special `-1` value which means that no view will be rendered.
    
    Example with an `else`:
    ```ts
    // {#if expr}
    //   ...
    //   {:else if otherExpr} ...
    //   {:else} ...
    // {/if}
    
    if (rf & 1) {
      ɵɵtemplate(0, App_Conditional_0_Template, 0, 0);
      ɵɵtemplate(1, App_Conditional_1_Template, 0, 0);
      ɵɵtemplate(2, App_Conditional_2_Template, 0, 0);
    }
    if (rf & 2) {
      ɵɵconditional(0, ctx.expr ? 0 : ctx.otherExpr ? 1 : 2);
    }
    ```
    
    Example without an `else`:
    ```ts
    // {#if expr}
    //   ...
    //   {:else if otherExpr} ...
    // {/if}
    
    if (rf & 1) {
      ɵɵtemplate(0, App_Conditional_0_Template, 0, 0);
      ɵɵtemplate(1, App_Conditional_1_Template, 0, 0);
    }
    if (rf & 2) {
      ɵɵconditional(0, ctx.expr ? 0 : ctx.otherExpr ? 1 : -1);
    }
    ```
    
    If a conditional captures it's value in an alias (e.g. `{#if expr; as foo}`) we need to assign the value to a temporary variable before passing it along to `conditional`.
    
    ```ts
    // {#if expr; as alias}...{/if}
    if (rf & 1) {
      ɵɵtemplate(0, App_Conditional_0_Template, 1, 0);
    }
    if (rf & 2) {
      let App_contFlowTmp;
      ɵɵconditional(0, (App_contFlowTmp = ctx.expr) ? 0 : -1, App_contFlowTmp);
    }
    ```
    
    PR Close #51380

---
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_control_flow/if_nested_alias.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+  template: `
+    {#if value(); as root}
+      Root: {{value()}}/{{root}}
+      {#if value(); as inner}
+        Inner: {{value()}}/{{root}}/{{inner}}
+        {#if value(); as innermost}
+          Innermost: {{value()}}/{{root}}/{{inner}}/{{innermost}}
+        {/if}
+      {/if}
+    {/if}
+  `,
+})
+export class MyApp {
+  value = () => 1;
+}
