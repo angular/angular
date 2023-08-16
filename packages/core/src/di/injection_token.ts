dc4f77c8518a36a12fbd8b64d31189b071c405fb
Luis Castro <luish.castroc@gmail.com>
2023-08-16 12:10:01 -0400

    docs(core): clarify InjectionToken usage (#51386)
    - Emphasized the importance of using the same InjectionToken instance for both provider and injection call.
    - Added examples to illustrate correct usages to prevent NullInjectorError.
    
    PR Close #51386

--- packages/core/src/di/injection_token.ts
+++ packages/core/src/di/injection_token.ts
@@ -21,11 +21,17 @@ import {ɵɵdefineInjectable} from './interface/defs';
  * `InjectionToken` is parameterized on `T` which is the type of object which will be returned by
  * the `Injector`. This provides an additional level of type safety.
  *
- * ```
- * interface MyInterface {...}
- * const myInterface = injector.get(new InjectionToken<MyInterface>('SomeToken'));
- * // myInterface is inferred to be MyInterface.
- * ```
+ * <div class="alert is-helpful">
+ *
+ * **Important Note**: Ensure that you use the same instance of the `InjectionToken` in both the
+ * provider and the injection call. Creating a new instance of `InjectionToken` in different places,
+ * even with the same description, will be treated as different tokens by Angular's DI system,
+ * leading to a `NullInjectorError`.
+ *
+ * </div>
+ *
+ * <code-example format="typescript" language="typescript" path="injection-token/src/main.ts"
+ * region="InjectionToken"></code-example>
  *
  * When creating an `InjectionToken`, you can optionally specify a factory function which returns
  * (possibly by creating) a default value of the parameterized type `T`. This sets up the
@@ -51,7 +57,6 @@ import {ɵɵdefineInjectable} from './interface/defs';
  * ### Tree-shakable InjectionToken
  *
  * {@example core/di/ts/injector_spec.ts region='ShakableInjectionToken'}
- *
  *
  * @publicApi
  */
