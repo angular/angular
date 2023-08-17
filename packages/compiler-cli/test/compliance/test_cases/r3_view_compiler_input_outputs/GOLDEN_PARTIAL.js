39ace8664b843225a75dd706236ae13e171126d3
JoostK <joost.koehoorn@gmail.com>
2023-08-17 22:14:38 +0200

    fix(compiler-cli): enforce a minimum version to be used when a library uses input transform (#51413)
    Angular 16.1 introduced the input transform feature, requiring the partial compilation output to be extended
    with a reference to the input transform function. This has resulted in a subtle breaking change, where older
    versions of the Angular linker can no longer consume libraries that have started to use this feature.
    
    We do try to support using a 16.1 library from an Angular 16.0 application, but if a library actually
    adopts a new feature then this is no longer possible. In such cases, it is desirable to report a message
    telling the user that their version of the Angular compiler is too old, as determined by the `"minVersion"`
    property that is present in each partial declaration. This version would still indicate that the declaration
    required at least Angular 14.0 to be compiled, but this is not accurate once input transforms are being
    used. Consequently, this error would not be reported, causing a less informative error once the input transform
    was being observed.
    
    Fixes #51411
    
    PR Close #51413

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_input_outputs/GOLDEN_PARTIAL.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_input_outputs/GOLDEN_PARTIAL.js
@@ -111,7 +111,7 @@ function toNumber(value) {
 export class MyDirective {
 }
 MyDirective.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "0.0.0-PLACEHOLDER", ngImport: i0, type: MyDirective, deps: [], target: i0.ɵɵFactoryTarget.Directive });
-MyDirective.ɵdir = i0.ɵɵngDeclareDirective({ minVersion: "14.0.0", version: "0.0.0-PLACEHOLDER", type: MyDirective, selector: "[my-directive]", inputs: { functionDeclarationInput: ["functionDeclarationInput", "functionDeclarationInput", toNumber], inlineFunctionInput: ["inlineFunctionInput", "inlineFunctionInput", (value, _) => value ? 1 : 0] }, ngImport: i0 });
+MyDirective.ɵdir = i0.ɵɵngDeclareDirective({ minVersion: "16.1.0", version: "0.0.0-PLACEHOLDER", type: MyDirective, selector: "[my-directive]", inputs: { functionDeclarationInput: ["functionDeclarationInput", "functionDeclarationInput", toNumber], inlineFunctionInput: ["inlineFunctionInput", "inlineFunctionInput", (value, _) => value ? 1 : 0] }, ngImport: i0 });
 i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "0.0.0-PLACEHOLDER", ngImport: i0, type: MyDirective, decorators: [{
             type: Directive,
             args: [{ selector: '[my-directive]' }]
