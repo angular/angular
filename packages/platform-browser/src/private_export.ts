0eae4261745833cf59d308c414e0660febf0d573
Matthieu Riegler <kyro38@gmail.com>
2023-08-17 21:05:29 +0200

    refactor(platform-browser): remove unused `ɵgetDOM` re-export
    That re-export `ɵgetDOM` does not serve any purpose anymore.

--- packages/platform-browser/src/private_export.ts
+++ packages/platform-browser/src/private_export.ts
@@ -6,7 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-export {ɵgetDOM} from '@angular/common';
 export {initDomAdapter as ɵinitDomAdapter, INTERNAL_BROWSER_PLATFORM_PROVIDERS as ɵINTERNAL_BROWSER_PLATFORM_PROVIDERS} from './browser';
 export {BrowserDomAdapter as ɵBrowserDomAdapter} from './browser/browser_adapter';
 export {BrowserGetTestability as ɵBrowserGetTestability} from './browser/testability';
