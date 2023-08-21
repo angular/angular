e7da746b303c8a06dd992dda4e6a35297b28bc46
Ben Hong <ben@bencodezen.io>
2023-08-21 11:05:20 -0700

    docs: migrate accessibility guide and example to standalone (#51445)
    PR Close #51445

--- aio/content/examples/accessibility/src/app/app.component.ts
+++ aio/content/examples/accessibility/src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ExampleProgressbarComponent} from './progress-bar.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
+  imports: [ExampleProgressbarComponent],
 })
-export class AppComponent  {
+export class AppComponent {
   progress = 0;
 
   setProgress($event: Event) {
