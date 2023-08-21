0f8fe073b0123f9bb66f961c667029ce182c2f8a
Andrew Scott <atscott@google.com>
2023-08-21 15:17:19 -0700

    docs(router): Update the basic router guide to use standalone (#51452)
    This commit updates the basic router-tutorial guide to use standalone features.
    
    PR Close #51452

--- aio/content/examples/router-tutorial/src/app/app.component.ts
+++ aio/content/examples/router-tutorial/src/app/app.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+
+import {CrisisListComponent} from './crisis-list/crisis-list.component';
+import {HeroesListComponent} from './heroes-list/heroes-list.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CrisisListComponent, HeroesListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
