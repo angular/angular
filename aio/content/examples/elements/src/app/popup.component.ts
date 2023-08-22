ef958ce1a4741dc76e8360b73bdaa3731a7147e3
Ben Hong <ben@bencodezen.io>
2023-08-21 17:06:26 -0700

    docs: migrate elements guide and code to standalone (#51455)
    PR Close #51455

--- aio/content/examples/elements/src/app/popup.component.ts
+++ aio/content/examples/elements/src/app/popup.component.ts
@@ -1,11 +1,12 @@
 // #docregion
-import { Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {Component, EventEmitter, HostBinding, Input, Output} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
+  standalone: true,
   selector: 'my-popup',
   template: `
-    <span>Popup: {{message}}</span>
+    <span>Popup: {{ message }}</span>
     <button type="button" (click)="closed.next()">&#x2716;</button>
   `,
   animations: [
@@ -13,35 +14,39 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
       state('opened', style({transform: 'translateY(0%)'})),
       state('void, closed', style({transform: 'translateY(100%)', opacity: 0})),
       transition('* => *', animate('100ms ease-in')),
-    ])
+    ]),
   ],
-  styles: [`
-    :host {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      background: #009cff;
-      height: 48px;
-      padding: 16px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      border-top: 1px solid black;
-      font-size: 24px;
-    }
+  styles: [
+    `
+      :host {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: #009cff;
+        height: 48px;
+        padding: 16px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border-top: 1px solid black;
+        font-size: 24px;
+      }
 
-    button {
-      border-radius: 50%;
-    }
-  `]
+      button {
+        border-radius: 50%;
+      }
+    `,
+  ],
 })
 export class PopupComponent {
   @HostBinding('@state')
   state: 'opened' | 'closed' = 'closed';
 
   @Input()
-  get message(): string { return this._message; }
+  get message(): string {
+    return this._message;
+  }
   set message(message: string) {
     this._message = message;
     this.state = 'opened';
