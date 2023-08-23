6c3e86586ee3d69824a7bc7fa0561a061ab6f4eb
Ben Hong <ben@bencodezen.io>
2023-08-22 18:16:49 -0700

    docs: migrate angular compiler options code to standalone (#51467)
    PR Close #51467

--- aio/content/examples/angular-compiler-options/src/app/app.component.ts
+++ aio/content/examples/angular-compiler-options/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
