9aa71cc8e0a899faaebd05d0eb4f483a99aa0a2b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-08-25 11:11:31 +0000

    feat(bazel): prohibit cross entry-point relative imports (#51500)
    Introduces a check into `ng_package` that will ensure that there are no
    cross entry-point or cross-package relative imports that would end up
    contributing to duplicate code. Not only would duplicate code result in
    size increases, but also it could cause subtle hard-to-debug bugs,
    especially when cross imports rely on e.g. singletons. Like for example
    the deps tracker that is used in angular/core but also in
    angular/core/testing.
    
    PR Close #51500

--- packages/bazel/src/ng_package/packager.ts
+++ packages/bazel/src/ng_package/packager.ts
@@ -9,40 +9,8 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-/**
- * Interface describing a file captured in the Bazel action.
- * https://docs.bazel.build/versions/main/skylark/lib/File.html.
- */
-interface BazelFileInfo {
-  /** Execroot-relative path pointing to the file. */
-  path: string;
-  /** The path of this file relative to its root. e.g. omitting `bazel-out/<..>/bin`. */
-  shortPath: string;
-}
-
-/** Interface describing an entry-point. */
-interface EntryPointInfo {
-  /** ES2022 index file for the APF entry-point. */
-  index: BazelFileInfo;
-  /** Flat ES2022 ES module bundle file. */
-  fesm2022Bundle: BazelFileInfo;
-  /** Index type definition file for the APF entry-point. */
-  typings: BazelFileInfo;
-  /**
-   * Whether the index or typing paths have been guessed. For entry-points built
-   * through `ts_library`, there is no explicit setting that declares the entry-point
-   * so the index file is guessed.
-   */
-  guessedPaths: boolean;
-}
-
-/** Interface capturing relevant metadata for packaging. */
-interface PackageMetadata {
-  /** NPM package name of the output. */
-  npmPackageName: string;
-  /** Record of entry-points (including the primary one) and their info. */
-  entryPoints: Record<string, EntryPointInfo>;
-}
+import {BazelFileInfo, PackageMetadata} from './api';
+import {analyzeFileAndEnsureNoCrossImports} from './cross_entry_points_imports';
 
 /**
  * List of known `package.json` fields which provide information about
@@ -222,7 +190,17 @@ function main(args: string[]): void {
     return getEntryPointSubpath(moduleName) !== '';
   }
 
-  esm2022.forEach(file => writeEsm2022File(file));
+  const crossEntryPointFailures: string[] = [];
+
+  esm2022.forEach(file => {
+    crossEntryPointFailures.push(...analyzeFileAndEnsureNoCrossImports(file, metadata));
+    writeEsm2022File(file);
+  });
+
+  if (crossEntryPointFailures.length) {
+    console.error(crossEntryPointFailures);
+    process.exit(1);
+  }
 
   // Copy all FESM files into the package output.
   fesm2022.forEach(f => copyFile(f.path, getFlatEsmOutputRelativePath(f)));
@@ -408,8 +386,8 @@ function main(args: string[]): void {
   function hasExplicitFormatProperties(parsedPackage: Readonly<PackageJson>): boolean {
     return Object.keys(parsedPackage)
         .some(
-            (fieldName: KnownPackageJsonFormatFields) =>
-                knownFormatPackageJsonFormatFields.includes(fieldName));
+            (fieldName: string) => knownFormatPackageJsonFormatFields.includes(
+                fieldName as KnownPackageJsonFormatFields));
   }
 
   /**
