260cb39c49b2c77b752ececef3e0b2c53677fd80
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-08-25 12:18:57 +0000

    refactor(localize): ignore code duplication error with compiler package (#51500)
    The localize package intentionally duplicates some logic from the
    compiler to avoid adding a dependency. This is now an error in the
    packaging rule to prevent common pitfalls/code duplication. Here it's
    an explicit decision though so we mark it as such and ask for the check
    to be ignored for the particular import.
    
    PR Close #51500

--- aio/content/examples/router/src/app/app.component.4.ts
+++ aio/content/examples/router/src/app/app.component.4.ts
@@ -1,15 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
-})
+@Component(
+    {selector: 'app-root', templateUrl: 'app.component.html', styleUrls: ['app.component.css']})
 export class AppComponent {
-// #docregion relative-to
   goToItems() {
-    this.router.navigate(['items'], { relativeTo: this.route });
+    this.router.navigate(['items'], {relativeTo: this.route});
   }
-// #enddocregion relative-to
-
 }
