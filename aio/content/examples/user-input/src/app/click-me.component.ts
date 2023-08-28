098c79e8c753d85882748c355233ea12f5357f27
Ben Hong <ben@bencodezen.io>
2023-08-28 09:06:57 -0700

    docs: migrate user-input code example to standalone (#51466)
    PR Close #51466

--- aio/content/examples/user-input/src/app/click-me.component.ts
+++ aio/content/examples/user-input/src/app/click-me.component.ts
@@ -5,14 +5,14 @@
 */
 
 // #docregion
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 // #docregion click-me-component
 @Component({
+  standalone: true,
   selector: 'app-click-me',
-  template: `
-    <button type="button" (click)="onClickMe()">Click me!</button>
-    {{clickMessage}}`
+  template: ` <button type="button" (click)="onClickMe()">Click me!</button>
+    {{ clickMessage }}`,
 })
 export class ClickMeComponent {
   clickMessage = '';
