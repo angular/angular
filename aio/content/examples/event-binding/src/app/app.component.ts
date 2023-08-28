7ed4c56c24d8340db06f2c79924b1e1b399c1e0c
Ben Hong <ben@bencodezen.io>
2023-08-28 10:01:42 -0700

    docs: migrate event-binding code to standalone (#51538)
    PR Close #51538

--- aio/content/examples/event-binding/src/app/app.component.ts
+++ aio/content/examples/event-binding/src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
-import { Item } from './item';
+import {Component} from '@angular/core';
+import {Item} from './item';
+
+import {ItemDetailComponent} from './item-detail/item-detail.component';
+import {ClickDirective} from './click.directive';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [ItemDetailComponent, ClickDirective],
 })
 export class AppComponent {
-
-  currentItem = { name: 'teapot'} ;
+  currentItem = {name: 'teapot'};
   clickMessage = '';
 
   onSave(event?: MouseEvent) {
     const evtMsg = event ? ' Event target is ' + (event.target as HTMLElement).textContent : '';
     alert('Saved.' + evtMsg);
-    if (event) { event.stopPropagation(); }
+    if (event) {
+      event.stopPropagation();
+    }
   }
 
   deleteItem(item: Item) {
@@ -22,7 +28,7 @@ export class AppComponent {
   }
 
   onClickMe(event?: MouseEvent) {
-    const evtMsg = event ? ' Event target class is ' + (event.target as HTMLElement).className  : '';
+    const evtMsg = event ? ' Event target class is ' + (event.target as HTMLElement).className : '';
     alert('Click me.' + evtMsg);
   }
 
