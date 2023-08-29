a5bbfd0e97b7f21dcd5238a6bfe14608969b4f2c
Andrew Scott <atscott@google.com>
2023-08-29 12:04:43 -0700

    docs: update router reference to use standalone
    this commit updates the router reference to use standalone features.

--- aio/content/examples/router/src/app/app.module.1.ts
+++ aio/content/examples/router/src/app/app.module.1.ts
@@ -1,50 +1,45 @@
 // #docplaster
 // #docregion
 // #docregion first-config
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-// #docregion import-router
-import { RouterModule, Routes } from '@angular/router';
-// #enddocregion import-router
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
+import {AppComponent} from './app.component';
+import {CrisisListComponent} from './crisis-list/crisis-list.component';
+import {HeroListComponent} from './hero-list/hero-list.component';
 // #enddocregion first-config
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
 // #docregion first-config
 
 const appRoutes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes', component: HeroListComponent },
-// #enddocregion first-config
+  {path: 'crisis-center', component: CrisisListComponent},
+  {path: 'heroes', component: HeroListComponent},
+  // #enddocregion first-config
 
-  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
-// #docregion wildcard
-  { path: '**', component: PageNotFoundComponent }
-// #enddocregion wildcard
-// #docregion first-config
+  {path: '', redirectTo: '/heroes', pathMatch: 'full'},
+  // #docregion wildcard
+  {path: '**', component: PageNotFoundComponent}  // #enddocregion wildcard
+                                                  // #docregion first-config
 ];
 
 @NgModule({
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule, FormsModule,
     RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+        appRoutes, {enableTracing: true}  // <-- debugging purposes only
+        )
   ],
   declarations: [
-    AppComponent,
-    HeroListComponent,
-    CrisisListComponent,
-// #enddocregion first-config
+    AppComponent, HeroListComponent, CrisisListComponent,
+    // #enddocregion first-config
     PageNotFoundComponent
-// #docregion first-config
+    // #docregion first-config
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
 // #enddocregion
