aca5b26458c405136cdc8321a56a9350642d612c
Dylan Hunn <dylhunn@gmail.com>
2023-08-28 21:29:54 -0700

    refactor(compiler): Enable some additional passing tests for template pipeline (#51544)
    A couple more tests pertaining to animations became passing at some point recently, as well as a few form other assorted areas.
    
    PR Close #51544

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_listener/implicit_receiver_keyed_write_inside_template_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_listener/implicit_receiver_keyed_write_inside_template_template.js
@@ -1,9 +1,9 @@
 function MyComponent_ng_template_0_Template(rf, $ctx$) {
   if (rf & 1) {
-    const _r3 = $i0$.ɵɵgetCurrentView();
+    const $_r3$ = $i0$.ɵɵgetCurrentView();
     $i0$.ɵɵelementStart(0, "button", 1);
     $i0$.ɵɵlistener("click", function MyComponent_ng_template_0_Template_button_click_0_listener() {
-      $i0$.ɵɵrestoreView(_r3);
+      $i0$.ɵɵrestoreView($_r3$);
       const $ctx_2$ = $i0$.ɵɵnextContext();
       return $i0$.ɵɵresetView(($ctx_2$["mes" + "sage"] = "hello"));
     });
