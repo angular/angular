685d01e1065dad6dc52eaac9eb9527100994f5ce
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-08-29 09:31:29 +0200

    perf(core): chain template instructions (#51546)
    With the new control flow and defer blocks it'll be common for several template instructions to be declare one after another. These changes add support for chaining to the `template` instruction which will allow us to save some bytes.
    
    PR Close #51546

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_control_flow/nested_switch_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_control_flow/nested_switch_template.js
@@ -24,9 +24,7 @@ function MyApp_Case_3_Case_2_Template(rf, ctx) {
 
 function MyApp_Case_3_Template(rf, ctx) {
   if (rf & 1) {
-    $r3$.ɵɵtemplate(0, MyApp_Case_3_Case_0_Template, 1, 0);
-    $r3$.ɵɵtemplate(1, MyApp_Case_3_Case_1_Template, 1, 0);
-    $r3$.ɵɵtemplate(2, MyApp_Case_3_Case_2_Template, 1, 0);
+    $r3$.ɵɵtemplate(0, MyApp_Case_3_Case_0_Template, 1, 0)(1, MyApp_Case_3_Case_1_Template, 1, 0)(2, MyApp_Case_3_Case_2_Template, 1, 0);
   }
   if (rf & 2) {
     const ctx_r1 = $r3$.ɵɵnextContext();
@@ -45,9 +43,7 @@ function MyApp_Template(rf, ctx) {
   if (rf & 1) {
     $r3$.ɵɵelementStart(0, "div");
     $r3$.ɵɵtext(1);
-    $r3$.ɵɵtemplate(2, MyApp_Case_2_Template, 1, 0);
-    $r3$.ɵɵtemplate(3, MyApp_Case_3_Template, 3, 4);
-    $r3$.ɵɵtemplate(4, MyApp_Case_4_Template, 1, 0);
+    $r3$.ɵɵtemplate(2, MyApp_Case_2_Template, 1, 0)(3, MyApp_Case_3_Template, 3, 4)(4, MyApp_Case_4_Template, 1, 0);
     $r3$.ɵɵelementEnd();
   }
   if (rf & 2) {
