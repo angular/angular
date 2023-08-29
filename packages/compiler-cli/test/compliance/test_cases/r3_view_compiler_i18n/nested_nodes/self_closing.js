c26918499dd6edeec8a88c89bff925880c91374c
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-08-29 09:31:29 +0200

    perf(core): chain template instructions
    With the new control flow and defer blocks it'll be common for several template instructions to be declare one after another. These changes add support for chaining to the `template` instruction which will allow us to save some bytes.

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_i18n/nested_nodes/self_closing.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_i18n/nested_nodes/self_closing.js
@@ -33,8 +33,7 @@ consts: function() {
 template: function MyComponent_Template(rf, ctx) {
   if (rf & 1) {
     $r3$.ɵɵelement(0, "img", 0);
-    $r3$.ɵɵtemplate(1, MyComponent_img_1_Template, 1, 0, "img", 1);
-    $r3$.ɵɵtemplate(2, MyComponent_img_2_Template, 2, 1, "img", 2);
+    $r3$.ɵɵtemplate(1, MyComponent_img_1_Template, 1, 0, "img", 1)(2, MyComponent_img_2_Template, 2, 1, "img", 2);
   }
   if (rf & 2) {
     $r3$.ɵɵadvance(1);
