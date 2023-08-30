3f2d783f65bcd96d1855f506676e7ce385a84a9b
Ben Hong <ben@bencodezen.io>
2023-08-30 10:58:03 -0700

    docs: migrate toh-pt3 to standalone (#51581)
    PR Close #51581

--- aio/content/examples/toh-pt3/src/app/app.component.ts
+++ aio/content/examples/toh-pt3/src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HeroesComponent} from './heroes/heroes.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [HeroesComponent],
 })
 export class AppComponent {
   title = 'Tour of Heroes';
