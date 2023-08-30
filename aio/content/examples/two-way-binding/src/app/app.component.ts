cc24249f1a607a763fc17135fc2a72be3c6abb86
Ben Hong <ben@bencodezen.io>
2023-08-30 09:17:30 -0700

    docs: migrate two-way-binding to standalone (#51574)
    PR Close #51574

--- aio/content/examples/two-way-binding/src/app/app.component.ts
+++ aio/content/examples/two-way-binding/src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SizerComponent} from './sizer/sizer.component';
+import {FormsModule} from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [FormsModule, SizerComponent],
 })
 export class AppComponent {
   // #docregion font-size
--- aio/content/examples/two-way-binding/src/main.ts
+++ aio/content/examples/two-way-binding/src/main.ts
@@ -1,7 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+import {AppComponent} from './app/app.component';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport()],
+});
