7f6d9a73ab8b658d0d8148080dfefb2550bee6b4
Jeremy Elbourn <jelbourn@google.com>
2023-09-06 16:53:08 -0700

    feat(compiler): expand class api doc extraction (#51733)
    Based on top of #51682
    
    This expands on the skeleton previously added to extract docs info for classes, including properties, methods, and method parameters. Type information and Angular-specific info (e.g. inputs) will come in future PRs.
    
    PR Close #51733

--- packages/compiler-cli/src/ngtsc/docs/src/entities.ts
+++ packages/compiler-cli/src/ngtsc/docs/src/entities.ts
@@ -6,7 +6,73 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+/** Type of top-level documentation entry. */
+export enum EntryType {
+  block = 'block',
+  component = 'component',
+  decorator = 'decorator',
+  directive = 'directive',
+  element = 'element',
+  enum = 'enum',
+  function = 'function',
+  interface = 'interface',
+  pipe = 'pipe',
+  type_alias = 'type_alias',
+  undecorated_class = 'undecorated_class',
+}
+
+/** Types of class members */
+export enum MemberType {
+  property = 'property',
+  method = 'method',
+}
+
+/** Informational tags applicable to class members. */
+export enum MemberTags {
+  static = 'static',
+  readonly = 'readonly',
+  protected = 'protected',
+  optional = 'optional',
+  input = 'input',
+  output = 'output',
+}
+
 /** Base type for all documentation entities. */
 export interface DocEntry {
+  entryType: EntryType;
   name: string;
+}
+
+/** Documentation entity for a TypeScript class. */
+export interface ClassEntry extends DocEntry {
+  members: MemberEntry[];
+}
+
+export interface FunctionEntry extends DocEntry {
+  params: ParameterEntry[];
+  returnType: string;
+}
+
+/** Sub-entry for a single class member. */
+export interface MemberEntry {
+  name: string;
+  memberType: MemberType;
+  memberTags: MemberTags[];
+}
+
+/** Sub-entry for a class property. */
+export interface PropertyEntry extends MemberEntry {
+  getType: string;
+  setType: string;
+}
+
+/** Sub-entry for a class method. */
+export type MethodEntry = MemberEntry&FunctionEntry;
+
+/** Sub-entry for a single function parameter. */
+export interface ParameterEntry {
+  name: string;
+  description: string;
+  type: string;
+  isOptional: boolean;
 }
