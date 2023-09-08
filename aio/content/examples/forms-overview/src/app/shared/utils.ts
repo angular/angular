4e0a5752626cfc713c30370cda3c0227ecddd248
Ben Hong <ben@bencodezen.io>
2023-09-08 06:28:12 -0700

    docs: migrate forms-overview to standalone (#51699)
    PR Close #51699

--- aio/content/examples/forms-overview/src/app/shared/utils.ts
+++ aio/content/examples/forms-overview/src/app/shared/utils.ts
@@ -1,4 +1,8 @@
-export function createNewEvent(eventName: string, bubbles = false, cancelable = false) {
+export function createNewEvent(
+  eventName: string,
+  bubbles = false,
+  cancelable = false,
+) {
   const evt = document.createEvent('CustomEvent');
   evt.initCustomEvent(eventName, bubbles, cancelable, null);
   return evt;
