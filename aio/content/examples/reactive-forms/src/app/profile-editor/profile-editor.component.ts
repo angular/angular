65f768676981bdbf55f1434245716e154a4a7a60
Ben Hong <ben@bencodezen.io>
2023-09-08 10:08:02 -0700

    docs: migrate reactive-forms to standalone (#51707)
    PR Close #51707

--- aio/content/examples/reactive-forms/src/app/profile-editor/profile-editor.component.ts
+++ aio/content/examples/reactive-forms/src/app/profile-editor/profile-editor.component.ts
@@ -1,18 +1,21 @@
 // #docplaster
+import { JsonPipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 // #docregion validator-imports
 import { Validators } from '@angular/forms';
 // #enddocregion validator-imports
 import { FormArray } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
-  styleUrls: ['./profile-editor.component.css']
+  styleUrls: ['./profile-editor.component.css'],
+  imports: [NgFor, ReactiveFormsModule, JsonPipe],
 })
 export class ProfileEditorComponent {
-// #docregion required-validator, aliases
+  // #docregion required-validator, aliases
   profileForm = this.fb.group({
     firstName: ['', Validators.required],
     lastName: [''],
@@ -20,43 +23,41 @@ export class ProfileEditorComponent {
       street: [''],
       city: [''],
       state: [''],
-      zip: ['']
+      zip: [''],
     }),
-// #enddocregion required-validator
-    aliases: this.fb.array([
-      this.fb.control('')
-    ])
-// #docregion required-validator
+    // #enddocregion required-validator
+    aliases: this.fb.array([this.fb.control('')]),
+    // #docregion required-validator
   });
-// #enddocregion required-validator, aliases
-// #docregion aliases-getter
+  // #enddocregion required-validator, aliases
+  // #docregion aliases-getter
 
   get aliases() {
     return this.profileForm.get('aliases') as FormArray;
   }
 
-// #enddocregion aliases-getter
-  constructor(private fb: FormBuilder) { }
+  // #enddocregion aliases-getter
+  constructor(private fb: FormBuilder) {}
 
   updateProfile() {
     this.profileForm.patchValue({
       firstName: 'Nancy',
       address: {
-        street: '123 Drew Street'
-      }
+        street: '123 Drew Street',
+      },
     });
   }
-// #docregion add-alias
+  // #docregion add-alias
 
   addAlias() {
     this.aliases.push(this.fb.control(''));
   }
-// #enddocregion add-alias
-// #docregion on-submit
+  // #enddocregion add-alias
+  // #docregion on-submit
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
   }
-// #enddocregion on-submit
+  // #enddocregion on-submit
 }
