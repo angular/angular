3ff788207727ce037a69f095f8c65e9acf69fa6c
Ben Hong <ben@bencodezen.io>
2023-09-08 10:08:02 -0700

    docs: migrate reactive-forms to standalone

--- aio/content/examples/reactive-forms/src/main.ts
+++ aio/content/examples/reactive-forms/src/main.ts
@@ -1,6 +1,5 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig);
