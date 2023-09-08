456a0342bfbceacb981616075f87a4a129cc31f3
Andrew Scott <atscott@google.com>
2023-09-08 11:52:03 -0700

    fix(docs-infra): Ensure experimental tag shows up on docs (#51712)
    The experimental tag is part of the stability tags along with "deprecated". This commit updates some code to pick up experimental as well.
    
    PR Close #51712

--- aio/tools/transforms/angular-api-package/index.js
+++ aio/tools/transforms/angular-api-package/index.js
@@ -191,6 +191,7 @@ module.exports =
             'description',
             'security',
             'deprecated',
+            'experimental',
             'see',
             'usageNotes',
           ];
---
+++ aio/tools/transforms/angular-api-package/tag-defs/experimental.js
@@ -0,0 +1,4 @@
+module.exports = function() {
+    return {name: 'experimental'};
+  };
+  
