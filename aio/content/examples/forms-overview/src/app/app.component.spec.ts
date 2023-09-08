d1c845c0bc7a805ee508b0002d950814f335ae9d
Ben Hong <ben@bencodezen.io>
2023-09-08 06:28:12 -0700

    docs: migrate forms-overview to standalone

--- aio/content/examples/forms-overview/src/app/app.component.spec.ts
+++ aio/content/examples/forms-overview/src/app/app.component.spec.ts
@@ -1,31 +1,28 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
-import { ReactiveModule } from './reactive/reactive.module';
-import { TemplateModule } from './template/template.module';
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
-    TestBed
-        .configureTestingModule({
-          imports: [ReactiveModule, TemplateModule],
-          declarations: [AppComponent],
-        })
-        .compileComponents();
+    TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
   }));
 
   it('should create the app', waitForAsync(() => {
-       const fixture = TestBed.createComponent(AppComponent);
-       const app = fixture.componentInstance;
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
 
-       expect(app).toBeTruthy();
-     }));
+    expect(app).toBeTruthy();
+  }));
 
   it('should render title', waitForAsync(() => {
-       const fixture = TestBed.createComponent(AppComponent);
-       fixture.detectChanges();
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
 
-       const compiled = fixture.nativeElement as HTMLElement;
-       expect(compiled.querySelector('h1')?.textContent).toContain('Forms Overview');
-     }));
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Forms Overview',
+    );
+  }));
 });
