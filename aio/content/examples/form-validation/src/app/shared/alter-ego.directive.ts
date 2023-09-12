a2eab5b5a29ed4cfbc6ae6467ff2e983edff30ea
Ben Hong <ben@bencodezen.io>
2023-09-12 09:32:26 -0700

    docs: migrate form-validation to standalone

--- aio/content/examples/form-validation/src/app/shared/alter-ego.directive.ts
+++ aio/content/examples/form-validation/src/app/shared/alter-ego.directive.ts
@@ -3,7 +3,7 @@ import {
   AsyncValidator,
   AbstractControl,
   NG_ASYNC_VALIDATORS,
-  ValidationErrors
+  ValidationErrors,
 } from '@angular/forms';
 import { catchError, map } from 'rxjs/operators';
 import { HeroesService } from './heroes.service';
@@ -14,12 +14,10 @@ import { Observable, of } from 'rxjs';
 export class UniqueAlterEgoValidator implements AsyncValidator {
   constructor(private heroesService: HeroesService) {}
 
-  validate(
-    control: AbstractControl
-  ): Observable<ValidationErrors | null> {
+  validate(control: AbstractControl): Observable<ValidationErrors | null> {
     return this.heroesService.isAlterEgoTaken(control.value).pipe(
-      map(isTaken => (isTaken ? { uniqueAlterEgo: true } : null)),
-      catchError(() => of(null))
+      map((isTaken) => (isTaken ? { uniqueAlterEgo: true } : null)),
+      catchError(() => of(null)),
     );
   }
 }
@@ -32,16 +30,15 @@ export class UniqueAlterEgoValidator implements AsyncValidator {
     {
       provide: NG_ASYNC_VALIDATORS,
       useExisting: forwardRef(() => UniqueAlterEgoValidatorDirective),
-      multi: true
-    }
-  ]
+      multi: true,
+    },
+  ],
+  standalone: true,
 })
 export class UniqueAlterEgoValidatorDirective implements AsyncValidator {
   constructor(private validator: UniqueAlterEgoValidator) {}
 
-  validate(
-    control: AbstractControl
-  ): Observable<ValidationErrors | null> {
+  validate(control: AbstractControl): Observable<ValidationErrors | null> {
     return this.validator.validate(control);
   }
 }
