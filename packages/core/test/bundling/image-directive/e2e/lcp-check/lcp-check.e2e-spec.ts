07f0356af7dc3c0d5f6b6162ceb9c7418b6358d7
Alex Castle <atcastle@gmail.com>
2023-09-12 15:45:14 -0700

    feat(common): make the warning for lazy-loaded lcp image an error (#51748)
    upgrade the warning for lazy-loaded lcp images when using NgOptimizedImage to an error
    
    BREAKING CHANGE:
    
    Previously when NgOptimizedImage directive detected that an LCP image is lazy-loaded, a console warning was produced. Now the directive throws an error to make it more discoverable in a console. If you receive this error, refer to this guide for additional information: https://angular.io/guide/image-directive#step-4-mark-images-as-priority
    
    PR Close #51748

--- packages/core/test/bundling/image-directive/e2e/lcp-check/lcp-check.e2e-spec.ts
+++ packages/core/test/bundling/image-directive/e2e/lcp-check/lcp-check.e2e-spec.ts
@@ -27,12 +27,11 @@ describe('NgOptimizedImage directive', () => {
     srcB = await imgs.get(2).getAttribute('src');
     expect(srcB.endsWith('b.png')).toBe(true);
 
-    // Make sure that only one warning is in the console for image `a.png`,
+    // Make sure that only one error is in the console for image `a.png`,
     // since the `b.png` should be below the fold and not treated as an LCP element.
-    const logs = await collectBrowserLogs(logging.Level.WARNING);
-    expect(logs.length).toEqual(2);
-    // Verify that the error code and the image src are present in the error message.
-    expect(logs[0].message).toMatch(/NG02955.*?a\.png/);
-    expect(logs[1].message).toMatch(/NG02964.*?logo-500w\.jpg/);
+    const logs = await collectBrowserLogs(logging.Level.SEVERE);
+    expect(logs.length).toEqual(1);
+
+    expect(logs[0].message).toMatch(/RuntimeError: NG02955/);
   });
 });
