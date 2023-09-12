6da0893b29bbb51407bc600dcb8b0b378e6effdb
Ben Hong <ben@bencodezen.io>
2023-09-12 09:32:26 -0700

    docs: migrate form-validation to standalone (#51709)
    PR Close #51709

--- aio/content/examples/form-validation/src/main.ts
+++ aio/content/examples/form-validation/src/main.ts
@@ -1,7 +1,6 @@
 // #docregion
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig);
