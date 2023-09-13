e2cbe464a8a5630f68a02517a165800644ae54c0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-13 16:16:18 +0200

    refactor(core): make sure that destroyed watch nodes dont run again
    This commit moves the destroy logic from 'effect' in the lower-level
    'watch' so this implementation is shared among varius watch implementations.

--- packages/core/src/signals/src/watch.ts
+++ packages/core/src/signals/src/watch.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {consumerAfterComputation, consumerBeforeComputation, consumerMarkDirty, consumerPollProducersForChange, REACTIVE_NODE, ReactiveNode} from './graph';
+import {consumerAfterComputation, consumerBeforeComputation, consumerDestroy, consumerMarkDirty, consumerPollProducersForChange, isInNotificationPhase, REACTIVE_NODE, ReactiveNode} from './graph';
 
 /**
  * A cleanup function that can be optionally registered from the watch logic. If registered, the
@@ -29,7 +29,15 @@ export interface Watch {
    * `schedule` hook is called by `Watch`.
    */
   run(): void;
+
   cleanup(): void;
+
+  /**
+   * Destroy the watcher:
+   * - disconnect it from the reactive graph;
+   * - mark it as destroyed so subsequent run and notify operations are noop.
+   */
+  destroy(): void;
 }
 
 export function watch(
@@ -47,7 +55,32 @@ export function watch(
     node.cleanupFn = cleanupFn;
   };
 
+  function isWatchNodeDestroyed(node: WatchNode) {
+    return node.fn === null && node.schedule === null;
+  }
+
+  function destroyWatchNode(node: WatchNode) {
+    if (!isWatchNodeDestroyed(node)) {
+      consumerDestroy(node);  // disconnect watcher from the reactive graph
+      node.cleanupFn();
+
+      // nullify references to the integration functions to mark node as destroyed
+      node.fn = null;
+      node.schedule = null;
+      node.cleanupFn = NOOP_CLEANUP_FN;
+    }
+  }
+
   const run = () => {
+    if (node.fn === null) {
+      // trying to run a destroyed watch is noop
+      return;
+    }
+
+    if (isInNotificationPhase()) {
+      throw new Error(`Schedulers cannot synchronously execute watches while scheduling.`);
+    }
+
     node.dirty = false;
     if (node.hasRun && !consumerPollProducersForChange(node)) {
       return;
@@ -68,6 +101,7 @@ export function watch(
     notify: () => consumerMarkDirty(node),
     run,
     cleanup: () => node.cleanupFn(),
+    destroy: () => destroyWatchNode(node),
   };
 
   return node.ref;
@@ -77,8 +111,8 @@ const NOOP_CLEANUP_FN: WatchCleanupFn = () => {};
 
 interface WatchNode extends ReactiveNode {
   hasRun: boolean;
-  fn: (onCleanup: WatchCleanupRegisterFn) => void;
-  schedule: (watch: Watch) => void;
+  fn: ((onCleanup: WatchCleanupRegisterFn) => void)|null;
+  schedule: ((watch: Watch) => void)|null;
   cleanupFn: WatchCleanupFn;
   ref: Watch;
 }
@@ -88,7 +122,9 @@ const WATCH_NODE: Partial<WatchNode> = {
   consumerIsAlwaysLive: true,
   consumerAllowSignalWrites: false,
   consumerMarkedDirty: (node: WatchNode) => {
-    node.schedule(node.ref);
+    if (node.schedule !== null) {
+      node.schedule(node.ref);
+    }
   },
   hasRun: false,
   cleanupFn: NOOP_CLEANUP_FN,
