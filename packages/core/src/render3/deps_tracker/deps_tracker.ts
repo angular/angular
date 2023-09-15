873e80b869f0f1817d8ccc5683297badfc5aaa6a
Payam Valadkhan <payamv@google.com>
2023-09-14 21:13:48 -0400

    refactor(core): fix NgModule compilation scope in deps tracker (#51791)
    Currently deps tracker includes the exported scope of the exported NgModule only in the exported scope of that NgModule. This is in agreement with what AoT does today. But JIT diverges from this behavior by including these exported scopes into the compilation scope as well. Since deps tracker is going to be used for both AoT (local compilation mode) and JIT, the question might be which behavior the deps tracker should follow? Today it follows the AoT one, but it breaks some tests in Google which seem to depend on this behavior of JIT. So it is better to migrate deps tracker to what JIT does. This leads to a wider compilation scope in local compilation compared to full compilations, but it won't break any existing thing.
    
    PR Close #51791

--- packages/core/src/render3/deps_tracker/deps_tracker.ts
+++ packages/core/src/render3/deps_tracker/deps_tracker.ts
@@ -209,6 +209,12 @@ class DepsTracker implements DepsTrackerApi {
         addSet(exportedScope.exported.directives, scope.exported.directives);
         addSet(exportedScope.exported.pipes, scope.exported.pipes);
 
+        // Some test toolings which run in JIT mode depend on this behavior that the exported scope
+        // should also be present in the compilation scope, even though AoT does not support this
+        // and it is also in odds with NgModule metadata definitions. Without this some tests in
+        // Google will fail.
+        addSet(exportedScope.exported.directives, scope.compilation.directives);
+        addSet(exportedScope.exported.pipes, scope.compilation.pipes);
       } else if (isPipe(exported)) {
         scope.exported.pipes.add(exported);
       } else {
