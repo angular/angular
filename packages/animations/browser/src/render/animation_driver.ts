b8a510eb7f95eda77709e8361c5406c28056854e
Matthieu Riegler <kyro38@gmail.com>
2023-09-21 01:13:46 +0200

    refactor(animations): deprecation of `AnimationDriver.NOOP` (#51843)
    The `NoopAnimationDriver` as static property of `AnimationDriver` prevents it from being removed by tree shaking. This commit deprecates it and exposes the `NoopAnimationDriver` on the public API to replace its usage.
    
    DEPRECATED:
    The `AnimationDriver.NOOP` symbol is deprecated, use `NoopAnimationDriver` instead.
    
    PR Close #51843

--- packages/animations/browser/src/render/animation_driver.ts
+++ packages/animations/browser/src/render/animation_driver.ts
@@ -10,33 +10,61 @@ import {Injectable} from '@angular/core';
 
 import {containsElement, getParentElement, invokeQuery, validateStyleProperty} from './shared';
 
+/**
+ * @publicApi
+ *
+ * `AnimationDriver` implentation for Noop animations
+ */
 @Injectable()
 export class NoopAnimationDriver implements AnimationDriver {
+  /**
+   * @returns Whether `prop` is a valid CSS property
+   */
   validateStyleProperty(prop: string): boolean {
     return validateStyleProperty(prop);
   }
 
+  /**
+   * @deprecated unused
+   */
   matchesElement(_element: any, _selector: string): boolean {
     // This method is deprecated and no longer in use so we return false.
     return false;
   }
 
+  /**
+   *
+   * @returns Whether elm1 contains elm2.
+   */
   containsElement(elm1: any, elm2: any): boolean {
     return containsElement(elm1, elm2);
   }
 
+  /**
+   * @returns Rhe parent of the given element or `null` if the element is the `document`
+   */
   getParentElement(element: unknown): unknown {
     return getParentElement(element);
   }
 
+  /**
+   * @returns The result of the query selector on the element. The array will contain up to 1 item
+   *     if `multi` is  `false`.
+   */
   query(element: any, selector: string, multi: boolean): any[] {
     return invokeQuery(element, selector, multi);
   }
 
+  /**
+   * @returns The `defaultValue` or empty string
+   */
   computeStyle(element: any, prop: string, defaultValue?: string): string {
     return defaultValue || '';
   }
 
+  /**
+   * @returns An `NoopAnimationPlayer`
+   */
   animate(
       element: any, keyframes: Array<Map<string, string|number>>, duration: number, delay: number,
       easing: string, previousPlayers: any[] = [],
@@ -49,6 +77,9 @@ export class NoopAnimationDriver implements AnimationDriver {
  * @publicApi
  */
 export abstract class AnimationDriver {
+  /**
+   * @deprecated Use the NoopAnimationDriver class.
+   */
   static NOOP: AnimationDriver = (/* @__PURE__ */ new NoopAnimationDriver());
 
   abstract validateStyleProperty(prop: string): boolean;
