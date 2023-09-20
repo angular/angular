d77c4b2aae03ac636c744866cbb868d343f03793
Andrew Scott <atscott@google.com>
2023-09-20 11:30:37 -0700

    refactor(core): Update view flags so only one number needs adjustment (#51839)
    When adding a new view flag, you currently need to adjust the last number of the last
    3 flags. All of these share the same number so the shifting ones can just use
    the base-10 IndexWithinInitPhaseShift.
    
    PR Close #51839

--- packages/core/src/render3/interfaces/view.ts
+++ packages/core/src/render3/interfaces/view.ts
@@ -436,17 +436,18 @@ export const enum LViewFlags {
   SignalView = 1 << 12,
 
   /**
-   * Index of the current init phase on last 21 bits
-   */
-  IndexWithinInitPhaseIncrementer = 1 << 13,
-  /**
    * This is the count of the bits the 1 was shifted above (base 10)
    */
   IndexWithinInitPhaseShift = 13,
 
+  /**
+   * Index of the current init phase on last 21 bits
+   */
+  IndexWithinInitPhaseIncrementer = 1 << IndexWithinInitPhaseShift,
+
   // Subtracting 1 gives all 1s to the right of the initial shift
   // So `(1 << 3) - 1` would give 3 1s: 1 << 3 = 0b01000, subtract 1 = 0b00111
-  IndexWithinInitPhaseReset = (1 << 13) - 1,
+  IndexWithinInitPhaseReset = (1 << IndexWithinInitPhaseShift) - 1,
 }
 
 /**
