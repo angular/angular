16f5fc40a49cee0d29711df1783f297ff30b5c6e
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-09-21 14:16:29 +0200

    feat(core): support deferred viewport triggers (#51874)
    Adds support for `on viewport` and `prefetch on viewport` triggers which will load the deferred content when the element comes into the view.
    
    PR Close #51874

--- packages/core/src/render3/instructions/defer_events.ts
+++ packages/core/src/render3/instructions/defer_events.ts
@@ -6,6 +6,9 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {inject, Injector, ɵɵdefineInjectable} from '../../di';
+import {NgZone} from '../../zone';
+
 /** Configuration object used to register passive and capturing events. */
 const eventListenerOptions: AddEventListenerOptions = {
   passive: true,
@@ -103,4 +106,78 @@ export function onHover(trigger: Element, callback: VoidFunction): VoidFunction 
       hoverTriggers.delete(trigger);
     }
   };
+}
+
+/**
+ * Registers a viewport trigger.
+ * @param trigger Element that is the trigger.
+ * @param callback Callback to be invoked when the trigger comes into the viewport.
+ * @param injector Injector that can be used by the trigger to resolve DI tokens.
+ */
+export function onViewport(
+    trigger: Element, callback: VoidFunction, injector: Injector): VoidFunction {
+  return injector.get(DeferIntersectionManager).register(trigger, callback);
+}
+
+/** Keeps track of the registered `viewport` triggers. */
+class DeferIntersectionManager {
+  /** @nocollapse */
+  static ɵprov = /** @pureOrBreakMyCode */ ɵɵdefineInjectable({
+    token: DeferIntersectionManager,
+    providedIn: 'root',
+    factory: () => new DeferIntersectionManager(inject(NgZone)),
+  });
+
+  /** `IntersectionObserver` used to observe `viewport` triggers. */
+  private intersectionObserver: IntersectionObserver|null = null;
+
+  /** Number of elements currently observed with `viewport` triggers. */
+  private observedViewportElements = 0;
+
+  /** Currently-registered `viewport` triggers. */
+  private viewportTriggers = new WeakMap<Element, DeferEventEntry>();
+
+  constructor(private ngZone: NgZone) {}
+
+  register(trigger: Element, callback: VoidFunction): VoidFunction {
+    let entry = this.viewportTriggers.get(trigger);
+
+    if (!this.intersectionObserver) {
+      this.intersectionObserver =
+          this.ngZone.runOutsideAngular(() => new IntersectionObserver(this.intersectionCallback));
+    }
+
+    if (!entry) {
+      entry = new DeferEventEntry();
+      this.ngZone.runOutsideAngular(() => this.intersectionObserver!.observe(trigger));
+      this.viewportTriggers.set(trigger, entry);
+      this.observedViewportElements++;
+    }
+
+    entry.callbacks.add(callback);
+
+    return () => {
+      entry!.callbacks.delete(callback);
+
+      if (entry!.callbacks.size === 0) {
+        this.intersectionObserver?.unobserve(trigger);
+        this.viewportTriggers.delete(trigger);
+        this.observedViewportElements--;
+      }
+
+      if (this.observedViewportElements === 0) {
+        this.intersectionObserver?.disconnect();
+        this.intersectionObserver = null;
+      }
+    };
+  }
+
+  private intersectionCallback: IntersectionObserverCallback = entries => {
+    for (const current of entries) {
+      // Only invoke the callbacks if the specific element is intersecting.
+      if (current.isIntersecting && this.viewportTriggers.has(current.target)) {
+        this.ngZone.run(this.viewportTriggers.get(current.target)!.listener);
+      }
+    }
+  }
 }
