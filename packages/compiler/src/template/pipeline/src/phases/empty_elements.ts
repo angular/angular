ac29522c3473c7b3d034324d098d2e4b9e0a63d3
Miles Malerba <mmalerba@google.com>
2023-09-22 20:25:51 -0700

    refactor(compiler): Refactor handling of pipes in i18n blocks (#51876)
    Rather than rely on the empty element collapsing phase to run first, add
    logic to the empty element phase to ignore pipes when deciding whether
    to collapse an element.
    
    PR Close #51876

--- packages/compiler/src/template/pipeline/src/phases/empty_elements.ts
+++ packages/compiler/src/template/pipeline/src/phases/empty_elements.ts
@@ -16,21 +16,35 @@ const REPLACEMENTS = new Map<ir.OpKind, [ir.OpKind, ir.OpKind]>([
 ]);
 
 /**
+ * Op kinds that should not prevent merging of start/end ops.
+ */
+const IGNORED_OP_KINDS = new Set([ir.OpKind.Pipe]);
+
+/**
  * Replace sequences of mergable elements (e.g. `ElementStart` and `ElementEnd`) with a consolidated
  * element (e.g. `Element`).
  */
 export function phaseEmptyElements(job: CompilationJob): void {
   for (const unit of job.units) {
     for (const op of unit.create) {
+      // Find end ops that may be able to be merged.
       const opReplacements = REPLACEMENTS.get(op.kind);
       if (opReplacements === undefined) {
         continue;
       }
       const [startKind, mergedKind] = opReplacements;
-      if (op.prev !== null && op.prev.kind === startKind) {
+
+      // Locate the previous (non-ignored) op.
+      let prevOp: ir.CreateOp|null = op.prev;
+      while (prevOp !== null && IGNORED_OP_KINDS.has(prevOp.kind)) {
+        prevOp = prevOp.prev;
+      }
+
+      // If the previous op is the corresponding start op, we can megre.
+      if (prevOp !== null && prevOp.kind === startKind) {
         // Transmute the start instruction to the merged version. This is safe as they're designed
         // to be identical apart from the `kind`.
-        (op.prev as ir.Op<ir.CreateOp>).kind = mergedKind;
+        (prevOp as ir.Op<ir.CreateOp>).kind = mergedKind;
 
         // Remove the end instruction.
         ir.OpList.remove<ir.CreateOp>(op);
