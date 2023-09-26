591cafbba4682d933bdeefd41354ab7d05147b3e
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-09-26 22:29:36 +0200

    fix(compiler): allocating unnecessary slots in conditional instruction (#51913)
    Fixes that we were allocating slots for the expressions of `if`, `else if`, `switch` and `case` blocks which we weren't using for anything.
    
    PR Close #51913

--- packages/core/test/acceptance/control_flow_exploration_spec.ts
+++ packages/core/test/acceptance/control_flow_exploration_spec.ts
@@ -12,17 +12,17 @@ import {Component, Pipe, PipeTransform} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
 
 describe('control flow', () => {
+  // Basic shared pipe used during testing.
+  @Pipe({name: 'multiply', pure: true, standalone: true})
+  class MultiplyPipe implements PipeTransform {
+    transform(value: number, amount: number) {
+      return value * amount;
+    }
+  }
+
   describe('if', () => {
     beforeEach(() => setEnabledBlockTypes(['if']));
     afterEach(() => setEnabledBlockTypes([]));
-
-    // Basic shared pipe used during testing.
-    @Pipe({name: 'multiply', pure: true, standalone: true})
-    class MultiplyPipe implements PipeTransform {
-      transform(value: number, amount: number) {
-        return value * amount;
-      }
-    }
 
     it('should add and remove views based on conditions change', () => {
       @Component({standalone: true, template: '@if (show) {Something} @else {Nothing}'})
@@ -209,6 +209,38 @@ describe('control flow', () => {
       fixture.detectChanges();
       expect(fixture.nativeElement.textContent).toBe('');
     });
+
+    it('should be able to use pipes in conditional expressions', () => {
+      @Component({
+        standalone: true,
+        imports: [MultiplyPipe],
+        template: `
+          @if ((value | multiply:2) === 2) {
+            one
+          } @else if ((value | multiply:2) === 4) {
+            two
+          } @else {
+            nothing
+          }
+        `,
+      })
+      class TestComponent {
+        value = 0;
+      }
+
+      const fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+
+      expect(fixture.nativeElement.textContent.trim()).toBe('nothing');
+
+      fixture.componentInstance.value = 2;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent.trim()).toBe('two');
+
+      fixture.componentInstance.value = 1;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent.trim()).toBe('one');
+    });
   });
 
   describe('switch', () => {
@@ -245,6 +277,66 @@ describe('control flow', () => {
       fixture.componentInstance.case = 5;
       fixture.detectChanges();
       expect(fixture.nativeElement.textContent).toBe('default');
+    });
+
+    it('should be able to use a pipe in the switch expression', () => {
+      @Component({
+        standalone: true,
+        imports: [MultiplyPipe],
+        template: `
+          @switch (case | multiply:2) {
+            @case (0) {case 0}
+            @case (2) {case 2}
+            @default {default}
+          }
+        `
+      })
+      class TestComponent {
+        case = 0;
+      }
+
+      const fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+
+      expect(fixture.nativeElement.textContent).toBe('case 0');
+
+      fixture.componentInstance.case = 1;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent).toBe('case 2');
+
+      fixture.componentInstance.case = 5;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent).toBe('default');
+    });
+
+    it('should be able to use a pipe in the case expression', () => {
+      @Component({
+        standalone: true,
+        imports: [MultiplyPipe],
+        template: `
+          @switch (case) {
+            @case (1 | multiply:2) {case 2}
+            @case (2 | multiply:2) {case 4}
+            @default {default}
+          }
+        `
+      })
+      class TestComponent {
+        case = 0;
+      }
+
+      const fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+
+      expect(fixture.nativeElement.textContent).toBe('default');
+
+      fixture.componentInstance.case = 4;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent).toBe('case 4');
+
+      fixture.componentInstance.case = 2;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent).toBe('case 2');
     });
   });
 
