193403e7fcb6d8a657f0fa81ac6698b6ed9619b0
Payam Valadkhan <payamv@google.com>
2023-09-26 19:06:01 -0400

    refactor(core): add a new stringifier for runtime errors which includes debug info such as the file path and line number (#51919)
    The current error stringifier only includes the class name. In this change a new stringifier is added which returns a more helpful string which includes the file path and line number. Note that this is only the case with components, and for other class types (directive, pipes) it will fallback to the current stringifier. Subsequent changes can cover the case of directive and pipes as well.
    
    PR Close #51919

--- packages/core/src/render3/util/stringify_utils.ts
+++ packages/core/src/render3/util/stringify_utils.ts
@@ -6,6 +6,9 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {Type} from '../../interface/type';
+import {NG_COMP_DEF} from '../fields';
+
 /**
  * Used for stringify render output in Ivy.
  * Important! This function is very performance-sensitive and we should
@@ -23,6 +26,7 @@ export function renderStringify(value: any): string {
 
 /**
  * Used to stringify a value so that it can be displayed in an error message.
+ *
  * Important! This function contains a megamorphic read and should only be
  * used for error messages.
  */
@@ -33,4 +37,31 @@ export function stringifyForError(value: any): string {
   }
 
   return renderStringify(value);
+}
+
+/**
+ * Used to stringify a `Type` and including the file path and line number in which it is defined, if
+ * possible, for better debugging experience.
+ *
+ * Important! This function contains a megamorphic read and should only be used for error messages.
+ */
+export function debugStringifyTypeForError(type: Type<any>): string {
+  // TODO(pmvald): Do some refactoring so that we can use getComponentDef here without creating
+  // circular deps.
+  let componentDef = (type as any)[NG_COMP_DEF] || null;
+  if (componentDef !== null && componentDef.debugInfo) {
+    return stringifyTypeFromDebugInfo(componentDef.debugInfo);
+  }
+
+  return stringifyForError(type);
+}
+
+// TODO(pmvald): Do some refactoring so that we can use the type ClassDebugInfo for the param
+// debugInfo here without creating circular deps.
+function stringifyTypeFromDebugInfo(debugInfo: any): string {
+  if (!debugInfo.filePath || !debugInfo.lineNumber) {
+    return debugInfo.className;
+  } else {
+    return `${debugInfo.className} (at ${debugInfo.filePath}:${debugInfo.lineNumber})`;
+  }
 }
