c83326e0925e248337ed5639ea681d6fc50c5621
Payam Valadkhan <payamv@google.com>
2023-10-05 18:46:56 -0400

    feat(bazel): make `forbidOrphanComponents` option configurable (#52061)
    Now users can configure the option `forbidOrphanComponents` in the tsconfig's angularCompilerOptions part.
    
    PR Close #52061

--- packages/bazel/src/ngc-wrapped/index.ts
+++ packages/bazel/src/ngc-wrapped/index.ts
@@ -86,6 +86,7 @@ export async function runOneBuild(
     'preserveWhitespaces',
     'createExternalSymbolFactoryReexports',
     'extendedDiagnostics',
+    'forbidOrphanComponents',
   ]);
 
   const userOverrides = Object.entries(userOptions)
