b88cc9eccbed93e8da4e0529f3b5dc7b87da8ee4
Andrew Kushnir <akushnir@google.com>
2023-10-05 18:45:09 -0700

    test(core): add symbol tracking for `@defer` feature (#52065)
    This commit adds a bundling test, which also includes symbol tracking for `@defer` feature.
    
    PR Close #52065

---
+++ packages/core/test/bundling/defer/defer.component.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'defer-cmp',
+  template: `
+    <h2>Defer-loaded component</h2>
+  `,
+})
+export class DeferComponent {
+}
