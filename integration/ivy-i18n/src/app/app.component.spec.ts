27b5e1043d7840abfa8099c5c742a3fae8ae8cb5
cexbrayat <cedric@ninja-squad.com>
2023-10-08 23:00:16 +0200

    refactor(core): remove test.ts files in integration tests
    These files are not necessary since CLI v15+

--- integration/ivy-i18n/src/app/app.component.spec.ts
+++ integration/ivy-i18n/src/app/app.component.spec.ts
@@ -1,14 +1,12 @@
-import {TestBed, async} from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed
-        .configureTestingModule({
-          declarations: [AppComponent],
-        })
-        .compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
