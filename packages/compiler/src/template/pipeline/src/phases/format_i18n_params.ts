ce54ead2b5490e22c18c0d5ac49499a8c8e5a1dc
Miles Malerba <mmalerba@google.com>
2023-10-23 12:55:48 -0700

    refactor(compiler): Split the i18n placeholder mega-phase (#52390)
    The i18n placeholder resolution phase has accumulated too much logic,
    making it difficult to understand. This commit refactors it into several
    smaller phases to make it easier to manage.
    
    I suspect this will undergo further refactoring in the near future as I
    work through the ICU logic. In particular `ExtractedMessageOp` feels
    like a bit of a grab bag of properties, and the i18n const collection
    phase is also starting to get quite heavy. This refactor at least feels
    like a good start.
    
    PR Close #52390

---
+++ packages/compiler/src/template/pipeline/src/phases/format_i18n_params.ts
@@ -0,0 +1,129 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import * as o from '../../../../output/output_ast';
+import * as ir from '../../ir';
+import {ComponentCompilationJob} from '../compilation';
+
+/**
+ * The escape sequence used indicate message param values.
+ */
+const ESCAPE = '\uFFFD';
+
+/**
+ * Marker used to indicate an element tag.
+ */
+const ELEMENT_MARKER = '#';
+
+/**
+ * Marker used to indicate a template tag.
+ */
+const TEMPLATE_MARKER = '*';
+
+/**
+ * Marker used to indicate closing of an element or template tag.
+ */
+const TAG_CLOSE_MARKER = '/';
+
+/**
+ * Marker used to indicate the sub-template context.
+ */
+const CONTEXT_MARKER = ':';
+
+/**
+ * Marker used to indicate the start of a list of values.
+ */
+const LIST_START_MARKER = '[';
+
+/**
+ * Marker used to indicate the end of a list of values.
+ */
+const LIST_END_MARKER = ']';
+
+/**
+ * Delimiter used to separate multiple values in a list.
+ */
+const LIST_DELIMITER = '|';
+
+/**
+ * Formats the param maps on extracted message ops into a maps of `Expression` objects that can be
+ * used in the final output.
+ */
+export function phaseFormatI18nParams(job: ComponentCompilationJob): void {
+  for (const unit of job.units) {
+    for (const op of unit.create) {
+      if (op.kind === ir.OpKind.ExtractedMessage) {
+        if (op.isRoot) {
+          op.formattedParams = formatParams(op.params);
+          op.formattedPostprocessingParams = formatParams(op.postprocessingParams);
+
+          // The message will need post-processing if there are any post-processing params, or if
+          // there are any normal params that have multiple values
+          op.needsPostprocessing = op.postprocessingParams.size > 0;
+          for (const [param, values] of op.params) {
+            if (values.length > 1) {
+              op.needsPostprocessing = true;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+/**
+ * Formats a map of `I18nParamValue[]` values into a map of `Expression` values.
+ */
+function formatParams(params: Map<string, ir.I18nParamValue[]>): Map<string, o.Expression> {
+  const result = new Map<string, o.Expression>();
+  for (const [placeholder, placeholderValues] of [...params].sort()) {
+    const serializedValues = formatParamValues(placeholderValues);
+    if (serializedValues !== null) {
+      result.set(placeholder, o.literal(formatParamValues(placeholderValues)));
+    }
+  }
+  return result;
+}
+
+/**
+ * Formats an `I18nParamValue[]` into a string (or null for empty array).
+ */
+function formatParamValues(values: ir.I18nParamValue[]): string|null {
+  if (values.length === 0) {
+    return null;
+  }
+  const serializedValues = values.map(value => formatValue(value));
+  return serializedValues.length === 1 ?
+      serializedValues[0] :
+      `${LIST_START_MARKER}${serializedValues.join(LIST_DELIMITER)}${LIST_END_MARKER}`;
+}
+
+/**
+ * Formats a single `I18nParamValue` into a string
+ */
+function formatValue(value: ir.I18nParamValue): string {
+  let tagMarker = '';
+  let closeMarker = '';
+  if (value.flags & ir.I18nParamValueFlags.ElementTag) {
+    tagMarker = ELEMENT_MARKER;
+  } else if (value.flags & ir.I18nParamValueFlags.TemplateTag) {
+    tagMarker = TEMPLATE_MARKER;
+  }
+  if (tagMarker !== '') {
+    closeMarker = value.flags & ir.I18nParamValueFlags.CloseTag ? TAG_CLOSE_MARKER : '';
+  }
+  const context =
+      value.subTemplateIndex === null ? '' : `${CONTEXT_MARKER}${value.subTemplateIndex}`;
+  // Self-closing tags use a special form that concatenates the start and close tag values.
+  if ((value.flags & ir.I18nParamValueFlags.OpenTag) &&
+      (value.flags & ir.I18nParamValueFlags.CloseTag)) {
+    return `${ESCAPE}${tagMarker}${value.value}${context}${ESCAPE}${ESCAPE}${closeMarker}${
+        tagMarker}${value.value}${context}${ESCAPE}`;
+  }
+  return `${ESCAPE}${closeMarker}${tagMarker}${value.value}${context}${ESCAPE}`;
+}
---
+++ packages/compiler/src/template/pipeline/src/phases/resolve_i18n_element_placeholders.ts
@@ -0,0 +1,128 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import * as ir from '../../ir';
+import {ComponentCompilationJob} from '../compilation';
+
+/**
+ * Resolve the element placeholders in i18n messages.
+ */
+export function phaseResolveI18nElementPlaceholders(job: ComponentCompilationJob) {
+  // Record all of the element and extracted message ops for use later.
+  const extractedMessageOps = new Map<ir.XrefId, ir.ExtractedMessageOp>();
+  const elements = new Map<ir.XrefId, ir.ElementStartOp>();
+  for (const unit of job.units) {
+    for (const op of unit.create) {
+      switch (op.kind) {
+        case ir.OpKind.ExtractedMessage:
+          extractedMessageOps.set(op.owner, op);
+          break;
+        case ir.OpKind.ElementStart:
+          elements.set(op.xref, op);
+          break;
+      }
+    }
+  }
+
+  for (const unit of job.units) {
+    // Track the current i18n op and corresponding extracted message op as we step through the
+    // creation IR.
+    let currentOps: {i18n: ir.I18nStartOp, extractedMessage: ir.ExtractedMessageOp}|null = null;
+
+    for (const op of unit.create) {
+      switch (op.kind) {
+        case ir.OpKind.I18nStart:
+          if (!extractedMessageOps.has(op.xref)) {
+            throw Error('Could not find extracted message for i18n op');
+          }
+          currentOps = {i18n: op, extractedMessage: extractedMessageOps.get(op.xref)!};
+          break;
+        case ir.OpKind.I18nEnd:
+          currentOps = null;
+          break;
+        case ir.OpKind.ElementStart:
+          // For elements with i18n placeholders, record its slot value in the params map under the
+          // corresponding tag start placeholder.
+          if (op.i18nPlaceholder !== undefined) {
+            if (currentOps === null) {
+              throw Error('i18n tag placeholder should only occur inside an i18n block');
+            }
+            const {startName, closeName} = op.i18nPlaceholder;
+            let flags = ir.I18nParamValueFlags.ElementTag | ir.I18nParamValueFlags.OpenTag;
+            // For self-closing tags, there is no close tag placeholder. Instead, the start tag
+            // placeholder accounts for the start and close of the element.
+            if (closeName === '') {
+              flags |= ir.I18nParamValueFlags.CloseTag;
+            }
+            addParam(
+                currentOps.extractedMessage.params, startName, op.slot!,
+                currentOps.i18n.subTemplateIndex, flags);
+          }
+          break;
+        case ir.OpKind.ElementEnd:
+          // For elements with i18n placeholders, record its slot value in the params map under the
+          // corresponding tag close placeholder.
+          const startOp = elements.get(op.xref);
+          if (startOp && startOp.i18nPlaceholder !== undefined) {
+            if (currentOps === null) {
+              throw Error('i18n tag placeholder should only occur inside an i18n block');
+            }
+            const {closeName} = startOp.i18nPlaceholder;
+            // Self-closing tags don't have a closing tag placeholder.
+            if (closeName !== '') {
+              addParam(
+                  currentOps.extractedMessage!.params, closeName, startOp.slot!,
+                  currentOps.i18n.subTemplateIndex,
+                  ir.I18nParamValueFlags.ElementTag | ir.I18nParamValueFlags.CloseTag);
+            }
+          }
+          break;
+        case ir.OpKind.Template:
+          // For templates with i18n placeholders, record its slot value in the params map under the
+          // corresponding template start and close placeholders.
+          if (op.i18nPlaceholder !== undefined) {
+            if (currentOps === null) {
+              throw Error('i18n tag placeholder should only occur inside an i18n block');
+            }
+            const subTemplateIndex = getSubTemplateIndexForTemplateTag(job, currentOps.i18n, op);
+            addParam(
+                currentOps.extractedMessage.params, op.i18nPlaceholder.startName, op.slot!,
+                subTemplateIndex, ir.I18nParamValueFlags.TemplateTag);
+            addParam(
+                currentOps.extractedMessage.params, op.i18nPlaceholder.closeName, op.slot!,
+                subTemplateIndex,
+                ir.I18nParamValueFlags.TemplateTag | ir.I18nParamValueFlags.CloseTag);
+          }
+          break;
+      }
+    }
+  }
+}
+
+/**
+ * Get the subTemplateIndex for the given template op. For template ops, use the subTemplateIndex of
+ * the child i18n block inside the template.
+ */
+function getSubTemplateIndexForTemplateTag(
+    job: ComponentCompilationJob, i18nOp: ir.I18nStartOp, op: ir.TemplateOp): number|null {
+  for (const childOp of job.views.get(op.xref)!.create) {
+    if (childOp.kind === ir.OpKind.I18nStart) {
+      return childOp.subTemplateIndex;
+    }
+  }
+  return i18nOp.subTemplateIndex;
+}
+
+/** Add a param value to the given params map. */
+function addParam(
+    params: Map<string, ir.I18nParamValue[]>, placeholder: string, value: string|number,
+    subTemplateIndex: number|null, flags = ir.I18nParamValueFlags.None) {
+  const values = params.get(placeholder) ?? [];
+  values.push({value, subTemplateIndex, flags});
+  params.set(placeholder, values);
+}
