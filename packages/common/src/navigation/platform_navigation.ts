77770c6d5b0c50a22c3bc57654fc067a5a75b5de
Thomas Wilkinson <twilkinson@google.com>
2023-10-24 17:57:07 -0500

    refactor(common): Add a new platform_navigation that provides the Navigation API. (#52363)
    This allows using the Navigation API in Angular packages like Router.
    
    PR Close #52363

---
+++ packages/common/src/navigation/platform_navigation.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Injectable} from '@angular/core';
+
+/**
+ * This class wraps the platform Navigation API which allows server-specific and test
+ * implementations.
+ *
+ * @internal
+ */
+@Injectable({providedIn: 'platform', useFactory: () => window.navigation})
+export abstract class PlatformNavigation extends Navigation {
+}
