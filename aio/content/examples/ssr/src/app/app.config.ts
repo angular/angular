07623caefe2ec71c63d5c427c00cda59322694f2
Andrew Kushnir <akushnir@google.com>
2023-10-26 15:54:33 -0700

    docs: update SSR docs (#52408)
    This commit updates the `ssr.md` file to restructure (and simplify) SSR docs.
    
    PR Close #52408

--- aio/content/examples/ssr/src/app/app.config.ts
+++ aio/content/examples/ssr/src/app/app.config.ts
@@ -1,35 +1,26 @@
 // #docplaster
-import { importProvidersFrom } from '@angular/core';
-import { provideProtractorTestingSupport } from '@angular/platform-browser';
-import { provideClientHydration} from '@angular/platform-browser';
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideClientHydration, provideProtractorTestingSupport} from '@angular/platform-browser';
+import {provideRouter} from '@angular/router';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 
-import { routes } from './app.routes';
-
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
+import {routes} from './app.routes';
+import {InMemoryDataService} from './in-memory-data.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-     // TODO: Enable using Fetch API when disabling `HttpClientInMemoryWebApiModule`.
-    provideHttpClient(/* withFetch()*/ ),
-    provideClientHydration(),
-    provideProtractorTestingSupport(), // essential for e2e testing
+    // TODO: Enable using Fetch API when disabling `HttpClientInMemoryWebApiModule`.
+    provideHttpClient(/* withFetch()*/), provideClientHydration(),
+    provideProtractorTestingSupport(),  // essential for e2e testing
 
-    // #docregion in-mem
     // TODO: Remove from production apps
     importProvidersFrom(
-      // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-      // and returns simulated server responses.
-      // Remove it when a real server is ready to receive requests.
-      HttpClientInMemoryWebApiModule.forRoot(
-        InMemoryDataService, { dataEncapsulation: false }
-      )
-    ),
-    // #enddocregion in-mem
+        // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+        // and returns simulated server responses.
+        // Remove it when a real server is ready to receive requests.
+        HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})),
     // ...
   ],
 };
