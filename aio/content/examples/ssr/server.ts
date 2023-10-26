1c1be8593e279fbbd540ffabdc7bbe3ddc59abbc
Andrew Kushnir <akushnir@google.com>
2023-10-26 15:54:33 -0700

    docs: update SSR docs
    This commit updates the `ssr.md` file to restructure (and simplify) SSR docs.

--- aio/content/examples/ssr/server.ts
+++ aio/content/examples/ssr/server.ts
@@ -1,10 +1,11 @@
 // #docplaster
 
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
+import {APP_BASE_HREF} from '@angular/common';
+import {CommonEngine} from '@angular/ssr';
 import express from 'express';
-import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
+import {dirname, join, resolve} from 'node:path';
+import {fileURLToPath} from 'node:url';
+
 import bootstrap from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
@@ -13,42 +14,34 @@ export function app(): express.Express {
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
-  // #docregion CommonEngine
   const commonEngine = new CommonEngine();
-  // #enddocregion CommonEngine
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
-  // #docregion data-request
   // TODO: implement data requests securely
   // Serve data from URLS that begin "/api/"
   server.get('/api/**', (req, res) => {
     res.status(404).send('data requests are not yet supported');
   });
-  // #enddocregion data-request
-  // #docregion static
   // Serve static files from /browser
-  server.get('*.*', express.static(browserDistFolder, {
-    maxAge: '1y'
-  }));
-  // #enddocregion static
+  server.get('*.*', express.static(browserDistFolder, {maxAge: '1y'}));
 
   // #docregion navigation-request
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
+    const {protocol, originalUrl, baseUrl, headers} = req;
 
     commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+        .render({
+          bootstrap,
+          documentFilePath: indexHtml,
+          url: `${protocol}://${headers.host}${originalUrl}`,
+          publicPath: browserDistFolder,
+          providers: [{provide: APP_BASE_HREF, useValue: req.baseUrl}],
+        })
+        .then((html) => res.send(html))
+        .catch((err) => next(err));
   });
   // #enddocregion navigation-request
 
