353d43ff1a05f32137099001aecf7a7cd2728bc2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-01 21:09:37 -0700

    refactor(core): add a flag for whether we skip logic in checkNoChanges (#52488)
    This can be used to patch different behavior in g3.
    
    PR Close #52488

---
+++ packages/core/src/render3/instructions/change_detection_flags.ts
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * Whether we should skip specific logic in checkNoChanges mode.
+ */
+export const RUN_IN_CHECK_NO_CHANGES_ANYWAY = false;
