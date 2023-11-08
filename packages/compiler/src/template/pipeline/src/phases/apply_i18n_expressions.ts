a3c24806400be17d3294fe05f75466ca4b2f5bb9
Miles Malerba <mmalerba@google.com>
2023-11-07 21:46:56 -0800

    refactor(compiler): Fix some issues with i18n expressions in ICUs (#52698)
    We were previously counting the i18n expression index and deciding when
    to apply i18n expressions based on the i18n context. These should be
    done based on the i18n block instead.
    
    PR Close #52698

--- packages/compiler/src/template/pipeline/src/phases/apply_i18n_expressions.ts
+++ packages/compiler/src/template/pipeline/src/phases/apply_i18n_expressions.ts
@@ -13,10 +13,19 @@ import {CompilationJob} from '../compilation';
  * Adds apply operations after i18n expressions.
  */
 export function applyI18nExpressions(job: CompilationJob): void {
+  const i18nContexts = new Map<ir.XrefId, ir.I18nContextOp>();
+  for (const unit of job.units) {
+    for (const op of unit.create) {
+      if (op.kind === ir.OpKind.I18nContext) {
+        i18nContexts.set(op.xref, op);
+      }
+    }
+  }
+
   for (const unit of job.units) {
     for (const op of unit.update) {
       // Only add apply after expressions that are not followed by more expressions.
-      if (op.kind === ir.OpKind.I18nExpression && needsApplication(op)) {
+      if (op.kind === ir.OpKind.I18nExpression && needsApplication(i18nContexts, op)) {
         // TODO: what should be the source span for the apply op?
         ir.OpList.insertAfter<ir.UpdateOp>(ir.createI18nApplyOp(op.target, op.handle, null!), op);
       }
@@ -27,13 +36,15 @@ export function applyI18nExpressions(job: CompilationJob): void {
 /**
  * Checks whether the given expression op needs to be followed with an apply op.
  */
-function needsApplication(op: ir.I18nExpressionOp) {
+function needsApplication(i18nContexts: Map<ir.XrefId, ir.I18nContextOp>, op: ir.I18nExpressionOp) {
   // If the next op is not another expression, we need to apply.
   if (op.next?.kind !== ir.OpKind.I18nExpression) {
     return true;
   }
-  // If the next op is an expression targeting a different i18n context, we need to apply.
-  if (op.next.context !== op.context) {
+  // If the next op is an expression targeting a different i18n block, we need to apply.
+  const context = i18nContexts.get(op.context)!;
+  const nextContext = i18nContexts.get(op.next.context)!;
+  if (context.i18nBlock !== nextContext.i18nBlock) {
     return true;
   }
   return false;
