577dc5954b40f6fdcb1c1dd5c8b3b55962349821
Miles Malerba <mmalerba@google.com>
2023-11-08 10:50:12 -0800

    refactor(compiler): Handle trailing spaces in ICU placeholders (#52698)
    In some cases ICU expression placeholders may have trailing spaces that
    need to be trimmed when matching the placeholder to its corresponding
    text binding.
    
    PR Close #52698

--- packages/compiler/src/template/pipeline/src/ingest.ts
+++ packages/compiler/src/template/pipeline/src/ingest.ts
@@ -492,7 +492,7 @@ function ingestIcu(unit: ViewCompilationUnit, icu: t.Icu) {
     const xref = unit.job.allocateXrefId();
     const icuNode = icu.i18n.nodes[0];
     unit.create.push(ir.createIcuStartOp(xref, icu.i18n, icuFromI18nMessage(icu.i18n).name, null!));
-    const {expressionPlaceholder} = icuNode;
+    const expressionPlaceholder = icuNode.expressionPlaceholder?.trimEnd();
     if (expressionPlaceholder === undefined || icu.vars[expressionPlaceholder] === undefined) {
       throw Error('ICU should have a text binding');
     }
