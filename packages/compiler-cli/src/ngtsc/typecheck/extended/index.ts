8a87e62e19b469746dc705451bb7066fe675ee7d
cexbrayat <cedric@ninja-squad.com>
2023-11-08 17:28:31 +0100

    fix(compiler-cli): add interpolatedSignalNotInvoked to diagnostics (#52687)
    This template diagnostic has been introduced in 8eef694def3dc660779168925a380179c7e30993 but was not enabled,
    as it was not added to `ALL_DIAGNOSTIC_FACTORIES`.
    
    PR Close #52687

--- packages/compiler-cli/src/ngtsc/typecheck/extended/index.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/extended/index.ts
@@ -9,6 +9,7 @@
 import {ErrorCode, ExtendedTemplateDiagnosticName} from '../../diagnostics';
 
 import {TemplateCheckFactory} from './api';
+import {factory as interpolatedSignalNotInvoked} from './checks/interpolated_signal_not_invoked';
 import {factory as invalidBananaInBoxFactory} from './checks/invalid_banana_in_box';
 import {factory as missingControlFlowDirectiveFactory} from './checks/missing_control_flow_directive';
 import {factory as missingNgForOfLetFactory} from './checks/missing_ngforof_let';
@@ -21,11 +22,8 @@ export {ExtendedTemplateCheckerImpl} from './src/extended_template_checker';
 
 export const ALL_DIAGNOSTIC_FACTORIES:
     readonly TemplateCheckFactory<ErrorCode, ExtendedTemplateDiagnosticName>[] = [
-      invalidBananaInBoxFactory,
-      nullishCoalescingNotNullableFactory,
-      optionalChainNotNullableFactory,
-      missingControlFlowDirectiveFactory,
-      textAttributeNotBindingFactory,
-      missingNgForOfLetFactory,
-      suffixNotSupportedFactory,
+      invalidBananaInBoxFactory, nullishCoalescingNotNullableFactory,
+      optionalChainNotNullableFactory, missingControlFlowDirectiveFactory,
+      textAttributeNotBindingFactory, missingNgForOfLetFactory, suffixNotSupportedFactory,
+      interpolatedSignalNotInvoked
     ];
