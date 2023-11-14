7a5f6552ada4cd1327c532f704010619c3d725d1
Andrew Kushnir <akushnir@google.com>
2023-11-13 22:08:26 -0800

    fix(core): collect providers from NgModules while rendering `@defer` block
    Currently, when a `@defer` block contains standalone components that import NgModules with providers, those providers are not available to components declared within the same NgModule. The problem is that the standalone injector is not created for the host component (that hosts this `@defer` block), since dependencies become defer-loaded, thus no information is available at host component creation time.
    
    This commit updates the logic to collect all providers from all NgModules used as a dependency for standalone components used within a `@defer` block. When an instance of a defer block is created, a new environment injector instance with those providers is created.
    
    Resolves #52876.

--- packages/core/test/acceptance/defer_spec.ts
+++ packages/core/test/acceptance/defer_spec.ts
@@ -7,7 +7,7 @@
  */
 
 import {ɵPLATFORM_BROWSER_ID as PLATFORM_BROWSER_ID} from '@angular/common';
-import {ApplicationRef, Attribute, ChangeDetectionStrategy, ChangeDetectorRef, Component, ComponentRef, createComponent, DebugElement, Directive, EnvironmentInjector, ErrorHandler, getDebugNode, inject, Input, NgZone, Pipe, PipeTransform, PLATFORM_ID, QueryList, Type, ViewChildren, ɵDEFER_BLOCK_DEPENDENCY_INTERCEPTOR} from '@angular/core';
+import {ApplicationRef, Attribute, ChangeDetectionStrategy, ChangeDetectorRef, Component, ComponentRef, createComponent, DebugElement, Directive, EnvironmentInjector, ErrorHandler, getDebugNode, inject, Injectable, InjectionToken, Input, NgModule, NgZone, Pipe, PipeTransform, PLATFORM_ID, QueryList, Type, ViewChildren, ɵDEFER_BLOCK_DEPENDENCY_INTERCEPTOR} from '@angular/core';
 import {getComponentDef} from '@angular/core/src/render3/definition';
 import {ComponentFixture, DeferBlockBehavior, fakeAsync, flush, TestBed, tick} from '@angular/core/testing';
 
@@ -3977,6 +3977,106 @@ describe('@defer', () => {
           .toHaveBeenCalledWith('mouseenter', jasmine.any(Function), jasmine.any(Object));
       expect(prefetchSpy)
           .toHaveBeenCalledWith('focusin', jasmine.any(Function), jasmine.any(Object));
+    });
+  });
+
+  describe('NgModules', () => {
+    it('should provide access to tokens from imported NgModules', async () => {
+      let serviceInitCount = 0;
+
+      const TokenA = new InjectionToken('');
+
+      @Injectable()
+      class Service {
+        id = 'ChartsModule.Service';
+        constructor() {
+          serviceInitCount++;
+        }
+      }
+
+      @Component({
+        selector: 'chart',
+        template: 'Service:{{ svc.id }}|TokenA:{{ tokenA }}',
+      })
+      class Chart {
+        svc = inject(Service);
+        tokenA = inject(TokenA);
+      }
+
+      @NgModule({
+        providers: [Service],
+        declarations: [Chart],
+        exports: [Chart],
+      })
+      class ChartsModule {
+      }
+
+      @Component({
+        selector: 'chart-collection',
+        template: '<chart />',
+        standalone: true,
+        imports: [ChartsModule],
+      })
+      class ChartCollectionComponent {
+      }
+
+      @Component({
+        selector: 'app-root',
+        standalone: true,
+        template: `
+          @for(item of items; track $index) {
+            @defer (when isVisible) {
+              <chart-collection />
+            }
+          }
+        `,
+        imports: [ChartCollectionComponent],
+        providers: [{provide: TokenA, useValue: 'MyCmp.A'}]
+      })
+      class MyCmp {
+        items = [1, 2, 3];
+        isVisible = true;
+      }
+
+      const deferDepsInterceptor = {
+        intercept() {
+          return () => {
+            return [dynamicImportOf(ChartCollectionComponent)];
+          };
+        }
+      };
+
+      TestBed.configureTestingModule({
+        providers: [
+          {provide: ɵDEFER_BLOCK_DEPENDENCY_INTERCEPTOR, useValue: deferDepsInterceptor},
+        ],
+        deferBlockBehavior: DeferBlockBehavior.Playthrough,
+      });
+
+      clearDirectiveDefs(MyCmp);
+
+      const fixture = TestBed.createComponent(MyCmp);
+      fixture.detectChanges();
+
+      await allPendingDynamicImports();
+      fixture.detectChanges();
+
+      // Verify that the `Service` injectable was initialized only once,
+      // even though it was injected in 3 instances of the `<chart>` component,
+      // used within defer blocks.
+      expect(serviceInitCount).toBe(1);
+      expect(fixture.nativeElement.querySelectorAll('chart').length).toBe(3);
+
+      // Verify that a service defined within an NgModule can inject services
+      // provided within the same NgModule.
+      const serviceFromNgModule = 'Service:ChartsModule.Service';
+
+      // Make sure sure that a nested `<chart>` component from the defer block
+      // can inject tokens provided in parent component (that contains `@defer`
+      // in its template).
+      const tokenFromRootComponent = 'TokenA:MyCmp.A';
+      expect(fixture.nativeElement.innerHTML)
+          .toContain(`<chart>${serviceFromNgModule}|${tokenFromRootComponent}</chart>`);
     });
   });
 });
