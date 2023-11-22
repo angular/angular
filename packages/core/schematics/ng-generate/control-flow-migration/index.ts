ce86dc303b82306295351fd05db8842e806f34b9
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-22 11:48:09 -0500

    fix(migrations): fixes control flow migration common module removal (#53076)
    Common module removal would not happen when a component used a templateUrl due to the checks being in separate files. This change passes the removal analysis back to the original source file to safely remove CommonModule.
    
    PR Close #53076

--- packages/core/schematics/ng-generate/control-flow-migration/index.ts
+++ packages/core/schematics/ng-generate/control-flow-migration/index.ts
@@ -78,7 +78,12 @@ function runControlFlowMigration(
     analyze(sourceFile, analysis);
   }
 
-  for (const [path, file] of analysis) {
+  // sort files with .html files first
+  // this ensures class files know if it's safe to remove CommonModule
+  const paths = sortFilePaths([...analysis.keys()]);
+
+  for (const path of paths) {
+    const file = analysis.get(path)!;
     const ranges = file.getSortedRanges();
     const relativePath = relative(basePath, path);
     const content = tree.readText(relativePath);
@@ -89,7 +94,7 @@ function runControlFlowMigration(
       const length = (end ?? content.length) - start;
 
       const {migrated, errors} =
-          migrateTemplate(template, type, node, file, schematicOptions.format);
+          migrateTemplate(template, type, node, file, schematicOptions.format, analysis);
 
       if (migrated !== null) {
         update.remove(start, length);
@@ -111,6 +116,12 @@ function runControlFlowMigration(
   }
 
   return errorList;
+}
+
+function sortFilePaths(names: string[]): string[] {
+  const templateFiles = names.filter(n => n.endsWith('.html'));
+  const classFiles = names.filter(n => !n.endsWith('.html'));
+  return [...templateFiles, ...classFiles];
 }
 
 function generateErrorMessage(path: string, errors: MigrateError[]): string {
