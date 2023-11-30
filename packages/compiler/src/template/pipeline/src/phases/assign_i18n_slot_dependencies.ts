5cc0c2b93e022732a39f8baf832f6b796cc3ca5a
Miles Malerba <mmalerba@google.com>
2023-11-30 10:25:51 -0800

    refactor(compiler): Put i18n expression ops in the correct order (#53300)
    When we re-assign the slot dependencies for the i18nExprs, we should
    move them down below the other ops that target their same slot. This
    keeps the behavior consistent with TDB
    
    PR Close #53300

--- packages/compiler/src/template/pipeline/src/phases/assign_i18n_slot_dependencies.ts
+++ packages/compiler/src/template/pipeline/src/phases/assign_i18n_slot_dependencies.ts
@@ -35,11 +35,44 @@ export function assignI18nSlotDependencies(job: CompilationJob) {
       }
     }
 
-    // Assign i18n expressions to target the last slot in its owning block.
+    // Assign i18n expressions to target the last slot in their owning block. Also move the ops
+    // below any other ops that depend on that same slot context to mimic the behavior of
+    // TemplateDefinitionBuilder.
+    let moveToTarget: ir.XrefId|null = null;
+    let opsToMove: ir.UpdateOp[] = [];
+    let previousTarget: ir.XrefId|null = null;
+    let currentTarget: ir.XrefId|null = null;
     for (const op of unit.update) {
+      currentTarget = ir.hasDependsOnSlotContextTrait(op) ? op.target : null;
+
+      // Re-target i18n expression ops.
       if (op.kind === ir.OpKind.I18nExpression) {
         op.target = i18nLastSlotConsumers.get(op.target)!;
+        moveToTarget = op.target;
       }
+
+      // Pull out i18n expression and apply ops to be moved.
+      if (op.kind === ir.OpKind.I18nExpression || op.kind === ir.OpKind.I18nApply) {
+        opsToMove.push(op);
+        ir.OpList.remove<ir.UpdateOp>(op);
+        currentTarget = moveToTarget;
+      }
+
+      // Add back any ops that were previously pulled once we pass the point where they should be
+      // inserted.
+      if (moveToTarget !== null && previousTarget === moveToTarget &&
+          currentTarget !== previousTarget) {
+        ir.OpList.insertBefore(opsToMove, op);
+        opsToMove = [];
+      }
+
+      // Update the previous target for the next pass through
+      previousTarget = currentTarget;
+    }
+
+    // If there are any mvoed ops that haven't been put back yet, put them back at the end.
+    if (opsToMove) {
+      unit.update.push(opsToMove);
     }
   }
 }
