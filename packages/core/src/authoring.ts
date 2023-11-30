776bd203557996e3d084537645192f352aed8ff4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-11-30 17:11:43 +0000

    refactor(core): introduce signal `input()` function and compiler detection (#53521)
    This commit introduces a function for declaring inputs in
    components. The function is called `input`. It comes in two flavors:
    
    - `input` for optional inputs with initial values
    - `input.required` for required inputs
    
    Inputs are declared as class members, like with `@Input`- except that
    the class field will no longer hold the input value directly. Angular
    takes control over the input field and exposes the input value as a
    signal. The runtime implementation will follow in future commits.
    
    This commit simply introduces:
    
    - initial compiler detection to recognize such inputs in classes
    - the initial signature of `input` and `input.required`.
    
    Note: the defer size test is flawed and there is no minification- hence
    this commit also needs to incorporate the new dependency graph changes.
    
    PR Close #53521

---
+++ packages/core/src/authoring.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+// Note: `input` is exported in `core.ts` due to:
+// https://docs.google.com/document/d/1RXb1wYwsbJotO1KBgSDsAtKpduGmIHod9ADxuXcAvV4/edit?tab=t.0.
+
+export {InputFunction as ɵInputFunction, inputFunction as ɵinputFunctionForApiGuard, inputRequiredFunction as ɵinputFunctionRequiredForApiGuard} from './authoring/input';
+export {InputOptions as ɵInputOptions, InputOptionsWithoutTransform as ɵInputOptionsWithoutTransform, InputOptionsWithTransform as ɵInputOptionsWithTransform, InputSignal} from './authoring/input_signal';
