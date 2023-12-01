4af7a4be8bffdf075b88d1f9b1f23897d4907327
Andrew Kushnir <akushnir@google.com>
2023-11-30 18:47:56 -0800

    fix(compiler): generate proper code for nullish coalescing in styling host bindings (#53305)
    This commit fixes an issue where having an expression with nullish coalescing in styling host bindings leads to JS errors due to the fact that a declaration for a temporary variable was not included into the generated code.
    
    Resolves #53295.
    
    PR Close #53305

---
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_bindings/host_bindings/host_style_bindings_with_temporaries.js
@@ -0,0 +1,8 @@
+hostBindings: function HostBindingDir_HostBindings(rf, ctx) {
+  if (rf & 2) {
+    let $tmp_0$;
+    let $tmp_1$;
+    $r3$.ɵɵstyleProp("font-size", ($tmp_0$ = ctx.value) !== null && $tmp_0$ !== undefined ? $tmp_0$ : "15px")
+                    ("font-weight", ($tmp_1$ = ctx.value) !== null && $tmp_1$ !== undefined ? $tmp_1$ : "bold");
+  }
+}
