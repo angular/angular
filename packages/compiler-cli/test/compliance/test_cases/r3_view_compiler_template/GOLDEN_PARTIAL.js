b2313e2c6e3336e4a96a26b9a042077e62fb93b5
Miles Malerba <mmalerba@google.com>
2023-12-03 08:51:17 -0800

    test(compiler): Update golden partial file
    Update the golden partial file to include the newly added tests in this
    PR.

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_template/GOLDEN_PARTIAL.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_template/GOLDEN_PARTIAL.js
@@ -1056,3 +1056,59 @@ export declare class MyComponent {
     static ɵcmp: i0.ɵɵComponentDeclaration<MyComponent, "my-component", never, {}, {}, never, never, true, never>;
 }
 
+/****************************************************************************************************
+ * PARTIAL FILE: self_closing_structural_directives.js
+ ****************************************************************************************************/
+import { Component } from '@angular/core';
+import * as i0 from "@angular/core";
+export class OtherComponent {
+}
+OtherComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "0.0.0-PLACEHOLDER", ngImport: i0, type: OtherComponent, deps: [], target: i0.ɵɵFactoryTarget.Component });
+OtherComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "0.0.0-PLACEHOLDER", type: OtherComponent, isStandalone: true, selector: "other-component", ngImport: i0, template: '', isInline: true });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "0.0.0-PLACEHOLDER", ngImport: i0, type: OtherComponent, decorators: [{
+            type: Component,
+            args: [{ selector: 'other-component', standalone: true, template: '' }]
+        }] });
+export class MyComponent {
+}
+MyComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "0.0.0-PLACEHOLDER", ngImport: i0, type: MyComponent, deps: [], target: i0.ɵɵFactoryTarget.Component });
+MyComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "0.0.0-PLACEHOLDER", type: MyComponent, isStandalone: true, selector: "my-component", ngImport: i0, template: `
+  <div i18n>
+    <img *ngIf="flag" />
+    <other-component *ngIf="flag" />
+    <ng-template *ngIf="flag" />
+    <ng-container *ngIf="flag" />
+    <ng-content *ngIf="flag" />
+  </div>
+`, isInline: true, dependencies: [{ kind: "component", type: OtherComponent, selector: "other-component" }] });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "0.0.0-PLACEHOLDER", ngImport: i0, type: MyComponent, decorators: [{
+            type: Component,
+            args: [{
+                    selector: 'my-component',
+                    standalone: true,
+                    imports: [OtherComponent],
+                    template: `
+  <div i18n>
+    <img *ngIf="flag" />
+    <other-component *ngIf="flag" />
+    <ng-template *ngIf="flag" />
+    <ng-container *ngIf="flag" />
+    <ng-content *ngIf="flag" />
+  </div>
+`,
+                }]
+        }] });
+
+/****************************************************************************************************
+ * PARTIAL FILE: self_closing_structural_directives.d.ts
+ ****************************************************************************************************/
+import * as i0 from "@angular/core";
+export declare class OtherComponent {
+    static ɵfac: i0.ɵɵFactoryDeclaration<OtherComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<OtherComponent, "other-component", never, {}, {}, never, never, true, never>;
+}
+export declare class MyComponent {
+    static ɵfac: i0.ɵɵFactoryDeclaration<MyComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<MyComponent, "my-component", never, {}, {}, never, ["*"], true, never>;
+}
+
