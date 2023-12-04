043ec13a95ba9989f1e0557939d3f239b8d0f0f6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-12-04 14:06:11 +0000

    test: fix linker compliance tests after not running for a while
    The linker compliance tests did not run for a while. There were a couple
    of new tests that were not passing as this wasn't flagged on CI. This commit fixes this.
    Fortunately there was no problematic code that did indicate issues with linking.
    
    In the follow-up commit, we fix the compliance test infrastructure to
    re-enable linker testing..
    
    One clear issue is still that the defer blocks are not handled properly
    in linked output- hence making defer not actually "lazy" for compiled
    libraries. This needs to be handled separately by the framework team.

--- packages/compiler-cli/test/compliance/test_cases/r3_compiler_compliance/ng_modules/imports_exports_jit_mode.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_compiler_compliance/ng_modules/imports_exports_jit_mode.js
@@ -1,8 +1,7 @@
-class AModule {}
+export class AModule {}
 AModule.ɵfac = function AModule_Factory(t) { return new (t || AModule)(); };
 AModule.ɵmod = /*@__PURE__*/ i0.ɵɵdefineNgModule({ type: AModule, declarations: [A1Component, A2Component], exports: [A1Component, A2Component] });
 AModule.ɵinj = /*@__PURE__*/ i0.ɵɵdefineInjector({});
-export { AModule };
 …
 (() => {
   (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(AModule, [{
@@ -11,12 +10,10 @@ export { AModule };
   }], null, null);
 })();
 …
-
-class BModule {}
+export class BModule {}
 BModule.ɵfac = function BModule_Factory(t) { return new (t || BModule)(); };
 BModule.ɵmod = /*@__PURE__*/ i0.ɵɵdefineNgModule({ type: BModule, declarations: [B1Component, B2Component], exports: [AModule] });
 BModule.ɵinj = /*@__PURE__*/ i0.ɵɵdefineInjector({ imports: [AModule] });
-export { BModule };
 …
 (() => {
   (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(BModule, [{
@@ -25,12 +22,10 @@ export { BModule };
   }], null, null);
 })();
 …
-
-class AppModule {}
+export class AppModule {}
 AppModule.ɵfac = function AppModule_Factory(t) { return new (t || AppModule)(); };
 AppModule.ɵmod = /*@__PURE__*/ i0.ɵɵdefineNgModule({ type: AppModule, imports: [BModule] });
 AppModule.ɵinj = /*@__PURE__*/ i0.ɵɵdefineInjector({ imports: [BModule] });
-export { AppModule };
 …
 (() => {
   (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(AppModule, [{
