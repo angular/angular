e0e0c97c2b7f77677f9e8596d8759bb030eb4e03
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-12-05 13:16:14 +0000

    test: add signal input compliance tests for linking, partial output and full compilations (#53808)
    Adds signal input compliance tests, ensuring linking works as expected,
    partial output is generated properly, types are inferred properly, and
    that the full, or linked output matches our expected runtime structure.
    
    PR Close #53808

---
+++ packages/compiler-cli/test/compliance/test_cases/signal_inputs/input_component_definition.js
@@ -0,0 +1,9 @@
+TestComp.ɵcmp = /*@__PURE__*/ $r3$.ɵɵdefineComponent({
+    …
+    inputs: {
+        counter: [$r3$.ɵɵInputFlags.SignalBased, "counter"],
+        name: [$r3$.ɵɵInputFlags.SignalBased, "name"]
+    },
+    …
+  });
+  
