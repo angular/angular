4a016886fa31c833c21d51ae3e0a323ecd9a3fd6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-12-05 13:16:14 +0000

    test: add signal input compliance tests for linking, partial output and full compilations (#53808)
    Adds signal input compliance tests, ensuring linking works as expected,
    partial output is generated properly, types are inferred properly, and
    that the full, or linked output matches our expected runtime structure.
    
    PR Close #53808

---
+++ packages/compiler-cli/test/compliance/test_cases/signal_inputs/complex_transform_functions.js
@@ -0,0 +1,11 @@
+TestDir.ɵdir = /*@__PURE__*/ $r3$.ɵɵdefineDirective({
+    …
+    inputs: {
+        name: [$r3$.ɵɵInputFlags.SignalBased, "name"],
+        name2: [$r3$.ɵɵInputFlags.SignalBased, "name2"],
+        genericTransform: [$r3$.ɵɵInputFlags.SignalBased, "genericTransform"],
+        genericTransform2: [$r3$.ɵɵInputFlags.SignalBased, "genericTransform2"]
+    },
+    …
+  });
+  
