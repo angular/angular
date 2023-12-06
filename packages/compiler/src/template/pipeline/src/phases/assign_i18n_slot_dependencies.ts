d805f4e9b10dfde724292945f2a5e83984e7ae64
Dylan Hunn <dylhunn@gmail.com>
2023-12-06 11:12:53 -0800

    refactor(compiler): Fix i18nExp moving phase in Template Pipeline
    Previously, our i18n slot moving process was buggy. Specifically, it was not resilient to cases in which a create op consumed a slot, but no update ops depended on that slot.
    
    The new algorithm fixes this issue, and is also easier to understand.

--- packages/compiler/src/template/pipeline/src/phases/assign_i18n_slot_dependencies.ts
+++ packages/compiler/src/template/pipeline/src/phases/assign_i18n_slot_dependencies.ts
@@ -9,80 +9,66 @@
 import * as ir from '../../ir';
 import {CompilationJob} from '../compilation';
 
+interface BlockState {
+  blockXref: ir.XrefId;
+  lastSlotConsumer: ir.XrefId;
+}
+
 /**
  * Updates i18n expression ops to target the last slot in their owning i18n block, and moves them
  * after the last update instruction that depends on that slot.
  */
 export function assignI18nSlotDependencies(job: CompilationJob) {
-  const i18nLastSlotConsumers = new Map<ir.XrefId, ir.XrefId>();
-  let lastSlotConsumer: ir.XrefId|null = null;
-  let currentI18nOp: ir.I18nStartOp|null = null;
-
   for (const unit of job.units) {
-    // Record the last consumed slot before each i18n end instruction.
-    for (const op of unit.create) {
-      if (ir.hasConsumesSlotTrait(op)) {
-        lastSlotConsumer = op.xref;
-      }
+    // The first update op.
+    let updateOp = unit.update.head;
 
-      switch (op.kind) {
-        case ir.OpKind.I18nStart:
-          currentI18nOp = op;
-          break;
-        case ir.OpKind.I18nEnd:
-          if (currentI18nOp === null) {
-            throw new Error(
-                'AssertionError: Expected an active I18n block while calculating last slot consumers');
-          }
-          if (lastSlotConsumer === null) {
-            throw new Error(
-                'AssertionError: Expected a last slot consumer while calculating last slot consumers');
-          }
-          i18nLastSlotConsumers.set(currentI18nOp.xref, lastSlotConsumer);
-          currentI18nOp = null;
-          break;
-      }
-    }
+    // I18n expressions currently being moved during the iteration.
+    let i18nExpressionsInProgress: ir.I18nExpressionOp[] = [];
 
-    // Expresions that are currently being moved.
-    let opsToMove: ir.I18nExpressionOp[] = [];
-    // Previously we found the last slot-consuming create mode op in the i18n block. That op will be
-    // the new target for any moved i18n expresion inside the i18n block, and that op's slot is
-    // stored here.
-    let moveAfterTarget: ir.XrefId|null = null;
-    // This is the last target in the create IR that we saw during iteration. Eventally, it will be
-    // equal to moveAfterTarget. But wait! We need to find the *last* such op whose target is equal
-    // to `moveAfterTarget`.
-    let previousTarget: ir.XrefId|null = null;
-    for (const op of unit.update) {
-      if (ir.hasDependsOnSlotContextTrait(op)) {
-        // We've found an op that depends on another slot other than the one that we want to move
-        // the expressions to, after previously having seen the one we want to move to.
-        if (moveAfterTarget !== null && previousTarget === moveAfterTarget &&
-            op.target !== previousTarget) {
-          ir.OpList.insertBefore<ir.UpdateOp>(opsToMove, op);
-          moveAfterTarget = null;
-          opsToMove = [];
+    // Non-null  while we are iterating through an i18nStart/i18nEnd pair
+    let state: BlockState|null = null;
+
+    for (const createOp of unit.create) {
+      if (createOp.kind === ir.OpKind.I18nStart) {
+        state = {
+          blockXref: createOp.xref,
+          lastSlotConsumer: createOp.xref,
+        };
+      } else if (createOp.kind === ir.OpKind.I18nEnd) {
+        for (const op of i18nExpressionsInProgress) {
+          op.target = state!.lastSlotConsumer;
+          ir.OpList.insertBefore(op as ir.UpdateOp, updateOp!);
         }
-        previousTarget = op.target;
+        i18nExpressionsInProgress.length = 0;
+        state = null;
       }
 
-      if (op.kind === ir.OpKind.I18nExpression && op.usage === ir.I18nExpressionFor.I18nText) {
-        // This is an I18nExpressionOps that is used for text (not attributes).
-        ir.OpList.remove<ir.UpdateOp>(op);
-        opsToMove.push(op);
-        const target = i18nLastSlotConsumers.get(op.i18nOwner);
-        if (target === undefined) {
-          throw new Error(
-              'AssertionError: Expected to find a last slot consumer for an I18nExpressionOp');
+      if (ir.hasConsumesSlotTrait(createOp)) {
+        if (state !== null) {
+          state.lastSlotConsumer = createOp.xref;
         }
-        op.target = target;
-        moveAfterTarget = op.target;
-      }
-    }
 
-    if (moveAfterTarget !== null) {
-      unit.update.push(opsToMove);
+        while (true) {
+          if (updateOp.next === null) {
+            break;
+          }
+
+          if (ir.hasDependsOnSlotContextTrait(updateOp) && updateOp.target !== createOp.xref) {
+            break;
+          }
+
+          if (state !== null && updateOp.kind === ir.OpKind.I18nExpression &&
+              updateOp.usage === ir.I18nExpressionFor.I18nText) {
+            const opToRemove = updateOp;
+            updateOp = updateOp.prev!;
+            ir.OpList.remove<ir.UpdateOp>(opToRemove);
+            i18nExpressionsInProgress.push(opToRemove);
+          }
+
+          updateOp = updateOp.next!;
+        }
+      }
     }
   }
 }
