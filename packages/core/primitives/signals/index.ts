6fe528cee1279c3661bbdb262bdc81219694ae63
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-12-07 14:37:05 +0000

    refactor(core): expose `SIGNAL_NODE` to allow for advanced extensions of signals
    For the implementation of input signals, we want to extend the signal
    primitive. The basic methods exposed here are not suitable as we'd like
    to store additional metadata on the reactive node, and also have a
    custom getter (for required inputs and throwing).
    
    To enable this, one small piece was missing. This commit exposes it and
    also improves type safety, now that `SignalNode` is typed properly after
    the previous commit.

--- packages/core/primitives/signals/index.ts
+++ packages/core/primitives/signals/index.ts
@@ -10,6 +10,6 @@ export {createComputed} from './src/computed';
 export {defaultEquals, ValueEqualityFn} from './src/equality';
 export {setThrowInvalidWriteToSignalError} from './src/errors';
 export {consumerAfterComputation, consumerBeforeComputation, consumerDestroy, consumerPollProducersForChange, getActiveConsumer, isInNotificationPhase, isReactive, producerAccessed, producerNotifyConsumers, producerUpdatesAllowed, producerUpdateValueVersion, Reactive, REACTIVE_NODE, ReactiveNode, setActiveConsumer, SIGNAL} from './src/graph';
-export {createSignal, setPostSignalSetFn, SignalGetter, signalMutateFn, SignalNode, signalSetFn, signalUpdateFn} from './src/signal';
+export {createSignal, setPostSignalSetFn, SIGNAL_NODE, SignalGetter, signalMutateFn, SignalNode, signalSetFn, signalUpdateFn} from './src/signal';
 export {createWatch, Watch, WatchCleanupFn, WatchCleanupRegisterFn} from './src/watch';
 export {setAlternateWeakRefImpl} from './src/weak_ref';
