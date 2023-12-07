80ea05d49dc2ee9cc1b573a8f19c965598fe3920
Dylan Hunn <dylhunn@gmail.com>
2023-12-07 11:20:34 -0800

    refactor(compiler): Add failing tests about structural attr bindings (#53405)
    While running a g3 presubmit, I discovered two related novel failure modes:
    
    1. Simple case: this new test uses an `ngFor` structural directive, which binds a context variable. That variable is immediately used in an attribute binding. It looks like we generate an extra attribute instruction, which might result in an invalid property read at runtime.
    2. Complex case: this is another attribute binding, this time on a structural element, inside of an `ng-template`. Not sure what's going on here.
    
    PR Close #53405

---
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_template/ng_for_context_in_attr_binding.ts
@@ -0,0 +1,17 @@
+import {Component, NgModule} from '@angular/core';
+
+@Component({
+  selector: 'my-component',
+  template: `
+	<div *ngFor="let someElem of someField.someMethod()"
+		[attr.someInputAttr]="someElem.someAttr()">
+	</div>
+`,
+})
+export class MyComponent {
+  someField!: any;
+}
+
+@NgModule({declarations: [MyComponent]})
+export class MyModule {
+}
