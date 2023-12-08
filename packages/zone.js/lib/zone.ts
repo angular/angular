829a10937a9da36fee4490efe37b8397d8303397
Doug Parker <dgp1130@users.noreply.github.com>
2023-12-07 20:18:12 -0800

    refactor(zone.js): wrap Zone.js global initialization in a `loadZone` function (#53443)
    This drop the top-level side effects in `zone.ts` and allows the initialization to be reused across the various entry points in the package.
    
    PR Close #53443

--- packages/zone.js/lib/zone.ts
+++ packages/zone.js/lib/zone.ts
@@ -43,20 +43,23 @@ declare global {
   }
 }
 
-// if global['Zone'] already exists (maybe zone.js was already loaded or
-// some other lib also registered a global object named Zone), we may need
-// to throw an error, but sometimes user may not want this error.
-// For example,
-// we have two web pages, page1 includes zone.js, page2 doesn't.
-// and the 1st time user load page1 and page2, everything work fine,
-// but when user load page2 again, error occurs because global['Zone'] already exists.
-// so we add a flag to let user choose whether to throw this error or not.
-// By default, if existing Zone is from zone.js, we will not throw the error.
-const global = globalThis as any;
-const checkDuplicate = global[__symbol__('forceDuplicateZoneCheck')] === true;
-if (global['Zone'] && (checkDuplicate || typeof global['Zone'].__symbol__ !== 'function')) {
-  throw new Error('Zone already loaded.');
-}
+export function loadZone(): ZoneType {
+  // if global['Zone'] already exists (maybe zone.js was already loaded or
+  // some other lib also registered a global object named Zone), we may need
+  // to throw an error, but sometimes user may not want this error.
+  // For example,
+  // we have two web pages, page1 includes zone.js, page2 doesn't.
+  // and the 1st time user load page1 and page2, everything work fine,
+  // but when user load page2 again, error occurs because global['Zone'] already exists.
+  // so we add a flag to let user choose whether to throw this error or not.
+  // By default, if existing Zone is from zone.js, we will not throw the error.
+  const global = globalThis as any;
+  const checkDuplicate = global[__symbol__('forceDuplicateZoneCheck')] === true;
+  if (global['Zone'] && (checkDuplicate || typeof global['Zone'].__symbol__ !== 'function')) {
+    throw new Error('Zone already loaded.');
+  }
 
-// Initialize global `Zone` constant.
-global['Zone'] ??= initZone();
+  // Initialize global `Zone` constant.
+  global['Zone'] ??= initZone();
+  return global['Zone'];
+}
