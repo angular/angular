7c1991048b29351af4ffc59a2b37873334f7929a
Doug Parker <dgp1130@users.noreply.github.com>
2023-12-07 18:24:29 -0800

    refactor(zone.js): wrap `Zone.__load_patch` calls in exported functions (#53443)
    This removes top-level side effects from each of these files and drops the dependency on global `Zone`, instead allowing it to be provided to each patch as a parameter.
    
    Most of these are pure mechanical transformations. A couple notable files which were somewhat unique:
    
    * `async-test.ts`, `fake-async-test.ts`, and `wtf.ts` had unique IIFE usage and patch `Zone` itself. This removes the IIFE and exports the function instead.
    * `jest.ts` and `jasmine.ts` have a unique `jest` global usage which needs to be declared.
    
    PR Close #53443

--- packages/zone.js/lib/browser/message-port.ts
+++ packages/zone.js/lib/browser/message-port.ts
@@ -6,13 +6,17 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-/**
- * Monkey patch `MessagePort.prototype.onmessage` and `MessagePort.prototype.onmessageerror`
- * properties to make the callback in the zone when the value are set.
- */
-Zone.__load_patch('MessagePort', (global: any, Zone: ZoneType, api: _ZonePrivate) => {
-  const MessagePort = global['MessagePort'];
-  if (typeof MessagePort !== 'undefined' && MessagePort.prototype) {
-    api.patchOnProperties(MessagePort.prototype, ['message', 'messageerror']);
-  }
-});
+import {ZoneType} from '../zone-impl';
+
+export function patchMessagePort(Zone: ZoneType): void {
+  /**
+   * Monkey patch `MessagePort.prototype.onmessage` and `MessagePort.prototype.onmessageerror`
+   * properties to make the callback in the zone when the value are set.
+   */
+  Zone.__load_patch('MessagePort', (global: any, Zone: ZoneType, api: _ZonePrivate) => {
+    const MessagePort = global['MessagePort'];
+    if (typeof MessagePort !== 'undefined' && MessagePort.prototype) {
+      api.patchOnProperties(MessagePort.prototype, ['message', 'messageerror']);
+    }
+  });
+}
