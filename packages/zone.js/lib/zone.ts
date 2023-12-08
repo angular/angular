c9e4413b0431eeae0f6f948f9efe294ff3f6f998
Doug Parker <dgp1130@users.noreply.github.com>
2023-12-07 17:09:05 -0800

    refactor(zone.js): move `Zone` global declaration and assignment to `zone.ts`
    Global state is to be managed in `zone.ts` rather than `zone-impl.ts`.

--- packages/zone.js/lib/zone.ts
+++ packages/zone.js/lib/zone.ts
@@ -5,3 +5,12 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+
+import {initZone, Zone as _Zone, ZoneType} from './zone-impl';
+
+declare global {
+  const Zone: ZoneType;
+  type Zone = _Zone;
+}
+
+(globalThis as any)['Zone'] = initZone();
