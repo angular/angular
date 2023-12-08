a934208681f8489e7107c6645c3cf26c3181d8b2
Miles Malerba <mmalerba@google.com>
2023-12-07 16:39:25 -0800

    refactor(compiler): Add i18n support for @defer blocks (#53440)
    Pass through the i18n placeholders for the various parts of the defer
    block during ingestion so its i18n message can be constructed
    
    PR Close #53440

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_i18n/blocks/defer_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_i18n/blocks/defer_template.js
@@ -40,7 +40,7 @@ MyApp.ɵcmp = /*@__PURE__*/ $r3$.ɵɵdefineComponent({
       /**
        * @suppress {msgDescriptions}
        */
-      const MSG_EXTERNAL_7863738818382242773$$DEFER_TS__1 = goog.getMsg(" Content: {$startBlockDefer} before{$startTagSpan}middle{$closeTagSpan}after {$closeBlockDefer}{$startBlockPlaceholder} before{$startTagDiv}placeholder{$closeTagDiv}after {$closeBlockPlaceholder}{$startBlockLoading} before{$startTagButton}loading{$closeTagButton}after {$closeBlockLoading}{$startBlockError} before{$startHeadingLevel1}error{$closeHeadingLevel1}after {$closeBlockError}", {
+      const $MSG_ID_WITH_SUFFIX$ = goog.getMsg(" Content: {$startBlockDefer} before{$startTagSpan}middle{$closeTagSpan}after {$closeBlockDefer}{$startBlockPlaceholder} before{$startTagDiv}placeholder{$closeTagDiv}after {$closeBlockPlaceholder}{$startBlockLoading} before{$startTagButton}loading{$closeTagButton}after {$closeBlockLoading}{$startBlockError} before{$startHeadingLevel1}error{$closeHeadingLevel1}after {$closeBlockError}", {
         "closeBlockDefer": "\uFFFD/*2:1\uFFFD",
         "closeBlockError": "\uFFFD/*5:4\uFFFD",
         "closeBlockLoading": "\uFFFD/*3:2\uFFFD",
@@ -77,7 +77,7 @@ MyApp.ɵcmp = /*@__PURE__*/ $r3$.ɵɵdefineComponent({
           "startTagSpan": "<span>"
         }
       });
-      i18n_0 = MSG_EXTERNAL_7863738818382242773$$DEFER_TS__1;
+      i18n_0 = $MSG_ID_WITH_SUFFIX$;
     } else {
       i18n_0 = $localize ` Content: ${"\uFFFD*2:1\uFFFD"}:START_BLOCK_DEFER: before${"\uFFFD#1:1\uFFFD"}:START_TAG_SPAN:middle${"\uFFFD/#1:1\uFFFD"}:CLOSE_TAG_SPAN:after ${"\uFFFD/*2:1\uFFFD"}:CLOSE_BLOCK_DEFER:${"\uFFFD*4:3\uFFFD"}:START_BLOCK_PLACEHOLDER: before${"\uFFFD#1:3\uFFFD"}:START_TAG_DIV:placeholder${"\uFFFD/#1:3\uFFFD"}:CLOSE_TAG_DIV:after ${"\uFFFD/*4:3\uFFFD"}:CLOSE_BLOCK_PLACEHOLDER:${"\uFFFD*3:2\uFFFD"}:START_BLOCK_LOADING: before${"\uFFFD#1:2\uFFFD"}:START_TAG_BUTTON:loading${"\uFFFD/#1:2\uFFFD"}:CLOSE_TAG_BUTTON:after ${"\uFFFD/*3:2\uFFFD"}:CLOSE_BLOCK_LOADING:${"\uFFFD*5:4\uFFFD"}:START_BLOCK_ERROR: before${"\uFFFD#1:4\uFFFD"}:START_HEADING_LEVEL1:error${"\uFFFD/#1:4\uFFFD"}:CLOSE_HEADING_LEVEL1:after ${"\uFFFD/*5:4\uFFFD"}:CLOSE_BLOCK_ERROR:`;
     }
