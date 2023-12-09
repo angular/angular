8d17bdc574192bd641ad0ca67db0f225a707a3d8
Miles Malerba <mmalerba@google.com>
2023-12-09 10:41:13 -0800

    refactor(compiler): Generate trusted const values for extracted attrs (#53473)
    Use the DomElementSchemaRegistry to determine the correct security
    context for static attributes, and pass it along during ingestion. Then
    during the resolve sanitizers phase, use the security context to
    determine if a trusted value function is needed
    
    PR Close #53473

--- packages/compiler/src/template/pipeline/src/phases/reify.ts
+++ packages/compiler/src/template/pipeline/src/phases/reify.ts
@@ -9,7 +9,7 @@
 import * as o from '../../../../output/output_ast';
 import {Identifiers} from '../../../../render3/r3_identifiers';
 import * as ir from '../../ir';
-import {ViewCompilationUnit, type CompilationJob, type CompilationUnit} from '../compilation';
+import {ComponentCompilationJob, ViewCompilationUnit, type CompilationJob, type CompilationUnit} from '../compilation';
 import * as ng from '../instruction';
 
 /**
@@ -21,6 +21,11 @@ const sanitizerIdentifierMap = new Map<ir.SanitizerFn, o.ExternalReference>([
   [ir.SanitizerFn.ResourceUrl, Identifiers.sanitizeResourceUrl],
   [ir.SanitizerFn.Script, Identifiers.sanitizeScript],
   [ir.SanitizerFn.Style, Identifiers.sanitizeStyle], [ir.SanitizerFn.Url, Identifiers.sanitizeUrl]
+]);
+
+const trustedValueFnIdentifierMap = new Map<ir.TrustedValueFn, o.ExternalReference>([
+  [ir.TrustedValueFn.Html, Identifiers.trustConstantHtml],
+  [ir.TrustedValueFn.ResourceUrl, Identifiers.trustConstantResourceUrl],
 ]);
 
 /**
@@ -35,6 +40,16 @@ export function reify(job: CompilationJob): void {
   for (const unit of job.units) {
     reifyCreateOperations(unit, unit.create);
     reifyUpdateOperations(unit, unit.update);
+  }
+  if (job instanceof ComponentCompilationJob) {
+    reifyConsts(job);
+  }
+}
+
+function reifyConsts(job: ComponentCompilationJob) {
+  for (let i = 0; i < job.consts.length; i++) {
+    job.consts[i] = ir.transformExpressionsInExpression(
+        job.consts[i], reifyIrExpression, ir.VisitorContextFlag.None);
   }
 }
 
@@ -417,6 +432,8 @@ function reifyIrExpression(expr: o.Expression): o.Expression {
       return ng.pipeBindV(expr.targetSlot.slot!, expr.varOffset!, expr.args);
     case ir.ExpressionKind.SanitizerExpr:
       return o.importExpr(sanitizerIdentifierMap.get(expr.fn)!);
+    case ir.ExpressionKind.TrustedValueFnExpr:
+      return o.importExpr(trustedValueFnIdentifierMap.get(expr.fn)!);
     case ir.ExpressionKind.SlotLiteralExpr:
       return o.literal(expr.slot.slot!);
     default:
