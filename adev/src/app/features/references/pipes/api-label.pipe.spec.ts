2e2e8aad9bb4d245c12f1e2768d8b7770527c59a
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2023-12-11 22:31:36 +0100

    test(docs-infra): unit tests in the reference pages (#53529)
    Implementation of additional unit tests for `Reference` pages in `adev`.
    
    PR Close #53529

--- adev/src/app/features/references/pipes/api-label.pipe.spec.ts
+++ adev/src/app/features/references/pipes/api-label.pipe.spec.ts
@@ -6,11 +6,23 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
+import {ApiItemType} from '../interfaces/api-item-type';
 import {ApiLabel} from './api-label.pipe';
 
 describe('ApiLabel', () => {
-  it('create an instance', () => {
-    const pipe = new ApiLabel();
-    expect(pipe).toBeTruthy();
+  let pipe: ApiLabel;
+
+  beforeEach(() => {
+    pipe = new ApiLabel();
+  });
+
+  it(`should return short label when labelType equals short`, () => {
+    const result = pipe.transform(ApiItemType.CLASS, 'short');
+    expect(result).toBe('C');
+  });
+
+  it(`should return short label when labelType equals short`, () => {
+    const result = pipe.transform(ApiItemType.CLASS, 'full');
+    expect(result).toBe('Class');
   });
 });
