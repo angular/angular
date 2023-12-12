a8ed3a65effb9c5a91a952a22d735da1c8199f99
Dylan Hunn <dylhunn@gmail.com>
2023-12-12 11:17:55 -0800

    refactor(compiler): Add test for ICU root message with element tags (#53557)
    Add test for ICU root message with element tags
    
    PR Close #53557

---
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_i18n/icu_logic/icu_root.ts
@@ -0,0 +1,28 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'my-component',
+  standalone: true,
+  template: `
+    <span i18n="someText1">{
+      someField,
+      select,
+      WEBSITE {
+        <strong>someText</strong>
+      }
+    }</span>
+
+    <span>
+    {
+      someField,
+      select,
+      WEBSITE {
+        <strong>someText</strong>
+      }
+    }
+    </span>
+`,
+})
+export class MyComponent {
+  someField!: any;
+}
