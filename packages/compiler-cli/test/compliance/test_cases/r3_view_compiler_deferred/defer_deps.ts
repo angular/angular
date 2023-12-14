721a19332f4c44730831944061c77916dfeff902
Dylan Hunn <dylhunn@gmail.com>
2023-12-14 09:30:02 -0800

    refactor(compiler): Add an escape hatch to use non-unique const pool names
    The template pipeline was producing slightly different names than TemplateDefinitionBuilder for defer deps functions. I have added a workaround in the name of backwards compatibility, to avoid suffixing the const pool function names.

---
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_deferred/defer_deps.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core';
+
+import {CmpA} from './defer_deps_ext';
+
+@Component({
+  selector: 'local-dep',
+  standalone: true,
+  template: 'Local dependency',
+})
+export class LocalDep {
+}
+
+@Component({
+  selector: 'test-cmp',
+  standalone: true,
+  imports: [CmpA, LocalDep],
+  template: `
+	@defer {
+	<cmp-a />
+	<local-dep />
+	}
+`,
+})
+export class TestCmp {
+}
