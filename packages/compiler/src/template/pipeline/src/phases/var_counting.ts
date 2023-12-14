c8c9aeda0737e839d0eb0236c5324b9406cd2446
Dylan Hunn <dylhunn@gmail.com>
2023-12-14 15:54:08 -0800

    refactor(compiler): Repeaters may consume a variable for their empty block (#53574)
    Repeaters consume a variable slot only when they have an empty block.
    
    PR Close #53574

--- packages/compiler/src/template/pipeline/src/phases/var_counting.ts
+++ packages/compiler/src/template/pipeline/src/phases/var_counting.ts
@@ -130,6 +130,12 @@ function varsUsedByOp(op: (ir.CreateOp|ir.UpdateOp)&ir.ConsumesVarsTrait): numbe
     case ir.OpKind.Conditional:
     case ir.OpKind.DeferWhen:
       return 1;
+    case ir.OpKind.RepeaterCreate:
+      // Repeaters may require an extra variable binding slot, if they have an empty view, for the
+      // empty block tracking.
+      // TODO: It's a bit odd to have a create mode instruction consume variable slots. Maybe we can
+      // find a way to use the Repeater update op instead.
+      return op.emptyView ? 1 : 0;
     default:
       throw new Error(`Unhandled op: ${ir.OpKind[op.kind]}`);
   }
