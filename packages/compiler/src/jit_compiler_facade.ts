e0df8534068ff63e45eb31359484826f84da8679
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-12-14 14:26:45 +0000

    refactor(core): add internal signal input support for `@Input` decorator (#53808)
    We are adding internal support for declaring signal inputs via the
    `@Input` decorator. This is needed for JIT unit testing, or JIT
    applications.
    
    In JIT, Angular is not able to recognize signal inputs due to the
    lack of static reflection metadata. Decorators attach their information
    on the class- without it needing to be instantiated. This allows Angular
    to know inputs when preparing/generating the directive definition. With
    signal inputs this is not possible- so we need a way to tell Angular
    about inputs for JIT applications. We've decided that this is not
    something users should have to deal with, so a transform will be added
    in a follow-up that will automatically derive/and add the decorators
    for signal inputs when requested in JIT environments.
    
    PR Close #53808

--- packages/compiler/src/jit_compiler_facade.ts
+++ packages/compiler/src/jit_compiler_facade.ts
@@ -335,8 +335,10 @@ function convertDirectiveFacadeToMetadata(facade: R3DirectiveMetadataFacade): R3
             bindingPropertyName: ann.alias || field,
             classPropertyName: field,
             required: ann.required || false,
-            // TODO(signals): Support JIT signal inputs via decorator transform.
-            isSignal: false,
+            // For JIT, decorators are used to declare signal inputs. That is because of
+            // a technical limitation where it's not possible to statically reflect class
+            // members of a directive/component at runtime before instantiating the class.
+            isSignal: !!ann.isSignal,
             transformFunction: ann.transform != null ? new WrappedNodeExpr(ann.transform) : null,
           };
         } else if (isOutput(ann)) {
