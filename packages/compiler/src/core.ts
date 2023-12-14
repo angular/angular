eee0af05993d71b8a078093d527de77a84e11e28
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-12-14 14:26:45 +0000

    refactor(core): add internal signal input support for `@Input` decorator (#53808)
    We are adding internal support for declaring signal inputs via the
    `@Input` decorator. This is needed for JIT unit testing, or JIT
    applications.
    
    In JIT, Angular is not able to recognize signal inputs due to the
    lack of static reflection metadata. Decorators attach their information
    on the class- without it needing to be instantiated. This allows Angular
    to know inputs when preparing/generating the directive definition. With
    signal inputs this is not possible- so we need a way to tell Angular
    about inputs for JIT applications. We've decided that this is not
    something users should have to deal with, so a transform will be added
    in a follow-up that will automatically derive/and add the decorators
    for signal inputs when requested in JIT environments.
    
    PR Close #53808

--- packages/compiler/src/core.ts
+++ packages/compiler/src/core.ts
@@ -34,6 +34,9 @@ export interface Input {
   alias?: string;
   required?: boolean;
   transform?: (value: any) => any;
+  // Note: This field is marked as `internal` in `@angular/core`, but in the compiler
+  // we rely on it for JIT processing at runtime.
+  isSignal: boolean;
 }
 
 /** Flags describing an input for a directive. */
