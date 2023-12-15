a67c88b88a9ea0b9710bba35114bcdabc41bf362
Miles Malerba <mmalerba@google.com>
2023-12-14 16:22:56 -0800

    refactor(compiler): Make attribute const collection less aggressive (#53580)
    Changes template pipeline to be less aggressive in const collecting
    attrs, to match the behavior of template definition builder. There is
    nothing wrong with the more aggressive const collection, and in fact it
    would be good to re-enable it later, but for now this makes it easier to
    transition from TDB to template pipeline.
    
    Also adds a test to verify that sensitive iframe attributes are properly
    validated.
    
    PR Close #53580

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_bindings/attribute_bindings/chain_multiple_bindings_with_child_elements_template.pipeline.js
+++
@@ -1,9 +0,0 @@
-template: function MyComponent_Template(rf, ctx) {
-	…
-	if (rf & 2) {
-	  $r3$.ɵɵattribute("title", ctx.myTitle)("id", ctx.buttonId)("tabindex", 1);
-	  $r3$.ɵɵadvance(1);
-	  $r3$.ɵɵattribute("id", 1)("some-attr", 1 + 2);
-	}
-  }
-  
