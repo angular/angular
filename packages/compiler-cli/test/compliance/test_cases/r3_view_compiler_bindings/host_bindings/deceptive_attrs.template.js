7c8a2e13eac1a1d6be1ed1ccf02eedf0c2f4630b
Miles Malerba <mmalerba@google.com>
2023-12-18 13:01:12 -0800

    test(compiler): Fix handling of deceptively named attributes (#53626)
    It's possible for the user to create a host attrbiute binding with a
    name that makes it _look_ like a class binding `{['class.foo']: ''}`, we
    were previously treating these as actual class property bindings. This
    change fixes the logic so that only true property bindings cam be
    converted to class property bindings.
    
    Note: A user who added an attribute like the above almost certainly
    intended to create an actual class property binding. It would be nice if
    we could add a diagnostic to warn them about this.
    
    PR Close #53626

---
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_bindings/host_bindings/deceptive_attrs.template.js
@@ -0,0 +1,9 @@
+hostAttrs: ["class.is-compact", "false", "style.width", "0", "attr.tabindex", "5"],
+…
+hostBindings: function MyComponent2_HostBindings(rf, ctx) {
+  if (rf & 2) {
+    i0.ɵɵattribute("tabindex", 5);
+    i0.ɵɵstyleProp("width", 0);
+    i0.ɵɵclassProp("is-compact", false);
+  }
+}
