c3250b01e05947e0d816932a4ee84d84881c5474
Miles Malerba <mmalerba@google.com>
2023-12-18 15:56:26 -0800

    refactor(compiler): Rework how ICU placeholders are handled (#53643)
    The way we were handling ICU placeholders was not compatible with using
    interpolations on attributes of elements inside the ICU. This change
    refactors the handling of ICU placeholders and unifies the way
    expression and tag placeholders work inside ICUs.
    
    The new approach modifies the ingest logic to add the placeholder on to
    the TextOp rather than the TextInterpolationOp. This is because, in
    ICUs, we may need multiple i18n expressions created from the
    interpolation expressions to roll up into the same placeholder. ICUs
    essentially do the interpolation at compile time, combining the static
    strings with special placeholder strings that represent the expression
    values.
    
    PR Close #53643

--- packages/compiler/src/template/pipeline/src/phases/i18n_text_extraction.ts
+++ packages/compiler/src/template/pipeline/src/phases/i18n_text_extraction.ts
@@ -22,6 +22,7 @@ export function convertI18nText(job: CompilationJob): void {
     let currentIcu: ir.IcuStartOp|null = null;
     const textNodeI18nBlocks = new Map<ir.XrefId, ir.I18nStartOp>();
     const textNodeIcus = new Map<ir.XrefId, ir.IcuStartOp|null>();
+    const icuPlaceholderByText = new Map<ir.XrefId, ir.IcuPlaceholderOp>();
     for (const op of unit.create) {
       switch (op.kind) {
         case ir.OpKind.I18nStart:
@@ -46,7 +47,19 @@ export function convertI18nText(job: CompilationJob): void {
           if (currentI18n !== null) {
             textNodeI18nBlocks.set(op.xref, currentI18n);
             textNodeIcus.set(op.xref, currentIcu);
-            ir.OpList.remove<ir.CreateOp>(op);
+            if (op.icuPlaceholder !== null) {
+              // Create an op to represent the ICU placeholder. Initially set its static text to the
+              // value of the text op, though this may be overwritten later if this text op is a
+              // placeholder for an interpolation.
+              const icuPlaceholderOp = ir.createIcuPlaceholderOp(
+                  job.allocateXrefId(), op.icuPlaceholder, [op.initialValue]);
+              ir.OpList.replace<ir.CreateOp>(op, icuPlaceholderOp);
+              icuPlaceholderByText.set(op.xref, icuPlaceholderOp);
+            } else {
+              // Otherwise just remove the text op, since its value is already accounted for in the
+              // translated message.
+              ir.OpList.remove<ir.CreateOp>(op);
+            }
           }
           break;
       }
@@ -63,20 +76,27 @@ export function convertI18nText(job: CompilationJob): void {
 
           const i18nOp = textNodeI18nBlocks.get(op.target)!;
           const icuOp = textNodeIcus.get(op.target);
+          const icuPlaceholder = icuPlaceholderByText.get(op.target);
           const contextId = icuOp ? icuOp.context : i18nOp.context;
           const resolutionTime = icuOp ? ir.I18nParamResolutionTime.Postproccessing :
                                          ir.I18nParamResolutionTime.Creation;
-          const ops: ir.UpdateOp[] = [];
+          const ops: ir.I18nExpressionOp[] = [];
           for (let i = 0; i < op.interpolation.expressions.length; i++) {
             const expr = op.interpolation.expressions[i];
             // For now, this i18nExpression depends on the slot context of the enclosing i18n block.
             // Later, we will modify this, and advance to a different point.
             ops.push(ir.createI18nExpressionOp(
                 contextId!, i18nOp.xref, i18nOp.xref, i18nOp.handle, expr,
-                op.interpolation.i18nPlaceholders[i], resolutionTime, ir.I18nExpressionFor.I18nText,
-                '', expr.sourceSpan ?? op.sourceSpan));
+                icuPlaceholder?.xref ?? null, op.interpolation.i18nPlaceholders[i] ?? null,
+                resolutionTime, ir.I18nExpressionFor.I18nText, '',
+                expr.sourceSpan ?? op.sourceSpan));
           }
           ir.OpList.replaceWithMany(op as ir.UpdateOp, ops);
+          // If this interpolation is part of an ICU placeholder, add the strings and expressions to
+          // the placeholder.
+          if (icuPlaceholder !== undefined) {
+            icuPlaceholder.strings = op.interpolation.strings;
+          }
           break;
       }
     }
