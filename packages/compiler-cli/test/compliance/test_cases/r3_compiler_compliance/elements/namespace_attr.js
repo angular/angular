61e544de90a17213fa37a0dea2a30f79c38184da
Dylan Hunn <dylhunn@gmail.com>
2023-12-19 13:56:25 -0800

    refactor(compiler): Add a test for attribute namespaces (#53646)
    We currently do not support the `namespace` argument for the attribute instruction. (Who knew it even existed!)
    
    PR Close #53646

---
+++ packages/compiler-cli/test/compliance/test_cases/r3_compiler_compliance/elements/namespace_attr.js
@@ -0,0 +1,9 @@
+consts: [["id", ""]],
+template: function MyComponent_Template(rf, ctx) {
+	if (rf & 1) {
+		i0.ɵɵnamespaceSVG();
+		i0.ɵɵelement(0, "use", null, 0);
+	} if (rf & 2) {
+		i0.ɵɵattribute("href", ctx.value, null, "xlink");
+	}
+}
