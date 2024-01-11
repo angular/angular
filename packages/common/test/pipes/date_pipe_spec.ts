cdc5e3953237a192beafd6330f9d9e36ede34f2c
Matthieu Riegler <kyro38@gmail.com>
2024-01-11 15:36:44 +0100

    fix(common): The date pipe should return ISO format for week and week-year as intended in the unit test. (#53879)
    ISO 8601 defines
    * Monday as the first day of the week.
    * week 01 is the week with the first Thursday
    
    Therefore:
    Sunday Dec 31st 2023 is the last day of the last week of the year : W52 2023.
    
    PR Close #53879

--- packages/common/test/pipes/date_pipe_spec.ts
+++ packages/common/test/pipes/date_pipe_spec.ts
@@ -120,13 +120,21 @@ describe('DatePipe', () => {
 
     it('should return first week if some dates fall in previous year but belong to next year according to ISO 8601 format', () => {
       expect(pipe.transform('2019-12-28T00:00:00', 'w')).toEqual('52');
-      expect(pipe.transform('2019-12-29T00:00:00', 'w')).toEqual('1');
+
+      // December 29th is a Sunday, week number is from previous thursday
+      expect(pipe.transform('2019-12-29T00:00:00', 'w')).toEqual('52');
+
+      // December 30th is a monday, week number is from next thursday
       expect(pipe.transform('2019-12-30T00:00:00', 'w')).toEqual('1');
     });
 
     it('should return first week if some dates fall in previous leap year but belong to next year according to ISO 8601 format', () => {
       expect(pipe.transform('2012-12-29T00:00:00', 'w')).toEqual('52');
-      expect(pipe.transform('2012-12-30T00:00:00', 'w')).toEqual('1');
+
+      // December 30th is a Sunday, week number is from previous thursday
+      expect(pipe.transform('2012-12-30T00:00:00', 'w')).toEqual('52');
+
+      // December 31th is a monday, week number is from next thursday
       expect(pipe.transform('2012-12-31T00:00:00', 'w')).toEqual('1');
     });
 
