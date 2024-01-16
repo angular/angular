1a0cbb5dab8297db6671a4a018dc0f38fa4ebb1a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-01-16 15:04:27 +0000

    refactor(compiler-cli): expose helper for recognizing initializer APIs (#53978)
    The new `input` API is recognized using class member initializers.
    We want to support similar APIs for queries, using e.g. `viewChild`
    or `viewChild.required`.
    
    This commit extracts the input recognition API and makes it reusable,
    so that the same logic can be used to detect queries on a class member.
    
    Additional changes:
      - replacing `coreModule` with the simpler `isCore` parameter. This is
        more readable.
      - support for detecting a list of API names on a single class member.
        This allows us to detect possible query functions on the same class
        member without having to check X times. We simply check for the
        initializer API pattern and check if one API function name matches.
    
    PR Close #53978

--- packages/compiler-cli/src/ngtsc/annotations/directive/src/input_function.ts
+++ packages/compiler-cli/src/ngtsc/annotations/directive/src/input_function.ts
@@ -12,103 +12,7 @@ import {ErrorCode, FatalDiagnosticError} from '../../../diagnostics';
 import {InputMapping} from '../../../metadata';
 import {ClassMember, ReflectionHost, reflectObjectLiteral} from '../../../reflection';
 
-/** Metadata describing an input declared via the `input` function. */
-interface InputMemberMetadata {
-  /** Node referring to the call expression. */
-  inputCall: ts.CallExpression;
-  /** Node referring to the options expression, if specified. */
-  optionsNode: ts.Expression|undefined;
-  /** Whether the input is required or not. i.e. `input.required` was used. */
-  isRequired: boolean;
-}
-
-/**
- * Attempts to identify and parse an Angular input that is declared
- * as a class member using the `input`/`input.required` functions.
- */
-function tryParseInputInitializerAndOptions(
-    member: Pick<ClassMember, 'value'>, reflector: ReflectionHost,
-    coreModule: string|undefined): InputMemberMetadata|null {
-  if (member.value === null || !ts.isCallExpression(member.value)) {
-    return null;
-  }
-  const call = member.value;
-
-  // Extract target. Either:
-  //    - `[input]`
-  //    - `core.[input]`
-  //    - `input.[required]`
-  //    - `core.input.[required]`.
-  let target = extractPropertyTarget(call.expression);
-  if (target === null) {
-    return null;
-  }
-
-  // Case 1: No `required`.
-  if (target.text === 'input') {
-    if (!isReferenceToInputFunction(target, coreModule, reflector)) {
-      return null;
-    }
-    const optionsNode: ts.Expression|undefined = call.arguments[1];
-    return {inputCall: call, optionsNode, isRequired: false};
-  }
-
-  // Case 2: Using `required.
-  // Ensure there is a property access to `[input].required` or `[core.input].required`.
-  if (target.text !== 'required' || !ts.isPropertyAccessExpression(call.expression)) {
-    return null;
-  }
-
-  const inputCall = call.expression;
-  target = extractPropertyTarget(inputCall.expression);
-  if (target === null || !isReferenceToInputFunction(target, coreModule, reflector)) {
-    // Ensure the call refers to the real `input` function from Angular core.
-    return null;
-  }
-
-  const optionsNode: ts.Expression|undefined = call.arguments[0];
-
-  return {
-    inputCall: call,
-    optionsNode,
-    isRequired: true,
-  };
-}
-
-/**
- * Extracts the identifier property target of a expression, supporting
- * one level deep property accesses.
- *
- * e.g. `input.required` will return `input`.
- * e.g. `input` will return `input`.
- *
- */
-function extractPropertyTarget(node: ts.Expression): ts.Identifier|null {
-  if (ts.isPropertyAccessExpression(node) && ts.isIdentifier(node.name)) {
-    return node.name;
-  } else if (ts.isIdentifier(node)) {
-    return node;
-  }
-  return null;
-}
-
-/**
- * Verifies that the given identifier resolves to the `input` expression from
- * Angular core.
- */
-function isReferenceToInputFunction(
-    target: ts.Identifier, coreModule: string|undefined, reflector: ReflectionHost): boolean {
-  let targetImport: {name: string}|null = reflector.getImportOfIdentifier(target);
-  if (targetImport === null) {
-    if (coreModule !== undefined) {
-      return false;
-    }
-    // We are compiling the core module, where no import can be present.
-    targetImport = {name: target.text};
-  }
-
-  return targetImport.name === 'input';
-}
+import {tryParseInitializerApiMember} from './initializer_functions';
 
 /** Parses and validates the input function options expression. */
 function parseAndValidateOptions(optionsNode: ts.Expression): {alias: string|undefined} {
@@ -141,13 +45,15 @@ function parseAndValidateOptions(optionsNode: ts.Expression): {alias: string|und
  */
 export function tryParseSignalInputMapping(
     member: Pick<ClassMember, 'name'|'value'>, reflector: ReflectionHost,
-    coreModule: string|undefined): InputMapping|null {
-  const signalInput = tryParseInputInitializerAndOptions(member, reflector, coreModule);
+    isCore: boolean): InputMapping|null {
+  const signalInput = tryParseInitializerApiMember(['input'], member, reflector, isCore);
   if (signalInput === null) {
     return null;
   }
 
-  const optionsNode = signalInput.optionsNode;
+  const optionsNode = (signalInput.isRequired ? signalInput.call.arguments[0] :
+                                                signalInput.call.arguments[1]) as ts.Expression |
+      undefined;
   const options = optionsNode !== undefined ? parseAndValidateOptions(optionsNode) : null;
   const classPropertyName = member.name;
 
