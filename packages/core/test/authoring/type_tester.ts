8f9fad683b26d98f692ae7953a31b28f6f3fcd2e
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 13:24:34 -0800

    Revert "test(core): support type tests for different authoring functions"
    This reverts commit 67e0e84a8752631ece1c070cd70f0223d879c70c.

--- packages/core/test/authoring/type_tester.ts
+++ packages/core/test/authoring/type_tester.ts
@@ -12,26 +12,15 @@ import ts from 'typescript';
 import url from 'url';
 
 const containingDir = path.dirname(url.fileURLToPath(import.meta.url));
+const testDtsFile = path.join(containingDir, 'signal_input_signature_test.d.ts');
 
-/**
- * Verify that we are looking at a test class declaration (a given file might have multiple class
- * declarations).
- */
-function isTestClass(classDeclaration: ts.ClassDeclaration): boolean {
-  return classDeclaration.name !== undefined && classDeclaration.name.text.endsWith('Test');
-}
-
-function testFile(testFileName: string, signalType: string, inferWriteType: boolean): boolean {
-  const fileContent = fs.readFileSync(path.join(containingDir, `${testFileName}.d.ts`), 'utf8');
+async function main() {
+  const fileContent = fs.readFileSync(testDtsFile, 'utf8');
   const sourceFile = ts.createSourceFile('test.ts', fileContent, ts.ScriptTarget.ESNext, true);
-  const testClazz = sourceFile.statements.find(
-      (s): s is ts.ClassDeclaration => ts.isClassDeclaration(s) && isTestClass(s));
-
-  if (testClazz === undefined) {
-    return false;
-  }
-
+  const testClazz =
+      sourceFile.statements.find((s): s is ts.ClassDeclaration => ts.isClassDeclaration(s))!;
   let failing = false;
+
   for (const member of testClazz.members) {
     if (!ts.isPropertyDeclaration(member)) {
       continue;
@@ -47,11 +36,11 @@ function testFile(testFileName: string, signalType: string, inferWriteType: bool
     // strip comment start, and beginning (plus whitespace).
     let expectedTypeComment = leadingComments[0].replace(/(^\/\*\*?\s*|\s*\*+\/$)/g, '');
     // expand shorthands where ReadT is the same as WriteT.
-    if (inferWriteType && !expectedTypeComment.includes(',')) {
+    if (!expectedTypeComment.includes(',')) {
       expectedTypeComment = `${expectedTypeComment}, ${expectedTypeComment}`;
     }
 
-    const expectedType = `${signalType}<${expectedTypeComment}>`;
+    const expectedType = `InputSignal<${expectedTypeComment}>`;
     // strip excess whitespace or newlines.
     const got = member.type?.getText().replace(/(\n+|\s\s+)/g, '');
 
@@ -60,15 +49,6 @@ function testFile(testFileName: string, signalType: string, inferWriteType: bool
       failing = true;
     }
   }
-
-  return failing;
-}
-
-async function main() {
-  let failing = false;
-
-  failing ||= testFile('signal_input_signature_test', 'InputSignal', true);
-  failing ||= testFile('signal_queries_signature_test', 'Signal', false);
 
   if (failing) {
     throw new Error('Failing assertions');
