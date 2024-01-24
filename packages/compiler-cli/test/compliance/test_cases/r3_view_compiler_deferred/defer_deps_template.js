c3bb00a2ebf40ceb6f58db3d3cc7d986fe1778f1
Dylan Hunn <dylhunn@gmail.com>
2024-01-24 14:25:49 -0800

    refactor(compiler): Fix defer deps fn duplicate names in Template Pipeline (#54060)
    Previously, defer deps fns names were only prefixed with the component name, meaning that distinct deps fns in the same component would produce a name collision. Now, we take into account the entire template function name when naming inner deps fns.
    
    PR Close #54060

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_deferred/defer_deps_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_deferred/defer_deps_template.js
@@ -1,4 +1,4 @@
-const TestCmp_Defer_1_DepsFn = () => [import("./defer_deps_ext").then(m => m.CmpA), LocalDep];
+const $TestCmp_Defer_1_DepsFn$ = () => [import("./defer_deps_ext").then(m => m.CmpA), LocalDep];
 
 function TestCmp_Defer_0_Template(rf, ctx) {
 	if (rf & 1) {
@@ -13,6 +13,6 @@ export class LocalDep {
 
 function TestCmp_Template(rf, ctx) { if (rf & 1) {
 	i0.ɵɵtemplate(0, TestCmp_Defer_0_Template, 2, 0);
-	i0.ɵɵdefer(1, 0, TestCmp_Defer_1_DepsFn);
+	i0.ɵɵdefer(1, 0, $TestCmp_Defer_1_DepsFn$);
 	i0.ɵɵdeferOnIdle();
 } }
--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_deferred/lazy_with_blocks_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_deferred/lazy_with_blocks_template.js
@@ -1,4 +1,4 @@
-const SimpleComponent_Defer_5_DepsFn = () => [MyLazyCmp];
+const $SimpleComponent_Defer_5_DepsFn$ = () => [MyLazyCmp];
 
 function SimpleComponent_Defer_1_Template(rf, ctx) {
 	if (rf & 1) {
@@ -42,7 +42,7 @@ template: function SimpleComponent_Template(rf, ctx) {
 	if (rf & 1) {
 		i0.ɵɵtext(0);
 		i0.ɵɵtemplate(1, SimpleComponent_Defer_1_Template, 1, 0)(2, SimpleComponent_DeferLoading_2_Template, 1, 0)(3, SimpleComponent_DeferPlaceholder_3_Template, 1, 0)(4, SimpleComponent_DeferError_4_Template, 1, 0);
-		i0.ɵɵdefer(5, 1, SimpleComponent_Defer_5_DepsFn, 2, 3, 4);
+		i0.ɵɵdefer(5, 1, $SimpleComponent_Defer_5_DepsFn$, 2, 3, 4);
 	} if (rf & 2) {
 		i0.ɵɵtextInterpolate1(" Visible: ", ctx.isVisible, ". ");
 		i0.ɵɵadvance(5);
