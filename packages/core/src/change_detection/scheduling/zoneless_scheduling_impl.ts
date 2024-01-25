a45e69f627f85719b0df28cca80ce11518d2c98c
Andrew Scott <atscott@google.com>
2024-01-25 15:48:13 -0800

    refactor(core): render hooks should always run on node attach or detach (#54083)
    This commit ensures that render hooks are rerun when a node is attached
    or detached. We do not necessarily need to run change detection but DOM
    did change so render hooks should execute.
    
    PR Close #54083

--- packages/core/src/change_detection/scheduling/zoneless_scheduling_impl.ts
+++ packages/core/src/change_detection/scheduling/zoneless_scheduling_impl.ts
@@ -12,21 +12,19 @@ import {PendingTasks} from '../../pending_tasks';
 import {global} from '../../util/global';
 import {NgZone, NoopNgZone} from '../../zone/ng_zone';
 
-import {ChangeDetectionScheduler, NotificationSource} from './zoneless_scheduling';
+import {ChangeDetectionScheduler, NotificationType} from './zoneless_scheduling';
 
 @Injectable({providedIn: 'root'})
 class ChangeDetectionSchedulerImpl implements ChangeDetectionScheduler {
   private appRef = inject(ApplicationRef);
   private taskService = inject(PendingTasks);
   private pendingRenderTaskId: number|null = null;
-  private shouldRefreshViews = true;
+  private shouldRefreshViews = false;
 
-  notify(source = NotificationSource.Other): void {
+  notify(type = NotificationType.RefreshViews): void {
     // When the only source of notification is an afterRender hook will skip straight to the hooks
     // rather than refreshing views in ApplicationRef.tick
-    const shouldRefreshViewsDueToNotification = source !== NotificationSource.AfterRenderHook;
-    this.shouldRefreshViews ||= shouldRefreshViewsDueToNotification;
-
+    this.shouldRefreshViews ||= type === NotificationType.RefreshViews;
     if (this.pendingRenderTaskId !== null) {
       return;
     }
@@ -74,7 +72,7 @@ class ChangeDetectionSchedulerImpl implements ChangeDetectionScheduler {
         this.appRef._tick(this.shouldRefreshViews);
       }
     } finally {
-      this.shouldRefreshViews = true;
+      this.shouldRefreshViews = false;
       // If this is the last task, the service will synchronously emit a stable notification. If
       // there is a subscriber that then acts in a way that tries to notify the scheduler again,
       // we need to be able to respond to schedule a new change detection. Therefore, we should
