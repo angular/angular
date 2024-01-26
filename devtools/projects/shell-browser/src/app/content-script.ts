02fcd928b22151a34bfa01e192dd1a6ef856ed8b
AleksanderBodurri <alexbodurri1117@gmail.com>
2024-01-25 19:31:57 -0500

    refactor(devtools): implement iframe support for Angular DevTools' browser code (#53934)
    Modifies the messaging layer of devtools to allow for switching communication between frames on a page. When served as a browser extension.
    
    Design:
    - When a page renders, DevTools installs a content script onto it through it's manifest file. The all_frames option is used here to install this script onto every frame in a page.
    - When Angular is detected, the content script will install a backend script into it's frame.
    - Each content script / backend script pairing is kept track of in the background script. This pairing represents an angular devtools context in a particular frame.
    - Angular DevTools is able to ask the background script to list each frame that has been registered on a page.
    - Angular Devtools is able to ask the background script to "enable" the connection on a particular frame. This enables the messaging between the content script <-> background script <-> devtools page
    
    Limitations:
    - The `inspectedWindow.eval` API is only able to target frames by frameURL. This means some features that integrate with Chrome DevTools like inspect element and open source will not be available when inspecting frames that do not have a unique url on the page.
    
    PR Close #53934

--- devtools/projects/shell-browser/src/app/content-script.ts
+++ devtools/projects/shell-browser/src/app/content-script.ts
@@ -9,59 +9,92 @@
 import {ChromeMessageBus} from './chrome-message-bus';
 import {SamePageMessageBus} from './same-page-message-bus';
 
-export const main = () => {
-  let backgroundDisconnected = false;
-  let backendInitialized = false;
+let backgroundDisconnected = false;
+let backendInstalled = false;
+let backendInitialized = false;
 
-  // console.log('Content script executing', (window as any));
+const port = chrome.runtime.connect({
+  name: `${document.title || location.href}`,
+});
 
-  const port = chrome.runtime.connect({
-    name: 'content-script',
-  });
-
-  const handleDisconnect = (): void => {
-    // console.log('Background disconnected', new Date());
-    localMessageBus.emit('shutdown');
-    localMessageBus.destroy();
-    chromeMessageBus.destroy();
-    backgroundDisconnected = true;
-  };
-
-  port.onDisconnect.addListener(handleDisconnect);
-
-  const localMessageBus = new SamePageMessageBus(
-    'angular-devtools-content-script',
-    'angular-devtools-backend',
-  );
-  const chromeMessageBus = new ChromeMessageBus(port);
-
-  const handshakeWithBackend = (): void => {
-    localMessageBus.emit('handshake');
-  };
-
-  chromeMessageBus.onAny((topic, args) => {
-    localMessageBus.emit(topic, args);
-  });
-
-  localMessageBus.onAny((topic, args) => {
-    backendInitialized = true;
-    chromeMessageBus.emit(topic, args);
-  });
-
-  if (!backendInitialized) {
-    // tslint:disable-next-line:no-console
-    console.log('Attempting initialization', new Date());
-
-    const retry = () => {
-      if (backendInitialized || backgroundDisconnected) {
-        return;
-      }
-      handshakeWithBackend();
-      setTimeout(retry, 500);
-    };
-    retry();
-  }
+const handleDisconnect = (): void => {
+  // console.log('Background disconnected', new Date());
+  localMessageBus.emit('shutdown');
+  localMessageBus.destroy();
+  chromeMessageBus.destroy();
+  backgroundDisconnected = true;
 };
 
-// expose to use as callback for chrome.tabs.executeScript in background.ts
-(globalThis as any).main = main;
+port.onDisconnect.addListener(handleDisconnect);
+
+const detectAngularMessageBus = new SamePageMessageBus(
+  `angular-devtools-content-script-${location.href}`,
+  `angular-devtools-detect-angular-${location.href}`,
+);
+
+detectAngularMessageBus.on('detectAngular', (detectionResult) => {
+  // only install backend once
+  if (backendInstalled) {
+    return;
+  }
+
+  if (detectionResult.isAngularDevTools !== true) {
+    return;
+  }
+
+  if (detectionResult.isAngular !== true) {
+    return;
+  }
+
+  if (detectionResult.isDebugMode !== true) {
+    return;
+  }
+
+  if (detectionResult.isSupportedAngularVersion !== true) {
+    return;
+  }
+
+  // Defensive check against non html page. Realistically this should never happen.
+  if (document.contentType !== 'text/html') {
+    return;
+  }
+
+  const script = document.createElement('script');
+  script.src = chrome.runtime.getURL('app/backend_bundle.js');
+  document.documentElement.appendChild(script);
+  document.documentElement.removeChild(script);
+  backendInstalled = true;
+});
+
+const localMessageBus = new SamePageMessageBus(
+  `angular-devtools-content-script-${location.href}`,
+  `angular-devtools-backend-${location.href}`,
+);
+const chromeMessageBus = new ChromeMessageBus(port);
+
+const handshakeWithBackend = (): void => {
+  localMessageBus.emit('handshake');
+};
+
+chromeMessageBus.onAny((topic, args) => {
+  localMessageBus.emit(topic, args);
+});
+
+localMessageBus.onAny((topic, args) => {
+  backendInitialized = true;
+  chromeMessageBus.emit(topic, args);
+});
+
+if (!backendInitialized) {
+  // tslint:disable-next-line:no-console
+  console.log('Attempting initialization', new Date());
+
+  const retry = () => {
+    if (backendInitialized || backgroundDisconnected) {
+      return;
+    }
+    handshakeWithBackend();
+    setTimeout(retry, 500);
+  };
+  retry();
+}
