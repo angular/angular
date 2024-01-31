e8e1178e42ba0fe2eb24247b1781f19dce8c8105
Doug Parker <dgp1130@users.noreply.github.com>
2024-01-30 17:25:04 -0800

    refactor(zone.js): fix Bluebird test by updating the entry point and calling patches appropriately (#53443)
    This executes the patches in the top-level scope of the Bluebird test.
    
    PR Close #53443

--- packages/zone.js/test/node_bluebird_entry_point.init.ts
+++ packages/zone.js/test/node_bluebird_entry_point.init.ts
@@ -6,25 +6,43 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-// Must be loaded before zone loads, so that zone can detect WTF.
-import './test_fake_polyfill';
-// Setup tests for Zone without microtask support
-import '../lib/zone';
-import '../lib/common/promise';
-import '../lib/common/to-string';
-import '../lib/node/node';
 // Setup test environment
 import './test-env-setup-jasmine';
 import './wtf_mock';
-import '../lib/zone-spec/async-test';
-import '../lib/zone-spec/fake-async-test';
-import '../lib/zone-spec/long-stack-trace';
-import '../lib/zone-spec/proxy';
-import '../lib/zone-spec/sync-test';
-import '../lib/zone-spec/task-tracking';
-import '../lib/zone-spec/wtf';
-import '../lib/rxjs/rxjs';
-import '../lib/testing/promise-testing';
+
+import {patchPromise} from '../lib/common/promise';
+import {patchToString} from '../lib/common/to-string';
+import {patchNode} from '../lib/node/node';
+import {patchRxJs} from '../lib/rxjs/rxjs';
+import {patchPromiseTesting} from '../lib/testing/promise-testing';
+import {loadZone} from '../lib/zone';
+import {patchAsyncTest} from '../lib/zone-spec/async-test';
+import {patchFakeAsyncTest} from '../lib/zone-spec/fake-async-test';
+import {patchLongStackTrace} from '../lib/zone-spec/long-stack-trace';
+import {patchProxyZoneSpec} from '../lib/zone-spec/proxy';
+import {patchSyncTest} from '../lib/zone-spec/sync-test';
+import {patchTaskTracking} from '../lib/zone-spec/task-tracking';
+import {patchWtf} from '../lib/zone-spec/wtf';
+
+import {setupFakePolyfill} from './test_fake_polyfill';
+
+// Must be loaded before zone loads, so that zone can detect WTF.
+setupFakePolyfill();
+
+// Setup tests for Zone without microtask support
+const Zone = loadZone();
+patchPromise(Zone);
+patchToString(Zone);
+patchNode(Zone);
+patchAsyncTest(Zone);
+patchFakeAsyncTest(Zone);
+patchLongStackTrace(Zone);
+patchProxyZoneSpec(Zone);
+patchSyncTest(Zone);
+patchTaskTracking(Zone);
+patchWtf(Zone);
+patchRxJs(Zone);
+patchPromiseTesting(Zone);
 
 const globalErrors = (jasmine as any).GlobalErrors;
 const symbol = Zone.__symbol__;
