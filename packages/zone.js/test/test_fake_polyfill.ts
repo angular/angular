417cd71830d51ff42f07da36355e52e1686e390b
Doug Parker <dgp1130@users.noreply.github.com>
2024-02-01 19:59:26 -0800

    refactor(zone.js): updates fake polyfill to remove top-level side effect (#53443)
    This shifts the side effect from `test_Fake_polyfill.ts` into `browser-env-setup.ts` instead.
    
    PR Close #53443

--- packages/zone.js/test/test_fake_polyfill.ts
+++ packages/zone.js/test/test_fake_polyfill.ts
@@ -9,81 +9,83 @@
 /// <reference types="node"/>
 
 'use strict';
-(function(global: any) {
-// add custom properties to Native Error
-const NativeError = global['Error'];
-NativeError.customProperty = 'customProperty';
-NativeError.customFunction = function() {};
 
-// add fake cordova polyfill for test
-const fakeCordova = function() {};
+const global: any = globalThis;
+export function setupFakePolyfill(): void {
+  // add custom properties to Native Error
+  const NativeError = global['Error'];
+  NativeError.customProperty = 'customProperty';
+  NativeError.customFunction = function() {};
 
-(fakeCordova as any).exec = function(
-    success: Function, error: Function, service: string, action: string, args: any[]) {
-  if (action === 'successAction') {
-    success();
-  } else {
-    error();
-  }
-};
+  // add fake cordova polyfill for test
+  const fakeCordova = function() {};
 
-global.cordova = fakeCordova;
+  (fakeCordova as any).exec = function(
+      success: Function, error: Function, service: string, action: string, args: any[]) {
+    if (action === 'successAction') {
+      success();
+    } else {
+      error();
+    }
+  };
 
-const TestTarget = global.TestTarget = function() {};
+  global.cordova = fakeCordova;
 
-Object.defineProperties(TestTarget.prototype, {
-  'onprop1': {configurable: true, writable: true},
-  'onprop2': {configurable: true, writable: true},
-  'onprop3': {
-    configurable: true,
-    get: function() {
-      return this._onprop3;
+  const TestTarget = global.TestTarget = function() {};
+
+  Object.defineProperties(TestTarget.prototype, {
+    'onprop1': {configurable: true, writable: true},
+    'onprop2': {configurable: true, writable: true},
+    'onprop3': {
+      configurable: true,
+      get: function() {
+        return this._onprop3;
+      },
+      set: function(_value) {
+        this._onprop3 = _value;
+      }
     },
-    set: function(_value) {
-      this._onprop3 = _value;
-    }
-  },
-  '_onprop3': {configurable: true, writable: true, value: function() {}},
-  'addEventListener': {
-    configurable: true,
-    writable: true,
-    value: function(eventName: string, callback: Function) {
-      if (!this.events) {
-        this.events = {};
+    '_onprop3': {configurable: true, writable: true, value: function() {}},
+    'addEventListener': {
+      configurable: true,
+      writable: true,
+      value: function(eventName: string, callback: Function) {
+        if (!this.events) {
+          this.events = {};
+        }
+        const Zone = global.Zone;
+        this.events.eventName = {zone: Zone.current, callback: callback};
       }
-      const Zone = global.Zone;
-      this.events.eventName = {zone: Zone.current, callback: callback};
-    }
-  },
-  'removeEventListener': {
-    configurable: true,
-    writable: true,
-    value: function(eventName: string, callback: Function) {
-      if (!this.events) {
-        return;
+    },
+    'removeEventListener': {
+      configurable: true,
+      writable: true,
+      value: function(eventName: string, callback: Function) {
+        if (!this.events) {
+          return;
+        }
+        this.events.eventName = null;
       }
-      this.events.eventName = null;
+    },
+    'dispatchEvent': {
+      configurable: true,
+      writable: true,
+      value: function(eventName: string) {
+        const zoneCallback = this.events && this.events.eventName;
+        zoneCallback && zoneCallback.zone.run(zoneCallback.callback, this, [{type: eventName}]);
+      }
     }
-  },
-  'dispatchEvent': {
-    configurable: true,
-    writable: true,
-    value: function(eventName: string) {
-      const zoneCallback = this.events && this.events.eventName;
-      zoneCallback && zoneCallback.zone.run(zoneCallback.callback, this, [{type: eventName}]);
-    }
-  }
-});
+  });
 
-// Zone symbol prefix may be set in *-env-setup.ts (browser & node),
-// but this file is used in multiple scenarios, and Zone isn't loaded at this point yet.
-const zoneSymbolPrefix = global['__Zone_symbol_prefix'] || '__zone_symbol__';
+  // Zone symbol prefix may be set in *-env-setup.ts (browser & node),
+  // but this file is used in multiple scenarios, and Zone isn't loaded at this point yet.
+  const zoneSymbolPrefix = global['__Zone_symbol_prefix'] || '__zone_symbol__';
 
-global['__Zone_ignore_on_properties'] =
-    [{target: TestTarget.prototype, ignoreProperties: ['prop1']}];
-global[zoneSymbolPrefix + 'FakeAsyncTestMacroTask'] = [{source: 'TestClass.myTimeout'}];
-// will not monkey patch scroll and wheel event.
-global[zoneSymbolPrefix + 'UNPATCHED_EVENTS'] = ['scroll', 'wheel'];
-// touchstart and scroll will be passive by default.
-global[zoneSymbolPrefix + 'PASSIVE_EVENTS'] = ['touchstart', 'scroll'];
-})(globalThis);
+  global['__Zone_ignore_on_properties'] =
+      [{target: TestTarget.prototype, ignoreProperties: ['prop1']}];
+  global[zoneSymbolPrefix + 'FakeAsyncTestMacroTask'] = [{source: 'TestClass.myTimeout'}];
+  // will not monkey patch scroll and wheel event.
+  global[zoneSymbolPrefix + 'UNPATCHED_EVENTS'] = ['scroll', 'wheel'];
+  // touchstart and scroll will be passive by default.
+  global[zoneSymbolPrefix + 'PASSIVE_EVENTS'] = ['touchstart', 'scroll'];
+}
