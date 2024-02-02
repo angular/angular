8eea1aeb7f0ad741670e294c7195f846539347c0
Doug Parker <dgp1130@users.noreply.github.com>
2024-02-01 17:32:09 -0800

    refactor(zone.js): export remaining Zone types as globals
    These are all the symbols originally under `declare global {}` with the same names. Unfortunatley TypeScript treats a `declare global {}` scope as the parent scope, so we need to import with a different name than the global, meaning everything gets an `_` prefix.

--- packages/zone.js/lib/zone.ts
+++ packages/zone.js/lib/zone.ts
@@ -6,11 +6,26 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {initZone, Zone as _Zone, ZoneType} from './zone-impl';
+import {EventTask as _EventTask, HasTaskState as _HasTaskState, initZone, MacroTask as _MacroTask, MicroTask as _MicroTask, PatchFn, Task as _Task, TaskData as _TaskData, TaskState as _TaskState, TaskType as _TaskType, UncaughtPromiseError as _UncaughtPromiseError, Zone as _Zone, ZoneDelegate as _ZoneDelegate, ZoneFrame, ZonePrivate, ZoneSpec as _ZoneSpec, ZoneType as _ZoneType} from './zone-impl';
 
 declare global {
   const Zone: ZoneType;
   type Zone = _Zone;
+  type ZoneType = _ZoneType;
+  type _PatchFn = PatchFn;
+  type _ZonePrivate = ZonePrivate;
+  type _ZoneFrame = ZoneFrame;
+  type UncaughtPromiseError = _UncaughtPromiseError;
+  type ZoneSpec = _ZoneSpec;
+  type ZoneDelegate = _ZoneDelegate;
+  type HasTaskState = _HasTaskState;
+  type TaskType = _TaskType;
+  type TaskState = _TaskState;
+  type TaskData = _TaskData;
+  type Task = _Task;
+  type MicroTask = _MicroTask;
+  type MacroTask = _MacroTask;
+  type EventTask = _EventTask;
 }
 
 (globalThis as any)['Zone'] = initZone();
