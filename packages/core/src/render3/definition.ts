4586d2341db293657ec31b891d3eb2f75f104782
Matthieu Riegler <kyro38@gmail.com>
2024-02-04 12:42:16 +0100

    docs: remove an outdated comment. (#54243)
    Note: We still expect `ngOnChanges` to not be supported in future signal components.
    
    PR Close #54243

--- packages/core/src/render3/definition.ts
+++ packages/core/src/render3/definition.ts
@@ -79,9 +79,7 @@ import {stringifyCSSSelectorList} from './node_selector_matcher';
  *  - Because declared and public name are usually same we only generate the array
  *    `['declared', 'public']` format when they differ, or there is a transform.
  *  - The reason why this API and `outputs` API is not the same is that `NgOnChanges` has
- *    inconsistent behavior in that it uses declared names rather than minified or public. For
- *    this reason `NgOnChanges` will be deprecated and removed in future version and this
- *    API will be simplified to be consistent with `output`.
+ *    inconsistent behavior in that it uses declared names rather than minified or public. 
  */
 type DirectiveInputs<T> = {
   [P in keyof T]?:
