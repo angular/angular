5a33faee23a409dae05c1fe1d7f2f72a5a891ecf
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-02-05 11:34:17 +0100

    refactor(compiler-cli): add tests for model inputs
    Adds tests in the compiler to verify the compiled output and template type checking behavior of model inputs.

---
+++ packages/compiler-cli/test/compliance/test_cases/model_inputs/model_component_definition.js
@@ -0,0 +1,12 @@
+TestComp.ɵcmp = /*@__PURE__*/ $r3$.ɵɵdefineComponent({
+  …
+  inputs: {
+    counter: [$r3$.ɵɵInputFlags.SignalBased, "counter"],
+    name: [$r3$.ɵɵInputFlags.SignalBased, "name"]
+  },
+  outputs: {
+    counter: "counterChange",
+    name: "nameChange"
+  },
+  …
+});
---
+++ packages/compiler-cli/test/compliance/test_cases/model_inputs/model_directive_definition.js
@@ -0,0 +1,12 @@
+TestDir.ɵdir = /*@__PURE__*/ $r3$.ɵɵdefineDirective({
+  …
+  inputs: {
+    counter: [$r3$.ɵɵInputFlags.SignalBased, "counter"],
+    name: [$r3$.ɵɵInputFlags.SignalBased, "name"]
+  },
+  outputs: {
+    counter: "counterChange",
+    name: "nameChange"
+  },
+  …
+});
