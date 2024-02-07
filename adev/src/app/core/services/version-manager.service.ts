1e066ea429796b05bd66fdae1d110edad74176f6
Ben Hong <ben@bencodezen.io>
2024-02-07 13:02:12 -0800

    docs: enable version picker

--- adev/src/app/core/services/version-manager.service.ts
+++ adev/src/app/core/services/version-manager.service.ts
@@ -10,22 +10,70 @@ import {Injectable, VERSION, computed, signal} from '@angular/core';
 
 // TODO(josephperrott): extract this out of the file into a managed location.
 const VERSIONS_CONFIG = {
-  currentVersion: "stable",
-  historicalVersionsLinkPattern: "https://v{{version}}.angular.dev",
+  currentVersion: 'stable',
+  historicalVersionsLinkPattern: 'https://v{{version}}.angular.dev',
   mainVersions: [
     {
-      version: "stable",
-      url: "https://angular.dev"
+      version: 'stable',
+      url: 'https://angular.dev',
     },
     {
-      version: "rc",
-      url: "https://rc.angular.dev"
+      version: 'v16',
+      url: 'https://v16.angular.io/docs',
     },
     {
-      version: "next",
-      url: "https://next.angular.dev"
-    }
-  ]
+      version: 'v15',
+      url: 'https://v15.angular.io/docs',
+    },
+    {
+      version: 'v14',
+      url: 'https://v14.angular.io/docs',
+    },
+    {
+      version: 'v13',
+      url: 'https://v13.angular.io/docs',
+    },
+    {
+      version: 'v12',
+      url: 'https://v12.angular.io/docs',
+    },
+    {
+      version: 'v11',
+      url: 'https://v11.angular.io/docs',
+    },
+    {
+      version: 'v10',
+      url: 'https://v10.angular.io/docs',
+    },
+    {
+      version: 'v9',
+      url: 'https://v9.angular.io/docs',
+    },
+    {
+      version: 'v8',
+      url: 'https://v8.angular.io/docs',
+    },
+    {
+      version: 'v7',
+      url: 'https://v7.angular.io/docs',
+    },
+    {
+      version: 'v6',
+      url: 'https://v6.angular.io/docs',
+    },
+    {
+      version: 'v5',
+      url: 'https://v5.angular.io/docs',
+    },
+    {
+      version: 'v4',
+      url: 'https://v4.angular.io/docs',
+    },
+    {
+      version: 'v2',
+      url: 'https://v2.angular.io/docs',
+    },
+  ],
 };
 
 export interface Version {
