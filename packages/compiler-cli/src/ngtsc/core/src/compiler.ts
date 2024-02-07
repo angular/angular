eeaec924ef25766fd8c29c7a06ae421da9e75f23
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-02-07 12:40:58 +0000

    refactor(compiler-cli): ensure `FatalDiagnosticError` extends `Error` (#54309)
    This helps with the Angular CLI currently swallowing fatal diagnostic
    errors in ways that are extremely difficult to debug due to workers
    executing Angular compiler logic.
    
    The worker logic, via piscina, is currently not forwarding such Angular
    errors because those don't extend `Error.`
    
    https://github.com/piscinajs/piscina/blob/a7042ea27d129f3cad75c422f5aa92f0663854ee/src/worker.ts#L175
    
    Even with access to these errors by manually forwarding errors, via
    patching of the Angular CLI, there is no stack trace due to us not using
    `Error` as base class for fatal diagnostic errors. This commit improves
    this for future debugging and also better reporting of such errors to
    our users- if we would accidentally leak one.
    
    PR Close #54309

--- packages/compiler-cli/src/ngtsc/core/src/compiler.ts
+++ packages/compiler-cli/src/ngtsc/core/src/compiler.ts
@@ -11,7 +11,7 @@ import ts from 'typescript';
 import {ComponentDecoratorHandler, DirectiveDecoratorHandler, InjectableDecoratorHandler, NgModuleDecoratorHandler, NoopReferencesRegistry, PipeDecoratorHandler, ReferencesRegistry} from '../../annotations';
 import {InjectableClassRegistry} from '../../annotations/common';
 import {CycleAnalyzer, CycleHandlingStrategy, ImportGraph} from '../../cycles';
-import {COMPILER_ERRORS_WITH_GUIDES, ERROR_DETAILS_PAGE_BASE_URL, ErrorCode, FatalDiagnosticError, ngErrorCode} from '../../diagnostics';
+import {COMPILER_ERRORS_WITH_GUIDES, ERROR_DETAILS_PAGE_BASE_URL, ErrorCode, isFatalDiagnosticError, ngErrorCode} from '../../diagnostics';
 import {DocEntry, DocsExtractor} from '../../docs';
 import {checkForPrivateExports, ReferenceGraph} from '../../entry_point';
 import {absoluteFromSourceFile, AbsoluteFsPath, LogicalFileSystem, resolve} from '../../file_system';
@@ -436,7 +436,7 @@ export class NgCompiler {
         diagnostics.push(...this.getExtendedTemplateDiagnostics());
       }
     } catch (err: unknown) {
-      if (!(err instanceof FatalDiagnosticError)) {
+      if (!isFatalDiagnosticError(err)) {
         throw err;
       }
       diagnostics.push(err.toDiagnostic());
@@ -466,7 +466,7 @@ export class NgCompiler {
         diagnostics.push(...this.getExtendedTemplateDiagnostics(file));
       }
     } catch (err: unknown) {
-      if (!(err instanceof FatalDiagnosticError)) {
+      if (!isFatalDiagnosticError(err)) {
         throw err;
       }
       diagnostics.push(err.toDiagnostic());
@@ -496,7 +496,7 @@ export class NgCompiler {
         diagnostics.push(...extendedTemplateChecker.getDiagnosticsForComponent(component));
       }
     } catch (err: unknown) {
-      if (!(err instanceof FatalDiagnosticError)) {
+      if (!isFatalDiagnosticError(err)) {
         throw err;
       }
       diagnostics.push(err.toDiagnostic());
