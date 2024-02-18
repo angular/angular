da14b1dc9a2e6b3c74592d2086ffbe58789bc743
JoostK <joost.koehoorn@gmail.com>
2024-02-18 20:48:17 +0100

    fix(compiler-cli): use correct symbol name for default imported symbols in defer blocks
    This commit addresses a problem with PR #53695 that introduced support for default imports,
    where the actual dynamic import used in the defer loading function continued to use the
    symbol name, instead of `.default` for the dynamic import. This issue went unnoticed in the
    testcase because a proper instance was being generated for the `ɵsetClassMetadataAsync` function,
    but not the generated dependency loader function.
    
    Fixes #54491

---
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_deferred/defer_default_deps_template.js
@@ -0,0 +1,18 @@
+const $TestCmp_Defer_1_DepsFn$ = () => [import("./defer_default_deps_ext").then(m => m.default), LocalDep];
+
+function TestCmp_Defer_0_Template(rf, ctx) {
+	if (rf & 1) {
+		i0.ɵɵelement(0, "cmp-a")(1, "local-dep");
+	}
+}
+
+export class LocalDep {
+}
+
+…
+
+function TestCmp_Template(rf, ctx) { if (rf & 1) {
+	i0.ɵɵtemplate(0, TestCmp_Defer_0_Template, 2, 0);
+	i0.ɵɵdefer(1, 0, $TestCmp_Defer_1_DepsFn$);
+	i0.ɵɵdeferOnIdle();
+} }
