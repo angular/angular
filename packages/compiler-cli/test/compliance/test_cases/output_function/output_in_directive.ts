e7ea6c05644bba3950648866427fa22b12d53fea
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-02-28 12:44:48 +0000

    test: add compliance generated output tests for `output()` (#54650)
    This commit adds compliance tests for the new output APIs.
    
    PR Close #54650

--- packages/compiler-cli/test/compliance/test_cases/output_function/output_in_directive.ts
+++ packages/compiler-cli/test/compliance/test_cases/output_function/output_in_directive.ts
@@ -1,4 +1,5 @@
-import {Directive, output} from '@angular/core';
+import {Directive, EventEmitter, output} from '@angular/core';
+import {outputFromObservable} from '@angular/core/rxjs-interop';
 
 @Directive({
   standalone: true,
@@ -7,4 +8,6 @@ export class TestDir {
   a = output();
   b = output<string>({});
   c = output<void>({alias: 'cPublic'});
+  d = outputFromObservable(new EventEmitter<string>());
+  e = outputFromObservable(new EventEmitter<number>());
 }
