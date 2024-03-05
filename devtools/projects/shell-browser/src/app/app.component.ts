821012d0f7a862765e358286a53f1025953b5aad
AleksanderBodurri <alexbodurri1117@gmail.com>
2024-03-04 22:54:58 -0500

    Revert "Revert "refactor(devtools): implement iframe support for Angular DevTools' browser code (#53934)" (#54629)" (#54805)
    This reverts commit dd9f9d7d4490907b148e27abfd7622d31f55c15f.
    
    PR Close #54805

--- devtools/projects/shell-browser/src/app/app.component.ts
+++ devtools/projects/shell-browser/src/app/app.component.ts
@@ -6,39 +6,21 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {ChangeDetectorRef, Component, NgZone, OnInit} from '@angular/core';
-import {Events, MessageBus, PriorityAwareMessageBus} from 'protocol';
-
-import {injectScripts} from './inject';
-import {ZoneAwareChromeMessageBus} from './zone-aware-chrome-message-bus';
-import {DevToolsComponent} from 'ng-devtools';
+import {ChangeDetectorRef, Component, inject, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [
-    {
-      provide: MessageBus,
-      useFactory(ngZone: NgZone): MessageBus<Events> {
-        const port = chrome.runtime.connect({
-          name: '' + chrome.devtools.inspectedWindow.tabId,
-        });
-        return new PriorityAwareMessageBus(new ZoneAwareChromeMessageBus(port, ngZone));
-      },
-      deps: [NgZone],
-    },
-  ],
 })
 export class AppComponent implements OnInit {
-  constructor(private _cd: ChangeDetectorRef) {}
+  private _cd = inject(ChangeDetectorRef);
 
   ngOnInit(): void {
     chrome.devtools.network.onNavigated.addListener(() => {
       window.location.reload();
     });
 
-    injectScripts(['app/backend_bundle.js']);
     this._cd.detectChanges();
   }
 }
