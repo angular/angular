74b7a355aee40568d0da7858372d29ed5bc1abda
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-03-11 14:31:30 +0000

    refactor(compiler-cli): introduce new implementation of `ImportManager`
    This commit introduces a new implementation of `ImportManager` that has
    numerous benefits:
    
    - It allows efficient re-use of original source file imports.
      * either fully re-using original imports if matching
      * updating existing import declarations to include new symbols.
    - It allows efficient re-use of previous generated imports.
    - The manager can be used for schematics and migrations.
    
    The implementation is a rework of the import manager that we originally
    built for schematics in Angular Material, but this commit improved it
    to be more flexible, more readable, and "correct".
    
    In follow-ups we can use this for schematics/migrations.

--- packages/compiler-cli/src/ngtsc/translator/src/api/import_generator.ts
+++ packages/compiler-cli/src/ngtsc/translator/src/api/import_generator.ts
@@ -18,6 +18,32 @@ export interface NamedImport<TExpression> {
 }
 
 /**
+ * A request to import a given symbol from the given module.
+ */
+export interface ImportRequest<TFile> {
+  /**
+   * Name of the export to be imported.
+   * May be `null` if a namespace import is requested.
+   */
+  exportSymbolName: string|null;
+
+  /**
+   * Module specifier to be imported.
+   * May be a module name, or a file-relative path.
+   */
+  exportModuleSpecifier: string;
+
+  /**
+   * File for which the import is requested for. This may
+   * be used by import generators to re-use existing imports.
+   *
+   * Import managers may also allow this to be nullable if
+   * imports are never re-used. E.g. in the linker generator.
+   */
+  requestedFile: TFile;
+}
+
+/**
  * Generate import information based on the context of the code being generated.
  *
  * Implementations of these methods return a specific identifier that corresponds to the imported
