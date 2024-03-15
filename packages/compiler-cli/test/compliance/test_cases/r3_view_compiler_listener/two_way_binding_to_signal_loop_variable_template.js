879bd80b571368bdc6e65c5415d98ad0f546267a
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-03-15 13:36:59 +0100

    fix(compiler): capture data bindings for content projection purposes in blocks (#54876)
    Fixes a regression in the template pipeline where data bindings weren't being captured for content projection purposes.
    
    Fixes #54872.
    
    PR Close #54876

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_listener/two_way_binding_to_signal_loop_variable_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_listener/two_way_binding_to_signal_loop_variable_template.js
@@ -1,7 +1,7 @@
 function TestCmp_For_1_Template(rf, ctx) {
   if (rf & 1) {
     const $_r1$ = $r3$.ɵɵgetCurrentView();
-    $r3$.ɵɵelementStart(0, "input", 0);
+    $r3$.ɵɵelementStart(0, "input", 1);
     $r3$.ɵɵtwoWayListener("ngModelChange", function TestCmp_For_1_Template_input_ngModelChange_0_listener($event) {
       const $name_r2$ = $r3$.ɵɵrestoreView($_r1$).$implicit;
       $r3$.ɵɵtwoWayBindingSet($name_r2$, $event);
@@ -19,7 +19,7 @@ function TestCmp_For_1_Template(rf, ctx) {
 
 function TestCmp_Template(rf, ctx) {
   if (rf & 1) {
-    $r3$.ɵɵrepeaterCreate(0, TestCmp_For_1_Template, 1, 1, "input", null, $r3$.ɵɵrepeaterTrackByIndex);
+    $r3$.ɵɵrepeaterCreate(0, TestCmp_For_1_Template, 1, 1, "input", 0, $r3$.ɵɵrepeaterTrackByIndex);
   }
   if (rf & 2) {
     $r3$.ɵɵrepeater(ctx.names);
