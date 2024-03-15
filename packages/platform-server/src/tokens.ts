57b6c359ae9288cab911d9a4c6d1a68337c375da
Alan Agius <alanagius@google.com>
2024-03-15 10:12:51 +0000

    refactor(platform-server): remove deprecated `useAbsoluteUrl` and `baseUrl` from `PlatformConfig`
    BREAKING CHANGE: deprecated `useAbsoluteUrl` and `baseUrl` been removed from `PlatformConfig`. Provide and absolute `url` instead.

--- packages/platform-server/src/tokens.ts
+++ packages/platform-server/src/tokens.ts
@@ -26,28 +26,6 @@ export interface PlatformConfig {
    * @default none
    */
   url?: string;
-  /**
-   * Note: this option has no effect and can be removed from the config.
-   *
-   * Whether to append the absolute URL to any relative HTTP requests. If set to
-   * true, this logic executes prior to any HTTP interceptors that may run later
-   * on in the request. `baseUrl` must be supplied if this flag is enabled.
-   *
-   * @deprecated This option is a noop.
-   * @default false
-   */
-  useAbsoluteUrl?: boolean;
-  /**
-   * Note: this option has no effect and can be removed from the config.
-   *
-   * The base URL for resolving absolute URL for HTTP requests. It must be set
-   * if `useAbsoluteUrl` is true, and must consist of protocol, hostname,
-   * and optional port. This option has no effect if `useAbsoluteUrl` is not
-   * enabled.
-   *
-   * @deprecated This option is a noop.
-   */
-  baseUrl?: string;
 }
 
 /**
