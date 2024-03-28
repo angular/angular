bd3bb16fcc70e9373ea619eae9bfefd6e63074f6
Andrew Scott <atscott@google.com>
2024-03-28 09:36:25 -0700

    refactor(core): Update method of grabbing native setTimeout and rAF
    As mentioned in https://github.com/angular/angular/pull/54600#discussion_r1528372804
    a more effective way of getting the unpatched version of a zone-patched
    API is to grab it from global[Zone.__symbol__('apiname')].

--- packages/core/src/util/callback_scheduler.ts
+++ packages/core/src/util/callback_scheduler.ts
@@ -35,7 +35,7 @@ import {global} from './global';
  * @returns a function to cancel the scheduled callback
  */
 export function scheduleCallback(callback: Function): () => void {
-  // Note: the `getNativeRequestAnimationFrame` is used in the `NgZone` class, but we cannot use the
+  // Note: the `scheduleCallback` is used in the `NgZone` class, but we cannot use the
   // `inject` function. The `NgZone` instance may be created manually, and thus the injection
   // context will be unavailable. This might be enough to check whether `requestAnimationFrame` is
   // available because otherwise, we'll fall back to `setTimeout`.
@@ -43,21 +43,12 @@ export function scheduleCallback(callback: Function): () => void {
   let nativeRequestAnimationFrame =
       hasRequestAnimationFrame ? global['requestAnimationFrame'] : null;
   let nativeSetTimeout = global['setTimeout'];
-
   if (typeof Zone !== 'undefined') {
-    // Note: zone.js sets original implementations on patched APIs behind the
-    // `__zone_symbol__OriginalDelegate` key (see `attachOriginToPatched`). Given the following
-    // example: `window.requestAnimationFrame.__zone_symbol__OriginalDelegate`; this would return an
-    // unpatched implementation of the `requestAnimationFrame`, which isn't intercepted by the
-    // Angular zone. We use the unpatched implementation to avoid another change detection when
-    // coalescing tasks.
-    const ORIGINAL_DELEGATE_SYMBOL = (Zone as any).__symbol__('OriginalDelegate');
-    if (nativeRequestAnimationFrame) {
+    if (hasRequestAnimationFrame) {
       nativeRequestAnimationFrame =
-          (nativeRequestAnimationFrame as any)[ORIGINAL_DELEGATE_SYMBOL] ??
-          nativeRequestAnimationFrame;
+          global[Zone.__symbol__('requestAnimationFrame')] ?? nativeRequestAnimationFrame;
     }
-    nativeSetTimeout = (nativeSetTimeout as any)[ORIGINAL_DELEGATE_SYMBOL] ?? nativeSetTimeout;
+    nativeSetTimeout = global[Zone.__symbol__('setTimeout')] ?? nativeSetTimeout;
   }
 
   let executeCallback = true;
