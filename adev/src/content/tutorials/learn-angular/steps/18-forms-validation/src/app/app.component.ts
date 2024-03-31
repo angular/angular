bc394de75aac553a11953f3d46eb972b5dc0a1f9
Martin Machava <Vergil333@gmail.com>
2024-03-31 18:54:09 +0200

    docs: fix input name (#55141)
    This commit fixes the name of the input used for the email.
    
    PR Close #55141

--- adev/src/content/tutorials/learn-angular/steps/18-forms-validation/src/app/app.component.ts
+++ adev/src/content/tutorials/learn-angular/steps/18-forms-validation/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {ReactiveFormsModule} from '@angular/forms';
   template: `
     <form [formGroup]="profileForm">
       <input type="text" formControlName="name" name="name" />
-      <input type="email" formControlName="email" name="password" />
+      <input type="email" formControlName="email" name="email" />
       <button type="submit">Submit</button>
     </form>
   `,
