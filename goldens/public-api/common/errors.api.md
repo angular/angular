7c510bedf93e734f0d9277f354279207a5d15c24
Matthieu Riegler <kyro38@gmail.com>
2024-04-01 13:08:51 +0200

    refactor(common): allow string urls in NgOptimizedImage input
    With this commit it is now possible to pass an url as placeholder for the image directive.

--- goldens/public-api/common/errors.api.md
+++ goldens/public-api/common/errors.api.md
@@ -31,6 +31,8 @@ export const enum RuntimeErrorCode {
     // (undocumented)
     PARENT_NG_SWITCH_NOT_FOUND = 2000,
     // (undocumented)
+    PLACEHOLDER_DIMENSION_LIMIT_EXCEEDED = 2967,
+    // (undocumented)
     PRIORITY_IMG_MISSING_PRECONNECT_TAG = 2956,
     // (undocumented)
     REQUIRED_INPUT_MISSING = 2954,
--- packages/common/src/directives/ng_optimized_image/ng_optimized_image.ts
+++ packages/common/src/directives/ng_optimized_image/ng_optimized_image.ts
@@ -118,6 +118,12 @@ const FIXED_SRCSET_HEIGHT_LIMIT = 1080;
 export const PLACEHOLDER_BLUR_AMOUNT = 15;
 
 /**
+ * Placeholder dimension (height or width) limit in pixels. Angular produces a warning
+ * when this limit is crossed.
+ */
+const PLACEHOLDER_DIMENSION_LIMIT = 1000;
+
+/**
  * Used to warn or error when the user provides an overly large dataURL for the placeholder
  * attribute.
  * Character count of Base64 images is 1 character per byte, and base64 encoding is approximately
@@ -364,7 +370,7 @@ export class NgOptimizedImage implements OnInit, OnChanges, OnDestroy {
   /**
    * A URL or data URL for an image to be used as a placeholder while this image loads.
    */
-  @Input({transform: booleanOrDataUrlAttribute}) placeholder?: string | boolean;
+  @Input({transform: booleanOrUrlAttribute}) placeholder?: string | boolean;
 
   /**
    * Configuration object for placeholder settings. Options:
@@ -518,6 +524,10 @@ export class NgOptimizedImage implements OnInit, OnChanges, OnDestroy {
         });
       }
     }
+
+    if (ngDevMode && changes['placeholder']?.currentValue && !this.isServer) {
+      assertPlaceholderDimensions(this, this.imgElement);
+    }
   }
 
   private callImageLoader(
@@ -651,7 +661,7 @@ export class NgOptimizedImage implements OnInit, OnChanges, OnDestroy {
         width: placeholderResolution,
         isPlaceholder: true,
       })})`;
-    } else if (typeof placeholderInput === 'string' && placeholderInput.startsWith('data:')) {
+    } else if (typeof placeholderInput === 'string') {
       return `url(${placeholderInput})`;
     }
     return null;
@@ -1286,6 +1296,28 @@ function assetPriorityCountBelowThreshold() {
   IMGS_WITH_PRIORITY_ATTR_COUNT++;
 }
 
+/**
+ * Warns if placeholder's dimension are over a threshold.
+ *
+ * This assert function is meant to only run on the browser.
+ */
+function assertPlaceholderDimensions(dir: NgOptimizedImage, imgElement: HTMLImageElement) {
+  const computedStyle = window.getComputedStyle(imgElement);
+  let renderedWidth = parseFloat(computedStyle.getPropertyValue('width'));
+  let renderedHeight = parseFloat(computedStyle.getPropertyValue('height'));
+
+  if (renderedWidth > PLACEHOLDER_DIMENSION_LIMIT || renderedHeight > PLACEHOLDER_DIMENSION_LIMIT) {
+    console.warn(
+      formatRuntimeError(
+        RuntimeErrorCode.PLACEHOLDER_DIMENSION_LIMIT_EXCEEDED,
+        `${imgDirectiveDetails(dir.ngSrc)} it uses a placeholder image, but at least one of ` +
+          `of the dimensions attribute (height or width) exceeds the limit of ${PLACEHOLDER_DIMENSION_LIMIT}px. ` +
+          `To fix this, use a smaller image as a placeholder.`,
+      ),
+    );
+  }
+}
+
 function round(input: number): number | string {
   return Number.isInteger(input) ? input : input.toFixed(2);
 }
@@ -1301,8 +1333,8 @@ function unwrapSafeUrl(value: string | SafeValue): string {
 
 // Transform function to handle inputs which may be booleans, strings, or string representations
 // of boolean values. Used for the placeholder attribute.
-export function booleanOrDataUrlAttribute(value: boolean | string): boolean | string {
-  if (typeof value === 'string' && value.startsWith(`data:`)) {
+export function booleanOrUrlAttribute(value: boolean | string): boolean | string {
+  if (typeof value === 'string' && value !== 'true' && value !== 'false' && value !== '') {
     return value;
   }
   return booleanAttribute(value);
