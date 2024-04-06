5c0004bb626b953fc9515eddcb1b461b2fa6b356
lilbeqiri <i.beqiri21@gmail.com>
2024-04-06 02:12:32 +0200

    refactor: do cleanups to theme, version and content loader services at adev (#55234)
    Keep theme constants in sync betwee index.html file and theme manager. Move versions config static data to their own file, and replace the deprecated toPromise() api with the new firstValueFrom() api
    
    PR Close #55234

--- adev/src/app/core/services/version-manager.service.ts
+++ adev/src/app/core/services/version-manager.service.ts
@@ -7,74 +7,7 @@
  */
 
 import {Injectable, VERSION, computed, signal} from '@angular/core';
-
-// TODO(josephperrott): extract this out of the file into a managed location.
-const VERSIONS_CONFIG = {
-  currentVersion: 'stable',
-  historicalVersionsLinkPattern: 'https://v{{version}}.angular.dev',
-  mainVersions: [
-    {
-      version: 'stable',
-      url: 'https://angular.dev',
-    },
-    {
-      version: 'v16',
-      url: 'https://v16.angular.io/docs',
-    },
-    {
-      version: 'v15',
-      url: 'https://v15.angular.io/docs',
-    },
-    {
-      version: 'v14',
-      url: 'https://v14.angular.io/docs',
-    },
-    {
-      version: 'v13',
-      url: 'https://v13.angular.io/docs',
-    },
-    {
-      version: 'v12',
-      url: 'https://v12.angular.io/docs',
-    },
-    {
-      version: 'v11',
-      url: 'https://v11.angular.io/docs',
-    },
-    {
-      version: 'v10',
-      url: 'https://v10.angular.io/docs',
-    },
-    {
-      version: 'v9',
-      url: 'https://v9.angular.io/docs',
-    },
-    {
-      version: 'v8',
-      url: 'https://v8.angular.io/docs',
-    },
-    {
-      version: 'v7',
-      url: 'https://v7.angular.io/docs',
-    },
-    {
-      version: 'v6',
-      url: 'https://v6.angular.io/docs',
-    },
-    {
-      version: 'v5',
-      url: 'https://v5.angular.io/docs',
-    },
-    {
-      version: 'v4',
-      url: 'https://v4.angular.io/docs',
-    },
-    {
-      version: 'v2',
-      url: 'https://v2.angular.io/docs',
-    },
-  ],
-};
+import {VERSIONS_CONFIG} from '../constants/versions';
 
 export interface Version {
   displayName: string;
