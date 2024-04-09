d556040b4c9869221000e32cd25a17f9523c62c7
Matthieu Riegler <kyro38@gmail.com>
2024-04-09 23:25:58 +0200

    docs: exposes `$localize` for adev
    fixes #54388

--- packages/localize/init/index.ts
+++ packages/localize/init/index.ts
@@ -5,9 +5,12 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import {ɵ$localize as $localize, ɵLocalizeFn as LocalizeFn, ɵTranslateFn as TranslateFn} from '@angular/localize';
+import {ɵ$localize, ɵLocalizeFn as LocalizeFn, ɵTranslateFn as TranslateFn} from '@angular/localize';
 
-export {$localize, LocalizeFn, TranslateFn};
+export {LocalizeFn, TranslateFn};
 
 // Attach $localize to the global context, as a side-effect of this module.
-(globalThis as any).$localize = $localize;
+(globalThis as any).$localize = ɵ$localize;
+
+// Doing an explicit re-export for docs purposes.
+export const $localize = ɵ$localize;
