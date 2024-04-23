9603773d36c0121c0d6f3c1412525098d3a2aa1c
iteriani <tomnguyen@google.com>
2024-04-23 15:38:57 -0700

    refactor(core): Switch keycode from an enum to a const.
    This is a prerequisite for removing keycode from the final bundle.

--- packages/core/primitives/event-dispatch/src/key_code.ts
+++ packages/core/primitives/event-dispatch/src/key_code.ts
@@ -7,16 +7,16 @@
  */
 
 /** Special keycodes used by jsaction for the generic click action. */
-export enum KeyCode {
+export const KeyCode = {
   /**
    * If on a Macintosh with an extended keyboard, the Enter key located in the
    * numeric pad has a different ASCII code.
    */
-  MAC_ENTER = 3,
+  MAC_ENTER: 3,
 
   /** The Enter key. */
-  ENTER = 13,
+  ENTER: 13,
 
   /** The Space key. */
-  SPACE = 32,
-}
+  SPACE: 32,
+};
