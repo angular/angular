a916047a838fbf9bb2bd290036138a19859f5575
Alan Agius <alanagius@google.com>
2024-05-07 09:40:00 +0000

    test: add Integration tests for hydration and event reply (#55708)
    This commit introduces integration tests for hydration and event reply functionalities. Additionally, it implements a payload size check for the `event-dispatch-contract.min.js`.
    
    PR Close #55708

---
+++ integration/platform-server-hydration/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'hydration';
+  onClick = () => {
+    document.querySelector('#divElement')!.textContent = 'click triggered.';
+  };
+}
