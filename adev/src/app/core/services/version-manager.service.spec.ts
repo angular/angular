6217e4290bc020d5f82787d1d31623bf44410d5e
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2024-05-21 22:49:55 +0200

    fix(docs-infra): calculate list of Angular Docs versions based on VERSION
    fix(docs-infra): calculate the list of Angular Docs versions based on VERSION, append --config=release to docs build and serve the script. Support displaying the right color of navigation for the deprecated state.
    fix(docs-infra): add scrollbar to version picker
    fix(docs-infra): set initial adev docs version to 18
    fix(docs-infra): remove --config=release from scripts for local development, write unit tests
    fix(docs-infra): update CI tests step
    fix(docs-infra): remove rc from the list
    fix(docs-infra): update unit test to check count of versions

--- adev/src/app/core/services/version-manager.service.spec.ts
+++ adev/src/app/core/services/version-manager.service.spec.ts
@@ -8,17 +8,51 @@
 
 import {TestBed} from '@angular/core/testing';
 
-import {VersionManager} from './version-manager.service';
+import {INITIAL_ADEV_DOCS_VERSION, VersionManager} from './version-manager.service';
+import {WINDOW} from '@angular/docs';
+import {VERSION} from '@angular/core';
 
 describe('VersionManager', () => {
+  const fakeWindow = {location: {hostname: 'angular.dev'}};
+
   let service: VersionManager;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: WINDOW,
+          useValue: fakeWindow,
+        },
+      ],
+    });
     service = TestBed.inject(VersionManager);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should receive major version', () => {
+    expect(VERSION.major).not.toBe('0');
+  });
+
+  it('should contain correct number of Angular Docs versions', () => {
+    // Note: From v2 to v17 (inclusive), there were no v3
+    const expectedAioDocsVersionsCount = 15;
+
+    // Last stable version and next
+    const expectedRecentDocsVersionCount = 2;
+
+    const expectedPreviousAdevVersionsCount = Number(VERSION.major) - INITIAL_ADEV_DOCS_VERSION;
+
+    expect(service['getAioVersions']().length).toBe(expectedAioDocsVersionsCount);
+    expect(service['getRecentVersions']().length).toBe(expectedRecentDocsVersionCount);
+    expect(service['getAdevVersions']().length).toBe(expectedPreviousAdevVersionsCount);
+    expect(service.versions().length).toBe(
+      expectedAioDocsVersionsCount +
+        expectedRecentDocsVersionCount +
+        expectedPreviousAdevVersionsCount,
+    );
   });
 });
