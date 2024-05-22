9f8855b5303df131a1ada9636cc1c4d1dfc3e83e
Joey Perrott <josephperrott@gmail.com>
2024-05-22 15:35:27 +0000

    fix(docs-infra): fix failing tests related to redirects
    Correct failing tests created by redirect changes

--- aio/tests/deployment/unit/testFirebaseRedirection.spec.ts
+++ aio/tests/deployment/unit/testFirebaseRedirection.spec.ts
@@ -7,6 +7,12 @@ describe('firebase.json redirect config', () => {
     const redirector = getRedirector();
 
     loadLocalSitemapUrls().forEach(url => {
+      // Skip check for `/` because it is not handled by the service worker because the redirect
+      // sending all trafic away from the site anyway on firebase.  We are okay with it being handled
+      // locally by the service worker during fallback.
+      if (url === '/') {
+        return;
+      }
       it(`should not redirect sitemap URL '${url}'`, () => {
         expect(redirector.redirect(url)).toEqual(url);
       });
