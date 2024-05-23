f939757ac2fc883496ed7b891c6fa2404f9fb8b3
Aristeidis Bampakos <bampakoa@gmail.com>
2024-05-23 21:32:24 +0300

    docs: use customized YouTube channel link

--- adev/src/app/core/constants/links.ts
+++ adev/src/app/core/constants/links.ts
@@ -9,4 +9,4 @@
 export const GITHUB = 'https://github.com/angular/angular';
 export const X = 'https://x.com/angular';
 export const MEDIUM = 'https://blog.angular.dev';
-export const YOUTUBE = 'https://www.youtube.com/channel/UCbn1OgGei-DV7aSRo_HaAiw';
+export const YOUTUBE = 'https://www.youtube.com/angular';
