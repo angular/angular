02e8c043cf2378634394e47877c7435cfaea186c
Matthieu Riegler <kyro38@gmail.com>
2024-05-26 16:57:54 +0200

    docs: archive banner link to adev
    fixes #56082

--- aio/src/app/layout/mode-banner/mode-banner.component.ts
+++ aio/src/app/layout/mode-banner/mode-banner.component.ts
@@ -1,6 +1,6 @@
-import { Location } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { VersionInfo } from 'app/navigation/navigation.service';
+import {Location} from '@angular/common';
+import {Component, Input} from '@angular/core';
+import {VersionInfo} from 'app/navigation/navigation.service';
 
 @Component({
   selector: 'aio-mode-banner',
@@ -8,7 +8,7 @@ import { VersionInfo } from 'app/navigation/navigation.service';
     <div *ngIf="mode === 'archive'" class="mode-banner alert archive-warning">
       <p>
         This is the <strong>archived documentation for Angular v{{ version.major }}.</strong> Please visit
-        <a href="https://angular.io{{currentPath}}?redirected_from={{version.major}}">angular.io</a>
+        <a href="https://angular.dev{{currentPath}}">angular.dev</a>
          to see this page for the current version of Angular.
       </p>
     </div>
