bbe655ff146b24ae11c0a18bc2714a0bd1fa2a09
Matthieu Riegler <kyro38@gmail.com>
2024-06-07 00:33:35 +0200

    refactor(compiler): include public constructor paramters to class properties. (#56315)
    Public properties declared in the constructor are part of the public API and we should extract them.
    
    Fixes #56310
    
    PR Close #56315

--- packages/compiler-cli/src/ngtsc/docs/src/class_extractor.ts
+++ packages/compiler-cli/src/ngtsc/docs/src/class_extractor.ts
@@ -293,10 +293,22 @@ class ClassExtractor {
     );
   }
 
+  /** Check if the parameter is a constructor parameter with a public modifier */
+  private isPublicConstructorParameterProperty(node: ts.Node): boolean {
+    if (ts.isParameterPropertyDeclaration(node, node.parent) && node.modifiers) {
+      return node.modifiers.some((modifier) => modifier.kind === ts.SyntaxKind.PublicKeyword);
+    }
+    return false;
+  }
+
   /** Gets whether a member is a property. */
   private isProperty(member: ts.Node): member is PropertyLike {
     // Classes have declarations, interface have signatures
-    return ts.isPropertyDeclaration(member) || ts.isPropertySignature(member);
+    return (
+      ts.isPropertyDeclaration(member) ||
+      ts.isPropertySignature(member) ||
+      this.isPublicConstructorParameterProperty(member)
+    );
   }
 
   /** Gets whether a member is a method. */
