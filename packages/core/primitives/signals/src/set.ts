c213895b856b1546cf84f837565d936ae3d5faf6
Rahat Ahmed <rahatarmanahmed@gmail.com>
2024-06-10 14:23:20 -0500

    try my first idea again

--- packages/core/primitives/signals/src/set.ts
+++ packages/core/primitives/signals/src/set.ts
@@ -1,4 +1,4 @@
-const ITEM_ID = Symbol('ITEM_ID');
+const SET_ID = Symbol('SET_ID');
 
 /**
  * A simplified alternate implementation of a Set implemented with a single
@@ -14,42 +14,38 @@ const ITEM_ID = Symbol('ITEM_ID');
  */
 export interface LiteSet<T extends Object> extends Array<T> {
   /**
-   * Key where we store the index the item is at. Sort of makes a reverse map from item to index.
+   * A hidden identifier for this instance of the set. Used to key the
+   * information stored on the set's items.
    */
-  idxMap: {[id: number]: number; length: number};
-  // [index: `__idx_for_item_${number}`]: number | undefined;
+  [SET_ID]: number;
 }
 
 interface LiteSetItem {
   /**
-   * A hidden globally unique identifier for any item used with LiteSets. Used
-   * to key the index reverse map on the set.
-   * NOTE: LiteSet doesn't make any effort to clean this up on items!
+   * Key where we store the index the item is at on a given set.
+   * Example: {'__idx_for_set_3': 1} Stored in set with ID 3 in 1th position.
    */
-  [ITEM_ID]: number;
+  [index: `__idx_for_set_${number}`]: number | undefined;
 }
 
-let nextItemId = 0;
+let nextSetId = 0;
 
 /** Create an instance of a LiteSet. */
 export function createLiteSet<T extends Object>(): LiteSet<T> {
   const set = [] as unknown as LiteSet<T>;
-  set.idxMap = [];
+  set[SET_ID] = nextSetId++;
   return set;
 }
 
 function indexOf<T extends Object>(set: LiteSet<T>, item: T): number | undefined {
-  const liteSetItem = item as unknown as LiteSetItem;
-  return set.idxMap[liteSetItem[ITEM_ID]];
+  return (item as unknown as LiteSetItem)[`__idx_for_set_${set[SET_ID]}`];
 }
 
 /** Add an item to the LiteSet. No-op if the item is already in the set. */
 export function addToLiteSet<T extends Object>(set: LiteSet<T>, item: T): void {
   if (indexOf(set, item) === undefined) {
     const index = set.push(item) - 1;
-    const liteSetItem = item as unknown as LiteSetItem;
-    liteSetItem[ITEM_ID] ??= nextItemId++;
-    set.idxMap[liteSetItem[ITEM_ID]] = index;
+    (item as unknown as LiteSetItem)[`__idx_for_set_${set[SET_ID]}`] = index;
   }
 }
 
@@ -61,13 +57,13 @@ export function removeFromLiteSet<T extends Object>(set: LiteSet<T>, item: T): v
   const liteSetItem = item as unknown as LiteSetItem;
 
   // Cleanup the stored index on the item.
-  delete set.idxMap[liteSetItem[ITEM_ID]];
+  delete (set[index] as unknown as LiteSetItem)[`__idx_for_set_${set[SET_ID]}`];
 
   const lastIndex = set.length - 1;
   if (index !== lastIndex) {
     // Swap the last item into the deleted position
     set[index] = set[lastIndex];
-    set.idxMap[(set[index] as unknown as LiteSetItem)[ITEM_ID]] = index;
+    (set[index] as unknown as LiteSetItem)[`__idx_for_set_${set[SET_ID]}`] = index;
   }
 
   // Truncate the array
@@ -79,5 +75,6 @@ export function removeFromLiteSet<T extends Object>(set: LiteSet<T>, item: T): v
  */
 export function clearLiteSet<T extends Object>(set: LiteSet<T>): void {
   set.length = 0;
-  set.idxMap.length = 0;
+  // TODO: potential memory leak on the items themselves...
 }
+
