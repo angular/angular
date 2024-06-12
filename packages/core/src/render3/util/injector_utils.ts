9c9f5cf1f231faceb9c092a88d1a1bc4e9b53327
Andrew Kushnir <akushnir@google.com>
2024-06-11 17:49:45 -0700

    refactor(core): avoid exposing `OutletInjector` in injector resolution path
    Router's `OutletInjector` required a special handling in cases when `@defer` is used, see https://github.com/angular/angular/pull/55374 for additional info. As a result, the `ChainedInjector` that represents an `OutletInjector` instance is currently exposed via `getInjectorResolutionPath` function. This creates a problem, because other debug APIs used by DevTools can not interact with `ChainedInjector`s. This commit updates the logic around `getInjectorResolutionPath` utility to avoid exposing `OutletInjector`in the resolution path.
    
    Resolves #56331.

--- packages/core/src/render3/util/injector_utils.ts
+++ packages/core/src/render3/util/injector_utils.ts
@@ -6,7 +6,9 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {type Injector} from '../../di/injector';
 import {assertGreaterThan, assertNotEqual, assertNumber} from '../../util/assert';
+import {ChainedInjector} from '../chained_injector';
 import {
   NO_PARENT_INJECTOR,
   RelativeInjectorLocation,
@@ -59,4 +61,15 @@ export function getParentInjectorView(location: RelativeInjectorLocation, startV
     viewOffset--;
   }
   return parentView;
+}
+
+/**
+ * Detects whether an injector is an instance of a `ChainedInjector`,
+ * created based on the `OutletInjector`.
+ */
+export function isRouterOutletInjector(currentInjector: Injector): boolean {
+  return (
+    currentInjector instanceof ChainedInjector &&
+    typeof (currentInjector.injector as any).__ngOutletInjector === 'function'
+  );
 }
