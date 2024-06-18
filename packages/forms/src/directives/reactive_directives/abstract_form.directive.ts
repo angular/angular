74d6af2af6a4f3877005b87e73dbca678cf4bb41
Matthieu Riegler <kyro38@gmail.com>
2024-06-18 10:51:51 +0200

    feat(forms): Add FormArrayDirective
    The `FormArrayDirective` will allow to have a `FormArray` as a top-level form object.
    
    * `NgControlStatusGroup` directive will be applied to the `FormArrayDirective`
    * `NgForm` will still create a `FormGroup`
    
    Fixes angular#30264
    
    BREAKING CHANGE: This new directive will conflict with existing FormArray directives or formArray inputs on the same element.

--- packages/forms/src/directives/reactive_directives/abstract_form.directive.ts
+++ packages/forms/src/directives/reactive_directives/abstract_form.directive.ts
@@ -298,10 +298,12 @@ export abstract class AbstractFormDirective
    *
    * @param value The new value for the form.
    */
-  resetForm(value: any = undefined): void {
-    this.form.reset(value);
-    (this as {submitted: boolean}).submitted = false;
-    this.form._events.next(new FormResetEvent(this.form));
+  resetForm(value: any = undefined, options: {onlySelf?: boolean; emitEvent?: boolean} = {}): void {
+    this.form.reset(value, options);
+    this._submittedReactive.set(false);
+    if (options?.emitEvent !== false) {
+      this.form._events.next(new FormResetEvent(this.form));
+    }
   }
 
   /**
