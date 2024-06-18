39df5fa9d2ee8bc7555159c80bc3d383932a5cb6
arturovt <arthurandrosovich@gmail.com>
2024-06-18 17:37:58 +0300

    refactor(core): missing space in zoneless warning (#56491)
    There's currently a missing space in the zoneless warning, showing words together.
    
    PR Close #56491

--- packages/core/src/change_detection/scheduling/zoneless_scheduling_impl.ts
+++ packages/core/src/change_detection/scheduling/zoneless_scheduling_impl.ts
@@ -298,7 +298,7 @@ export function provideExperimentalZonelessChangeDetection(): EnvironmentProvide
   if ((typeof ngDevMode === 'undefined' || ngDevMode) && typeof Zone !== 'undefined' && Zone) {
     const message = formatRuntimeError(
       RuntimeErrorCode.UNEXPECTED_ZONEJS_PRESENT_IN_ZONELESS_MODE,
-      `The application is using zoneless change detection, but is still loading Zone.js.` +
+      `The application is using zoneless change detection, but is still loading Zone.js. ` +
         `Consider removing Zone.js to get the full benefits of zoneless. ` +
         `In applications using the Angular CLI, Zone.js is typically included in the "polyfills" section of the angular.json file.`,
     );
