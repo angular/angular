826bf77d30378c7acdfa6130e869bfe38cc15a3b
Andrew Kushnir <akushnir@google.com>
2024-06-18 18:05:03 -0700

    fix(core): do not activate event replay when no events are registered
    This commit adds extra checks to handle a situation when an application has no events configured, but the Event Replay feature was enabled. This situation can happen when some routes in an application are mostly static, when other routes are more interactive.
    
    Resolves #56423.

--- packages/platform-server/test/event_replay_spec.ts
+++ packages/platform-server/test/event_replay_spec.ts
@@ -7,7 +7,14 @@
  */
 
 import {DOCUMENT} from '@angular/common';
-import {Component, destroyPlatform, getPlatform, Type} from '@angular/core';
+import {
+  Component,
+  destroyPlatform,
+  ErrorHandler,
+  getPlatform,
+  PLATFORM_ID,
+  Type,
+} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
 import {
   withEventReplay,
@@ -19,7 +26,14 @@ import {provideServerRendering} from '../public_api';
 import {EVENT_DISPATCH_SCRIPT_ID, renderApplication} from '../src/utils';
 import {EventPhase} from '@angular/core/primitives/event-dispatch';
 
-import {getAppContents, hydrate, render as renderHtml, resetTViewsFor} from './dom_utils';
+import {
+  getAppContents,
+  hydrate,
+  renderAndHydrate,
+  render as renderHtml,
+  resetTViewsFor,
+} from './dom_utils';
+import {CONTRACT_PROPERTY} from '@angular/core/src/hydration/event_replay';
 
 /**
  * Represents the <script> tag added by the build process to inject
@@ -36,6 +50,24 @@ function hasEventDispatchScript(content: string) {
 /** Checks whether there are any `jsaction` attributes present in the generated HTML */
 function hasJSActionAttrs(content: string) {
   return content.includes('jsaction="');
+}
+
+/**
+ * Enables strict error handler that fails a test
+ * if there was an error reported to the ErrorHandler.
+ */
+function withStrictErrorHandler() {
+  class StrictErrorHandler extends ErrorHandler {
+    override handleError(error: any): void {
+      fail(error);
+    }
+  }
+  return [
+    {
+      provide: ErrorHandler,
+      useClass: StrictErrorHandler,
+    },
+  ];
 }
 
 describe('event replay', () => {
@@ -61,6 +93,7 @@ describe('event replay', () => {
 
   afterEach(() => {
     doc.body.outerHTML = '<body></body>';
+    globalThis[CONTRACT_PROPERTY] = {};
   });
 
   /**
@@ -402,6 +435,17 @@ describe('event replay', () => {
 
       expect(hasJSActionAttrs(ssrContents)).toBeFalse();
       expect(hasEventDispatchScript(ssrContents)).toBeFalse();
+
+      resetTViewsFor(SimpleComponent);
+      await renderAndHydrate(doc, ssrContents, SimpleComponent, {
+        envProviders: [
+          {provide: PLATFORM_ID, useValue: 'browser'},
+          // This ensures that there are no errors while bootstrapping an application
+          // that has no events, but enables Event Replay feature.
+          withStrictErrorHandler(),
+        ],
+        hydrationFeatures: [withEventReplay()],
+      });
     });
 
     it('should not replay mouse events', async () => {
