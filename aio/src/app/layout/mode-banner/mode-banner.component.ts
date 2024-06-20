f25e77b83e9a8cae40fc27577c3ad86132e0aa7f
Matthieu Riegler <kyro38@gmail.com>
2024-06-21 00:42:05 +0200

    docs(docs-infra): canonical link to ADEV (#56535)
    Also fixes the mode-banner link when changing pages
    
    PR Close #56535

--- aio/src/app/layout/mode-banner/mode-banner.component.ts
+++ aio/src/app/layout/mode-banner/mode-banner.component.ts
@@ -1,7 +1,8 @@
-import {Location} from '@angular/common';
 import {Component, Input} from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {VersionInfo} from 'app/navigation/navigation.service';
-import { rewriteLink } from 'app/shared/angular-dot-dev-redirects';
+import {rewriteLink} from 'app/shared/angular-dot-dev-redirects';
+import {LocationService} from 'app/shared/location.service';
 
 @Component({
   selector: 'aio-mode-banner',
@@ -21,7 +22,9 @@ export class ModeBannerComponent {
 
   currentPath: string;
 
-  constructor(private location: Location) {
-    this.currentPath = rewriteLink(this.location.path());
+  constructor(private locationService: LocationService) {
+    this.locationService.currentPath.pipe(takeUntilDestroyed()).subscribe((path) => {
+      this.currentPath = rewriteLink(path);
+    });
   }
 }
