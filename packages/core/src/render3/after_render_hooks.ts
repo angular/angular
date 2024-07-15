e504ad97d44159cf632ec987fce94f66bfddef37
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-07-15 08:44:22 +0200

    fix(core): not all callbacks running when registered at the same time (#56981)
    Fixes that only the first callback was firing when multiple are registered in the same call to `afterNextRender`, e.g. `afterNextRender({earlyRead: fn, read: fn});`
    
    Fixes #56979.
    
    PR Close #56981

--- packages/core/src/render3/after_render_hooks.ts
+++ packages/core/src/render3/after_render_hooks.ts
@@ -598,6 +598,7 @@ function afterRenderImpl(
     unregisterFn();
   };
   const unregisterFn = injector.get(DestroyRef).onDestroy(destroy);
+  let callbacksLeftToRun = 0;
 
   const registerCallback = (
     phase: AfterRenderPhase,
@@ -608,7 +609,10 @@ function afterRenderImpl(
     }
     const callback = once
       ? (...args: [unknown]) => {
-          destroy();
+          callbacksLeftToRun--;
+          if (callbacksLeftToRun < 1) {
+            destroy();
+          }
           phaseCallback(...args);
         }
       : phaseCallback;
@@ -619,6 +623,7 @@ function afterRenderImpl(
     );
     callbackHandler.register(instance);
     instances.push(instance);
+    callbacksLeftToRun++;
   };
 
   registerCallback(AfterRenderPhase.EarlyRead, spec.earlyRead);
