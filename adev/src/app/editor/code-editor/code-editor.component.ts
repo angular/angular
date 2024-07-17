8c47e67b084f90fc2a807038f987c9deec4f412a
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2024-07-17 13:37:11 +0200

    docs(docs-infra): add open in StackBlitz button to code editor

--- adev/src/app/editor/code-editor/code-editor.component.ts
+++ adev/src/app/editor/code-editor/code-editor.component.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {NgFor, NgIf} from '@angular/common';
+import {Location, NgFor, NgIf} from '@angular/common';
 import {
   AfterViewInit,
   ChangeDetectionStrategy,
@@ -20,6 +20,7 @@ import {
 } from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {MatTabGroup, MatTabsModule} from '@angular/material/tabs';
+import {Title} from '@angular/platform-browser';
 import {debounceTime, map} from 'rxjs';
 
 import {TerminalType} from '../terminal/terminal-handler.service';
@@ -28,6 +29,7 @@ import {EmbeddedTutorialManager} from '../embedded-tutorial-manager.service';
 import {CodeMirrorEditor} from './code-mirror-editor.service';
 import {DiagnosticWithLocation, DiagnosticsState} from './services/diagnostics-state.service';
 import {DownloadManager} from '../download-manager.service';
+import {StackBlitzOpener} from '../stackblitz-opener.service';
 import {ClickOutside, IconComponent} from '@angular/docs';
 
 export const REQUIRED_FILES = new Set([
@@ -35,6 +37,8 @@ export const REQUIRED_FILES = new Set([
   'src/index.html',
   'src/app/app.component.ts',
 ]);
+
+const ANGULAR_DEV = 'https://angular.dev';
 
 @Component({
   selector: 'docs-tutorial-code-editor',
@@ -73,6 +77,9 @@ export class CodeEditor implements AfterViewInit, OnDestroy {
   private readonly codeMirrorEditor = inject(CodeMirrorEditor);
   private readonly diagnosticsState = inject(DiagnosticsState);
   private readonly downloadManager = inject(DownloadManager);
+  private readonly stackblitzOpener = inject(StackBlitzOpener);
+  private readonly title = inject(Title);
+  private readonly location = inject(Location);
   private readonly embeddedTutorialManager = inject(EmbeddedTutorialManager);
 
   private readonly errors$ = this.diagnosticsState.diagnostics$.pipe(
@@ -108,6 +115,16 @@ export class CodeEditor implements AfterViewInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.codeMirrorEditor.disable();
+  }
+
+  async openCurrentCodeInStackBlitz(): Promise<void> {
+    const title = this.title.getTitle();
+
+    const path = this.location.path();
+    const editorUrl = `${ANGULAR_DEV}${path}`;
+    const description = `Angular.dev example generated from [${editorUrl}](${editorUrl})`;
+
+    await this.stackblitzOpener.openCurrentSolutionInStackBlitz({title, description});
   }
 
   async downloadCurrentCodeEditorState(): Promise<void> {
