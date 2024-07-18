6489397b9929a84892a2ac5b0187266b0105a925
Sheik Althaf <althafmech@outlook.com>
2024-07-18 23:51:31 +0530

    refactor(devtools): use signal apis in injection and router tree (#57047)
    Refactor the injection and router tree components to use signal apis, in future we can make the components onPush and zoneless
    
    PR Close #57047

--- devtools/projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/property-tab/component-metadata.component.ts
+++ devtools/projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/property-tab/component-metadata.component.ts
@@ -6,10 +6,9 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject, input} from '@angular/core';
 import {ComponentType} from 'protocol';
 
-import {DirectivePropertyResolver} from '../property-resolver/directive-property-resolver';
 import {ElementPropertyResolver} from '../property-resolver/element-property-resolver';
 import {MatButton} from '@angular/material/button';
 
@@ -22,29 +21,30 @@ import {MatButton} from '@angular/material/button';
   imports: [MatButton],
 })
 export class ComponentMetadataComponent {
-  @Input({required: true}) currentSelectedComponent!: ComponentType;
+  readonly currentSelectedComponent = input.required<ComponentType>();
 
-  constructor(private _nestedProps: ElementPropertyResolver) {}
+  private _nestedProps = inject(ElementPropertyResolver);
 
   viewEncapsulationModes = ['Emulated', 'Native', 'None', 'ShadowDom'];
 
-  get controller(): DirectivePropertyResolver | undefined {
-    if (!this.currentSelectedComponent) {
+  readonly controller = computed(() => {
+    const comp = this.currentSelectedComponent();
+    if (!comp) {
       return;
     }
-    return this._nestedProps.getDirectiveController(this.currentSelectedComponent.name);
-  }
+    return this._nestedProps.getDirectiveController(comp.name);
+  });
 
-  get viewEncapsulation(): string | undefined {
-    const encapsulationIndex = this?.controller?.directiveViewEncapsulation;
+  readonly viewEncapsulation = computed(() => {
+    const encapsulationIndex = this.controller()?.directiveViewEncapsulation;
     if (encapsulationIndex !== undefined) {
       return this.viewEncapsulationModes[encapsulationIndex];
     }
     return undefined;
-  }
+  });
 
-  get changeDetectionStrategy(): string | undefined {
-    const onPush = this?.controller?.directiveHasOnPushStrategy;
+  readonly changeDetectionStrategy = computed(() => {
+    const onPush = this.controller()?.directiveHasOnPushStrategy;
     return onPush ? 'OnPush' : onPush !== undefined ? 'Default' : undefined;
-  }
+  });
 }
