6c2fbda6942adbc7b21f3dfc1db0a42638223a1a
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-07-23 06:15:31 +0200

    fix(compiler-cli): extended diagnostic visitor not visiting template attributes (#57033)
    Fixes that the visitor which is used to implement template diagnostics isn't visiting the template attributes of structural directives.
    
    PR Close #57033

--- packages/compiler-cli/src/ngtsc/typecheck/extended/api/api.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/extended/api/api.ts
@@ -109,6 +109,17 @@ export interface TemplateCheckFactory<
 export abstract class TemplateCheckWithVisitor<Code extends ErrorCode>
   implements TemplateCheck<Code>
 {
+  /**
+   * When extended diagnostics were first introduced, the visitor wasn't implemented correctly
+   * which meant that it wasn't visiting the `templateAttrs` of structural directives (e.g.
+   * the expression of `*ngIf`). Fixing the issue causes a lot of internal breakages and will likely
+   * need to be done in a major version to avoid external breakages. This flag is used to opt out
+   * pre-existing diagnostics from the correct behavior until the breakages have been fixed while
+   * ensuring that newly-written diagnostics are correct from the beginning.
+   * TODO(crisbeto): remove this flag and fix the internal brekages.
+   */
+  readonly canVisitStructuralAttributes: boolean = true;
+
   abstract code: Code;
 
   /**
@@ -179,15 +190,23 @@ class TemplateVisitor<Code extends ErrorCode>
   }
 
   visitTemplate(template: TmplAstTemplate) {
+    const isInlineTemplate = template.tagName === 'ng-template';
     this.visitAllNodes(template.attributes);
-    if (template.tagName === 'ng-template') {
-      // Only visit input/outputs/templateAttrs if this isn't an inline template node
-      // generated for a structural directive (like `<div *ngIf></div>`). These nodes
-      // would be visited when the underlying element of an inline template node is processed.
+
+    if (isInlineTemplate) {
+      // Only visit input/outputs if this isn't an inline template node generated for a structural
+      // directive (like `<div *ngIf></div>`). These nodes would be visited when the underlying
+      // element of an inline template node is processed.
       this.visitAllNodes(template.inputs);
       this.visitAllNodes(template.outputs);
+    }
+
+    // TODO(crisbeto): remove this condition when deleting `canVisitStructuralAttributes`.
+    if (this.check.canVisitStructuralAttributes || isInlineTemplate) {
+      // `templateAttrs` aren't transferred over to the inner element so we always have to visit them.
       this.visitAllNodes(template.templateAttrs);
     }
+
     this.visitAllNodes(template.variables);
     this.visitAllNodes(template.references);
     this.visitAllNodes(template.children);
