a59ed66d87ba792f4e2005b946078debdba91ccf
Tom Wilkinson <tbondwilkinson@gmail.com>
2024-07-23 15:29:47 -0500

    refactor(core): Switch to using concat rather than spread operator for events. (#57103)
    Internally, the JSCompiler does not dead code eliminate this code when its using spread.
    
    PR Close #57103

--- packages/core/primitives/event-dispatch/src/event_type.ts
+++ packages/core/primitives/event-dispatch/src/event_type.ts
@@ -372,7 +372,7 @@ export const isCaptureEventType = (eventType: string) =>
   CAPTURE_EVENT_TYPES.indexOf(eventType) >= 0;
 
 /** All event types that are registered early.  */
-const EARLY_EVENT_TYPES = [...BUBBLE_EVENT_TYPES, ...CAPTURE_EVENT_TYPES];
+const EARLY_EVENT_TYPES = BUBBLE_EVENT_TYPES.concat(CAPTURE_EVENT_TYPES);
 
 /**
  * Whether or not an event type is registered in the early contract.
