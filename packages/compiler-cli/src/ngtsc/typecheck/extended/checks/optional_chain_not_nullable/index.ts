9af619f6f58a034157dd25648b731b73ea893d37
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-07-23 06:15:31 +0200

    fix(compiler-cli): extended diagnostic visitor not visiting template attributes
    Fixes that the visitor which is used to implement template diagnostics isn't visiting the template attributes of structural directives.

--- packages/compiler-cli/src/ngtsc/typecheck/extended/checks/optional_chain_not_nullable/index.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/extended/checks/optional_chain_not_nullable/index.ts
@@ -21,6 +21,7 @@ import {TemplateCheckFactory, TemplateCheckWithVisitor, TemplateContext} from '.
  * otherwise it would produce inaccurate results.
  */
 class OptionalChainNotNullableCheck extends TemplateCheckWithVisitor<ErrorCode.OPTIONAL_CHAIN_NOT_NULLABLE> {
+  override readonly canVisitStructuralAttributes = false;
   override code = ErrorCode.OPTIONAL_CHAIN_NOT_NULLABLE as const;
 
   override visitNode(
