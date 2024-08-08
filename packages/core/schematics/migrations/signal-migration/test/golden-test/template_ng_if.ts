2d35f64995cc7c7051e6526086983ea14ad710c5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-08-08 15:41:06 +0000

    refactor(migrations): detect if an input is not narrowed and can be migrated (#57308)
    By default, we don't migrate inputs if they are part of e.g. `@if` for
    now. That is because we don't have the template narrowing feature
    available yet.
    
    To improve impact of the migration until we have the narrowing, we add
    some additional checks that allow us to migrate instances of inputs that
    are part of e.g. `@if` but are actually not used inside (and hence are
    guaranteed to be _not_ narrowed).
    
    PR Close #57308

--- packages/core/schematics/migrations/signal-migration/test/golden-test/template_ng_if.ts
+++ packages/core/schematics/migrations/signal-migration/test/golden-test/template_ng_if.ts
@@ -15,10 +15,20 @@ import {Component, Input} from '@angular/core';
     <div *ngIf="third">
       {{third}}
     </div>
+
+    <div *ngIf="fourth">
+      {{notTheInput}}
+    </div>
+
+    @if (fifth) {
+      {{notTheInput}}
+    }
   `,
 })
 export class MyComp {
   @Input() first = true;
   @Input() second = false;
   @Input() third = true;
+  @Input() fourth = true;
+  @Input() fifth = true;
 }
