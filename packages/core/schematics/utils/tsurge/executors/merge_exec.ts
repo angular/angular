57db366522a4ba31d673d01517cbac3d459c32e1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-08-14 15:48:47 +0000

    refactor(migrations): framework to build batchable migrations (#57396)
    Introduces a migration framework to build batchable migrations that
    can run in Large Scale mode against e.g. all of Google, using workers.
    
    This is the original signal input migration infrastructure extracted
    into a more generic framework that we can use for writing additional
    ones for output, signal queries etc, while making sure those are not
    scoped to a single `ts.Program` that limits them to per-directory
    execution in very large projects (e.g. G3).
    
    The migration will be updated to use this, and in 1P we will add
    helpers to easily integrate such migrations into a Go-based pipeline
    runner.
    
    PR Close #57396

---
+++ packages/core/schematics/utils/tsurge/executors/merge_exec.ts
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Serializable} from '../helpers/serializable';
+import {TsurgeMigration} from '../migration';
+
+/**
+ * Executes the merge phase for the given migration against
+ * the given set of analysis unit data.
+ *
+ * @returns the serializable migration global data.
+ */
+export async function executeMergePhase<UnitData, GlobalData>(
+  migration: TsurgeMigration<UnitData, GlobalData>,
+  units: UnitData[],
+): Promise<Serializable<GlobalData>> {
+  return await migration.merge(units);
+}
---
+++ packages/core/schematics/utils/tsurge/test/output_migration.ts
@@ -0,0 +1,111 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {NgtscProgram} from '../../../../../compiler-cli/src/ngtsc/program';
+import {absoluteFromSourceFile} from '../../../../../compiler-cli/src/ngtsc/file_system';
+import {TypeScriptReflectionHost} from '../../../../../compiler-cli/src/ngtsc/reflection';
+import {DtsMetadataReader} from '../../../../../compiler-cli/src/ngtsc/metadata';
+import {confirmAsSerializable} from '../helpers/serializable';
+import {TsurgeMigration} from '../migration';
+import {Replacement, TextUpdate} from '../replacement';
+import {findOutputDeclarationsAndReferences, OutputID} from './output_helpers';
+import {ProgramInfo} from '../program_info';
+
+type AnalysisUnit = {[id: OutputID]: {seenProblematicUsage: boolean}};
+type GlobalMetadata = {[id: OutputID]: {canBeMigrated: boolean}};
+
+/**
+ * A `Tsurge` migration that can migrate instances of `@Output()` to
+ * the new `output()` API.
+ *
+ * Note that this is simply a testing construct for now, to verify the migration
+ * framework works as expected. This is **not a full migration**, but rather an example.
+ */
+export class OutputMigration extends TsurgeMigration<AnalysisUnit, GlobalMetadata> {
+  override async analyze(info: ProgramInfo<NgtscProgram>) {
+    const program = info.program.getTsProgram();
+    const typeChecker = program.getTypeChecker();
+    const reflector = new TypeScriptReflectionHost(typeChecker, false);
+    const dtsReader = new DtsMetadataReader(typeChecker, reflector);
+
+    const {sourceOutputs, problematicReferencedOutputs} = findOutputDeclarationsAndReferences(
+      info,
+      typeChecker,
+      reflector,
+      dtsReader,
+    );
+
+    const discoveredOutputs: AnalysisUnit = {};
+    for (const id of sourceOutputs.keys()) {
+      discoveredOutputs[id] = {seenProblematicUsage: false};
+    }
+    for (const id of problematicReferencedOutputs) {
+      discoveredOutputs[id] = {seenProblematicUsage: true};
+    }
+
+    // Here we confirm it as serializable..
+    return confirmAsSerializable(discoveredOutputs);
+  }
+
+  override async merge(data: AnalysisUnit[]) {
+    const merged: GlobalMetadata = {};
+
+    // Merge information from all compilation units. Mark
+    // outputs that cannot be migrated due to seen problematic usages.
+    for (const unit of data) {
+      for (const [idStr, info] of Object.entries(unit)) {
+        const id = idStr as OutputID;
+        const existing = merged[id];
+
+        if (existing === undefined) {
+          merged[id] = {canBeMigrated: info.seenProblematicUsage === false};
+        } else if (existing.canBeMigrated && info.seenProblematicUsage) {
+          merged[id].canBeMigrated = false;
+        }
+      }
+    }
+
+    // merge units into global metadata.
+    return confirmAsSerializable(merged);
+  }
+
+  override async migrate(globalAnalysisData: GlobalMetadata, info: ProgramInfo<NgtscProgram>) {
+    const program = info.program.getTsProgram();
+    const typeChecker = program.getTypeChecker();
+    const reflector = new TypeScriptReflectionHost(typeChecker, false);
+    const dtsReader = new DtsMetadataReader(typeChecker, reflector);
+
+    const {sourceOutputs} = findOutputDeclarationsAndReferences(
+      info,
+      typeChecker,
+      reflector,
+      dtsReader,
+    );
+    const replacements: Replacement[] = [];
+
+    for (const [id, node] of sourceOutputs.entries()) {
+      // Output cannot be migrated as per global analysis metadata; skip.
+      if (globalAnalysisData[id].canBeMigrated === false) {
+        continue;
+      }
+
+      replacements.push(
+        new Replacement(
+          absoluteFromSourceFile(node.getSourceFile()),
+          new TextUpdate({
+            position: node.getStart(),
+            end: node.getStart(),
+            toInsert: `// TODO: Actual migration logic\n`,
+          }),
+        ),
+      );
+    }
+
+    return replacements;
+  }
+}
