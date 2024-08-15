8d3ad536ebbf07b79813fc9f23a5e8b6e19fa970
Thomas Nguyen <tomnguyen@google.com>
2024-08-15 15:30:18 -0700

    refactor(core): Remove experimental code.
    This doesn't need to work since Pantheon doesn't need the host app to use event delegation. The other solution of a "host Selector" doesn't work either since dialogs appear outside of the rif app.

--- packages/core/src/event_dispatch/event_delegation.ts
+++ packages/core/src/event_dispatch/event_delegation.ts
@@ -19,18 +19,10 @@ import {
 
 import {IS_GLOBAL_EVENT_DELEGATION_ENABLED} from '../hydration/tokens';
 
-declare global {
-  interface Window {
-    __jsaction_contract: EventContract | undefined;
-  }
-}
-
 /**
  * Returns a set of providers required to setup support for event delegation.
- * @param multiContract - Experimental support to provide one event contract
- * when there are multiple binaries on the page.
  */
-export function provideGlobalEventDelegation(multiContract = false): Provider[] {
+export function provideGlobalEventDelegation(): Provider[] {
   return [
     {
       provide: IS_GLOBAL_EVENT_DELEGATION_ENABLED,
@@ -41,12 +33,7 @@ export function provideGlobalEventDelegation(multiContract = false): Provider[] 
       useValue: () => {
         const injector = inject(Injector);
         const eventContractDetails = injector.get(JSACTION_EVENT_CONTRACT);
-        if (multiContract && window.__jsaction_contract) {
-          eventContractDetails.instance = window.__jsaction_contract;
-          return;
-        }
         initGlobalEventDelegation(eventContractDetails, injector);
-        window.__jsaction_contract = eventContractDetails.instance;
       },
       multi: true,
     },
