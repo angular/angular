9814767d341c0a79eee01430ce9482bdd005f58d
Matthieu Riegler <kyro38@gmail.com>
2024-08-21 01:02:51 +0200

    refactor(compiler-cli): Add a map of every symbols used inside a package (#57346)
    This commit changes the structure of the API extraction files to include all symbols used inside a package.
    
    The structure is a `Map`, Symbol => package
    eg: 'ApplicationRef' => '@angular/core'
    
    PR Close #57346

--- packages/compiler-cli/src/ngtsc/core/src/compiler.ts
+++ packages/compiler-cli/src/ngtsc/core/src/compiler.ts
@@ -902,7 +902,7 @@ export class NgCompiler {
    * @param entryPoint Path to the entry point for the package for which API
    *     docs should be extracted.
    */
-  getApiDocumentation(entryPoint: string): DocEntry[] {
+  getApiDocumentation(entryPoint: string): {entries: DocEntry[]; symbols: Map<string, string>} {
     const compilation = this.ensureAnalyzed();
     const checker = this.inputProgram.getTypeChecker();
     const docsExtractor = new DocsExtractor(checker, compilation.metaReader);
@@ -918,7 +918,7 @@ export class NgCompiler {
     }
 
     // TODO: Technically the current directory is not the root dir.
-    //  Should probably be derived from the config.
+    // Should probably be derived from the config.
     const rootDir = this.inputProgram.getCurrentDirectory();
     return docsExtractor.extractAll(entryPointSourceFile, rootDir);
   }
