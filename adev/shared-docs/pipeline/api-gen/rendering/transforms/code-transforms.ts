angular/angular

commit 04911d7599c3a410bdea56c72c4cc317a15ffc78

docs(docs-infra): Add support for function/method overloads (#57255)
PR Close #57255

adev/shared-docs/pipeline/api-gen/rendering/transforms/code-transforms.ts
@@ -8,12 +8,11 @@
 
 import {
   DocEntry,
-  FunctionEntry,
+  FunctionSignatureMetadata,
   MemberEntry,
   MemberTags,
   ParameterEntry,
   PropertyEntry,
-  isFunctionEntryWithOverloads,
 } from '../entities';
 
 import {
@@ -64,7 +63,9 @@ export function addRenderableCodeToc<T extends DocEntry & HasModuleName>(
   const metadata = mapDocEntryToCode(entry);
   appendPrefixAndSuffix(entry, metadata);
 
-  const codeWithSyntaxHighlighting = codeToHtml(metadata.contents, 'typescript');
+  const codeWithSyntaxHighlighting = codeToHtml(metadata.contents, 'typescript', {
+    removeFunctionKeyword: true,
+  });
 
   // shiki returns the lines wrapped by 2 node : 1 pre node, 1 code node.
   // As leveraging jsdom isn't trivial here, we rely on a regex to extract the line nodes
@@ -78,7 +79,7 @@ export function addRenderableCodeToc<T extends DocEntry & HasModuleName>(
   const afterCode = match[3];
 
   const lines = splitLines(insideCode);
-  const groups = groupCodeLines(lines, metadata);
+  const groups = groupCodeLines(lines, metadata, entry);
 
   return {
     ...entry,
@@ -89,11 +90,12 @@ export function addRenderableCodeToc<T extends DocEntry & HasModuleName>(
 }
 
 /** Group overloaded methods */
-function groupCodeLines(lines: string[], metadata: CodeTableOfContentsData) {
+function groupCodeLines(lines: string[], metadata: CodeTableOfContentsData, entry: DocEntry) {
+  const hasSingleSignature = isFunctionEntry(entry) && entry.signatures.length === 1;
   return lines.reduce((groups, line, index) => {
-    const tocItem = {
+    const tocItem: CodeLineRenderable = {
       contents: line,
-      id: metadata.codeLineNumbersWithIdentifiers.get(index),
+      id: hasSingleSignature ? undefined : metadata.codeLineNumbersWithIdentifiers.get(index),
       isDeprecated: metadata.deprecatedLineNumbers.some((lineNumber) => lineNumber === index),
     };
 
@@ -134,21 +136,26 @@ export function mapDocEntryToCode(entry: DocEntry): CodeTableOfContentsData {
   if (isFunctionEntry(entry)) {
     const isDeprecated = isDeprecatedEntry(entry);
     const codeLineNumbersWithIdentifiers = new Map<number, string>();
+    const hasSingleSignature = entry.signatures.length === 1;
 
-    if (isFunctionEntryWithOverloads(entry) && entry.overloads) {
+    if (entry.signatures.length > 0) {
       const initialMetadata: CodeTableOfContentsData = {
         contents: '',
         codeLineNumbersWithIdentifiers: new Map<number, string>(),
         deprecatedLineNumbers: [],
       };
 
-      return entry.overloads.reduce(
-        (acc: CodeTableOfContentsData, curr: FunctionEntry, index: number) => {
+      return entry.signatures.reduce(
+        (acc: CodeTableOfContentsData, curr: FunctionSignatureMetadata, index: number) => {
           const lineNumber = index;
-          acc.codeLineNumbersWithIdentifiers.set(lineNumber, curr.name);
-          acc.contents += `${curr.name}(${curr.params
-            .map((param) => mapParamEntry(param))
-            .join(`, `)}): ${curr.returnType}\n`;
+          acc.codeLineNumbersWithIdentifiers.set(lineNumber, `${curr.name}_${index}`);
+          acc.contents += getMethodCodeLine(curr, [], hasSingleSignature, true);
+
+          // We don't want to add line break after the last item
+          if (!hasSingleSignature && index < entry.signatures.length - 1) {
+            acc.contents += '\n';
+          }
+
           if (isDeprecatedEntry(curr)) {
             acc.deprecatedLineNumbers.push(lineNumber);
           }
@@ -157,9 +164,10 @@ export function mapDocEntryToCode(entry: DocEntry): CodeTableOfContentsData {
         initialMetadata,
       );
     }
+
     return {
       // It is important to add the function keyword as shiki will only highlight valid ts
-      contents: `function ${getMethodCodeLine(entry, [], true)}`,
+      contents: `function ${getMethodCodeLine(entry.implementation, [], true)}`,
       codeLineNumbersWithIdentifiers,
       deprecatedLineNumbers: isDeprecated ? [0] : [],
     };
@@ -248,22 +256,31 @@ function getCodeTocData(members: MemberEntry[], hasPrefixLine: boolean): CodeTab
   // In case when hasPrefixLine is true we should take it into account when we're generating
   // `codeLineNumbersWithIdentifiers` below.
   const skip = !!hasPrefixLine ? 1 : 0;
+  let lineNumber = skip;
 
   return members.reduce((acc: CodeTableOfContentsData, curr: MemberEntry, index: number) => {
-    const lineNumber = index + skip;
-    acc.codeLineNumbersWithIdentifiers.set(lineNumber, curr.name);
-    acc.contents += `  ${getCodeLine(curr).trim()}\n`;
-    if (isDeprecatedEntry(curr)) {
-      acc.deprecatedLineNumbers.push(lineNumber);
+    const setTocData = (content: string) => {
+      acc.contents += `  ${content.trim()}\n`;
+      acc.codeLineNumbersWithIdentifiers.set(lineNumber, curr.name);
+      if (isDeprecatedEntry(curr)) {
+        acc.deprecatedLineNumbers.push(lineNumber);
+      }
+      lineNumber++;
+    };
+
+    if (isClassMethodEntry(curr)) {
+      curr.signatures.forEach((signature) => {
+        setTocData(getMethodCodeLine(signature, curr.memberTags));
+      });
+    } else {
+      setTocData(getCodeLine(curr));
     }
     return acc;
   }, initialMetadata);
 }
 
-function getCodeLine(member: MemberEntry) {
-  if (isClassMethodEntry(member)) {
-    return getMethodCodeLine(member, member.memberTags);
-  } else if (isGetterEntry(member)) {
+function getCodeLine(member: MemberEntry): string {
+  if (isGetterEntry(member)) {
     return getGetterCodeLine(member);
   } else if (isSetterEntry(member)) {
     return getSetterCodeLine(member);
@@ -281,11 +298,12 @@ function getPropertyCodeLine(member: PropertyEntry): string {
 
 /** Map method entry to text */
 function getMethodCodeLine(
-  member: FunctionEntry,
+  member: FunctionSignatureMetadata,
   memberTags: MemberTags[] = [],
   displayParamsInNewLines: boolean = false,
+  isFunction: boolean = false,
 ): string {
-  return `${memberTags.join(' ')} ${member.name}(${displayParamsInNewLines ? '\n  ' : ''}${member.params
+  return `${isFunction ? 'function' : ''}${memberTags.join(' ')} ${member.name}(${displayParamsInNewLines ? '\n  ' : ''}${member.params
     .map((param) => mapParamEntry(param))
     .join(`,${displayParamsInNewLines ? '\n  ' : ' '}`)}${
     displayParamsInNewLines ? '\n' : ''
@@ -344,7 +362,7 @@ function getNumberOfLinesOfCode(contents: string): number {
 /** Prints an initializer function signature into a single line. */
 export function printInitializerFunctionSignatureLine(
   name: string,
-  signature: FunctionEntry,
+  signature: FunctionSignatureMetadata,
   showTypesInSignaturePreview: boolean,
 ): string {
   let res = name;
@@ -381,7 +399,7 @@ export function printInitializerFunctionSignatureLine(
     res += `: ${signature.returnType}`;
   }
   res += ';';
-  return res;
+  return `function ${res}`;
 }
 
 function appendPrefixAndSuffix(entry: DocEntry, codeTocData: CodeTableOfContentsData): void {

adev/shared-docs/pipeline/api-gen/rendering/entities.ts
@@ -132,15 +132,25 @@ export interface PipeEntry extends ClassEntry {
   // TODO: add `isPure`.
 }
 
-export interface FunctionEntry extends DocEntry {
+export interface FunctionSignatureMetadata extends DocEntry {
   params: ParameterEntry[];
   returnType: string;
+  returnDescription?: string;
+
   generics: GenericEntry[];
   isNewType: boolean;
 }
 
-export interface FunctionWithOverloadsEntry extends FunctionEntry {
-  overloads: FunctionEntry[] | null;
+export type FunctionEntry = FunctionDefinitionEntry &
+  DocEntry & {
+    implementation: FunctionSignatureMetadata;
+  };
+
+/** Interface describing a function with overload signatures. */
+export interface FunctionDefinitionEntry {
+  name: string;
+  signatures: FunctionSignatureMetadata[];
+  implementation: FunctionSignatureMetadata | null;
 }
 
 /** Sub-entry for a single class or enum member. */
@@ -178,15 +188,9 @@ export interface ParameterEntry {
   isRestParam: boolean;
 }
 
-export interface FunctionWithOverloads {
-  name: string;
-  signatures: FunctionEntry[];
-  implementation: FunctionEntry | null;
-}
-
 export interface InitializerApiFunctionEntry extends DocEntry {
-  callFunction: FunctionWithOverloads;
-  subFunctions: FunctionWithOverloads[];
+  callFunction: FunctionDefinitionEntry;
+  subFunctions: FunctionDefinitionEntry[];
 
   __docsMetadata__?: {
     /**
@@ -203,8 +207,4 @@ export interface InitializerApiFunctionEntry extends DocEntry {
 
 export function isDocEntryWithSourceInfo(entry: DocEntry): entry is DocEntryWithSourceInfo {
   return 'source' in entry;
-}
-
-export function isFunctionEntryWithOverloads(entry: DocEntry): entry is FunctionWithOverloadsEntry {
-  return 'overloads' in entry;
 }

adev/shared-docs/pipeline/api-gen/rendering/entities/renderables.ts
@@ -12,6 +12,7 @@ import {
   DocEntry,
   EnumEntry,
   FunctionEntry,
+  FunctionSignatureMetadata,
   InitializerApiFunctionEntry,
   JsDocTagEntry,
   MemberEntry,
@@ -55,7 +56,7 @@ export type TypeAliasEntryRenderable = TypeAliasEntry & DocEntryRenderable & Has
 export type ClassEntryRenderable = ClassEntry &
   DocEntryRenderable &
   HasRenderableToc & {
-    membersGroups: Map<string, MemberEntryRenderable[]>;
+    members: MemberEntryRenderable[];
   };
 
 /** Documentation entity for a TypeScript enum augmented transformed content for rendering. */
@@ -71,9 +72,12 @@ export type InterfaceEntryRenderable = ClassEntryRenderable;
 export type FunctionEntryRenderable = FunctionEntry &
   DocEntryRenderable &
   HasRenderableToc & {
-    params: ParameterEntryRenderable[];
     deprecationMessage: string | null;
-    overloads: FunctionEntryRenderable[] | null;
+  };
+
+export type FunctionSignatureMetadataRenderable = FunctionSignatureMetadata &
+  DocEntryRenderable & {
+    params: ParameterEntryRenderable[];
   };
 
 /** Sub-entry for a single class or enum member augmented with transformed content for rendering. */

adev/shared-docs/pipeline/api-gen/rendering/entities/traits.ts
@@ -1,8 +1,7 @@
-import {FunctionEntry, JsDocTagEntry, MemberEntry, ParameterEntry} from '../entities';
+import {JsDocTagEntry, MemberEntry, ParameterEntry} from '../entities';
 
 import {
   CodeLineRenderable,
-  FunctionEntryRenderable,
   JsDocTagRenderable,
   LinkEntryRenderable,
   MemberEntryRenderable,
@@ -79,14 +78,6 @@ export interface HasParams {
 /** A doc entry that has params for rendering. */
 export interface HasRenderableParams {
   params: ParameterEntryRenderable[];
-}
-
-export interface HasOverloads {
-  overloads: FunctionEntry[] | null;
-}
-
-export interface HasRenderableOverloads {
-  overloads: FunctionEntryRenderable[] | null;
 }
 
 export interface HasDeprecatedFlag {

adev/shared-docs/pipeline/api-gen/rendering/marked/renderer.ts
@@ -15,7 +15,9 @@ import {codeToHtml} from '../shiki/shiki';
  */
 export const renderer: Partial<Renderer> = {
   code({lang, text}): string {
-    const highlightResult = codeToHtml(text, lang).replace(/>\s+</g, '><');
+    const highlightResult = codeToHtml(text, lang)
+      // remove spaces/line-breaks between elements to not mess-up `pre` style
+      .replace(/>\s+</g, '><');
 
     return `
       <div class="docs-code" role="group">

adev/shared-docs/pipeline/api-gen/rendering/shiki/shiki.ts
@@ -21,8 +21,12 @@ export async function initHighlighter() {
   });
 }
 
-export function codeToHtml(code: string, language: string | undefined): string {
-  return highlighter.codeToHtml(code, {
+export function codeToHtml(
+  code: string,
+  language: string | undefined,
+  options?: {removeFunctionKeyword?: boolean},
+): string {
+  const html = highlighter.codeToHtml(code, {
     lang: language ?? 'text',
     themes: {
       light: 'github-light',
@@ -31,4 +35,20 @@ export function codeToHtml(code: string, language: string | undefined): string {
     cssVariablePrefix: '--shiki-',
     defaultColor: false,
   });
+
+  if (options?.removeFunctionKeyword) {
+    return removeFunctionKeywordFromShikiHtml(html);
+  }
+  return html;
+}
+
+export function removeFunctionKeywordFromShikiHtml(shikiHtml: string): string {
+  return (
+    shikiHtml
+      // remove the leading space of the element after the "function" element
+      .replace(/(<[^>]*>function<\/\w+><[^>]*>)(\s)(\w+<\/\w+>)/g, '$1$3')
+      // Shiki requires the keyword function for highlighting functions signatures
+      // We don't want to display it so we remove elements with the keyword
+      .replace(/<[^>]*>function<\/\w+>/g, '')
+  );
 }

adev/shared-docs/pipeline/api-gen/rendering/styling/css-classes.ts
@@ -16,6 +16,7 @@ export const REFERENCE_MEMBERS = 'docs-reference-members';
 export const REFERENCE_DEPRECATED = 'docs-reference-deprecated';
 export const REFERENCE_MEMBERS_CONTAINER = 'docs-reference-members-container';
 export const REFERENCE_MEMBER_CARD = 'docs-reference-member-card';
+export const REFERENCE_MEMBER_CARD_HEADER = 'docs-reference-card-header';
 export const REFERENCE_MEMBER_CARD_BODY = 'docs-reference-card-body';
 export const REFERENCE_MEMBER_CARD_ITEM = 'docs-reference-card-item';
 

adev/shared-docs/pipeline/api-gen/rendering/templates/class-member-list.tsx
@@ -10,11 +10,11 @@ import {h} from 'preact';
 import {MemberEntryRenderable} from '../entities/renderables';
 import {ClassMember} from './class-member';
 
-export function ClassMemberList(props: {membersGroups: Map<string, MemberEntryRenderable[]>}) {
+export function ClassMemberList(props: {members: MemberEntryRenderable[]}) {
   return (
     <div class="docs-reference-members">
-      {Array.from(props.membersGroups).map(([_, group]) => (
-        <ClassMember members={group} />
+      {props.members.map((member) => (
+        <ClassMember member={member} />
       ))}
     </div>
   );

commit f3c3c26e4453ae3595eaf8deb269100ec81bd245

docs(docs-infra): fix regressions around members & deprecations
This commit fixes regressions introduced by #57255

adev/shared-docs/pipeline/api-gen/rendering/transforms/code-transforms.ts
@@ -259,21 +259,25 @@ function getCodeTocData(members: MemberEntry[], hasPrefixLine: boolean): CodeTab
   let lineNumber = skip;
 
   return members.reduce((acc: CodeTableOfContentsData, curr: MemberEntry, index: number) => {
-    const setTocData = (content: string) => {
+    const setTocData = (entry: DocEntry | MemberEntry, content: string) => {
       acc.contents += `  ${content.trim()}\n`;
-      acc.codeLineNumbersWithIdentifiers.set(lineNumber, curr.name);
-      if (isDeprecatedEntry(curr)) {
+      acc.codeLineNumbersWithIdentifiers.set(lineNumber, entry.name);
+      if (isDeprecatedEntry(entry)) {
         acc.deprecatedLineNumbers.push(lineNumber);
       }
       lineNumber++;
     };
 
     if (isClassMethodEntry(curr)) {
-      curr.signatures.forEach((signature) => {
-        setTocData(getMethodCodeLine(signature, curr.memberTags));
-      });
+      if (curr.signatures.length > 0) {
+        curr.signatures.forEach((signature) => {
+          setTocData(signature, getMethodCodeLine(signature, curr.memberTags));
+        });
+      } else {
+        setTocData(curr, getMethodCodeLine(curr.implementation, curr.memberTags));
+      }
     } else {
-      setTocData(getCodeLine(curr));
+      setTocData(curr, getCodeLine(curr));
     }
     return acc;
   }, initialMetadata);