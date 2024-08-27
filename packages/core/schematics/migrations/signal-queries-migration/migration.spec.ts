0ba2a95bf60e4418c04f39d79f80d969de440f3a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 13:58:53 -0700

    Revert "refactor(migrations): properly handle multi query migration (#57525)" (#57555)
    This reverts commit f454ad3bcff801895d8fa1e4316bdb5da8b7368c.
    
    Reason: breaks g3
    
    PR Close #57555

--- packages/core/schematics/migrations/signal-queries-migration/migration.spec.ts
+++ packages/core/schematics/migrations/signal-queries-migration/migration.spec.ts
@@ -19,8 +19,7 @@ interface TestCase {
   focus?: boolean;
 }
 
-const declarationTestCases: TestCase[] = [
-  // View Child
+const testCases: TestCase[] = [
   {
     id: 'viewChild with string locator and nullable',
     before: `@ViewChild('myBtn') button: MyButton|undefined = undefined;`,
@@ -51,129 +50,6 @@ const declarationTestCases: TestCase[] = [
     before: `@ViewChild('myBtn', {read: ElementRef}) buttonEl!: ElementRef;`,
     after: `readonly buttonEl = viewChild.required('myBtn', { read: ElementRef });`,
   },
-  // Content Child
-  {
-    id: 'contentChild with string locator and nullable',
-    before: `@ContentChild('myBtn') button: MyButton|undefined = undefined;`,
-    after: `readonly button = contentChild<MyButton>('myBtn');`,
-  },
-  {
-    id: 'contentChild with class type locator and nullable',
-    before: `@ContentChild(MyButton) button: MyButton|undefined = undefined;`,
-    after: `readonly button = contentChild(MyButton);`,
-  },
-  {
-    id: 'contentChild with class type locator and nullable via question-mark shorthand',
-    before: `@ContentChild(MyButton) button?: MyButton;`,
-    after: `readonly button = contentChild(MyButton);`,
-  },
-  {
-    id: 'contentChild with class type locator and exclamation mark to simulate required',
-    before: `@ContentChild(MyButton) button!: MyButton;`,
-    after: `readonly button = contentChild.required(MyButton);`,
-  },
-  {
-    id: 'contentChild with string locator and read option, nullable shorthand',
-    before: `@ContentChild('myBtn', {read: ElementRef}) buttonEl?: ElementRef;`,
-    after: `readonly buttonEl = contentChild('myBtn', { read: ElementRef });`,
-  },
-  {
-    id: 'contentChild with string locator and read option, required',
-    before: `@ContentChild('myBtn', {read: ElementRef}) buttonEl!: ElementRef;`,
-    after: `readonly buttonEl = contentChild.required('myBtn', { read: ElementRef });`,
-  },
-  {
-    id: 'contentChild with string locator and read option, required',
-    before: `@ContentChild('myBtn', {read: ElementRef}) buttonEl!: ElementRef;`,
-    after: `readonly buttonEl = contentChild.required('myBtn', { read: ElementRef });`,
-  },
-  {
-    id: 'contentChild with descendants option',
-    before: `@ContentChild('myBtn', {descendants: false}) buttonEl!: ElementRef;`,
-    after: `readonly buttonEl = contentChild.required<ElementRef>('myBtn', { descendants: false });`,
-  },
-  // ViewChildren
-  {
-    id: 'viewChildren with string locator and nullable',
-    before: `@ViewChildren('myBtn') button?: QueryList<ElementRef>;`,
-    after: `readonly button = viewChildren<ElementRef>('myBtn');`,
-  },
-  {
-    id: 'viewChildren with class type locator and nullable',
-    before: `@ViewChildren(MyButton) button?: QueryList<MyButton>;`,
-    after: `readonly button = viewChildren(MyButton);`,
-  },
-  {
-    id: 'viewChildren with class type locator and exclamation mark',
-    before: `@ViewChildren(MyButton) button!: QueryList<MyButton>;`,
-    after: `readonly button = viewChildren(MyButton);`,
-  },
-  {
-    id: 'viewChild with string locator and read option, nullable shorthand',
-    before: `@ViewChildren('myBtn', {read: ElementRef}) buttonEl?: QueryList<ElementRef>;`,
-    after: `readonly buttonEl = viewChildren('myBtn', { read: ElementRef });`,
-  },
-  {
-    id: 'viewChildren with string locator and read option, required',
-    before: `@ViewChildren('myBtn', {read: ElementRef}) buttonEl!: QueryList<ElementRef>;`,
-    after: `readonly buttonEl = viewChildren('myBtn', { read: ElementRef });`,
-  },
-  {
-    id: 'viewChildren with query list as initializer value',
-    before: `@ViewChildren('myBtn') buttonEl = new QueryList<ElementRef>()`,
-    after: `readonly buttonEl = viewChildren<ElementRef>('myBtn');`,
-  },
-  {
-    id: 'viewChildren with query list as initializer value, and descendants option',
-    before: `@ViewChildren('myBtn', {descendants: false}) buttonEl = new QueryList<ElementRef>()`,
-    after: `readonly buttonEl = viewChildren<ElementRef>('myBtn', { descendants: false });`,
-  },
-  {
-    id: 'viewChildren with query list as initializer value, and descendants option but same as default',
-    before: `@ViewChildren('myBtn', {descendants: true}) buttonEl = new QueryList<ElementRef>()`,
-    after: `readonly buttonEl = viewChildren<ElementRef>('myBtn');`,
-  },
-  // ContentChildren
-  {
-    id: 'contentChildren with string locator and nullable',
-    before: `@ContentChildren('myBtn') button?: QueryList<ElementRef>;`,
-    after: `readonly button = contentChildren<ElementRef>('myBtn');`,
-  },
-  {
-    id: 'contentChildren with class type locator and nullable',
-    before: `@ContentChildren(MyButton) button?: QueryList<MyButton>;`,
-    after: `readonly button = contentChildren(MyButton);`,
-  },
-  {
-    id: 'contentChildren with class type locator and exclamation mark',
-    before: `@ContentChildren(MyButton) button!: QueryList<MyButton>;`,
-    after: `readonly button = contentChildren(MyButton);`,
-  },
-  {
-    id: 'contentChildren with string locator and read option, nullable shorthand',
-    before: `@ContentChildren('myBtn', {read: ElementRef}) buttonEl?: QueryList<ElementRef>;`,
-    after: `readonly buttonEl = contentChildren('myBtn', { read: ElementRef });`,
-  },
-  {
-    id: 'contentChildren with string locator and read option, required',
-    before: `@ContentChildren('myBtn', {read: ElementRef}) buttonEl!: QueryList<ElementRef>;`,
-    after: `readonly buttonEl = contentChildren('myBtn', { read: ElementRef });`,
-  },
-  {
-    id: 'contentChildren with query list as initializer value',
-    before: `@ContentChildren('myBtn') buttonEl = new QueryList<ElementRef>()`,
-    after: `readonly buttonEl = contentChildren<ElementRef>('myBtn');`,
-  },
-  {
-    id: 'contentChildren with query list as initializer value, and descendants option',
-    before: `@ContentChildren('myBtn', {descendants: true}) buttonEl = new QueryList<ElementRef>()`,
-    after: `readonly buttonEl = contentChildren<ElementRef>('myBtn', { descendants: true });`,
-  },
-  {
-    id: 'contentChildren with query list as initializer value, and descendants option but same as default',
-    before: `@ContentChildren('myBtn', {descendants: false}) buttonEl = new QueryList<ElementRef>()`,
-    after: `readonly buttonEl = contentChildren<ElementRef>('myBtn');`,
-  },
 ];
 
 describe('signal queries migration', () => {
@@ -181,24 +57,19 @@ describe('signal queries migration', () => {
     initMockFileSystem('Native');
   });
 
-  describe('declaration test cases', () => {
-    for (const c of declarationTestCases) {
+  describe('test cases', () => {
+    for (const c of testCases) {
       (c.focus ? fit : it)(c.id, async () => {
         const fs = await runTsurgeMigration(new SignalQueriesMigration(), [
           {
             name: absoluteFrom('/app.component.ts'),
             isProgramRootFile: true,
-            contents: populateDeclarationTestCaseComponent(c.before),
+            contents: populateTestCaseComponent(c.before),
           },
         ]);
 
-        let actual = fs.readFile(absoluteFrom('/app.component.ts'));
-        let expected = populateDeclarationTestCaseComponent(c.after);
-
-        // Cut off the string before the class declaration.
-        // The import diff is irrelevant here for now.
-        actual = actual.substring(actual.indexOf('@Directive'));
-        expected = expected.substring(expected.indexOf('@Directive'));
+        const actual = fs.readFile(absoluteFrom('/app.component.ts'));
+        const expected = populateTestCaseComponent(c.after);
 
         if (actual !== expected) {
           expect(diffText(expected, actual)).toBe('');
@@ -231,59 +102,9 @@ describe('signal queries migration', () => {
     expect(actual).not.toContain(`viewChild`);
     expect(actual).toContain(`@ViewChild('labelRef') label?: ElementRef;`);
   });
-
-  it('should update imports when migrating', async () => {
-    const fs = await runTsurgeMigration(new SignalQueriesMigration(), [
-      {
-        name: absoluteFrom('/app.component.ts'),
-        isProgramRootFile: true,
-        contents: `
-          import {ViewChild, ElementRef, Directive} from '@angular/core';
-
-          @Directive()
-          class MyComp {
-            @ViewChild('labelRef') label?: ElementRef;
-          }
-        `,
-      },
-    ]);
-
-    const actual = fs.readFile(absoluteFrom('/app.component.ts'));
-    expect(actual).toContain(`import { ElementRef, Directive, viewChild } from '@angular/core';`);
-    expect(actual).toContain(`label = viewChild<ElementRef>('labelRef')`);
-  });
-
-  it('should not remove imports when partially migrating', async () => {
-    const fs = await runTsurgeMigration(new SignalQueriesMigration(), [
-      {
-        name: absoluteFrom('/app.component.ts'),
-        isProgramRootFile: true,
-        contents: `
-          import {ViewChild, ElementRef, Directive} from '@angular/core';
-
-          @Directive()
-          class MyComp {
-            @ViewChild('labelRef') label?: ElementRef;
-            @ViewChild('labelRef2') label2?: ElementRef;
-
-            click() {
-              this.label2 = undefined;
-            }
-          }
-        `,
-      },
-    ]);
-
-    const actual = fs.readFile(absoluteFrom('/app.component.ts'));
-    expect(actual).toContain(
-      `import { ViewChild, ElementRef, Directive, viewChild } from '@angular/core';`,
-    );
-    expect(actual).toContain(`label = viewChild<ElementRef>('labelRef')`);
-    expect(actual).toContain(`@ViewChild('labelRef2') label2?: ElementRef;`);
-  });
 });
 
-function populateDeclarationTestCaseComponent(declaration: string): string {
+function populateTestCaseComponent(declaration: string): string {
   return `
     import {
       ViewChild,
