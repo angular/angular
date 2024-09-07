2936db397bc940d2cbec40d568fbc4f7f18e2f17
Matthieu Riegler <kyro38@gmail.com>
2024-09-08 00:34:12 +0200

    feat(compiler-cli): Directives, Components and pipes are standalone by default
    With this commit, Directives, Components and Pipes are now standalone by default and don't require `standalone: true` anymore
    
    BREAKING CHANGE: Directive, Components and Pipes are now standalone by default

---
+++ packages/core/src/render3/jit/standalone-default-value.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * A constant defining the default value for the standalone attribute in Directive and Pipes decorators.
+ * Extracted to a separate file to facilitate G3 patches.
+ */
+export const NG_STANDALONE_DEFAULT_VALUE = true;
--- packages/core/testing/src/test_bed_compiler.ts
+++ packages/core/testing/src/test_bed_compiler.ts
@@ -102,9 +102,11 @@ function assertNoStandaloneComponents(
   types.forEach((type) => {
     if (!getAsyncClassMetadataFn(type)) {
       const component = resolver.resolve(type);
-      if (component && component.standalone) {
+      // TODO: That's probably not what we want.
+      if (component && (component.standalone === undefined || component.standalone === true)) {
         throw new Error(ɵgenerateStandaloneInDeclarationsError(type, location));
       }
+
     }
   });
 }
