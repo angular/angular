d2861e8f845d7bbf3f0f4a799ebb8f414810e4fd
ChinoUkaegbu <77782533+ChinoUkaegbu@users.noreply.github.com>
2024-09-07 07:10:02 +0400

    change wording in directives.ts

--- packages/core/src/metadata/directives.ts
+++ packages/core/src/metadata/directives.ts
@@ -401,9 +401,9 @@ export interface ComponentDecorator {
    * Unlike other directives, only one component can be instantiated for a given element in a
    * template.
    *
-   * A component must belong to an NgModule in order for it to be available
-   * to another component or application. To make it a member of an NgModule,
-   * list it in the `declarations` field of the `NgModule` metadata.
+   * When running Angular with NgModule, a component must belong to an NgModule in
+   * order for it to be available to another component or application. To make it a
+   * member of an NgModule, list it in the `declarations` field of the `NgModule` metadata.
    *
    * Note that, in addition to these options for configuring a directive,
    * you can control a component's runtime behavior by implementing
