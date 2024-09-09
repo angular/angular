bfbe003b878869fd73f3e45793474c0231b1a113
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-09-09 11:31:51 +0000

    refactor(migrations): gracefully handle resource loading issues
    This can happen when we manually parse templates and end up with an
    invalid URL. We should not fail the migration in those cases.

---
+++ packages/core/schematics/migrations/signal-migration/test/golden-test/external_templates.ts
@@ -0,0 +1,9 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  templateUrl: './external_templates.html',
+  selector: 'with-template',
+})
+export class WithTemplate {
+  @Input() test = true;
+}
