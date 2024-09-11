1c663ca82069e5dc830cd7de67f4556351f9f362
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-09-11 12:12:29 +0000

    refactor(migrations): add helper for dedenting to tsurge (#57777)
    Dedenting the smallest leading whitespace is useful for conveniently
    testing the output of a migration. This commit includes the helper in
    Tsurge.
    
    PR Close #57777

--- packages/core/schematics/test/helpers.ts
+++ packages/core/schematics/test/helpers.ts
@@ -6,26 +6,11 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {dedent as dedentFn} from '../utils/tsurge/testing/dedent';
+
 /**
  * Template string function that can be used to dedent the resulting
  * string literal. The smallest common indentation will be omitted.
  * Additionally, whitespace in empty lines is removed.
  */
-export function dedent(strings: TemplateStringsArray, ...values: any[]) {
-  let joinedString = '';
-  for (let i = 0; i < values.length; i++) {
-    joinedString += `${strings[i]}${values[i]}`;
-  }
-  joinedString += strings[strings.length - 1];
-
-  const matches = joinedString.match(/^[ \t]*(?=\S)/gm);
-  if (matches === null) {
-    return joinedString;
-  }
-
-  const minLineIndent = Math.min(...matches.map((el) => el.length));
-  const omitMinIndentRegex = new RegExp(`^[ \\t]{${minLineIndent}}`, 'gm');
-  const omitEmptyLineWhitespaceRegex = /^[ \t]+$/gm;
-  const result = minLineIndent > 0 ? joinedString.replace(omitMinIndentRegex, '') : joinedString;
-  return result.replace(omitEmptyLineWhitespaceRegex, '');
-}
+export const dedent = dedentFn;
