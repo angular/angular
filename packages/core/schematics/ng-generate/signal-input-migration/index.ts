59fe9bc77236f1374427a851e55b0fa5216d9cf9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-09-13 06:22:05 +0000

    feat(migrations): introduce signal input migration as `ng generate` schematic (#57805)
    Introduces a new `ng generate` schematic for migration `@Input()`
    declarations to signal inputs.
    
    This migration is the same that is also integrated into the
    VSCode extension.
    
    Note: In a follow-up the documentation for this will be improved, and a
    flag to report reasons on why certain inputs weren't migrated, is added.
    
    PR Close #57805

---
+++ packages/core/schematics/ng-generate/signal-input-migration/index.ts
@@ -0,0 +1,113 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import {Rule, SchematicsException} from '@angular-devkit/schematics';
+
+import assert from 'assert';
+import {SignalInputMigration} from '../../migrations/signal-migration/src';
+import {getProjectTsConfigPaths} from '../../utils/project_tsconfig_paths';
+import {DevkitMigrationFilesystem} from '../../utils/tsurge/helpers/angular_devkit/devkit_filesystem';
+import {groupReplacementsByFile} from '../../utils/tsurge/helpers/group_replacements';
+import {setFileSystem} from '@angular/compiler-cli/src/ngtsc/file_system';
+import {CompilationUnitData} from '../../migrations/signal-migration/src/batch/unit_data';
+import {ProjectRootRelativePath, Replacement, TextUpdate} from '../../utils/tsurge';
+
+interface Options {
+  path: string;
+  bestEffortMode?: boolean;
+  analysisDir: string;
+}
+
+export function migrate(options: Options): Rule {
+  return async (tree, context) => {
+    const {buildPaths, testPaths} = await getProjectTsConfigPaths(tree);
+
+    if (!buildPaths.length && !testPaths.length) {
+      throw new SchematicsException(
+        'Could not find any tsconfig file. Cannot run signal input migration.',
+      );
+    }
+
+    const fs = new DevkitMigrationFilesystem(tree);
+    setFileSystem(fs);
+
+    const migration = new SignalInputMigration({
+      bestEffortMode: options.bestEffortMode,
+      shouldMigrateInput: (input) => {
+        return (
+          input.file.rootRelativePath.startsWith(fs.normalize(options.path)) &&
+          !/(^|\/)node_modules\//.test(input.file.rootRelativePath)
+        );
+      },
+    });
+    const analysisPath = fs.resolve(options.analysisDir);
+    const unitResults: CompilationUnitData[] = [];
+    const programInfos = [...buildPaths, ...testPaths].map((tsconfigPath) => {
+      const baseInfo = migration.createProgram(tsconfigPath, fs);
+      const info = migration.prepareProgram(baseInfo);
+
+      // Support restricting the analysis to subfolders for larger projects.
+      if (analysisPath !== '/') {
+        info.sourceFiles = info.sourceFiles.filter((sf) => sf.fileName.startsWith(analysisPath));
+        info.fullProgramSourceFiles = info.fullProgramSourceFiles.filter((sf) =>
+          sf.fileName.startsWith(analysisPath),
+        );
+      }
+
+      return {info, tsconfigPath};
+    });
+
+    // Analyze phase. Treat all projects as compilation units as
+    // this allows us to support references between those.
+    for (const {info, tsconfigPath} of programInfos) {
+      context.logger.info(`Analyzing: ${tsconfigPath}..`);
+
+      unitResults.push(await migration.analyze(info));
+    }
+
+    context.logger.info(``);
+    context.logger.info(`Processing analysis data between targets..`);
+    context.logger.info(``);
+
+    const merged = await migration.merge(unitResults);
+    const replacementsPerFile: Map<ProjectRootRelativePath, TextUpdate[]> = new Map();
+
+    for (const {info, tsconfigPath} of programInfos) {
+      context.logger.info(`Migrating: ${tsconfigPath}..`);
+
+      const replacements = await migration.migrate(merged, info);
+      const changesPerFile = groupReplacementsByFile(replacements);
+
+      for (const [file, changes] of changesPerFile) {
+        if (!replacementsPerFile.has(file)) {
+          replacementsPerFile.set(file, changes);
+        }
+      }
+    }
+
+    context.logger.info(`Applying changes..`);
+    for (const [file, changes] of replacementsPerFile) {
+      const recorder = tree.beginUpdate(file);
+      for (const c of changes) {
+        recorder
+          .remove(c.data.position, c.data.end - c.data.position)
+          .insertLeft(c.data.position, c.data.toInsert);
+      }
+      tree.commitUpdate(recorder);
+    }
+
+    context.logger.info('');
+    context.logger.info(`Successfully migrated to signal inputs 🎉`);
+    if (options.bestEffortMode) {
+      context.logger.warn(
+        `You ran with best effort mode. Manually verify all code ` +
+          `works as intended, and fix where necessary.`,
+      );
+    }
+  };
+}
