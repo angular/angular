d30cef290d0d3b480dbe05a3124cc1ce355728b8
ChinoUkaegbu <77782533+ChinoUkaegbu@users.noreply.github.com>
2024-09-19 15:39:13 +0100

    docs(core): update wording in ComponentDecorator (#57878) PR Close #57878

--- packages/core/src/metadata/directives.ts
+++ packages/core/src/metadata/directives.ts
@@ -401,9 +401,10 @@ export interface ComponentDecorator {
    * Unlike other directives, only one component can be instantiated for a given element in a
    * template.
    *
-   * A component must belong to an NgModule in order for it to be available
-   * to another component or application. To make it a member of an NgModule,
-   * list it in the `declarations` field of the `NgModule` metadata.
+   * Standalone components can be directly imported in any other standalone component or NgModule.
+   * NgModule based apps on the other hand require components to belong to an NgModule in
+   * order for them to be available to another component or application. To make a component a
+   * member of an NgModule, list it in the `declarations` field of the `NgModule` metadata.
    *
    * Note that, in addition to these options for configuring a directive,
    * you can control a component's runtime behavior by implementing
