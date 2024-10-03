52e9b4dd6d7a31d91e73f6527c36b849b9c4129b
Matthieu Riegler <kyro38@gmail.com>
2024-10-03 23:35:21 +0200

    docs(docs-infra): Add NgModule exports for directives.
    This information is extracted from the @NgModule Jsdoc tag.
    
    fixes #57906

--- adev/shared-docs/pipeline/api-gen/rendering/templates/tab-description.tsx
+++ adev/shared-docs/pipeline/api-gen/rendering/templates/tab-description.tsx
@@ -10,18 +10,41 @@ import {Fragment, h} from 'preact';
 import {DocEntryRenderable} from '../entities/renderables';
 import {normalizeTabUrl} from '../transforms/url-transforms';
 import {RawHtml} from './raw-html';
+import {CodeSymbol} from './code-symbols';
 
 const DESCRIPTION_TAB_NAME = 'Description';
 
 /** Component to render the description tab. */
 export function TabDescription(props: {entry: DocEntryRenderable}) {
-  if (!props.entry.htmlDescription || props.entry.htmlDescription === props.entry.shortHtmlDescription) {
-    return (<></>);
+  const exportedBy = props.entry.jsdocTags.filter((t) => t.name === 'ngModule');
+  if (
+    (!props.entry.htmlDescription ||
+      props.entry.htmlDescription === props.entry.shortHtmlDescription) &&
+    !exportedBy.length
+  ) {
+    return <></>;
   }
 
   return (
     <div data-tab={DESCRIPTION_TAB_NAME} data-tab-url={normalizeTabUrl(DESCRIPTION_TAB_NAME)}>
       <RawHtml value={props.entry.htmlDescription} />
+
+      {exportedBy.length ? (
+        <>
+          <hr />
+          <h2>Exported by</h2>
+
+          <ul>
+            {exportedBy.map((tag) => (
+              <li>
+                <CodeSymbol code={tag.comment} />
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
