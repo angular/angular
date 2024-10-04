0e4e17cd97d7a5f7ccc40405ba2103a78e1e1298
Jeremy Kescher <jeremy@kescher.at>
2024-10-04 15:15:12 +0200

    refactor(http): `HttpResponseBase.statusText` (#64176)
    Since HTTP/2, responses no longer contain a status text besides the status code, which caused our default value of 'OK' to be used in HttpErrorResponse.message.
    
    DEPRECATED: `HttpResponseBase.statusText` is deprecated
    
    PR Close #64176

--- packages/common/http/src/response.ts
+++ packages/common/http/src/response.ts
@@ -168,6 +168,8 @@ export abstract class HttpResponseBase {
    * Textual description of response status code, defaults to OK.
    *
    * Do not depend on this.
+   *
+   * @deprecated With HTTP/2 and later versions, this will incorrectly remain set to 'OK' even when the status code of a response is not 200.
    */
   readonly statusText: string;
 
@@ -405,6 +407,7 @@ export class HttpErrorResponse extends HttpResponseBase implements Error {
     if (this.status >= 200 && this.status < 300) {
       this.message = `Http failure during parsing for ${init.url || '(unknown url)'}`;
     } else {
+      // TODO: Cleanup G3 to update the tests that rely on having the status text in the Error message.
       this.message = `Http failure response for ${init.url || '(unknown url)'}: ${init.status} ${
         init.statusText
       }`;
