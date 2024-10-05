66c39804db7d518f3cea4373b721259a3f8f52bb
Matthieu Riegler <kyro38@gmail.com>
2024-10-05 18:45:59 +0200

    refactor(core): drop `ViewRefTracker` in favor of `ApplicationRef`. (#58096)
    We can leverage an `import type` to prevent the circular import.
    
    PR Close #58096

--- packages/core/src/linker/view_ref.ts
+++ packages/core/src/linker/view_ref.ts
@@ -100,13 +100,3 @@ export abstract class EmbeddedViewRef<C> extends ViewRef {
    */
   abstract get rootNodes(): any[];
 }
-
-/**
- * Interface for tracking root `ViewRef`s in `ApplicationRef`.
- *
- * NOTE: Importing `ApplicationRef` here directly creates circular dependency, which is why we have
- * a subset of the `ApplicationRef` interface `ViewRefTracker` here.
- */
-export interface ViewRefTracker {
-  detachView(viewRef: ViewRef): void;
-}
