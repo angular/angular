b4de7acbf5d4aef8b8599619fcc6d9a06d7f0c73
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-14 13:39:24 +0000

    refactor(migrations): improve heuristic of untyped `.componentInstance` in signal migrations (#58191)
    We currently extend `.componentInstance` accesses (which are `any` for
    `DebugElement`). We are doing this for `By.directive(T)` invocations,
    but can expand this for other common expressions like with harnesses.
    
    E.g.
    
    `harness.query(T).componentInstance`.
    
    PR Close #58191

--- packages/core/schematics/migrations/signal-migration/test/golden-test/any_test.ts
+++ packages/core/schematics/migrations/signal-migration/test/golden-test/any_test.ts
@@ -1,13 +1,19 @@
 // tslint:disable
 
-import {Input} from '@angular/core';
+import {DebugElement, Input} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
 import {By} from '@angular/platform-browser';
 
 function it(msg: string, fn: () => void) {}
+const harness = {
+  query<T>(v: T): DebugElement {
+    return null!;
+  },
+};
 
 class SubDir {
   @Input() name = 'John';
+  @Input() name2 = '';
 }
 
 class MyComp {
@@ -20,4 +26,12 @@ it('should work', () => {
   const sub = fixture.debugElement.query(By.directive(SubDir)).componentInstance;
 
   expect(sub.name).toBe('John');
+});
+
+it('should work2', () => {
+  const fixture = TestBed.createComponent(MyComp);
+  // `.componentInstance` is using `any` :O
+  const sub = harness.query(SubDir).componentInstance;
+
+  expect(sub.name2).toBe('John');
 });
