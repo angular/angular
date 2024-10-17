a67f8fb4e7147086af0bdc947c7d2b15ab5424da
Matthieu Riegler <kyro38@gmail.com>
2024-10-16 21:37:45 -0600

    refactor(core): remove `standalone: true` from integration tests
    We standalone by default, we can remove them !

--- integration/cli-hello-world-lazy/src/app/app.component.ts
+++ integration/cli-hello-world-lazy/src/app/app.component.ts
@@ -4,7 +4,6 @@ import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
 })
--- integration/platform-server/projects/standalone/src/app/app.component.ts
+++ integration/platform-server/projects/standalone/src/app/app.component.ts
@@ -4,7 +4,6 @@ import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: '<router-outlet></router-outlet>',
 })
