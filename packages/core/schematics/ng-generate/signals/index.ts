4ef032e11fe4c38f5043b52aee25167f3d78d589
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-10-18 10:39:59 +0200

    feat(migrations): add a combined migration for all signals APIs
    Adds a combined `@angular/core:signals` migration that combines all of the signals-related migrations into one for the apps that want to do it all in one go.
    
    All of the heavy-lifting was already done by the individual migrations, these changes only chain them together for a more convenient developer experience.

---
+++ packages/core/schematics/ng-generate/signals/index.ts
@@ -0,0 +1,48 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import {chain, Rule, SchematicsException} from '@angular-devkit/schematics';
+import {migrate as toSignalQueries} from '../signal-queries-migration';
+import {migrate as toSignalInputs} from '../signal-input-migration';
+
+const enum SupportedMigrations {
+  inputs = 'inputs',
+  queries = 'queries',
+}
+
+interface Options {
+  path: string;
+  migrations: SupportedMigrations[];
+  analysisDir: string;
+  bestEffortMode?: boolean;
+  insertTodos?: boolean;
+}
+
+export function migrate(options: Options): Rule {
+  // The migrations are independent so we can run them in any order, but we sort them here
+  // alphabetically so we get a consistent execution order in case of issue reports.
+  const migrations = options.migrations.slice().sort();
+  const rules: Rule[] = [];
+
+  for (const migration of migrations) {
+    switch (migration) {
+      case SupportedMigrations.inputs:
+        rules.push(toSignalInputs(options));
+        break;
+
+      case SupportedMigrations.queries:
+        rules.push(toSignalQueries(options));
+        break;
+
+      default:
+        throw new SchematicsException(`Unsupported migration "${migration}"`);
+    }
+  }
+
+  return chain(rules);
+}
