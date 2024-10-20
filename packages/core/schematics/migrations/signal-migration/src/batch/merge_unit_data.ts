15080d766ee8f4fd22a0dd171fa0c0f36ee4a59c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-20 08:15:28 +0000

    refactor(migrations): update migrations to combine analysis data in parallel
    This is necessary given the previous Tsurge refactorings. It should
    speed up migrations in the merge phase signficiantly and reduce memory
    pressure. In 1P, we already have workers from analyze phase anyway— so
    those can be re-used for parallel metadata merging.

--- packages/core/schematics/migrations/signal-migration/src/batch/merge_unit_data.ts
+++ packages/core/schematics/migrations/signal-migration/src/batch/merge_unit_data.ts
@@ -16,30 +16,19 @@ import {CompilationUnitData} from './unit_data';
 type InputData = {key: string; info: CompilationUnitData['knownInputs'][string]};
 
 /** Merges a list of compilation units into a combined unit. */
-export function mergeCompilationUnitData(
-  metadataFiles: CompilationUnitData[],
+export function combineCompilationUnitData(
+  unitA: CompilationUnitData,
+  unitB: CompilationUnitData,
 ): CompilationUnitData {
   const result: CompilationUnitData = {
     knownInputs: {},
   };
 
-  const idToGraphNode = new Map<string, GraphNode<InputData>>();
-  const inheritanceGraph: GraphNode<InputData>[] = [];
-  const isNodeIncompatible = (node: InputData) =>
-    node.info.memberIncompatibility !== null || node.info.owningClassIncompatibility !== null;
-
-  for (const file of metadataFiles) {
+  for (const file of [unitA, unitB]) {
     for (const [key, info] of Object.entries(file.knownInputs)) {
       const existing = result.knownInputs[key];
       if (existing === undefined) {
         result.knownInputs[key] = info;
-        const node: GraphNode<InputData> = {
-          incoming: new Set(),
-          outgoing: new Set(),
-          data: {info, key},
-        };
-        inheritanceGraph.push(node);
-        idToGraphNode.set(key, node);
         continue;
       }
 
@@ -77,7 +66,37 @@ export function mergeCompilationUnitData(
     }
   }
 
-  for (const [key, info] of Object.entries(result.knownInputs)) {
+  return result;
+}
+
+export function convertToGlobalMeta(combinedData: CompilationUnitData): CompilationUnitData {
+  const globalMeta: CompilationUnitData = {
+    knownInputs: {},
+  };
+
+  const idToGraphNode = new Map<string, GraphNode<InputData>>();
+  const inheritanceGraph: GraphNode<InputData>[] = [];
+  const isNodeIncompatible = (node: InputData) =>
+    node.info.memberIncompatibility !== null || node.info.owningClassIncompatibility !== null;
+
+  for (const [key, info] of Object.entries(combinedData.knownInputs)) {
+    const existing = globalMeta.knownInputs[key];
+    if (existing !== undefined) {
+      continue;
+    }
+
+    const node: GraphNode<InputData> = {
+      incoming: new Set(),
+      outgoing: new Set(),
+      data: {info, key},
+    };
+    inheritanceGraph.push(node);
+    idToGraphNode.set(key, node);
+
+    globalMeta.knownInputs[key] = info;
+  }
+
+  for (const [key, info] of Object.entries(globalMeta.knownInputs)) {
     if (info.extendsFrom !== null) {
       const from = idToGraphNode.get(key)!;
       const target = idToGraphNode.get(info.extendsFrom)!;
@@ -109,7 +128,7 @@ export function mergeCompilationUnitData(
     }
   }
 
-  for (const info of Object.values(result.knownInputs)) {
+  for (const info of Object.values(combinedData.knownInputs)) {
     // We never saw a source file for this input, globally. Try marking it as incompatible,
     // so that all references and inheritance checks can propagate accordingly.
     if (!info.seenAsSourceInput) {
@@ -125,5 +144,5 @@ export function mergeCompilationUnitData(
     }
   }
 
-  return result;
+  return globalMeta;
 }
