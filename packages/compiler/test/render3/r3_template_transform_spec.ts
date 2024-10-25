98804fd4beb6292f5a50ce728424fdb33c47f654
Brandon Roberts <robertsbt@gmail.com>
2024-10-25 09:18:38 -0500

    fix(compiler): add more specific matcher for hydrate never block (#58360)
    Fixes an issue where additional characters were allowed afte the "hydrate never" block.
    
    Closes #58358
    
    PR Close #58360

--- packages/compiler/test/render3/r3_template_transform_spec.ts
+++ packages/compiler/test/render3/r3_template_transform_spec.ts
@@ -1419,6 +1419,32 @@ describe('R3 template transform', () => {
         );
       });
 
+      it('should report `hydrate never` used with additonal characters', () => {
+        expect(() => parse('@defer (hydrate never, and thank you) {hello}')).toThrowError(
+          /Unrecognized trigger/,
+        );
+      });
+
+      it('should not report an error when `hydrate never` is used with additonal blocks', () => {
+        expect(() => parse('@defer (hydrate never; on idle;) {hello}')).not.toThrowError(
+          /Unrecognized trigger/,
+        );
+      });
+
+      it('should not report an error when `hydrate never` is used with spaces', () => {
+        expect(() => parse('@defer(hydrate never ; on idle ;) {hello}')).not.toThrowError(
+          /Unrecognized trigger/,
+        );
+      });
+
+      it('should not report an error when `hydrate never` is used after another block', () => {
+        expect(() =>
+          parse(`@defer(
+        on idle;
+        hydrate never) {hello}`),
+        ).not.toThrowError(/Unrecognized trigger/);
+      });
+
       it('should report when `hydrate never` is used together with another `hydrate` trigger', () => {
         // Extra trigger after `hydrate never`.
         expect(() =>
