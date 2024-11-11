c77b53be110031cd31a0dea8322ad395288994b0
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 13:22:26 -0500

    refactor(core): reorganize defer codebase (#58598)
    This moves all the helpers out of the instructions file, keeping the instructions limited to the actual instruction set. This adds files for defer block rendering functions and triggering functions, respectively.
    
    PR Close #58598

--- packages/core/src/render3/index.ts
+++ packages/core/src/render3/index.ts
@@ -158,7 +158,6 @@ export {
   ɵɵdeferHydrateOnHover,
   ɵɵdeferHydrateOnInteraction,
   ɵɵdeferHydrateOnViewport,
-  ɵɵdeferEnableTimerScheduling,
   ɵɵtext,
   ɵɵtextInterpolate,
   ɵɵtextInterpolate1,
@@ -182,9 +181,10 @@ export {
   ɵɵreadContextLet,
 } from './instructions/all';
 export {
+  ɵɵdeferEnableTimerScheduling,
   DEFER_BLOCK_DEPENDENCY_INTERCEPTOR as ɵDEFER_BLOCK_DEPENDENCY_INTERCEPTOR,
   DEFER_BLOCK_CONFIG as ɵDEFER_BLOCK_CONFIG,
-} from '../defer/instructions';
+} from '../defer/rendering';
 export {DeferBlockDependencyInterceptor as ɵDeferBlockDependencyInterceptor} from '../defer/interfaces';
 export {
   ɵɵi18n,
