e467e797c27c906cb89df7d1526a5db9db4625c6
Matthieu Riegler <kyro38@gmail.com>
2024-11-25 20:31:14 +0100

    docs(docs-infra): remove unused `api-examples`

--- adev/src/content/api-examples/core/testability/ts/whenStable/module.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.dev/license
- */
-export {AppModule, StableTestCmp as AppComponent} from './testability_example';
