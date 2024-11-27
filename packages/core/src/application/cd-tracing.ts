536f18d4e58f866beace9607aa35dae9fb0c7c08
Matthieu Riegler <kyro38@gmail.com>
2024-11-27 02:49:33 +0100

    fixup! feat(core): add change detection tracing service

--- packages/core/src/application/cd-tracing.ts
+++ packages/core/src/application/cd-tracing.ts
@@ -8,7 +8,6 @@
 
 import {Console} from '../console';
 import {inject, InjectionToken, Provider} from '../di';
-import {NgZone} from '../zone';
 import {TracingAction, TracingService, TracingSnapshot} from './tracing';
 
 interface CDTracingOptions {
@@ -16,48 +15,77 @@ interface CDTracingOptions {
   cdThreshold: number;
 }
 
-const CD_TRACING_OPTIONS = new InjectionToken<CDTracingOptions>('CD_TRACING_OPTIONS');
+const CD_TRACING_OPTIONS = new InjectionToken<CDTracingOptions>(
+  ngDevMode ? 'CD_TRACING_OPTIONS' : '',
+);
 
 /**
  * A change detection tracing service that logs a warning if the number of change detections
  * exceeds a threshold. This is useful for debugging performance issues.
  */
 class CDTracingService implements TracingService<TracingSnapshot> {
-  private ngZone = inject(NgZone);
   private console = inject(Console);
   private options = inject(CD_TRACING_OPTIONS);
 
-  private cds: {timestamp: number}[] = [];
+  private cdList: {timestamp: number}[] = [];
+  private readonly listMaxSize = this.options.cdThreshold * 3;
+  private warningTimeout: ReturnType<typeof setTimeout> | null = null;
 
   constructor() {
-    this.ngZone.runOutsideAngular(() => {
-      setInterval(() => {
-        if (this.cds.length > this.options.cdThreshold) {
-          this.console.warn(
-            `You have [${this.cds.length}] change detections in [${this.options.msThresholdWindow / 1000} seconds]!`,
-          );
-          this.cds = [];
-        }
-      }, this.options.msThresholdWindow);
-    });
+    if (!ngDevMode) {
+      this.console.warn(
+        `Warning: You're running the change detection tracing service in production mode!`,
+      );
+    }
   }
 
   snapshot(linkedSnapshot: TracingSnapshot | null): TracingSnapshot {
     return {
       run: (action: TracingAction, fn: () => any) => {
         if (action === TracingAction.CHANGE_DETECTION) {
-          this.cds.push({
-            timestamp: Date.now(),
-          });
+          this.addTimestamp();
         }
         return fn();
       },
     };
   }
+
+  private addTimestamp() {
+    const now = Date.now();
+
+    // removing timestamps that are older than the threshold window
+    while (this.cdList[0] && this.cdList[0].timestamp + this.options.msThresholdWindow < now) {
+      this.cdList.shift();
+    }
+
+    this.cdList.push({timestamp: now});
+
+    // We're over the threshold but still within the window. Schedule a warning if not already
+    if (this.cdList.length > this.options.cdThreshold && this.warningTimeout === null) {
+      const timeRemaining = this.options.msThresholdWindow - (now - this.cdList[0].timestamp);
+      this.warningTimeout = setTimeout(() => this.checkCdCount(), timeRemaining);
+    }
+  }
+
+  private checkCdCount() {
+    this.warningTimeout = null;
+
+    const count =
+      this.cdList.length === this.listMaxSize ? `${this.listMaxSize}+` : `${this.cdList.length}`;
+    this.console.warn(
+      `You had [${count}] change detections in [${this.options.msThresholdWindow / 1000} seconds]!`,
+    );
+
+    // Reset the list to start tracking a new window.
+    this.cdList.length = 0;
+  }
 }
 
 /**
  * Configures the change detection tracing service.
+ *
+ * The change detection tracing service logs a warning if the number of change detections
+ * exceeds a threshold. This is useful for debugging performance issues.
  *
  * @usageNotes
  *
@@ -77,7 +105,7 @@ export function provideChangeDetectionTracingService(
     msThresholdWindow: 1000,
     cdThreshold: 10,
   },
-) {
+): Provider[] {
   return [
     {
       provide: CD_TRACING_OPTIONS,
@@ -90,5 +118,5 @@ export function provideChangeDetectionTracingService(
       provide: TracingService,
       useClass: CDTracingService,
     },
-  ] as Provider[];
+  ];
 }
