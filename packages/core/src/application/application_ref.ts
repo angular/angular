82df85e01e378a7236a20bdaf936138d3675ff4c
Matthieu Riegler <kyro38@gmail.com>
2024-11-30 16:54:54 +0100

    refactor(core): Instantiate the ErrorHandler lazily.
    This change prevents that user code in a custom `ErrorHandler` to run as part of `ApplicationRef`'s creation.
    
    fixes #58970

--- packages/core/src/application/application_ref.ts
+++ packages/core/src/application/application_ref.ts
@@ -281,7 +281,7 @@ export class ApplicationRef {
   private _destroyListeners: Array<() => void> = [];
   /** @internal */
   _views: InternalViewRef<unknown>[] = [];
-  private readonly internalErrorHandler = inject(INTERNAL_APPLICATION_ERROR_HANDLER);
+  private internalErrorHandler: ((error: any) => void) | undefined;
   private readonly afterRenderManager = inject(AfterRenderManager);
   private readonly zonelessEnabled = inject(ZONELESS_ENABLED);
   private readonly rootEffectScheduler = inject(EffectScheduler);
@@ -754,6 +754,7 @@ export class ApplicationRef {
     try {
       this.tick();
     } catch (e) {
+      this.internalErrorHandler ??= this._injector.get(INTERNAL_APPLICATION_ERROR_HANDLER);
       this.internalErrorHandler(e);
     }
     this.components.push(componentRef);
