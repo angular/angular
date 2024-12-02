8b468421bd6de5c6a80c9282435cdde93eb6a9e4
hawkgs <hawk.dvp@gmail.com>
2024-12-02 13:13:30 +0200

    refactor(docs-infra): update comments (#59004)
    Add missing file header comments and update others.
    
    PR Close #59004

--- packages/examples/injection-token/src/main.ts
+++ packages/examples/injection-token/src/main.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
 // TODO: Add unit tests for this file.
 /* eslint-disable @angular-eslint/no-output-native */
 // #docregion
