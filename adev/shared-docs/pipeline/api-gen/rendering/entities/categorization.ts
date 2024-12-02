d0ea622040efdfb4277c19365178a3f438d8aff0
Matthieu Riegler <kyro38@gmail.com>
2024-12-02 03:40:58 +0100

    docs(docs-infra): read jsdoctags from function overloads (#58994)
    Functions like `linkedSignal` have there `@developerPreview` tags on the overload signature. This commit adds the support for them.
    
    This commit also removes the logic for multiple entries, as now overloads are a single entry.
    
    fixes #58817
    
    PR Close #58994

--- adev/shared-docs/pipeline/api-gen/rendering/entities/categorization.ts
+++ adev/shared-docs/pipeline/api-gen/rendering/entities/categorization.ts
@@ -8,6 +8,7 @@ import {
   FunctionEntry,
   InitializerApiFunctionEntry,
   InterfaceEntry,
+  JsDocTagEntry,
   MemberEntry,
   MemberType,
   MethodEntry,
@@ -125,7 +126,7 @@ export function isSetterEntry(entry: MemberEntry): entry is PropertyEntry {
 
 /** Gets whether the given entry is deprecated. */
 export function isDeprecatedEntry<T extends HasJsDocTags>(entry: T) {
-  return entry.jsdocTags.some((tag) => tag.name === 'deprecated');
+  return hasTag(entry, 'deprecated', /* every */ true);
 }
 
 export function getDeprecatedEntry<T extends HasJsDocTags>(entry: T) {
@@ -133,13 +134,30 @@ export function getDeprecatedEntry<T extends HasJsDocTags>(entry: T) {
 }
 
 /** Gets whether the given entry is developer preview. */
-export function isDeveloperPreview<T extends HasJsDocTags>(entry: T) {
-  return entry.jsdocTags.some((tag) => tag.name === 'developerPreview');
+export function isDeveloperPreview<T extends HasJsDocTags | FunctionEntry>(entry: T) {
+  return hasTag(entry, 'developerPreview', false);
 }
 
 /** Gets whether the given entry is is experimental. */
 export function isExperimental<T extends HasJsDocTags>(entry: T) {
-  return entry.jsdocTags.some((tag) => tag.name === 'experimental');
+  return hasTag(entry, 'experimental');
+}
+/** Gets whether the given entry has a given JsDoc tag. */
+function hasTag<T extends HasJsDocTags | FunctionEntry>(entry: T, tag: string, every = false) {
+  const hasTagName = (t: JsDocTagEntry) => t.name === tag;
+
+  if (every && 'signatures' in entry && entry.signatures.length > 1) {
+    // For overloads we need to check all signatures.
+    return entry.signatures.every((s) => s.jsdocTags.some(hasTagName));
+  }
+
+  const jsdocTags = [
+    ...entry.jsdocTags,
+    ...((entry as FunctionEntry).signatures?.flatMap((s) => s.jsdocTags) ?? []),
+    ...((entry as FunctionEntry).implementation?.jsdocTags ?? []),
+  ];
+
+  return jsdocTags.some(hasTagName);
 }
 
 /** Gets whether the given entry is a cli entry. */
