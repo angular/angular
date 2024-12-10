c1cc8015a01f3a6ceb4808710a4b9562540e437a
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-12-10 16:53:18 +0100

    refactor(core): allow tracing service to wrap event listeners (#59134)
    Allows the tracing service to wrap event listeners bound by the framework.
    
    PR Close #59134

--- packages/core/test/acceptance/tracing_spec.ts
+++ packages/core/test/acceptance/tracing_spec.ts
@@ -17,11 +17,15 @@ import {fakeAsync, TestBed} from '@angular/core/testing';
 
 describe('TracingService', () => {
   let actions: TracingAction[];
+  let listeners: {event: string; handler: Function}[];
   let fakeSnapshot: TracingSnapshot;
   let mockTracingService: TracingService<TracingSnapshot>;
+  let clickCount: number;
 
   beforeEach(() => {
     actions = [];
+    listeners = [];
+    clickCount = 0;
     fakeSnapshot = {
       run: function <T>(action: TracingAction, fn: () => T): T {
         actions.push(action);
@@ -31,6 +35,19 @@ describe('TracingService', () => {
     };
     mockTracingService = {
       snapshot: jasmine.createSpy('snapshot').and.returnValue(fakeSnapshot),
+      wrapEventListener: jasmine
+        .createSpy('wrapEventListener')
+        .and.callFake((_element, event: string, handler: Function) => {
+          if (event === 'click') {
+            const originalHandler = handler;
+            handler = function (this: unknown) {
+              clickCount++;
+              originalHandler.apply(this, arguments);
+            };
+          }
+          listeners.push({event, handler});
+          return handler;
+        }),
     };
   });
 
@@ -71,5 +88,30 @@ describe('TracingService', () => {
     TestBed.createComponent(App);
     expect(mockTracingService.snapshot).toHaveBeenCalledTimes(2);
     expect(actions).toEqual([TracingAction.CHANGE_DETECTION, TracingAction.AFTER_NEXT_RENDER]);
+  }));
+
+  it('should be able to wrap event listeners through the tracing service', fakeAsync(() => {
+    TestBed.configureTestingModule({
+      providers: [{provide: TracingService, useValue: mockTracingService}],
+    });
+
+    @Component({template: '<button (click)="noop()"></button> <span (mousedown)="noop()"></span>'})
+    class App {
+      noop() {}
+    }
+
+    const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
+
+    expect(listeners).toEqual([
+      {event: 'click', handler: jasmine.any(Function)},
+      {event: 'mousedown', handler: jasmine.any(Function)},
+    ]);
+    expect(clickCount).toBe(0);
+
+    fixture.nativeElement.querySelector('button').click();
+    fixture.detectChanges();
+
+    expect(clickCount).toBe(1);
   }));
 });
