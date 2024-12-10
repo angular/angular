24e317cb157bf1ef159ed8554f1b79cb3443edf4
Alan Agius <alanagius@google.com>
2024-12-10 16:02:20 +0000

    refactor: replace `ɵPendingTasks` with `ɵPendingTasksInternal` (#59138)
    This commits remove usage of the old export.
    
    PR Close #59138

--- packages/core/test/defer_fixture_spec.ts
+++ packages/core/test/defer_fixture_spec.ts
@@ -7,7 +7,7 @@
  */
 
 import {ɵPLATFORM_BROWSER_ID as PLATFORM_BROWSER_ID} from '@angular/common';
-import {Component, PLATFORM_ID, ɵPendingTasks as PendingTasks} from '@angular/core';
+import {Component, PLATFORM_ID, ɵPendingTasksInternal as PendingTasks} from '@angular/core';
 import {DeferBlockBehavior, DeferBlockState, TestBed} from '@angular/core/testing';
 import {expect} from '@angular/platform-browser/testing/src/matchers';
 
