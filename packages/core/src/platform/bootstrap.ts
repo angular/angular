c2c8b75d7b259dfd48fc215c757dd37d8983b50b
Matthieu Riegler <kyro38@gmail.com>
2024-12-16 18:42:24 +0100

    refactor(core): remove module bootstrap code when using standalone. (#59208)
    This commit improves tree shaking in standalone apps.
    
    PR Close #59208

--- packages/core/src/platform/bootstrap.ts
+++ packages/core/src/platform/bootstrap.ts
@@ -162,7 +162,7 @@ export function bootstrap<M>(
           }
           return appRef;
         } else {
-          moduleDoBootstrap(config.moduleRef, config.allPlatformModules);
+          moduleBootstrapImpl?.(config.moduleRef, config.allPlatformModules);
           return config.moduleRef;
         }
       });
@@ -170,7 +170,20 @@ export function bootstrap<M>(
   });
 }
 
-function moduleDoBootstrap(
+/**
+ * Having a separate symbol for the module boostrap implementation allows us to
+ * tree shake the module based boostrap implementation in standalone apps.
+ */
+let moduleBootstrapImpl: undefined | typeof _moduleDoBootstrap;
+
+/**
+ * Set the implementation of the module based bootstrap.
+ */
+export function setModuleBootstrapImpl() {
+  moduleBootstrapImpl = _moduleDoBootstrap;
+}
+
+function _moduleDoBootstrap(
   moduleRef: InternalNgModuleRef<any>,
   allPlatformModules: NgModuleRef<unknown>[],
 ): void {
