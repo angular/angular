8c5db3cfb75700dd64f4c8c073554c7086835950
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-12-20 11:53:29 +0200

    fix(platform-browser): avoid circular DI error in async renderer (#59256)
    In https://github.com/angular/components/pull/30179 the CDK overlay started depending on the `Renderer2Factory`. Since the overlay is used in the `MatSnackbar` which is commonly used in error handlers, `Overlay` can end up being injected as a part of the app initialization. Because `AsyncAnimationRendererFactory` depends on the `ChangeDetectionScheduler`, it may cause a circular dependency.
    
    These changes inject the `ChangeDetectionScheduler` lazily to avoid the error.
    
    Note: this will also be resolved by #58984, but I decided to send it out, because:
    1. #58984 seems to be stuck on some internal cleanup.
    2. The `AsyncAnimationRendererFactory` doesn't need the `scheduler` eagerly anyway so the change is fairly safe.
    
    Fixes #59255.
    
    PR Close #59256

--- packages/platform-browser/animations/async/src/async_animation_renderer.ts
+++ packages/platform-browser/animations/async/src/async_animation_renderer.ts
@@ -26,6 +26,7 @@ import {
   ɵRuntimeError as RuntimeError,
   InjectionToken,
   type ListenerOptions,
+  Injector,
 } from '@angular/core';
 import {ɵRuntimeErrorCode as RuntimeErrorCode} from '@angular/platform-browser';
 
@@ -34,7 +35,8 @@ const ANIMATION_PREFIX = '@';
 @Injectable()
 export class AsyncAnimationRendererFactory implements OnDestroy, RendererFactory2 {
   private _rendererFactoryPromise: Promise<AnimationRendererFactory> | null = null;
-  private readonly scheduler = inject(ChangeDetectionScheduler, {optional: true});
+  private scheduler: ChangeDetectionScheduler | null = null;
+  private readonly injector = inject(Injector);
   private readonly loadingSchedulerFn = inject(ɵASYNC_ANIMATION_LOADING_SCHEDULER_FN, {
     optional: true,
   });
@@ -143,6 +145,7 @@ export class AsyncAnimationRendererFactory implements OnDestroy, RendererFactory
           rendererType,
         );
         dynamicRenderer.use(animationRenderer);
+        this.scheduler ??= this.injector.get(ChangeDetectionScheduler, null, {optional: true});
         this.scheduler?.notify(NotificationSource.AsyncAnimationsLoaded);
       })
       .catch((e) => {
