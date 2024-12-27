17fb20f85db9f3c172c194c0436644f34b7176b1
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-12-27 10:04:45 +0200

    fix(compiler-cli): preserve defer block dependencies during HMR when class metadata is disabled (#59313)
    Fixes that the compiler wasn't capturing defer block dependencies correctly when `supportTestBed` is disabled. We had tests for this, but we didn't notice the issue because the dependencies ended up being captured because of the `setClassMetadata` calls. Once they're disabled, the dependencies stopped being recorded.
    
    Fixes #59310.
    
    PR Close #59313

--- packages/compiler-cli/src/ngtsc/hmr/src/extract_dependencies.ts
+++ packages/compiler-cli/src/ngtsc/hmr/src/extract_dependencies.ts
@@ -6,7 +6,13 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {R3CompiledExpression, R3HmrNamespaceDependency, outputAst as o} from '@angular/compiler';
+import {
+  DeferBlockDepsEmitMode,
+  R3CompiledExpression,
+  R3ComponentDeferMetadata,
+  R3HmrNamespaceDependency,
+  outputAst as o,
+} from '@angular/compiler';
 import {DeclarationNode} from '../../reflection';
 import {CompileResult} from '../../transform';
 import ts from 'typescript';
@@ -16,6 +22,7 @@ import ts from 'typescript';
  * @param sourceFile File in which the file is being compiled.
  * @param definition Compiled component definition.
  * @param factory Compiled component factory.
+ * @param deferBlockMetadata Metadata about the defer blocks in the component.
  * @param classMetadata Compiled `setClassMetadata` expression, if any.
  * @param debugInfo Compiled `setClassDebugInfo` expression, if any.
  */
@@ -23,6 +30,7 @@ export function extractHmrDependencies(
   node: DeclarationNode,
   definition: R3CompiledExpression,
   factory: CompileResult,
+  deferBlockMetadata: R3ComponentDeferMetadata,
   classMetadata: o.Statement | null,
   debugInfo: o.Statement | null,
 ): {local: string[]; external: R3HmrNamespaceDependency[]} {
@@ -30,7 +38,7 @@ export function extractHmrDependencies(
   const visitor = new PotentialTopLevelReadsVisitor();
   const sourceFile = node.getSourceFile();
 
-  // Visit all of the compiled expression to look for potential
+  // Visit all of the compiled expressions to look for potential
   // local references that would have to be retained.
   definition.expression.visitExpression(visitor, null);
   definition.statements.forEach((statement) => statement.visitStatement(visitor, null));
@@ -38,6 +46,12 @@ export function extractHmrDependencies(
   factory.statements.forEach((statement) => statement.visitStatement(visitor, null));
   classMetadata?.visitStatement(visitor, null);
   debugInfo?.visitStatement(visitor, null);
+
+  if (deferBlockMetadata.mode === DeferBlockDepsEmitMode.PerBlock) {
+    deferBlockMetadata.blocks.forEach((loader) => loader?.visitExpression(visitor, null));
+  } else {
+    deferBlockMetadata.dependenciesFn?.visitExpression(visitor, null);
+  }
 
   // Filter out only the references to defined top-level symbols. This allows us to ignore local
   // variables inside of functions. Note that we filter out the class name since it is always
