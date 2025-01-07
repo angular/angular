d4b3f6182f645a781f7465fc154d6ab7ded8e97d
arturovt <arthurandrosovich@gmail.com>
2025-01-08 00:50:52 +0200

    refactor(common): tree-shake fetch backend
    This commit updates the code of the HTTP code to make the `FetchBackend` class tree-shakable. The class is only needed with `withFetch()` is called and it should not be included into bundles that do not use that feature.

--- packages/common/http/src/provider.ts
+++ packages/common/http/src/provider.ts
@@ -16,7 +16,7 @@ import {
 
 import {HttpBackend, HttpHandler} from './backend';
 import {HttpClient} from './client';
-import {FetchBackend} from './fetch';
+import {FETCH_BACKEND, FetchBackend} from './fetch';
 import {
   HTTP_INTERCEPTOR_FNS,
   HttpInterceptorFn,
@@ -128,7 +128,7 @@ export function provideHttpClient(
     {
       provide: HttpBackend,
       useFactory: () => {
-        return inject(FetchBackend, {optional: true}) ?? inject(HttpXhrBackend);
+        return inject(FETCH_BACKEND, {optional: true}) ?? inject(HttpXhrBackend);
       },
     },
     {
@@ -305,6 +305,7 @@ export function withRequestsMadeViaParent(): HttpFeature<HttpFeatureKind.Request
 export function withFetch(): HttpFeature<HttpFeatureKind.Fetch> {
   return makeHttpFeature(HttpFeatureKind.Fetch, [
     FetchBackend,
+    {provide: FETCH_BACKEND, useExisting: FetchBackend},
     {provide: HttpBackend, useExisting: FetchBackend},
   ]);
 }
