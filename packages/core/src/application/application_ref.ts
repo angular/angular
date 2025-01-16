6789c7ef947952551d7598fe37a3d86093b75720
Miles Malerba <mmalerba@google.com>
2025-01-16 03:14:07 +0000

    fix(core): Defer afterRender until after first CD (#59455) (#59551)
    This reverts commit ac2dbe3eb1205ce0683c69c801c1eeb2b5adfc1b.
    
    PR Close #59551

--- packages/core/src/application/application_ref.ts
+++ packages/core/src/application/application_ref.ts
@@ -300,13 +300,6 @@ export class ApplicationRef {
   dirtyFlags = ApplicationRefDirtyFlags.None;
 
   /**
-   * Like `dirtyFlags` but don't cause `tick()` to loop.
-   *
-   * @internal
-   */
-  deferredDirtyFlags = ApplicationRefDirtyFlags.None;
-
-  /**
    * Most recent snapshot from the `TracingService`, if any.
    *
    * This snapshot attempts to capture the context when `tick()` was first
@@ -634,10 +627,6 @@ export class ApplicationRef {
       this._rendererFactory = this._injector.get(RendererFactory2, null, {optional: true});
     }
 
-    // When beginning synchronization, all deferred dirtiness becomes active dirtiness.
-    this.dirtyFlags |= this.deferredDirtyFlags;
-    this.deferredDirtyFlags = ApplicationRefDirtyFlags.None;
-
     let runs = 0;
     while (this.dirtyFlags !== ApplicationRefDirtyFlags.None && runs++ < MAXIMUM_REFRESH_RERUNS) {
       profiler(ProfilerEvent.ChangeDetectionSyncStart);
@@ -660,10 +649,6 @@ export class ApplicationRef {
    * Perform a single synchronization pass.
    */
   private synchronizeOnce(): void {
-    // If we happened to loop, deferred dirtiness can be processed as active dirtiness again.
-    this.dirtyFlags |= this.deferredDirtyFlags;
-    this.deferredDirtyFlags = ApplicationRefDirtyFlags.None;
-
     // First, process any dirty root effects.
     if (this.dirtyFlags & ApplicationRefDirtyFlags.RootEffects) {
       this.dirtyFlags &= ~ApplicationRefDirtyFlags.RootEffects;
