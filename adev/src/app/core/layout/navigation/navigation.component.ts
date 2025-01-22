0c46d0f3fec85f5e893ed7391b9893d8a9f1f047
Jens Kuehlers <kuehlers@google.com>
2025-01-22 12:41:39 +0000

    docs: add Bluesky links

--- adev/src/app/core/layout/navigation/navigation.component.ts
+++ adev/src/app/core/layout/navigation/navigation.component.ts
@@ -28,7 +28,7 @@ import {
 import {NavigationEnd, Router, RouterLink} from '@angular/router';
 import {filter, map, startWith} from 'rxjs/operators';
 import {DOCS_ROUTES, REFERENCE_ROUTES, TUTORIALS_ROUTES} from '../../../routes';
-import {GITHUB, MEDIUM, X, YOUTUBE, DISCORD} from '../../constants/links';
+import {GITHUB, MEDIUM, X, YOUTUBE, DISCORD, BLUESKY} from '../../constants/links';
 import {PagePrefix} from '../../enums/pages';
 import {Theme, ThemeManager} from '../../services/theme-manager.service';
 import {VersionManager} from '../../services/version-manager.service';
@@ -67,6 +67,7 @@ export class Navigation implements OnInit {
   readonly MEDIUM = MEDIUM;
   readonly YOUTUBE = YOUTUBE;
   readonly DISCORD = DISCORD;
+  readonly BLUESKY = BLUESKY;
 
   readonly PRIMARY_NAV_ID = PRIMARY_NAV_ID;
   readonly SECONDARY_NAV_ID = SECONDARY_NAV_ID;
