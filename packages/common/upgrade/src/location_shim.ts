e9f10eb4c950692992098619b9628ecefd1b36ce
arturovt <arthurandrosovich@gmail.com>
2025-01-24 16:38:28 +0200

    fix(common): clean up `urlChanges` subscribers when root scope is destroyed (#59703)
    In this commit, the `urlChanges` subject is completed to release all active observers when the root scope is destroyed. Previously, subscribing to the `urlChanges` subject caused the subscriber to capture `this`, resulting in a memory leak after the root scope was destroyed.
    
    PR Close #59703

--- packages/common/upgrade/src/location_shim.ts
+++ packages/common/upgrade/src/location_shim.ts
@@ -179,7 +179,10 @@ export class $locationShim {
       }
     });
 
-    // update browser
+    // Synchronize the browser's URL and state with the application.
+    // Note: There is no need to save the `$watch` return value (deregister listener)
+    // into a variable because `$scope.$$watchers` is automatically cleaned up when
+    // the root scope is destroyed.
     $rootScope.$watch(() => {
       if (this.initializing || this.updateBrowser) {
         this.updateBrowser = false;
@@ -243,6 +246,14 @@ export class $locationShim {
         }
       }
       this.$$replace = false;
+    });
+
+    $rootScope.$on('$destroy', () => {
+      // Complete the subject to release all active observers when the root
+      // scope is destroyed. Before this change, we subscribed to the `urlChanges`
+      // subject, and the subscriber captured `this`, leading to a memory leak
+      // after the root scope was destroyed.
+      this.urlChanges.complete();
     });
   }
 
