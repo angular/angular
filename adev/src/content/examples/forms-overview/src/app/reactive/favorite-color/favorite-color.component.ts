5ed3bbdd299ef539781fe1b1096d2c4687597199
Iury Nogueira <nogueiraiury20@gmail.com>
2025-02-06 12:05:59 -0300

    fix: add missing import to input behavior

--- adev/src/content/examples/forms-overview/src/app/reactive/favorite-color/favorite-color.component.ts
+++ adev/src/content/examples/forms-overview/src/app/reactive/favorite-color/favorite-color.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-favorite-color',
   template: `
     Favorite Color: <input type="text" [formControl]="favoriteColorControl">
   `,
+  imports: [ReactiveFormsModule],
   standalone: false,
 })
 export class FavoriteColorComponent {
