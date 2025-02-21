dd494e22b2d83c80e375be03665cc13a1256563e
Miles Malerba <mmalerba@google.com>
2025-02-21 21:59:40 +0000

    more thinking & prototyping

--- packages/forms/experimental/test/engine/node.spec.ts
+++ packages/forms/experimental/test/engine/node.spec.ts
@@ -1,7 +1,7 @@
-import {FormNode} from '../../src/engine/node';
 import {computed, signal} from '@angular/core';
+import {FormNode} from '../../src/engine/node';
 
-describe('Node', () => {
+fdescribe('Node', () => {
   it('is untouched initially', () => {
     const value = signal({a: 1, b: 2});
     const node = new FormNode(value);
@@ -18,13 +18,25 @@ describe('Node', () => {
     expect(child.value()).toBe(1);
   });
 
-  it('should get the same instance when asking for a child multiple times', () => {
-    const value = signal({a: 1, b: 2});
-    const node = new FormNode(value);
+  describe('instances', () => {
+    it('should get the same instance when asking for a child multiple times', () => {
+      const value = signal({a: 1, b: 2});
+      const node = new FormNode(value);
 
-    const child = node.getChild('a')!;
+      const child = node.getChild('a')!;
 
-    expect(node.getChild('a')).toBe(child);
+      expect(node.getChild('a')).toBe(child);
+    });
+
+    it('should get the same instance when asking for a child multiple times', () => {
+      const value = signal<Object>({a: 1, b: 2});
+      const node = new FormNode(value);
+
+      const child = node.getChild('a')!;
+
+      value.set({a: 3});
+      expect(node.getChild('a')).toBe(child);
+    });
   });
 
   it('cannot get a child of a key that does not exist', () => {
@@ -32,6 +44,14 @@ describe('Node', () => {
     const node = new FormNode(value);
 
     expect(node.getChild('c')).toBeUndefined();
+  });
+
+  it('can get a child inside of a computed', () => {
+    const value = signal({a: 1, b: 2});
+    const node = new FormNode(value);
+    const childA = computed(() => node.getChild('a')!);
+
+    expect(childA()).toBeDefined();
   });
 
   it('can get a child inside of a computed', () => {
@@ -59,6 +79,27 @@ describe('Node', () => {
       expect(node.touched()).toBe(false);
       node.getChild('a')!.markAsTouched();
       expect(node.touched()).toBe(true);
+    });
+
+    it('does not propagate down', () => {
+      const value = signal({a: 1, b: 2});
+      const node = new FormNode(value);
+
+      expect(node.getChild('a')!.touched()).toBe(false);
+      node.markAsTouched();
+      expect(node.getChild('a')!.touched()).toBe(false);
+    });
+
+    it('does not consider children that get removed', () => {
+      const value = signal<Object>({a: 1, b: 2});
+      const node = new FormNode(value);
+
+      expect(node.touched()).toBe(false);
+      node.getChild('b')!.markAsTouched();
+      expect(node.touched()).toBe(true);
+      value.set({a: 2});
+      expect(node.touched()).toBe(false);
+      expect(node.getChild('b')).toBeUndefined();
     });
   });
 });
