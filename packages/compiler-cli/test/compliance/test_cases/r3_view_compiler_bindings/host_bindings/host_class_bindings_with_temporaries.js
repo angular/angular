7e2d5c22036fe25098e767a510f22044cb889172
Miles Malerba <mmalerba@google.com>
2025-02-22 00:59:00 +0000

    test(compiler-cli): Update tests for nullish coalescing (#60060)
    Updates tests to reflect the fact that we no loger down-level nullish
    coalescing.
    
    PR Close #60060

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_bindings/host_bindings/host_class_bindings_with_temporaries.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_bindings/host_bindings/host_class_bindings_with_temporaries.js
@@ -1,8 +1,6 @@
 hostBindings: function HostBindingDir_HostBindings(rf, ctx) {
   if (rf & 2) {
-    let $tmp_0$;
-    let $tmp_1$;
-    $r3$.ɵɵclassProp("a", ($tmp_0$ = ctx.value) !== null && $tmp_0$ !== undefined ? $tmp_0$ : "class-a")
-                    ("b", ($tmp_1$ = ctx.value) !== null && $tmp_1$ !== undefined ? $tmp_1$ : "class-b");
+    $r3$.ɵɵclassProp("a", ctx.value ?? "class-a")
+                    ("b", ctx.value ?? "class-b");
   }
 }
--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_bindings/host_bindings/host_style_bindings_with_temporaries.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_bindings/host_bindings/host_style_bindings_with_temporaries.js
@@ -1,8 +1,6 @@
 hostBindings: function HostBindingDir_HostBindings(rf, ctx) {
   if (rf & 2) {
-    let $tmp_0$;
-    let $tmp_1$;
-    $r3$.ɵɵstyleProp("font-size", ($tmp_0$ = ctx.value) !== null && $tmp_0$ !== undefined ? $tmp_0$ : "15px")
-                    ("font-weight", ($tmp_1$ = ctx.value) !== null && $tmp_1$ !== undefined ? $tmp_1$ : "bold");
+    $r3$.ɵɵstyleProp("font-size", ctx.value ?? "15px")
+                    ("font-weight", ctx.value ?? "bold");
   }
 }
