c1ddf323c9eb4f3d0cf31620378e3a3e72f3fecc
Miles Malerba <mmalerba@google.com>
2025-02-22 00:59:00 +0000

    test(compiler-cli): Update tests for nullish coalescing
    Updates tests to reflect the fact that we no loger down-level nullish
    coalescing.

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler/nullish_coalescing/nullish_coalescing_property_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler/nullish_coalescing/nullish_coalescing_property_template.js
@@ -3,10 +3,8 @@ template: function MyApp_Template(rf, ctx) {
     $r3$.ɵɵelement(0, "div", 0)(1, "span", 0);
   }
   if (rf & 2) {
-    let $tmp_0_0$;
-    let $tmp_1_0$;
-    $r3$.ɵɵproperty("title", "Hello, " + (($tmp_0_0$ = ctx.firstName) !== null && $tmp_0_0$ !== undefined ? $tmp_0_0$ : "Frodo") + "!");
+    $r3$.ɵɵproperty("title", "Hello, " + (ctx.firstName ?? "Frodo") + "!");
     $r3$.ɵɵadvance();
-    $r3$.ɵɵproperty("title", "Your last name is " + (($tmp_1_0$ = ($tmp_1_0$ = ctx.lastName) !== null && $tmp_1_0$ !== undefined ? $tmp_1_0$ : ctx.lastNameFallback) !== null && $tmp_1_0$ !== undefined ? $tmp_1_0$ : "unknown"));
+    $r3$.ɵɵproperty("title", "Your last name is " + (ctx.lastName ?? ctx.lastNameFallback ?? "unknown"));
   }
 }
