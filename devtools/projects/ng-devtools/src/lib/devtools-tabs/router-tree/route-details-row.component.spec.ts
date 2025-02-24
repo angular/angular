b6c8a4f44eedf9b6e7c8d009655e79e46718de74
Sumit Arora <er.sumitarora@gmail.com>
2025-02-24 11:11:05 -0500

    refactor(devtools): fixing PR comments and adding unit tests
    - Update code to fix PR comments and cleanup code
    - Add unit tests for the new code

---
+++ devtools/projects/ng-devtools/src/lib/devtools-tabs/router-tree/route-details-row.component.spec.ts
@@ -0,0 +1,118 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+
+import {RouteDetailsRowComponent} from './route-details-row.component';
+
+describe('RouteDetailsRowComponent', () => {
+  let component: RouteDetailsRowComponent;
+  let fixture: ComponentFixture<RouteDetailsRowComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouteDetailsRowComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(RouteDetailsRowComponent);
+    component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('label', 'Route Title');
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render a label and text data', () => {
+    fixture.componentRef.setInput('label', 'Route Title');
+    fixture.componentRef.setInput('data', 'Route Data');
+    fixture.detectChanges();
+
+    const labelElement = fixture.debugElement.query(By.css('.row-title'));
+    expect(labelElement.nativeElement.innerText).toEqual('Route Title:');
+
+    const dataElements = fixture.debugElement.queryAll(By.css('.row-data'));
+    expect(dataElements.length).toEqual(1);
+    expect(dataElements[0].nativeElement.innerText).toEqual('Route Data');
+  });
+
+  it('should render a label and flag data true', () => {
+    fixture.componentRef.setInput('label', 'Route Title');
+    fixture.componentRef.setInput('type', 'flag');
+    fixture.componentRef.setInput('data', 'true');
+    fixture.detectChanges();
+
+    const labelElement = fixture.debugElement.query(By.css('.row-title'));
+    expect(labelElement.nativeElement.innerText).toEqual('Route Title:');
+
+    const dataElements = fixture.debugElement.queryAll(By.css('.tag-active'));
+    expect(dataElements.length).toEqual(1);
+    expect(dataElements[0].nativeElement.innerText).toEqual('true');
+  });
+
+  it('should render a label and flag data false', () => {
+    fixture.componentRef.setInput('label', 'Route Title');
+    fixture.componentRef.setInput('type', 'flag');
+    fixture.componentRef.setInput('data', false);
+    fixture.detectChanges();
+
+    const labelElement = fixture.debugElement.query(By.css('.row-title'));
+    expect(labelElement.nativeElement.innerText).toEqual('Route Title:');
+
+    const dataElements = fixture.debugElement.queryAll(By.css('.tag-inactive'));
+    expect(dataElements.length).toEqual(1);
+    expect(dataElements[0].nativeElement.innerText).toEqual('false');
+  });
+
+  it('should render a label and chip data', () => {
+    fixture.componentRef.setInput('label', 'Route Title');
+    fixture.componentRef.setInput('type', 'chip');
+    fixture.componentRef.setInput('data', 'Component Name');
+    fixture.detectChanges();
+
+    const labelElement = fixture.debugElement.query(By.css('.row-title'));
+    expect(labelElement.nativeElement.innerText).toEqual('Route Title:');
+
+    const dataElements = fixture.debugElement.queryAll(By.css('.mdc-button'));
+    expect(dataElements.length).toEqual(1);
+    expect(dataElements[0].nativeElement.innerText).toEqual('Component Name');
+  });
+
+  it('should render a label and chip data disabled', () => {
+    fixture.componentRef.setInput('label', 'Route Title');
+    fixture.componentRef.setInput('type', 'chip');
+    fixture.componentRef.setInput('data', 'Lazy Component Name');
+    fixture.detectChanges();
+
+    const labelElement = fixture.debugElement.query(By.css('.row-title'));
+    expect(labelElement.nativeElement.innerText).toEqual('Route Title:');
+
+    const dataElements = fixture.debugElement.queryAll(By.css('.mdc-button'));
+    expect(dataElements.length).toEqual(1);
+    expect(dataElements[0].nativeElement.innerText).toEqual('Lazy Component Name');
+    expect(dataElements[0].nativeElement.disabled).toEqual(true);
+  });
+
+  it('should render a label and list data', () => {
+    fixture.componentRef.setInput('label', 'Route Title');
+    fixture.componentRef.setInput('type', 'list');
+    fixture.componentRef.setInput('data', ['Guard 1', 'Guard 2']);
+    fixture.detectChanges();
+
+    const labelElement = fixture.debugElement.query(By.css('.row-title'));
+    expect(labelElement.nativeElement.innerText).toEqual('Route Title:');
+
+    const dataElements = fixture.debugElement.queryAll(By.css('.mdc-button'));
+    expect(dataElements.length).toEqual(2);
+    expect(dataElements[0].nativeElement.innerText).toEqual('Guard 1');
+    expect(dataElements[1].nativeElement.innerText).toEqual('Guard 2');
+  });
+});
