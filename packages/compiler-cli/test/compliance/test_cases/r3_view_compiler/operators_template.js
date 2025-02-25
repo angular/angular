e69d0e49bf4906f85c969e973042efa6667d6b64
Miles Malerba <mmalerba@google.com>
2025-02-25 18:45:02 +0000

    fix(compiler-cli): preserve required parens in exponentiation expressions
    Parentheses are required around a unary operator used in the base of an
    exponentiation expression. For example: `(-1) ** 3`

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler/operators_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler/operators_template.js
@@ -3,16 +3,17 @@ template: function MyApp_Template(rf, $ctx$) {
 	  $i0$.ɵɵtext(0);
 	  i0.ɵɵpipe(1, "identity");
 	} if (rf & 2) {
-	  i0.ɵɵtextInterpolate8(" ", 
+	  i0.ɵɵtextInterpolateV([" ", 
 		1 + 2, " ", 
 		1 % 2 + 3 / 4 * 5 ** 6, " ", 
 		+1, " ", 
-		typeof i0.ɵɵpureFunction0(10, _c0) === "object", " ", 
-		!(typeof i0.ɵɵpureFunction0(11, _c0) === "object"), " ", 
+		typeof i0.ɵɵpureFunction0(11, _c0) === "object", " ", 
+		!(typeof i0.ɵɵpureFunction0(12, _c0) === "object"), " ", 
 		typeof (ctx.foo == null ? null : ctx.foo.bar) === "string", " ", 
-		i0.ɵɵpipeBind1(1, 8, typeof (ctx.foo == null ? null : ctx.foo.bar)), " ",
-		void "test", " "
-	  );	
+		i0.ɵɵpipeBind1(1, 9, typeof (ctx.foo == null ? null : ctx.foo.bar)), " ",
+		void "test", " ",
+		(-1) ** 3, " "
+	  ]);	
 	}
   }
   
