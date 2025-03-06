61abe5b9321531e253fe6b72a0ffc87175bd506c
Milo <mmighdoll@google.com>
2025-03-06 18:56:18 +0000

    feat(devtools): add signals graph

--- devtools/projects/protocol/src/lib/messages.ts
+++ devtools/projects/protocol/src/lib/messages.ts
@@ -16,6 +16,32 @@ import {
   ViewEncapsulation as AngularViewEncapsulation,
 } from '@angular/core';
 
+export interface DebugSignalGraphNode {
+  kind: string;
+  label?: string;
+  value?: unknown;
+}
+
+export interface DebugSignalGraphEdge {
+  /**
+   * Index of a signal node in the `nodes` array that is a consumer of the signal produced by the producer node.
+   */
+  consumer: number;
+
+  /**
+   * Index of a signal node in the `nodes` array that is a producer of the signal consumed by the consumer node.
+   */
+  producer: number;
+}
+
+/**
+ * A debug representation of the signal graph.
+ */
+export interface DebugSignalGraph {
+  nodes: DebugSignalGraphNode[];
+  edges: DebugSignalGraphEdge[];
+}
+
 export interface DirectiveType {
   name: string;
   id: number;
@@ -273,11 +299,6 @@ export interface AngularDetection {
 
 export type Topic = keyof Events;
 
-export interface InjectorGraphViewQuery {
-  directivePosition: DirectivePosition;
-  paramIndex: number;
-}
-
 export interface Events {
   handshake: () => void;
   shutdown: () => void;
@@ -291,6 +312,9 @@ export interface Events {
 
   inspectorStart: () => void;
   inspectorEnd: () => void;
+
+  getSignalGraph: (query: any) => void;
+  latestSignalGraph: (graph: DebugSignalGraph) => void;
 
   getNestedProperties: (position: DirectivePosition, path: string[]) => void;
   nestedProperties: (position: DirectivePosition, data: Properties, path: string[]) => void;
