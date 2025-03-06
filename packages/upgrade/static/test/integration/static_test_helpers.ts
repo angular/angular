acb3d51671ac29cc5d157756e32fc17d54167378
Andrew Kushnir <akushnir@google.com>
2025-03-05 20:43:10 -0800

    refactor(upgrade): convert scripts within `packages/upgrade` to relative imports
    This commit updates scripts within `packages/upgrade` to relative imports as a prep work to the upcoming infra updates.

--- packages/upgrade/static/test/integration/static_test_helpers.ts
+++ packages/upgrade/static/test/integration/static_test_helpers.ts
@@ -7,7 +7,7 @@
  */
 
 import {NgZone, PlatformRef, Type} from '@angular/core';
-import {UpgradeModule} from '@angular/upgrade/static';
+import {UpgradeModule} from '../../../static';
 import * as angular from '../../../src/common/src/angular1';
 import {$EXCEPTION_HANDLER, $ROOT_SCOPE} from '../../../src/common/src/constants';
 
