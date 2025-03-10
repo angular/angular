904e4f20b4bee612c26b942f231aaa9423062d43
arturovt <arthurandrosovich@gmail.com>
2025-03-10 17:06:03 +0200

    refactor(core): inline standalone check to be dropped
    Inlines `standalone` check into the message to be dropped.

--- packages/core/src/application/application_ref.ts
+++ packages/core/src/application/application_ref.ts
@@ -508,13 +508,15 @@ export class ApplicationRef {
     const initStatus = this._injector.get(ApplicationInitStatus);
 
     if (!initStatus.done) {
-      const standalone = !isComponentFactory && isStandalone(componentOrFactory);
-      const errorMessage =
-        (typeof ngDevMode === 'undefined' || ngDevMode) &&
-        'Cannot bootstrap as there are still asynchronous initializers running.' +
+      let errorMessage = '';
+      if (typeof ngDevMode === 'undefined' || ngDevMode) {
+        const standalone = !isComponentFactory && isStandalone(componentOrFactory);
+        errorMessage =
+          'Cannot bootstrap as there are still asynchronous initializers running.' +
           (standalone
             ? ''
             : ' Bootstrap components in the `ngDoBootstrap` method of the root module.');
+      }
       throw new RuntimeError(RuntimeErrorCode.ASYNC_INITIALIZERS_STILL_RUNNING, errorMessage);
     }
 
