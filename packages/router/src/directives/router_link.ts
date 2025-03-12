9a2260b7658ed439c2e73120fa1131ac0a9085bc
Philipp Burgmer <burgmer@pdtec.de>
2025-03-12 15:17:13 +0100

    fix(router): relax required types on router commands to readonly array
    Only require a readonly array as input for router commands instead of a writable array. Router itself does not need writable access to the array of commands. If router requires a writable array somebody on the outside would have to spread a readonly array to a new writable array. We can avoid this by just require what route actually needs.
    
    Fixes #60269

--- packages/router/src/directives/router_link.ts
+++ packages/router/src/directives/router_link.ts
@@ -284,7 +284,7 @@ export class RouterLink implements OnChanges, OnDestroy {
     this.onChanges.next(this);
   }
 
-  private routerLinkInput: any[] | UrlTree | null = null;
+  private routerLinkInput: readonly any[] | UrlTree | null = null;
 
   /**
    * Commands to pass to {@link Router#createUrlTree} or a `UrlTree`.
@@ -296,7 +296,7 @@ export class RouterLink implements OnChanges, OnDestroy {
    * @see {@link Router#createUrlTree}
    */
   @Input()
-  set routerLink(commandsOrUrlTree: any[] | string | UrlTree | null | undefined) {
+  set routerLink(commandsOrUrlTree: readonly any[] | string | UrlTree | null | undefined) {
     if (commandsOrUrlTree == null) {
       this.routerLinkInput = null;
       this.setTabIndexIfNotOnNativeEl(null);
--- packages/router/src/router.ts
+++ packages/router/src/router.ts
@@ -440,7 +440,7 @@ export class Router {
    * tree should be created relative to the root.
    * ```
    */
-  createUrlTree(commands: any[], navigationExtras: UrlCreationOptions = {}): UrlTree {
+  createUrlTree(commands: readonly any[], navigationExtras: UrlCreationOptions = {}): UrlTree {
     const {relativeTo, queryParams, fragment, queryParamsHandling, preserveFragment} =
       navigationExtras;
     const f = preserveFragment ? this.currentUrlTree.fragment : fragment;
@@ -551,7 +551,7 @@ export class Router {
    *
    */
   navigate(
-    commands: any[],
+    commands: readonly any[],
     extras: NavigationExtras = {skipLocationChange: false},
   ): Promise<boolean> {
     validateCommands(commands);
@@ -675,7 +675,7 @@ export class Router {
   }
 }
 
-function validateCommands(commands: string[]): void {
+function validateCommands(commands: readonly string[]): void {
   for (let i = 0; i < commands.length; i++) {
     const cmd = commands[i];
     if (cmd == null) {
