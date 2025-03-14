c147a0d6dea1f59fa55a8fb55ba9ac04ae1e7421
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-03-14 11:13:26 +0100

    refactor(migrations): simplify integration of tsurge migrations into the CLI (#60386)
    Currently when we reuse a Tsurge migration is reused externally, there's some glue code that needs to be executed in a specific order. The code gets copied between the different migrations which is error-prone and means that bugs may have to be fixed several times.
    
    These changes move the common steps out into a separate function so that only the migration-specific logic (mostly instantiation and logging) is left in the schematic.
    
    PR Close #60386

--- packages/core/schematics/migrations/inject-flags/index.ts
+++ packages/core/schematics/migrations/inject-flags/index.ts
@@ -6,65 +6,15 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {Rule, SchematicsException} from '@angular-devkit/schematics';
-
-import {getProjectTsConfigPaths} from '../../utils/project_tsconfig_paths';
-import {DevkitMigrationFilesystem} from '../../utils/tsurge/helpers/angular_devkit/devkit_filesystem';
-import {groupReplacementsByFile} from '../../utils/tsurge/helpers/group_replacements';
-import {setFileSystem} from '@angular/compiler-cli/src/ngtsc/file_system';
-import {ProjectRootRelativePath, TextUpdate} from '../../utils/tsurge';
-import {synchronouslyCombineUnitData} from '../../utils/tsurge/helpers/combine_units';
-import {CompilationUnitData, InjectFlagsMigration} from './inject_flags_migration';
+import {Rule} from '@angular-devkit/schematics';
+import {InjectFlagsMigration} from './inject_flags_migration';
+import {runMigrationInDevkit} from '../../utils/tsurge/helpers/angular_devkit';
 
 export function migrate(): Rule {
   return async (tree) => {
-    const {buildPaths, testPaths} = await getProjectTsConfigPaths(tree);
-
-    if (!buildPaths.length && !testPaths.length) {
-      throw new SchematicsException(
-        'Could not find any tsconfig file. Cannot replace `InjectFlags` usages.',
-      );
-    }
-
-    const fs = new DevkitMigrationFilesystem(tree);
-    setFileSystem(fs);
-
-    const migration = new InjectFlagsMigration();
-    const unitResults: CompilationUnitData[] = [];
-    const programInfos = [...buildPaths, ...testPaths].map((tsconfigPath) => {
-      const baseInfo = migration.createProgram(tsconfigPath, fs);
-      const info = migration.prepareProgram(baseInfo);
-      return {info, tsconfigPath};
+    await runMigrationInDevkit({
+      tree,
+      getMigration: () => new InjectFlagsMigration(),
     });
-
-    for (const {info} of programInfos) {
-      unitResults.push(await migration.analyze(info));
-    }
-
-    const combined = await synchronouslyCombineUnitData(migration, unitResults);
-    if (combined === null) {
-      return;
-    }
-
-    const globalMeta = await migration.globalMeta(combined);
-    const replacementsPerFile: Map<ProjectRootRelativePath, TextUpdate[]> = new Map();
-    const {replacements} = await migration.migrate(globalMeta);
-    const changesPerFile = groupReplacementsByFile(replacements);
-
-    for (const [file, changes] of changesPerFile) {
-      if (!replacementsPerFile.has(file)) {
-        replacementsPerFile.set(file, changes);
-      }
-    }
-
-    for (const [file, changes] of replacementsPerFile) {
-      const recorder = tree.beginUpdate(file);
-      for (const c of changes) {
-        recorder
-          .remove(c.data.position, c.data.end - c.data.position)
-          .insertRight(c.data.position, c.data.toInsert);
-      }
-      tree.commitUpdate(recorder);
-    }
   };
 }
