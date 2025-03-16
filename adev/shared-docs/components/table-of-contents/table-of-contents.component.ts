a43570d62304b15e32b508e329c913b66ffd43a4
Matthieu Riegler <kyro38@gmail.com>
2025-03-16 13:50:34 +0100

    docs(docs-infra): fix & enable shared-docs tests (#60409)
    This commit includes a refactoring that removes `TableOfContentsScrollSpy` as this is only directly handled via a browser API.
    
    PR Close #60409

--- adev/shared-docs/components/table-of-contents/table-of-contents.component.ts
+++ adev/shared-docs/components/table-of-contents/table-of-contents.component.ts
@@ -13,11 +13,11 @@ import {
   input,
   inject,
   afterNextRender,
+  signal,
 } from '@angular/core';
-import {Location} from '@angular/common';
+import {Location, ViewportScroller} from '@angular/common';
 import {TableOfContentsLevel} from '../../interfaces/index';
 import {TableOfContentsLoader} from '../../services/table-of-contents-loader.service';
-import {TableOfContentsScrollSpy} from '../../services/table-of-contents-scroll-spy.service';
 import {IconComponent} from '../icon/icon.component';
 
 @Component({
@@ -32,25 +32,37 @@ export class TableOfContents {
   readonly contentSourceElement = input.required<HTMLElement>();
   readonly location = inject(Location);
 
-  private readonly scrollSpy = inject(TableOfContentsScrollSpy);
   private readonly tableOfContentsLoader = inject(TableOfContentsLoader);
+  private readonly viewportScroller = inject(ViewportScroller);
   private readonly destroyRef = inject(DestroyRef);
 
   tableOfContentItems = this.tableOfContentsLoader.tableOfContentItems;
 
-  activeItemId = this.scrollSpy.activeItemId;
+  activeItemId = signal<string | null>(null);
   TableOfContentsLevel = TableOfContentsLevel;
 
   constructor() {
     afterNextRender({
       read: () => {
         this.tableOfContentsLoader.buildTableOfContent(this.contentSourceElement());
-        this.scrollSpy.setupActiveItemListener(this.contentSourceElement(), this.destroyRef);
+        this.setupActiveItemListener(this.contentSourceElement());
       },
     });
   }
 
   scrollToTop(): void {
-    this.scrollSpy.scrollToTop();
+    this.viewportScroller.scrollToPosition([0, 0]);
+  }
+
+  setupActiveItemListener(contentSourceElement: HTMLElement): void {
+    if (contentSourceElement) {
+      this.tableOfContentsLoader.setupIntersectionObserver(
+        contentSourceElement,
+        this.destroyRef,
+        (id) => {
+          this.activeItemId.set(id);
+        },
+      );
+    }
   }
 }
