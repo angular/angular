e00dc9fd97e8c9be14ce5014552fc217968a7e91
iteriani <tomnguyen@google.com>
2025-03-18 16:15:02 -0700

    refactor(core): Return NOT_FOUND for retrieve instead of null
    This fits within the spec of the retrieve.

--- packages/core/src/di/null_injector.ts
+++ packages/core/src/di/null_injector.ts
@@ -6,6 +6,7 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
+import {NotFoundError} from '@angular/core/primitives/di';
 import {stringify} from '../util/stringify';
 import type {Injector} from './injector';
 import {THROW_IF_NOT_FOUND} from './injector_compatibility';
@@ -13,8 +14,7 @@ import {THROW_IF_NOT_FOUND} from './injector_compatibility';
 export class NullInjector implements Injector {
   get(token: any, notFoundValue: any = THROW_IF_NOT_FOUND): any {
     if (notFoundValue === THROW_IF_NOT_FOUND) {
-      const error = new Error(`NullInjectorError: No provider for ${stringify(token)}!`);
-      error.name = 'NullInjectorError';
+      const error = new NotFoundError(`NullInjectorError: No provider for ${stringify(token)}!`);
       throw error;
     }
     return notFoundValue;
