1e06c8e8b6473a19584ceb66945fd435ea4e6af9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-19 12:59:29 +0000

    fix(platform-browser-dynamic): ensure compiler is loaded before `@angular/common` (#60458)
    With the recent changes to the APF bundling rules, we turned on
    tree-shaking in rollup to support proper code splitting for FESM bundles.
    
    This resulted in Rollup re-ordering imports in the FESM bundles of
    `@angular/platform-browser-dynamic`— highlighting that over the past
    years, this package "accidentally" resulted in the side-effects of the
    compiler registering itself globally.
    
    This continues to be the case, and our changes generally didn't cause
    any issues in CLI applications because the CLI explicitly wires up the
    compiler (as expected) before `-dynamic` is even loaded. For custom
    setup, like Analog, this order change surfaced a breakage because e.g.
    `@angular/common` with its JIT decorators of e.g. directives/services
    are triggered before the compiler is actually loaded/made available.
    
    This commit fixes this. The explicit imports in practice are a noop
    because our FESM bundling doesn't recognize compiler as side-effects
    true; but marking the whole -dynamic package as having side-effects;
    prevents rollup from swapping the import order. Long-term, we should
    look into improving this by teaching `ng_package` that e.g. the compiler
    has side-effects; so that the `import @angular/compiler` statement is
    not dropped when constructing FESM bundles.
    
    PR Close #60458

--- packages/platform-browser-dynamic/public_api.ts
+++ packages/platform-browser-dynamic/public_api.ts
@@ -11,6 +11,11 @@
  * @description
  * Entry point for all public APIs of this package.
  */
+
+// Note: Historically people relied on `platform-browser-dynamic` magically
+// exposing the compiler for JIT. This is now made more explicit via this import.
+import '@angular/compiler';
+
 export * from './src/platform-browser-dynamic';
 
 // This file only reexports content of the `src` folder. Keep it that way.
