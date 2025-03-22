80ff489ca188001f2034751f5d62d23c4d66ba45
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-03-22 07:46:26 +0100

    perf(compiler): reduce allocations for let declarations only used in the same view
    We have some code that avoids `storeLet` calls for declarations only used in the same view, however we didn't previously remove the corresponding `declareLet` calls, because of the following case:
    
    ```
    @let foo = something$ | async; <!-- First in the template -->
    {{foo}}
    ```
    
    Here we need a `TNode` (created by `declareLet`) in order for DI to work correctly. Since this is only required when using pipes, we can optimize away expressions that do not have pipes.

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_let/let_optimization_listener_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_let/let_optimization_listener_template.js
@@ -1,18 +1,18 @@
 $r3$.ɵɵdefineComponent({
   …
-  decls: 7,
+  decls: 4,
   vars: 3,
   …
   template: function MyApp_Template(rf, ctx) {
     if (rf & 1) {
       const $_r1$ = $r3$.ɵɵgetCurrentView();
       $r3$.ɵɵtext(0);
-      $r3$.ɵɵdeclareLet(1)(2)(3)(4);
-      $r3$.ɵɵtext(5);
-      $r3$.ɵɵelementStart(6, "button", 0);
-      $r3$.ɵɵlistener("click", function MyApp_Template_button_click_6_listener() {
+      $r3$.ɵɵdeclareLet(1);
+      $r3$.ɵɵtext(2);
+      $r3$.ɵɵelementStart(3, "button", 0);
+      $r3$.ɵɵlistener("click", function MyApp_Template_button_click_3_listener() {
         $r3$.ɵɵrestoreView($_r1$);
-        const $three_1$ = $r3$.ɵɵreadContextLet(3);
+        const $three_1$ = $r3$.ɵɵreadContextLet(1);
         return $r3$.ɵɵresetView(ctx.callback($three_1$));
       });
       $r3$.ɵɵelementEnd();
@@ -21,10 +21,10 @@ $r3$.ɵɵdefineComponent({
       $r3$.ɵɵtextInterpolate1(" ", ctx.value, " ");
       const $one_2$ = ctx.value + 1;
       const $two_3$ = $one_2$ + 1;
-      $r3$.ɵɵadvance(3);
+      $r3$.ɵɵadvance();
       const $three_5$ = $r3$.ɵɵstoreLet($two_3$ + 1);
       $three_5$ + 1;
-      $r3$.ɵɵadvance(2);
+      $r3$.ɵɵadvance();
       $r3$.ɵɵtextInterpolate1(" ", ctx.value, " ");
     }
   },
