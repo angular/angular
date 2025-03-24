8b990a31c3f9b27e096c4ac63a7fa1873fadbe72
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-03-24 14:34:33 +0100

    fix(compiler): error if rawText isn't estimated correctly (#60529)
    The `TemplateLiteralElementExpr` has some logic where it tries to estimate the `rawText` if one isn't provided by looking at the node's source span. The problem with this approach is that we have some long-standing issues with our expression AST parser (see https://github.com/angular/angular/pull/60267#discussion_r1986402524) where it might not produce accurate spans if escape sequences are involved. This in turn can lead to unrecoverable errors, because TypeScript will throw an error if the raw string doesn't match the cooked one when constructing a TypeScript AST node.
    
    These changes remove the logic that depends on the source span and relies purely on the secondary fallback that inserts escaped characters manually.
    
    It's also worth noting that the `rawText` doesn't seem to matter much at this point, because the main usage of it is when downlevelling template literals to ES5 which we no longer support.
    
    Fixes #60528.
    
    PR Close #60529

--- packages/compiler-cli/test/compliance/test_cases/r3_compiler_compliance/components_and_directives/value_composition/template_literals.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_compiler_compliance/components_and_directives/value_composition/template_literals.js
@@ -4,5 +4,8 @@ if (rf & 2) {
   $r3$.ɵɵadvance(2);
   $r3$.ɵɵtextInterpolate1("With interpolations: ", `hello ${ctx.name}, it is currently ${ctx.timeOfDay}!`);
   $r3$.ɵɵadvance(2);
-  $r3$.ɵɵtextInterpolate1("With pipe: ", $r3$.ɵɵpipeBind1(6, 3, `hello ${ctx.name}`));
+  $r3$.ɵɵtextInterpolate1("With pipe: ", $r3$.ɵɵpipeBind1(6, 4, `hello ${ctx.name}`));
+  const $insideLet_r1$ = `Hello ${ctx.name}`;
+  $r3$.ɵɵadvance(4);
+  $r3$.ɵɵtextInterpolate1(" Inside let: ", $insideLet_r1$);
 }
