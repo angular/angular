64f6fc00954c3b68cefed05224b5e8c6bcd764a7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-25 11:12:31 +0100

    refactor(core): allow multiple profilers (#60548)
    This commit changes the profiler infrastructure to
    allow multiple profilers running at the same time.
    
    PR Close #60548

--- packages/core/src/render3/profiler.ts
+++ packages/core/src/render3/profiler.ts
@@ -8,21 +8,40 @@
 
 import {type Profiler} from './profiler_types';
 
-let profilerCallback: Profiler | null = null;
+const profilerCallbacks: Profiler[] = [];
+
+const NOOP_PROFILER_REMOVAL = () => {};
+
+function removeProfiler(profiler: Profiler) {
+  const profilerIdx = profilerCallbacks.indexOf(profiler);
+  if (profilerIdx !== -1) {
+    profilerCallbacks.splice(profilerIdx, 1);
+  }
+}
 
 /**
- * Sets the callback function which will be invoked before and after performing certain actions at
- * runtime (for example, before and after running change detection).
+ * Adds a callback function which will be invoked before and after performing certain actions at
+ * runtime (for example, before and after running change detection). Multiple profiler callbacks can be set:
+ * in this case profiling events are reported to every registered callback.
  *
  * Warning: this function is *INTERNAL* and should not be relied upon in application's code.
  * The contract of the function might be changed in any release and/or the function can be removed
  * completely.
  *
- * @param profiler function provided by the caller or null value to disable profiling.
+ * @param profiler function provided by the caller or null value to disable all profilers.
+ * @returns a cleanup function that, when invoked, removes a given profiler callback.
  */
-export const setProfiler = (profiler: Profiler | null) => {
-  profilerCallback = profiler;
-};
+export function setProfiler(profiler: Profiler | null): () => void {
+  if (profiler !== null) {
+    if (!profilerCallbacks.includes(profiler)) {
+      profilerCallbacks.push(profiler);
+    }
+    return () => removeProfiler(profiler);
+  } else {
+    profilerCallbacks.length = 0;
+    return NOOP_PROFILER_REMOVAL;
+  }
+}
 
 /**
  * Profiler function which wraps user code executed by the runtime.
@@ -32,10 +51,10 @@ export const setProfiler = (profiler: Profiler | null) => {
  * @param eventFn function associated with event.
  *    For example a template function, lifecycle hook, or output listener.
  *    The value depends on the execution context
- * @returns
  */
-export const profiler: Profiler = function (event, instance = null, eventFn) {
-  if (profilerCallback != null /* both `null` and `undefined` */) {
+export const profiler: Profiler = function (event, instance = null, eventFn): void {
+  for (let i = 0; i < profilerCallbacks.length; i++) {
+    const profilerCallback = profilerCallbacks[i];
     profilerCallback(event, instance, eventFn);
   }
 };
