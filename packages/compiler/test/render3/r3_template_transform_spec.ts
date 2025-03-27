e6d2afbfb9114274eb4374b1ae0683d8f62f3a9f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-03-27 09:23:32 +0100

    fix(compiler): throw for invalid "as" expression in if block (#60580)
    Adds some validation that the "as" expression in an `@if` block is valid.
    
    Fixes #59939.
    
    PR Close #60580

--- packages/compiler/test/render3/r3_template_transform_spec.ts
+++ packages/compiler/test/render3/r3_template_transform_spec.ts
@@ -2361,6 +2361,14 @@ describe('R3 template transform', () => {
         `),
         ).toThrowError(/@else block must be last inside the conditional/);
       });
+
+      it('should throw if "as" expression is not a valid identifier', () => {
+        expect(() =>
+          parse(`
+          @if (foo; as foo && bar) {hello}
+        `),
+        ).toThrowError(/"as" expression must be a valid JavaScript identifier/);
+      });
     });
   });
 
