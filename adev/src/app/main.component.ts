3734cc690ecd40e31ec9913b18f83776e36a6a8f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 16:30:04 -0400

    docs: fix missing file header (#60649)
    PR Close #60649

--- adev/src/app/main.component.ts
+++ adev/src/app/main.component.ts
@@ -1,3 +1,11 @@
+/*!
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
 import {Component, effect, inject, model, WritableSignal} from '@angular/core';
 import {IS_SEARCH_DIALOG_OPEN, Search} from '@angular/docs';
 import {RouterOutlet} from '@angular/router';
