saqibumar/test-angular

feat(forms): add markAllAsDirty to AbstractControl
Adds the `markAllAsDirty` method to the `AbstractControl` class. This method will mark the control and all its
descendants as dirty.

I pretty much just duplicated the behaviour and tests of `markAllAsTouched`.

Fixes #55990

packages/forms/test/form_group_spec.ts
@@ -81,6 +81,68 @@ import {StatusChangeEvent} from '../src/model/abstract_model';
       });
     });
 
+    describe('markAllAsDirty', () => {
+      it('should mark all descendants as dirty', () => {
+        const formGroup: FormGroup = new FormGroup({
+          'c1': new FormControl('v1'),
+          'group': new FormGroup({'c2': new FormControl('v2'), 'c3': new FormControl('v3')}),
+          'array': new FormArray([
+            new FormControl('v4') as any,
+            new FormControl('v5'),
+            new FormGroup({'c4': new FormControl('v4')}),
+          ]),
+        });
+
+        expect(formGroup.dirty).toBe(false);
+
+        const control1 = formGroup.get('c1') as FormControl;
+
+        expect(control1.dirty).toBe(false);
+
+        const innerGroup = formGroup.get('group') as FormGroup;
+
+        expect(innerGroup.dirty).toBe(false);
+
+        const innerGroupFirstChildCtrl = innerGroup.get('c2') as FormControl;
+
+        expect(innerGroupFirstChildCtrl.dirty).toBe(false);
+
+        formGroup.markAllAsDirty();
+
+        expect(formGroup.dirty).toBe(true);
+
+        expect(control1.dirty).toBe(true);
+
+        expect(innerGroup.dirty).toBe(true);
+
+        expect(innerGroupFirstChildCtrl.dirty).toBe(true);
+
+        const innerGroupSecondChildCtrl = innerGroup.get('c3') as FormControl;
+
+        expect(innerGroupSecondChildCtrl.dirty).toBe(true);
+
+        const array = formGroup.get('array') as FormArray;
+
+        expect(array.dirty).toBe(true);
+
+        const arrayFirstChildCtrl = array.at(0) as FormControl;
+
+        expect(arrayFirstChildCtrl.dirty).toBe(true);
+
+        const arraySecondChildCtrl = array.at(1) as FormControl;
+
+        expect(arraySecondChildCtrl.dirty).toBe(true);
+
+        const arrayFirstChildGroup = array.at(2) as FormGroup;
+
+        expect(arrayFirstChildGroup.dirty).toBe(true);
+
+        const arrayFirstChildGroupFirstChildCtrl = arrayFirstChildGroup.get('c4') as FormControl;
+
+        expect(arrayFirstChildGroupFirstChildCtrl.dirty).toBe(true);
+      });
+    });
+
     describe('markAllAsTouched', () => {
       it('should mark all descendants as touched', () => {
         const formGroup: FormGroup = new FormGroup({

angular/angular

packages/forms/test/form_group_spec.ts
@@ -82,9 +82,12 @@ import {FormControlStatus, StatusChangeEvent} from '../src/model/abstract_model'
       });
     });
 
-    describe('markAllAsTouched', () => {
-      it('should mark all descendants as touched', () => {
-        const formGroup: FormGroup = new FormGroup({
+    describe('markAllAsDirty', () => {
+      let form: FormGroup;
+      let logger: any[];
+
+      beforeEach(() => {
+        form = new FormGroup({
           'c1': new FormControl('v1'),
           'group': new FormGroup({'c2': new FormControl('v2'), 'c3': new FormControl('v3')}),
           'array': new FormArray([
@@ -93,14 +96,91 @@ import {FormControlStatus, StatusChangeEvent} from '../src/model/abstract_model'
             new FormGroup({'c4': new FormControl('v4')}),
           ]),
         });
+        logger = [];
+      });
 
-        expect(formGroup.touched).toBe(false);
+      it('should mark all descendants as dirty', () => {
+        expect(form.dirty).toBe(false);
 
-        const control1 = formGroup.get('c1') as FormControl;
+        const control1 = form.get('c1') as FormControl;
+
+        expect(control1.dirty).toBe(false);
+
+        const innerGroup = form.get('group') as FormGroup;
+
+        expect(innerGroup.dirty).toBe(false);
+
+        const innerGroupFirstChildCtrl = innerGroup.get('c2') as FormControl;
+
+        expect(innerGroupFirstChildCtrl.dirty).toBe(false);
+
+        form.markAllAsDirty();
+
+        expect(form.dirty).toBe(true);
+
+        expect(control1.dirty).toBe(true);
+
+        expect(innerGroup.dirty).toBe(true);
+
+        expect(innerGroupFirstChildCtrl.dirty).toBe(true);
+
+        const innerGroupSecondChildCtrl = innerGroup.get('c3') as FormControl;
+
+        expect(innerGroupSecondChildCtrl.dirty).toBe(true);
+
+        const array = form.get('array') as FormArray;
+
+        expect(array.dirty).toBe(true);
+
+        const arrayFirstChildCtrl = array.at(0) as FormControl;
+
+        expect(arrayFirstChildCtrl.dirty).toBe(true);
+
+        const arraySecondChildCtrl = array.at(1) as FormControl;
+
+        expect(arraySecondChildCtrl.dirty).toBe(true);
+
+        const arrayFirstChildGroup = array.at(2) as FormGroup;
+
+        expect(arrayFirstChildGroup.dirty).toBe(true);
+
+        const arrayFirstChildGroupFirstChildCtrl = arrayFirstChildGroup.get('c4') as FormControl;
+
+        expect(arrayFirstChildGroupFirstChildCtrl.dirty).toBe(true);
+      });
+
+      it('should not emit events when `FormGroup.markAllAsDirty` is called with `emitEvent: false`', () => {
+        form.valueChanges.subscribe(() => logger.push('form'));
+        form.markAllAsDirty({emitEvent: false});
+        expect(logger).toEqual([]);
+      });
+    });
+
+    describe('markAllAsTouched', () => {
+      let form: FormGroup;
+      let logger: any[];
+
+      beforeEach(() => {
+        form = new FormGroup({
+          'c1': new FormControl('v1'),
+          'group': new FormGroup({'c2': new FormControl('v2'), 'c3': new FormControl('v3')}),
+          'array': new FormArray([
+            new FormControl('v4') as any,
+            new FormControl('v5'),
+            new FormGroup({'c4': new FormControl('v4')}),
+          ]),
+        });
+        logger = [];
+      });
+
+      it('should mark all descendants as touched', () => {
+        expect(form.touched).toBe(false);
+
+        const control1 = form.get('c1') as FormControl;
 
         expect(control1.touched).toBe(false);
 
-        const innerGroup = formGroup.get('group') as FormGroup;
+        const innerGroup = form.get('group') as FormGroup;
 
         expect(innerGroup.touched).toBe(false);
 
@@ -108,9 +188,9 @@ import {FormControlStatus, StatusChangeEvent} from '../src/model/abstract_model'
 
         expect(innerGroupFirstChildCtrl.touched).toBe(false);
 
-        formGroup.markAllAsTouched();
+        form.markAllAsTouched();
 
-        expect(formGroup.touched).toBe(true);
+        expect(form.touched).toBe(true);
 
         expect(control1.touched).toBe(true);
 
@@ -122,7 +202,7 @@ import {FormControlStatus, StatusChangeEvent} from '../src/model/abstract_model'
 
         expect(innerGroupSecondChildCtrl.touched).toBe(true);
 
-        const array = formGroup.get('array') as FormArray;
+        const array = form.get('array') as FormArray;
 
         expect(array.touched).toBe(true);
 
@@ -141,6 +221,12 @@ import {FormControlStatus, StatusChangeEvent} from '../src/model/abstract_model'
         const arrayFirstChildGroupFirstChildCtrl = arrayFirstChildGroup.get('c4') as FormControl;
 
         expect(arrayFirstChildGroupFirstChildCtrl.touched).toBe(true);
+      });
+
+      it('should not emit events when `FormGroup.markAllAsTouched` is called with `emitEvent: false`', () => {
+        form.valueChanges.subscribe(() => logger.push('form'));
+        form.markAllAsTouched({emitEvent: false});
+        expect(logger).toEqual([]);
       });
     });
 