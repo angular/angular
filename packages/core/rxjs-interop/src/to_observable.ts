a227d7d2bbeeed9eae75f54fa378cdc09215796d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-04-02 10:11:53 -0700

    Revert "refactor(core): support an opt-in sync version of `toObservable` (#60640)"
    This reverts commit 57a240b07e62f9960e1f9e1be1bfd19eba698852. We're no
    longer attempting to convert `toObservable` to a synchronous API.

--- packages/core/rxjs-interop/src/to_observable.ts
+++ packages/core/rxjs-interop/src/to_observable.ts
@@ -15,7 +15,6 @@ import {
   Signal,
   untracked,
 } from '../../src/core';
-import {SIGNAL, ReactiveNode} from '../../primitives/signals';
 import {Observable, ReplaySubject} from 'rxjs';
 
 /**
@@ -31,16 +30,6 @@ export interface ToObservableOptions {
    * will be used.
    */
   injector?: Injector;
-
-  /**
-   * Temporary option for forcing a synchronous emit of the signal's initial value.
-   *
-   * This will eventually become the default behavior, but is opt-in to allow a short migration
-   * period.
-   *
-   * @deprecated will become default behavior
-   */
-  forceSyncFirstEmit?: true;
 }
 
 /**
@@ -53,10 +42,6 @@ export interface ToObservableOptions {
  * @developerPreview
  */
 export function toObservable<T>(source: Signal<T>, options?: ToObservableOptions): Observable<T> {
-  if (options?.forceSyncFirstEmit === true) {
-    return toObservableNext(source, options);
-  }
-
   !options?.injector && assertInInjectionContext(toObservable);
   const injector = options?.injector ?? inject(Injector);
   const subject = new ReplaySubject<T>(1);
@@ -81,89 +66,4 @@ export function toObservable<T>(source: Signal<T>, options?: ToObservableOptions
   });
 
   return subject.asObservable();
-}
-
-/**
- * New version of `toObservable` with always-synchronous first emit.
- *
- * This will eventually replace the other implementation.
- */
-function toObservableNext<T>(source: Signal<T>, options?: ToObservableOptions): Observable<T> {
-  !options?.injector && assertInInjectionContext(toObservable);
-  const injector = options?.injector ?? inject(Injector);
-
-  return new Observable<T>((subscriber) => {
-    let firstVersion: number = -1;
-    let firstValue: T;
-    try {
-      firstValue = untracked(source);
-    } catch (err) {
-      // A failure on the first read just errors the observable without
-      // creating an effect.
-      subscriber.error(err);
-      return;
-    }
-    // We cache the `version` of the first value. This lets us avoid emitting
-    // this value a second time during the `effect`.
-    firstVersion = signalVersion(source);
-
-    // Emit the first value synchronously on subscription.
-    subscriber.next(firstValue);
-
-    // Create an effect that will watch the signal for future changes.
-    let firstEmit = true;
-    const ref = effect(
-      () => {
-        let value: T;
-        try {
-          // Read the value (& track it).
-          value = source();
-        } catch (err) {
-          // Errors cause the Observable stream to terminate.
-          untracked(() => subscriber.error(err));
-          cleanup(false);
-          return;
-        }
-
-        // Skip the emit of the value if it hasn't changed since the
-        // synchronous emit.
-        if (firstEmit) {
-          firstEmit = false;
-          if (signalVersion(source) === firstVersion) {
-            return;
-          }
-        }
-
-        untracked(() => subscriber.next(value));
-      },
-      {injector, manualCleanup: true},
-    );
-
-    const cleanup = (fromInjector: boolean) => {
-      ref.destroy();
-
-      if (fromInjector) {
-        // If we're cleaning up because the injector is destroyed, then our
-        // subscription is still active and we need to complete it.
-        subscriber.complete();
-      } else {
-        // Otherwise, remove the cleanup function. This both prevents the
-        // complete() event from being produced and allows memory to be
-        // reclaimed.
-        removeInjectorCleanupFn();
-      }
-    };
-
-    const removeInjectorCleanupFn = injector.get(DestroyRef).onDestroy(() => {
-      // Cleaning up from the `DestroyRef` means the stream is still active, so
-      // we should emit completion.
-      cleanup(true);
-    });
-
-    return () => cleanup(false);
-  });
-}
-
-function signalVersion(source: Signal<unknown>): number {
-  return (source[SIGNAL] as ReactiveNode).version;
 }
