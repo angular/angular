7e5921293d49a46244c798273812c5f89a972f92
kirjs <kirjs@users.noreply.github.com>
2025-04-02 13:29:12 -0400

    Make sure all files have licence header.

--- packages/forms/experimental/src/api/control.ts
+++ packages/forms/experimental/src/api/control.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
 import {InputSignal, ModelSignal, OutputRef} from '@angular/core';
 import {FormError} from './types';
 
