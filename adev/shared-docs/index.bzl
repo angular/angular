angular/angular

commit c4de4e1f894001d8f80b70297c5e576f2d11ec6f

refactor(docs-infra): build adev application using local generated assets (#53511)
Use local generated assets to build adev application.

PR Close #53511

adev/BUILD.bazel
@@ -1,24 +1,27 @@
-load("@npm//@angular-devkit/architect-cli:index.bzl", "architect")
+load("//:packages.bzl", "link_packages")
 load("@build_bazel_rules_nodejs//:index.bzl", "copy_to_bin")
+load("@npm//@angular-devkit/architect-cli:index.bzl", "architect")
+
+package(default_visibility = ["//visibility:public"])
 
 # All source and configuration files required to build the docs app
 APPLICATION_FILES = [
-    "//:angular.json",
-    "adev/tsconfig.app.json",
-    "adev/tsconfig.json",
-    "adev/tsconfig.worker.json",
+    "angular.json",
+    "tsconfig.app.json",
+    "tsconfig.json",
+    "tsconfig.worker.json",
 ] + glob(
-    ["adev/src/**/*"],
+    ["src/**/*"],
     exclude = [
-        "adev/src/**/*.spec.ts",
+        "src/**/*.spec.ts",
         # Temporarily exclude generated sources produced by the non-bazel
         # build until the whole project is built by bazel and this directory
         # isn't needed.
-        "adev/src/generated/**/*",
+        "src/generated/**/*",
     ],
 )
 
-APPLICATION_DEPS = [
+APPLICATION_DEPS = link_packages([
     "@npm//@angular-devkit/build-angular",
     "@npm//@angular/animations",
     "@npm//@angular/cdk",
@@ -38,7 +41,33 @@ APPLICATION_DEPS = [
     "@npm//ogl",
     "@npm//rxjs",
     "@npm//typescript",
-]
+    "//adev/src/assets/images",
+    "//adev/src/assets/textures",
+    "//adev/src/assets/previews",
+    "//adev/src/assets:tutorials",
+    "//adev/src/assets/icons",
+    "//adev/src/assets:api",
+    "//adev/src/assets:content",
+    "@npm//@typescript/vfs",
+    "@npm//@codemirror/state",
+    "@npm//@codemirror/view",
+    "@npm//@codemirror/language",
+    "@npm//@codemirror/commands",
+    "@npm//@codemirror/search",
+    "@npm//@codemirror/autocomplete",
+    "@npm//@codemirror/lint",
+    "@npm//@codemirror/lang-html",
+    "@npm//@codemirror/lang-angular",
+    "@npm//@codemirror/lang-css",
+    "@npm//@codemirror/lang-sass",
+    "@npm//@codemirror/lang-javascript",
+    "@npm//@lezer/highlight",
+    "@npm//@lezer/javascript",
+    "@npm//@lezer/common",
+    "@npm//xterm",
+    "@npm//xterm-addon-fit",
+    "@npm//angular-split",
+])
 
 copy_to_bin(
     name = "application_files_bin",
@@ -56,9 +85,6 @@ architect(
         ":application_files_bin",
     ],
     output_dir = True,
-    # When building with local packages (--config=aio_local_deps), RBE complains about
-    # the input tree being too large (> 70,000 files).
-    tags = ["no-remote-exec"],
 )
 
 architect(
@@ -73,7 +99,4 @@ architect(
     data = APPLICATION_DEPS + [
         ":application_files_bin",
     ],
-    # When building with local packages (--config=aio_local_deps), RBE complains about
-    # the input tree being too large (> 70,000 files).
-    tags = ["no-remote-exec"],
 )

adev/src/assets/previews/BUILD.bazel
@@ -1,0 +1,15 @@
+load("@build_bazel_rules_nodejs//:index.bzl", "copy_to_bin")
+
+# TODO(josephperrott): Generate previews from source instead of using already generated files
+
+exports_files(
+    glob(["*"]),
+)
+
+copy_to_bin(
+    name = "previews",
+    srcs = glob(["*"]),
+    visibility = [
+        "//visibility:public",
+    ],
+)

adev/src/assets/BUILD.bazel
@@ -1,0 +1,95 @@
+load("@aspect_bazel_lib//lib:copy_to_directory.bzl", "copy_to_directory")
+
+package(default_visibility = ["//adev:__subpackages__"])
+
+copy_to_directory(
+    name = "content",
+    srcs = [
+        "//adev/src/content/best-practices",
+        "//adev/src/content/best-practices/runtime-performance",
+        "//adev/src/content/ecosystem",
+        "//adev/src/content/ecosystem/service-workers",
+        "//adev/src/content/guide",
+        "//adev/src/content/guide/animations",
+        "//adev/src/content/guide/components",
+        "//adev/src/content/guide/di",
+        "//adev/src/content/guide/directives",
+        "//adev/src/content/guide/forms",
+        "//adev/src/content/guide/http",
+        "//adev/src/content/guide/i18n",
+        "//adev/src/content/guide/ngmodules",
+        "//adev/src/content/guide/pipes",
+        "//adev/src/content/guide/routing",
+        "//adev/src/content/guide/signals",
+        "//adev/src/content/guide/templates",
+        "//adev/src/content/guide/testing",
+        "//adev/src/content/introduction",
+        "//adev/src/content/introduction/essentials",
+        "//adev/src/content/reference",
+        "//adev/src/content/reference/configs",
+        "//adev/src/content/reference/errors",
+        "//adev/src/content/reference/extended-diagnostics",
+        "//adev/src/content/reference/migrations",
+        "//adev/src/content/tools",
+        "//adev/src/content/tools/cli",
+        "//adev/src/content/tools/libraries",
+        "//adev/src/content/tutorials",
+        "//adev/src/content/tutorials/first-app:first-app-guides",
+        "//adev/src/content/tutorials/learn-angular:learn-angular-guides",
+        "//packages/animations:animations_docs",
+        "//packages/animations/browser:animations_browser_docs",
+        "//packages/animations/browser/testing:animations_browser_testing_docs",
+        "//packages/common:common_docs",
+        "//packages/common/http:http_docs",
+        "//packages/common/http/testing:http_testing_docs",
+        "//packages/common/testing:common_testing_docs",
+        "//packages/common/upgrade:common_upgrade_docs",
+        "//packages/core:core_docs",
+        "//packages/core/rxjs-interop:core_rxjs-interop_docs",
+        "//packages/core/testing:core_testing_docs",
+        "//packages/elements:elements_docs",
+        "//packages/forms:forms_docs",
+        "//packages/localize:localize_docs",
+        "//packages/platform-browser:platform-browser_docs",
+        "//packages/platform-browser-dynamic:platform-browser_dynamic_docs",
+        "//packages/platform-browser-dynamic/testing:platform-browser_dynamic_testing_docs",
+        "//packages/platform-browser/animations:platform-browser_animations_docs",
+        "//packages/platform-browser/animations/async:platform-browser_animations_async_docs",
+        "//packages/platform-browser/testing:platform-browser_testing_docs",
+        "//packages/platform-server:platform-server_docs",
+        "//packages/platform-server/testing:platform-server_testing_docs",
+        "//packages/router:router_docs",
+        "//packages/router/testing:router_testing_docs",
+        "//packages/router/upgrade:router_upgrade_docs",
+        "//packages/service-worker:service-worker_docs",
+        "//packages/upgrade:upgrade_docs",
+        "//packages/upgrade/static:upgrade_static_docs",
+        "//packages/upgrade/static/testing:upgrade_static_testing_docs",
+    ],
+    replace_prefixes = {
+        "adev/src/content": "",
+        "packages/**/": "api/",
+    },
+)
+
+copy_to_directory(
+    name = "tutorials",
+    srcs = [
+        "//adev/src/content/tutorials/first-app",
+        "//adev/src/content/tutorials/learn-angular",
+        "//adev/src/content/tutorials/playground",
+    ],
+    replace_prefixes = {
+        "adev/**/tutorials/*/": "",
+    },
+)
+
+copy_to_directory(
+    name = "api",
+    srcs = [
+        "//packages:docs_api_manifest",
+    ],
+    replace_prefixes = {
+        "**/": "",
+    },
+)

adev/src/assets/icons/BUILD.bazel
@@ -1,0 +1,13 @@
+load("@build_bazel_rules_nodejs//:index.bzl", "copy_to_bin")
+
+exports_files(
+    glob(["*"]),
+)
+
+copy_to_bin(
+    name = "icons",
+    srcs = glob(["*"]),
+    visibility = [
+        "//visibility:public",
+    ],
+)

adev/src/assets/images/BUILD.bazel
@@ -1,0 +1,13 @@
+load("@build_bazel_rules_nodejs//:index.bzl", "copy_to_bin")
+
+exports_files(
+    glob(["*"]),
+)
+
+copy_to_bin(
+    name = "images",
+    srcs = glob(["*"]),
+    visibility = [
+        "//visibility:public",
+    ],
+)

adev/src/assets/textures/BUILD.bazel
@@ -1,0 +1,13 @@
+load("@build_bazel_rules_nodejs//:index.bzl", "copy_to_bin")
+
+exports_files(
+    glob(["*"]),
+)
+
+copy_to_bin(
+    name = "textures",
+    srcs = glob(["*"]),
+    visibility = [
+        "//visibility:public",
+    ],
+)

adev/src/content/best-practices/BUILD.bazel
@@ -72,4 +72,5 @@ generate_guides(
         "//adev/src/content/examples/styleguide:src/09-01/app/heroes/shared/hero-button/hero-button.component.avoid.ts",
         "//adev/src/content/examples/styleguide:src/09-01/app/heroes/shared/hero-button/hero-button.component.ts",
     ],
+    visibility = ["//adev:__subpackages__"],
 )

adev/src/content/best-practices/runtime-performance/BUILD.bazel
@@ -5,4 +5,5 @@ generate_guides(
     srcs = glob([
         "*.md",
     ]),
+    visibility = ["//adev:__subpackages__"],
 )

commit e8b4673826b5d1d37dc5c5fb144b9221f4b29739

docs: Add back script to generate example previews map
Restores the ability to have example components rendered alongside the
code. This functionality was broken a while back by #53511.

To enable the embedded preview for an example, add `preview` and
`path=adev/src/content/expamples/component/to/render.ts` attributes to
the `<docs-code>` or `<docs-code-multifile>` tag.

Tested with one of the accessibility examples and it seems to work now.

adev/BUILD.bazel
@@ -40,6 +40,7 @@ APPLICATION_ASSETS = [
     "//adev/src/assets/icons",
     "//adev/src/assets:api",
     "//adev/src/assets:content",
+    "//adev/src/content/examples/accessibility:example",
 ]
 
 APPLICATION_DEPS = [

adev/shared-docs/index.bzl
@@ -1,11 +1,13 @@
 load("//adev/shared-docs/pipeline:_guides.bzl", _generate_guides = "generate_guides")
 load("//adev/shared-docs/pipeline:_navigation.bzl", _generate_nav_items = "generate_nav_items")
 load("//adev/shared-docs/pipeline:_playground.bzl", _generate_playground = "generate_playground")
+load("//adev/shared-docs/pipeline:_previews.bzl", _generate_previews = "generate_previews")
 load("//adev/shared-docs/pipeline:_stackblitz.bzl", _generate_stackblitz = "generate_stackblitz")
 load("//adev/shared-docs/pipeline:_tutorial.bzl", _generate_tutorial = "generate_tutorial")
 
 generate_guides = _generate_guides
 generate_stackblitz = _generate_stackblitz
+generate_previews = _generate_previews
 generate_playground = _generate_playground
 generate_tutorial = _generate_tutorial
 generate_nav_items = _generate_nav_items

adev/shared-docs/pipeline/BUILD.bazel
@@ -62,6 +62,22 @@ esbuild_esm_bundle(
 )
 
 esbuild_esm_bundle(
+    name = "previews-bundle",
+    entry_point = "//adev/shared-docs/pipeline/examples/previews:index.ts",
+    external = [
+        "typescript",
+    ],
+    output = "previews.mjs",
+    platform = "node",
+    target = "es2022",
+    visibility = ["//visibility:public"],
+    deps = [
+        "//adev/shared-docs/pipeline/examples/previews:index",
+        "@npm//typescript",
+    ],
+)
+
+esbuild_esm_bundle(
     name = "zip-bundle",
     entry_point = "//adev/shared-docs/pipeline/examples/zip:index.ts",
     output = "zip.mjs",
@@ -112,6 +128,7 @@ esbuild_esm_bundle(
 exports_files([
     "_guides.bzl",
     "_stackblitz.bzl",
+    "_previews.bzl",
     "_playground.bzl",
     "_tutorial.bzl",
     "_navigation.bzl",
@@ -126,6 +143,15 @@ nodejs_binary(
         "@npm//jsdom",
     ],
     entry_point = "//adev/shared-docs/pipeline:stackblitz.mjs",
+    visibility = ["//visibility:public"],
+)
+
+nodejs_binary(
+    name = "previews",
+    data = [
+        "@npm//typescript",
+    ],
+    entry_point = "//adev/shared-docs/pipeline:previews.mjs",
     visibility = ["//visibility:public"],
 )
 

adev/shared-docs/pipeline/_previews.bzl
@@ -1,0 +1,56 @@
+load("@build_bazel_rules_nodejs//:providers.bzl", "run_node")
+
+def _generate_previews(ctx):
+    """Implementation of the previews generator rule"""
+
+    # File declaration of the generated ts file
+    ts_output = ctx.actions.declare_file("previews.ts")
+
+    # Set the arguments for the actions inputs and out put location.
+    args = ctx.actions.args()
+
+    # Path to the examples for which previews are being generated.
+    args.add(ctx.attr.example_srcs.label.package)
+
+    # Path to the preview map template.
+    args.add(ctx.file.template_src)
+
+    # Path to the ts output file to write to.
+    args.add(ts_output.path)
+
+    ctx.runfiles(files = ctx.files.template_src)
+
+    run_node(
+        ctx = ctx,
+        inputs = depset(ctx.files.example_srcs + ctx.files.template_src),
+        executable = "_generate_previews",
+        outputs = [ts_output],
+        arguments = [args],
+    )
+
+    # The return value describes what the rule is producing. In this case we need to specify
+    # the "DefaultInfo" with the output ts file.
+    return [DefaultInfo(files = depset([ts_output]))]
+
+generate_previews = rule(
+    # Point to the starlark function that will execute for this rule.
+    implementation = _generate_previews,
+    doc = """Rule that generates a map of example previews to their component""",
+
+    # The attributes that can be set to this rule.
+    attrs = {
+        "example_srcs": attr.label(
+            doc = """Files used for the previews map generation.""",
+        ),
+        "template_src": attr.label(
+            doc = """The previews map template file to base the generated file on.""",
+            default = Label("//adev/shared-docs/pipeline/examples/previews:template"),
+            allow_single_file = True,
+        ),
+        "_generate_previews": attr.label(
+            default = Label("//adev/shared-docs/pipeline:previews"),
+            executable = True,
+            cfg = "exec",
+        ),
+    },
+)

adev/shared-docs/pipeline/examples/previews/BUILD.bazel
@@ -1,0 +1,28 @@
+load("//tools:defaults.bzl", "ts_library")
+
+package(default_visibility = ["//visibility:public"])
+
+ts_library(
+    name = "index",
+    srcs = [
+        "index.ts",
+    ],
+    visibility = [
+        "//adev/shared-docs:__subpackages__",
+    ],
+    deps = [
+        "@npm//@types/node",
+        "@npm//typescript",
+    ],
+)
+
+filegroup(
+    name = "template",
+    srcs = ["previews.template"],
+    visibility = ["//visibility:public"],
+)
+
+exports_files([
+    "index.ts",
+    "previews.template",
+])

adev/shared-docs/pipeline/examples/previews/index.ts
@@ -1,0 +1,149 @@
+/*!
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import * as fs from 'fs';
+import {readFile, writeFile} from 'fs/promises';
+import {join, relative} from 'path';
+import ts from 'typescript';
+
+const [examplesDir, templateFilePath, outputFilePath] = process.argv.slice(2);
+
+const TYPESCRIPT_EXTENSION = '.ts';
+const SKIP_FILES_WITH_EXTENSIONS = ['.e2e-spec.ts', '.spec.ts', '.po.ts'];
+const EXAMPLES_PATH = `../../content/examples`;
+
+interface File {
+  path: string;
+  content: string;
+}
+
+interface AnalyzedFiles {
+  path: string;
+  componentNames: string[];
+}
+
+main();
+
+async function main() {
+  const files = await retrieveAllTypescriptFiles(
+    examplesDir,
+    (path) => !SKIP_FILES_WITH_EXTENSIONS.some((extensionToSkip) => path.endsWith(extensionToSkip)),
+  );
+
+  const filesWithComponent = files
+    .map((file) => ({
+      componentNames: analyzeFile(file),
+      path: file.path,
+    }))
+    .filter((result) => result.componentNames.length > 0);
+
+  const previewsComponentMap = generatePreviewsComponentMap(filesWithComponent);
+
+  await writeFile(outputFilePath, previewsComponentMap);
+}
+
+/** Recursively search the provided directory for all typescript files and asynchronously load them. */
+function retrieveAllTypescriptFiles(
+  baseDir: string,
+  predicateFn: (path: string) => boolean,
+): Promise<File[]> {
+  const typescriptFiles: Promise<File>[] = [];
+
+  const checkFilesInDirectory = (dir: string) => {
+    const files = fs.readdirSync(dir, {withFileTypes: true});
+    for (const file of files) {
+      const fullPathToFile = join(dir, file.name);
+      const relativeFilePath = relative(baseDir, fullPathToFile);
+
+      if (
+        file.isFile() &&
+        file.name.endsWith(TYPESCRIPT_EXTENSION) &&
+        predicateFn(relativeFilePath)
+      ) {
+        typescriptFiles.push(
+          readFile(fullPathToFile, {encoding: 'utf-8'}).then((fileContent) => {
+            return {
+              path: relativeFilePath,
+              content: fileContent,
+            };
+          }),
+        );
+      } else if (file.isDirectory()) {
+        checkFilesInDirectory(fullPathToFile);
+      }
+    }
+  };
+
+  checkFilesInDirectory(baseDir);
+
+  return Promise.all(typescriptFiles);
+}
+
+/** Returns list of the `Standalone` @Component class names for given file */
+function analyzeFile(file: File): string[] {
+  const componentClassNames: string[] = [];
+  const sourceFile = ts.createSourceFile(file.path, file.content, ts.ScriptTarget.Latest, false);
+
+  const visitNode = (node: ts.Node): void => {
+    if (ts.isClassDeclaration(node)) {
+      const decorators = ts.getDecorators(node);
+      const componentName = node.name ? node.name.text : null;
+
+      if (decorators && decorators.length) {
+        for (const decorator of decorators) {
+          const call = decorator.expression;
+
+          if (
+            ts.isCallExpression(call) &&
+            ts.isIdentifier(call.expression) &&
+            call.expression.text === 'Component' &&
+            call.arguments.length > 0 &&
+            ts.isObjectLiteralExpression(call.arguments[0])
+          ) {
+            const standaloneProperty = call.arguments[0].properties.find(
+              (property) =>
+                property.name &&
+                ts.isIdentifier(property.name) &&
+                property.name.text === 'standalone',
+            );
+
+            const isStandalone =
+              !standaloneProperty ||
+              (ts.isPropertyAssignment(standaloneProperty) &&
+                standaloneProperty.initializer.kind === ts.SyntaxKind.TrueKeyword);
+
+            if (isStandalone && componentName) {
+              componentClassNames.push(componentName);
+            }
+          }
+        }
+      }
+    }
+
+    ts.forEachChild(node, visitNode);
+  };
+
+  visitNode(sourceFile);
+
+  return componentClassNames;
+}
+
+function generatePreviewsComponentMap(data: AnalyzedFiles[]): string {
+  let result = '';
+  for (const fileData of data) {
+    for (const componentName of fileData.componentNames) {
+      const key = `adev/src/content/examples/${fileData.path}${
+        fileData.componentNames.length > 1 ? '_' + componentName : ''
+      }`.replace(/\\/g, '/');
+      result += `['${key}']: () => import('${EXAMPLES_PATH}/${fileData.path
+        .replace(/\\/g, '/')
+        .replace('.ts', '')}').then(c => c.${componentName}),\n`;
+    }
+  }
+  return fs.readFileSync(templateFilePath, 'utf8').replace(/\${previewsComponents}/g, result);
+}

adev/shared-docs/pipeline/examples/previews/previews.template
@@ -6,16 +6,12 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-/**
- ******************************************************************************
- * DO NOT MANUALLY EDIT THIS FILE. THIS FILE IS AUTOMATICALLY GENERATED.
- ******************************************************************************
- */
-
 import {Type} from '@angular/core';
 
 /**
  * Map of the previews components, values are functions which returns the promise of the component type, which will be displayed as preview in the ExampleViewer component.
  * Keys has to be equal to paths written down in the docs markdown files.
  */
-export const PREVIEWS_COMPONENTS_MAP: Record<string, () => Promise<Type<unknown>>> = {};
+export const PREVIEWS_COMPONENTS_MAP: Record<string, () => Promise<Type<unknown>>> = {
+  ${previewsComponents}
+};

adev/src/assets/previews/BUILD.bazel
@@ -1,15 +1,7 @@
-load("@build_bazel_rules_nodejs//:index.bzl", "copy_to_bin")
+load("//adev/shared-docs:index.bzl", "generate_previews")
 
-# TODO(josephperrott): Generate previews from source instead of using already generated files
-
-exports_files(
-    glob(["*"]),
-)
-
-copy_to_bin(
+generate_previews(
     name = "previews",
-    srcs = glob(["*"]),
-    visibility = [
-        "//visibility:public",
-    ],
+    example_srcs = "//adev/src/content/examples",
+    visibility = ["//visibility:public"],
 )

adev/src/content/examples/BUILD.bazel
@@ -1,0 +1,7 @@
+filegroup(
+    name = "examples",
+    srcs = [
+        "//adev/src/content/examples/accessibility:ts_files",
+    ],
+    visibility = ["//visibility:public"],
+)

adev/src/content/examples/accessibility/BUILD.bazel
@@ -1,4 +1,25 @@
+load("//tools:defaults.bzl", "ng_module")
+
 package(default_visibility = ["//visibility:public"])
+
+filegroup(
+    name = "ts_files",
+    srcs = glob(["src/app/**/*.ts"]),
+    visibility = ["//visibility:public"],
+)
+
+ng_module(
+    name = "example",
+    srcs = [":ts_files"],
+    assets = glob([
+        "src/app/**/*.html",
+        "src/app/**/*.css",
+    ]),
+    visibility = ["//visibility:public"],
+    deps = [
+        "@npm//@angular/core",
+    ],
+)
 
 exports_files([
     "src/app/app.component.html",