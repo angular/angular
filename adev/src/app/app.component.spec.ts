a91c6d4edfdf571f1c21bf35573549bf0d147526
Matthieu Riegler <kyro38@gmail.com>
2025-04-15 11:21:54 +0200

    docs(docs-infra): Version info are pulled from an json file.
    It should pull the file from the main adev site and fallback to local data is anything goes wrong

--- adev/src/app/app.component.spec.ts
+++ adev/src/app/app.component.spec.ts
@@ -11,16 +11,18 @@ import {AppComponent} from './app.component';
 import {provideRouter, withComponentInputBinding} from '@angular/router';
 import {routes} from './routes';
 import {Search, WINDOW} from '@angular/docs';
-import {CURRENT_MAJOR_VERSION} from './core/providers/current-version';
+import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   const fakeSearch = {};
   const fakeWindow = {location: {hostname: 'angular.dev'}};
-  const fakeCurrentMajorVersion = 19;
 
   it('should create the app', () => {
     TestBed.configureTestingModule({
       providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
         provideRouter(routes, withComponentInputBinding()),
         {
           provide: WINDOW,
@@ -29,10 +31,6 @@ describe('AppComponent', () => {
         {
           provide: Search,
           useValue: fakeSearch,
-        },
-        {
-          provide: CURRENT_MAJOR_VERSION,
-          useValue: fakeCurrentMajorVersion,
         },
       ],
     });
