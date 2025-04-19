8f87b4c9bdc2d68c83e1c1bf955ff78254e64cb6
Matthieu Riegler <kyro38@gmail.com>
2025-04-19 22:02:07 +0200

    docs(docs-infra): remove style guide examples

--- adev/src/content/examples/styleguide/src/05-12/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-import {HeroButtonComponent} from './heroes/shared/hero-button/hero-button.component';
-
-@Component({
-  selector: 'sg-app',
-  template: '<toh-hero-button label="OK"></toh-hero-button>',
-  imports: [HeroButtonComponent],
-})
-export class AppComponent {}
