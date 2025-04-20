03a5d1cb001d6ed1ca69679463200606b31da238
Matthieu Riegler <kyro38@gmail.com>
2025-04-20 15:57:51 +0200

    build(devtools): cleanup dependencies
    Removing `platform-browser-dynamic` because we don't need it anymore.

--- devtools/projects/demo-no-zone/src/main.ts
+++ devtools/projects/demo-no-zone/src/main.ts
@@ -6,12 +6,10 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
+import {provideExperimentalZonelessChangeDetection} from '@angular/core';
 
-import {AppModule} from './app/app.module';
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule, {
-    ngZone: 'noop',
-  })
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideExperimentalZonelessChangeDetection()],
+}).catch((err) => console.error(err));
