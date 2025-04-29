0c9e7d980e261501842a5548c92befae20c39b83
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-04-29 09:45:54 +0000

    test: add integration test for defer with input on SSR with zones (#61040)
    Add an integration test to verify defer block behavior with input handling during server-side rendering (SSR).
    
    PR Close #61040

---
+++ integration/platform-server/projects/standalone/src/app/defer/defer.component.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+import {Component} from '@angular/core';
+import {HydratedComponent} from './hydrated.component';
+
+@Component({
+  selector: 'app-defer',
+  imports: [HydratedComponent],
+  template: `
+  @defer (hydrate never) {
+    <app-hydrated [title]="hydrateNeverTitle" />
+  }`,
+})
+export class DeferComponent {
+  hydrateNeverTitle = 'Hydrate Never';
+}
