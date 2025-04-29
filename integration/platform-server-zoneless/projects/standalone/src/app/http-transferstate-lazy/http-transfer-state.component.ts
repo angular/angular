400dbc5b89a2af0ae5fd7830f6ea47352c8556ef
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-04-29 15:49:26 +0000

    fix(core): properly handle app stabilization with defer blocks (#61056)
    Previously, the app was marked as stable prematurely. For more details, see https://github.com/angular/angular/issues/61038#issuecomment-2837917180
    
    Closes: #61038
    (cherry picked from commit de649c9cfd945b7d4f7df0533fa884a56a3d4158)
    
    PR Close #61056

--- integration/platform-server-zoneless/projects/standalone/src/app/http-transferstate-lazy/http-transfer-state.component.ts
+++ integration/platform-server-zoneless/projects/standalone/src/app/http-transferstate-lazy/http-transfer-state.component.ts
@@ -7,7 +7,7 @@
  */
 
 import {HttpClient} from '@angular/common/http';
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit} from '@angular/core';
 
 @Component({
   selector: 'transfer-state-http',
@@ -17,15 +17,19 @@ import {Component, OnInit} from '@angular/core';
     <div class="two">{{ responseTwo }}</div>
   `,
   providers: [HttpClient],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TransferStateComponent implements OnInit {
   responseOne: string = '';
   responseTwo: string = '';
+  private readonly httpClient: HttpClient = inject(HttpClient);
+  private readonly cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
 
-  constructor(private readonly httpClient: HttpClient) {
+  constructor() {
     // Test that HTTP cache works when HTTP call is made in the constructor.
     this.httpClient.get<any>('http://localhost:4206/api').subscribe((response) => {
       this.responseOne = response.data;
+      this.cdr.markForCheck();
     });
   }
 
@@ -33,6 +37,7 @@ export class TransferStateComponent implements OnInit {
     // Test that HTTP cache works when HTTP call is made in a lifecycle hook.
     this.httpClient.get<any>('/api-2').subscribe((response) => {
       this.responseTwo = response.data;
+      this.cdr.markForCheck();
     });
   }
 }
