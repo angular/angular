152920a1b1f4e1a07e2df81688e5982f337dc22f
Miles Malerba <mmalerba@google.com>
2025-05-06 23:00:15 +0000

    refactor: refactor builder api

--- packages/forms/experimental/src/logic_node_2.ts
+++ packages/forms/experimental/src/logic_node_2.ts
@@ -7,6 +7,135 @@ import {
   Predicate,
 } from './logic_node';
 
+abstract class AbstractLogicNodeBuilder {
+  constructor(readonly predicates: ReadonlyArray<Predicate>) {}
+
+  abstract addHiddenRule(logic: LogicFn<unknown, boolean>): void;
+  abstract addDisabledRule(logic: LogicFn<unknown, boolean>): void;
+  abstract addErrorRule(logic: LogicFn<unknown, ValidationResult>): void;
+  abstract addMetadataRule<T>(key: MetadataKey<T>, logic: LogicFn<unknown, T>): void;
+  abstract getChild(key: PropertyKey): LogicNodeBuilder;
+  abstract predicate(predicates: ReadonlyArray<Predicate>): AbstractLogicNodeBuilder;
+
+  build(): LogicNode {
+    return new LogicNode(this);
+  }
+}
+
+/**
+ * A builder for `LogicNode`. Used to add logic to the final `LogicNode` tree.
+ */
+export class LogicNodeBuilder extends AbstractLogicNodeBuilder {
+  private current: NonMergableLogicNodeBuilder | undefined;
+  readonly all: AbstractLogicNodeBuilder[] = [];
+
+  constructor(predicates: ReadonlyArray<Predicate>) {
+    super(predicates);
+  }
+
+  addHiddenRule(logic: LogicFn<unknown, boolean>): void {
+    this.getCurrent().addHiddenRule(logic);
+  }
+
+  addDisabledRule(logic: LogicFn<unknown, boolean>): void {
+    this.getCurrent().addDisabledRule(logic);
+  }
+
+  addErrorRule(logic: LogicFn<unknown, ValidationResult>): void {
+    this.getCurrent().addErrorRule(logic);
+  }
+
+  addMetadataRule<T>(key: MetadataKey<T>, logic: LogicFn<unknown, T>): void {
+    this.getCurrent().addMetadataRule(key, logic);
+  }
+
+  getChild(key: PropertyKey): LogicNodeBuilder {
+    return this.getCurrent().getChild(key);
+  }
+
+  predicate(predicates: ReadonlyArray<Predicate>) {
+    const newPredicates = [...this.predicates, ...predicates];
+    const clone = new LogicNodeBuilder(newPredicates);
+    clone.all.push(...this.all.map((b) => b.predicate(newPredicates)));
+    clone.current = this.current?.predicate(newPredicates);
+    return clone;
+  }
+
+  mergeIn(other: LogicNodeBuilder, predicate?: Predicate): void {
+    // Add the other builder to our collection, we'll defer the actual merging of the logic until
+    // the logic node is requested to be created. In order to preserve the original ordering of the
+    // rules, we close off the current builder to any further edits. If additional logic is added,
+    // a new current builder will be created to capture it.
+    const predicates = [...this.predicates];
+    if (predicate) {
+      predicates.push(predicate);
+    }
+    if (predicates.length !== 0) {
+      this.all.push(other.predicate(predicates));
+    } else {
+      this.all.push(other);
+    }
+    this.current = undefined;
+  }
+
+  private getCurrent(): NonMergableLogicNodeBuilder {
+    // All rules added to this builder get added on to the current builder. If there is no current
+    // builder, a new one is created. In order to preserve the original ordering of the rules, we
+    // clear the current builder whenever a separate builder tree is merged in.
+    if (this.current === undefined) {
+      this.current = new NonMergableLogicNodeBuilder(this.predicates);
+      this.all.push(this.current);
+    }
+    return this.current;
+  }
+
+  static newRoot(): LogicNodeBuilder {
+    return new LogicNodeBuilder([]);
+  }
+}
+
+/**
+ * A type of `AbstractLogicNodeBuilder` used internally by the `LogicNodeBuilder` to record "pure"
+ * chunks of logic that do not require merging in other builders.
+ */
+class NonMergableLogicNodeBuilder extends AbstractLogicNodeBuilder {
+  logic = new Logic([]);
+  readonly children = new Map<PropertyKey, LogicNodeBuilder>();
+
+  override addHiddenRule(logic: LogicFn<unknown, boolean>): void {
+    this.logic.hidden.push(logic);
+  }
+
+  override addDisabledRule(logic: LogicFn<unknown, boolean>): void {
+    this.logic.disabled.push(logic);
+  }
+
+  override addErrorRule(logic: LogicFn<unknown, ValidationResult>): void {
+    this.logic.errors.push(logic);
+  }
+
+  override addMetadataRule<T>(key: MetadataKey<T>, logic: LogicFn<unknown, T>): void {
+    this.logic.getMetadata(key).push(logic);
+  }
+
+  override predicate(predicates: ReadonlyArray<Predicate>) {
+    const newPredicates = [...this.predicates, ...predicates];
+    const clone = new NonMergableLogicNodeBuilder(newPredicates);
+    for (const [prop, child] of this.children) {
+      clone.children.set(prop, child.predicate(newPredicates));
+    }
+    clone.logic = this.logic;
+    return clone;
+  }
+
+  override getChild(key: PropertyKey): LogicNodeBuilder {
+    if (!this.children.has(key)) {
+      this.children.set(key, new LogicNodeBuilder(this.predicates));
+    }
+    return this.children.get(key)!;
+  }
+}
+
 /**
  * Container for all the different types of logic that can be applied to a field
  * (disabled, hidden, errors, etc.)
@@ -49,10 +178,11 @@ export class Logic {
 export class LogicNode {
   readonly logic: Logic;
 
-  constructor(private builder: LogicNodeBuilder | undefined) {
+  constructor(private builder: AbstractLogicNodeBuilder | undefined) {
     this.logic = builder ? createLogic(builder) : new Logic([]);
   }
 
+  // TODO: cache here, or just rely on the user of this API to do caching?
   getChild(key: PropertyKey): LogicNode {
     // The logic for a particular child may be spread across multiple builders. We lazily combine
     // this logic at the time the child logic node is requested to be created.
@@ -72,154 +202,12 @@ export class LogicNode {
 }
 
 /**
- * A builder for `LogicNode`, which itself is a tree.
- */
-export abstract class LogicNodeBuilder {
-  protected constructor(readonly predicates: ReadonlyArray<Predicate>) {}
-
-  abstract addHiddenRule(logic: LogicFn<unknown, boolean>): void;
-  abstract addDisabledRule(logic: LogicFn<unknown, boolean>): void;
-  abstract addErrorRule(logic: LogicFn<unknown, ValidationResult>): void;
-  abstract addMetadataRule<T>(key: MetadataKey<T>, logic: LogicFn<unknown, T>): void;
-  abstract getChild(key: PropertyKey): MergableLogicNodeBuilder;
-  abstract predicate(predicates: ReadonlyArray<Predicate>): LogicNodeBuilder;
-
-  build(): LogicNode {
-    return new LogicNode(this);
-  }
-
-  static newRoot(): MergableLogicNodeBuilder {
-    return new CompositeLogicNodeBuilder([]);
-  }
-}
-
-/**
- * A subclass of `LogicNodeBuilder` that supports merging with other logic trees.
- */
-export interface MergableLogicNodeBuilder extends LogicNodeBuilder {
-  mergeIn(other: MergableLogicNodeBuilder, predicate?: Predicate): void;
-  predicate(predicates: ReadonlyArray<Predicate>): MergableLogicNodeBuilder;
-}
-
-/**
- * A `MergableLogicNodeBuilder` that delegates to its "current" builder and creates a new current
- * builder after another builder tree is merged in.
- */
-class CompositeLogicNodeBuilder extends LogicNodeBuilder implements MergableLogicNodeBuilder {
-  private current: SimpleLogicNodeBuilder | undefined;
-  readonly all: LogicNodeBuilder[] = [];
-
-  override addHiddenRule(logic: LogicFn<unknown, boolean>): void {
-    this.getCurrent().addHiddenRule(logic);
-  }
-
-  override addDisabledRule(logic: LogicFn<unknown, boolean>): void {
-    this.getCurrent().addDisabledRule(logic);
-  }
-
-  override addErrorRule(logic: LogicFn<unknown, FormError>): void {
-    this.getCurrent().addErrorRule(logic);
-  }
-
-  override addMetadataRule<T>(key: MetadataKey<T>, logic: LogicFn<unknown, T>): void {
-    this.getCurrent().addMetadataRule(key, logic);
-  }
-
-  override getChild(key: PropertyKey): MergableLogicNodeBuilder {
-    return this.getCurrent().getChild(key);
-  }
-
-  override predicate(predicates: ReadonlyArray<Predicate>) {
-    const newPredicates = [...this.predicates, ...predicates];
-    const clone = new CompositeLogicNodeBuilder(newPredicates);
-    clone.all.push(...this.all.map((b) => b.predicate(newPredicates)));
-    clone.current = this.current?.predicate(newPredicates);
-    return clone;
-  }
-
-  mergeIn(other: MergableLogicNodeBuilder, predicate?: Predicate): void {
-    // Add the other builder to our collection, we'll defer the actual merging of the logic until
-    // the logic node is requested to be created. In order to preserve the original ordering of the
-    // rules, we close off the current builder to any further edits. If additional logic is added,
-    // a new current builder will be created to capture it.
-    const predicates = [...this.predicates];
-    if (predicate) {
-      predicates.push(predicate);
-    }
-    if (predicates.length !== 0) {
-      this.all.push(other.predicate(predicates));
-    } else {
-      this.all.push(other);
-    }
-    this.current = undefined;
-  }
-
-  private getCurrent(): SimpleLogicNodeBuilder {
-    // All rules added to this builder get added on to the current builder. If there is no current
-    // builder, a new one is created. In order to preserve the original ordering of the rules, we
-    // clear the current builder whenever a separate builder tree is merged in.
-    if (this.current === undefined) {
-      this.current = new SimpleLogicNodeBuilder(this.predicates);
-      this.all.push(this.current);
-    }
-    return this.current;
-  }
-}
-
-/**
- * A `LogicNodeBuilder` that directly adds logic to its backing `Logic` instance.
- *
- * The user should not be given a reference to this class, it is used internally to keep track of
- * concrete tree chunks within the `CompositeLogicNodeBuilder`. When handing a `LogicNodeBuilder`
- * to the user it should always be a `CompositeLogicNodeBuilder`.
- */
-class SimpleLogicNodeBuilder extends LogicNodeBuilder {
-  logic = new Logic([]);
-  readonly children = new Map<PropertyKey, MergableLogicNodeBuilder>();
-
-  override addHiddenRule(logic: LogicFn<unknown, boolean>): void {
-    this.logic.hidden.push(logic);
-  }
-
-  override addDisabledRule(logic: LogicFn<unknown, boolean>): void {
-    this.logic.disabled.push(logic);
-  }
-
-  override addErrorRule(logic: LogicFn<unknown, ValidationResult>): void {
-    this.logic.errors.push(logic);
-  }
-
-  override addMetadataRule<T>(key: MetadataKey<T>, logic: LogicFn<unknown, T>): void {
-    this.logic.getMetadata(key).push(logic);
-  }
-
-  override predicate(predicates: ReadonlyArray<Predicate>) {
-    const newPredicates = [...this.predicates, ...predicates];
-    const clone = new SimpleLogicNodeBuilder(newPredicates);
-    for (const [prop, child] of this.children) {
-      clone.children.set(prop, child.predicate(newPredicates));
-    }
-    clone.logic = this.logic;
-    return clone;
-  }
-
-  override getChild(key: PropertyKey): MergableLogicNodeBuilder {
-    // We always create a `CompositeLogicNodeBuilder` for children since someone may call `apply` on
-    // the child.
-    if (!this.children.has(key)) {
-      this.children.set(key, new CompositeLogicNodeBuilder(this.predicates));
-    }
-    return this.children.get(key)!;
-  }
-}
-
-/**
  * Gets all of the builders that contribute logic to the given child of the parent builder.
  */
-function getAllChildren(builder: LogicNodeBuilder, key: PropertyKey): MergableLogicNodeBuilder[] {
-  if (builder instanceof CompositeLogicNodeBuilder) {
+function getAllChildren(builder: AbstractLogicNodeBuilder, key: PropertyKey): LogicNodeBuilder[] {
+  if (builder instanceof LogicNodeBuilder) {
     return builder.all.flatMap((node) => getAllChildren(node, key));
-  } else if (builder instanceof SimpleLogicNodeBuilder) {
+  } else if (builder instanceof NonMergableLogicNodeBuilder) {
     if (builder.children.has(key)) {
       return [builder.children.get(key)!];
     }
@@ -232,9 +220,10 @@ function getAllChildren(builder: LogicNodeBuilder, key: PropertyKey): MergableLo
 /**
  * Creates the full `Logic` for a given builder.
  */
-function createLogic(builder: LogicNodeBuilder): Logic {
-  const logic = new Logic(builder.predicates);
-  if (builder instanceof CompositeLogicNodeBuilder) {
+function createLogic(builder: AbstractLogicNodeBuilder): Logic {
+  if (builder instanceof LogicNodeBuilder) {
+    // Build the logic for all sub-builders, and merge them into one.
+    const logic = new Logic([]);
     const builtNodes = builder.all.map((b) => b.build());
     for (const node of builtNodes) {
       logic.disabled.mergeIn(node.logic.disabled);
@@ -244,15 +233,17 @@ function createLogic(builder: LogicNodeBuilder): Logic {
         logic.getMetadata(key).mergeIn(node.logic.getMetadata(key));
       }
     }
-  } else if (builder instanceof SimpleLogicNodeBuilder) {
+    return logic;
+  } else if (builder instanceof NonMergableLogicNodeBuilder) {
+    const logic = new Logic(builder.predicates);
     logic.disabled.mergeIn(builder.logic.disabled);
     logic.hidden.mergeIn(builder.logic.hidden);
     logic.errors.mergeIn(builder.logic.errors);
     for (const key of builder.logic.getMetadataKeys()) {
       logic.getMetadata(key).mergeIn(builder.logic.getMetadata(key));
     }
+    return logic;
   } else {
     throw new Error('Unknown LogicNodeBuilder type');
   }
-  return logic;
 }
