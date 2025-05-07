efe498698c496671f1db9533c4ac30ee99c52b4b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-05-07 17:35:17 +0000

    build: migrate `compiler-cli/src/ngtsc` to `ts_project`
    Migrates all of `compiler-cli/src/ngtsc` to `ts_project`. This change
    was generated using Gemini.

--- packages/compiler-cli/src/ngtsc/transform/test/compilation_spec.ts
+++ packages/compiler-cli/src/ngtsc/transform/test/compilation_spec.ts
@@ -5,8 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-import {ConstantPool} from '@angular/compiler';
-import * as o from '@angular/compiler/src/output/output_ast';
+import {ConstantPool, outputAst as o} from '@angular/compiler';
 
 import {absoluteFrom} from '../../file_system';
 import {runInEachFileSystem} from '../../file_system/testing';
