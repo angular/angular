e0d378d20e768d353b01bc28e10ad53c5485b426
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-09 18:04:40 +0200

    fix(compiler): incorrectly handling let declarations inside i18n (#60512)
    The compiler wasn't handling `@let` declarations placed inside i18n blocks. The problem is that `assignI18nSlotDependencies` phase assigns the `target` of i18n ops much earlier than the `@let` optimization. If the `@let` ends up getting optimized because it isn't used in any child views, the pointer in the i18n instruction becomes invalid. This hadn't surfaced so far, because we didn't optimize `declareLet` ops, however once we do, we start hitting assertions that the optimized `declareLet` isn't used anywhere.
    
    These changes resolve the issue by moving the i18n phases after the `@let` optimization.
    
    PR Close #60512

---
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_let/let_in_i18n_template.js
@@ -0,0 +1,36 @@
+$r3$.ɵɵdefineComponent({
+  …
+  decls: 2,
+  vars: 1,
+  consts: () => {
+    let $i18n_0$;
+    if (typeof ngI18nClosureMode !== "undefined" && ngI18nClosureMode) {
+    /**
+     * @suppress {msgDescriptions}
+     */
+      const $MSG_ID_WITH_SUFFIX$ = goog.getMsg(" The result is {$interpolation} ", {
+        "interpolation": "\uFFFD0\uFFFD"
+      }, {
+        original_code: { "interpolation": "{{result}}" }
+      });
+      $i18n_0$ = $MSG_ID_WITH_SUFFIX$;
+    } else {
+      $i18n_0$ = $localize ` The result is ${"\uFFFD0\uFFFD"}:INTERPOLATION: `;
+    }
+    return [$i18n_0$];
+  },
+  template: function MyApp_Template(rf, ctx) {
+    if (rf & 1) {
+      $r3$.ɵɵelementStart(0, "div");
+      $r3$.ɵɵi18n(1, 0);
+      $r3$.ɵɵelementEnd();
+    }
+    if (rf & 2) {
+      const result_r1 = ctx.value * 2;
+      $r3$.ɵɵadvance();
+      $r3$.ɵɵi18nExp(result_r1);
+      $r3$.ɵɵi18nApply(1);
+    }
+  },
+  …
+});
