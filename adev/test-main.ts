3d9cb638473274dea402f8b5b485f97c0eb672f3
Andrew Scott <atscott@google.com>
2025-05-09 07:46:50 -0700

    refactor(docs-infra): Remove rethrowing error handler (#61243)
    This is no longer necessary since the work in the FW to rethrow in the
    TestBed error handler.
    
    PR Close #61243

--- adev/test-main.ts
+++ adev/test-main.ts
@@ -6,22 +6,12 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {ErrorHandler, NgModule, provideZonelessChangeDetection} from '@angular/core';
+import {NgModule, provideZonelessChangeDetection} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
 import {BrowserTestingModule, platformBrowserTesting} from '@angular/platform-browser/testing';
 
 @NgModule({
-  providers: [
-    provideZonelessChangeDetection(),
-    {
-      provide: ErrorHandler,
-      useValue: {
-        handleError: (e: unknown) => {
-          throw e;
-        },
-      },
-    },
-  ],
+  providers: [provideZonelessChangeDetection()],
 })
 export class TestModule {}
 
