378dea9bc47e357bdb9a1f9b6b3bb84444f274ad
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-05-15 11:26:19 +0000

    build: migrate more targets of `@angular/core` to `ts_project`
    Migrates more targets of `@angular/core` to `ts_project`. Remaining are:
    
     - tests
     - schematics

--- packages/core/testing/src/fake_async.ts
+++ packages/core/testing/src/fake_async.ts
@@ -5,6 +5,10 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
+
+// Needed for the global `Zone` ambient types to be available.
+import type {} from 'zone.js';
+
 const _Zone: any = typeof Zone !== 'undefined' ? Zone : null;
 const fakeAsyncTestModule = _Zone && _Zone[_Zone.__symbol__('fakeAsyncTest')];
 
