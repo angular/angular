4282dc3a5488182917d10de268f29e4bf30f2833
Matthieu Riegler <kyro38@gmail.com>
2025-05-16 17:56:16 +0200

    docs: replace references to `platform-brower-dynamic`  with `platform-brower` (#61428)
    PR Close #61428

--- adev/src/content/examples/testing/src/app/demo/demo-main.ts
+++
@@ -1,5 +0,0 @@
-// main app entry point
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {DemoModule} from './demo';
-
-platformBrowserDynamic().bootstrapModule(DemoModule);
