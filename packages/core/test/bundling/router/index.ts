a037a5be309d604052ffd65eb6065f281bee0111
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-05-19 13:07:04 +0000

    build: migrate all `ng_module` in `packages/core/test`
    Migrates all `ng_module` to `ng_project` in `packages/core/test/`.

--- packages/core/test/bundling/router/index.ts
+++ packages/core/test/bundling/router/index.ts
@@ -11,6 +11,7 @@ import {Component, OnInit} from '../../../src/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {
   ActivatedRoute,
+  ParamMap,
   provideRouter,
   Router,
   RouterLink,
@@ -48,7 +49,7 @@ class ItemComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.activatedRoute.paramMap.subscribe((paramsMap) => {
+    this.activatedRoute.paramMap.subscribe((paramsMap: ParamMap) => {
       this.id = +paramsMap.get('id')!;
     });
   }
