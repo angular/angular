e11f48f65e881e7a79160091f2765dd32d574335
Joey Perrott <josephperrott@gmail.com>
2025-05-20 19:54:56 +0000

    build: migrate forms to rules_js (#61535)
    Migrate forms to use ng_project and ts_project
    
    PR Close #61535

--- packages/forms/test/template_integration_spec.ts
+++ packages/forms/test/template_integration_spec.ts
@@ -25,7 +25,7 @@ import {
   NgModel,
   Validator,
 } from '../index';
-import {By} from '@angular/platform-browser/src/dom/debug/by';
+import {By} from '@angular/platform-browser';
 import {dispatchEvent, sortedClassList} from '@angular/private/testing';
 import {merge} from 'rxjs';
 
