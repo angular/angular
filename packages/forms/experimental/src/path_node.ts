e61da79b3cd77f870c2b224c4a4c49b80ef8cc7a
Kirill Cherkashin <kirts@google.com>
2025-05-20 15:52:23 -0400

    feat(forms): Support marking a field as dirty (#61183)
    Co-authored-by: kirjs <kirjs@users.noreply.github.com>

--- packages/forms/experimental/src/path_node.ts
+++ packages/forms/experimental/src/path_node.ts
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 import {FieldPath} from './api/types';
 import {DYNAMIC, FieldLogicNode, Predicate} from './logic_node';
 
