32ae421526903b2bc2926f37d1851296ffa0c64e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-21 12:25:23 +0200

    refactor(compiler): replace attribute interpolation instructions (#61557)
    Replaces the attribute interpolation instructions with `attribute` plus the new `interpolateX` instruction. This allows to reduce our overall instruction footprint.
    
    PR Close #61557

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_bindings/attribute_bindings/chain_multiple_bindings_mixed.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_bindings/attribute_bindings/chain_multiple_bindings_mixed.js
@@ -1,7 +1,7 @@
 template: function MyComponent_Template(rf, ctx) {
   …
   if (rf & 2) {
-    $r3$.ɵɵattributeInterpolate1("aria-label", "prefix-", 1 + 3);
+    $r3$.ɵɵattribute("aria-label", $r3$.ɵɵinterpolate1("prefix-", 1 + 3));
     $r3$.ɵɵproperty("id", 2);
     $r3$.ɵɵattribute("title", 1)("tabindex", 3);
   }
