24bab55f0c89c4fe6037780fd7b2e8c8aa5429b2
Matthieu Riegler <kyro38@gmail.com>
2025-05-22 05:50:56 +0200

    fix(compiler): lexer support for template literals in object literals (#61601)
    This commit fixes a shortcoming of the lexer with template literals
    
    fixes #61572
    
    PR Close #61601

--- packages/compiler/test/expression_parser/parser_spec.ts
+++ packages/compiler/test/expression_parser/parser_spec.ts
@@ -439,6 +439,13 @@ describe('parser', () => {
         checkBinding('`hello ${(name | capitalize)}!!!`');
       });
 
+      it('should parse template literals in objects literals', () => {
+        checkBinding('{"a": `${name}`}');
+        checkBinding('{"a": `hello ${name}!`}');
+        checkBinding('{"a": `hello ${`hello ${`hello`}`}!`}');
+        checkBinding('{"a": `hello ${{"b": `hello`}}`}');
+      });
+
       it('should report error if interpolation is empty', () => {
         expectBindingError(
           '`hello ${}`',
