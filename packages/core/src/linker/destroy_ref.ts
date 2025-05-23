61088c1e301967b696141182c5b05bba20e335ab
Andrew Scott <atscott@google.com>
2025-05-23 13:37:13 -0700

    fixup! Revert "fix(core): call `DestroyRef` on destroy callback if view is destroyed (#58008)"

--- packages/core/src/linker/destroy_ref.ts
+++ packages/core/src/linker/destroy_ref.ts
@@ -6,10 +6,13 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {EnvironmentInjector} from '../di';
-import {LView} from '../render3/interfaces/view';
+import type {EnvironmentInjector} from '../di';
+import {isDestroyed} from '../render3/interfaces/type_checks';
+import type {LView} from '../render3/interfaces/view';
 import {getLView} from '../render3/state';
 import {removeLViewOnDestroy, storeLViewOnDestroy} from '../render3/util/view_utils';
+
+const EXECUTE_CALLBACK_IF_ALREADY_DESTROYED = false;
 
 /**
  * `DestroyRef` lets you set callbacks to run for any cleanup or destruction behavior.
@@ -62,8 +65,18 @@ export class NodeInjectorDestroyRef extends DestroyRef {
   }
 
   override onDestroy(callback: () => void): () => void {
-    storeLViewOnDestroy(this._lView, callback);
-    return () => removeLViewOnDestroy(this._lView, callback);
+    const lView = this._lView;
+
+    // TODO(atscott): Remove after g3 cleanup is finished
+    if (isDestroyed(lView) && EXECUTE_CALLBACK_IF_ALREADY_DESTROYED) {
+      callback();
+      // We return a "noop" callback, which, when executed, does nothing because
+      // we haven't stored anything on the `lView`, and thus there's nothing to remove.
+      return () => {};
+    }
+
+    storeLViewOnDestroy(lView, callback);
+    return () => removeLViewOnDestroy(lView, callback);
   }
 }
 
