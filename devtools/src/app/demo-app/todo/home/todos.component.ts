a42169a7ab6d6a49b30cfe4d9967a617a0614847
AleksanderBodurri <alexbodurri1117@gmail.com>
2025-06-15 19:10:52 -0400

    test(devtools): revive cypress tests (#61972)
    Previously these tests would run automatically when Angular DevTools lived in another repo. These files have continued to live here but have not been running automatically on each PR.
    
    Now, these test files have been revived to run properly with our changes since the repo merge. This is a first step to reviving our e2e testing.
    
    Next steps include writing cypress tests for new features like Injector Graph, Router tree, signals visualizations, etc.
    
    PR Close #61972

--- devtools/src/app/demo-app/todo/home/todos.component.ts
+++ devtools/src/app/demo-app/todo/home/todos.component.ts
@@ -28,6 +28,8 @@ const fib = (n: number): number => {
   imports: [RouterLink, TodoComponent, TooltipDirective, SamplePipe, TodosFilter],
 })
 export class TodosComponent implements OnInit, OnDestroy {
+  title = 'Angular Todo';
+
   todos: Todo[] = [
     {
       label: 'Buy milk',
