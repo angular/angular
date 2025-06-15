d93325061dff9d3f6eb8080d2afc6755fbb51b26
AleksanderBodurri <alexbodurri1117@gmail.com>
2025-06-15 19:10:52 -0400

    test(devtools): revive cypress tests
    Previously these tests would run automatically when Angular DevTools lived in another repo. These files have continued to live here but have not been running automatically on each PR.
    
    Now, these test files have been revived to run properly with our changes since the repo merge. This is a first step to reviving our e2e testing.
    
    Next steps include writing cypress tests for new features like Injector Graph, Router tree, signals visualizations, etc.

--- devtools/cypress/integration/view-component-metadata.e2e.js
+++ devtools/cypress/integration/view-component-metadata.e2e.js
@@ -16,49 +16,49 @@ describe('Viewing component metadata', () => {
     cy.visit('/');
   });
 
-  describe('viewing TodoComponent', () => {
+  describe('viewing TodosComponent', () => {
     beforeEach(() =>
-      prepareHeaderExpansionPanelForAssertions('.tree-node:contains("app-todo[TooltipDirective]")'),
+      prepareHeaderExpansionPanelForAssertions('ng-tree-node:contains("app-todos")'),
     );
 
     it('should display view encapsulation', () => {
-      cy.contains('.meta-data-container .mat-button:first', 'View Encapsulation: Emulated');
+      cy.contains('.meta-data-container', 'View Encapsulation: None');
     });
 
     it('should display change detection strategy', () => {
-      cy.contains('.meta-data-container .mat-button:last', 'Change Detection Strategy: OnPush');
+      cy.contains('.meta-data-container', 'Change Detection Strategy: Default');
     });
   });
 
   describe('viewing DemoAppComponent', () => {
     beforeEach(() =>
-      prepareHeaderExpansionPanelForAssertions('.tree-node:contains("app-demo-component")'),
+      prepareHeaderExpansionPanelForAssertions('ng-tree-node:contains("app-demo-component")'),
     );
 
     it('should display view encapsulation', () => {
-      cy.contains('.meta-data-container .mat-button:first', 'View Encapsulation: None');
+      cy.contains('.meta-data-container', 'View Encapsulation: None');
     });
 
     it('should display change detection strategy', () => {
-      cy.contains('.meta-data-container .mat-button:last', 'Change Detection Strategy: Default');
+      cy.contains('.meta-data-container', 'Change Detection Strategy: Default');
     });
 
     it('should display correct set of inputs', () => {
-      cy.contains('.cy-inputs', 'Inputs');
-      cy.contains('.cy-inputs mat-tree-node:first span:first', 'inputOne');
-      cy.contains('.cy-inputs mat-tree-node:last span:first', 'inputTwo');
+      cy.contains('.mat-accordion-content#Inputs', 'Inputs');
+      cy.contains('.mat-accordion-content#Inputs mat-tree-node:first span:first', 'inputOne');
+      cy.contains('.mat-accordion-content#Inputs mat-tree-node:last span:first', 'inputTwo');
     });
 
     it('should display correct set of outputs', () => {
-      cy.contains('.cy-outputs', 'Outputs');
-      cy.contains('.cy-outputs mat-tree-node:first span:first', 'outputOne');
-      cy.contains('.cy-outputs mat-tree-node:last span:first', 'outputTwo');
+      cy.contains('.mat-accordion-content#Outputs', 'Outputs');
+      cy.contains('.mat-accordion-content#Outputs mat-tree-node:first span:first', 'outputOne');
+      cy.contains('.mat-accordion-content#Outputs mat-tree-node:last span:first', 'outputTwo');
     });
 
     it('should display correct set of properties', () => {
-      cy.contains('.cy-properties', 'Properties');
-      cy.contains('.cy-properties mat-tree-node:first span:first', 'elementRef');
-      cy.contains('.cy-properties mat-tree-node:last span:first', 'zippy');
+      cy.contains('.mat-accordion-content#Properties', 'Properties');
+      cy.contains('.mat-accordion-content#Properties mat-tree-node:first span:first', 'elementRef');
+      cy.contains('.mat-accordion-content#Properties mat-tree-node:last span:first', 'zippy');
     });
   });
 });
