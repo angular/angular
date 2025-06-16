c370780a94b11fb4425aa15d207a43941798de9c
iteriani <tomnguyen@google.com>
2025-06-16 14:42:36 -0700

    refactor(core): Add injection primitives for shared services.
    This way, an arbitrary service can implement Angular's service requirements without a hard dependency on @angular/core
    
    ex:
    
    class Foo {
       bar = inject(Bar);
    }
    
    registerInjectable(Foo);

--- packages/core/primitives/di/index.ts
+++ packages/core/primitives/di/index.ts
@@ -6,8 +6,9 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-export {setCurrentInjector, getCurrentInjector} from './src/injector';
+export {setCurrentInjector, getCurrentInjector, inject} from './src/injector';
 export type {Injector} from './src/injector';
 export {NOT_FOUND, NotFoundError, isNotFound} from './src/not_found';
 export type {NotFound} from './src/not_found';
 export type {InjectionToken, ɵɵInjectableDeclaration} from './src/injection_token';
+export {registerInjectable} from './src/injection_token';
