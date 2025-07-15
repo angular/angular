2ab638d5145d01c065e8f894c8a93943283c3ce4
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-15 16:30:56 +0200

    refactor(core): expose function to assert type of variables (#62648)
    Adds the `ɵassertType` function that will be used during type checking to assert the type of a value.
    
    PR Close #62648

--- packages/compiler/src/render3/r3_identifiers.ts
+++ packages/compiler/src/render3/r3_identifiers.ts
@@ -469,4 +469,5 @@ export class Identifiers {
   static InputSignalBrandWriteType = {name: 'ɵINPUT_SIGNAL_BRAND_WRITE_TYPE', moduleName: CORE};
   static UnwrapDirectiveSignalInputs = {name: 'ɵUnwrapDirectiveSignalInputs', moduleName: CORE};
   static unwrapWritableSignal = {name: 'ɵunwrapWritableSignal', moduleName: CORE};
+  static assertType = {name: 'ɵassertType', moduleName: CORE};
 }
