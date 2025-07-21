02efeef2debb14ab208f2ac929f62a33fae41a76
Joey Perrott <josephperrott@gmail.com>
2025-07-21 20:01:17 +0000

    fixup! build: move generating doc files for @angular/ssr to using an intermediate typescript file

--- packages/ssr/docs/_index.ts
+++ packages/ssr/docs/_index.ts
@@ -6,4 +6,8 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
+/**
+ * This re-export is in place to allow us to get the types from @angular/ssr for
+ * generate_api_docs. This reexport cannot be used for importing from @angular/ssr.
+ */
 export * from '@angular/ssr';
