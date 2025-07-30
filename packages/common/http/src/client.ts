7a497ef189ed9dd63b0f885826c9504fd0578593
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 16:17:56 +0200

    Revert "fix(http): add missing http options allowed in fetch API (#62881)"
    This reverts commit ab6033979a3b409738d55d0c01effb378473c05a.

--- packages/common/http/src/client.ts
+++ packages/common/http/src/client.ts
@@ -58,15 +58,12 @@ function addBody<T>(
     reportProgress: options.reportProgress,
     responseType: options.responseType,
     withCredentials: options.withCredentials,
-    credentials: options.credentials,
     transferCache: options.transferCache,
     keepalive: options.keepalive,
     priority: options.priority,
     cache: options.cache,
     mode: options.mode,
     redirect: options.redirect,
-    integrity: options.integrity,
-    referrer: options.referrer,
   };
 }
 
