fdf999da56a471dd4a985e142541a4296eac73c2
Matthieu Riegler <kyro38@gmail.com>
2025-08-21 00:25:31 +0200

    fix(compiler): Keep paraenthesis in Nullish + Boolean expression.
    Ts 5.9 introduced a regression coming from 5.8 when parenthesis aren't generated for expressions like (`(a ?? b) && c`).
    This fix works around this explicitly specifying that we want to keep those parenthesis that we're aware of in this specific case;
    
    This change can be reverted if the root issue (https://github.com/microsoft/TypeScript/issues/61369) is fixed. (but let's keep the tests in any case for the coverage)
    
    fixes #63287

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler/nullish_coalescing/nullish_coalescing_parens_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler/nullish_coalescing/nullish_coalescing_parens_template.js
@@ -5,4 +5,12 @@ if (rf & 2) {
   $i0$.ɵɵtextInterpolate(ctx.x && (ctx.y ?? ctx.z));
   $i0$.ɵɵadvance(2);
   $i0$.ɵɵtextInterpolate((ctx.x == null ? null : ctx.x.y) ?? (ctx.y == null ? null : ctx.y.z));
+  $i0$.ɵɵadvance(2);
+  $i0$.ɵɵtextInterpolate(((ctx.x == null ? null : ctx.x.y) ?? ctx.y) || ctx.z);
+  $i0$.ɵɵadvance(2);
+  $i0$.ɵɵtextInterpolate(((ctx.x == null ? null : ctx.x.y) ?? ctx.y) && ctx.z);
+  $i0$.ɵɵadvance(2);
+  $i0$.ɵɵtextInterpolate(ctx.z || ((ctx.x == null ? null : ctx.x.y) ?? ctx.y));
+  $i0$.ɵɵadvance(2);
+  $i0$.ɵɵtextInterpolate(ctx.z && ((ctx.x == null ? null : ctx.x.y) ?? ctx.y));
 }
