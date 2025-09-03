82da0cf71798ed4d46317bd72676947f2b520dfd
Cheng-Hsuan Tsai <webcat@google.com>
2025-09-03 22:25:50 +0000

    docs(docs-infra): enable using Tailwind CSS in code examples

---
+++ adev/src/content/examples/hello-world/src/app/tailwind-app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  templateUrl: 'tailwind-app.component.html',
+})
+export class HelloWorldComponent {
+  message = 'Hello Angular World!';
+}
