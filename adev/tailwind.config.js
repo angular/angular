f0488bd3168eda2795375da35b6f4d5f159d34ab
Cheng-Hsuan Tsai <webcat@google.com>
2025-09-04 04:23:02 +0000

    fixup! docs(docs-infra): enable using Tailwind CSS in code examples

--- adev/tailwind.config.js
+++ adev/tailwind.config.js
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
