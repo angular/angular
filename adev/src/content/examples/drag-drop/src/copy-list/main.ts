3fb7a120786980267241d012d7c8b259919669f9
Miles Malerba <mmalerba@google.com>
2025-09-05 16:22:44 -0700

    docs: add missing drag & drop docs (#63619)
    Add some documentation that's mentioned in the material.angular.dev
    version of the drag & drop docs, but was missing from the adev version.
    
    PR Close #63619

---
+++ adev/src/content/examples/drag-drop/src/copy-list/main.ts
@@ -0,0 +1,5 @@
+import {bootstrapApplication} from '@angular/platform-browser';
+
+import {CdkDragDropCopyListExample} from './app/app.component';
+
+bootstrapApplication(CdkDragDropCopyListExample);
