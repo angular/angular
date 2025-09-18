33d2ecffbe1d8b83971d5466c03d78a18374abf7
PranamBhat <pranam707@gmail.com>
2025-09-18 11:40:59 +0530

    fix for `bootstrapApplication` on 20.3.2

--- adev/src/main.server.ts
+++ adev/src/main.server.ts
@@ -6,11 +6,10 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {bootstrapApplication, BootstrapContext} from '@angular/platform-browser';
-import {AppComponent} from './app/app.component';
-import {config} from './app/app.config.server';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) =>
-  bootstrapApplication(AppComponent, config, context);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
 
 export default bootstrap;
