41455f53b65f99309eebca1e88ffc8cabb940925
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-09-19 12:56:38 +0000

    wip!

--- adev/src/main.ts
+++ adev/src/main.ts
@@ -11,3 +11,5 @@ import {appConfig} from './app/app.config';
 import {AppComponent} from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+
+console.log('foo12');
