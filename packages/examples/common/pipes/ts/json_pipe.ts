586000dacac2f2cb7dbae3e4dc54fecdbb5602e5
Shuaib Hasan Akib <akibh805@gmail.com>
2025-09-29 19:31:16 +0600

    refactor(common): update pipe examples and remove redundant standalone flag
    Updated pipe examples to explicitly import the pipe for clarity and
    correctness. Also removed `standalone: true` since it is now the default
    and no longer necessary.

--- packages/examples/common/pipes/ts/json_pipe.ts
+++ packages/examples/common/pipes/ts/json_pipe.ts
@@ -6,18 +6,19 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
+import {JsonPipe} from '@angular/common';
 import {Component} from '@angular/core';
 
 // #docregion JsonPipe
 @Component({
   selector: 'json-pipe',
+  imports: [JsonPipe],
   template: `<div>
     <p>Without JSON pipe:</p>
     <pre>{{ object }}</pre>
     <p>With JSON pipe:</p>
     <pre>{{ object | json }}</pre>
   </div>`,
-  standalone: false,
 })
 export class JsonPipeComponent {
   object: Object = {foo: 'bar', baz: 'qux', nested: {xyz: 3, numbers: [1, 2, 3, 4, 5]}};
