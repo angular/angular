d4bd4d0ed1fd40ee7dc8baf43d4873b4b3978a20
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-09-30 15:41:20 +0000

    build: update dependencies and build config
    The VS Code extension build is failing due to some dependency issues. This commit updates the dependencies and build configuration to fix the build.
    
    - Update `pnpm-lock.yaml` to reflect the dependency changes.
    - Update `BUILD.bazel` files to adjust the external dependencies for `esbuild` and to correctly package the VSIX file.

--- vscode-ng-language-service/server/esbuild.mjs
+++ vscode-ng-language-service/server/esbuild.mjs
@@ -2,6 +2,7 @@ import {readFileSync} from 'fs';
 const banner = readFileSync('vscode-ng-language-service/server/banner.js', 'utf8');
 export default {
   banner: {js: banner},
+  mainFields: ['module', 'main'],
   // Workaround for https://github.com/aspect-build/rules_esbuild/issues/58
   resolveExtensions: ['.js'],
 };
