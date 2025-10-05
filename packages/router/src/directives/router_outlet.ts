cfaf6e60baf3f6b445976041b08bbcbec6f469d5
Jaime Burgos <73321943+SkyZeroZx@users.noreply.github.com>
2025-10-05 13:51:00 -0500

    docs: add documentation for routerOutletData input and its usage (#64242)
    PR Close #64242

--- packages/router/src/directives/router_outlet.ts
+++ packages/router/src/directives/router_outlet.ts
@@ -55,6 +55,7 @@ import {PRIMARY_OUTLET} from '../shared';
  * ```
  *
  * @publicApi
+ * @see [Page routerOutletData](guide/routing/show-routes-with-outlets#passing-contextual-data-to-routed-components)
  */
 export const ROUTER_OUTLET_DATA = new InjectionToken<Signal<unknown | undefined>>(
   typeof ngDevMode !== undefined && ngDevMode ? 'RouterOutlet data' : '',
