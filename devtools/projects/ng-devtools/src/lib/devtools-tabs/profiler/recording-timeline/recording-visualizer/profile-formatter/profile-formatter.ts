67531d39cb15db51998b88069fb87612be6ee878
Matthieu Riegler <kyro38@gmail.com>
2025-10-10 16:06:24 +0200

    refactor(devtools): remove "changes" entry. (#64338)
    This is just the same value as total time spent. Also it is confusing to some users.
    
    fixes #51206
    
    PR Close #64338

--- devtools/projects/ng-devtools/src/lib/devtools-tabs/profiler/recording-timeline/recording-visualizer/profile-formatter/profile-formatter.ts
+++ devtools/projects/ng-devtools/src/lib/devtools-tabs/profiler/recording-timeline/recording-visualizer/profile-formatter/profile-formatter.ts
@@ -31,14 +31,6 @@ const formatOutput = (outputName: string) => {
 export const formatDirectiveProfile = (nodes: DirectiveProfile[]) => {
   const graphData: SelectedDirective[] = [];
   nodes.forEach((node) => {
-    const {changeDetection} = node;
-    if (changeDetection) {
-      graphData.push({
-        directive: node.name,
-        method: 'changes',
-        value: parseFloat(changeDetection.toFixed(2)),
-      });
-    }
     Object.entries(node.lifecycle).forEach(([key, lifeCycleProfile]) => {
       graphData.push({
         directive: node.name,
