angular/angular

commit cc1032bd9251e57228b7b81897cdb91ef3cf37e1

docs(docs-infra): removed unused common module (#63915)
PR Close #63915

adev/shared-docs/components/viewers/example-viewer/example-viewer.component.ts
@@ -21,7 +21,7 @@ import {
   Type,
   viewChild,
 } from '@angular/core';
-import {CommonModule, DOCUMENT} from '@angular/common';
+import {DOCUMENT, NgTemplateOutlet} from '@angular/common';
 import {MatTabGroup, MatTabsModule} from '@angular/material/tabs';
 import {Clipboard} from '@angular/cdk/clipboard';
 import {CopySourceCodeButton} from '../../copy-source-code-button/copy-source-code-button.component';
@@ -43,7 +43,7 @@ export const HIDDEN_CLASS_NAME = 'hidden';
 
 @Component({
   selector: 'docs-example-viewer',
-  imports: [CommonModule, CopySourceCodeButton, MatTabsModule, MatTooltipModule, IconComponent],
+  imports: [CopySourceCodeButton, MatTabsModule, MatTooltipModule, IconComponent, NgTemplateOutlet],
   templateUrl: './example-viewer.component.html',
   styleUrls: ['./example-viewer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,

adev/shared-docs/components/copy-source-code-button/copy-source-code-button.component.ts
@@ -15,7 +15,6 @@ import {
   inject,
   signal,
 } from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {Clipboard} from '@angular/cdk/clipboard';
 import {IconComponent} from '../icon/icon.component';
 
@@ -24,7 +23,7 @@ export const CONFIRMATION_DISPLAY_TIME_MS = 2000;
 
 @Component({
   selector: 'button[docs-copy-source-code]',
-  imports: [CommonModule, IconComponent],
+  imports: [IconComponent],
   templateUrl: './copy-source-code-button.component.html',
   host: {
     'type': 'button',

adev/shared-docs/components/select/select.component.ts
@@ -8,7 +8,6 @@
 
 import {ControlValueAccessor, NG_VALUE_ACCESSOR, FormsModule} from '@angular/forms';
 import {ChangeDetectionStrategy, Component, model, forwardRef, input, signal} from '@angular/core';
-import {CommonModule} from '@angular/common';
 
 type SelectOptionValue = string | number | boolean;
 
@@ -20,7 +19,7 @@ export interface SelectOption {
 @Component({
   selector: 'docs-select',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, FormsModule],
+  imports: [FormsModule],
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
   providers: [

adev/shared-docs/components/slide-toggle/slide-toggle.component.ts
@@ -7,12 +7,11 @@
  */
 
 import {ChangeDetectionStrategy, Component, forwardRef, model, input, signal} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'docs-slide-toggle',
-  imports: [CommonModule],
+  imports: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './slide-toggle.component.html',
   styleUrls: ['./slide-toggle.component.scss'],

adev/shared-docs/components/text-field/text-field.component.ts
@@ -17,14 +17,13 @@ import {
   model,
   viewChild,
 } from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {IconComponent} from '../icon/icon.component';
 
 @Component({
   selector: 'docs-text-field',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, FormsModule, IconComponent],
+  imports: [FormsModule, IconComponent],
   templateUrl: './text-field.component.html',
   styleUrls: ['./text-field.component.scss'],
   providers: [

adev/src/app/features/references/api-reference-list/api-reference-list.component.ts
@@ -71,9 +71,9 @@ export default class ApiReferenceList {
 
   // inputs are route binded, they can reset to undefined
   // also we want a writable state, so we use a linked signal
-  query = linkedSignal(() => this.queryInput() ?? '');
-  type = linkedSignal(() => this.typeInput() ?? ALL_TYPES_KEY);
-  status = linkedSignal(() => this.statusInput() ?? DEFAULT_STATUS);
+  protected query = linkedSignal(() => this.queryInput() ?? '');
+  public type = linkedSignal(() => this.typeInput() ?? ALL_TYPES_KEY);
+  private status = linkedSignal(() => this.statusInput() ?? DEFAULT_STATUS);
 
   // const state
   protected readonly itemTypes = Object.values(ApiItemType);

commit 68a9f22fd72e0d0c87f9c63c3b616abaaf3f4b5b

docs(docs-infra): fix example viewer
It was a regression introduced by #63915

adev/shared-docs/components/viewers/example-viewer/example-viewer.component.ts
@@ -21,7 +21,7 @@ import {
   Type,
   viewChild,
 } from '@angular/core';
-import {DOCUMENT, NgTemplateOutlet} from '@angular/common';
+import {DOCUMENT, NgComponentOutlet, NgTemplateOutlet} from '@angular/common';
 import {MatTabGroup, MatTabsModule} from '@angular/material/tabs';
 import {Clipboard} from '@angular/cdk/clipboard';
 import {CopySourceCodeButton} from '../../copy-source-code-button/copy-source-code-button.component';
@@ -43,7 +43,14 @@ export const HIDDEN_CLASS_NAME = 'hidden';
 
 @Component({
   selector: 'docs-example-viewer',
-  imports: [CopySourceCodeButton, MatTabsModule, MatTooltipModule, IconComponent, NgTemplateOutlet],
+  imports: [
+    CopySourceCodeButton,
+    MatTabsModule,
+    MatTooltipModule,
+    IconComponent,
+    NgTemplateOutlet,
+    NgComponentOutlet,
+  ],
   templateUrl: './example-viewer.component.html',
   styleUrls: ['./example-viewer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -92,10 +99,9 @@ export class ExampleViewer {
 
   async renderExample(): Promise<void> {
     // Lazy load live example component
-    if (this.exampleMetadata()?.path && this.exampleMetadata()?.preview) {
-      this.exampleComponent = await this.exampleViewerContentLoader.loadPreview(
-        this.exampleMetadata()?.path!,
-      );
+    const path = this.exampleMetadata()?.path;
+    if (path && this.exampleMetadata()?.preview) {
+      this.exampleComponent = await this.exampleViewerContentLoader.loadPreview(path);
     }
 
     this.snippetCode.set(this.exampleMetadata()?.files[0]);

adev/shared-docs/components/viewers/example-viewer/example-viewer.component.spec.ts
@@ -10,7 +10,7 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {ExampleViewer} from './example-viewer.component';
 import {ExampleMetadata, ExampleViewerContentLoader} from '../../../interfaces';
 import {EXAMPLE_VIEWER_CONTENT_LOADER} from '../../../providers';
-import {Component, provideZonelessChangeDetection, ComponentRef} from '@angular/core';
+import {Component, provideZonelessChangeDetection, ComponentRef, signal} from '@angular/core';
 import {HarnessLoader} from '@angular/cdk/testing';
 import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
 import {Clipboard} from '@angular/cdk/clipboard';
@@ -316,6 +316,24 @@ describe('ExampleViewer', () => {
     codeContainer = fixture.debugElement.query(By.css('.docs-example-viewer-code-wrapper'));
     expect(codeContainer).not.toBeNull();
   });
+
+  it('should render example', async () => {
+    exampleContentSpy.loadPreview.and.resolveTo(ExampleComponent);
+    componentRef.setInput(
+      'metadata',
+      getMetadata({
+        path: 'example.ts',
+        preview: true,
+      }),
+    );
+    await component.renderExample();
+    fixture.detectChanges();
+    expect(component.exampleComponent).toBeDefined();
+
+    const previewContainer = fixture.debugElement.query(By.css('.docs-example-viewer-preview'));
+    expect(previewContainer.nativeElement.innerHTML).toContain('ng-component');
+    expect(previewContainer.nativeElement.textContent).toContain('foobar');
+  });
 });
 
 const getMetadata = (value: Partial<ExampleMetadata> = {}): ExampleMetadata => {
@@ -332,6 +350,8 @@ const getMetadata = (value: Partial<ExampleMetadata> = {}): ExampleMetadata => {
 };
 
 @Component({
-  template: '',
+  template: '{{foobar}}',
 })
-class ExampleComponent {}
+class ExampleComponent {
+  foobar = signal('foobar');
+}