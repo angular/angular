dcdd1bcdbbd2a2fb4bd1fc4330259824d0bc8cb9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-31 08:58:27 -0700

    fix(core): skip leave animations on view swaps
    We accounted for skipping leave animations during moves, but not swaps.
    This accounts for the swap cases and updates how we deal with swaps and
    moves. Now we always queue animations and then essentially dequeue them
    if we attach them back in the same render pass.
    
    fixes: #64818
    fixes: #64730

--- packages/core/src/render3/list_reconciliation.ts
+++ packages/core/src/render3/list_reconciliation.ts
@@ -20,7 +20,7 @@ export abstract class LiveCollection<T, V> {
   abstract get length(): number;
   abstract at(index: number): V;
   abstract attach(index: number, item: T): void;
-  abstract detach(index: number, skipLeaveAnimations?: boolean): T;
+  abstract detach(index: number): T;
   abstract create(index: number, value: V): T;
   destroy(item: T): void {
     // noop by default
@@ -49,7 +49,7 @@ export abstract class LiveCollection<T, V> {
     // DOM nodes, which would trigger `animate.leave` bindings. We need to skip
     // those animations in the case of a move operation so the moving elements don't
     // unexpectedly disappear.
-    this.attach(newIdx, this.detach(prevIndex, true /* skipLeaveAnimations */));
+    this.attach(newIdx, this.detach(prevIndex));
   }
 }
 
