25e13e9f96f56ddc073a5a5f63dc9be96333bd41
Miles Malerba <mmalerba@google.com>
2025-11-06 09:05:32 -0800

    build: add a dev-app
    Adds a dev app that can be used to prototype and debug things while
    developing Angular. Use `pnpm dev` to run it.

---
+++ dev-app/src/app/app.spec.ts
@@ -0,0 +1,23 @@
+import {TestBed} from '@angular/core/testing';
+import {App} from './app';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render title', async () => {
+    const fixture = TestBed.createComponent(App);
+    await fixture.whenStable();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toBe('Welcome to the dev app!');
+  });
+});
