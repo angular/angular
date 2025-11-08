1705090d4025e27f160df7111dd37674ac8ab094
Shuaib Hasan Akib <akibh805@gmail.com>
2025-11-08 09:41:23 +0600

    docs(docs-infra): removed unused import and use self-closing syntax for component

--- adev/src/content/examples/dependency-injection/src/app/app.component.ts
+++ adev/src/content/examples/dependency-injection/src/app/app.component.ts
@@ -8,30 +8,29 @@ import {ProvidersComponent} from './providers.component';
 import {CarComponent} from './car/car.component';
 import {InjectorComponent} from './injector.component';
 import {TestComponent} from './test.component';
-import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
-    <app-car></app-car>
-    <app-injectors></app-injectors>
-    <app-tests></app-tests>
+    <app-car />
+    <app-injectors />
+    <app-tests />
     <h2>User</h2>
     <p id="user">
       {{userInfo}}
       <button type="button" (click)="nextUser()">Next User</button>
     <p>
     @if (isAuthorized) {
-      <app-heroes id="authorized"></app-heroes>
+      <app-heroes id="authorized" />
     }
     @if (!isAuthorized) {
-      <app-heroes id="unauthorized"></app-heroes>
+      <app-heroes id="unauthorized" />
     }
     @if (isAuthorized) {
-      <app-heroes-tsp id="tspAuthorized"></app-heroes-tsp>
+      <app-heroes-tsp id="tspAuthorized" />
     }
-    <app-providers></app-providers>
+    <app-providers />
   `,
   imports: [
     HeroesComponent,
@@ -40,7 +39,6 @@ import {NgIf} from '@angular/common';
     CarComponent,
     InjectorComponent,
     TestComponent,
-    NgIf,
   ],
 })
 export class AppComponent {
