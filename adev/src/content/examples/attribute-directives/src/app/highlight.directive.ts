0cb1f33da5e7d9b2bbaee551d4260c46f3d1a70a
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2025-11-17 17:54:28 -0500

    docs: replace @HostListener with host event bindings in attribute directives

--- adev/src/content/examples/attribute-directives/src/app/highlight.directive.ts
+++ adev/src/content/examples/attribute-directives/src/app/highlight.directive.ts
@@ -1,7 +1,11 @@
-import {Directive, ElementRef, HostListener, inject, input} from '@angular/core';
+import {Directive, ElementRef, inject, input} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
+  host: {
+    '(mouseenter)': 'onMouseEnter()',
+    '(mouseleave)': 'onMouseLeave()',
+  },
 })
 export class HighlightDirective {
   private el = inject(ElementRef);
@@ -13,12 +17,12 @@ export class HighlightDirective {
   appHighlight = input('');
 
   // #docregion mouse-enter
-  @HostListener('mouseenter') onMouseEnter() {
+  onMouseEnter() {
     this.highlight(this.appHighlight() || this.defaultColor() || 'red');
   }
   // #enddocregion mouse-enter
 
-  @HostListener('mouseleave') onMouseLeave() {
+  onMouseLeave() {
     this.highlight('');
   }
 
