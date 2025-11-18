cfec318b111b8c8c61eb28c31a1545cefcf2f415
Wagner Maciel <wagnermaciel@google.com>
2025-11-18 12:14:13 -0500

    docs: aria menu examples

--- adev/src/content/examples/aria/menu/src/menu-trigger/app/app.ts
+++ adev/src/content/examples/aria/menu/src/menu-trigger/app/app.ts
@@ -1,10 +1,14 @@
-import {Component} from '@angular/core';
-import {Toolbar, ToolbarWidget, ToolbarWidgetGroup} from '@angular/aria/toolbar';
+import {Component, viewChild} from '@angular/core';
+import {Menu, MenuContent, MenuItem, MenuTrigger} from '@angular/aria/menu';
+import {OverlayModule} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.html',
   styleUrl: 'app.css',
-  imports: [Toolbar, ToolbarWidget, ToolbarWidgetGroup],
+  imports: [Menu, MenuContent, MenuItem, MenuTrigger, OverlayModule],
 })
-export class App {}
+export class App {
+  formatMenu = viewChild<Menu<string>>('formatMenu');
+  categorizeMenu = viewChild<Menu<string>>('categorizeMenu');
+}
