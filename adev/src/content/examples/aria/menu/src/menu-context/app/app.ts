dce20b9289ce913a5707ddb7896ebc9b13563f85
Wagner Maciel <wagnermaciel@google.com>
2025-11-18 12:14:13 -0500

    docs: aria menu examples
    (cherry picked from commit 1963bd67cc41f66a8e3fab8a70f9d678ac1ef266)

--- adev/src/content/examples/aria/menu/src/menu-context/app/app.ts
+++ adev/src/content/examples/aria/menu/src/menu-context/app/app.ts
@@ -1,10 +1,8 @@
 import {Component} from '@angular/core';
-import {Toolbar, ToolbarWidget, ToolbarWidgetGroup} from '@angular/aria/toolbar';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.html',
-  styleUrl: 'app.css',
-  imports: [Toolbar, ToolbarWidget, ToolbarWidgetGroup],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
 })
 export class App {}
