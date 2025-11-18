d285ba27a624ba32cdeb36a1a242badc9167d75e
Wagner Maciel <wagnermaciel@google.com>
2025-11-18 14:52:23 -0500

    docs: aria menubar examples
    (cherry picked from commit 695f4df912950970ebe001e9122af78dbf4c6ab6)

--- adev/src/content/examples/aria/menubar/src/disabled/app/app.ts
+++ adev/src/content/examples/aria/menubar/src/disabled/app/app.ts
@@ -1,8 +1,33 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, signal, viewChild} from '@angular/core';
+import {MenuBar, Menu, MenuContent, MenuItem} from '@angular/aria/menu';
+import {OverlayModule} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  templateUrl: 'app.html',
+  styleUrl: 'app.css',
+  imports: [MenuBar, Menu, MenuContent, MenuItem, OverlayModule],
+  standalone: true,
 })
-export class App {}
+export class App implements AfterViewInit {
+  fileMenu = viewChild<Menu<string>>('fileMenu');
+  shareMenu = viewChild<Menu<string>>('shareMenu');
+  editMenu = viewChild<Menu<string>>('editMenu');
+  viewMenu = viewChild<Menu<string>>('viewMenu');
+  insertMenu = viewChild<Menu<string>>('insertMenu');
+  imageMenu = viewChild<Menu<string>>('imageMenu');
+  chartMenu = viewChild<Menu<string>>('chartMenu');
+  formatMenu = viewChild<Menu<string>>('formatMenu');
+  textMenu = viewChild<Menu<string>>('textMenu');
+  sizeMenu = viewChild<Menu<string>>('sizeMenu');
+  paragraphMenu = viewChild<Menu<string>>('paragraphMenu');
+  alignMenu = viewChild<Menu<string>>('alignMenu');
+
+  rendered = signal(false);
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      debugger;
+    }, 500);
+  }
+}
