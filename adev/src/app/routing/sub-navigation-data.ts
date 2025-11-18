7a910e66d4cd62ce2bc288af04c00b40c5b04508
Ben Hong <ben@bencodezen.io>
2025-11-18 18:41:22 -0500

    docs: add signal forms comparison guide

--- adev/src/app/routing/sub-navigation-data.ts
+++ adev/src/app/routing/sub-navigation-data.ts
@@ -443,6 +443,12 @@ const DOCS_SUB_NAVIGATION_DATA: NavigationItem[] = [
                 contentPath: 'guide/forms/signals/models',
                 status: 'new',
               },
+              {
+                label: 'Comparison with other form systems',
+                path: 'guide/forms/signals/comparison',
+                contentPath: 'guide/forms/signals/comparison',
+                status: 'new',
+              },
             ],
           },
           {
