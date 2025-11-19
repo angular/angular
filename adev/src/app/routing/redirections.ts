7a177fecd5273af4146012a1d5640b9e1195ce57
Ben Hong <ben@bencodezen.io>
2025-11-19 17:36:25 -0500

    docs: add redirect for angular overview

--- adev/src/app/routing/redirections.ts
+++ adev/src/app/routing/redirections.ts
@@ -164,4 +164,8 @@ export const REDIRECT_ROUTES: Route[] = [
     path: 'guide/animations/reusable-animations',
     redirectTo: '/guide/legacy-animations/reusable-animations',
   },
+  {
+    path: 'guide/aria',
+    redirectTo: '/guide/aria/overview',
+  },
 ];
