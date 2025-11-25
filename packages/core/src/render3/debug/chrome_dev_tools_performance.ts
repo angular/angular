51d4cb54cc56f789f4fb5820bdb6668b6b3521ab
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2025-11-24 22:27:49 -0500

    feat(core): enhance profiling with documentation URLs
    Enhances the Chrome DevTools performance profiling integration by adding links to relevant Angular documentation for lifecycle hooks and profiler events.
    
    Fixes #63959

--- packages/core/src/render3/debug/chrome_dev_tools_performance.ts
+++ packages/core/src/render3/debug/chrome_dev_tools_performance.ts
@@ -7,7 +7,7 @@
  */
 
 import {isTypeProvider} from '../../di/provider_collection';
-import {assertDefined, assertEqual} from '../../util/assert';
+import {assertDefined} from '../../util/assert';
 import {performanceMarkFeature} from '../../util/performance';
 import {setProfiler} from '../profiler';
 import {Profiler, ProfilerEvent} from '../../../primitives/devtools';
@@ -46,12 +46,102 @@ declare global {
   }
 }
 
+const hasPerformanceMeasure = 'measure' in performance;
+
+const TRACK_NAME = '\u{1F170}\uFE0F Angular';
+
+function timestampMark(markName: string): void {
+  if (hasPerformanceMeasure) {
+    performance.mark(markName);
+    return;
+  }
+
+  console.timeStamp(markName);
+}
+
+function timestampMeasure(
+  entryName: string,
+  startMarkName: string,
+  color: DevToolsColor,
+  docUrl?: string,
+): void {
+  if (hasPerformanceMeasure) {
+    performance.measure(entryName, {
+      start: startMarkName,
+      detail: {
+        devtools: {
+          dataType: 'track-entry',
+          track: TRACK_NAME,
+          color,
+          ...(docUrl && {properties: [['Documentation', docUrl]]}),
+        },
+      },
+    });
+    return;
+  }
+
+  console.timeStamp(entryName, startMarkName, undefined, TRACK_NAME, undefined, color);
+}
+
 let changeDetectionRuns = 0;
 let changeDetectionSyncRuns = 0;
 
 let counter = 0;
 type stackEntry = [ProfilerEvent | ProfilerDIEvent, number];
 const eventsStack: stackEntry[] = [];
+
+const BASE_DOC_URL = 'https://angular.dev';
+const DOC_URL_LIFECYCLE_HOOKS = `${BASE_DOC_URL}/guide/components/lifecycle`;
+
+/**
+ * Returns documentation URL for lifecycle hooks.
+ * Extracts lifecycle hook name from the component method string and maps to Angular docs.
+ */
+function getLifecycleHookDocUrl(hookName: string): string | undefined {
+  // Extract lifecycle hook name (e.g., "MyComponent:ngOnInit" -> "ngOnInit")
+  const match = hookName.match(/:(ng\w+)$/);
+  if (!match) return undefined;
+
+  const lifecycleHook = match[1].toLowerCase();
+
+  const lifecycleHookUrls: Record<string, string> = {
+    ngoninit: `${DOC_URL_LIFECYCLE_HOOKS}#ngoninit`,
+    ngonchanges: `${DOC_URL_LIFECYCLE_HOOKS}#ngonchanges`,
+    ngdocheck: `${DOC_URL_LIFECYCLE_HOOKS}#ngdocheck`,
+    ngaftercontentinit: `${DOC_URL_LIFECYCLE_HOOKS}#ngaftercontentinit`,
+    ngaftercontentchecked: `${DOC_URL_LIFECYCLE_HOOKS}#ngaftercontentchecked`,
+    ngafterviewinit: `${DOC_URL_LIFECYCLE_HOOKS}#ngafterviewinit`,
+    ngafterviewchecked: `${DOC_URL_LIFECYCLE_HOOKS}#ngafterviewchecked`,
+    ngondestroy: `${DOC_URL_LIFECYCLE_HOOKS}#ngondestroy`,
+  };
+
+  return lifecycleHookUrls[lifecycleHook];
+}
+
+/**
+ * Returns documentation URL for profiler events.
+ */
+function getProfilerEventDocUrl(event: ProfilerEvent | ProfilerDIEvent): string | undefined {
+  switch (event) {
+    case ProfilerEvent.ChangeDetectionStart:
+    case ProfilerEvent.ChangeDetectionEnd:
+    case ProfilerEvent.ChangeDetectionSyncStart:
+    case ProfilerEvent.ChangeDetectionSyncEnd:
+      return ` ${BASE_DOC_URL}/best-practices/runtime-performance/overview`;
+    case ProfilerEvent.AfterRenderHooksStart:
+    case ProfilerEvent.AfterRenderHooksEnd:
+      return `${DOC_URL_LIFECYCLE_HOOKS}#aftereveryrender-and-afternextrender`;
+    case ProfilerEvent.DeferBlockStateStart:
+    case ProfilerEvent.DeferBlockStateEnd:
+      return `${BASE_DOC_URL}/guide/defer`;
+    case ProfilerEvent.BootstrapApplicationStart:
+    case ProfilerEvent.BootstrapApplicationEnd:
+    case ProfilerEvent.BootstrapComponentStart:
+    case ProfilerEvent.BootstrapComponentEnd:
+    default:
+      return undefined;
+  }
+}
 
 /**
  * Enum mimicking ProfilerEvent. The idea is to have unique event identifiers for both DI and other profiling events.
@@ -62,14 +152,16 @@ const enum ProfilerDIEvent {
 }
 
 function measureStart(startEvent: ProfilerEvent | ProfilerDIEvent) {
-  eventsStack.push([startEvent, counter]);
-  console.timeStamp('Event_' + startEvent + '_' + counter++);
+  const markName = 'Event_' + startEvent + '_' + counter++;
+  eventsStack.push([startEvent, counter - 1]);
+  timestampMark(markName);
 }
 
 function measureEnd(
   startEvent: ProfilerEvent | ProfilerDIEvent,
   entryName: string,
   color: DevToolsColor,
+  docUrl?: string,
 ) {
   let top: stackEntry | undefined;
 
@@ -80,14 +172,7 @@ function measureEnd(
     assertDefined(top, 'Profiling error: could not find start event entry ' + startEvent);
   } while (top[0] !== startEvent);
 
-  console.timeStamp(
-    entryName,
-    'Event_' + top[0] + '_' + top[1],
-    undefined,
-    '\u{1F170}\uFE0F Angular',
-    undefined,
-    color,
-  );
+  timestampMeasure(entryName, 'Event_' + top[0] + '_' + top[1], color, docUrl);
 }
 
 const chromeDevToolsInjectorProfiler: InjectorProfiler = (event: InjectorProfilerEvent) => {
@@ -127,11 +212,21 @@ const devToolsProfiler: Profiler = (
       break;
     }
     case ProfilerEvent.BootstrapApplicationEnd: {
-      measureEnd(ProfilerEvent.BootstrapApplicationStart, 'Bootstrap application', 'primary-dark');
+      measureEnd(
+        ProfilerEvent.BootstrapApplicationStart,
+        'Bootstrap application',
+        'primary-dark',
+        getProfilerEventDocUrl(ProfilerEvent.BootstrapApplicationStart),
+      );
       break;
     }
     case ProfilerEvent.BootstrapComponentEnd: {
-      measureEnd(ProfilerEvent.BootstrapComponentStart, 'Bootstrap component', 'primary-dark');
+      measureEnd(
+        ProfilerEvent.BootstrapComponentStart,
+        'Bootstrap component',
+        'primary-dark',
+        getProfilerEventDocUrl(ProfilerEvent.BootstrapComponentStart),
+      );
       break;
     }
     case ProfilerEvent.ChangeDetectionEnd: {
@@ -140,6 +235,7 @@ const devToolsProfiler: Profiler = (
         ProfilerEvent.ChangeDetectionStart,
         'Change detection ' + changeDetectionRuns++,
         'primary-dark',
+        getProfilerEventDocUrl(ProfilerEvent.ChangeDetectionStart),
       );
       break;
     }
@@ -149,11 +245,17 @@ const devToolsProfiler: Profiler = (
         ProfilerEvent.ChangeDetectionSyncStart,
         'Synchronization ' + changeDetectionSyncRuns++,
         'primary',
+        getProfilerEventDocUrl(ProfilerEvent.ChangeDetectionSyncStart),
       );
       break;
     }
     case ProfilerEvent.AfterRenderHooksEnd: {
-      measureEnd(ProfilerEvent.AfterRenderHooksStart, 'After render hooks', 'primary');
+      measureEnd(
+        ProfilerEvent.AfterRenderHooksStart,
+        'After render hooks',
+        'primary',
+        getProfilerEventDocUrl(ProfilerEvent.AfterRenderHooksStart),
+      );
       break;
     }
 
@@ -163,7 +265,12 @@ const devToolsProfiler: Profiler = (
       break;
     }
     case ProfilerEvent.DeferBlockStateEnd: {
-      measureEnd(ProfilerEvent.DeferBlockStateStart, 'Defer block', 'primary-dark');
+      measureEnd(
+        ProfilerEvent.DeferBlockStateStart,
+        'Defer block',
+        'primary-dark',
+        getProfilerEventDocUrl(ProfilerEvent.DeferBlockStateStart),
+      );
       break;
     }
     case ProfilerEvent.DynamicComponentEnd: {
@@ -192,10 +299,12 @@ const devToolsProfiler: Profiler = (
     }
     case ProfilerEvent.LifecycleHookEnd: {
       const typeName = getComponentMeasureName(instance!);
+      const hookName = `${typeName}:${stringifyForError(eventFn)}`;
       measureEnd(
         ProfilerEvent.LifecycleHookStart,
-        `${typeName}:${stringifyForError(eventFn)}`,
+        hookName,
         'tertiary',
+        getLifecycleHookDocUrl(hookName),
       );
       break;
     }
