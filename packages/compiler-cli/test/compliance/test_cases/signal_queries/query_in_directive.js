90cfe640fc1b2497ad8f32df1acf80e79e7e4571
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-12-06 10:52:05 +0100

    perf(compiler): chain query creation instructions
    We always emit the query creation instructions in a group which makes them good candidates for chaining.

--- packages/compiler-cli/test/compliance/test_cases/signal_queries/query_in_directive.js
+++ packages/compiler-cli/test/compliance/test_cases/signal_queries/query_in_directive.js
@@ -11,23 +11,16 @@ TestDir.ɵdir = /*@__PURE__*/ $r3$.ɵɵdefineDirective({
     …
     contentQueries: function TestDir_ContentQueries(rf, ctx, dirIndex) {
         if (rf & 1) {
-            i0.ɵɵcontentQuerySignal(dirIndex, ctx.query3, _c0, 5);
-            i0.ɵɵcontentQuerySignal(dirIndex, ctx.query4, _c1, 4);
-            i0.ɵɵcontentQuerySignal(dirIndex, ctx.query8, _c2, 5);
-            i0.ɵɵcontentQuerySignal(dirIndex, ctx.query9, nonAnalyzableRefersToString, 5);
+            $r3$.ɵɵcontentQuerySignal(dirIndex, ctx.query3, _c0, 5)(dirIndex, ctx.query4, _c1, 4)(dirIndex, ctx.query8, _c2, 5)(dirIndex, ctx.query9, nonAnalyzableRefersToString, 5);
         } if (rf & 2) {
-            i0.ɵɵqueryAdvance(4);
+            $r3$.ɵɵqueryAdvance(4);
         }
     },
     viewQuery: function TestDir_Query(rf, ctx) {
         if (rf & 1) {
-            i0.ɵɵviewQuerySignal(ctx.query1, _c3, 5);
-            i0.ɵɵviewQuerySignal(ctx.query2, _c4, 5);
-            i0.ɵɵviewQuerySignal(ctx.query5, SomeToken, 5);
-            i0.ɵɵviewQuerySignal(ctx.query6, SomeToken, 5);
-            i0.ɵɵviewQuerySignal(ctx.query7, _c5, 5, SomeToken);
+            $r3$.ɵɵviewQuerySignal(ctx.query1, _c3, 5)(ctx.query2, _c4, 5)(ctx.query5, SomeToken, 5)(ctx.query6, SomeToken, 5)(ctx.query7, _c5, 5, SomeToken);
         } if (rf & 2) {
-            i0.ɵɵqueryAdvance(5);
+            $r3$.ɵɵqueryAdvance(5);
         }
     }
     …
