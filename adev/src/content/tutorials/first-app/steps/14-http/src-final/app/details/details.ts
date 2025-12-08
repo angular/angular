200d923436cca9c457f2d886b4b22a2d6951664b
Andrew Scott <atscott@google.com>
2025-12-08 10:35:34 -0800

    docs: Update tutorial last step to use markForCheck
    Ideally this tutorial would have used signals but didn't. That's a bigger change than
    we are able to make right now and there are other things we would like
    to update at the same time. This fix is a temporary patch to the
    existing content until there is time to redo the tutorial and videos
    entirely.
    
    fixes #65863

--- adev/src/content/tutorials/first-app/steps/14-http/src-final/app/details/details.ts
+++ adev/src/content/tutorials/first-app/steps/14-http/src-final/app/details/details.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {ChangeDetectorRef, Component, inject} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {HousingService} from '../housing.service';
 import {HousingLocationInfo} from '../housinglocation';
@@ -46,6 +46,7 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
   styleUrls: ['./details.css'],
 })
 export class Details {
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocationInfo | undefined;
@@ -60,6 +61,7 @@ export class Details {
     const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
     this.housingService.getHousingLocationById(housingLocationId).then((housingLocation) => {
       this.housingLocation = housingLocation;
+      this.changeDetectorRef.markForCheck();
     });
   }
 
