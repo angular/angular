5da4b44cb4dd7ee42423989d83beb91cdd5be770
Максим Иванов <omaxphp@yandex.ru>
2025-12-16 14:10:23 +0300

    feat(common): pretty wrap `BigInt` into `JsonPipe`

--- packages/common/src/pipes/json_pipe.ts
+++ packages/common/src/pipes/json_pipe.ts
@@ -34,6 +34,6 @@ export class JsonPipe implements PipeTransform {
    * @param value A value of any type to convert into a JSON-format string.
    */
   transform(value: any): string {
-    return JSON.stringify(value, null, 2);
+    return JSON.stringify(value, (_, x) => (typeof x === 'bigint' ? `${String(x)}n` : x), 2);
   }
 }
