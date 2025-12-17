650002a059b49daaa28d6a7677fe7fb8c29b65e8
kirjs <kirts@google.com>
2025-12-17 13:42:30 -0500

    dev-infra: enable markdown formatting in ng-dev

--- .ng-dev/format.mjs
+++ .ng-dev/format.mjs
@@ -4,6 +4,14 @@
  * @type { import("@angular/ng-dev").FormatConfig }
  */
 export const format = {
-  'prettier': true,
+  'prettier': {
+    'matchers': [
+      '**/*.{js,cjs,mjs}',
+      '**/*.{ts,cts,mts}',
+      '**/*.{json,json5}',
+      '**/*.{yml,yaml}',
+      '**/*.md',
+    ],
+  },
   'buildifier': true,
 };
