angular/angular

commit c53310ee81db5fc64776f6038df69dba42564e9d

refactor(router): Update RouterLink href to use host binding and signals (#60875)
This commit updates the method of setting the href attribute on
`RouterLink` to use built in host binding rather than custom attribute
setting and sanitization. The advantage here would be automatic handling
of the sanitization and avoiding of writing the same value to the DOM
that we had before.

This change does mean that we _always_ write to the href attribute where
before we only wrote to it when the elemnt was known to support `href`.
That said, the implementation attempts to retain behavior that is as
close as possible: the original value of `href` is used and never updated.

PR Close #60875

packages/router/src/directives/router_link.ts
@@ -20,9 +20,11 @@ import {
   Renderer2,
   ɵRuntimeError as RuntimeError,
   SimpleChanges,
-  ɵɵsanitizeUrlOrResourceUrl,
   ɵINTERNAL_APPLICATION_ERROR_HANDLER,
   inject,
+  signal,
+  untracked,
+  HostAttributeToken,
 } from '@angular/core';
 import {Subject, Subscription} from 'rxjs';
 
@@ -142,14 +144,25 @@ import {RuntimeErrorCode} from '../errors';
  */
 @Directive({
   selector: '[routerLink]',
+  host: {
+    '[attr.href]': 'reactiveHref()',
+  },
 })
 export class RouterLink implements OnChanges, OnDestroy {
+  /** @nodoc */
+  protected readonly reactiveHref = signal<string | null>(null);
   /**
    * Represents an `href` attribute value applied to a host element,
    * when a host element is an `<a>`/`<area>` tag or a compatible custom element.
    * For other tags, the value is `null`.
    */
-  href: string | null = null;
+  get href() {
+    return untracked(this.reactiveHref);
+  }
+  /** @deprecated */
+  set href(value: string | null) {
+    this.reactiveHref.set(value);
+  }
 
   /**
    * Represents the `target` attribute on a host element.
@@ -222,6 +235,8 @@ export class RouterLink implements OnChanges, OnDestroy {
     private readonly el: ElementRef,
     private locationStrategy?: LocationStrategy,
   ) {
+    // Set the initial href value to whatever exists on the host element already
+    this.reactiveHref.set(inject(new HostAttributeToken('href'), {optional: true}));
     const tagName = el.nativeElement.tagName?.toLowerCase();
     this.isAnchorElement =
       tagName === 'a' ||
@@ -392,30 +407,11 @@ export class RouterLink implements OnChanges, OnDestroy {
 
   private updateHref(): void {
     const urlTree = this.urlTree;
-    this.href =
+    this.reactiveHref.set(
       urlTree !== null && this.locationStrategy
-        ? this.locationStrategy?.prepareExternalUrl(this.router.serializeUrl(urlTree))
-        : null;
-
-    const sanitizedValue =
-      this.href === null
-        ? null
-        : // This class represents a directive that can be added to both `<a>` elements,
-          // as well as other elements. As a result, we can't define security context at
-          // compile time. So the security context is deferred to runtime.
-          // The `ɵɵsanitizeUrlOrResourceUrl` selects the necessary sanitizer function
-          // based on the tag and property names. The logic mimics the one from
-          // `packages/compiler/src/schema/dom_security_schema.ts`, which is used at compile time.
-          //
-          // Note: we should investigate whether we can switch to using `@HostBinding('attr.href')`
-          // instead of applying a value via a renderer, after a final merge of the
-          // `RouterLinkWithHref` directive.
-          ɵɵsanitizeUrlOrResourceUrl(
-            this.href,
-            this.el.nativeElement.tagName.toLowerCase(),
-            'href',
-          );
-    this.applyAttributeValue('href', sanitizedValue);
+        ? (this.locationStrategy?.prepareExternalUrl(this.router.serializeUrl(urlTree)) ?? '')
+        : null,
+    );
   }
 
   private applyAttributeValue(attrName: string, attrValue: string | null) {

commit f3363088b5e0827e45d0b2a40426dff9764d63bc

fix(router): Fix RouterLink href not updating with `queryParamsHandling`
There was a bug introduced in #60875. While RouterLink doesn't
necessarily depend on Router state, its link can change when navigations
cause the `ActivatedRoute` paths to change. It is difficult to determine
which segments the link depends on. Luckily, the commit had flawed
logic:

```
!this.queryParamsHandling && !dependsOnRouterState(this.options?.defaultQueryParamsHandling);
```

The subscription gets created whenever `queryParamsHandling` was not
defined, or it was non-default. This is pretty much all scenarios since
nobody is likely to set it explicitly to the default value. In addition,
the `click` handler recomputes the tree because `urlTree` is a getter
that does the computation.

This commit effectively rolls back #60875

packages/router/src/directives/router_link.ts
@@ -254,27 +254,14 @@ export class RouterLink implements OnChanges, OnDestroy {
         )
       );
 
-    if (!this.isAnchorElement) {
-      this.subscribeToNavigationEventsIfNecessary();
-    } else {
+    if (this.isAnchorElement) {
       this.setTabIndexIfNotOnNativeEl('0');
+      this.subscribeToNavigationEventsIfNecessary();
     }
   }
 
   private subscribeToNavigationEventsIfNecessary() {
-    if (this.subscription !== undefined || !this.isAnchorElement) {
-      return;
-    }
-
-    // preserving fragment in router state
-    let createSubcription = this.preserveFragment;
-    // preserving or merging with query params in router state
-    const dependsOnRouterState = (handling?: QueryParamsHandling | null) =>
-      handling === 'merge' || handling === 'preserve';
-    createSubcription ||= dependsOnRouterState(this.queryParamsHandling);
-    createSubcription ||=
-      !this.queryParamsHandling && !dependsOnRouterState(this.options?.defaultQueryParamsHandling);
-    if (!createSubcription) {
+    if (this.subscription !== undefined) {
       return;
     }
 
@@ -339,7 +326,6 @@ export class RouterLink implements OnChanges, OnDestroy {
     }
     if (this.isAnchorElement) {
       this.updateHref();
-      this.subscribeToNavigationEventsIfNecessary();
     }
     // This is subscribed to by `RouterLinkActive` so that it knows to update when there are changes
     // to the RouterLinks it's tracking.

packages/router/test/router_link_spec.ts
@@ -10,12 +10,9 @@ import {Component, inject, signal, provideZonelessChangeDetection} from '@angula
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {By} from '@angular/platform-browser';
 import {Router, RouterLink, RouterModule, provideRouter} from '../index';
+import {RouterTestingHarness} from '../testing';
 
 describe('RouterLink', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({providers: [provideZonelessChangeDetection()]});
-  });
-
   it('does not modify tabindex if already set on non-anchor element', async () => {
     @Component({
       template: `<div [routerLink]="link" tabindex="1"></div>`,
@@ -314,5 +311,22 @@ describe('RouterLink', () => {
 
     const fixture = TestBed.createComponent(WithUrlTree);
     expect(() => fixture.changeDetectorRef.detectChanges()).toThrow();
+  });
+
+  it('correctly updates when relativeTo segments change', async () => {
+    @Component({
+      template: `<a [routerLink]="['./child']" queryParamsHandling="'replace'">link</a>`,
+      imports: [RouterLink],
+    })
+    class WithLink {}
+    TestBed.configureTestingModule({
+      providers: [provideRouter([{path: '**', component: WithLink}])],
+    });
+
+    const harness = await RouterTestingHarness.create('/initial');
+    const anchor = harness.fixture.nativeElement.querySelector('a');
+    expect(anchor.getAttribute('href')).toBe('/initial/child');
+    await harness.navigateByUrl('/different');
+    expect(anchor.getAttribute('href')).toBe('/different/child');
   });
 });