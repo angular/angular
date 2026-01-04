6f6cbe19403fba1ba78664580683d2e8cf34a150
Matthieu Riegler <kyro38@gmail.com>
2026-01-04 23:27:51 +0100

    docs(docs-infra): Add package filter to the API list
    This commit also introduces the usage of signal forms on adev

--- adev/shared-docs/components/select/select.component.ts
+++ adev/shared-docs/components/select/select.component.ts
@@ -6,8 +6,8 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {ControlValueAccessor, NG_VALUE_ACCESSOR, FormsModule} from '@angular/forms';
-import {ChangeDetectionStrategy, Component, model, forwardRef, input, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, model} from '@angular/core';
+import {FormValueControl} from '@angular/forms/signals';
 
 type SelectOptionValue = string | number | boolean;
 
@@ -19,59 +19,25 @@ export interface SelectOption {
 @Component({
   selector: 'docs-select',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [FormsModule],
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => Select),
-      multi: true,
-    },
-  ],
   host: {
     class: 'docs-form-element',
   },
 })
-export class Select implements ControlValueAccessor {
+export class Select implements FormValueControl<string | null> {
+  readonly value = model<string | null>(null);
+
   readonly id = input.required<string>({alias: 'selectId'});
   readonly name = input.required<string>();
   readonly options = input.required<SelectOption[]>();
-  readonly disabled = model(false);
-
-  // Implemented as part of ControlValueAccessor.
-  private onChange: (value: SelectOptionValue) => void = (_: SelectOptionValue) => {};
-  private onTouched: () => void = () => {};
-
-  protected readonly selectedOption = signal<SelectOptionValue | null>(null);
-
-  // Implemented as part of ControlValueAccessor.
-  writeValue(value: SelectOptionValue): void {
-    this.selectedOption.set(value);
-  }
-
-  // Implemented as part of ControlValueAccessor.
-  registerOnChange(fn: any): void {
-    this.onChange = fn;
-  }
-
-  // Implemented as part of ControlValueAccessor.
-  registerOnTouched(fn: any): void {
-    this.onTouched = fn;
-  }
-
-  // Implemented as part of ControlValueAccessor.
-  setDisabledState?(isDisabled: boolean): void {
-    this.disabled.set(isDisabled);
-  }
+  readonly disabled = input(false);
 
   setOption($event: SelectOptionValue): void {
     if (this.disabled()) {
       return;
     }
 
-    this.selectedOption.set($event);
-    this.onChange($event);
-    this.onTouched();
+    this.value.set($event as string);
   }
 }
