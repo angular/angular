bac823c051a66170ae84de040ffab5d3627c99ad
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2026-01-06 08:06:56 +0000

    fixup! fix(core): sanitize sensitive attributes on SVG script elements
    (cherry picked from commit 329b98551a1be62921da0cbe91d4cf03c427703e)

--- packages/compiler-cli/test/ngtsc/ngtsc_spec.ts
+++ packages/compiler-cli/test/ngtsc/ngtsc_spec.ts
@@ -8286,7 +8286,11 @@ runInEachFileSystem((os: string) => {
 
             @Component({
               selector: 'test-cmp',
-              template: '<svg><script [attr.href]="attr"></script></svg>',
+              template: \`
+                <svg>
+                  <script [attr.xlink:href]="attr" [attr.href]="attr"></script>
+                </svg>
+              \`,
             })
             export class TestCmp {
               attr = './script.js';
@@ -8297,7 +8301,9 @@ runInEachFileSystem((os: string) => {
         env.driveMain();
 
         const jsContents = env.getContents('test.js');
-        expect(jsContents).toContain('i0.ɵɵattribute("href", ctx.attr, i0.ɵɵsanitizeResourceUrl);');
+        expect(jsContents).toContain(
+          'i0.ɵɵattribute("href", ctx.attr, i0.ɵɵsanitizeResourceUrl, "xlink")("href", ctx.attr, i0.ɵɵsanitizeResourceUrl);',
+        );
       });
 
       it('should not generate sanitizers for URL properties in hostBindings fn in Component', () => {
