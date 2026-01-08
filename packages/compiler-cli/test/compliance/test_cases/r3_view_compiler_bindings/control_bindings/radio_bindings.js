65fa5b5439b9489dfa5f8fdfa24567cd9a32fac4
Matthieu Riegler <kyro38@gmail.com>
2026-01-08 16:49:43 +0100

    fix(forms): Ensure the control instruction comes after the other bindings
    Prior to this change, binding to radio value was sensitive to the order in which `value` & `formField` where binding in the template.
    The compiler change makes that order non-important.
    
    fixes #66402

---
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_bindings/control_bindings/radio_bindings.js
@@ -0,0 +1,27 @@
+export class MyComponent {
+  // ...
+  static ɵcmp = /* @__PURE__ */i0.ɵɵdefineComponent({
+    type: MyComponent,
+    selectors: [["ng-component"]],
+    decls: 2,
+    vars: 6,
+    consts: [["type", "radio", "id", "radio", 3, "formField", "value"], ["type", "radio", "id", "radio", 3, "value", "formField"]],
+    template: function MyComponent_Template(rf, ctx) {
+      if (rf & 1) {
+        i0.ɵɵelement(0, "input", 0);
+        i0.ɵɵcontrolCreate();
+        i0.ɵɵelement(1, "input", 1);
+        i0.ɵɵcontrolCreate();
+      }
+      if (rf & 2) {
+        i0.ɵɵproperty("value", "foo");
+        i0.ɵɵcontrol(ctx.value, "formField");
+        i0.ɵɵadvance();
+        i0.ɵɵproperty("value", "foo");
+        i0.ɵɵcontrol(ctx.value, "formField");
+      }
+    },
+    dependencies: [FormField],
+    encapsulation: 2
+  });
+}
