89a51c84151284035f760b5218e4cd54ba860212
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-01-10 09:03:52 +0100

    build: update license for vscode extension
    The vscode extension still has the old license headers pointing pointing to AIO.
    
    (cherry picked from commit f0dba6deb9d1f1942684b014649868ecd2b1e9bd)

--- vscode-ng-language-service/integration/test_constants.ts
+++ vscode-ng-language-service/integration/test_constants.ts
@@ -1,3 +1,11 @@
+/*!
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
 import {mkdtempSync} from 'node:fs';
 import {join, resolve} from 'node:path';
 import {pathToFileURL} from 'node:url';
