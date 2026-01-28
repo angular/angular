6601f06e44189ec06d2651f4c326e8ba058273ce
Jaime Burgos <73321943+SkyZeroZx@users.noreply.github.com>
2026-01-27 19:08:46 -0500

    test(common): enables zoneless change detection in tests
    Adds `provideZonelessChangeDetection` to TestBed configurations in  `ngComponentOutlet` , `ngOptimizedImage` , `ngTemplateOutlet` and `ngPlural`.
    
    (cherry picked from commit 44483563135d173643ed93f738a929d974c7a9ee)

--- packages/common/test/directives/ng_component_outlet_spec.ts
+++ packages/common/test/directives/ng_component_outlet_spec.ts
@@ -9,7 +9,6 @@
 import {CommonModule} from '../../index';
 import {NgComponentOutlet} from '../../src/directives/ng_component_outlet';
 import {
-  Compiler,
   Component,
   ComponentRef,
   createEnvironmentInjector,
@@ -19,7 +18,6 @@ import {
   Injector,
   Input,
   NgModule,
-  NgModuleFactory,
   NO_ERRORS_SCHEMA,
   Optional,
   QueryList,
@@ -34,7 +32,9 @@ import {expect} from '@angular/private/testing/matchers';
 
 describe('insert/remove', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({imports: [TestModule]});
+    TestBed.configureTestingModule({
+      imports: [TestModule],
+    });
   });
 
   it('should do nothing if component is null', waitForAsync(() => {
