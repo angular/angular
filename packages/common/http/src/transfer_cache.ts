cb1163e5e5434d5d96897180f6f0eb86d3992964
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2026-01-31 14:30:03 -0500

    fix(http): correctly parse ArrayBuffer and Blob in transfer cache
    Encodes arraybuffer and blob response bodies as base64 when storing in the transfer cache, ensuring correct retrieval and usage on the client side.
    
    Fixes #66827

--- packages/common/http/src/transfer_cache.ts
+++ packages/common/http/src/transfer_cache.ts
@@ -188,10 +188,10 @@ export function transferCacheInterceptorFn(
 
     switch (responseType) {
       case 'arraybuffer':
-        body = new TextEncoder().encode(undecodedBody).buffer;
+        body = fromBase64(undecodedBody);
         break;
       case 'blob':
-        body = new Blob([undecodedBody]);
+        body = new Blob([fromBase64(undecodedBody)]);
         break;
     }
 
@@ -226,7 +226,10 @@ export function transferCacheInterceptorFn(
         // Only cache successful HTTP responses.
         if (event instanceof HttpResponse) {
           transferState.set<TransferHttpResponse>(storeKey, {
-            [BODY]: event.body,
+            [BODY]:
+              req.responseType === 'arraybuffer' || req.responseType === 'blob'
+                ? toBase64(event.body)
+                : event.body,
             [HEADERS]: getFilteredHeaders(event.headers, headersToInclude),
             [STATUS]: event.status,
             [STATUS_TEXT]: event.statusText,
@@ -311,6 +314,28 @@ function generateHash(value: string): string {
   hash += 2147483647 + 1;
 
   return hash.toString();
+}
+
+function toBase64(buffer: unknown): string {
+  //TODO: replace with when is Baseline widely available
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array/toBase64
+  const bytes = new Uint8Array(buffer as ArrayBufferLike);
+
+  const CHUNK_SIZE = 0x8000; // 32,768 bytes (~32 KB) per chunk, to avoid stack overflow
+
+  let binaryString = '';
+
+  for (let i = 0; i < bytes.length; i += CHUNK_SIZE) {
+    const chunk = bytes.subarray(i, i + CHUNK_SIZE);
+    binaryString += String.fromCharCode.apply(null, chunk as unknown as number[]);
+  }
+  return btoa(binaryString);
+}
+
+function fromBase64(base64: string): ArrayBuffer {
+  const binary = atob(base64);
+  const bytes = Uint8Array.from(binary, (c) => c.charCodeAt(0));
+  return bytes.buffer;
 }
 
 /**
