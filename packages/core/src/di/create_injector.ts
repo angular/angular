b7f5a07bd9d03353d10c25bf8a13a1f76085551d
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2026-02-07 17:15:31 -0500

    refactor(core): guards stringify calls with ngDevMode
    The `stringify` function is only needed for debugging purposes and
    should not be called in production mode.

--- packages/core/src/di/create_injector.ts
+++ packages/core/src/di/create_injector.ts
@@ -47,7 +47,10 @@ export function createInjectorWithoutInjectorInstances(
   scopes = new Set<InjectorScope>(),
 ): R3Injector {
   const providers = [additionalProviders || EMPTY_ARRAY, importProvidersFrom(defType)];
-  name = name || (typeof defType === 'object' ? undefined : stringify(defType));
+  let source: string | undefined = undefined;
+  if (ngDevMode) {
+    source = name || (typeof defType === 'object' ? undefined : stringify(defType));
+  }
 
-  return new R3Injector(providers, parent || getNullInjector(), name || null, scopes);
+  return new R3Injector(providers, parent || getNullInjector(), source || null, scopes);
 }
