b87f8aed0672ebe0cd55d49d782e55adf2f36458
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2026-02-20 17:21:44 -0500

    refactor(common): log a warning when a KeyValuePipe receives a signal
    Add signal warning for `KeyValuePipe` and consolidates the `invalidPipeArgumentError` function into a `utils`

--- packages/common/src/pipes/i18n_select_pipe.ts
+++ packages/common/src/pipes/i18n_select_pipe.ts
@@ -8,7 +8,7 @@
 
 import {Pipe, PipeTransform} from '@angular/core';
 
-import {invalidPipeArgumentError} from './invalid_pipe_argument_error';
+import {invalidPipeArgumentError} from './utils';
 
 /**
  * @ngModule CommonModule
