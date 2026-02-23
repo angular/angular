5ac1c025f9ff0bc8777b3181855a29824577ce97
Shuaib Hasan Akib <akibh805@gmail.com>
2026-02-23 19:27:05 +0600

    refactor(core): remove outdated TODO comments
    Cleans up TODO comments that are no longer planned
    
    Fixes #67191

--- packages/core/primitives/signals/src/signal.ts
+++ packages/core/primitives/signals/src/signal.ts
@@ -108,7 +108,6 @@ export function runPostSignalSetFn<T>(node: SignalNode<T>): void {
 
 // Note: Using an IIFE here to ensure that the spread assignment is not considered
 // a side-effect, ending up preserving `COMPUTED_NODE` and `REACTIVE_NODE`.
-// TODO: remove when https://github.com/evanw/esbuild/issues/3392 is resolved.
 export const SIGNAL_NODE: SignalNode<unknown> = /* @__PURE__ */ (() => {
   return {
     ...REACTIVE_NODE,
