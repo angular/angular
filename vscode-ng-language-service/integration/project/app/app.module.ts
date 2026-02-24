cc64def294cbbae96485a3f9b933c27625a03d9c
kbrilla <criskrzysiu@gmail.com>
2026-02-24 13:39:08 +0100

    test(language-service): add inlay hint regression and integration coverage
    test(language-service): add inlay hint regression and integration coverage

--- vscode-ng-language-service/integration/project/app/app.module.ts
+++ vscode-ng-language-service/integration/project/app/app.module.ts
@@ -4,10 +4,11 @@ import {PostModule} from 'post';
 
 import {AppComponent} from './app.component';
 import {FooComponent} from './foo.component';
+import {HighlightDirective} from './highlight.directive';
 
 @NgModule({
   imports: [CommonModule, PostModule],
-  declarations: [AppComponent, FooComponent],
+  declarations: [AppComponent, FooComponent, HighlightDirective],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
