f1ce2b678f3f33d44e0bc370009807af3c30946e
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2026-02-24 11:07:25 -0500

    feat(router): adds browserUrl input support to router links
    Enables specifying a custom browser URL for router links via a new input,
    allowing navigation to use an explicit browser URL in navigation options.
    
    Closes #66805

--- packages/router/src/directives/router_link.ts
+++ packages/router/src/directives/router_link.ts
@@ -28,6 +28,7 @@ import {
   untracked,
   ɵINTERNAL_APPLICATION_ERROR_HANDLER,
   ɵRuntimeError as RuntimeError,
+  input,
 } from '@angular/core';
 import {Subject} from 'rxjs';
 
@@ -354,6 +355,14 @@ export class RouterLink implements OnChanges, OnDestroy {
   private _replaceUrl = signal<boolean>(false);
 
   /**
+   * Passed to {@link Router#navigateByUrl} as part of the
+   * `NavigationBehaviorOptions`.
+   * @see {@link NavigationBehaviorOptions#browserUrl}
+   * @see {@link Router#navigateByUrl}
+   */
+  browserUrl = input<UrlTree | string | undefined>(undefined);
+
+  /**
    * Whether a host element is an `<a>`/`<area>` tag or a compatible custom
    * element.
    */
@@ -490,11 +499,13 @@ export class RouterLink implements OnChanges, OnDestroy {
       }
     }
 
+    const browserUrl = this.browserUrl();
     const extras = {
       skipLocationChange: this.skipLocationChange,
       replaceUrl: this.replaceUrl,
       state: this.state,
       info: this.info,
+      ...(browserUrl !== undefined && {browserUrl}),
     };
     // navigateByUrl is mocked frequently in tests... Reduce breakages when
     // adding `catch`
