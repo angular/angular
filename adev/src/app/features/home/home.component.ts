957466d02a290e7dc8c737ffcf5e0da220faf367
hawkgs <hawk.dvp@gmail.com>
2026-02-25 13:41:40 +0200

    docs(docs-infra): scroll home features section into view on tab change
    A small UX improvement where the Features section on the home page is scrolled into view when you change the feature tab.

--- adev/src/app/features/home/home.component.ts
+++ adev/src/app/features/home/home.component.ts
@@ -8,14 +8,28 @@
 
 import {Tab, TabContent, TabList, TabPanel, Tabs} from '@angular/aria/tabs';
 import {A11yModule} from '@angular/cdk/a11y';
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {
+  afterRenderEffect,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  inject,
+  signal,
+  viewChild,
+} from '@angular/core';
 import {IS_SEARCH_DIALOG_OPEN, IconComponent, TextField} from '@angular/docs';
 import {ActivatedRoute, RouterLink} from '@angular/router';
-import {CodeHighligher} from './code-highlighting/code-highlighter';
 import {ControlFlowExample} from './components/control-flow/control-flow-example';
 import {DeferrableViewsExample} from './components/deferrable-views-example/deferrable-views-example';
 import {HydrationExample} from './components/hydration-example/hydration-example';
 import {SignalsDemo} from './components/signals-demo/signals-demo';
+
+const FEATURE_TAB = {
+  signals: 'signals',
+  controlFlow: 'control-flow',
+  deferrableViews: 'deferrable-views',
+  hydration: 'hydration',
+} as const;
 
 @Component({
   selector: 'adev-home',
@@ -41,9 +55,26 @@ import {SignalsDemo} from './components/signals-demo/signals-demo';
 export default class Home {
   private readonly activatedRoute = inject(ActivatedRoute);
   protected readonly isUwu = 'uwu' in this.activatedRoute.snapshot.queryParams;
-  private readonly codeHighlighter = inject(CodeHighligher);
 
   protected readonly displaySearchDialog = inject(IS_SEARCH_DIALOG_OPEN);
+
+  protected readonly FEATURE_TAB = FEATURE_TAB;
+  protected readonly selectedFeatureTab = signal<keyof typeof FEATURE_TAB>(FEATURE_TAB.signals);
+  protected readonly featuresSection = viewChild<ElementRef>('featuresSection');
+
+  constructor() {
+    let lastFeatureTab = this.selectedFeatureTab();
+
+    afterRenderEffect({
+      read: () => {
+        const featureTab = this.selectedFeatureTab();
+        if (lastFeatureTab !== featureTab) {
+          this.featuresSection()?.nativeElement.scrollIntoView();
+          lastFeatureTab = featureTab;
+        }
+      },
+    });
+  }
 
   openSearch() {
     this.displaySearchDialog.set(true);
