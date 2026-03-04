d4d96a99cc3d42e18495be8d58389ac9de1bd8c4
Angular Robot <angular-robot@google.com>
2026-03-04 22:16:14 +0000

    build: update cross-repo angular dependencies
    See associated pull request for more information.

--- .github/actions/deploy-docs-site/main.js
+++ .github/actions/deploy-docs-site/main.js
@@ -33495,7 +33495,7 @@ tmp/lib/tmp.js:
   (* v8 ignore next -- @preserve *)
   (* v8 ignore else -- @preserve *)
 
-@angular/ng-dev/bundles/chunk-WTYCKKHM.mjs:
+@angular/ng-dev/bundles/chunk-YN3IWAKJ.mjs:
   (*! Bundled license information:
   
   yargs-parser/build/lib/string-utils.js:
@@ -33536,7 +33536,7 @@ tmp/lib/tmp.js:
      *)
   *)
 
-@angular/ng-dev/bundles/chunk-PELL5AHH.mjs:
+@angular/ng-dev/bundles/chunk-34WCNHIG.mjs:
   (*! Bundled license information:
   
   @octokit/request-error/dist-src/index.js:
