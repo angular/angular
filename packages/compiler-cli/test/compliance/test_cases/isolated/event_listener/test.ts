419944b8000b9865161c9a11afc8a63765c9b08c
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-03-13 12:28:33 +0100

    refactor(compiler-cli): check if generated code compiles in compliance tests
    Updates the compliance tests to check if the generated code compiles.

--- packages/compiler-cli/test/compliance/test_cases/isolated/event_listener/test.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'test-cmp',
-  template: '<button (click)="handleClick($event)"></button>',
-})
-export class TestCmp {
-  handleClick(event: MouseEvent) {}
-}
