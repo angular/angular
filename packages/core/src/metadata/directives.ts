cf3b7fe489f723e045faba5e80e6393be363db29
Matthieu Riegler <kyro38@gmail.com>
2026-03-25 00:37:13 +0100

    refactor(core): remove todo
    This TODO hinted that we also needed to remove a g3 patch.

--- packages/core/src/metadata/directives.ts
+++ packages/core/src/metadata/directives.ts
@@ -668,8 +668,6 @@ export interface Component extends Directive {
  */
 export const Component: ComponentDecorator = makeDecorator(
   'Component',
-  // TODO(jeanmeche): remove the ts-ignore when OnPush is the default
-  // @ts-ignore
   (c: Component = {}) => ({changeDetection: ChangeDetectionStrategy.Eager, ...c}),
   Directive,
   undefined,
