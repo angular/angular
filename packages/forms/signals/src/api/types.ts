44ba7f8dfba2bb14f9a6fb095ee41f3b9d3a43db
Anushka Geeta Singh <anushkageetasingh@gmail.com>
2026-03-27 14:37:28 +0530

    fix(forms): support PromiseLike return in FormSubmitOptions action with test

--- packages/forms/signals/src/api/types.ts
+++ packages/forms/signals/src/api/types.ts
@@ -34,7 +34,7 @@ export interface FormSubmitOptions<TRootModel, TSubmittedModel> {
   action: (
     field: FieldTree<TRootModel & TSubmittedModel>,
     detail: {root: FieldTree<TRootModel>; submitted: FieldTree<TSubmittedModel>},
-  ) => Promise<TreeValidationResult>;
+  ) => TreeValidationResult | PromiseLike<TreeValidationResult>;
   /**
    * Function to run when attempting to submit the form data but validation is failing.
    *
