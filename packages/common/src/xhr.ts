67034ecbd4789902c0f37e08d15f0f3167341cb4
Matthieu Riegler <kyro38@gmail.com>
2026-04-12 13:10:51 +0200

    refactor(http): Make `BrowserXhr`/`XhrFactory` tree-shakable
    3bc095d made the `FetchBackend` the default, we shouldn't load anything Xhr related by default anymroe.

--- packages/common/src/xhr.ts
+++ packages/common/src/xhr.ts
@@ -6,11 +6,24 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
+import {Injectable} from '@angular/core';
+
+/**
+ * A factory for `HttpXhrBackend` that uses the `XMLHttpRequest` browser API.
+ */
+@Injectable({providedIn: 'root'})
+export class BrowserXhr implements XhrFactory {
+  build(): XMLHttpRequest {
+    return new XMLHttpRequest();
+  }
+}
+
 /**
  * A wrapper around the `XMLHttpRequest` constructor.
  *
  * @publicApi
  */
+@Injectable({providedIn: 'root', useExisting: BrowserXhr})
 export abstract class XhrFactory {
   abstract build(): XMLHttpRequest;
 }
