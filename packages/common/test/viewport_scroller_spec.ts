42d57c35781fb65fc4d44df59b6a85287664216a
Matthieu Riegler <kyro38@gmail.com>
2026-04-29 01:02:56 +0200

    refactor(common): fix viewport tests
    10ad3c0 broke the 21.2.x branch

--- packages/common/test/viewport_scroller_spec.ts
+++ packages/common/test/viewport_scroller_spec.ts
@@ -6,8 +6,8 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {BrowserViewportScroller, ViewportScroller} from '../src/viewport_scroller';
 import {isNode, waitFor} from '@angular/private/testing';
+import {BrowserViewportScroller, ViewportScroller} from '../src/viewport_scroller';
 
 describe('BrowserViewportScroller', () => {
   describe('setHistoryScrollRestoration', () => {
