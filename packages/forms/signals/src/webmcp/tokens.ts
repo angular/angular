cf63996c9e9293ee6f4293df2da0abbaf0cd9b2d
Douglas Parker <dgp1130@users.noreply.github.com>
2026-05-04 14:05:20 -0700

    refactor(forms): add `provideExperimentalWebMcpForms`
    This enables the use of the `experimentalWebMcpTool` option on signal forms and implicitly declares a WebMCP tool based on the form data model. This is an experiment inspirted by the WebMCP declarative forms API to see if Angular's framework-level knowledge of the form's declarative data model can produce higher quality WebMCP tools than the web standard can on its own with less effort from the developer.
    
    Example:
    
    ```typescript
    // main.ts
    
    import {bootstrapApplication} from '@angular/platform-browser';
    import {provideExperimentalWebMcpForms} from '@angular/forms';
    import {MyComp} from './form';
    
    bootstrapApplication(MyComp, {
      providers: [
        // Activate the feature.
        provideExperimentalWebMcpForms(),
      ],
    });
    ```
    
    ```typescript
    // form.ts
    
    import {Component, signal} from '@angular/core';
    import {form} from '@angular/forms';
    
    @Component({ /* ... */ })
    export class MyComp {
      private readonly f = form(signal({
        firstName: '',
        lastName: '',
      }), {
        // Implicitly creates a WebMCP tool named `createUser` which accepts a `firstName` and `lastName` as parameters.
        experimentalWebMcpTool: {
          name: 'createUser',
          description: 'Creates a user with the given name.',
        },
    
        // Invokes the submit action when the agent calls the WebMCP tool.
        submission: {
          action: () => {
            console.log('User clicked submit, or agent called the tool!');
          },
        },
      });
    
      // ...
    }
    ```

---
+++ packages/forms/signals/src/webmcp/tokens.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import {InjectionToken} from '@angular/core';
+import {FieldTree} from '../api/types';
+
+/** A function to register a signal form as a WebMCP tool. */
+export const REGISTER_WEBMCP_FORM = new InjectionToken<RegisterWebMcpForm>(
+  typeof ngDevMode !== 'undefined' && ngDevMode ? 'REGISTER_WEBMCP_FORM' : '',
+);
+
+/**
+ * Registers a Signal Form as a WebMCP tool.
+ *
+ * @param formTree The form to register.
+ * @param options Configuration options for the tool.
+ */
+export type RegisterWebMcpForm = (
+  form: FieldTree<unknown>,
+  options: {name: string; description?: string},
+) => void;
