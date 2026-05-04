2b5d59e6372b8e3d132583e4d294267f9c24295f
Hexix23 <cgquintana23@gmail.com>
2026-05-04 23:41:02 +0200

    fix(compiler): disallow i18n on svg animation attributes
    Reject translated SVG animation attributes so localization cannot retarget safe animations to href values.

--- packages/core/test/linker/security_integration_spec.ts
+++ packages/core/test/linker/security_integration_spec.ts
@@ -273,5 +273,27 @@ describe('security integration tests', function () {
         /Translating attribute 'innerHTML' is disallowed for security reasons./,
       );
     });
+
+    it('should throw error on SVG animation retargeting attributes', () => {
+      const template = `
+        <svg>
+          <a href="/safe">
+            <set
+              attributeName="display"
+              to="inline"
+              i18n-attributeName
+              i18n-to
+              begin="0s"
+              fill="freeze">
+            </set>
+          </a>
+        </svg>
+      `;
+      TestBed.overrideComponent(SecuredComponent, {set: {template}});
+
+      expect(() => TestBed.createComponent(SecuredComponent)).toThrowError(
+        /Translating attribute 'attributeName' is disallowed for security reasons./,
+      );
+    });
   });
 });
