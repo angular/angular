61a97f22e8cc18c1a0405d9d880b39d2eb61a012
Alan Agius <alanagius@google.com>
2026-05-19 22:00:32 +0200

    fix(core): support prefix-insensitive DOM schema lookups and compile-time i18n attribute validation
    Updates `DomElementSchemaRegistry` to strip `:svg:` and `:math:` namespace prefixes
    from tag names before querying `SECURITY_SCHEMA` at compile-time. This allows SVG
    and MathML attributes to correctly match their security contexts during compilation.

--- packages/compiler/src/compiler.ts
+++ packages/compiler/src/compiler.ts
@@ -32,7 +32,6 @@ import {publishFacade} from './jit_compiler_facade';
 import * as outputAst from './output/output_ast';
 import {global} from './util';
 
-export {SECURITY_SCHEMA} from './schema/dom_security_schema';
 export {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, SchemaMetadata} from './core';
 export {core};
 
