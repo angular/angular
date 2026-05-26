dc3a544a7479989df18d044e1294c8e4761fe026
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2026-05-26 07:31:25 +0000

    test(core): update spec files to match 20.3.x limits and actual contexts
    Updates spec files to replace modern Eager change detection and remove obsolete ins[cite] expectations.

--- packages/core/test/acceptance/security_spec.ts
+++ packages/core/test/acceptance/security_spec.ts
@@ -907,7 +907,7 @@ describe('SVG <a> link sanitization', () => {
   it('should sanitize dynamic `href` bindings on <svg:a>', () => {
     @Component({
       template: '<svg><a [attr.href]="url"></a></svg>',
-      changeDetection: ChangeDetectionStrategy.Eager,
+      changeDetection: ChangeDetectionStrategy.Default,
     })
     class TestCmp {
       url = 'javascript:alert(1)';
@@ -923,7 +923,7 @@ describe('SVG <a> link sanitization', () => {
   it('should sanitize dynamic `xlink:href` bindings on <svg:a>', () => {
     @Component({
       template: '<svg><a [attr.xlink:href]="url"></a></svg>',
-      changeDetection: ChangeDetectionStrategy.Eager,
+      changeDetection: ChangeDetectionStrategy.Default,
     })
     class TestCmp {
       url = 'javascript:alert(1)';
@@ -944,7 +944,7 @@ describe('SVG <a> link sanitization', () => {
           <a xlink:href="javascript:alert(2)"></a>
         </svg>
       `,
-      changeDetection: ChangeDetectionStrategy.Eager,
+      changeDetection: ChangeDetectionStrategy.Default,
     })
     class TestCmp {}
 
