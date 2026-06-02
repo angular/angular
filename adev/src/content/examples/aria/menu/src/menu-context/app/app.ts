0066a640a2d361ef6e315cafad4cf7a79dab7edf
Cheng-Hsuan Tsai <webcat@google.com>
2026-06-02 21:42:24 +0000

    docs: update Aria Menu focus/hover style and add context menu example
    (cherry picked from commit af62d88bbd3972a207a9106c8a156e3b317f2573)

--- adev/src/content/examples/aria/menu/src/menu-context/app/app.ts
+++ adev/src/content/examples/aria/menu/src/menu-context/app/app.ts
@@ -1,8 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, viewChild, signal} from '@angular/core';
+import {Menu, MenuContent, MenuItem, MenuTrigger} from '@angular/aria/menu';
+import {OverlayModule} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
+  imports: [Menu, MenuContent, MenuItem, MenuTrigger, OverlayModule],
 })
-export class App {}
+export class App {
+  trigger = viewChild.required<MenuTrigger<string>>('trigger');
+  contextMenu = viewChild<Menu<string>>('contextMenu');
+
+  // Position of the context menu
+  menuPosition = signal({x: 0, y: 0});
+
+  onContextMenu(event: MouseEvent) {
+    event.preventDefault();
+
+    // Update position coordinates
+    this.menuPosition.set({x: event.clientX, y: event.clientY});
+
+    // Open the menu via the trigger
+    this.trigger().open();
+  }
+
+  onItemSelected(value: string) {
+    console.log(`Action selected: ${value}`);
+  }
+}
