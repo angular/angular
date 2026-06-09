58291777291952e48478b7694db6addf3a128b56
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-06-09 11:41:40 +0200

    fix(core): update comment for Default change detection
    Updates the comment on `ChangeDetectionStrategy.Default` to mention that it's the same as `Eager`.
    
    Fixes #69253.

--- packages/core/src/change_detection/constants.ts
+++ packages/core/src/change_detection/constants.ts
@@ -34,8 +34,7 @@ export enum ChangeDetectionStrategy {
   Eager = 1,
 
   /**
-   * Use the default `CheckAlways` strategy, in which change detection is automatic until
-   * explicitly deactivated.
+   * This value is equivalent to setting `Eager` and is due to be removed.
    * @deprecated Use `Eager` instead.
    */
   // 3p-only-start
