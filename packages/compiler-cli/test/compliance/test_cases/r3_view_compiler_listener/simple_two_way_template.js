78c7ab392627d87d76df3e28df5cda2449d93a68
Suraj Yadav <surajyr93@gmail.com>
2026-07-28 17:54:48 +0530

    fix(forms): create controls before listeners
    Create form controls before registering template listeners so custom control values are synchronized before user callbacks run. Cover Signal Forms, reactive forms, and template-driven forms.
    
    (cherry picked from commit 6688a3d5502f159479c4e71cbd44f6713a40fe25)

--- packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_listener/simple_two_way_template.js
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_listener/simple_two_way_template.js
@@ -2,12 +2,12 @@ function TestCmp_Template(rf, ctx) {
   if (rf & 1) {
     $r3$.ɵɵtext(0, "Name: ");
     $r3$.ɵɵelementStart(1, "input", 0);
+    $r3$.ɵɵcontrolCreate();
     $r3$.ɵɵtwoWayListener("ngModelChange", function TestCmp_Template_input_ngModelChange_1_listener($event) {
       $r3$.ɵɵtwoWayBindingSet(ctx.name, $event) || (ctx.name = $event);
       return $event;
     });
     $r3$.ɵɵelementEnd();
-    $r3$.ɵɵcontrolCreate();
   } if (rf & 2) {
     $r3$.ɵɵadvance();
     $r3$.ɵɵtwoWayProperty("ngModel", ctx.name);
