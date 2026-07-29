bbbd357bd2b29f0b04578e3fa651418df4aefc67
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-07-29 13:56:21 +0200

    fix(forms): add utility to assert that value is a field tree
    Adds the `isFieldTree` utility that allows users to assert whether a value is a field tree. This is something that has come up on Material recently and will be useful for users as well.
    
    Fixes #69984.
    
    (cherry picked from commit 2a141847a5ea2f24a98d5cc672df4e7abe67ec2d)

---
+++ packages/forms/signals/src/api/symbols.ts
@@ -0,0 +1,17 @@
+/*!
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+/**
+ * Symbol used to retain generic type information when it would otherwise be lost.
+ */
+export declare const ɵɵTYPE: unique symbol;
+
+/**
+ * Symbol used to indicate that a value is a field tree.
+ */
+export const FIELD_TREE: unique symbol = /* @__PURE__ */ Symbol('FIELD_TREE');
