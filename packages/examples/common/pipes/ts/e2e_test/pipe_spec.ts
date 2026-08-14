204265f9a3a754c9082733fb76bb90535cd16ab4
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-08-14 09:15:20 +0200

    build: switch examples away from protractor
    Reworks the `examples` tests not to depend on Protractor.

--- packages/examples/common/pipes/ts/e2e_test/pipe_spec.ts
+++ packages/examples/common/pipes/ts/e2e_test/pipe_spec.ts
@@ -6,108 +6,114 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {$, browser, by, element, ExpectedConditions} from 'protractor';
-
-import {verifyNoBrowserErrors} from '../../../../test-utils';
-
-function waitForElement(selector: string) {
-  const EC = ExpectedConditions;
-  // Waits for the element with id 'abc' to be present on the dom.
-  browser.wait(EC.presenceOf($(selector)), 20000);
-}
+import * as webdriver from 'selenium-webdriver';
+import {createWebDriver, verifyNoBrowserErrors, waitForAngular} from '../../../../test-utils';
 
 describe('pipe', () => {
-  afterEach(verifyNoBrowserErrors);
-  const URL = '/pipes';
+  let driver: webdriver.WebDriver;
+  let baseUrl: string;
+
+  beforeAll(async () => {
+    ({driver, baseUrl} = await createWebDriver());
+  });
+
+  afterAll(async () => {
+    await driver.quit();
+  });
+
+  afterEach(async () => {
+    await verifyNoBrowserErrors(driver);
+  });
+
+  async function loadPage() {
+    await driver.get(`${baseUrl}/pipes`);
+    await waitForAngular(driver);
+  }
 
   describe('async', () => {
-    it('should resolve and display promise', () => {
-      browser.get(URL);
-      waitForElement('async-promise-pipe');
-      expect(element.all(by.css('async-promise-pipe span')).get(0).getText()).toEqual(
-        'Wait for it...',
-      );
-      element(by.css('async-promise-pipe button')).click();
-      expect(element.all(by.css('async-promise-pipe span')).get(0).getText()).toEqual(
-        'Wait for it... hi there!',
-      );
+    it('should resolve and display promise', async () => {
+      await loadPage();
+      const spans = await driver.findElements(webdriver.By.css('async-promise-pipe span'));
+      expect(await spans[0].getText()).toEqual('Wait for it...');
+      const button = await driver.findElement(webdriver.By.css('async-promise-pipe button'));
+      await button.click();
+      await waitForAngular(driver);
+      const updatedSpans = await driver.findElements(webdriver.By.css('async-promise-pipe span'));
+      expect(await updatedSpans[0].getText()).toEqual('Wait for it... hi there!');
     });
   });
 
   describe('lowercase/uppercase', () => {
-    it('should work properly', () => {
-      browser.get(URL);
-      waitForElement('lowerupper-pipe');
-      element(by.css('lowerupper-pipe input')).sendKeys('Hello World!');
-      expect(element.all(by.css('lowerupper-pipe pre')).get(0).getText()).toEqual("'hello world!'");
-      expect(element.all(by.css('lowerupper-pipe pre')).get(1).getText()).toEqual("'HELLO WORLD!'");
+    it('should work properly', async () => {
+      await loadPage();
+      const input = await driver.findElement(webdriver.By.css('lowerupper-pipe input'));
+      await input.sendKeys('Hello World!');
+      await waitForAngular(driver);
+      const pres = await driver.findElements(webdriver.By.css('lowerupper-pipe pre'));
+      expect(await pres[0].getText()).toEqual("'hello world!'");
+      expect(await pres[1].getText()).toEqual("'HELLO WORLD!'");
     });
   });
 
   describe('titlecase', () => {
-    it('should work properly', () => {
-      browser.get(URL);
-      waitForElement('titlecase-pipe');
-      expect(element.all(by.css('titlecase-pipe p')).get(0).getText()).toEqual('Some String');
-      expect(element.all(by.css('titlecase-pipe p')).get(1).getText()).toEqual(
-        'This Is Mixed Case',
-      );
-      expect(element.all(by.css('titlecase-pipe p')).get(2).getText()).toEqual(
-        "It's Non-trivial Question",
-      );
-      expect(element.all(by.css('titlecase-pipe p')).get(3).getText()).toEqual('One,two,three');
-      expect(element.all(by.css('titlecase-pipe p')).get(4).getText()).toEqual('True|false');
-      expect(element.all(by.css('titlecase-pipe p')).get(5).getText()).toEqual('Foo-vs-bar');
+    it('should work properly', async () => {
+      await loadPage();
+      const ps = await driver.findElements(webdriver.By.css('titlecase-pipe p'));
+      expect(await ps[0].getText()).toEqual('Some String');
+      expect(await ps[1].getText()).toEqual('This Is Mixed Case');
+      expect(await ps[2].getText()).toEqual("It's Non-trivial Question");
+      expect(await ps[3].getText()).toEqual('One,two,three');
+      expect(await ps[4].getText()).toEqual('True|false');
+      expect(await ps[5].getText()).toEqual('Foo-vs-bar');
     });
   });
 
   describe('keyvalue', () => {
-    it('should work properly', () => {
-      browser.get(URL);
-      waitForElement('keyvalue-pipe');
-      expect(element.all(by.css('keyvalue-pipe div')).get(0).getText()).toEqual('1:bar');
-      expect(element.all(by.css('keyvalue-pipe div')).get(1).getText()).toEqual('2:foo');
-      expect(element.all(by.css('keyvalue-pipe div')).get(2).getText()).toEqual('1:bar');
-      expect(element.all(by.css('keyvalue-pipe div')).get(3).getText()).toEqual('2:foo');
+    it('should work properly', async () => {
+      await loadPage();
+      const divs = await driver.findElements(webdriver.By.css('keyvalue-pipe div'));
+      expect(await divs[0].getText()).toEqual('1:bar');
+      expect(await divs[1].getText()).toEqual('2:foo');
+      expect(await divs[2].getText()).toEqual('1:bar');
+      expect(await divs[3].getText()).toEqual('2:foo');
     });
   });
 
   describe('number', () => {
-    it('should work properly', () => {
-      browser.get(URL);
-      waitForElement('number-pipe');
-      const examples = element.all(by.css('number-pipe p'));
-      expect(examples.get(0).getText()).toEqual('No specified formatting: 3.142');
-      expect(examples.get(1).getText()).toEqual('With digitsInfo parameter specified: 0,003.14159');
-      expect(examples.get(2).getText()).toEqual(
+    it('should work properly', async () => {
+      await loadPage();
+      const examples = await driver.findElements(webdriver.By.css('number-pipe p'));
+      expect(await examples[0].getText()).toEqual('No specified formatting: 3.142');
+      expect(await examples[1].getText()).toEqual(
+        'With digitsInfo parameter specified: 0,003.14159',
+      );
+      expect(await examples[2].getText()).toEqual(
         'With digitsInfo and locale parameters specified: 0\u202f003,14159',
       );
     });
   });
 
   describe('percent', () => {
-    it('should work properly', () => {
-      browser.get(URL);
-      waitForElement('percent-pipe');
-      const examples = element.all(by.css('percent-pipe p'));
-      expect(examples.get(0).getText()).toEqual('A: 26%');
-      expect(examples.get(1).getText()).toEqual('B: 0,134.950%');
-      expect(examples.get(2).getText()).toEqual('B: 0\u202f134,950 %');
+    it('should work properly', async () => {
+      await loadPage();
+      const examples = await driver.findElements(webdriver.By.css('percent-pipe p'));
+      expect(await examples[0].getText()).toEqual('A: 26%');
+      expect(await examples[1].getText()).toEqual('B: 0,134.950%');
+      expect(await examples[2].getText()).toEqual('B: 0\u202f134,950 %');
     });
   });
 
   describe('currency', () => {
-    it('should work properly', () => {
-      browser.get(URL);
-      waitForElement('currency-pipe');
-      const examples = element.all(by.css('currency-pipe p'));
-      expect(examples.get(0).getText()).toEqual('A: $0.26');
-      expect(examples.get(1).getText()).toEqual('A: CA$0.26');
-      expect(examples.get(2).getText()).toEqual('A: CAD0.26');
-      expect(examples.get(3).getText()).toEqual('B: CA$0,001.35');
-      expect(examples.get(4).getText()).toEqual('B: $0,001.35');
-      expect(examples.get(5).getText()).toEqual('B: 0\u202f001,35 $CA');
-      expect(examples.get(6).getText()).toEqual('B: CLP1');
+    it('should work properly', async () => {
+      await loadPage();
+      const examples = await driver.findElements(webdriver.By.css('currency-pipe p'));
+      expect(await examples[0].getText()).toEqual('A: $0.26');
+      expect(await examples[1].getText()).toEqual('A: CA$0.26');
+      expect(await examples[2].getText()).toEqual('A: CAD0.26');
+      expect(await examples[3].getText()).toEqual('B: CA$0,001.35');
+      expect(await examples[4].getText()).toEqual('B: $0,001.35');
+      expect(await examples[5].getText()).toEqual('B: 0\u202f001,35 $CA');
+      expect(await examples[6].getText()).toEqual('B: CLP1');
     });
   });
 });
--- packages/examples/core/di/ts/contentChild/e2e_test/content_child_spec.ts
+++ packages/examples/core/di/ts/contentChild/e2e_test/content_child_spec.ts
@@ -6,26 +6,38 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {browser, by, element, ElementFinder} from 'protractor';
-
-import {verifyNoBrowserErrors} from '../../../../../test-utils';
+import * as webdriver from 'selenium-webdriver';
+import {createWebDriver, verifyNoBrowserErrors, waitForAngular} from '../../../../../test-utils';
 
 describe('contentChild example', () => {
-  afterEach(verifyNoBrowserErrors);
-  let button: ElementFinder;
-  let result: ElementFinder;
+  let driver: webdriver.WebDriver;
+  let baseUrl: string;
 
-  beforeEach(() => {
-    browser.get('/di/contentChild');
-    button = element(by.css('button'));
-    result = element(by.css('div'));
+  beforeAll(async () => {
+    ({driver, baseUrl} = await createWebDriver());
   });
 
-  it('should query content child', () => {
-    expect(result.getText()).toEqual('pane: 1');
+  afterAll(async () => {
+    await driver.quit();
+  });
 
-    button.click();
+  afterEach(async () => {
+    await verifyNoBrowserErrors(driver);
+  });
 
-    expect(result.getText()).toEqual('pane: 2');
+  beforeEach(async () => {
+    await driver.get(`${baseUrl}/di/contentChild`);
+    await waitForAngular(driver);
+  });
+
+  it('should query content child', async () => {
+    const result = await driver.findElement(webdriver.By.css('div'));
+    expect(await result.getText()).toEqual('pane: 1');
+
+    const button = await driver.findElement(webdriver.By.css('button'));
+    await button.click();
+    await waitForAngular(driver);
+
+    expect(await result.getText()).toEqual('pane: 2');
   });
 });
