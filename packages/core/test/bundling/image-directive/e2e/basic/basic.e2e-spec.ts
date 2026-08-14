413c79c10b51f0a90120a1561e022518c6c807a5
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-08-14 09:13:11 +0200

    build: switch core tests away from protractor
    Reworks the tests under `core` not to depend on Protractor.

--- packages/core/test/bundling/image-directive/e2e/basic/basic.e2e-spec.ts
+++ packages/core/test/bundling/image-directive/e2e/basic/basic.e2e-spec.ts
@@ -6,21 +6,30 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {browser, by, element} from 'protractor';
-import {logging} from 'selenium-webdriver';
+import * as webdriver from 'selenium-webdriver';
+import {collectBrowserLogs, createWebDriver} from '../browser-logs-util';
 
-import {collectBrowserLogs} from '../browser-logs-util';
+describe('NgOptimizedImage directive (basic)', () => {
+  let driver: webdriver.WebDriver;
+  let baseUrl: string;
 
-describe('NgOptimizedImage directive', () => {
+  beforeAll(async () => {
+    ({driver, baseUrl} = await createWebDriver());
+  });
+
+  afterAll(async () => {
+    await driver.quit();
+  });
+
   it('should render an image with an updated `src`', async () => {
-    await browser.get('/e2e/basic');
-    const imgs = element.all(by.css('img'));
-    const src = await imgs.get(0).getAttribute('src');
+    await driver.get(`${baseUrl}/e2e/basic`);
+    const imgs = await driver.findElements(webdriver.By.css('img'));
+    const src = await imgs[0].getAttribute('src');
     expect(/angular\.svg/.test(src)).toBe(true);
 
     // Since there are no preconnect tags on a page,
     // we expect a log in a console that mentions that.
-    const logs = await collectBrowserLogs(logging.Level.WARNING);
+    const logs = await collectBrowserLogs(driver, webdriver.logging.Level.WARNING);
     expect(logs.length).toEqual(1);
 
     // Verify that the error code and a raw image src are present.
