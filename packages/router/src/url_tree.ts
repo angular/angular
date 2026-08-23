5dfe753bfc3adf86da09a523e63478c63721daf4
splincode <omaxphp@yandex.ru>
2026-08-23 13:36:07 +0300

    refactor(router): replace internal any types
    Infer URL parameter values as strings and type preloading work as Observable<void> to match the values produced by these internal flows.
    
    (cherry picked from commit f275d289a0a999e91730c8e22553d771b5ab3fd5)

--- packages/router/src/url_tree.ts
+++ packages/router/src/url_tree.ts
@@ -714,7 +714,7 @@ class UrlParser {
       return;
     }
     this.capture(key);
-    let value: any = '';
+    let value = '';
     if (this.consumeOptional('=')) {
       const valueMatch = matchSegments(this.remaining);
       if (valueMatch) {
@@ -733,7 +733,7 @@ class UrlParser {
       return;
     }
     this.capture(key);
-    let value: any = '';
+    let value = '';
     if (this.consumeOptional('=')) {
       const valueMatch = matchUrlQueryParamValue(this.remaining);
       if (valueMatch) {
