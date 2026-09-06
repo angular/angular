60cedbc701d775d7ebfc483d7373651a0f1492e6
JaYWei <wzy398848627@gmail.com>
2026-09-07 01:45:14 +1200

    fix(core): clean up old styles in HMR if REMOVE_STYLES_ON_COMPONENT_DESTROY is false

--- packages/platform-browser/src/dom/dom_renderer.ts
+++ packages/platform-browser/src/dom/dom_renderer.ts
@@ -296,6 +296,8 @@ export class DomRendererFactory2 implements RendererFactory2, OnDestroy {
    * @param componentId ID of the component that is being replaced.
    */
   protected componentReplaced(componentId: string) {
+    const renderer = this.rendererByCompId.get(componentId);
+    renderer?.removeStylesHMR();
     this.rendererByCompId.delete(componentId);
   }
 }
@@ -680,6 +682,15 @@ class NoneEncapsulationDomRenderer extends DefaultDomRenderer2 {
     if (!this.removeStylesOnCompDestroy) {
       return;
     }
+    this.removeStyles();
+  }
+
+  removeStylesHMR(): void {
+    if (!this.removeStylesOnCompDestroy) {
+      this.removeStyles();
+    }
+  }
+  removeStyles(): void {
     if (allLeavingAnimations.size === 0) {
       this.sharedStylesHost.removeStyles(this.styles, this.styleUrls);
     }
