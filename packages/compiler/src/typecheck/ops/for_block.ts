37ca679192e0a45cb7fa2e3f40ed68689298649e
Andrew Scott <atscott01@gmail.com>
2026-09-11 09:14:25 -0700

    Revert "fix(compiler): wrap `@for` collection expression before appending non-null assertion"
    This reverts commit ed8f16c078d83215cbdda2ca5017f70a291561b9.
    
    This caused a breakage in TGP because the changes (correctly) produced new
    diagnostics on ` @for (a of (x | async) || []; track a) {` x is `any`. This
    results in `<any> | async` which produces `unknown | null` and then the `@if`
    finally narrows this down to `{}` which isn't iteratable.

--- packages/compiler/src/typecheck/ops/for_block.ts
+++ packages/compiler/src/typecheck/ops/for_block.ts
@@ -45,9 +45,8 @@ export class TcbForOfOp extends TcbOp {
 
     // It's common to have a for loop over a nullable value (e.g. produced by the `async` pipe).
     // Add a non-null expression to allow such values to be assigned.
-    const expr = tcbExpression(this.block.expression, this.tcb, this.scope).wrapForTypeChecker();
-    const expression = new TcbExpr(`${expr.print()}!`).addParseSpanInfo(
-      this.block.expression.sourceSpan,
+    const expression = new TcbExpr(
+      `${tcbExpression(this.block.expression, this.tcb, this.scope).print()}!`,
     );
 
     let statements: TcbExpr[];
